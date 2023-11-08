01f1a2d2e073602b7e4e36cb87c3e5c38e60a649
ebang <dskhe8@gmail.com>
2023-11-08 21:29:09 +0900

    refactor(lotto) :Game 클래스 리팩토링

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -9,6 +9,23 @@ public class Game {
     private Lotto winningLotto;
     private int bonusNumber;
 
+
+    public int getBonusNmber() {
+        return this.bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return this.winningLotto;
+    }
+
+    public void setWinningLotto(List<Integer> numbers) {
+        this.winningLotto = new Lotto(numbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
     public void askWinningNumbers() {
         while (true) {
             try {
@@ -25,35 +42,18 @@ public class Game {
         }
     }
 
-    public void askBonusNumber(){
-        while(true){
-
-            try{
+    public void askBonusNumber() {
+        while (true) {
+            try {
                 String input = InputService.askBonusNumber();
                 InputValidator.validateBlankString(input);
                 int bonusNumber = Integer.parseInt(input);
                 this.setBonusNumber(bonusNumber);
-            }
-            catch(Exception e){
+                break;
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    public void setWinningLotto(List<Integer> numbers) {
-        this.winningLotto = new Lotto(numbers);
-    }
-
-    public void setBonusNumber(int bonusNumber) {
-        this.bonusNumber = bonusNumber;
-    }
-
-    public int getBonusNmber(){
-        return this.bonusNumber;
-    }
-
-    public Lotto getWinningLotto() {
-        return this.winningLotto;
     }
 
 

2f2958d5472a3f37af0cf9ab2b4ec2d2792860f7
gkwk <124476558+gkwk@users.noreply.github.com>
2023-11-08 13:51:34 +0900

    feat : bonus 숫자를 반영하도록 추첨기 코드 수정

--- src/main/java/lotto/NumberComparing.java
+++ src/main/java/lotto/NumberComparing.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+import lotto.LottoEnum.Prize;
+
 public class NumberComparing {
     private final int SIZEOFLOTTONUMBERS = 6;
 
     private final List<Integer> winningNumbers;
+    private final int bonusWinningNumber;
 
-    public NumberComparing(List<Integer> winningNumbers) {
+    public NumberComparing(List<Integer> winningNumbers, int bonusWinningNumber) {
         this.winningNumbers = winningNumbers;
+        this.bonusWinningNumber = bonusWinningNumber;
     }
 
     public int getNumberOfMatch(Lotto lotto) {
@@ -24,11 +28,38 @@ public class NumberComparing {
         return result;
     }
 
-    public List<Integer> getNumberOfMatchResults(List<Lotto> lottos) {
-        List<Integer> results = new ArrayList<>();
+    public Boolean isMatchBonusWinningNumber(Lotto lotto) {
+        HashSet<Integer> lottoNumberBundle = new HashSet<>(lotto.getNumbers());
+
+        Boolean result = lottoNumberBundle.contains(bonusWinningNumber);
+
+        return result;
+    }
+
+    public Prize getPrizeResult(int numberOfMatch, Boolean isMatchBonusWinningNumber) {
+        if (numberOfMatch == 6) {
+            return Prize.FIRST;
+        } else if (numberOfMatch == 5 && isMatchBonusWinningNumber) {
+            return Prize.SECOND;
+        } else if (numberOfMatch == 5) {
+            return Prize.THIRD;
+        } else if (numberOfMatch == 4) {
+            return Prize.FOURTH;
+        } else if (numberOfMatch == 3) {
+            return Prize.FIFTH;
+        }
+
+        return Prize.LOSE;
+    }
+
+    public List<Prize> getPrizeResults(List<Lotto> lottos) {
+        List<Prize> results = new ArrayList<>();
 
         for (Lotto lotto : lottos) {
-            results.add(getNumberOfMatch(lotto));
+            int numberOfMatch = getNumberOfMatch(lotto);
+            Boolean isMatchBonusWinningNumber = isMatchBonusWinningNumber(lotto);
+
+            results.add(getPrizeResult(numberOfMatch, isMatchBonusWinningNumber));
         }
 
         return results;

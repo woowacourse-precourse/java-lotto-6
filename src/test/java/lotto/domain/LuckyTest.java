d596862366593131a39dc9567daa542bb85c465b
Dowon <3120kang@gmail.com>
2023-11-08 21:17:13 +0900

    Test(Lucky): 예외 테스트 코드 추가 작성

--- src/test/java/lotto/domain/LuckyTest.java
+++ src/test/java/lotto/domain/LuckyTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LuckyTest {
 
@@ -30,5 +33,33 @@ class LuckyTest {
                 Arguments.arguments(Arrays.asList(1, 2, 3, 4, 8, 9), Rank.FOURTH),
                 Arguments.arguments(Arrays.asList(1, 2, 3, 11, 12, 13), Rank.FIFTH)
         );
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLuckyByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lucky(List.of(1, 2, 3, 4, 5, 5),7))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 보너스 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLuckyByDuplicatedNumberWithBonus() {
+        assertThatThrownBy(() -> new Lucky(List.of(1, 2, 3, 4, 5, 6),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 1미만 45초과의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLuckyByRange() {
+        assertThatThrownBy(() -> new Lucky(List.of(1, 2, 3, 4, 5, 46),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호에 1미만 45초과의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createBonusByRange() {
+        assertThatThrownBy(() -> new Lucky(List.of(1, 2, 3, 4, 5, 6),47))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }

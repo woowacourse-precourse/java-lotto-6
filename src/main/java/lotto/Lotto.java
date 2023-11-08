c25e1c376104439a42136c2ebee3c78f7df1f2ff
권태현 <kygkth2011@gmail.com>
2023-11-08 20:58:44 +0900

    feat(Lotto) : Lotto 입력에 대한 예외처리 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,40 @@
 package lotto;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Lotto {
+    static final int SMALLEST_LOTTO_NUMBER = 1;
+    static final int BIGGEST_LOTTO_NUMBER = 45;
+    static final int LOTTO_NUMBER_COUNT = 6;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        countValidate(numbers);
+        duplicateValidate(numbers);
+        inRangeValidate(numbers);
+        numbers.sort(Comparator.naturalOrder());
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void countValidate(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개가 입력되어야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void duplicateValidate(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private void inRangeValidate(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < SMALLEST_LOTTO_NUMBER || number > BIGGEST_LOTTO_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 1~45의 숫자여야 합니다.");
+            }
+        }
+    }
 }

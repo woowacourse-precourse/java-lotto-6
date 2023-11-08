70cc9eb3a0d24a2b33ede3ef83d5d2e43ef877f3
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 22:09:59 +0900

    refactor:"에러메세지 추가"

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -5,7 +5,7 @@ public enum ErrorMessage {
     NON_NUMERIC_ERROR("[ERROR] 숫자만 입력해주세요."),
     INTEGER_FORMAT_ERROR("[ERROR] 정수만 입력해주세요."),
     NUMBER_RANGE_ERROR("[ERROR] 유효한 범위의 숫자만 입력해주세요."),
-    MONEY_UNIT_ERROR("[ERROR] " + LottoInformation.MONEY_UNIT.getNumber() + "원 단위로 입력해주세요."),
+    MONEY_UNIT_ERROR("[ERROR] " + LottoGameConfig.LOTTO_TICKET_PRICE_UNIT.getNumber() + "원 단위로 입력해주세요."),
     LOTTO_SIZE_ERROR("[ERROR] 6개의 번호를 입력해주세요."),
     LOTTO_NUMBER_RANGE_ERROR("[ERROR] 1에서 45 사이의 숫자로 입력해주세요."),
     DUPLICATE_LOTTO_NUMBER_ERROR("[ERROR] 중복된 숫자는 허용되지 않습니다.");

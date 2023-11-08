0dc34702469677de429bc8f31735a5a01e8160d4
7ngenious <zxvs1218@gmail.com>
2023-11-08 21:46:28 +0900

    refactor: Split Method

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,11 +1,12 @@
 package lotto.Controller;
 
+import lotto.Domain.EntireMessage;
 import lotto.Domain.Lotto;
 import lotto.Domain.Prize;
 import lotto.View.LottoInputView;
 import lotto.View.LottoOutputView;
+import lotto.Util.InputValidation;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ public class LottoController {
     private LottoInputView inputView;
     private LottoOutputView outputView;
 
-    public LottoController(LottoInputView inputView, LottoOutputView outputView){
+    public LottoController(LottoInputView inputView, LottoOutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
     }
@@ -25,7 +26,7 @@ public class LottoController {
         List<Lotto> purchasedTickets = generateTickets(ticketCount);
         outputView.displayPurchasedTickets(purchasedTickets);
 
-        List<Integer> winningNumbers = inputView.getWinningNumbers();
+        List<Integer> winningNumbers = InputValidation.getWinningNumbers();
         int bonusNumber = inputView.getBonusNumber();
         List<String> results = calculatePrizes(purchasedTickets, winningNumbers, bonusNumber);
         outputView.displayResults(results);
@@ -44,34 +45,50 @@ public class LottoController {
         List<String> results = new ArrayList<>();
         int[] prizeCounts = new int[8];
 
+        calculatePrizeCounts(purchasedTickets, winningNumbers, bonusNumber, prizeCounts);
+
+        results.add(EntireMessage.WINNING_STATISTICS_MSG);
+        results.add(EntireMessage.DASH);
+
+        Prize[] prizeLevels = Prize.values();
+
+        generatePrizeMessages(prizeCounts, prizeLevels, results);
+
+        double profitRate = calculateProfitRate(purchasedTickets.size(), prizeCounts);
+        results.add(EntireMessage.STATISTICS_PREFIX_MSG + String.format("%.1f", profitRate) + EntireMessage.STATISTICS_SUFFIX_MSG);
+        return results;
+    }
+
+    private void calculatePrizeCounts(List<Lotto> purchasedTickets, List<Integer> winningNumbers, int bonusNumber, int[] prizeCounts) {
         for (Lotto lotto : purchasedTickets) {
             int matchingNumbers = countMatchingNumbers(lotto.getNumbers(), winningNumbers);
             boolean hasBonusNumber = lotto.getNumbers().contains(bonusNumber);
             prizeCounts[matchingNumbers]++;
-
-            if (matchingNumbers == 5 && hasBonusNumber) {
-                prizeCounts[5]++;
-            } else if (matchingNumbers == 6) {
-                prizeCounts[6]++;
-            }
+            updatePrizeCounts(prizeCounts, matchingNumbers, hasBonusNumber);
         }
-
-        results.add("당첨 통계");
-        results.add("---");
-
-        Prize[] prizeLevels = Prize.values();
-
+    }
+    private void updatePrizeCounts(int[] prizeCounts, int matchingNumbers, boolean hasBonusNumber) {
+        if (matchingNumbers == 5 && hasBonusNumber) {
+            prizeCounts[5]++;
+        } else if (matchingNumbers == 6) {
+            prizeCounts[6]++;
+        }
+    }
+    private void generatePrizeMessages(int[] prizeCounts, Prize[] prizeLevels, List<String> results) {
         for (int i = 3; i <= 7; i++) {
             int count = prizeCounts[i];
-            String message = prizeLevels[i - 3].getMessage() + count + "개";
+            String message = generatePrizeMessage(prizeLevels[i - 3], count);
             results.add(message);
         }
-
-        double profitRate = ((calculateTotalPrize(prizeCounts) / (purchasedTickets.size() * 1000.0)) * 100);
-        results.add("총 수익률은 " + String.format("%.1f", profitRate) + "%입니다.");
-        return results;
     }
 
+    private String generatePrizeMessage(Prize prizeLevel, int count) {
+        return prizeLevel.getMessage() + count + EntireMessage.COUNT_SUFFIX;
+    }
+
+    private double calculateProfitRate(int ticketCount, int[] prizeCounts) {
+        return ((calculateTotalPrize(prizeCounts) / (ticketCount * 1000.0)) * 100);
+    }
     private int countMatchingNumbers(List<Integer> lottoNumbers, List<Integer> winningNumbers) {
         int count = 0;
         for (int number : lottoNumbers) {
@@ -81,16 +98,6 @@ public class LottoController {
         }
         return count;
     }
-
-    private int getPrizeAmount(int matchingNumbers) {
-        int[] prizeAmounts = {0, 0, 0, 5000, 50000, 1500000, 30000000};
-        if (matchingNumbers >= 0 && matchingNumbers <= 6) {
-            return prizeAmounts[matchingNumbers];
-        } else {
-            return 0; // 유효하지 않은 인덱스에 대한 처리
-        }
-    }
-
     private int calculateTotalPrize(int[] prizeCounts) {
         int totalPrize = 0;
         for (int i = 3; i < prizeCounts.length; i++) {
@@ -98,5 +105,8 @@ public class LottoController {
         }
         return totalPrize;
     }
-
+    private int getPrizeAmount(int matchingNumbers) {
+        int[] prizeAmounts = {0, 0, 0, 5000, 50000, 1500000, 30000000};
+        return (matchingNumbers >= 0 && matchingNumbers <= 6) ? prizeAmounts[matchingNumbers] : 0;
+    }
 }

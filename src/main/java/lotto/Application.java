ee0b8190c9eaeaf3dee944109679643d9e7445f3
SongJIH <wlgus7592@gmail.com>
2023-11-08 13:36:39 +0900

    main

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.ArrayList;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) {    	
         // TODO: 프로그램 구현
+    	Function function= new Function();
+    	int money= function.inputMoney();
+    	ArrayList<Integer>[] publishedNumbers= function.publishNumbers(money/1000);
+    	
+    	ArrayList<Integer> winningNumber= function.inputWinningNumber();
+    	int bonusNumver= function.bonusNumber();
+    	int[] matched= function.matchNumbers(publishedNumbers, winningNumber, bonusNumver);
+    	function.showWinningDetails(matched);
+    	function.showRateOfReturn(matched, money);
+    	
     }
 }

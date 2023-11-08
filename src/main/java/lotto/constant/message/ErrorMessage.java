e7e49631b36b47bf289a9471a6df097ee2853f92
saebyeok2 <tomato8269@naver.com>
2023-11-08 22:18:32 +0900

    feat: 보너스 번호를 검증하는 기능 구현
    - 보너스 번호가 당첨 번호와 중복되는 경우
    - 보너스 번호가 1~45 사이의 수를 벗어나는 경우

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -4,5 +4,8 @@ public class ErrorMessage {
     public static String MONEY_UNIT_ERROR = "[ERROR] 1000원 단위로 입력이 가능합니다.";
     public static String MONEY_MINIMUM_ERROR = "[ERROR] 최소 1000원 이상 입력해야 합니다.";
     public static String INT_FORMAT_ERROR = "[ERROR] 입력값이 정수 형태가 아닙니다.";
+    public static String LOTTO_NUMBER_RANGE_ERROR = "[ERROR] 1부터 45 사이의 수만 입력 가능합니다.";
+    public static String BONUS_NUMBER_DUPLICATION_ERROR = "[ERROR] 보너스 번호는 로또 번호와 중복될 수 없습니다.";
+
 
 }
--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,10 +1,33 @@
 package lotto.domain;
 
+import static lotto.constant.LottoConstant.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoConstant.MIN_LOTTO_NUMBER;
+import static lotto.constant.message.ErrorMessage.BONUS_NUMBER_DUPLICATION_ERROR;
+import static lotto.constant.message.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR;
+
 public class WinningLotto extends Lotto {
     private int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
         super(lotto.getNumbers());
+        validateBonusNumber(lotto, bonusNumber);
         this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto lotto, int bonusNumber) throws IllegalArgumentException {
+        validateNumberBoundary(bonusNumber);
+        validateDuplication(lotto, bonusNumber);
+    }
+
+    private void validateNumberBoundary(int bonusNumber) throws IllegalArgumentException {
+        if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR);
+        }
+    }
+
+    private void validateDuplication(Lotto lotto, int bonusNumber) throws IllegalArgumentException {
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_DUPLICATION_ERROR);
+        }
     }
 }

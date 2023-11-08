1d7311341cc8017f5f4994e39e761567ea3a600a
ulsandonghun <donghoon141@naver.com>
2023-11-08 21:13:56 +0900

    [feat] : 로또 구입금액을 이용해 로또 발행 갯수를 구하는 기능

--- src/main/java/lotto/Domain/Lotto.java
+++ src/main/java/lotto/Domain/Lotto.java
@@ -1,9 +1,10 @@
-package lotto;
+package lotto.Domain;
 
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    public int lottosize;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,6 +15,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public void generateLottoSize(String price) {
+        Integer priceint = Integer.valueOf(price);
+        lottosize=priceint/1000;
     }
 
     // TODO: 추가 기능 구현
--- src/test/java/lotto/Domain/LottoTest.java
+++ src/test/java/lotto/Domain/LottoTest.java
@@ -1,5 +1,9 @@
-package lotto;
+package lotto.Domain;
 
+import lotto.Domain.Lotto;
+import lotto.service.InputService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +12,16 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    Lotto lotto ;
+    InputService inputService;
+
+    @BeforeEach
+    @Test
+    void init() {
+        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        inputService=new InputService();
+    }
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
@@ -24,4 +38,12 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 구입금액 을 이용해 로또 발행 갯수를 구하는 기능.")
+    @Test
+    void lotto3() {
+        String price="14000";
+        lotto.generateLottoSize(price);
+        Assertions.assertThat(lotto.lottosize).isEqualTo(14);
+    }
 }

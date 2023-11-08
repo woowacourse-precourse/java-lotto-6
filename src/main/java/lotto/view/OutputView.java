60e20f16790f8d4816a30db19efef6ac607683b7
JoonKyo <joonkyolee@gmail.com>
2023-11-08 17:36:59 +0900

    docs, feat(OutputView): 로또 당첨 결과를 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import static lotto.view.OutputMessage.*;
+import static lotto.domain.LottoRank.*;
+import static lotto.constant.LottoResultIndex.*;
 
 import java.util.List;
 import lotto.domain.Lotto;
@@ -37,6 +39,45 @@ public class OutputView {
         }
     }
 
+    public void printLottoResult(int[] rankResult, String lottoYield) {
+        printNewLine();
+        printLottoResultIntroduction();
+        printLottoPrize(rankResult);
+        printYieldOfLottoResult(lottoYield);
+    }
+
+    private void printLottoResultIntroduction() {
+        System.out.println(SHOW_LOTTO_STATISTIC_INTRODUCTION.getMessage());
+        System.out.println(SHOW_LOTTO_SEPARATOR_LINE.getMessage());
+    }
+
+    private void printLottoPrize(int[] rankResult) {
+        for (int matchedNumber = FIFTH_RANK_MATCH_INDEX.getIndex();
+             matchedNumber <= FIRST_RANK_MATCH_INDEX.getIndex(); matchedNumber++) {
+            printMatchedLotto(rankResult, matchedNumber);
+        }
+    }
+
+    private void printMatchedLotto(int[] rankResult, int matchedNumber) {
+        System.out.printf(SHOW_LOTTO_PRIZE.getMessage(),
+                matchedNumber, getLottoRank(matchedNumber).getWinningAmount(), rankResult[matchedNumber]);
+        printNewLine();
+
+        if (matchedNumber == THIRD_RANK.getMatchNumbers()) {
+            printMatchedLottoWithBonus(rankResult, matchedNumber);
+        }
+    }
+
+    private void printMatchedLottoWithBonus(int[] rankResult, int matchedNumber) {
+        System.out.printf(SHOW_LOTTO_PRIZE_WITH_BONUS.getMessage(),
+                matchedNumber, SECOND_RANK.getWinningAmount(), rankResult[SECOND_RANK_INDEX.getIndex()]);
+        printNewLine();
+    }
+
+    private void printYieldOfLottoResult(String lottoYield) {
+        System.out.printf(SHOW_LOTTO_RESULT_YIELD.getMessage(), lottoYield);
+        printNewLine();
+    }
 
     public void printNewLine() {
         System.out.println();

cd8a91b9dd6eabc3244a373256746101a89a2987
YuJeongYun <yunyj99@naver.com>
2023-11-08 20:16:18 +0900

    test: Lotto 클래스 테스트 코드 추가
    * 사이즈 예외 발생 확인
    * 중복값 존재 예외 발생 확인
    * 정상 생성 확인

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import lotto.constant.ErrorMessage;
 import lotto.constant.LottoValue;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;

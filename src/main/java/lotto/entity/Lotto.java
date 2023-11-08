47d0c6f8239a17f8e4d4d1f39cb5db485d417030
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 21:04:22 +0900

    Feat : validator에 예외사항 추가 및 getter 추가 패키지 위치 이동

---
+++ src/main/java/lotto/entity/Lotto.java
@@ -0,0 +1,36 @@
+package lotto.entity;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    //해당 클래스는
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요");
+        }
+        Set<Integer> set = new HashSet<>();
+        for (Integer number : numbers) {
+            if (!set.add(number)) {
+                throw new IllegalArgumentException("[ERROR] 중복되는 숫자가 있습니다.");
+            }
+            if(number>45||number<1){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+
+    }
+
+    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers(){
+        return numbers;
+    }
+}

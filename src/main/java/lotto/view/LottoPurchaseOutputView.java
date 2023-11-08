c0f7e15d8fc5538fed5ea1910a57ff2406805931
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 20:27:02 +0900

    Feat : 로또 출력 기능 구현
    로또 구매 개수와 개수만큼의 로또 출력하는 기능 구현

--- src/main/java/lotto/view/LottoPurchaseOutputView.java
+++ src/main/java/lotto/view/LottoPurchaseOutputView.java
@@ -1,10 +1,29 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 
 public class LottoPurchaseOutputView {
 
-    public void printLotto(Lottos lottos, int lottoCount) {
+    private final static String LOTTO_COUNT = "개를 구매했습니다.";
 
+    public void printLotto(Lottos lottos, int lottoCount) {
+        System.out.println(lottoCount + LOTTO_COUNT);
+        for (int i = 0; i < lottos.size(); i++) {
+            Lotto lotto = lottos.get(i);
+            printOneLotto(lotto);
+        }
     }
+
+    public void printOneLotto(Lotto lotto) {
+        System.out.print("[");
+        for (int i = 0; i < lotto.size(); i++) {
+            System.out.print(lotto.get(i));
+            if (i != lotto.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]");
+    }
+
 }

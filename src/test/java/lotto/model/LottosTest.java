1addc07583315400495f7c68f55c7ced9552b526
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 14:54:38 +0900

    test(Lottos): 구매한 로또 개수를 구한다

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -2,49 +2,24 @@ package lotto.model;
 
 import static lotto.constant.LottoGameMessage.NUMBER_OF_LOTTO_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 
-import java.util.List;
-import java.util.stream.IntStream;
 import lotto.util.RandomUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 class LottosTest {
     private final RandomUtil randomUtil = new RandomUtil();
     private final int SUCCESSFUL_PAYMENT_AMOUNT = 3000;
-    private final List<Integer> LOTTO_NUMBER = List.of(3, 15, 23, 30, 36, 45);
 
     @DisplayName("구매한 로또 개수 구하기 테스트")
     @Test
     void paymentAmountTest() {
-        Lottos lottos = new Lottos(new PaymentAmount(SUCCESSFUL_PAYMENT_AMOUNT), randomUtil);
+        Lottos lottos = new Lottos(SUCCESSFUL_PAYMENT_AMOUNT, randomUtil);
         int numberOfLotto = computeNumberOfLotto(SUCCESSFUL_PAYMENT_AMOUNT);
         assertThat(lottos.toString()).contains(String.format(NUMBER_OF_LOTTO_MESSAGE.getMessage(), numberOfLotto));
     }
 
     private int computeNumberOfLotto(int paymentAmount) {
         return paymentAmount / 1000;
-    }
-
-    @DisplayName("구매한 로또 개수만큼 로또 생성 테스트")
-    @Test
-    void createLottoTest() {
-        RandomUtil randomUtil = Mockito.mock(RandomUtil.class);
-        when(randomUtil.createRandomNumbersInRange(anyInt(), anyInt(), anyInt())).thenReturn(LOTTO_NUMBER);
-
-        Lottos lottos = new Lottos(new PaymentAmount(SUCCESSFUL_PAYMENT_AMOUNT), randomUtil);
-        assertThat(lottos).hasToString(lottosInfo());
-    }
-
-    private String lottosInfo() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(String.format(NUMBER_OF_LOTTO_MESSAGE.getMessage(), computeNumberOfLotto(SUCCESSFUL_PAYMENT_AMOUNT)))
-                .append('\n');
-        IntStream.range(0, 3).forEach(i -> sb.append(LOTTO_NUMBER.toString()).append('\n'));
-
-        return sb.toString();
     }
 }

eeabc548927272ec32e22bccd68f421525e2d2dc
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:50:58 +0900

    fix : LottoController 변수 다시 선언

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,12 +1,18 @@
 package lotto.controller;
 
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningLotto;
 import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
 
 public class LottoController {
-    private final LottoService lottoService;
-
-    public LottoController(LottoService lottoService) {
-        this.lottoService = lottoService;
-    }
+    private LottoService lottoService;
+    private InputView inputView;
+    private OutputView outputView;
+    
 
 }

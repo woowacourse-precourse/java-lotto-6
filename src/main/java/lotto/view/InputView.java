eeec64b5fbe51baf19ab12d2d53bd8daddad72bd
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 17:02:41 +0900

    refactor(InputView) : add try logic to all methods

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,19 +7,48 @@ import lotto.domain.WinningNumber;
 
 public class InputView {
 
+    private static final String INPUT_PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+    private static final String NEW_LINE = "\n";
+
     public static PurchaseAmount inputPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String purchaseAmount = Console.readLine();
-        return new PurchaseAmount(Integer.parseInt(purchaseAmount));
+        while (true) {
+            try {
+                System.out.println(INPUT_PURCHASE_AMOUNT_MESSAGE);
+                String purchaseAmount = Console.readLine();
+                System.out.print(NEW_LINE);
+                return new PurchaseAmount(purchaseAmount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
     public static WinningNumber inputLottoWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
-        return new WinningNumber(Console.readLine());
+        while (true) {
+            try {
+                System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+                String inputWinningNumber = Console.readLine();
+                System.out.print(NEW_LINE);
+                return new WinningNumber(inputWinningNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public static BonusNumber inputLottoBonusNumbers() {
-        System.out.println("보너스 번호를 입력해 주세요.");
-        return new BonusNumber(Console.readLine());
+        while (true) {
+            try {
+                System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+                String inputBonusNumber = Console.readLine();
+                System.out.print(NEW_LINE);
+                return new BonusNumber(inputBonusNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }

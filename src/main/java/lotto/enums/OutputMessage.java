8c40c0f18babd23bd94c5ffd59671088d0d0300f
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 21:01:39 +0900

    feat: 수익률 출력 메시지 구현

--- src/main/java/lotto/enums/OutputMessage.java
+++ src/main/java/lotto/enums/OutputMessage.java
@@ -5,7 +5,8 @@ public enum OutputMessage {
     WINNING_NUMBER_PROMPT("\n당첨 번호를 입력해 주세요."),
     BONUS_NUMBER_PROMPT("\n보너스 번호를 입력해 주세요."),
     PURCHASE_NUMBER_OUTPUT("\n%d개를 구매했습니다.\n"),
-    WINNING_STATISTICS_OUTPUT("\n당첨 통계\n---");
+    WINNING_STATISTICS_OUTPUT("\n당첨 통계\n---"),
+    TOTAL_PROFIT_OUTPUT("총 수익률은 %.1f%%입니다.");;
 
     private final String message;
 

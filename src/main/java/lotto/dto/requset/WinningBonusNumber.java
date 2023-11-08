f3ae208db546621c09fc96e485f6b6e581e4d990
witwint <witwint1234@gmail.com>
2023-11-08 16:51:08 +0900

    test: 승리번호 보너스번호 입력 테스트

--- src/main/java/lotto/dto/requset/WinningBonusNumber.java
+++ src/main/java/lotto/dto/requset/WinningBonusNumber.java
@@ -1,5 +1,10 @@
 package lotto.dto.requset;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import lotto.view.ExceptionText;
+
 public class WinningBonusNumber {
 
 	String winningNumber;
@@ -11,6 +16,7 @@ public class WinningBonusNumber {
 	}
 
 	public void setWinningNumber(String winningNumber) {
+		validateWinningInput(winningNumber);
 		this.winningNumber = winningNumber;
 	}
 
@@ -19,6 +25,67 @@ public class WinningBonusNumber {
 	}
 
 	public void setBonusNumber(String bonusNumber) {
+		checkInputIsEmpty(bonusNumber);
+		ScopeCheck(Integer.parseInt(bonusNumber));
+		checkForOverlap(bonusNumber);
 		this.bonusNumber = bonusNumber;
 	}
+
+	private void validateWinningInput(String winningNumber) {
+		checkInputIsEmpty(winningNumber);
+		String[] parts = winningNumber.split(",");
+		numbersCountCheck(parts);
+		Set<Integer> uniqueNumbers = new HashSet<>();
+		for (String part : parts) {
+			numbersCheck(uniqueNumbers, part);
+		}
+	}
+
+	private static void checkInputIsEmpty(String input) {
+		if (input == null || input.isEmpty()) {
+			throw new IllegalArgumentException(ExceptionText.EMPTY.getText());
+		}
+	}
+	private static void numbersCountCheck(String[] parts) {
+		if (parts.length != 6) {
+			throw new IllegalArgumentException(ExceptionText.DIFFERENT_NUMBER.getText());
+		}
+	}
+
+	private static void numbersCheck(Set<Integer> uniqueNumbers, String part) {
+		try {
+			int number = Integer.parseInt(part);
+			ScopeCheck(number);
+			DuplicateCheck(uniqueNumbers, number);
+			uniqueNumbers.add(number);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(ExceptionText.INPUT_NUMBER_ERROR.getText());
+
+		}
+	}
+	private static void ScopeCheck(int number) {
+		if (number < 1 || number > 45) {
+			throw new IllegalArgumentException(ExceptionText.BEYOND_SCOPE.getText());
+		}
+	}
+
+	private static void DuplicateCheck(Set<Integer> uniqueNumbers, int number) {
+		if (uniqueNumbers.contains(number)) {
+			throw new IllegalArgumentException(ExceptionText.DUPLICATE_NUMBERS.getText());
+		}
+	}
+	public void checkForOverlap(String bonusNumber) {
+		String[] winningNumbers = this.winningNumber.split(",");
+		String[] bonusNumbers = bonusNumber.split(",");
+
+		Set<String> winningSet = new HashSet<>(Arrays.asList(winningNumbers));
+		Set<String> bonusSet = new HashSet(Arrays.asList(bonusNumbers));
+
+		winningSet.retainAll(bonusSet);
+
+		if (!winningSet.isEmpty()) {
+			throw new IllegalArgumentException(ExceptionText.DUPLICATE_WIN_BONUS.getText());
+		}
+	}
+
 }
---
+++ src/test/java/lotto/dto/requset/WinningBonusNumberTest.java
@@ -0,0 +1,88 @@
+package lotto.dto.requset;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningBonusNumberTest {
+
+	WinningBonusNumber winningBonusNumber = new WinningBonusNumber();
+
+	@DisplayName("빈값이면 예외가 발생한다.")
+	@Test
+	void createWinningNumByEmpty() {
+		assertThatThrownBy(() -> winningBonusNumber.setWinningNumber(""))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("숫자가 6개가 아니면 예외가 발생한다.")
+	@Test
+	void createWinningNumByCount() {
+		assertThatThrownBy(() -> winningBonusNumber.setWinningNumber("1,2,3,4,5"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("중복값이 있으면 예외가 발생한다.")
+	@Test
+	void createWinningNumByDuplicate() {
+		assertThatThrownBy(() -> winningBonusNumber.setWinningNumber("1,1,3,4,5,6"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("범위가 1과 45 사이가 아니면 예외가 발생한다.")
+	@Test
+	void createWinningNumByScope() {
+		assertThatThrownBy(() -> winningBonusNumber.setWinningNumber("-1,1,3,4,5,46"))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("성공 테스트")
+	@Test
+	void createWinningNumBySuccess() {
+		assertThatCode(() -> winningBonusNumber.setWinningNumber("1,2,3,4,5,6"))
+			.doesNotThrowAnyException();
+	}
+
+	@DisplayName("빈값이면 예외가 발생한다.")
+	@Test
+	void createBonusNumByEmpty() {
+		assertThatThrownBy(() -> {
+			winningBonusNumber.setWinningNumber("1,2,3,4,5,6");
+			winningBonusNumber.setBonusNumber("");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("범위가 1과 45 사이가 아니면 예외가 발생한다.")
+	@Test
+	void createBonusNumByScope() {
+		assertThatThrownBy(() -> {
+			winningBonusNumber.setWinningNumber("1,2,3,4,5,6");
+			winningBonusNumber.setBonusNumber("46");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+
+	@DisplayName("승리번호와 보너스번호가 겹치면 오류가 발생한다.")
+	@Test
+	void createBonusNumByOverlap() {
+		assertThatThrownBy(() -> {
+			winningBonusNumber.setWinningNumber("1,2,3,4,5,6");
+			winningBonusNumber.setBonusNumber("1");
+		}).isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("보너스번호 성공")
+	@Test
+	void createBonusNumBySuccess() {
+		assertThatCode(() -> {
+			winningBonusNumber.setWinningNumber("1,2,3,4,5,6");
+			winningBonusNumber.setBonusNumber("7");
+		}).doesNotThrowAnyException();
+	}
+
+
+
+
+}

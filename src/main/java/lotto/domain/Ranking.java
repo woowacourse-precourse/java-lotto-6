f981ba05a0a469910d361544d7fd53be2b4312b0
SunHyeYoon <haehae96@jbnu.ac.kr>
2023-11-08 20:48:06 +0900

    fix: 등수 표기 오류 수정

--- src/main/java/lotto/domain/Ranking.java
+++ src/main/java/lotto/domain/Ranking.java
@@ -1,12 +1,13 @@
 package lotto.domain;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public enum Ranking {
     FIFTH(3, false, 5_000, "3개 일치 (5,000원) - "),
     FOURTH(4, false, 50_000, "4개 일치 (50,000원) - "),
-    THIRD(5, true, 1_500_000, "5개 일치 (1,500,000원) - "),
-    SECOND(5, false, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, false, 1_500_000, "5개 일치 (1,500,000원) - "),
+    SECOND(5, true, 30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     FIRST(6, false, 2_000_000_000, "6개 일치 (2,000,000,000원) - ");
 
     private final int count;
@@ -14,30 +15,21 @@ public enum Ranking {
     private final int prize;
     private final String message;
 
-    private Ranking(int count, boolean hasBonus, int prize, String message) {
+    Ranking(int count, boolean hasBonus, int prize, String message) {
         this.count = count;
         this.hasBonus = hasBonus;
         this.prize = prize;
         this.message = message;
     }
 
-    public static Ranking rank(int count, boolean hasBonus) {
+    public static Optional<Ranking> rank(int count, boolean hasBonus) {
         return Arrays.stream(values())
-                .filter(ranking -> isEqualToRanking(ranking, count, hasBonus))
-                .findFirst()
-                .orElse(null);
+                .filter(ranking -> ranking.isEqualToRanking(count, hasBonus))
+                .findFirst();
     }
 
-    private static Boolean isEqualToRanking(Ranking ranking, int count, boolean hasBonus) {
-        return ranking.getCount() == count && ranking.hasBonus == hasBonus;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public boolean isHasBonus() {
-        return hasBonus;
+    private boolean isEqualToRanking(int count, boolean hasBonus) {
+        return this.count == count && this.hasBonus == hasBonus;
     }
 
     public int getPrize() {

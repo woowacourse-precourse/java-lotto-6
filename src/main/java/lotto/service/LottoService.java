7932ac9b62d303a789ddca7860b5cd08840c9ce0
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 14:44:52 +0900

    feat(LottoService): reshapeWinningNumber()메소드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,28 +1,62 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.LottoWinningCase;
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static lotto.ErrorMessage.*;
 
 public class LottoService {
 
     public List<Integer> reshapeWinningNumber(String inputNumbersWithComma) {
+        String[] splitedInputNumbers = getSplitedInputNumbers(inputNumbersWithComma);
+        return convertInputType(splitedInputNumbers);
+    }
+
+    private static List<Integer> convertInputType(String[] splitedInputNumbers) {
+        List<Integer> toBeReturnedInputNumbers = new ArrayList<>();
+        for (String splitedInputNumber : splitedInputNumbers) {
+            toBeReturnedInputNumbers.add(Integer.parseInt(splitedInputNumber.trim()));
+        }
+        if(toBeReturnedInputNumbers.contains("")){
+            throw new IllegalArgumentException(CONTAINS_EMPTY_NUMBER);
+        }
+        return toBeReturnedInputNumbers;
+    }
+
+    private static String[] getSplitedInputNumbers(String inputNumbersWithComma) {
+        String[] splitedInputNumbers = new String[0];
+        try {
+            splitedInputNumbers = inputNumbersWithComma.split(",");
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(CONTAINS_STRING_IN_LOTTO_NUMBER);
+        }
+        return splitedInputNumbers;
+    }
+
+    public int calculateAvailableNumberOfLotteryTickets(int spentAmount) {
+        return spentAmount / 1000;
+    }
+
+    public List<Lotto> generateRandomLottoNums(int availableTicketsCount) {
 
     }
 
-    public int calculateAvailableNumberOfLotteryTickets(int spentAmount) {}
+    public int compareMyNumberWithWinningNumber(List<Lotto> myLottoNumbers, List<Integer> winningNumber) {
 
-    public List<Lotto> generateRandomLottoNums(int availableTicketsCount) {}
+        return 0;
+    }
 
-    public int compareMyNumberWithWinningNumber(List<Integer> myLottoNumbers, List<Integer> winningNumber) {}
+    public boolean checkBonusPoint(List<Integer> myLottoNumber, int bonusNumber) {return true;}
 
-    public boolean checkBonusPoint(List<Integer> myLottoNumber, int bonusNumber) {}
-
-    public int getCaseNum(int lottoWinCase) {}
+    public int getCaseNum(int lottoWinCase) {return 0;}
 
     public void increaseTotalIncomeTest(int lottoWinCase) {}
 
-    public String typeConvertIncomeRate(float incomeRate) {}
+    public String typeConvertIncomeRate(float incomeRate) {return "A";}
 
-    public String calculateIncomeRate(int totalIncomde, int spentAmount) {}
+    public String calculateIncomeRate(int totalIncomde, int spentAmount) {return "A";}
 }

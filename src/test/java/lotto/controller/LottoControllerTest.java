ab800839bf974fa33442fc466ba3f51d1aa1250e
Potato-Y <projectpotatoy@gmail.com>
2023-11-08 22:36:45 +0900

    test: 개선된 LottoController에 맞게 테스트 코드 재작성
    - 중복되는 불필요한 테스트 삭제
    - 단위 테스트 기반으로 재작성

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -7,58 +7,98 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import lotto.dto.DatabaseDto;
+import lotto.exception.LottoPriceUnitException;
+import lotto.exception.NonPositiveAmountException;
 import lotto.model.LottoBonusNumber;
 import lotto.model.LottoWinningNumbers;
+import lotto.model.Money;
+import lotto.repository.LottoWinningRepository;
+import lotto.repository.UserLottoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoControllerTest extends NsTest {
 
+    UserLottoRepository userLottoRepository;
+    LottoWinningRepository lottoWinningRepository;
     LottoController lottoController;
 
     @BeforeEach
     void setup() {
-        this.lottoController = new LottoController();
+        this.userLottoRepository = new UserLottoRepository();
+        this.lottoWinningRepository = new LottoWinningRepository();
+        DatabaseDto dto = new DatabaseDto(userLottoRepository, lottoWinningRepository);
+        this.lottoController = new LottoController(dto);
     }
 
-    @DisplayName("로또를 구입할 때 잘못된 값을 넣으면 예외 발생: 단위")
+    @DisplayName("사용자원 단위로 받아온다.")
     @Test
-    void buyLottoThrowCase1() throws NoSuchMethodException {
+    void successGetAmount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // given
+        final String money = "1000";
+        run(money);
+        Method method = getAccessibleMethod("getAmount");
+
+        // when
+        Money moneyValue = (Money) method.invoke(lottoController);
+
+        // then
+        assertThat(moneyValue.getMoney())
+                .isEqualTo(Integer.parseInt(money));
+    }
+
+    @DisplayName("값을 넣으면 예외 발생: 단위")
+    @Test
+    void getAmountThrowCase1() throws NoSuchMethodException {
         final String money = "1100";
         run(money);
-        Method method = getAccessibleMethod("buyLotto");
+        Method method = getAccessibleMethod("getAmount");
 
         assertThatThrownBy(() -> {
             method.invoke(lottoController);
         })
-                .isInstanceOf(InvocationTargetException.class);
+                .isInstanceOf(InvocationTargetException.class)
+                .hasCauseInstanceOf(LottoPriceUnitException.class);
     }
 
-    @DisplayName("로또를 구입할 때 잘못된 값을 넣으면 예외 발생: 음수")
+    @DisplayName("잘못된 값을 넣으면 예외 발생: 음수")
     @Test
-    void buyLottoThrowCase2() throws NoSuchMethodException {
+    void getAmountThrowCase2() throws NoSuchMethodException {
         final String money = "-1000";
         run(money);
-        Method method = getAccessibleMethod("buyLotto");
+        Method method = getAccessibleMethod("getAmount");
 
         assertThatThrownBy(() -> {
             method.invoke(lottoController);
         })
-                .isInstanceOf(InvocationTargetException.class);
+                .isInstanceOf(InvocationTargetException.class)
+                .hasCauseInstanceOf(NonPositiveAmountException.class);
     }
 
-    @DisplayName("로또를 구입할 때 잘못된 값을 넣으면 예외 발생: 음수")
+    @DisplayName("잘못된 값을 넣으면 예외 발생: 0")
     @Test
-    void buyLottoThrowCase3() throws NoSuchMethodException {
+    void getAmountThrowCase3() throws NoSuchMethodException {
         final String money = "0";
         run(money);
-        Method method = getAccessibleMethod("buyLotto");
+        Method method = getAccessibleMethod("getAmount");
 
         assertThatThrownBy(() -> {
             method.invoke(lottoController);
         })
-                .isInstanceOf(InvocationTargetException.class);
+                .isInstanceOf(InvocationTargetException.class)
+                .hasCauseInstanceOf(NonPositiveAmountException.class);
+    }
+
+    @DisplayName("로또를 구입할 때 잘못된 값을 넣으면 다시 받는다.")
+    @Test
+    void retryBuyLotto() {
+        run("1100", "1000");
+
+        lottoController.buyLotto();
+
+        assertThat(output()).contains("1개를 구매했습니다.");
     }
 
     @DisplayName("로또를 구입하고, 로또를 반환받는다.")
@@ -72,7 +112,7 @@ class LottoControllerTest extends NsTest {
         assertThat(output()).contains(count + "개");
     }
 
-    @DisplayName("1등 당첨 번호를 받아온다.")
+    @DisplayName("사용자로부터 1등 당첨 번호를 받아온다.")
     @Test
     void successGetLottoWinningNumbers()
             throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
@@ -110,59 +150,57 @@ class LottoControllerTest extends NsTest {
     void getLottoBonusNumber() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         int number = 25;
         run(String.valueOf(number));
-        Method method = getAccessibleMethod("getLottoBonusNumber");
+        LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers("1,2,3,4,5,6");
+        Method method = getAccessibleMethod("getLottoBonusNumber", LottoWinningNumbers.class);
 
-        LottoBonusNumber outBonusNumber = (LottoBonusNumber) method.invoke(lottoController);
+        LottoBonusNumber outBonusNumber = (LottoBonusNumber) method.invoke(lottoController, lottoWinningNumbers);
 
         assertThat(outBonusNumber.getBonusNumber()).isEqualTo(number);
     }
 
     @DisplayName("잘못된 번호는 오류를 반환하고 다시 받는다.: 문자") // 이외 케이스는 LottoBonusNumber에서 처리
     @Test
-    void getLottoBonusNumberThrowCase()
+    void getLottoBonusNumberThrowCase1()
             throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         run("w", "25");
-        Method method = getAccessibleMethod("getLottoBonusNumber");
+        LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers("1,2,3,4,5,6");
+        Method method = getAccessibleMethod("getLottoBonusNumber", LottoWinningNumbers.class);
 
-        LottoBonusNumber bonusNumber = (LottoBonusNumber) method.invoke(lottoController);
+        LottoBonusNumber bonusNumber = (LottoBonusNumber) method.invoke(lottoController, lottoWinningNumbers);
 
         assertThat(output()).contains(ERROR_PREFACE);
         assertThat(bonusNumber)
                 .isNotNull();
     }
 
-    @DisplayName("사용자로부터 1등 번호와 보너스 번호를 받고 서비스에 넘긴다.")
+    @DisplayName("중복된 번호는 오류를 반환하고 다시 받는다.") // 이외 케이스는 LottoBonusNumber에서 처리
     @Test
-    void getLottoWinningAmount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void getLottoBonusNumberThrowCase2()
+            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        run("1", "25");
+        LottoWinningNumbers lottoWinningNumbers = new LottoWinningNumbers("1,2,3,4,5,6");
+        Method method = getAccessibleMethod("getLottoBonusNumber", LottoWinningNumbers.class);
+
+        LottoBonusNumber bonusNumber = (LottoBonusNumber) method.invoke(lottoController, lottoWinningNumbers);
+
+        assertThat(output()).contains(ERROR_PREFACE);
+        assertThat(bonusNumber)
+                .isNotNull();
+    }
+
+    @DisplayName("사용자로부터 1등 번호와 보너스 번호를 받고 서비스에 넘기고 저장된다.")
+    @Test
+    void saveLottoWinningAmount() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         run("1,2,3,4,5,6", "25");
-        Method method = getAccessibleMethod("getLottoBonusNumber");
 
-        // 예외 없이 잘 끝난다.
-        method.invoke(lottoController);
-    }
+        // when
+        lottoController.saveLottoWinningAmount();
 
-    @DisplayName("잘못된 1등 번호를 입력하면 다시 받고 전달한다.") // 다양한 케이스는 각 메서드에서 테스트
-    @Test
-    void getLottoWinningAmountRetry1() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        run("1,2,3,4,5,7", "1,2,3,4,5,6", "25");
-        Method method = getAccessibleMethod("getLottoBonusNumber");
-
-        // 예외 없이 잘 끝난다.
-        method.invoke(lottoController);
-
-        assertThat(output()).contains(ERROR_PREFACE);
-    }
-
-    @DisplayName("잘못된 보너스 번호를 입력하면 다시 받고 전달한다.") // 다양한 케이스는 각 메서드에서 테스트
-    @Test
-    void getLottoWinningAmountRetry2() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        run("1,2,3,4,5,6", "-10", "25");
-        Method method = getAccessibleMethod("getLottoBonusNumber");
-
-        // 예외 없이 잘 끝난다.
-        method.invoke(lottoController);
-
-        assertThat(output()).contains(ERROR_PREFACE);
+        // then
+        assertThat(lottoWinningRepository.getLottoWinningNumbers())
+                .isNotNull();
+        assertThat(lottoWinningRepository.getLottoBonusNumber())
+                .isNotNull();
     }
 
     private Method getAccessibleMethod(String methodName, Class<?>... parameterTypes)

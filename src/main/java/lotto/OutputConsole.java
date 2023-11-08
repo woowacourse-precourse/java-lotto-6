e5781b7fdfbe2738e043a139daf772b1f216dfd4
yeop0740 <ssucgoo@gmail.com>
2023-11-08 15:35:58 +0900

    feat: add method to print Message instance

--- src/main/java/lotto/OutputConsole.java
+++ src/main/java/lotto/OutputConsole.java
@@ -2,12 +2,13 @@ package lotto;
 
 public class OutputConsole {
 
-    public static final String INPUT_PRICE_MESSAGE = "구입금액을 입력해 주세요";
-    public static final String INPUT_WINNING_NUMBERS_MESSAGE = "\n당첨 번호를 입력해 주세요";
-    public static final String INPUT_BONUS_NUMBER_MESSAGE = "\n보너스 번호를 입력해 주세요";
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void printMessage(Message message) {
+        System.out.println(message.getMessage());
     }
 
     public static void print(Lottos lottos) {

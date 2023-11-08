6015b0661bbb56df0744df45e69e35fb9300acc5
JungJei <greteljds0251@gmail.com>
2023-11-08 20:18:21 +0900

    feat: LottoTicket 클래스 생성

--- src/main/java/Model/Lotto.java
+++ src/main/java/Model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package Model;
 
 import java.util.HashSet;
 import java.util.List;
--- src/main/java/Model/LottoTicket.java
+++ src/main/java/Model/LottoTicket.java
@@ -1,2 +1,22 @@
-package Model;public class LottoTicket {
+package Model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTicket {
+    private List<Lotto> lottos;
+
+    public LottoTicket(int num) {
+        lottos = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+            lottos.add(lotto);
+        }
+    }
+
+    public List<Lotto> getLottoTicket() {
+        return lottos;
+    }
 }

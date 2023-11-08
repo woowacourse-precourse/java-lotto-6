cc52e68c3447325883e6aef83cb1b1bb21a6be7a
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-08 21:46:10 +0900

    [refactor]: Dto 최적화

--- src/main/java/lotto/dto/ValidateAmountResponseDto.java
+++ src/main/java/lotto/dto/ValidateAmountResponseDto.java
@@ -1,17 +1,17 @@
 package lotto.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ValidateAmountResponseDto {
     private int lottoCount;
     private boolean validate;
-    public ValidateAmountResponseDto(int lottoCount, boolean validate) {
-        this.lottoCount = lottoCount;
-        this.validate = validate;
-    }
     public static ValidateAmountResponseDto of(int lottoCount,boolean validate){
         return ValidateAmountResponseDto.builder()
                 .lottoCount(lottoCount)

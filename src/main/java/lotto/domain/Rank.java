e0c30b86dec8f718243d12fa88bebd830af7594c
이정우 <acg6138@naver.com>
2023-11-08 11:43:53 +0900

    refactor(Rank): 각 순위에 대한 안내 메시지를 담은 winningMessage 필드 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -3,19 +3,21 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000),
-    BLANK(0, 0);
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - "),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - "),
+    FIFTH(3, 5000, "3개 일치 (5,000원) - "),
+    BLANK(0, 0, "꽝"),;
 
     private final int matchCount;
     private final int prizeMoney;
+    private final String winningMessage;
 
-    Rank(int matchCount, int prizeMoney) {
+    Rank(int matchCount, int prizeMoney, String winningMessage) {
         this.matchCount = matchCount;
         this.prizeMoney = prizeMoney;
+        this.winningMessage = winningMessage;
     }
 
     public static Rank of(int matchCount, boolean isBonus) {
@@ -34,6 +36,10 @@ public enum Rank {
 
     public int getPrizeMoney() {
         return prizeMoney;
+    }
+
+    public String getWinningMessage() {
+        return winningMessage;
     }
 
 }

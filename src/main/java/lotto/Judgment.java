28932a97419588497efcbea3a43f41b3d3b59607
Jungminkyung01 <nmnceeon33@gmail.com>
2023-11-08 20:42:06 +0900

    feat(lotto): create skeleton code

---
+++ src/main/java/lotto/Judgment.java
@@ -0,0 +1,8 @@
+package lotto;
+
+public class Judgment {
+    public int correctCheck(){
+
+        return 0;
+    }
+}
---
+++ src/main/java/lotto/NumberCreator.java
@@ -0,0 +1,10 @@
+package lotto;
+
+import java.util.List;
+
+public class NumberCreator {
+    public List<Integer> randomNumbers() {
+        //int[] lottoBall = {1,2,3,4,5,6};
+        return null;
+    }
+}

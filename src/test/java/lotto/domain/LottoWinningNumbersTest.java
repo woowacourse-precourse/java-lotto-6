163a859b541fab32d38530f01cbdf1ce9bf64269
eora21 <eora21@naver.com>
2023-11-08 15:42:57 +0900

    test: 💍 추가 당첨 로또와 우승 로또 매칭 시 결과 확인 테스트 작성

--- src/test/java/lotto/domain/LottoWinningNumbersTest.java
+++ src/test/java/lotto/domain/LottoWinningNumbersTest.java
@@ -1,15 +1,23 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import domain.Amount;
 import domain.BonusNumber;
+import domain.Lotto;
 import domain.LottoWinningNumbers;
+import domain.LottoWinningTier;
 import domain.WinningNumbers;
+import dto.LottoTickets;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoWinningNumbersTest {
@@ -75,5 +83,56 @@ public class LottoWinningNumbersTest {
                 () -> LottoWinningNumbers.createWinningNumbers(
                         WinningNumbers.createWinningNumbers(List.of(1, 2, 3, 4, 5, 6)),
                         BonusNumber.createBonusNumber(7)));
+    }
+
+    @Test
+    @DisplayName("로또가 3개 미만으로 일치한 경우 당첨 정보는 비어있다.")
+    void winningMatchResultIsEmptyIfAllLottoTicketsFail() {
+        LottoWinningNumbers lottoWinningNumbers = LottoWinningNumbers.createWinningNumbers(
+                WinningNumbers.createWinningNumbers(List.of(31, 32, 33, 34, 35, 36)),
+                BonusNumber.createBonusNumber(37));
+
+        LottoTickets lottoTickets = new LottoTickets(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(31, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(31, 32, 3, 4, 5, 6))
+        ));
+
+        assertThat(lottoWinningNumbers.calculateWinningStatistics(lottoTickets))
+                .isEmpty();
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또가 3개 이상 일치한 경우 당첨 정보는 생성된다.")
+    @MethodSource("lottoAndExpectWinningTier")
+    void winningMatchResult(Lotto lotto, LottoWinningTier expectWinningTier) {
+        LottoWinningNumbers lottoWinningNumbers = LottoWinningNumbers.createWinningNumbers(
+                WinningNumbers.createWinningNumbers(List.of(31, 32, 33, 34, 35, 36)),
+                BonusNumber.createBonusNumber(37));
+
+        LottoTickets lottoTickets = new LottoTickets(List.of(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(31, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(31, 32, 3, 4, 5, 6)),
+                lotto
+        ));
+
+        assertThat(lottoWinningNumbers.calculateWinningStatistics(lottoTickets))
+                .isEqualTo(List.of(expectWinningTier));
+    }
+
+    public static Stream<Arguments> lottoAndExpectWinningTier() {
+        return Stream.of(
+                Arguments.of(new Lotto(List.of(31, 32, 33, 4, 5, 6)),
+                        LottoWinningTier.FIFTH_TIER),
+                Arguments.of(new Lotto(List.of(31, 32, 33, 34, 5, 6)),
+                        LottoWinningTier.FOURTH_TIER),
+                Arguments.of(new Lotto(List.of(31, 32, 33, 34, 35, 6)),
+                        LottoWinningTier.THIRD_TIER),
+                Arguments.of(new Lotto(List.of(31, 32, 33, 34, 35, 37)),
+                        LottoWinningTier.SECOND_TIER),
+                Arguments.of(new Lotto(List.of(31, 32, 33, 34, 35, 36)),
+                        LottoWinningTier.FIRST_TIER)
+        );
     }
 }

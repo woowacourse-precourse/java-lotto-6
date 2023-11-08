363f0cd47478aa5afe9eb88219350068d2a52dbe
leorb159 <ldgsocial159@naver.com>
2023-11-08 14:56:56 +0900

    feat: LottoManagement 생성, buyLotto 메소드 작성

---
+++ src/main/java/lotto/LottoManagement.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoManagement {
+    private static final int PRICE_LOTTO = 1000;
+    private RandomNumberGenerator lottoGenerator;
+
+    public LottoManagement() {
+        lottoGenerator = new RandomNumberGenerator();
+    }
+
+    public List<Lotto> buyLotto(int amount) {
+        int lottoCount = amount / PRICE_LOTTO;
+        List<Lotto> lottery = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> randomNumbers = lottoGenerator.generate();
+            Lotto lotto = new Lotto(randomNumbers);
+            lottery.add(lotto);
+        }
+        return lottery;
+    }
+}

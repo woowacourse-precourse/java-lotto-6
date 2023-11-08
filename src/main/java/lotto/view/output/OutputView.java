f3429d2024e48833e895f81dcaaa7ca0d4d98186
chanhoim <cksgh201@naver.com>
2023-11-08 23:22:47 +0900

    refactor(output): 공백 라인 수정

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -12,6 +12,5 @@ public interface OutputView {
     void writeAskingWinningNumbers();
     void writeLottos(List<LottoDto> lottoDtos);
     void writeWinningStatistics(WinningStatisticsDto winningStatisticsDto, LottoRankDto lottoRankDto);
-    }
 
 }
--- src/main/java/lotto/view/output/PrintMessage.java
+++ src/main/java/lotto/view/output/PrintMessage.java
@@ -2,11 +2,11 @@ package lotto.view.output;
 
 public enum PrintMessage {
 
-    ASKING_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    ASKING_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     ASKING_COST("구입 금액을 입력해 주세요."),
     ASKING_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    PRINT_AMOUNT("%d개를 구매했습니다."),
-    PRINT_STATISTICS("당첨 통계\n---\n%s\n%s\n%s\n%s\n%s\n%s"),
+    PRINT_AMOUNT("\n%d개를 구매했습니다."),
+    PRINT_STATISTICS("\n당첨 통계\n---\n%s%s%s%s%s%s"),
     PRINT_FIFTH_RANK("%d개 일치 (%d) - %d개"),
     PRINT_FOURTH_RANK("%d개 일치 (%d) - %d개"),
     PRINT_THIRD_RANK("%d개 일치 (%d) - %d개"),

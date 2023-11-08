d50cf8e3de8d1ba892453a18e9caca608bc4cdbc
Seol_JY <wlsdud5654@gmail.com>
2023-11-08 15:08:02 +0900

    test(LottoTest): 테스트 코드 작성
    내역:
    - 로또 번호의 개수가 6개를 초과하는 경우 예외 확인.
    - 로또 번호에 중복된 숫자가 있는 경우 예외 확인.
    - 로또 번호의 개수가 6개보다 작은 경우 예외 확인.
    - 올바른 크기의 로또 번호 목록으로 Lotto 객체 생성 확인.
    - 범위를 벗어나는 번호가 있는 로또 번호 목록으로 Lotto 객체 생성 시 예외 확인.
    - Lotto 객체가 올바른 번호를 반환하는지 확인.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,5 +1,11 @@
 package lotto.domain;
 
+import static lotto.constants.ErrorMessage.INVALID_SIZE;
+import static lotto.constants.ErrorMessage.NUMBER_OUT_OF_RANGE;
+import static lotto.constants.LottoOption.END_LOTTO_NUMBER;
+import static lotto.constants.LottoOption.LOTTO_COUNT;
+import static lotto.constants.LottoOption.START_LOTTO_NUMBER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
@@ -10,17 +16,49 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        assertThatThrownBy(() -> Lotto.from(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호의 개수가 6개보다 작으면 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidSize() {
+        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5); // 크기 부적합
+        assertThatThrownBy(() -> Lotto.from(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format(INVALID_SIZE.getMessage(), LOTTO_COUNT.getValue()));
+    }
+
+    @DisplayName("올바른 크기의 로또 번호 목록으로 Lotto 객체를 생성할 수 있다.")
+    @Test
+    void createLottoWithValidSize() {
+        List<Integer> validNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = Lotto.from(validNumbers);
+        assertThat(lotto.getNumbers()).containsExactlyInAnyOrderElementsOf(validNumbers);
+    }
+
+    @DisplayName("범위를 벗어나는 번호가 있는 로또 번호 목록으로 Lotto 객체를 생성하면 예외가 발생한다.")
+    @Test
+    void createLottoWithOutOfRangeNumbers() {
+        List<Integer> outOfRangeNumbers = List.of(1, 2, 3, 4, 5, 50); // 범위를 벗어난 번호 포함
+        assertThatThrownBy(() -> Lotto.from(outOfRangeNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format(NUMBER_OUT_OF_RANGE.getMessage(), START_LOTTO_NUMBER.getValue(),
+                        END_LOTTO_NUMBER.getValue()));
+    }
+
+    @DisplayName("Lotto 객체가 올바른 번호를 반환한다.")
+    @Test
+    void getLottoNumbers() {
+        List<Integer> validNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = Lotto.from(validNumbers);
+        assertThat(lotto.getNumbers()).containsExactlyInAnyOrderElementsOf(validNumbers);
+    }
 }

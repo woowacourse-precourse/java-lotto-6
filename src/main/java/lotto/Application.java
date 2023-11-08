2871689134e07f134d4d9707cab6074db06e3da1
skj4244 <skj4244@naver.com>
2023-11-08 13:25:55 +0900

    feat: Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.replaceAll;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        LottoController lottoController = new LottoController(new LottoView(), new InputJudgement());
+        Integer inputPurchasePrice = lottoController.inputPurchasePrice();
+        lottoController.buyLotto(inputPurchasePrice);
+        List<Integer> winningNumbers = lottoController.inputWinningNumber();
+        Integer bonusNumber = lottoController.inputBonusNumber(winningNumbers);
+        lottoController.requestWinningStatistics(winningNumbers, bonusNumber);
     }
 }

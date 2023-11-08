173277eef0b42bd1a998ef2bd1f9b7febf814d53
dabin <db021129@naver.com>
2023-11-08 18:31:00 +0900

    feat : corrected setSelectedNumber

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,10 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.Exception.LottoSetPriceException;
+import lotto.Exception.LottoIntegerException;
+import lotto.Exception.LottoListException;
 import lotto.domain.Lotto;
+import lotto.domain.SelectedLottoNumber;
 import lotto.service.LottoService;
 
 import java.util.ArrayList;
@@ -10,12 +12,13 @@ import java.util.List;
 
 public class LottoController {
 
-    private final LottoSetPriceException lottoSetPriceException = new LottoSetPriceException();
+    private final LottoIntegerException lottoIntegerException = new LottoIntegerException();
+    private final LottoListException lottoListException =new LottoListException();
     private final LottoService lottoService = new LottoService();
 
     public int LottoSetPrice() {
         String readPrice = Console.readLine();
-        int lottoPrice = lottoSetPriceException.checkPriceException(readPrice);
+        int lottoPrice = lottoIntegerException.checkForPriceException(readPrice);
         return lottoPrice;
     }
 
@@ -29,5 +32,17 @@ public class LottoController {
             lottos.add(lotto);
         }
         return lottos;
+    }
+
+    public SelectedLottoNumber setSelectedNumber() {
+
+        String initialNumbers = Console.readLine();
+        List<Integer> numbers = lottoListException.checkSelectedNumber(initialNumbers);
+
+        String initialBonus = Console.readLine();
+        int bonus = lottoIntegerException.checkForBonus(initialBonus);
+
+        SelectedLottoNumber selectedLottoNumber = new SelectedLottoNumber(numbers, bonus);
+        return selectedLottoNumber;
     }
 }

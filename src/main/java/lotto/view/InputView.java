44032035e0a124264d08c54d0b59439a0d8c39cf
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 13:50:44 +0900

    feat(InputView): 보너스 번호 입력받는 view 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INSERT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INSERT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INSERT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public String readPurchaseAmount() {
         System.out.println(INSERT_PURCHASE_AMOUNT);
@@ -13,6 +14,11 @@ public class InputView {
 
     public String readWinningNumbers() {
         System.out.println(INSERT_WINNING_NUMBERS);
+        return Console.readLine();
+    }
+
+    public String readBonusNUmber() {
+        System.out.println(INSERT_BONUS_NUMBER);
         return Console.readLine();
     }
 }

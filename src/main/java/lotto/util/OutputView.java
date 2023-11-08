173c542fa23cfdb532c9e5757ce9a88d31f79237
FL <refrain2k@gmail.com>
2023-11-08 17:56:23 +0900

    feat: 결과 출력을 위한 기능 구현

--- src/main/java/lotto/util/OutputView.java
+++ src/main/java/lotto/util/OutputView.java
@@ -1,14 +1,45 @@
 package lotto.util;
 
+import lotto.domain.EarnedCalculator;
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.message.OutPutMessage;
 
+import java.util.EnumMap;
 import java.util.List;
 
+import static lotto.message.OutPutMessage.*;
+
 public class OutputView {
-    public void viewBuyLotteries(List<Lotto> lotteries) {
+    public void printLotteries(int purchaseAmount, List<Lotto> lotteries) {
+        System.out.println(OutPutMessage.OUTPUT_PURCHASE_QUANTITY.format(purchaseAmount));
         for (Lotto lotto : lotteries) {
             System.out.println(lotto);
         }
+    }
+
+    public void printLottoResult(EnumMap<LottoRank, Integer> lottoResultMap) {
+
+        System.out.println(OUTPUT_STATISTICS.getMessage());
+
+        for (LottoRank lottoRank : lottoResultMap.keySet()) {
+            if (lottoRank == LottoRank.NONE) {
+                continue;
+            }
+            int matches = lottoRank.getMatchedCount();
+            int earnings = lottoRank.getEarnings();
+            int matchesCount = lottoResultMap.get(lottoRank);
+            if (matches == 5 && lottoRank.isMatchesBonusNumber()) {
+                System.out.println(OUTPUT_MATCH_COUNT_WITH_BONUS.format(matches, earnings, matchesCount));
+            } else {
+                System.out.println(OUTPUT_MATCH_COUNT.format(matches, earnings, matchesCount));
+            }
+        }
+    }
+
+    public void printLottoEarningRate(int purchaseAmount, EnumMap<LottoRank, Integer> lottoResultMap) {
+        double earningRate = new EarnedCalculator().getEarningRate(lottoResultMap, purchaseAmount);
+        System.out.println(OutPutMessage.OUTPUT_TOTAL.format(earningRate));
     }
 
 }

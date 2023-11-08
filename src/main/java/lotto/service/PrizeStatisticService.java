e84189f0d81941dfcb7c698382270efd1a566974
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 16:45:23 +0900

    refactor: move two methods from lotto result class
    LottoResult에서 PrizeStatisticService 클래스로 메서드 이동

--- src/main/java/lotto/service/PrizeStatisticService.java
+++ src/main/java/lotto/service/PrizeStatisticService.java
@@ -1,10 +1,37 @@
 package lotto.service;
 
+import java.util.EnumMap;
 import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+import lotto.domain.LottoTicket;
 import lotto.domain.Prize;
 import lotto.view.OutputView;
 
 public class PrizeStatisticService {
+
+    public static Map<Prize, Integer> initializePrizeStatistics() {
+        Map<Prize, Integer> initialStatistics = new EnumMap<>(Prize.class);
+        for (Prize prize : Prize.values()) {
+            if (prize != Prize.NONE) {
+                initialStatistics.put(prize, 0);
+            }
+        }
+        return initialStatistics;
+    }
+
+    public static Map<Prize, Integer> compilePrizeStatistics(Lotto winningLotto, int bonusNumber) {
+        Map<Prize, Integer> matchNumberCount = initializePrizeStatistics();
+
+        // 상금의 수를 맵에 넣어주기
+        for (Lotto lottoGame : LottoTicket.lottoGames) {
+            LottoResult lottoResult = new LottoResult(lottoGame, winningLotto, bonusNumber);
+            LottoResult.updateMatchNumberCount(matchNumberCount, lottoResult);
+        }
+
+        return matchNumberCount;
+    }
+
     public static int processAndReportPrizeResults(Map<Prize, Integer> prizeCounts) {
         OutputView.printPrizeStaticIntro();
         int totalPrizeMoney = 0;

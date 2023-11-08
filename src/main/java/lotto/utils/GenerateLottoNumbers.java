0c95039725556cd6af5914c42c003ba83167c843
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 10:56:08 +0900

    feat(GenerateLottoNumbers) : buyingLottos 메서드 생성

--- src/main/java/lotto/utils/GenerateLottoNumbers.java
+++ src/main/java/lotto/utils/GenerateLottoNumbers.java
@@ -1,8 +1,10 @@
 package lotto.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Constants;
 import lotto.domain.UserLotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateLottoNumbers {
@@ -14,5 +16,14 @@ public class GenerateLottoNumbers {
 
     public static int getBuyingQuantity(int buyingPrice) {
         return buyingPrice / Constants.LOTTO_PRICE;
+    }
+
+    public static List<UserLotto> buyingLottos(int quantity) {
+        List<UserLotto> lottos = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(Constants.MIN_RANGE_NUMBER, Constants.MAX_RANGE_NUMBER, Constants.DESIGNATED_LOTTO_LENGTH);
+            lottos.add(new UserLotto(sortedList(lottoNumbers)));
+        }
+        return lottos;
     }
 }

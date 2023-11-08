1694603e9ae62e4631a73e505f9e847590144515
Leegeonwoo7 <workgeon07@naver.com>
2023-11-08 18:54:50 +0900

    실행클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,17 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	DrawLotto drawLotto = new DrawLotto();
+    	Person person = new Person();
+    	LottoMachine lottoMachine = new LottoMachine();
+    	
+    	try {
+            person.buyLotto(lottoMachine);
+            person.displayTicket();
+            int[] result = person.checkWinningNumber(drawLotto);
+            person.displayResult(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }

85eab4274c4ee120b0f6058bd39a9ac6dc941f28
You <ytn1574@gmail.com>
2023-11-08 09:43:35 +0900

    chore: 오타 수정

--- src/main/java/lotto/WinInformation.java
+++ src/main/java/lotto/WinInformation.java
@@ -9,7 +9,7 @@ public enum WinInformation {
     MATCH_FOUR_RANK_FOURTH(4, 50000L),
     MATCH_FIVE_NO_BONUS_RANK_THIRD(5, 1500000L),
     MATCH_FIVE_YES_BONUS_RANK_SECOND(6, 30000000L),
-    MATCH_SIX_NO_RANK_FIRST(6, 2000000000L);
+    MATCH_SIX_RANK_FIRST(6, 2000000000L);
 
     private int matchCount;
     private long winMoney;

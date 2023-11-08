a5bfbe77e11c73777176141e95fa57672f4be2b8
Yonghyun Yoon <dragonappear1@gmail.com>
2023-11-09 01:09:48 +0900

    refactor(Grade): 필터링 메서드를 추가한다

--- src/main/java/lotto/domain/grade/Grade.java
+++ src/main/java/lotto/domain/grade/Grade.java
@@ -1,15 +1,50 @@
 package lotto.domain.grade;
 
-import lotto.utils.IntegerConstant;
+import static lotto.utils.IntegerConstant.FIVE;
+import static lotto.utils.IntegerConstant.FOUR;
+import static lotto.utils.IntegerConstant.ONE;
+import static lotto.utils.IntegerConstant.SIX;
+import static lotto.utils.IntegerConstant.THREE;
+import static lotto.utils.IntegerConstant.ZERO;
 
 public enum Grade {
 
-    FIFTH("3개 일치 (5,000원)", 5_000),
-    FOURTH("4개 일치 (50,000원)", 50_000),
-    THIRD("5개 일치 (1,500,000원)", 1_500_000),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000),
-    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000),
-    NONE("꽝", 0);
+    FIFTH("3개 일치 (5,000원)", 5_000) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return numberMatchCount == THREE;
+        }
+    },
+    FOURTH("4개 일치 (50,000원)", 50_000) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return numberMatchCount == FOUR;
+        }
+    },
+    THIRD("5개 일치 (1,500,000원)", 1_500_000) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return numberMatchCount == FIVE && bonusMatchCount == ZERO;
+        }
+    },
+    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)", 30_000_000) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return numberMatchCount == FIVE && bonusMatchCount == ONE;
+        }
+    },
+    FIRST("6개 일치 (2,000,000,000원)", 2_000_000_000) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return numberMatchCount == SIX;
+        }
+    },
+    NONE("꽝", 0) {
+        @Override
+        public boolean filter(int numberMatchCount, int bonusMatchCount) {
+            return true;
+        }
+    };
 
     private final String description;
     private final int winningMoney;
@@ -27,23 +62,15 @@ public enum Grade {
         this.winningMoney = winningMoney;
     }
 
-    public static Grade of(int numberMatchCount, int bonusMatchCount) {
-        if (numberMatchCount == IntegerConstant.SIX) {
-            return FIRST;
-        }
-        if (numberMatchCount == IntegerConstant.FIVE && bonusMatchCount == IntegerConstant.ONE) {
-            return SECOND;
-        }
-        if (numberMatchCount == IntegerConstant.FIVE) {
-            return THIRD;
-        }
-        if (numberMatchCount == IntegerConstant.FOUR) {
-            return FOURTH;
-        }
-        if (numberMatchCount == IntegerConstant.THREE) {
-            return FIFTH;
-        }
+    public abstract boolean filter(int numberMatchCount, int bonusMatchCount);
 
+
+    public static Grade of(int numberMatchCount, int bonusMatchCount) {
+        for (Grade grade : Grade.values()) {
+            if (grade.filter(numberMatchCount, bonusMatchCount)) {
+                return grade;
+            }
+        }
         return NONE;
     }
 }

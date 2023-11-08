96cc809d2195b965ba6e86b67f8035b4bb532e54
hwangkyojune <shane5969@naver.com>
2023-11-08 17:42:58 +0900

    Randoms.pickNumberInRange()에서 pickUniqueNumberInRange()로 변경

--- src/main/java/lotto/LottoMarketService.java
+++ src/main/java/lotto/LottoMarketService.java
@@ -1,37 +1,33 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
-import static lotto.ValidateService.validateProperMoney;
 
 public class LottoMarketService {
     static private final int LOTTO_NUM_SIZE = 6;
     static private final int LOTTO_PRICE = 1000;
+    private final static int MIN_LOTTO_NUM = 1;
+    private final static int MAX_LOTTO_NUM = 45;
 
     public List<Lotto> issueLottoes(int lottoCount){
         List<Lotto> lottoes = new ArrayList<>();
         Supplier<Integer> lottoNumSupplier = new LottoNumSupplier();
         for(int i=0;i<lottoCount;i++){
-            List<Integer> lottoNums = generateNonDuplicatedNums(lottoNumSupplier);
-            lottoNums.sort(Integer::compare);
+            List<Integer> lottoNums = generateNonDuplicatedNums();
+            Collections.sort(lottoNums);
             lottoes.add(new Lotto(lottoNums));
         }
         return lottoes;
     }
 
-    public List<Integer> generateNonDuplicatedNums(Supplier<Integer> lottoNumSupplier){
-        List<Integer> lottoNumbers = new ArrayList<>();
-        while(lottoNumbers.size() < LOTTO_NUM_SIZE){
-            int candidateNum = lottoNumSupplier.get();
-            boolean isDuplicated = lottoNumbers.stream()
-                    .anyMatch(num->num.equals(candidateNum));
-            if(!isDuplicated){
-                lottoNumbers.add(candidateNum);
-            }
-        }
-        return lottoNumbers;
+    public List<Integer> generateNonDuplicatedNums(){
+        List<Integer> lottoesNums = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM,MAX_LOTTO_NUM,LOTTO_NUM_SIZE);
+        return lottoesNums;
     }
 
     public int calculateLottoCount(int money){

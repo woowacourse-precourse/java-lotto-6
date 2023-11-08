c23288122c82931afe844b204d701e8c41a9485a
unknown <winternrabbit@gmail.com>
2023-11-08 13:06:24 +0900

    test(WinningNumber): 당첨 번호에 1~45 외의 숫자가 있을 경우 테스트 작성

--- src/test/java/lotto/domain/WinningNumberTest.java
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -11,16 +11,24 @@ class WinningNumberTest {
 
     @DisplayName("당첨 번호의 개수가 6개가 아닐 경우 에러 메시지를 출력한다.")
     @Test
-    void createLottoByOverSize() {
+    void createWinningNumberByOverSize() {
         assertThatThrownBy(() -> new WinningNumber(new Lotto(List.of(11, 12, 13, 14, 15))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }
 
-    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 에러 메시지를 출력한다.")
     @Test
-    void createLottoByDuplicatedNumber() {
+    void createWinningNumberByDuplicatedNumber() {
         assertThatThrownBy(() -> new WinningNumber(new Lotto(List.of(41,42,43,44,45,45))))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @DisplayName("당첨 번호에 1~45 외의 숫자가 있으면 에러 메시지를 출력한다.")
+    @Test
+    void createWinningNumberByOutOfRange() {
+        assertThatThrownBy(() -> new WinningNumber(new Lotto(List.of(41,42,43,44,45,46))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
     }

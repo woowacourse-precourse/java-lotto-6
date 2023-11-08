d103923b6ec45f224f8a0fcd4456c6d0847c0fe6
mingtorr <mingtorr@nave.com>
2023-11-08 20:10:56 +0900

    feat: 로또 번호와 당첨 번호, 보너스 번호를 비교 기능 추가

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,47 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
+import lotto.domain.LottoTicket;
+import lotto.domain.WinningNumbers;
+
+import java.util.List;
+
+public class LottoService {
+    private final int[] winningCount;
+
+    public LottoService() {
+        winningCount = new int[LottoPrize.values().length];
+    }
+
+    public void calculateWinningsCounts(LottoTicket lottoTickets, WinningNumbers winningNumbers) {
+        List<Integer> winningList = winningNumbers.getWinningNumber();
+        int bonusNum = winningNumbers.getBonusNumber();
+
+        for (Lotto lotto : lottoTickets.getLottoList()) {
+            int count = lotto.countMatches(winningList);
+            boolean hasBonusNum = lotto.contains(bonusNum);
+            determineRank(count, hasBonusNum);
+        }
+    }
+
+    private void determineRank(int count, boolean hasBonusNum) {
+        if (count == LottoPrize.SECOND.getMatchCount() && hasBonusNum) {
+            updatePrizeQuantity(LottoPrize.SECOND, count);
+            return;
+        }
+
+        for (LottoPrize prize : LottoPrize.values()) {
+            updatePrizeQuantity(prize, count);
+        }
+    }
+
+    private void updatePrizeQuantity(LottoPrize prize, int count) {
+        if (count == prize.getMatchCount()) {
+            int index = prize.ordinal(); // 열거형 순서를 인덱스로 활용
+            winningCount[index]++;
+        }
+    }
+
+
+}

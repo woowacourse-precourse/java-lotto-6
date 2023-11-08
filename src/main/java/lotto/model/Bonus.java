fc42872c129fb0c7b1f34557be4cce73750b135c
geunsu <geunsu23@naver.com>
2023-11-08 11:20:26 +0900

    [add] enum tkdyd

--- src/main/java/lotto/model/Bonus.java
+++ src/main/java/lotto/model/Bonus.java
@@ -1,14 +1,22 @@
 package lotto.model;
 
+import lotto.validator.InputValidator;
+
 import java.util.List;
+
+import static lotto.model.Constants.MAX_LOTTO_NUMBER;
+import static lotto.model.Constants.MIN_LOTTO_NUMBER;
 
 public class Bonus {
     private final Integer bonus;
 
-    public Bonus(Integer bonus) {
-        this.bonus = bonus;
+    public Bonus(String bonus) {
+        InputValidator.checkUserInputIsInteger(bonus);
+        int tmpBonus = Integer.parseInt(bonus);
+        InputValidator.checkUserInputOutOfRange(tmpBonus,MIN_LOTTO_NUMBER,MAX_LOTTO_NUMBER);
+        this.bonus = tmpBonus;
     }
-
+    public int getBonus(){ return bonus; }
     public boolean checkMatchEachLotto(List<Integer> eachLotto) {
         return eachLotto.contains(bonus);
     }
--- src/main/java/lotto/model/LottoConstants.java
+++ src/main/java/lotto/model/LottoConstants.java
@@ -1,2 +1,16 @@
-package lotto.model;public enum LottoConstants {
+package lotto.model;
+
+public enum LottoConstants {
+    FIRST_PRIZE(2000000000),
+    SECOND_PRIZE(30000000),
+    THIRD_PRIZE(1500000),
+    FOURTH_PRIZE(50000),
+    FIFTH_PRIZE(5000),
+    NOTHING(0);
+
+    private final int value;
+    LottoConstants(int value) {this.value = value;}
+
+    public int getValue() {return value;}
+
 }

110753af21e7cb032287cf2789ad3a274f820339
cozym <ddongyee99@daum.net>
2023-11-08 19:48:28 +0900

    test: 보너스번호 중복확인 테스트 추가

--- src/test/java/lotto/model/MyLottoNumbersTest.java
+++ src/test/java/lotto/model/MyLottoNumbersTest.java
@@ -30,6 +30,14 @@ class MyLottoNumbersTest {
                 .hasMessageContaining("6개의 숫자로 구성되어야 합니다");
     }
 
+    @DisplayName("중복된 숫자가 있을 경우 예외처리")
+    @Test
+    void 입력값_중복검사() {
+        assertThatThrownBy(() -> new MyLottoNumbers("1,2,2,4,5,6"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 숫자가 존재합니다");
+    }
+    
     @DisplayName("보너스 입력값 숫자가 아닐 경우 예외처리")
     @Test
     void 보너스_입력검사() {
@@ -39,11 +47,12 @@ class MyLottoNumbersTest {
                 .hasMessageContaining("숫자만 입력 가능합니다");
     }
 
-    @DisplayName("중복된 숫자가 있을 경우 예외처리")
+    @DisplayName("보너스 입력값 숫자가 이미 존재할 경우 예외처리")
     @Test
-    void 입력값_중복검사() {
-        assertThatThrownBy(() -> new MyLottoNumbers("1,2,2,4,5,6"))
+    void 보너스_중복검사() {
+        MyLottoNumbers numbers = new MyLottoNumbers("1,2,3,4,5,6");
+        assertThatThrownBy(() -> numbers.setBonusNumber("6"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("중복된 숫자가 존재합니다");
+                .hasMessageContaining("입력하신 보너스 숫자가 이미 존재합니다");
     }
 }

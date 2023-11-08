182b0b7e8ca8f1f3fe13afa7d575494cc3cb5939
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 22:02:12 +0900

    feat: 예외 처리 후, 재입력을 받도록 수정

--- src/main/java/lotto/domain/LottoAmount.java
+++ src/main/java/lotto/domain/LottoAmount.java
@@ -6,14 +6,24 @@ public class LottoAmount {
 	private static final int LOTTO_PRICE = 1000;
 
 	public static int inputAmount() {
-		int amount = validateNum(InputView.inputAmountOfMoney());
-		validateDivide(amount);
-		return amount / LOTTO_PRICE;
+		while (true) {
+			try {
+				int amount = validateNum(InputView.inputAmountOfMoney());
+				validateDivide(amount);
+				return amount / LOTTO_PRICE;
+			} catch (IllegalArgumentException e) {
+				System.out.println("[ERROR] 수가 아닙니다.");
+			}
+		}
 	}
 
 	private static void validateDivide(int amount) {
 		if (amount % LOTTO_PRICE != 0) {
 			System.out.println("[ERROR] 1000으로 나누어떨어지지 않습니다.");
+			throw new IllegalArgumentException();
+		}
+		if (amount <= 0) {
+			System.out.println("[ERROR] 금액은 양수여야 합니다.");
 			throw new IllegalArgumentException();
 		}
 	}

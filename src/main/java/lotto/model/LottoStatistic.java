e6c3a7445bf165136de840c39461e09244b4cb65
Ukj0ng <xb253q@gmail.com>
2023-11-08 19:26:09 +0900

    feat(LottoStatistic): 수익률 추가
    추가 사항:
    - 수익률 멤버 변수 추가
    - 수익률 getter 함수 추가

--- src/main/java/lotto/model/LottoStatistic.java
+++ src/main/java/lotto/model/LottoStatistic.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class LottoStatistic {
     private final Map<LottoPrize, Integer> lottoStatistic;
+    private double yield;
 
     public LottoStatistic() {
         this.lottoStatistic = new HashMap<>();
@@ -19,5 +20,13 @@ public class LottoStatistic {
             prize,
             lottoStatistic.getOrDefault(prize, 0) + 1
         );
+    }
+
+    public void setYield(double yield) {
+        this.yield = yield;
+    }
+
+    public double getYield() {
+        return this.yield;
     }
 }

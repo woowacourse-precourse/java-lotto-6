0690800ca98a73d9ba69ec33f0195df0ec2bdd52
Yminji <ymg0818@naver.com>
2023-11-08 23:50:22 +0900

    feat(Lotto) : 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.CalculateStats;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.domain.UserNumberInput;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = amountValidate();
+
+        int lottoCount = purchaseAmount / 1000;
+        Lottos lottos = new Lottos(new UserNumberInput());
+        lottos.lottoTicketsNum(lottoCount); //가격에 맞춰 로또 번호 추출
+    }
+
+    public static int amountValidate() {
+        int amount = Integer.parseInt(Console.readLine());
+        try{
+            if(amount % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력하셔야 합니다.");
+            }else if(amount == 0){
+                throw new IllegalArgumentException("[ERROR] 1000원 이상 입력하셔야 합니다.");
+            }
+        }catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return amountValidate();
+        }
+        return amount;
     }
 }
+

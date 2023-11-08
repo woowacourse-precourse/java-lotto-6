f7cee050d1727bc4efbb7e2750516636bbb15e06
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 17:18:08 +0900

    기존 테스트 코드가 잘못되어 수정
    기존 테스트코드는 보너스 번호 일치 여부를 판단하지 않았습니다.
    그래서 테스트코드를 수정했습니다.

--- src/test/java/lotto/LottoRankTest.java
+++ src/test/java/lotto/LottoRankTest.java
@@ -8,38 +8,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LottoRankTest {
 
     @Test
-    @DisplayName("6개 번호가 일치하면 FIRST 등수를 반환해야 한다")
-    public void testFirstRank() {
-        assertEquals(LottoRank.FIRST, LottoRank.fromMatchCount(6));
+    @DisplayName("6개 번호 일치시 FIRST 반환")
+    public void testValueOfFirst() {
+        LottoRank rank = LottoRank.valueOf(6, false);
+        assertEquals(LottoRank.FIRST, rank, "6개 번호가 일치할 때는 FIRST 등수를 반환해야 합니다.");
     }
 
     @Test
-    @DisplayName("5개 번호가 일치하면 SECOND 등수를 반환해야 한다")
-    public void testSecondRank() {
-        assertEquals(LottoRank.SECOND, LottoRank.fromMatchCount(5));
+    @DisplayName("5개 번호와 보너스 번호 일치시 SECOND 반환")
+    public void testValueOfSecondWithBonus() {
+        LottoRank rank = LottoRank.valueOf(5, true);
+        assertEquals(LottoRank.SECOND, rank, "5개 번호와 보너스 번호가 일치할 때는 SECOND 등수를 반환해야 합니다.");
     }
 
     @Test
-    @DisplayName("4개 번호가 일치하면 THIRD 등수를 반환해야 한다")
-    public void testThirdRank() {
-        assertEquals(LottoRank.THIRD, LottoRank.fromMatchCount(4));
+    @DisplayName("5개 번호만 일치시 THIRD 반환")
+    public void testValueOfThirdWithoutBonus() {
+        LottoRank rank = LottoRank.valueOf(5, false);
+        assertEquals(LottoRank.THIRD, rank, "5개 번호만 일치할 때는 THIRD 등수를 반환해야 합니다.");
     }
 
     @Test
-    @DisplayName("3개 번호가 일치하면 FOURTH 등수를 반환해야 한다")
-    public void testFourthRank() {
-        assertEquals(LottoRank.FOURTH, LottoRank.fromMatchCount(3));
+    @DisplayName("4개 번호 일치시 FOURTH 반환")
+    public void testValueOfFourth() {
+        LottoRank rank = LottoRank.valueOf(4, false);
+        assertEquals(LottoRank.FOURTH, rank, "4개 번호가 일치할 때는 FOURTH 등수를 반환해야 합니다.");
     }
 
     @Test
-    @DisplayName("2개 번호가 일치하면 FIFTH 등수를 반환해야 한다")
-    public void testFifthRank() {
-        assertEquals(LottoRank.FIFTH, LottoRank.fromMatchCount(2));
+    @DisplayName("3개 번호 일치시 FIFTH 반환")
+    public void testValueOfFifth() {
+        LottoRank rank = LottoRank.valueOf(3, false);
+        assertEquals(LottoRank.FIFTH, rank, "3개 번호가 일치할 때는 FIFTH 등수를 반환해야 합니다.");
     }
 
     @Test
-    @DisplayName("일치하는 번호가 없으면 NONE 등수를 반환해야 한다")
-    public void testNoMatch() {
-        assertEquals(LottoRank.NONE, LottoRank.fromMatchCount(0));
+    @DisplayName("일치하는 번호가 없을 경우 NONE 반환")
+    public void testValueOfNone() {
+        LottoRank rank = LottoRank.valueOf(0, false);
+        assertEquals(LottoRank.NONE, rank, "일치하는 번호가 없을 경우 NONE 등수를 반환해야 합니다.");
     }
 }

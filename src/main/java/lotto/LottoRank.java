f7320814f010ef024f221978831923f48df80c8f
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 22:33:02 +0900

    로또 랭크 Enum 클래스 수정
    출력 조건을 맞추기 위해서 랭크의 순서를 수정했습니다.

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,12 +1,12 @@
 package lotto;
 
 public enum LottoRank {
-    FIRST(6, 2_000_000_000),
-    SECOND(5, 30_000_000),
-    THIRD(4, 1_500_000),
-    FOURTH(3, 50_000),
-    FIFTH(2, 5_000),
-    NONE(0, 0);
+    NONE(0, 0),
+    FIFTH(3, 5000),
+    FOURTH(4, 50000),
+    THIRD(5, 1500000),
+    SECOND(5, 30000000),
+    FIRST(6, 2000000000);
 
     private final int matchCount;
     private final int reward;

0128f4529969d30cbfb25004d44e02da7c26373a
HanSeongLEE <leehs98712@gmail.com>
2023-11-08 18:53:16 +0900

    Test: 결과 생성을 위한 Repository test생성
    로또의 결과 값을 LottoRepository에 저장하기위해 테스트 코드 작성

--- src/test/java/lotto/domain/LottoRepositoryTest.java
+++ src/test/java/lotto/domain/LottoRepositoryTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +10,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoRepositoryTest {
+    private LottoRepository lottoRepository =new LottoRepository();
     @DisplayName("Lotto 저장 로직")
     @Test
     void saveLotto(){
-//        List<Lotto> lottos = new ArrayList<>();
-//        Lotto lottoElement = new Lotto(List.of(1,2,3,4,5,6));
-//        lottos.add(lottoElement);
-//        LottoRepository testRepository = new LottoRepository(lottos);
-//        assertThat(testRepository.getLottoRepository().get(0)).isEqualTo(lottoElement);
+        Lotto lottoElement = new Lotto(List.of(1,2,3,4,5,6));
+        lottoRepository.addOne(lottoElement);
+        assertThat(lottoRepository.getLottoRepository().get(0)).isEqualTo(lottoElement);
+    }
+
+    @Test
+    public void testAddResult() {
+        // Add some LottoPrize results
+        lottoRepository.addResult(LottoPrize.FIRST);
+        lottoRepository.addResult(LottoPrize.SECOND);
+
+        // Check if the counts are updated correctly
+        assertEquals(1, (int) lottoRepository.getLottoResult().get(LottoPrize.FIRST));
+        assertEquals(1, (int) lottoRepository.getLottoResult().get(LottoPrize.SECOND));
     }
 }

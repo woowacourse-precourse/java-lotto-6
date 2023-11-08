1d8856995418252648ea0384469a5506dcd332e9
honeydanji <tjdwls3680@naver.com>
2023-11-08 14:46:03 +0900

    REFACTOR : 2가지 오류 발생(SOF, 무한 예외 발생) ★★★★★
    1. 구입금액 입력을 받고 예외 처리 진행 시 SOF 발생
    // 원인
    - 입력 함수와 예외 처리 함수를 분리해서 구현함.
    - 서로 생성자를 호출 하니 무한 루프에 빠지게 되어 SOF 발생.
    
    // 해결
    - 필드에 생성자를 선언만 한 상태에서 예외 함수를 호출할 때만 초기화 함.
    
    2. 예외 발생 시 재입력 하면 정상값이 들어와도 첫번째로 입력한 값에 대한 예외만 발생.
    // 원인
    - 서로 다른 클래스에 존재하는 함수를 호출 하는 방식 즉 간접재귀를 이용했음.
    - n번째 정상 입력이 들어와도 이전에 발생한 값이 리턴되기 때문에 무조건 예외가 발생함.
    
    // 해결
    - 디버깅을 통해서 코드의 문제점을 이해함.
    - 예외 처리 클래스에는 예외만 던지도록 변경
    - 던져진 예외는 입력 클래스에서 받아서 재입력 받음.
    - 재귀 >> 반복문으로 변경

--- src/main/java/lotto/utils/PurchaseAmountException.java
+++ src/main/java/lotto/utils/PurchaseAmountException.java
@@ -1,32 +1,23 @@
 package lotto.utils;
 
-import lotto.view.Input;
-
 import static lotto.utils.ExceptionMessage.NON_DIVISIBLE_BY_1000;
 import static lotto.utils.ExceptionMessage.NOT_NUMBER;
 
 public class PurchaseAmountException {
-    Input input = new Input();
-
-    public void totalException(String input) {
-        notNumeric(input);
-        notDivisible(input);
-    }
 
     public void notNumeric(String input) {
-        try {
-            Integer.parseInt(input);
-        }catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NOT_NUMBER.getMessage());
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                throw new IllegalArgumentException(NOT_NUMBER.getMessage());
+            }
         }
-        this.input.inputPurchaseAmount();
     }
 
     public void notDivisible(String input) {
         int numberInput = Integer.parseInt(input);
-        if(numberInput % 1000 != 0) {
+        if (numberInput % 1000 != 0) {
             throw new IllegalArgumentException(NON_DIVISIBLE_BY_1000.getMessage());
         }
-        this.input.inputPurchaseAmount();
     }
 }
+
--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -1,13 +1,25 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.utils.PurchaseAmountException;
 
 import static lotto.utils.GameMessage.*;
 
 public class Input {
+    PurchaseAmountException purchaseAmountException = new PurchaseAmountException();
+
     public String inputPurchaseAmount() {
-        System.out.println(PURCHASE_AMOUNT.getMessage());
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println(PURCHASE_AMOUNT.getMessage());
+                String input = Console.readLine();
+                purchaseAmountException.notNumeric(input);
+                purchaseAmountException.notDivisible(input);
+                return input;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public String inputWinningNumber() {

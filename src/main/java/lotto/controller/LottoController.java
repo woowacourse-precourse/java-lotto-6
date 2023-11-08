bab88bcf86e610533a9df60bf49c7a483e89e3db
WoongKi1115 <nwk6638@gmail.com>
2023-11-08 16:16:07 +0900

    feat(lottoController) : 로또 컨트롤러 input 까지 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,68 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+import lotto.exception.LottoGameException;
+import lotto.service.LottoService;
+import lotto.util.LottoUtil;
+import lotto.util.Validate;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.List;
+
 public class LottoController {
 
+    private static LottoGame lottoGame;
+
+    public static void Lotto() {
+        startGame();
+        winningNumber();
+        bonusNumber();
+    }
+
+    private static void startGame() {
+        long purchaseAmount = 0L;
+        while (purchaseAmount == 0) {
+            try {
+                purchaseAmount = Validate.checkPurchaseAmountValidate(InputView.getPurchaseAmount());
+            } catch (LottoGameException e) {
+                System.out.println(e.getMessage());
+                purchaseAmount = 0L;
+            }
+        }
+        lottoGame = new LottoGame(purchaseAmount);
+        lottoGame.setLottos(getLottoNumbers(lottoGame.getLottoCnt()));
+        OutputView.printLottos(lottoGame);
+    }
+
+    private static List<Lotto> getLottoNumbers(Long count) {
+        return LottoService.getAllLottos(count);
+    }
+
+
+    private static void winningNumber() {
+        Lotto winningNumber = null;
+        while (winningNumber == null) {
+            try {
+                winningNumber = LottoUtil.convertList(InputView.getWinningNumber());
+            } catch (LottoGameException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        lottoGame.setWinningNumber(winningNumber);
+    }
+
+    private static void bonusNumber() {
+        int bonusNumber = 0;
+        bonusNumber = Validate.checkBonusNumberValidate(InputView.getBonusNumber());
+        while (bonusNumber == 0) {
+            try {
+                bonusNumber = Validate.checkBonusNumberValidate(InputView.getBonusNumber());
+            } catch (LottoGameException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        lottoGame.setBonusNumber(bonusNumber);
+    }
 }

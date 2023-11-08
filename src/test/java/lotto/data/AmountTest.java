e8a7d51c0a987160f5c3efa1b7ee44c5593bc903
alscjf1329 <alscjf1329@gmail.com>
2023-11-08 14:03:05 +0900

    test(AmountTest): 금액 생성시 1000원 단위가 아닐 시 예외 발생하는 테스트 작성

--- src/test/java/lotto/data/AmountTest.java
+++ src/test/java/lotto/data/AmountTest.java
@@ -17,4 +17,13 @@ public class AmountTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorMessage.PURCHASE_AMOUNT_IS_LESS_THAN_LOTTO_PRICE.getMessage());
     }
+
+    @ParameterizedTest
+    @DisplayName("금액 생성시 1000원 단위가 아닐 시 예외 발생")
+    @CsvSource(value = {"2330", "1234", "1001"})
+    void createAmountByNotThousandWonUnits(String purchaseAmount) {
+        assertThatThrownBy(() -> new Amount(purchaseAmount, BigDecimal.valueOf(1000)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorMessage.AMOUNT_IS_NOT_IN_THOUSAND_WON_UNITS.getMessage());
+    }
 }

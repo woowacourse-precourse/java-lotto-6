6040f3dbeca379f725ce958bd9bb0cd3db845e9f
KongJunWoo <joonoo3@naver.com>
2023-11-08 22:55:57 +0900

    feat : 당첨 번호 입력 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,5 +8,10 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
+
+    public static String inputWinningNumber(){
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
     
 }

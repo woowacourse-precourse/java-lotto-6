77641ddf7e640146a64f2199bdec2d615a909c13
skj4244 <skj4244@naver.com>
2023-11-08 13:26:43 +0900

    test: Lotto

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,4 +24,10 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 6개가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호_5개(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

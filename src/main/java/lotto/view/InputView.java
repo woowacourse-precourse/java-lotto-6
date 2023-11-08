4bf1433a907601d3aa69a5113ac86cdaeaec6da8
Kim mingyu <minq0302@naver.com>
2023-11-08 20:58:30 +0900

    feat(view): add printing demanding input winning numbers message method

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,5 +5,7 @@ public class InputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    
+    public static void showInputWinningNumbersMessage() {
+        System.out.println("당첨 번호를 입력해주세요.");
+    }
 }

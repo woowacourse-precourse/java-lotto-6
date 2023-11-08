0129a4ce7856450068000b4d35556edef4f9f5c3
Dendrog <goodshs335@gmail.com>
2023-11-08 21:56:50 +0900

    Feat: 로또 구입 금액 입력 로직 및 검증 작성

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -5,6 +5,13 @@ public class Input {
     public static int buyLotto(){
         System.out.println("구입금액을 입력해 주세요.");
         String inputText = Console.readLine();
-
+        int result;
+        try{
+            Validator.buyCheck(inputText);
+            result = Integer.parseInt(inputText) / 1000;
+        }catch (IllegalArgumentException e){
+            result = buyLotto();
+        }
+        return result;
     }
 }
--- src/main/java/lotto/Validator.java
+++ src/main/java/lotto/Validator.java
@@ -1,12 +1,14 @@
 package lotto;
 
 public class Validator {
-    public static void buyCheck(String inputText){
-        try{
+    public static void buyCheck(String inputText) {
+        try {
             int price = Integer.parseInt(inputText);
-        }catch (NumberFormatException e){
+            if (price % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해 주세요.");
+            }
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 정수만 입력해 주세요.");
         }
-
     }
 }

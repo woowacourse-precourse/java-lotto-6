30c8569d4a1a9947790ee444dac2d38f4cc3a6e1
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 21:55:54 +0900

    refactor: printStatistics in Result add: printRank in Result add: printSecondRank in Result add: formatPrize in Result

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -19,18 +19,41 @@ public class Result {
     public void printStatistics(Map<Rank, Integer> statistics) {
         System.out.println("당첨 통계");
         System.out.println("---------");
-        for (Rank rank : Rank.values()) {
-            if (rank == Rank.NONE) {
-                continue; // 'NONE' 등수는 출력하지 않음
-            }
-            System.out.printf("%d개 일치 (%d원)- %d개\n",
-                    rank.getMatchCount(),
-                    rank.getPrize(),
-                    statistics.getOrDefault(rank, 0));
-        }
+        printRank(statistics, Rank.FIFTH);
+        printRank(statistics, Rank.FOURTH);
+        printRank(statistics, Rank.THIRD);
+        printSecondRank(statistics);
+        printRank(statistics, Rank.FIRST);
+    }
+
+    private void printRank(Map<Rank, Integer> statistics, Rank rank) {
+        System.out.printf("%d개 일치 (%s원)- %d개\n",
+                rank.getMatchCount(),
+                formatPrize(rank.getPrize()),
+                statistics.get(rank));
+
+    }
+
+    private void printSecondRank(Map<Rank, Integer> statistics) {
+        System.out.printf("5개 일치, 보너스 볼 일치 (%s원)- %d개\n",
+                formatPrize(Rank.SECOND.getPrize()),
+                statistics.get(Rank.SECOND));
+
+    }
+
+    private String formatPrize(int prize) {
+        return String.format("%,d", prize);
     }
 
     public void printProfitRate(double profitRate) {
-        System.out.printf("총 수익률은 %.2f%%입니다.\n", profitRate);
+        String formattedProfitRate = formatProfitRate(profitRate);
+        System.out.printf("총 수익률은 %s%%입니다.\n", formattedProfitRate);
+    }
+
+    private String formatProfitRate(double profitRate) {
+        if (profitRate % 1.0 == 0) {
+            return String.valueOf((int)profitRate);
+        }
+        return String.format("%.2f", profitRate);
     }
 }

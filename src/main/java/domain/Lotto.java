d13ae001fff3645a575beac4e4ac9edeaf152001
yurrim <yurimkang20@gmail.com>
2023-11-08 13:27:52 +0900

    feat: 동일한 번호 포함 여부 기능 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,13 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +23,14 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        List<Integer> temp = new ArrayList<>();
+        for (int num : numbers) {
+            if (temp.contains(num)) {
+                throw new IllegalArgumentException();
+            } else {
+                temp.add(num);
+            }
+        }
+    }
 }

c878862187061a0c8cc2ca43af8ef0c3b51c9150
wschoe <chldntjr0425@naver.com>
2023-11-08 20:16:26 +0900

    feat: add printStaticstics method in Printer

--- src/main/java/lotto/presentation/printer/Printer.java
+++ src/main/java/lotto/presentation/printer/Printer.java
@@ -12,4 +12,14 @@ public class Printer {
             System.out.println(ticket);
         }
     }
+
+    public static void printStatistics(StatisticsVO stats) {
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + stats.win3Count() + "개");
+        System.out.println("4개 일치 (50,000원) - " + stats.win4Count() + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + stats.win5Count() + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + stats.win5WithBonusCount() + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + stats.win6Count() + "개");
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", stats.revenueRatio());
+    }
 }

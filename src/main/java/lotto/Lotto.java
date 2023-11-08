8c2a68f014bcef4c3598798e6e98bf7c4ad775e3
flame-possible <werq2003@naver.com>
2023-11-08 22:06:26 +0900

    feat : lotto 번호의 예외사항, 당첨 비교 메소드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,43 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        CheckNumberSize(numbers);
+        CheckOverlap(numbers);
+        CheckValidateNumbers(numbers);
+    }
+
+    private void CheckNumberSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 총 갯수는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void CheckOverlap(List<Integer> numbers) {
+        Set<Integer> temp_set = new HashSet<>(numbers);
+
+        if (temp_set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+
+    }
+
+    private static void CheckValidateNumbers(List<Integer> numbers) {
+        for (int a : numbers) {
+            if (a < 1 || a > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    public int CompWinner(Lotto winner) {
+        return (int) numbers.stream().filter(winner::ContainNumber).count();
+    }
+
+    public boolean ContainNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }

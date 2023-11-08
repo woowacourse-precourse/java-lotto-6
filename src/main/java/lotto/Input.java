a1edcf3366d1d5a7c97092c8b35cfd51947b8d81
elizabethgim <yujingim@naver.com>
2023-11-08 23:02:15 +0900

    feat: 문자열이 들어갔을 경우 예외처리

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -2,75 +2,39 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Input {
     private static final String ERROR_MESSAGE = "[ERROR]";
     private int money = 0;
+    private int numberOfLotto = 0;
 
-    public int getNumberOfLotto(){
-        int numberOfLotto = calculate(money);
-        return numberOfLotto;
-    }
     public int getMoney(){
+        return this.money;
+    }
+    public int getNumberOfLotto(){
+        return this.numberOfLotto;
+    }
+    public void setNumberOfLotto(){
+        int numberOfLotto = calculate(money);
+        System.out.println(numberOfLotto+"개를 구매했습니다.");
+    }
+    public void setMoney(){
         try{
             System.out.println("구입금액을 입력해 주세요.\n");
             String input = Console.readLine();
-            validateInput(input);
             this.money = Integer.parseInt(input);
             validateMoney(money);
-        } catch (IllegalArgumentException error){
-            System.out.println(error.getMessage());
-            return getMoney();
+        } catch (NumberFormatException error){
+            System.out.println(ERROR_MESSAGE + " 숫자를 입력해주세요.");
         }
+    }
 
-        return money;
-    }
-    private void validateInput(String input){
-        if(!input.matches("[0-9]")){
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
-        }
-    }
     private void validateMoney(int money){
         if(money % 1000 != 0){
             throw new IllegalArgumentException(ERROR_MESSAGE+" 로또는 1000원 단위로 구매가 가능합니다.");
         }
     }
 
-    public List<Integer> getWinningNumber(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-
-        List<Integer> numbers = Arrays
-                .stream(Console.readLine().split(","))
-                .map(number-> Integer.parseInt(number.trim()))
-                .collect(Collectors.toList());
-
-        checkWinningNumberSize(numbers);
-        checkSameNumberInWinningNumber(numbers);
-
-        return numbers;
-    }
-
-    private void checkWinningNumberSize(List<Integer> numbers){
-        if(numbers.size() > 6 || numbers.size() < 6){
-            throw new IllegalArgumentException(ERROR_MESSAGE +" 당첨 번호는 총 6개로 이루어져 있습니다.");
-        }
-    }
-
-    private void checkSameNumberInWinningNumber(List<Integer> numbers){
-        boolean[] visited = new boolean[46];
-
-        for(int i=0;i<6;i++){
-            if(!visited[numbers.get(i)]){
-                visited[numbers.get(i)] = true;
-                continue;
-            }
-
-            throw new IllegalArgumentException(ERROR_MESSAGE+" 중복된 당첨 번호가 있습니다.");
-        }
-    }
 
     private int calculate(int money){
         return money / 1000;

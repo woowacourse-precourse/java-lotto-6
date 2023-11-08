7b879e7c0cc6a14f12f501306b727721b0cf7743
seondays <agseon9@gmail.com>
2023-11-08 11:38:17 +0900

    refactor(OutputView): 프로세스 진행 메시지 상수 enum으로 분리

---
+++ src/main/java/lotto/utils/ProcessMessage.java
@@ -0,0 +1,19 @@
+package lotto.utils;
+
+public enum ProcessMessage {
+    WINNING_RESULT("%n당첨 통계%n"),
+    DIVISION_MARK("--%n"),
+    GET_MONEY("구입금액을 입력해 주세요.%n"),
+    GET_WINNING_NUMBER("%n당첨 번호를 입력해 주세요.%n"),
+    GET_BONUS_NUMBER("%n보너스 번호를 입력해 주세요.%n");
+
+    private String message;
+
+    ProcessMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,28 +1,29 @@
 package lotto.view;
 
 import java.util.Map;
+import lotto.utils.ProcessMessage;
 
 public class OutputView {
     public static void printWinningResult(Map<String, Integer> result) {
-        System.out.println("\n당첨 통계");
-        System.out.println("--");
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 3, "5,000", result.get("5th"));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 4, "50,000", result.get("4th"));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 5, "1,500,000", result.get("3rd"));
-        System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개\n", 5, "30,000,000", result.get("2nd"));
-        System.out.printf("%d개 일치 (%s원) - %d개\n", 6, "2,000,000,000", result.get("1st"));
+        System.out.printf(ProcessMessage.WINNING_RESULT.getMessage());
+        System.out.printf(ProcessMessage.DIVISION_MARK.getMessage());
+        System.out.printf("%d개 일치 (%s원) - %d개%n", 3, "5,000", result.get("5th"));
+        System.out.printf("%d개 일치 (%s원) - %d개%n", 4, "50,000", result.get("4th"));
+        System.out.printf("%d개 일치 (%s원) - %d개%n", 5, "1,500,000", result.get("3rd"));
+        System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개%n", 5, "30,000,000", result.get("2nd"));
+        System.out.printf("%d개 일치 (%s원) - %d개%n", 6, "2,000,000,000", result.get("1st"));
     }
 
     public static void printGetMoneyMessage() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.printf(ProcessMessage.GET_MONEY.getMessage());
     }
 
     public static void printGetWinningNumberMessage() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
+        System.out.printf(ProcessMessage.GET_WINNING_NUMBER.getMessage());
     }
 
     public static void printGetBonusNumberMessage() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
+        System.out.printf(ProcessMessage.GET_BONUS_NUMBER.getMessage());
     }
 
     public static void printCommonString(String string) {
@@ -30,7 +31,7 @@ public class OutputView {
     }
 
     public static void printLottoCount(int money) {
-        System.out.printf("\n%d개를 구매했습니다.\n", money);
+        System.out.printf("%n%d개를 구매했습니다.%n", money);
     }
 
     public static void printLottoProfit(double profit) {

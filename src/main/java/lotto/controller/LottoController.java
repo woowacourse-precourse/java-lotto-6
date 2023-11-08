dcbe90992ea0f5f6d605adee9261cce25b4aff66
ziy00n <jiyun4188@gmail.com>
2023-11-08 23:44:57 +0900

    feat : 당첨번호와 로또번호가 일치하는 숫자의 수를 세는 기능, 당첨 결과 출력하는 기능, 수익률 계산하는 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,10 +2,12 @@ package lotto.controller;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoMachine;
+import lotto.domain.Ranking;
 import lotto.domain.WinningLotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
@@ -15,5 +17,30 @@ public class LottoController {
         List<Lotto> boughtLotto = LottoMachine.buy(inputMoney);
         int count = LottoMachine.getLottoCount(inputMoney);
         OutputView.printPurchasedLottos(boughtLotto, count);
+
+        WinningLotto winningLotto = getWinningLotto();
+        List<Ranking> rankings = getRanks(boughtLotto, winningLotto);
+
+        System.out.println("run에서 getRanks후: "+rankings);
+        OutputView.printLottoResult(inputMoney, rankings);
+    }
+
+    public WinningLotto getWinningLotto() {
+        List<Integer> winningNumbers = InputView.getWinningNumber();
+        Lotto lotto = new Lotto(winningNumbers);
+//        Lotto lotto = new Lotto(InputView.getWinningNumber());
+        int bonusNumber = InputView.getBonusNumber(winningNumbers);
+        // return new WinningLotto(lotto, bonusNumber);
+        return new WinningLotto(lotto, bonusNumber);
+    }
+
+    public List<Ranking> getRanks(List<Lotto> boughtLotto, WinningLotto winningLotto) {
+        List<Ranking> ranks = new ArrayList<>();
+
+        for (Lotto lotto : boughtLotto) {
+            Ranking ranking = Ranking.calculate(lotto, winningLotto);
+            ranks.add(ranking);
+        }
+        return ranks;
     }
 }

a27e889ba6d7c8337162aa34954facbbd99031d3
hajungIm <ihj_ewha@ewhain.net>
2023-11-08 11:06:34 +0900

    Feat(LottoRepository): 로또 리포지토리 생성, 저장 기능 구현
    `LottoRepository` 싱글톤으로 생성
    `save()` 구현

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -1,5 +1,43 @@
 package lotto.repository;
 
-public class LottoRepository {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import lotto.domain.Lotto;
 
+public class LottoRepository {
+    public static final int LOTTO_PRICE = 1000;
+    private static Map<Long, Lotto> lottos = new ConcurrentHashMap<>();
+    private static long id = 0L;
+    private static int budget;
+
+    private static LottoRepository instance;
+
+    private LottoRepository() {
+    }
+
+    public static LottoRepository getInstance() {
+        if (instance == null) {
+            instance = new LottoRepository();
+        }
+        return instance;
+    }
+    public static void setBudget(int budget) {
+        LottoRepository.budget = budget;
+    }
+
+    public static int getBudget() {
+        return budget;
+    }
+
+    public static List<Lotto> getLottos() {
+        return new ArrayList<>(lottos.values());
+    }
+
+    public Lotto save(Lotto lotto) {
+        lotto.id = id++;
+        lottos.put(lotto.id, lotto);
+        return lotto;
+    }
 }

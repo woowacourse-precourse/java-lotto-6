791853335a8c4e1c64e05f3f3833e4fe6bdd1268
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-08 22:30:42 +0900

    feat: 당첨 번호와 보너스 번호에 대한 검증 및 예외 처리 구현
    1. (DrawResult): 객체 생성 방법 수정
    기존 생성자를 삭제하고 빌더 패턴을 적용한다. 빌더 패턴을 적용한 이유는 검증 로직을 외부로 노출시키지 않은
    상태에서 당첨 번호와 보너스 번호에 대한 예외 검증을 객체 생성 도중에 하기 위함이다.
    
    검증 메소드의 경우 빌더 클래스 내부로 넣었다.
     - validateWinningNumbers: 당첨 번호의 길이와 각 요소의 값 범위를 검증
     - validateBonusNumber: 보너스 번호의 값의 범위를 검증
     - validateDuplicationBetweenWinningNumbersAndBonusNumber: 당첨 번호와 보너스 번호 사이의
    중복 여부를 검증한다. buid 메소드를 통해 객체를 반환하기 전에 호출된다.
    
    2. (LottoController): drawResult 메소드 변경
    빌더 패턴을 통해 DrawResult 객체를 생성한다.
    이때 당첨 번호와 보너스 번호 입력 부분을 아래의 두 메소드로 분리하였다.
    
    3. applyWinningNumbers(Builder)
    DrawResult의 Builder 객체를 받아 입력 받은 당첨 번호를 적용한다.
    만약 유효하지 않은 당첨 번호가 입력된 경우 다시 입력하도록 한다.
    
    4. applyBonusNumberAndBuildDrawResult
    DrawResult의 Builder 객체를 받아 입력 받은 보너스 번호를 적용한다.
    만약 유효하지 않은 보너스 번호가 입력된 경우 다시 입력하도록 한다.

--- src/main/java/lotto/DrawResult.java
+++ src/main/java/lotto/DrawResult.java
@@ -1,16 +1,66 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DrawResult {
 
-    private final List<Integer> winningNumbers;
+    private List<Integer> winningNumbers;
     private int bonusNumber;
 
-    public DrawResult(List<Integer> winningNumbers, int bonusNumber) {
-        this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
+    public DrawResult(Builder builder) {
+        this.winningNumbers = builder.winningNumbers;
+        this.bonusNumber = builder.bonusNumber;
+    }
+
+    public static class Builder {
+        private List<Integer> winningNumbers;
+        private int bonusNumber;
+
+        public Builder winningNumbers(List<Integer> winningNumbers) {
+            validateWinningNumbers(winningNumbers);
+            this.winningNumbers = winningNumbers;
+            return this;
+        }
+
+        public Builder bonusNumber(int bonusNumber) {
+            validateBonusNumber(bonusNumber);
+            this.bonusNumber = bonusNumber;
+            return this;
+        }
+
+        public DrawResult build() {
+            validateDuplicationBetweenWinningNumbersAndBonusNumber();
+            return new DrawResult(this);
+        }
+
+        private void validateWinningNumbers(List<Integer> winningNumbers) {
+            boolean isSizeInvalid = (winningNumbers.size() != 6);
+            boolean isThereAnyDuplicatedNumber = (winningNumbers.size() != winningNumbers.stream().distinct().count());
+            boolean isThereAnyOutrangedNumber = false;
+            for (Integer number : winningNumbers) {
+                if (number < 1 || 45 < number) {
+                    isThereAnyOutrangedNumber = true;
+                }
+            }
+
+            if (isSizeInvalid || isThereAnyDuplicatedNumber || isThereAnyOutrangedNumber) {
+                throw new IllegalArgumentException(
+                        "당첨 번호는 1이상 45 이하의 중복되지 않는 6개의 정수여야 합니다.");
+            }
+        }
+
+        private void validateBonusNumber(int bonusNumber) {
+            if (bonusNumber < 1 || 45 < bonusNumber) {
+                throw new IllegalArgumentException
+                        ("보너스 번호는 1이상 45이하의 정수여야합니다.");
+            }
+        }
+
+        private void validateDuplicationBetweenWinningNumbersAndBonusNumber() {
+            if (winningNumbers.contains(bonusNumber)) {
+                throw new IllegalArgumentException("보너스 번호와 당첨 번호는 중복되어서는 안됩니다.");
+            }
+        }
     }
 
     public Prize check(Lotto lotto) {
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,6 +2,7 @@ package lotto.controller;
 
 import java.util.List;
 import lotto.DrawResult;
+import lotto.DrawResult.Builder;
 import lotto.Lotto;
 import lotto.LottoLover;
 import lotto.view.LottoView;
@@ -29,8 +30,34 @@ public class LottoController {
     }
 
     public DrawResult getDrawResult() {
-        List<Integer> winningNumbers = LottoView.requestWinningNumbers();
-        int bonusNumber = LottoView.requestBonusNumber();
-        return new DrawResult(winningNumbers, bonusNumber);
+        Builder drawResultBuilder = new DrawResult.Builder();
+        applyWinningNumbers(drawResultBuilder);
+        return applyBonusNumberAndBuildDrawResult(drawResultBuilder);
+    }
+
+    private void applyWinningNumbers(Builder builder) {
+        while (true) {
+            try {
+                List<Integer> winningNumbers = LottoView.requestWinningNumbers();
+                builder.winningNumbers(winningNumbers);
+                return;
+            } catch (IllegalArgumentException e) {
+                // 메시지 출력 후 다시 입력하도록 한다.
+                System.out.println("[Error] " + e);
+            }
+        }
+    }
+
+    private DrawResult applyBonusNumberAndBuildDrawResult(Builder builder) {
+        while (true) {
+            try {
+                int bonusNumber = LottoView.requestBonusNumber();
+                builder.bonusNumber(bonusNumber);
+                return builder.build();
+            } catch (IllegalArgumentException e) {
+                // 메시지 출력 후 다시 입력하도록 한다.
+                System.out.println("[Error] " + e);
+            }
+        }
     }
 }

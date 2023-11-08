6f1202b017eb0f213f9f66afe104920281e40969
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 11:01:35 +0900

    :sparkles: Feat: 구매 금액 음수에 대한 error message 추가
    - 구매 금액으로 음수가 들어온다면 예외를 던진기 위한 메시지 추가

--- src/main/java/lotto/common/exception/PurchaseAmountErrorMessage.java
+++ src/main/java/lotto/common/exception/PurchaseAmountErrorMessage.java
@@ -4,6 +4,7 @@ public enum PurchaseAmountErrorMessage {
 
     NOT_NULL("구매 금액은 NULL이 아닙니다."),
     NOT_BLANK("구매 금액은 빈 값이 아닙니다."),
+    NOT_NEGATIVE_NUMBER("구매 금액은 음수가 아닙니다."),
     NOT_DIVIDE_BY_LOTTO_PRICE("구매 금액은 로또 가격 단위이어야 합니다.");
 
     private final String errorMessage;

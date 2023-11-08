6b1c0de601f01a572259067ada6e855b2c109f77
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-08 22:21:58 +0900

    feat: 당첨금을 저장하기 위한 enum 정의

---
+++ src/main/java/lotto/constant/WinningAmount.java
@@ -0,0 +1,27 @@
+package lotto.constant;
+
+public enum WinningAmount {
+    NONE(0, 0),
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    WinningAmount(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+}

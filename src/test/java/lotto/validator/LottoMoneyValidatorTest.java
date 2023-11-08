107dc84f2e2566b73f7ccdcfbaaf3bf3524fbd36
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-08 23:18:27 +0900

    test: 테스트 코드 작성

--- src/test/java/lotto/validator/LottoMoneyValidatorTest.java
+++ src/test/java/lotto/validator/LottoMoneyValidatorTest.java
@@ -3,10 +3,12 @@ package lotto.validator;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.constant.ErrorMessages;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoMoneyValidatorTest {
 
+    @DisplayName("로또 금액은 정수이면서 1000원 단위여야 하고, 1000원 이상이어야 한다.")
     @Test
     void validate() {
         String notNumber = "notNumber";

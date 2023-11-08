2b2602e5095d33e67b63524cc3751f479eca05f5
최진원 <etudent39@gmail.com>
2023-11-08 23:01:25 +0900

    feat : 등수별 당첨 개수에 대한 출력문 구현

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -7,7 +7,12 @@ public class Output {
         LOTTO_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
         WIN_STATISTICS("당첨 통계"),
         SEPERATION_LINE("---"),
-        TOTAL_PROFIT_RATING("총 수익률은 %f%입니다.");
+        TOTAL_PROFIT_RATING("총 수익률은 %f%입니다."),
+        FIFTH_RANKING("3개 일치 (5,000원) - %d개%n"),
+        FOURTH_RANKING("4개 일치 (50,000원) - %d개%n"),
+        THIRD_RANKING("5개 일치 (1,500,000원) - %d개%n"),
+        SECOND_RANKING("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개%n"),
+        FIRST_RANKING("6개 일치 (2,000,000,000원) - %d개%n");
 
         private final String statement;
         Statement(String statement) {this.statement = statement;}
@@ -41,6 +46,25 @@ public class Output {
         printStatement(Statement.SEPERATION_LINE.statement);
     }
 
+    public static final void printFifthRanking(Integer count) {
+        System.out.printf(Statement.FIFTH_RANKING.statement, count);
+    }
+
+    public static final void printFourthRanking(Integer count) {
+        System.out.printf(Statement.FOURTH_RANKING.statement, count);
+    }
+
+    public static final void printThirdRanking(Integer count) {
+        System.out.printf(Statement.THIRD_RANKING.statement, count);
+    }
+
+    public static final void printSecondRanking(Integer count) {
+        System.out.printf(Statement.SECOND_RANKING.statement, count);
+    }
+
+    public static final void printFirstRanking(Integer count) {
+        System.out.printf(Statement.FIRST_RANKING.statement, count);
+    }
     public static final void printTotalProfitRating(Double rating) {
         System.out.printf(Statement.TOTAL_PROFIT_RATING.statement, rating);
     }

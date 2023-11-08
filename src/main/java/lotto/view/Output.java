51551595a69f5f29572561eb473bcb65dc482ea3
mac <seunghune12@gmail.com>
2023-11-08 14:33:34 +0900

    feat: 문장을 출력하는 기능

---
+++ src/main/java/lotto/view/Output.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+public class Output {
+
+    public static void console(Object message) {
+        System.out.printf(message.toString());
+    }
+    public static void consoleLine(Object message) {
+        System.out.println(message.toString());
+    }
+
+}

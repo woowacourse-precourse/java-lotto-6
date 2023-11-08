67b5377149b247eec681f2f5d974cc5ba621f9ef
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-08 18:38:22 +0900

    feat: 구입 금액을 입력 받고 검증하는 메소드 삭제 및 모든 lotto 들과 당첨 lotto 를 비교하여 모든 결과를 반환하는 메소드 구현
    구입 금액 검증은 Validation 에서 하므로 삭제

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -11,22 +11,23 @@ public class Lottos {
 
     public Lottos(RandomNumbers randomNumbers, InputView inputView) {
         int money = inputView.insertMoney();
-        validate(money);
         lottos = createLottos(money, randomNumbers);
     }
 
     private List<Lotto> createLottos(int money, RandomNumbers randomNumbers) {
         List<Lotto> lottos = new ArrayList<>();
-        int lotto_count = money / LOTTO_PRICE;
-        for (int i=0;i<lotto_count;i++) {
+        int lottoCount = money / LOTTO_PRICE;
+        for (int i = 0; i < lottoCount; i++) {
             lottos.add(new Lotto(randomNumbers.generate()));
         }
         return lottos;
     }
 
-    private void validate(int money) {
-        if (money % LOTTO_PRICE > 0) {
-            throw new IllegalArgumentException();
+    public List<Integer> matchAllLottoWithWinningOne(WinningLotto winningLotto) {
+        List<Integer> result = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            result.add(winningLotto.matchLottoWithWinningOne(lotto));
         }
+        return result;
     }
 }

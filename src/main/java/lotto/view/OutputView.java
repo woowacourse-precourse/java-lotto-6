ab7f2b1614abcd0c5437ed3061dd4e281fb2ca0e
pwrwpw <dlgustjr9912@gmail.com>
2023-11-08 19:14:29 +0900

    refactor : 메서드 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -32,21 +32,32 @@ public class OutputView {
         System.out.println(OutputMessages.WINNING_STATISTICS.getMessage());
         System.out.println(OutputMessages.WINNING_STATISTICS_DELIMITER.getMessage());
     }
-
     public void printStatistics(Map<Winning, Integer> winningCounts, double totalPrize, double totalCost) {
+        printMatchResults(winningCounts);
+        printProfit(totalPrize, totalCost);
+    }
+
+    private void printMatchResults(Map<Winning, Integer> winningCounts) {
         for (Winning result : Winning.values()) {
             if (result == Winning.NOT_WINNING) {
                 continue;
             }
-            String resultName = result.getMatchCount() + "개 일치";
-            if (result == Winning.FIFTH_BONUS) {
-                resultName = OutputMessages.FIFTH_BONUS_RESULT.getMessage();
-            }
+            String resultName = getResultName(result);
             int count = winningCounts.getOrDefault(result, 0);
             int prize = result.getWinningAmount().amount();
             String message = String.format(OutputMessages.MATCH_RESULT.getMessage(), resultName, prize, count);
             System.out.println(message);
         }
+    }
+
+    private String getResultName(Winning result) {
+        if (result == Winning.FIFTH_BONUS) {
+            return OutputMessages.FIFTH_BONUS_RESULT.getMessage();
+        }
+        return result.getMatchCount() + "개 일치";
+    }
+
+    private void printProfit(double totalPrize, double totalCost) {
         double profit = totalPrize - totalCost;
         double profitPercentage = HUNDRED + (profit / totalCost) * HUNDRED;
         System.out.printf(OutputMessages.PROFIT_MESSAGE.getMessage(), profitPercentage);

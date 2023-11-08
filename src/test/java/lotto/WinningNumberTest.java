2061b7973ca957541dc3ef71c34373c8aa99f6d2
Hajuuu <hpt0127@naver.com>
2023-11-08 17:56:14 +0900

    test(WinningNumberTest): 보너스 번호에 따른 예외 테스트 작성

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -13,17 +13,15 @@ class WinningNumberTest {
 
     @DisplayName("당첨 번호에 따른 예외 테스트")
     @ParameterizedTest
-    @MethodSource("parameterProvider")
+    @MethodSource("winningNumber")
     void checkWinningNumber(List<Integer> winningNumbers, String message) {
 
-        WinningNumber winningNumber = new WinningNumber();
-
-        Assertions.assertThatThrownBy(() -> winningNumber.checkWinningNumber(winningNumbers))
+        Assertions.assertThatThrownBy(() -> WinningNumber.checkWinningNumber(winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(message);
     }
 
-    private static Stream<Arguments> parameterProvider() {
+    private static Stream<Arguments> winningNumber() {
         return Stream.of(
                 Arguments.arguments(List.of(1, 2, 3, 4, 5, 6, 7), "[ERROR] 당첨 번호는 6개를 입력해야 합니다."),
                 Arguments.arguments(List.of(1, 1, 2, 3, 4, 5), "[ERROR] 당첨 번호는 서로 다른 번호를 입력해야 합니다."),
@@ -31,8 +29,23 @@ class WinningNumberTest {
         );
     }
 
-    @Test
-    void checkBonusNumber() {
+    @DisplayName("보너스 번호에 따른 예외 테스트")
+    @ParameterizedTest
+    @MethodSource("bonusNumber")
+    void checkBonusNumber(int bonusNumber, String message) {
+
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        
+        Assertions.assertThatThrownBy(() -> WinningNumber.checkBonusNumber(winningNumbers, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(message);
+    }
+
+    private static Stream<Arguments> bonusNumber() {
+        return Stream.of(
+                Arguments.arguments(56, "[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
+                Arguments.arguments(2, "[ERROR] 보너스 번호는 당첨 번호와 다른 숫자여야 합니다.")
+        );
     }
 
     @Test

5e3e1ebc7a801256027abaa77741b75fe24db41d
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 11:00:06 +0900

    :test_tube: Test: 로또 당첨내역 관한 테스트 추가
    - 당첨 내역에 관한 결과가 나올 시 count가 증가하는지 테스트

---
+++ src/test/java/lotto/model/LottoRankTest.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class LottoRankTest {
+
+    @ParameterizedTest
+    @MethodSource("lottoRankProvider")
+    @DisplayName("당첨 내역의 winning count가 증가하는지 테스트")
+    void increaseWinningCountByMatchingCount(int matchingCount, boolean isMatchBonus, LottoRank lottoRank) {
+        // given
+        MatchResult matchResult = new MatchResult(matchingCount, isMatchBonus);
+
+        // when
+        assertThat(lottoRank.getWinningCount()).isZero();
+
+        // then
+        LottoRank.increaseWinningCountByMatchingCount(matchResult);
+        assertThat(lottoRank.getWinningCount()).isEqualTo(1);
+    }
+
+    private static Stream<Arguments> lottoRankProvider() {
+        return Stream.of(
+                Arguments.of(6, false, LottoRank.FIRST),
+                Arguments.of(5, true, LottoRank.SECOND),
+                Arguments.of(5, false, LottoRank.THIRD),
+                Arguments.of(4, false, LottoRank.FOURTH),
+                Arguments.of(3, false, LottoRank.FIFTH)
+        );
+    }
+}

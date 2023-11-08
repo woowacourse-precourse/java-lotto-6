b8a4cd81d9a790554906e1ab032953538b6df5f1
hoyeon <highoption@naver.com>
2023-11-08 11:17:27 +0900

    test(BonusNumberTest) : 보너스 넘버가 45보다 크면 예외 발생

--- src/test/java/lotto/model/BonusNumberTest.java
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,15 @@ public class BonusNumberTest {
 
         assertThat(bonusNumber).isNotNull();
         assertThat(bonusNumber.getBonusNumber()).isEqualTo(Integer.parseInt(bonus));
+    }
+
+    @Test
+    @DisplayName("보너스 번호가 45보다 큰 숫자가 들어오면 예외 발생")
+    public void testBonusNumberByAboveRange() {
+        String bonus = "50";
+        assertThatThrownBy(() -> new BonusNumber(bonus))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
 }

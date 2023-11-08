20be20162794f8a15087c64aac7f2113b29dce0a
Hyeon-hak Kim <07ily@naver.com>
2023-11-08 14:17:04 +0900

    feat(LottoVendorTest): 로또 구매 시, 지불 금액이 양수가 아닌 경우

---
+++ src/test/java/lotto/LottoVendorTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoVendorTest {
+    private static final Integer LOTTO_PRICE = LottoEnum.PRICE.getValue();
+
+    @DisplayName("전달된 금액이 양수가 아니라면 예외가 발생한다.")
+    @Test
+    void purchaseWithoutMoney() {
+        assertThatThrownBy(() -> new LottoVendor(LOTTO_PRICE).purchaseAll(0))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

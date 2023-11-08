7cdd9d6cf557b9bb47ad6104dfd962b6c9abfe80
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 20:04:38 +0900

    implement: lotto.utils.Rank

---
+++ src/main/java/lotto/utils/Rank.java
@@ -0,0 +1,26 @@
+package lotto.utils;
+
+public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}

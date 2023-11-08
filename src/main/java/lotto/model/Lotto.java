8ea7732167965f6ad8c527112072d8ea60a58831
BangMingGing <privatebmk802@naver.com>
2023-11-08 20:51:46 +0900

    feat(Lotto): Add print Lotto function

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -30,5 +31,12 @@ public class Lotto {
         if (new HashSet<>(numbers).size() != numbers.size()) {
             throw new IllegalArgumentException(("[ERROR] 로또 번호는 중복되어선 안된다."));
         }
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.numbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", ")) + "]";
     }
 }

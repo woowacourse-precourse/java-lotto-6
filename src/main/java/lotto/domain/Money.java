81a57e977c74ab77fdf2316df078ed68c5e2e7b1
Jeongmin39 <wjdals20309@naver.com>
2023-11-08 22:16:31 +0900

    feat : lotto 구입금액 입력 및 발행수량 확인

---
+++ src/main/java/lotto/domain/Money.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import lotto.exception.ExceptionMessage;
+
+public class Money {
+
+    private static final int ZERO = 0;
+    private static final int THOUSAND = 1000;
+    private final int money;
+
+    public Money(int money){
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int inputMoney) {
+        validateZero(inputMoney);
+        validateThousand(inputMoney);
+    }
+
+    private void validateZero(int inputMoney) {
+        if (inputMoney == ZERO) {
+            throw new IllegalArgumentException(ExceptionMessage.ZeroMoneyError);
+        }
+    }
+
+    private void validateThousand(int inputMoney) {
+        if (inputMoney % THOUSAND != ZERO) {
+            throw new IllegalArgumentException(ExceptionMessage.DivideMoneyError);
+        }
+    }
+
+    public int getLottoCount() {
+        return money / 1000;
+    }
+}

22790b61470ea9622e1aa4a08cb691a38fd5636e
MyStraw <kikongdosa@gmail.com>
2023-11-08 22:10:37 +0900

    Refactor: 삭제 LottoUtils 유효성 검사
    -InputView에 입력관련 유효성 검사를 옮김으로서 중복된 유효성검사코드 제거

--- src/main/java/lotto/util/LottoUtils.java
+++ src/main/java/lotto/util/LottoUtils.java
@@ -1,32 +1,18 @@
 package lotto.util;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
-
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class LottoUtils {
     public static List<Integer> convertStringToLottoNumberList(String input) {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-    }
-
-    public static boolean validateLottoNumbers(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
-        }
-        if (numbers.stream().anyMatch(num -> num < 1 || num > 45)) {
-            throw new IllegalArgumentException("로또 번호는 1과 45 사이의 값이어야 합니다.");
-        }
-        long uniqueNumbersCount = numbers.stream().distinct().count();
-        if (uniqueNumbersCount != numbers.size()) {
-            throw new IllegalArgumentException("로또 번호에 중복된 값이 있습니다.");
-        }
-        return true;
     }
 
     public static List<Lotto> generateLottoPaper(int howManyLotto) {

823be3d31d355e24348044b20b2759676c709db4
DonghaOh <donghadarwin@naver.com>
2023-11-08 13:17:40 +0900

    feat: 금액 음수이거나 0이면 제외
    moneyNotZero 메소드

--- src/main/java/validator/MoneyValidator.java
+++ src/main/java/validator/MoneyValidator.java
@@ -1,8 +1,10 @@
 package validator;
 
 public class MoneyValidator {
-    public static void moneyValidate() {
-
+    public static void moneyValidate(String money) {
+        moneyNotNull(money);
+        moneyIsNumber(money);
+        int moneyNumber = Integer.parseInt(money);
     }
 
     private static void moneyNotNull(String money) {
@@ -13,6 +15,12 @@ public class MoneyValidator {
 
     private static void moneyIsNumber(String money) {
         if (!money.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해주세요.");
+        }
+    }
+
+    private static void moneyNotZero(int moneyNumber) {
+        if (moneyNumber == 0) {
             throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해주세요.");
         }
     }

8f1e89bb3a9804565981b8fa5f0b152c09d7ed0d
taekgyun <show9510@naver.com>
2023-11-08 22:12:41 +0900

    Domian.LottoCompany

--- src/main/java/lotto/domain/LottoCompany.java
+++ src/main/java/lotto/domain/LottoCompany.java
@@ -1,4 +1,77 @@
 package lotto.domain;
 
-public class LottoCompany {
+import static lotto.constant.Constant.INT_NULL;
+import static lotto.constant.Constant.PRIZE_RANK_WEIGHT;
+import static lotto.constant.Constant.ZERO;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import lotto.util.NumberValidate;
+
+public enum LottoCompany {
+    INSTANCE;
+    private static List<Integer> prizeNumbers = new ArrayList<Integer>();
+    private static int bonusNumber;
+
+    public void setPrizeNumbers(List<Integer> prizeNumbers) {
+        NumberValidate.validateLottoNumbers(prizeNumbers);
+        LottoCompany.prizeNumbers = prizeNumbers;
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        NumberValidate.validateBonusNumber(bonusNumber);
+        LottoCompany.bonusNumber = bonusNumber;
+    }
+
+    public int matchPrize(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        int weight = calcWeight(set);
+        weight = calcBonusNumber(weight, set);
+        return getRankIndex(weight);
+    }
+
+    private int calcWeight(Set<Integer> set) {
+        int weight = ZERO;
+        for (int number : prizeNumbers) {
+            weight = matchPrizeNumber(weight, set, number);
+        }
+        if (weight == prizeNumbers.size()) {
+            weight = PRIZE_RANK_WEIGHT.get("FIRST");
+        }
+        return weight;
+    }
+
+    private int matchPrizeNumber(int weight, Set<Integer> set, int number) {
+        if (set.contains(number)) {
+            return ++weight;
+        }
+        return weight;
+    }
+
+    private int calcBonusNumber(int weight, Set<Integer> set) {
+        if (weight == PRIZE_RANK_WEIGHT.get("THIRD")) {
+            return matchBonusNumber(set);
+        }
+        return weight;
+    }
+
+    private int matchBonusNumber(Set<Integer> set) {
+        if (set.contains(bonusNumber)) {
+            return PRIZE_RANK_WEIGHT.get("SECOND");
+        }
+        return PRIZE_RANK_WEIGHT.get("THIRD");
+    }
+
+    private int getRankIndex(int weight) {
+        if (weight >= PRIZE_RANK_WEIGHT.get("FIFTH")) {
+            return calcRankIndex(weight);
+        }
+        return INT_NULL;
+    }
+
+    private int calcRankIndex(int weight) {
+        return weight - PRIZE_RANK_WEIGHT.get("FIFTH");
+    }
 }

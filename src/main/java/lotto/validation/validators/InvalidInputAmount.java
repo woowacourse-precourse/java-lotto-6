9904bb45813b3beff122246b081c5dbb66fd529c
AidenRoh <colohrby@gmail.com>
2023-11-08 19:22:17 +0900

    refactor: add additional validate condition

--- src/main/java/lotto/validation/validators/InvalidInputAmount.java
+++ src/main/java/lotto/validation/validators/InvalidInputAmount.java
@@ -5,9 +5,12 @@ import lotto.domain.constant.LottoValue;
 import lotto.exception.ErrorMessage;
 import lotto.exception.LottoException;
 
-public class InvalidAmount extends LottoValue implements LottoGameValidator<Integer> {
+public class InvalidInputAmount extends LottoValue implements LottoGameValidator<Integer> {
 	@Override
 	public void validate(Integer value) {
+		if (value < MINIMUM_MONEY_UNIT) {
+			throw new LottoException(ErrorMessage.UNAFFORDABLE_STATUS);
+		}
 		if (value % MINIMUM_MONEY_UNIT != 0) {
 			throw new LottoException(ErrorMessage.INVALID_AMOUNT);
 		}

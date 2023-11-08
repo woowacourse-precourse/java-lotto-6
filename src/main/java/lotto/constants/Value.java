e8ddce035e9a579248d5831c64dc4850bb0c0aef
seastamp <seastamp10@gmail.com>
2023-11-08 16:17:32 +0900

    feat: 랜덤하게 정렬된 리스트 생성 기능 구현

--- src/main/java/lotto/constants/Value.java
+++ src/main/java/lotto/constants/Value.java
@@ -4,7 +4,9 @@ public enum Value {
     LOTTO_SIZE(6),
     UNIT_OF_AMOUNT(1000),
     REMAINDER_ZERO(0),
-    ZERO(0);
+    ZERO(0),
+    MIN_LOTTO_NUMBER(1),
+    MAX_LOTTO_NUMBER(45);
 
     private final int value;
 
--- src/main/java/lotto/utils/NumbersGenerator.java
+++ src/main/java/lotto/utils/NumbersGenerator.java
@@ -1,4 +1,18 @@
 package lotto.utils;
 
+import static lotto.constants.Value.LOTTO_SIZE;
+import static lotto.constants.Value.MAX_LOTTO_NUMBER;
+import static lotto.constants.Value.MIN_LOTTO_NUMBER;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
 public class NumbersGenerator {
+    public static List<Integer> randomNumbers() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER.get(), MAX_LOTTO_NUMBER.get(),
+                LOTTO_SIZE.get());
+        Collections.sort(numbers);
+        return numbers;
+    }
 }

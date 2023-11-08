3328417bb274b7c3371812bf82eaa393bc8b411a
GiJung <weeds2577@gmail.com>
2023-11-08 14:23:57 +0900

    test(PurchaseLottosTest): 구입한 로또들 객체 단위 테스트 작성

---
+++ src/test/java/lotto/domain/PurchaseLottosTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import lotto.exception.ErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PurchaseLottosTest {
+
+    @DisplayName("구입된 로또가 없는 경우")
+    @Test
+    void validatorSize() {{
+        assertThatThrownBy(() -> new PurchaseLottos(List.of()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorCode.NOT_FOUND_PURCHASE_LOTTO.getMessage());
+    }
+}

72fecab20ac661e266b67b889ecdfa0e959b078f
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:22:54 +0900

    test(RankTest): wildcard import를 single class import로 수정 및 static import

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -1,8 +1,12 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static lotto.domain.Rank.FIFTH;
+import static lotto.domain.Rank.FIRST;
+import static lotto.domain.Rank.FOURTH;
+import static lotto.domain.Rank.SECOND;
+import static lotto.domain.Rank.THIRD;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +14,11 @@ class RankTest {
     @DisplayName("count와 hasBonusNumber에 따라 순위를 반환한다.")
     @Test
     void getRank() {
-        assertThat(Rank.getRank(6, true)).isEqualTo(Rank.FIRST);
-        assertThat(Rank.getRank(6, false)).isEqualTo(Rank.FIRST);
-        assertThat(Rank.getRank(5, true)).isEqualTo(Rank.SECOND);
-        assertThat(Rank.getRank(5, false)).isEqualTo(Rank.THIRD);
-        assertThat(Rank.getRank(4, true)).isEqualTo(Rank.FOURTH);
-        assertThat(Rank.getRank(3, true)).isEqualTo(Rank.FIFTH);
+        assertThat(Rank.getRank(6, true)).isEqualTo(FIRST);
+        assertThat(Rank.getRank(6, false)).isEqualTo(FIRST);
+        assertThat(Rank.getRank(5, true)).isEqualTo(SECOND);
+        assertThat(Rank.getRank(5, false)).isEqualTo(THIRD);
+        assertThat(Rank.getRank(4, true)).isEqualTo(FOURTH);
+        assertThat(Rank.getRank(3, true)).isEqualTo(FIFTH);
     }
 }

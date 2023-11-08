7cd62c5103c6af0986495fb2c536934cac1ef814
Dawin <dawin002@naver.com>
2023-11-08 18:47:16 +0900

    test(LottoGeneratorTest): 로또 번호 생성 기능 테스트 추가
    로또 객체 생성 테스트 추가:
    - createInstanceOfLottoClass()
    로또 숫자 개수 테스트 추가:
    - createLottoWithSixNumbers()
    로또 숫자 범위 테스트 추가:
    - createNumberRangeFrom1to45Lotto()
    로또 숫자 중복 테스트 추가
    - createNotDuplicatedLottoNumbers()

---
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -0,0 +1,65 @@
+package lotto.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoGeneratorTest {
+
+    private LottoGenerator generator;
+    @BeforeEach
+    void setUp() {
+        generator = new LottoGenerator();
+    }
+
+    @DisplayName("Lotto 클래스의 객체를 생성한다.")
+    @Test
+    void createInstanceOfLottoClass() {
+        assertThat(generator.createSingleLotto())
+                .isInstanceOf(Lotto.class);
+    }
+
+    @DisplayName("6개의 숫자를 가진 로또 번호를 생성한다.")
+    @Test
+    void createLottoWithSixNumbers() {
+        assertThat(generator.createSingleLotto()
+                .getNumbers()
+                .size()
+        ).isEqualTo(6);
+    }
+
+    @DisplayName("1~45 사이의 숫자만 가진 로또 번호를 생성한다.")
+    @Test
+    void createNumberRangeFrom1to45Lotto() {
+        Lotto lotto = generator.createSingleLotto();
+
+        assertThat(lotto.getNumbers()
+                .stream()
+                .max(Integer::compareTo)
+                .orElseThrow()
+        ).isLessThanOrEqualTo(45);
+
+        assertThat(lotto.getNumbers()
+                .stream()
+                .min(Integer::compareTo)
+                .orElseThrow()
+        ).isGreaterThanOrEqualTo(1);
+    }
+
+    @DisplayName("중복이 없는 숫자를 가진 로또 번호를 생성한다.")
+    @Test
+    void createNotDuplicatedLottoNumbers() {
+        assertThat(generator.createSingleLotto()
+                .getNumbers()
+                .stream()
+                .distinct()
+                .toList()
+                .size()
+        ).isEqualTo(6);
+    }
+
+}

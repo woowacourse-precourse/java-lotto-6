7031577205a1f376239c59e2b21c2abffef5033c
mung96 <mung5079@gmail.com>
2023-11-08 12:04:59 +0900

    feat:Game() 생성자
    당첨번호 6자리와 보너스번호 1자리를 받아서 생성
    생성시 다음 3가지 검사를 실행
    -당첨번호가 6자리가 맞는지
    -당첨번호 6자리끼리 중복이 없는지
    -보너스 번호 1개는 당첨번호 6개와 중복되지 않는지

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,6 +1,35 @@
 package lotto.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
+    private final List<Integer> answer;
+    private final int bonusNumber;
+
+    public Game(List<Integer> answer, int bonusNumber) {
+        validateOverSize(answer);
+        validateDuplicateNumber(answer);
+        validateDuplicateBonusNumber(answer,bonusNumber);
+        this.answer = answer;
+        this.bonusNumber = bonusNumber;
+    }
+    private void validateOverSize(List<Integer> answer) {
+        if (answer.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDuplicateNumber(List<Integer> answer) {
+        if (!answer.equals(answer.stream().distinct().collect(Collectors.toList()))) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void validateDuplicateBonusNumber(List<Integer> answer, int bonusNumber) {
+        if (answer.contains(bonusNumber)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 
     public int countTickets(int money){
         validateDivisibleMoney(money);
@@ -11,4 +40,7 @@ public class Game {
             throw new IllegalArgumentException("[ERROR]1,000원 단위로만 입력가능합니다.");
         }
     }
+
+
+
 }

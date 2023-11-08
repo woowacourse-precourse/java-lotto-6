ac4bfe68085e99ff98ab9223cf6303604eb6140e
hyeonwoo <ziczin7176@gmail.com>
2023-11-08 19:54:15 +0900

    feat: 구입금액, 당첨, 보너스 번호 검증 구현

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -4,7 +4,25 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class InputValidation {
-    public int isNumber(String input) {
+
+    public void validatePayment(String input) {
+        isNumber(input);
+        isThousandMultiple(input);
+    }
+
+    public void validateWinningNumbers(String input) {
+        HashSet<String> winningNumbers = new HashSet<>(Arrays.asList(input.split(",")));
+        checkWinningNumbersAreNumeric(winningNumbers);
+        hasSixNumbers(winningNumbers);
+        checkWinningNumberRange(winningNumbers);
+    }
+
+    public void validateBonusNumber(String input) {
+        isNumber(input);
+        checkNumberRange(input);
+    }
+
+    private int isNumber(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException exception) {
@@ -12,17 +30,11 @@ public class InputValidation {
         }
     }
 
-    public void isThousandMultiple(String input) {
+    private void isThousandMultiple(String input) {
         int payment = isNumber(input);
         if (payment % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액이 1000원 단위가 아닙니다.");
         }
-    }
-
-    public void validateWinningNumbers(String input) {
-        HashSet<String> winningNumbers = new HashSet<>(Arrays.asList(input.split(",")));
-        checkWinningNumbersAreNumeric(winningNumbers);
-        hasSixNumbers(winningNumbers);
     }
 
     private void checkWinningNumbersAreNumeric(HashSet<String> winningNumbers) {
@@ -33,5 +45,16 @@ public class InputValidation {
         if (winningNumbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호가 6가지가 아닙니다.");
         }
+    }
+
+    private void checkNumberRange(String input) {
+        int number = Integer.parseInt(input);
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45 입니다.");
+        }
+    }
+
+    private void checkWinningNumberRange(HashSet<String> winningNumbers) {
+        winningNumbers.forEach(this::checkNumberRange);
     }
 }

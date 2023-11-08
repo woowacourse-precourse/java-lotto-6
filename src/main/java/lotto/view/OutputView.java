b5bfc09d8ba7abcb020313bb9f3fbc1903130c78
ycis5013@naver.com <ycis5013@naver.com>
2023-11-08 17:15:05 +0900

    feat(OutputView) : 당첨 내역 출력하는 함수 printResult() 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,9 +16,19 @@ public class OutputView {
     }
 
     public void printIssueNumbers(List<Integer>[] issueNumbers, int count) {
-        System.out.println(count+"개를 구매했습니다.");
-        for(int i=0;i<count;i++){
+        System.out.println("\n"+count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
             System.out.println(issueNumbers[i]);
         }
+    }
+
+    public void printResult(int[] sameNumberCount) {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + sameNumberCount[3] + "개");
+        System.out.println("4개 일치 (50,000원) - " + sameNumberCount[4] + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + sameNumberCount[5] + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + sameNumberCount[7] + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + sameNumberCount[6] + "개");
     }
 }

4f7149676d0a0781bbd5f8ec63a3a1bf03d7ef92
ddingmin <ddingminyt@gmail.com>
2023-11-08 19:42:59 +0900

    리팩토링: 매칭 결과 출력 메소드 분리

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -19,22 +19,26 @@ public class Output {
 	public static void winningResult(Result result) {
 		System.out.println("당첨 통계\n"
 			+ "---");
-		System.out.println(
-			Rank.FIFTH.getMatchNormalCount() + "개 일치 (" + Rank.FIFTH.getRewardString() + "원) - " + result.getCount(Rank.FIFTH)
-				+ "개");
-		System.out.println(
-			Rank.FOURTH.getMatchNormalCount() + "개 일치 (" + Rank.FOURTH.getRewardString() + "원) - " + result.getCount(
-				Rank.FOURTH) + "개");
-		System.out.println(
-			Rank.THIRD.getMatchNormalCount() + "개 일치 (" + Rank.THIRD.getRewardString() + "원) - " + result.getCount(Rank.THIRD)
-				+ "개");
-		System.out.println(
-			Rank.SECOND.getMatchNormalCount() + "개 일치, 보너스 볼 일치 (" + Rank.SECOND.getRewardString() + "원) - " + result.getCount(
-				Rank.SECOND) + "개");
-		System.out.println(
-			Rank.FIRST.getMatchNormalCount() + "개 일치 (" + Rank.FIRST.getRewardString() + "원) - " + result.getCount(Rank.FIRST)
-				+ "개");
+		matchingNormal(Rank.FIFTH, result);
+		matchingNormal(Rank.FOURTH, result);
+		matchingNormal(Rank.THIRD, result);
+		matchingBonus(Rank.SECOND, result);
+		matchingNormal(Rank.FIRST, result);
 		System.out.println("총 수익률은 " + result.getRateOfReturn() + "%입니다.");
+	}
+
+	private static void matchingNormal(Rank rank, Result result) {
+		System.out.println(
+			rank.getMatchNormalCount() + "개 일치 (" + rank.getRewardString() + "원) - " + result.getCount(
+				rank)
+				+ "개");
+	}
+
+	private static void matchingBonus(Rank rank, Result result) {
+		System.out.println(
+			rank.getMatchNormalCount() + "개 일치, 보너스 볼 일치 (" + rank.getRewardString() + "원) - "
+				+ result.getCount(
+				rank) + "개");
 	}
 
 	public static void newLine() {

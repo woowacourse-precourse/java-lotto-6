f5947f441782f474da55da830a737fb0a4ad72ed
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 11:58:36 +0900

    [feat] Validator - 구입 금액을 공백으로 입력했을 경우 검증 로직

--- src/main/java/lotto/Validator/MoneyValidator.java
+++ src/main/java/lotto/Validator/MoneyValidator.java
@@ -1,4 +1,19 @@
 package lotto.Validator;
 
+import lotto.Exception.MoneyErrorMessage;
+
 public class MoneyValidator {
+
+    public static String validatePurchaseAmount(String input) {
+        validateNotEmpty(input);
+
+        return input;
+    }
+
+    private static void validateNotEmpty(String input) {
+        if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException(MoneyErrorMessage.INVALID_MONEY_INPUT.getMessage());
+        }
+    }
+
 }

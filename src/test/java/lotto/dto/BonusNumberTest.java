fdc0710d9b89c358c303c4541dbe6f85ace9cf91
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-08 18:49:41 +0900

    feat: BonusNumber 예외처리 테스트 작성

---
+++ src/test/java/lotto/dto/BonusNumberTest.java
@@ -0,0 +1,24 @@
+package lotto.dto;
+
+import lotto.exception.LottoGameException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class BonusNumberTest {
+
+    @DisplayName("null값 입력시 예외가 발생")
+    @Test
+    void createBonusNumberByNull() {
+        assertThatThrownBy(() -> BonusNumber.create(""))
+                .isInstanceOf(LottoGameException.class);
+    }
+
+    @DisplayName("범위 밖 정수값 입력 시 예외 발생")
+    @Test
+    void createBonusNumberOverRange() {
+        assertThatThrownBy(() -> BonusNumber.create("46"))
+                .isInstanceOf(LottoGameException.class);
+    }
+}

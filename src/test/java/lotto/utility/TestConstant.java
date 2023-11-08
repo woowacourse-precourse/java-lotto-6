38fb79ec4851730e11be593e2b9490b5de63b0e4
minsub <chrkb1569@naver.com>
2023-11-08 14:00:40 +0900

    feat : 테스트 환경에서 사용하기 위한 상수들을 저장하는 열거형 클래스 생성

---
+++ src/test/java/lotto/utility/TestConstant.java
@@ -0,0 +1,22 @@
+package lotto.utility;
+
+public enum TestConstant {
+    INIT_COST(10_000),
+    LOTTO_PRICE(1_000),
+    LOTTO_PRIZE(10_000_000),
+    CONSTANT_ZERO(0),
+    CONSTANT_ONE(1),
+    VALID_GRADE(3),
+    NOT_VALID_GRADE(7)
+    ;
+
+    private final int constant;
+
+    TestConstant(int constant) {
+        this.constant = constant;
+    }
+
+    public int getConstant() {
+        return this.constant;
+    }
+}

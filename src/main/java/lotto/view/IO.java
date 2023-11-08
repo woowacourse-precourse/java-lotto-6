2f99da5a4d25194dd0da7cf71b7d7e0430caed98
kangyuseok <kingyuseok@naver.com>
2023-11-08 13:19:36 +0900

    Refactor: 당첨 일치 여부 출력문 수정

--- src/main/java/lotto/view/IO.java
+++ src/main/java/lotto/view/IO.java
@@ -1,7 +1,14 @@
 package lotto.view;
 
+import static lotto.view.constants.Front.FIVE_AND_BONUS_MATCH;
+import static lotto.view.constants.Front.FIVE_MATCH;
+import static lotto.view.constants.Front.FOUR_MATCH;
+import static lotto.view.constants.Front.SIX_MATCH;
+import static lotto.view.constants.Front.THREE_MATCH;
+
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
 import lotto.view.constants.Front;
 
@@ -14,9 +21,27 @@ public class IO {
         System.out.println(lottoTicketCount+message.getMessage());
     }
 
-    public static void viewLottoTickets(final List<Lotto>lottoTickets){
+    public static void showLottoTickets(final List<Lotto>lottoTickets){
         for (Lotto lotto : lottoTickets) {
             System.out.println(lotto);
         }
+    }
+
+    public static void requestWinningNumbers(Front message){
+        System.out.println(message.getMessage());
+    }
+
+    public static void requestBonusNumber(Front message){
+        System.out.println(message.getMessage());
+    }
+
+    public static void showResults(Front message, Integer[]matches){
+        System.out.println(message.getMessage());
+        System.out.println("---");
+        System.out.println(THREE_MATCH.getMessage() + String.valueOf(matches[3]) + "개");
+        System.out.println(FOUR_MATCH.getMessage() + String.valueOf(matches[4]) + "개");
+        System.out.println(FIVE_MATCH.getMessage() + String.valueOf(matches[5]) + "개");
+        System.out.println(FIVE_AND_BONUS_MATCH.getMessage() + String.valueOf(matches[7]) + "개");
+        System.out.println(SIX_MATCH.getMessage() + String.valueOf(matches[6]) + "개");
     }
 }

e0ae87308aefe7d358cfd55555e3c4cdc0d3e5bc
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-11-08 18:49:50 +0900

    fix: error 메시지 출력 후 재시작

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -14,22 +14,54 @@ import java.util.EnumMap;
 import java.util.List;
 
 public class LottoController {
+
+    private static Amount amount;
+    private static Lotto lotto;
+    private static Bonus bonus;
+
     private LottoService lottoService = LottoService.getInstance();
 
     public void run(){
-        String inputAmount = InputView.purchaseAmount();
-        Amount amount = new Amount(Converter.stringToInteger(inputAmount));
+        inputAmount();
         Purchase purchase = lottoService.start(amount);
         OutputView.showBuying(purchase);
 
-        List<Integer> numbers = Converter.stringToList(InputView.winningNumber());
-        Lotto lotto = new Lotto(numbers);
-        int bonusNum = Converter.stringToInteger(InputView.bonusNumber());
-        Bonus bonus = new Bonus(numbers, bonusNum);
+        inputWinning();
+        inputBonus();
         EnumMap<Reward, Integer> reward = lottoService.pick(lotto, bonus);
         OutputView.showResult(reward);
 
         Double rateOfReturn = lottoService.calRateOfReturn(amount);
         OutputView.showRateOfReturn(rateOfReturn);
+    }
+
+    private void inputAmount(){
+        try{
+            String inputAmount = InputView.purchaseAmount();
+            amount = new Amount(Converter.stringToInteger(inputAmount));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputAmount();
+        }
+    }
+
+    private void inputWinning(){
+        try{
+            List<Integer> numbers = Converter.stringToList(InputView.winningNumber());
+            lotto = new Lotto(numbers);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputWinning();
+        }
+    }
+
+    private void inputBonus(){
+        try{
+            int bonusNum = Converter.stringToInteger(InputView.bonusNumber());
+            bonus = new Bonus(lotto.getNumbers(), bonusNum);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            inputBonus();
+        }
     }
 }

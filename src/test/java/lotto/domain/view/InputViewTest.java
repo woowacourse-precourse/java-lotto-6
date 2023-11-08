731e48d468e46a1434e249102a47f0e98d9a344c
정찬영 <jcy0342@gmail.com>
2023-11-08 09:26:38 +0900

    test: 입력 값이 숫자가 아닐 경우 예외 발생

--- src/test/java/lotto/domain/view/InputViewTest.java
+++ src/test/java/lotto/domain/view/InputViewTest.java
@@ -1,22 +1,37 @@
 package lotto.domain.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InputViewTest {
+    InputView inputView;
+
+    @BeforeEach
+    void setUp() {
+        inputView = new InputView();
+    }
 
     @DisplayName("입력 받은 숫자 문자열을 숫자 타입으로 변환한다.")
     @Test
     void convertToInt() {
         //Given
         String input = "5000";
-        InputView inputView = new InputView();
         //When
         int number = inputView.convertToInt(input);
         //Then
         assertThat(number).isEqualTo(5000);
+    }
+
+    @Test
+    void convertToIntByNotNumber() {
+        assertThatThrownBy(() -> inputView.convertToInt("aaa1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자만 입력하실 수 있습니다.");
     }
 }

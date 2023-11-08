4d6ed53ece954d67a11e30d48ac7b2bebfaa62d8
lee jihyeon <lejinhy@gmail.com>
2023-11-08 16:09:51 +0900

    refactor: 매직 넘버 enum 활용

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
+import lotto.constant.MagicNumber;
 import lotto.constant.StatisticsMessage;
 import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,22 +16,23 @@ public class OutputView {
     public void printPurchasedLotteries(List<Lotto> lotteries) {
         List<Integer> lottoTemp;
         outputMessage = new StringBuilder();
-        outputMessage.append(lotteries.size()).append("개를 구매했습니다.\n");
+        outputMessage.append("\n").append(lotteries.size()).append("개를 구매했습니다.\n");
 
         for (Lotto lotto : lotteries) {
-            lottoTemp = lotto.getNumbers();
+            lottoTemp = new ArrayList<>(lotto.getNumbers());
             sortNumbers(lottoTemp);
             outputMessage.append(lottoTemp).append("\n");
         }
 
+        outputMessage.deleteCharAt(outputMessage.length() - 1);
         System.out.println(outputMessage);
     }
 
     public void printWinningStatistics(List<Integer> rank) {
-        System.out.println(StatisticsMessage.STATISTICS_INFO.getMessage());
+        System.out.println("\n" + StatisticsMessage.STATISTICS_INFO.getMessage());
 
         List<StatisticsMessage> messages = List.of(StatisticsMessage.values());
-        for (int i = 4; i >= 0; i--) {
+        for (int i = MagicNumber.RANK_COUNT.getNumber()-1; i >= 0; i--) {
             System.out.println(messages.get(i).getLottoStatisticsDescription(rank.get(i)));
         }
     }

a1576514f29e21e804943b1fcab3caa3b7e990f5
wonchanjoo <wcj0911@naver.com>
2023-11-08 14:19:32 +0900

    refactor: 로또 관련 정보를 클래스에 저장

---
+++ src/main/java/lotto/domain/LottoConfiguration.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class LottoConfiguration {
+
+    public static final int UNIT = 1_000;
+    public static final int NUMBER_RANGE_START = 1;
+    public static final int NUMBER_RANGE_END = 45;
+    public static final int NUMBERS_SIZE = 6;
+
+}

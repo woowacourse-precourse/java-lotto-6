f657d87d2f6aabc9aea496e35c52f25687fed114
yuseonjun <seonjun0906@gmail.com>
2023-11-08 19:12:10 +0900

    refactor(InputView): 오직 쉼표(,)로 구분짓는지만 확인하는 정규표현식으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
 
-    private static final String WINNING_NUMBERS_REGEX = "^(\\d(,\\d+)*|)$";
+    private static final String WINNING_NUMBERS_REGEX = "^\\w+(?:,\\w+)*$";
 
     private InputView() {
     }

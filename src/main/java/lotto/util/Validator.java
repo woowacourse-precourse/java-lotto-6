72122a28ef438680fbf5dee0dcf09849f88b3a89
jiwon1217 <koagjw@gmail.com>
2023-11-08 17:16:43 +0900

    :recycle: refactor: 에러 메시지 수정

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class Validator {
     private static final Pattern INPUT_PATTERN = Pattern.compile("^[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+,[\\d]+$");
     private static final String INVALID_INPUT_TYPE = "[ERROR] 입력은 숫자여야 합니다.";
-    private static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
+    private static final String INPUT_MUST_NOT_NEGATIVE = "[ERROR] 입력은 음수일 수 없습니다.";
     private static final String INVALID_INPUT_PATTERN = "[ERROR] 입력 형식과 맞지 않습니다.";
 
     private Validator() {
@@ -27,7 +27,7 @@ public class Validator {
 
     private static void validateInputRange(String input) {
         if (Integer.parseInt(input) < 0) {
-            throw new IllegalArgumentException(MONEY_MUST_NOT_NEGATIVE);
+            throw new IllegalArgumentException(INPUT_MUST_NOT_NEGATIVE);
         }
     }
 

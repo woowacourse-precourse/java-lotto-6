a248f4ca56395b1e9eb3f89c7441d0ddaa7eeb36
Jung Yunho <a01049048063@gmail.com>
2023-11-08 12:22:52 +0900

    test(TicketTest): ticket이 null이면 예외처리

---
+++ src/test/java/lotto/domain/TicketTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Ticket의")
+public class TicketTest {
+
+    @Test
+    @DisplayName("ticket이 null값이면 예외를 던지는가")
+    void is_null() {
+        //given
+        final Ticket ticket = new Ticket(null);
+
+        //when & then
+        assertThatThrownBy(ticket::toValue).isInstanceOf(IllegalStateException.class);
+    }
+}

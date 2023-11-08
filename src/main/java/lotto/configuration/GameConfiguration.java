4cc698733033f82d2424e7560c41e616c039f0a0
kmw2378 <rlarla677@gmail.com>
2023-11-08 11:49:47 +0900

    feat(lotto): 입력 구분자, 금액 소수점 구분자 및 정규 표현식 추가

--- src/main/java/lotto/configuration/GameConfiguration.java
+++ src/main/java/lotto/configuration/GameConfiguration.java
@@ -5,5 +5,8 @@ public final class GameConfiguration {
     public static final int MAX_LOTTO_NUMBER = 45;
     public static final int LOTTO_NUMBER_SIZE = 6;
     public static final int LOTTO_PRICE_PER_UNIT = 1000;
+    public static final String INPUT_SEPARATOR = ",";
+    public static final String DECIMAL_REGEX = "\\B(?=(\\d{3})+(?!\\d))";
+    public static final String DECIMAL_SEPARATOR = ",";
     private GameConfiguration() {}
 }

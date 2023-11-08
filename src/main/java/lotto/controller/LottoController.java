c5a9270568d122da24ad5d4ffbbe9fbad8ec75c7
haebyun <bhk4392@gmail.com>
2023-11-08 18:05:32 +0900

    feat(LottoController): 당첨 로또를 구성하는 숫자들과 보너스 숫자를 입력받아 와서 당첨 로또를 생성하도록 요청하는 기능 구현.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.service.LottoService;
 import lotto.util.Converter;
 import lotto.view.InputView;
@@ -7,12 +8,26 @@ import lotto.view.OutputView;
 
 public class LottoController {
     private final LottoService lottoService = new LottoService();
+
     public void investInLotto() {
         while (true) {
             try {
                 int purchaseAmount = Converter.convertPurchaseAmount(InputView.readPurchaseAmount());
                 lottoService.buyLotto(purchaseAmount);
                 OutputView.printBuyResults(lottoService.createBuyResults());
+                break;
+            } catch (IllegalArgumentException e) {
+                OutputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    public void drawLottoNumbers() {
+        while (true) {
+            try {
+                List<Integer> winningNumbers = Converter.convertWinningNumbers(InputView.readWinningNumbers());
+                int bonusNumber = Converter.convertBonusNumber(winningNumbers, InputView.readBonusNumber());
+                lottoService.makeWinningLotto(winningNumbers, bonusNumber);
                 break;
             } catch (IllegalArgumentException e) {
                 OutputView.printErrorMessage(e.getMessage());

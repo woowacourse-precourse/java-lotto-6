7f93953812a2c2a17f9af3c2205dce773705f522
aeyongdodam <kikititi0510@gmail.com>
2023-11-08 23:12:12 +0900

    feat(Constants): Constants 클래스 생성

---
+++ src/main/java/lotto/constants/Constants.java
@@ -0,0 +1,7 @@
+package lotto.constants;
+
+public class Constants {
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    public static final int LOTTO_SIZE = 6;
+}
---
+++ src/main/java/lotto/constants/MessageConstants.java
@@ -0,0 +1,14 @@
+package lotto.constants;
+
+public class MessageConstants {
+    public static final String UNIQUE_ERROR = "각 숫자는 고유해야함";
+    public static final String INVALID_INPUT_ERROR = "금액은 1000원 단위 양수만 가능";
+    public static final String NOT_INTEGER_ERROR = "정수만 입력 가능";
+    public static final String NOT_POSITIVE_ERROR = "양의 정수만 입력 가능";
+    public static final String INVALID_LOTTO_COUNT_ERROR = "로또 6개만 입력 가능";
+    public static final String ERROR_HEADER = "[ERROR]";
+
+    public static final String BLANK_INPUT_ERROR = "빈 문자열 입력 불가";
+    public static final String NULL_INPUT_ERROR = "null 입력 불가";
+    public static final String VALID_RANGE = "로또번호는 1과 45 사이만 가능";
+}

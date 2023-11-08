06fa56c4f7247b5572735f4b42109af3b0ff5109
TaeDongUm <etd93@naver.com>
2023-11-08 13:09:18 +0900

    [feat] InputUserInfoService 클래스 setUserLottos, printLottoNumbers 추가

--- src/main/java/lotto/service/InputUserInfoService.java
+++ src/main/java/lotto/service/InputUserInfoService.java
@@ -1,13 +1,17 @@
 package lotto.service;
 
 import lotto.domain.User;
+import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class InputUserInfoService {
 	private final OutputView outputView = new OutputView();
+	private final InputView inputView = new InputView();
 
-	public static User inputUserInfo() {
+	public User inputUserInfo() {
 		User user = new User(userInputBuyingPrice());
+		setUserLottos(user);
+		printLottoNumbers(user);
 		return user;
 	}
 
@@ -15,6 +19,16 @@ public class InputUserInfoService {
 		outputView.askingHowMuchPrice();
 		int buyingPrice = inputView.inputBuyingPrice();
 		return buyingPrice;
+	}
+	public void setUserLottos(User user) {
+		user.setLottos(getBuyingPrice(user));
+		outputView.printAmountOfBuyingLotto(user);
+	}
+	public void getBuyingPrice(User user) {
+		
+	}
+	public int printLottoNumbers(User user) {
+		return user.getBuyingLottosPrice();
 	}
 
 }

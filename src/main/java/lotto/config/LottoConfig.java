519cadc6d25b563b066150147a2a01883ce8d6dc
shortboy7 <thdwldnd7@naver.com>
2023-11-08 22:10:38 +0900

    feat: 로또 설정 값 추가

--- src/main/java/lotto/config/LottoConfig.java
+++ src/main/java/lotto/config/LottoConfig.java
@@ -2,7 +2,10 @@ package lotto.config;
 
 public enum LottoConfig {
     START_LOTTO_NUM(1),
-    END_LOTTO_NUM(45);
+    END_LOTTO_NUM(45),
+    LOTTO_COUPON_PRIZE(1000),
+    LOTTO_SLOT_NUMBER(8)
+    ;
     private int num;
     private LottoConfig(int num) {
         this.num = num;

8c4fb7069d6d899729559fad8bb1c6c81f1e49ea
wochae <officialwochae@gmail.com>
2023-11-08 20:47:00 +0900

    test(moneyInput): view test adapting 3 way format
    문자열 000으로 받아 온 것도 숫자 0으로 간주하기 위해 콜렉션 내 함수 적용 테스트

--- src/test/java/lotto/MoneyInputViewTest.java
+++ src/test/java/lotto/MoneyInputViewTest.java
@@ -5,18 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MoneyInputViewTest {
-
     @Test
-    void multiple_zero() {
+    void testZeroInput() {
+        // given
         String input = "000";
-        if (Integer.parseInt(input) == 0) {
-            System.out.println("0을 간주");
-        }
+        // when
+        int result = Integer.parseInt(input);
+        // then
+        assertEquals(0, result);
     }
-
-    @Test
-    void least_one_lotto() {
-
-    }
-
 }

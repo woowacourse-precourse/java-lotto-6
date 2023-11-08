950c1ec612dc4430953e4bdd8fbdd700bc665161
yohanii <dygks924@gmail.com>
2023-11-08 11:31:35 +0900

    refactor: Input클래스 핵심 로직과 UI로직 메소드 분리

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -4,15 +4,27 @@ import java.util.List;
 
 public class Input {
 
-    public List<Integer> inputWinNums() {
+    public static List<Integer> inputWinNums() {
         return null;
     }
 
-    public int inputBonusNum() {
+    public static int inputBonusNum() {
         return 0;
     }
 
-    public int inputPrice() {
+    public static int inputPrice() {
+        return 0;
+    }
+
+    public static List<Integer> validateWinNumsInput(String winNumsInput) {
+        return null;
+    }
+
+    public static int validateBonusNumInput(String bonusNumInput) {
+        return 0;
+    }
+
+    public static int validatePriceInput(String priceInput) {
         return 0;
     }
 }

e4dd06503eadae7ab4dff287f8c82e5ef23a1054
JeonJW24 <tyui153@naver.com>
2023-11-08 12:04:47 +0900

    feat: 로또 번호 비교 기능 구현

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,48 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int compare(List<Integer> winningNumbers) {
+        int count = 0;
+        for (int i : numbers) {
+            if (winningNumbers.contains(i)) {
+                winningNumbers.remove(i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Rank compareResult(int count, int bonusNumber) {
+        if (count == 6) {
+            return Rank.FIRST_PLACE;
+        }
+        if (count == 5) {
+            if (numbers.contains(bonusNumber)) {
+                return Rank.SECOND_PLACE;
+            }
+            return Rank.THIRD_PLACE;
+        }
+        if (count == 4) {
+            return Rank.FOURTH_PLACE;
+        }
+        if (count == 3) {
+            return Rank.FIFTH_PLACE;
+        }
+        return Rank.LAST_PLACE;
+    }
+}
---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,5 @@
+package lotto.domain;
+
+public enum Rank {
+    FIRST_PLACE, SECOND_PLACE, THIRD_PLACE, FOURTH_PLACE, FIFTH_PLACE, LAST_PLACE
+}

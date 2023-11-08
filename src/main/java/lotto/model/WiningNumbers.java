33a205f48cf73e636e11cc3b30dea9589ac68982
dustjq1004 <dustjq1005@naver.com>
2023-11-08 15:00:15 +0900

    yeonsup/refactor : 당첨 번호 Refactoring 후 담청 개수 구하는 기능 이동

---
+++ src/main/java/lotto/model/WiningNumbers.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class WiningNumbers {
+
+    private Lotto numbers;
+    private Bonus bonus;
+
+    public WiningNumbers(final Lotto numbers, final Bonus bonus) {
+        this.numbers = numbers;
+        this.bonus = bonus;
+    }
+
+    public WiningTypeCount countWining(List<Lotto> lottos) {
+        Map<WiningType, Integer> winingCountMap = WiningType.winingCountToMap();
+        lottos.forEach(lotto -> {
+            int correctCount = lotto.countCorrectNumbers(lotto);
+            boolean hasBonus = bonus.compareNumber(lotto);
+            WiningType wining = WiningType.findByWiningType(correctCount, hasBonus);
+            winingCountMap.computeIfPresent(wining, (key, count) -> ++count);
+        });
+        return new WiningTypeCount(winingCountMap);
+    }
+}

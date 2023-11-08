565d822c20eefd4c87287ae52f8504f6b1eb3673
Kim mingyu <minq0302@naver.com>
2023-11-08 23:16:53 +0900

    feat(domain): add getter

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Lottos {
     List<Lotto> lottos;
 
-    public Lottos(List<Lotto> lottos){
+    public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
     }
 }

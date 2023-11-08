3a79b861c7dd8bb2e7ef34af8df63d2720418fd3
정인모 <cat10830@naver.com>
2023-11-08 22:18:59 +0900

    feat : 생성한 복권 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -9,15 +9,39 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
+    public static List<Lotto> setLottoList(Integer purchaseCount) {
+
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; purchaseCount > i; i++) {
+            List<Integer> luckyNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            luckyNumbers.sort(null);
+
+            Lotto lotto = new Lotto(luckyNumbers);
+            lottoList.add(lotto);
+        }
+
+        return lottoList;
+    }
+
+    public static void printLotto(List<Lotto> lottoList) {
+        Integer lottoCount = lottoList.size();
+        System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+        for (int i = 0; lottoCount > i; i++) {
+            lottoList.get(i).printLotto();
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
         System.out.println("구매금액을 입력해 주세요.");
-        Integer purchaseAmount;
+        Integer purchaseCount;
 
         while (true) {
             try {
-                String purchaseAmountString = Console.readLine();
-                purchaseAmount = Validator.purchaseAmountValidation(purchaseAmountString);
+                String purchaseAmount = Console.readLine();
+                purchaseCount = Validator.purchaseAmountValidation(purchaseAmount);
                 break;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -25,13 +49,8 @@ public class Application {
             }
         }
 
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; purchaseAmount > i; i++) {
-            List<Integer> luckyNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            luckyNumbers.sort(null);
+        List<Lotto> lottoList = setLottoList(purchaseCount);
+        printLotto(lottoList);
 
-            Lotto lotto = new Lotto(luckyNumbers);
-            lottoList.add(lotto);
-        }
     }
 }

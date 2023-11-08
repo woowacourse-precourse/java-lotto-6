f6002178bf50f538dc5aa022fbb5c0d608c11fc4
KimHongHee <qaz101044@naver.com>
2023-11-08 21:04:06 +0900

    feature($Lotto) : add a features related to overall lotto function.

--- src/main/java/lotto/service/Lotto.java
+++ src/main/java/lotto/service/Lotto.java
@@ -1,22 +1,74 @@
 package lotto.service;
 
+import lotto.view.Exception_Message;
+
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Collections;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private static final int min_number = 1; // 추가된 부분
+    private static final int max_number = 45; // 추가된 부분
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validate_range(numbers);
+        validate_overlap(numbers);
+
+        Collections.sort(numbers);
+
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            Exception_Message.exception_check_sizes_equals_numbers_size(); // 추가된 부분
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validate_range(List<Integer> numbers) {
+        for (int winning_number = 0; winning_number < numbers.size(); winning_number++) {
+            if (numbers.get(winning_number) < min_number || numbers.get(winning_number) > max_number) {
+                Exception_Message.exception_check_range_equals_certain_ranges();
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validate_overlap(List<Integer> numbers) {
+        Set<Integer> check_overlap = new HashSet<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            check_overlap.add(numbers.get(i));
+        }
+
+        if (check_overlap.size() != 6) {
+            Exception_Message.exception_check_lotto_numbers_overlap();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validate_bonus_number(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            Exception_Message.exception_check_bonus_number_overlap();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean check_contain_numbers(int number) {
+        return numbers.contains(number);
+    }
+
+    public List<Integer> get_lotto_numbers() {
+        return numbers;
+    }
+
+    public int count_match_winning_numbers(Lotto winning_numbers) {
+        return (int) numbers.stream().filter(winning_numbers::check_contain_numbers).count();
+    }
 
 
 }
--- src/main/java/lotto/view/Exception_Message.java
+++ src/main/java/lotto/view/Exception_Message.java
@@ -7,6 +7,10 @@ public class Exception_Message {
     private static final String exception_check_input_equals_integer_MESSAGE = "[ERROR] 정수인 숫자만 입력해주십시오.";
     private static final String exception_check_input_equals_positive_number_MESSAGE = "[ERROR] 0보다 큰 숫자만 입력해야합니다.";
     private static final String exception_check_input_equals_satisfy_unit_MESSAGE = "[ERROR] " + lotto_purchase_unit + "원으로 나누어 떨어지는 금액만 입력해야합니다.";
+    private static final String exception_check_sizes_equals_numbers_size_MESSAGE = "[ERROR] 6개의 숫자를 입력해야합니다.";
+    private static final String exception_check_range_equals_certain_ranges_MESSAGE = "[ERROR] 0~45 사이의 숫자만 입력할 수 있습니다.";
+    private static final String exception_check_lotto_numbers_overlap_MESSAGE = "[ERROR] 중복된 숫자는 사용 불가합니다.";
+    private static final String exception_check_bonus_number_overlap_MESSAGE = "[ERROR] 보너스 번호는 앞의 6개의 숫자와 중복되지 않은 번호만 가능합니다.";
 
 
     public static void exception_check_input_equals_integer() {
@@ -14,11 +18,26 @@ public class Exception_Message {
     }
 
     public static void exception_check_input_equals_positive_number() {
-        System.out.println(exception_check_input_equals_positive_number_MESSAGE); // 정수 입력이 아닐 때
+        System.out.println(exception_check_input_equals_positive_number_MESSAGE); // 양수가 아닐 때
     }
 
     public static void exception_check_input_equals_satisfy_unit() {
-        System.out.println(exception_check_input_equals_satisfy_unit_MESSAGE); // 정수 입력이 아닐 때
+        System.out.println(exception_check_input_equals_satisfy_unit_MESSAGE); // 최소 단위로 나누어지지 않을 때
+    }
+
+    public static void exception_check_sizes_equals_numbers_size() {
+        System.out.println(exception_check_sizes_equals_numbers_size_MESSAGE); // 당첨 번호가 6개가 아닐 경우
+    }
+    public static void exception_check_range_equals_certain_ranges() {
+        System.out.println(exception_check_range_equals_certain_ranges_MESSAGE); // 로또 번호 범위가 벗어날 경우
+    }
+
+    public static void exception_check_lotto_numbers_overlap() {
+        System.out.println(exception_check_lotto_numbers_overlap_MESSAGE); // 중복된 로또 번호 입력
+    }
+
+    public static void exception_check_bonus_number_overlap() {
+        System.out.println(exception_check_bonus_number_overlap_MESSAGE); // 중복된 로또 번호 입력
     }
 
 }

1a4f939a767d492dfc791eb63c562360612a5070
daki <daki@pop-os.localdomain>
2023-11-08 12:34:20 +0900

    Feat(Lotto): 범위 validation 추가
    1부터 45까지의 범위로 제한되기 떄문에,
    해당 범위에 존재하는지 검증하는 코드를 추가 하였습니다!

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,11 +6,14 @@ import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_LENGTH = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
     private final List<Integer> lottoNumbers;
 
     public Lotto(List<Integer> lottoNumbers) {
         validateLength(lottoNumbers);
         validateDuplicate(lottoNumbers);
+        validateRange(lottoNumbers);
         this.lottoNumbers = lottoNumbers;
     }
 
@@ -33,6 +36,14 @@ public class Lotto {
         Set<Integer> uniqueNumbers = new HashSet<>(lottoNumbers);
         if(uniqueNumbers.size() != LOTTO_LENGTH) {
             throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다!");
+        }
+    }
+
+    private void validateRange(List<Integer> lottoNumbers) {
+        for (Integer number : lottoNumbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException("로또 번호는 " + MIN_NUMBER + " 보다 크고, " + MAX_NUMBER + "보다 작아야 합니다!");
+            }
         }
     }
 }

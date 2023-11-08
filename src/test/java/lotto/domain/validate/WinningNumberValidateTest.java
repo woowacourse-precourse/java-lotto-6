5ffc641d2308bc342ffa31b77184c298563aa127
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 12:21:56 +0900

    test(WinningNumberValidateTest): 당첨 번호 검증 기능 예외 테스트 코드 추가

--- src/test/java/lotto/domain/validate/WinningNumberValidateTest.java
+++ src/test/java/lotto/domain/validate/WinningNumberValidateTest.java
@@ -1,8 +1,10 @@
 package lotto.domain.validate;
 
+import lotto.domain.validate.exception.InputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class WinningNumberValidateTest {
@@ -18,5 +20,31 @@ class WinningNumberValidateTest {
         String validatedInput = "1,2,3,4,5,6";
 
         assertDoesNotThrow(() -> inputValidator.validateWinningNumber(validatedInput));
+    }
+    @Test
+    void 당첨_번호_입력_중복예외() {
+        String duplicatedInput = "1,2,2,4,5,6";
+
+        assertThatThrownBy(() -> inputValidator.validateWinningNumber(duplicatedInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NOT_DISTINCTNESS.getMessage());
+    }
+
+    @Test
+    void 당첨_번호_입력_범위예외() {
+        String outRangeInput = "46,102,2,4,5,6";
+
+        assertThatThrownBy(() -> inputValidator.validateWinningNumber(outRangeInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NOT_IN_RANGE_LOTTO_NUMBER.getMessage());
+    }
+
+    @Test
+    void 당첨_번호_입력_문자예외() {
+        String duplicatedInput = "1,2,3,4,5,a";
+
+        assertThatThrownBy(() -> inputValidator.validateWinningNumber(duplicatedInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NO_NUMBER.getMessage());
     }
 }

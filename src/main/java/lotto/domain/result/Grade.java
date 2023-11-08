75909bc2198b87b18d702c08de9194c2d606608e
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-08 15:04:12 +0900

    Refactor(Grade): Grade enum에 NOTHING 상수 추가

--- src/main/java/lotto/domain/result/Grade.java
+++ src/main/java/lotto/domain/result/Grade.java
@@ -5,7 +5,8 @@ public enum Grade {
     SECOND("5개 일치, 보너스 볼 일치", "30,000,000원"),
     THIRD("5개 일치", "1,500,000원"),
     FOURTH("4개 일치", "50,000원"),
-    FIFTH("3개 일치", "5,000원");
+    FIFTH("3개 일치", "5,000원"),
+    NOTHING("1 또는 2개 일치", "0원");
 
     private String condition;
     private String prize;

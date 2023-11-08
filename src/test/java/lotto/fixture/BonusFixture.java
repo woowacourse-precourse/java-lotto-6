b97c8c078b3c4e2af132e7a2c86cb9f15edcf76d
kmw2378 <rlarla677@gmail.com>
2023-11-08 21:24:06 +0900

    test(lotto): BonusFixture 필드 추가 당첨 번호에 포함되는 보너스 번호 필드를 추가하였습니다.

--- src/test/java/lotto/fixture/BonusFixture.java
+++ src/test/java/lotto/fixture/BonusFixture.java
@@ -3,8 +3,9 @@ package lotto.fixture;
 import lotto.domain.Bonus;
 
 public enum BonusFixture {
-    정상_보너스_번호(1),
+    당첨_번호에_포함되는_보너스_번호(1),
     당첨_보너스_번호(7),
+    정상_보너스_번호(10),
     범위를_벗어난_보너스_번호(50);
     private final int bonusNumber;
 

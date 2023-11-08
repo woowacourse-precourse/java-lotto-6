4e3833280afd308876c8e0027f332e69837d355a
peeaaccee <wlgp8958@naver.com>
2023-11-08 21:22:08 +0900

    test(WinningNumbersTest): 당첨번호 입력 시 쉼표를 제외한 특수문자를 입력하면 예외가 발생하는 테스트

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -17,6 +17,19 @@ class WinningNumbersTest {
             "1,2,3,4,5,6,,7"
     })
     void testConsecutiveCommasInInput(String input) {
-        assertThrows(IllegalArgumentException.class, () -> new WinningNumbers(Collections.singletonList(input)));
+        assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumbers(Collections.singletonList(input)));
+    }
+
+    @ParameterizedTest
+    @DisplayName("당첨번호 입력 시 쉼표를 제외한 특수문자를 입력하면 예외가 발생한다.")
+    @CsvSource({
+            "1,2,3,4!5,6",
+            "1,2,3~4,5,6",
+            "1@2,3,4,5,6"
+    })
+    void testSpecialCharactersInInput(String input) {
+        assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumbers(Collections.singletonList(input)));
     }
 }

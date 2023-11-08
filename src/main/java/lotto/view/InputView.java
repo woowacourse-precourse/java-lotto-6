3b5cd64f14e620f738aa688c7d4f970fb88bd6ad
realisshomyang <realisshoman@kakao.com>
2023-11-08 12:04:34 +0900

    feat : input 기능을 담당하는 InputView 구현

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class InputView {
+    private final InputParser inputParser;
+    private static final String INPUT_LOTTO_PURCHASE_PRICE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
+    private static final String INPUT_WINNING_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public InputView(InputParser inputParser) {
+        this.inputParser = inputParser;
+    }
+
+    public Integer getLottoPurchasePrice(){
+        System.out.println(INPUT_LOTTO_PURCHASE_PRICE);
+        return inputParser.parseInt(Console.readLine());
+    }
+
+    public List<Integer> getWinningLottoNumbers() {
+        System.out.println(INPUT_WINNING_LOTTO_NUMBERS);
+        return inputParser.parsingNumbers(Console.readLine());
+    }
+
+    public Integer getWinningBonusNumber() {
+        System.out.println(INPUT_WINNING_BONUS_NUMBER);
+        return inputParser.parseInt(Console.readLine());
+    }
+}

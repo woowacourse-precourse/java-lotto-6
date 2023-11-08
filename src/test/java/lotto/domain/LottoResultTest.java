499840711c6262ba91fc0bdbb3b575e839c86c0d
SeongIl <biuld1234@gmail.com>
2023-11-08 14:36:29 +0900

    test(LottoResultTest): replace 중복되는 코드 -> beforeEach

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -3,38 +3,32 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lotto.constants.Prize;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
+    private LottoResult lottoResult;
+
+    @BeforeEach
+    void beforeEach() {
+        // given
+        lottoResult = new LottoResult();
+    }
 
     @DisplayName("로또 결과 초기화 -> key를 Prize 열거형 초기화")
     @Test
     void initializeLottoResultOfKey() {
-        // given
-        LottoResult lottoResult;
-
-        // when
-        lottoResult = new LottoResult();
-
-        // then, key - Prize
+        // key - Prize
         for (Prize prize : Prize.values()) {
             assertThat(lottoResult.state.containsKey(prize)).isTrue();
         }
-
-
     }
 
     @DisplayName("로또 결과 초기화 -> value를 0으로 초기화")
     @Test
     void initializeLottoResultOfValue() {
-        // given
-        LottoResult lottoResult;
-
-        // when
-        lottoResult = new LottoResult();
-
-        // then, value - 0
+        // value - 0
         for (Prize prize : lottoResult.state.keySet()) {
             assertThat(lottoResult.state.get(prize)).isEqualTo(0);
         }
@@ -43,23 +37,13 @@ class LottoResultTest {
     @DisplayName("로또 결과 초기화 -> 크기는 5")
     @Test
     void initializeLottoResultOfSize() {
-        // given
-        LottoResult lottoResult;
-
-        // when
-        lottoResult = new LottoResult();
-
-        // then
         assertThat(lottoResult.state.size()).isEqualTo(5);
-
     }
-
 
     @DisplayName("수익률 계산")
     @Test
     void calculateEarningsRate() {
         // given
-        LottoResult lottoResult = new LottoResult();
         lottoResult.state.replace(Prize.FIFTH, 1);   // 5등 5000원, 1개
 
         // when

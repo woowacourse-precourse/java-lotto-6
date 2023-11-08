f2454245775c2a43936dd860e06d3ecd554ac904
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:41:06 +0900

    feat : lottoMachine 상수 선언

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,7 @@
 package lotto.domain;
 
 public class LottoMachine {
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
 }

841743ab570d64cf8a181408e461812fd2e77b25
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 22:08:38 +0900

    refactor:"클래스명 변경 및 상수변수명 변경"

---
+++ src/main/java/lotto/constant/OutputViewMessages.java
@@ -0,0 +1,11 @@
+package lotto.constant;
+
+public class OutputViewMessages {
+    public static final String PURCHASED_LOTTO_TICKETS = "개를 구매했습니다.";
+    public static final String LOTTO_WINNING_STATS_HEADER = "당첨 통계";
+    public static final String DASH_SEPARATOR = "---";
+    public static final String WINNING_RANK_COUNT_FORMAT = "%d개 일치 (%,d원) - %d개\n";
+    public static final String WINNING_RANK_BONUS_COUNT_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
+    public static final String TOTAL_PROFIT_MARGIN_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+
+}

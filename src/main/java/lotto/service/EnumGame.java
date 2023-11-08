6be0417767b6586d6af4a9ddf1f6d9aeb2074ab3
Nari <avakray@hansung.ac.kr>
2023-11-08 12:59:20 +0900

    feat(lotto): Enum 작성 (로또 가격 / 상금, 랭크별 당첨 번호 범위, 보너스 번호 여부)

---
+++ src/main/java/lotto/service/EnumGame.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+public enum EnumGame {
+    LOTTO_PRICE(1000, 0, false),
+    RANK_1(2000000000 ,6, false),
+    RANK_2(30000000, 5, true),
+    RANK_3(1500000, 5, false),
+    RANK_4(50000, 4, false),
+    RANK_5(5000, 3, false);
+
+    private final int MONEY;
+    private final int RANK;
+    private final boolean IS_BONUS;
+
+    EnumGame(int money, int rank, boolean isBonus) {
+        this.MONEY = money;
+        this.RANK = rank;
+        this.IS_BONUS = isBonus;
+    }
+
+    public int getMoney() {
+        return MONEY;
+    }
+
+    public int getRank() {
+        return RANK;
+    }
+
+    public boolean isBonus() {
+        return IS_BONUS;
+    }
+}

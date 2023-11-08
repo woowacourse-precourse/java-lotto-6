fd95ba32870739eb024327e98ce1a72b63e81a69
ddohyeong <dohyeong7026@gmail.com>
2023-11-08 13:06:51 +0900

    refactor(IllegalArgumentExceptionMessage) : IllegalArgumentException 처리 메시지 만 담당하도록 기능 분리 및 클래스명 리팩토링

--- src/main/java/exception/IllegalArgumentExceptionMessage.java
+++ src/main/java/exception/IllegalArgumentExceptionMessage.java
@@ -1,6 +1,6 @@
-package domain;
+package exception;
 
-public enum ExceptionMessage {
+public enum IllegalArgumentExceptionMessage {
 	LOTTO_OUT_OF_RANGE("1~45 사이의 숫자를 입력해 주세요"),
 	LOTTO_WRONG_FORMAT("로또번호는 6개를 쉼표로 구분해서 입력해 주세요"),
 	LOTTO_NUMBER_DUPLICATE("서로 다른 숫자를 입력해 주세요"),
@@ -8,13 +8,12 @@ public enum ExceptionMessage {
 	BONUS_OUT_OF_RANGE("1~45 사이의 숫자로 입력헤 주세요"),
 	PURCHASE_AMOUNT_NUMERIC("금액을 숫자로만 다시 입력해 주세요"),
 	PURCHASE_AMOUNT_THOUSAND_UNIT("입력 단위는 1000단위 입니다"),
-	PURCHASE_AMOUNT_NOT_EXCEED_ZERO("0을 초과한 금액을 입력을 입력해 주세요"),
-	NUMBERS_ONLY("숫자만 입력해 주세요");
+	PURCHASE_AMOUNT_NOT_EXCEED_ZERO("0을 초과한 금액을 입력을 입력해 주세요");
 
 	private static final String ERROR_TAG = "[ERROR] ";
 	private final String message;
 
-	ExceptionMessage(String message) {
+	IllegalArgumentExceptionMessage(String message) {
 		this.message = ERROR_TAG + message;
 	}
 

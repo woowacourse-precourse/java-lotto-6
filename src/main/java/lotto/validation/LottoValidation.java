0270d3d67c86e0edca743b93c76fe422493ddd87
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 16:30:42 +0900

    Feat[validation] 로또 번호 중복 값 검증

--- src/main/java/lotto/validation/LottoValidation.java
+++ src/main/java/lotto/validation/LottoValidation.java
@@ -1,6 +1,8 @@
 package lotto.validation;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoValidation {
     public static final int LOTTO_START_NUMBER = 1;
@@ -8,5 +10,10 @@ public class LottoValidation {
 
     public static boolean validateRange(List<Integer> numbers) {
         return numbers.stream().allMatch(number -> number >= LOTTO_START_NUMBER && number <= LOTTO_LAST_NUMBER);
+    }
+
+    public static boolean validateDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() == numbers.size();
     }
 }

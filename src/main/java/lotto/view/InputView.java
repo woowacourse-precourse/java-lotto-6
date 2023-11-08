db5a641f445e82736438c00defdc5e3c67ef060c
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-08 16:24:25 +0900

    feat: user input 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,20 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static int lottoBoughtPrice;
+    public static String userNum, userBonusStr;
+
+    public static void inputLottoBought() {
+        lottoBoughtPrice = Integer.parseInt(Console.readLine());
+    }
+
+    public static void inputUserNum() {
+        userNum = Console.readLine();
+    }
+
+    public static void inputUserBonusStr() {
+        userBonusStr = Console.readLine();
+    }
 }

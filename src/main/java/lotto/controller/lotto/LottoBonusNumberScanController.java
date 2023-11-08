ae5dc35590fe1d86d3589c80994846756a8bbdc5
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:29:10 +0900

    feat : 보너스 번호를 입력하고 저장하는 객체 생성

--- src/main/java/lotto/controller/lotto/LottoBonusNumberScanController.java
+++ src/main/java/lotto/controller/lotto/LottoBonusNumberScanController.java
@@ -1,11 +1,26 @@
 package lotto.controller.lotto;
 
-import lotto.domain.lotto.Lotto;
-import lotto.domain.lotto.LottoBonus;
+import lotto.controller.input.InputController;
+import lotto.controller.validation.bonus.LottoBonusNumberValueValidator;
+import lotto.model.lotto.Lotto;
+import lotto.model.lotto.LottoBonus;
+import lotto.view.MessageOutputView;
 
 public class LottoBonusNumberScanController {
-
+	MessageOutputView messageOutputView = new MessageOutputView();
+	InputController inputController = new InputController();
+	LottoBonusNumberValueValidator lottoBonusNumberValueValidator = new LottoBonusNumberValueValidator();
 	public void scanLottoBonusNumber(Lotto lotto, LottoBonus lottoBonus) {
+		try{
+			messageOutputView.printScanningLottoBonusNumberMessage();
+			String scannedLottoBonusNumberString = inputController.scanLottoNumberBonusString();
+			lottoBonusNumberValueValidator.checkValidationOfLottoBonusNumber(lotto,scannedLottoBonusNumberString);
+			Integer scannedLottoBonusNumber = Integer.parseInt(scannedLottoBonusNumberString);
+			lottoBonus.setLottoBonusNumber(scannedLottoBonusNumber);
+		}
+		catch(IllegalArgumentException illegalArgumentException){
+			scanLottoBonusNumber(lotto,lottoBonus);
+		}
 
 	}
 }
---
+++ src/main/java/lotto/model/lotto/LottoBonus.java
@@ -0,0 +1,12 @@
+package lotto.model.lotto;
+
+public class LottoBonus {
+	private Integer lottoBonusNumber;
+	public Integer getLottoBonusNumber() {
+		return lottoBonusNumber;
+	}
+
+	public void setLottoBonusNumber(Integer lottoBonusNumber) {
+		this.lottoBonusNumber = lottoBonusNumber;
+	}
+}

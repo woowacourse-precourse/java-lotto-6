9f8ea8ba0dd5037e12afc87a35f17bebd63b2f7c
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 22:37:46 +0900

    refactor:"로또 자동생성 클래스 수정"

--- src/main/java/lotto/domain/Computer.java
+++ src/main/java/lotto/domain/Computer.java
@@ -1,7 +1,7 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.constant.LottoInformation;
+import lotto.constant.LottoGameConfig;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +10,11 @@ public class Computer {
 
     private Lotto lotto;
 
-    public List<Lotto> drawLottoRandomNumber(int money) {
+    public List<Lotto> generateLottos(int money) {
         List<Lotto> lottos = new ArrayList<>();
-        for (int drawAttempt = 0;  drawAttempt < money / LottoInformation.MONEY_UNIT.getNumber(); drawAttempt++) {
+        for (int drawAttempt = 0; drawAttempt < money / LottoGameConfig.LOTTO_TICKET_PRICE_UNIT.getNumber(); drawAttempt++) {
             List<Integer> numbers = new ArrayList<>(
-                    Randoms.pickUniqueNumbersInRange(LottoInformation.MIN_LOTTO_NUMBER.getNumber(), LottoInformation.MAX_LOTTO_NUMBER.getNumber(), LottoInformation.LOTTO_NUMBER_TOTAL_SIZE.getNumber()));
+                    Randoms.pickUniqueNumbersInRange(LottoGameConfig.MIN_LOTTO_NUMBER_RANGE.getNumber(), LottoGameConfig.MAX_LOTTO_NUMBER_RANGE.getNumber(), LottoGameConfig.LOTTO_NUMBER_COUNT.getNumber()));
             lotto = new Lotto(numbers);
             lottos.add(lotto.getPuschaseLotto());
         }

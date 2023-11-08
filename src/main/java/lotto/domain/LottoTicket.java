b25c8816a0bf55af4f5910e1361e8cc04774c4d0
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 16:47:52 +0900

    refactor: change static
    메서드 이동에 따른 final -> static 변경

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -2,16 +2,15 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
+
 
 
 public class LottoTicket {
-    private final List<Lotto> lottoGames;
+    public static List<Lotto> lottoGames;
 
     public LottoTicket() {
-        this.lottoGames = new ArrayList<>();
+        lottoGames = new ArrayList<>();
     }
 
     public void addLottoGame(Lotto lotto) {
@@ -24,6 +23,7 @@ public class LottoTicket {
 
     public static List<Lotto> randomLottoNumbers(int lottoTicketCount) {
         List<Lotto> lottoRandomNumbers = new ArrayList<>();
+
         for (int i = 0; i < lottoTicketCount; i++) {
             lottoRandomNumbers.add(generateRandomLotto());
         }
@@ -34,29 +34,4 @@ public class LottoTicket {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return new Lotto(randomNumbers);
     }
-
-    public Map<Prize, Integer> initializePrizeStatistics() {
-        Map<Prize, Integer> initialStatistics = new EnumMap<>(Prize.class);
-        for (Prize prize : Prize.values()) {
-            if (prize != Prize.NONE) {
-                initialStatistics.put(prize, 0);
-            }
-        }
-        return initialStatistics;
-    }
-
-    public Map<Prize, Integer> compilePrizeStatistics(Lotto winningLotto, int bonusNumber) {
-        Map<Prize, Integer> matchNumberCount = initializePrizeStatistics();
-
-        // 상금의 수를 맵에 넣어주기
-        for (Lotto lottoGame : lottoGames) {
-            LottoResult lottoResult = new LottoResult(lottoGame, winningLotto, bonusNumber);
-            LottoResult.updateMatchNumberCount(matchNumberCount, lottoResult);
-        }
-
-        return matchNumberCount;
-    }
-
-
-
 }

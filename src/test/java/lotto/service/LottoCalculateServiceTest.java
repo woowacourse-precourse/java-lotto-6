c0e2f77009f716a74689482b033387f6751a8fa4
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 14:15:00 +0900

    test(LottoCalculateServiceTest) : 로또 번호 매칭 개수 5개 + 보너스 번호 테스트 코드 작성

--- src/test/java/lotto/service/LottoCalculateServiceTest.java
+++ src/test/java/lotto/service/LottoCalculateServiceTest.java
@@ -1,9 +1,11 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import lotto.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,6 +55,27 @@ public class LottoCalculateServiceTest {
         assertThat(result2).isEqualTo(true);
         assertThat(result3).isEqualTo(true);
         assertThat(result4).isEqualTo(false);
+    }
 
+    @DisplayName("로또 번호 매칭 개수 5개 + 보너스 번호 테스트")
+    @Test
+    public void isNumberMatchedFiveWithBonus() {
+        // given
+        int countContainsOfLotto = 5;
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(2);
+        lottoNumbers.add(3);
+        lottoNumbers.add(4);
+        lottoNumbers.add(5);
+        lottoNumbers.add(8);
+        Lotto lotto = new Lotto(lottoNumbers);
+        lotto.setBonusNumber(6);
+        // when
+        boolean result = lottoCalculateService.isNumberMatchedFiveWithBonus(countContainsOfLotto, numbers, lotto);
+        // then
+        assertThat(result).isEqualTo(true);
     }
 }

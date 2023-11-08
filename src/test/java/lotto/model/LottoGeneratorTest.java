a14a95c1a23a843b4a24e3fdac6ffbfda261d9fc
heiler <threepebbles@naver.com>
2023-11-08 22:54:02 +0900

    Test: LottoGenerator 테스트 코드 주석 추가

--- src/test/java/lotto/model/LottoGeneratorTest.java
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoGeneratorTest {
-    @DisplayName("로또 생성기가 로또 객체를 생성하는지 테스트")
+    @DisplayName("로또 생성기가 Lotto 객체를 생성하는지 테스트. 생성한 객체가 Lotto가 아니라면 테스트는 실패한다.")
     @Test
     void 로또_생성_테스트() {
         // given

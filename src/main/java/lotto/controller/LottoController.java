56810fe86dcb62003bd95b55670580885bea6632
DongHoonYu96 <dbehdgns71@naver.com>
2023-11-08 18:33:09 +0900

    feat : 6자리 랜덤넘버 출력구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,7 @@
 package lotto.controller;
 
+import static lotto.domain.LottoNumbers.setRandomLottoNumbers;
+
 import java.util.List;
 import lotto.domain.PlayerLottoAmount;
 import lotto.view.InputView;
@@ -19,12 +21,13 @@ public class LottoController {
 
     public void start() {
         int ticketCount = inputPlayerAmount();
-
         List<Integer> list = InputView.inputLottoNumber();
-
         int bonusNum = InputView.inputBonusNumber();
 
         OutputView.printPurchaseCount(ticketCount);
+        for (int i = 0; i < ticketCount; ++i) {
+            OutputView.printRandomLottoNumbers(setRandomLottoNumbers());
+        }
     }
 
     private int inputPlayerAmount() {
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,27 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String OUTPUT_PURCHASE_AMOUNT = "개를 구매했습니다.";
+    private static final String OPEN_BRACKET = "[";
+    private static final String CLOSE_RACKET = "]";
 
     public static void printPurchaseCount(int ticketCount) {
         System.out.println(ticketCount + OUTPUT_PURCHASE_AMOUNT);
+    }
+
+    public static String printRandomLottoNumbers(List<Integer> randomLottoNumbers) {
+        //[8, 21, 23, 41, 42, 43]
+        StringBuilder randomLottoNumbersResult = new StringBuilder();
+        randomLottoNumbersResult.append(OPEN_BRACKET);
+        for (int i : randomLottoNumbers) {
+            randomLottoNumbersResult.append(i);
+            randomLottoNumbersResult.append(", ");
+        }
+        randomLottoNumbersResult.delete(randomLottoNumbersResult.length() - 2, randomLottoNumbersResult.length());
+        randomLottoNumbersResult.append(CLOSE_RACKET);
+        System.out.println(randomLottoNumbersResult);
+        return String.valueOf(randomLottoNumbersResult);
     }
 }

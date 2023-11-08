2c2258a0bcd86716c69bec8a9a65a4d7870cec1f
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-08 19:16:12 +0900

    feat: read lotto numbers

--- src/main/java/lotto/common/console/Input.java
+++ src/main/java/lotto/common/console/Input.java
@@ -4,9 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     public static final String READ_PURCHASE_AMOUNT = "구입금액을 입력해주세요.";
+    public static final String READ_LOTTO_NUMBERS = "당첨 번호를 입력해 주세요.";
 
     public static String readPurchaseAmount() {
         Output.writeMessage(READ_PURCHASE_AMOUNT);
+
+        return Console.readLine();
+    }
+
+
+    public static String readLottoNumbers() {
+        Output.writeMessage(READ_LOTTO_NUMBERS);
 
         return Console.readLine();
     }

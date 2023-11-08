f0d8a8d9bda92e4e845d3e9d46faa253a685a657
jiwon1217 <koagjw@gmail.com>
2023-11-08 15:07:24 +0900

    :recycle: refactor: 요구 사항에서 주어진 메서드 이름 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,10 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+    private static final String INVALID_LOTTO_SIZE = "[ERROR] 로또는 6개의 숫자로 이루어져야 합니다.";
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoSize(numbers);
         this.numbers = numbers;
     }
 
@@ -15,9 +17,9 @@ public class Lotto {
         return Collections.unmodifiableList(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(INVALID_LOTTO_SIZE);
         }
     }
 }

8e6b32ddebfb3691eed1f07a587f0805d10b9f47
MingyeomKim <mingyum119@naver.com>
2023-11-08 13:21:37 +0900

    dosc(WinningResult): javadoc을 작성 후 메서드 순서 리팩토링

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -5,6 +5,9 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * 로또 당첨 결과를 저장하는 클래스 당첨 등수와 그에 해당하는 개수를 저장
+ */
 public class WinningResult {
     private Map<WinningType, Integer> winningResult;
 
@@ -14,8 +17,20 @@ public class WinningResult {
         generateWinningResult(lottos, drawnNumbers);
     }
 
+    /**
+     * 당첨 내역 객체를 생성하는 메서드
+     *
+     * @param lottos       발행한 로또들의 숫자들
+     * @param drawnNumbers 당첨 번호들과 보너스 번호
+     * @return 당첨 등수와 그에 해당하는 로또 개수
+     */
     public static WinningResult of(final Lottos lottos, final DrawnNumbers drawnNumbers) {
         return new WinningResult(lottos, drawnNumbers);
+    }
+
+    private void initializeWinningResult() {
+        Arrays.stream(WinningType.values())
+                .forEach(winning -> winningResult.put(winning, 0));
     }
 
     private void generateWinningResult(final Lottos lottos, final DrawnNumbers drawnNumbers) {
@@ -32,15 +47,22 @@ public class WinningResult {
         winningResult.put(winningType, winningResult.get(winningType) + 1);
     }
 
-    private void initializeWinningResult() {
-        Arrays.stream(WinningType.values())
-                .forEach(winning -> winningResult.put(winning, 0));
-    }
-
+    /**
+     * 당첨 등수에 해당하는 로또의 개수를 반환하는 메서드
+     *
+     * @param winningType 당첨 등수
+     * @return 당첨 등수에 해당하는 로또의 개수
+     */
     public Integer getValue(final WinningType winningType) {
         return winningResult.get(winningType);
     }
 
+    /**
+     * 당첨 내역으로부터 수익률을 계산하는 메서드.
+     *
+     * @param cost 로또를 구매한 총 비용
+     * @return 총 수익에서 비용을 나눈 수익률
+     */
     public double calculateEarningsRate(final int cost) {
         return getEarnings() / cost;
     }

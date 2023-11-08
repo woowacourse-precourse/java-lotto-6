45eb08bae519ba41102514e39df724d41b8dc142
KimKyuHwan <vkdls754@gmail.com>
2023-11-08 20:08:49 +0900

    refactor(Application): 의존성 주입 코드로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,13 +4,18 @@ import lotto.repository.LottoRepository;
 import lotto.repository.MemoryRandomLottoRepository;
 import lotto.service.LottoService;
 import lotto.service.LottoServiceImpl;
+import lotto.service.RateOfReturnCalculationService;
+import lotto.service.RateOfReturnCalculationServiceImpl;
+import lotto.service.WinningCalculationService;
+import lotto.service.WinningCalculationServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
         LottoRepository lottoRepository = new MemoryRandomLottoRepository();
         LottoService lottoService = new LottoServiceImpl(lottoRepository);
-
-        LottoGameManager lottoGameManager = new LottoGameManager(lottoService);
+        WinningCalculationService winningCalculationService = new WinningCalculationServiceImpl(lottoRepository);
+        RateOfReturnCalculationService rateOfReturnCalculationService = new RateOfReturnCalculationServiceImpl();
+        LottoGameManager lottoGameManager = new LottoGameManager(lottoService, winningCalculationService, rateOfReturnCalculationService);
         lottoGameManager.run();
     }
 }

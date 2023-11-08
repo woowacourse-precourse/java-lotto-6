db6e0235cc08fa9e8bd411ebf04bba3404c7f859
taekgyun <show9510@naver.com>
2023-11-08 22:59:04 +0900

    MoneyValidateTest

---
+++ src/test/java/lotto/MoneyValidateTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.util.MoneyValidate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyValidatorTest {
+    @DisplayName("1000원 이하의 값을 입력하면 예외가 발생한다.")
+    @Test
+    void testUnderThousand() {
+        assertThatThrownBy(() -> MoneyValidate.validateMoney(500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("1000원 단위가 아닌 값을 입력하면 예외가 발생한다.")
+    @Test
+    void testThousandUnits() {
+        assertThatThrownBy(() -> MoneyValidate.validateMoney(3500))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

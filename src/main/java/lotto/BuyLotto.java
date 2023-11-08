0ff42eb0411993edb9574df7e637a8f8e4c4d0fc
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-08 19:05:37 +0900

    feat: add buying lotto process
    Add class BuyLotto
    - receive input budget
    - validate input
    - calculate the number of lotto tickets purchased
    
    Add class LottoProcess for control the overall process flow.

---
+++ src/main/java/lotto/BuyLotto.java
@@ -0,0 +1,45 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class BuyLotto {
+    private int numLotto;
+    private boolean checkValidBudget = true;
+    public int BuyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        while (checkValidBudget) {
+            inputBudget();
+        }
+        return numLotto;
+    }
+
+    private void inputBudget() {
+        try {
+            String budget = readLine();
+            calculateNumLotto(budget);
+            checkValidBudget = true;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            checkValidBudget = true;
+        }
+    }
+
+    public void calculateNumLotto(String input) {
+        validateIsNum(input);
+        int budget = Integer.parseInt(input);
+        validateIsThousand(budget);
+        numLotto = budget / 1000;
+    }
+
+    private void validateIsNum(String buget) {
+        if (!buget.matches("[+-]?\\d*(\\.\\d+)?")){
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해주세요.");
+        }
+    }
+
+    private void validateIsThousand(int budget) {
+        if (budget % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액을 1,000단위로 입력해주세요.");
+        }
+    }
+}
---
+++ src/main/java/lotto/LottoProcess.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class LottoProcess {
+    private int numLotto;
+
+    public void setNumLotto() {
+        numLotto = new BuyLotto().BuyLotto();
+
+
+    }
+
+}

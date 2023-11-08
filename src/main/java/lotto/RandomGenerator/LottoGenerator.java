050795b6eccd23869eb6be62eb14512d57725370
Sanghee0820 <lsh277604@gmail.com>
2023-11-08 16:07:06 +0900

    Fix: Use camp.nextstep.edu.missionutils.Randoms Library

--- src/main/java/lotto/RandomGenerator/LottoGenerator.java
+++ src/main/java/lotto/RandomGenerator/LottoGenerator.java
@@ -1,29 +1,11 @@
 package lotto.RandomGenerator;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LottoGenerator {
-    private List<Integer> numbers;
-
-    public LottoGenerator() {
-        this.numbers = IntStream.rangeClosed(1, 45)
-                .boxed()
-                .collect(Collectors.toList())
-        ;
-    }
-
-    public void numberShuffle() {
-        Collections.shuffle(numbers);
-    }
-
     public List<Integer> getLottoNumber() {
-        numberShuffle();
-        List<Integer> selectedNumbers = new ArrayList<>(numbers.subList(0, 6));
-        Collections.sort(selectedNumbers);
+        List<Integer> selectedNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return selectedNumbers;
     }
 }

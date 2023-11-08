4bcb5c71a78e8a25911edae1b5527d918ff3867e
HyeIn <helen6662@naver.com>
2023-11-08 22:02:00 +0900

    feat : 기능별 출력 문구 / 로또 번호 비교 및 검증

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,28 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void validateDuplicate(List<Integer> winNum){
+        Set<Integer> removeDuplicate = new HashSet<>(winNum);
+        if(winNum.size() != removeDuplicate.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void printLotto(){
+        System.out.println(numbers);
+    }
+
+    public boolean isExistNumber(int number){
+        return numbers.contains(number);
+    }
+
+    public int countExistNumber(Lotto l){
+        int count = 0;
+        for(int i=0;i<5;i++){
+            if(numbers.contains(l.numbers.get(i))){
+                count++;
+            }
+        }
+        return count;
+    }
 }

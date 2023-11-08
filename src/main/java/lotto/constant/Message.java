c159c507f62975fb659ccbd096e66addeef0d5d1
Cycrypto <jh01love@naver.com>
2023-11-08 12:41:37 +0900

    refactor:NULL 열거형 추가

--- src/main/java/lotto/constant/Message.java
+++ src/main/java/lotto/constant/Message.java
@@ -1,7 +1,7 @@
 package lotto.constant;
 
 public enum Message {
-
+    NULL(null),
     INPUT_BUDGET("구입금액을 입력해 주세요."),
     INPUT_WINNING_NUMBER("당첨번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),

e2b39025e6e0468c9ab87deb7f38b9eb8d5ebe79
kang jihyeong <her4386@gmail.com>
2023-11-08 19:11:19 +0900

    [test] 로또 번호 숫자 외 유효성 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.domain;
 
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
@@ -19,10 +19,28 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 6개보다 작을 경우 예외 발생")
+    @Test
+    void validate_isRightLottoCnt() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 1보다 작을 경우 예외 발생")
+    @Test
+    void validate_isRightLottoRange1() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호 45보다 클 경우 예외 발생")
+    @Test
+    void validate_isRightLottoRange2() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 99)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

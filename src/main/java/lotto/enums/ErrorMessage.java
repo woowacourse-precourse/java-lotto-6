176b91280675c76a9cfed69ac9f2d491fa235d4f
Sinorang <aodkrak@naver.com>
2023-11-08 22:34:47 +0900

    fix. 출력 메시지 수정

--- src/main/java/lotto/enums/ErrorMessage.java
+++ src/main/java/lotto/enums/ErrorMessage.java
@@ -3,7 +3,7 @@ package lotto.enums;
 public enum ErrorMessage {
     INVALID_AMOUNT_INPUT(ConstVariable.STANDARD.getValue() + "원 단위로 입력해야 합니다."),
     INVALID_WIN_NUMBERS_INPUT(
-            "당첨 로또 번호는" + ConstVariable.MIN_NUMBER.getValue() + "부터" + ConstVariable.MAX_NUMBER.getValue()
+            "당첨 로또 번호는 " + ConstVariable.MIN_NUMBER.getValue() + "부터" + ConstVariable.MAX_NUMBER.getValue()
                     + "사이의 숫자이면서, 쉼표로 구분되어야 합니다."),
     INVALID_NUMBER("로또 번호는 " + ConstVariable.MIN_NUMBER.getValue() + "부터" + ConstVariable.MAX_NUMBER.getValue()
             + "사이의 숫자여야 합니다."),

ab69f7247e919907b6635fd4f769b76a05ff5187
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 14:05:10 +0900

    test(util) : 당첨 번호에 숫자 이외의 갑이 들어갈 경우 예외 발생 테스트

--- src/test/java/lotto/util/validator/LottoInputValidatorTest.java
+++ src/test/java/lotto/util/validator/LottoInputValidatorTest.java
@@ -14,10 +14,21 @@ class LottoInputValidatorTest {
     @Test
     void inputWinningNumberOverBySize() {
         //given
-        String values = "1234567";
+        String values = "1,2,3,4,5,6,7";
 
         //when,then
         Assertions.assertThatThrownBy(() -> lottoInputValidator.validate(values))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessageConstant.LOTTO_LENGTH_NOT_SIX_MESSAGE.getMessage());
+    }
+
+    @DisplayName("당첨 번호에 숫자 이외의 값이 들어가있을 경우 예외가 발생한다.")
+    @Test
+    void inputWinningNumbersNotNumber() {
+        //given
+        String values = "1,w,2,3,4,5";
+
+        //when,then
+        Assertions.assertThatThrownBy(() -> lottoInputValidator.validate(values))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessageConstant.LOTTO_NUMBER_IS_NOT_NUMERIC.getMessage());
     }
 }

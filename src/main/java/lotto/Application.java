99c198cdba4961abe0cefc93b8c6fa6ecd9d6972
jyeun722 <skqhs7276@gamil.com>
2023-11-08 18:20:33 +0900

    Feat: Input bonus number with exception

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -41,11 +41,42 @@ public class Application {
         return winningLotto;
     }
 
+    private static LottoBonus setLottoBonusNumber(List<Integer> WinningNumbers) {
+        try {
+            System.out.println("보너스 번호를 입력해 주세요.");
+            int userInputNumber = inputBonusNumber();
+
+            return generateBonusNumber(userInputNumber, WinningNumbers);
+        } catch (IllegalArgumentException e) {
+            return setLottoBonusNumber(WinningNumbers);
+        }
+    }
+
+    private static int inputBonusNumber() {
+        try {
+            int userInputNumber = Integer.parseInt(Console.readLine());
+
+            return userInputNumber;
+        } catch (NumberFormatException e) {
+            System.out.println(ERROR_MESSAGE + " 보너스 번호는 숫자만 입력해야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static LottoBonus generateBonusNumber(int bonus, List<Integer> winningNumbers) {
+        LottoBonus lottoBonus = new LottoBonus(bonus, winningNumbers);
+
+        return lottoBonus;
+    }
+
     public static void main(String[] args) {
         LottoMoney lottoMoney = new LottoMoney();
         System.out.println(lottoMoney.money);
 
         Lotto winningNumbers = setLottoWinningNumbers();
         System.out.println(winningNumbers.numbers);
+
+        LottoBonus bonusNumber = setLottoBonusNumber(winningNumbers.numbers);
+        System.out.println(bonusNumber.bonus);
     }
 }

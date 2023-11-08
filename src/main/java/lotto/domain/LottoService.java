a4a862f418bd0fd6f5da96382dcc7c7b8735ca6b
yebin <dev.devin.kr@gmail.com>
2023-11-08 17:28:24 +0900

    feat: add lotto service interface for domain logic

---
+++ src/main/java/lotto/domain/LottoService.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.util.WinningResult;
+
+public interface LottoService {
+
+    /**
+     * 로또 개수를 저장하고 로또를 개수만큼 발행한다
+     * @param amount 로또 구매 금액
+     */
+    public void saveUser(int amount);
+
+    /**
+     * 로또 개수를 조회한다
+     * @return 로또 개수
+     */
+    public int getCount();
+
+    /**
+     * 로또 번호(들을)를 조회한다
+     * @return 로또 번호 목록
+     */
+    public List<Lotto> getLottos();
+
+    /**
+     * 로또 당첨 결과를 확인한다
+     * @return 로또 당첨 결과 목록
+     */
+    public List<WinningResult> checkWinningResult();
+
+    /**
+     * 로또 수익률을 확인한다
+     * @return 로또 수익률 (소수점 2자리에서 반올)
+     */
+    public double checkEarningRate(List<WinningResult> results);
+}

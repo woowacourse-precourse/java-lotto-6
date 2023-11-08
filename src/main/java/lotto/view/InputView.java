2965b42892d33bd6af4f126280ce6d1ec78ccdb5
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 15:40:35 +0900

    [FEAT] InputView에 splitInput 함수 구현
    winningNumber 입력을 위해 splitInput 함수 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,21 +2,23 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Objects;
+import java.util.List;
+import java.util.ArrayList;
 
 public class InputView {
     private static final String TOTAL_COST = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    static final String DELIMITER = ",";
 
     public static int totalCost() {
         System.out.println(TOTAL_COST);
         return parseToInt(input());
     }
 
-    public static String winningNumber() {
+    public static List<Integer> winningNumber() {
         System.out.println(WINNING_NUMBER);
-        return input();
+        return splitInput(input());
     }
 
     public static int bonusNumber() {
@@ -43,5 +45,15 @@ public class InputView {
         if (input.isBlank()) {
             throw new IllegalArgumentException("입력값을 입력해주세요.");
         }
+    }
+
+    private static List<Integer> splitInput(String input) {
+        String[] inputParts = input.split(DELIMITER);
+        List<Integer> result = new ArrayList<>();
+
+        for (String part : inputParts) {
+            result.add(parseToInt(part));
+        }
+        return result;
     }
 }

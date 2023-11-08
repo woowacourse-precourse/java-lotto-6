b8387ae47b47d50d81f4b73d812d806ef9468b60
carpediem365 <k2309123@gmail.com>
2023-11-08 20:32:22 +0900

    feat(lotto):사용자로부터 당첨번호와 보너스 번호 입력 받는 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
 import lotto.domain.LottoComputer;
 import lotto.domain.LottoPlayer;
 
 public class Application {
         public static String money;
         public static int ticketNumber;
+        public static List<Integer> playerNumber = new ArrayList<>();
+        public static int playerBonusNumber;
     public static void main(String[] args) {
-        LottoPlayer.buyLotto();
-        LottoComputer.makingLottoIndex();
+//        LottoPlayer.buyLotto();
+//        LottoComputer.makingLottoIndex();
+
+        LottoPlayer.inputNumber();
+        System.out.println(playerNumber);
+        System.out.println(playerBonusNumber);
     }
 }

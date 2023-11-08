175c83852753a9eb01ea6b4488d42fe9ccf1ae38
yebin <dev.devin.kr@gmail.com>
2023-11-08 23:12:28 +0900

    feat: add constants class

--- src/main/java/lotto/domain/LottoServiceImpl.java
+++ src/main/java/lotto/domain/LottoServiceImpl.java
@@ -3,15 +3,12 @@ package lotto.domain;
 import java.util.List;
 import lotto.model.Lotto;
 import lotto.model.User;
+import lotto.util.Constants;
 import lotto.util.RandomUtil;
 import lotto.util.WinningCase;
 import lotto.util.WinningResult;
 
 public class LottoServiceImpl implements LottoService {
-
-    static final int LOTTO_NUMBER_MIN = 1;
-    static final int LOTTO_NUMBER_MAX = 45;
-    static final int LOTTO_NUMBER_COUNT = 6;
 
     private User user;
 
@@ -21,7 +18,7 @@ public class LottoServiceImpl implements LottoService {
 
         for (int i = 0; i < user.getCount(); i++) {
             List<Integer> lottoTicket = RandomUtil.getRandomDistinctIntegers(
-                    LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT);
+                    Constants.LOTTO_NUMBER_MIN, Constants.LOTTO_NUMBER_MAX, Constants.LOTTO_NUMBER_COUNT);
             user.addLotto(new Lotto(lottoTicket));
         }
     }
@@ -42,7 +39,9 @@ public class LottoServiceImpl implements LottoService {
 
         for (Lotto ticket : user.getLottoTickets()) {
             WinningCase winningCase = ticket.confirmWin(winningNumbers, bonusNumber);
-            if (winningCase == null) continue;
+            if (winningCase == null) {
+                continue;
+            }
             winningResult.addCount(winningCase);
         }
         winningResult.setEarningRate(user.getAmount());
---
+++ src/main/java/lotto/util/Constants.java
@@ -0,0 +1,8 @@
+package lotto.util;
+
+public class Constants {
+
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_NUMBER_COUNT = 6;
+}

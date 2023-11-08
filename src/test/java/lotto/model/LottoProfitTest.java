34d5eee2107941258103346f1863989d878da81c
seondays <agseon9@gmail.com>
2023-11-08 14:16:02 +0900

    test(Rank): 테스트코드 Rank enum 변경 적용

--- src/test/java/lotto/model/LottoProfitTest.java
+++ src/test/java/lotto/model/LottoProfitTest.java
@@ -3,6 +3,7 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
+import lotto.utils.Rank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +11,9 @@ class LottoProfitTest {
     @Test
     @DisplayName("수익률을 올바르게 계산하는지")
     void calculateProfit() {
-        assertThat(new LottoProfit(Map.of("1st",0,"2nd",0,"3rd",0,"4th",0,"5th",1),new Money(8000))
+        assertThat(new LottoProfit(Map.of(Rank.FIRST,0,
+                Rank.SECOND,0, Rank.THIRD,0, Rank.FOURTH,0, Rank.FIFTH,1)
+                ,new Money(8000))
                 .getResult())
                 .isEqualTo(62.5);
     }
--- src/test/java/lotto/model/WinningNumberCheckerTest.java
+++ src/test/java/lotto/model/WinningNumberCheckerTest.java
@@ -3,9 +3,11 @@ package lotto.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import lotto.utils.Rank;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class WinningNumberCheckerTest {
     private WinningNumberChecker winningNumberChecker;
@@ -14,45 +16,51 @@ class WinningNumberCheckerTest {
     @BeforeEach
     void init() {
         lottos = new Lottos();
+        lottos.addLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"FIRST,1", "SECOND,0", "THIRD,0", "FOURTH,0", "FIFTH,0"})
     @DisplayName("1등일 경우를 올바르게 체크할 수 있는지")
-    void checkRank1st() {
-        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,6)));
-        winningNumberChecker = new WinningNumberChecker(List.of(1,2,3,4,5,6),7,lottos);
-        assertThat(winningNumberChecker.getWinningRankResult().get("1st")).isEqualTo(1);
+    void checkRank1st(Rank rank, int result) {
+        winningNumberChecker = new WinningNumberChecker(List.of(1, 2, 3, 4, 5, 6), 7, lottos);
+        winningNumberChecker.checkWinningRankResult();
+        assertThat(winningNumberChecker.getWinningRankResult().get(rank)).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"FIRST,0", "SECOND,1", "THIRD,0", "FOURTH,0", "FIFTH,0"})
     @DisplayName("2등일 경우를 올바르게 체크할 수 있는지")
-    void checkRank2nd() {
-        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,6)));
-        winningNumberChecker = new WinningNumberChecker(List.of(1,2,3,4,5,8),6,lottos);
-        assertThat(winningNumberChecker.getWinningRankResult().get("2nd")).isEqualTo(1);
+    void checkRank2nd(Rank rank, int result) {
+        winningNumberChecker = new WinningNumberChecker(List.of(1, 2, 3, 4, 5, 8), 6, lottos);
+        winningNumberChecker.checkWinningRankResult();
+        assertThat(winningNumberChecker.getWinningRankResult().get(rank)).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"FIRST,0", "SECOND,0", "THIRD,1", "FOURTH,0", "FIFTH,0"})
     @DisplayName("3등일 경우를 올바르게 체크할 수 있는지")
-    void checkRank3rd() {
-        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,7)));
-        winningNumberChecker = new WinningNumberChecker(List.of(1,2,3,4,5,8),9,lottos);
-        assertThat(winningNumberChecker.getWinningRankResult().get("3rd")).isEqualTo(1);
+    void checkRank3rd(Rank rank, int result) {
+        winningNumberChecker = new WinningNumberChecker(List.of(1, 2, 3, 4, 5, 8), 9, lottos);
+        winningNumberChecker.checkWinningRankResult();
+        assertThat(winningNumberChecker.getWinningRankResult().get(rank)).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"FIRST,0", "SECOND,0", "THIRD,0", "FOURTH,1", "FIFTH,0"})
     @DisplayName("4등일 경우를 올바르게 체크할 수 있는지")
-    void checkRank4th() {
-        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,7)));
-        winningNumberChecker = new WinningNumberChecker(List.of(1,2,3,4,45,44),43,lottos);
-        assertThat(winningNumberChecker.getWinningRankResult().get("4th")).isEqualTo(1);
+    void checkRank4th(Rank rank, int result) {
+        winningNumberChecker = new WinningNumberChecker(List.of(1, 2, 3, 4, 45, 44), 43, lottos);
+        winningNumberChecker.checkWinningRankResult();
+        assertThat(winningNumberChecker.getWinningRankResult().get(rank)).isEqualTo(result);
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"FIRST,0", "SECOND,0", "THIRD,0", "FOURTH,0", "FIFTH,1"})
     @DisplayName("5등일 경우를 올바르게 체크할 수 있는지")
-    void checkRank5th() {
-        lottos.addLotto(new Lotto(List.of(1,2,3,4,5,7)));
-        winningNumberChecker = new WinningNumberChecker(List.of(1,2,3,45,44,43),7,lottos);
-        assertThat(winningNumberChecker.getWinningRankResult().get("5th")).isEqualTo(1);
+    void checkRank5th(Rank rank, int result) {
+        winningNumberChecker = new WinningNumberChecker(List.of(1, 2, 3, 45, 44, 43), 7, lottos);
+        winningNumberChecker.checkWinningRankResult();
+        assertThat(winningNumberChecker.getWinningRankResult().get(rank)).isEqualTo(result);
     }
 }

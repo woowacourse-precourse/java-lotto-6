93cd727fe97bb993fb83e04c2e4b6a8434c34740
junghunim07 <dlawndgns9@naver.com>
2023-11-08 15:56:14 +0900

    refactor: 자바 컨벤션 준수

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,10 @@
 package lotto;
 
+import java.util.List;
+
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 

bb48272b1c642f59c78846a953c09867a4620788
whereami0404 <whereami2048@gmail.com>
2023-11-08 19:28:55 +0900

    test: 보너스 번호가 당첨 번호와 중복되는 경우 테스트 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -31,5 +32,18 @@ class BonusNumberTest {
         })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 1부터 45사이의 숫자이어야 합니다.");
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
+    @Test
+    void createBonusNumberByDuplicateNumber() {
+        //given
+        BonusNumber bonusNumber = new BonusNumber(3);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            bonusNumber.validateBonusNumberDuplicatedByWinningNumbers(bonusNumber, winningNumbers);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }

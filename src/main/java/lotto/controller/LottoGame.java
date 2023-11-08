58306836f9e519f4483701f45f653b67b1fe323f
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-08 19:04:24 +0900

    Feat : 당첨 통계 계산하는 StatsService 클래스 생성

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -2,9 +2,11 @@ package lotto.controller;
 
 import lotto.model.Player;
 import lotto.service.LottoService;
+import lotto.service.StatsService;
 
 public class LottoGame {
     private final LottoService lottoService = new LottoService();
+    private StatsService statsService;
     private Player player;
     private static final int DIVIDED_AMOUNT = 1000;
 
@@ -18,6 +20,7 @@ public class LottoGame {
         lottoService.createWinningNumbers();
         lottoService.createBonusNumber();
         player = new Player(lottoService.getPayment(), lottoService.getWinningNumbers(), lottoService.getBonusNumber());
+        statsService = new StatsService(player);
     }
 
 }
---
+++ src/main/java/lotto/service/StatsService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+import lotto.model.Player;
+
+public class StatsService {
+    private Player player;
+
+    public StatsService(Player player) {
+        this.player = player;
+    }
+
+}

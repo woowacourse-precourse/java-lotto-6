44af422a20b669ba7699b5d00c8d07ae43b7f242
yeondori <yeonseo.jung0@gmail.com>
2023-11-08 22:54:04 +0900

    refactor: 메서드 생성에 따른 불필요한 반복, 에외 처리 구문 삭제

--- src/main/java/lotto/ui/InputManager.java
+++ src/main/java/lotto/ui/InputManager.java
@@ -12,36 +12,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputManager {
     public int requestAmount() {
-        while (true) {
-            System.out.println("\n구입금액을 입력해 주세요.");
-            try {
-                return validatedNumber(readLine());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        System.out.println("\n구입금액을 입력해 주세요.");
+
+        return validatedNumber(readLine());
     }
 
     public List<Integer> requestNumbers() {
-        while (true) {
-            System.out.println("\n당첨 번호를 입력해 주세요.");
-            try {
-                return validatedNumbers(readLine());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+
+        return validatedNumbers(readLine());
     }
 
     public int requestBonus(List<Integer> winningNumbers) {
-        while (true) {
-            System.out.println("\n보너스 번호를 입력해 주세요.");
-            try {
-                return validatedNumber(readLine());
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+
+        return validatedNumber(readLine());
     }
 
     private List<Integer> validatedNumbers(String input) {

3b4cd6966ff0f90a0759a7bd61d3ca761856d548
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 14:41:07 +0900

    feat(util) : 구매 금액이 양수인지 검증하는 메소드

--- src/main/java/lotto/util/validator/ParchaseAmountInputValidator.java
+++ src/main/java/lotto/util/validator/ParchaseAmountInputValidator.java
@@ -5,7 +5,9 @@ import lotto.util.validator.constant.ErrorMessageConstant;
 public class ParchaseAmountInputValidator implements InputValidator{
     @Override
     public void validate(String checkValue) {
-
+        validateNumber(checkValue);
+        validateDivisibleThousand(checkValue);
+        validatePositiveNumber(checkValue);
     }
 
     private static void validateNumber(String checkValue) {
@@ -19,6 +21,12 @@ public class ParchaseAmountInputValidator implements InputValidator{
     private static void validateDivisibleThousand(String checkValue) {
         if(Integer.parseInt(checkValue) % 1000 != 0) {
             throw new IllegalArgumentException(ErrorMessageConstant.PARCHASE_AMOUNT_IS_NOT_DIVISIBLE_BY_THOUSAND.getMessage());
+        }
+    }
+
+    private static void validatePositiveNumber(String checkValue) {
+        if(Integer.parseInt(checkValue) < 0) {
+            throw new IllegalArgumentException(ErrorMessageConstant.PARCHASE_AMOUNT_IS_NOT_POSITIVE_NUMBER.getMessage());
         }
     }
 }

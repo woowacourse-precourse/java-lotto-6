a8d6ca705b09b5baf0026b26353b8e8ae9688396
UnityChan3000 <lego0313@naver.com>
2023-11-08 21:25:58 +0900

    보너스 번호 범위 확인후 에러메세지 송출 기능 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -102,7 +102,14 @@ public class LottoManager {
         }
     }
 
+    private void insertBonusNumber(){
 
+    }
 
+    private void checkBonusNumberRange(int number){
+        if(number<1 || number>45){
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 
 }

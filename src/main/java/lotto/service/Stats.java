026ac79874d75bb5546227f3841e7d6425576e17
KimHongHee <qaz101044@naver.com>
2023-11-08 21:12:14 +0900

    feature(Stats) : add a features related to check a game stats.

--- src/main/java/lotto/service/Stats.java
+++ src/main/java/lotto/service/Stats.java
@@ -1,4 +1,18 @@
 package lotto.service;
 
 public class Stats {
+
+    private final int bonus;
+    private final Lotto lotto;
+
+    public Stats(Lotto lotto, int bonus) {
+        this.lotto = lotto;
+        this.bonus = bonus;
+    }
+
+    public Prizes check_match(Lotto player_number) {
+        int count_match_numbers = player_number.count_match_winning_numbers(lotto);
+        boolean check_bonus = player_number.check_contain_numbers(bonus);
+        return Prizes.return_prize(count_match_numbers,check_bonus);
+    }
 }

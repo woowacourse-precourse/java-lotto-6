667fc47f4e0c785290e6b330a3b456e50f203fdb
ogi <ogi@naver.com>
2023-11-08 22:59:59 +0900

    [feat] 로또 구매 금액 입력 및 입력 값 검증 로직

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,8 @@
+package lotto.view;
+
+public class OutputView {
+    public void showQuantityMessage(int quantity) {
+        System.out.println();
+        System.out.println(quantity + "개를 구매했습니다.");
+    }
+}

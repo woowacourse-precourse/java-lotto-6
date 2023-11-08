cf17414f71b5f0e3e4dfc0b646a0b97d3325a01f
LeeJieon <dlwldjs7544@naver.com>
2023-11-08 13:18:44 +0900

    test: 정상적으로 수익률을 계산하는 테스트 추가

--- src/test/java/lotto/domain/WinningStatisticsTest.java
+++ src/test/java/lotto/domain/WinningStatisticsTest.java
@@ -41,4 +41,30 @@ class WinningStatisticsTest {
         assertThat(winningStatistics.getWinningStatistics()).isEqualTo(result);
         assertThat(winningStatistics.getTotalPrize()).isEqualTo(2031555000);
     }
+
+    @Test
+    @DisplayName("정상적으로 수익률을 계산")
+    void calculateWinningRateTest() {
+        // Given
+        List<Lotto> randomLotto = List.of(
+                new Lotto(List.of(8, 21, 23, 41, 42, 43)),
+                new Lotto(List.of(3, 5, 11, 16, 32, 38)),
+                new Lotto(List.of(7, 11, 16, 35, 36, 44)),
+                new Lotto(List.of(1, 8, 11, 31, 41, 42)),
+                new Lotto(List.of(13, 14, 16, 38, 42, 45)),
+                new Lotto(List.of(7, 11, 30, 40, 42, 43)),
+                new Lotto(List.of(2, 13, 22, 32, 38, 45)),
+                new Lotto(List.of(1, 3, 5, 14, 22, 45))
+        );
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = new BonusNumber("7", winningLotto);
+
+        // When
+        WinningStatistics winningStatistics = new WinningStatistics();
+        winningStatistics.updateWinningStatistics(randomLotto, winningLotto, bonusNumber.getBonusNumber());
+
+        // Then
+        assertThat(winningStatistics.getWinningRate(8000)).isEqualTo("62.5");
+    }
 }

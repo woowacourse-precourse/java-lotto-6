55dad642066a8cdb18afacb40a40d72a18687c25
olive-su <1466su@gmail.com>
2023-11-08 22:09:53 +0900

    feat: 기본 실행 모듈 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import lotto.data.Purchasing;
+import lotto.data.Statics;
+import lotto.presentation.Input;
+import lotto.presentation.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        start();
+    }
+    static void start(){
+        Input input = new Input();
+        Output output = new Output();
+        Statics statics = new Statics(5);
+
+        String purchasePrice = input.inputPurchasePrice();
+
+        Purchasing purchasing = new Purchasing(Integer.parseInt(purchasePrice));
+
+        output.printPurchasedLottos(purchasing.getLottos());
+        String[] winngingNumber = input.inputWinningNumber();
+        String bonusNumber = input.inputBonusNumber();
+        output.printMultipleWinningStatics(statics);
     }
 }

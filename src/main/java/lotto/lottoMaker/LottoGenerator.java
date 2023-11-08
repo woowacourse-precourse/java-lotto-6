cb445595531a66909acd9e72bd6e7e2f04973ec1
san <soh7174@gmail.com>
2023-11-08 16:58:57 +0900

    fix : 로또 인스턴스 생성 라인 수정

--- src/main/java/lotto/lottoMaker/LottoGenerator.java
+++ src/main/java/lotto/lottoMaker/LottoGenerator.java
@@ -1,12 +1,14 @@
 package lotto.lottoMaker;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoGenerator {
     public static void generate(List<Lotto> lottoTicket, int numOfTicket) {
         for(int i=0; i<numOfTicket; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             lottoTicket.add(new Lotto(numbers));
         }
     }

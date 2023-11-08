60d41f7a8314c2b3b30ffe32ac44064ca08ff250
SeungYeop-Han <106862797+SeungYeop-Han@users.noreply.github.com>
2023-11-08 20:22:20 +0900

    feat(printPurchaseList): 문자열 누락 문제 해결
    구입한 로또 목록 출력 전 출력해야 하는 문자열 "n개를 구매했습니다.\n" 를 추가했습니다.

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -16,6 +16,7 @@ public class LottoView {
 
     public static void printPurchaseList(List<Lotto> purchased) {
         StringBuilder sb = new StringBuilder();
+        sb.append(purchased.size()).append("개를 구매했습니다.\n");
         for (Lotto lotto : purchased) {
             sb.append(lotto.getImmutableSortedNumbers());
             sb.append("\n");

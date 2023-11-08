111a063b5d8e17dc2a85485841b67328c9d1ef89
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-11-08 18:07:11 +0900

    test(inputView): 검증 예외 테스트 보완 및 테스트 이름 변경

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,23 +1,31 @@
 package lotto.view;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.exception.Exception.DUPLICATE_BONUS_NUMBER;
+import static lotto.exception.Exception.DUPLICATE_WINNING_NUMBER;
 import static lotto.exception.Exception.INVALID_BONUS_NUMBER_INPUT_TYPE;
+import static lotto.exception.Exception.INVALID_NUMBER_RANGE;
 import static lotto.exception.Exception.INVALID_WINNING_NUMBERS_INPUT_TYPE;
 import static lotto.exception.Exception.ONLY_NUMERIC_INPUT_FOR_MONEY;
+import static lotto.exception.Exception.WINNING_NUMBERS_SIZE_SHOULD_BE_SIX;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class InputViewTest {
     @Nested
     class 구입금액_입력_테스트 extends NsTest {
 
+        @DisplayName("구입금액이 숫자가 아닌 경우 예외가 발생되는가.")
         @Test
-        void 구입금액이_숫자가_아닌_경우_예외() {
+        void inputMoney() {
             assertSimpleTest(() -> {
                 assertThatThrownBy(() -> runException("input"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -34,8 +42,30 @@ class InputViewTest {
 
     @Nested
     class 당첨번호_입력_테스트 extends NsTest {
+        @DisplayName("당첨번호 입력시 6개가 아닌 경우 예외가 발생하는가")
+        @ParameterizedTest
+        @CsvSource(value = {"1,2,3,4,5", "1,2,3,4,5,6,7"}, delimiter = ',')
+        void inputWinningNumbersByOverSize(String input) {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(WINNING_NUMBERS_SIZE_SHOULD_BE_SIX.getMessage());
+            });
+        }
+
+        @DisplayName("당첨번호 입력시 숫자가 중복된 경우 예외가 발생하는가")
         @Test
-        void 당첨번호_입력시_숫자와_구분자_공백이_아닌_다른_문자가_포함된_경우_예외() {
+        void inputWinningNumbersByDuplication() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1, 1, 1, 1, 1, 1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(DUPLICATE_WINNING_NUMBER.getMessage());
+            });
+        }
+
+        @DisplayName("당첨번호 입력시 숫자와 구분자, 공백이 아닌 다른 문자가 포함된 경우 예외가 발생하는가")
+        @Test
+        void inputWinningNumbersByInvalidType() {
             assertSimpleTest(() -> {
                 assertThatThrownBy(() -> runException("1.2.3.4.8.6"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -43,20 +73,22 @@ class InputViewTest {
             });
         }
 
+        @DisplayName("당첨번호에 공백이 있는 경우 제거하는가")
         @Test
-        void 당첨번호에_공백이_있는_경우_제거() {
+        void inputWinningNumbersWithSpace() {
             assertSimpleTest(() -> {
                 run("1,2,3,4,  5, 7");
                 assertThat(output()).contains("1", "2", "3", "4", "5", "7");
             });
         }
 
+        @DisplayName("당첨번호가 1~45 사이의 수가 아닌 경우 예외가 발생하는가")
         @Test
-        void 당첨번호가_숫자가_아닌_경우_예외() {
+        void inputWinningNumbers() {
             assertSimpleTest(() -> {
-                assertThatThrownBy(() -> runException("1,2,3,4, a, 6"))
+                assertThatThrownBy(() -> runException("1,2,3,4, 50, 6"))
                         .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage(INVALID_WINNING_NUMBERS_INPUT_TYPE.getMessage());
+                        .hasMessage(INVALID_NUMBER_RANGE.getMessage());
             });
         }
 
@@ -70,19 +102,30 @@ class InputViewTest {
 
     @Nested
     class 보너스번호_입력_테스트 extends NsTest {
+        @DisplayName("보너스 번호가 숫자가 아닌 경우 예외가 발생하는가")
         @Test
-        void 보너스_번호가_숫자가_아닌_경우_예외() {
+        void inputBonusNumberByInvalidType() {
             assertSimpleTest(() -> {
                 assertThatThrownBy(() -> runException("a"))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessage(INVALID_BONUS_NUMBER_INPUT_TYPE.getMessage());
             });
         }
+        @DisplayName("보너스 번호가 당첨 번호와 중복된 경우 예외가 발생하는가")
+        @Test
+        void inputBonusNumberByDuplication() {
+            assertSimpleTest(() -> {
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(DUPLICATE_BONUS_NUMBER.getMessage());
+            });
+        }
 
         @Override
         protected void runMain() {
+            List<Integer> validWinningNumbers = List.of(1, 2, 3, 4, 5, 6);
             InputView inputView = new InputView();
-            inputView.inputBonusNumber();
+            inputView.inputBonusNumber(validWinningNumbers);
         }
     }
 }

8f031e47520df01587d1c6d22a2c281597a99683
SeongIl <biuld1234@gmail.com>
2023-11-08 14:49:31 +0900

    test(UserTest): replace 중복코드 -> beforeEach

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -2,19 +2,23 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class UserTest {
+    private User user;
+
+    @BeforeEach
+    void beforeEach() {
+        user = new User();
+    }
 
     @DisplayName("사용자가 로또 구매시, 구매 가격에 따라 로또 개수 생성")
     @ParameterizedTest
     @CsvSource(value = {"1000,1", "2000,2"})
     void purchaseLottoCount(String money, Integer lottoCount) {
-        // given
-        User user = new User();
-
         // when
         user.purchaseLotto(new Money(money));
 
@@ -26,9 +30,6 @@ class UserTest {
     @ParameterizedTest
     @CsvSource(value = {"1000,1", "2000,2"})
     void purchaseLottos(String money, Integer lottoCount) {
-        // given
-        User user = new User();
-
         // when
         user.purchaseLotto(new Money(money));
 

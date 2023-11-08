3df0bcc87ccaebd5233366d399afc4383411a2d6
minju <minju9711@naver.com>
2023-11-08 19:08:23 +0900

    [feat] 쓸모없는 주석 삭제

--- src/main/java/lotto/LottoFactory/LottoController.java
+++ src/main/java/lotto/LottoFactory/LottoController.java
@@ -3,7 +3,7 @@ package lotto.LottoFactory;
 public class LottoController {
 
     public void CheckDuplicateNum(LottoChecker lottoContainer, Lotto lotto, boolean isBonusDuplicate){
-        int count = lottoContainer.countMatchingNum(lotto);  // Lotto 객체를 인자로 전달
+        int count = lottoContainer.countMatchingNum(lotto);
         LottoRank.CheckAndStoreRank(count, count == 5 && isBonusDuplicate);
     }
 

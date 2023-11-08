9d3dd32d04622c3383b3164c38535258c27b88b8
김현재 <khjmr@naver.com>
2023-11-08 16:57:33 +0900

    test(getResults): RankCounter를 뽑아 내는 테스트 추가

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -4,12 +4,31 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottosTest {
+    @DisplayName("RankCounter 를 뽑아내는 테스트 추가")
+    @Test
+    public void getResults() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(2, 3, 4, 5, 6, 7));
+        Lottos lottos = Lottos.create(List.of(lotto, lotto2));
+        WinningLotto winningLotto = WinningLotto.create(List.of(2, 3, 4, 5, 6, 7), 1);
+        RankCounter results = lottos.getResults(winningLotto);
+
+        String expected = "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 1개\n";
+
+        Assertions.assertThat(results.toString()).isEqualTo(expected);
+    }
+
     @DisplayName("Lotto 들의 결과를 출력하는 테스트")
     @Test
     void toStringTest() {

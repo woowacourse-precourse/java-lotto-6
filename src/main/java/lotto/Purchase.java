e18386ec714a3907a046dc950c0bfd8b03c0a8c7
realisshomyang <realisshoman@kakao.com>
2023-11-08 12:15:23 +0900

    feat : 구매금액 도메인 기능 구현
    기능 : 1000으로 나누어 떨어지는 것을 처리해야한다.

---
+++ src/main/java/lotto/Purchase.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Purchase {
+
+    private final Integer price;
+    private final Integer ticketCount;
+
+    public Purchase(Integer price, Integer ticketCount) {
+        validateIsMultipleOf1000(price);
+        this.price = price;
+        this.ticketCount = price/1000;
+    }
+
+    private void validateIsMultipleOf1000(Integer value) {
+        if (value % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getTicketCount() {
+        return ticketCount;
+    }
+}

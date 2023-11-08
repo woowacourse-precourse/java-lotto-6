fd141a53ffbf1d6c7db3ecc25549cf3393143ed4
wschoe <chldntjr0425@naver.com>
2023-11-08 22:19:40 +0900

    feat: move duplication checking log into Lotto class

--- src/main/java/lotto/lottosystem/bussiness/Lotto.java
+++ src/main/java/lotto/lottosystem/bussiness/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.lottosystem.bussiness;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +14,17 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6자리입니다..");
         }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("로또 번호 간의 중복이 발생했습니다.");
+        }
+
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
     }
 
     public List<Integer> getNumbers() {

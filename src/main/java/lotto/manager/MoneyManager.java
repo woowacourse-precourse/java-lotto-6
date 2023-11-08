ec59445888868372992cd8f880f74d297faf7de8
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-08 14:16:29 +0900

    feat: LottoManager, MoneyManager 클래스 구현
    각각 Lotto, Money 도메인을 관리하는 객체를 구현함

---
+++ src/main/java/lotto/manager/MoneyManager.java
@@ -0,0 +1,31 @@
+package lotto.manager;
+
+import java.util.Map;
+import lotto.model.money.PrizeMoney;
+import lotto.model.money.UserBudget;
+import lotto.utils.constant.LottoWinningConstant;
+
+public class MoneyManager {
+
+    private UserBudget userBudget;
+    private PrizeMoney prizeMoney;
+
+    public MoneyManager(UserBudget userBudget, PrizeMoney prizeMoney) {
+        this.userBudget = userBudget;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public float getProfitRate(Map<LottoWinningConstant, Integer> statistic) {
+        calculate(statistic);
+        return prizeMoney.getProfitRateInfo(userBudget.getBudget());
+    }
+
+    public String getResultString() {
+        StringBuilder sb = new StringBuilder();
+        return sb.toString();
+    }
+
+    private void calculate(Map<LottoWinningConstant, Integer> statistic) {
+        prizeMoney.totalPrizeMoney.calculateTotalPrize(statistic);
+    }
+}

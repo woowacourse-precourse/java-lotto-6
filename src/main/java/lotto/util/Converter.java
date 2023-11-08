afff80fbf9bfb9cfc9e514a593934e0940868b44
haebyun <bhk4392@gmail.com>
2023-11-08 14:42:05 +0900

    feat(Converter): 로또 번호들 문자열을 정수 리스트로 변환하고 변환할 수 없다면 예외를 발생시키는 기능 구현

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.validation.LottoException;
 
 public class Converter {
@@ -13,5 +16,14 @@ public class Converter {
         }
     }
 
-
+    public static List<Integer> convertWinningNumbers(String input) {
+        try {
+            List<Integer> inputNumbers = Arrays.stream(input.split(SPLIT_POINT))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            return inputNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
+        }
+    }
 }

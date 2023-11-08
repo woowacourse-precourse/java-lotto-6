ec3d570ceb30f205b0d156713698c02fa7f39766
권장순 <jsoonworld@gmail.com>
2023-11-08 12:58:40 +0900

    feat( LottoGmaeManager ) : 수익률 계산을 제외한 로직들까지 적용해서 구현.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,36 +1,6 @@
 package lotto;
 
-
-import lotto.v3.controller.LottoBonusNumberController;
-import lotto.v3.controller.LottoGenerationController;
-import lotto.v3.controller.LottoPurchaseController;
-import lotto.v3.controller.LottoWinningNumberController;
-import lotto.v3.model.LottoMachine;
-import lotto.v3.model.LottoWinningNumber;
-import lotto.v3.view.LottoBonusNumberView;
-import lotto.v3.view.LottoGenerateView;
-import lotto.v3.view.LottoPurchaseView;
-import lotto.v3.view.LottoWinningNumbersView;
-
 public class Application {
     public static void main(String[] args) {
-        LottoPurchaseView purchaseView = new LottoPurchaseView();
-        LottoPurchaseController purchaseController = new LottoPurchaseController(purchaseView);
-
-        LottoGenerateView generateView = new LottoGenerateView();
-        LottoMachine machine = new LottoMachine();
-        LottoGenerationController generationController = new LottoGenerationController(generateView, machine);
-
-        int numberOfTickets = purchaseController.startPurchaseProcess();
-        generationController.generateLottoTickets(numberOfTickets);
-
-        LottoWinningNumbersView winningNumbersView = new LottoWinningNumbersView();
-        LottoWinningNumberController winningNumberController = new LottoWinningNumberController(winningNumbersView);
-        LottoWinningNumber winningNumber = winningNumberController.createWinningNumber();
-
-        LottoBonusNumberView bonusNumberView = new LottoBonusNumberView();
-        LottoBonusNumberController bonusNumberController = new LottoBonusNumberController(bonusNumberView, winningNumber);
-        bonusNumberController.createBonusNumber();
-
     }
 }

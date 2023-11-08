c400149ba2b0e4d192a8f9c7a27943e81130d501
Ozwong <owongs99@gmail.com>
2023-11-08 15:22:45 +0900

    fix($AmountOfPurchase): 로직과 반환값 수정
    - validated 변수를 없애기 위해 리팩토링
    - 구입 가격을 1000으로 나눈 구입 개수를 반환

--- src/main/java/lotto/Input/AmountOfPurchase.java
+++ src/main/java/lotto/Input/AmountOfPurchase.java
@@ -1,21 +1,24 @@
 package lotto.Input;
 
 public class AmountOfPurchase {
+    static final int PRICE = 1000;
+
     private static int amount;
 
     public static int takeInput() {
-        boolean validated;
         do {
             String input = camp.nextstep.edu.missionutils.Console.readLine();
-            validated = calculateAmount(input);
-        } while (!validated);
+            if (calculateAmount(input)) {
+                break;
+            }
+        } while (true);
         return amount;
     }
 
     private static boolean calculateAmount(String input) {
         try {
             PriceCheck.validatePrice(input);
-            amount = Integer.parseInt(input);
+            amount = Integer.parseInt(input) / PRICE;
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 구입 금액은 1000으로 나누어 떨어지는 수여야 합니다.");
             return false;

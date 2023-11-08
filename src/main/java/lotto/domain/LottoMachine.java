3bcf6db5a08f47520578eb245432e9230bf41057
kssumin <201566@jnu.ac.kr>
2023-11-08 13:59:21 +0900

    fix(LottoMachine) : lotto 발행 갯수를 구하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+public class LottoMachine {
+    private static final int PURCHASE_LOTTO_MONEY = 1000;
+    private final LottoGenerator lottoGenerator;
+
+    public LottoMachine(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public LottoTicket buy(Money money) {
+        int lottoCount = getLottoCount(money);
+        return new LottoTicket(lottoCount, lottoGenerator);
+    }
+
+    public int getLottoCount(Money money) {
+        int requestMoney = money.getMoney();
+        return requestMoney / PURCHASE_LOTTO_MONEY;
+    }
+}

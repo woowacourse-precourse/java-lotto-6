92d5f0da13309669fe4b57d6e6562f0e5e0f3004
leorb159 <ldgsocial159@naver.com>
2023-11-08 15:23:58 +0900

    feat: OutputView 생성, writePurchaseDetails 메소드 작성

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -1,2 +1,17 @@
-package lotto;public class OutputView {
+package lotto;
+
+import java.util.List;
+
+public class OutputView {
+
+    private static final String MESSAGE_PURCHASE_COMPLETED = "개를 구매했습니다.";
+
+    public void writePurchaseDetails(List<Lotto> lottery) {
+        System.out.println(lottery.size() + MESSAGE_PURCHASE_COMPLETED);
+        for (Lotto lotto: lottery) {
+            List<Integer> lottoNumbers = lotto.getNumbers();
+            System.out.println(lottoNumbers);
+        }
+        System.out.println();
+    }
 }

df25d72ff03df902ad3dcb040acfa123bd591d0c
gyuoo <yeomky98@naver.com>
2023-11-08 23:45:20 +0900

    feat: implement lotto numbers to string method

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -74,4 +74,9 @@ public class Lotto {
     public boolean contains(int number) {
         return numbers.contains(number);
     }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }

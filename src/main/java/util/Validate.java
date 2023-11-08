af64bd6bf1ab008ebdc4405b4e416d3805cfe682
icebear0111 <wltjq0901@naver.com>
2023-11-08 23:31:40 +0900

    feat: if input is not integer error

--- src/main/java/util/Validate.java
+++ src/main/java/util/Validate.java
@@ -3,6 +3,13 @@ package util;
 import java.util.List;
 
 public class Validate {
+    public static void isValidInput(String input) {
+        try {
+            int price = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
+        }
+    }
     public static void isValidPrice(int price) {
         if (price % 1000 != 0)
             throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력해야 합니다.");
@@ -14,6 +21,9 @@ public class Validate {
         for (int num : numbers) {
             if (num < 1 || num > 45)
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 값은 입력하실 수 없습니다.");
         }
     }
 

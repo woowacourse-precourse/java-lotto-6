59ea4ff380583f8b5394db72afe57a96ece460b9
cousim46 <wjdghldns24@naver.com>
2023-11-08 21:27:33 +0900

    test : 예외에 대한 테스트 코드 수정

--- src/test/java/lotto/CustomerTest.java
+++ src/test/java/lotto/CustomerTest.java
@@ -5,33 +5,42 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CustomerTest {
-
+    @Test
     @DisplayName("숫자로만 이루어진 문자열이 아닐 경우 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1000j", "12t"})
-    void occurNotMadeNumberException(String buyPrice) {
+    void occurNotMadeNumberException() {
+        //given
+        String buyPrice = "1000J";
+
+        //when && then
         assertThatThrownBy(() -> Customer.create(buyPrice)).isInstanceOf(
-            NumberFormatException.class);
+            NumberFormatException.class)
+            .hasMessage("숫자를 입력해주세요.");
     }
 
-    @DisplayName("입력한 금액이 1000원 미만일경우 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"900", "999"})
-    void occur1000LessThanException(String buyPrice) {
+    @Test
+    @DisplayName("입력한 금액이 빈 문자열일 경우에 예외가 발생한다.")
+    void occur1000LessThanException() {
+        //given
+        String buyPrice = "";
+
+        //when && then
         assertThatThrownBy(() -> Customer.create(buyPrice)).isInstanceOf(
-            IllegalArgumentException.class);
+                NumberFormatException.class)
+            .hasMessage("숫자를 입력해주세요.");
     }
 
-    @DisplayName("입력한 금액이 1000원 단위가 아닐경우 예외가 발생한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1010", "9100"})
-    void occur1000UnitException(String buyPrice) {
+    @Test
+    @DisplayName("입력한 금액이 공백일 경우에 예외가 발생한다.")
+    void occur1000UnitException() {
+        //given
+        String buyPrice = " ";
+
+        //when && then
         assertThatThrownBy(() -> Customer.create(buyPrice)).isInstanceOf(
-            IllegalArgumentException.class);
+                NumberFormatException.class)
+            .hasMessage("숫자를 입력해주세요.");
     }
 
     @Test

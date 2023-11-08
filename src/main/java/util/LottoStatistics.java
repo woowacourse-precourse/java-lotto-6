4b4ada7207020cecfe0af3ba79b3ee1f1f5f0570
dyjeong365 <dyjeong365@gmail.com>
2023-11-08 16:50:14 +0900

    refactor: introduce variables

--- src/main/java/util/LottoStatistics.java
+++ src/main/java/util/LottoStatistics.java
@@ -12,11 +12,15 @@ public class LottoStatistics {
         System.out.println("당첨 통계");
         System.out.println("-".repeat(3));
 
-        System.out.println(LottoRank.FIFTH.getMatchedCount() + "개 일치 (" + LottoRank.FIFTH.getPrizeMoney() + "원) - " + rankCounter.get(LottoRank.FIFTH) + "개");
-        System.out.println(LottoRank.FOURTH.getMatchedCount() + "개 일치 (" + LottoRank.FOURTH.getPrizeMoney() + "원) - " + rankCounter.get(LottoRank.FOURTH) + "개");
-        System.out.println(LottoRank.THIRD.getMatchedCount() + "개 일치 (" + LottoRank.THIRD.getPrizeMoney() + "원) - " + rankCounter.get(LottoRank.THIRD) + "개");
-        System.out.println(LottoRank.SECOND.getMatchedCount() + "개 일치, 보너스 볼 일치 (" + LottoRank.SECOND.getPrizeMoney() + "원) - " + rankCounter.get(LottoRank.SECOND) + "개");
-        System.out.println(LottoRank.FIRST.getMatchedCount() + "개 일치 (" + LottoRank.FIRST.getPrizeMoney() + "원) - " + rankCounter.get(LottoRank.FIRST) + "개");
+        final String WON = "원) - ";
+        final String COUNT = "개";
+        final String matchedCount = "개 일치 (";
+        
+        System.out.println(LottoRank.FIFTH.getMatchedCount() + matchedCount + LottoRank.FIFTH.getPrizeMoney() + WON + rankCounter.get(LottoRank.FIFTH) + COUNT);
+        System.out.println(LottoRank.FOURTH.getMatchedCount() + matchedCount + LottoRank.FOURTH.getPrizeMoney() + WON + rankCounter.get(LottoRank.FOURTH) + COUNT);
+        System.out.println(LottoRank.THIRD.getMatchedCount() + matchedCount + LottoRank.THIRD.getPrizeMoney() + WON + rankCounter.get(LottoRank.THIRD) + COUNT);
+        System.out.println(LottoRank.SECOND.getMatchedCount() + "개 일치, 보너스 볼 일치 (" + LottoRank.SECOND.getPrizeMoney() + WON + rankCounter.get(LottoRank.SECOND) + COUNT);
+        System.out.println(LottoRank.FIRST.getMatchedCount() + matchedCount + LottoRank.FIRST.getPrizeMoney() + WON + rankCounter.get(LottoRank.FIRST) + COUNT);
         System.out.printf("총 수익률은 %.1f%%입니다.", calculateYield(rankCounter, ticketPurchaseAmount));
     }
 

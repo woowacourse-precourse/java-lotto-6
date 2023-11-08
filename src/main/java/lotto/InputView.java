d6ca4a33a2e575938e0ecaf9bc36ed264ea11443
leorb159 <ldgsocial159@naver.com>
2023-11-08 13:52:06 +0900

    feat: 보너스 번호 받기 작성

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -9,24 +9,30 @@ public class InputView {
 
     private static final String MESSAGE_ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String MESSAGE_ENTER_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-    public int purchaseLottoAmount(){
+    public int purchaseLottoAmount() {
         System.out.println(MESSAGE_ENTER_PURCHASE_AMOUNT);
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
 
-    public List<Integer> readWinningLotto(){
+    public List<Integer> readWinningLotto() {
         System.out.println(MESSAGE_ENTER_WINNING_LOTTO);
         String input = Console.readLine();
         String[] inputSplit = input.split(",");
         List<Integer> winningLotto = new ArrayList<>();
-        for (String str: inputSplit) {
+        for (String str : inputSplit) {
             winningLotto.add(Integer.parseInt(str));
         }
         return winningLotto;
     }
 
+    public int readBonusNumber() {
+        System.out.println(MESSAGE_ENTER_BONUS_NUMBER);
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 
 
 }

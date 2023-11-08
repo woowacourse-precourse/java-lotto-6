23542948cae3cf2a098130073da24cb3dd21f7d1
i3kae <snoger1234@gmail.com>
2023-11-08 19:47:37 +0900

    style : 테스트 함수들 DisplayName 변경

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UtilsTest {
-    @DisplayName("숫자로 이루어진 문자열이 들어온 경우")
+    @DisplayName("숫자로 이루어진 문자열이 들어올 때 true 반환한다.")
     @Test
     void normalNumberisNumericTest() {
         assertThat(Utils.isNumeric("1234500")).isEqualTo(true);
         assertThat(Utils.isNumeric("0001234")).isEqualTo(true);
     }
-    @DisplayName("비어있는 문자열이 들어온 경우")
+    @DisplayName("비어있는 문자열이 들어온 경우 false 반환한다.")
     @Test
     void emptyStringisNumericTest() {
         assertThat(Utils.isNumeric("")).isEqualTo(false);
     }
-    @DisplayName("특수 문자가 포함된 문자열이 들어온 경우")
+    @DisplayName("특수 문자가 포함된 문자열이 들어온 경우 false를 반환한다.")
     @Test
     void specialWordStringisNumericTest() {
         assertThat(Utils.isNumeric("!1234")).isEqualTo(false);

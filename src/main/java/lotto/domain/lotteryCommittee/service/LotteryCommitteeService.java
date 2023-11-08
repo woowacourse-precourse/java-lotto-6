95a7f60509978d8188d52792b790978e1bdc282e
SeoMoonk <sunghoon1216@skuniv.ac.kr>
2023-11-08 19:25:36 +0900

    Refactor: 배열 나누기와 변환 ë 리팩토링

--- src/main/java/lotto/domain/lotteryCommittee/service/LotteryCommitteeService.java
+++ src/main/java/lotto/domain/lotteryCommittee/service/LotteryCommitteeService.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 public class LotteryCommitteeService {
 
+    static final String SUCCESE_CODE = "[SUCCESS]";
     static final String ERROR_MSG_HEADER = "[ERROR]";
     static final int START_NUMBER = 1;
     static final int END_NUMBER = 45;
@@ -19,20 +20,85 @@ public class LotteryCommitteeService {
 
         System.out.println("당첨 번호를 입력해 주세요.");
         String WeeklyNumber = Console.readLine();
-        checkNumber(WeeklyNumber);
 
-    }
-
-    public void checkNumber(String numbers) {
-
-        Map<String, Object> splitResult = splitNumbers(numbers);
-        if(!checkResult(splitResult)) {
+        if(!checkResult(checkNumber(WeeklyNumber).get("Result").toString())) {
+            failWorks(checkNumber(WeeklyNumber).get("Result").toString());
+            getWeeklyNumber();
             return;
         }
+    }
 
-        String[] splitData = (String[]) Arrays.array(splitResult.get("Data"));
+    public Map<String, Object> checkNumber(String numbers) {
 
-        return;
+        Map<String, Object> finalResultMap = new HashMap<>();
+
+        /*
+        체크해야 하는 것
+        1. 쉼표로 구분된 **6개**의
+        2. **숫자**인가?
+        3. 1~45 사이의 값인가? (Arange 체크)
+        4. 중복된 값이 들어있지는 않은가?
+        */
+
+        Map<String, Object> splitResult = splitNumbers(numbers);
+        if(!checkResult(splitResult.get("Result").toString())) {
+            return splitResult;
+        }
+
+        Map<String, Object> parsingResult = parsingNumbers((String[]) splitResult.get("Data"));
+        if(!checkResult(parsingResult.get("Result").toString())) {
+            return parsingResult;
+        }
+
+        finalResultMap.put("Result", SUCCESE_CODE);
+        finalResultMap.put("Data", "");
+        return finalResultMap;
+    }
+
+    public Map<String, Object> splitNumbers(String numbers) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        String[] splitNumbers = numbers.split(",");
+
+        if(splitNumbers.length != 6) {
+            resultMap.put("Result", ERROR_MSG_HEADER + "쉼표로 구분된 6개의 숫자가 입력되어야 합니다.");
+            return resultMap;
+        }
+
+        resultMap.put("Result", SUCCESE_CODE);
+        resultMap.put("Data", splitNumbers);
+
+        return resultMap;
+    }
+
+    public Map<String, Object> parsingNumbers(String[] splitNumbers) {
+
+        Map<String, Object> resultMap = new HashMap<>();
+        List<Integer> parsedNums = new ArrayList<>();
+
+        for(String number : splitNumbers) {
+            try {
+                 parsedNums.add(Integer.parseInt(number));
+            } catch(NumberFormatException e) {
+                resultMap.put("Result", ERROR_MSG_HEADER + "숫자 이외의 값은 입력할 수 없습니다.");
+                return resultMap;
+            }
+        }
+
+        resultMap.put("Result", SUCCESE_CODE);
+        resultMap.put("Data", parsedNums);
+
+        return resultMap;
+    }
+
+    public boolean checkResult(String resultMsg) {
+
+        if(resultMsg.startsWith(ERROR_MSG_HEADER)) {
+            return false;
+        }
+
+        return true;
     }
 
     public void failWorks(String errorMsg) {
@@ -44,54 +110,6 @@ public class LotteryCommitteeService {
             getWeeklyNumber();
         }
     }
-
-    public boolean checkResult (Map<String, Object> resultMap) {
-
-        String resultMsg = resultMap.toString();
-
-        if(resultMsg.startsWith(ERROR_MSG_HEADER)) {
-            failWorks(resultMsg);
-            return false;
-        }
-
-        return true;
-    }
-
-    public Map<String, Object> splitNumbers(String numbers) {
-
-        Map<String, Object> resultMap = new HashMap<>();
-
-        String[] splitNumbers = numbers.split(",");
-
-        if(splitNumbers.length != 6) {
-            resultMap.put("Result", ERROR_MSG_HEADER + "쉼표로 구분된 6자리 숫자가 입력되어야 합니다.");
-            return resultMap;
-        }
-
-        resultMap.put("Result", "SUCCESS");
-        resultMap.put("Data", splitNumbers);
-        return resultMap;
-    }
-
-//    public Map<String, Object> parsingNumbers(String[] splitNumbers) {
-//
-//        Map<String, Object> resultMap = new HashMap<>();
-//        List<Integer> parsedNumbers = new ArrayList<>();
-//
-//        for(String number : splitNumbers) {
-//            try {
-//                int num = Integer.parseInt(number);
-//                parsedNumbers.add(num);
-//            } catch (NumberFormatException e) {
-//                resultMap.put("RESULT", ERROR_MSG_HEADER + "숫자만 입력되어야 합니다.");
-//                return resultMap;
-//            }
-//        }
-//        resultMap.put("Result", "SUCCESS");
-//        resultMap.put("Data", parsedNumbers);
-//
-//        return resultMap;
-//    }
 
     public void announceResult(Buyer buyer) {
 

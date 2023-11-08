1777b0e636be8b07a300f65010568263c418a67b
dhlrwu <yso7414@sookmyung.ac.kr>
2023-11-08 13:13:38 +0900

    feat(lotto): 로또 구입 금액 입력받기 및 예외 처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.service.Input;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int amount = validateAmount(Input.getPurchaseAmount());
+        validateDivisible(amount);
+
+    }
+
+    public static int validateAmount(String amount) {
+        try {
+            return Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+          throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateDivisible(int amount) {
+        if (amount % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }

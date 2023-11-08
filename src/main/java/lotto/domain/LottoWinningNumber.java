1cd3ff865af8e14ef49ff8ef1753efd94ba15f7f
JP-company <wjsdj2008@gmail.com>
2023-11-08 17:57:37 +0900

    Refactor: [수정] 당첨 로또 설정시 검증하는 메서드 이름 변경
    각 검증 메서드의 이름을 의미상 더 맞는 이름으로 변경했다.

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -1,38 +1,38 @@
 package lotto.domain;
 
 import java.util.List;
-import java.util.Objects;
 
 public class LottoWinningNumber {
     private Lotto lotto;
     private int bonusNumber;
 
     public LottoWinningNumber() {}
+
     public void setLotto(Lotto lotto) {
-        validateDuplicationBetweenLottoAndBonusNumber(lotto);
+        validateDuplication(lotto);
         this.lotto = lotto;
     }
 
     public void setBonusNumber(int bonusNumber) {
-        validateBonusNumber(bonusNumber);
-        validateDuplicationBetweenLottoAndBonusNumber(bonusNumber);
+        validateRangeOfNumbers(bonusNumber);
+        validateDuplication(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
 
-    private void validateBonusNumber(int bonusNumber) {
+    private void validateRangeOfNumbers(int bonusNumber) {
         if (bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1이상 45이하의 숫자여야 합니다.");
         }
     }
 
-    private void validateDuplicationBetweenLottoAndBonusNumber(int bonusNumber) {
+    private void validateDuplication(int bonusNumber) {
         if (this.lotto == null) { return; }
         if (this.lotto.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 로또 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
         }
     }
 
-    private void validateDuplicationBetweenLottoAndBonusNumber(Lotto lotto) {
+    private void validateDuplication(Lotto lotto) {
         if (lotto.getNumbers().contains(this.bonusNumber)) {
             throw new IllegalArgumentException("[ERROR] 로또 당첨 번호와 보너스 번호는 중복될 수 없습니다.");
         }
@@ -41,6 +41,7 @@ public class LottoWinningNumber {
     public List<Integer> getNumbers() {
         return this.lotto.getNumbers();
     }
+
     public int getBonusNumber() {
         return this.bonusNumber;
     }

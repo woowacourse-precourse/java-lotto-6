f447ca1efc407ce7c2ebc7d23dc7978e566f8756
ebang <dskhe8@gmail.com>
2023-11-08 22:25:05 +0900

    refactor(lotto): Game 클래스 GameService로 이름 변경 후 Service 패키지 하위로 이동

---
+++ src/main/java/lotto/Service/GameService.java
@@ -0,0 +1,97 @@
+package lotto.Service;
+
+import java.util.List;
+import lotto.Lotto;
+import lotto.Player;
+import lotto.Validator.InputValidator;
+
+
+public class GameService {
+
+    private Lotto winningLotto;
+    private int bonusNumber;
+
+    private int money;
+
+    public int getBonusNumber() {
+        return this.bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return this.winningLotto;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+    public void setWinningLotto(List<Integer> numbers) {
+        this.winningLotto = new Lotto(numbers);
+    }
+
+    public void setBonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void setMoney(long money) {
+        InputValidator.validateMoney(money);
+        this.money = (int) money;
+    }
+
+    private void askWinningNumbers() {
+        while (true) {
+            try {
+                String input = InputService.askUserWinningGame();
+                InputValidator.validateBlankString(input);
+                String[] inputs = InputValidator.seperateStrings(input);
+                List<Integer> numbers = InputValidator.getListOfIntegerByStrings(inputs);
+
+                this.setWinningLotto(numbers);
+                break;
+            } catch (Exception e) {
+                OutputService.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void askBonusNumber() {
+        while (true) {
+            try {
+                String input = InputService.askBonusNumber();
+                InputValidator.validateBlankString(input);
+                int bonusNumber = Integer.parseInt(input);
+                this.setBonusNumber(bonusNumber);
+                break;
+            } catch (Exception e) {
+                OutputService.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void askMoney() {
+        while (true) {
+            try {
+                String input = InputService.askMoney();
+                InputValidator.validateBlankString(input);
+
+                long money = Long.parseLong((input));
+                this.setMoney(money);
+                break;
+            } catch (Exception e) {
+                OutputService.printErrorMessage(e.getMessage());
+            }
+        }
+
+    }
+    private int convertMoneyToLotto() {
+        return this.money / 1000;
+    }
+
+    public void playGame(){
+        this.askMoney();
+        this.askWinningNumbers();
+        this.askBonusNumber();
+
+        Player player = new Player(this.money/1000);
+    }
+}
--- src/test/java/lotto/GameServiceTest.java
+++ src/test/java/lotto/GameServiceTest.java
@@ -4,17 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.Service.GameService;
 import lotto.Validator.LottoValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class GameTest {
+public class GameServiceTest {
     @DisplayName("게임서비스는 유저로부터 당첨번호를 입력받아 저장한다.")
     @Test
     void askWinningNumbers() {
-        Game game = new Game();
+        GameService game = new GameService();
         game.setWinningLotto(List.of(1, 2, 3, 4, 5, 6));
 
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
@@ -25,7 +26,7 @@ public class GameTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3, 4, 5})
     void setWinningNumbers(int n) {
-        Game game = new Game();
+        GameService game = new GameService();
         game.setBonusNumber(n);
         assertThat(game.getBonusNumber()).isEqualTo(n);
     }
@@ -34,7 +35,7 @@ public class GameTest {
     @ParameterizedTest
     @ValueSource(ints = {14000, 1000})
     void setMoney(int money) {
-        Game game = new Game();
+        GameService game = new GameService();
         game.setMoney(money);
         assertThat(game.getMoney()).isEqualTo(money);
     }
@@ -43,7 +44,7 @@ public class GameTest {
     @ParameterizedTest
     @ValueSource(ints = {0, Integer.MIN_VALUE, -1000})
     void setMoneyWithExceptionMinMax(int money) {
-        Game game = new Game();
+        GameService game = new GameService();
 
         assertThatThrownBy(() -> game.setMoney(money))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -54,7 +55,7 @@ public class GameTest {
     @ParameterizedTest
     @ValueSource(ints = {1001, 1003, 1500})
     void setMoneyWithExceptionThousand(int money) {
-        Game game = new Game();
+        GameService game = new GameService();
 
         assertThatThrownBy(() -> game.setMoney(money))
                 .isInstanceOf(IllegalArgumentException.class);

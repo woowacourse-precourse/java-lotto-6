d6a6a65a0ff6256cfb3a8da45f0181c835e77122
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 20:55:50 +0900

    feat(Lottos): 로또 get 메서드 생성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -7,15 +7,37 @@ import lotto.utils.PaymentValidator;
 public class Lottos {
     private List<Lotto> lottos;
     private int payment;
+    private int lottoCount;
 
     public Lottos(int payment) {
         PaymentValidator.validateNaturalNumber(payment);
         PaymentValidator.validateUnit(payment);
         this.payment = payment;
         lottos = new ArrayList<>();
+        lottoCount = 0;
     }
 
     public void addLotto(Lotto lotto) {
         lottos.add(lotto);
+        lottoCount++;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    public int getLottoCount() {
+        return lottoCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Lottos{" +
+                "lottos=" + lottos +
+                '}';
     }
 }

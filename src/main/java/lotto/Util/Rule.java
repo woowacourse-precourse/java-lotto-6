11fc333f52c33dc993a83a078d88f3a74f1f3329
ekeprl <ykeon26@naver.com>
2023-11-08 21:17:09 +0900

    OUTPUTMATCH_MESSAGE(보너스번호 포함되지않는 메시지 추가 DIVIDEMONEY_ERROR 문구수정

--- src/main/java/lotto/Util/Rule.java
+++ src/main/java/lotto/Util/Rule.java
@@ -22,9 +22,9 @@ public class Rule {
     public static final String INPUTBONUSNUM_MESSAGE = "보너스 번호를 입력해 주세요.";
     //메시지(OUTPUT)
     public static final String OUTPUTLOTTOCOUNT_MESSAGE = "%d개를 구매했습니다.";
-    public static final String OUTPUTWINNING_MESSAGE = "당첨 통계\" + NEWLINE + \"---\"";
-    public static final String OUTPUTLOTTORESULT_MESSAGE = "%d개 일치 (%s원) - %d개";
+    public static final String OUTPUTWINNING_MESSAGE = "당첨 통계";
     public static final String OUTPUTBONUSMATCH_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    public static final String OUTPUTMATCH_MESSAGE = "%d개 일치 (%s원) - %d개";
     public static final String OUTPUTRATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
 
@@ -34,8 +34,7 @@ public class Rule {
     public static final String LOTTO_NUM_RANGE = ERROR + MIN_NUMBER + "부터" + MAX_NUMBER + "까지의 숫자만 입력가능합니다.";
     public static final String LOTTO_NUM_SIZE_ERROR = ERROR + "6개의 번호를 입력해야 합니다";
     public static final String NOTNUMBER_PATTERN = ERROR + "당첨번호의 입력 형식이 맞지 않습니다.";
-    public static final String LOTTO_BUY_ERROR = ERROR + "1000원 단위의 금액만 입력할 수 있습니다";
     public static final String MONEYZERO_ERROR = ERROR + "0원은 입력할 수 없습니다.";
-    public static final String DIVIDEMONEY_ERROR = ERROR + "1000원으로 나눠떨어지지 않습니다.";
+    public static final String DIVIDEMONEY_ERROR = ERROR + "1000원 단위의 금액만 입력할 수 있습니다";
 
  }

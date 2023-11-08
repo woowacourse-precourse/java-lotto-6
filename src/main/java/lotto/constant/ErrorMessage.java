f6b6ad0d36f5765aec5cd6f00ca31dbc7866ecd3
parang <69078515+pjy2163@users.noreply.github.com>
2023-11-08 18:25:30 +0900

    refactor : 오타 수정

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -8,8 +8,8 @@ public enum ErrorMessage {
     INVALID_BONUS_NUMBER_RANGE("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다." ),
     INVALID_BONUS_NUMBER_SIZE("[ERROR] 보너스 번호의 개수가 1개여야 합니다." ),
     INVALID_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다." ),
-    INVALID_LOTTE_NUMBER_SIZE("[ERROR] 로또 번호의 개수가 6개여야 합니다."),
-    DUPLICATION_LOTTE_NUMBER("[ERROR] 로또 번호에 중복이 없어야 합니다." );
+    INVALID_LOTTO_NUMBER_SIZE("[ERROR] 로또 번호의 개수가 6개여야 합니다."),
+    DUPLICATION_LOTTO_NUMBER("[ERROR] 로또 번호에 중복이 없어야 합니다." );
 
     private final String message;
 

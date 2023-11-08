c5ac9769c196abc00dca447b44838cb1a40ac42a
huhdy32 <mong3268@gmail.com>
2023-11-08 12:17:02 +0900

    test: 오름차순 정렬 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -37,6 +37,13 @@ class LottoTest {
                 .isEqualTo("[1, 2, 3, 4, 5, 6]");
     }
 
+    @Test
+    @DisplayName("로또 번호가 오름차순으로 정렬되는지 확인한다.")
+    void provide_UnSorted_Numbers() {
+        assertThat(new Lotto(List.of(6,5,4,3,2,1)))
+                .isEqualTo("[1, 2, 3, 4, 5 ,6]");
+    }
+
     @ParameterizedTest
     @MethodSource("provide_getScore_TestData")
     @DisplayName("점수를 제대로 리턴하는지 확인")

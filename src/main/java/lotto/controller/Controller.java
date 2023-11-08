c646fe364425dc07bdb93698c280df4115fa2ce5
Nari <avakray@hansung.ac.kr>
2023-11-08 19:59:11 +0900

    feat(lotto): 로또 생성 처리

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,55 +1,52 @@
 package lotto.controller;
 
+import lotto.domain.EnumLotto;
 import lotto.domain.Lotto;
-import lotto.service.EnumGame;
 import lotto.service.Game;
-import lotto.util.Validator;
 import lotto.view.Input;
+import lotto.view.Output;
 import lotto.repository.LottoRepository;
 import lotto.util.RandomUtil;
 
 public class Controller {
-    private Game game;
-    private LottoRepository lottoRepository;
-    private RandomUtil randomUtil;
+    private final Game game;
+    private final LottoRepository lottoRepository;
+    private final RandomUtil randomUtil;
+    private final Output output;
+    private final Input input;
 
-    public Controller(Game game) {
-        lottoRepository = new LottoRepository(getNumberOfLotto());
+    public Controller(Game game, LottoRepository lottoRepository, Input input) {
+        this.game = game;
+        this.lottoRepository = lottoRepository;
+        this.input = input;
+        this.output = new Output(lottoRepository);
         randomUtil = new RandomUtil();
     }
 
-    public void createLotto(){
-        for(int i = 0; i < lottoRepository.getNumberOfLotto(); i++){
-            setLottoNumbers();
-        }
-        for(Lotto lotto : lottoRepository.getLottos()){
-            System.out.println(lotto.getNumbers());
-        }
+    public void createLotto() {
+        setBuyNumberOfLotto();
+        setLottoNumbers();
     }
 
-    private int getNumberOfLotto() {
-        boolean isValidate = false;
-        String input = "";
-
-        while(!isValidate){
-            input = Input.inputBuyPrice();
-            isValidate = validateBuyPrice(input);
-        }
-
-        return Integer.parseInt(input) / EnumGame.LOTTO_PRICE.getMoney();
+    public void printLottoGameResult() {
+        output.printGameResult(game.getRankCount());
+        output.printGameResult(game.getEarningsRate());
     }
 
-    private boolean validateBuyPrice(String input) {
-        try{
-            Validator.validateRemainderNumber(input);
-            return true;
-        }catch(IllegalArgumentException e){
-            System.out.println(e.getMessage());
-            return false;
-        }
+    private void setBuyNumberOfLotto() {
+        int numberOfLotto = input.inputValidateNumberOfLotto(EnumLotto.LOTTO_PRICE.getNumber());
+        lottoRepository.setNumberOfLotto(numberOfLotto);
+        output.printBuyNumberOfLotto();
     }
 
     private void setLottoNumbers() {
+        for(int i = 0; i < lottoRepository.getNumberOfLotto(); i++) {
+            createLottoNumbers();
+        }
+        output.printGetLottoNumbers();
+    }
+
+    private void createLottoNumbers() {
         lottoRepository.createLotto(new Lotto(randomUtil.getRandomNumbers()));
     }
 }

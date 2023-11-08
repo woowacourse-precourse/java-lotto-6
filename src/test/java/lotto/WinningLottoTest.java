ca8b6aa3da438e435bad907c40d691212f7cd734
hj-k66 <hjx66@naver.com>
2023-11-08 22:10:33 +0900

    test: WinningLotto 단위 테스트 수행

---
+++ src/test/java/lotto/WinningLottoTest.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningLottoTest {
+    @DisplayName("당첨 번호에 보너스 번호와 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByDuplicatedBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호에 숫자 범위가 1~45를 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningLottoByOverRangeBonusNumber() {
+        assertThatThrownBy(() -> new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 사용자의 로또를 비교해 등수를 구한다.")
+    @Test
+    void matchTest() {
+        //given
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        //when
+        Rank rank = winningLotto.match(userLotto);
+        //then
+        assertThat(rank.name()).isEqualTo("SECOND");
+    }
+}

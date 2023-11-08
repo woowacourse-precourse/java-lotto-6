a0890d48d4362ee314cb597cb26606d96dc42054
jmj005 <jmj00005@naver.com>
2023-11-08 18:27:22 +0900

    refactor: LottoResult 리팩토링

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -15,20 +15,24 @@ public class LottoResult {
     public static LottoResult calculateLottoResult(WinningLotto winningLotto, LottoTickets lottoTickets) {
         EnumMap<Rank, Integer> rank = new EnumMap<>(Rank.class);
         EnumSet.allOf(Rank.class).forEach(grade -> rank.put(grade, 0));
+        updateRank(rank, winningLotto, lottoTickets);
+        return new LottoResult(rank);
+    }
+
+    private static void updateRank(EnumMap<Rank, Integer> rank, WinningLotto winningLotto, LottoTickets lottoTickets) {
         for (Lotto lottoTicket : lottoTickets.getLottoTickets()) {
             int matchCount = winningLotto.getMatchCount(lottoTicket);
             Rank currentRank = Rank.findRankByMatchCount(matchCount, winningLotto.hasBonusNumber(lottoTicket));
             rank.put(currentRank, rank.get(currentRank) + 1);
         }
-        return new LottoResult(rank);
     }
 
     public double getRate(Money money) {
-        long sumPrize = 0;
-        for (Rank rank : getLottoResult().keySet()) {
-            sumPrize += rank.getReward() * lottoResult.get(rank);
-        }
-        return ((double)sumPrize / THOUSAND) / money.getLottoCount() * 100;
+        long sumReward = getLottoResult().entrySet()
+                .stream()
+                .mapToLong(entry -> entry.getKey().getReward() * entry.getValue())
+                .sum();
+        return ((double)sumReward / THOUSAND) / money.getLottoCount() * 100;
     }
 
     public EnumMap<Rank, Integer> getLottoResult() {

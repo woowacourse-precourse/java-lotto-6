34938d60d9be104a1fd97c78e4d68957d517a9fe
eora21 <eora21@naver.com>
2023-11-08 10:02:56 +0900

    feat: 🎸 (ConsoleOutputView) 추가 발행한 로또 수량 및 번호 출력 메서드 작성

--- src/main/java/view/ConsoleOutputView.java
+++ src/main/java/view/ConsoleOutputView.java
@@ -1,10 +1,34 @@
 package view;
 
+import domain.Lotto;
+import domain.LottoNumber;
+import dto.LottoTickets;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ConsoleOutputView implements OutputView {
     private static final String REQUEST_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String PRINT_LOTTO_TICKETS_SIZE = "개를 구매했습니다.";
+    private static final String PRINT_LOTTO_NUMBERS = "[%s]%n";
 
     @Override
     public void requestPurchaseAmount() {
         System.out.println(REQUEST_PURCHASE_AMOUNT);
+    }
+
+    @Override
+    public void printLottoTickets(LottoTickets lottoTickets) {
+        List<Lotto> tickets = lottoTickets.getLottoTickets();
+        System.out.println(tickets.size() + PRINT_LOTTO_TICKETS_SIZE);
+        tickets.forEach(this::printLottoNumbers);
+    }
+
+    private void printLottoNumbers(Lotto lotto) {
+        List<LottoNumber> lottoNumbers = lotto.getLottoNumbers();
+        String numbersJoinComma = lottoNumbers.stream()
+                .map(LottoNumber::lottoNumberValue)
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        System.out.printf(PRINT_LOTTO_NUMBERS, numbersJoinComma);
     }
 }

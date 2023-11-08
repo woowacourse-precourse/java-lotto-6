d4066dfec3b210a1fadcbc98a956f12d15521909
Hyun Woo <hwsa10041@gmail.com>
2023-11-08 13:13:18 +0900

    feat : 로또 번호가 1 ~ 45 사이의 수 인지 검증하는 메서드 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -7,11 +7,13 @@ import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 public class Lotto {
     public static final String ERROR_MESSAGE_DUPLICATE_NUMBERS = "[ERROR] 로또 번호가 중복되어서는 안됩니다.";
     public static final String ERROR_MESSAGE_NOT_SIX_NUMBERS = "[ERROR] 로또 번호는 6개로 이루어져야 합니다.";
+    public static final String ERROR_MESSAGE_NUMBER_IN_RANGE = "[ERROR] 로또 번호는 1 ~ 45 사이의 숫자여야 합니다.";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSixNumbers(numbers);
         validateDuplicateNumbers(numbers);
+        validateNumberInRange(numbers);
         this.numbers = new ArrayList<>(numbers);
         Collections.sort(this.numbers);
     }
@@ -23,6 +25,14 @@ public class Lotto {
             lottos.add(new Lotto(numbers));
         }
         return lottos;
+    }
+
+    private static void validateNumberInRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ERROR_MESSAGE_NUMBER_IN_RANGE);
+            }
+        }
     }
 
     private static void validateDuplicateNumbers(List<Integer> numbers) {

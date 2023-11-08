bdfcf97053bf46494055b58f0a215edf0845e315
KwangHo <skh990427@naver.com>
2023-11-08 18:40:25 +0900

    feat: 로또 결과 확인 후 저장하는 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,8 +4,11 @@ import domain.LottoGenerator;
 import domain.LottoPrinter;
 import domain.PurchaseInput;
 import domain.WinningNumbers;
+import enums.LottoScore;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -22,5 +25,11 @@ public class Application {
         winningNumbers.inputWinningNumbers();
         winningNumbers.inputBonusNumber();
 
+        Map<LottoScore, Integer> scoreCountMap = new HashMap<>();
+
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            LottoScore score = lottoNumbers.get(i).calculate(winningNumbers.getWinningNumbers(), winningNumbers.getBonusNumber());
+            scoreCountMap.put(score, scoreCountMap.getOrDefault(score, 0) + 1);
+        }
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import enums.LottoError;
+import enums.LottoScore;
 
 import java.util.HashSet;
 import java.util.List;
@@ -35,5 +36,35 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public LottoScore calculate(List<Integer> winningNumbers, int bonusNumber) {
+        int correctCount = countCorrectNumbers(winningNumbers);
+        boolean correctBonus = isCorrectBonusNumber(bonusNumber);
+        if(correctCount == 6)
+            return LottoScore.FIRST;
+        if(correctCount == 5 && correctBonus)
+            return LottoScore.SECOND;
+        if(correctCount == 5)
+            return LottoScore.THIRD;
+        if(correctCount == 4)
+            return LottoScore.FOURTH;
+        if(correctCount == 3)
+            return LottoScore.FIFTH;
+        return LottoScore.NO;
+    }
+
+    private int countCorrectNumbers(List<Integer> winningNumbers) {
+        int count = 0;
+        for (Integer number : winningNumbers) {
+            if (numbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isCorrectBonusNumber(int bonusNumber) {
+        return numbers.contains(bonusNumber);
     }
 }

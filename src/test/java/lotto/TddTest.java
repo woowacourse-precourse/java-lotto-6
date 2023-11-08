9436f44920682ad12a76b03e4efcf6b27a78e9ec
jeonseong-gwon <jeonsg9904@gmail.com>
2023-11-08 23:56:29 +0900

    Test : 이전 Test 주석처리

--- src/test/java/lotto/TddTest.java
+++ src/test/java/lotto/TddTest.java
@@ -25,16 +25,16 @@ public class TddTest {
         assertThat(lottoCount).isEqualTo(8);
     }
 
-    @Test
-    public void 로또_구매입력이_1000원_단위가_아닐때() {
-        LottoController lottoController = new LottoController();
-
-        int money = 1500;
-        assertThatThrownBy(() -> {
-            lottoController.calculateLottoCount(money);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또는 1000원 단위만 구매할 수 있습니다.");
-    }
+//    @Test
+//    public void 로또_구매입력이_1000원_단위가_아닐때() {
+//        LottoController lottoController = new LottoController();
+//
+//        int money = 1500;
+//        assertThatThrownBy(() -> {
+//            lottoController.calculateLottoCount(money);
+//        }).isInstanceOf(IllegalArgumentException.class)
+//                .hasMessage("로또는 1000원 단위만 구매할 수 있습니다.");
+//    }
 
     @Test
     public void _1부터_45사이_로또번호_생성_테스트() {

0bba9b526c3be200f184c3a9398df929317ab982
Jeonwoojae <dnwo0719@icloud.com>
2023-11-08 18:04:14 +0900

    feat: Application에서 프로그램 종료 후 콘솔 닫기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController();
         lottoController.run();
+        Console.close();
     }
 }

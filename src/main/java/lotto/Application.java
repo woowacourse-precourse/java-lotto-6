3dbfdc9ac369f217dd881b40dce37a351122dceb
elizabethgim <yujingim@naver.com>
2023-11-08 14:55:15 +0900

    feat: 1000원 단위로 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,7 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Input input = new Input();
+        int money = input.purchase();
     }
 }
--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -5,7 +5,14 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public int purchase(){
         System.out.println("구입금액을 입력해 주세요.\n");
-        int input = Integer.parseInt(Console.readLine());
-        return input;
+        int money = Integer.parseInt(Console.readLine());
+        validatePurchase(money);
+        return money;
+    }
+
+    private void validatePurchase(int money){
+        if(money % 1000 != 0){
+            throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매가 가능합니다.");
+        }
     }
 }

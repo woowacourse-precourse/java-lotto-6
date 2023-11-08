23316ec0eda8035dc9a3ff5d28639b9a2db50895
BangMingGing <privatebmk802@naver.com>
2023-11-08 18:24:08 +0900

    feat(LottoController): Add get winning numbers function

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,15 @@
 package lotto.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.model.Bonus;
 import lotto.model.Lotto;
 import lotto.service.LottoService;
 import lotto.utils.PurchasePriceValidator;
 
+import lotto.utils.WinningNumbersValidator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -21,6 +26,7 @@ public class LottoController {
     public void start() {
         int purchasePrice = getPurchasePrice();
         List<Lotto> lottos = purchaseLottos(purchasePrice);
+        Lotto WinningNumbers = getWinningNumbers();
     }
 
     private int getPurchasePrice() {
@@ -35,6 +41,22 @@ public class LottoController {
         List<Lotto> lottos = lottoService.publishLottos(price);
         outputView.printPurchaseLottos(lottos);
         return lottos;
+    }
+
+    private Lotto getWinningNumbers() {
+        String input = inputView.getWinningNumbers();
+        WinningNumbersValidator.validate(input);
+        return convertStringToLotto(input);
+    }
+
+    private Lotto convertStringToLotto(String input) {
+        String[] digits = input.split(",");
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String digit : digits) {
+            WinningNumbersValidator.validDigitIsInt(digit);
+            winningNumbers.add(Integer.parseInt(digit));
+        }
+        return new Lotto(winningNumbers);
     }
 
 }

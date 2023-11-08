eb124ebfa28a055bda50f881371d9779fd6791c2
DonghaOh <donghadarwin@naver.com>
2023-11-08 14:05:10 +0900

    feat: 발행한 로또 수량 문구 출력
    generateLottoTickets 메소드

---
+++ src/main/java/view/View.java
@@ -0,0 +1,11 @@
+package view;
+
+public class View {
+    public static void moneyMessagePrint() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void lottoTicketsCountPrint(int lottoTicketsCount) {
+        System.out.println(lottoTicketsCount + "개를 구매했습니다.");
+    }
+}
--- src/main/java/view/inputView.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-public class inputView {
-    public static void moneyMessagePrint() {
-        System.out.println("구입금액을 입력해 주세요.");
-    }
-}

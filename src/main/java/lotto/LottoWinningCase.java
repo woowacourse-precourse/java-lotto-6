ac8ea74ba8892dfe8d4cced216954f4d3bfccdc0
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 15:37:49 +0900

    feat(LottoWinningCase): 당첨 되는 상황에 관한 enum클래스 생성 및 구현
    나의 로또 번호가 몇등을 했는지 등수를 정리해놓았고 해당 클래스에서 내
    번호가 몇등을 했는지 판단하는 메소드 또한 구현

--- src/main/java/lotto/LottoWinningCase.java
+++ src/main/java/lotto/LottoWinningCase.java
@@ -1,2 +1,56 @@
-package lotto;public enum LottoWinningCase {
+package lotto;
+
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.ErrorMessage.FIVE;
+
+public enum LottoWinningCase {
+    UNRANK(0, List.of(0, 1, 2)),
+    FIRST_PLACE(1, List.of(6)),
+    SECONND_PLACE(2, List.of(5)),
+    THRID_PLACE(3, List.of(5)),
+    FOURTH_PLACE(4, List.of(4)),
+    FIFTH_PLACE(5, List.of(3));
+
+    private final int place;
+    private final List<Integer> numberMatchWithWinningLotto;
+
+    LottoWinningCase(int place, List<Integer> numberMatchWithWinningLotto) {
+        this.place = place;
+        this.numberMatchWithWinningLotto = numberMatchWithWinningLotto;
+    }
+
+    public static LottoWinningCase compareWhichCaseItIs(Lotto myLottoNumbers, List<Integer> winningNumber, int bonusNumnber) {
+        int matchCount = getHowManyMathch(myLottoNumbers, winningNumber);
+        LottoWinningCase myCase = Arrays.stream(LottoWinningCase.values())
+                .filter(lottoWinningCase -> lottoWinningCase.getNumberMatchWithWinningLotto().contains(matchCount))
+                .findFirst()
+                .orElse(null);
+        if(myCase.getPlace() == FIVE && !BonusNumberMatch(myLottoNumbers.getNumbers(), bonusNumnber)) {
+            myCase = THRID_PLACE;
+        }
+        return myCase;
+    }
+
+    private static boolean BonusNumberMatch(List<Integer> myLottoNumbers, int bonusNumnber) {
+        return myLottoNumbers.contains(bonusNumnber);
+    }
+
+    private static int getHowManyMathch(Lotto myLottoNumbers, List<Integer> winningNumber) {
+        return (int) myLottoNumbers.getNumbers().stream()
+                .filter(winningNumber::contains)
+                .count();
+    }
+
+    public int getPlace() {
+        return place;
+    }
+
+    public List<Integer> getNumberMatchWithWinningLotto() {
+        return numberMatchWithWinningLotto;
+    }
 }

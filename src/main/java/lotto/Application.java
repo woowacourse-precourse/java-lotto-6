bc6f334af88abbf2478d74416c6fc37f98087831
Jiihyun <cwlgusc@gmail.com>
2023-11-08 09:41:42 +0900

    refactor(Config): 주입 위치 Application -> LottoConfig로 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,25 +1,12 @@
 package lotto;
 
+import lotto.config.LottoConfig;
 import lotto.controller.GameController;
-import lotto.domain.LottoNumberGenerator;
-import lotto.domain.LottoStore;
-import lotto.io.InputReader;
-import lotto.io.OutputWriter;
-import lotto.view.InputView;
-import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        GameController gameController = getGameController();
+        LottoConfig lottoConfig = new LottoConfig();
+        GameController gameController = lottoConfig.getGameController();
         gameController.start();
-    }
-
-    private static GameController getGameController() {
-        InputReader reader = new InputReader();
-        OutputWriter writer = new OutputWriter();
-        InputView inputView = new InputView(writer, reader);
-        OutputView outputView = new OutputView(writer);
-        LottoStore lottoStore = new LottoStore(new LottoNumberGenerator());
-        return new GameController(inputView, outputView, lottoStore);
     }
 }

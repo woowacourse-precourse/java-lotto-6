c49777def3b98419f1f2b1e0beabc60bdfe71ad8
Bellwin98 <jongseunghan@naver.com>
2023-11-08 09:08:23 +0900

    Feat(Lotto): 입력값 중복 여부 검증, 당첨 횟수 업데이트 기능, Getter 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,24 +1,53 @@
 package lotto.domain;
 
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.domain.exception.LottoExceptionList.NUMBERS_DUPLICATE_ERROR;
+import static lotto.domain.exception.LottoExceptionList.NUMBERS_LENGTH_ERROR;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int hits = 0;
+    private boolean bonusStatus;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateNumbersDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    public List<Integer> getSortedNumbers(){
-        Collections.sort(numbers);
+    public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public boolean getBonusStatus() {
+        return bonusStatus;
+    }
+
+    public void updateHits() {
+        hits++;
+    }
+
+    public void changeBonusStatus() {
+        bonusStatus = true;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NUMBERS_LENGTH_ERROR.getMessage());
+        }
+    }
+
+    private void validateNumbersDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersDuplicateCheck = new HashSet<>(numbers);
+        if (numbersDuplicateCheck.size() != numbers.size()) {
+            throw new IllegalArgumentException(NUMBERS_DUPLICATE_ERROR.getMessage());
         }
     }
 }

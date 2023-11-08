a9c76a79fd501a12287e1e5073a3395857e63d7d
shining013 <nevertheless113@naver.com>
2023-11-08 21:42:34 +0900

    feat: compareWithBonus test 메서드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,9 +4,11 @@ package lotto;
     import org.junit.jupiter.api.DisplayName;
     import org.junit.jupiter.api.Test;
 
+    import java.util.ArrayList;
     import java.util.List;
 
     import static org.assertj.core.api.Assertions.assertThatThrownBy;
+    import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +26,31 @@ class LottoTest {
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    public void testCompareWith() {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        lottoNumbers.add(1);
+        lottoNumbers.add(2);
+        lottoNumbers.add(3);
+        lottoNumbers.add(4);
+        lottoNumbers.add(5);
+        lottoNumbers.add(6);
+
+        Lotto lotto = new Lotto(lottoNumbers);
+
+        List<Integer> inputNumbers = new ArrayList<>();
+        inputNumbers.add(1);
+        inputNumbers.add(2);
+        inputNumbers.add(3);
+        inputNumbers.add(7);
+        inputNumbers.add(8);
+        inputNumbers.add(9);
+
+        int matchCount = lotto.compareWith(inputNumbers);
+
+        assertThat(matchCount).isEqualTo(3); // 1, 2, 3이 일치
+    }
+
+
+
 }

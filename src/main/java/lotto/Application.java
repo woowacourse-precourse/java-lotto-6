39b5e8db51ac52fe93ceb7e94f0553f999e8d923
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-11-08 16:10:16 +0900

    feat : 프로젝트 도메인 설정
    - Application 애플리케이션 실행을 위한 클래스
    - View 사용자 입력을 받는 클래스
    - Controller 상황에 맞는 서비스 로직을 호출하는 클래스
    - Service 기능 동작 로직을 담은 클래스
    - Lotto 로또 번호를 관리하기 위한 클래스

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,5 +3,11 @@ package lotto;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Application application = new Application();
+        application.run();
+    }
+
+    void run() {
+
     }
 }

0b2b3fd676f6331c87d3e24dcc09a7e20945a553
jay <wlguswhd0809@naver.com>
2023-11-08 21:00:59 +0900

    feat: 금액 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,37 @@
 package lotto.view;
 
 import static lotto.constant.InputViewConstant.START_MESSAGE;
+import static lotto.util.InputValidation.validateEmptyInput;
+import static lotto.util.InputValidation.validateMoneyInput;
+import static lotto.util.InputValidation.validationStringInput;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    public static int inputMoney;
+
     public static void setStartMessage() {
         System.out.println(START_MESSAGE);
+    }
+
+    public static int setInputMoney() {
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                String input = Console.readLine();
+                validateEmptyInput(input);
+                validationStringInput(input);
+
+                inputMoney = Integer.parseInt(input);
+                validateMoneyInput(inputMoney);
+
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return inputMoney;
     }
 }

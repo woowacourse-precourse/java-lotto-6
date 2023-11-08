55eef7eb121c4b920612994985f8586587bcc531
pjaehyun <show6756@gmail.com>
2023-11-08 14:33:09 +0900

    feat: 추가 로또 관련 상수

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -11,4 +11,12 @@ public class LottoConstants {
 
     public static final String COUNT_MESSAGE = "개";
     public static final String TOTAL_PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
+
+    public static final int LOTTO_START_NUMBER = 1;
+
+    public static final int LOTTO_END_NUMBER = 45;
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+
+    public static final int LOTTO_PRICE = 1000;
 }

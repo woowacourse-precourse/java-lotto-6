d8e97f20f709f6844f1b90db4de3962c7ed2c4eb
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:16:05 +0900

    feat(ErrorMessage): 클래스 생성 및 에러 메시지 상수 정의

---
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -0,0 +1,14 @@
+package lotto.constants;
+
+public class ErrorMessage {
+    public static final String LOTTO_NUMBER_COUNT_ERROR_MESSAGE = "로또 번호의 개수는 6개여야 합니다.";
+    public static final String LOTTO_NUMBER_RANGE_ERROR_MESSAGE = "로또 번호는 1부터 45사이의 숫자여야 합니다.";
+    public static final String LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE = "로또 번호는 중복될 수 없습니다.";
+
+    public static final String AMOUNT_UNDER_THOUSAND_ERROR_MESSAGE = "구입 금액은 1000원 이상이어야 합니다.";
+    public static final String NON_THOUSAND_MULTIPLE_ERROR_MESSAGE = "구입 금액이 1000원 단위이어야 합니다.";
+
+    public static final String BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE = "당첨 번호와 보너스 번호는 중복될 수 없습니다.";
+
+    public static final String IS_NOT_NUMERIC_ERROR_MESSAGE = "입력에 숫자가 아닌 문자가 포함되어 있거나 숫자가 너무 큽니다.";
+}

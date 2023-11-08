9113aab201b52014ef64864448898fa3c57b514c
Hyun Woo <hwsa10041@gmail.com>
2023-11-08 12:55:12 +0900

    refactor : 수익률 계산 메서드 기능 단위 분리 및 상수화 진행

--- src/main/java/lotto/model/Result.java
+++ src/main/java/lotto/model/Result.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 public class Result {
+    private static final double PROFIT_RATE_MULTIPLIER = 1000.0;
+    private static final double PROFIT_RATE_PERCENTAGE = 100.0;
+    private static final double PROFIT_RATE_BASE = 10.0;
     private static final int DEFAULT_COUNT = 0;
 
     private final Map<PrizeRank, Integer> prizeCounts;
@@ -50,19 +53,25 @@ public class Result {
     }
 
     public Double calculateProfitRate() {
+        int sum = sumPrizeAmounts();
+        int purchaseAmount = getPurchaseAmount();
+        double profitRate = ((double) (sum - purchaseAmount) / purchaseAmount) * PROFIT_RATE_MULTIPLIER;
+        profitRate = PROFIT_RATE_PERCENTAGE + (Math.round(profitRate) / PROFIT_RATE_BASE);
+
+        return profitRate;
+    }
+
+    private int getPurchaseAmount() {
+        return purchase.getPurchaseCount() * 1000;
+    }
+
+    private int sumPrizeAmounts() {
         int sum = 0;
         for (PrizeRank prizeRank : prizeCounts.keySet()) {
             Integer count = prizeCounts.get(prizeRank);
             int price = prizeRank.getPrice();
             sum += (count * price);
         }
-
-        int purchaseAmount = purchase.getPurchaseCount() * 1000;
-        double profitRate = ((double) (sum - purchaseAmount) / purchaseAmount) * 100;
-
-        // 소수점 둘째 자리에서 반올림
-        profitRate = Math.round(profitRate * 10) / 10.0;
-
-        return 100 + profitRate;
+        return sum;
     }
 }

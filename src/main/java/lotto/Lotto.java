38f0bb156abef024046d295241f4e322cbfe62a7
SangCheol <scpark0698@gmail.com>
2023-11-08 21:30:45 +0900

    feat: Lotto 검증 로직 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +14,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateLottoSize(numbers);
+        validateLottoDuplicate(numbers);
+    }
+
+    private void validateLottoDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>();
+        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호들은 중복될 수 없습니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private static void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
+        }
+    }
 }

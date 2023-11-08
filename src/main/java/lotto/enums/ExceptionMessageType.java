a11aadf059d9e52ca3a3cdc014a263f9adde565a
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 10:29:30 +0900

    feat(ExceptionMessageType): 구분자 검사 메시지 수정

--- src/main/java/lotto/enums/ExceptionMessageType.java
+++ src/main/java/lotto/enums/ExceptionMessageType.java
@@ -4,7 +4,7 @@ public enum ExceptionMessageType {
     NUMBER_NOT_INTEGER("문자를 제외한 2147483647 이하 정수만 가능합니다."),
     PAYMENT_NOT_NATURAL_NUMBER("구입 금액은 0원보다 커야 합니다."),
     PAYMENT_INVALID_UNIT("구입 금액은 1,000원 단위만 가능합니다."),
-    INVALID_SEPARATOR_OR_NUMBER_COUNT("6개의 숫자를 쉼표(,)로 구분하여 입력하세요."),
+    INVALID_SEPARATOR_OR_NUMBER_COUNT("공백 없이 6개의 숫자를 쉼표(,)로 구분하여 입력하세요."),
     NUMBER_OUT_OF_RANGE("번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_WINNING_NUMBERS("당첨 번호는 중복될 수 없습니다."),
     DUPLICATE_BONUS_NUMBER("당첨 번호와 중복되지 않은 번호를 입력하세요.");

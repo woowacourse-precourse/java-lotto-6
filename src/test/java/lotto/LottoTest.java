d78d4f82cc6a4b9a30c2b260dabbd4f27c66102d
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 19:27:41 +0900

    test: 로또와 번호가 주어졌을 때 번호가 로또 내에 포함된 번호 확인하는 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,9 +1,13 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -31,6 +35,15 @@ class LottoTest {
         Lotto comparisonTargetLotto = new Lotto(List.of(2, 3, 4, 5, 6, 7));
         int countMatchNumbers = lotto.countMatchNumbers(comparisonTargetLotto);
         Assertions.assertThat(countMatchNumbers).isEqualTo(5);
+    }
+
+    @DisplayName("로또와 번호가 주어졌을 때 번호가 로또 내에 포함된 번호인지 알 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,true","2,true","3,true","10,false"})
+    void containNumber(int number, boolean expectedContains) {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        boolean contains = lotto.contains(new LottoNumber(number));
+        Assertions.assertThat(contains).isEqualTo(expectedContains);
     }
 
 }

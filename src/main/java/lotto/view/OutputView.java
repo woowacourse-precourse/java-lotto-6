8a2113e21d1a2f6a6d8b46a054034382db54b99b
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 22:06:43 +0900

    feat: 당첨 값까지 정상적으로 출력하도록 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,27 +1,42 @@
 package lotto.view;
 
-import java.util.List;
+import java.util.Map;
 import lotto.domain.Lotto;
+import lotto.domain.Prize;
 
 public class OutputView {
 	public static void printLottoAmount(int amount) {
 		System.out.println(amount + "개를 구매했습니다.");
 	}
 
-	public static void printLottoNumbers(List<Lotto> lottoNumber) {
-		System.out.println(lottoNumber);
+	public static void printLottoNumbers(Lotto lottoNumber) {
+		System.out.println(lottoNumber.getNumbers());
 	}
 
-	public static void printPrizeResult() {
+	public static void printResult() {
 		System.out.println("당첨 통계");
 		System.out.println("---");
 	}
 
-	public static void printPrizeMessage() {
-		// TODO 당첨 값을 어떻게 보낼 지 구현 후 수정
-	}
 
-	public static void printEarningRate(double rate) {
+	public static void printPrizeResult(Map<Prize, Integer> prizeCount, long totalPrizeMoney, int amount) {
+		int chk = 0;
+		for (Prize prize : Prize.values()) {
+			if (prize.getPrizeMoney() > 0 && chk != 4) {
+				System.out.printf("%d개 일치 (%s원) - %d개%n",
+						prize.getCount(),
+						String.format("%,d", prize.getPrizeMoney()),
+						prizeCount.get(prize));
+			}
+			if (prize.getPrizeMoney() > 0 && chk == 4) {
+				System.out.printf("%d개 일치, 보너스 볼 일치 (%s원) - %d개%n",
+						prize.getCount(),
+						String.format("%,d", prize.getPrizeMoney()),
+						prizeCount.get(prize));
+			}
+			chk++;
+		}
+		double rate = (double) totalPrizeMoney / amount / 10;
 		System.out.println("총 수익률은 " + String.format("%.1f", rate) + "%입니다.");
 	}
 }

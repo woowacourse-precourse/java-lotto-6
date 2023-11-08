1a2a1d4d912551b203f8d63e83641f7e71d78f37
ASak1104 <khw56184@gmail.com>
2023-11-08 09:33:25 +0900

    fix: 잘못된 메시지 수정

--- src/main/java/lotto/game/LottoGameMessage.java
+++ src/main/java/lotto/game/LottoGameMessage.java
@@ -4,8 +4,8 @@ import lotto.constant.LottoGameNumber;
 
 public enum LottoGameMessage {
     NON_NUMBER("[ERROR] 숫자를 입력해주세요."),
-    INVALID_COST_UNIT("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.".formatted(
-            LottoGameNumber.MINIMUM, LottoGameNumber.MAXIMUM)),
+    INVALID_COST_UNIT("[ERROR] 로또 구입 가격은 %d 단위로 나누어져야 합니다.".formatted(
+            LottoGameNumber.COST_UNIT)),
     NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.".formatted(
             LottoGameNumber.MINIMUM, LottoGameNumber.MAXIMUM)),
     INVALID_LOTTO_NUMBERS("[ERROR] 로또는 중복되지 않은 %d부터 %d 사이의 숫자 %d자리여야 합니다.".formatted(

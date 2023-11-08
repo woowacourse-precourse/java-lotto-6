b8f9fc58fed3a6a35d397fd77213d6cec12484c7
BangMingGing <privatebmk802@naver.com>
2023-11-08 13:57:26 +0900

    feat(OutputView): Print output functions complete

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,30 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.model.WinningStatics;
+
+public class OutputView {
+    private static final String PURCHASE_LOTTOS = "\n%d개를 구매했습니다.\n";
+    private static final String WINNING_STATICS_PREFIX = "\n당첨 통계\n---\n";
+    private static final String RATE_OF_RETURN = "총 수익률은 %.1f%입니다.\n";
+
+    public void printPurchaseLottos(int lottoCount) {
+        System.out.printf(PURCHASE_LOTTOS, lottoCount);
+    }
+
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+    public void printWinningStatics(WinningStatics winningStatics) {
+        System.out.print(WINNING_STATICS_PREFIX);
+        System.out.println(winningStatics);
+    }
+
+    public void printRateOfReturn(float rateOfReturn) {
+        System.out.printf(RATE_OF_RETURN, rateOfReturn);
+    }
 }

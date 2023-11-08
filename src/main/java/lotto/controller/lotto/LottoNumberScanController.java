752e91325a7c6f56daab261f2ea1376b282d0caf
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:31:08 +0900

    feat : 당첨 번호를 입력 받는 객체 생성

--- src/main/java/lotto/controller/lotto/LottoNumberScanController.java
+++ src/main/java/lotto/controller/lotto/LottoNumberScanController.java
@@ -1,9 +1,30 @@
 package lotto.controller.lotto;
 
-import lotto.domain.lotto.Lotto;
+import static lotto.model.constant.StringSymbolicConstantEnum.SPLIT_POINT;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lotto.controller.input.InputController;
+import lotto.controller.validation.lotto.LottoNumberValueValidator;
+import lotto.model.lotto.Lotto;
+import lotto.view.MessageOutputView;
 
 public class LottoNumberScanController {
-
-	public void scanLottoNumber(Lotto lotto) {
+	MessageOutputView messageOutputView = new MessageOutputView();
+	InputController inputController = new InputController();
+	LottoNumberValueValidator lottoNumberValueValidator = new LottoNumberValueValidator();
+	public Lotto scanLottoNumber() {
+		try{
+			messageOutputView.printScanningLottoNumberMessage();
+			String scannedLottoNumberListString = inputController.scanLottoNumberListString();
+			lottoNumberValueValidator.checkStringValidationOfLottoNumber(scannedLottoNumberListString);
+			List<String> lottoNumberStringList = Arrays.asList(scannedLottoNumberListString.split(SPLIT_POINT.getValue()));
+			List<Integer> number = lottoNumberStringList.stream().map(string -> Integer.parseInt(string)).collect(Collectors.toList());
+			return new Lotto(number);
+		}
+		catch(IllegalArgumentException illegalArgumentException){
+			return scanLottoNumber();
+		}
 	}
 }

77cdd07634b1c05cb844dde267485e0497a60985
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-08 14:52:22 +0900

    style: 적절한 네이밍으로 변경

--- src/main/java/lotto/Model/WinningLotto.java
+++ src/main/java/lotto/Model/WinningLotto.java
@@ -7,7 +7,7 @@ public class WinningLotto {
     private static final String LOTTO_NUMBER_DELIMETER = ",";
     public static final String ERROR_NOT_LOTTO_FORMAT =
             String.format("[ERROR] 숫자와 쉼표(%s)만을 이용하고, 쉼표 사이에 숫자가 있는지 확인해주세요.", LOTTO_NUMBER_DELIMETER);
-    public static final String ERROR_END_LOTTO_NUMBER_DELEMETER =
+    public static final String ERROR_END_DELEMETER =
             String.format("[ERROR] 마지막은 %s가 올 수 없습니다.", LOTTO_NUMBER_DELIMETER);
 
     private final Lotto winningLotto;
@@ -34,7 +34,7 @@ public class WinningLotto {
 
     private void validate(String input) {
         if (input.endsWith(LOTTO_NUMBER_DELIMETER)) {
-            throw new IllegalArgumentException(ERROR_END_LOTTO_NUMBER_DELEMETER);
+            throw new IllegalArgumentException(ERROR_END_DELEMETER);
         }
     }
 }

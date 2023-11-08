d05f0fb981e92a8b5ea249d388c63bd0b27d9103
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:06:11 +0900

    [feat] Service - 구입 금액에 따른 횟수만큼 로또 번호 생성

--- src/main/java/lotto/Service/LottoService.java
+++ src/main/java/lotto/Service/LottoService.java
@@ -1,4 +1,25 @@
 package lotto.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Domain.Money;
+import lotto.Domain.LottoNumbers;
+
 public class LottoService {
+
+    private static final int STANDARD_UNIT = 1000;
+
+    public List<LottoNumbers> generateLottoNumbers(Money money) {
+        int purchaseAmount = money.getPurchaseAmount();
+        int numberOfTickets = purchaseAmount / STANDARD_UNIT;
+        List<LottoNumbers> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            LottoNumbers lottoNumbers = new LottoNumbers();
+            lottoTickets.add(lottoNumbers);
+        }
+
+        return lottoTickets;
+    }
+
 }

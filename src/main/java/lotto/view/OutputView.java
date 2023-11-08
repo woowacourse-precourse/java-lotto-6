e91223fadeaede9273d73d4b42dc2ce3fb0b2d51
haebyun <bhk4392@gmail.com>
2023-11-08 18:02:04 +0900

    feat(OutputView): 구매한 로또들의 정보를 출력한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,20 @@
 package lotto.view;
 
+import java.util.List;
+
 public class OutputView {
     private static final String BOUNDARY_LINE = "--------------------------------------------------------";
+    private static final String BUY_RESULT_MESSAGE = "%d개를 구매했습니다. \n";
+
     public static void printErrorMessage(String message) {
         System.out.println(message);
         System.out.println(BOUNDARY_LINE);
         System.out.println();
+    }
+
+    public static void printBuyResults(List<String> buyResults) {
+        System.out.println();
+        System.out.printf((BUY_RESULT_MESSAGE), buyResults.size());
+        buyResults.forEach(buyResult -> System.out.println("[" + buyResult + "]"));
     }
 }

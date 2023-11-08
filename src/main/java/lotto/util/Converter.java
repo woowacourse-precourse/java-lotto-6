d7497e2bb8bcde04defa6c8ff92c41bc1a90d33b
haebyun <bhk4392@gmail.com>
2023-11-08 15:46:15 +0900

    feat(Converter): 입력 문자열을 변환하는 과정들에서 발생할 수 있는 예외들을 처리하도록 코드 추가

--- src/main/java/lotto/util/Converter.java
+++ src/main/java/lotto/util/Converter.java
@@ -3,7 +3,10 @@ package lotto.util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.validation.BonusNumberValidator;
 import lotto.validation.LottoException;
+import lotto.validation.LottoNumbersValidator;
+import lotto.validation.PurchaseValidator;
 
 public class Converter {
     private static final String SPLIT_POINT = ",";
@@ -11,6 +14,7 @@ public class Converter {
     public static int convertPurchaseAmount(String input) {
         try {
             int amount = Integer.parseInt(input);
+            PurchaseValidator.validate(amount);
             return amount;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
@@ -22,15 +26,18 @@ public class Converter {
             List<Integer> inputNumbers = Arrays.stream(input.split(SPLIT_POINT))
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
+            LottoNumbersValidator.validate(inputNumbers);
             return inputNumbers;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
         }
     }
 
-    public static int convertBonusNumber(String input) {
+    public static int convertBonusNumber(List<Integer> winningNumbers, String input) {
         try {
-            return Integer.parseInt(input);
+            int bonusNumber = Integer.parseInt(input);
+            BonusNumberValidator.validate(winningNumbers, bonusNumber);
+            return bonusNumber;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(LottoException.LETTER_ERROR_MESSAGE.getMessage());
         }

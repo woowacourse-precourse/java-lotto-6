8a06e93c22982ca2e82bb456b7ea9e99be124035
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-08 15:28:28 +0900

    feat: String 검사 메서드 추가

--- src/main/java/lotto/CheckValid.java
+++ src/main/java/lotto/CheckValid.java
@@ -8,57 +8,59 @@ import java.util.HashSet;
 public class CheckValid {
 
 	public static void numbers(List<Integer> numbers) {
-
-		validateDigits(numbers);
-		validateEmpty(numbers);
-		validateLength(numbers);
-		validateRange(numbers);
+		
+	    for (Integer number : numbers) {
+	        validateRange(number);
+	    }
 		validateDuplicates(numbers);
-	}
-
-	public static void numbers(String inputNums, String bonusUserNum) {
-
-		String[] usernums = inputNums.split(",");
-		int bonus = Integer.parseInt(bonusUserNum);
-
-		for (String usernum : usernums) {
-			int num = Integer.parseInt(usernum);
-			validateDuplicate(num, bonus);
-		}
-
-		List<Integer> numbers = new ArrayList<>();
-
-		for (String usernum : usernums) {
-			numbers.add(Integer.parseInt(usernum));
-		}
-
 		validateEmpty(numbers);
-		validateLength(numbers);
-		validateRange(numbers);
-
+		validateSize(numbers);
 	}
+
 
 	public static void money(String strMoney) {
-		try {
+		
+			CheckValid.string(strMoney);
 			int money = Integer.parseInt(strMoney);
 
 			validateNatural(money);
 			validateDivisible(money);
 			validateNumber(money);
 
-		} catch (NumberFormatException e) {
-			String message = "[ERROR] 구입금액은 숫자여야 합니다";
-			throw new NumberFormatException(message);
-		}
 	}
 
-	private static void validateDuplicate(int usernums, int num) {
+	public static void bonus(List<Integer> numbers, int bonus) {
+		
+	    for (Integer number : numbers) {
+	    	validateNoOverLap(number,bonus);
+	    }
+        validateRange(bonus);
+
+	}
+	
+
+	public static void string(String input) {
+		
+		validateDigit(input);
+	}
+	
+	private static void validateDigit(String input) {
+	    String regex = "-?\\d+";
+	    String message = "[ERROR] 입력은 반드시 숫자여야 합니다.";
+	    if (!input.matches(regex)) {
+	        throw new NumberFormatException(message);
+	    }
+	}
+
+	
+	
+	private static void validateNoOverLap(int number, int bonus) {
 		String message = "[ERROR] 입력 번호와 보너스 입력 번호는 중복될 수 없어요";
-		if (usernums == num) {
+		if (number == bonus) {
 			throw new IllegalArgumentException(message);
 		}
-
 	}
+
 
 	private static void validateNatural(int money) {
 		String message = "[ERROR] 구입금액은 0보다 커야 합니다.";
@@ -71,7 +73,8 @@ public class CheckValid {
 		String message = "[ERROR] 구입금액은 1,000원 단위여야 합니다.";
 
 		if (money % 1000 != 0) {
-			throw new IllegalArgumentException(message);
+			System.out.println(message);
+			throw new IllegalArgumentException();
 		}
 	}
 
@@ -83,6 +86,16 @@ public class CheckValid {
 		}
 	}
 
+	
+	private static void validateRange(int number) {
+	    String message = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+	    if (number < 1 || number > 45) {
+	        throw new NumberFormatException(message);
+	    }
+	}
+
+
 	private static void validateEmpty(List<Integer> numbers) {
 		String message = "[ERROR] 로또 번호는 비어 있을 수 없습니다.";
 
@@ -92,32 +105,11 @@ public class CheckValid {
 
 	}
 
-	private static void validateLength(List<Integer> numbers) {
+	private static void validateSize(List<Integer> numbers) {
 		String message = "[ERROR] 로또 번호는 6개여야 합니다.";
 
 		if (numbers.size() != 6) {
 			throw new IllegalArgumentException(message);
-		}
-	}
-
-	private static void validateRange(List<Integer> numbers) {
-		String message = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
-
-		for (int number : numbers) {
-			if (number < 1 || number > 45) {
-				throw new IllegalArgumentException(message);
-			}
-		}
-
-	}
-
-	private static void validateDigits(List<Integer> numbers) {
-		String message = "[ERROR] 로또 번호는 반드시 숫자여야만 합니다.";
-
-		for (int number : numbers) {
-			if (number < 1 || number > 45) {
-				throw new NumberFormatException(message);
-			}
 		}
 	}
 
@@ -130,5 +122,6 @@ public class CheckValid {
 		}
 
 	}
+
 
 }

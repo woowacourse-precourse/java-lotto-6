4741c6485d8f84bd17fc172cb434aefdd5678719
potatomoong <star901210@hanmail.net>
2023-11-08 14:31:33 +0900

    Feat: 당첨내역 출력하는 기능 추가

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -14,6 +14,17 @@ public class Result {
         this.winningNumber = winningNumber;
     }
 
+    public void printResult() {
+        int[] winningCount = checkWinning();
+
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        Arrays.stream(ResultType.values())
+                .filter(resultType -> resultType != ResultType.NONE)
+                .forEach(resultType -> resultType.printWinningResult(winningCount[resultType.ordinal()]));
+    }
+
     public int[] checkWinning() {
         int[] result = new int[ResultType.values().length];
         for (Lotto lotto : purchasedLotto) {
@@ -33,21 +44,32 @@ public class Result {
 
         private final int matchCount;
         private final boolean hasBonusBall;
+        private final int prize;
 
         ResultType(int matchCount, boolean hasBonusBall) {
             this.matchCount = matchCount;
             this.hasBonusBall = hasBonusBall;
+            this.prize = calculatePrize();
+        }
+
+        private int calculatePrize() {
+            return 0;
         }
 
         public static ResultType getResultType(Lotto lotto, List<Integer> winningNumber, int bonusBall) {
 
-                long matchCount = lotto.getNumbers().stream().filter(winningNumber::contains).count();
-                boolean hasBounsBall = lotto.getNumbers().contains(bonusBall);
+            long matchCount = lotto.getNumbers().stream().filter(winningNumber::contains).count();
+            boolean hasBonusBall = lotto.getNumbers().contains(bonusBall);
 
             return Arrays.stream(values())
-                    .filter(type -> type.matchCount == matchCount && type.hasBonusBall == hasBounsBall)
+                    .filter(type -> type.matchCount == matchCount && type.hasBonusBall == hasBonusBall)
                     .findFirst()
                     .orElse(NONE);
+        }
+
+        public void printWinningResult(int count) {
+            System.out.printf("%d개 일치 (%,d원) - %d개%n", this.matchCount, this.prize, count);
+
         }
     }
 }

dd160e963eb9a4abcef29f8b521c1d8dec625818
Terry <t3rryahn.dev@gmail.com>
2023-11-08 15:45:19 +0900

    feat: 결과 계산 기능 총 당첨 금액을 저장하는 기능 당첨 등수의 갯수를 저장하는 기능
    LottoResult 클래스에 추가
    totalReward 변수 타입 int > long으로 변경
    
    WinningRank에서 prize 변수 타입 int > long으로 변경
    
    이유:상금의 int 범위 초과
    
    LottoResultTest 클래스에 테스트 추가
    테스트 내용
    총 결과 메서드 확인.
    > 테스트 통과

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -1,12 +1,32 @@
 package lotto.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoResult {
+    private Map<WinningRank, Integer> rankCounts;
+    private long totalReward;
 
-    int totalReward;
+    public LottoResult() {
+        rankCounts = new HashMap<>();
 
-    public WinningRank check(Lotto lotto, WinningNumbers winningNumbers) {
+        for (WinningRank rank : WinningRank.values()) {
+            rankCounts.put(rank, 0);
+        }
+    }
+
+    //총 결과 메서드
+    public void makeResult(List<Lotto> lottoTickets, WinningNumbers winningNumbers) {
+        for (Lotto lotto : lottoTickets) {
+            WinningRank rank = checkLotto(lotto, winningNumbers);
+            updateRankCount(rank);
+            updateTotalReward(rank);
+        }
+    }
+
+    //로또 한장 결과 확인
+    public WinningRank checkLotto(Lotto lotto, WinningNumbers winningNumbers) {
         List<Integer> numbers = lotto.getNumbers();
         List<Integer> comparingNumbers = winningNumbers.getWinningNumbers();
 
@@ -14,5 +34,22 @@ public class LottoResult {
         boolean bonusMatched = numbers.contains(winningNumbers.getBonusNumber());
 
         return WinningRank.valueOf(matchedCount, bonusMatched);
+    }
+
+    public void updateRankCount(WinningRank rank) {
+        int count = rankCounts.get(rank);
+        rankCounts.put(rank, count + 1);
+    }
+
+    public void updateTotalReward(WinningRank rank) {
+        totalReward += rank.getPrize();
+    }
+
+    public int getCountByRank(WinningRank rank) {
+        return rankCounts.get(rank);
+    }
+
+    public long getTotalReward() {
+        return totalReward;
     }
 }

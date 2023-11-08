469f595092252dd19a535d68f856c6f54f49c45c
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 10:04:00 +0900

    feat(run): LottoController 생성 및 컨트롤러 실행 기능

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        getPurchase();
+        createLottoPlayer();
+        createLottoList();
+        getPlayerNumbers();
+        displayResult();
+    }
+
+    private void getPurchase() {
+    }
+
+    private void createLottoPlayer() {
+    }
+
+    private void createLottoList() {
+    }
+
+    private void getPlayerNumbers() {
+    }
+
+    private void displayResult() {
+    }
+}

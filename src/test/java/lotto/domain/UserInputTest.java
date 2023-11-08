eb2fb2e4288fa2e64df925afbc353f9d8093abbc
JeonJW24 <tyui153@naver.com>
2023-11-08 19:51:36 +0900

    test: 구입 금액 글자 입력 테스트

---
+++ src/test/java/lotto/domain/UserInputTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserInputTest {
+    private UserInput userInput;
+
+    @BeforeEach
+    void setUp() {
+        userInput = new UserInput();
+    }
+
+    @Test
+    void 구입_금액_글자_입력() {
+        assertThatThrownBy(() -> userInput.validateMoney("글자"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

1ecc6601a70bc42b85e961485a2ef8f258b9dfde
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 20:51:05 +0900

    test: 수익률 계산 테스트

--- src/test/java/lotto/domain/ResultTest.java
+++ src/test/java/lotto/domain/ResultTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +15,17 @@ class ResultTest {
         Result result = new Result();
         result.put(Ranking.SECOND);
         assertAll(
-                () ->assertThat(result.getResult().get(Ranking.SECOND)).isEqualTo(1),
-                () ->assertThat(result.getResult().get(Ranking.FIRST)).isEqualTo(0)
+                () -> assertThat(result.getResult().get(Ranking.SECOND)).isEqualTo(1),
+                () -> assertThat(result.getResult().get(Ranking.FIRST)).isEqualTo(0)
         );
     }
 
+    @DisplayName("결과가 주어졌을 때 수익률을 계산할 수 있다.")
+    @Test
+    void calculateRateOfReturn() {
+        Result result = new Result();
+        result.put(Ranking.FIFTH);
+        result.put(Ranking.FAIL);
+        Assertions.assertThat(result.calculateRateOfReturn()).isEqualTo(2.5);
+    }
 }

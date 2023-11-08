ed92c9f7ffc35353e91a26921aeed009ed4b89fb
senzoo-oh <osj3418@gmail.com>
2023-11-08 20:47:16 +0900

    feat(lotto): implement main method in Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lotto.domain.*;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Generator generator = new Generator();
+        WinningNumber winningNumber = new WinningNumber();
+        BonusNumber bonusNumber = new BonusNumber();
+        Judge judge = new Judge();
+
+        List<Lotto> generatedLottery = generator.getAmountOfMoney();
+
+        winningNumber.createWinningNumber();
+        bonusNumber.createBonusNumber(winningNumber.getWinningNumbers());
+
+        judge.calculateResult(generatedLottery, winningNumber.getWinningNumbers(), bonusNumber.getBONUS_NUMBER());
+        judge.calculateProfit();
     }
 }

5f448d60ba8de8c909338474d05a53fc475c8f93
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 11:44:09 +0900

    feat(LottoUtil) : lottoNumbersParser 메서드 작성

---
+++ src/main/java/lotto/utils/LottoUtil.java
@@ -0,0 +1,25 @@
+package lotto.utils;
+
+import lotto.exception.LottoException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LottoUtil {
+    private static final LottoException lottoException = new LottoException();
+
+    public static List<Integer> lottoNumbersParser(String inputLottoNumbers) {
+        lottoException.validInputLottoNumbers(inputLottoNumbers);
+        List<Integer> lottoNumbers = new ArrayList<>();
+        Arrays.stream(inputLottoNumbers.split(","))
+                .map(number -> lottoNumbers.add(Integer.parseInt(number)))
+                .collect(Collectors.toList());
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
+
+
+}

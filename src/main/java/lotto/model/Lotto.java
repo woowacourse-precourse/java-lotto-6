a301485d5e080b76b9903cff0e1864ff0834ed46
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 11:19:35 +0900

    :sparkles: Feat: 로또에 필요한 로직 추가
    - 로또에 사용되는 번호들을 반환하는 로직 추가
    - 로또끼리 번호들 비교를 하기 위한 로직 추가
    - 로또 사이즈 만큼 랜덤으로 번호들을 생성해주는 로직 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,34 +1,61 @@
 package lotto.model;
 
+import static lotto.common.exception.LottoErrorMessage.DUPLICATE_LOTTO_NUMBERS;
+import static lotto.common.exception.LottoErrorMessage.INVALID_LOTTO_SIZE;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
+import lotto.common.exception.LottoException;
 
 public class Lotto {
     private static final int LOTTO_SIZE = 6;
     private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validateNumbers(numbers);
-        this.numbers = numbers.stream()
-                .map(LottoNumber::generateLottoNumber)
-                .toList();
+    private Lotto(final List<LottoNumber> lottoNumbers) {
+        validateNumbers(lottoNumbers);
+        this.numbers = lottoNumbers;
     }
 
-    private void validateNumbers(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplication(numbers);
+    public static Lotto generateGandomLotto() {
+        List<LottoNumber> lottoNumbers = LottoNumber.generateLottoNumbers(LOTTO_SIZE);
+        return new Lotto(lottoNumbers);
     }
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+
+    public static Lotto generateLotto(List<Integer> numbers) {
+        List<LottoNumber> lottoNumbers = numbers.stream()
+                .map(LottoNumber::generateLottoNumber)
+                .toList();
+        return new Lotto(lottoNumbers);
+    }
+
+    public void compare(Consumer<LottoNumber> biConsumer){
+        for (LottoNumber number : numbers) {
+            biConsumer.accept(number);
         }
     }
 
-    private void validateDuplication(List<Integer> numbers) {
-        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+    public List<Integer> getNumbers() {
+        return numbers.stream()
+                .map(LottoNumber::getNumber)
+                .toList();
+    }
+
+    private void validateNumbers(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateDuplication(numbers);
+    }
+    private void validateSize(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new LottoException(INVALID_LOTTO_SIZE);
+        }
+    }
+
+    private void validateDuplication(List<LottoNumber> numbers) {
+        Set<LottoNumber> uniqueNumbers = new HashSet<>(numbers);
         if(numbers.size() != uniqueNumbers.size()) {
-            throw new IllegalArgumentException();
+            throw new LottoException(DUPLICATE_LOTTO_NUMBERS);
         }
     }
 }

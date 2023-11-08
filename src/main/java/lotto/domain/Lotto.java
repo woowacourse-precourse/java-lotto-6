be60842f0906d74cd787062df2ec81f60eada7ed
cmsxi <cms0755@naver.com>
2023-11-08 09:17:18 +0900

    feat: 로또 검증 규칙 추가 및 활용  변수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,10 +1,16 @@
-package lotto;
+package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static lotto.controller.LottoGameController.bonusNumber;
+import static lotto.controller.LottoGameController.winningLotto;
 
 public class Lotto {
     private final List<Integer> numbers;
-    public static final int LOTTO_NUMBER_SIZE = 6;
+    public final int LOTTO_NUMBER_SIZE = 6;
+    public int countWinningNum;
+    public boolean hasBonus;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -15,7 +21,26 @@ public class Lotto {
         if (numbers.size() != LOTTO_NUMBER_SIZE) {
             throw new IllegalArgumentException();
         }
+        if (numbers.size() != numbers.stream().distinct().count()){
+            throw new IllegalArgumentException();
+        }
     }
 
     // TODO: 추가 기능 구현
+    public int countSameNum() {
+        int sameNum =  (int) this.numbers.stream().filter(winningLotto.numbers::contains).count();
+        this.countWinningNum = sameNum;
+        return sameNum;
+    }
+
+
+
+    public List<Integer> getNumbers(){
+        return this.numbers;
+    }
+
+    public void checkHasBonus(){
+        if(this.numbers.contains(bonusNumber)) this.hasBonus = true;
+    }
 }
+

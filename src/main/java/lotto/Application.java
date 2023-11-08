22e282633164b069198bec3e88be69b3295f4d3a
ysy56 <ysy561356@naver.com>
2023-11-08 21:59:26 +0900

    feat : 구입 금액 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,36 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static String inputPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String purchaseAmount = "";
+
+        while (purchaseAmount.isEmpty()) {
+            purchaseAmount = Console.readLine();
+            if (!isValidPurchaseAmount(purchaseAmount)) {
+                purchaseAmount = "";
+                IllegalArgumentException e = new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위입니다.");
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return purchaseAmount;
+    }
+
+    public static boolean isValidPurchaseAmount(String amount) {
+        try {
+            double value = Double.parseDouble(amount);
+            return value % 1000 == 0;
+        } catch (NumberFormatException e) {
+            return false; // 숫자로 변환할 수 없는 경우 유효하지 않음
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String purchaseAmount = inputPurchaseAmount();
+
     }
 }

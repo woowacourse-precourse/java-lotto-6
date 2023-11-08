a456933be7293a0ae858c054ad0ed15b5ee994e6
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-08 20:18:53 +0900

    feat: 도메인 추가
    Game에서 필요한 클래스들 추가
    - PurchaseAmount 추가
    - WinningNumbers 추가
    - BonusNumber 추가
    - 값에 의미를 부여하고 값 생성 시 검증하기 위함

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,52 @@
+package lotto.domain;
+
+import static lotto.domain.Lotto.MAX_NUMBER;
+import static lotto.domain.Lotto.MIN_NUMBER;
+import static lotto.error.ErrorMessage.BONUS_NUMBER_OUT_OF_RANGE;
+import static lotto.error.ErrorMessage.DUPLICATE_BONUS_NUMBER;
+import static lotto.error.ErrorMessage.INVALID_BONUS_NUMBER_FORMAT;
+
+import java.util.List;
+
+public class BonusNumber {
+    private final Integer bonusNumber;
+
+    private BonusNumber(Integer bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public static BonusNumber from(String input,WinningNumbers winningNumbers) {
+        return new BonusNumber(validateBonusNumber(winningNumbers,input));
+    }
+
+    private static Integer validateBonusNumber(WinningNumbers winningNumbers, String input) {
+        Integer bonusNumber = validateFormatAndConvert(input, INVALID_BONUS_NUMBER_FORMAT);
+        validateRange(bonusNumber, BONUS_NUMBER_OUT_OF_RANGE);
+        validateDuplicate(winningNumbers, bonusNumber, DUPLICATE_BONUS_NUMBER);
+        return bonusNumber;
+    }
+
+    private static Integer validateFormatAndConvert(String input, String error) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private static void validateRange(Integer number, String error) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private static void validateDuplicate(WinningNumbers winningNumbers, Integer number, String error) {
+        if (winningNumbers.contains(number)) {
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    public boolean equals(Integer number){
+        return bonusNumber.equals(number);
+    }
+}
---
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -0,0 +1,76 @@
+package lotto.domain;
+
+import static lotto.domain.Lotto.MAX_NUMBER;
+import static lotto.domain.Lotto.MIN_NUMBER;
+import static lotto.domain.Lotto.NUMBER_LENGTH;
+import static lotto.error.ErrorMessage.DUPLICATE_WINNING_NUMBER;
+import static lotto.error.ErrorMessage.INVALID_WINNING_NUMBER_FORMAT;
+import static lotto.error.ErrorMessage.INVALID_WINNING_NUMBER_LENGTH;
+import static lotto.error.ErrorMessage.WINNING_NUMBER_OUT_OF_RANGE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningNumbers {
+
+    private static final String COMMA = ",";
+
+    private List<Integer> winningNumbers;
+
+    private WinningNumbers(List<Integer> winningNumbers) {
+        this.winningNumbers = winningNumbers;
+    }
+
+    public static WinningNumbers of(String input) {
+        return new WinningNumbers(validateWinningNumbers(input));
+    }
+
+    private static List<Integer> validateWinningNumbers(String input) {
+        List<Integer> winningNumbers = validateFormatAndConvert(input.split(COMMA), INVALID_WINNING_NUMBER_FORMAT);
+        validateLength(winningNumbers, INVALID_WINNING_NUMBER_LENGTH);
+        for (Integer number : winningNumbers) {
+            validateRange(number, WINNING_NUMBER_OUT_OF_RANGE);
+        }
+        validateDuplicate(winningNumbers, DUPLICATE_WINNING_NUMBER);
+        return winningNumbers;
+    }
+
+    private static List<Integer> validateFormatAndConvert(String[] input, String error) {
+        List<Integer> result = new ArrayList<>();
+        try {
+            for (int index = 0; index < input.length; index++) {
+                int converted = Integer.parseInt(input[index]);
+                result.add(converted);
+            }
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(error);
+        }
+        return result;
+    }
+
+    private static void validateLength(List<Integer> winningNumbers, String error) {
+        if (winningNumbers.size() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private static void validateRange(Integer number, String error) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(error);
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers, String error) {
+        List<Integer> checkDuplicate = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (checkDuplicate.contains(number)) {
+                throw new IllegalArgumentException(error);
+            }
+            checkDuplicate.add(number);
+        }
+    }
+
+    public boolean contains(Integer number){
+        return winningNumbers.contains(number);
+    }
+}

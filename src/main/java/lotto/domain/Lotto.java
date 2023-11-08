b3381fd9de2a061411099ea0abf781bcf421efb4
gumgu <bienlee@naver.com>
2023-11-08 14:42:48 +0900

    [Refactor] 가독성 증진되도록 메서드 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.sql.Array;
 import java.util.*;
 
 public class Lotto {
@@ -18,9 +19,32 @@ public class Lotto {
         validateHasDuplicate(numbers);
         validateNumberRange(numbers);
 
-        sortAscending(numbers);
-
         this.numbers = numbers;
+    }
+
+    // TODO: 접근제한자 private 변경 불가 X.
+    private void validateCount(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBERS_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        boolean isInNumberRange = numbers.stream()
+                .allMatch(number ->
+                        (number >= MINIMUM_LOTTO_RANGE && number <= MAXIMUM_LOTTO_RANGE));
+
+        if (!isInNumberRange) {
+            throw new IllegalArgumentException("[ERROR] 각 로또 번호는 1 에서 45 까지 입력 가능합니다.");
+        }
+    }
+
+    private void validateHasDuplicate(List<Integer> numbers) {
+        boolean hasDuplicate = numbers.size() != Set.copyOf(numbers).size();
+
+        if (hasDuplicate) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 
     public Ranking matchWithWinLotto(WinLotto winLotto) {
@@ -33,22 +57,13 @@ public class Lotto {
         if (numbers.contains(bonus)) {
             return 1;
         }
-
         return 0;
     }
 
     private Integer matchWithNumbers(Lotto winLotto) {
-        Integer matchNumber = 0;
-
-        List<Integer> winNumbers = winLotto.numbers;
-
-        for (Integer winNumber : winNumbers) {
-            if(numbers.contains(winNumber)) {
-                matchNumber ++;
-            }
-        }
-
-        return matchNumber;
+        return (int) numbers.stream()
+                .filter(winLotto.numbers::contains)
+                .count();
     }
 
     // TODO: 접근제한자 private 변경 불가 X.

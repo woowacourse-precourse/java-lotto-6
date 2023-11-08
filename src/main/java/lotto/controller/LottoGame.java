67e1b7adb076fb77d9aa38314e4d88b4e6993ab8
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-08 22:15:54 +0900

    feat(LottoGame) : 로또 게임 컨트롤 기능 구현

---
+++ src/main/java/lotto/controller/LottoGame.java
@@ -0,0 +1,111 @@
+package lotto.controller;
+
+import java.util.List;
+import lotto.model.Lotto;
+import lotto.service.LottoGenerator;
+import lotto.service.LottoProfitCalculator;
+import lotto.service.LottoResultCalculator;
+import lotto.util.InputParser;
+import lotto.util.InputValidator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGame {
+
+    private static final int LOTTO_PRICE = 1000;
+
+    private int purchaseAmount;
+    private List<Lotto> lottoTickets;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public void startLottoGame() {
+        purchaseLotto();
+        inputWinningAndBonusNumbers();
+        finalizeLottoGame();
+    }
+
+    private void purchaseLotto() {
+        purchaseAmount = getPurchaseAmount();
+        int lottoTicketCount = purchaseAmount / LOTTO_PRICE;
+
+        OutputView.printLottoBuy(lottoTicketCount);
+        lottoTickets = LottoGenerator.generateLottoTickets(lottoTicketCount);
+        OutputView.printAllLottoNumber(lottoTickets);
+    }
+
+    private void inputWinningAndBonusNumbers() {
+        winningNumbers = getWinningNumbers();
+        bonusNumber = getBonusNumber();
+    }
+
+    private void finalizeLottoGame() {
+        LottoResultCalculator lottoResultCalculator = new LottoResultCalculator(winningNumbers, bonusNumber);
+        List<Integer> lottoResult = lottoResultCalculator.getLottoResult(lottoTickets);
+        OutputView.printLottoResult(lottoResult);
+
+        double profit = LottoProfitCalculator.calculateLottoProfit(purchaseAmount, lottoResult);
+        OutputView.printProfitRate(profit);
+    }
+
+    private int getPurchaseAmount() {
+        Integer purchaseAmount = getPurchaseAmountAttempt();
+
+        while (purchaseAmount == null) {
+            purchaseAmount = getPurchaseAmountAttempt();
+        }
+        return purchaseAmount;
+    }
+
+    private List<Integer> getWinningNumbers() {
+        List<Integer> undividedWinningNumbers = getWinningNumbersAttempt();
+
+        while (undividedWinningNumbers == null) {
+            undividedWinningNumbers = getWinningNumbersAttempt();
+        }
+        return undividedWinningNumbers;
+    }
+
+    private int getBonusNumber() {
+        Integer bonusNumber = getBonusNumberAttempt();
+
+        while (bonusNumber == null) {
+            bonusNumber = getBonusNumberAttempt();
+        }
+        return bonusNumber;
+    }
+
+    private static Integer getPurchaseAmountAttempt() {
+        try {
+            String purchaseAmount = InputView.inputPurchaseAmount();
+            InputValidator.validatePurchaseAmount(purchaseAmount);
+            return InputParser.parsePurchaseAmount(purchaseAmount);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printErrorMessage(illegalArgumentException.getMessage());
+        }
+        return null;
+    }
+
+    private static List<Integer> getWinningNumbersAttempt() {
+        try {
+            String undividedWinningNumbers = InputView.inputWinningNumbers();
+            InputValidator.validateWinningNumbers(undividedWinningNumbers);
+            return InputParser.parseWinningNumbers(undividedWinningNumbers);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printErrorMessage(illegalArgumentException.getMessage());
+        }
+        return null;
+    }
+
+    private Integer getBonusNumberAttempt() {
+        try {
+            String bonusNumber = InputView.inputBonusNumber();
+            InputValidator.validateBonusNumber(winningNumbers, bonusNumber);
+            return InputParser.parseBonusNumber(bonusNumber);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            OutputView.printErrorMessage(illegalArgumentException.getMessage());
+        }
+        return null;
+    }
+
+}

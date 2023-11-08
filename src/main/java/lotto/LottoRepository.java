17e4d61f9e1b5108251d020c7edc0c28770eaa89
gusgh075 <gusgh075@naver.com>
2023-11-08 13:36:17 +0900

    FIX : 우승당첨번호 및 보너스번호 확인하는 기능 수정

--- src/main/java/lotto/LottoRepository.java
+++ src/main/java/lotto/LottoRepository.java
@@ -3,21 +3,46 @@ package lotto;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class LottoRepository {
     private List<Lotto> lottos=new ArrayList<>();
     private int[] ranks = new int[6];
     private Lotto winningLotto;
-    private int bonusNum;
+    private int bonusNum=-1;
     private final int[] PRIZE = {20000000, 3000000, 1500000, 50000, 5000};
     private static final String RANK_ERROR_MESSAGE = "[ERROR] 로또의 번호 개수는 6개여야 합니다.";
 
-    public void setWinningLotto(Lotto lotto) {
-        this.winningLotto = lotto;
+    public boolean setWinningLotto(Lotto lotto) {
+        if(bonusNum==-1){
+            this.winningLotto=lotto;
+            return true;
+        }
+        else {
+            if (winningLotto.getNumbers().stream().anyMatch(number -> number == bonusNum)) {
+                System.out.println("[ERROR] 당첨 로또 번호와 보너스 번호 중 중복되는 것이 있습니다.");
+                return false;
+            } else {
+                this.winningLotto = lotto;
+                return true;
+            }
+        }
     }
 
-    public void setBonusNum(int bonusNum) {
-        this.bonusNum = bonusNum;
+    public boolean setBonusNum(int bonusNum) {
+        if(winningLotto == null) {
+            this.bonusNum = bonusNum;
+            return true;
+        }
+        else {
+            if (winningLotto.getNumbers().stream().anyMatch(number -> number == bonusNum)) {
+                System.out.println("[ERROR] 보너스 번호가 당첨 로또 번호와 중복되는 것이 있습니다.");
+                return false;
+            } else {
+                this.bonusNum = bonusNum;
+                return true;
+            }
+        }
     }
 
     public int[] getRanks() {

fd4737136556c2341dda382a9e01e55e9f93c277
seohyun kwon <seohyunkwon98@gmail.com>
2023-11-08 21:55:44 +0900

    refact(lottorank): 메서드 및 변수명 변경

--- src/main/java/lotto/utils/LottoRank.java
+++ src/main/java/lotto/utils/LottoRank.java
@@ -7,25 +7,25 @@ public enum LottoRank {
     SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
     FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - ");
 
-    private final int matchedNumber;
+    private final int count;
     private final long prize;
-    private final String resultMessage;
+    private final String message;
 
-    LottoRank(int matchedNumbers, long prize, String resultMessage) {
-        this.matchedNumber = matchedNumbers;
+    LottoRank(int count, long prize, String message) {
+        this.count = count;
         this.prize = prize;
-        this.resultMessage = resultMessage;
+        this.message = message;
     }
 
-    public String getResult() {
-        return resultMessage;
+    public int count(){
+        return count;
     }
 
-    public long getPrize() {
+    public long prize(){
         return prize;
     }
 
-    public int getMatchedNumber() {
-        return matchedNumber;
+    public String message(){
+        return message;
     }
 }

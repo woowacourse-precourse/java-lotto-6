6e62595eea03fa54898e0369e0ecf58b81edb1c7
jjw0071 <jjw0071@naver.com>
2023-11-08 12:35:48 +0900

    feat(Validator) : 입력 받은 당첨 번호 유효성 검사 기능 구현

--- src/main/java/lotto/domain/Validator.java
+++ src/main/java/lotto/domain/Validator.java
@@ -1,5 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
 
     public Validator() {
@@ -34,6 +39,65 @@ public class Validator {
     }
 
 
+    public List<Integer> validateLottoNumbersInput(String input) throws IllegalArgumentException {
+        String[] inputInArray = validateSixNumbers(input);
+        String[] trimmedElements = trimElements(inputInArray);
+        validateNoDuplicates(trimmedElements);
+        validateElemnetsAreIntegers(trimmedElements);
+        List<Integer> lottoNumbers = putElementsIntoList(trimmedElements);
+        validateNumbersRange(lottoNumbers);
+        return lottoNumbers;
+    }
+
+    private void validateNumbersRange(List<Integer> lottoNumbers) throws IllegalArgumentException {
+        for(int lottoNumber:lottoNumbers){
+            validateLottoNumberRange(lottoNumber);
+        }
+    }
+
+    private void validateLottoNumberRange(int lottoNumber) {
+        if(lottoNumber<1 || lottoNumber>45){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+    private List<Integer> putElementsIntoList(String[] trimmedElements) {
+        List<Integer> lottoNumbers = new ArrayList<>();
+        for(String element:trimmedElements){
+            lottoNumbers.add(Integer.parseInt(element));
+        }
+        return lottoNumbers;
+    }
+
+    private void validateElemnetsAreIntegers(String[] trimmedElements) {
+        for(String element:trimmedElements){
+            validateInteger(element);
+        }
+    }
+
+    private String[] trimElements(String[] inputInArray) {
+        for (int i = 0; i < inputInArray.length; i++) {
+            inputInArray[i] = inputInArray[i].trim();
+        }
+        return inputInArray;
+    }
+
+    private void validateNoDuplicates(String[] trimmedElements) {
+        Set<String> uniqueInputs = new HashSet<>();
+        for(String element:trimmedElements){
+            if(!uniqueInputs.add(element)){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+            }
+        }
+    }
+
+    private String[] validateSixNumbers(String input) {
+        String[] inputInArray = input.split(",");
+        if(inputInArray.length != 6){
+            throw new IllegalArgumentException("[ERROR] 6개의 값을 입력해야 합니다.");
+        }
+        return inputInArray;
+    }
 
 
 }

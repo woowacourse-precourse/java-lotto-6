abf8bd20c2e2b34ebdfd6b358a9fcb88ad49362d
mingtorr <mingtorr@nave.com>
2023-11-08 19:22:08 +0900

    refactor: 오류 메시지는 각 모듈마다 다르게 출력할 수 있도록 반환값을 boolean으로 변경

--- src/main/java/lotto/utils/ListUtil.java
+++ src/main/java/lotto/utils/ListUtil.java
@@ -1,28 +1,17 @@
 package lotto.utils;
 
-import lotto.constants.ErrorConstants;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Set;
 
 public class ListUtil {
-    public static void checkDuplicateValue(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>(numbers);
-        if(numbers.size() != set.size()) {
-            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_DUPLICATED_VALUE.getData());
-        }
+    public static boolean checkDuplicateValue(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == numbers.size();
     }
-    public static void checkLength(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_LENGTH.getData());
-        }
+
+    public static boolean checkLength(List<Integer> numbers) {
+        return numbers.size() == 6;
     }
-    public static void checkRange(List<Integer> numbers) {
-        for(int num : numbers) {
-            if( num <0 || num >46)
-                throw new IllegalArgumentException(ErrorConstants.ERROR_LOTTO_INVALID_RANGE.getData());
-        }
+
+    public static boolean checkRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(num -> num >= 0 && num <= 46);
     }
 }

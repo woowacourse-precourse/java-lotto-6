7f9407cb541e3b26bb9c16f633afdc84d9f2b06a
황준혁 <hyug0819@gmail.com>
2023-11-08 21:01:34 +0900

    test : 보너스 번호 중복 확인

--- src/test/java/lotto/util/validate/UserValidateTest.java
+++ src/test/java/lotto/util/validate/UserValidateTest.java
@@ -39,4 +39,11 @@ class UserValidateTest {
         assertThatThrownBy(() -> UserValidate.checkDuplicate(List.of(1, 2, 1, 2, 1, 2)))
             .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("입력된 6자리 로또 번호에 보너스 번호가 포함되어 있으면 예외 발생")
+    @Test
+    void 로또_보너스_번호_중복(){
+        assertThatThrownBy(() -> UserValidate.checkDuplicate(List.of(1, 2, 3, 4, 5, 6), 6))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 }

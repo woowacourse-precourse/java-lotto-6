b235ea9cf986f3c768415f69647e39e196d95db9
HongYeseul <ghd8119@naver.com>
2023-11-08 21:09:20 +0900

    :white_check_mark: test(OrderAmount): 빠진 테스트 케이스 추가 - 문자, 숫자 같이 입력 - 공백 입력

--- src/test/java/lotto/model/user/OrderAmountTest.java
+++ src/test/java/lotto/model/user/OrderAmountTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class OrderAmountTest {
     @Test
-    @DisplayName("로또 금액을 정상적인 가격으로 입력")
+    @DisplayName("로또 금액을 정상적인 가격으로 입력하면 예외가 발생하지 않는다.")
     void orderNormalPrice(){
         OrderAmount order = new OrderAmount("8000");
         int orderAmount = order.amountOfLotto();
@@ -18,7 +18,7 @@ class OrderAmountTest {
     }
 
     @Test
-    @DisplayName("로또 수익률 계산")
+    @DisplayName("로또 수익률 계산시 정확한 값인지 확인해 본다.")
     void calculateProfit(){
         OrderAmount order = new OrderAmount("8000");
         LottoResults results = new LottoResults( 0, 0, 0, 0, 1);
@@ -29,6 +29,20 @@ class OrderAmountTest {
     @DisplayName("구입 금액 입력시 문자를 입력하면 예외가 발생한다.")
     void createOrderAmountByLetter(){
         assertThatThrownBy(() -> new OrderAmount("letter"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 금액 입력시 문자와 숫자를 같이 입력하면 예외가 발생한다.")
+    void createOrderAmountByLetterAndNumber(){
+        assertThatThrownBy(() -> new OrderAmount("letter1000"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 금액 입력시 공백을 입력하면 예외가 발생한다.")
+    void createOrderAmountByBlank(){
+        assertThatThrownBy(() -> new OrderAmount(""))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 

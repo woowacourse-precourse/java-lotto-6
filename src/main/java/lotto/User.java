dabb5244a628750ceae88337821fae69f03db32f
soyoungxxx <otherplet@gmail.com>
2023-11-08 15:04:36 +0900

    feat(User): 구입금액 입력 기능 구현

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -1,13 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
     private int numberOfLotto;
     private int money;
     private Lotto winningNumber;
     private int bonusNumber;
 
-    public void enterAmount() {
+    private final static String START = "구입금액을 입력해 주세요.";
+    private final static String DIVIDE_ERROR = "[ERROR] 구입 금액을 1,000원 단위로 입력해주세요.";
+    private final static String NOT_NUMBER = "[ERROR] 숫자를 입력해주세요.";
 
+    public void enterAmount() {
+        System.out.println(START);
+        while(true) {
+            try {
+                money = Integer.parseInt(Console.readLine());
+                validateNumber();
+                break;
+            } catch (NumberFormatException e){
+                System.out.println(NOT_NUMBER);
+            } catch (IllegalArgumentException e) {
+                System.out.println(DIVIDE_ERROR);
+            }
+        }
     }
 
     public void enterWinningNumber() {
@@ -19,7 +36,9 @@ public class User {
     }
 
     private void validateNumber() {
-
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void validateLotto() {

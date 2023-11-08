37e1f51bb2a57fd45a1b65bc8cbe7eb578d842bd
lkdcode <dlrlejr1@naver.com>
2023-11-08 16:46:59 +0900

    Add [LottoResultManager] : 결과 계산과 수익률 계산 메서드 구현

---
+++ src/main/java/lotto/module/result/LottoResultManager.java
@@ -0,0 +1,34 @@
+package lotto.module.result;
+
+import lotto.module.domain.LottoProfit;
+import lotto.module.domain.PurchaseAmount;
+import lotto.module.domain.TotalPrize;
+import lotto.module.lotto.Lotto;
+import lotto.module.lotto.WinningLotto;
+
+import java.util.List;
+
+public class LottoResultManager {
+    private final LottoProfitCalculator lottoProfitCalculator;
+    private final LottoResultChecker lottoResultChecker;
+
+    private LottoResultManager(LottoProfitCalculator lottoProfitCalculator, LottoResultChecker lottoResultChecker) {
+        this.lottoProfitCalculator = lottoProfitCalculator;
+        this.lottoResultChecker = lottoResultChecker;
+    }
+
+    public static LottoResultManager of(LottoProfitCalculator lottoProfitCalculator, LottoResultChecker lottoResultChecker) {
+        return new LottoResultManager(lottoProfitCalculator, lottoResultChecker);
+    }
+
+    public LottoResult calculateResult(List<Lotto> userLottoTicket, WinningLotto winningLotto) {
+        return lottoResultChecker.getResult(userLottoTicket, winningLotto);
+    }
+
+    public LottoProfit calculateProfit(PurchaseAmount purchaseAmount) {
+        TotalPrize totalPrize = lottoResultChecker.getTotalPrize();
+        System.out.println("totalPrize = " + totalPrize);
+        return lottoProfitCalculator.getProfitRate(purchaseAmount, totalPrize);
+    }
+
+}

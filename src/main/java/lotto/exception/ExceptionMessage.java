7a684b9fc60e0041c14d49213b4ebb0719ed14e9
Jeongmin39 <wjdals20309@naver.com>
2023-11-08 22:18:57 +0900

    feat : 에러메시지 클래스 생성

---
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package lotto.exception;
+
+public class ExceptionMessage {
+
+    public static final String SizeError = "[ERROR] 6개의 숫자를 입력해야 합니다.";
+    public static final String RangeError = "[ERROR] 1 ~ 45 사이의 숫자를 입력해야 합니다.";
+    public static final String DuplicateError = "[ERROR] 중복인 값이 존재합니다.";
+    public static final String ZeroMoneyError = "[ERROR] 로또 구입 금액은 0원일 수 없습니다.";
+    public static final String DivideMoneyError = "[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.";
+}

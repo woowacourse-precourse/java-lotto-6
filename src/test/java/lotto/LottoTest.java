d693072d51f858c961cf7e3dd945c5eb6716e5bd
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 17:59:30 +0900

    test(LottoTest): Lotto List 예외 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.util.exception.ErrorCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,5 +25,28 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호 리스트의 길이가 6이 아니면 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1 미만 또는 45 초과인 경우, 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidNumberRange() {
+        List<Integer> invalidRangeNumbers = List.of(0, 46, 20, 30, 40, 50);
+        assertThatThrownBy(() -> new Lotto(invalidRangeNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+    }
+
+    @DisplayName("로또 번호가 6개가 아닌 경우, 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidNumberCount() {
+        List<Integer> invalidNumbers = List.of(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> new Lotto(invalidNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorCode.INVALID_LOTTO_NUMBER_COUNT.getMessage());
+    }
 }

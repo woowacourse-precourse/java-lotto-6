c178e44128967ef7802355b7c10ca67136fc24b1
Concepty <khs1903b@gmail.com>
2023-11-08 18:04:30 +0900

    test(LottoWinning): 당첨별 횟수를 세는 로직의 테스트 추가

--- src/test/java/lotto/LottoWinningTest.java
+++ src/test/java/lotto/LottoWinningTest.java
@@ -4,7 +4,9 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,9 +14,41 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoWinningTest {
-    @DisplayName("로또가 당첨 기준에 따라 올바른 등수에 당첨되는지 확인한다.")
+    @DisplayName("당첨 순위 별 숫자가 잘 세어지는지 확인한다.")
     @ParameterizedTest
     @MethodSource("provideLottoTickets")
+    void testAssignedRanksCount(List<Lotto> tickets, List<Integer> expected) {
+        LottoWinning testLottoWinning = new LottoWinning(List.of(5, 10, 15, 20, 25, 30));
+        testLottoWinning.setBonusNumber(35);
+        Map<LottoRank, Integer> winningCounts= testLottoWinning.assignRanks(tickets);
+        assertEquals(expected.get(0), winningCounts.get(LottoRank.FIRST_PLACE));
+        assertEquals(expected.get(1), winningCounts.get(LottoRank.SECOND_PLACE));
+        assertEquals(expected.get(2), winningCounts.get(LottoRank.THIRD_PLACE));
+        assertEquals(expected.get(3), winningCounts.get(LottoRank.FOURTH_PLACE));
+        assertEquals(expected.get(4), winningCounts.get(LottoRank.FIFTH_PLACE));
+    }
+
+    static Stream<Arguments> provideLottoTickets() {
+        List<Lotto> tickets = List.of(
+                new Lotto(List.of(5,10,15,42,44,45)),
+                new Lotto(List.of(5,10,15,43,44,45)),
+                new Lotto(List.of(5,10,15,20,44,45)),
+                new Lotto(List.of(5,10,15,20,43,45)),
+                new Lotto(List.of(5,10,15,20,42,45)),
+                new Lotto(List.of(5,10,15,20,25,40)),
+                new Lotto(List.of(5,10,15,20,25,41)),
+                new Lotto(List.of(5,10,15,20,25,35)),
+                new Lotto(List.of(5,10,15,20,25,30))
+        );
+        // firstCount = 1; secondCount = 1; thirdCount = 2; fourthCount = 3; fifthCount = 2;
+        return Stream.of(
+                Arguments.of(tickets, List.of(1,1,2,3,2))
+        );
+    }
+
+    @DisplayName("로또가 당첨 기준에 따라 올바른 등수에 당첨되는지 확인한다.")
+    @ParameterizedTest
+    @MethodSource("provideLottoTicket")
     void testAssignedRank(Lotto ticket, LottoRank expected) {
         LottoWinning testLottoWinning = new LottoWinning(List.of(5, 10, 15, 20, 25, 30));
         testLottoWinning.setBonusNumber(35);
@@ -22,10 +56,10 @@ class LottoWinningTest {
         assertEquals(expected, actual);
     }
 
-    static Stream<Arguments> provideLottoTickets() {
+    static Stream<Arguments> provideLottoTicket() {
         return Stream.of(
                 Arguments.of(new Lotto(List.of(5,10,15,43,44,45)), LottoRank.FIFTH_PLACE),
-                Arguments.of(new Lotto(List.of(5,10,15,20,44,45)), LottoRank.FORTH_PLACE),
+                Arguments.of(new Lotto(List.of(5,10,15,20,44,45)), LottoRank.FOURTH_PLACE),
                 Arguments.of(new Lotto(List.of(5,10,15,20,25,40)), LottoRank.THIRD_PLACE),
                 Arguments.of(new Lotto(List.of(5,10,15,20,25,35)), LottoRank.SECOND_PLACE),
                 Arguments.of(new Lotto(List.of(5,10,15,20,25,30)), LottoRank.FIRST_PLACE)

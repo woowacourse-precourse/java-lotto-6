092ef85de7ceb1474b694549b2bc8af727af38fb
wooyongjeong <wooyong.dev@gmail.com>
2023-11-08 20:55:07 +0900

    feat(Money): Money 기능 구현

--- src/main/java/lotto/domain/money/Money.java
+++ src/main/java/lotto/domain/money/Money.java
@@ -1,9 +1,21 @@
 package lotto.domain.money;
 
 public class Money {
+    private final int LOTTO_PRICE = 1_000;
     private final int amount;
 
     public Money(int amount) {
+        validate(amount);
         this.amount = amount;
+    }
+
+    private void validate(int amount) {
+        if (amount < LOTTO_PRICE) {
+            throw new IllegalArgumentException();
+        }
+    }
+    
+    public int ticketCount() {
+        return amount / LOTTO_PRICE;
     }
 }

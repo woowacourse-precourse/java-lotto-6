dbcc259b7a885c683a4d2eb6bf7e8d4a0400c011
SangCheol <scpark0698@gmail.com>
2023-11-08 21:35:36 +0900

    refactor: Lotto 검증 로직을 LottoGameValidator 클래스로

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,33 +1,13 @@
 package lotto;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
-    public static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoGameValidator.validateLotteNumber(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        validateLottoSize(numbers);
-        validateLottoDuplicate(numbers);
-    }
-
-    private void validateLottoDuplicate(List<Integer> numbers) {
-        Set<Integer> nonDuplicateNumbers = new HashSet<>();
-        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호들은 중복될 수 없습니다.");
-        }
-    }
-
-    private static void validateLottoSize(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
-        }
-    }
 }
--- src/main/java/lotto/LottoGameValidator.java
+++ src/main/java/lotto/LottoGameValidator.java
@@ -1,6 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static lotto.Application.LOTTO_SIZE;
+
 public class LottoGameValidator {
+
     public static void validateInputAmount(String inputBuyAmount) {
         validateNumericInput(inputBuyAmount);
         validateBuyAmountFormat(inputBuyAmount);
@@ -20,6 +27,25 @@ public class LottoGameValidator {
 
         if (buyAmount <= 0 || buyAmount % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 0보다 큰 1000원 단위의 숫자로 입력해야 합니다");
+        }
+    }
+
+    public static void validateLotteNumber(List<Integer> numbers) {
+        validateLottoDuplicate(numbers);
+        validateLottoSize(numbers);
+    }
+
+    private static void validateLottoDuplicate(List<Integer> numbers) {
+        Set<Integer> nonDuplicateNumbers = new HashSet<>();
+        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호들은 중복될 수 없습니다.");
+        }
+    }
+
+
+    private static void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
         }
     }
 }

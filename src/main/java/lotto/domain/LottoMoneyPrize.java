0864fafb353c03da1577c3e0704d16936f9832fa
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 11:14:27 +0900

    feat(LottoMoneyPrize) : 로또 결과 계산 하는 기능 구현 - set() : Map 초기화 - LottoMoneyPrize() 생성자에 로직 추가 - getTotalReward() 추가 : 총 얻은 금액 리턴

--- src/main/java/lotto/domain/LottoMoneyPrize.java
+++ src/main/java/lotto/domain/LottoMoneyPrize.java
@@ -7,26 +7,39 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoMoneyPrize {
-    private final Map<LottoMoney,Integer> lottoResult;
+    private final Map<LottoMoney,Integer> lottoResult = new EnumMap<>(LottoMoney.class);
     private CompareNumber compareNumber = new CompareNumber();
 
-    public LottoMoneyPrize() {
-        lottoResult = new EnumMap<LottoMoney, Integer>(LottoMoney.class);
+    private void set() {
         for(LottoMoney lottoMoney : LottoMoney.values()){
             lottoResult.put(lottoMoney, 0);
         }
     }
 
-    public void printLottoPrize(List<Integer> userWin, List<Integer> randomWin, int bonusNum){
-        int winnerNum = compareNumber.compareWinnerNum(userWin, randomWin);
-        boolean isBonusNumber = compareNumber.compareBonusNum(bonusNum, randomWin);
-        LottoMoney lottoMoney = LottoMoney.getLotto(winnerNum,isBonusNumber);
-        updateLottoPrize(lottoMoney);
+    public LottoMoneyPrize(List<Lotto> lottos, List<Integer> randomWin, int bonusNum) {
+        set();
+        for (Lotto lotto : lottos) {
+            int winnerNum = compareNumber.compareWinnerNum(lotto.getNumber(), randomWin);
+            boolean isBonusNumber = compareNumber.compareBonusNum(bonusNum,lotto.getNumber());
+            LottoMoney lottoMoney = LottoMoney.getLotto(winnerNum, isBonusNumber);
+            updateLottoPrize(lottoMoney);
+        }
+    }
+
+    public int getTotalReward(){
+        int total = 0;
+        for(Map.Entry<LottoMoney, Integer> entry : lottoResult.entrySet()){
+            LottoMoney lottoMoney = entry.getKey();
+            int count = entry.getValue();
+            total += lottoMoney.getMoney() * count;
+        }
+        return total;
     }
 
     private void updateLottoPrize(LottoMoney lottoMoney) {
         lottoResult.put(lottoMoney,lottoResult.get(lottoMoney) + 1);
     }
+
 
     public Map<LottoMoney, Integer> getLottoMoneyPrize(){
         return lottoResult;

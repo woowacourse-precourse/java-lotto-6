95cde5e458ba7dbda0a928976efb4ab744cfe090
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-08 16:42:52 +0900

    refactor : 불필요한 필드 제거
    - 기존의 message는 고유 속성이 아닌, 다른 필드를 조합해서 만들 수 있는 값이므로 제거하였다.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,26 +1,25 @@
 package lotto;
 
+import java.text.NumberFormat;
 import java.util.Arrays;
 
 public enum Rank {
 
-    FIFTH(3, false, 5000, "3개 일치 (5,000원) - "),
-    FOURTH(4, false, 50000, "4개 일치 (50,000원) - "),
-    THIRD(5, false, 1500000, "5개 일치 (1,500,000원) - "),
-    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
-    FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원) - "),
-    NO_RANK(0, false, 0, "");
+    FIFTH(3, false, 5000),
+    FOURTH(4, false, 50000),
+    THIRD(5, false, 1500000),
+    SECOND(5, true, 30000000),
+    FIRST(6, false, 2000000000),
+    NO_RANK(0, false, 0);
 
     private final int matchCount;
     private final boolean bonus;
     private final int cashPrize;
-    private final String message;
 
-    Rank(int matchCount, boolean bonus, int cashPrize, String message) {
+    Rank(int matchCount, boolean bonus, int cashPrize) {
         this.matchCount = matchCount;
         this.bonus = bonus;
         this.cashPrize = cashPrize;
-        this.message = message;
     }
 
     public static Rank getRank(int matchCount, boolean bonus) {
@@ -32,7 +31,16 @@ public enum Rank {
     }
 
     public String getMessage() {
-        return message;
+        String cashPrizeFormatted = cashPrizeFormatting();
+        if (bonus) {
+            return matchCount + "개 일치, 보너스 볼 일치 (" + cashPrizeFormatted + "원) - ";
+        }
+        return matchCount + "개 일치 (" + cashPrizeFormatted + "원) - ";
+    }
+
+    private String cashPrizeFormatting() {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        return numberFormat.format(cashPrize);
     }
 
     public int getCashPrize() {

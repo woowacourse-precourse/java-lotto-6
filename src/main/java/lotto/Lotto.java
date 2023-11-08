03e212b954993517e4999dc1ed8d2b9e6d4bd01d
developerhongjulee <developerhongjulee@gmail.com>
2023-11-08 23:53:37 +0900

    add Lotto class

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,39 @@
 package lotto;
 
-import java.util.List;
+import lotto.util.Validator;
+
+import java.util.*;
 
 public class Lotto {
+    public static final int SIZE = 6;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 45;
+    public static final int PRICE = 1000;
     private final List<Integer> numbers;
-
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+    public List<Integer> getNumberListSorted(){
+        return this.numbers;
     }
 
-    // TODO: 추가 기능 구현
+    private void validate(List<Integer> numbers) {
+        Validator.validateSize(numbers, SIZE);
+        Validator.validateIfDuplicateExist(numbers);
+    }
+    public final String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for(int i=0; i<numbers.size(); i++){
+            sb.append(numbers.get(i));
+            if(i != numbers.size()-1){
+                sb.append(", ");
+            }
+        }
+        sb.append(']');
+        return sb.toString();
+    }
 }

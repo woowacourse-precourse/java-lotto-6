8b70ca6f9be7052241ecca98b777a46ce9fcbba9
GyungA <GyungA@users.noreply.github.com>
2023-11-08 20:01:19 +0900

    feat: 로또 구입 금액 입력 기능

---
+++ src/main/java/lotto/View/LottoBuyer.java
@@ -0,0 +1,20 @@
+package lotto.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class LottoBuyer {
+    public static final Integer LOTTO_PRICE = 1_000;
+    private static final String LOTTO_PRICE_ERROR_MESSAGE = "입력한 금액은 로또 한 장 가격(1,000원)의 배수여야 합니다.";
+    public static Integer getLottoCount() {
+        Integer money = Integer.valueOf(Console.readLine());
+        validateLottoPrice(money);
+        Integer lottoCount = money / LOTTO_PRICE;
+        return lottoCount;
+    }
+
+    private static void validateLottoPrice(Integer money) {
+        if (money % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(LOTTO_PRICE_ERROR_MESSAGE);
+        }
+    }
+}

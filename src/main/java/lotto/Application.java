9edccf55aa87e9ca6cb57f7a6edb7d45d62ed529
alenjb <alenjb@naver.com>
2023-11-08 15:56:49 +0900

    ✨feat(Application): 로또 구입 금액을 입력 받는 기능과 입력값이 빈 값이거나 숫자가 아닌형식인지 확인하는 기능 구현
    로또 구입 금액을 입력 받는 기능과 입력값이 빈 값이거나 숫자가 아닌형식인지 확인하는 기능을 구현함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -17,4 +17,18 @@ public class Application {
             throw new IllegalArgumentException("[ERROR] 구매금액은 1,000원 단위로 입력하셔야 합니다."); // 예외처리: 1,000으로 나누어떨어지지 않는 경우
         return number / ONE_LOTTO_PRICE;
     }
+
+    private static int askPurchaseNumber() {
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            double purchaseAmount = Double.parseDouble(camp.nextstep.edu.missionutils.Console.readLine());
+            return (int) isDivisibleByOneThousand(purchaseAmount); // 구매 금액이 1,000으로 나누어지는지 확인 후 정수로 바꾸어 리턴
+        } catch (NullPointerException e) {
+            System.out.println("[ERROR] 구매금액을 입력하셔야 합니다.");
+            throw e;
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 구매금액은 숫자형식이어야 합니다.");
+            throw e;
+        }
+    }
 }

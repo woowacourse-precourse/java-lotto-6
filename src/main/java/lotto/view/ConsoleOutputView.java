01b4a109c5f658826d90206bb7b76fa96e9313d2
kmw2378 <rlarla677@gmail.com>
2023-11-08 12:58:58 +0900

    feat(lotto): 콘솔 출력시 사용되는 View 클래스 추가 콘솔을 통해 출력 시 사용되는 View 클래스를 추가하였습니다. OutputView를 구현하며 인터페이스의 상수와 파라미터를 통해 값을 출력합니다.

---
+++ src/main/java/lotto/view/ConsoleOutputView.java
@@ -0,0 +1,93 @@
+package lotto.view;
+
+import lotto.dto.response.PurchasePriceResponse;
+import lotto.dto.response.WinningResponse;
+import lotto.dto.response.WinningStatistic;
+import java.util.List;
+import java.util.Map;
+
+import static lotto.configuration.GameConfiguration.DECIMAL_REGEX;
+import static lotto.configuration.GameConfiguration.DECIMAL_SEPARATOR;
+
+public class ConsoleOutputView implements OutputView {
+    @Override
+    public void printPurchasePriceQuestionMessage() {
+        System.out.println(INPUT_PURCHASE_PRICE_MESSAGE);
+    }
+
+    @Override
+    public void printWinningNumberQuestionMessage() {
+        System.out.println();
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
+    }
+
+    @Override
+    public void printBonusNumberQuestionMessage() {
+        System.out.println();
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+    }
+
+    @Override
+    public void printPurchaseLottos(final List<PurchasePriceResponse> purchasePriceResponses) {
+        System.out.println();
+        System.out.println(String.format(PURCHASE_LOTTO_COUNT_FORMAT, purchasePriceResponses.size()));
+        for (PurchasePriceResponse purchasePriceResponse : purchasePriceResponses) {
+            final List<Integer> numbers = purchasePriceResponse.getNumbers();
+            System.out.println(numbers);
+        }
+    }
+
+    @Override
+    public void printWinningStatistic(final WinningResponse winningResponse) {
+        printWinningStatisticPrefix();
+
+        final Map<Integer, WinningStatistic> countGroupingByRank = winningResponse.getStatisticGroupingByRank();
+        final double profitRate = winningResponse.getProfitRate();
+        final List<Integer> descendingOrderedKeys = getDescendingOrderedKeys(countGroupingByRank);
+
+        printWinningStatistic(countGroupingByRank, descendingOrderedKeys);
+        printProfitRate(profitRate);
+    }
+
+    private void printWinningStatisticPrefix() {
+        System.out.println();
+        System.out.println(WINNING_RESULT_MESSAGE_PREFIX);
+        System.out.println(WINNING_RESULT_SEPARATE_LINE);
+    }
+
+    private List<Integer> getDescendingOrderedKeys(final Map<Integer, WinningStatistic> countGroupingByRank) {
+        return countGroupingByRank.keySet()
+                .stream()
+                .sorted((a, b) -> Integer.compare(b, a))
+                .toList();
+    }
+
+    private String createResultFormat(final int rank) {
+        final StringBuilder sb = new StringBuilder(MATCH_COUNT_FORMAT);
+        if (rank == 2) {
+            sb.append(MATCH_BONUS_BALL_MESSAGE);
+        }
+
+        sb.append(WINNING_PRICE_COUNT_FORMAT);
+        return sb.toString();
+    }
+
+    private void printWinningStatistic(final Map<Integer, WinningStatistic> countGroupingByRank,
+                                              final List<Integer> descendingOrderedKeys) {
+        for (int rank : descendingOrderedKeys) {
+            final String resultFormat = createResultFormat(rank);
+            final WinningStatistic winningStatistic = countGroupingByRank.get(rank);
+            final String decimalAmount = getDecimalAmount(winningStatistic.getAmount());
+            System.out.println(String.format(resultFormat, winningStatistic.getMatchedCount(), decimalAmount, winningStatistic.getCount()));
+        }
+    }
+
+    private String getDecimalAmount(final long amount) {
+        return String.valueOf(amount)
+                .replaceAll(DECIMAL_REGEX, DECIMAL_SEPARATOR);
+    }
+
+    private void printProfitRate(final double profitRate) {
+        System.out.println(String.format(PROFIT_RATE_FORMAT, profitRate));
+    }
+}

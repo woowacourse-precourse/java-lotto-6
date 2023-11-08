2f807d0501727be6c09b1058f6b514c573075250
김주영 <gimjuyeong0620@gmail.com>
2023-11-08 13:21:12 +0900

    [FEAT] 로또 번호 유효성 검증
    1. validate 생성
    2. 에러 메시지 생성

---
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -0,0 +1,18 @@
+package lotto.common;
+
+public enum ErrorMessage {
+    INVALID_LOTTO_COUNT("[ERROR] 로또 번호는 6개만 가능합니다."),
+    INVALID_LOTTO_RANGE("[ERROR] 로또 번호는 1~45만 가능합니다."),
+    DUPLICATE_LOTTO_NUMBER("[ERROR] 로또 번호는 중복되면 안됩니다.");
+
+
+    private final String message;
+
+    ErrorMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}

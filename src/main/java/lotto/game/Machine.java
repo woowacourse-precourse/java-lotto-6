e047009ee5800d3af0b1aa960116ecf58e46543d
DaeyeonCho <daecho8@gmail.com>
2023-11-08 12:41:30 +0900

    refactor: 머신 클래스 당첨결과, 수익률 메소드 변경
    당첨번호와 보너스번호로 당첨결과를 반환하는 메소드를 변경하였습니다.
    당첨결과를 기준으로 수익률을 계산하는 메소드로 변경하였습니다.

--- src/main/java/lotto/game/Machine.java
+++ src/main/java/lotto/game/Machine.java
@@ -2,110 +2,99 @@ package lotto.game;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
-import lotto.domain.Numbers;
-import lotto.type.ErrorCode;
-import lotto.type.LottoPayout;
+import lotto.domain.WinningNumber;
+import lotto.type.WinningRanking;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
-import static java.util.Arrays.stream;
+import static lotto.game.UserConsole.showLottos;
 
 public class Machine {
 
-    private static final int PRICE = 1000;
-    private final int count;
+    private static final long PRICE = 1000;
+    private static final int EMPTY = 0;
+    private final int purchasedLottoCount;
 
-    public Machine(String moneyInput) {
-        int money = validateMoneyNumber(moneyInput);
+    public Machine(long money) {
         validateMoneyByAmount(money);
-        this.count = money / PRICE;
-    }
-    public int getCount() {
-        return count;
+        this.purchasedLottoCount = (int) (money / PRICE);
     }
 
-    private int validateMoneyNumber(String moneyInput) {
-        int money = 0;
-        try {
-            money = Integer.parseInt(moneyInput);
-        } catch (IllegalArgumentException e){
-            System.out.println((ErrorCode.INVALID_MONEY_TO_BUY.getDescription()));
+    public long getPurchasedLottoCount() {
+        return purchasedLottoCount;
+    }
+
+    private void validateMoneyByAmount(long money) {
+        if (money % PRICE != 0) {
+            throw new IllegalArgumentException();
         }
-        return money;
     }
 
-    private void validateMoneyByAmount(int money) {
-        if (money % PRICE != 0) throw new IllegalArgumentException(
-                ErrorCode.INVALID_MONEY_TO_BUY.getDescription()
-        );
-    }
-
-    public List<Lotto> createLottoNumbers() {
+    public List<Lotto> createLottos() {
         List<Lotto> lottos = new ArrayList<>();
-        for (int i = 0; i < this.count; i++) {
+        int remainedLottoCount = this.purchasedLottoCount;
+
+        while (remainedLottoCount > EMPTY) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-                    .stream().sorted().toList();
-            lottos.add(new Lotto(numbers));
+                    .stream()
+                    .sorted()
+                    .toList();
+
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+            showLottos(lotto);
+            remainedLottoCount--;
         }
         return lottos;
     }
 
-    public List<Integer> createWinningNumber(String numInput) {
-        List<String> numbers = stream(numInput.split(",")).toList();
-        return numbers.stream()
-                .map((num -> Integer.parseInt(num)))
-                .toList();
-    }
-
-    public Numbers createBonusNumber(List<Integer> winningNumber, int bonusNumber) {
-        return new Numbers(winningNumber, bonusNumber);
-    }
-
-    public int[] createMatchResult(List<Lotto> lottos, Numbers numbers) {
-        List<int[]> results = new ArrayList<>();
+    public Map<Double, Integer> createMatchResult(List<Lotto> lottos, WinningNumber winningNumber) {
+        Map<Double, Integer> matchResult = new HashMap<>();
+        List<WinningRanking> winningRanking = new ArrayList<>(List.of(WinningRanking.values()));
+        for (WinningRanking ranking : winningRanking) {
+            matchResult.put(ranking.getPrize(), 0);
+        }
         for (Lotto lotto : lottos) {
-            int matchCount;
-            int bonusCount = 0;
-            matchCount = lotto.getNumbers().stream().filter(o -> (numbers.getWinningNumbers())
-                    .stream().anyMatch(Predicate.isEqual(o))).toList().size();
-            if (lotto.getNumbers().contains(numbers.getBonusNumber())) {
-                bonusCount = 1;
-            }
-            results.add(new int[]{matchCount, bonusCount});
-        }
-        return countMatchResult(results);
-    }
-
-    private int[] countMatchResult(List<int[]> results) {
-        int[] countResult = new int[5];
-        for (int[] result : results) {
-            if (result[0] == 3) {
-                countResult[0]++;
-            } else if (result[0] == 4) {
-                countResult[1]++;
-            } else if (result[0] == 5) {
-                if (result[1] == 0) {
-                    countResult[2]++;
-                } else {
-                    countResult[3]++;
-                }
-            } else if (result[0] == 6) {
-                countResult[4]++;
+            int matchCount = getMatchCount(winningNumber, lotto);
+            boolean isBonusMatch = lotto.getNumbers().contains(winningNumber.bonusNumber());
+            if (matchCount == 6) {
+                int count = matchResult.get(WinningRanking.FIRST_PLACE.getPrize());
+                matchResult.put(WinningRanking.FIRST_PLACE.getPrize(), count + 1);
+            } else if (matchCount == 5 && isBonusMatch) {
+                int count = matchResult.get(WinningRanking.SECOND_PLACE.getPrize());
+                matchResult.put(WinningRanking.SECOND_PLACE.getPrize(), count + 1);
+            } else if (matchCount == 5 & !isBonusMatch) {
+                int count = matchResult.get(WinningRanking.THIRD_PLACE.getPrize());
+                matchResult.put(WinningRanking.THIRD_PLACE.getPrize(), count + 1);
+            } else if (matchCount == 4) {
+                int count = matchResult.get(WinningRanking.FOURTH_PLACE.getPrize());
+                matchResult.put(WinningRanking.FOURTH_PLACE.getPrize(), count + 1);
+            } else if (matchCount == 3) {
+                int count = matchResult.get(WinningRanking.FIFTH_PLACE.getPrize());
+                matchResult.put(WinningRanking.FIFTH_PLACE.getPrize(), count + 1);
             }
         }
-        return countResult;
+        return matchResult;
     }
 
-    public double calculateInvestmentResult(int[] matchResult) {
-        List<LottoPayout> lottoPayouts = new ArrayList<>(List.of(LottoPayout.values()));
-//        LottoPayout[] lottoPayout = LottoPayout.values();
+    private static int getMatchCount(WinningNumber winningNumber, Lotto lotto) {
+        int matchCount = lotto.getNumbers()
+                .stream()
+                .filter(o -> winningNumber.firstWinningNumber().stream()
+                        .anyMatch(Predicate.isEqual(o)))
+                .toList()
+                .size();
+        return matchCount;
+    }
+
+    public double calculateInvestmentResult(Map<Double, Integer> matchResult) {
         double investmentSum = 0;
-        for (int index = 0; index < matchResult.length; index++) {
-            investmentSum += (double) (matchResult[index] * lottoPayouts.get(index).getAmount()) / (PRICE * this.count);
+        Set<Double> keySet = matchResult.keySet();
+        for (double key : keySet) {
+            investmentSum += (key * (matchResult.get(key)));
         }
-        return investmentSum * 100;
+        return investmentSum / (PRICE * purchasedLottoCount) * 100;
     }
 
 }

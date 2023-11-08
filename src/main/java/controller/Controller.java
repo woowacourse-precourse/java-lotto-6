eaeb27300d4a7bf3760f12165df49d760c90c86f
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-08 15:28:47 +0900

    feat(controller/Controller): 당첨번호와 보너스 번호를 입력받는 기능 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,13 +2,30 @@ package controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import domain.Utils;
+import java.util.List;
 import view.InputView;
 
 public class Controller {
+
+    public void run() {
+        
+    }
 
     private int getPurchaseAmount() {
         System.out.println(InputView.INPUT_PURCHASE_AMOUNT);
         String userInput = Console.readLine().trim();
         return Utils.stringToInteger(userInput);
+    }
+
+    private List<Integer> getWinningNumbers() {
+        System.out.println(InputView.INPUT_WINNING_NUMBERS);
+        String winningNumbers = Console.readLine().trim();
+        return Utils.stringToIntegerList(winningNumbers);
+    }
+
+    private int getBonusNumber() {
+        System.out.println(InputView.INPUT_BONUS_NUMBERS);
+        String bonusNumbers = Console.readLine().trim();
+        return Utils.stringToInteger(bonusNumbers);
     }
 }

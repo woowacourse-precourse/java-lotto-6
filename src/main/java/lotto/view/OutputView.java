8be274f23ecba4217efe30d37c4ff0c2ea851ba2
Laniel Park <Laniel8177@gmail.com>
2023-11-08 22:56:46 +0900

    fix : Apply DTO to OutputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import lotto.constant.Config;
 import lotto.constant.ConsoleMessage;
+import lotto.dto.LottoDto;
+import lotto.dto.LottosDto;
 
 import java.util.List;
 
@@ -9,22 +11,33 @@ public class OutputView {
     private OutputView() {
     }
 
-    public static void printPurchasedLottoHeader() {
-        System.out.println(ConsoleMessage.PURCHASED_LOTTO_HEADER.getMessage());
+    public static void printPurchasedLottos(LottosDto lottos) {
+        String header = String.format(ConsoleMessage.PURCHASED_LOTTO_HEADER.getMessage(), lottos.getSize());
+        System.out.println(header);
+        for (LottoDto lotto : lottos.getLottosDto()) {
+            printPurchasedSingleLotto(lotto.getNumbers());
+        }
     }
 
-    public static void printPurchasedSingleLotto(List<Integer> lottoNumbers) {
-        assert Config.NUMBER_OF_LOTTO_NUMBERS.getConfig() == lottoNumbers.size();
+    private static void printPurchasedSingleLotto(List<Integer> lottoNumbers) {
+        if (lottoNumbers.size() != Config.NUMBER_OF_LOTTO_NUMBERS.getConfig()) {
+            throw new IllegalStateException();
+        }
 
         Object[] lottoNumberStrings = lottoNumbers.stream()
                 .map(Object::toString)
                 .toArray();
 
-        System.out.println();
+        String result = String.format(ConsoleMessage.PURCHASED_SINGLE_LOTTO.getMessage(), lottoNumberStrings);
+
+        System.out.println(result);
     }
 
     public static void printStatistics() {
         System.out.println(ConsoleMessage.STATISTICS_HEADER.getMessage());
+    }
 
+    public static void lineBreak() {
+        System.out.println();
     }
 }

132876c4242eef8d29912d99e8d98b828def8f39
wonindev <sangwonparkdev@gmail.com>
2023-11-08 22:28:40 +0900

    Fix: 불변 리스트 정렬을 하려는 오류 때문에 리스트 복사 후 출력 전에 정렬돼서 출력되게 변경

--- src/main/java/lotto/UI/OutputHandler.java
+++ src/main/java/lotto/UI/OutputHandler.java
@@ -1,12 +1,16 @@
 package lotto.UI;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class OutputHandler {
     public static StringBuilder lottosSB = new StringBuilder();
 
     public static void addLotto(List<Integer> numbers) {
-        lottosSB.append(numbers.toString()).append("\n");
+        List<Integer> copyNumbers = new ArrayList<>(numbers);
+        Collections.sort(copyNumbers);
+        lottosSB.append(copyNumbers.toString()).append("\n");
     }
 
     public void printLottos() {

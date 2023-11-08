1fa4eedc3689bbd4567c9b8c33d1d22f749b09c6
becooq81 <becooq81@gmail.com>
2023-11-08 15:06:49 +0900

    refactor: optimize imports

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -1,15 +1,13 @@
 package lotto.controller;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import lotto.Application;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 
 class LottoControllerTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {

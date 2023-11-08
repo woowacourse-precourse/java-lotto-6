9fe56a5d232402653d8fcd371a08f414096c4ca8
nayonsoso <panda0329@naver.com>
2023-11-08 22:14:46 +0900

    refactor: 수익률 소수 둘쨋자리에서 반올림하도록 수정

--- src/main/java/lotto/constants/message/StatisticsMessage.java
+++ src/main/java/lotto/constants/message/StatisticsMessage.java
@@ -5,5 +5,5 @@ public class StatisticsMessage {
     public static final String START_LINE = "---";
     public static final String CONTENT = "%d개 일치 (%s원) - %d개";
     public static final String CONTENT_INCLUDE_BONUS_NUMBER = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
-    public static final String PROFIT_RATE = "총 수익률은 %.2f%%입니다.";
+    public static final String PROFIT_RATE = "총 수익률은 %.1f%%입니다.";
 }

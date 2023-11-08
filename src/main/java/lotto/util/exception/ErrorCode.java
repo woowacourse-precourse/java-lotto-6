9b2fa0e5cfcd903c61e0f5429471965e435bb869
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 16:50:22 +0900

    feat(ErrorCode): Winning Number 관련 에러 코드 추가

--- src/main/java/lotto/util/exception/ErrorCode.java
+++ src/main/java/lotto/util/exception/ErrorCode.java
@@ -8,7 +8,13 @@ public enum ErrorCode {
 
     INVALID_PURCHASE_AMOUNT("구매 금액은 자연수 값만이 들어갈 수 있습니다."),
     NULL_OR_EMPTY_PURCHASE_AMOUNT("구매 금액은 null이나 공백이 들어갈 수 없습니다."),
-    INVALID_PURCHASE_AMOUNT_FORMAT("구입 금액은 1,000원 단위로 입력해주세요.");
+    INVALID_PURCHASE_AMOUNT_FORMAT("구입 금액은 1,000원 단위로 입력해주세요."),
+
+    INVALID_WINNING_NUMBER_RANGE("당첨 번호는 1부터 45 사이의 숫자여야 합니다."),
+    DUPLICATE_WINNING_NUMBERS("당첨 번호는 중복될 수 없습니다."),
+    INVALID_WINNING_NUMBER_FORMAT("당첨 번호는 정수값이어야 합니다."),
+    INVALID_WINNING_NUMBER_COUNT("6개의 당첨 번호를 입력해주세요."),
+    NULL_OR_EMPTY_WINNING_NUMBERS("당첨 번호는 null이나 공백이 들어갈 수 없습니다.");
 
     private final String message;
 

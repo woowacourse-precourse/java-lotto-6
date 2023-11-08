59d1fc77044d86c48962f8def4e9b78da851fdf4
nahyeon99 <nahyeonee99@gmail.com>
2023-11-08 13:05:25 +0900

    refactor(domain/PrizeCount): 당첨 개수 관리 클래스 추가
    1. 당첨 개수 관리 클래스 추가로 인한, 이에 대한 응답 dto 제거
    2. 출력 시, Map<Prize, Integer> 타입으로 바로 반환하도록 구현

---
+++ src/main/java/lotto/domain/PrizeCount.java
@@ -0,0 +1,46 @@
+package lotto.domain;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class PrizeCount {
+    private Map<Prize, Integer> prizeCount;
+
+    public PrizeCount(List<Prize> matchResult) {
+        this.prizeCount = new LinkedHashMap<>();
+        List<Prize> prizes = Stream.of(Prize.values())
+                .filter(prize -> !prize.equals(Prize.NONE))
+                .sorted(Comparator.reverseOrder())
+                .toList();
+
+        prizes.forEach(prize -> prizeCount.put(prize, 0));
+        setPrizeCount(matchResult);
+    }
+
+    private void setPrizeCount(List<Prize> matchResult) {
+        for (Prize prize : matchResult) {
+            if (prize.equals(Prize.NONE)) {
+                continue;
+            }
+            prizeCount.put(prize, prizeCount.get(prize) + 1);
+        }
+    }
+
+    public Map<Prize, Integer> getPrizeCount() {
+        return Collections.unmodifiableMap(this.prizeCount);
+    }
+
+    public double calculateReturnRate(Money money) {
+        return money.calculateReturnRate(getTotalWinningAmount());
+    }
+
+    private long getTotalWinningAmount() {
+        return prizeCount.keySet().stream()
+                .mapToLong(prize -> prize.calculateAmount(prizeCount.get(prize)))
+                .sum();
+    }
+}

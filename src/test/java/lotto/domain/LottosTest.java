2ceb7aac9351dfb1a357246bebcb5b71ac94658e
hong <kevin0928@naver.com>
2023-11-08 17:43:25 +0900

    test : Lottos의 생성자 테스트
    - Lottos의 생성자를 통해 Lotto를 만든 뒤, 개수 비교 테스트

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -2,6 +2,25 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottosTest {
+    @DisplayName("Lotto 생성한 뒤 개수 비교 테스트")
+    @Test
+    public void testCreateLotto() {
+        // Given
+        List<List<Integer>> numbers = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4, 5, 6),
+                Arrays.asList(7, 8, 9, 10, 11, 12)
+        );
+        // When
+        Lottos lottos = new Lottos(numbers);
+        // Then
+        List<Lotto> lottoList = lottos.getLottos();
+        assertEquals(2, lottoList.size());
+    }
 
 }

01f978000842fe715a9d2754d60fd6afc9998065
킹받드라슈 <soccer_by@naver.com>
2023-11-08 23:09:05 +0900

    test: 집계하기
    - 수익률 및 당첨로또와 비교한 매칭 결과 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.engine.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/lotto/engine/LottoFactoryTest.java
+++ src/test/java/lotto/engine/LottoFactoryTest.java
@@ -1,9 +1,14 @@
 package lotto.engine;
 
+import static lotto.engine.LottoSystemConstant.LOTTO_MAXIMUM_NUMBER_OF_WINNINGS;
+import static lotto.engine.LottoSystemConstant.WINNING_PRIZE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
+import lotto.engine.domain.Customer;
 import lotto.engine.domain.Lotto;
+import lotto.engine.dto.WinningResult;
 import lotto.engine.manager.LottoFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -34,13 +39,34 @@ class LottoFactoryTest {
     @DisplayName("로또 발행 내역을 보여준다.")
     void testShowTotalLotto() {
         //given
-        Lotto lottoA = new Lotto(List.of(10, 20, 30, 31, 32, 33));
-        Lotto lottoB = new Lotto(List.of(3, 7, 11, 14, 43, 44));
-        List<Lotto> lottos = List.of(lottoA, lottoB);
+        List<Lotto> lottos = getLottos();
         //when
         String issuedLottos = lottoFactory.showTotalLotto(lottos);
         //then
-        assertThat(issuedLottos).contains("[", "]", ",","\n");
+        assertThat(issuedLottos).contains("[", "]", ",", "\n");
+    }
+
+    @Test
+    @DisplayName("사용자가 입력한 당첨번호와 램덤하게 발행된 로또 내역을 집계한다.")
+    void testAggregate() {
+        //given
+        List<Integer> userWinningNumbers = List.of(10, 20, 30, 31, 32, 33);
+        Customer customer = new Customer(userWinningNumbers, 15);
+        List<Lotto> lottos = getLottos();
+        //when
+        WinningResult winningResult = lottoFactory.aggregate(customer, lottos);
+        Map<Integer, Integer> actual = winningResult.winningCriteria();
+
+        //then
+        assertThat(winningResult.profit()).isGreaterThan(100);
+        assertThat(actual).containsEntry(LOTTO_MAXIMUM_NUMBER_OF_WINNINGS.value(),1);
+    }
+
+    public List<Lotto> getLottos() {
+        return List.of(
+                new Lotto(List.of(10, 20, 30, 31, 32, 33)),
+                new Lotto(List.of(3, 7, 11, 14, 43, 44))
+        );
     }
 
 

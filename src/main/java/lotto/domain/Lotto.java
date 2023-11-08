d55a838eec9c9c71f35f8f0327a79a282db34039
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-08 12:49:51 +0900

    refactor: Lotto 리팩터링
    Lotto 리팩터링
    - 의미가 더 잘 전달되도록 rename
    - 상수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -13,14 +13,17 @@ public class Lotto {
     public static final Integer MAX_NUMBER = 45;
     public static final Integer MIN_NUMBER = 1;
     public static final Integer NUMBER_LENGTH = 6;
+    private static final String DELIMITER = ", ";
+    private static final String PREFIX = "[";
+    private static final String SUFFIX = "]";
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateDuplicate(numbers);
-        List<Integer> sortable = new ArrayList<>(numbers);
-        Collections.sort(sortable);
-        this.numbers = sortable;
+        List<Integer> sortableNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortableNumbers);
+        this.numbers = sortableNumbers;
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -31,33 +34,33 @@ public class Lotto {
 
     // TODO: 추가 기능 구현
     private void validateDuplicate(List<Integer> numbers) {
-        List<Integer> validDuplicate = new ArrayList<>();
+        List<Integer> validateDuplicate = new ArrayList<>();
         for (Integer number : numbers) {
-            if (validDuplicate.contains(number)) {
+            if (validateDuplicate.contains(number)) {
                 throw new IllegalArgumentException(DUPLICATED_LOTTO_NUMBER);
             }
-            validDuplicate.add(number);
+            validateDuplicate.add(number);
         }
     }
 
     public Grade grade(List<Integer> winningNumbers, Integer bonusNumber) {
         Integer answer = 0;
         Integer bonus = 0;
-        for (Integer number : winningNumbers){
-            if (numbers.contains(number)){
+        for (Integer number : winningNumbers) {
+            if (numbers.contains(number)) {
                 answer++;
             }
         }
-        if (numbers.contains(bonusNumber)){
+        if (numbers.contains(bonusNumber)) {
             bonus++;
         }
-        return Grade.from(answer,bonus);
+        return Grade.from(answer, bonus);
     }
 
     @Override
     public String toString() {
-        StringJoiner stringJoiner = new StringJoiner(", ","[","]");
-        for (Integer number : numbers){
+        StringJoiner stringJoiner = new StringJoiner(DELIMITER, PREFIX, SUFFIX);
+        for (Integer number : numbers) {
             stringJoiner.add(number.toString());
         }
         return stringJoiner.toString();

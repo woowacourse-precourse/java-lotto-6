d5997116ce0a4499dc895028f42e3b009140e227
GiJung <weeds2577@gmail.com>
2023-11-08 17:57:57 +0900

    test(BonusNumberTest): LottoTest의 테스트 코드 형식으로 변경 및 BeforeAll를 사용해 중복되는 값을 설정

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,24 +1,53 @@
 package lotto.domain;
 
 import lotto.exception.ErrorCode;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BonusNumberTest {
 
+    private static List<Integer> lottoNumbers;
+
+    @BeforeAll
+    static void beforeAll() {
+        lottoNumbers = new ArrayList<>(List.of(1,2,3,4,5,6));
+    }
+
+    @DisplayName("보너스 번호가 1보다 작을 경우")
     @Test
-    void 보너스_번호의_범위가_잘못된_경우 () {
-        assertThatThrownBy(() -> new BonusNumber(0, List.of(1,2,3,4,5,6)))
+    void createBonusNumberByUnderRange () {
+
+        int bonusNumber = 0;
+
+        assertThatThrownBy(() -> new BonusNumber(bonusNumber, lottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorCode.INVALID_NUMBER_RANGE.getMessage());
     }
 
+    @DisplayName("보너스 번호가 45보다 큰 경우")
     @Test
-    void 당첨_번호중_보너스_번호와_중복되는_번호가_있는_경우 () {
-        assertThatThrownBy(() -> new BonusNumber(1, List.of(1,2,3,4,5,6)))
+    void createBonusNumberByOverRange () {
+
+        int bonusNumber = 46;
+
+        assertThatThrownBy(() -> new BonusNumber(bonusNumber, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ErrorCode.INVALID_NUMBER_RANGE.getMessage());
+    }
+
+    @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우")
+    @Test
+    void createBonusNumberByDuplicateLottoNumbers () {
+
+        int bonusNumber = 1;
+
+        assertThatThrownBy(() -> new BonusNumber(bonusNumber, lottoNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorCode.DUPLICATE_NUMBER.getMessage());
     }

ad28a069e184fb47184cd481ba43aa24223df9ca
gmu1026 <curiouscomponent@gmail.com>
2023-11-08 19:53:26 +0900

    feat: 문자열 관련 공용 기능 분리

--- src/main/java/lotto/util/StringUtils.java
+++ src/main/java/lotto/util/StringUtils.java
@@ -1,15 +1,22 @@
 package lotto.util;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class StringUtils {
     private static final Pattern NUMBER_ALLOWED_PATTERN = Pattern.compile("\\D");
     private static final String NUMBER_SEPARATOR = ",";
+    private static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
 
     private StringUtils() {
 
+    }
+
+    public static void validateBlank(String target) {
+        if (target.isBlank()) {
+            throw new IllegalArgumentException(createErrorMessage("공백 빈 문자열을 입력할 수 없습니다."));
+        }
     }
 
     public static boolean isNumeric(String target) {
@@ -20,14 +27,21 @@ public class StringUtils {
         return true;
     }
 
-    public static List<Integer> convertStringToList(String target) {
-        List<Integer> result = new ArrayList<>();
+    public static List<Integer> convertNumbersStringToList(String target) {
+        validateIntegerNumbers(target);
 
-        String[] numbers = target.split(NUMBER_SEPARATOR);
-        for (String number : numbers) {
-            result.add(Integer.valueOf(number));
+        return Arrays.stream(target.split(NUMBER_SEPARATOR)).map(Integer::parseInt).toList();
+    }
+
+    private static void validateIntegerNumbers(String target) {
+        try {
+            Arrays.stream(target.split(NUMBER_SEPARATOR)).mapToInt(Integer::parseInt);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new IllegalArgumentException(createErrorMessage("숫자와 콤마(,)만 입력해주세요."));
         }
+    }
 
-        return result;
+    public static String createErrorMessage(String message) {
+        return ERROR_MESSAGE_PREFIX + message;
     }
 }

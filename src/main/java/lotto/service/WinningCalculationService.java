507ce2c6dac182be38ff06f32bf01159ee7dc71d
KimKyuHwan <vkdls754@gmail.com>
2023-11-08 20:13:06 +0900

    feat(WinningCalculationService): 당첨번호와 로또번호를 비교해서 당첨 여부를 판단하는 서비스 기능 구현, 테스트

---
+++ src/main/java/lotto/service/WinningCalculationService.java
@@ -0,0 +1,10 @@
+package lotto.service;
+
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.WinningStatus;
+
+public interface WinningCalculationService {
+    Map<WinningStatus, Integer> calculateWinningStatistics(List<Integer> winningNumbers, int bonusNumber);
+}

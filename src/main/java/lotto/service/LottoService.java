89359afee5cc0361e7ed5c750d4b39702d2ac677
BangMingGing <privatebmk802@naver.com>
2023-11-08 16:56:49 +0900

    feat(LottoService): Add publish lottos function

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,2 +1,26 @@
-package lotto.service;public class LottoService {
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.model.Lotto;
+
+public class LottoService {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+
+    public List<Lotto> publishLottos(int price) {
+        List<Lotto> lottos = new ArrayList<>();
+        int lottoCount = price/1000;
+        while (lottoCount-- > 0) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                    LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_NUMBER_COUNT);
+            Lotto lotto = new Lotto(numbers);
+            lottos.add(lotto);
+        }
+        return lottos;
+    }
 }

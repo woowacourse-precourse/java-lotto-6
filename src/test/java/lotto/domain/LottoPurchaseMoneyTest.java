58073de47c111b6c95a22fa9a65fbf014ca111be
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 19:34:23 +0900

    test: 로또 구매금에 따라 구매할 수 있는 로또 개수 확인 기능 테스트

--- src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
+++ src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
@@ -1,7 +1,10 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static lotto.constant.ErrorMessage.INCORRECT_MONEY_UNIT;
 import static lotto.constant.ErrorMessage.NOT_ENOUGH_MONEY;
@@ -25,4 +28,12 @@ class LottoPurchaseMoneyTest {
                 .hasMessage(INCORRECT_MONEY_UNIT);
     }
 
+    @DisplayName("로또 구매금에 따라 구매할 수 있는 로또 개수를 알 수 있다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1000,1","2000,2","4000,4"})
+    void calculateNumberOfLottoAvailableForPurchase(int money, int expectedNumberOfLotto) {
+        LottoPurchaseMoney lottoPurchaseMoney = new LottoPurchaseMoney(money);
+        int numberOfLottoAvailableForPurchase = lottoPurchaseMoney.getNumberOfLottoAvailableForPurchase();
+        Assertions.assertThat(numberOfLottoAvailableForPurchase).isEqualTo(expectedNumberOfLotto);
+    }
 }

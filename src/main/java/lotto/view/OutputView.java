1ede65f743c3736558b7d7dca7a76b4b381646c1
geunsu <geunsu23@naver.com>
2023-11-08 15:19:47 +0900

    [update]시작 출력문 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,13 +1,11 @@
 package lotto.view;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class OutputView {
 
     public static void printGetPurchasePriceInput() {
-        System.out.println("구매금액을 입력해 주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
     }
 
     public static void printGetWinningNumberInput() {

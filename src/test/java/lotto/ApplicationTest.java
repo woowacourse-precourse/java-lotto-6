f17ae8757720806eef17dd52e919e06568de78d8
inwoo <korean5078@gmail.com>
2023-11-08 22:16:31 +0900

    test(ApplicationTest): 단순 출력 테스트 함수 제거

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -86,35 +86,6 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    @DisplayName("로또 출력만 되는지 확인")
-    void 로또_출력_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]"
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
-    }
-
-    @Test
     @DisplayName("비교 기능 테스트")
     void 비교_로직_테스트() {
         LottoService lottoService = new LottoService();

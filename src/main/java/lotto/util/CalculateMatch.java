540b3441da00b85e1a5e06610673319294a6c669
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:20:14 +0900

    feat : CalculateMatch 생성
    데이터 초기화 해주는 메소드 선언

---
+++ src/main/java/lotto/util/CalculateMatch.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+public class CalculateMatch {
+
+    private static int matchCount;
+    private static boolean matchBonus;
+
+    public static void dataReset(){
+        matchCount = 0;
+        matchBonus = false;
+    }
+
+}

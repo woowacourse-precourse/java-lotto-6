752cc48b8a111692fdd6fe234dc341f9a859832a
nhlee98 <e5slnh9876@gmail.com>
2023-11-08 15:45:46 +0900

    feat: 예외가 발생하면 사용자로부터 다시 입력 받는 기능 구현

--- src/main/java/lotto/util/ExceptionRetryHandler.java
+++ src/main/java/lotto/util/ExceptionRetryHandler.java
@@ -4,19 +4,23 @@ import java.util.function.Supplier;
 
 public class ExceptionRetryHandler {
     public static <R> R retryUntilValid(Supplier<R> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
-        return null;
     }
 
     public static void retryUntilValid(Runnable runnable) {
-        try {
-            runnable.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+        while (true) {
+            try {
+                runnable.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }

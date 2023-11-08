6efdf7cadcb31307fca8530d563415e6e632fda9
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:36:02 +0900

    feat : 프로그램에 필요한 문자열 관리 객체 생성

---
+++ src/main/java/lotto/model/constant/StringSymbolicConstantEnum.java
@@ -0,0 +1,15 @@
+package lotto.model.constant;
+
+public enum StringSymbolicConstantEnum {
+	SPLIT_POINT(","),
+	CHANGE_LINE("\n"),
+	END_STRING_FOR_PURCHASE_AMOUNT("개를 구매했습니다."),
+	TITLE_OF_LOTTO_WINNING_STATUS("당첨통계\n---"),
+	INITIALIZE_FOR_BIG_INT("0");
+
+
+	private final String value;
+	StringSymbolicConstantEnum(String value) { this.value = value; }
+
+	public String getValue() { return value; }
+}

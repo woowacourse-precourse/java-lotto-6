178ea2aac712956475744b04ac20398581bf48f7
choiyewon <choiyewon1997@gmail.com>
2023-11-08 10:56:28 +0900

    fix: (v2)Class Game 로또 개수 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-    int money;
+    int money, count;
     List<Lotto> buy = new ArrayList<>();
     List<Integer> answer = new ArrayList<>();
 
     private void start(){
         System.out.println("구입 금액을 입력해 주세요.");
         this.money = Integer.parseInt(Console.readLine());
+        this.count = this.money/1000;
     }
 }

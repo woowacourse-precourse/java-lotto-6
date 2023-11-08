47b398b68fba4f04b7b1a828c4ddefecb7924218
Han Kyuhyun <khh5345@naver.com>
2023-11-08 17:21:15 +0900

    feat: 사용자로부터 구입 금액을 입력받는 기능구현
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,47 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        public static void main(String[] args) {
+            try {
+                int purchaseAmount = getInputAmount();
+                LottoGame game = new LottoGame(purchaseAmount);
+                game.printLottos();
+
+                List<Integer> winningNumbers = getWinningNumbers();
+                int bonusNumber = getBonusNumber();
+
+                game.checkResults(winningNumbers, bonusNumber);
+                System.out.printf("총 수익률은 %.1f%%입니다.", game.calculateProfit());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        private static int getInputAmount() {
+            System.out.println("구입 금액을 입력해 주세요.");
+            String input = Console.readLine();
+            try {
+                int amount = Integer.parseInt(input);
+                if (amount <= 0 || amount % LottoGame.PRICE_PER_LOTTO != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 양수여야 합니다.");
+                }
+                return amount;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("[ERROR] 숫자를 입력해 주세요.");
+            }
+        }
+
+        private static List<Integer> getWinningNumbers() {
+
+        }
+
+        private static int getBonusNumber() {
+
+        }
     }
 }

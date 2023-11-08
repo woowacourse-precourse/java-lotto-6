b2770254fe37f87a4d8b88e1e9e785cc3d8c81e3
KimSehoon <shggm2000@gmail.com>
2023-11-08 15:37:23 +0900

    test: validateNonNumericalWinningNumbers_test method 구현
    - 숫자가 아닌 당첨번호 입력 시 예외 발생을 테스트하는 validateNonNumericalWinningNumbers_test method 구현

--- src/test/java/lotto/InputWinningNumbersTest.java
+++ src/test/java/lotto/InputWinningNumbersTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static lotto.message.ErrorMessage.NUMBER_FORMAT_MONEY;
+import static lotto.message.ErrorMessage.NUMBER_FORMAT_WINNING_NUMBERS;
 import static lotto.message.ErrorMessage.WINNING_NUMBERS_LENGTH;
 import static lotto.message.ErrorMessage.WINNING_NUMBERS_RANGE;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +43,15 @@ public class InputWinningNumbersTest extends NsTest {
         assertSimpleTest(() -> {
             runException("1,2,3,4,5,1000");
             assertThat(output()).contains(WINNING_NUMBERS_RANGE.errorMessage());
+        });
+    }
+
+    @Test
+    @DisplayName("숫자가 아닌 당첨번호 입력 시 예외 테스트")
+    void validateNonNumericalWinningNumbers_test() {
+        assertSimpleTest(() -> {
+            runException("1,2,3,4,5,pobi");
+            assertThat(output()).contains(NUMBER_FORMAT_WINNING_NUMBERS.errorMessage());
         });
     }
 

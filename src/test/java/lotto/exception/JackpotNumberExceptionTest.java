e03232586fd8a68dc1cb278fe53184610f6b7ac1
KJBig <kjgi73k@gmail.com>
2023-11-08 20:43:15 +0900

    feat : 당첨번호 입력 시 쉼표(,)가 없을 시 예외처리 테스트.
    - 당첨번호 입력 시 쉼표(,)가 없을 때 IllegalArgumentException이 발생하는지 확인하기 위한 테스트 구현.

--- src/test/java/lotto/exception/JackpotNumberExceptionTest.java
+++ src/test/java/lotto/exception/JackpotNumberExceptionTest.java
@@ -1,4 +1,18 @@
 package lotto.exception;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.utils.InputValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class JackpotNumberExceptionTest {
+
+    @DisplayName("당첨번호 입력 시 쉼표(,)가 없으면 예외가 발생한다.")
+    @Test
+    public void inputJackpotNumberByNoComma() {
+        assertThatThrownBy(() -> InputValidator.checkJackpotNumberInput("123456"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

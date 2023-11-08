40e9bf7a3bc50b352653188020ec2c1c43ecd0b6
jjw0071 <jjw0071@naver.com>
2023-11-08 16:51:44 +0900

    feat(LottoResult) : 로또의 결과에 대한 정보를 가진 LottoResult enum 생성

---
+++ src/main/java/lotto/domain/LottoResult.java
@@ -0,0 +1,45 @@
+package lotto.domain;
+
+public enum LottoResult {
+    FIRST(6, 2_000_000_000, "2,000,000,000원"),
+    SECOND(5, 30_000_000, "30,000,000원"),
+    THIRD(5, 1_500_000, "1,500,000원"),
+    FOURTH(4, 50_000, "50,000원"),
+    FIFTH(3, 5_000, "5,000원"),
+    NONE(0, 0, "0원");
+
+    private final int matchCount;
+    private final int prizeMoney;
+    private final String prizeString;
+
+    LottoResult(int matchCount, int prizeMoney, String prizeString) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+        this.prizeString = prizeString;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public String getPrizeString() {
+        return prizeString;
+    }
+
+    public static LottoResult valueOf(int matchCount, boolean bonusMatch) {
+        if (matchCount == 5 && bonusMatch) {
+            return SECOND;
+        }
+        for (LottoResult result : values()) {
+            if (result.getMatchCount() == matchCount && result != SECOND) {
+                return result;
+            }
+        }
+
+        return NONE;
+    }
+}

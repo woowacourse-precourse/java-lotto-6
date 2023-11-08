0aa790456a4b52af0e53254db101e3cc7ffb7b4b
uwoobeat <desktop3711@gmail.com>
2023-11-08 17:33:13 +0900

    feat: 고정 숫자 생성기에 카운터 추가

--- src/test/java/lotto/helper/FixedNumberGenerator.java
+++ src/test/java/lotto/helper/FixedNumberGenerator.java
@@ -4,14 +4,26 @@ import java.util.List;
 import lotto.generator.NumberGenerator;
 
 public class FixedNumberGenerator implements NumberGenerator {
-    private final List<List<Integer>> numbers;
+    private List<List<Integer>> numbers;
+    private int index = 0;
+    private int counter = 0;
 
-    public FixedNumberGenerator(List<List<Integer>> numbers) {
+    public void setNumbers(List<List<Integer>> numbers) {
         this.numbers = numbers;
+    }
+
+    public int getCounter() {
+        return counter;
     }
 
     @Override
     public List<Integer> generate(int start, int end, int size) {
-        return numbers.remove(0);
+        if (index == numbers.size()) {
+            return numbers.get(index - 1);
+        }
+
+        counter++;
+
+        return numbers.get(index++);
     }
 }

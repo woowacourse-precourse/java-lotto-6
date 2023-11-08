1c2d0b5274891ea4e1ed9b81393e461bd9d6d45a
kangyuseok <kingyuseok@naver.com>
2023-11-08 13:20:25 +0900

    Feat: 로또를 맞춘 개수 마다 enum 상수 구현

--- src/main/java/lotto/view/constants/Front.java
+++ src/main/java/lotto/view/constants/Front.java
@@ -4,8 +4,13 @@ public enum Front {
     REQUEST_PURCHASE_AMOUNT("구입 금액을 입력해 주세요."),
     CHECK_LOTTO_TICKETS("개를 구매했습니다."),
     REQUEST_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    REQUEST_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
-    SHOW_THE_WINNING_STATISTICS("당첨 통계");
+    REQUEST_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    SHOW_THE_WINNING_STATISTICS("당첨 통계"),
+    THREE_MATCH("3개 일치 (5,000원) - "),
+    FOUR_MATCH("4개 일치 (50,000원) - "),
+    FIVE_MATCH("5개 일치 (1,500,000원) - "),
+    FIVE_AND_BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    SIX_MATCH("6개 일치 (2,000,000,000원) - ");
 
     private final String message;
 

3331754c762f8c5a05126c9182c0e7377eacc512
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 16:03:21 +0900

    [FEAT] LottoController에 generateLotto 함수 구현
    lottoCount 개수의 맞는 로또 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,29 @@
 package lotto.controller;
 
+import java.util.List;
 import lotto.domain.LottoCost;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
+import lotto.domain.Lotto;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class LottoController {
     public void run() {
         LottoCost lottoCost = getLottoCost();
         int lottoCount = lottoCost.getLottoCount();
+        Lottos lottos = generateLotto(lottoCount);
+
     }
 
     private LottoCost getLottoCost() {
         return new LottoCost(InputView.totalCost());
+    }
+
+    private Lottos generateLotto(int lottoCount) {
+        List<Lotto> lottos = LottoGenerator.generateLottos(lottoCount);
+        OutputView.printLottoCount(lottoCount);
+        OutputView.printLottos(lottos);
+        return new Lottos(lottos);
     }
 }

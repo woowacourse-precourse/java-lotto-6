e570bf5c1e4659eae30fe39bfb5c65bd444d22cc
DaehoSon <thseogh135@gmail.com>
2023-11-08 23:46:11 +0900

    feat: 로또 비교 후 수익률 파악 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,5 +6,7 @@ public class Application {
         lottoManager.purchaseLottos();
         LottoDrawMachine lottoDrawMachine = new LottoDrawMachine();
         lottoDrawMachine.drawNumberManual();
+        lottoManager.calculateMatchResults(lottoDrawMachine);
+        lottoManager.printWinningReport();
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -12,4 +12,8 @@ public class Lotto {
         this.numbers = numbers;
     }
 
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
 }

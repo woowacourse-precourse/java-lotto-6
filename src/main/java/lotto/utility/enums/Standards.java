c2127b737d2cdf610d4b0caba26f0c9c8e1adb16
“sangjun121” <juncho12011201@gmail.com>
2023-11-08 20:48:29 +0900

    당첨 종류별 개수 표현할 상수 enum 구현

--- src/main/java/lotto/utility/enums/Standards.java
+++ src/main/java/lotto/utility/enums/Standards.java
@@ -5,7 +5,12 @@ public enum Standards {
     ZERO(0),
     CORRECT_COMMA_COUNT(5),
     MIN_LOTTO_NUMBER(1),
-    MAX_LOTTO_NUMBER(45);
+    MAX_LOTTO_NUMBER(45),
+    COUNT_THREE(3),
+    COUNT_FOUR(4),
+    COUNT_FIVE(5),
+    COUNT_SIX(6);
+
 
 
     private int number;

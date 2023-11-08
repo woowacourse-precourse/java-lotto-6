ffc200aeade594c4d7f75a38cc11d72556d604a6
ebang <dskhe8@gmail.com>
2023-11-08 17:44:01 +0900

    test: 당첨 번호에 맞는 로또를 만드는 로직 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -18,10 +21,15 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1에서 45사이의 숫자가 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {46, 47, 0, -1})
+    void createExceptionNumbers(int number) {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, number)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

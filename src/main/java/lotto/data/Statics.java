30c14c5c5400d379e503d816e8eb8415ed9b4bda
olive-su <1466su@gmail.com>
2023-11-08 20:36:50 +0900

    feat: Statics 클래스 생성

---
+++ src/main/java/lotto/data/Statics.java
@@ -0,0 +1,38 @@
+package lotto.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Statics {
+    private int profit;
+    private List<Integer> count = new ArrayList<>();
+
+
+    public Statics(int c) {
+
+    } // CHECK
+
+    public void EvaluateProfit(Purchasing purchasing){
+        int purchasedPrice = purchasing.getPurchasedPrice();
+
+        int totalCost = 0;
+        for (int i = 0; i < count.size(); i++) {
+            totalCost += count.get(i) * Prize.getMoneyByNumber(i);
+        }
+
+        double profitRate = ((double) (purchasedPrice - totalCost) / totalCost) * 100;
+        this.profit = (int) Math.round(profitRate);
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public List<Integer> getCount() {
+        return count;
+    }
+
+    public int getCountNumber(int number) {
+        return count.get(number);
+    }
+}

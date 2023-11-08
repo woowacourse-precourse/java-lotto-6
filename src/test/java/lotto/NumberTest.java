198a7283cff88c879255b5dcace7ba822e9fb340
심석현 <shim9597@gmail.com>
2023-11-08 15:05:13 +0900

    test: 개별 숫자 값 범위 검증 기능 테스트

---
+++ src/test/java/lotto/NumberTest.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.*;
+
+import lotto.domain.Number;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class NumberTest {
+    private static final String NUMBER_OUT_OF_RANGE = "[ERROR] 1에서 45사이의 숫자를 입력해주세요.";
+
+    @DisplayName("")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 46})
+    void createNumberByOutOfRange(int number) {
+        assertThatThrownBy(
+                () -> Number.from(number))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NUMBER_OUT_OF_RANGE);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 45})
+    void createNumberByWithinRange(int number) {
+        assertThatCode(
+                () -> Number.from(number))
+                .doesNotThrowAnyException();
+    }
+}

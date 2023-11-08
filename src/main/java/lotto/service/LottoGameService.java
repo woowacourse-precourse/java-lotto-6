e5a8bb5a1558b4adec898b5fa27d3beadced41c3
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 15:43:28 +0900

    feat(LottoGameService): 로또 게임 진행을 위한 서비스 로직 작성

---
+++ src/main/java/lotto/service/LottoGameService.java
@@ -0,0 +1,35 @@
+package lotto.service;
+
+import java.util.List;
+import lotto.model.Player;
+import lotto.model.WinningNumbers;
+import lotto.util.RandomUtil;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameService {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomUtil randomUtil;
+
+    public LottoGameService(InputView inputView, OutputView outputView, RandomUtil randomUtil) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.randomUtil = randomUtil;
+    }
+
+    public Player createPlayer() {
+        outputView.requestPaymentAmount();
+        int paymentAmount = inputView.purchaseCost();
+        return new Player(paymentAmount, randomUtil);
+    }
+
+    public WinningNumbers createWinningNumber() {
+        outputView.requestWinningNumber();
+        List<Integer> winningNumbers = inputView.winningNumber();
+        outputView.requestBonusNumber();
+        int bonusNumber = inputView.bonusNumber(winningNumbers);
+
+        return new WinningNumbers(winningNumbers, bonusNumber);
+    }
+}

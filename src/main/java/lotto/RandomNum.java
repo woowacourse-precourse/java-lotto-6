f890dc40ee8102f7fbe9b791cd598dff4f702898
naayoung <ask5046a@gmail.com>
2023-11-08 22:53:32 +0900

    refactor: 랜덤값 설정 및 정렬

--- src/main/java/lotto/RandomNum.java
+++ src/main/java/lotto/RandomNum.java
@@ -1,18 +1,20 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
 public class RandomNum {
-    public List<Integer> getRandomNum(){
-        List<Integer> lottoNum = new ArrayList<>();
-        while(lottoNum.size() < 6) {
-            int randomNum = Randoms.pickNumberInRange(1, 45);
-            if (!lottoNum.contains(randomNum)) {
-                lottoNum.add(randomNum);
-            }
-        }
-        return lottoNum;
+    public RandomNum() {
+    }
+    private static List<Integer> lottoNum;
+
+    public static List<Integer> setRandomNumbers() {
+        lottoNum = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        List<Integer> lottoNumList = new ArrayList<>(lottoNum);
+        Collections.sort(lottoNumList); //정렬
+        return lottoNumList;
     }
 }

550a3f6455ff2ca94e67098e8843d6955ee508d8
Laniel Park <Laniel8177@gmail.com>
2023-11-08 19:36:46 +0900

    fix : ConsoleMessage - Rename Enum

--- src/main/java/lotto/constant/ConsoleMessage.java
+++ src/main/java/lotto/constant/ConsoleMessage.java
@@ -1,16 +1,18 @@
 package lotto.constant;
 
+import java.util.List;
+
 public enum ConsoleMessage {
     ENTER_PURCHASE_AMOUNT("구입금액을 입력해주세요."),
-    LOTTO_PURCHASED("%s개를 구매했습니다."),
-    DISPLAY_LOTTO_NUMBERS("[%s, %s, %s, %s, %s, %s]"),
+    PURCHASED_LOTTO_HEADER("%s개를 구매했습니다."),
+    PURCHASED_SINGLE_LOTTO("[%s, %s, %s, %s, %s, %s]"),
     ENTER_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     ENTER_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS_HEADER("당첨 통계"),
-    DIVIDER("---"),
-    WINNING_STATISTICS_WITHOUT_BONUS_BALL("%s개 일치 (%s원) - %s개"),
-    WINNING_STATISTICS_WITH_BONUS_BALL("%s개 일치, 보너스 볼 일치 (%s원) - %s개"),
-    TOTAL_RATE_OF_RETURN("총 수익률은 %s입니다.");
+    STATISTICS_HEADER("당첨 통계\n---"),
+    STATISTICS_WITHOUT_BONUS_BALL("%s개 일치 (%s원) - %s개"),
+    STATISTICS_WITH_BONUS_BALL("%s개 일치, 보너스 볼 일치 (%s원) - %s개"),
+    TOTAL_RATE_OF_RETURN("총 수익률은 %s입니다."),
+    ERROR("[ERROR] %s");
 
     private final String message;
 
@@ -28,9 +30,10 @@ public enum ConsoleMessage {
         return message;
     }
 
-    public String getMessageWithValues(String... values) {
-        assert values.length == getExpectedValueCount(); // Format 할 문자가 있는 메시지 (개수 일치 확인)
+    public String getFormattedMessage(List<String> values) {
+        assert values.size() == getExpectedValueCount(); // Format 할 문자가 있는 메시지 (개수 일치 확인)
 
-        return String.format(message, (Object[]) values);
+        return String.format(message, values.toArray());
     }
+
 }

1e108bdae6e485f43d409ca31a669871797d56f1
yohanii <dygks924@gmail.com>
2023-11-08 15:09:01 +0900

    feat: 로또 1장이 몇 등 했는지 구한다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -16,8 +18,31 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        if (new HashSet<>(numbers).size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되면 안됩니다.");
+        }
+    }
+
     public int getRank(List<Integer> winNums, int bonusNum) {
-        return 0;
+        long correctCount = numbers.stream()
+                .filter(number -> winNums.contains(number))
+                .count();
+
+        int rank = calculateRank((int) correctCount, bonusNum);
+        return rank;
+    }
+
+    private int calculateRank(int correctCount, int bonusNum) {
+        if(correctCount == 6) {
+            return 1;
+        }
+
+        if (correctCount == 5 && numbers.contains(bonusNum)) {
+            return 2;
+        }
+
+        int rank = (correctCount >= 3) ? 8 - (int) correctCount : -1;
+        return rank;
     }
 }

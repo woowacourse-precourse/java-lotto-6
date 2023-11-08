2c18dba2914b4cee070bd7a940f00acc119fbcd1
hong <kevin0928@naver.com>
2023-11-08 17:54:41 +0900

    test : LottoShop의 sellLottos 테스트
    - 돈을 입력한 뒤, sellLottos의 반환값과 기대값과 비교 테스트

--- src/test/java/lotto/domain/LottoShopTest.java
+++ src/test/java/lotto/domain/LottoShopTest.java
@@ -2,6 +2,23 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lotto.dto.response.LottosInfoDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class LottoShopTest {
+
+    @DisplayName("Ticket이 돈에 맞게 생성 되는지 테스트")
+    @Test
+    public void testSellLottos() {
+        // Given
+        LottoShop lottoShop = new LottoShop();
+        int money = 14000;
+        // When
+        LottosInfoDto lottosInfoDto = lottoShop.sellLottos(money);
+        int expectedTicketCount = money / ConstantValue.THOUSAND;
+        // Then
+        assertEquals(expectedTicketCount, lottosInfoDto.ticket());
+    }
 
 }

0dcccdba5d80e2cfefa9a49b736c44858b793e6d
mksystem98 <mksystem98@gmail.com>
2023-11-08 20:23:17 +0900

    feat: LottoErrorCodes에 LottoConstants 상수를 가져올 수 있도록 기능 추가
    에러메시지에 로또 한 장의 금액 1000원이나, 1에서 45범위 등 LottoConstants에 속한 상수들을 포함하여 출력할 수 있도록 기능 추가

--- src/main/java/lotto/enums/LottoErrorCodes.java
+++ src/main/java/lotto/enums/LottoErrorCodes.java
@@ -3,28 +3,34 @@ package lotto.enums;
 public enum LottoErrorCodes {
 
     INVALID_INTEGER_INPUT("[ERROR] 정수형 숫자만 입력할 수 있습니다."),
-    MINIMUM_AMOUNT_ERROR("[ERROR] 구매 금액은 최소 1000원 입니다."),
-    AMOUNT_UNIT_ERROR("[ERROR] 구매 금액은 1000원 단위로 입력해야 합니다."),
-    BONUS_NUMBER_ERROR("[ERROR] 보너스 번호는 당첨 번호와 다른 번호여야 합니다."),
-    ONLY_NUMBER_ALLOWED("[ERROR] 숫자만 입력할 수 있습니다."),
-    NUMBER_RANGE_ERROR("[ERROR] 숫자는 %d에서 %d 사이어야 합니다."), // Placeholder for actual numbers
-    EXACT_NUMBER_REQUIRED("[ERROR] 정확히 6개의 숫자를 입력해야 합니다.");
+    MINIMUM_AMOUNT_ERROR("[ERROR] 구매 금액은 최소 " + LottoConstants.LOTTO_TICKET_PRICE.getValue() + "원 입니다."),
+    AMOUNT_UNIT_ERROR("[ERROR] 구매 금액은 " + LottoConstants.LOTTO_TICKET_PRICE.getValue() + "원 단위로 입력해야 합니다."),
+    DIFFERENT_BONUS_NUMBER_ERROR("[ERROR] 보너스 번호는 당첨 번호와 다른 번호여야 합니다: "),
+    NUMBERS_ONLY_INPUT("[ERROR] 숫자만 입력할 수 있습니다."),
+    NUMBER_RANGE_ERROR("[ERROR] 숫자는 %d에서 %d 사이어야 합니다.", LottoConstants.MINIMUM_NUMBER.getValue(), LottoConstants.MAXIMUM_NUMBER.getValue()),
+    EXACT_NUMBER_REQUIRED("[ERROR] 정확히 " + LottoConstants.NUMBERS_PER_TICKET.getValue() + "개의 숫자를 입력해야 합니다.");
 
     private final String message;
+    private final int minimumNumber;
+    private final int maximumNumber;
 
     LottoErrorCodes(String message) {
         this.message = message;
+        this.minimumNumber = -1;
+        this.maximumNumber = -1;
+    }
+
+    LottoErrorCodes(String message, int minimumNumber, int maximumNumber) {
+        this.message = message;
+        this.minimumNumber = minimumNumber;
+        this.maximumNumber = maximumNumber;
     }
 
     public String getMessage() {
-        return message;
-    }
-
-    public String getMessageWithNumbers(int minimumNumber, int maximumNumber) {
         if (this == NUMBER_RANGE_ERROR) {
             return String.format(message, minimumNumber, maximumNumber);
         } else {
-            return getMessage();
+            return message;
         }
     }
 }

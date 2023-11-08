ba56bfe31912350e6b84473c059203dd569a9d77
aka-nick <bechalrang@gmail.com>
2023-11-08 22:10:54 +0900

    추가(PrizeLottos) : 추첨대상로또를 위한 전송객체
    추첨대상이 되는 로또 목록을 위한 전송객체를 추가함
    해당 협력객체의 테스트 클래스 추가함
    
    PrizeLottos
    PrizeLottosTest

---
+++ src/main/java/lotto/app/collaboration/dto/PrizeLottos.java
@@ -0,0 +1,29 @@
+package lotto.app.collaboration.dto;
+
+import java.util.List;
+import java.util.Map;
+import lotto.app.collaboration.enums.Prize;
+
+public class PrizeLottos {
+
+    private final Map<Prize, List<PlayerLotto>> lottosWithPrize;
+
+    public PrizeLottos(final Map<Prize, List<PlayerLotto>> lottosWithPrize) {
+        this.lottosWithPrize = lottosWithPrize;
+    }
+
+    public String getMessageCountOfPrizeLottosBy(final Prize prize) {
+        List<PlayerLotto> lottos = lottosWithPrize.getOrDefault(prize, List.of());
+        return prize.makeCountOfPrizeLottos(lottos.size());
+    }
+
+    public long calculateTotalPrizeMoney() {
+        long totalPrizeMoney = 0L;
+        for (Prize prize : Prize.prizeByRank()) {
+            List<PlayerLotto> lottos = lottosWithPrize.getOrDefault(prize, List.of());
+            totalPrizeMoney += prize.money() * lottos.size();
+        }
+        return totalPrizeMoney;
+    }
+
+}

c8dd3290cacabdc1af9076319f35e215479b2abb
sw801733 <sw801733@naver.com>
2023-11-08 11:09:13 +0900

    feat : LottoPlay 유저 입력 기능 추가

--- src/main/java/lotto/controller/LottoPlay.java
+++ src/main/java/lotto/controller/LottoPlay.java
@@ -1,8 +1,26 @@
 package lotto.controller;
 
+import lotto.view.InputView;
+import lotto.domain.*;
+
 public class LottoPlay {
 
+    private Lotto Lotto;
+    private BonusNumber BonusNumber;
+
+    private LottoResult LottoResult;
+
+    private LottoPurchase LottoPurchase;
+
     public void run() {
+        userInput();
+    }
+
+    private void userInput() {
+        InputView input = new InputView();
+        LottoPurchase = new LottoPurchase(input.getLottoPurchase());
+        Lotto = new Lotto(input.getLotto());
+        BonusNumber = new BonusNumber(input.getBonusNumber());
 
     }
 }

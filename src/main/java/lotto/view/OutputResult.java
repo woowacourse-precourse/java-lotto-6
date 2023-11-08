096f5a42e682f49398ccf731e965712b3d7c89aa
banxzxx <jwbang1643@gmail.com>
2023-11-08 17:41:08 +0900

    Refactor : 단순 출력에서 Result enum을 통한 결과 출력으로 코드 수정

--- src/main/java/lotto/view/OutputResult.java
+++ src/main/java/lotto/view/OutputResult.java
@@ -3,15 +3,37 @@ package lotto.view;
 import lotto.dto.Bingo;
 
 public class OutputResult {
+
+    enum Result {
+        BINGO3(Bingo.getBingo3().correctLotto,"3개 일치 (5,000원) - "),
+        BINGO4(Bingo.getBingo4().correctLotto,"4개 일치 (50,000원) - "),
+        BINGO5(Bingo.getBingo5().correctLotto,"5개 일치 (1,500,000원) - "),
+        BINGO5WB(Bingo.getBingo5wB().correctLotto,"5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+        BINGO6(Bingo.getBingo6().correctLotto,"6개 일치 (2,000,000,000원) - ");
+
+        final private int matchNum;
+
+        final private String message;
+
+        Result(int matchNum, final String message){
+            this.matchNum = matchNum;
+            this.message = message;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+
+        public int getMatchNum(){
+            return matchNum;
+        }
+    }
+
     public static void printResult(){
         System.out.println("\n당첨통계\n---");
-
-        System.out.println("3개 일치 (5,000원) - " + Bingo.getBingo3().correctLotto + "개");
-        System.out.println("4개 일치 (50,000원) - " + Bingo.getBingo4().correctLotto + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + Bingo.getBingo5().correctLotto + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Bingo.getBingo5wB().correctLotto + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + Bingo.getBingo6().correctLotto + "개");
-
+        for ( Result result : Result.values() ) {
+            System.out.println(result.getMessage() + result.getMatchNum() + "개");
+        }
     }
 
 }

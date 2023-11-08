52739fcd1d581e396239804d8fc4cf41afeeb3b2
dong2ast <kdh01132006@naver.com>
2023-11-08 19:15:06 +0900

    ✅ rename: 테스트명 명시적으로 변경

--- src/test/java/lotto/domain/GeneratorTest.java
+++ src/test/java/lotto/domain/GeneratorTest.java
@@ -13,7 +13,7 @@ public class GeneratorTest {
 
     @DisplayName("구입한 로또 개수만큼 로또가 생성되는지 확인")
     @Test
-    void generateLotto() {
+    void generateLottoByCount() {
         //given, when
         List<Integer> lottoCount = new ArrayList<>();
         for (int i = 1; i < 50; i++) {

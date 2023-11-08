92df0dad58f2caef6c30a26caddd8d814c66d5ef
mini1115 <kmini9611@naver.com>
2023-11-08 14:01:40 +0900

    feat: Error 처리 메시지 enum 추가

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public enum ErrorMessage {
+    ERROR_IS_NUMBER("[ERROR]숫자만 입력가능합니다."),
+    ERROR_NUMBER_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMessage() {
+        return this.message;
+    }
+}

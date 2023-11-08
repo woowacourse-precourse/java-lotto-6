3130f3ecde970a28c9104e39c129543abe7f95a9
choiyewon <choiyewon1997@gmail.com>
2023-11-08 10:53:34 +0900

    feat: (v2)[Game]구입 금액 받는 method

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -9,4 +9,9 @@ public class Game {
     int money;
     List<Lotto> buy = new ArrayList<>();
     List<Integer> answer = new ArrayList<>();
+
+    private void start(){
+        System.out.println("구입 금액을 입력해 주세요.");
+        this.money = Integer.parseInt(Console.readLine());
+    }
 }

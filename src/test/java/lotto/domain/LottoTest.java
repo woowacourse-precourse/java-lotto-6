a6c747c69c1809a2cbb4d9d6bba9b077fd9c5305
gywns0417 <gywns0417@gmail.com>
2023-11-08 20:32:48 +0900

    test: [구매한 로또 번호 오름차순 정렬] 기능 테스트 작성 및 통과

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+    @DisplayName("로또 객체 생성시 로또 번호 오름차순 정렬")
+    @Test
+    void success_SortAscending() {
+        // give
+        Lotto lotto = new Lotto(List.of(6,5,4,3,2,1));
+        // when
+        List<Integer> lottoNumber = lotto.getNumbers();
+        // then
+        for (int i = 0; i < lottoNumber.size(); i++) {
+            assertThat(lottoNumber.get(i)).isEqualTo(i + 1);
+        }
+    }
+}

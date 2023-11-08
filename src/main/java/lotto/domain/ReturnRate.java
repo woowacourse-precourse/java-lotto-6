66cad3336abdfe93309c99429ed4df13f1df60b5
dabin <db021129@naver.com>
2023-11-08 14:04:56 +0900

    feat : returnRate 추가

---
+++ src/main/java/lotto/domain/ReturnRate.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+public class ReturnRate {
+
+    private final float returnRate;
+
+    public ReturnRate(float returnRate) {
+        this.returnRate = returnRate;
+    }
+}

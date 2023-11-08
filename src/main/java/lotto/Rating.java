fc12dd1b07902963bd9b069e78e1b6be3730be10
최진원 <etudent39@gmail.com>
2023-11-08 22:13:23 +0900

    feat :당첨 번호와 로또 번호 비교 후 이에 따른 등수 반화놔는 기능 구현

---
+++ src/main/java/lotto/Rating.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import java.util.List;
+
+public class Rating {
+    private final static Integer INITIALIZATION_COUNT = 0;
+    private final static Integer COUNT_INCREMENT = 1;
+    private final static boolean TRUE = true;
+    private final static boolean FALSE = false;
+
+    public static Rankings ranking(List<Integer> lotto, List<Integer> winNumbers, Integer bonusNumber) {
+        Integer winNumberCount = INITIALIZATION_COUNT;
+        boolean needBonusBall = FALSE;
+        for(Integer number : lotto) {
+            if(winNumbers.contains(number)) {
+                winNumberCount += COUNT_INCREMENT;
+            }
+        }
+
+        if(winNumbers.contains(bonusNumber)) {
+            needBonusBall = TRUE;
+        }
+
+        return Rankings.getRanking(winNumberCount, needBonusBall);
+    }
+}

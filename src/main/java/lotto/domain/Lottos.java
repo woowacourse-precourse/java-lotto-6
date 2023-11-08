f7f37e4c5d080dd59c63a9d9f2e75b73236bcb4d
PeterChoi <94peterchoi@gmail.com>
2023-11-08 15:25:47 +0900

    feat: 당첨번호 개수 및 보너스볼 일치여부를 바탕으로 당첨결과와 통계를 구하는 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -10,6 +10,24 @@ public class Lottos {
         this.lottos = lottos;
     }
 
+    public PrizeStatistics calculatePrizeStatistics(WinningLotto winningLotto) {
+        PrizeStatistics prizeStatistics = new PrizeStatistics();
+
+        for (Lotto lotto : lottos) {
+            Prize prize = determinePrize(lotto, winningLotto);
+            prizeStatistics.updatePrizeStatistics(prize);
+        }
+
+        return prizeStatistics;
+    }
+
+    private Prize determinePrize(Lotto lotto, WinningLotto winningLotto) {
+        int count = lotto.countMatchingNumbers(winningLotto.getWinningLottoNumbers());
+        boolean isBonusMatched = lotto.containNumber(winningLotto.getBonusNumber());
+
+        return Prize.getPrize(count, isBonusMatched);
+    }
+
     @Override
     public String toString() {
         StringBuilder allLottoNumbers = new StringBuilder();

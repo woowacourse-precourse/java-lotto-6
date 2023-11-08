9bf05fe24eb429b8758e41d8b815103d2be165fa
dragon0622 <iamdragon.dev@gmail.com>
2023-11-08 18:40:20 +0900

    feat(Lotto) : 예외 추가

--- src/main/java/lotto/Exception.java
+++ src/main/java/lotto/Exception.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Exception {
@@ -12,12 +14,17 @@ public class Exception {
     }
 
     public static boolean isValidWinningNumberSingle(String[] splitNumbers) {
+        HashSet<String> hm = new HashSet<>();
         for (int i = 0; i < splitNumbers.length; i++) {
             if (Integer.parseInt(splitNumbers[i]) < 1 || Integer.parseInt(splitNumbers[i])>45){
                 return false;
             }
-
+            hm.add(splitNumbers[i]);
+        }
+        if (hm.size() != splitNumbers.length){
+            return false;
         }
         return true;
     }
+
 }

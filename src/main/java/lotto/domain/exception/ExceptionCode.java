b70a10df96c8fb18a11dc0dc2e7549a14c9381ce
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 13:44:52 +0900

    feat: 예외 코드 열거형 구현
    - 상황에 따른 예외 메세지와 코드를 갖는 열거형

---
+++ src/main/java/lotto/domain/exception/ExceptionCode.java
@@ -0,0 +1,32 @@
+package lotto.domain.exception;
+
+public enum ExceptionCode {
+    NO_ATTRIBUTE("MODEL-001", "값이 존재하지 않습니다."),
+
+    INVALID_RANDOM_NUMBERS_SIZE("SERVICE-001", "랜덤 숫자 길이가 자동차 숫자와 다릅니다.")
+    ;
+
+    private final String code;
+    private final String description;
+
+    ExceptionCode(final String code, final String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "ExceptionCode{" +
+                "code='" + code + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}

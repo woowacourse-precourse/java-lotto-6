fb8424df09062f533e480e672f055376009b4cec
i3kae <snoger1234@gmail.com>
2023-11-08 21:35:14 +0900

    style : Validater 클래스 변수명 재정의 1. inputValidation 메소드 인자 inputStr -> userInput 2. purchaseAmountValidate 함수 인자 inputStr -> purchaseInput 3. bonusNumberValidate 함수 인자 inputStr -> bonusInput 4. winningNumberValidate 함수 인자 inputStr -> winningInput 4. strEmptyCheck, strNumericCheck, strNumericCheck, strSplitedCheck 함수 인자 str -> testValue 5. strNumericCheck 함수 인자 toExclude -> excludedWords 6. numberIsContainedCheck 함수 인자 winningNumber -> numbers

--- src/main/java/lotto/Validater.java
+++ src/main/java/lotto/Validater.java
@@ -7,71 +7,71 @@ import lotto.Messages.ErrorMessage;
 public class Validater {
     private static final String DIVIDING_POINT = ",";
 
-    public void inputValidation(InputType inputType, String inputStr){
+    public void inputValidation(InputType inputType, String userInput){
         if (inputType == InputType.PURCHASE_AMOUNT){
-            purchaseAmountValidate(inputStr);
+            purchaseAmountValidate(userInput);
         }
         if (inputType == InputType.BONUS_NUMBER){
-            bonusNumberValidate(inputStr);
+            bonusNumberValidate(userInput);
         }
         if (inputType == InputType.WINNING_NUMBERS){
-            winningNumberValidate(inputStr);
+            winningNumberValidate(userInput);
         }
     }
-    private void purchaseAmountValidate(String inputStr){
-        strEmptyCheck(inputStr);
-        strNumericCheck(inputStr);
-        if (Integer.parseInt(inputStr) % Lotto.LOTTO_PRICE != 0 || Integer.parseInt(inputStr) == 0){
+    private void purchaseAmountValidate(String purchaseInput){
+        strEmptyCheck(purchaseInput);
+        strNumericCheck(purchaseInput);
+        if (Integer.parseInt(purchaseInput) % Lotto.LOTTO_PRICE != 0 || Integer.parseInt(purchaseInput) == 0){
             Print.printMessage(ErrorMessage.NOT_DIVIED_1000.getMessage());
             throw new IllegalArgumentException(ErrorMessage.NOT_DIVIED_1000.getMessage());
         }
     }
-    private void bonusNumberValidate(String inputStr){
-        strEmptyCheck(inputStr);
-        strNumericCheck(inputStr);
-        if (Integer.parseInt(inputStr) < Lotto.LOTTO_MIN_VALUE || Lotto.LOTTO_MAX_VALUE < Integer.parseInt(inputStr)){
+    private void bonusNumberValidate(String bonusInput){
+        strEmptyCheck(bonusInput);
+        strNumericCheck(bonusInput);
+        if (Integer.parseInt(bonusInput) < Lotto.LOTTO_MIN_VALUE || Lotto.LOTTO_MAX_VALUE < Integer.parseInt(bonusInput)){
             Print.printMessage(ErrorMessage.NOT_IN_1_TO_45.getMessage());
             throw new IllegalArgumentException(ErrorMessage.NOT_IN_1_TO_45.getMessage());
         }
     }
-    private void winningNumberValidate(String inputStr){
-        strEmptyCheck(inputStr);
-        strNumericCheck(inputStr, List.of(DIVIDING_POINT));
-        strSplitedCheck(inputStr, DIVIDING_POINT);
+    private void winningNumberValidate(String winningInput){
+        strEmptyCheck(winningInput);
+        strNumericCheck(winningInput, List.of(DIVIDING_POINT));
+        strSplitedCheck(winningInput, DIVIDING_POINT);
     }
 
-    private void strEmptyCheck(String str){
-        if (str.isEmpty()){
+    private void strEmptyCheck(String testValue){
+        if (testValue.isEmpty()){
             Print.printMessage(ErrorMessage.NULL_STRING.getMessage());
             throw new IllegalArgumentException(ErrorMessage.NULL_STRING.getMessage());
         }
     }
-    private void strNumericCheck(String str){
-        if (Utils.isNumeric(str) == false) {
+    private void strNumericCheck(String testValue){
+        if (Utils.isNumeric(testValue) == false) {
             Print.printMessage(ErrorMessage.NON_NUMERIC_STR.getMessage());
             throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_STR.getMessage());
         }
     }
-    private void strNumericCheck(String str, List<String> toExclude){
-        if (Utils.isNumeric(str, toExclude) == false) {
+    private void strNumericCheck(String testValue, List<String> excludedWords){
+        if (Utils.isNumeric(testValue, excludedWords) == false) {
             Print.printMessage(ErrorMessage.NON_NUMERIC_STR.getMessage());
             throw new IllegalArgumentException(ErrorMessage.NON_NUMERIC_STR.getMessage());
         }
     }
-    private void strSplitedCheck(String str, String regex){
-        if (str.startsWith(regex) == true || str.endsWith(regex) == true){
+    private void strSplitedCheck(String testValue, String regex){
+        if (testValue.startsWith(regex) == true || testValue.endsWith(regex) == true){
             Print.printMessage(ErrorMessage.ILLIGAL_DIVIDING.getMessage());
             throw new IllegalArgumentException(ErrorMessage.ILLIGAL_DIVIDING.getMessage());
         }
-        for (String splited : str.split(regex)){
+        for (String splited : testValue.split(regex)){
             if (splited.isEmpty()){
                 Print.printMessage(ErrorMessage.ILLIGAL_DIVIDING.getMessage());
                 throw new IllegalArgumentException(ErrorMessage.NULL_STRING.getMessage());
             }
         }
     }
-    public void numberIsContainedCheck(Lotto winningNumber, Integer checkNumber){
-        if (winningNumber.isContains(checkNumber)){
+    public void numberIsContainedCheck(Lotto numbers, Integer checkNumber){
+        if (numbers.isContains(checkNumber)){
             Print.printMessage(ErrorMessage.DUPLICATED_NUMBER.getMessage());
             throw new IllegalArgumentException(ErrorMessage.DUPLICATED_NUMBER.getMessage());
         }

3dc32481b75e9c98663143f4ffacc60e61b40e00
JeongMin <ajhappy12@gamil.com>
2023-11-08 22:29:02 +0900

    feat: lotto checker를 주입받는 lotto center 구현

---
+++ src/main/java/lotto/LottoRewardsCenter.java
@@ -0,0 +1,13 @@
+package lotto;
+
+import config.LottoConfig;
+
+import java.util.List;
+
+public class LottoRewardsCenter {
+    private final LottoChecker lottoChecker;
+
+    public LottoRewardsCenter(LottoChecker lottoChecker) {
+        this.lottoChecker = lottoChecker;
+    }
+}

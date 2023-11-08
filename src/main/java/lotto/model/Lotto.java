fc3779590ce903bdd2736ab00e94cdd0c7dc67c3
BBOXEEEE <noeyhesx@naver.com>
2023-11-08 19:56:54 +0900

    feat: Lotto 번호 검증 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,20 +1,61 @@
-package lotto.domain;
+package lotto.model;
 
+import lotto.message.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+        this.numbers = numbers.stream()
+                .sorted()
+                .toList();
     }
 
-    private void validate(List<Integer> numbers) {
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public int countMatch(Lotto winningNumber) {
+        return (int) numbers.stream()
+                .filter(winningNumber::containNumber)
+                .count();
+    }
+
+    public boolean containNumber(int number) {
+        return numbers.contains(number);
+    }
+
+    public LottoRank checkRank(Lotto userLotto, int bonusNumber) {
+        int matchCount = userLotto.countMatch(this);
+        boolean matchBonus = userLotto.getNumbers().contains(bonusNumber);
+        return LottoRank.valueOf(matchCount, matchBonus);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_SIZE_OVER.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> numbersCheck = new HashSet<>(numbers);
+        if (numbersCheck.size() < 6) {
+            throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_IS_DUPLICATE.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number: numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessage.WINNING_NUMBER_NOT_ARRANGE.getMessage());
+            }
+        }
+    }
 }

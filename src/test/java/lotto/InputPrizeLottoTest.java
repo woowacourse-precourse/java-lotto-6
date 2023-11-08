94ac2bfb619d7bf206e078322353e35e0512874a
Jaewon <choijawon99@gmail.com>
2023-11-08 16:37:58 +0900

    feat(test): 주석 추가

--- src/test/java/lotto/InputPrizeLottoTest.java
+++ src/test/java/lotto/InputPrizeLottoTest.java
@@ -10,98 +10,94 @@ public class InputPrizeLottoTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     //로또번호 입력 기능
-    /*
     @Test
-    void 예외_테스트_빈칸_입력(){
+    void 예외_테스트_빈칸_입력() {
         assertSimpleTest(() -> {
-            runException("1000"," ");
-            assertThat(output()).contains(ERROR_MESSAGE);
-        });
-    }
-
-     */
-
-    @Test
-    void 예외_테스트_입력_형태_오류(){
-        assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,");
+            runException("1000", " ");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_입력_번호_개수(){
+    void 예외_테스트_입력_형태_오류() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5");
+            runException("1000", "1,2,3,4,5,");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_입력_번호_문자_여부(){
+    void 예외_테스트_입력_번호_개수() {
         assertSimpleTest(() -> {
-            runException("1000","a,2,3,4,5,6");
+            runException("1000", "1,2,3,4,5");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_입력_번호_범위_작은수(){
+    void 예외_테스트_입력_번호_문자_여부() {
         assertSimpleTest(() -> {
-            runException("1000","-1,1,2,3,4,5");
+            runException("1000", "a,2,3,4,5,6");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_입력_번호_범위_큰수(){
+    void 예외_테스트_입력_번호_범위_작은수() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,46");
+            runException("1000", "-1,1,2,3,4,5");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_입력_번호_중복(){
+    void 예외_테스트_입력_번호_범위_큰수() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5");
+            runException("1000", "1,2,3,4,5,46");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 예외_테스트_입력_번호_중복() {
+        assertSimpleTest(() -> {
+            runException("1000", "1,2,3,4,5,5");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     //보너스번호 입력 기능
     @Test
-    void 예외_테스트_보너스_번호_빈칸_입력(){
+    void 예외_테스트_보너스_번호_빈칸_입력() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5"," ");
+            runException("1000", "1,2,3,4,5,5", " ");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_보너스_번호_숫자_외_입력(){
+    void 예외_테스트_보너스_번호_숫자_외_입력() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5","O");
+            runException("1000", "1,2,3,4,5,5", "O");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_보너스_번호_범위_작은수(){
+    void 예외_테스트_보너스_번호_범위_작은수() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5","-1");
+            runException("1000", "1,2,3,4,5,5", "-1");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
 
     @Test
-    void 예외_테스트_보너스_번호_범위_큰수(){
+    void 예외_테스트_보너스_번호_범위_큰수() {
         assertSimpleTest(() -> {
-            runException("1000","1,2,3,4,5,5","46");
+            runException("1000", "1,2,3,4,5,5", "46");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
-
 
     @Override
     public void runMain() {

dbca768241faee2f75a867a26ab7eb4fd462b2f9
DonghaOh <donghadarwin@naver.com>
2023-11-08 16:10:52 +0900

    feat: 보너스 번호 1부터 45까지의 정수가 아니면 예외
    bonusNumberRange 메소드

--- src/main/java/validator/BonusNumberValidator.java
+++ src/main/java/validator/BonusNumberValidator.java
@@ -1,13 +1,20 @@
 package validator;
 
 public class BonusNumberValidator {
-    public static void bonusNumberValidate(String bounusNumber) {
-        bonusNumberNotNull(bounusNumber);
+    public static void bonusNumberValidate(String bonusNumber) {
+        bonusNumberNotNull(bonusNumber);
+        bonusNumberRange(bonusNumber);
     }
 
     private static void bonusNumberNotNull(String bonusNumber) {
         if (bonusNumber.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
+        }
+    }
+
+    private static void bonusNumberRange(String bonusNumber) {
+        if (!bonusNumber.matches("([1-9]|[1-3][0-9]|4[0-5])")) {
+            throw new IllegalArgumentException("[ERROR] 1부터 45까지의 수를 입력해주세요.");
         }
     }
 }

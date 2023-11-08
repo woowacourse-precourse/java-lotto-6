219e489bfe4e3258c7b0bc68128050ad00d2edc2
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 17:04:45 +0900

    feat(LottoMachine) : add createLottos() method

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -7,6 +7,16 @@ import java.util.List;
 
 public class LottoMachine {
 
+    public Lottos createLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+
+        while (lottos.size() < lottoCount) {
+            lottos.add(createLotto());
+        }
+
+        return new Lottos(lottos);
+    }
+
     private Lotto createLotto() {
         return new Lotto(RandomUtil.createRandomNumbers());
     }

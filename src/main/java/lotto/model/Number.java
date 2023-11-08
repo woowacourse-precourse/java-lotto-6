cfe0fbb617408421fbbbfe9cba43652ef437f707
SeoYoung <seoyoung7623@naver.com>
2023-11-08 20:36:31 +0900

    fix: Number 랭킹 enum 분리

--- src/main/java/lotto/model/Number.java
+++ src/main/java/lotto/model/Number.java
@@ -2,11 +2,12 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Number {
     private List<Integer> numberList;
-    private WinningGrade grade;
+    private WinningResult grade;
 
     public Number() {
         numberList = createNumbers();
@@ -14,18 +15,12 @@ public class Number {
 
     public List<Integer> createNumbers(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+        Collections.sort(numbers);
         return numbers;
     }
 
     public List<Integer> getNumberList() {
         return numberList;
     }
-
-    public WinningGrade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(WinningGrade grade) {
-        this.grade = grade;
-    }
+    
 }

1aa8b9e89228824cd18ad0b810256ae9b67000ad
WoongKi1115 <nwk6638@gmail.com>
2023-11-08 17:34:41 +0900

    feat(statistic) : 당첨 통계 계산 함수 구현

--- src/main/java/lotto/domain/LottoStatistic.java
+++ src/main/java/lotto/domain/LottoStatistic.java
@@ -1,19 +1,31 @@
 package lotto.domain;
 
 public enum LottoStatistic {
-    FIRST("6개 일치 (2,000,000,000원)"),
-    SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD("5개 일치 (1,500,000원)"),
-    FOURTH("4개 일치 (50,000원)"),
-    FIFTH("3개 일치 (5,000원)");
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    FIFTH(3, 5000, "3개 일치 (5,000원)");
 
+    private final int matchNumber;
+    private final long award;
     private final String message;
 
-    LottoStatistic(String message) {
+    LottoStatistic(int matchNumber, long award, String message) {
+        this.matchNumber = matchNumber;
+        this.award = award;
         this.message = message;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public int getMatchNumber() {
+        return matchNumber;
+    }
+
+    public long getAward() {
+        return award;
     }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -2,6 +2,8 @@ package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Lotto;
+import lotto.domain.LottoGame;
+import lotto.domain.LottoStatistic;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,5 +28,34 @@ public class LottoService {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(minNum, maxNum, pickNum);
         Collections.sort(numbers);
         return new Lotto(numbers);
+    }
+
+    public static void calculateLottoResult(LottoGame lottoGame) {
+        for (Lotto lotto : lottoGame.getLottos()) {
+            calculateLotto(lotto, lottoGame);
+        }
+    }
+
+    private static void calculateLotto(Lotto lotto, LottoGame lottoGame) {
+        int matchNumber = 0;
+        for (int number : lotto.getNumbers()) {
+            if (lottoGame.getWinningNumber().getNumbers().contains(number)) {
+                matchNumber++;
+            }
+        }
+        if (matchNumber == 5 && lotto.getNumbers().contains(lottoGame.getBonusNumber())) {
+            lottoGame.setStatisticMap(LottoStatistic.SECOND);
+        }
+        if (matchNumber >= 3 && !lotto.getNumbers().contains(lottoGame.getBonusNumber())) {
+            setStatistic(matchNumber, lottoGame);
+        }
+    }
+
+    private static void setStatistic(int matchNumber, LottoGame lottoGame) {
+        for (LottoStatistic i : LottoStatistic.values()) {
+            if (matchNumber == i.getMatchNumber()) {
+                lottoGame.setStatisticMap(i);
+            }
+        }
     }
 }

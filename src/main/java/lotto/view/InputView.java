b99c21b375506e53463a6ef3a31ffcb128e18fe6
jum0624 <jum0624@naver.com>
2023-11-08 19:36:18 +0900

    feat: 내부 상수 메세지를 상수 클래스로 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,22 +4,24 @@ import lotto.util.InputUtil;
 
 import java.util.List;
 
+import static lotto.constant.SystemConstant.*;
+
 public class InputView {
 
     public int inputMoney() {
-        System.out.print("구입금액을 입력해 주세요.\n");
+        System.out.print(INPUT_MONEY_MESSAGE.getMessage());
         return InputUtil.inputNumber();
     }
 
     public List<Integer> inputWinningNumber() {
         System.out.println();
-        System.out.print("당첨 번호를 입력해 주세요.\n");
+        System.out.print(INPUT_WINNING_NUMBER_MESSAGE.getMessage());
         return InputUtil.inputNumbers();
     }
 
     public int inputBonusNumber() {
         System.out.println();
-        System.out.print("보너스 번호를 입력해 주세요.\n");
+        System.out.print(INPUT_BONUS_NUMBER_MESSAGE.getMessage());
         return InputUtil.inputNumber();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,10 +6,12 @@ import lotto.domain.roulette.RouletteResult;
 import java.util.List;
 import java.util.Map;
 
+import static lotto.constant.SystemConstant.*;
+
 public class OutputView {
     public void outputLottoCount(int count) {
         System.out.println();
-        System.out.printf("%d개를 구매했습니다.\n", count);
+        System.out.printf(OUTPUT_LOTTO_BUY_COUNT_MESSAGE.getMessage(), count);
     }
 
     public void outputLottosNumber(List<Lotto> lottos) {
@@ -19,17 +21,17 @@ public class OutputView {
     }
     public void showResult(Map<RouletteResult, Integer> result) {
         System.out.println();
-        System.out.print("당첨 통계\n");
-        System.out.print("---\n");
+        System.out.print(OUTPUT_RESULT_MESSAGE.getMessage());
+        System.out.print(LINE.getMessage());
         for (RouletteResult rouletteResult : RouletteResult.values()
         ) {
             if (rouletteResult != RouletteResult.FAILURE) {
-                System.out.printf("%s - %d개\n", rouletteResult.getMessage(), result.get(rouletteResult));
+                System.out.printf(OUTPUT_RESULT_COUNT_MESSAGE.getMessage(), rouletteResult.getMessage(), result.get(rouletteResult));
             }
         }
     }
 
     public void showRate(double rate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", rate);
+        System.out.printf(OUTPUT_RESULT_RATE_MESSAGE.getMessage(), rate);
     }
 }

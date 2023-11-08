b72d98dad9a4bef1ae927918a733c02b020c6f8e
wonindev <sangwonparkdev@gmail.com>
2023-11-08 22:26:04 +0900

    Feat: 당첨 번호와 로또 번호가 같은 개수를 저장하는 리스트를 반환하는 메소드 추가

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -3,8 +3,7 @@ package lotto.domain;
 import lotto.constants.LottoEnum;
 import lotto.utils.CommonHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class LottoManager {
     int numberOfTickets;
@@ -14,10 +13,28 @@ public class LottoManager {
     }
 
     public List<Lotto> createLotto(int ticketCnt) {
-        List<Lotto> lottos = new ArrayList<>();
+        List<Lotto> lottos = new LinkedList<>();
         for (int i = 0; i < ticketCnt; i++) {
-            lottos.add(new Lotto(CommonHelper.listSort(CommonHelper.numGenerator())));
+            lottos.add(new Lotto(CommonHelper.numGenerator()));
         }
         return lottos;
+    }
+
+    public List<Integer> sameNumberForWin(List<Integer> userLottoNumbers, List<Lotto> lottos, int bonusNumber) {
+        List<Integer> sameNumbers = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0));
+        int index = 0;
+        int addCnt = 0;
+        for (Lotto lotto : lottos) {
+            index = CommonHelper.countSameNumber(userLottoNumbers, lotto.getNumbers());
+            addCnt = sameNumbers.get(index) + 1;
+            sameNumbers.set(index, addCnt);
+            if (index == 5 && lotto.getNumbers().contains(bonusNumber)) {
+                addCnt = sameNumbers.get(5) - 1;
+                sameNumbers.set(5, addCnt);
+                addCnt = sameNumbers.get(7) + 1;
+                sameNumbers.set(7, addCnt);
+            }
+        }
+        return sameNumbers;
     }
 }

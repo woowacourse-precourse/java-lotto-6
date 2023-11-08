906dcfa287c77865b1d3e32f582b5fd85b6541ec
sulee <ok6737@naver.com>
2023-11-08 23:03:59 +0900

    feat(lotto) : 당첨 금액 집계와 보너스 번호 확인을 위한 enum 구현

--- src/main/java/lotto/LotteryMatchNumber.java
+++ src/main/java/lotto/LotteryMatchNumber.java
@@ -1,4 +1,30 @@
 package lotto;
 
 public enum LotteryMatchNumber{
+    THREE(3,"3개 일치 (5,000원)", 5000,0),
+    FOUR(4, "4개 일치 (50,000원)", 50000,0),
+    FIVE(5, "5개 일치 (1,500,000원)", 1500000,0),
+    FIVEBONUS(6, "5개 일치, 보너스 볼 일치 (30,000,000원)", 30000000,1),
+    SIX(6, "6개 일치 (2,000,000,000원)", 2000000000,0)
+    ;
+
+    private final int count;
+    private final String label;
+    private final int profits;
+    private final int bonus;
+
+    LotteryMatchNumber(int count, String label, int profits, int bonus) {
+        this.count = count;
+        this.label = label;
+        this.profits = profits;
+        this.bonus = bonus;
+    }
+
+    public int getProfits() {
+        return profits;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
 }

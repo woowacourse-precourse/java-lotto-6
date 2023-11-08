9311891ab036e778cd017c78c8284d9b565cfa54
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 15:18:55 +0900

    [FEAT] LottoCost 클래스 구현
    로또 사는 금액을 담고, lottoCount를 계산하기 위해 구현.

---
+++ src/main/java/lotto/domain/LottoCost.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+public class LottoCost {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int ZERO = 0;
+    private final int lottoCost;
+
+    public LottoCost(int lottoCost) {
+        this.lottoCost = lottoCost;
+    }
+
+}

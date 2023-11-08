5fa2fe0273adee8537168076b66d20f0906e6028
U-keun Song <sukeun319@gmail.com>
2023-11-08 15:45:53 +0900

    Test : ConfirmWinningService 클래스 단위 테스트 코드 수정

--- src/test/java/lotto/service/ConfirmWinningServiceTest.java
+++ src/test/java/lotto/service/ConfirmWinningServiceTest.java
@@ -2,6 +2,7 @@ package lotto.service;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import lotto.model.Lotto;
@@ -16,7 +17,7 @@ class ConfirmWinningServiceTest extends OutputChecker {
     private final Lotto WINNER_NUMBER = new Lotto(List.of(3, 4, 5, 6, 7, 8));
     private final Integer BONUS_NUMBER = 13;
     private final LottoOutputView OUTPUT_VIEW = (LottoOutputView) View.generateLottoOutputView();
-
+    
     @DisplayName("confirmWinning 메서드 작동 확인 테스트")
     @Test
     void confirmWinningTest() {
@@ -31,14 +32,12 @@ class ConfirmWinningServiceTest extends OutputChecker {
                     assertThat(output()).contains(
                             "4개 일치 (50,000원) - 1개"
                     );
+
+                    assertEquals(50000L, testInstance.getTotalReward());
                 },
                 List.of(1, 2, 3, 4, 5, 6),
                 List.of(7, 8, 9, 10, 11, 12),
                 List.of(13, 14, 15, 16, 17, 18)
         );
-    }
-
-    @Test
-    void getTotalReward() {
     }
 }

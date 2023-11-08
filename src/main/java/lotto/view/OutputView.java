e0a8c12ccca51d0aa0d848d2136db40a00518285
YYRi9 <mesmgr01@gmail.com>
2023-11-08 11:15:20 +0900

    feat: 발행 수량 출력 구현 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+
+    public static void printLottoCount(int lottoCount) {
+        System.out.println("\n" + lottoCount + "개를 구매했습니다.");
+    }
+
+}

148deb3f412c4f4dc3b0cc1cb4e08f39f8276fc1
kijen723 <kij2646@gmail.com>
2023-11-08 20:46:57 +0900

    feat(lotto): Implement converter to lotto numbers input

---
+++ src/main/java/lotto/Converter.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Converter {
+    public Converter() {
+
+    }
+
+    public List<String> splitNumbers(String numbers) {
+        List<String> result = Arrays.asList(numbers.split(","));
+
+        return result;
+    }
+
+    public List<Integer> convertTypes(List<String> numbers) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            String lottoNumber = numbers.get(i);
+            result.add(Integer.parseInt(lottoNumber));
+        }
+
+        return result;
+    }
+
+    public List<Integer> sortToAscending(List<Integer> numbers) {
+        Collections.sort(numbers);
+
+        return numbers;
+    }
+}

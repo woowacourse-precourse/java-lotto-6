c50a2e458b64842ed6cf51eb577b502c4ee913df
jihoon98 <tlswlgns1003@sju.ac.kr>
2023-11-08 21:45:07 +0900

    feat:"로또번호를 자동으로 생성하는 클래스 생성"

---
+++ src/main/java/lotto/domain/Computer.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.LottoInformation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private Lotto lotto;
+
+    public List<Lotto> drawLottoRandomNumber(int money) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int drawAttempt = 0;  drawAttempt < money / LottoInformation.MONEY_UNIT.getNumber(); drawAttempt++) {
+            List<Integer> numbers = new ArrayList<>(
+                    Randoms.pickUniqueNumbersInRange(LottoInformation.MIN_LOTTO_NUMBER.getNumber(), LottoInformation.MAX_LOTTO_NUMBER.getNumber(), LottoInformation.LOTTO_NUMBER_TOTAL_SIZE.getNumber()));
+            lotto = new Lotto(numbers);
+            lottos.add(lotto.getPuschaseLotto());
+        }
+        return lottos;
+    }
+}

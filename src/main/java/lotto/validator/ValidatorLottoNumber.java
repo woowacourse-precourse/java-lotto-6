8b82b4b114a8c0f89c0ec5713ed23f1d29841fa0
fromitive <fromitive@gmail.com>
2023-11-08 13:28:46 +0900

    feat: 당첨번호 및 보너스번호 검증 기능 추가

--- src/main/java/lotto/validator/ValidatorLottoNumber.java
+++ src/main/java/lotto/validator/ValidatorLottoNumber.java
@@ -1,6 +1,7 @@
 package lotto.validator;
 
 import java.util.stream.IntStream;
+import java.util.List;
 
 import lotto.error.LottoErrorMessage;
 import lotto.exception.LottoNumberException;
@@ -9,18 +10,37 @@ import static lotto.common.LottoNumberRange.LOTTO_START_NUMBER;
 import static lotto.common.LottoNumberRange.LOTTO_END_NUMBER;
 
 public class ValidatorLottoNumber {
-    static void validate(final int validateNumber){
+    public static void validate(final int validateNumber) {
         isLottoNumberOutOfRange(validateNumber);
     }
 
+    public static void validateString(final String validateStringNumber){
+        isNumericValue(validateStringNumber);
+        validate(Integer.parseInt(validateStringNumber));
+    }
     private static void isLottoNumberOutOfRange(final int validateInteger) {
-        if(isNumberOutOfRange(validateInteger, LOTTO_START_NUMBER, LOTTO_END_NUMBER)){
-            throw LottoNumberException.of(LottoErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE) ;
+        if (isNumberOutOfRange(validateInteger, LOTTO_START_NUMBER.getNumber(), LOTTO_END_NUMBER.getNumber())) {
+            throw LottoNumberException.of(LottoErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE);
         }
     }
 
     private static boolean isNumberOutOfRange(int number, int start, int end) {
         return IntStream.rangeClosed(start, end)
-                        .noneMatch(n -> n == number);
+                .noneMatch(n -> n == number);
+    }
+
+    private static void isNumericValue(String buyingPrice) {
+        try {
+            Integer.parseInt(buyingPrice);
+        } catch (NumberFormatException e) {
+            throw LottoNumberException.of(LottoErrorMessage.WRONG_LOTTO_NUMBER);
+        }
+    }
+
+    public static void validateBonusNumber(String bonusNumber, List<Integer> winnerNumber) {
+        validateString(bonusNumber);
+        if(winnerNumber.contains(Integer.parseInt(bonusNumber))){
+            throw LottoNumberException.of(LottoErrorMessage.ALREADY_WINNING_NUMBER);
+        }
     }
 }
--- src/test/java/lotto/validator/ValidatorLottoNumberTest.java
+++ src/test/java/lotto/validator/ValidatorLottoNumberTest.java
@@ -4,27 +4,71 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.util.stream.IntStream;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
+import lotto.error.LottoErrorMessage;
 
 class ValidatorLottoNumberTest {
     final int START_LOTTO_NUMBER = 1;
     final int END_LOTTO_NUMBER = 45;
     final int OUT_OF_RANGE_LOTTO_NUMBER = 46;
+    final String OUT_OF_RANGE_LOTTO_NUMBER_STRING = "46";
+    final String WRONG_LOTTO_NUMBER = "123abc";
+    final IntStream LOTTO_IN_RANGE = IntStream.range(START_LOTTO_NUMBER, END_LOTTO_NUMBER + 1);
     final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
-    void 로또_번호는_1부터_45이내에_생성되어야_함_성공(){
-        //when & then
-        assertDoesNotThrow(() -> IntStream.range(START_LOTTO_NUMBER, END_LOTTO_NUMBER + 1)
+    void 로또_번호는_1부터_45이내에_생성되어야_함_성공() {
+        // when & then
+        assertDoesNotThrow(() -> LOTTO_IN_RANGE
                 .forEach(ValidatorLottoNumber::validate));
     }
-    
+
     @Test
-    void 로또_번호_범위_밖에서_생성될_경우_오류(){
+    void 로또_번호_범위_밖에서_생성될_경우_오류() {
         assertThatThrownBy(() -> ValidatorLottoNumber.validate(OUT_OF_RANGE_LOTTO_NUMBER))
                 .hasMessageContaining(ERROR_MESSAGE)
                 .isInstanceOf(IllegalArgumentException.class);
     }
-    
+
+    @Test
+    void 로또_당첨_입력값이_숫자이고_1부터_45일_경우_성공() {
+        // then
+        assertDoesNotThrow(() -> LOTTO_IN_RANGE
+                // given
+                .mapToObj(String::valueOf)
+                // when
+                .forEach(ValidatorLottoNumber::validateString));
+    }
+
+    @Test
+    void 로또_당첨_입력값이_숫자가_아닐경우_예외발생_후_오류_식별이_가능한_메시지_발생() {
+        assertThatThrownBy(() -> ValidatorLottoNumber.validateString(WRONG_LOTTO_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE)
+                .hasMessageContaining(LottoErrorMessage.WRONG_LOTTO_NUMBER.getMessage());
+    }
+
+    @Test
+    void 로또_당첨_입력값이_숫자이고_범위_밖에서_생성되면_예외발생_후_오류_식별이_가능한_메시지_발생() {
+        assertThatThrownBy(() -> ValidatorLottoNumber.validateString(OUT_OF_RANGE_LOTTO_NUMBER_STRING))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE)
+                .hasMessageContaining(LottoErrorMessage.LOTTO_NUMBER_OUT_OF_RANGE.getMessage());
+    }
+
+    @Test
+    void 보너스_번호의_입력값이_당첨_번호와_겹칠경우_예외발생_후_오류_식별이_가능한_메시지_발생() {
+        // given
+        List<Integer> winnerNumber = List.of(1, 2, 3, 4, 5, 6);
+        String bonusNumber = "1";
+
+        // when
+        assertThatThrownBy(()-> ValidatorLottoNumber.validateBonusNumber(bonusNumber, winnerNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE)
+                .hasMessageContaining(LottoErrorMessage.ALREADY_WINNING_NUMBER.getMessage());
+
+    }
 }

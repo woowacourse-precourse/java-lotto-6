f350da5356993d7ec949612eb12ee7845ca1f099
i3kae <snoger1234@gmail.com>
2023-11-08 20:09:58 +0900

    test : 현재 Lotto 클래스의 번호와 인자의 List를 비교하는 메소드 검증 테스트 구현 1. 당첨된 갯수를 확인하는 메소드에 6개가 아닌 로또 번호가 인자로 들어오면 예외를 발생하도록 테스트 코드 작성 2. 당첨된 갯수를 확인하는 메소드에 중복된 로또 번호가 인자로 들어오면 예외를 발생하는 테스트 코드 작성 3. 당첨된 갯수를 확인하는 메소드에 1 ~ 45 밖의 로또 번호가 인자로 들어오면 예외를 발생하는 테스트 코드 작성 4. 당첨된 갯수를 확인하는 메소드에 보너스 번호가 1 ~ 45 이외의 숫자가 인자로 들어오면 예외를 발생하는 테스트 코드 작성 5. 당첨된 갯수를 확인하는 메소드에 보너스 번호가 로또 번호와 중복된 숫자가 인자로 들어오면 예외를 발생하는 테스트 코드 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -14,7 +14,6 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
@@ -23,5 +22,50 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("당첨된 갯수를 확인하는 메소드에 6개가 아닌 로또 번호가 인자로 들어오면 예외를 발생한다.")
+    @Test
+    void calcMatchNumberOverLottoNumberCheck() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 6, 7), 8))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("당첨된 갯수를 확인하는 메소드에 중복된 로또 번호가 인자로 들어오면 예외를 발생한다.")
+    @Test
+    void calcMatchNumberDuplicatedLottoNumberCheck() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("당첨된 갯수를 확인하는 메소드에 1 ~ 45 밖의 로또 번호가 인자로 들어오면 예외를 발생한다.")
+    @Test
+    void calcMatchNumberNotIn1To45LottoNumberCheck() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(0, 1, 2, 3, 4, 5), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 46), 6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("당첨된 갯수를 확인하는 메소드에 보너스 번호가 1 ~ 45 이외의 숫자가 인자로 들어오면 예외를 발생한다.")
+    @Test
+    void calcMatchNumberNotIn1To45BonusNumberCheck() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 6), 0))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 6), 46))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("당첨된 갯수를 확인하는 메소드에 보너스 번호가 로또 번호와 중복된 숫자가 인자로 들어오면 예외를 발생한다.")
+    @Test
+    void calcMatchNumberDuplicatedBonusNumberCheck() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThatThrownBy(() -> lotto.calcMatchNumber(List.of(1, 2, 3, 4, 5, 6), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

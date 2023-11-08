0533b9ff082cef7a34c4d8c3a1c00bea15505282
YoonSub Lim <yslim37@naver.com>
2023-11-08 23:09:32 +0900

    feat: Exception 발생시 다시 입력받는 로직 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -19,10 +19,18 @@ public class LottoGameController {
 
     public void run() {
         /// 로또 구입 금액 입력
-        String budgetInput = InputView.inputBudget();
-        InputValidator.validateBudget(budgetInput);
-
-        Integer budget = Integer.parseInt(budgetInput);
+        Integer budget;
+        while (true) {
+            try {
+                String budgetInput = InputView.inputBudget();
+                InputValidator.validateBudget(budgetInput);
+                budget = Integer.parseInt(budgetInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
 
         /// 로또 생성 및 출력
         List<Lotto> lottos = lottoGameService.buyLottosWithBudget(budget);
@@ -33,19 +41,37 @@ public class LottoGameController {
         }
 
         /// 당첨 번호 입력
-        String winningNumberInput = InputView.inputWinningNumbers();
-        InputValidator.validateWinningNumber(winningNumberInput);
+        List<Integer> winningNumbers;
+        while(true) {
+            try {
+                String winningNumberInput = InputView.inputWinningNumbers();
+                InputValidator.validateWinningNumber(winningNumberInput);
 
-        String[] numbers = winningNumberInput.split(",");
-        List<Integer> winningNumbers = Arrays.stream(numbers)
-            .map(Integer::parseInt)
-            .toList();
+                String[] numbers = winningNumberInput.split(",");
+                winningNumbers = Arrays.stream(numbers)
+                    .map(Integer::parseInt)
+                    .toList();
+                break;
+            }catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
 
         /// 보너스 번호 입력
-        String bonusNumberInput = InputView.inputBonusNumber();
-        InputValidator.validateBonusNumber(bonusNumberInput, winningNumbers);
+        Integer bonusNumber;
+        while (true) {
+            try {
+                String bonusNumberInput = InputView.inputBonusNumber();
+                InputValidator.validateBonusNumber(bonusNumberInput, winningNumbers);
 
-        Integer bonusNumber = Integer.parseInt(bonusNumberInput);
+                bonusNumber = Integer.parseInt(bonusNumberInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
 
         /// 결과 계산 및 출력
         LottoResult lottoResult = lottoGameService.calculateTotalResult(lottos, winningNumbers,

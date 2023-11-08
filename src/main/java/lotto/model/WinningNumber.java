436a507094cda5aa17dd8b7b2d04c0f020e6ff9f
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 15:32:07 +0900

    refactor(model) : 당첨번호와 보너스 번호 분리해서 모델 구현

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public class WinningNumber {
     private final List<Integer> winningNumbers;
-    private final int bonusNumber;
 
     public WinningNumber(List<Integer> winningNumbers, int bonusNumber) {
         this.winningNumbers = winningNumbers;
-        this.bonusNumber = bonusNumber;
     }
 }

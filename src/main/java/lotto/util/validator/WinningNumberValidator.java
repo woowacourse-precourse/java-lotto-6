96d67687862e1eb59f2cb392add946075323b9c1
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 16:58:49 +0900

    feat(WinningNumberValidator): Winning Number 공백 검증 구현

---
+++ src/main/java/lotto/util/validator/WinningNumberValidator.java
@@ -0,0 +1,15 @@
+package lotto.util.validator;
+
+import lotto.util.exception.ErrorCode;
+
+public class WinningNumberValidator {
+    public static final int MIN_RANDOM_BOUND = 1;
+    public static final int MAX_RANDOM_BOUND = 45;
+    public static final int TOTAL_COUNT = 6;
+
+    private static void validateNullorEmpty(String inputWinningNumbers) {
+        if (inputWinningNumbers == null || inputWinningNumbers.trim().isEmpty()) {
+            throw new IllegalArgumentException(ErrorCode.NULL_OR_EMPTY_WINNING_NUMBERS.getMessage());
+        }
+    }
+}

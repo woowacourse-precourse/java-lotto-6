2fc3664b35f4d0a35c9e40529d538206728ec2d2
minisyu <iswater.kim@gmail.com>
2023-11-08 18:01:30 +0900

    refactor: LottoValidatorTest에 해당 예외 메시지가 나타나도록 수정

--- src/test/java/lotto/domain/validator/LottoValidatorTest.java
+++ src/test/java/lotto/domain/validator/LottoValidatorTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import lotto.domain.lotto.BonusNumber;
 import lotto.domain.lotto.Lotto;
+import lotto.util.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -24,11 +25,13 @@ class LottoValidatorTest {
         // then
         assertThatThrownBy(() ->
                 LottoValidator.verifyNumbersSize(numbers1))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBERS_COUNT.get());
 
         assertThatThrownBy(() ->
                 LottoValidator.verifyNumbersSize(numbers2))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.INVALID_LOTTO_NUMBERS_COUNT.get());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있는 경우 예외 발생")
@@ -44,7 +47,8 @@ class LottoValidatorTest {
         // then
         assertThatThrownBy(() ->
                 LottoValidator.verifyNoDuplication(numbers))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_NUMBERS.get());
     }
 
     @DisplayName("로또 번호가 1에서 45 사이의 범위를 벗어나는 경우 예외 발생")
@@ -59,7 +63,8 @@ class LottoValidatorTest {
         // then
         assertThatThrownBy(() ->
                 LottoValidator.verifyLottoNumberRange(numbers))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.LOTTO_NUMBERS_OUT_OF_RANGE.get());
     }
 
     @DisplayName("보너스 번호가 1에서 45 사이의 범위를 벗어나는 경우 예외 발생")
@@ -70,7 +75,8 @@ class LottoValidatorTest {
         // then
         assertThatThrownBy(() ->
                 LottoValidator.verifyBonusNumberRange(number))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.BONUS_NUMBER_OUT_OF_RANGE.get());
     }
 
     @DisplayName("보너스 번호가 당첨 번호와 중복된 경우 예외 발생")
@@ -86,6 +92,7 @@ class LottoValidatorTest {
         // then
         assertThatThrownBy(() ->
                 LottoValidator.verifyBonusIsNotContainWinningNumbers(winningNumbers, bonusNumber))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_WINNING_NUMBERS_AND_BONUS_NUMBER.get());
     }
 }

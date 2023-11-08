c9518f72b5ce07e2d2ceaea5e474725aa3d1799f
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:18:16 +0900

    feat: 에러 메시지 정의할 클래스 생성 ErrorMessage 클래스를 생성하여 에러 상황에 출력할 메시지를 정의

---
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package lotto.constant;
+
+public class ErrorMessage {
+    // 문자열 처리
+    public static final String INPUT_EMPTY = "입력하신 문자열은 빈 문자열입니다.";
+    public static final String INPUT_CONTAINS_WHITE_SPACE = "입력하신 문자열에 공백이 포함되어 있습니다.";
+}
--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -1,15 +1,18 @@
 package lotto.utils;
 
+import static lotto.constant.ErrorMessage.INPUT_CONTAINS_WHITE_SPACE;
+import static lotto.constant.ErrorMessage.INPUT_EMPTY;
+
 public class StringUtils {
     public static void validEmptyString(String str) {
         if (str == null || str.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INPUT_EMPTY);
         }
     }
 
     public static void validContainsWhiteSpace(String str) {
         if (str != null && str.contains(" ")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INPUT_CONTAINS_WHITE_SPACE);
         }
     }
 }

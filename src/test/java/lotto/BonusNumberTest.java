0f01c2fc5755898fccfcbb40f544381205da25f4
nhn1030 <shgusska12@gmail.com>
2023-11-08 21:30:50 +0900

    test: BonusNumber 클래스 구현을 위한 테스트 코드

---
+++ src/test/java/lotto/BonusNumberTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import lotto.domain.BonusNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BonusNumberTest {
+
+    @Test
+    void testGetBonusNumber() {
+        int bonusNumberValue = 7;
+        BonusNumber bonusNumber = new BonusNumber(bonusNumberValue);
+        assertEquals(bonusNumberValue, bonusNumber.getBonusNumber());
+    }
+}

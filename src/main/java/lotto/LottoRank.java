d9a0d717e44074d4d42144df2ff0b6d2e0b0f82b
Concepty <khs1903b@gmail.com>
2023-11-08 18:41:41 +0900

    feat(LottRank): LottoRank 인스턴스의 정렬 기준 제공

--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,27 +1,30 @@
 package lotto;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public enum LottoRank {
 
-    FIRST_PLACE(6, 2000000000),
-    SECOND_PLACE(5, true, 300000000),
-    THIRD_PLACE(5, false, 1500000),
-    FOURTH_PLACE(4, 50000),
-    FIFTH_PLACE(3, 5000),
-    NO_WINNING(0, 0);
+    FIRST_PLACE(6, 2000000000, 5),
+    SECOND_PLACE(5, true, 300000000, 4),
+    THIRD_PLACE(5, false, 1500000, 3),
+    FOURTH_PLACE(4, 50000, 2),
+    FIFTH_PLACE(3, 5000, 1),
+    NO_WINNING(0, 0, 0);
 
     private int match;
     private Boolean bonusRequired;
     private long reward;
+    private int order;
 
-    LottoRank(int match, long reward) {
-        this(match, null, reward);
+    LottoRank(int match, long reward, int order) {
+        this(match, null, reward, order);
     }
-    LottoRank(int match, Boolean bonusRequired, long reward) {
+    LottoRank(int match, Boolean bonusRequired, long reward, int order) {
         this.match = match;
         this.bonusRequired = bonusRequired;
         this.reward = reward;
+        this.order = order;
     }
 
     public int getMatch() {
@@ -42,5 +45,9 @@ public enum LottoRank {
                 .filter(w -> (w.match == match)
                         && (w.bonusRequired == null || w.bonusRequired == bonus))
                 .findAny().orElse(NO_WINNING);
+    }
+
+    public static Comparator<LottoRank> getComparator() {
+        return Comparator.comparingInt(r -> r.order);
     }
 }

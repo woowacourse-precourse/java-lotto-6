30ba00d2a6e39f99da48af8a377fea8c885ca1d0
youhyeoneee <khukyh2018@khu.ac.kr>
2023-11-08 16:16:36 +0900

    style: 폴더 구조 변경

---
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OutputViewTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void 구매금액_출력테스트() {
+        OutputView.promptForBuyAmount();
+        assertThat(outContent.toString()).isEqualTo("구입금액을 입력해 주세요. ");
+    }
+}

70f7b27a99c6693869f8be8b7cc8596899104ac9
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-08 20:39:50 +0900

    [feat]: main클래스 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import lombok.RequiredArgsConstructor;
+import lotto.dto.LottoResultAndProfitResponseDto;
+import lotto.service.LottoService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class Application {
+    private final LottoService lottoService;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService();
+        int lottoCount = lottoService.inputAmountAndGetLottoCount();
+        List<Integer> lottoNum = lottoService.inputLottoNum();
+        List<List<Integer>> userLottoNum = lottoService.getUserLottoNum(lottoCount);
+        int bonusNum = lottoService.getBonusNum();
+        LottoResultAndProfitResponseDto lottoResultAndProfitResponseDto = lottoService.getLottoResultResponse(userLottoNum, lottoNum,bonusNum);
+        lottoService.PrintResult(lottoResultAndProfitResponseDto);
     }
 }

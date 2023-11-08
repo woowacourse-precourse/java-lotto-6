1458ab5c38ccf9aa44c0e8dd5eb0a4b0c836ec0e
skj4244 <skj4244@naver.com>
2023-11-08 14:45:22 +0900

    style: add method comment Purchaser

--- src/main/java/lotto/Purchaser.java
+++ src/main/java/lotto/Purchaser.java
@@ -24,6 +24,10 @@ public class Purchaser {
         return price / InputJudgement.getLottoPrice();
     }
 
+    /**
+     * 초기 로또의 당첨 상태를 None으로 세팅
+     * @return None 상태로 세팅된 로또
+     */
     private Map<WinningPrice, List<Lotto>> setInitialLotto() {
         Map<WinningPrice, List<Lotto>> lotto = new LinkedHashMap<>();
         Arrays.stream(WinningPrice.values()).toList().forEach(grade -> {
@@ -34,6 +38,10 @@ public class Purchaser {
         return lotto;
     }
 
+    /**
+     * 구매 금액에 기반하여 로또 구매
+     * @return 구매한 로또들
+     */
     public List<Lotto> buy() {
         List<Lotto> lottos = new ArrayList<>();
 
@@ -49,8 +57,15 @@ public class Purchaser {
         lottos.forEach(lotto -> this.lotto.get(WinningPrice.NONE).add(lotto));
     }
 
+    /**
+     * 로또 당첨번호와 보너스 번호를 기반으로 로또 당첨 상태를 갱신
+     * @param winningNumbers 로또 당첨 번호
+     * @param bonusNumber 보너스 번호
+     * @return 당첨 상태가 갱신된 로또
+     */
     public Map<WinningPrice, List<Lotto>> checkWinningResult(List<Integer> winningNumbers, Integer bonusNumber) {
-        List<Lotto> lottos = popNoneValue(lotto);
+        // 우선 None 상태인 로또를 모두 뽑아내고, 다시 작동하게 합니다.
+        List<Lotto> lottos = popAllNoneValueLotto(lotto);
         Queue<Lotto> q = new LinkedList<>();
         q.addAll(lottos);
 
@@ -59,20 +74,30 @@ public class Purchaser {
             Integer correctCount = calculateWinning(winningNumbers, lotto);
             boolean bonus = checkBonus(correctCount, lotto, bonusNumber);
             WinningPrice winningPrice = determineResult(correctCount, bonus);
-
             putResultLotto(winningPrice, lotto);
         }
 
         return this.lotto;
     }
 
-    private List<Lotto> popNoneValue(Map<WinningPrice, List<Lotto>> lotto) {
+    /**
+     * None 상태의 로또를 모두 pop
+     * @param lotto 구입한 로또
+     * @return None 상태의 로또
+     */
+    private List<Lotto> popAllNoneValueLotto(Map<WinningPrice, List<Lotto>> lotto) {
         List<Lotto> lottos = lotto.get(WinningPrice.NONE);
         lotto.remove(WinningPrice.NONE);
         lotto.put(WinningPrice.NONE, new ArrayList<>());
         return lottos;
     }
 
+    /**
+     * 각 로또의 번호와 당첨 번호의 일치 개수 계산
+     * @param winningNumbers 로또 당첨 번호
+     * @param lotto 구입한 로또
+     * @return 구입한 로또와 일치하는 구매 로또 번호의 개수
+     */
     private Integer calculateWinning(List<Integer> winningNumbers, Lotto lotto) {
         Integer winningCount = 0;
 
@@ -89,12 +114,20 @@ public class Purchaser {
         return 0;
     }
 
+    /**
+     * 보너스 번호의 일치 확인
+     * @param correctCount 로또 당첨번호와 일치하는 번호의 개수
+     * @param lotto 구매한 로또
+     * @param bonusNumber 보너스 번호
+     * @return 로또 당첨 번호와 일치하는 번호의 개수가 5개이고 보너스 번호가 일치하면 true 그렇지 않으면 false
+     */
     private boolean checkBonus(Integer correctCount, Lotto lotto, Integer bonusNumber) {
         if(correctCount == 5 && lotto.getNumbers().contains(bonusNumber)){
             return true;
         }
         return false;
     }
+
 
     private WinningPrice determineResult(Integer correctCount, boolean bonus) {
         return WinningPrice.getWinningPrice(correctCount, bonus);

2676cb58e478aadba634779258b5803d4a20cdbb
elizabethgim <yujingim@naver.com>
2023-11-08 15:04:09 +0900

    refactor: Error 태그 상수로 빼기

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -3,6 +3,8 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
     public int purchase(){
         System.out.println("구입금액을 입력해 주세요.\n");
         int money = Integer.parseInt(Console.readLine());
@@ -12,7 +14,7 @@ public class Input {
 
     private void validatePurchase(int money){
         if(money % 1000 != 0){
-            throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매가 가능합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE+" 로또는 1000원 단위로 구매가 가능합니다.");
         }
     }
 }

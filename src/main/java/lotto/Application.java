f3282ce4b4635a6279b1c39d860817d40c4b8cda
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 13:07:55 +0900

    테스트 케이스에 맞게 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,17 +1,12 @@
 package lotto;
 
-
 import lotto.entity.PrizePrint;
 import lotto.entity.WinCount;
 import lotto.entity.WinMessage;
-import lotto.io.BonusNumberReader;
 import lotto.generator.ClientLottoNumber;
-import lotto.io.LottoNumberReader;
 import lotto.service.NumCountInList;
 import lotto.view.PrintView;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -22,30 +17,27 @@ public class Application {
         PrintView printView = new PrintView();
         printView.outView();
 
+
         List<Integer> lottoWinNumber = PrintView.lottoWinNumber;
         int bonusNumber = PrintView.bonusNumber;
-
-        List<Integer> winCounts = new ArrayList<>();
+        List<Integer> winCounts = new ArrayList<>(); // winCounts 초기화
         for (int i = 0; i < clientLottoNumber.LottoNumberList.size(); i++) {
             List<Integer> compare_list = clientLottoNumber.LottoNumberList.get(i);
             int winCount = WinCount.calculateWinCount(compare_list, lottoWinNumber, bonusNumber);
-            winCounts.add(winCount);
-        }
-        for (int i = 0; i < winCounts.size(); i++) {
-            int winCount = winCounts.get(i);
-        }
-        int totalProfit = 0;
-        int o = 3;
-        for (WinMessage.WinType winType : WinMessage.WinType.values()) {
-            int count = NumCountInList.countOccurrences(winCounts, o);
-            int prize = PrizePrint.CalculatorPrize.calculatePrize(o);
-            totalProfit += prize * count;
-            o++;
-            System.out.println(winType.getLabel() + " - " + count + "개");
-        }
-        int p = clientLottoNumber.t * 1000;
+            winCounts.add(winCount); // 각 요소 추가
+            int totalProfit = 0;
+            int o = 3;
+            for (WinMessage.WinType winType : WinMessage.WinType.values()) {
+                int count = NumCountInList.countOccurrences(winCounts, o);
+                int prize = PrizePrint.CalculatorPrize.calculatePrize(o);
+                totalProfit += prize * count;
+                o++;
+                System.out.println(winType.getLabel() + " - " + count + "개");
+            }
+            int p = clientLottoNumber.t * 1000;
 
-        float a = (float) totalProfit / p * 10;
-        System.out.println("총 수익률은 " + String.format("%.1f", a) + "%입니다.");
+            float a = (float) totalProfit / p * 10;
+            System.out.println("총 수익률은 " + String.format("%.1f", a) + "%입니다.");
+        }
     }
 }
--- src/main/java/lotto/io/LottoNumberPrinter.java
+++
@@ -1,8 +0,0 @@
-package lotto.io;
-
-public class LottoNumberPrinter {
-    public void lottonumberprinter(){
-
-    }
-
-}

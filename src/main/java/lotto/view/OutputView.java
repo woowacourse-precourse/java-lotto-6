4c26cc4cb4bae60948e34c894699239cc3732044
hj-k66 <hjx66@naver.com>
2023-11-08 23:19:30 +0900

    move: 패키지 이동

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import lotto.domain.Lotto;
-import lotto.domain.Rank;
+import lotto.Lotto;
+import lotto.Rank;
 
 public class OutputView {
 
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,4 +1,4 @@
-package lotto.domain;
+package lotto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

8b10c995102d6e49802d09951cf43b6bcd59420a
kmw2378 <rlarla677@gmail.com>
2023-11-08 12:50:06 +0900

    feat(lotto): 게임 진행시 필요한 객체를 저장 및 정산을 수행하는 클래스 추가 게임 진행시 필요한 값을 가지고 정산을 수행하여 특정 값을 반환하는 역할을 하는 클래슬르 추가하였습니다.

---
+++ src/main/java/lotto/domain/GameManager.java
@@ -0,0 +1,36 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameManager {
+    private final User user;
+    private final Winning winning;
+    private final Rewards rewards;
+
+    public GameManager(final User user, final Winning winning) {
+        this.user = user;
+        this.winning = winning;
+        this.rewards = createRewards(user, winning);
+    }
+
+    public double getProfitRate() {
+        final long totalAmount = rewards.getTotalAmount();
+        return user.getProfitRate(totalAmount);
+    }
+
+    public int getCountFromRank(final int rank) {
+        return rewards.getCountFromRank(rank);
+    }
+
+    private Rewards createRewards(final User user, final Winning winning) {
+        final List<Reward> rewards = new ArrayList<>();
+        for (int i = 0; i < user.getPurchaseCount(); i++) {
+            final int matchedCount = winning.getMatchedCount(user.getLottoFromIndex(i));
+            final boolean containBonusNumber = winning.containBonusNumber(user.getLottoFromIndex(i));
+            rewards.add(Reward.getInstance(containBonusNumber, matchedCount));   // 메서드명 변경
+        }
+
+        return new Rewards(rewards);
+    }
+}

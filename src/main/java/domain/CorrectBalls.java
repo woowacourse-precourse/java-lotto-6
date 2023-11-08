b99389a123bde4aa01e38592991660ccdd6e88b9
yurrim <yurimkang20@gmail.com>
2023-11-08 15:21:23 +0900

    feat: 당첨된 로또 관련 정보 추가
    - 가격 및 동일한 숫자 개수 추가

--- src/main/java/domain/CorrectBalls.java
+++ src/main/java/domain/CorrectBalls.java
@@ -2,19 +2,31 @@ package domain;
 
 public enum CorrectBalls {
 
-    ThreeSameBalls("3개 일치 (5,000원) - %d개"),
-    FourSameBalls("4개 일치 (50,000원) - %d개"),
-    FiveSameBalls("5개 일치 (1,500,000원) - %d개"),
-    FiveSameBallsWithBonus("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    SixSameBalls("6개 일치 (2,000,000,000원) - %d개");
+    Three(3, "3개 일치 (5,000원) - %d개", 5000),
+    Four(4, "4개 일치 (50,000원) - %d개", 50000),
+    Five(5, "5개 일치 (1,500,000원) - %d개", 1500000),
+    FiveWithBonus(7, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", 30000000),
+    Six(6, "6개 일치 (2,000,000,000원) - %d개", 2000000000);
 
+    private final int ballCount;
     private final String sentence;
+    private final int price;
 
-    CorrectBalls(String sentence) {
+    CorrectBalls(int ballCount, String sentence, int price) {
+        this.ballCount = ballCount;
         this.sentence = sentence;
+        this.price = price;
     }
 
-    public String makeBallSentence(int count) {
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String makeSentence(int count) {
         return String.format(this.sentence, count);
     }
 }

dcf3450746fc2fcb67bb491186c7733885864cca
sora0319 <sojeoung536985@gmail.com>
2023-11-08 19:12:57 +0900

    refactor (LottoResult) : change won content

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,23 +1,24 @@
 package lotto;
 
 public enum LottoResult {
-    THIRD("3", 5000, "(5,000원)"),
-    FOURTH("4", 50000, "(50,000원)"),
-    FIFTH("5", 1500000, "(1,500,000원)"),
-    SIXTH("6", 2000000000, "(2,000,000,000원)"),
-    BONUS("7", 30000000, "(30,000,000원)");
+    THIRD(3, 5000, "3개 일치 (5,000원)"),
+    FOURTH(4, 50000, "4개 일치 (50,000원)"),
+    FIFTH(5, 1500000, "5개 일치 (1,500,000원)"),
+    BONUS(7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    SIXTH(6, 2000000000, "6개 일치 (2,000,000,000원)");
 
-    private final String count;
+
+    private final int count;
     private final int money;
     private final String won;
 
-    LottoResult(String count, int money, String won) {
+    LottoResult(int count, int money, String won) {
         this.count = count;
         this.money = money;
         this.won = won;
     }
 
-    public String getCount() {
+    public int getCount() {
         return count;
     }
 

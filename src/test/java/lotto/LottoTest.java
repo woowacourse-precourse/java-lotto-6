92d34045464c2969ec9e4657e087c9c66f81c168
hyeongjun im <limbs713@naver.com>
2023-11-08 09:33:43 +0900

    :white_check_mark: 로또 객체 중복 숫자 예외 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,21 @@
 package lotto;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import lotto.dto.LottoDto;
+import lotto.model.domain.Lotto;
+import lotto.model.repository.LottoRepository;
+import lotto.model.service.LottoService;
+import lotto.view.OutputView;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +34,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있을 경우 예외가 발생한다.")
+    @Test
+    void createLottoByOutofBoundNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(-1, 2, 3, 4, 5, 65)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

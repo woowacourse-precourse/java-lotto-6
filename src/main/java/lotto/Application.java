107dce27cd7d1b92a55a702601df6649b03a64cc
dmdwns2 <ssreki1@naver.com>
2023-11-08 16:01:15 +0900

    style: trim code

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,20 +7,28 @@ import java.util.List;
 import java.util.Map;
 
 public class Application {
-    public static void main(String[] args) {
-        Output output = new Output();
-        Input input = new Input();
-        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        Analyzer analyzer = new Analyzer();
+    private Output output;
+    private Input input;
+    private Lotto lotto;
+    private Analyzer analyzer;
 
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.run();
+    }
+
+    private Application() {
+        output = new Output();
+        input = new Input();
+        lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        analyzer = new Analyzer();
+    }
+
+    private void run() {
         output.outputPurchaseAmount();
         int purchaseAmount = input.inputPurchaseAmount();
         int ticket = purchaseAmount / Ticket.TICKET_PRICE.getTicketPrice();
-        List<List<Integer>> allTicketNumbers = new ArrayList<>();
-        for (int i = 0; i < ticket; i++) {
-            allTicketNumbers.add(new ArrayList<>(lotto.generateLotto()));
-        }
-        output.outputTicketPurchaseHistory(ticket, allTicketNumbers);
+        List<List<Integer>> allTicketNumbers = generateAllTickets(ticket);
 
         output.outputWinningNumbers();
         List<Integer> winningNumbers = input.inputWinningNumbers();
@@ -28,9 +36,17 @@ public class Application {
         output.outputBonusNumber();
         int bonusNumber = input.inputBonusNumber();
 
-        Map<Integer, Integer> ticketResults;
-        ticketResults = analyzer.fillTicketResults(ticket, winningNumbers, allTicketNumbers, bonusNumber);
+        Map<Integer, Integer> ticketResults = analyzer.fillTicketResults(ticket, winningNumbers, allTicketNumbers, bonusNumber);
         double yieldRate = analyzer.calculateYield(purchaseAmount, ticketResults);
         output.outputResult(ticketResults, yieldRate);
+    }
+
+    private List<List<Integer>> generateAllTickets(int ticket) {
+        List<List<Integer>> allTicketNumbers = new ArrayList<>();
+        for (int i = 0; i < ticket; i++) {
+            allTicketNumbers.add(new ArrayList<>(lotto.generateLotto()));
+        }
+        output.outputTicketPurchaseHistory(ticket, allTicketNumbers);
+        return allTicketNumbers;
     }
 }

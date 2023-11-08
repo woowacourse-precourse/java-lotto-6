a76e8b20ee2736236cdbe9640209f78e9504b544
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:15:47 +0900

    feat : 보너스 볼 입력받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -13,5 +13,10 @@ public class InputView {
         System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
-    
+
+    public static String inputBonusNumber(){
+        System.out.println("보너스 볼을 입력해 주세요.");
+        return Console.readLine();
+    }
+
 }

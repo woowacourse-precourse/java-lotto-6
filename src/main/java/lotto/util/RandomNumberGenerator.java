1741d596b0411926a0b918743b67d0e18b9ad644
sudhdkso <waga1009@gmail.com>
2023-11-08 21:33:25 +0900

    refactor: randomGenerator 불필요한 코드 삭제

--- src/main/java/lotto/util/RandomNumberGenerator.java
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -5,17 +5,11 @@ import static lotto.domain.enums.LottoConstants.MAX_LOTTO_NUM;
 import static lotto.domain.enums.LottoConstants.MIN_LOTTO_NUM;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class RandomNumberGenerator {
-    public RandomNumberGenerator() {
-
-    }
 
     public static List<Integer> generate() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE);
-        Collections.sort(randomNumbers);
-        return randomNumbers;
+        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUM, MAX_LOTTO_NUM, LOTTO_SIZE);
     }
 }

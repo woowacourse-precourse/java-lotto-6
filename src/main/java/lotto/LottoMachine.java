534f36ed6fbb3f39515b52bfe4df3acc344e56eb
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 16:16:52 +0900

    로또 머신 클래스 생성
    로또 번호 생성기와 로또 객체 클래스를 생성했으므로, 로또 번호를 생성하고 이를 관리하여 등수를 구분하는 클래스를 생성했습니다.
    이 클래스는
     - LottoGenerator를 이용해서 로또 티켓을 구매하고, 번호를 생성합니다.
     - 구매한 로또 티켓의 리스트를 관리합니다.
     - 당첨 번호를 체크하고 결과를 집계합니다.
     - 수익률을 계산합니다.

---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class LottoMachine {
+    private final LottoGenerator lottoGenerator;
+    public LottoMachine(LottoGenerator generator) {
+        this.lottoGenerator = generator;
+    }
+
+}

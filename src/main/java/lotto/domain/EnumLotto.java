15980077c45e67c31c910ac5706fc6609480f6d9
Nari <avakray@hansung.ac.kr>
2023-11-08 13:00:32 +0900

    feat(lotto): Enum 작성 (로또 개수 제한 범위, 로또 시작 번호, 로또 마지막 번호)

---
+++ src/main/java/lotto/domain/EnumLotto.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+public enum EnumLotto {
+    LOTTO_LENGTH(6),
+    START_NUMBER(1),
+    END_NUMBER(45);
+
+    private final int NUMBER;
+
+    EnumLotto(int number) {
+        this.NUMBER = number;
+    }
+
+    public int getNumber() {
+        return NUMBER;
+    }
+}

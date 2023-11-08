ce6930951450402cc92aa7b3f93a5ea843fa8bf0
realcold0 <realcold0@gmail.com>
2023-11-08 16:05:59 +0900

    feat(lotto) : 당첨번호와 보너스 번호를 입력받는 InputWinningNumView 기능

---
+++ src/main/java/lotto/view/InputWinningNumView.java
@@ -0,0 +1,38 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.util.ConvertStringToNum;
+
+public class InputWinningNumView {
+    public List<Integer> printWinningNum() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        List<Integer> winningNums = validateInputWinningNum(input);
+        return winningNums;
+    }
+
+    public int printBonusNum() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        int bonusNum = validateInputBonusNum(input);
+        return bonusNum;
+    }
+
+    private List<Integer> validateInputWinningNum(String input) {
+        String[] nums = input.split(",");
+        int numberOfComma = nums.length - 1;
+        if (numberOfComma != 5) {
+            throw new IllegalArgumentException("5개의 숫자를 입력해주세요");
+        }
+        return ConvertStringToNum.toNum(nums);
+    }
+
+    private int validateInputBonusNum(String input) {
+        if (ConvertStringToNum.isNumeric(input)) {
+            return Integer.parseInt(input);
+        } else {
+            throw new IllegalArgumentException("올바른 보너스 숫자를 입력해주세요");
+        }
+    }
+}

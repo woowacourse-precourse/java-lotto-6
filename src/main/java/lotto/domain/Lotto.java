c4ceb503fa27f1414e69eda5ab7bdaa0dc0f9a58
SehyeonKang <gedfgrdt@naver.com>
2023-11-08 15:28:22 +0900

    feat: 로또 생성시 1~45 사이의 값인지 검증하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,11 +7,14 @@ import java.util.List;
 
 public class Lotto {
     private static final int LOTTO_NUMBERS_SIZE = 6;
+    private static final int LOTTO_NUMBERS_MINIMUM = 1;
+    private static final int LOTTO_NUMBERS_MAXIMUM = 45;
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validateNumbersCount(numbers);
+        validateNumberRange(numbers);
         validateDuplicateNumbers(numbers);
         this.numbers = numbers;
     }
@@ -19,6 +22,14 @@ public class Lotto {
     private void validateNumbersCount(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBERS_SIZE) {
             ExceptionMessage.LOTTO_WRONG_NUMBERS_COUNT.throwIllegalArgumentException();
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < LOTTO_NUMBERS_MINIMUM || number > LOTTO_NUMBERS_MAXIMUM) {
+                ExceptionMessage.LOTTO_WRONG_NUMBER_RANGE.throwIllegalArgumentException();
+            }
         }
     }
 

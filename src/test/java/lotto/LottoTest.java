19d358dc51d838cef18bc86325c1c73537e06091
SCY <helenason@naver.com>
2023-11-08 23:03:32 +0900

    test: Lotto 클래스 단위 테스트 코드 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -28,5 +29,26 @@ class LottoTest {
     void createLottoByOverRange() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("리스트 타입의 로또 번호를 정렬한 후 문자열 타입으로 보기 좋게 변환하여 반환한다.")
+    @Test
+    void convertLottoListToString() {
+        Lotto lotto = new Lotto(List.of(3, 4, 5, 1, 2, 6));
+
+        String lottoFormat = lotto.getNumbersToString();
+
+        assertThat(lottoFormat).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
+
+    @DisplayName("다른 로또와 일치하는 숫자의 개수를 센다.")
+    @Test
+    void countSameNumbersWithAnotherLotto() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto anotherLotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+
+        int sameNumbersCount = lotto.countSameNumbers(anotherLotto);
+
+        assertThat(sameNumbersCount).isEqualTo(3);
     }
 }

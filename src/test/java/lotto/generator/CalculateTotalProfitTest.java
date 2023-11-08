1a56c2d630105c91f1b74e251bd28f52c4d91664
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:25:50 +0900

    CalculateWinCounts 당첨번호 확인과 wincounts 리스트 값 확인

--- src/test/java/lotto/generator/CalculateTotalProfitTest.java
+++ src/test/java/lotto/generator/CalculateTotalProfitTest.java
@@ -1,7 +1,32 @@
 package lotto.generator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-class CalculateTotalProfitTest {
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculateTotalProfitTest {
+    @DisplayName("총 수익을 올바르게 계산한다.")
+    @Test
+    void testCalculateTotalProfit() {
+        List<Integer> winCounts = List.of(0, 2, 3, 1, 0, 4); // 당첨 번호 개수
+        int t = 10; // 투자 금액
+
+        int totalProfit = CalculateTotalProfit.calculateTotalProfit(winCounts, t);
+
+        assertThat(totalProfit).isEqualTo(55000);
+    }
+
+    @DisplayName("모든 당첨 번호 개수가 0이면 총 수익은 0이다.")
+    @Test
+    void testCalculateTotalProfit_AllZeroWinCounts() {
+        List<Integer> winCounts = List.of(0, 0, 0, 0, 0, 0); // 모든 당첨 번호 개수가 0
+        int t = 10; // 투자 금액
+
+        int totalProfit = CalculateTotalProfit.calculateTotalProfit(winCounts, t);
+
+        assertThat(totalProfit).isEqualTo(0);
+    }
 }

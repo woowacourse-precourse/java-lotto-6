d629fa2e828ce609c1a91113310e1fb4d8e5a920
ljkhyeong <jolri24@gmail.com>
2023-11-08 14:07:07 +0900

    test: LottoResultTest 예외처리 테스트 추가

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -1,18 +1,19 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoResultTest {
 
-
 	@DisplayName("로또 결과 생성 - 1개의 일치, 보너스 번호 없음")
 	@Test
 	void createLottoResultTest0() {
 		LottoResult lottoResult = new LottoResult(1, false);
 
-		Assertions.assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FAIL);
+		assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FAIL);
 	}
 
 	@DisplayName("로또 결과 생성 - 3개의 일치, 보너스 번호 없음")
@@ -20,7 +21,7 @@ public class LottoResultTest {
 	void createLottoResultTest1() {
 		LottoResult lottoResult = new LottoResult(3, false);
 
-		Assertions.assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FIFTH);
+		assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FIFTH);
 	}
 
 	@DisplayName("로또 결과 생성 - 3개의 일치, 보너스 번호 있음")
@@ -28,7 +29,7 @@ public class LottoResultTest {
 	void createLottoResultTest1_2() {
 		LottoResult lottoResult = new LottoResult(3, true);
 
-		Assertions.assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FIFTH);
+		assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.FIFTH);
 	}
 
 	@DisplayName("로또 결과 생성 - 5개의 일치, 보너스 번호 없음")
@@ -36,7 +37,7 @@ public class LottoResultTest {
 	void createLottoResultTest3() {
 		LottoResult lottoResult = new LottoResult(5, false);
 
-		Assertions.assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.THIRD);
+		assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.THIRD);
 	}
 
 	@DisplayName("로또 결과 생성 - 5개의 일치, 보너스 번호 있음")
@@ -44,8 +45,21 @@ public class LottoResultTest {
 	void createLottoResultTest4() {
 		LottoResult lottoResult = new LottoResult(5, true);
 
-		Assertions.assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.SECOND);
+		assertThat(lottoResult.getWinningRank()).isEqualTo(WinningRank.SECOND);
 	}
 
+	@DisplayName("로또 결과 생성 중 예외 발생 - -1개의 일치")
+	@Test
+	void createLottoResultByMinusMatchCount() {
+		assertThatThrownBy(() -> new LottoResult(-1, false))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("로또 결과 생성 중 예외 발생 - 6개의 일치와 보너스 번호 일치")
+	@Test
+	void createLottoResultBy6MatchCountAndMatchBonusNumber() {
+		assertThatThrownBy(() -> new LottoResult(6, true))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
 }

fcfd5230b7f1788a4b41f7bb0917e808a4b3cae4
sihyunjojo <si4018@naver.com>
2023-11-08 13:39:45 +0900

    test: InputUtil에 메서드에 대한 정상작동 테스트 구현

--- src/test/java/lotto/util/InputUtilTest.java
+++ src/test/java/lotto/util/InputUtilTest.java
@@ -2,6 +2,8 @@ package lotto.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,7 +12,7 @@ class InputUtilTest {
 
     @Test
     @DisplayName("정상적인 숫자가 들어왔을 때, int 값으로 반환을 한다.")
-    void testParseIntWithValidInput() {
+    void testParseInt() {
         // given
         String input = "123";
 
@@ -18,8 +20,36 @@ class InputUtilTest {
         int result = InputUtil.parseInt(input);
 
         // then
-        assertEquals(123, result);
+        int expectedResult = 123;
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    @DisplayName("정상적인 숫자가 들어왔을 때, int 값으로 반환을 한다.")
+    void testParseNums() {
+        // given
+        String input = "1,2,3,4,5,6";
+
+        // when
+        List<Integer> result = InputUtil.parseNums(input);
+
+        // then
+        List<Integer> expectedResult = List.of(1,2,3,4,5,6);
+        assertEquals(expectedResult, result);
     }
 
 
+    @Test
+    @DisplayName("(,)와 함께 입력이 들어오면 , 를 기준으로 나눠줘야한다.")
+    void testSplitInput() {
+        // given
+        String input = "1,2,3,4,5,6";
+
+        // when
+        List<String> result = InputUtil.splitInput(input);
+
+        // then
+        List<String> expectedResult = List.of("1", "2", "3", "4", "5", "6");
+        assertEquals(expectedResult, result);
+    }
 }

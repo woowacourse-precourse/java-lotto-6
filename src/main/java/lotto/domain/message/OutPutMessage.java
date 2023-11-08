829b23640ae1998d0a3e90901868b1220a35ea19
syy0O <86238720+syy0O@users.noreply.github.com>
2023-11-08 19:34:52 +0900

    refactor: output 메시지 수정

--- src/main/java/lotto/domain/message/OutPutMessage.java
+++ src/main/java/lotto/domain/message/OutPutMessage.java
@@ -2,8 +2,7 @@ package lotto.domain.message;
 
 public enum OutPutMessage {
     BUYING_COUNT_FORMAT("%d개를 구매했습니다."),
-    WINNING_STATISTICS_TITLE("당첨 통계\n---"),
-    WINNING_RESULT("%s (%,d원) - %d개"),
+    WINNING_STATISTICS_MESSAGE("당첨 통계\n---"),
     WINNING_STATISTICS("총 수익률은 %.1f%%입니다.");
 
     private String value;

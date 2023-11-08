70a9143829d094fdeca42de033929976ed7ee7df
JiEung2 <https://github.com/JiEung2>
2023-11-08 14:36:42 +0900

    Feat: 의존성 주입 및 복권 결과를 알려주는 기능 추가

--- src/main/java/lotto/Lottos.java
+++ src/main/java/lotto/Lottos.java
@@ -1,17 +1,18 @@
 package lotto;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import lotto.model.RevenueCalculator;
 import lotto.model.WinningStatistics;
 import lotto.view.OutputView;
 
 public class Lottos {
     private final List<Lotto> lottos;
     OutputView outputView = new OutputView();
+    RevenueCalculator revenueCalculator = new RevenueCalculator();
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
@@ -32,7 +33,6 @@ public class Lottos {
     public Map<WinningStatistics, Long> getWinningStatistics(List<Integer> winningNumber, int bonusNumber) {
         List<Integer> matchingCounts = checkWinning(winningNumber);
         List<Boolean> containBonusNumber = checkBonusNumber(bonusNumber);
-        Map<WinningStatistics, Integer> result = new HashMap<>();
         return IntStream.range(0, matchingCounts.size())
                 .mapToObj(i -> WinningStatistics.getStatistic(matchingCounts.get(i), containBonusNumber.get(i)))
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
@@ -49,5 +49,17 @@ public class Lottos {
 
     public void getLottoListSize() {
         outputView.printLottoCount(lottos.size());
+    }
+
+    public void getLotto() {
+        getLottoListSize();
+        getLottoList();
+    }
+
+    public void announceWinning(List<Integer> winningNumber, int bonusNumber, int buyPrice) {
+        Map<WinningStatistics, Long> winningStatistics = getWinningStatistics(winningNumber, bonusNumber);
+        outputView.printWinningStatisticMessage();
+        outputView.printWinningStatistic(winningStatistics);
+        outputView.printEarningRate(revenueCalculator.calculateEarningRate(winningStatistics, buyPrice));
     }
 }

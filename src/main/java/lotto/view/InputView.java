d6dbfc642128d66880870c6fd6d72c680545ec42
Woongbae Yun <devbelly@naver.com>
2023-11-08 10:50:20 +0900

    feat : 정수를 읽는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,7 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INVALID_INPUT_ERROR = "[ERROR] 입력을 다시 확인해주세요.";
+
     public static String readLine() {
         return Console.readLine();
+    }
+
+    public static Integer readInteger() {
+        try {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_ERROR);
+        }
     }
 }

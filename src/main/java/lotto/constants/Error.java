7f604dabe33adc86588ab5a99d9f3ca2c07ee59c
Park Eunhyeong <silbroeh@gmail.com>
2023-11-08 21:06:54 +0900

    fix : 에러메시지 오류 수정

--- src/main/java/lotto/constants/Error.java
+++ src/main/java/lotto/constants/Error.java
@@ -8,7 +8,7 @@ public enum Error {
     CONTINUAL_COMMA("콤마(,)를 연속해서 입력할 수 없습니다. 한 개씩만 입력해주세요."),
     COMMA_EDGE("양끝에 콤마(,)를 입력할 수 없습니다. 양끝에는 숫자를 입력해주세요."),
     NOT_PLUS_SIGN("0 또는 음수를 입력할 수 없습니다. 양수를 입력해주세요."),
-    NOT_UNIT(String.format("%d원으로 나누어 떨어지지 않는 수를 입력할 수 없습니다. %d원 단위의 수를 입력해주세요", LOTTO_PRICE, LOTTO_PRICE)),
+    NOT_UNIT(String.format("%d원으로 나누어 떨어지지 않는 수를 입력할 수 없습니다. %d원 단위의 수를 입력해주세요.", LOTTO_PRICE, LOTTO_PRICE)),
     NOT_IN_RANGE(String.format("로또 번호 범위 외의 수를 포함할 수 없습니다. %d이상 %d이하의 수만 입력해주세요.", MIN_NUMBER, MAX_NUMBER)),
     DUPLICATES("로또/보너스 번호는 서로 중복될 수 없습니다. 서로 중복되지 않게 입력해주세요."),
     WRONG_SIZE(String.format("로또 번호의 개수가 %d개가 아닙니다. %d개의 번호를 입력해주세요.", LOTTO_SIZE, LOTTO_SIZE));

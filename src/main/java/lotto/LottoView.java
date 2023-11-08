ca80b2fe776f1d76fd47b6ba654b3f3297ebccb9
ks00919 <ks00919@naver.com>
2023-11-08 22:20:45 +0900

    feat: 로또 출력 메소드 구현

---
+++ src/main/java/lotto/LottoView.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class LottoView {
+
+    public static void printOf(Lotto... lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+}

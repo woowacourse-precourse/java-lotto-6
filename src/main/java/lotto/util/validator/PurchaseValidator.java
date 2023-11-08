3f5901e2d424204cfab67f797271c7b47ccb6ae8
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 17:13:45 +0900

    feat(PurchaseValidator): Purchase Amount 단위 검증 구현

--- src/main/java/lotto/util/validator/PurchaseValidator.java
+++ src/main/java/lotto/util/validator/PurchaseValidator.java
@@ -3,6 +3,15 @@ package lotto.util.validator;
 import lotto.util.exception.ErrorCode;
 
 public class PurchaseValidator {
+    public long validatePurchaseAmount(String inputPurchaseAmount) {
+        validateNullorEmpty(inputPurchaseAmount);
+
+        long amount = validateNumberType(inputPurchaseAmount);
+        validateAmountPositive(amount);
+        validateAmountFormate(amount);
+        return amount;
+    }
+
     private void validateNullorEmpty(String inputPurchaseAmount) {
         if (inputPurchaseAmount == null || inputPurchaseAmount.trim().isEmpty()) {
             throw new IllegalArgumentException(ErrorCode.NULL_OR_EMPTY_PURCHASE_AMOUNT.getMessage());
@@ -20,6 +29,12 @@ public class PurchaseValidator {
     private void validateAmountPositive(long amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException(ErrorCode.INVALID_PURCHASE_AMOUNT.getMessage());
+        }
+    }
+
+    private void validateAmountFormate(long amount) {
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_PURCHASE_AMOUNT_FORMAT.getMessage());
         }
     }
 }

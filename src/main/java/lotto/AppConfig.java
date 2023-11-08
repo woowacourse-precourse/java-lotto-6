54d083b4d8a436d6dda25bb7e572fd29efe7189e
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 20:00:54 +0900

    refactor: output 뷰 분리에 따른 의존성 주입

--- src/main/java/lotto/AppConfig.java
+++ src/main/java/lotto/AppConfig.java
@@ -5,6 +5,9 @@ import lotto.service.generator.LottoGenerator;
 import lotto.service.LottoService;
 import lotto.view.input.LottoInputView;
 import lotto.view.output.LottoOutputView;
+import lotto.view.output.component.NoticeComponent;
+import lotto.view.output.component.PrintAllLottoComponent;
+import lotto.view.output.component.PrintResultComponent;
 
 public class AppConfig {
 
@@ -31,7 +34,23 @@ public class AppConfig {
     }
 
     private static LottoOutputView getLottoOutputView() {
-        return new LottoOutputView();
+        return new LottoOutputView(
+                getNoticeComponent(),
+                getPrintAllLottoComponent(),
+                getPrintResultComponent()
+        );
+    }
+
+    private static PrintResultComponent getPrintResultComponent() {
+        return new PrintResultComponent();
+    }
+
+    private static PrintAllLottoComponent getPrintAllLottoComponent() {
+        return new PrintAllLottoComponent();
+    }
+
+    private static NoticeComponent getNoticeComponent() {
+        return new NoticeComponent();
     }
 
     private static LottoInputView getLottoInputView() {

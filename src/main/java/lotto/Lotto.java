fedda0e82a27e22013be3d04d6ef4d22959499f0
Hwangwoosam <sam1783@naver.com>
2023-11-08 13:26:56 +0900

    리팩토링(): 로또 생성자 코드 변경
    로또 생성자 validate()과정에서 에러메세지 출력을 위한 인자 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -5,18 +5,20 @@ import java.util.*;
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
+    public Lotto(List<Integer> numbers,String[] errorCode) {
+        validate(numbers,errorCode);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<Integer> numbers,String[] errorCode) {
         if (numbers.size() != 6) {
+            errorCode[0] = "[ERROR]번호의 개수는 6개 이어야 합니다.";
             throw new IllegalArgumentException();
         }
 
         Set<Integer> uniqueNumber = new HashSet<>(numbers);
         if(uniqueNumber.size() != numbers.size()){
+            errorCode[0] = "[ERROR]번호는 서로 중복되지 않아야 합니다.";
             throw new IllegalArgumentException();
         }
     }
@@ -31,20 +33,14 @@ public class Lotto {
 
     public int compareNumber(Lotto catchNumbers,int bonusNumber){
         int cnt = 0;
-
         for(int i = 0; i < 6; i++){
             if(numbers.contains(catchNumbers.getElement(i))){
                 cnt++;
             }
         }
 
-        if(cnt == 6){
-            cnt++;
-        }
-
-        if(numbers.contains(bonusNumber) && cnt == 5){
-            cnt++;
-        }
+        if(cnt == 6) cnt++;
+        if(numbers.contains(bonusNumber) && cnt == 5) cnt++;
 
         return cnt;
     }

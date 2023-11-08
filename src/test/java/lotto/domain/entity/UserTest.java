0b5d3f3ffb372e34d5818d45881aeba2da697f5b
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 17:20:20 +0900

    test: 구매 수량과 수익률 계산 메서드 각각 테스트 코드 작성

--- src/test/java/lotto/domain/entity/UserTest.java
+++ src/test/java/lotto/domain/entity/UserTest.java
@@ -1,13 +1,13 @@
 package lotto.domain.entity;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class UserTest {
     @DisplayName("구매 금액이 1,000단위가 아니면 예외가 발생한다.")
@@ -38,5 +38,30 @@ class UserTest {
         return new Lottos(List.of(lotto));
     }
 
+    @DisplayName("구매 금액에 따른 로또 구매 수량 계산 성공")
+    @Test
+    void calculatePurchaseAmount() throws  Exception {
+        //given
+        long purchasePrice = 10_000L;
+        long expected = 10L;
+        //when
+        Long result = User.calculatePurchaseAmount(purchasePrice);
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("상금이 주어졌을 때 수익률 계산 성공")
+    @Test
+    void calculateTotalEarning() throws  Exception {
+        //given
+        long totalPrize = 111_000L;
+        long purchasePrice = 10_000L;
+        User user = new User(getLottos(), purchasePrice);
+        float expected = (float) (totalPrize * 100) / purchasePrice;
+        //when
+        float result = user.calculateEarningRate(totalPrize);
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
 
 }

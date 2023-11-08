99c6f61ed4cab96a6489d7be861666da5d6744ac
sinminseok12 <sinminseok12@naver.com>
2023-11-08 15:25:01 +0900

    test(LottoTicket): 당첨로또와 비교해 등수를 반환하는 기능 테스트 구현

--- src/test/java/lotto/model/LottoTicketTest.java
+++ src/test/java/lotto/model/LottoTicketTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,17 +10,31 @@ import java.util.stream.IntStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketTest {
+    List<Lotto> lottos;
+
+    @BeforeEach
+    void setInit() {
+        lottos = IntStream.range(0, 5)
+                .mapToObj(i -> Lotto.from(List.of(1, 2, 3, 4, 5, 6)))
+                .collect(Collectors.toList());
+    }
 
     @Test
     void 전달받은_로또_갯수만큼_저장한다() {
-        //given
-        int lottoCount = 5;
-        List<Lotto> collect = IntStream.range(0, lottoCount)
-                .mapToObj(i -> Lotto.from(List.of(1, 2, 3, 4, 5, 6)))
-                .collect(Collectors.toList());
         //when
-        LottoTicket lottoTicket = new LottoTicket(collect);
+        LottoTicket lottoTicket = new LottoTicket(lottos);
         //then
-        assertThat(lottoTicket.getLottos().size()).isEqualTo(lottoCount);
+        assertThat(lottoTicket.getLottos().size()).isEqualTo(5);
+    }
+
+    @Test
+    void checkRankings_메서드는_당첨_로또와_비교해_등수를_반환한다() {
+        //given
+        LottoTicket lottoTicket = new LottoTicket(lottos);
+        WinLotto winLotto = new WinLotto(Lotto.from(List.of(1, 2, 3, 20, 21, 22)), new LottoNumber(23));
+        //when
+        List<Ranking> rankings = lottoTicket.checkRankings(winLotto);
+        //then
+        assertThat(rankings).containsExactly(Ranking.FIFTH, Ranking.FIFTH, Ranking.FIFTH, Ranking.FIFTH, Ranking.FIFTH);
     }
 }

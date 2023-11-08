d0860c82b9cf36154e8420888d5b10986cd31ec1
ycis5013@naver.com <ycis5013@naver.com>
2023-11-08 17:14:15 +0900

    feat(Committee) : 당첨 내역 계산하는 함수 calculateResult() 작성

--- src/main/java/lotto/Committee.java
+++ src/main/java/lotto/Committee.java
@@ -3,6 +3,7 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Set;
 
 public class Committee {
     private static Committee committee = new Committee();
@@ -15,7 +16,28 @@ public class Committee {
         return committee;
     }
 
-    public List<Integer> setWinningNum() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    public int[] calculateResult(List<Integer>[] issueNumbers, Lotto lotto, int bonusNumber) {
+        int sameNumberCount[] = new int[8];
+
+        for (int i = 0; i < issueNumbers.length; i++) {
+            int count = checkSameNumbers(issueNumbers, lotto, i);
+            sameNumberCount[count]++;
+            if (issueNumbers[i].contains(bonusNumber) && count == 5) {
+                sameNumberCount[7]++; //5개 일치, 보너스 볼 일치하는 경우
+                sameNumberCount[5]--;
+            }
+        }
+        return sameNumberCount;
     }
+
+    private int checkSameNumbers(List<Integer>[] issueNumbers, Lotto lotto, int i) {
+        int count = 0;
+        for (int j = 0; j < 6; j++) {
+            if (lotto.containsNum(issueNumbers[i].get(j))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }

67c46e16ab828ba6d0c0bc50577192d5a1dba101
dong_hy <kimadonghyon99@gmail.com>
2023-11-08 20:01:42 +0900

    test : Lotto클래스의 메서드 테스트케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,15 +1,25 @@
 package lotto;
 
+import java.util.Arrays;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    private Lotto lotto;
+    @BeforeEach
+    void before(){
+        lotto = new Lotto(Arrays.asList(1,2,3,4,5,6));
+    }
+
     @Test
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -23,5 +33,20 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+
+    @Test
+    @DisplayName("일치하는 로또의 번호가 몇개인지 리턴")
+    void countMatchTest(){
+        assertThat(
+                lotto.countMatch(Arrays.asList(1,2,3,4,5,6))
+        ).isEqualTo(6);
+    }
+
+    @Test
+    @DisplayName("보너스 번호와 일치하는 번호가있는지 확인")
+    void bonusMatchTest(){
+        assertThat(
+                lotto.bonusMatch(5)
+        ).isEqualTo(true);
+    }
 }

47fb2f4f8546849a36d84779c78d995470c82d65
1015s <1015_s@naver.com>
2023-11-08 02:58:07 -0800

    feat(OutputView) : 컴퓨터의 출력 보여주는 클래스 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,34 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import lotto.domain.Lotto;
+import lotto.domain.Ranking;
+
+import java.util.*;
+
+public class OutputView {
+    private static final String PURCHASED_QUANTITY_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_RESULT_TITLE_MESSAGE = "당첨 통계\n---";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %s%%입니다.";
+
+    public static void printPurchasedQuantity(int quantity){
+        System.out.printf(PURCHASED_QUANTITY_MESSAGE, quantity);
+    }
+
+    public static void printPurchasedLottos(List<Lotto> purchasedLottos){
+        for(Lotto lotto : purchasedLottos){
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void printWinningResult(HashMap<Ranking, Integer> winningResult){
+        System.out.println(WINNING_RESULT_TITLE_MESSAGE);
+
+        for (Ranking ranking : Ranking.getValues()){
+            System.out.println(ranking.getMessage(winningResult.get(ranking)));
+        }
+    }
+
+    public static void printRateOfReturn(String rateOfReturn){
+        System.out.printf(RATE_OF_RETURN_MESSAGE, rateOfReturn);
+    }
 }

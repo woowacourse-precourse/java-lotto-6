e0ff3f3581d65128ef1502ae0b5d33b6a2ae372d
yyubin <hazing120@gmail.com>
2023-11-08 14:08:51 +0900

    feat: 메세지 정의 및 출력 기능 추가

---
+++ src/main/java/lotto/view/Messages.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+public class Messages {
+    public static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요: \n";
+    public static final String PURCHASE_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    public static final String LOTTO_NUMBERS_HEADER = "당첨 번호를 입력해 주세요.\n";
+    public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.\n";
+    public static final String WINNING_STATISTICS_HEADER = "당첨 통계\n---\n";
+    public static final String WINNING_MESSAGE_FORMAT = "%s (%s원) - %d개\n";
+    public static final String TOTAL_PROFIT_MESSAGE_FORMAT = "총 수익률은 %.2f%%입니다.\n";
+
+    public static final String LOTTO_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호는 %d부터 %d 사이의 숫자여야 합니다.\n";
+    public static final String LOTTO_NUMBER_SIZE_ERROR_MESSAGE = "[ERROR] 로또는 총 6개의 숫자여야 합니다.\n";
+    public static final String LOTTO_STATE_ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요.\n";
+    public static final String LOTTO_BONUS_DUPLICATE_MESSAGE = "[ERROR] 보너스 번호는 이미 당첨 번호에 포함되어 있습니다.\n";
+    public static final String LOTTO_PRICE_ERROR_MESSAGE = "[ERROR] 로또 구입 금액은 로또 금액보다 높아야 합니다.\n";
+}

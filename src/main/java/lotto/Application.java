f788d0951596e7b5cfa22c479e1f70d2e8534ff6
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-08 19:57:20 +0900

    feat: add issuing lotto process
    Add the feature to purchase and issue lotto tickets for the amount

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+
 public class Application {
     public static void main(String[] args) {
-
+        LottoProcess lottoProcess = new LottoProcess();
+        lottoProcess.setNumLotto();
     }
 }
--- src/main/java/lotto/LottoProcess.java
+++ src/main/java/lotto/LottoProcess.java
@@ -1,14 +1,41 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class LottoProcess {
-    private int numLotto;
-
+    private static final int numLottoNum = 6;
+    private int quantityLotto;
+    private Lotto[] lottos;
+    
     public void setNumLotto() {
-        numLotto = new BuyLotto().BuyLotto();
+        quantityLotto = new BuyLotto().BuyLotto();
+        printLotto();
+    }
 
+    public void printLotto() {
+        System.out.println("\n" + quantityLotto + "개를 구매했습니다.");
+        issueLotto();
+    }
 
+    public void issueLotto() {
+        lottos = new Lotto[numLottoNum];
+        for (int i = 0; i < quantityLotto; i++) {
+            List<Integer> lottoNumbers = generateLottoNum();
+            System.out.println(lottoNumbers);
+            lottos[i] = new Lotto(lottoNumbers);
+        }
+    }
+
+    public List<Integer> generateLottoNum() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
     }
 
 }

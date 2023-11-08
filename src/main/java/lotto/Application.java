8c8526e8cd379385b9fdcc9a3c8054b0de2dd03b
박승민 <tmddudals369@naver.com>
2023-11-08 17:48:22 +0900

    feat: 로또 게임 수행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+import lotto.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View(new InputView(), new OutputView());
+        LottoController lottoController = new LottoController(new LottoService(), view);
+        lottoController.run();
     }
 }

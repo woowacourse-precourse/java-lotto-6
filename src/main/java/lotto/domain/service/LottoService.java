e5b8bf518886ec3cfa9b07b836147ad7eb6556ab
Junho Lee <ghzm888@gmail.com>
2023-11-08 19:51:20 +0900

    refactor : 직관적으로 보이도록 WinningPrizeType 에서 받아와 사용되도록 수정

--- src/main/java/lotto/domain/service/LottoService.java
+++ src/main/java/lotto/domain/service/LottoService.java
@@ -1,9 +1,10 @@
 package lotto.domain.service;
 
 import lotto.domain.*;
-import lotto.global.utils.console.ConsoleUtil;
 import lotto.global.utils.constant.OutputType;
+import lotto.global.utils.constant.WinningPrizeType;
 import lotto.global.utils.generator.RandomNumberGenerator;
+import lotto.global.utils.view.OutputView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,15 +12,19 @@ import java.util.List;
 
 public class LottoService {
 
+    private static final double LOTTO_PRICE = 1000.0;
+    private static final int PERCENTAGE_VALUE = 100;
+    private static final double MULTIPLIER = 10.0;
+
     public AllOfRandomLottoList getLottoList(int lottoCount) {
         AllOfRandomLottoList allOfRandomLottoList = new AllOfRandomLottoList();
-        ConsoleUtil.commonOutputLine(lottoCount + OutputType.OUTPUT_NUMBER_OF_PURCHASES.getComment());
+        OutputView.commonOutputLine(lottoCount + OutputType.OUTPUT_NUMBER_OF_PURCHASES.getComment());
 
         for (int index = 0; index < lottoCount; index++) {
             RandomLottoList randomLottoList = new RandomLottoList(new RandomNumberGenerator());
             allOfRandomLottoList.increaseLottoCount();
             allOfRandomLottoList.addRandomLottoList(randomLottoList);
-            ConsoleUtil.outputRandomList(randomLottoList.getRandomLottoList());
+            OutputView.outputRandomList(randomLottoList.getRandomLottoList());
         }
         return allOfRandomLottoList;
     }
@@ -27,27 +32,20 @@ public class LottoService {
     public void outputCorrectLottoList(AllOfRandomLottoList allOfRandomLottoList, Lotto inputLotto, BonusNumber bonusNumber) {
         CorrectLottoList correctLottoList = getCorrectLottoList(allOfRandomLottoList, inputLotto, bonusNumber);
         List<Integer> correctCountList = getCorrectCountList(correctLottoList);
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS_5.getComment()
-                + correctCountList.get(4) + OutputType.OUTPUT_COUNT.getComment());
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS_4.getComment()
-                + correctCountList.get(3) + OutputType.OUTPUT_COUNT.getComment());
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS_3.getComment()
-                + correctCountList.get(2) + OutputType.OUTPUT_COUNT.getComment());
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS_2.getComment()
-                + correctCountList.get(1) + OutputType.OUTPUT_COUNT.getComment());
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS_1.getComment()
-                + correctCountList.get(0) + OutputType.OUTPUT_COUNT.getComment());
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_RESULT_1.getComment() + getRateOfReturn(correctCountList,
-                allOfRandomLottoList.getLottoCount()) + OutputType.OUTPUT_RESULT_2.getComment());
+        OutputView.outputStatistics(correctCountList);
+        double rateOfReturn = getRateOfReturn(correctCountList, allOfRandomLottoList.getLottoCount());
+        OutputView.outputRateOfReturn(rateOfReturn);
     }
 
     private double getRateOfReturn(List<Integer> correctCountList, int lottoCount) {
         int winningPrice = 0;
-        winningPrice += (correctCountList.get(0) * 2000000000 + correctCountList.get(1) * 30000000
-                + correctCountList.get(2) * 1500000 + correctCountList.get(3) * 50000
-                + correctCountList.get(4) * 5000);
-        double rateBeforeRound = (winningPrice / (lottoCount * 1000.0)) * 100.0;
-        return Math.round(rateBeforeRound * 10.0) / 10.0;
+        winningPrice += (correctCountList.get(0) * WinningPrizeType.FIRST.getPrizePrice()
+                + correctCountList.get(1) * WinningPrizeType.SECOND.getPrizePrice()
+                + correctCountList.get(2) * WinningPrizeType.THIRD.getPrizePrice()
+                + correctCountList.get(3) * WinningPrizeType.FORTH.getPrizePrice()
+                + correctCountList.get(4) * WinningPrizeType.FIFTH.getPrizePrice());
+        double rateBeforeRound = (winningPrice / (lottoCount * LOTTO_PRICE)) * PERCENTAGE_VALUE;
+        return Math.round(rateBeforeRound * MULTIPLIER) / MULTIPLIER;
     }
 
     private List<Integer> getCorrectCountList(CorrectLottoList correctLottoList) {
@@ -61,19 +59,19 @@ public class LottoService {
     }
 
     private void updateCorrectCountList(List<Integer> correctCountList, int correctCount, int correctBonusCount) {
-        if (correctCount == 6) {
+        if (correctCount == WinningPrizeType.FIRST.getCorrectCount()) {
             correctCountList.set(0, correctCountList.get(0) + 1);
         }
-        if (correctCount == 5 && correctBonusCount == 1) {
+        if (correctCount == WinningPrizeType.SECOND.getCorrectCount() && correctBonusCount == 1) {
             correctCountList.set(1, correctCountList.get(1) + 1);
         }
-        if (correctCount == 5 && correctBonusCount == 0) {
+        if (correctCount == WinningPrizeType.THIRD.getCorrectCount() && correctBonusCount == 0) {
             correctCountList.set(2, correctCountList.get(2) + 1);
         }
-        if (correctCount == 4) {
+        if (correctCount == WinningPrizeType.FORTH.getCorrectCount()) {
             correctCountList.set(3, correctCountList.get(3) + 1);
         }
-        if (correctCount == 3) {
+        if (correctCount == WinningPrizeType.FIFTH.getCorrectCount()) {
             correctCountList.set(4, correctCountList.get(4) + 1);
         }
     }
@@ -81,7 +79,7 @@ public class LottoService {
     private CorrectLottoList getCorrectLottoList(AllOfRandomLottoList allOfRandomLottoList, Lotto inputLotto, BonusNumber bonusNumber) {
         CorrectLottoList correctLottoList = new CorrectLottoList();
         int lottoCount = allOfRandomLottoList.getLottoCount();
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS.getComment());
+        OutputView.commonOutputLine(OutputType.OUTPUT_WINNING_STATISTICS.getComment());
 
         for (int index = 0; index < lottoCount; index++) {
             RandomLottoList randomLottoList = allOfRandomLottoList.getAllOfRandomLottoList().get(index);

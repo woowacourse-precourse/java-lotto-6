dc46c5d36793944b2c028e9ddafe05410ac53bff
Han <yulmam1@naver.com>
2023-11-08 15:49:26 +0900

    feat(Money) : 돈이 1000원보다 적게 입력되는 경우 예외처리 하는 기능 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -19,6 +19,16 @@ public class Money {
     }
 
     private void validate(int money) {
+        validateMoneyDevided(money);
+        validateMoneyMinimum(money);
+    }
+
+    private void validateMoneyMinimum(int money){
+        if(money < LOTTO_PRICE){
+            throw new IllegalArgumentException("[ERROR] 돈은 최소 1000원입니다.");
+        }
+    }
+    private void validateMoneyDevided(int money){
         if(0 != money % LOTTO_PRICE){
             throw new IllegalArgumentException("[ERROR] 돈의 단위는 1000원입니다.");
         }

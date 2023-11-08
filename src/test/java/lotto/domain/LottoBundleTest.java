e72d5cb1664cea01fee7b8b60769e2ee68031f6e
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-11-08 19:47:59 +0900

    test : LottoBundleTest 로또 뭉치의 순위를 알려주는 기능 테스트 추가

--- src/test/java/lotto/domain/LottoBundleTest.java
+++ src/test/java/lotto/domain/LottoBundleTest.java
@@ -1,11 +1,18 @@
 package lotto.domain;
 
+import static lotto.domain.Rank.FIFTH;
+import static lotto.domain.Rank.FIRST;
+import static lotto.domain.Rank.FOURTH;
+import static lotto.domain.Rank.SECOND;
+import static lotto.domain.Rank.THIRD;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -48,8 +55,34 @@ public class LottoBundleTest {
     }
 
     @Test
+    @DisplayName("로또 뭉치의 순위를 확인할 수 있다.")
+    void check_rankings() {
+        // given
+        LottoBundle lottoBundle = new LottoBundle(createNewLotteries());
+        Lotto winnerLotto = new Lotto(Arrays.asList(3, 5, 7, 9, 14, 43));
+        Bonus bonus = new Bonus(winnerLotto, 6);
+
+        // when
+        Map<Rank, Integer> winningLottoTable = lottoBundle.checkRankings(winnerLotto, bonus);
+
+
+        // then
+        assertThat(winningLottoTable).isEqualTo(createResultTable());
+    }
+
+    private Map<Rank, Integer> createResultTable() {
+        Map<Rank, Integer> rankTable = new HashMap<>();
+        rankTable.put(FIFTH, 0);
+        rankTable.put(FOURTH, 0);
+        rankTable.put(THIRD, 0);
+        rankTable.put(SECOND, 0);
+        rankTable.put(FIRST, 1);
+        return rankTable;
+    }
+
+    @Test
     @DisplayName("로또 뭉치의 사이즈를 알려준다.")
-    void size(){
+    void size() {
         // given
         LottoBundle lottoBundle = new LottoBundle(createNewLotteries());
 

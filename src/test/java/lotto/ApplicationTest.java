aea71cf1f3fe05b027589209a4fb12cb12a53afa
ãJunChangM <jsjs1209@g.skku.edu>
2023-11-08 11:43:49 +0900

    Test: add exception case

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -55,12 +55,75 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 예외_테스트_잘못된입력_로또번호() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1 2 3 4 5 6", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "1개를 구매했습니다.",
+                            "[1, 2, 3, 41, 42, 43]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 500.0%입니다."
+                    );
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                },
+                List.of(1, 2, 3, 41, 42, 43)
+        );
+    }
+
+    @Test
+    void 예외_테스트_잘못된입력_보너스번호() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "a", "7");
+                    assertThat(output()).contains(
+                            "1개를 구매했습니다.",
+                            "[1, 2, 3, 41, 42, 43]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 500.0%입니다."
+                    );
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                },
+                List.of(1, 2, 3, 41, 42, 43)
+        );
+    }
+
+    @Test
+    void 예외_테스트_오름차순생성() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("1000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "1개를 구매했습니다.",
+                            "[1, 2, 3, 41, 42, 43]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 0개",
+                            "5개 일치 (1,500,000원) - 0개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
+                            "6개 일치 (2,000,000,000원) - 0개",
+                            "총 수익률은 500.0%입니다."
+                    );
+                },
+                List.of(3, 2, 41, 1, 42, 43)
+        );
+    }
+
+    @Test
     void 예외_테스트_음수값() {
         assertSimpleTest(() -> {
             runException("-1000");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }
+
 
     @Test
     void 기능_테스트_큰수() {

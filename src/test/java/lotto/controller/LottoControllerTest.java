2d87ff029ad0d3ce268f474ca079ebff9953fc1b
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-08 23:58:57 +0900

    test: 주석 추가

--- src/test/java/lotto/controller/LottoControllerTest.java
+++ src/test/java/lotto/controller/LottoControllerTest.java
@@ -7,39 +7,51 @@ import lotto.Application;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LottoControllerTest extends NsTest {
-    @DisplayName("잘못된 LottoCount를 입력했을 때 메시지를 출력하고, 재입력받는지 확인")
+class LottoControllerTest extends NsTest {
+    @DisplayName("잘못된 LottoCount를 입력했을 때 메시지를 출력하고, 재입력받는지 확인한다.")
     @Test
     void printErrorMessageAndReInputLottoCount() {
+        //given
+        //when
         runException("8000j", "8000k", "8000k", "8000k");
+        //then
         assertThat(output()).contains("[ERROR]");
     }
 
-    @DisplayName("잘못된 Lotto를 입력했을 때 메시지를 출력하고, 재입력받는지 확인")
+    @DisplayName("잘못된 Lotto를 입력했을 때 메시지를 출력하고, 재입력받는지 확인한다.")
     @Test
     void printErrorMessageAndReInputLotto() {
+        //given
+        //when
         runException("8000",
                 "1,2", "111,1,1,1,1,1", "1,2,2,3,4,5", "", " ");
+        //then
         assertThat(output()).contains("[ERROR]");
     }
 
-    @DisplayName("잘못된 Bonus를 입력했을 때 메시지를 출력하고, 재입력받는지 확인")
+    @DisplayName("잘못된 Bonus를 입력했을 때 메시지를 출력하고, 재입력받는지 확인한다.")
     @Test
     void printErrorMessageAndReInputBonus() {
+        //given
+        //when
         runException("8000",
                 "1,2,3,4,5,6",
                 "0", "-1", "j");
+        //then
         assertThat(output()).contains("[ERROR]");
     }
 
-    @DisplayName("잘못된 WinLotto를 입력했을 때 메시지를 출력하고, 재입력받는지 확인")
+    @DisplayName("잘못된 WinLotto를 입력했을 때 메시지를 출력하고, 재입력받는지 확인한다.")
     @Test
     void printErrorMessageAndReInputWinLotto() {
+        //given
+        //when
         runException("8000",
                 "1,2,3,4,5,6",
                 "1",
                 "1,2,3,4,5,6",
                 "1");
+        //then
         assertThat(output()).contains("[ERROR]");
     }
 

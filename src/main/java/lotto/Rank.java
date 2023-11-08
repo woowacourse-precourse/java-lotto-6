055b97888013545e930dac460b2e30b36271a8e3
hj-k66 <hjx66@naver.com>
2023-11-08 20:37:59 +0900

    fix: 맞은 번호 갯수 따라 Rank 구하는 로직 변경

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public enum Rank {
     FIRST(6, 2000000000),
@@ -11,10 +8,8 @@ public enum Rank {
     THIRD(5, 1500000),
     FOURTH(4, 50000),
     FIFTH(3, 5000),
-    NORANK(0, 0);
-
-    private static final Map<Integer, String> RANK_MAP = Collections.unmodifiableMap(
-            Stream.of(values()).collect(Collectors.toMap(Rank::getCorrectNumberCnt, Rank::name)));
+    NORANK(0, 0),
+    ;
 
     private final int correctNumberCnt;
     private final int rewardMoney;
@@ -36,7 +31,14 @@ public enum Rank {
         if (SECOND.correctNumberCnt == matchNumberCnt && isBonus) {
             return SECOND;
         }
-        return Rank.valueOf(RANK_MAP.get(matchNumberCnt));
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.find(matchNumberCnt) && rank != SECOND)
+                .findAny()
+                .orElse(NORANK);
+    }
+
+    private boolean find(int count){
+        return this.correctNumberCnt == count;
     }
 
 }

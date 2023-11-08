01d3c6cc3284290058ae42456ddd73cdd7004827
jjongwon7 <jjongwon7@naver.com>
2023-11-08 22:08:29 +0900

    feat: 구입 가격만큼 로또 생성하는 기능 추가 사용자가 입력한 로또 구입 가격만큼 로또를 생성하는 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,10 +3,18 @@ package lotto;
 import static lotto.views.UserInput.readPurchasePrice;
 import static lotto.views.UserOutput.askPurchasePrice;
 
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoStore;
+
 public class Application {
+    private static final LottoStore lottoStore = new LottoStore();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         askPurchasePrice();
         Integer purchasePrice = readPurchasePrice();
+
+        List<Lotto> lottos = lottoStore.purchaseLotto(purchasePrice);
     }
 }
--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -4,6 +4,7 @@ import static lotto.constant.LottoInfo.LOTTO_NUMBER_COUNT;
 import static lotto.constant.LottoInfo.LOTTO_PRICE;
 import static lotto.constant.LottoInfo.MAX_LOTTO_NUMBER;
 import static lotto.constant.LottoInfo.MIN_LOTTO_NUMBER;
+import static lotto.domain.LottoStore.LottoMachine.generateLotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -11,6 +12,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoStore {
+    public List<Lotto> purchaseLotto(Integer purchasePrice) {
+        List<Lotto> lottos = new ArrayList<>();
+        int numberOfLottoPurchases = getNumberOfLottoPurchases(purchasePrice);
+
+        for (int i = 0; i < numberOfLottoPurchases; i++) {
+            lottos.add(generateLotto());
+        }
+
+        return lottos;
+    }
     private int getNumberOfLottoPurchases(Integer purchasePrice) {
         return purchasePrice / LOTTO_PRICE;
     }

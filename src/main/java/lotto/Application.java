3cb39f041a03a18ca5ba6aa6568833058317ba8e
doublekm153 <doublekm153@gmail.com>
2023-11-08 18:28:33 +0900

    들여쓰기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,29 +5,29 @@ import java.util.List;
 import java.util.Map;
 
 public class Application {
-    public static void main(String[] args) {
-        InputView inputView = new InputView();
-        ResultView resultView = new ResultView();
-
-        int purchaseAmount = inputView.requestPurchaseAmount();
-        LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
-        List<Lotto> lottos = lottoMachine.purchaseLottos();
-
-        resultView.printLottos(lottos);
-
-        List<Integer> winningNumbers = inputView.requestWinningNumbers();
-        int bonusBall = inputView.requestBonusBall();
-
-        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusBall);
-
-        Map<LottoRank, Integer> result = new HashMap<>();
-
-        for (Lotto lotto : lottos) {
-            LottoRank rank = winningLotto.rank(lotto);
-            result.put(rank, result.getOrDefault(rank, 0) + 1);
-        }
-
-        resultView.printResult(result);
+  public static void main(String[] args) {
+    InputView inputView = new InputView();
+    ResultView resultView = new ResultView();
+    
+    int purchaseAmount = inputView.requestPurchaseAmount();
+    LottoMachine lottoMachine = new LottoMachine(purchaseAmount);
+    List<Lotto> lottos = lottoMachine.purchaseLottos();
+    
+    resultView.printLottos(lottos);
+    
+    List<Integer> winningNumbers = inputView.requestWinningNumbers();
+    int bonusBall = inputView.requestBonusBall();
+    
+    WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusBall);
+    
+    Map<LottoRank, Integer> result = new HashMap<>();
+    
+    for (Lotto lotto : lottos) {
+      LottoRank rank = winningLotto.rank(lotto);
+      result.put(rank, result.getOrDefault(rank, 0) + 1);
     }
+    
+    resultView.printResult(result);
+  }
 }
 

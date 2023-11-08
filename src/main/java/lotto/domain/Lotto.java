a2cb5aaab3fc2bcb87e84ee7d8f6c8abd6116183
JP-company <wjsdj2008@gmail.com>
2023-11-08 17:56:36 +0900

    Refactor: [수정] 로또 생성 검증 기능 메서드 분리
    로또 생성할 때 번호를 검증하는 기능을 시나리오별로 분리했다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 
 public class Lotto {
     public final static int PRICE = 1000;
@@ -22,16 +21,28 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != Lotto.SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateLottoSize(numbers);
+        validateRangeOfNumbers(numbers);
+        validateDuplication(numbers);
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+    }
+
+    private void validateRangeOfNumbers(List<Integer> numbers) {
         numbers.forEach((number) -> {
             if (number < Lotto.MIN_NUMBER || number > Lotto.MAX_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호는 1이상 45이하의 숫자여야 합니다.");
             }
         });
-        if (new HashSet<>(numbers).size() != numbers.size()) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != Lotto.SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 

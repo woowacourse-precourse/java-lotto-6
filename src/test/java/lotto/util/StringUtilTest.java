48adb2faf2f5bdf1cf3adb391fe37484a5e17590
donghoonyeom <dua5410773@naver.com>
2023-11-08 22:02:39 +0900

    test: StringUtil 테스트 케이스 업데이트
    StringUtil 클래스의 splitAndParse 메서드에 대한 테스트 케이스가 업데이트되었습니다. 변경된 부분은 다음과 같습니다:
    
    - 단언문 변경: JUnit의 assertEquals 대신 AssertJ의 assertThat을 사용하여 더 간결하고 가독성 있는 테스트 코드로 변경되었습니다. 이는 코드의 가독성과 유지보수성을 높이기 위함입니다.
    - 다양한 시나리오에 대한 테스트 추가: 다양한 시나리오에 대한 테스트가 추가되었습니다. 콤마와 공백, 특수문자 등 다양한 구분자와 유효하지 않은 입력값에 대한 테스트가 포함되어 더 강력한 테스트 커버리지를 제공합니다.
    - 예외 처리 테스트 추가: 유효하지 않은 입력에 대한 예외 처리를 테스트하는 케이스가 추가되어 예외 발생 시 올바르게 동작하는지 확인합니다.

--- src/test/java/lotto/util/StringUtilTest.java
+++ src/test/java/lotto/util/StringUtilTest.java
@@ -1,6 +1,8 @@
 package lotto.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -16,10 +18,52 @@ class StringUtilTest {
         List<Integer> result = StringUtil.splitAndParse(input, delimiter);
 
         assertEquals(5, result.size());
-        assertEquals(1, result.get(0));
-        assertEquals(2, result.get(1));
-        assertEquals(3, result.get(2));
-        assertEquals(4, result.get(3));
-        assertEquals(5, result.get(4));
+        assertThat(result).containsExactly(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    @DisplayName("다른 구분자를 사용하여 정수 리스트로 변환하는지 테스트")
+    void splitAndParseWithDifferentDelimiter() {
+        String input = "1|2|3|4|5";
+        String delimiter = "\\|";
+
+        List<Integer> result = StringUtil.splitAndParse(input, delimiter);
+
+        assertEquals(5, result.size());
+        assertThat(result).containsExactly(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    @DisplayName("공백을 포함한 문자열을 정수 리스트로 변환하는지 테스트")
+    void splitAndParseWithSpaceInInput() {
+        String input = "1, 2, 3, 4, 5";
+        String delimiter = ", ";
+
+        List<Integer> result = StringUtil.splitAndParse(input, delimiter);
+
+        assertEquals(5, result.size());
+        assertThat(result).containsExactly(1, 2, 3, 4, 5);
+    }
+
+    @Test
+    @DisplayName("특정 값을 포함하는지 테스트")
+    void splitAndParseContainsSpecificValue() {
+        String input = "1, 2, 3, 4, 5";
+        String delimiter = ", ";
+
+        List<Integer> result = StringUtil.splitAndParse(input, delimiter);
+
+        assertThat(result).contains(3);
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 입력에 대한 예외 처리 테스트")
+    void splitAndParseWithInvalidInput() {
+        String input = "1, 2, 3, abc, 5";
+        String delimiter = ", ";
+
+        assertThrows(NumberFormatException.class, () -> {
+            StringUtil.splitAndParse(input, delimiter);
+        });
     }
 }

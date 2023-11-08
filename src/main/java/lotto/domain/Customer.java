d10778a18ce1b5be7a99abb91172c243ee435bae
watermelon3737 <pshrina1@naver.com>
2023-11-08 14:19:53 +0900

    refactor(Customer) : final 추가, 메서드명 변경

--- src/main/java/lotto/domain/Customer.java
+++ src/main/java/lotto/domain/Customer.java
@@ -6,19 +6,19 @@ import java.util.List;
 public class Customer {
     private List<Lotto> lottos;
 
-    public void setLotto(List<Lotto> lottos) {
+    public void setLottos(final List<Lotto> lottos) {
         this.lottos = lottos;
     }
 
     public void insertMoney(final LottoStore lottoStore, final long money) {
-        lottoStore.getMoney(money);
+        lottoStore.insertMoney(money);
     }
 
-    public void checkResult(LottoChecker lottoChecker) {
-        lottoChecker.checkLottos(this.lottos);
+    public void checkResult(final LottoChecker lottoChecker) {
+        lottoChecker.checkLottos(lottos);
     }
 
     public List<Lotto> getLottos() {
-        return Collections.unmodifiableList(this.lottos);
+        return Collections.unmodifiableList(lottos);
     }
 }

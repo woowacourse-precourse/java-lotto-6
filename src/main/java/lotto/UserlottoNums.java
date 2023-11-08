5b34cea2ca0ba92e0919fcf43a0528b0580d20b3
ratatuwee <pkqwaszx@gmail.com>
2023-11-08 17:11:52 +0900

    refactor: 멤버 변수 불변성 강화

--- src/main/java/lotto/UserlottoNums.java
+++ src/main/java/lotto/UserlottoNums.java
@@ -6,7 +6,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
 
 public class UserlottoNums {
-    private ArrayList<Lotto> userLottos = new ArrayList<Lotto>();
+    private final ArrayList<Lotto> userLottos = new ArrayList<Lotto>();
 
     public UserlottoNums(int numberLottoCreations){
         for(int i = 0; i<numberLottoCreations; i++){

7379b71040ed703dc1f35278a655f84786094ead
S-DPR <thankyouformuch@naver.com>
2023-11-08 19:51:52 +0900

    fix: validate를 Validation클래스에 있는 메소드를 활용해 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.enums.LottoInfo;
+
+import java.util.Arrays;
 import java.util.List;
+
+import static lotto.enums.LottoInfo.MAX_NUMBER;
+import static lotto.enums.LottoInfo.MIN_NUMBER;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private final int LOTTO_COUNT;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        this.LOTTO_COUNT = LottoInfo.NUMBERS_PER_LOTTO.getNumber();
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        Validation.validateNotNull(numbers);
+        Validation.validateInRange(MIN_NUMBER.getNumber(), MAX_NUMBER.getNumber(), numbers);
+        Validation.validateLottoNumbersCount(numbers);
+        Validation.validateDuplicate(numbers);
     }
-
-    // TODO: 추가 기능 구현
 }

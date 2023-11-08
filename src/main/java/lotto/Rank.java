4c0618a9a9f0430a6fdde0fcc89a17a7dbbc6450
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-08 17:27:42 +0900

    Fix : 통계 순위가 순서대로 출력되지 않는 오류를 수정

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,13 +1,13 @@
 package lotto;
 
 public enum Rank {
-    FIRST(6, false, 2_000_000_000),
-    SECOND(5, true, 30_000_000),
-    THIRD(5, false, 1_500_000),
-
-    FOURTH(4, false, 50_000),
+    NONE(0, false, 0),
     FIFTH(3, false, 5_000),
-    NONE(0, false, 0);
+    FOURTH(4, false, 50_000),
+    THIRD(5, false, 1_500_000),
+    SECOND(5, true, 30_000_000),
+    FIRST(6, false, 2_000_000_000);
+
 
     private final int matchNumber;
     private final boolean hasBonus;

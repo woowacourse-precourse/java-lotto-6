92d1d174d920a6006a4f1feb4bbbda2f8f8760ec
GiJung <weeds2577@gmail.com>
2023-11-08 16:34:12 +0900

    refactor(PriceService): 총 우승 금액을 계산하고 WinningPrice에 저장하도록 변경, 총 수익률 계산 메서드 추가

--- src/main/java/lotto/service/PriceService.java
+++ src/main/java/lotto/service/PriceService.java
@@ -1,12 +1,17 @@
 package lotto.service;
 
 import lotto.domain.PurchasePrice;
+import lotto.domain.WinningCriteria;
+import lotto.domain.WinningDetails;
+import lotto.domain.WinningPrice;
 
 public class PriceService {
 
     private static final int LOTTO_UNIT = 1000;
+    private static final int PERCENTAGE = 100;
 
     private PurchasePrice purchasePrice;
+    private WinningPrice winningPrice;
 
     public void setPurchasePrice(int purchasePrice) {
         this.purchasePrice = new PurchasePrice(purchasePrice);
@@ -16,4 +21,17 @@ public class PriceService {
         return purchasePrice.getPurchasePrice() / LOTTO_UNIT;
     }
 
+    public void calculateWinningPrice(WinningDetails winningDetails) {
+        long winningPrice = 0;
+
+        for (WinningCriteria winningCriteria : WinningCriteria.values()) {
+            winningPrice += winningDetails.getWinningCount(winningCriteria.getRank()) * winningCriteria.getPrice();
+        }
+
+        this.winningPrice = new WinningPrice(winningPrice);
+    }
+
+    public float getEarningRate() {
+        return winningPrice.getWinningPrice() / purchasePrice.getPurchasePrice() * PERCENTAGE;
+    }
 }

8456007535a606104a944a8b10a4890e5b298175
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 22:32:08 +0900

    Feat : 로또 번호 오름차순 정렬
    로또 번호 오름차순으로 보여줘야 해서 생성할 때 정렬해서 생성하도록
    LottoMachine 클래스의 makeLotto 메소드에서 정렬하도록 기능 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoMachine {
 
     public Lotto makeLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         Lotto lotto = new Lotto(numbers);
         return lotto;
     }

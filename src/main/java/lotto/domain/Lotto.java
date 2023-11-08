fc0e31ab7b8068d3dca688e06069704161cbc218
sw801733 <sw801733@naver.com>
2023-11-08 10:21:13 +0900

    feat : domain의 클래스들 getter 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,14 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+
     private final List<Integer> numbers;
+
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);

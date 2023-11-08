f91cddb1de1f45a92815a599050cbfff15a927a3
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 16:55:19 +0900

    feat: 몇개의 랜덤번호가 생성되었는지 출력하는 enum 구현

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -1,2 +1,15 @@
-package lotto.constant;public enum OutputMessage {
+package lotto.constant;
+
+public enum OutputMessage {
+    OUTPUT_PURCHASE_AMOUNT_MESSAGE("%d개를 구매했습니다.");
+
+    private final String template;
+
+    OutputMessage(String template) {
+        this.template = template;
+    }
+
+    public String getMessage(int count) {
+        return String.format(template, count);
+    }
 }

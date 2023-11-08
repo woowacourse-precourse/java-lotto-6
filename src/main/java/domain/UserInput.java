2060fb709e2682425c83deb62ce7cee168572939
hanriverkitty <danieltls@naver.com>
2023-11-08 13:50:10 +0900

    Feat: 구입금액 구입금액을 입력받고 예외처리와 입력을 다시받음

--- src/main/java/domain/UserInput.java
+++ src/main/java/domain/UserInput.java
@@ -1,5 +1,6 @@
 package domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,8 +8,29 @@ import java.util.List;
 
 public class UserInput {
 
-    public List<Integer> RandomNumber(){
+    public List<Integer> randomNumber(){
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
+    }
+    public int ableBuy(){
+        while(true){
+            try {
+                int money = buy();
+                return money;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    public int buy() {
+
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            int money = Integer.parseInt(Console.readLine());
+            return money;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요");
+        }
+
     }
 }

ffa51c647106fec2e6e2447dd5b4d1ac243133b3
hoyeon <highoption@naver.com>
2023-11-08 11:25:02 +0900

    test(LottoTicketTest) : 불변 리스트를 가변 리스트로 변경

--- src/test/java/lotto/model/LottoTicketsTest.java
+++ src/test/java/lotto/model/LottoTicketsTest.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,8 @@ class LottoTicketsTest {
     @DisplayName("만든 LottoTickets가 제대로 된 Lotto를 담고 있는지 테스트")
     public void testLottoTicketsCreation() {
         List<Lotto> lottos = List.of(
-                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
-                new Lotto(List.of(7, 8, 9, 10, 11, 12)));
+                new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
+                new Lotto(new ArrayList<>(List.of(7, 8, 9, 10, 11, 12))));
 
         LottoTickets lottoTickets = new LottoTickets(lottos);
 

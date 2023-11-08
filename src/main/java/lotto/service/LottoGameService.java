cb2370b3f0c9eae37cfb4f9c5c869f9a2d091059
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:43:20 +0900

    refactor(LottoGameService): 불필요한 필드 제거
    InputView에 대한 모든 메서드에 static 키워드를 붙임으로써 InputView 객체가 불필요하기 때문에 해당 필드를 제거한다

--- src/main/java/lotto/service/LottoGameService.java
+++ src/main/java/lotto/service/LottoGameService.java
@@ -1,38 +1,37 @@
 package lotto.service;
 
-import java.util.List;
+import lotto.model.BonusNumber;
 import lotto.model.Player;
+import lotto.model.WinningNumber;
 import lotto.model.WinningNumbers;
 import lotto.util.RandomUtil;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoGameService {
-    private final InputView inputView;
     private final OutputView outputView;
     private final RandomUtil randomUtil;
 
-    public LottoGameService(InputView inputView, OutputView outputView, RandomUtil randomUtil) {
-        this.inputView = inputView;
+    public LottoGameService(OutputView outputView, RandomUtil randomUtil) {
         this.outputView = outputView;
         this.randomUtil = randomUtil;
     }
 
     public Player createPlayer() {
         outputView.requestPaymentAmount();
-        int paymentAmount = inputView.purchaseCost();
-        outputView.printLineOfSpace();
-        return new Player(paymentAmount, randomUtil);
+        return InputView.commonInput(() -> new Player(InputView.paymentAmount(), randomUtil));
     }
 
     public WinningNumbers createWinningNumber() {
         outputView.requestWinningNumber();
-        List<Integer> winningNumbers = inputView.winningNumber();
+        WinningNumber winningNumber = InputView.commonInput(() -> new WinningNumber(InputView.winningNumber()));
         outputView.printLineOfSpace();
         outputView.requestBonusNumber();
-        int bonusNumber = inputView.bonusNumber(winningNumbers);
+        BonusNumber bonusNumber = InputView.commonInput(
+                () -> new BonusNumber(InputView.bonusNumber(), winningNumber.getNumbers()));
         outputView.printLineOfSpace();
 
-        return new WinningNumbers(winningNumbers, bonusNumber);
+        return new WinningNumbers(winningNumber.getNumbers().stream().mapToInt(Integer::valueOf).toArray(),
+                bonusNumber.getNumber());
     }
 }

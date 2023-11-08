4312d34f1ad09e5bb59563ae360438f24a065fda
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:22:19 +0900

    test(PurchaseAmountTest): wildcard import를 single class import로 수정

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;

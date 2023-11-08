4876f9e91e408309fa43c54f8974be2a36b13897
geunsu <geunsu23@naver.com>
2023-11-08 11:21:59 +0900

    remove Lotto constants

--- src/main/java/lotto/model/Constants.java
+++ src/main/java/lotto/model/Constants.java
@@ -8,5 +8,4 @@ public class Constants {
     public static final int LOTTO_LENGTH = 6;
     public static final int MIN_LOTTO_NUMBER = 1;
     public static final int MAX_LOTTO_NUMBER = 45;
-    public static final List<Integer> LOTTO_CASH= Arrays.asList(2000000000,30000000,1500000,50000,5000);
 }

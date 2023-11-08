4b166520da4c658b2b47b9472127dfec6c0bdfde
다운 <j_d_woon@naver.com>
2023-11-08 12:16:46 +0900

    fix: Message 클래스 삭제에 따른 inputView 출력 내용 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,25 +1,20 @@
 package lotto.view;
 
-
-import static lotto.utils.Message.INPUT_BONUS_NUMBER;
-import static lotto.utils.Message.INPUT_WINNING_NUMBER;
-import static lotto.utils.Message.PURCAHSE_AMOUNT_LOTTO;
-
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String buyLotto() {
-        System.out.println(PURCAHSE_AMOUNT_LOTTO);
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
     public String inputWinningNumber() {
-        System.out.println(INPUT_WINNING_NUMBER);
+        System.out.println("\n당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     public String inputBonusNumber() {
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println("\n보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 

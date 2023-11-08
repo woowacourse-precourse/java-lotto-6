68c2cbdaf3e0c627e974ca9d3cee2286c9e45163
Hooni <gkxogns21@gmail.com>
2023-11-08 09:36:55 +0900

    fix: fix GenerateRandomNumber

--- src/main/java/lotto/Domain/GenerateRandomNumber.java
+++ src/main/java/lotto/Domain/GenerateRandomNumber.java
@@ -11,7 +11,7 @@ public class GenerateRandomNumber {
     public static List<Lotto> generateLottos(int count) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             Collections.sort(numbers);
             lottos.add(new Lotto(numbers));
         }

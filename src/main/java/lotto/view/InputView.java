4796f49ae17ebf775f084711617a9104fdb656a6
minkyole <7719s@nave.com>
2023-11-08 20:35:52 +0900

    refactor(InputView.java) : 사용자입력 받는 기능 하나로 통합
    - 사용자입력 받는 기능 getUserInput 하나로 리펙토링

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,36 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.NoSuchElementException;
 
 public class InputView {
-    public static String getPurchaseAmount() {
-        String purchaseAmount;
+    public static String getUserInput() {
+        String userInput;
 
-        try {
-            purchaseAmount = Console.readLine();
-        } catch (NoSuchElementException noSuchElementException){
-            throw new IllegalArgumentException("[ERROR] 구입금액에 값이 안들어왔습니다");
-        }
-        return purchaseAmount;
-    }
-
-    public static String getWinningLottoNumber() {
-        String winningLottoNumber;
-
-        try {
-            winningLottoNumber = Console.readLine();
-        } catch (NoSuchElementException noSuchElementException){
-            throw new IllegalArgumentException("[ERROR] 당첨번호에 값이 안들어왔습니다");
-        }
-        return winningLottoNumber;
-    }
-
-    public static String getWinningBonusNumber() {
-        String winningBonusNumber;
-
-        try {
-            winningBonusNumber = Console.readLine();
-        } catch (NoSuchElementException noSuchElementException){
-            throw new IllegalArgumentException("[ERROR] 보너스번호에 값이 안들어왔습니다");
-        }
-        return winningBonusNumber;
+        userInput = Console.readLine();
+        return userInput;
     }
 }

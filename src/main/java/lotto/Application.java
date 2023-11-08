57dbdc37798124a172b9941ba9f3142885789cf3
jjw0071 <jjw0071@naver.com>
2023-11-08 17:49:12 +0900

    feat(Application): 로또 게임 실행 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,27 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+import lotto.util.Validator;
+import lotto.service.LottoService;
+import lotto.ui.ConsoleUI;
+
+import java.util.List;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Validator validator = new Validator();
+        LottoService lottoService = new LottoService();
+        ConsoleUI consoleUI = new ConsoleUI(validator);
+
+        final int purchaseAmount = consoleUI    .getPurchaseAmount();
+        final List<Lotto> lottoTickets = lottoService.purchaseLotto(purchaseAmount);
+        consoleUI.displayLottoNumbers(lottoTickets);
+        final List<Integer> winningLottoNumbers = consoleUI.getLottoNumbers();
+        final int bonusNumber = consoleUI.getBonusNumber(winningLottoNumbers);
+        final Map<LottoResult, Integer> lottoResult = lottoService.calculateResult(lottoTickets, winningLottoNumbers, bonusNumber);
+        final double earningsRate = lottoService.getEarningsRate(lottoResult,purchaseAmount);
+        consoleUI.displayResult(lottoResult,earningsRate);
     }
 }

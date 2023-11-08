b964a0b0fc415c3df36aae6b6b611917ec8832ad
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:30:55 +0900

    refactor(InputValidation): 불필요한 검증 로직 삭제
    기존에는 InputView에서 각 입력에 대한 모든 조건을 확인하였는데, 모든 입력에 공통되는 조건을 제외한 나머지 유효성 검증은 각 클래스 책임으로 변경하였기 때문에 공통되는 유효성 검증만 남겨둔다

--- src/main/java/lotto/validation/InputValidation.java
+++ src/main/java/lotto/validation/InputValidation.java
@@ -1,99 +1,40 @@
 package lotto.validation;
 
 import static lotto.constant.ErrorMessage.DIGIT_EXCEPTION;
-import static lotto.constant.ErrorMessage.DUPLICATION_EXCEPTION;
-import static lotto.constant.ErrorMessage.MULTIPLE_EXCEPTION;
 import static lotto.constant.ErrorMessage.NULL_EXCEPTION;
-import static lotto.constant.ErrorMessage.NUMBER_IN_RANGE_EXCEPTION;
-import static lotto.constant.ErrorMessage.SIZE_EXCEPTION;
-import static lotto.constant.LottoConstant.LAST_LOTTO_NUMBER;
-import static lotto.constant.LottoConstant.LOTTO_NUMBER_COUNT;
-import static lotto.constant.LottoConstant.LOTTO_PRICE;
-import static lotto.constant.LottoConstant.START_LOTTO_NUMBER;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Arrays;
 
 public class InputValidation {
     private static final String DELIMITER = ",";
 
-    public void validatePurchaseCost(String purchaseCost) {
-        validateBlank(purchaseCost);
-        validateDigit(purchaseCost);
-        validateMultiple(purchaseCost);
+    public static int validateDigitAndConvertToDigit(String info) {
+        validateBlank(info);
+        validateDigit(info);
+        return Integer.parseInt(info);
     }
 
-    public void validateWinningNumber(String winningNumberInput) {
-        validateBlank(winningNumberInput);
-
-        String[] winningNumbers = winningNumberInput.split(DELIMITER);
-        validateSize(winningNumbers);
-
-        Set<Integer> distinctWinningNumbers = new HashSet<>();
-        for (String eachNumber : winningNumbers) {
-            validateDigit(eachNumber);
-
-            int winningNumber = Integer.parseInt(eachNumber);
-            validateNumberInRange(winningNumber);
-            validateDuplication(winningNumber, distinctWinningNumbers);
-        }
+    public static int[] validateDigitsAndConvertToDigits(String info) {
+        validateBlank(info);
+        String[] splitDigits = info.split(DELIMITER);
+        return Arrays.stream(splitDigits).mapToInt(InputValidation::validateDigitAndConvertToDigit).toArray();
     }
 
-    public void validateBonusNumber(String bonusNumberInput, List<Integer> winningNumbers) {
-        validateBlank(bonusNumberInput);
-        validateDigit(bonusNumberInput);
-
-        Set<Integer> distinctWinningNumbers = new HashSet<>(winningNumbers);
-        int bonusNumber = Integer.parseInt(bonusNumberInput);
-        validateNumberInRange(bonusNumber);
-        validateDuplication(bonusNumber, distinctWinningNumbers);
-    }
-
-    private void validateBlank(String info) {
+    private static void validateBlank(String info) {
         if (isBlank(info)) {
             throw new IllegalArgumentException(NULL_EXCEPTION.getMessage());
         }
     }
 
-    private boolean isBlank(String info) {
+    private static boolean isBlank(String info) {
         return info == null || info.isBlank();
     }
 
-    private void validateDigit(String info) {
+    private static void validateDigit(String info) {
         try {
             Integer.parseInt(info);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(DIGIT_EXCEPTION.getMessage());
-        }
-    }
-
-    private void validateMultiple(String info) {
-        int money = Integer.parseInt(info);
-        if (money % LOTTO_PRICE != 0) {
-            throw new IllegalArgumentException(MULTIPLE_EXCEPTION.getMessage());
-        }
-    }
-
-    private void validateSize(String[] winningNumbers) {
-        if (winningNumbers.length != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException(SIZE_EXCEPTION.getMessage());
-        }
-    }
-
-    private void validateNumberInRange(int winningNumber) {
-        if (!isNumberInRange(winningNumber)) {
-            throw new IllegalArgumentException(NUMBER_IN_RANGE_EXCEPTION.getMessage());
-        }
-    }
-
-    private boolean isNumberInRange(int winningNumber) {
-        return START_LOTTO_NUMBER <= winningNumber && winningNumber <= LAST_LOTTO_NUMBER;
-    }
-
-    private void validateDuplication(int winningNumber, Set<Integer> distinctWinningNumbers) {
-        if (!distinctWinningNumbers.add(winningNumber)) {
-            throw new IllegalArgumentException(DUPLICATION_EXCEPTION.getMessage());
         }
     }
 }

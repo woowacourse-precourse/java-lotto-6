da74767aee9bdd210f59c7bfa0710fe30dc46fc4
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 19:08:03 +0900

    refactor: 프로그램 요구사항에 맞춰 콤마(,)뒤에 띄어쓰기 추가

--- src/main/java/lotto/constant/OutputMessage.java
+++ src/main/java/lotto/constant/OutputMessage.java
@@ -4,7 +4,7 @@ public class OutputMessage {
 
     public static final String NUMBER_OF_LOTTO = "%d개를 구매했습니다.";
     public static final String LOTTO_FORMAT = "[%s]";
-    public static final String LOTTO_NUMBERS_SEPARATOR = ",";
+    public static final String LOTTO_NUMBERS_SEPARATOR = ", ";
     public static final String LOTTO_RESULT_FORMAT = "%s - %d개";
     public static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %.1f%%입니다.";
 

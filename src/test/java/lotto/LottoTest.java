f4a30cf4470f93e989603ba7f44eb8ea53021510
Henry-Cha <sungwon4728@naver.com>
2023-11-08 13:31:35 +0900

    test: lotto 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +26,37 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 6개 모두 일치하면 1등을 반환한다.")
+    @Test
+    void allMatchTest() {
+        final Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        List<Integer> goal = List.of(1,2,3,4,5,6);
+        int bonus = 7;
+
+        int rank = lotto.match(goal, bonus);
+
+        assertThat(rank).isEqualTo(1);
+    }
+    @DisplayName("로또 번호 5개와 보너스번호가 일치하면 2등을 반환한다.")
+    @Test
+    void match5andBonusTest() {
+        final Lotto lotto = new Lotto(List.of(1,2,3,4,5,7));
+        List<Integer> goal = List.of(1,2,3,4,5,6);
+        int bonus = 7;
+
+        int rank = lotto.match(goal, bonus);
+
+        assertThat(rank).isEqualTo(2);
+    }
+    @DisplayName("로또 번호 5개가 일치하면 3등을 반환한다.")
+    @Test
+    void match5Test() {
+        final Lotto lotto = new Lotto(List.of(1,2,3,4,5,8));
+        List<Integer> goal = List.of(1,2,3,4,5,6);
+        int bonus = 7;
+
+        int rank = lotto.match(goal, bonus);
+
+        assertThat(rank).isEqualTo(3);
+    }
 }

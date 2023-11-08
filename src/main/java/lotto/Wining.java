569b2245072e4f6cda0620f29c71c15dfbed4697
kyeongseo90 <rudtj9099@gmail.com>
2023-11-08 23:02:18 +0900

    보너스 정보 입력 구현 & 보너스 정보 입력 시 예외

--- src/main/java/lotto/Wining.java
+++ src/main/java/lotto/Wining.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Wining {
 
-    private List<Integer> jackpot = new ArrayList<>();
+    private List<Integer> jackpot;
     private Integer bonus;
 
     public Wining() {
+        jackpot = new ArrayList<>();
     }
 
     public void setWinningNumber(List<Integer> numbers){
@@ -18,21 +19,25 @@ public class Wining {
 
     private void validateWins(List<Integer> numbers) {
         validateDuplicated(numbers);
-        validateValidNumber(numbers);
+        validateValidList(numbers);
         validateSizeOfNumber(numbers.size());
     }
 
     private void validateSizeOfNumber(int size) {
         if (size != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 당첨 숫자는 6개여야 합니다.")
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 숫자는 6개여야 합니다.");
         }
     }
 
-    private void validateValidNumber(List<Integer> numbers) {
+    private void validateValidList(List<Integer> numbers) {
         for (int num:numbers) {
-            if (num <= 0 || num > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 1과 45 사이여야 합니다.");
-            }
+            validateValidNumber(num);
+        }
+    }
+
+    private void validateValidNumber(int num){
+        if (num <= 0 || num > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 1과 45 사이여야 합니다.");
         }
     }
 
@@ -50,6 +55,16 @@ public class Wining {
     }
 
     private void validateBonus(Integer number) {
+        validateValidNumber(number);
+        validateDupliWithJackpot(number);
+    }
+
+    private void validateDupliWithJackpot(Integer number) {
+        for (int j: jackpot) {
+            if (j == number) {
+                throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복이 되면 안됩니다.");
+            }
+        }
     }
 
 }

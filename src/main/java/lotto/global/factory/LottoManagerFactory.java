30e550ae181870ef4f07db4f82268502a925e464
lkdcode <dlrlejr1@naver.com>
2023-11-08 16:42:58 +0900

    Refactor [LottoManagerFactory] : LottoManagerFactory
     LottoManager만 싱글톤으로 생성하도록 변경

--- src/main/java/lotto/global/factory/LottoManagerFactory.java
+++ src/main/java/lotto/global/factory/LottoManagerFactory.java
@@ -1,47 +1,23 @@
 package lotto.global.factory;
 
-import lotto.module.domain.lotto.LottoNumberGenerator;
-import lotto.module.domain.lotto.UserLottoTicketGenerator;
 import lotto.module.manager.LottoManager;
-import lotto.module.store.LottoStore;
-import lotto.module.store.LottoTicketQuantityCalculator;
+
+import static lotto.global.factory.LottoConsoleManagerFactory.getSingletonLottoConsoleManager;
+import static lotto.global.factory.LottoResultManagerFactory.getSingletonLottoResultManager;
+import static lotto.global.factory.LottoStoreFactory.getSingletonLottoStore;
+
 
 public class LottoManagerFactory {
     private static LottoManager lottoManager;
-    private static LottoStore lottoStore;
-
-    private static LottoTicketQuantityCalculator lottoTicketQuantityCalculator;
-    private static LottoNumberGenerator lottoNumberGenerator;
 
     private LottoManagerFactory() {
     }
 
     public static LottoManager getSingletonLottoManager() {
         if (lottoManager == null) {
-            lottoManager = LottoManager.of(LottoConsoleManagerFactory.getSingletonLottoConsoleManager(), getSingletonLottoStore());
+            lottoManager = LottoManager.of(getSingletonLottoConsoleManager(), getSingletonLottoResultManager(), getSingletonLottoStore());
         }
         return lottoManager;
-    }
-
-    private static LottoStore getSingletonLottoStore() {
-        if (lottoStore == null) {
-            lottoStore = LottoStore.of(getSingletonLottoTicketQuantityCalculator(), getSingletonLottoNumberGenerator());
-        }
-        return lottoStore;
-    }
-
-    private static LottoTicketQuantityCalculator getSingletonLottoTicketQuantityCalculator() {
-        if (lottoTicketQuantityCalculator == null) {
-            lottoTicketQuantityCalculator = LottoTicketQuantityCalculator.newInstance();
-        }
-        return lottoTicketQuantityCalculator;
-    }
-
-    private static LottoNumberGenerator getSingletonLottoNumberGenerator() {
-        if (lottoNumberGenerator == null) {
-            lottoNumberGenerator = UserLottoTicketGenerator.newInstance();
-        }
-        return lottoNumberGenerator;
     }
 
 }

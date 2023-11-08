79a1c03b6ec09518c52bfcf890cbbc2be9bd09c9
SuhyungK <dook4966@gmail.com>
2023-11-08 13:59:18 +0900

    add(StaticMessage): 보너스 번호 입력 메세지 추가

--- src/main/java/lotto/constants/StaticMessage.java
+++ src/main/java/lotto/constants/StaticMessage.java
@@ -3,6 +3,7 @@ package lotto.constants;
 public enum StaticMessage {
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_PAYMENT("구입금액을 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     PRINT_LOTTO_COUNT("개를 구매했습니다."),
     PRINT_RESULT_NOTICE("당첨 통계%n---");
 

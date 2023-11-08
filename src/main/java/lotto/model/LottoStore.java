520b3f8eaca3776d19b8a02e4ccdb85c0d040869
gmu1026 <curiouscomponent@gmail.com>
2023-11-08 19:55:46 +0900

    feat: 로또 구매 기능 구현

--- src/main/java/lotto/model/LottoStore.java
+++ src/main/java/lotto/model/LottoStore.java
@@ -2,19 +2,17 @@ package lotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class LottoStore {
-    private static final int LOTTO_COST = 1000;
-
     private LottoStore() {
 
     }
+
     public static List<Lotto> buyLotto(Money lottoPurchaseMoney) {
         List<Lotto> lottos = new ArrayList<>();
 
-        long payableLottoCount = lottoPurchaseMoney.getCountByCost(LOTTO_COST);
+        int payableLottoCount = lottoPurchaseMoney.getCountByCost(Constants.LOTTO_UNIT.constants);
         for (int buyCount = 0; buyCount < payableLottoCount; buyCount++) {
             lottos.add(new Lotto(createRandomLottoNumbers()));
         }
@@ -23,9 +21,7 @@ public class LottoStore {
     }
 
     private static List<Integer> createRandomLottoNumbers() {
-        List<Integer> randomLottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-        randomLottoNumbers.sort(Comparator.naturalOrder());
-
-        return randomLottoNumbers;
+        return Randoms.pickUniqueNumbersInRange(Constants.LOTTO_MIN_NUMBER.constants,
+                Constants.LOTTO_MAX_NUMBER.constants, Constants.LOTTO_LENGTH.constants).stream().sorted().toList();
     }
 }

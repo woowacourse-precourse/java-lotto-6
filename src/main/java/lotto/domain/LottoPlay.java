00c583aff3e4495f4e455219ba9362ddf7b3f559
Yang woosung <ngxk1217@gmail.com>
2023-11-08 13:06:16 +0900

    구매 금액을 계산 하고 번호를 생성하는 기능을 추가 하였습니다.

--- src/main/java/lotto/domain/LottoPlay.java
+++ src/main/java/lotto/domain/LottoPlay.java
@@ -1,8 +1,13 @@
 package lotto.domain;
 
+import lotto.Lotto;
+import lotto.constant.LottoNumber;
 import lotto.util.InputUtil;
 import lotto.util.MessageUtil;
 import lotto.util.ValidateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoPlay {
 
@@ -10,9 +15,13 @@ public class LottoPlay {
     private final InputUtil inputUtil = new InputUtil();
     private final ValidateUtil validateUtil = new ValidateUtil();
 
+    private final NumberGenerator numberGenerator = new NumberGenerator();
+    private final List<Lotto> userLottos = new ArrayList<>();
+
     public void play(){
         messageUtil.printBonusInput();
-        getUserPurchaseAmount();
+        int purchaseAmount = getUserPurchaseAmount();
+        getPurchaseInfo(purchaseAmount);
     }
 
     private int getUserPurchaseAmount(){
@@ -20,5 +29,19 @@ public class LottoPlay {
         validateUtil.validatePurchase(purchaseAmount);
         return purchaseAmount;
     }
+    private void getPurchaseInfo(int purchaseAmount){
+        int purchaseCount = purchaseAmount /
+                LottoNumber.PURCHASE_AMOUNT_COND.getNumber();
+        messageUtil.printPurchaseCount(purchaseCount);
+
+        for (int i = 0; i <purchaseCount; i++){
+            List<Integer>lottoNumbers=numberGenerator.getLottoNumbers();
+            Lotto lotto = new Lotto(lottoNumbers);
+            messageUtil.printPurchaseInfo(lotto.getLottoNumbers());
+            userLottos.add(lotto);
+        }
+
+    }
+
 
 }

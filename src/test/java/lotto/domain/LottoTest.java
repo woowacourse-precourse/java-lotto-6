c2505c35f6dadd69485f5397cfa03ae1d09382c1
syy0O <86238720+syy0O@users.noreply.github.com>
2023-11-08 19:54:16 +0900

    test: LottoTest 테스트케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,5 +23,12 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-    
+
+    @DisplayName("특정 숫자가 로또에 존재함을 확인할 수 있다.")
+    @Test
+    void contains() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        boolean result = lotto.contains(1);
+        assertThat(result).isTrue();
+    }
 }

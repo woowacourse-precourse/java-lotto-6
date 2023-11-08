5b7ac0225b9c27581c1dd57fcf3679e6282255d1
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 17:00:50 +0900

    feat(WinningNumberValidator): Winning Number 중복 검증 구현

--- src/main/java/lotto/util/validator/WinningNumberValidator.java
+++ src/main/java/lotto/util/validator/WinningNumberValidator.java
@@ -3,12 +3,26 @@ package lotto.util.validator;
 import lotto.util.exception.ErrorCode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningNumberValidator {
     public static final int MIN_RANDOM_BOUND = 1;
     public static final int MAX_RANDOM_BOUND = 45;
     public static final int TOTAL_COUNT = 6;
+
+    public static List<Integer> validateWinningNumbers(String inputWinningNumbers) {
+        validateNullorEmpty(inputWinningNumbers);
+
+        String[] numberStrings = inputWinningNumbers.replace(" ", "").split(",");
+        validateNumberCount(numberStrings);
+
+        List<Integer> numbers = validateNumberFormate(numberStrings);
+        validateNumberRange(numbers);
+        validateNumberDuplication(numbers);
+        return numbers;
+    }
 
     private static void validateNullorEmpty(String inputWinningNumbers) {
         if (inputWinningNumbers == null || inputWinningNumbers.trim().isEmpty()) {
@@ -40,6 +54,13 @@ public class WinningNumberValidator {
             if (number < MIN_RANDOM_BOUND || number > MAX_RANDOM_BOUND) {
                 throw new IllegalArgumentException(ErrorCode.INVALID_WINNING_NUMBER_RANGE.getMessage());
             }
+        }
+    }
+
+    private static void validateNumberDuplication(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != TOTAL_COUNT) {
+            throw new IllegalArgumentException(ErrorCode.DUPLICATE_WINNING_NUMBERS.getMessage());
         }
     }
 }

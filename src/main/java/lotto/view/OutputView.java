f13efdfe9e189559fc50fd26e45f9af367183721
bokyung <bbookng@gmail.com>
2023-11-08 16:06:48 +0900

    fix(OutputView) : 전체 결과 출력 로직 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,8 +8,6 @@ public class OutputView {
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String DASHES = "---";
 
-    public static final String REVENUE_MESSAGE = "총 수익률은 %d입니다.";
-
     public void printPurchasedResult(int count) {
         System.out.println(String.format(CHECK_PURCHASE_COUNT, count));
     }
@@ -21,19 +19,13 @@ public class OutputView {
         System.out.println();
     }
 
-    public void printTotalResult(List<String> result, float revenue) {
+    public void printTotalResult(String result) {
         printResultMessage();
-        result.forEach(System.out::println);
-        printTotalRevenue(revenue);
+        System.out.println(result);
     }
 
     public void printResultMessage() {
         System.out.println(WINNING_STATISTICS);
         System.out.println(DASHES);
-    }
-
-
-    public void printTotalRevenue(float revenue) {
-        System.out.println(String.format(REVENUE_MESSAGE, revenue));
     }
 }

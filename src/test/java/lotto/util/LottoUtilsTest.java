10b7b715421dcec24174760729064bbed192b7c2
MyStraw <kikongdosa@gmail.com>
2023-11-08 19:43:34 +0900

    Test: 추가 Util 유효성 검사
    -입력받은 숫자들을 리스트로 변환 검사
    -로또 번호의 유효성 검사
    -오름차순 정렬 검사

---
+++ src/test/java/lotto/util/LottoUtilsTest.java
@@ -0,0 +1,39 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+public class LottoUtilsTest {
+    @Test
+    @DisplayName("입력받은 숫자 문자열을 로또 번호 리스트로 변환한다")
+    void shouldConvertStringToLottoNumberList() {
+        String input = "1, 2, 3, 4, 5, 6";
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> result = LottoUtils.convertStringToLottoNumberList(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("로또 번호의 유효성을 검사한다")
+    void shouldValidateLottoNumbers() {
+        List<Integer> validNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        assertThat(LottoUtils.validateLottoNumbers(validNumbers)).isTrue();
+
+        List<Integer> invalidNumbers = Arrays.asList(0, 2, 3, 4, 5, 51);
+        assertThrows(IllegalArgumentException.class, () -> LottoUtils.validateLottoNumbers(invalidNumbers));
+    }
+
+    @Test
+    @DisplayName("로또 번호를 정렬하여 반환한다")
+    void shouldSortLottoNumbers() {
+        List<Integer> numbers = Arrays.asList(6, 5, 4, 3, 2, 1);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> result = LottoUtils.sortLottoNumbers(numbers);
+        assertThat(result).isEqualTo(expected);
+    }
+}

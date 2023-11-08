0529a0276a535718a00ece4f8466237089e38b22
takoyakimchi <c4nyou@gmail.com>
2023-11-08 17:26:08 +0900

    feat(game): 가격 입력받는 inputBuyPrice 메서드 구현

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -1,8 +1,24 @@
 package lotto;
 
+import static lotto.constant.GameMessage.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.constant.GameMessage;
+
 public class Game {
 
     public static void start() {
+        Price price = inputBuyPrice();
+    }
 
+    private static Price inputBuyPrice() {
+        while (true) {
+            System.out.println(INPUT_BUY_PRICE.getMessage());
+            try {
+                return new Price(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }

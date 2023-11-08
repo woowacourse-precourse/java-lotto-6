6830f6074380297bb8978711f28e5900dd394ca9
최휘용 <chy2581@naver.com>
2023-11-08 16:27:55 +0900

    feat: 구입 금액 입력받고 예외 처리
    로또를 발행하고 계산 및 출력을 하는 LottoStore 클래스 구현
    상수인 로또 가격, 필드인 로또의 리스트, 당첨번호, 보너스 번호를 구현
    금액을 입력받는 recieveMoney와 예외처리를 하는 validateMoney 메서드를 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoStore lottoStore = new LottoStore();
+
+        lottoStore.receiveMoney();
     }
 }
---
+++ src/main/java/lotto/LottoStore.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LottoStore {
+
+    private final int lottoPrice = 1000;
+
+    private List<Lotto> myLottos;
+    private Lotto winningNumber;
+    private int bonusNumber;
+
+    public LottoStore() {
+        myLottos = new ArrayList<Lotto>();
+    }
+    public void receiveMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        validateMoney(input);
+    }
+
+    private void validateMoney(String number) {
+        for (int i = 0; i < number.length(); ++i) {
+            if (number.charAt(i) < '0' || number.charAt(i) > '9') {
+                throw new IllegalArgumentException("[ERROR] 숫자로 된 금액을 입력해야 합니다.");
+            }
+        }
+        if (Integer.parseInt(number) % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1,000원 단위로 금액을 입력해야 합니다.");
+        }
+    }
+}

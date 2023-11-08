2529aa41ec2a4d2d2d2bc16409e272584580898a
kkkkimtaehyeon <kth1927@naver.com>
2023-11-08 12:35:23 +0900

    refactor : 사용되지 않는 변수, 메소드 제거

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,22 +1,15 @@
 package lotto.model;
 
 public class BonusNumber {
-    private static final String SPECIAL_REGEX = "[!@#$%^&*()_+=\\[\\]{};':,\"\\\\|.<>/?-]";
     private static final String NUMERIC_REGEX = "^[0-9]+$";
 
     private final String BONUS_NUMBER;
-    private final int PURCHASE_PRICE;
     public BonusNumber(String bonusNumber){
 
         isNotDigits(bonusNumber);
         isWrongNumberRange(bonusNumber);
-        this.PURCHASE_PRICE = Integer.parseInt(bonusNumber);
         this.BONUS_NUMBER = bonusNumber;
 
-    }
-
-    public int getPurchasePrice(){
-        return PURCHASE_PRICE;
     }
     public String getBonusNumber(){
         return this.BONUS_NUMBER;

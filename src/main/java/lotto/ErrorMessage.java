0bde5c89486f2675c75f46caf5d038cf7e8a7643
leorb159 <ldgsocial159@naver.com>
2023-11-08 23:00:46 +0900

    feat: 유효하지 않은 보너스 번호 에러 메시지 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -4,8 +4,9 @@ public enum ErrorMessage {
     INPUT_NOT_NUMBER("[ERROR] 숫자로 변환할 수 없는 입력입니다. 다시 입력해주세요."),
     NOT_DIVISIBLE_LOTTO_PRICE(String.format("[ERROR] %d으로 나누어 떨어지는 금액을 입력해야 합니다. 다시 입력해주세요.", LottoManagement.PRICE_LOTTO)),
     INVALID_LOTTO_NUMBERS(String.format("[ERROR] 입력한 로또 번호가 유효하지 않습니다. ,를 포함하여 %d ~ %d 범위의 중복되지 않는 %d 자리를 입력해주세요.",
-            LottoConst.MIN_RANGE_NUMBER.getValue(), LottoConst.MAX_RANGE_NUMBER.getValue(), LottoConst.LOTTO_NUMBER_COUNT.getValue()));
-
+            LottoConst.MIN_RANGE_NUMBER.getValue(), LottoConst.MAX_RANGE_NUMBER.getValue(), LottoConst.LOTTO_NUMBER_COUNT.getValue())),
+    INVALID_BONUS_NUMBER(String.format("[ERROR] 입력한 보너스 번호가 유효하지 않습니다. %d ~ %d 범위의 당첨 번호와 중복되지 않는 숫자를 입력해주세요.",
+            LottoConst.MIN_RANGE_NUMBER.getValue(), LottoConst.MAX_RANGE_NUMBER.getValue()));
 
     private final String message;
 

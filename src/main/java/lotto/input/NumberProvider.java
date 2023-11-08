a7ac6ab7752744d8c6137cc9ad7d7e78363a6c8c
shortboy7 <thdwldnd7@naver.com>
2023-11-08 21:42:39 +0900

    fix : NumberProvider 발생 예외 추가

--- src/main/java/lotto/input/NumberProvider.java
+++
@@ -1,9 +0,0 @@
-package lotto.input;
-
-import java.util.List;
-import lotto.exception.BaseException;
-
-public interface NumberProvider {
-    public Integer getSingle() throws BaseException;
-    public List<Integer> getMultiple() throws BaseException;
-}
---
+++ src/main/java/lotto/input/provider/NumberProvider.java
@@ -0,0 +1,9 @@
+package lotto.input.provider;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface NumberProvider {
+    public Integer getSingle() throws IllegalArgumentException, IOException;
+    public List<Integer> getMultiple() throws IllegalArgumentException;
+}

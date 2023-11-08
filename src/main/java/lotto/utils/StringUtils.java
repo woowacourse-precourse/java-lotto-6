68fe627cdf935c6b7b262b62d77ca29f02db96f9
wooni97 <jhw970714@gmail.com>
2023-11-08 20:22:25 +0900

    refactor: StringUtils 클래스 수정
    - 이 커밋은 StringUtils 클래스에서 여러가지를 수정합니다.
    기존의 쉼표를 기준으로 split 하기 위해 하드코딩된 쉼표를 상수로 선언합니다. 또한 for문을 forEach 문으로 변경하여 코드를 간결하게 표현합니다.

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -1,11 +1,13 @@
 package lotto.utils;
 
+import lotto.constant.ErrorMessage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StringUtils {
+    private static final String SPLIT_DELIMITER = ",";
+
     private StringUtils() {
     }
 
@@ -13,19 +15,19 @@ public class StringUtils {
         try {
             return Integer.parseInt(inputString);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_INPUT);
         }
     }
 
     public static List<String> splitString(String inputString) {
-        return Arrays.stream(inputString.split(",")).toList();
+        return Arrays.stream(inputString.split(SPLIT_DELIMITER)).toList();
     }
 
     public static List<Integer> convertToIntegerList(List<String> stringList) {
         List<Integer> result = new ArrayList<>();
-        for (String s : stringList) {
+        stringList.forEach(s -> {
             result.add(StringUtils.stringToInt(s.trim()));
-        }
+        });
         return result;
     }
 }

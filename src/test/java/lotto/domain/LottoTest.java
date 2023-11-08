e05e806f83f5d19455402267021ee1bcbb927397
Hoony0321 <eogns0321@gmail.com>
2023-11-08 15:12:42 +0900

    test : Lotto getRank 메서드 테스트 추가 (로또 번호 비교하여 Rank 반환)

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -45,6 +45,49 @@ class LottoTest {
         assertThat(lotto.getNumbers()).containsExactly(1, 2, 3, 4, 5, 6);
     }
 
+    @DisplayName("당첨 로또와 입력한 로또 번호 및 보너스 번호 비교를 하여 Rank를 반환한다.")
+    @Test
+    void getRank_로또_번호_비교() {
+        compareLottos_로또_번호_6개_일치();
+        compareLottos_로또_번호_5개_일치_보너스_일치();
+        compareLottos_로또_번호_5개_일치();
+        compareLottos_로또_번호_4개_일치();
+        compareLottos_로또_번호_3개_일치();
+    }
 
+    void compareLottos_로또_번호_6개_일치() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(winningLotto.getRank(lotto, bonusNumber)).isEqualTo(Rank.FIRST);
+    }
+
+    void compareLottos_로또_번호_5개_일치_보너스_일치() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        assertThat(winningLotto.getRank(lotto, bonusNumber)).isEqualTo(Rank.SECOND);
+    }
+
+    void compareLottos_로또_번호_5개_일치() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 8));
+        assertThat(winningLotto.getRank(lotto, bonusNumber)).isEqualTo(Rank.THIRD);
+    }
+
+    void compareLottos_로또_번호_4개_일치() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 7, 8));
+        assertThat(winningLotto.getRank(lotto, bonusNumber)).isEqualTo(Rank.FOURTH);
+    }
+
+    void compareLottos_로또_번호_3개_일치() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        assertThat(winningLotto.getRank(lotto, bonusNumber)).isEqualTo(Rank.FIFTH);
+    }
 
 }

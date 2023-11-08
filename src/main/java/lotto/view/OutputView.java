24648eb8c0f517085b463ef279bf283b1ab7524c
bangjaeyoung <qkdwo4613@gmail.com>
2023-11-08 18:39:46 +0900

    refactor(output): 상수 적용 및 스트림 처리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,8 +9,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
-    private static final String PURCHASE_TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
+    private static final String PURCHASE_TICKET_COUNT_MESSAGE = "%d개를 구매했습니다.";
     private static final String WINNING_STASTISTICS_MESSAGE = "당첨 통계";
+    private static final String WINNING_RESULT_MESSAGE = "%s - %d개";
+    private static final String PROFIT_RATE_MESSAGE = "총 수익률은 %.1f%s입니다.";
     private static final String LINE = "---";
     private static final String PERCENTAGE = "%";
     
@@ -19,20 +21,21 @@ public class OutputView {
     }
     
     public void printTicketCount(int ticketCount) {
-        System.out.println(ticketCount + PURCHASE_TICKET_COUNT_MESSAGE);
+        String sentence = String.format(PURCHASE_TICKET_COUNT_MESSAGE, ticketCount);
+        System.out.println(sentence);
     }
     
     public void printTickets(List<Lotto> lottoTickets) {
-        StringBuilder messageBuilder = new StringBuilder();
-        for (Lotto ticket : lottoTickets) {
-            List<Integer> sortedTicket = sortByAscending(ticket);
-            messageBuilder.append(sortedTicket).append("\n");
-        }
-        System.out.println(messageBuilder);
+        lottoTickets.stream()
+                .map(this::sortByAscending)
+                .forEach(System.out::println);
+        printNewLine();
     }
     
     private List<Integer> sortByAscending(Lotto ticket) {
-        return ticket.getNumbers().stream().sorted().collect(Collectors.toCollection(LinkedList::new));
+        return ticket.getNumbers().stream()
+                .sorted()
+                .collect(Collectors.toCollection(LinkedList::new));
     }
     
     public void printWinningstatistics(Map<Ranking, Integer> winningResult) {
@@ -43,12 +46,12 @@ public class OutputView {
     
     private String makeWinningStatisticsText(Map<Ranking, Integer> winningResult) {
         return winningResult.entrySet().stream()
-                .map(entry -> String.format("%s - %d개", entry.getKey().getMessage(), entry.getValue()))
+                .map(entry -> String.format(WINNING_RESULT_MESSAGE, entry.getKey().getMessage(), entry.getValue()))
                 .collect(Collectors.joining("\n"));
     }
     
     public void printProfitRate(double profitRate) {
-        String sentence = String.format("총 수익률은 %.1f%s입니다.", profitRate, PERCENTAGE);
+        String sentence = String.format(PROFIT_RATE_MESSAGE, profitRate, PERCENTAGE);
         System.out.println(sentence);
     }
     

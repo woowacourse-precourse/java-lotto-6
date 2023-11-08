5a0792666f1f26155267f508f0eaa8098ad29a31
vvvv1111vvvv <105331246+vvvv1111vvvv@users.noreply.github.com>
2023-11-08 12:31:06 +0900

    feat: add bonus num getting process

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import View.BatInput;
+import View.WinnerInput;
+import View.WinnerInput;
+import Controller.Lotto;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int price = BatInput.getPrice();
+        int batCount=price/1000;
+        List<Integer> winners;
+        int bonus;
+        Lotto lotto;
+        int i =1;
+        do {
+            try {
+                winners = WinnerInput.getWinner();
+                lotto = new Lotto(winners);
+                i=0;
+                Console.close();
+            }
+            catch(IllegalArgumentException e){
+                winners.clear();
+            }
+        }while (i==1);
+        //winners는 6개
+        bonus=lotto.getWinnerBonus();
+
+
     }
 }

e64c92c39123a690bf59a900704f07a746c5e03d
DonghaOh <donghadarwin@naver.com>
2023-11-08 15:00:32 +0900

    feat: 당첨 번호 6개의 요소가 아니면 예외
    winningNumbersSix 메소드

--- src/main/java/validator/WinningNumbersValidator.java
+++ src/main/java/validator/WinningNumbersValidator.java
@@ -4,6 +4,7 @@ public class WinningNumbersValidator {
     public static void winningNumbersValidate(String winningNumbers) {
         winningNumbersNotNull(winningNumbers);
         winningNumbersComma(winningNumbers);
+        winningNumbersSix(winningNumbers);
     }
 
     private static void winningNumbersNotNull(String winningNumbers) {
@@ -12,9 +13,15 @@ public class WinningNumbersValidator {
         }
     }
 
-    private static void winningNumbersComma(String money) {
-        if (!money.matches("^[0-9,]*$")) {
+    private static void winningNumbersComma(String winningNumbers) {
+        if (!winningNumbers.matches("^[0-9,]*$")) {
             throw new IllegalArgumentException("[ERROR] 양의 정수를 쉼표로 구분해주세요.");
+        }
+    }
+
+    private static void winningNumbersSix(String winningNumbers) {
+        if (!winningNumbers.matches("^(.*?,){5}.*?$")) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수 6개를 쉼표로 구분해주세요.");
         }
     }
 }

331275389c95cebdbc89fb3042bda52deb999ec2
suddiyo <suddiyo@naver.com>
2023-11-08 20:37:21 +0900

    feat: 로또 구입 금액 입력 기능 구현

---
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -0,0 +1,13 @@
+package lotto.model;
+
+public class PurchasePrice {
+    int price;
+
+    public PurchasePrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,17 @@
 package lotto.view;
 
+import static lotto.view.LottoConsole.*;
+import static lotto.view.LottoConstants.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.model.PurchasePrice;
+
 public class InputView {
+    public PurchasePrice readPurchasePrice() {
+        print(MSG_PURCHASE_MONEY_PROMPT);
+        int price = Integer.parseInt(Console.readLine());
+        return new PurchasePrice(price);
+    }
+
+
 }

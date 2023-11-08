795f11068b0925e98bfecf69137b48f695e0390f
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-08 15:24:48 +0900

    feat: 잘못된 입력을 받을 시 에러 문구 출력 기능 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -3,8 +3,8 @@ package lotto.View;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.View.InputConvertor.InputConvertor;
+import lotto.View.InputValidator.BonusWinningNumValidator;
 import lotto.View.InputValidator.BudgetInputValidator;
-import lotto.View.InputValidator.Validator;
 import lotto.View.InputValidator.WinningNumValidator;
 
 public class InputView {
@@ -12,37 +12,49 @@ public class InputView {
     private static final String ASK_LOTTERY_BUDGET = "구입금액을 입력해 주세요.";
     private static final String ASK_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String ASK_WINNING_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-    Validator budgetValidator = new BudgetInputValidator();
-    Validator winningNumValidator = new WinningNumValidator();
+    BudgetInputValidator budgetValidator = new BudgetInputValidator();
+    WinningNumValidator winningNumValidator = new WinningNumValidator();
+    BonusWinningNumValidator bonusWinningNumValidator = new BonusWinningNumValidator();
+
 
     InputConvertor inputConvertor = new InputConvertor();
 
-    public Integer lotteryBudgetInput(){
+    public Integer lotteryBudgetInput() {
         while (true) {
-            try{
+            try {
                 System.out.println(ASK_LOTTERY_BUDGET);
                 String lotteryBudgetInput = Console.readLine();
                 budgetValidator.validate(lotteryBudgetInput);
                 return inputConvertor.convertLotteryBudget(lotteryBudgetInput);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                System.out.println(illegalArgumentException.getMessage());
             }
-            catch (IllegalArgumentException illegalArgumentException){}
         }
     }
 
-    public List<Integer> winningNumInput(){
+    public List<Integer> winningNumInput() {
         while (true) {
-            try{
+            try {
                 System.out.println(ASK_WINNING_NUMBER);
                 String winningNumInput = Console.readLine();
                 winningNumValidator.validate(winningNumInput);
                 return inputConvertor.convertWinningNum(winningNumInput);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                System.out.println(illegalArgumentException.getMessage());
             }
-            catch (IllegalArgumentException illegalArgumentException){}
         }
     }
 
-    public String winningBonusNumInput(){
-        System.out.println(ASK_WINNING_BONUS_NUMBER);
-        return Console.readLine();
+    public Integer bonusWinningNumInput(List<Integer> winningNumInput) {
+        while (true) {
+            try {
+                System.out.println(ASK_WINNING_BONUS_NUMBER);
+                String bonusWinningNumInput = Console.readLine();
+                bonusWinningNumValidator.validate(bonusWinningNumInput, winningNumInput);
+                return inputConvertor.convertBonusWinningNum(bonusWinningNumInput);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                System.out.println(illegalArgumentException.getMessage());
+            }
+        }
     }
 }

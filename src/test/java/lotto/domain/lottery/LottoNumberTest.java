96bc534854d8c66b22d6e3b748e12127f3a7b2d9
june-777 <wlwhswnsrl96@gmail.com>
2023-11-08 20:45:41 +0900

    test(LottoNumberTest): 로또 번호 반환 기능 테스트

--- src/test/java/lotto/domain/lottery/LottoNumberTest.java
+++ src/test/java/lotto/domain/lottery/LottoNumberTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoNumberTest {
@@ -32,6 +33,15 @@ class LottoNumberTest {
     void outOfRange(int wrongInput) {
         Assertions.assertThatThrownBy(() -> new LottoNumber(wrongInput))
                 .hasMessage(LottoExceptionMessage.NOT_SATISFY_LOTTO_NUMBER_RANGE.getError());
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2:2", "45:45", "34:34", "17:17"}, delimiter = ':')
+    @DisplayName("[Success] 유효한 범위의 숫자인 경우 해당 숫자의 객체가 생성된다.")
+    void outOfRange(int numberInput, int expectedNumber) {
+        LottoNumber lottoNumber = new LottoNumber(numberInput);
+        Assertions.assertThat(lottoNumber.getNumber()).isSameAs(expectedNumber);
+        Assertions.assertThat(lottoNumber).isEqualTo(new LottoNumber(expectedNumber));
     }
 
 }

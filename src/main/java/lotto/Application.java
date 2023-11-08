25b0de84ba39ca03a3369f0f62fe355e2d0f1725
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 17:36:45 +0900

    feat: 메인클래스 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.controller.GameProcess;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameProcess gameProcess = new GameProcess();
+        gameProcess.buyLotto();
+        gameProcess.publishLotto();
+        gameProcess.enterWiningNumber();
+        gameProcess.checkWiningResult();
     }
 }

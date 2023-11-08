b72c83402ccdd1c4ce17ff2266dffa79e46c066e
jacomyou1026 <jacomyou1026@naver.com>
2023-11-08 22:59:24 +0900

    test: [fix]구조 재조정

---
+++ src/test/java/lotto/BonusTest.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import lotto.domain.Bonus;
+import lotto.view.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class BonusTest {
+
+
+
+
+}
---
+++ src/test/java/lotto/ExceptionTest.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.domain.Exception;
+import lotto.view.ExceptionMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.domain.Exception.NumberCheck;
+import static lotto.domain.Exception.overNumber;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ExceptionTest {
+
+
+
+
+}

46a1622f55af1c3f164b3f8cc4e4b70887c69a14
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:21:02 +0900

    feat : 로또 번호와 보너스 번호 입력 기능 추가

---
+++ src/main/java/lotto/controller/input/InputController.java
@@ -0,0 +1,18 @@
+package lotto.controller.input;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputController {
+
+	public String scanLottoPurchaseMoneyString() {
+		return readLine();
+	}
+
+	public String scanLottoNumberListString() {
+		return readLine();
+	}
+
+	public String scanLottoNumberBonusString() {
+		return readLine();
+	}
+}

499df4e27dee768cee5f9073d1b6dba737797f0a
gundorit <packdev937@gmail.com>
2023-11-08 13:25:41 +0900

    feat (OutputView) : 당첨 통계를 구분하는 문구를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import java.util.StringJoiner;
 import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoPrize;
+import lotto.dto.LottoStatisticResponse;
 import lotto.dto.LottosResponse;
 
 public class OutputView {
@@ -10,6 +12,8 @@ public class OutputView {
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
     private static final String WINNING_NUMBERS_PROMPT = "\n당첨 번호를 입력해 주세요.\n";
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.\n";
+    private static final String WINNING_STATISTICS_HEADER = "\n당첨 통계\n";
+    private static final String DIVIDER = "---\n";
 
     public static void printLottoPurchaseAmountPrompt() {
         System.out.print(LOTTO_PURCHASE_AMOUNT_PROMPT);
@@ -43,5 +47,10 @@ public class OutputView {
 
     public static void printError(String errorMessage) {
         System.out.print(errorMessage + "\n");
+    }
+
+    public static void printLottoStatistic(LottoStatisticResponse response) {
+        System.out.print(WINNING_STATISTICS_HEADER);
+        System.out.print(DIVIDER);
     }
 }

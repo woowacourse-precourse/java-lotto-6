31a58e7b1cfe4067f9a621eed07068675f33e26d
GO-TE <alsrudxo9810@naver.com>
2023-11-08 21:44:14 +0900

    feat(OutputView) 출력하는 클래스 구현 printByLotto() : - 구입한 로또를 출력한다 printWinningStatistics() : - 로또 당첨 결과를 출력한다 printProfit() : 로또로 인한 수익률을 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,41 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import static lotto.util.Message.LottoMessage.FIFTH;
+import static lotto.util.Message.LottoMessage.FOURTH;
+import static lotto.util.Message.LottoMessage.THIRD;
+import static lotto.util.Message.LottoMessage.SECOND;
+import static lotto.util.Message.LottoMessage.FIRST;
+
+import java.util.HashMap;
+import java.util.List;
+import lotto.model.Lotto;
+
+public class OutputView {
+    private final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.\n";
+    private final String LINE = "---";
+    private final String HEADLINE = "당첨 통계";
+    private final String PROFIT = "총 수익률은 %.1f%%입니다.";
+
+    public void printBuyLotto(List<Lotto> lottos) {
+        System.out.printf(LOTTO_COUNT_MESSAGE, lottos.size());
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getLottoValue());
+        }
+        System.out.println();
+    }
+
+    public void printWinningStatistics(HashMap<String, Integer> lottoResult) {
+        System.out.println();
+        System.out.println(HEADLINE);
+        System.out.println(LINE);
+        FIFTH.printMessage(lottoResult.get(FIFTH.getRank()));
+        FOURTH.printMessage(lottoResult.get(FOURTH.getRank()));
+        THIRD.printMessage(lottoResult.get(THIRD.getRank()));
+        SECOND.printMessage(lottoResult.get(SECOND.getRank()));
+        FIRST.printMessage(lottoResult.get(FIRST.getRank()));
+    }
+
+    public void printProfit(double winningProfit) {
+        System.out.printf(PROFIT, winningProfit);
+    }
 }

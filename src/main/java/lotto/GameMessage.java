bcd465a2df35153f3d00f365f58199fbc31d3acf
kijen723 <kij2646@gmail.com>
2023-11-08 21:07:25 +0900

    feat(lotto): Implement print all lotto numbers

--- src/main/java/lotto/GameMessage.java
+++ src/main/java/lotto/GameMessage.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class GameMessage {
     public GameMessage() {
 
@@ -11,6 +13,12 @@ public class GameMessage {
 
     public void lottoCountMessage(int lottoCount) {
         System.out.printf("%d개를 구매했습니다.\n", lottoCount);
+    }
+
+    public void lottoNumbersPrintMessage(List<List<Integer>> lottoNumbers) {
+        for (int i = 0; i < lottoNumbers.size(); i++) {
+            System.out.println(lottoNumbers.get(i));
+        }
     }
 
     public void lottoNumbersInputMessage() {

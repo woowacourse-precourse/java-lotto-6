3791d419321fc6d97b2eedf7af85588a0be87e4f
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-11-08 16:13:05 +0900

    feat: 로또 위치 이동 및 로또 출력 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
-package lotto;
+package lotto.domain;
 
+import lotto.constants.LottoConstant;
+
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,13 +11,17 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        Collections.sort(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoConstant.LOTTO_SIZE.getConstant()) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    @Override
+    public String toString() {
+         return numbers.toString();
+    }
 }

e489e7e9e07bd39712de917f8eef54da4ebc8598
uwoobeat <desktop3711@gmail.com>
2023-11-08 17:33:26 +0900

    test: 로또 판매기 테스트 수정

--- src/test/java/lotto/LottoSellerTest.java
+++ src/test/java/lotto/LottoSellerTest.java
@@ -2,19 +2,25 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import lotto.exception.lottoseller.InsufficientMoneyException;
 import lotto.exception.lottoseller.NonMultipleMoneyException;
 import lotto.helper.FixedNumberGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LottoSellerTest {
-    List<List<Integer>> numbers = List.of(List.of(1, 2, 3, 4, 5, 6));
-    FixedNumberGenerator fixedNumberGenerator = new FixedNumberGenerator(numbers);
-    LottoPublisher lottoPublisher = new LottoPublisher(fixedNumberGenerator);
-    LottoSeller lottoSeller = new LottoSeller(lottoPublisher);
+    FixedNumberGenerator fixedNumberGenerator;
+    LottoPublisher lottoPublisher;
+    LottoSeller lottoSeller;
+
+    @BeforeEach
+    void setUp() {
+        fixedNumberGenerator = new FixedNumberGenerator();
+        lottoPublisher = new LottoPublisher(fixedNumberGenerator);
+        lottoSeller = new LottoSeller(lottoPublisher);
+    }
 
     @DisplayName("로또 판매 테스트")
     @Nested

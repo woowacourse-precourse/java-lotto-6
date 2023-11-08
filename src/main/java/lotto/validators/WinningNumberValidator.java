3c6d7e71bb3a4ae0444e76fddb4041a9e84b4211
jjongwon7 <jjongwon7@naver.com>
2023-11-08 22:21:54 +0900

    feat: 로또 당첨 번호 유효성 검증 기능 추가 당첨 번호가 1~45 이내의 값을 가지는지 체크하고, 검증하는 메서드 작성

--- src/main/java/lotto/validators/WinningNumberValidator.java
+++ src/main/java/lotto/validators/WinningNumberValidator.java
@@ -1,7 +1,12 @@
 package lotto.validators;
 
 import static lotto.constant.ErrorMessage.INPUT_WINNING_NUMBER_COUNT_IS_INCORRECT;
+import static lotto.constant.ErrorMessage.INPUT_WINNING_NUMBER_VALUE_OUT_OF_RANGE;
 import static lotto.constant.LottoInfo.LOTTO_NUMBER_COUNT;
+import static lotto.constant.LottoInfo.MAX_LOTTO_NUMBER;
+import static lotto.constant.LottoInfo.MIN_LOTTO_NUMBER;
+
+import java.util.List;
 
 public class WinningNumberValidator {
     public static void validWinningNumberCount(String[] winningNumbers) {
@@ -9,8 +14,20 @@ public class WinningNumberValidator {
             throw new IllegalArgumentException(INPUT_WINNING_NUMBER_COUNT_IS_INCORRECT);
         }
     }
-    
+
+    public static void validWinningNumberValueInRange(List<Integer> winningNumbers) {
+        for (Integer winningNumber : winningNumbers) {
+            if (!isWinningNumberValueInRange(winningNumber)) {
+                throw new IllegalArgumentException(INPUT_WINNING_NUMBER_VALUE_OUT_OF_RANGE);
+            }
+        }
+    }
+
     private static boolean isCorrectWinningNumberCount(String[] winningNumbers) {
         return winningNumbers.length == LOTTO_NUMBER_COUNT;
+    }
+
+    private static boolean isWinningNumberValueInRange(Integer winningNumber) {
+        return winningNumber >= MIN_LOTTO_NUMBER && winningNumber <= MAX_LOTTO_NUMBER;
     }
 }

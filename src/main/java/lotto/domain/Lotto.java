2345069ac715bee01431831a83f76e6395876af7
Dowon <3120kang@gmail.com>
2023-11-08 20:51:50 +0900

    Refactor(Lotto): 하드코딩 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,28 +5,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String DUPLICATE_ERROR = "[ERROR] 중복되는 숫자는 불가능합니다.";
+    private static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateDuplication(numbers);
-        //Collections.sort(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException();
         }
     }
 
-    private void validateDuplication(List<Integer> numbers){
-        if(numbers.size()!=numbers.stream().distinct().count()){
-            throw new IllegalArgumentException("[ERROR] 중복되는 숫자는 불가능합니다.");
+    private void validateDuplication(List<Integer> numbers) {
+        if (numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR);
         }
     }
 
-    public List<Integer> getNumbers(){
+    public List<Integer> getNumbers() {
         return Collections.unmodifiableList(numbers);
     }
 

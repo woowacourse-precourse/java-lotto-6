818ad7d7c3c8b53781f4a64786275dfd15f5c7d5
ud00ng <dagam1240@gmail.com>
2023-11-08 21:42:38 +0900

    feat : Ranking enum Class 추가

---
+++ src/main/java/lotto/model/Ranking.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+public enum Ranking {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int winningMoney;
+
+    Ranking(int matchCount, int winningMoney) {
+        this.matchCount = matchCount;
+        this.winningMoney = winningMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
+    }
+}

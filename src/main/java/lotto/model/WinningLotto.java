348876b06d35283dd30c6d945da857ee5ce43914
MyStraw <kikongdosa@gmail.com>
2023-11-08 22:07:46 +0900

    Refactor: 삭제 중복된 유효성 검사코드
    InputView에 입력관련 유효성 검사를 함으로서
    WinningLotto 클래스의 유효성검사 코드를 삭제
    안쓰는 import를 삭제

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -1,25 +1,12 @@
 package lotto.model;
 
-import lotto.model.Lotto;
-
 public class WinningLotto {
     private final Lotto lotto;
     private final int bonusNumber;
 
     public WinningLotto(Lotto lotto, int bonusNumber) {
-        validateBonusNumber(lotto, bonusNumber);
         this.lotto = lotto;
         this.bonusNumber = bonusNumber;
-    }
-
-    private void validateBonusNumber(Lotto lotto, int bonusNumber) {
-        if (lotto.getNumbers().contains(bonusNumber)) {
-            throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복이에요");
-        }
-
-        if (bonusNumber <1 || bonusNumber > 45){
-            throw new IllegalArgumentException("보너스 번호가 범위를 벗어났습니다.");
-        }
     }
 
     public Lotto getLotto() {

a6c32db843277a0d8327dfba95933d11e8c2e7cd
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 15:52:47 +0900

    Feat : 쉼표가 5개인지 검증하는 기능 구현
    당첨 번호 입력에서 쉼표가 5개인지 검증하는 기능 구현

--- src/main/java/lotto/validation/WinningNumberInputValidator.java
+++ src/main/java/lotto/validation/WinningNumberInputValidator.java
@@ -5,7 +5,14 @@ import lotto.domain.WinningNumber;
 public class WinningNumberInputValidator {
 
     public boolean isCommaFive(String winningNumberInput) {
-        return false;
+        int commaCount = 0;
+        for (int i = 0; i < winningNumberInput.length(); i++) {
+            char oneCharacter = winningNumberInput.charAt(i);
+            if (oneCharacter == ',') {
+                commaCount++;
+            }
+        }
+        return commaCount == 5;
     }
 
     public boolean isLottoNumber(int oneLottoNumber) {

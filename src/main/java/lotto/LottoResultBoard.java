91a27f6d141d21f7a224761a3d696d00d463e3a3
choiejae <ejae8319@gmail.com>
2023-11-08 16:03:17 +0900

    feat(LottoResultBoard): Lotto 결과가 저장되는 LottoResultBoard 기능 구현
    - EnumMap 으로 LottoRank 별 matchCount 를 저장한다
    - findMatchCount(): key 값을 통해 value 값을 반환한다
    - calculateWinningAmountSum(): 총 수익금을 반환한다
    - calculateProfitabilityPercent(): PurchaseAmount 를 받아와 수익률을 계산한다

---
+++ src/main/java/lotto/LottoResultBoard.java
@@ -0,0 +1,41 @@
+package lotto;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LottoResultBoard {
+
+    public static final int NUMBER_PERCENT = 100;
+    public final Map<LottoRank, Long> lottoRankBoard;
+
+    private LottoResultBoard(final Map<LottoRank, Long> lottoRankBoard) {
+        this.lottoRankBoard = lottoRankBoard;
+    }
+
+    public static LottoResultBoard of(final Map<LottoRank, Long> lottoRankBoard) {
+        return new LottoResultBoard(lottoRankBoard);
+    }
+
+    public long findMatchCount(final LottoRank lottoRank) {
+        return lottoRankBoard.get(lottoRank);
+    }
+
+    public long calculateWinningAmountSum() {
+        long winningAmountSum = 0;
+
+        for (Entry<LottoRank, Long> entrySet : lottoRankBoard.entrySet()) {
+            long rank = entrySet.getKey().getWinningAmount();
+            long count = entrySet.getValue();
+            winningAmountSum += rank * count;
+        }
+        return winningAmountSum;
+    }
+
+    public double calculateProfitabilityPercent(final PurchaseAmount purchaseAmount) {
+        long grossRevenue = calculateWinningAmountSum();
+        int cost = purchaseAmount.getPurchaseAmount();
+        double profitabilityRatio = (double) grossRevenue / cost;
+
+        return profitabilityRatio * NUMBER_PERCENT;
+    }
+}

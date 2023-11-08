1f0ea25ee694d981ed9ae6acfa658b5ca0db7f58
wooni97 <jhw970714@gmail.com>
2023-11-08 20:19:53 +0900

    refactor: 상수 클래스로 이동함에 따른 상수 선언 삭제

--- src/main/java/lotto/utils/RandomNumberGenerator.java
+++ src/main/java/lotto/utils/RandomNumberGenerator.java
@@ -2,16 +2,12 @@ package lotto.utils;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.constant.Numbers;
 
 public class RandomNumberGenerator {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int QUANTITY = 6;
-
     private RandomNumberGenerator() {
     }
-
     public static List<Integer> pickUniqueNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, QUANTITY);
+        return Randoms.pickUniqueNumbersInRange(Numbers.RANGE_MIN_NUMBER, Numbers.RANGE_MAX_NUMBER, Numbers.INVALID_SIZE);
     }
 }

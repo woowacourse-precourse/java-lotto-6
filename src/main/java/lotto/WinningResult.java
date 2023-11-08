fa6972f78a0df24544a4989542594be8f7bbf6c1
MisaSohee <sohee5951@naver.com>
2023-11-08 14:49:43 +0900

    refactor(WinningResult): 당첨 통계 출력 기능 분리

--- src/main/java/lotto/WinningResult.java
+++ src/main/java/lotto/WinningResult.java
@@ -19,13 +19,25 @@ public class WinningResult {
     public void printResult() {
         System.out.println("당첨 통계");
         System.out.println("---------");
+        printMatchCounts();
+    }
+
+    private void printMatchCounts() {
         NumberFormat numberFormat = NumberFormat.getInstance();
         for (Rank rank : Rank.values()) {
             if (rank == Rank.MISS) continue;
-            System.out.println(rank.getCountOfMatch() + "개 일치" + rank.getBonus() +
-                    " (" + numberFormat.format(rank.getWinningMoney()) + "원) - " +
-                    result.getOrDefault(rank, 0) + "개");
+            printMatchCount(rank, numberFormat);
         }
     }
 
+    private void printMatchCount(Rank rank, NumberFormat numberFormat) {
+        int count = getCount(rank);
+        int matchCount = rank.getCountOfMatch();
+        String bonusNumber = rank.getBonus();
+        long winningMoney = rank.getWinningMoney();
+
+        System.out.println(rank.getCountOfMatch() + "개 일치" + rank.getBonus() +
+                " (" + numberFormat.format(rank.getWinningMoney()) + "원) - " +
+                result.getOrDefault(rank, 0) + "개");
+    }
 }

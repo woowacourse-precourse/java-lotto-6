4951acf6a9cd0bd6348b4f18a6d7cc7b62120986
Jun Hyeok Choi <chrisgold@naver.com>
2023-11-08 20:00:46 +0900

    style(All) : 가독성 향상 작업, 코드 정렬 및 주석처리

--- src/main/java/lotto/Lotto_Wallet.java
+++ src/main/java/lotto/Lotto_Wallet.java
@@ -1,33 +1,42 @@
 package lotto;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class Lotto_Wallet {
-    private List<Lotto> Lottos =  new ArrayList<>();;
+    private List<Lotto> Lottos = new ArrayList<>();
+    ;
     private HashMap<String, Integer> winning_count = new HashMap<>();
 
-    public Lotto_Wallet(){
-        for(Prize p : Prize.values()){
-            winning_count.put(p.name(),0);
+    // 생성자 winning_count에 각 등수별로 횟수를 0으로 초기회
+    public Lotto_Wallet() {
+        for (Prize p : Prize.values()) {
+            winning_count.put(p.name(), 0);
         }
     }
-    public void setLottos(Lotto lotto) { // 로토를 지갑에 넣기
+
+    // 로또를 지갑에 넣기
+    public void setLottos(Lotto lotto) {
         Lottos.add(lotto);
     }
 
+
     // 지갑에 든 모든 로또를 확인 -> HashMap 값을 변경
-    public void Check_All_Lotto(Lotto winning, Integer bonus){
-        for(Lotto my_lotto : Lottos){
+    // Check_All_Lotto에 Check_One_Lotto사용
+    public void Check_All_Lotto(Lotto winning, Integer bonus) {
+        for (Lotto my_lotto : Lottos) {
             Prize rank = Check_One_Lotto(my_lotto, winning, bonus);
             String rank_name = rank.name();
             int temp = winning_count.get(rank_name);
-            winning_count.put(rank_name,temp+1);
+            winning_count.put(rank_name, temp + 1);
         }
     }
+
     // 1개의 로또 확인 : 내로또, 당첨로또, 보너스 번호를 입력 받아 -> Prize 등수 반환
-    public Prize Check_One_Lotto(Lotto lotto, Lotto winning, Integer bonus){
+    // Check_One_Lotto에 bonus_check, numbers_check, find_Rank 쓰임
+    public Prize Check_One_Lotto(Lotto lotto, Lotto winning, Integer bonus) {
         List<Integer> my_nums = lotto.getNumbers();
         List<Integer> winning_nums = winning.getNumbers();
 
@@ -36,18 +45,26 @@ public class Lotto_Wallet {
 
         return find_Rank(lotto_cnt, bonus_cnt);
     }
-    private int bonus_check(List<Integer> my_nums, Integer bonus){ // 보너스 번호가 있는지 체크
-        if (my_nums.contains(bonus)){ return 1;}
+
+    // 보너스 번호 있는지 체크
+    private int bonus_check(List<Integer> my_nums, Integer bonus) {
+        if (my_nums.contains(bonus)) {
+            return 1;
+        }
         return 0;
     }
-    private int numbers_check(List<Integer> my_nums, List<Integer> winning_nums){ // 로또(보너스X) 당첨 개수 체크
+
+    // 로또 숫자만 몇 개 맞는지 체크
+    private int numbers_check(List<Integer> my_nums, List<Integer> winning_nums) {
         int lotto_cnt = 0;
         HashSet<Integer> interA = new HashSet<>(my_nums);
         HashSet<Integer> interB = new HashSet<>(winning_nums);
         interA.retainAll(interB);
         return interA.size();
     }
-    private Prize find_Rank(int lotto_cnt,int bonus_cnt){      // 로또 중복수와 보너스 수로 랭킹 반환
+
+    // 로또 중복수와 보너스 수로 랭킹 반환
+    private Prize find_Rank(int lotto_cnt, int bonus_cnt) {
         if (lotto_cnt == 6)
             return Prize.First;
         if (lotto_cnt == 5 && bonus_cnt == 1)
@@ -61,37 +78,47 @@ public class Lotto_Wallet {
         return Prize.None;
     }
 
-    public void Result_Print(){
+
+    // 당첨 결과 개수 프린트
+    // Result_Print에 getWinning_count 사용
+    public void Result_Print() {
         System.out.print("\n당첨 통계\n---\n");
-        System.out.print("3개 일치 (5,000원) - " + winning_count.get(Prize.Fifth.name())+"개\n");
-        System.out.print("4개 일치 (50,000원) - " + winning_count.get(Prize.Fourth.name())+"개\n");
-        System.out.print("5개 일치 (1,500,000원) - " + winning_count.get(Prize.Third.name())+"개\n");
-        System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winning_count.get(Prize.Second.name())+"개\n");
-        System.out.print("6개 일치 (2,000,000,000원) - " + winning_count.get(Prize.First.name())+"개\n");
+        System.out.print("3개 일치 (5,000원) - " + winning_count.get(Prize.Fifth.name()) + "개\n");
+        System.out.print("4개 일치 (50,000원) - " + winning_count.get(Prize.Fourth.name()) + "개\n");
+        System.out.print("5개 일치 (1,500,000원) - " + winning_count.get(Prize.Third.name()) + "개\n");
+        System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winning_count.get(Prize.Second.name()) + "개\n");
+        System.out.print("6개 일치 (2,000,000,000원) - " + winning_count.get(Prize.First.name()) + "개\n");
     }
 
     public HashMap<String, Integer> getWinning_count() {
         return winning_count;
     }
 
-    public void print_Return(int n){
+
+    // 수익률 출력
+    // print_Return에 RateOfReturn 사용
+    public void print_Return(int n) {
         float ret = RateOfReturn(n);
-        float rounded_ret =Round(ret, 1);
-        System.out.print("총 수익률은 "+ rounded_ret + "%입니다.\n");
+        float rounded_ret = Round(ret, 1);
+        System.out.print("총 수익률은 " + rounded_ret + "%입니다.\n");
     }
-    private float RateOfReturn(int n){
+
+    // 로또 산 금액 대비 수익률 구함
+    // RateOfReturn에 Round사용
+    private float RateOfReturn(int n) {
         float cost = 1000 * n;
         float total_prize = 0;
-        for (Prize prize: Prize.values()){
+        for (Prize prize : Prize.values()) {
             String name = prize.name();
-            int individual_cnt= winning_count.get(name);
+            int individual_cnt = winning_count.get(name);
             int individual_prize = prize.getPrize();
             total_prize += individual_prize * individual_cnt;
         }
         return total_prize / cost * 100;
     }
-    private float Round(float num, int under_dot){
-        double location = Math.pow(10,under_dot);
-        return Math.round(num * location)/(float)location;
+
+    private float Round(float num, int under_dot) {
+        double location = Math.pow(10, under_dot);
+        return Math.round(num * location) / (float) location;
     }
 }
--- src/main/java/lotto/Prize.java
+++ src/main/java/lotto/Prize.java
@@ -1,14 +1,16 @@
 package lotto;
 
-public enum Prize{
-    First(2000000000) ,Second(30000000), Third(1500000), Fourth(50000), Fifth(5000), None(0);
+public enum Prize {  // 등수별 상금을 Enum화
+    First(2000000000), Second(30000000), Third(1500000), Fourth(50000), Fifth(5000), None(0);
     private final int prize;
+
+    // Enum 생생자
     Prize(int prize) {
         this.prize = prize;
     }
 
+    // 각 상금 getter
     public int getPrize() {
         return prize;
     }
-
 }

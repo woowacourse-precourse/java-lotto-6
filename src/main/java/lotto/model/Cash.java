9d6538602316054c0d779b2f66c308ee51701132
ParkKyuIl <hakim@nex.market>
2023-11-08 11:28:28 +0900

    feat: 현금 클래스 생성

---
+++ src/main/java/lotto/model/Cash.java
@@ -0,0 +1,31 @@
+package lotto.model;
+
+
+public class Cash {
+
+    private final int UNIT = 1000;
+    private final int amount;
+    public Cash(int amount) {
+        validateAmount(amount);
+        validateAmountUnit(amount);
+        this.amount = amount;
+    }
+
+    public int getCount() {
+        return this.amount / UNIT;
+    }
+
+    public int getAmount() { return this.amount; }
+
+    private void validateAmount(int amount) {
+        if(amount < 1000) {
+            throw new IllegalArgumentException("최소 금액은 1000원 입니다.");
+        }
+    }
+
+    private void validateAmountUnit(int amount) {
+        if (amount % UNIT != 0) {
+            throw new IllegalArgumentException("1000원 단위로만 참여 가능합니다.");
+        }
+    }
+}

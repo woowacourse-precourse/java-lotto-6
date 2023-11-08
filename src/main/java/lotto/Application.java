8df431e74f65e19e51b813f835fbd06ebdfac2fa
dmdwns2 <ssreki1@naver.com>
2023-11-08 13:27:42 +0900

    feat: implementation of features invoke to input class to get value create generate lotto create list to list method

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,31 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+    public static void listToList(List<Integer> fromList, List<Integer> toList) {
+        Collections.sort(fromList);
+        toList.addAll(fromList);
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Analyzer analyzer = new Analyzer();
+
+        int ticket = input.inputPurchaseAmount();
+        List<Integer> winningNumbers = input.inputWinningNumbers();
+        int bonusNumber = input.inputBonusNumber();
+        List<Integer> allTicketNumbers = new ArrayList<>();
+
+        for (int i = 0; i < ticket; i++) {
+            listToList(lotto.generateLotto(), allTicketNumbers);
+        }
+
+
     }
 }

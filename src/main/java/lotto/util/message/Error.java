56ec2efb76b96c5e341b65e3f26d138247b04b08
BH946 <kobh2895@gmail.com>
2023-11-08 20:57:22 +0900

    feat: 메시지 상수들 추가

--- src/main/java/lotto/util/message/Error.java
+++ src/main/java/lotto/util/message/Error.java
@@ -1,5 +1,5 @@
-package lotto.message;
+package lotto.util.message;
 
 public class Error {
-
+    public static final String VALID_INPUT = "[ERROR]잘못된 값을 입력하셨습니다. 다시 입력해주세요.";
 }
--- src/main/java/lotto/util/message/Output.java
+++ src/main/java/lotto/util/message/Output.java
@@ -1,4 +1,5 @@
-package lotto.message;
+package lotto.util.message;
 
 public class Output {
+    public static final String PURCHASE_COUNT = "개를 구매했습니다.";
 }

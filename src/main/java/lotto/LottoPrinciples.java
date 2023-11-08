94b2dffe99740c7f2068fa6e540bff97b0461933
junseoks-99 <angec5645@naver.com>
2023-11-08 20:23:55 +0900

    feat: 서로 매치되는 숫자의 개수 상수 추가

--- src/main/java/lotto/LottoPrinciples.java
+++ src/main/java/lotto/LottoPrinciples.java
@@ -4,7 +4,11 @@ public enum LottoPrinciples {
     MIN_NUMBER(1),
     MAX_NUMBER(45),
     LIMIT_NUMBER(6),
-    MAX_ALL_NUMBER(12);
+    MAX_SUM_COUNT(12),
+    SIXTH_MATCHES(6),
+    FIFTH_MATCHES(5),
+    FOURTH_MATCHES(4),
+    THIRD_MATCHES(3);
 
     private Integer number;
 

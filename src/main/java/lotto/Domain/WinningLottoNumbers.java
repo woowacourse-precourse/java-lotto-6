c79d8ad9bcf311b0a7b2ab99e0ffd2b206e85421
lee-JunR <wns2610@icloud.com>
2023-11-08 16:47:55 +0900

    chore : "검증 메시지에 [Error] 추가"

--- src/main/java/lotto/Domain/WinningLottoNumbers.java
+++ src/main/java/lotto/Domain/WinningLottoNumbers.java
@@ -2,11 +2,12 @@ package lotto.Domain;
 
 import java.util.List;
 
-public class WinningNumber {
+public class WinningLottoNumbers {
+
   private final List<Integer> numbers;
   private final int bonusNumber;
 
-  public WinningNumber(List<Integer> numbers, int bonusNumber) {
+  public WinningLottoNumbers(List<Integer> numbers, int bonusNumber) {
     validate(numbers, bonusNumber);
     this.numbers = numbers;
     this.bonusNumber = bonusNumber;
@@ -14,11 +15,12 @@ public class WinningNumber {
 
   private void validate(List<Integer> numbers, int bonusNumber) {
     if (numbers.size() != 6) {
-      throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
+      throw new IllegalArgumentException("[Error] 로또 번호의 개수는 6개여야 합니다.");
     }
     if (numbers.contains(bonusNumber)) {
-      throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복될 수 없습니다.");
+      throw new IllegalArgumentException("[Error] 보너스 번호는 로또 번호와 중복될 수 없습니다.");
     }
+
   }
 
   public List<Integer> getNumbers() {
@@ -29,6 +31,5 @@ public class WinningNumber {
     return bonusNumber;
   }
 
-  // 당첨 여부 확인 메서드 등 다른 필요한 기능을 구현할 수 있습니다.
 }
 

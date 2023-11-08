7792a7348c84694cf6669295bf70275bf2ab515b
minkyuu <minkyu023@gmail.com>
2023-11-08 22:22:48 +0900

    refactor: Game객체 리팩토링

--- src/main/java/lotto/game/Game.java
+++ src/main/java/lotto/game/Game.java
@@ -1,15 +1,14 @@
 package lotto.game;
 
 import lotto.buyer.Buyer;
-import lotto.company.LotteryResult;
 import lotto.company.Lotto;
 import lotto.company.LottoCompany;
 
 import java.util.List;
 
 public class Game {
-    private Buyer buyer;
-    private LottoCompany lottoCompany;
+    private final Buyer buyer;
+    private final LottoCompany lottoCompany;
 
     public Game() {
         this.buyer = new Buyer();

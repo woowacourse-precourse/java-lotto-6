822d2b4b2e57bbf9554b9afb03ea410ecbccc48d
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 15:30:27 +0900

    feat: 로또 컨트롤러 구조 수정 및 예외처리 로직 추가

--- src/main/java/lotto/Controller/LottoController.java
+++ src/main/java/lotto/Controller/LottoController.java
@@ -1,23 +1,24 @@
 package lotto.Controller;
 
-import static lotto.enums.ErrorMessage.NUMBER_FORMAT_ERROR;
-
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
-import lotto.model.LottoRankPolicy;
+import java.util.Map;
+import lotto.enums.LottoRank;
+import lotto.model.LottoResultPolicy;
 import lotto.model.LottoResultManager;
 import lotto.model.LottoPurchaseManager;
 import lotto.util.ProfitCalculator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-import org.junit.jupiter.api.DisplayName;
 
 public class LottoController implements Controller {
 
     private InputView inputView;
     private OutputView outputView;
     private LottoPurchaseManager lottoPurchaseManager;
+    private LottoResultManager lottoResultManager;
+    private LottoResultPolicy lottoResultPolicy = new LottoResultPolicy();
 
     public LottoController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
@@ -27,41 +28,66 @@ public class LottoController implements Controller {
     public void run() {
         purchaseLotto();
         generateLotto();
-        getWinningNumber();
+        readWinningNumber();
+        readBonusNumber();
         checkLottoResult();
     }
 
     private void purchaseLotto() {
-        outputView.promptForPurchaseAmount();
-        int purchaseAmount = inputView.readPurchaseAmount();
-        lottoPurchaseManager = new LottoPurchaseManager(purchaseAmount);
+        try {
+            outputView.promptForPurchaseAmount();
+            int purchaseAmount = inputView.readPurchaseAmount();
+            lottoPurchaseManager = new LottoPurchaseManager(purchaseAmount);
+        } catch (NumberFormatException e){
+            outputView.displayNumberFormatError(e);
+            purchaseLotto();
+        } catch (IllegalArgumentException e){
+            outputView.displayArgumentError(e);
+            purchaseLotto();
+        }
     }
 
     private void generateLotto() {
         lottoPurchaseManager.generateLottos();
-        outputView.displayPurchaseAmount(lottoPurchaseManager.getCount());
+        outputView.displayPurchaseAmount(lottoPurchaseManager.getPurchaseCount());
         outputView.displayPurchaseLotto(lottoPurchaseManager.getLottos());
     }
 
-    private void getWinningNumber() {
-        //사용자에게 당첨될 로또 번호와 보너스 번호 받아오기
-        List<Integer> winningNumbers = new ArrayList<Integer>();
-        int bonusNumber = 0;
-        outputView.promptForWinningNumbers();
-        winningNumbers = inputView.readWinningNumbers();
-        Collections.sort(winningNumbers);
-        outputView.promptForBonusNumbers();
-        bonusNumber = inputView.readBonusNumber();
+    private void readWinningNumber() {
+        try {
+            outputView.promptForWinningNumbers();
+            List<Integer> winningNumbers = inputView.readWinningNumbers();
+            lottoResultPolicy.setWinningNumbers(winningNumbers);
+        } catch (NumberFormatException e){
+            outputView.displayNumberFormatError(e);
+            readWinningNumber();
+        } catch (IllegalArgumentException e){
+            outputView.displayArgumentError(e);
+            readWinningNumber();
+        }
+    }
+
+    private void readBonusNumber() {
+        try {
+            outputView.promptForBonusNumbers();
+            int bonusNumber = inputView.readBonusNumber();
+            lottoResultPolicy.setBonusNumber(bonusNumber);
+        } catch (NumberFormatException e){
+            outputView.displayNumberFormatError(e);
+            readBonusNumber();
+        } catch (IllegalArgumentException e){
+            outputView.displayArgumentError(e);
+            readBonusNumber();
+        }
     }
 
     private void checkLottoResult() {
-        //TODO 구현
-        //필요한 값 계산하기
-        LottoResultManager lottoResultManager = new LottoResultManager(
-            lottoPurchaseManager.getLottos(), new LottoRankPolicy(winningNumbers, bonusNumber));
-        double lottoProfit = ProfitCalculator.calculateProfitPercentage(purchaseAmount,
+        lottoResultManager = new LottoResultManager(
+            lottoPurchaseManager.getLottos(), lottoResultPolicy);
+        Map<LottoRank, Integer> lottoResults = lottoResultManager.getLottoResults();
+        double lottoProfit = ProfitCalculator.calculateProfitPercentage(lottoPurchaseManager.getPurchaseAmount(),
             lottoResultManager.getWinningAmount());
-        outputView.displayResult(lottoResultManager.getLottoResult(), lottoProfit);
+        outputView.displayResult(lottoResults,lottoProfit);
     }
 
 }

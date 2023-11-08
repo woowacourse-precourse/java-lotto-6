b6b267b5aae1ee0fbec0683bac02b30c75817669
Ukj0ng <xb253q@gmail.com>
2023-11-08 19:24:18 +0900

    fix(LottoPrize): 오타 수정
    수정 사항:
    - 3등의 일치 번호 개수
    - 5등의 일치 번호 개수

--- src/main/java/lotto/model/LottoPrize.java
+++ src/main/java/lotto/model/LottoPrize.java
@@ -3,9 +3,9 @@ package lotto.model;
 public enum LottoPrize {
     FIRST_PRIZE(6, 2000000000, false),
     SECOND_PRIZE(5, 30000000, true),
-    THIRD_PRIZE(3, 1500000, false),
+    THIRD_PRIZE(5, 1500000, false),
     FOURTH_PRIZE(4, 50000, false),
-    FIFTH_PRIZE(5, 5000, false);
+    FIFTH_PRIZE(3, 5000, false);
 
 
     private final int matchedNumbers;

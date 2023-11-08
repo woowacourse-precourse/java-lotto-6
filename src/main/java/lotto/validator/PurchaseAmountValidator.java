9721fbf19dffaf7580a38bad4a7d064f80dbb9e0
Laniel Park <Laniel8177@gmail.com>
2023-11-08 21:48:56 +0900

    feat : PurchaseAmountValidator (Test Passed)

--- src/main/java/lotto/validator/PurchaseAmountValidator.java
+++ src/main/java/lotto/validator/PurchaseAmountValidator.java
@@ -12,11 +12,24 @@ public class PurchaseAmountValidator {
 
 
     private static void validateInteger(String amount) {
+        try {
+            Integer.parseInt(amount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid amount format. Please enter a valid integer.");
+        }
     }
 
     private static void validatePositive(String amount) {
+        int intValue = Integer.parseInt(amount);
+        if (intValue <= 0) {
+            throw new IllegalArgumentException("Amount must be a positive multiple of 1000.");
+        }
     }
 
     private static void validateMultipleOf1000(String amount) {
+        int intValue = Integer.parseInt(amount);
+        if (intValue % 1000 != 0) {
+            throw new IllegalArgumentException("Amount must be a positive multiple of 1000.");
+        }
     }
 }

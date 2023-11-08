fa8df242cf613a5ec3cab1044972ec558a29d78e
sejin park <seijin0722@naver.com>
2023-11-09 00:00:52 +0900

    test(InputViewTest,InputValidatorTest) : input 테스트 추가

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.Buyer;
+import lotto.domain.GenerateRandomNum;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,32 +10,36 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static lotto.utils.ErrorMessages.CHECK_NUMBER_IN_RANGE;
+import static lotto.utils.ErrorMessages.INPUT_NUMBER_FORMAT;
+import static lotto.view.InputValidator.parseNumber;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
-//    private InputView inputView;
-//    private ByteArrayOutputStream outputStream;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        inputView = new InputView();
-//        outputStream = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(outputStream));
-//    }
-//
-//    @AfterEach
-//    public void tearDown() {
-//        System.setOut(System.out);
-//    }
-//
-//    @Test
-//    @DisplayName("금액을 지불하고 몇개를 구매했는지 출력 문구 테스트")
-//    public void getTickets() {
-//        int payment = 3000;
-//
-//        inputView.printTicketCntForTest(payment);
-//
-//        String expectedOutput = "\n3개를 구매했습니다.\n";
-//        assertEquals(expectedOutput, outputStream.toString());
-//    }
+    private InputView inputView;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void setUp() {
+        inputView = new InputView();
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    @DisplayName("printTicketCnt 메서드 테스트 - 금액을 지불 후 몇 개를 구매했는지 출력 문구")
+    public void printTicketCnt() {
+        Buyer buyer = new Buyer(3000, 3);
+
+        inputView.printTicketCnt(buyer);
+
+        String expectedOutput = "\n3개를 구매했습니다.\n";
+        assertEquals(expectedOutput, outputStream.toString());
+    }
 }

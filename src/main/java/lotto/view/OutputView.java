f7070953198373ac4f8e6ea0cac523650dd3bcf6
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 22:03:00 +0900

    [FEAT] OutputView에 printLottoResult ,printException 기능 구현
    .

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,11 +1,23 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.LottoPrize;
 
 public class OutputView {
     private static final String LOTTO_COUNT = "개를 구매했습니다.";
     private static final String PROFITRATE = "총 수익률은 %.1f%%입니다.";
+    private static final String DECIMALFORMAT = "#,###원";
+    private static final String ONLY_SECOND = "개 일치, 보너스 볼 일치 (";
+    private static final String OTHER_PRIZE = "개 일치 (";
+    private static final String COUNT = "개";
+
+
+    private static final String PRINT_FORMAT =") - ";
 
     public static void printLottoCount(int lottoCount) {
         System.out.println();
@@ -13,9 +25,35 @@ public class OutputView {
     }
 
     public static void printLottos(List<Lotto> lottos) {
-        for (Lotto lotto : lottos) {
-            System.out.println(lotto);
+      for (Lotto lotto : lottos) {
+            List<Integer> sortedNumbers = new ArrayList<>(lotto.getNumbers());
+            Collections.sort(sortedNumbers);
+            System.out.println(sortedNumbers);
         }
         System.out.println();
+    }
+
+    public static void printLottoResult(EnumMap<LottoPrize, Integer> prizeCounts, int totalCost) {
+        DecimalFormat decimalFormat = new DecimalFormat(DECIMALFORMAT);
+        double sum = 0;
+        for (LottoPrize prize : LottoPrize.values()) {
+            int prizeMoney = prize.getPrizeMoney();
+            String formattedPrizeMoney = decimalFormat.format(prizeMoney);
+            int count = prizeCounts.getOrDefault(prize, 0);
+            if (prize != LottoPrize.NOTHING && prize == LottoPrize.SECOND) {
+                sum += count * prizeMoney;
+                System.out.println(prize.getMatchedCount() + ONLY_SECOND + formattedPrizeMoney + PRINT_FORMAT + count + COUNT);
+            }
+            if (prize != LottoPrize.NOTHING) {
+                sum += count * prizeMoney;
+                System.out.println(prize.getMatchedCount() + OTHER_PRIZE + formattedPrizeMoney + PRINT_FORMAT + count + COUNT);
+            }
+        }
+        double profitRate = (sum / totalCost) * 100;
+        System.out.println(String.format(PROFITRATE, profitRate));
+    }
+
+    public static void printException(Exception e){
+        System.out.println(e.getMessage());
     }
 }

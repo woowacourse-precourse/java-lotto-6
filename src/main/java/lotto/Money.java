b242c4a1d955602e8079d2c5adab1f44107b9ebc
sora0319 <sojeoung536985@gmail.com>
2023-11-08 10:58:28 +0900

    refactor (Money) : Create a class to manage money

---
+++ src/main/java/lotto/Money.java
@@ -0,0 +1,22 @@
+package lotto;
+
+public class Money {
+    private final int money;
+
+    public Money(int money) {
+        validate(money);
+        this.money = money;
+    }
+
+    private void validate(int money) {
+        validateMultiple(money);
+    }
+
+    private void validateMultiple(int money) {
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 로또 한장이 1000원이므로 구매금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
+
+
+}
---
+++ src/test/java/lotto/MoneyTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @DisplayName("입력 값이 1000으로 나누어지는 값인지 확인한다")
+    @Test
+    void checkMultipleOfThousand() {
+        assertThatThrownBy(() -> new Money(1500))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 한장이 1000원이므로 구매금액은 1000원 단위로 입력해야 합니다.");
+    }
+}

f4eb6361fbb718193f8307974a291cf24b14bfd6
eora21 <eora21@naver.com>
2023-11-08 13:21:18 +0900

    test: 💍 수정 로또 등급 계산 시 로또 매칭 결과 이용하도록 변경한 사항 적용

--- src/test/java/lotto/domain/LottoWinningTierTest.java
+++ src/test/java/lotto/domain/LottoWinningTierTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import domain.LottoWinningTier;
+import dto.WinningMatchResult;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -16,26 +17,30 @@ public class LottoWinningTierTest {
     @Test
     @DisplayName("일치한 로또 숫자 개수가 음수라면 예외를 발생시킨다.")
     void calculateLottoWinningTierByNegativeCount() {
-        assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(-1, false));
+        assertThrows(IllegalArgumentException.class,
+                () -> LottoWinningTier.calculateTier(new WinningMatchResult(-1, false)));
     }
 
     @Test
     @DisplayName("일치한 로또 숫자 개수가 6개를 초과하면 예외를 발생시킨다.")
     void calculateLottoWinningTierByOverCount() {
-        assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(7, false));
+        assertThrows(IllegalArgumentException.class,
+                () -> LottoWinningTier.calculateTier(new WinningMatchResult(7, false)));
     }
 
     @Test
     @DisplayName("일치한 로또 숫자 개수가 6개이며 보너스 번호까지 맞췄다면 예외를 발생시킨다.")
     void calculateLottoWinningTierByMaxCountPlusBonus() {
-        assertThrows(IllegalArgumentException.class, () -> LottoWinningTier.calculateTier(6, true));
+        assertThrows(IllegalArgumentException.class,
+                () -> LottoWinningTier.calculateTier(new WinningMatchResult(6, true)));
     }
 
     @ParameterizedTest
     @DisplayName("일치한 로또 숫자 개수가 3 미만이면 빈 옵셔널 값을 반환한다.")
     @MethodSource("matchCountAndMatchBonus")
     void calculateLottoWinningTierByLessMinMatchCount(int matchCount, boolean matchBonus) {
-        Optional<LottoWinningTier> tierOptional = LottoWinningTier.calculateTier(matchCount, matchBonus);
+        Optional<LottoWinningTier> tierOptional = LottoWinningTier.calculateTier(
+                new WinningMatchResult(matchCount, matchBonus));
         assertThat(tierOptional).isEmpty();
     }
 
@@ -54,7 +59,8 @@ public class LottoWinningTierTest {
     @DisplayName("일치한 로또 숫자 개수가 3 이상이면 상태가 들어있는 옵셔널 값을 반환한다.")
     @MethodSource("matchCountMatchBonusAndExpectTier")
     void calculateLottoMatchStateByLessMinCount(int matchCount, boolean matchBonus, LottoWinningTier expectTier) {
-        Optional<LottoWinningTier> stateOptional = LottoWinningTier.calculateTier(matchCount, matchBonus);
+        Optional<LottoWinningTier> stateOptional = LottoWinningTier.calculateTier(
+                new WinningMatchResult(matchCount, matchBonus));
         assertThat(stateOptional).hasValue(expectTier);
     }
 

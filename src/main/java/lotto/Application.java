7e83636f836dda4649436199e7356911aa01189c
SiHyun-Lee113 <tlgus0113@gmail.com>
2023-11-08 20:15:00 +0900

    feat : 전체 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import lotto.Domain.GameInfo;
+import lotto.Domain.Lotto;
+import lotto.Service.InputService;
+import lotto.Service.LottoService;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputService inputService = new InputService();
+        List<Lotto> lottoTickets = inputService.createLottoTickets();
+        GameInfo gameInfo = inputService.createGameInfo();
+
+        LottoService lottoService = new LottoService(lottoTickets, gameInfo);
+        lottoService.checkLottoResult();
     }
 }

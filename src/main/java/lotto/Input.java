7dbaf481c088fbf64529dd84fb3670f8c1534ae5
elizabethgim <yujingim@naver.com>
2023-11-08 21:04:23 +0900

    feat: 수익률 계산을 위한 금액도 반환하기

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -8,17 +8,20 @@ import java.util.stream.Collectors;
 
 public class Input {
     private static final String ERROR_MESSAGE = "[ERROR]";
+    private int money = 0;
 
     public int getNumberOfLotto(){
-        System.out.println("구입금액을 입력해 주세요.\n");
 
-        int money = Integer.parseInt(Console.readLine());
-        validateMoney(money);
 
         int numberOfLotto = calculate(money);
         return numberOfLotto;
     }
-
+    public int getMoney(){
+        System.out.println("구입금액을 입력해 주세요.\n");
+        this.money = Integer.parseInt(Console.readLine());
+        validateMoney(money);
+        return money;
+    }
     private void validateMoney(int money){
         if(money % 1000 != 0){
             throw new IllegalArgumentException(ERROR_MESSAGE+" 로또는 1000원 단위로 구매가 가능합니다.");

e3aae7c290e9887cbe65ceaceda055b0f68b1794
swhyeon98 <smreo75@gmail.com>
2023-11-08 19:23:02 +0900

    refactor: 로또 번호 생성기 상수를 글로벌 상수로 이동

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 
-public class RandomLottoGenerator {
+import static lotto.grobal.LottoConstants.MIN_LOTTO_NUMBER;
+import static lotto.grobal.LottoConstants.MAX_LOTTO_NUMBER;
+import static lotto.grobal.LottoConstants.LOTTO_NUMBER_SIZE;
 
-    private static final int MIN_LOTTO_NUMBER = 1;
-    private static final int MAX_LOTTO_NUMBER = 45;
-    private static final int LOTTO_NUMBER_SIZE = 6;
+public class RandomLottoGenerator {
 
     public List<Integer> generate() {
         return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_SIZE);

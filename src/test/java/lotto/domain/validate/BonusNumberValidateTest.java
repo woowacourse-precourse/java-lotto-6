ca06feabcc0e9ec933e3710bbe581e492c82bd46
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 12:33:08 +0900

    test(BonusNumberValidateTest): 보너스 번호 검증 기능 예외 테스트 코드 추가

--- src/test/java/lotto/domain/validate/BonusNumberValidateTest.java
+++ src/test/java/lotto/domain/validate/BonusNumberValidateTest.java
@@ -1,13 +1,15 @@
 package lotto.domain.validate;
 
+import lotto.domain.validate.exception.InputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class BonusNumberValidateTest {
+class BonusNumberValidateTest {
     InputValidator inputValidator;
     List<Integer> lottoNumbers;
 
@@ -21,6 +23,33 @@ public class BonusNumberValidateTest {
     void 보너스_번호_입력_성공() {
         String validatedInput = "23";
 
-        assertDoesNotThrow(() -> inputValidator.validateBonusNumber(validatedInput,lottoNumbers));
+        assertDoesNotThrow(() -> inputValidator.validateBonusNumber(validatedInput, lottoNumbers));
+    }
+
+    @Test
+    void 보너스_번호_입력_중복예외() {
+        String duplicatedInput = "2";
+
+        assertThatThrownBy(() -> inputValidator.validateBonusNumber(duplicatedInput, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NOT_DISTINCT_BONUS_NUMBER.getMessage());
+    }
+
+    @Test
+    void 보너스_번호_입력_문자예외() {
+        String noNumberInput = "a";
+
+        assertThatThrownBy(() -> inputValidator.validateBonusNumber(noNumberInput, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NO_NUMBER.getMessage());
+    }
+
+    @Test
+    void 보너스_번호_입력_범위예외() {
+        String outRangeInput = "100";
+
+        assertThatThrownBy(() -> inputValidator.validateBonusNumber(outRangeInput, lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(InputException.NOT_IN_RANGE_LOTTO_NUMBER.getMessage());
     }
 }

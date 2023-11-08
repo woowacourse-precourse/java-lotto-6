b71a5e27a2e7d7915cf95cf29b94a5793649ce98
Kim Taeyeong <tyeong1102@gmail.com>
2023-11-08 20:56:30 +0900

    feat: OutputView

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,2 +1,43 @@
-package lotto.view;public class OutputView {
+package lotto.view;
+
+import static lotto.model.constants.OutputMessage.*;
+import static lotto.model.constants.StringConstant.NEW_LINE;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class OutputView {
+    public void printMessage(String outputMessage){
+        System.out.println(outputMessage);
+    }
+
+    public String askPurchasingAmount(){
+        printMessage(ASK_PURCHASING_AMOUNT.getMessage());
+        return Console.readLine();
+    }
+
+    public void printOrderedLottoCount(int ticketCount) {
+        printMessage(NEW_LINE.get() + ticketCount + COUNT_BUYING_TICKETS.getMessage());
+    }
+
+    public void printLottoTickets(String tickets){
+        printMessage(tickets);
+    }
+
+    public String askWinningNumbers(){
+        printMessage(ASK_WINNING_NUMBERS.getMessage());
+        return Console.readLine();
+    }
+
+    public String askBonusNumber(){
+        printMessage(ASK_BONUS_NUMBER.getMessage());
+        return Console.readLine();
+    }
+
+    public void printStatistics(String result){
+        printMessage(STATISTICS.getMessage() + result);
+    }
+
+    public void printProfit(String profit) {
+        printMessage(PROFIT_START.getMessage() + profit + PROFIT_END.getMessage());
+    }
 }

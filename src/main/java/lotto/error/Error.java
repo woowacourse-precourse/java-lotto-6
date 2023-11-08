f6e9da386507f2e9a72e834ff696a2243dc6837c
HyunmoBae <tsi0520@naver.com>
2023-11-08 19:51:28 +0900

    feat(lotto) : modify ErrorType and about message methods

--- src/main/java/lotto/error/Error.java
+++ src/main/java/lotto/error/Error.java
@@ -1,27 +1,28 @@
 package lotto.error;
 
 public class Error {
-    public enum ErrorType{
-        INVALID_AMOUNT,
-        INVALID_INPUT_TYPE,
-        INVALID_LOTTO_INPUT,
-        DUPLICATION
-    }
+    public enum ErrorType {
+        INVALID_MONEY_TYPE("[ERROR] 유효하지 않은 값입니다."),
+        INVALID_MONEY_VALUE("[ERROR] 1000이상이며, 1000으로 나누어 떨어지는 값을 입력해 주세요."),
 
-    public static void errorMessage(ErrorType errorType) {
-        if (errorType == ErrorType.INVALID_AMOUNT) {
-            System.out.println("[ERROR] 입력값에러");
-            throw new IllegalArgumentException();
-        } else if (errorType == ErrorType.INVALID_INPUT_TYPE) {
-            System.out.println("[ERROR] 입력타입에러");
-            throw new IllegalArgumentException();
-        } else if (errorType == ErrorType.INVALID_LOTTO_INPUT) {
-            System.out.println("[ERROR] 로또 값 에러");
-            throw new IllegalArgumentException();
-        } else if (errorType == ErrorType.DUPLICATION) {
-            System.out.println("[ERROR] 중복 에러");
-            throw new IllegalArgumentException();
+        INVALID_LOTTO_SIZE("[ERROR] 로또 숫자는 6개를 입력해야 합니다."),
+        INVALID_LOTTO_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+        DUPLICATION("[ERROR] 중복된 숫자가 있으면 안됩니다."),
+        INVALID_LOTTO_INPUT("[ERROR] 유효하지 않은 값입니다."),
+        ;
+
+        private final String message;
+
+        ErrorType(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return this.message;
         }
     }
 
+    public static void showErrorMessage(ErrorType errorType) {
+        System.out.println(errorType.message);
+    }
 }

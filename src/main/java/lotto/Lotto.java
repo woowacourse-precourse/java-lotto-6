2b50c5cb81ca281a5be20ab5ba36f572e4c93cbc
ehdtndla123 <ehdtndla123@naver.com>
2023-11-08 13:54:21 +0900

    feat : Lotto, Lotto 의 멤버 변수인 numbers를 이용하여 WinningNubmer와 비교하는 함수를 가지는 클래스 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,13 +1,53 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicated(numbers);
         this.numbers = numbers;
+    }
+
+    public void printNumbers() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        String numberString = numbers.stream()
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+        sb.append(numberString);
+        sb.append("]");
+        System.out.println(sb.toString());
+    }
+
+    public Rank checkWinning(WinningNumber winningNumber) {
+        int matchCount = countMatchNumbers(winningNumber);
+        boolean bonus = false;
+
+        if (matchCount == 5) {
+            bonus = this.numbers.contains(winningNumber.getBonus());
+        }
+
+        boolean finalBonus = bonus;
+
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.isMatchCountEqual(matchCount))
+                .filter(rank -> rank.isBonusEqual(finalBonus))
+                .findAny()
+                .orElse(Rank.NO_PRIZE);
+    }
+
+    private int countMatchNumbers(WinningNumber winningNumber) {
+        return (int) winningNumber.getNumbers()
+                .stream().filter(this.numbers::contains)
+                .count();
     }
 
     private void validate(List<Integer> numbers) {
@@ -16,5 +56,11 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplicated(List<Integer> numbers) {
+        Set<Integer> isNotDuplicated = new HashSet<>(numbers);
+        if (isNotDuplicated.size() < numbers.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

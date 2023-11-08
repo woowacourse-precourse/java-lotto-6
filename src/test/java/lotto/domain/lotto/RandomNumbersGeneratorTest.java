33464e06be07e16571fa317b322ea95863d68243
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-08 19:33:22 +0900

    test: RandomNumbersGenerator 테스트코드 추가

---
+++ src/test/java/lotto/domain/lotto/RandomNumbersGeneratorTest.java
@@ -0,0 +1,18 @@
+package lotto.domain.lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomNumbersGeneratorTest {
+
+    RandomNumbersGenerator randomNumbersGenerator =new RandomNumbersGenerator();
+
+    @DisplayName("랜덤 넘버 정상 발행 테스트")
+    @Test
+    void TestRandomNumbersGenerator(){
+        assertThat(randomNumbersGenerator.generate().size()).isEqualTo(6);
+    }
+}

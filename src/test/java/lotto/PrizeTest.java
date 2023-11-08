a771a65d62cc973a1fc807a7e2094d0961878c20
nhn1030 <shgusska12@gmail.com>
2023-11-08 21:32:38 +0900

    test: enum 상수명 가독성있게 변경 테스트 코드 반영

--- src/test/java/lotto/PrizeTest.java
+++ src/test/java/lotto/PrizeTest.java
@@ -9,10 +9,10 @@ public class PrizeTest {
 
     @Test
     void testPrizeAmount() {
-        assertEquals(1_000_000_000, Prize.WinningPrize.FIRST_PRIZE.getPrizeAmount());
-        assertEquals(50_000_000, Prize.WinningPrize.SECOND_PRIZE.getPrizeAmount());
-        assertEquals(1_000_000, Prize.WinningPrize.THIRD_PRIZE.getPrizeAmount());
-        assertEquals(50_000, Prize.WinningPrize.FOURTH_PRIZE.getPrizeAmount());
-        assertEquals(5_000, Prize.WinningPrize.FIFTH_PRIZE.getPrizeAmount());
+        assertEquals(1_000_000_000, Prize.WinningPrize.FIRST_RANK.getPrizeAmount());
+        assertEquals(50_000_000, Prize.WinningPrize.SECOND_RANK.getPrizeAmount());
+        assertEquals(1_000_000, Prize.WinningPrize.THIRD_RANK.getPrizeAmount());
+        assertEquals(50_000, Prize.WinningPrize.FOURTH_RANK.getPrizeAmount());
+        assertEquals(5_000, Prize.WinningPrize.FIFTH_RANK.getPrizeAmount());
     }
 }

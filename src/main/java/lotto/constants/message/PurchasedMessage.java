ac5b95208d03d5f44931a036c343037a3f661255
nayonsoso <panda0329@naver.com>
2023-11-08 21:03:10 +0900

    refactor: 변수명 가독성 고려하여 수정

--- src/main/java/lotto/constants/message/PurchasedMessage.java
+++ src/main/java/lotto/constants/message/PurchasedMessage.java
@@ -1,7 +1,7 @@
 package lotto.constants.message;
 
 public class PurchasedMessage {
-    public static final String PURCHASE_AMOUNT = "%d개를 구매했습니다.\n";
+    public static final String PURCHASE_AMOUNT = "\n%d개를 구매했습니다.\n";
     public static final String PURCHASED_LOTTO_START = "[";
     public static final String PURCHASED_LOTTO_DELIMITER = ", ";
     public static final String PURCHASED_LOTTO_END = "]";

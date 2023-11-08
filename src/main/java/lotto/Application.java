8b12ec327169e3fdfa52f5eea641a4bb6a2a8d01
park_yeongyu <sjjs7771@naver.com>
2023-11-08 20:27:15 +0900

    feat(Application) : 전반적인 로또 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,17 @@
 package lotto;
 
 import lotto.Controller.LottoController;
+import lotto.Model.CalculateWinning;
 import lotto.Model.User;
 import lotto.View.LottoView;
 
 public class Application {
     public static void main(String[] args) {
-        User[] user = new User[3];
-        LottoView lottoView = new LottoView();
-        LottoController lottoControl = new LottoController(user,lottoView);
-        lottoControl.start();
+
+       User user = new User();
+        LottoView view = new LottoView();
+        CalculateWinning calculateWinning = new CalculateWinning();
+        LottoController control = new LottoController(user,view,calculateWinning);
+        control.lottoGame();
     }
 }

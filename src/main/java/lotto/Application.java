42ff8e7fe53c1aafdefd30dd287f348fa688522d
songminhyuk <dnfldpden32@gmail.com>
2023-11-08 20:09:21 +0900

    feat: 로또번호/보너스 입력 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,24 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         //로또 구입 금액 입력
-        String money = Console.readLine();
-        //로또 구수
-        LottoManage bought_lotto=new LottoManage(money);
+        String input = Console.readLine();
+        //로또 구입을 해주는 생성자
+        LottoManage lottomanager=new LottoManage(input);
+        //당첨번호 입력
+        input=Console.readLine();
+        List<Integer> lotto_numbers=new ArrayList<>();
+        for(String st:input.split(",")){
+            lotto_numbers.add(Integer.parseInt(st));
+        }
+        int bonus=Integer.parseInt(Console.readLine());
+        lottomanager.setCorrect_lotto(lotto_numbers,bonus);
+        //로또 수량 출력
     }
 }

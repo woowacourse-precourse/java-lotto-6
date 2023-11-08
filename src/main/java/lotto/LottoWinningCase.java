4f90609919380b4650dd0e2f83c6744b766b909d
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 19:57:40 +0900

    refactor(LottoWinningCase): 맞는 번호가 5일때 문제가 있어 수정

--- src/main/java/lotto/LottoWinningCase.java
+++ src/main/java/lotto/LottoWinningCase.java
@@ -1,27 +1,28 @@
 package lotto;
 
-import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static lotto.ErrorMessage.FIVE;
+import static lotto.constant.DefinedNumber.*;
 
 public enum LottoWinningCase {
-    UNRANK(0, List.of(0, 1, 2)),
-    FIRST_PLACE(1, List.of(6)),
-    SECONND_PLACE(2, List.of(5)),
-    THRID_PLACE(3, List.of(5)),
-    FOURTH_PLACE(4, List.of(4)),
-    FIFTH_PLACE(5, List.of(3));
+    UNRANK(0, List.of(0, 1, 2), 0),
+    FIRST_PLACE(1, List.of(6), FIRST_PLACE_REWARD),
+    SECONND_PLACE(2, List.of(5), SECOND_PLACE_REWARD),
+    THRID_PLACE(3, List.of(5), THIRD_PLACE_REWARD),
+    FOURTH_PLACE(4, List.of(4), FOURTH_PLACE_REWARD),
+    FIFTH_PLACE(5, List.of(3), FIFTH_PLACE_REWARD);
 
     private final int place;
     private final List<Integer> numberMatchWithWinningLotto;
+    private final int reward;
 
-    LottoWinningCase(int place, List<Integer> numberMatchWithWinningLotto) {
+    LottoWinningCase(int place, List<Integer> numberMatchWithWinningLotto, int reward) {
         this.place = place;
         this.numberMatchWithWinningLotto = numberMatchWithWinningLotto;
+        this.reward = reward;
     }
 
     public static LottoWinningCase compareWhichCaseItIs(Lotto myLottoNumbers, List<Integer> winningNumber, int bonusNumnber) {
@@ -30,7 +31,7 @@ public enum LottoWinningCase {
                 .filter(lottoWinningCase -> lottoWinningCase.getNumberMatchWithWinningLotto().contains(matchCount))
                 .findFirst()
                 .orElse(null);
-        if(myCase.getPlace() == FIVE && !BonusNumberMatch(myLottoNumbers.getNumbers(), bonusNumnber)) {
+        if (matchCount == FIVE && !BonusNumberMatch(myLottoNumbers.getNumbers(), bonusNumnber)) {
             myCase = THRID_PLACE;
         }
         return myCase;
@@ -52,5 +53,9 @@ public enum LottoWinningCase {
 
     public List<Integer> getNumberMatchWithWinningLotto() {
         return numberMatchWithWinningLotto;
+    }
+
+    public int getReward() {
+        return reward;
     }
 }

f9baad5bf9078654a26acdff3c1f0c258a5cd9cb
mdgome <mdgome3@gmail.com>
2023-11-08 17:21:21 +0900

    fix: 보너스 번호 테스트 케이스 로또 사이즈 수정
    기존 로또 번호 7개로 원하는 테스트 불가로 인하여
    로또 번호 6개로 수정
    
    7개 입력 시 Lotto Class에서 IllegalArgumentException 발생

--- src/test/java/lotto/model/BonusTest.java
+++ src/test/java/lotto/model/BonusTest.java
@@ -13,7 +13,7 @@ public class BonusTest {
     @DisplayName("보너스 번호가 1부터 45가 아닌 경우 예외가 발생한다.")
     @Test
     void createLottoByOutOfRange() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6, 7));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThatThrownBy(() -> new Bonus(46, lotto))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR_MESSAGE);
@@ -22,7 +22,7 @@ public class BonusTest {
     @DisplayName("보너스 번호가 없는 경우 예외가 발생한다.")
     @Test
     void createLottoWithNoInput() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6, 7));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         Integer nullValue = null;
 
         assertThatThrownBy(() -> new Bonus(nullValue, lotto))
@@ -33,7 +33,7 @@ public class BonusTest {
     @DisplayName("보너스 번호가 로또 번호와 중복이면 예외가 발생한다.")
     @Test
     void compareLottoAndBonusByDuplicateNumber() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6, 7));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         int bonus = 7;
         assertThatThrownBy(() -> new Bonus(bonus, lotto))
                 .isInstanceOf(IllegalArgumentException.class)

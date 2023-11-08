5ddb364639b241a295edb9b78091411dc34e9da5
eora21 <eora21@naver.com>
2023-11-08 09:03:19 +0900

    refactor: 💡 수정 로또 티켓 구매 과정 정적 메서드화

--- src/main/java/domain/LottoTicketsPurchasingMachine.java
+++ src/main/java/domain/LottoTicketsPurchasingMachine.java
@@ -8,26 +8,31 @@ public class LottoTicketsPurchasingMachine {
     private static final Amount MAX_PURCHASE_AMOUNT = new Amount(2_000_000_000);
     private static final Amount LOTTO_UNIT_AMOUNT = new Amount(1_000);
 
-    public int purchaseOfLottoTickets(Amount purchaseAmount) {
+    private LottoTicketsPurchasingMachine() {
+
+    }
+
+    public static int purchaseOfLottoTickets(int amount) {
+        Amount purchaseAmount = new Amount(amount);
         validateZeroAmount(purchaseAmount);
         validateMaxPurchaseAmount(purchaseAmount);
         validatePurchaseAmountWithUnitAmount(purchaseAmount);
         return (int) purchaseAmount.calculateQuotientByUnitAmount(LOTTO_UNIT_AMOUNT);
     }
 
-    private void validateZeroAmount(Amount purchaseAmount) {
+    private static void validateZeroAmount(Amount purchaseAmount) {
         if (purchaseAmount.isZeroAmount()) {
             throw new NotPositivePurchaseAmountException();
         }
     }
 
-    private void validateMaxPurchaseAmount(Amount purchaseAmount) {
+    private static void validateMaxPurchaseAmount(Amount purchaseAmount) {
         if (purchaseAmount.isOverLimitAmount(MAX_PURCHASE_AMOUNT)) {
             throw new OverMaxPurchaseAmountException(MAX_PURCHASE_AMOUNT);
         }
     }
 
-    private void validatePurchaseAmountWithUnitAmount(Amount purchaseAmount) {
+    private static void validatePurchaseAmountWithUnitAmount(Amount purchaseAmount) {
         Amount modAmount = purchaseAmount.calculateModAmountByUnitAmount(LOTTO_UNIT_AMOUNT);
 
         if (modAmount.isZeroAmount()) {

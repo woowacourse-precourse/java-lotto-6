ae75f91459dcd61de0346d5ad0ce58803a51d36d
Dowon <3120kang@gmail.com>
2023-11-08 19:15:28 +0900

    Refactor(Lucky): Lucky에 있는 예외코드 삭제

--- src/main/java/lotto/domain/Lucky.java
+++ src/main/java/lotto/domain/Lucky.java
@@ -1,8 +1,5 @@
 package lotto.domain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Lucky {
@@ -11,21 +8,19 @@ public class Lucky {
     private final int bonus;
 
     public Lucky(List<Integer> lucky, int bonus) {
-
-        validateBonus(lucky,bonus);
         this.lucky = lucky;
         this.bonus = bonus;
     }
 
     public Rank checkWinning(Lotto lotto) {
         Comparator comparator = new Comparator();
-        boolean bonus=false;
+        boolean bonus = false;
         int count = comparator.compare(lotto.getNumbers(), lucky);
-        if(count==5){
+        if (count == 5) {
             bonus = checkBonus(lotto.getNumbers());
         }
-        if(count<3){
-            count=0;
+        if (count < 3) {
+            count = 0;
         }
         return Rank.valueOfCount(count, bonus);
     }
@@ -33,27 +28,5 @@ public class Lucky {
     public boolean checkBonus(List<Integer> lotto) {
         return lotto.contains(bonus);
     }
-
-
-
-
-    private void validateDuplication(List<Integer> numbers){
-        if(numbers.size()!=numbers.stream().distinct().count()){
-            throw new IllegalArgumentException("[ERROR] 중복되는 숫자는 불가능합니다.");
-        }
-    }
-
-    private void validateBonus(List<Integer> numbers,int bonus){
-        if(numbers.contains(bonus)){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.");
-        }
-    }
-
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 6자리의 당첨 번호를 입력해야 합니다.");
-        }
-    }
-
 
 }

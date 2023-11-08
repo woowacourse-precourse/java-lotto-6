bb3c95cf22ebdc57c8fabb810d483c7815103359
ay-eonii <naayen@naver.com>
2023-11-08 21:56:08 +0900

    test(view): `@ParameterizedTest` 사용

--- src/test/java/lotto/view/ViewTest.java
+++ src/test/java/lotto/view/ViewTest.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.Application;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -13,29 +16,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ViewTest extends NsTest {
 
-    @DisplayName("구입금액에 숫자가 아닌 문자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputNonNumericCost() {
+    @DisplayName("구입금액에 int 타입으로 변경할 수 없는 문자를 입력하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdef", "2200000000", "   ", "\n"})
+    void inputNonNumericCost(String cost) {
         assertSimpleTest(() -> {
-            runException("abcdef");
-            assertThat(output()).contains("[ERROR] 20억 이하의 숫자를 입력해주세요.");
-        });
-    }
-
-    @DisplayName("구입금액에 int 범위 이상의 숫자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputOverIntRangeCost() {
-        assertSimpleTest(() -> {
-            runException("2200000000");
-            assertThat(output()).contains("[ERROR] 20억 이하의 숫자를 입력해주세요.");
-        });
-    }
-
-    @DisplayName("구입금액에 공백만 입력하면 예외가 발생한다.")
-    @Test
-    void inputCostOnlyBlank() {
-        assertSimpleTest(() -> {
-            runException("   ");
+            runException(cost);
             assertThat(output()).contains("[ERROR] 20억 이하의 숫자를 입력해주세요.");
         });
     }
@@ -63,20 +49,12 @@ class ViewTest extends NsTest {
         );
     }
 
-    @DisplayName("당첨 로또에 숫자가 아닌 문자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputNonNumericWinningLotto() {
+    @DisplayName("당첨 로또에 int 타입으로 변경할 수 없는 문자를 입력하면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1000:a,b,c,d,e,f", "1000:2200000000,1,2,3,4,5"}, delimiter = ':')
+    void inputNonNumericWinningLotto(String cost, String winningLotto) {
         assertSimpleTest(() -> {
-            runException("1000", "a,b,c,d,e,f");
-            assertThat(output()).contains("[ERROR] 로또 번호는 쉼표로 구분되는 1에서 45 사이의 숫자여야 합니다.");
-        });
-    }
-
-    @DisplayName("당첨 로또에 int 범위 이상의 숫자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputOverIntRangeLotto() {
-        assertSimpleTest(() -> {
-            runException("1000", "2200000000,1,2,3,4,5");
+            runException(cost, winningLotto);
             assertThat(output()).contains("[ERROR] 로또 번호는 쉼표로 구분되는 1에서 45 사이의 숫자여야 합니다.");
         });
     }
@@ -102,24 +80,15 @@ class ViewTest extends NsTest {
         );
     }
 
-    @DisplayName("보너스 번호에 숫자가 아닌 문자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputNonNumericBonus() {
+    @DisplayName("보너스 번호에 int 타입으로 변경할 수 없는 문자를 입력하면 예외가 발생한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1000:1,2,3,4,5,6:!", "1000:1,2,3,4,5,6:2200000000"}, delimiter = ':')
+    void inputNonNumericBonus(String cost, String winningLotto, String bonus) {
         assertSimpleTest(() -> {
-            runException("1000", "1,2,3,4,5,6", "!");
+            runException(cost, winningLotto, bonus);
             assertThat(output()).contains("[ERROR] 보너스 번호는 1에서 45 사이인 하나의 숫자여야 합니다.");
         });
     }
-
-    @DisplayName("당첨 로또에 int 범위 이상의 숫자를 입력하면 예외가 발생한다.")
-    @Test
-    void inputOverIntRangeBonus() {
-        assertSimpleTest(() -> {
-            runException("1000", "1,2,3,4,5,6", "2200000000");
-            assertThat(output()).contains("[ERROR] 보너스 번호는 1에서 45 사이인 하나의 숫자여야 합니다.");
-        });
-    }
-
 
     @DisplayName("당첨 로또에 숫자와 공백을 입력하면 공백을 무시한다.")
     @Test

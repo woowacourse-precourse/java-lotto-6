c74d5ae2073af7d40fd9d5e09a4d2c8b850177a4
kkw2758 <dtc3229@gmail.com>
2023-11-08 12:02:12 +0900

    Fix(AnswerLottoTest) : AnswerLotto, Lotto 구조 수정으로 인한 테스트 코드 수정
    - countMatchNumber메서드의 Lotto클래스로 이동으로 인한 테스트 countMatchNumber에 대한 테스트 코드 삭제
    - AnswerLotto, Lotto 구조 수정으로 인한 버그 수정

--- src/test/java/lotto/AnswerLottoTest.java
+++ src/test/java/lotto/AnswerLottoTest.java
@@ -16,13 +16,13 @@ class AnswerLottoTest {
     @Test
     void 당첨번호_중복_예외_처리_테스트() {
         //Given
-        List<LottoNumber> lottoNumbers = Stream.of(1, 2, 3, 3, 5, 6)
+        List<LottoNumber> answerlottoNumbers = Stream.of(1, 2, 3, 3, 5, 6)
                 .map(LottoNumber::of)
                 .toList();
         LottoNumber bonusNumber = LottoNumber.of(7);
 
         //When & Then
-        assertThatThrownBy(() -> new AnswerLotto(lottoNumbers, bonusNumber))
+        assertThatThrownBy(() -> new AnswerLotto(new Lotto(answerlottoNumbers), bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.LOTTO_HAS_DUPLICATE_NUMBER_ERROR.getMessage());
     }
@@ -30,50 +30,28 @@ class AnswerLottoTest {
     @Test
     void 당첨번호_길이_예외_처리_테스트() {
         //Given
-        List<LottoNumber> lottoNumbers = Stream.of(1, 2, 3, 4, 5, 6, 8)
+        List<LottoNumber> answerlottoNumbers = Stream.of(1, 2, 3, 4, 5, 6, 8)
                 .map(LottoNumber::of)
                 .toList();
         LottoNumber bonusNumber = LottoNumber.of(7);
 
         //When & Then
-        assertThatThrownBy(() -> new AnswerLotto(lottoNumbers, bonusNumber))
+        assertThatThrownBy(() -> new AnswerLotto(new Lotto(answerlottoNumbers), bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(String.format(ErrorMessage.LOTTO_LENGTH_ERROR.getMessage(),
                         NumberType.LOTTO_LENGTH.getValue()));
     }
 
     @Test
-    void 발행한_로또와_당첨_번호의_일치하는_숫자의_개수_확인_테스트() {
-        //Given
-        System.out.println("test START");
-        Lotto lotto = new Lotto(Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::of)
-                .toList());
-        LottoNumber bonusNumber = LottoNumber.of(7);
-        System.out.println("MIDDLE");
-        AnswerLotto answerLotto = new AnswerLotto(Stream.of(1, 2, 3, 4, 5, 6)
-                .map(LottoNumber::of)
-                .toList(), bonusNumber);
-        System.out.println("AA");
-
-        //When
-        int expectedResult = 6;
-        int result = answerLotto.countMatchNumber(lotto);
-
-        //When & Then
-        assertThat(result).isEqualTo(expectedResult);
-    }
-
-    @Test
     void 보너스_번호가_당첨번호에_포함된다면_예외_처리_테스트() {
         //Given
-        List<LottoNumber> lottoNumbers = Stream.of(1, 2, 3, 4, 5, 6)
+        List<LottoNumber> answerlottoNumbers = Stream.of(1, 2, 3, 4, 5, 6)
                 .map(LottoNumber::of)
                 .toList();
         LottoNumber bonusNumber = LottoNumber.of(6);
 
         //When & Then
-        assertThatThrownBy(() -> new AnswerLotto(lottoNumbers, bonusNumber))
+        assertThatThrownBy(() -> new AnswerLotto(new Lotto(answerlottoNumbers), bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorMessage.BONUS_NUMBER_IN_ANSWER_LOTTO_ERROR.getMessage());
     }

b71af3ffd7e22c4efea538bed7b929905b224762
honee8583 <honee8583@naver.com>
2023-11-08 18:25:59 +0900

    refactor: refactor LottoResult.java
    refactor structure of LottoResult.java
    
    - add constant array represents prices.
    - change instance fields to instance array.
    - refactor calculate().
    - refactor getLottoResult().
    - refactor total().

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,49 +1,35 @@
 package lotto;
 
 public class LottoResult {
-    private int three;
-    private int four;
-    private int five;
-    private int five_bonus;
-    private int six;
+    private static final int[] PRICE = {5000, 50000, 1500000, 2000000000, 30000000};
+
+    private int[] lottoWinningCounts;
 
     public LottoResult() {
-        this.three = 0;
-        this.four = 0;
-        this.five = 0;
-        this.five_bonus = 0;
-        this.six = 0;
+        lottoWinningCounts = new int[5];
     }
 
-    public void plusCnt(int cnt, boolean correct) {
-        if (cnt == 3) three++;
-        if (cnt == 4) four++;
-        if (cnt == 5) five++;
-        if (cnt == 5 && correct) five_bonus++;
-        if (cnt == 6) six++;
+    public void calculate(int cnt, boolean correct) {
+        if (!correct) {
+            if (cnt <= 5 || cnt == 6) {
+                this.lottoWinningCounts[cnt - 3]++;
+            }
+            return;
+        }
+        if (cnt == 5) {
+            this.lottoWinningCounts[4]++;
+        }
     }
 
-    public int getThree() {
-        return three;
-    }
-
-    public int getFour() {
-        return four;
-    }
-
-    public int getFive() {
-        return five;
-    }
-
-    public int getFive_bonus() {
-        return five_bonus;
-    }
-
-    public int getSix() {
-        return six;
+    public int[] getLottoResult() {
+        return this.lottoWinningCounts;
     }
 
     public int total() {
-        return 5000 * three + 50000 * four + 1500000 * five + 30000000 * five_bonus + 2000000000 * six;
+        int total = 0;
+        for (int i = 0; i < lottoWinningCounts.length; i++) {
+            total += (PRICE[i] * lottoWinningCounts[i]);
+        }
+        return total;
     }
 }

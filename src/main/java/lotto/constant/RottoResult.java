49b5901899b03ee5c6803428f9537b77254839f8
namtndus <rnrmf0721@gmail.com>
2023-11-08 13:10:31 +0900

    feat(RottoResult): 기능 구현

--- src/main/java/lotto/constant/RottoResult.java
+++ src/main/java/lotto/constant/RottoResult.java
@@ -1,21 +1,54 @@
 package lotto.constant;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum RottoResult {
-    FIVE(3, 5_000),
-    FOUR(4,50_000),
-    THREE(5,1_500_000),
-    TWO(5,30_000_000),
-    ONE(6,2_000_000_000);
+    FIVE(5, 3, 5_000),
+    FOUR(4, 4, 50_000),
+    THREE(3, 5, 1_500_000),
+    TWO(2, 7, 30_000_000),
+    ONE(1, 6, 2_000_000_000);
 
     private final int matchingNumber;
     private final int amout;
+    private final int index;
 
-    RottoResult(int matchingNumber, int amout) {
+    RottoResult(int index, int matchingNumber, int amout) {
         this.matchingNumber = matchingNumber;
         this.amout = amout;
+        this.index = index;
     }
 
-    public String printRottoResult(){
-        return String.format("%d개 일치 (%,d원) - ",matchingNumber,amout);
+    private int getMatchingNumber() {
+        return matchingNumber;
+    }
+
+    private int getAmout() {
+        return amout;
+    }
+
+    private int getIndex() {
+        return index;
+    }
+
+    public int getAmout(int num) {
+        return Arrays.stream(RottoResult.values())
+                .filter(rottoResult -> rottoResult.getMatchingNumber() == num)
+                .findFirst().get().getAmout();
+    }
+
+    public int getIndex(int num) {
+        return Arrays.stream(RottoResult.values())
+                .filter(rottoResult -> rottoResult.getMatchingNumber() == num)
+                .findFirst().get().getIndex();
+    }
+
+    public int calculateAmout(List<Integer> numbers) {
+        int sum = 0;
+        for (Integer num : numbers) {
+            sum += getAmout(num);
+        }
+        return sum;
     }
 }

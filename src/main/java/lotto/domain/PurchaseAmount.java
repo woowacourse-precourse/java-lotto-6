87f6735822daae4198678335c80537bee6d27036
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 17:16:38 +0900

    feat(PurchaseAmount) : add getLottoCount() method

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -1,14 +1,19 @@
 package lotto.domain;
 
 public class PurchaseAmount {
+    private static final int LOTTO_PRICE = 1000;
     private final int amount;
 
-    public PurchaseAmount(int amount) {
-        // 유효성 검증 로직
-        this.amount = amount;
+    public PurchaseAmount(String amount) {
+//        verification(amount);
+        this.amount = Integer.parseInt(amount);
     }
 
     public int getAmount() {
         return amount;
+    }
+
+    public int getLottoCount() {
+        return amount / LOTTO_PRICE;
     }
 }

a66bcfe204eb51444c9c8da4c612f82883c4067a
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 16:54:10 +0900

    feat(Configuration): 서비스 클래스와 컨트롤러 클래스 의존성 주입을 위한 Configuration 클래스 생성

---
+++ src/main/java/lotto/Configuration.java
@@ -0,0 +1,19 @@
+package lotto;
+
+import lotto.controller.LottoController;
+import lotto.domain.Player;
+import lotto.domain.lotto.Lottos;
+import lotto.domain.validate.InputValidator;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class Configuration {
+    public static LottoController controller() {
+        return new LottoController(lottoService(),new InputView(), new OutputView());
+    }
+
+    private static LottoService lottoService() {
+        return new LottoService(new Player(), new Lottos(), new InputValidator());
+    }
+}

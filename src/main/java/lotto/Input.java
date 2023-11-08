c46a98795f37d293a1e65b6052fc32b8a2cbfcf0
yohanii <dygks924@gmail.com>
2023-11-08 10:42:35 +0900

    feat: skeleton code 작성

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,18 @@
+package lotto;
+
+import java.util.List;
+
+public class Input {
+
+    public List<Integer> inputWinNums() {
+        return null;
+    }
+
+    public int inputBonusNum() {
+        return 0;
+    }
+
+    public int inputPrice() {
+        return 0;
+    }
+}
---
+++ src/main/java/lotto/LottoGame.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.List;
+
+public class LottoGame {
+
+    public List<Integer> generateLottoNums() {
+        return null;
+    }
+
+    public int getLottoCounts(int price) {
+        return 0;
+    }
+
+    public void printResult(List<Integer> ranks) {
+
+    }
+
+    public void printEarningRate(List<Integer> ranks) {
+
+    }
+}

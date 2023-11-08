e4e8b6f8e9065f397b591dd084faca8c5d854498
MrKeeplearning <jaeseok1115@gmail.com>
2023-11-08 14:48:10 +0900

    feat(domain): 사용자의 로또 구매 금액을 검증
    - feat(User): 사용자가 로또 구매 가능 금액의 범위에 속하고 1000으로
      나누어 떨어지는 숫자를 입력했는지 검증하는 기능을 User 클래스에 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -5,8 +5,32 @@ import java.util.ArrayList;
 public class User {
 
     private final ArrayList<Lotto> purchasedLotto = new ArrayList<>();
+    private final int purchaseAmount;
+
+    public User(int purchaseAmount) {
+        validatePurchaseAmount(purchaseAmount);
+        this.purchaseAmount = purchaseAmount;
+    }
+
+    public void usersLotto(Lotto lotto) {
+        purchasedLotto.add(lotto);
+    }
 
     public ArrayList<Lotto> getPurchasedLotto() {
         return purchasedLotto;
+    }
+
+    public int getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public double getLotteryReturns() {
+        return 0;
+    }
+
+    private void validatePurchaseAmount(int purchaseAmount) {
+        Validations.validatePurchase(purchaseAmount);
+        Validations.validateMaxPurchase(purchaseAmount);
+        Validations.validateMinPurchase(purchaseAmount);
     }
 }

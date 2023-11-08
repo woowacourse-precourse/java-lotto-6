14e79c4e417ea024165c80d748c247c5c1a30836
PeterChoi <94peterchoi@gmail.com>
2023-11-08 21:01:52 +0900

    fix: Application 전체에 적용한 예외처리 로직 이관
    - Application 전체에 적용한 try-catch문 제거
    - 유저 입력을 처리하는 InputView에서 잘못된 입력에 대한 예외처리
    - InputView 클래스 각 메소드에 유효한 유저 입력이 들어올 때까지 반복문 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,12 +4,7 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            LottoController lottoController = new LottoController();
-            lottoController.run();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,32 +1,61 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
 import lotto.domain.Price;
 import lotto.domain.WinningLotto;
 import lotto.util.LottoGenerator;
 import lotto.util.LottoUtil;
 
-import java.util.List;
-
 public class InputView {
 
     public static Price getPurchasePrice() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = Console.readLine();
-        int purchasePrice = LottoUtil.parseInteger(input);
+        int purchasePrice = 0;
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String input = Console.readLine();
+                purchasePrice = LottoUtil.parseInteger(input);
+                isValidInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
         return new Price(purchasePrice);
     }
 
     public static WinningLotto getWinningLottoNumbers() {
-        System.out.println("당첨번호를 입력해주세요.");
-        String winningNumbersInput = Console.readLine();
-        List<Integer> winningLottoNumbers = LottoGenerator.generateWinningLottoNumbers(winningNumbersInput);
+        Lotto winningLotto = getValidatedWinningNumbers();
+        int bonusNumber = getValidatedBonusNumber(winningLotto);
 
-        System.out.println("\n보너스번호를 입력해주세요.");
-        String bonusNumberInput = Console.readLine();
-        int bonusNumber = LottoUtil.parseLottoNumber(bonusNumberInput);
+        return new WinningLotto(winningLotto, bonusNumber);
+    }
 
-        return new WinningLotto(winningLottoNumbers, bonusNumber);
+    private static Lotto getValidatedWinningNumbers() {
+        while (true) {
+            try {
+                System.out.println("당첨번호를 입력해주세요.");
+                String winningNumbersInput = Console.readLine();
+                return LottoGenerator.generateWinningLottoNumbers(winningNumbersInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static int getValidatedBonusNumber(Lotto winningLotto) {
+        while (true) {
+            try {
+                System.out.println("\n보너스번호를 입력해주세요.");
+                String bonusNumberInput = Console.readLine();
+                return LottoUtil.parseLottoBonusNumber(bonusNumberInput, winningLotto);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
 }

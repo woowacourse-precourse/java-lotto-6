66cb7eba5e368c1d4439ba512d2733e7d4cbd78c
nhn1030 <shgusska12@gmail.com>
2023-11-08 21:33:06 +0900

    refactor: enum 상수명 수정

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -3,11 +3,11 @@ package lotto.domain;
 
 public class Prize {
     public enum WinningPrize {
-        FIRST_PRIZE(1_000_000_000),
-        SECOND_PRIZE(50_000_000),
-        THIRD_PRIZE(1_000_000),
-        FOURTH_PRIZE(50_000),
-        FIFTH_PRIZE(5_000);
+        FIRST_RANK(1_000_000_000),
+        SECOND_RANK(50_000_000),
+        THIRD_RANK(1_000_000),
+        FOURTH_RANK(50_000),
+        FIFTH_RANK(5_000);
 
         private final int prizeAmount;
 

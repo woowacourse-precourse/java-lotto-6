4e1680848ed66ac2fee2ea334ca382fb8b89b07b
2021110806 <2021110806@dgu.ac.kr>
2023-11-08 20:01:28 +0900

    refactor(LottoService.java): add LottoService class 기존 LottoGameController class의 역할 중 로또 당첨 번호를 입력받는 부분을 담당하는 LottoService class를 만들어, LottoGameController class의 코드 일부를 담았습니다.

---
+++ src/main/java/service/LottoService.java
@@ -0,0 +1,41 @@
+package service;
+
+import model.Lotto;
+import model.WinningLotto;
+import view.InputView;
+import view.OutputView;
+
+public class LottoService {
+
+    public Lotto getValidateWinningNumber() {
+        boolean isSuccess = false;
+        Lotto lotto = null;
+        while (!isSuccess) {
+            try {
+                OutputView.printWinningNumberInputRequestMessage();
+                String winningNumber = InputView.getWinningNumber();
+                lotto = new Lotto(winningNumber);
+                isSuccess = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return lotto;
+    }
+
+    public WinningLotto getValidateBonusNumber(Lotto lotto) {
+        boolean isSuccess = false;
+        WinningLotto winningLotto = null;
+        while (!isSuccess) {
+            try {
+                OutputView.printBonusNumberInputRequestMessage();
+                String bonusNumber = InputView.getBonusNumber();
+                winningLotto = new WinningLotto(lotto.getNumbers(), bonusNumber);
+                isSuccess = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return winningLotto;
+    }
+}

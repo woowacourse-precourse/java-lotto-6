114d6e96e92b9a06fdac09615ccb2138bbd25ad0
JongMinCh0i <wonderland314592@gmail.com>
2023-11-08 10:43:16 +0900

    style(RandomLottoCreate): 클래스 네이밍 변경 (RandomLottoCreate -> RandomLottoGenerator)

--- src/main/java/lotto/domain/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/RandomLottoGenerator.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class RandomLottoCreate {
+public class RandomLottoGenerator {
 
     public static List<Lotto> issueRandomLottos(int lottoQuantity) {
         return IntStream.range(0, lottoQuantity)

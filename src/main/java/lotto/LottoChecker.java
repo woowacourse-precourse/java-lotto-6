a8559b9a7b3ec9dbe16cecfeaf9572bcacf81829
Hyeon-hak Kim <07ily@naver.com>
2023-11-08 14:11:54 +0900

    feat(LottoChecker): 당첨 번호 설정 시, 1에서 45 사이의 숫자가 아닌 경우

---
+++ src/main/java/lotto/LottoChecker.java
@@ -0,0 +1,25 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LottoChecker {
+
+    private final Lotto winner;
+    private final Integer winnerBonus;
+
+    public LottoChecker(Lotto winner, Integer winnerBonus) {
+        if (winnerBonus < LottoEnum.NUMBER_MIN.getValue() || LottoEnum.NUMBER_MAX.getValue() < winnerBonus) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 "
+                    + LottoEnum.NUMBER_MIN.getValue()
+                    + "부터 "
+                    + LottoEnum.NUMBER_MAX.getValue()
+                    + " 사이의 숫자여야 합니다.");
+        }
+
+        this.winner = winner;
+        this.winnerBonus = winnerBonus;
+    }
+
+}

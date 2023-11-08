f48bf02c3d4ca9239567cd97b3c29a4d3bff1f66
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 22:03:19 +0900

    [REFACTOR] 에러메세지 수정
    .

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,10 +6,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class InputView {
+    private static final String ERROR_ISSTRING = "[ERROR] 숫자만 입력해 주세요.";
+
+    private static final String ERROR_ISBLANK ="[ERROR] 입력값을 입력해주세요.";
+
+    private static final String ERROR_START_OR_END_WITH_DELIMITER = "[ERROR] 델리미터로 시작하거나 끝나면 안 됩니다.";
+    private static final String ERROR_DELIMITER_WITH_BLANK = "[ERROR] 연속된 델리미터 또는 델리미터와 공백이 포함되어 있으면 안 됩니다.";
+
     private static final String TOTAL_COST = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     static final String DELIMITER = ",";
+    static final String SPACE = " ";
 
     public static int totalCost() {
         System.out.println(TOTAL_COST);
@@ -37,13 +45,13 @@ public class InputView {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자를 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_ISSTRING);
         }
     }
 
     private static void isBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException("입력값을 입력해주세요.");
+            throw new IllegalArgumentException(ERROR_ISBLANK);
         }
     }
 
@@ -60,11 +68,10 @@ public class InputView {
 
     private static void validateDelimiter(String input) {
         if (input.startsWith(DELIMITER) || input.endsWith(DELIMITER)) {
-            throw new IllegalArgumentException("델리미터로 시작하거나 끝나면 안 됩니다.");
+            throw new IllegalArgumentException(ERROR_START_OR_END_WITH_DELIMITER);
         }
-
-        if (input.contains(DELIMITER + DELIMITER) || input.contains(DELIMITER + " ")) {
-            throw new IllegalArgumentException("연속된 델리미터 또는 델리미터와 공백이 포함되어 있으면 안 됩니다.");
+        if (input.contains(DELIMITER + DELIMITER) || input.contains(DELIMITER + SPACE)) {
+            throw new IllegalArgumentException(ERROR_DELIMITER_WITH_BLANK);
         }
     }
 }

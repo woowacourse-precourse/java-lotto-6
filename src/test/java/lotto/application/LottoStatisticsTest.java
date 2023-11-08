dbc883b0beb865e443f1536ec8f0d52380e666d5
dltjdgh0428 <dltjdgh0428@naver.com>
2023-11-08 14:58:43 +0900

    style : 불필요한 주석 제거
    불필요한 주석과 공백이 제거 되었습니다. Test에 given when then 주석이 추가되었습니다.

--- src/test/java/lotto/application/LottoStatisticsTest.java
+++ src/test/java/lotto/application/LottoStatisticsTest.java
@@ -48,12 +48,9 @@ public class LottoStatisticsTest {
     @Test
     @DisplayName("각 로또 티켓의 당첨 여부가 일치하는지 테스트")
     public void testPrizeStatistics() {
-
+        // given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(1, 2, 3, 4, 5, 6), 7);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
-
         Map<Prize, Integer> expectedPrizeCount = Map.of(
                 Prize.NONE, 4,
                 Prize.THREE_MATCH, 1,
@@ -63,91 +60,95 @@ public class LottoStatisticsTest {
                 Prize.SIX_MATCH, 1
         );
 
+        // when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
+
+        // then
         assertEquals(expectedPrizeCount, lottoResult.getPrizeCount());
     }
 
     @Test
     @DisplayName("5등 당첨 10000원을 투자하여 5000원을 얻었을때의 수익률")
     public void testTotalEarningsRate_Fifth() {
-
+        // given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(40, 41, 42, 11, 21, 32), 31);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
-
         double expectedEarningsRate = 50.0;
+
+        // when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
         LottoStatistics statistics = new LottoStatistics(lottoResult, 10_000);
         double actualEarningsRate = statistics.calculateEarningsRate();
 
-        assertEquals(expectedEarningsRate, actualEarningsRate); // 오차 범위를 지정
-
+        // then
+        assertEquals(expectedEarningsRate, actualEarningsRate);
     }
 
     @Test
     @DisplayName("4등 당첨 10000원을 투자하여 5만원을 얻었을때의 수익률")
     public void testTotalEarningsRate_Fourth() {
-
+        // given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(31, 32, 33, 34, 1, 41), 21);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
-
         double expectedEarningsRate = 500.0;
+
+        // when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
         LottoStatistics statistics = new LottoStatistics(lottoResult, 10_000);
         double actualEarningsRate = statistics.calculateEarningsRate();
 
-        assertEquals(expectedEarningsRate, actualEarningsRate); // 오차 범위를 지정
-
+        // then
+        assertEquals(expectedEarningsRate, actualEarningsRate);
     }
 
     @Test
     @DisplayName("3등 당첨 10000원을 투자하여 150만원을 얻었을때의 수익률")
     public void testTotalEarningsRate_Third() {
-
+        //given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(40, 25, 26, 27, 28, 29), 1);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
-
         double expectedEarningsRate = 15_000.0;
+
+        //when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
         LottoStatistics statistics = new LottoStatistics(lottoResult, 10_000);
         double actualEarningsRate = statistics.calculateEarningsRate();
 
+        //then
         assertEquals(expectedEarningsRate, actualEarningsRate);
-
     }
 
     @Test
     @DisplayName("2등 당첨 10000원을 투자하여 3000만원을 얻었을때의 수익률")
     public void testTotalEarningsRate_Second() {
-
+        //given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(40, 19, 20, 21, 22, 23), 24);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
-
         double expectedEarningsRate = 300_000.0;
+
+        //when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
         LottoStatistics statistics = new LottoStatistics(lottoResult, 10_000);
         double actualEarningsRate = statistics.calculateEarningsRate();
 
+        //then
         assertEquals(expectedEarningsRate, actualEarningsRate);
-
     }
 
     @Test
     @DisplayName("1등 당첨 10000원을 투자하여 20억원을 얻었을때의 수익률")
     public void testTotalEarningsRate_First() {
-
+        // given
         WinningNumbersDto winningNumbers = new WinningNumbersDto(List.of(13, 14, 15, 16, 17, 18), 23);
-
         LottoResult lottoResult = new LottoResult();
-        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
+        double expectedEarningsRate = 20_000_000.0;
 
-        double expectedEarningsRate = 20_000_000.0; // 50%를 소수로 표현
+        // when
+        lottoResult.calculateResults(lottoService.getLottoDtos(), winningNumbers);
         LottoStatistics statistics = new LottoStatistics(lottoResult, 10_000);
         double actualEarningsRate = statistics.calculateEarningsRate();
 
+        // then
         assertEquals(expectedEarningsRate, actualEarningsRate);
-
     }
 }

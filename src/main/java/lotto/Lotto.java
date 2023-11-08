440868bce3be02db99d33466e602ad32fc60fba6
jun23314 <dlwnsfml@naver.com>
2023-11-08 21:44:24 +0900

    사용자로부터 로또 번호 입력시 예외사항 정리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,14 +7,30 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkOverlap(numbers);
+        lottoInRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("로또 번호는 6개입니다");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void checkOverlap(List<Integer> numbers){
+        int k = 0;
+        for(int i = k+1; i < numbers.size(); i++){
+            if(numbers.get(k) == numbers.get(i)) throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+            k++;
+        }
+    }
+
+    private void lottoInRange(List<Integer> numbers){
+        for(int i = 0; i < numbers.size(); i++){
+            int num = numbers.get(i);
+            if(num < 1 || num > 45) throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
 }

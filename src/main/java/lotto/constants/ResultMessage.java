6f98506a81e7cbe57289f183aa348ef4f9e01f6f
Hyunjun KIM <hihj070914@icloud.com>
2023-11-08 19:13:47 +0900

    feat: ResultMessage Constants - 로또 게임에 대한 결과로 출력할 문자열의 포맷을 정의하였습니다.

---
+++ src/main/java/lotto/constants/ResultMessage.java
@@ -0,0 +1,9 @@
+package lotto.constants;
+
+public class ResultMessage {
+    public static final String PURCHASE_MESSAGE_FORMAT = "%d개를 구매했습니다.\n";
+    public static final String RESULT_VIEW_FORMAT = "당첨 통계\n---\n";
+    public static final String RESULT_OUTPUT_FORMAT = "%d개 일치 (%s원) - %d개\n";
+    public static final String RESULT_BONUS_OUTPUT_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
+    public static final String REVENUE_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+}

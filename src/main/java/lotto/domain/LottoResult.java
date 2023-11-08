cab15f521f75888d0831468bdc4ebb33550bc781
jjw0071 <jjw0071@naver.com>
2023-11-08 17:43:58 +0900

    refactor(lottoResult): enum 상수 순서 변경

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,12 +1,12 @@
 package lotto.domain;
 
 public enum LottoResult {
-    FIRST(6, 2_000_000_000, "2,000,000,000원"),
-    SECOND(5, 30_000_000, "30,000,000원"),
-    THIRD(5, 1_500_000, "1,500,000원"),
-    FOURTH(4, 50_000, "50,000원"),
+    NONE(0, 0, "0원"),
     FIFTH(3, 5_000, "5,000원"),
-    NONE(0, 0, "0원");
+    FOURTH(4, 50_000, "50,000원"),
+    THIRD(5, 1_500_000, "1,500,000원"),
+    SECOND(5, 30_000_000, "30,000,000원"),
+    FIRST(6, 2_000_000_000, "2,000,000,000원");
 
     private final int matchCount;
     private final int prizeMoney;

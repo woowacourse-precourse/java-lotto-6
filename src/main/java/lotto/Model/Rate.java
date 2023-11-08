dad6d5794cf948a85f544642ecad1ade76d0fca7
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-08 15:16:39 +0900

    style: 적절한 네이밍으로 변경

--- src/main/java/lotto/Model/Rate.java
+++ src/main/java/lotto/Model/Rate.java
@@ -4,14 +4,14 @@ public class Rate {
     int totalPrize;
 
     public Rate(Result result) {
-        this.totalPrize = calculate(result);
+        this.totalPrize = setTotalPrize(result);
     }
 
     public double calculateWinningRate(Price price) {
         return price.makeWinningRate(this.totalPrize);
     }
 
-    private int calculate(Result result) {
-        return result.calculateTotalWinningPrize();
+    private int setTotalPrize(Result result) {
+        return result.calculateTotalPrize();
     }
 }

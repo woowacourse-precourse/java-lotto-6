32b4ac962595924642b3904db0b83399655b77dd
Jang DaeYoung <789456jang@naver.com>
2023-11-08 14:24:01 +0900

    feat: 당첨 번호에 보너스 번호가 포함되면 에러 처리

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -2,19 +2,29 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.util.message.ExceptionMessage.CHECK_DUPLICATES;
+
 public class WinningNumbers {
 
     private final Lotto winningNumbers;
     private final BonusNumber bonusNumber;
 
     public WinningNumbers(Lotto winningNumbers, BonusNumber bonusNumber) {
+        checkDuplicateWinningNumInBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }
 
+    private void checkDuplicateWinningNumInBonusNumber(Lotto winningNumbers, BonusNumber bonusNumber) {
+        List<Integer> numbers = winningNumbers.getNumbers();
+        if (bonusNumber.containsIn(numbers)) {
+            throw new IllegalArgumentException(CHECK_DUPLICATES.getMessage());
+        }
+    }
+
     public RankInfo compareTo(Lotto lottoTicket) {
         List<Integer> numbers = lottoTicket.getNumbers();
-        int count = (int) winningNumbers.getNumbers().stream()
+        Integer count = (int) winningNumbers.getNumbers().stream()
                 .filter(numbers::contains)
                 .count();
         boolean isBonusNumContained = bonusNumber.containsIn(numbers);

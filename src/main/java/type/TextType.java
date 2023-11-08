dd3592e678e39352ba2c607b16cbf6290b588fbf
win9 <krrr883@gmail.com>
2023-11-08 17:55:31 +0900

    [Feat]: 텍스트 추가
    텍스트타입 변경 및 추가

--- src/main/java/type/TextType.java
+++ src/main/java/type/TextType.java
@@ -6,7 +6,10 @@ public enum TextType {
     PRICE_TO_NUMBER("개를 구매했습니다."),
     LOTTO_BONUS("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS("당첨 통계"),
-    WINNING_BAR("---");
+    WINNING_BAR("---"),
+    MATCH_NUMBER_RESULT("개 일치"),
+    COUNT_UNIT("개"),
+    EARN_PERCENT("총 수익률은");
 
     private final String text;
 

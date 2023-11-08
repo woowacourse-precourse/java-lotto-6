66013967788ffdd0887bc1961c6efe9358fa64b5
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:12:46 +0900

    MoneyAmountException 1000으로 나눠 떨어지는값

--- src/test/java/lotto/exception/LottoNumberRangeExceptionTest.java
+++ src/test/java/lotto/exception/LottoNumberRangeExceptionTest.java
@@ -1,35 +1,45 @@
 package lotto.exception;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 class LottoNumberRangeExceptionTest {
+    @DisplayName("1에서 45 사이의 유효한 숫자는 예외가 발생하지 않는다.")
     @Test
-    public void testNumberRangeException_ValidNumber() {
-
+    void testNumberRangeException_ValidNumber() {
+        // Arrange
         int validNumber = 42;
 
-
-        LottoNumberRangeException.NumberRangeException(validNumber);
-
-
+        // Act & Assert
+        assertThatCode(() -> LottoNumberRangeException.NumberRangeException(validNumber))
+                .doesNotThrowAnyException();
     }
 
+    @DisplayName("범위를 벗어난 숫자는 NumberFormatException 예외가 발생한다.")
     @Test
-    public void testNumberRangeException_OutOfRange() {
+    void testNumberRangeException_OutOfRange() {
         // Arrange
         int outOfRangeNumber = 50;
 
-        // Act
-        LottoNumberRangeException.NumberRangeException(outOfRangeNumber);
+        // Act & Assert
+        assertThatCode(() -> LottoNumberRangeException.NumberRangeException(outOfRangeNumber))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("[ERROR]1~45사이만 입력가능합니다");
     }
 
+    @DisplayName("음수 숫자는 NumberFormatException 예외가 발생한다.")
     @Test
-    public void testNumberRangeException_NegativeNumber() {
+    void testNumberRangeException_NegativeNumber() {
         // Arrange
         int negativeNumber = -5;
 
-        // Act
-        LottoNumberRangeException.NumberRangeException(negativeNumber);
+        // Act & Assert
+        assertThatCode(() -> LottoNumberRangeException.NumberRangeException(negativeNumber))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("[ERROR]1~45사이만 입력가능합니다");
     }
 }
--- src/test/java/lotto/exception/MoneyAmountExceptionTest.java
+++ src/test/java/lotto/exception/MoneyAmountExceptionTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
-class MoneyAmountExceptionTest {
-  
+package lotto.exception;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class MoneyAmountExceptionTest {
+    @DisplayName("1000원 단위로 나누어 떨어지는 금액은 예외가 발생하지 않는다.")
+    @Test
+    void testMoneyReader_ValidAmount() {
+        int validAmount = 3000; // 1000원 단위로 나누어 떨어진다
+
+
+        assertThatCode(() -> MoneyAmountException.money_reader(validAmount))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("1000원 단위로 나누어 떨어지지 않는 금액은 NumberFormatException 예외가 발생한다.")
+    @Test
+    void testMoneyReader_InvalidAmount() {
+        // Arrange
+        int invalidAmount = 2500; // 1000원 단위로 나누어 떨어지지 않는다
+
+        // Act & Assert
+        assertThatThrownBy(() -> MoneyAmountException.money_reader(invalidAmount))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessage("[ERROR]알맞는 값이 아닙니다 다시 입력 하세요");
+    }
 }

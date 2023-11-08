af3ea702d64ecc8eb7c699f80fd76a2fcfb4ec87
sora0319 <sojeoung536985@gmail.com>
2023-11-08 12:29:52 +0900

    refactor (LottoTicketMaker) : create class to make lotto list

---
+++ src/main/java/lotto/LottoTicketMaker.java
@@ -0,0 +1,31 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.view.OutputView;
+
+public class LottoTicketMaker {
+    public List<Lotto> make(int purchasedCount) {
+        List<Lotto> lottoTicket = new ArrayList<>();
+        for (int count = 0; count < purchasedCount; count++) {
+            lottoTicket.add(makeLotto());
+        }
+        return lottoTicket;
+    }
+
+    private Lotto makeLotto() {
+        try {
+            return new Lotto(makeRandomUniqueNumber(1, 45, 6));
+        } catch (IllegalStateException exception) {
+            OutputView.print(exception.getMessage());
+            OutputView.print("\n");
+            return makeLotto();
+        }
+
+    }
+
+    private List<Integer> makeRandomUniqueNumber(int start, int end, int count) {
+        return Randoms.pickUniqueNumbersInRange(start, end, count);
+    }
+}
---
+++ src/test/java/lotto/LottoTicketMakerTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoTicketMakerTest {
+    @DisplayName("로또 티켓을 만들어서 전달한다")
+    @Test
+    void createLottoTicket() {
+        assertThat(new LottoTicketMaker().make(5)).isInstanceOf(List.class);
+    }
+}

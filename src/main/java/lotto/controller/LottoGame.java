856bbe0fd468d5f59ba61dcd15e66a6bb1954c32
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 16:27:05 +0900

    :sparkles: Feat: 사용자 입력에 대한 예외 처리
    - 사용자 입력이 Null이 들어왔을 때 예외를 던진다.
    - 사용자 입력이 빈칸으로 들어왔을 때 예외를 던진다.

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
+import static java.util.Objects.isNull;
+import static lotto.common.exception.InputErrorMessage.NOT_BLANK;
+import static lotto.common.exception.InputErrorMessage.NOT_NULL;
 import static lotto.util.StringUtil.convertToNumber;
 import static lotto.util.StringUtil.convertToNumbers;
 
-import lotto.model.LottoVendor;
+import lotto.common.exception.InputException;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutPutView;
@@ -37,7 +40,9 @@ public class LottoGame {
         while(true) {
             try {
                 String winningNumbers = InputView.askWinningNumber();
+                validateInput(winningNumbers);
                 String bonusNumber = InputView.askBonusNumber();
+                validateInput(bonusNumber);
                 lottoService.generateWinningNumber(convertToNumbers(winningNumbers), convertToNumber(bonusNumber));
                 break;
             }
@@ -51,6 +56,7 @@ public class LottoGame {
         while(true) {
             try {
                 String paymentAmount = InputView.askPurchaseAmount();
+                validateInput(paymentAmount);
                 lottoService.purchaseLotto(convertToNumber(paymentAmount));
                 OutPutView.printLottoNumbers(lottoService.getLottoNumbers());
                 break;
@@ -59,6 +65,19 @@ public class LottoGame {
                 OutPutView.printErrorMessage(exception.getMessage());
             }
         }
+    }
+
+    private void validateInput(String input) {
+        validateInputIsNull(input);
+        validateInputIsBlank(input);
+    }
+
+    private void validateInputIsNull(String input) {
+        if(isNull(input)) throw new InputException(NOT_NULL);
+    }
+
+    private void validateInputIsBlank(String input) {
+        if(input.isBlank()) throw new InputException(NOT_BLANK);
     }
 
 }

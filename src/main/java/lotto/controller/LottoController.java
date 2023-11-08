72c7cf2acbf9782d955d27e62bcf04843e9234a6
JayMon0327 <kioplm0211@naver.com>
2023-11-08 20:22:40 +0900

    로또 컨트롤러에 당첨 내역 선정 메서드 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -2,11 +2,18 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
+import lotto.model.LottoRank;
+import lotto.model.WinningLotto;
+import lotto.view.InputView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoController {
+
+    private final InputView inputView = new InputView();
 
     private List<Lotto> generateLottos(int numberOfLottos) {
         List<Lotto> lottos = new ArrayList<>();
@@ -15,6 +22,26 @@ public class LottoController {
             lottos.add(new Lotto(numbers));
         }
         return lottos;
+    }
+
+    private WinningLotto getWinningLotto() {
+        Lotto winningLotto = inputView.promptWinningNumbers();
+        int bonusNumber = inputView.promptBonusNumber(winningLotto);
+        return new WinningLotto(winningLotto, bonusNumber);
+    }
+
+    private Map<LottoRank, Integer> determineResults(List<Lotto> lottos, WinningLotto winningLotto) {
+        Map<LottoRank, Integer> resultMap = new HashMap<>();
+        for (LottoRank rank : LottoRank.values()) {
+            resultMap.put(rank, 0);
+        }
+
+        for (Lotto lotto : lottos) {
+            LottoRank rank = winningLotto.match(lotto);
+            resultMap.merge(rank, 1, Integer::sum);
+        }
+
+        return resultMap;
     }
 
 }

fd184ea86eb6e7a44366e124d18114ab990cc355
노정완 <jeongwan.noh@gmail.com>
2023-11-08 16:25:49 +0900

    refactor: ExceptionMessage 내의 값에 확장성 부여

--- src/main/java/lotto/domain/validation/ExceptionMessage.java
+++ src/main/java/lotto/domain/validation/ExceptionMessage.java
@@ -1,10 +1,17 @@
 package lotto.domain.validation;
 
+import static lotto.domain.constants.LottoConstraint.*;
+
 public enum ExceptionMessage {
     EXCEPTION_MESSAGE_PREFIX("[ERROR] "),
-    NUMBER_FORMAT_EXCEPTION("1000원 단위의 숫자를 입력해 주세요. 최소 금액은 1000원 입니다."),
-    BONUS_NUMBER_FORMAT_EXCEPTION("1~45범위의 숫자를 입력해 주세요."),
-    LOTTERY_NUMBER_FORMAT_EXCEPTION("1~45범위의 서로 다른 6개의 숫자를 \",\"로 구분해 입력해 주세요."),
+    NUMBER_FORMAT_EXCEPTION(LOTTO_PRICE.getValue() +
+            "원 단위의 숫자를 입력해 주세요. 최소 금액은 " +
+            LOTTO_PRICE.getValue() + "원 입니다."),
+    BONUS_NUMBER_FORMAT_EXCEPTION(NUMBER_RANGE_MINIMUM.getValue() + "~" +
+            NUMBER_RANGE_MAXIMUM.getValue() + "범위의 숫자를 입력해 주세요."),
+    LOTTERY_NUMBER_FORMAT_EXCEPTION(NUMBER_RANGE_MINIMUM.getValue() + "~" +
+            NUMBER_RANGE_MAXIMUM.getValue() + "범위의 서로 다른 " + NUMBER_COUNT.getValue() +
+            "개의 숫자를 \",\"로 구분해 입력해 주세요."),
     BONUS_NUMBER_DUPLICATES_EXCEPTION("보너스 번호는 당첨 번호와 서로 달라야 합니다.");
 
     private final String message;

03bdde8374a40517c429e1e01d0cdfb86b7673b1
DrivenByCode <pjk146195@gmail.com>
2023-11-08 17:11:37 +0900

    feat(Purchase): 구매 금액 입력 기능 추가

---
+++ src/main/java/input/Purchase.java
@@ -0,0 +1,55 @@
+package input;
+
+import static constant.ErrorMessage.buildErrorMessage;
+
+import constant.ErrorMessage;
+import constant.ServiceMessage;
+import java.util.Scanner;
+
+public class Purchase {
+    private String money;
+    private final int PRICE_PER_TICKET = 1000;
+    private boolean isValidInput = false;
+
+    public Purchase() {
+        while(!isValidInput) {
+            getPurchaseCount();
+        }
+    }
+
+    private void getPurchaseCount(){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print(ServiceMessage.getMessageByCode(101));
+            this.money = scanner.nextLine();
+            isValidInput = isValidate(money);
+        } catch (IllegalArgumentException e) {
+            System.out.print(buildErrorMessage(902));
+        }
+    }
+    private boolean isValidate(final String money) {
+        validNumber(money);
+        int amount = Integer.parseInt(money);
+        validAmount(amount);
+
+        return true;
+    }
+
+    private void validNumber(final String number) {
+        if(!number.matches("\\d+")) {
+            throw new IllegalArgumentException(buildErrorMessage(901));
+        }
+    }
+    private boolean isValidAmount(final int amount) {
+        return (amount >= PRICE_PER_TICKET) && (amount % PRICE_PER_TICKET == 0);
+    }
+    private void validAmount(final int amount) {
+        if(!isValidAmount(amount)) {
+            throw new IllegalArgumentException(buildErrorMessage(902));
+        }
+    }
+
+    public int getMoney(){
+        return Integer.parseInt(money);
+    }
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import constant.ErrorMessage;
+import constant.ServiceMessage;
+import input.Purchase;
+import java.util.List;
+import output.LottoCounter;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Purchase purchase = new Purchase();
+        int money = purchase.getMoney();
+
+        LottoCounter.printLottos(money);
     }
 }

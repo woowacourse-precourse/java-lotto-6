dcde2f6ba5327316ece9a880603198062c56e668
eun-byeol <joeunbyeol98@gmail.com>
2023-11-08 22:18:24 +0900

    test(LottoServiceImplTest): 로또 당첨 순위 카운팅하기 테스트 코드 작성

--- src/test/java/lotto/service/LottoServiceImplTest.java
+++ src/test/java/lotto/service/LottoServiceImplTest.java
@@ -1,12 +1,18 @@
 package lotto.service;
 
+import static lotto.enums.WinningRankType.FIRST_PLACE;
 import static lotto.enums.WinningRankType.FOURTH_PLACE;
+import static lotto.enums.WinningRankType.NO_RANK;
+import static lotto.enums.WinningRankType.SECOND_PLACE;
+import static lotto.enums.WinningRankType.THIRD_PLACE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import lotto.domain.Lotto;
+import lotto.domain.Lottos;
 import lotto.domain.WinningLotto;
 
+import lotto.domain.WinningResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -37,13 +43,36 @@ class LottoServiceImplTest {
     @DisplayName("당첨 순위 매칭하기")
     @Test
     void testMatchRankType() {
-        Lotto lotto = new Lotto(List.of(11,22,33,44,15,26));
-        WinningLotto winningLotto = new WinningLotto();
-        winningLotto.setWinningLotto(new Lotto(List.of(11,22,33,44,35,36)));
-        winningLotto.setBonusNumber(15);
+        Lotto lotto = new Lotto(List.of(11,22,3,4,5,6));
+        WinningLotto winningLotto = setupWinningLotto();
 
         assertThat(lottoService.matchRankType(lotto, winningLotto))
                 .isEqualTo(FOURTH_PLACE);
     }
 
+    @DisplayName("당첨 순위 카운팅하기")
+    @Test
+    void testCalculateWinning() {
+        WinningResult winningResult = lottoService.calculateWinning(setupLottos(), setupWinningLotto());
+        assertThat(winningResult.getResult().get(FIRST_PLACE)).isEqualTo(1);
+        assertThat(winningResult.getResult().get(SECOND_PLACE)).isEqualTo(1);
+        assertThat(winningResult.getResult().get(THIRD_PLACE)).isEqualTo(1);
+        assertThat(winningResult.getResult().get(NO_RANK)).isEqualTo(1);
+    }
+
+    private WinningLotto setupWinningLotto() {
+        WinningLotto winningLotto = new WinningLotto();
+        winningLotto.setWinningLotto(new Lotto(List.of(3,4,5,6,10,20)));
+        winningLotto.setBonusNumber(40);
+        return winningLotto;
+    }
+
+    Lottos setupLottos() {
+        Lottos lottos = new Lottos(4000);
+        lottos.addLotto(new Lotto(List.of(1,20,3,4,5,6)));
+        lottos.addLotto(new Lotto(List.of(10,20,3,40,5,6)));
+        lottos.addLotto(new Lotto(List.of(10,20,6,5,4,3)));
+        lottos.addLotto(new Lotto(List.of(1,2,3,40,16,22)));
+        return lottos;
+    }
 }

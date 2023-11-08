cc3b9add6316d9e566b8e2a1f991062dda4845fa
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 14:27:05 +0900

    feat: prize statistic file first commit
    총 상금 계산하고 출력하는 파일 first commit

---
+++ src/main/java/lotto/service/PrizeStatisticService.java
@@ -0,0 +1,32 @@
+package lotto.service;
+
+import java.util.Map;
+import lotto.domain.Prize;
+import lotto.view.OutputView;
+
+public class PrizeStatisticService {
+    public static int processAndReportPrizeResults(Map<Prize, Integer> prizeCounts) {
+        OutputView.printPrizeStaticIntro();
+        int totalPrizeMoney = 0;
+        for (Map.Entry<Prize, Integer> entry : prizeCounts.entrySet()) {
+            Prize prize = entry.getKey();
+            Integer count = entry.getValue();
+
+            totalPrizeMoney += calculateWinningPrize(prize, count);
+            printPrizeStatistics(prize, count);
+        }
+        return totalPrizeMoney;
+    }
+
+    public static int calculateWinningPrize(Prize prize, int count) {
+        return prize.getPrizeAmount() * count;
+    }
+
+    public static void printPrizeStatistics(Prize prize, int count) {
+        OutputView.printTotalStats(prize, count);
+    }
+
+
+}
+
+

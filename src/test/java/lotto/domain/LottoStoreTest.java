c80cbe931a3f9ad75eeb0e82b94f79d0a17dbfc0
twoosky <lhjae3@naver.com>
2023-11-08 17:27:40 +0900

    test: 로또 판매점 싱글톤 패턴 객체 반환 메소드 테스트 작성

--- src/test/java/lotto/domain/LottoStoreTest.java
+++ src/test/java/lotto/domain/LottoStoreTest.java
@@ -1,4 +1,23 @@
 package lotto.domain;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@DisplayName("LottoStore 클래스")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class LottoStoreTest {
+
+    @Test
+    void getInstance_정적_팩토리_메소드는_같은_인스턴스를_반환한다() {
+        LottoStore firstInstance = LottoStore.getInstance();
+        LottoStore secondInstance = LottoStore.getInstance();
+
+        Assertions.assertEquals(firstInstance, secondInstance);
+    }
+
 }

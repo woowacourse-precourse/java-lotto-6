ec58ec052a7720aacd2b7834fc698a2b8b3dfc77
sangdol <priceelees@gmail.com>
2023-11-08 13:08:22 +0900

    refactor(LottoViewController): 새롭게 정의한 InputViewable을 적용

--- src/main/java/lotto/controller/LottoViewController.java
+++ src/main/java/lotto/controller/LottoViewController.java
@@ -6,14 +6,15 @@ import static lotto.view.constant.ViewConstant.OutputViewConstant.WINNING_NUMBER
 
 import java.util.List;
 import java.util.function.Supplier;
-import lotto.view.InputView;
+import lotto.view.BonusNumberInputView;
 import lotto.view.OutputView;
+import lotto.view.PurchaseAmountInputView;
+import lotto.view.WinningNumberInputView;
 
 public class LottoViewController {
-    private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
 
-    private <T> T receive(final String order, final Supplier<T> supplier) {
+    private <T> T retryInputOnException(final String order, final Supplier<T> supplier) {
         try {
             outputView.request(order);
             final T result = supplier.get();
@@ -22,20 +23,23 @@ public class LottoViewController {
             return result;
         } catch (IllegalArgumentException e) {
             outputView.printErrorMessage(e.getMessage());
-            return receive(order, supplier);
+            return retryInputOnException(order, supplier);
         }
     }
 
     long receivePurchaseAmount() {
-        return receive(AMOUNT, inputView::receivePurchaseAmountInput);
+        PurchaseAmountInputView purchaseAmountInputView = new PurchaseAmountInputView();
+        return retryInputOnException(AMOUNT, purchaseAmountInputView);
     }
 
     String receiveWinningNumber() {
-        return receive(WINNING_NUMBER, inputView::receiveWinningNumberInput);
+        WinningNumberInputView winningNumberInputView = new WinningNumberInputView();
+        return retryInputOnException(WINNING_NUMBER, winningNumberInputView);
     }
 
     int receiveBonusNumber(final String winningNumber) {
-        return receive(BONUS_NUMBER, () -> inputView.receiveBonusNumberInput(winningNumber));
+        BonusNumberInputView bonusNumberInputView = new BonusNumberInputView(winningNumber);
+        return retryInputOnException(BONUS_NUMBER, bonusNumberInputView);
     }
 
     void printResultMessage(final List<Long> winCounts, final double earningRate) {

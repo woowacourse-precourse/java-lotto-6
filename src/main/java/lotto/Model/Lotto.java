96d588be59362e270ad33ae0e73ffb5628197a6d
myoungjin-kim <kaudwls58@naver.com>
2023-11-08 21:45:40 +0900

    feat: Lotto 클래스 추가
    로또 객체를 생성하고 번호가 유효한지 검증하는 기능을 가집니다.
    로또 번호를 반환하는 기능을 가집니다.

---
+++ src/main/java/lotto/Model/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.Model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
+        }
+        if (hasDuplicate(numbers)) {
+            throw new IllegalArgumentException("로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private boolean hasDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() != numbers.size();
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+}

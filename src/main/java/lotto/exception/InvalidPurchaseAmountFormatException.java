43bebd5f9ee0c4cede87f28c5438453f326666b3
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 19:12:52 +0900

    feat(exception) : add InvalidPurchaseAmountFormatException class

---
+++ src/main/java/lotto/exception/InvalidPurchaseAmountFormatException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class InvalidPurchaseAmountFormatException extends IllegalArgumentException {
+    public InvalidPurchaseAmountFormatException() {
+        super("[ERROR] 구입 금액은 숫자만 입력할 수 있습니다.");
+    }
+}

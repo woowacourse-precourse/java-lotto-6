75a4a1fca1460eda04bbc9edb6f75b7b70530cde
jhsseonn <hs125bt@gmail.com>
2023-11-08 23:05:31 +0900

    test: 사용자가 입력한 당첨번호가 6개가 아닌 경우 예외처리 테스트 통과

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoServiceTest implements LottoFinalConsts {
@@ -19,21 +20,18 @@ class LottoServiceTest implements LottoFinalConsts {
     private List<Lotto> lottos = new ArrayList<>(List.of(lotto));
     private HashMap<LottoRank, Integer> lottoRanks = new HashMap<>();
     private LottoMachine lottoMachine = new LottoMachine(lottoPurchase, lottos, lottoRanks);
-
-    @Test
-    void getLottos() {
-    }
-
-    @Test
-    void createLottoMachine() {
-    }
+    private LottoService lottoService = new LottoService();
 
     @Test
     void saveWinningAndBonusNumbers() {
     }
 
     @Test
-    void validateWinningSize() {
+    void 사용자가_입력한_당첨번호가_6개가_아닌_경우_예외를_발생한다() {
+        String winningNumber = "1,2,3,4,5,6,7";
+        String[] winning = winningNumber.split(LOTTO_WINNING_SPLIT);
+        assertThatThrownBy(() -> lottoService.validateWinningSize(winning))
+                .isInstanceOf(ArrayIndexOutOfBoundsException.class);
     }
 
     @Test

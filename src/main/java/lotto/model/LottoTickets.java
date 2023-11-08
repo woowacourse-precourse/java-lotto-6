78bc7b54e424b5f38d0144c986ccd340755128bc
voyager003 <miraclex000@gmail.com>
2023-11-08 20:38:03 +0900

    refactor(LottoTickets): fix buyLotto method to lambda expression
    FIX using lambda expression for improved readability

--- src/main/java/lotto/model/LottoTickets.java
+++ src/main/java/lotto/model/LottoTickets.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class LottoTickets {
 
@@ -19,9 +20,9 @@ public class LottoTickets {
     }
 
     public void buyLotto(int count) {
-        for (int i = 0; i < count; i++) {
-            tickets.add(new Lotto(generateLottoNumbers()));
-        }
+        IntStream.range(0, count)
+                .mapToObj(i -> new Lotto(generateLottoNumbers()))
+                .forEach(tickets::add);
     }
 
     private List<Integer> generateLottoNumbers() {

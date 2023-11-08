6358730750d1e32f66f6ff217a7584e45ad24237
ogi <ogi@naver.com>
2023-11-08 23:15:18 +0900

    [fix] RuntimeException 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,16 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.buyLotto();
-        lottoController.checkLottoResult();
-
+        try {
+            LottoController lottoController = LottoController.lottoPlayController();
+            lottoController.buyLotto();
+            lottoController.checkLottoResult();
+        } catch (RuntimeException e) {
+            OutputView.showErrorMessage(String.valueOf(e));
+        }
     }
 }

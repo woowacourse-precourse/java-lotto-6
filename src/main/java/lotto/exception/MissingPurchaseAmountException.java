e20bcea8239efc9ad69287d1a955649298a94ced
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 19:13:44 +0900

    feat(exception) : add MissingPurchaseAmountException class

---
+++ src/main/java/lotto/exception/MissingPurchaseAmountException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class MissingPurchaseAmountException extends IllegalArgumentException {
+    public MissingPurchaseAmountException() {
+        super("[ERROR] 구입 금액은 공백일 수 없습니다.");
+    }
+}

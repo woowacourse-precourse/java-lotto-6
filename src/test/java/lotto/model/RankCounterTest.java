f8a088fa88df0a8df6a1de8a7d965dfa35b5affa
김현재 <khjmr@naver.com>
2023-11-08 16:49:32 +0900

    test(toStringTest): 통계 결과를 뽑아내는 테스트 추가

--- src/test/java/lotto/model/RankCounterTest.java
+++ src/test/java/lotto/model/RankCounterTest.java
@@ -9,7 +9,21 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RankCounterTest {
+    @DisplayName("통계 결과를 올바르게 뽑아내는지 테스트")
+    @Test
+    void toStringTest() {
+        Integer numberOfLottos = 10;
+        List<Rank> ranks = List.of(Rank.THIRD, Rank.SECOND);
+        RankCounter rankCounter = new RankCounter(ranks);
 
+        String expected = "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 1개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 0개\n";
+
+        Assertions.assertThat(rankCounter.toString()).isEqualTo(expected);
+    }
     @DisplayName("이익률을 올바르게 계산하는지 테스트한다.")
     @Test
     void computeBenefit() {

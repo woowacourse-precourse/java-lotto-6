7a5ce23818f0f28b5a172b86cbb6b5e47577d240
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 13:23:52 +0900

    feat: Judgement 기능 구현

--- src/main/java/lotto/service/Judgement.java
+++ src/main/java/lotto/service/Judgement.java
@@ -1,4 +1,10 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.config.WiningRank;
+
 public interface Judgement {
+    public int judgeWiningNumber(List<Integer> lotto, List<Integer> winingNumber);
+    public boolean judgeBonusNumber(List<Integer> lotto, int bonusNumber);
+    public WiningRank judgeRanking(int winingNumberCount, boolean bonusNumCount);
 }

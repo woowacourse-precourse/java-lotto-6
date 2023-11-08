af2fecb1d876af3effa59c5f71a4a72d1def4a05
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 18:21:11 +0900

    test: 당첨 로또번호와 보너스번호가 주어졌을 때 로또컬렉션의 추첨 결과 반환 테스트코드 작성

---
+++ src/test/java/lotto/domain/entity/LottosTest.java
@@ -0,0 +1,55 @@
+package lotto.domain.entity;
+
+import lotto.service.manager.Rank;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottosTest {
+
+    @DisplayName("추첨 등수 계산 성공")
+    @Test
+    public void calculateRankings() throws  Exception {
+        //given
+        Lotto target = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 10;
+
+        Lotto notingLotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
+        Lotto firstLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto secondLotto = new Lotto(List.of(2, 3, 4, 5, 6, bonusNumber));
+        Lotto thirdLotto = new Lotto(List.of(2, 3, 4, 5, 6, 7));
+        Lotto fourthLotto = new Lotto(List.of(3, 4, 5, 6, 7, 8));
+        Lotto fifthLotto = new Lotto(List.of(4, 5, 6, 7, 8, 9));
+
+        Lottos lottos = new Lottos(
+                List.of(
+                        notingLotto,
+                        firstLotto,
+                        secondLotto,
+                        thirdLotto,
+                        fourthLotto,
+                        fifthLotto
+                )
+        );
+
+        long expectedSize = 5;
+        Rank[] expected = {
+                Rank.FIRST,
+                Rank.SECOND,
+                Rank.THIRD,
+                Rank.FOURTH,
+                Rank.FIFTH
+        };
+
+        //when
+        List<Rank> result = lottos.calculateRankings(target, bonusNumber);
+        //then
+        Assertions.assertThat(result.size()).isEqualTo(expectedSize);
+        Assertions.assertThat(result).containsExactly(expected);
+    }
+
+}

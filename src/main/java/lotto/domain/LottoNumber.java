6d41d719823cab116c2d2161745297c51619725e
Jiihyun <cwlgusc@gmail.com>
2023-11-08 13:00:03 +0900

    refactor(LottoNumber): 번호 검증 메서드 LottoValidator 클래스로 옮김

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,23 +1,15 @@
 package lotto.domain;
 
-import java.util.Objects;
+import lotto.validator.LottoValidator;
 
-import static lotto.domain.LottoNumberGenerator.END_INCLUSIVE;
-import static lotto.domain.LottoNumberGenerator.START_INCLUSIVE;
-import static lotto.exception.ExceptionMessage.LOTTO_NUMBER_RANGE;
+import java.util.Objects;
 
 public class LottoNumber {
     private final int number;
 
     public LottoNumber(int number) {
-        validateNumberRange(number);
+        LottoValidator.validateNumberRange(number);
         this.number = number;
-    }
-
-    public void validateNumberRange(int number) {
-        if (number < START_INCLUSIVE || number > END_INCLUSIVE) {
-            throw LOTTO_NUMBER_RANGE.getException(START_INCLUSIVE, END_INCLUSIVE);
-        }
     }
 
     public int getNumber() {
--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -7,7 +7,10 @@ import lotto.exception.ExceptionMessage;
 import java.util.List;
 
 import static lotto.domain.Lotto.LOTTO_NUMBERS_LENGTH;
+import static lotto.domain.LottoNumberGenerator.END_INCLUSIVE;
+import static lotto.domain.LottoNumberGenerator.START_INCLUSIVE;
 import static lotto.exception.ExceptionMessage.LOTTO_NUMBERS_DUPLICATED;
+import static lotto.exception.ExceptionMessage.LOTTO_NUMBER_RANGE;
 
 public class LottoValidator {
 
@@ -33,9 +36,16 @@ public class LottoValidator {
         }
     }
 
+    public static void validateNumberRange(int number) {
+        if (number < START_INCLUSIVE || number > END_INCLUSIVE) {
+            throw LOTTO_NUMBER_RANGE.getException(START_INCLUSIVE, END_INCLUSIVE);
+        }
+    }
+
     private static long getDistinctLength(List<Integer> numbers) {
         return numbers.stream()
                 .distinct()
                 .count();
     }
+
 }

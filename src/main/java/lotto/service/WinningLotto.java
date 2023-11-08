7e651cdcc76a0af6b3abde535bb402f9e8a538af
devhoya97 <diajbh123@gmail.com>
2023-11-08 10:28:51 +0900

    refactor(service): PrizeChecker의 이름을 WinningLotto로 변경 및 책임 변경
    BonusNumber 클래스를 없앰에 따라 당첨 번호와 보너스 번호의 중복을 검사해줄
    객체가 필요했고, PrizeChecker가 이 둘을 멤버변수로 가지고 있으므로
    해당 책임을 위임하면서, 좀 더 적절한 이름으로 변경함

--- src/main/java/lotto/service/WinningLotto.java
+++ src/main/java/lotto/service/WinningLotto.java
@@ -1,21 +1,32 @@
 package lotto.service;
 
+import static lotto.utils.ErrorMessages.WINNING_NUMBERS_CONTAINS_BONUS_NUMBER;
+
 import lotto.domain.Lotto;
+import lotto.domain.wrapper.PurchaseAmount;
+import lotto.utils.LottoConstantValue;
 import lotto.utils.Prize;
 
-public class PrizeChecker {
+public class WinningLotto {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public PrizeChecker(Lotto winningLotto, int bonusNumber) {
+    public WinningLotto(Lotto winningLotto, int bonusNumber) {
+        validateWinningLottoNotContainsBonusNumber(winningLotto, bonusNumber);
         this.winningLotto = winningLotto;
         this.bonusNumber = bonusNumber;
     }
 
-    public Prize calculatePrize(Lotto lotto) {
+    private void validateWinningLottoNotContainsBonusNumber(Lotto winningLotto, int bonusNumber) {
+        if (winningLotto.hasNumber(bonusNumber)) {
+            throw new IllegalArgumentException(WINNING_NUMBERS_CONTAINS_BONUS_NUMBER);
+        }
+    }
+
+    public Prize getPrize(Lotto lotto) {
         int sameCount = lotto.getSameCount(winningLotto);
         if (Prize.canGetPrize(sameCount)) {
-            boolean hasBonusNumber = lotto.doesHaveLottoNumber(bonusNumber);
+            boolean hasBonusNumber = lotto.hasNumber(bonusNumber);
             return Prize.getPrize(sameCount, hasBonusNumber);
         }
         return Prize.NO_PRIZE;

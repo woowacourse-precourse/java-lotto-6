935a6ed7c200fc3e9c88cf3548e3fdace9c679ea
kkonii <119216472+kkonii@users.noreply.github.com>
2023-11-08 13:59:55 +0900

    Refactor(inputView) : 중복되는 함수를 메서드화

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -6,18 +6,22 @@ public class InputView {
     public static String requestPrice() {
         System.out.println("구입금액을 입력해 주세요.");
 
-        return Console.readLine();
+        return reqeustInput();
     }
 
     public static String requestNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        return Console.readLine();
+        return reqeustInput();
     }
 
     public static String requestBonus() {
         System.out.println("보너스 번호를 입력해 주세요.");
 
+        return reqeustInput();
+    }
+
+    private static String reqeustInput() {
         return Console.readLine();
     }
 }

130f3dc988328832d688da3be26dce1936026a98
“sangjun121” <juncho12011201@gmail.com>
2023-11-08 23:33:26 +0900

    fix: 테스트 오류로 인한 Collection.sort()메소드 제거

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -3,9 +3,8 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.utility.enums.Standards;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class LottoGenerator {
     private List<Integer> lottoNumbers;
@@ -19,12 +18,19 @@ public class LottoGenerator {
     public List<Lotto> generate(int lottoCount){
         for(int i = 0; i < lottoCount; i++){
             lottoNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-            Collections.sort(lottoNumbers);
+            lottoNumbers = sortNumbers(lottoNumbers);
             Lotto lotto = new Lotto(Collections.unmodifiableList(lottoNumbers));
             lottoCollection.add(lotto);
         }
         Collections.unmodifiableList(lottoCollection);
 
         return lottoCollection;
+    }
+
+    private List<Integer> sortNumbers(List<Integer> lottoNumbers){
+        int arrSize = lottoNumbers.size();
+        Integer arr[] = lottoNumbers.toArray(new Integer[arrSize]);
+        Arrays.sort(arr);
+        return Arrays.asList(arr);
     }
 }

2bc42093049a96bedb30ca897729b6f2dc343a20
방준호 <yureka0920@naver.com>
2023-11-08 21:03:44 +0900

    NumberFormatException 예외처리 추가. 테스트 성공.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +21,12 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void createLottoByLessSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
@@ -27,6 +34,14 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호에 문자가 있으면 예외 발생한다.")
+    @Test
+    void createLottoByNumberFormatException() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        Assertions.assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt("1000j");
+        });
     }
 
     // 아래에 추가 테스트 작성 가능

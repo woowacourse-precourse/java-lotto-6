0bec4126d7801bf62bab203ce14872eaebe4c1f8
mdgome <mdgome3@gmail.com>
2023-11-08 14:19:04 +0900

    feat: 당첨 번호 예외 처리 추가
    - [X] 당첨 번호 구분자가 쉼표가 아닌 경우
    - [X] 입력 값이 없는 경우
      - [X] 로또 번호
    - [X] 입력 값이 숫자가 아닌 경우
      - [X] 로또 번호

--- src/main/java/lotto/util/SplitGenerator.java
+++ src/main/java/lotto/util/SplitGenerator.java
@@ -1,7 +1,10 @@
-package lotto.util.generator;
+package lotto.util;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import lotto.constant.ValidatorMessage;
 import lotto.model.Lotto;
 
 public class SplitGenerator {
@@ -10,23 +13,43 @@ public class SplitGenerator {
     private final Lotto lotto;
 
     public SplitGenerator(String lotto) {
+        validate(lotto);
 
+        this.lotto = new Lotto(split(lotto));
     }
 
     public void validate(String lotto) {
-
+        validateIsNull(lotto);
+        validateIsNonContainsComma(lotto);
     }
 
+    public Lotto getLotto() {
+        return this.lotto;
+    }
 
     private List<Integer> split(String lotto) {
+        String[] splitLotto = lotto.split(delimiter);
 
+        try {
+            return Arrays.stream(splitLotto)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_LOTTO_NUMERIC);
+        } catch (NullPointerException exception) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_IS_NULL);
+        }
     }
 
     private void validateIsNonContainsComma(String lotto) {
-
+        if (!lotto.contains(delimiter)) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_WIN_NUMBER_IS_NUMERIC);
+        }
     }
 
     private void validateIsNull(String lotto) {
-
+        if (lotto == null || lotto.isEmpty()) {
+            throw new IllegalArgumentException(ValidatorMessage.INVALID_IS_NULL);
+        }
     }
 }

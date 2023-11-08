6177897b41869c045de7506fe577dad248a8d9d4
dsadara <chaehyun.forest@gmail.com>
2023-11-08 17:06:22 +0900

    refactor: readCash 내부 메소드 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,25 +10,10 @@ public class InputView {
 
     private static final String INPUT_IS_NOT_POSTIVE_NUMBER_ERROR = "양수를 입력해 주세요.";
     private static final String INPUT_IS_NOT_NUMBER_ERROR = "문자가 아닌 숫자를 입력해 주세요.";
+    private static final String INPUT_IS_WRONG = "올바른 숫자를 입력해 주세요.";
 
     public static int readCash() {
-        int cash = StringToInteger(Console.readLine());
-        validatePostiveNumber(cash);
-        return cash;
-    }
-
-    private static int StringToInteger(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException numberFormatException) {
-            throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER_ERROR);
-        }
-    }
-
-    private static void validatePostiveNumber(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException(INPUT_IS_NOT_POSTIVE_NUMBER_ERROR);
-        }
+        return convertToPositiveNumber(Console.readLine());
     }
 
     public static List<Integer> readWinningNumber() {
@@ -38,9 +23,32 @@ public class InputView {
     private static List<Integer> splitToList(String input) {
         return Stream.of(input.split(","))
                 .map(String::trim)
-                .map(InputView::StringToInteger)
-                .peek(InputView::validatePostiveNumber)
+                .map(InputView::convertToPositiveNumber)
                 .collect(Collectors.toList());
+    }
+
+    private static int convertToPositiveNumber(String input) {
+        int number;
+        try {
+            validateNumeric(input);
+            number = Integer.parseInt(input);
+            validatePostiveNumber(number);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(INPUT_IS_WRONG);
+        }
+        return number;
+    }
+
+    private static void validateNumeric(String input) {
+        if (input != null && input.matches("![0-9]]")) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_NUMBER_ERROR);
+        }
+    }
+
+    private static void validatePostiveNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(INPUT_IS_NOT_POSTIVE_NUMBER_ERROR);
+        }
     }
 
 }

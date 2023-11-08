6083a159e68fd273fce5b4dda5b4f7e0d6196730
haebyun <bhk4392@gmail.com>
2023-11-08 19:33:31 +0900

    feat(OutputView): 당첨 내역을 출력하는 기능과 수익률을 형식에 맞추어 출력하는 기능을 구현한다.

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,10 +1,12 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class OutputView {
     private static final String BOUNDARY_LINE = "--------------------------------------------------------";
     private static final String BUY_RESULT_MESSAGE = "%d개를 구매했습니다. \n";
+    private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %s%%입니다. \n";
 
     public static void printErrorMessage(String message) {
         System.out.println(message);
@@ -16,5 +18,15 @@ public class OutputView {
         System.out.println();
         System.out.printf((BUY_RESULT_MESSAGE), buyResults.size());
         buyResults.forEach(buyResult -> System.out.println("[" + buyResult + "]"));
+    }
+
+    public static void printWinningResult(List<String> winningResults) {
+        System.out.println();
+        winningResults.forEach(System.out::println);
+    }
+
+    public static void printRateOfReturn(Double rateOfReturn) {
+        DecimalFormat formatter = new DecimalFormat("###,###.0");
+        System.out.printf((RATE_OF_RETURN_MESSAGE), formatter.format((double)rateOfReturn));
     }
 }

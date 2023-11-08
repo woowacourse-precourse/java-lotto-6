c72a3e722f6695d318f73ca29cdfc8540affe965
jiho kim <jihokim1024@gmail.com>
2023-11-08 23:52:10 +0900

    로또 번호 및 에러 확인 클래스

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,57 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers = new ArrayList<>();
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+    public List<Integer> getLotto() {
+        List<Integer> number = new ArrayList<>(numbers);
+        Collections.sort(number);
+        return number;
+    }
+
+    public void validate(List<Integer> numbers) {
+        validateSize(numbers.size());
+        validateEmpty(numbers);
+        validate1to45(numbers);
+        validateDuplication(numbers);
+    }
+
+    public void validate1to45(List<Integer> numbers) {
+        for(int num : numbers) {
+            if(num < 1 || num > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public void validateEmpty(List<Integer> numbers) {
+        if(numbers.isEmpty()){
+            System.out.println("2");
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void validateSize(int size) {
+        if(size != 6){
+            System.out.println("3");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateDuplication(List<Integer> numbers) {
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            OutputError.error_duplication();
+            throw new IllegalArgumentException();
+        }
+    }
 }

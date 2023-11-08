c486d345fdb8c9de97428d983cf343f8e470e7eb
Jaewon <choijawon99@gmail.com>
2023-11-08 13:35:37 +0900

    feat(prizelotto): 당첨 결과 반환 메서드 추가
    - enum rank를 활용하여 당첨 결과에 맞는 맞은개수, 보너스 번호 당첨 여부 반환

--- src/main/java/lotto/domain/PrizeLotto.java
+++ src/main/java/lotto/domain/PrizeLotto.java
@@ -1,12 +1,30 @@
 package lotto.domain;
 
 public class PrizeLotto {
-    private final Lotto lotto;
+    private final Lotto winLotto;
     private final int bonusNum;
 
     public PrizeLotto(Lotto lotto, int bonusNum){
-        this.lotto = lotto;
+        this.winLotto = lotto;
         this.bonusNum = bonusNum;
     }
 
+    public Lotto getWinLotto(){
+        return this.winLotto;
+    }
+
+    public int getBonusNum(){
+        return this.bonusNum;
+    }
+
+    public Rank match(Lotto purchasedLotto){
+        int count = (int) purchasedLotto.getNumbers().stream()
+                .filter(i -> winLotto.getNumbers().contains(i))
+                .count();
+
+        boolean isBonusMatch = purchasedLotto.getNumbers().stream()
+                .anyMatch(i->i == bonusNum);
+
+        return Rank.valueOf(count, isBonusMatch);
+    }
 }

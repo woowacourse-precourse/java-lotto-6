e6b33b8bbdcba069671882d703eeb0b2891dc628
김주영 <gimjuyeong0620@gmail.com>
2023-11-08 15:37:38 +0900

    [FEAT] 로또 구매 금액 유효성 검증
    1. 로또 구매 금액 validate 추가

--- src/main/java/lotto/view/InputBuyLottoView.java
+++ src/main/java/lotto/view/InputBuyLottoView.java
@@ -4,11 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.common.ErrorMessage;
 import lotto.common.SystemMessage;
 
-public class InputBuyLottoView {
+import java.util.regex.Pattern;
 
+public class InputBuyLottoView {
+    private static final Pattern PATTERN = Pattern.compile("\\d+");
     public Integer insertCoin(){
         System.out.println(SystemMessage.INPUT_MONEY.getMessage());
         String input = Console.readLine();
+        System.out.println();
+        validate(input);
         return Integer.parseInt(input);
+    }
+
+    private void validate(String inputValue){
+        validateFormat(inputValue);
+    }
+
+    private void validateFormat(String inputValue){
+        if(!PATTERN.matcher(inputValue).matches()){
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+        }
     }
 }

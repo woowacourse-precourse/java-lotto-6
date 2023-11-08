a18bc28a29a303fe35099dcc4b3322953843224b
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-08 16:27:05 +0900

    feat: sort Lotto numbers and add get
    Sort when saving a list of numbers in class Lotto
    Add getNumbers()

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -7,6 +9,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        Collections.sort(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +19,8 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }

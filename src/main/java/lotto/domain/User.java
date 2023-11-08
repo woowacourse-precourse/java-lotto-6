1bf42d7c84fb191d41db975c9bcf9ee1f6b8dffb
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 11:17:30 +0900

    test(InputUtilTest) : StringToInt 타입 변환 테스트 코드 작성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -3,6 +3,8 @@ package lotto.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import static lotto.utils.GenerateLottoNumbers.generateLottoNumbers;
+
 // 로또 구매 및 저장, 구매 금액과 총 상금 관리
 public class User {
     private int buyingPrice;

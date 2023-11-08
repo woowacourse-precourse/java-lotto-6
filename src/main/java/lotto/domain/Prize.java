056c2e5dff8ee1697caa494b3447a297fc83ba98
Sungkyu <kxu4583@naver.com>
2023-11-08 15:18:36 +0900

    feat: '등수별 상금 enum' 기능 구현

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public enum Prize {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    FAIL(0, false, 0);
+
+    private final int correctCount;
+    private final boolean bonusMatch;
+    private final int prizeMoney;
+
+    Prize(int correctCount, boolean bonusMatch, int prizeMoney) {
+        this.correctCount = correctCount;
+        this.bonusMatch = bonusMatch;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public static Prize judgeRank(int correctCount, boolean bonusMatch) {
+        for (Prize prize : Prize.values()) {
+            if (prize.correctCount == correctCount && (!prize.bonusMatch || bonusMatch)) {
+                return prize;
+            }
+        }
+        return FAIL;
+    }
+}

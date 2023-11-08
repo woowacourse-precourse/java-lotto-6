df6767ac4413659994a7b6b773d834daa0d0e64a
Mi <mi_aa_@naver.com>
2023-11-09 00:39:37 +0900

    feat: 주요 당첨 번호 6개 추출

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,15 +1,18 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.domain.Cunsumer;
+import lotto.domain.Consumer;
 import lotto.domain.Lotto;
 import lotto.domain.PrizeLotto;
+import lotto.exception.DuplicatedNumber;
 import lotto.exception.NotRangeException;
 import lotto.utils.RandomUtils;
 import lotto.view.LottoPrint;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoService {
     final int amount = 1000;
@@ -18,11 +21,14 @@ public class LottoService {
     final int end = 45;
     RandomUtils randomUtils = new RandomUtils();
     LottoPrint lottoPrint = new LottoPrint();
-    Cunsumer consumer;
-    PrizeLotto prizeLotto;
-    List<Integer> primaryNumber;
+    Consumer consumer;
+    PrizeLotto prizeLotto = new PrizeLotto();
+    Map<Integer, Integer> winning = new HashMap<>();
+    double winningRate = 0;
 
-
+    /**
+     * 로또 구입하기
+     */
     public void purchaseLotto(){
         int money = inputMoney();
         int quantity = money / amount;
@@ -31,14 +37,17 @@ public class LottoService {
         for(int i = 0; i < quantity; i++){
             lotto.add(purchaseOneLotto());
         }
-        consumer = new Cunsumer(lotto);
+        consumer = new Consumer(lotto);
         lottoPrint.purchaseLottoPrint(quantity, consumer.getPurchasedLotto());
     }
 
+    // 로또 한 개 구입하기
     private Lotto purchaseOneLotto(){
         List<Integer> numbers = randomUtils.randomNumber(size, start, end);
         return new Lotto(numbers);
     }
+
+    // 구입 금액 입력
     public int inputMoney() {
         String input = Console.readLine();
         int money = 0;
@@ -57,13 +66,16 @@ public class LottoService {
         return money;
     }
 
+    /**
+     * 당첨 번호 추출
+     */
+    // 주요 6개 번호 추출
     public void inputPrimaryNumber() {
         String[] input = Console.readLine().split(",");
-        primaryNumber = new ArrayList<>();
+        List<Integer> primaryNumber = new ArrayList<>();
         try{
             for(String s : input){
                 int num = Integer.parseInt(s);
-
                 if(num < 1 || num > 45) throw new NotRangeException();
                 primaryNumber.add(num);
             }
@@ -78,23 +90,7 @@ public class LottoService {
             System.out.println("[ERROR] 범위 내의 숫자를 입력해주세요.");
             inputPrimaryNumber();
         }
-    }
-
-    public void inputBonusNumber() {
-        String input = Console.readLine();
-        try{
-            int num = Integer.parseInt(input);
-
-            if(num < 1 || num > 45) throw new NotRangeException();
-            primaryNumber.add(num);
-        }catch (NumberFormatException e){
-            System.out.println("[ERROR] 숫자를 입력해주세요.");
-            inputBonusNumber();
-        }catch (NotRangeException e){
-            System.out.println("[ERROR] 범위 내의 숫자를 입력해주세요.");
-            inputBonusNumber();
-        }
-        prizeLotto = new PrizeLotto(primaryNumber);
+        prizeLotto.setPrimaryNumbers(new Lotto(primaryNumber));
     }
 
 }

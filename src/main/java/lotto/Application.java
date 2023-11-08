f913497c52959d8467faf8b109160a030b4a8abc
Yang woosung <ngxk1217@gmail.com>
2023-11-08 20:09:14 +0900

    main메서드에 로또기능이 실행되도록 추가하였습니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.LottoPlay;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoPlay lottoPlay = new LottoPlay();
+        lottoPlay.play();
     }
 }

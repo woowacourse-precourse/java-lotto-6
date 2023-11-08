c822e8d220c2b46f38188898a599453fd57f9e81
mutjin <mutjin23@naver.com>
2023-11-08 23:16:59 +0900

    first commit

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,20 @@
+// Application.java
 package lotto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Lotto lotto = new Lotto();
+
+        lotto.Play();
+
     }
 }

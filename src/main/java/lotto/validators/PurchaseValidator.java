20095b2ce799c282ea74b2b47f116034338b9651
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:28:46 +0900

    feat: 구입 금액 음수 검증 기능 추가 입력한 로또 구입 금액이 음수인지 확인하는 메서드 작성

--- src/main/java/lotto/validators/PurchaseValidator.java
+++ src/main/java/lotto/validators/PurchaseValidator.java
@@ -6,4 +6,8 @@ public class PurchaseValidator {
     private static boolean isMultipleOfLottoPrice(Integer purchasePrice) {
         return purchasePrice % LOTTO_PRICE == 0;
     }
+
+    private static boolean isGreaterThanZero(Integer purchasePrice) {
+        return purchasePrice > 0;
+    }
 }

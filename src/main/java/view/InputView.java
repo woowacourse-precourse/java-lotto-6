90c7f933e11581ef83a334be2594167954b2f37c
gyuoo <yeomky98@naver.com>
2023-11-08 23:33:59 +0900

    feat: implement user input for bonus number

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String PAYMENT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String setUserPayment() {
         System.out.println(PAYMENT_MESSAGE);
@@ -28,5 +29,10 @@ public class InputView {
         String userInput = Console.readLine();
         removeBlank(userInput);
         return userInput;
+    }
+
+    public static String setBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
     }
 }

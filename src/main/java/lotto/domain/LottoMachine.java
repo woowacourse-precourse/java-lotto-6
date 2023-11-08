7468ab1b7155ca39c33ca425635658f463103567
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:47:52 +0900

    feat : generateTicket 함수 생성

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,7 +1,16 @@
 package lotto.domain;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class LottoMachine {
     private static final int MIN_LOTTO_NUMBER = 1;
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int LOTTO_NUMBER_COUNT = 6;
+
+    public LottoTicket generateTicket() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT);
+        Lotto lotto = new Lotto(numbers);
+        return new LottoTicket(lotto);
+    }
 }

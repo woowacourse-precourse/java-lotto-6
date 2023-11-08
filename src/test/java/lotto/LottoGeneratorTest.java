54abd8818ad518d3def2fee6179a4aeef635ae3a
gusgh075 <gusgh075@naver.com>
2023-11-08 11:24:41 +0900

    TEST : 1~45 사이의 6개 숫자로 로또를 생성하는지 확인하는 코드

---
+++ src/test/java/lotto/LottoGeneratorTest.java
@@ -0,0 +1,15 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LottoGeneratorTest {
+    @DisplayName("1~45 사이의 6개 숫자로 로또를 생성한다.")
+    @Test
+    void createLottoInRange(){
+        assertTrue(LottoGenerator.generateRandomLotto().getNumbers().
+                stream().allMatch(n->n>=1&&n<=45));
+    }
+}

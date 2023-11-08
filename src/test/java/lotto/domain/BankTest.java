b7508ce11fa71ce878d2e47b50ba60e5cef658be
hong <kevin0928@naver.com>
2023-11-08 17:14:02 +0900

    test : "당첨 번호와 로또 번호 일치하는 개수 테스트" 구현

--- src/test/java/lotto/domain/BankTest.java
+++ src/test/java/lotto/domain/BankTest.java
@@ -2,6 +2,19 @@ package lotto.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BankTest {
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+class BankTest {
+    @DisplayName("당첨 번호와 로또 번호 일치하는 개수 테스트")
+    @Test
+    public void testCountCorrect() {
+        Bank bank = new Bank();
+        bank.setWinningNumbers(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        int correctCount = bank.countCorrect(lotto);
+        assertEquals(6, correctCount);
+    }
 }

e6f9cf2a6390042aa95ebb0af46adf58b6faadea
Siwon Park <won1105@naver.com>
2023-11-08 19:17:23 +0900

    구입금액 예외처리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
+
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        /*try {
+            Lotto lotto = new Lotto(numbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }*/
+        Lotto lotto = new Lotto(numbers);
+        lotto.inputMoney();
+
+
+
+
         // TODO: 프로그램 구현
     }
 }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +18,27 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    public void inputMoney() {
+
+        boolean validInput = false;
+        System.out.println("구입금액을 입력해 주세요.");
+        while (!validInput) {
+            try {
+                String input = Console.readLine();
+                validateMoney(input);
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("[ERROR] 구입 금액은 1000원 단위로 입력해주세요.");
+                System.out.println("구입금액을 입력해 주세요.");
+            }
+        }
+    }
+
+    public static void validateMoney(String input) {
+        int money = Integer.parseInt(input);
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

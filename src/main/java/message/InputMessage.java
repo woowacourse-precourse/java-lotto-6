c9a5acce40fee5ed433cc7a37ab80c9455cd4182
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-11-08 23:23:06 +0900

    feat (WinningLotto) : 당첨 로또와 보너스 숫자 가진 클래스 - 클래스 내부에서 숫자 검증 - 당첨 로또는 Lotto클래스로 검증

---
+++ src/main/java/message/InputMessage.java
@@ -0,0 +1,15 @@
+package message;
+
+public enum InputMessage {
+    PAYMENT("구입금액을 입력해 주세요."),
+    WINNINGNUMBERS("당첨 번호를 입력해 주세요."),
+    BONUSNUMBER("보너스 번호를 입력해 주세요.");
+
+    private final String message;
+    InputMessage(String message){
+        this.message = message;
+    }
+    public String toString(){
+        return message;
+    }
+}

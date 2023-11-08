4f7afe30655064f373dfebd6150a585b92305cf2
peeaaccee <wlgp8958@naver.com>
2023-11-08 23:18:28 +0900

    feat(OutputView): "n개를 구매했습니다." 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,8 +2,12 @@ package lotto.view;
 
 public class OutputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String CHECK_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
     public static void printInputAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
     }
 
+    public static void printCheckAmount(int amount) {
+        System.out.printf(CHECK_AMOUNT_MESSAGE + "%n", amount);
+    }
 }

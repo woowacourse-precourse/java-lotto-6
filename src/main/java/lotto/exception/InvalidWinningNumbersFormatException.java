36f8ee0708b7864183755bc939b9f1b6b1536942
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 22:45:08 +0900

    remove(exception) : delete exception class

--- src/main/java/lotto/exception/InvalidWinningNumbersFormatException.java
+++
@@ -1,7 +0,0 @@
-package lotto.exception;
-
-public class InvalidWinningNumbersFormatException extends IllegalArgumentException {
-    public InvalidWinningNumbersFormatException() {
-        super("[ERROR] 당첨 번호는 숫자와 ,(쉼표)로만 입력할 수 있습니다.\n");
-    }
-}

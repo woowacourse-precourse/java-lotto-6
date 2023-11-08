f10ede5d61b935f6220e1e7c02b7889939fa7096
kkkkimtaehyeon <kth1927@naver.com>
2023-11-08 12:28:17 +0900

    feat : 로또 숫자 입력 범위 예외처리 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,13 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("로또 번호에 범위를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRange() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 50, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-    // 아래에 추가 테스트 작성 가능
+
 }

a3f02a017ba9a437b980228c2cb581e58d9930ea
박은지 <qkrdmswl1018@naver.com>
2023-11-08 18:42:59 +0900

    refactor: Lotto 검증 로직 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -13,13 +13,14 @@ import static lotto.util.Constant.MIN_LOTTO_NUMBER;
 public class Lotto {
 
     public static final String LOTTO_DUPLICATED_MESSAGE = ERROR + "중복된 숫자를 입력할 수 없습니다.";
-    private static final String LOTTO_RANGE_MESSAGE = ERROR + "로또 범위는 1 ~ 45 사이의 6개여야 가능합니다.";
+    private static final String LOTTO_RANGE_MESSAGE = ERROR + "로또 범위는 1 ~ 45 사이어야 합니다.";
+    private static final String LOTTO_LENGTH_MESSAGE = ERROR + "로또 개수는 6개여야 가능합니다.";
 
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = checkDuplicated(numbers);
+        this.numbers = numbers;
     }
 
     public List<Integer> getNumbers() {
@@ -27,16 +28,27 @@ public class Lotto {
     }
 
     private void validate(final List<Integer> numbers) {
-        if (numbers.size() != LOTTO_LENGTH || !numbers.stream().allMatch(num -> num >= MIN_LOTTO_NUMBER && num <= MAX_LOTTO_NUMBER)) {
-            throw new IllegalArgumentException(LOTTO_RANGE_MESSAGE);
+        checkLottoSize(numbers);
+        checkDuplicated(numbers);
+        checkLottoRange(numbers);
+    }
+
+    private void checkDuplicated(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>(numbers);
+        if (numbers.size() != set.size()) {
+            throw new IllegalArgumentException(LOTTO_DUPLICATED_MESSAGE);
         }
     }
 
-    private List<Integer> checkDuplicated(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>(numbers);
-        if (numbers.size() == set.size()) {
-            return numbers;
+    private void checkLottoSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(LOTTO_LENGTH_MESSAGE);
         }
-        throw new IllegalArgumentException(LOTTO_DUPLICATED_MESSAGE);
+    }
+
+    private void checkLottoRange(List<Integer> numbers) {
+        if (!numbers.stream().allMatch(num -> num >= MIN_LOTTO_NUMBER && num <= MAX_LOTTO_NUMBER)) {
+            throw new IllegalArgumentException(LOTTO_RANGE_MESSAGE);
+        }
     }
 }

7a1f6ceb76f5e99e69549b133204e479213dd1a0
jeondui <jeondui@gmail.com>
2023-11-08 22:24:24 +0900

    feat(Lotto): 로또 티켓끼리의 비교 하여 일치하는 숫자 반환 기능

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,9 +3,12 @@ package lotto.model;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import lotto.exception.Constant;
-import lotto.exception.ErrorMessage;
+import lotto.common.Constant;
+import lotto.common.ErrorMessage;
 
+/**
+ * 당첨 로또
+ */
 public class Lotto {
 
     private final List<Number> numbers; // private 변경 및 필드 추가 불가능
@@ -21,6 +24,55 @@ public class Lotto {
         return new Lotto(numbers);
     }
 
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+
+    private static void validateInputString(String inputNumber) {
+        isEmpty(inputNumber);
+        isContainsBlank(inputNumber);
+        isEndsWithComma(inputNumber);
+    }
+
+    private static void isEmpty(String inputNumber) {
+        if (inputNumber.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_NULL.get());
+        }
+    }
+
+    private static void isContainsBlank(String inputNumber) {
+        if (inputNumber.contains(Constant.BLANK.getCharValue())) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_CONTAINS_BLANK.get());
+        }
+    }
+
+    private static void isEndsWithComma(String inputNumber) {
+        if (inputNumber.endsWith(Constant.COMMA.getCharValue())) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_ENDS_WITH_COMMA.get());
+        }
+    }
+
+    private void validateNumbersLength(List<Number> numbers) {
+        if (numbers.size() != Constant.LOTTO_NUMBERS_MAX_LENGTH.getIntValue()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_LENGTH.get());
+        }
+    }
+
+    private void validateNumbersDuplicate(List<Number> numbers) {
+        if (numbers.stream()
+                .map(Number::getNumber)
+                .distinct()
+                .count() != numbers.size()) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBERS_DUPLICATE.get());
+        }
+    }
+
+    public int countMatchingNumbers(LottoTicket lottoTicket) {
+        return (int) numbers.stream()
+                .filter(lottoTicket.getTicket()::contains)
+                .count();
+    }
+
     private static List<Number> convertStringToNumbers(String inputNumber) {
         List<Integer> inputNumbers = convertStringToList(inputNumber);
         return inputNumbers.stream()
@@ -34,45 +86,6 @@ public class Lotto {
         return Arrays.stream(inputNumber.split(Constant.COMMA.getCharValue()))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-    }
-
-    private static void validateInputString(String inputNumber) {
-        isEmpty(inputNumber);
-        isContainsBlank(inputNumber);
-        isEndsWithComma(inputNumber);
-    }
-
-    private static void isEmpty(String inputNumber) {
-        if (inputNumber.isEmpty()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_NULL.get());
-        }
-    }
-
-    private static void isContainsBlank(String inputNumber) {
-        if (inputNumber.contains(Constant.BLANK.getCharValue())) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_CONTAINS_BLANK.get());
-        }
-    }
-
-    private static void isEndsWithComma(String inputNumber) {
-        if (inputNumber.endsWith(Constant.COMMA.getCharValue())) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_ENDS_WITH_COMMA.get());
-        }
-    }
-
-    private void validateNumbersLength(List<Number> numbers) {
-        if (numbers.size() != Constant.LOTTO_NUMBERS_MAX_LENGTH.getIntValue()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_LENGTH.get());
-        }
-    }
-
-    private void validateNumbersDuplicate(List<Number> numbers) {
-        if (numbers.stream()
-                .map(Number::getNumber)
-                .distinct()
-                .count() != numbers.size()) {
-            throw new IllegalArgumentException(ErrorMessage.ERROR_LOTTO_NUMBERS_DUPLICATE.get());
-        }
     }
 
 }

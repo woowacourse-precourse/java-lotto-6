b63eac0d51c4ebfdb8e66cd28515f9bf366c6bee
현 <sohyun95277@gmail.com>
2023-11-08 19:42:31 +0900

    refactor(WinningNumberTest): 결과 반환 기능 PrizeResult로 이동

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,11 +1,9 @@
 package lotto.domain;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,29 +17,5 @@ class WinningNumbersTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new WinningNumbers(winningNumbers, input);
         });
-    }
-
-    @DisplayName("5개를 맞췄을 경우 - 보너스 볼 매치 O")
-    @Test
-    void checkSecondPrize() {
-        Lotto winningNumbers = new Lotto(List.of(4, 12, 13, 41, 1, 17));
-        int bonusNumber = 3;
-        WinningNumbers winningLotto = new WinningNumbers(winningNumbers, bonusNumber);
-        Lotto purchaseLotto = new Lotto(List.of(4, 12, 13, 41, 1, 3));
-
-        assertThat(winningLotto.matchCount(purchaseLotto)).isEqualTo(5);
-        assertThat(winningLotto.checkPrize(purchaseLotto)).isEqualTo(Prize.SECOND);
-    }
-
-    @DisplayName("5개를 맞췄을 경우 - 보너스 볼 매치 X")
-    @Test
-    void checkThirdPrize() {
-        Lotto winningNumbers = new Lotto(List.of(4, 12, 13, 41, 1, 17));
-        int bonusNumber = 3;
-        WinningNumbers winningLotto = new WinningNumbers(winningNumbers, bonusNumber);
-        Lotto purchaseLotto = new Lotto(List.of(4, 12, 13, 41, 1, 9));
-
-        assertThat(winningLotto.matchCount(purchaseLotto)).isEqualTo(5);
-        assertThat(winningLotto.checkPrize(purchaseLotto)).isEqualTo(Prize.THIRD);
     }
 }

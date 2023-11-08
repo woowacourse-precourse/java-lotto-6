ae15e51b3dc0925741686ee1070e4c4c4b4c6d5b
J-petto <hminji4565@gmail.com>
2023-11-08 22:13:20 +0900

    feat: Money input & lottoNumbers input

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,9 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
     }
 }

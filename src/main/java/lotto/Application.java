c2b991d2f329bd12a5e537dceda4dc939cdc0365
7ngenious <zxvs1218@gmail.com>
2023-11-08 20:36:14 +0900

    feat: Apply Splited View

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,13 +1,15 @@
 package lotto;
 
 import lotto.Controller.LottoController;
-import lotto.View.LottoView;
+import lotto.View.LottoInputView;
+import lotto.View.LottoOutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoView view = new LottoView();
-        LottoController controller = new LottoController(view);
+        LottoInputView inputView = new LottoInputView();
+        LottoOutputView outputView = new LottoOutputView();
+        LottoController controller = new LottoController(inputView, outputView);
 
         controller.runGame();
     }

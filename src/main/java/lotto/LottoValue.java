04f0eea8a78bf951d68ba3dba96e4c17f1e2431e
Roista57 <Roista57@gmail.com>
2023-11-08 13:53:45 +0900

    refactor: 당첨 금액을 계산하기 위한 값을 상수에서 enum으로 변경

--- src/main/java/lotto/LottoValue.java
+++ src/main/java/lotto/LottoValue.java
@@ -13,4 +13,20 @@ public class LottoValue {
             return Integer.parseInt(value);
         }
     }
+    public enum PRICE{
+        PRICE_0("1000"),
+        PRICE_1("5000"),
+        PRICE_2("50000"),
+        PRICE_3("1500000"),
+        PRICE_4("3000000"),
+        PRICE_5("2000000000");
+
+        private final String price;
+        PRICE(String price){
+            this.price = price;
+        }
+        public int getPrice(){
+            return Integer.parseInt(price);
+        }
+    }
 }

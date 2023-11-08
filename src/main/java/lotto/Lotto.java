0db7e5086cda25bd93b704fc7cfbfaa37df98a34
Leegeonwoo7 <workgeon07@naver.com>
2023-11-08 15:34:15 +0900

    예외처리 추가구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,9 +1,6 @@
 package lotto;
 
 import java.util.List;
-import java.util.StringTokenizer;
-
-import camp.nextstep.edu.missionutils.Console;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,13 +10,28 @@ public class Lotto {
         this.numbers = numbers;
     }
     
-    //입력값이 6자리의 숫자인지 검증
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+        	System.out.println("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
             throw new IllegalArgumentException();
         }
+        if(duplicateNumbers(numbers)) {
+        	System.out.println("[ERROR] 로또 숫자는 중복되서는 안됩니다.");
+        	throw new IllegalArgumentException();
+        }
     }
-
+    
+    private boolean duplicateNumbers(List<Integer> numbers) {
+    	for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++) {
+                if (numbers.get(i).equals(numbers.get(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
     public List<Integer> getNumbers(){
     	return numbers;
     }

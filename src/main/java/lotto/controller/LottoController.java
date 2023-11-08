c8943b5ace270e0c0bfc106b9807c9f8cf32116a
다운 <j_d_woon@naver.com>
2023-11-08 12:18:21 +0900

    feat: 당첨 통계 계산 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 import lotto.Lotto;
+import lotto.Lotto.Rank;
 import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -12,17 +15,55 @@ public class LottoController {
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
     private final LottoService lottoService = new LottoService();
+    private final int LOTTOPRICE = 1000;
     public void run() {
+
         List<Lotto> lottos = new ArrayList<>();
 
-        String purchasePrice = inputView.buyLotto();
-        int lottoAmount = lottoService.checkLottoNumber(purchasePrice);
+        int lottoAmount = inputPurchasePrice();
 
         IntStream.range(0,lottoAmount)
                 .forEach(i -> lottos.add(lottoService.generateLotto()));
 
         outputView.printBuyLotto(lottoAmount);
         outputView.printTotalLotto(lottos);
+
+        String winningNumber = inputView.inputWinningNumber();
+        String bonusNumber = inputView.inputBonusNumber();
+
+        Map<Rank, Integer> lottoResult = lottoService.checkwin(winningNumber,bonusNumber,lottos);
+        outputView.printWinningStatistics();
+        result(lottoResult);
+        outputView.printEarningRate(checkEarningRate(lottoResult, lottoAmount * LOTTOPRICE));
     }
 
+    public int inputPurchasePrice() {
+        while (true) {
+            try {
+                String purchasePrice = inputView.buyLotto();
+                return lottoService.checkLottoNumber(purchasePrice);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                inputPurchasePrice();
+            }
+        }
+    }
+
+    public void result(Map<Rank, Integer> rankCounts) {
+        EnumSet.allOf(Rank.class).stream()
+                .filter(rank -> rank != Rank.NONE)
+                .forEach(rank -> {
+                    int count = rankCounts.getOrDefault(rank, 0);
+                    if (rank == Rank.SECOND) {
+                        outputView.printSecondResult(rank.getMatchCount(), rank.getWinningMoney(), count);
+                        return;
+                    }
+                    outputView.printResult(rank.getMatchCount(), rank.getWinningMoney(), count);
+                });
+    }
+
+    public double checkEarningRate(Map<Rank, Integer> rankCounts, int purchasePrice) {
+       int totalEarning = lottoService.calculateTotalEarning(rankCounts);
+       return lottoService.calculateEarningRate(totalEarning, purchasePrice);
+    }
 }
--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,8 +1,15 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lotto.Lotto;
+import lotto.Lotto.Rank;
 
 public class LottoService {
     private final int LOTTO_PRICE = 1000;
@@ -11,16 +18,64 @@ public class LottoService {
     }
 
     public Lotto generateLotto() {
-        List<Integer> lottoNumber = generateRandomNumber();
+        List<Integer> lottoNumber = new ArrayList<>(generateRandomNumber());
+        Collections.sort(lottoNumber);
         Lotto lotto = new Lotto(lottoNumber);
         return lotto;
     }
+
+
+    public Map<Rank, Integer> checkwin(String winningNumber, String bonusNumber, List<Lotto> lottos) {
+        List<Integer> winningLottoNumbers = convertStringToList(winningNumber);
+        int bonusLottoNumber = convertToInt(bonusNumber);
+
+        return checkRankCounts(lottos, winningLottoNumbers,bonusLottoNumber);
+    }
+
+    private Map<Rank, Integer> checkRankCounts(List<Lotto> lottos, List<Integer> winningLottoNumbers, int bonusLottoNumber) {
+        Map<Rank, Integer> rankCounts = new HashMap<>();
+        for (Lotto lotto : lottos) {
+            Rank rank = lotto.checkRank(winningLottoNumbers, bonusLottoNumber);
+            rankCounts.put(rank, rankCounts.getOrDefault(rank, 0) + 1);
+        }
+        return rankCounts;
+    }
+
+    private List<Integer> convertStringToList(String numbers) {
+        return Arrays.stream(numbers.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
     private int convertToInt(String purchaseAmount) {
+        validatePurchasePrice(purchaseAmount);
         return Integer.parseInt(purchaseAmount);
+    }
+
+    private void validatePurchasePrice(String purchasePrice) {
+        if(!purchasePrice.matches("^[1-9][0-9]*$")) {
+            throw new IllegalArgumentException("[ERROR] 올바른 값을 입력해주세요.");
+        }
     }
 
     private List<Integer> generateRandomNumber() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;
     }
+
+    public double calculateEarningRate(int totalEarnings, int purchasePrice) {
+        return (double) totalEarnings / purchasePrice * 100;
+    }
+
+    public int calculateTotalEarning(Map<Rank, Integer> rankCounts) {
+        int totalEarnings = 0;
+
+        for (Map.Entry<Rank, Integer> entry : rankCounts.entrySet()) {
+            totalEarnings += (int) (entry.getKey()
+                                .getWinningMoney() * entry.getValue());
+        }
+        return totalEarnings;
+    }
+
 }

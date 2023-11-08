795095607f3cb2af13915d4e735dcef468f083ba
dudxo <father8475@naver.com>
2023-11-08 18:36:00 +0900

    [Fix] 필드 WinningNum 타입 변경 및 기능 추가

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,11 +1,41 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoMachine {
-    private List<String> WinningNum;
+    private List<Integer> WinningNum;
     private int maxLotto;
     private List<Lotto> lottos;
 
 
+    public void run() {
+
+    }
+
+    public void decideMaxLotto(User user) {
+        int userAmount = user.getAmount();
+        this.maxLotto = userAmount / 1000;
+        LottoInfo();
+    }
+
+    public void LottoInfo() {
+        System.out.printf("%d개를 구매했습니다.\n", this.maxLotto);
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
+
+    public void lottery() {
+        lottos = new ArrayList<>(maxLotto);
+        for (int i = 0; i < lottos.size(); i++) {
+            lottos.add(new Lotto(generateLottoNum()));
+        }
+    }
+
+    public List<Integer> generateLottoNum() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return numbers;
+    }
 }

08fbd3e9f8089d76b0a477b2606d106ef9c6c857
alenjb <alenjb@naver.com>
2023-11-08 15:52:42 +0900

    💡style(Rank): Rank의 2등 부분의 이해가 쉽도록 주석을 추가
    Rank의 "SECOND_PLACE(30000000, 5.5)"에서 매치되는 숫자를 나타내는 부분에 "5.5"를 입력한 것에 대해 설명하기 위해 주석을 추가함.

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -2,7 +2,7 @@ package lotto;
 
 public enum Rank {
     FIRST_PLACE(2000000000, 6),
-    SECOND_PLACE(30000000, 5.5),
+    SECOND_PLACE(30000000, 5.5), // 2등은 매치되는 개수가 6개이지만 1등과 구분하기 위해 5.5개로 설정함
     THIRD_PLACE(1500000, 5),
     FOURTH_PLACE(50000, 4),
     FIFTH_PLACE(5000, 3),

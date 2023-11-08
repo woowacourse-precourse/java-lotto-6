8a16d0ee9f073587d1f7d2dfd17a0e683f232bfc
IToriginal <gitshineit@gmail.com>
2023-11-08 23:38:59 +0900

    :recycle: [refactor] #2 통계 출력 메서드 분리

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -24,27 +24,51 @@ public class OutputView {
     }
 
     public static void displayStatistic(int[] rank, Integer purchaseAmount) {
-        System.out.println(NEXT_LINE.getContent() + RESULT_TITLE.getContent());
-        int totalPrizeAmount = 0;
-        for (int i = rank.length - 1; i >= 0; i--) {
-            String prize = RankingRule.values()[i].getPrize();
-            String rankContent = RankingRule.values()[i].getContent();
-            int count = rank[i];
-            System.out.println(
-                    String.format(RESULT_FORMAT.getContent(), rankContent, prize, count));
-            if (count > 0) {
-                totalPrizeAmount += Integer.parseInt(
-                        prize.replace(",", "")) * count;
-            }
-        }
-        double profitRate = (double) totalPrizeAmount / purchaseAmount * 100;
-        System.out.println(
-                TOTAL_YIELD.getContent() + String.format(POINT_FORMAT.getContent(), profitRate)
-                        + END_CONTENT.getContent());
+        displayStatisticTitle();
+        int totalPrizeAmount = calculateTotalPrizeAmount(rank);
+        double profitRate = calculateRate(totalPrizeAmount, purchaseAmount);
+        displayTotalYield(profitRate);
     }
 
     public static void displayErrorMessage(IllegalArgumentException e) {
         System.out.println(ERROR_WORD.getContent() + e.getMessage());
     }
+
+    private static void displayStatisticTitle() {
+        System.out.println(NEXT_LINE.getContent() + RESULT_TITLE.getContent());
+    }
+
+    private static int calculateTotalPrizeAmount(int[] rank) {
+        int totalPrizeAmount = 0;
+        for (int i = rank.length - 1; i >= 0; i--) {
+            String prize = RankingRule.values()[i].getPrize();
+            String rankContent = RankingRule.values()[i].getContent();
+            int count = rank[i];
+
+            String formattedResult = String.format(RESULT_FORMAT.getContent(), rankContent, prize,
+                    count);
+            System.out.println(formattedResult);
+
+            if (count > 0) {
+                totalPrizeAmount += parsePrizeAmount(prize) * count;
+            }
+        }
+        return totalPrizeAmount;
+    }
+
+    private static int parsePrizeAmount(String prize) {
+        return Integer.parseInt(prize.replace(",", ""));
+    }
+
+    private static double calculateRate(int totalPrizeAmount, Integer purchaseAmount) {
+        return (double) totalPrizeAmount / purchaseAmount * 100;
+    }
+
+    private static void displayTotalYield(double profitRate) {
+        System.out.println(
+                TOTAL_YIELD.getContent() + String.format(POINT_FORMAT.getContent(), profitRate)
+                        + END_CONTENT.getContent());
+    }
+
 
 }

3260fa7e9de440a9f88063fab19681a27a10389f
hajungIm <ihj_ewha@ewhain.net>
2023-11-08 11:20:51 +0900

    Test(LottoRepositoryTest): 리포지토리 저장 테스트 완료
    Lotto 객체 `save` 테스트 완료

--- src/test/java/lotto/repository/LottoRepositoryTest.java
+++ src/test/java/lotto/repository/LottoRepositoryTest.java
@@ -1,8 +1,12 @@
 package lotto.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +15,7 @@ class LottoRepositoryTest {
 
     @BeforeEach
     void init() {
-        LottoRepository.resetInstance();
-        LottoRepository.resetField();
+        LottoRepository.resetInstanceAndFields();
     }
 
     @Test
@@ -29,4 +32,18 @@ class LottoRepositoryTest {
         assertSame(lottoRepository, lottoRepository2);
     }
 
+    @Test
+    void lottoRepository_save_테스트() {
+        lottoRepository = LottoRepository.getInstance();
+        lottoRepository.save(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)));
+
+        List<Lotto> lottos = LottoRepository.getLottos();
+        Lotto actual = lottos.get(0);
+
+        Lotto expected = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        expected.id = 0L;
+
+        assertThat(lottos.size()).isEqualTo(1);
+        assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    }
 }

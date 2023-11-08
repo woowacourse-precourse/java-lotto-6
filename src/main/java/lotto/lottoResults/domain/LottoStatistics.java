e43cffd67c59e339a810097e085399a4b655aa38
fing9 <djajsl54@naver.com>
2023-11-08 20:57:16 +0900

    bugfix : 1의 자리와, 소숫점 첫 째 자리에서 0을 표시 못하는 버그 수정
    예시) 0.1%가 .1로 출력
          1.0%가 1로 출력

--- src/main/java/lotto/lottoResults/domain/LottoStatistics.java
+++ src/main/java/lotto/lottoResults/domain/LottoStatistics.java
@@ -16,26 +16,26 @@ public class LottoStatistics {
     public LottoStatistics() {
         totalReward = 0L;
         counts = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 7; i++) {
             counts.add(0);
         }
     }
 
     public void incrementCount(Integer matchedCount, Integer bonusMatched) {
         Integer rank = convertRank(matchedCount, bonusMatched);
-        counts.set(rank - 1, counts.get(rank - 1) + 1);
+        counts.set(rank, counts.get(rank) + 1);
         totalReward += getReward(rank);
     }
 
     public void printStatistics() {
         System.out.println("당첨 통계\n" + "---");
-        for (int i = 4; i >= 0; i--) {
+        for (int i = 5; i >= 1; i--) {
             System.out.println(getStatistics(i + 1) + " - " + counts.get(i) + "개");
         }
     }
 
     public void printReturnRate(Integer spendMoney) {
-        DecimalFormat decFormat = new DecimalFormat("###,###.#");
+        DecimalFormat decFormat = new DecimalFormat("###,##0.0");
 
         String str = decFormat.format(calculateReturnRate(spendMoney));
         System.out.println("총 수익률은 " + str + "%입니다.");

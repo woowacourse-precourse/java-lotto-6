15af879eaeb863c4e51c2f310f6b967970bf191f
Kim mingyu <minq0302@naver.com>
2023-11-08 23:33:24 +0900

    test: add lotto numbers test

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 정렬이 되는지 확인한다.")
+    @Test
+    void sortedLottoNumber() {
+        // given
+        List<Integer> numbers = List.of(6, 4, 5, 2, 1, 3);
+        List<Integer> answerNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = new Lotto(numbers);
+
+        // then
+        assertThat(lotto.toString()).isEqualTo(answerNumbers.toString());
+    }
+}

770e349284b10085ad1ba5e1c16c497f18d41b8e
wwan13 <wwan13@naver.com>
2023-11-08 23:33:13 +0900

    refactor : 와일드카드 import 제거

--- src/main/java/lotto/domain/lottogenerator/RandomLottoGenerator.java
+++ src/main/java/lotto/domain/lottogenerator/RandomLottoGenerator.java
@@ -1,6 +1,8 @@
 package lotto.domain.lottogenerator;
 
-import static lotto.domain.constant.LottoConstant.*;
+import static lotto.domain.constant.LottoConstant.LOTTO_MAXIMUM_NUMBER;
+import static lotto.domain.constant.LottoConstant.LOTTO_MINIMUM_NUMBER;
+import static lotto.domain.constant.LottoConstant.LOTTO_NUMBERS_SIZE;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;

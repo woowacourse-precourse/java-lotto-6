c1dbd41e66bd9d2da1209b76724b7c175c015334
정인모 <cat10830@naver.com>
2023-11-08 21:11:45 +0900

    feat : enum 상수 객체 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,27 @@ package lotto;
 
 import java.util.List;
 
+enum Reward {
+    FIRST(2000000000),
+    SECOND(30000000),
+    THIRD(1500000),
+    FOURTH(50000),
+    FIFTH(5000);
+
+    private Integer amount;
+
+    private Reward(Integer amount) {
+        this.amount = amount;
+    }
+
+    private Reward[] rewards = Reward.values();
+
+    public Integer getReward(Integer ranking) {
+        ranking = ranking - 1;
+        return rewards[ranking].amount;
+    }
+}
+
 public class Lotto {
     private final List<Integer> numbers;
 

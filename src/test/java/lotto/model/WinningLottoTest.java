4ce2c143ccddd8d0fbc0e27bf8e482d9ba0de45f
hoyeon <highoption@naver.com>
2023-11-08 13:55:56 +0900

    test(WinningLottoTest) : 정상적으로 WinningLotto가 생성되는지 테스트

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -1,6 +1,6 @@
 package lotto.model;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,35 +10,17 @@ import org.junit.jupiter.api.Test;
 public class WinningLottoTest {
 
     @Test
-    @DisplayName("보너스 넘버가 숫자가 아니라면 예외 발생")
-    void createWinningLottoByIntegerBonusNumber() {
+    @DisplayName("정상적으로 WinningLotto가 생가는지 테스트")
+    void testWinningLottoByValidList() {
         Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
-        String bonus = "hoyeon";
+        BonusNumber bonusNumber = new BonusNumber("7");
 
-        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 숫자여야 합니다.");
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+
+        assertThat(winningLotto.getWinningLotto())
+                .containsExactlyElementsOf(lotto.getNumbers());
+        assertThat(winningLotto.getBonusNumber())
+                .isEqualTo(bonusNumber.getBonusNumber());
     }
 
-    @Test
-    @DisplayName("보너스 넘버의 범위가 1부터 45사이인지 테스트")
-    void createWinningLottoByBonusNumberRange() {
-        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
-        String bonus = "46";
-
-        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-    }
-
-    @Test
-    @DisplayName("보너스 넘버가 당첨번호와 중복 된다면 예외 발생")
-    void createWinningLottoByDuplicateBonusNumber() {
-        Lotto lotto = new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6)));
-        String bonus = "3";
-
-        assertThatThrownBy(() -> new WinningLotto(lotto, bonus))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 보너스 번호는 당첨 번호와 중복되면 안됩니다.");
-    }
 }

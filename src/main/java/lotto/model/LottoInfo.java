317db4c8771e35208c4dac931c59bd7897f19a2a
Henry-Cha <sungwon4728@naver.com>
2023-11-08 09:49:32 +0900

    feat: 로또 상금 정보 enum 추가

--- src/main/java/lotto/model/LottoInfo.java
+++ src/main/java/lotto/model/LottoInfo.java
@@ -6,4 +6,20 @@ public class LottoInfo {
     public static final int LOTTO_PRICE = 1000;
     public static final int NUMBER_COUNT = 6;
     public static final int RANK_COUNT = 6;
+
+    public enum Prize{
+        PRIZE_0ST(0),
+        PRIZE_1ST(2_000_000_000),
+        PRIZE_2ST(30_000_000),
+        PRIZE_3ST(1_500_000),
+        PRIZE_4ST(50_000),
+        PRIZE_5ST(5_000),
+        ;
+
+        final int money;
+
+        Prize(int money) {
+            this.money=money;
+        }
+    }
 }

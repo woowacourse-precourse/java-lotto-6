a427c241c93d62afb8a1cbca8b389492466a5c4c
2021110806 <2021110806@dgu.ac.kr>
2023-11-08 20:30:02 +0900

    feat(Utils.java): add formatAndRoundNumber function double형 숫자를 인자로 받아 소수점 두 번째 자리에서 반올림하고 반점을 1000단위로 찍는 기능을 구현하였습니다.

--- src/main/java/lotto/Utils.java
+++ src/main/java/lotto/Utils.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Utils {
@@ -12,6 +15,11 @@ public class Utils {
                 .mapToInt(Integer::parseInt)
                 .boxed().collect(Collectors.toList());
         return parsedNumbers;
+    }
+
+    public static String formatAndRoundNumber(double number) {
+        DecimalFormat decimalFormat = new DecimalFormat("#,##0.0");
+        return decimalFormat.format(number);
     }
 
 }

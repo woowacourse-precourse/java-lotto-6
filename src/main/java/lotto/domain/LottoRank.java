8dc3cd860aeeb9b087cd13871b4838dc7a7bf795
soddong <aledjdygus9300@gmail.com>
2023-11-08 16:48:35 +0900

    feat : 당첨 내역을 출력한다.

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,17 +1,19 @@
 package lotto.domain;
 
 public enum LottoRank {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    NONE(0);
+    FIRST(2_000_000_000, "6개 일치 (2,000,000,000원) - "),
+    SECOND(30_000_000, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    THIRD(1_500_000, "5개 일치 (1,500,000원) - "),
+    FOURTH(50_000, "4개 일치 (50,000원) - "),
+    FIFTH(5_000, "3개 일치 (5,000원) - "),
+    NONE(0, "");
 
     private final int prizeAmount;
+    private final String message;
 
-    LottoRank(int prizeAmount) {
+    LottoRank(int prizeAmount, String message) {
         this.prizeAmount = prizeAmount;
+        this.message = message;
     }
 
     public static LottoRank valueOf(int hitCount, boolean bonusMatch) {
---
+++ src/main/java/lotto/domain/message/OutputMessage.java
@@ -0,0 +1,12 @@
+package lotto.domain.message;
+
+public class OutputMessage {
+
+    public void printMessage(String message) {
+        System.out.print(message);
+    }
+
+    public void newLine() {
+        System.out.print(System.lineSeparator());
+    }
+}

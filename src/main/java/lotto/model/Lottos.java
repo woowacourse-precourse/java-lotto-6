b1c98a71e89e0eb771b00f85e27eb6e94f9fd923
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 12:32:24 +0900

    feat(model) : lottos 모델 구현

---
+++ src/main/java/lotto/model/Lottos.java
@@ -0,0 +1,11 @@
+package lotto.model;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottoNumbers;
+
+    public Lottos(List<Lotto> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+}

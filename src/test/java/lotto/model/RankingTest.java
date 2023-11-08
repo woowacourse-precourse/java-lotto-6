5172a4ff9a342dace598f0d1a150086311122595
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2023-11-08 22:44:20 +0900

    test: add test code for price money calculation

--- src/test/java/lotto/model/RankingTest.java
+++ src/test/java/lotto/model/RankingTest.java
@@ -3,6 +3,7 @@ package lotto.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RankingTest {
@@ -11,5 +12,16 @@ class RankingTest {
     void createRankingWithWrongParameters() {
         assertThatThrownBy(() -> Ranking.getRanking(1, true))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("랭킹과 당첨 횟수에 따른 상금")
+    @Test
+    void checkPriceByRankingAndCount() {
+        // given
+        PriceMoney priceMoney = new PriceMoney(30_000);
+        Ranking fifth = Ranking.FIFTH;
+
+        // when, then
+        assertThat(fifth.calculateCurrentPrice(6)).isEqualTo(priceMoney);
     }
 }

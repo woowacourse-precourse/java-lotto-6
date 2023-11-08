fa93c37b0260e42438a7a0b6c9e1ca7f425930bb
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:13:32 +0900

    MoneyAmountException 1000으로 나눠 떨어지는 값

--- src/test/java/lotto/exception/MoneyAmountExceptionTest.java
+++ src/test/java/lotto/exception/MoneyAmountExceptionTest.java
@@ -20,10 +20,10 @@ public class MoneyAmountExceptionTest {
     @DisplayName("1000원 단위로 나누어 떨어지지 않는 금액은 NumberFormatException 예외가 발생한다.")
     @Test
     void testMoneyReader_InvalidAmount() {
-        // Arrange
+
         int invalidAmount = 2500; // 1000원 단위로 나누어 떨어지지 않는다
 
-        // Act & Assert
+
         assertThatThrownBy(() -> MoneyAmountException.money_reader(invalidAmount))
                 .isInstanceOf(NumberFormatException.class)
                 .hasMessage("[ERROR]알맞는 값이 아닙니다 다시 입력 하세요");

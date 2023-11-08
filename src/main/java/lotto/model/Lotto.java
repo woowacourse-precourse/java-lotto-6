4ce7a39522e51f116a83b80836e1ab7a33927cdf
Yerinnnn <shinyerin0216@gmail.com>
2023-11-08 19:04:17 +0900

    Feat: Lotto 클래스에 중복되는 번호가 있는지 확인하는 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,17 +1,29 @@
 package lotto.model;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static lotto.model.ErrorMessage.NUMBER_COUNT_ERROR;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        numberDuplication(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException(NUMBER_COUNT_ERROR.toString());
+        }
+    }
+
+    private void numberDuplication(List<Integer> winNumber) {
+        Set<Integer> setWinNumber = winNumber.stream().collect(Collectors.toSet());
+        if (winNumber.size() != setWinNumber.size()) {
             throw new IllegalArgumentException();
         }
     }
@@ -19,6 +31,4 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
-
-    // TODO: 추가 기능 구현
 }

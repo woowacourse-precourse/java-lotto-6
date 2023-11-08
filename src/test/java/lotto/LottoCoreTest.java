2b3df45767a6f7b6e40684a163974c69c8f7654f
dmswl6310 <dmswl6310@gmail.com>
2023-11-08 16:48:37 +0900

    test(LottoTest): test코드를 Core와 UI 클래스로 분리

--- src/test/java/lotto/LottoCoreTest.java
+++ src/test/java/lotto/LottoCoreTest.java
@@ -1,32 +1,14 @@
 package lotto;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.List;
 import java.util.Map;
-import lotto.domain.Lotto;
 import lotto.domain.LottoPurchaseInfo;
 import lotto.domain.Profit;
 import lotto.domain.WinningDetails;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
-    @Test
-    void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
-    @Test
-    void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
+class LottoCoreTest {
     @Test
     void 구입금액에_맞는_로또갯수가_생성() {
         LottoPurchaseInfo lottoPurchaseInfo = new LottoPurchaseInfo(5000);
@@ -40,7 +22,7 @@ class LottoTest {
     }
 
     @Test
-    void 해당하는_통계enum_갯수_증가() {
+    void 해당하는_통계_갯수_증가() {
         Profit profit = new Profit();
         profit.addResult(WinningDetails.FOUR_MATCH);
         Map<WinningDetails, Integer> winningMap = profit.getWinningResult();
---
+++ src/test/java/lotto/LottoUITest.java
@@ -0,0 +1,24 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoUITest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

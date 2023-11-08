cff51429aea247d3fab393b15170676a099c4617
Hanjaemo <hjm2530@gmail.com>
2023-11-08 15:06:21 +0900

    refactor(Rank): long 타입 값에 'L' 추가

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -4,12 +4,12 @@ import java.util.function.BiFunction;
 
 public enum Rank {
 
-    FIRST(6, false, 2_000_000_000, (match, hasBonusNumber) -> match == 6),
-    SECOND(5, true, 30_000_000, (match, hasBonusNumber) -> match == 5 && hasBonusNumber),
-    THIRD(5, false, 1_500_000, (match, hasBonusNumber) -> match == 5 && !hasBonusNumber),
-    FOURTH(4, false, 50_000,(match, hasBonusNumber) -> match == 4),
-    FIFTH(3, false, 5_000, (match, hasBonusNumber) -> match == 3),
-    MISS(0, false, 0, (match, hasBonusNumber) -> match <= 2);
+    FIRST(6, false, 2_000_000_000L, (match, hasBonusNumber) -> match == 6),
+    SECOND(5, true, 30_000_000L, (match, hasBonusNumber) -> match == 5 && hasBonusNumber),
+    THIRD(5, false, 1_500_000L, (match, hasBonusNumber) -> match == 5 && !hasBonusNumber),
+    FOURTH(4, false, 50_000L,(match, hasBonusNumber) -> match == 4),
+    FIFTH(3, false, 5_000L, (match, hasBonusNumber) -> match == 3),
+    MISS(0, false, 0L, (match, hasBonusNumber) -> match <= 2);
 
     private final int match;
     private final boolean hasBonusNumber;

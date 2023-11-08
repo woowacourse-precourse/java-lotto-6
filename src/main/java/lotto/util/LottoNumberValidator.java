066ae75cabb50b8091f56a440d65847a997411a5
hansg931 <hansg0509@cau.ac.kr>
2023-11-08 21:47:17 +0900

    feat(LottoNumberValidator): 입력받은 로또 숫자의 범위를 검사하는 메소드 작성

--- src/main/java/lotto/util/LottoNumberValidator.java
+++ src/main/java/lotto/util/LottoNumberValidator.java
@@ -3,11 +3,20 @@ package lotto.util;
 public class LottoNumberValidator {
 	public void validateLottoNumber(String input) {
 		validateNumberCount(input);
+		validateNumberRange(input);
 	}
 
 	private void validateNumberCount(String input) {
 		if (input.split(",").length != Constant.LOTTO_SIZE) {
 			throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUM_SIZE_MESSAGE);
+		}
+	}
+
+	private void validateNumberRange(String input) {
+		for (String number : input.split(",")) {
+			if (Integer.parseInt(number) < Constant.LOTTO_MIN_NUMBER || Integer.parseInt(number) > Constant.LOTTO_MAX_NUMBER) {
+				throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUM_OUT_OF_RANGE_MESSAGE);
+			}
 		}
 	}
 }

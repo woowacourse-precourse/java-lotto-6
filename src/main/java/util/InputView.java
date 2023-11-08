a5e98017c9ce53d5d3ecedfab829411adb23c5cb
kmh0601 <pongkmh@naver.com>
2023-11-08 17:56:26 +0900

    test: 사용자로부터 입력을 요구하는 문구를 출력하는 기능 추가
    로또와 보너스 숫자에 대한 입력을 요구하는 문구 출력하는 기능

---
+++ src/main/java/util/InputView.java
@@ -0,0 +1,13 @@
+package util;
+
+public class InputView {
+    public static final String INPUT_LOTTO = "당첨 번호를 입력해주세요.";
+    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요";
+
+    public void printUserInputLotto() {
+        System.out.println(INPUT_LOTTO);
+    }
+    public void printUserBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER);
+    }
+}

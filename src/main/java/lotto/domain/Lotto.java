d7dc8dfd118472311bfb5b7aa45793330d40f9ff
nahyeon99 <nahyeonee99@gmail.com>
2023-11-08 12:33:34 +0900

    refactor(domain/Lotto): Lotto 번호 타입 Integer -> LottoNumber로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,20 +5,24 @@ import java.util.List;
 public class Lotto {
     private static final String SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개를 입력해야 합니다.";
     private static final int LOTTO_COUNT = 6;
-    private final List<Integer> numbers;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<LottoNumber> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = List.copyOf(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validate(List<LottoNumber> numbers) {
         if (numbers.size() != LOTTO_COUNT) {
             throw new IllegalArgumentException(SIZE_ERROR_MESSAGE);
         }
     }
 
     public List<Integer> getNumbers() {
-        return numbers;
+        return this.numbers.stream().map(LottoNumber::getNumber).toList();
+    }
+
+    public boolean contains(LottoNumber lottoNumber) {
+        return numbers.contains(lottoNumber);
     }
 }

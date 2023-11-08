e26cf23119fc5e25383433e7ee9b36ce23182097
Sinorang <aodkrak@naver.com>
2023-11-08 16:49:38 +0900

    feat. 로또 유효 번호 판단 메소드
    - 매개변수로 받은 값이 유효한 로또 번호인지 판단하는 기능 구현

---
+++ src/main/java/lotto/util/ValidationUtil.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import java.util.List;
+import java.util.Optional;
+import lotto.enums.ConstVariable;
+
+public class ValidationUtil {
+
+    public static void isValidNumber(List<Integer> numbers) {
+        Optional<Integer> result = numbers.stream()
+                .filter(number -> number < ConstVariable.MIN_NUMBER.getValue()
+                        || number > ConstVariable.MAX_NUMBER.getValue())
+                .findFirst();
+
+        if (result.isPresent()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

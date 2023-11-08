bab680301e18f66e4bbc8e5d5378d181bf530b8d
swhyeon98 <smreo75@gmail.com>
2023-11-08 19:56:22 +0900

    test: 로또 구매 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,7 +1,12 @@
 package lotto.service;
 
+import lotto.domain.Lotto;
 import lotto.domain.RandomLottoGenerator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class LottoServiceTest {
 
@@ -14,5 +19,15 @@ class LottoServiceTest {
         lottoService = new LottoService(lottoGenerator);
     }
 
-    
+    @Test
+    void 로또_구매() {
+        // given
+        int purchaseAmount = 2000;
+
+        // when
+        List<Lotto> lottos = lottoService.buyLotto(purchaseAmount);
+
+        // then
+        Assertions.assertThat(lottos.size()).isEqualTo(2);
+    }
 }

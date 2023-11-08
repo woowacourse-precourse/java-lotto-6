43957280073ce1f95a26627eb484289939da0261
m5s3 <devmyoungsoo2@gmail.com>
2023-11-08 19:47:42 +0900

    refactor(InputView): 의미 없는 띄어쓰기 삭제

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -26,7 +26,6 @@ public class InputView {
     private static final String PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String LOTTO_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-
     private static final String INPUT_LOTTO_NUMBER_SPLITTER = ",";
 
     public int requestLottoPurchaseAmount() {

a791373b22b0a9f9295ddf803217f1729a3dd249
hyonun321 <hyo_nun@kakao.com>
2023-11-08 22:13:25 +0900

    feat : 1과 45가 아닐때의 추가 예외 테스트 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -10,6 +10,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
+
     private static final String ERROR_MESSAGE = "[ERROR]";
 
     @Test
@@ -50,6 +51,18 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() -> {
             runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 추가예외_테스트_1과45가_아닐때() {
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_MESSAGE);
+            runException("46");
+            assertThat(output()).contains(ERROR_MESSAGE);
+            runException("-1");
             assertThat(output()).contains(ERROR_MESSAGE);
         });
     }

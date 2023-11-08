0f833f20ce2e16c1c556b3d9d9bf7eb2dbd237b8
kmw2378 <rlarla677@gmail.com>
2023-11-08 11:57:38 +0900

    feat(lotto): Reward에 일치 개수, 보너스볼 일치 여부에 따라 적절한 인스턴스를 반환하는 메서드 추가 일치 개수, 보너스볼 일치 여부에 따라 보상 정보를 제공하는 메서드를 추가하였습니다. 이 메서드에 사용되는 Map 상수 필드를 추가하였습니다.

--- src/main/java/lotto/domain/Reward.java
+++ src/main/java/lotto/domain/Reward.java
@@ -1,5 +1,10 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum Reward {
     FIRST_PLACE(2000000000L, false, 6, 1),
     SECOND_PLACE(30000000L, true, 5, 2),
@@ -7,6 +12,9 @@ public enum Reward {
     FOURTH_PLACE(50000L, false, 4, 4),
     LAST_PLACE(5000L, false, 3, 5),
     NONE(0L, false, 0, 6);
+
+    private static final Map<Integer, Map<Boolean, List<Reward>>> REWARD_GROUPING_BY_RANK = Arrays.stream(values())
+            .collect(Collectors.groupingBy(Reward::getMatchedCount, Collectors.groupingBy(Reward::isMatchedBonusNumber)));
 
     private final long amount;
     private final boolean matchedBonusNumber;
@@ -18,6 +26,17 @@ public enum Reward {
         this.matchedBonusNumber = matchedBonusNumber;
         this.matchedCount = matchedCount;
         this.rank = rank;
+    }
+
+    public static Reward getInstance(final boolean bonusNumberMatched, final int matchedCount) {
+        if (matchedCount < LAST_PLACE.matchedCount) {
+            return NONE;
+        }
+
+        return REWARD_GROUPING_BY_RANK
+                .get(matchedCount)
+                .get(bonusNumberMatched)
+                .get(0);
     }
 
     public long getAmount() {

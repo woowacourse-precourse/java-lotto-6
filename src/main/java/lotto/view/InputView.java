15e56bdc0de0924a02b4249f9b17e29e63e96566
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 20:05:52 +0900

    fix: 입력 문구 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,19 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public void inputPurchaseAmount() {
-        System.out.println("구입 금액을 입력해주세요.");
+        System.out.println("구입금액을 입력해 주세요.");
 
         String purchaseAmountString = Console.readLine();
     }
 
     public void inputWinningNumbers() {
-        System.out.println("당첨 번호를 입력해주세요.");
+        System.out.println("당첨 번호를 입력해 주세요.");
 
         String winningNumbersString = Console.readLine();
     }
 
     public void inputBonusNumber() {
-        System.out.println("보너스 번호를 입력해주세요.");
+        System.out.println("보너스 번호를 입력해 주세요.");
 
         String bonusNumberString = Console.readLine();
     }

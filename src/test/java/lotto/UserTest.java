5101fb257bb26e5c647a24f5ff7fbe4e28450360
bitesnail <rkwoal216@gmail.com>
2023-11-08 14:02:38 +0900

    test: test code for print lotteries

--- src/test/java/lotto/UserTest.java
+++ src/test/java/lotto/UserTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import lotto.models.Machine;
 import lotto.models.User;
+import lotto.views.OutputView;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,16 @@ public class UserTest {
             user.buyLottery(machine);
         }
         assertThat(count).isEqualTo(14);
+    }
+
+    @DisplayName("로또 출력 테스트")
+    @Test
+    void printLotteries(){
+        user.setMoney(10000);
+        while(user.hasMoney()){
+            user.buyLottery(machine);
+        }
+        OutputView outputView = new OutputView();
+        outputView.printLotteries(user.getLotteries());
     }
 }

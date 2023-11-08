dd70cd4f09f42740a4f10052ba380ac2b302260a
dyeongkim <dyeongkim98@gmail.com>
2023-11-08 17:32:57 +0900

    Feat(OutputUser): Add 로또 게임 결과 출력
    게임결과 출력 부분 추가

--- src/main/java/lotto/OutputUser.java
+++ src/main/java/lotto/OutputUser.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import java.util.List;
+import java.util.Map;
+import lotto.Game.LottoRank;
 
 public class OutputUser {
     public void outputBuyLotto(Integer totalLottoCount){
@@ -10,5 +12,15 @@ public class OutputUser {
         for (Lotto lotto : numberTickets){
             System.out.println(lotto.getNumbers());
         }
+    }
+    public void outputLottoGameResult(Map<LottoRank, Integer> lottoGameResult, double profitRate){
+        System.out.println("당첨 통계\n---");
+        for (LottoRank rank : LottoRank.values()){
+            if (rank.equals(LottoRank.NONE)){
+                continue;
+            }
+            System.out.printf("%s - %d개\n", rank.getMaseege(),lottoGameResult.get(rank));
+        }
+        System.out.printf("총 수익률은 %.2f%%입니다.\n", profitRate);
     }
 }

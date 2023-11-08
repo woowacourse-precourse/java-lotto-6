8a2b66a3995b10dcc93be80f386a90b89385b71d
LeeJaeHoon <dlwogns3413@naver.com>
2023-11-08 19:09:02 +0900

    feat: Lotto 도메인 단위 테스트 작성

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -1,14 +1,6 @@
 package lotto.domain;
 
 public class WinningResult {
-    /**
-     * - 당첨은 1등부터 5등까지 있다. 당첨 기준과 금액은 아래와 같다.
-     *     - 1등: 6개 번호 일치 / 2,000,000,000원
-     *     - 2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
-     *     - 3등: 5개 번호 일치 / 1,500,000원
-     *     - 4등: 4개 번호 일치 / 50,000원
-     *     - 5등: 3개 번호 일치 / 5,000원
-     */
     private Winning winning;
     private int matchCount;
     public WinningResult(User user, WinningLotto winningLotto, Winning winning) {
@@ -20,7 +12,6 @@ public class WinningResult {
         return winning;
     }
 
-//    public String
 
     public int getMatchCount() {
         return matchCount;
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -25,4 +26,44 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호와 다른 로또 번호는 서로 몇개가 맞는지 비교할 수 있다.")
+    @Test
+    void matchCount() {
+        //given
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+
+        //when
+        int matchCount = lotto1.matchCount(lotto2);
+
+        //then
+        assertThat(matchCount).isEqualTo(5);
+    }
+
+    @DisplayName("로또 번호에 인자로 들어온 번호가 있으면 true를 반환한다.")
+    @Test
+    void includesWithTrue() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        boolean isInclude = lotto.includes(1);
+
+        //then
+        assertThat(isInclude).isTrue();
+    }
+
+    @DisplayName("로또 번호에 인자로 들어온 번호가 없으면 false를 반환한다.")
+    @Test
+    void includesWithFalse() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //when
+        boolean isInclude = lotto.includes(7);
+
+        //then
+        assertThat(isInclude).isFalse();
+    }
 }

63476f235a25920b521f0b63c32c4be22fd4c44c
박승민 <tmddudals369@naver.com>
2023-11-08 17:51:12 +0900

    fix(resultResponse): 2등 당첨 결과 출력 포맷 문제 해결

--- src/main/java/lotto/dto/ResultResponse.java
+++ src/main/java/lotto/dto/ResultResponse.java
@@ -4,7 +4,7 @@ import lotto.utils.Rank;
 
 public record ResultResponse(Rank rank, int count) {
     private static final String BASIC_RESULT_FORMAT = "%d개 일치 (%,d원) - %d개\n";
-    private static final String SECOND_RESULT_FORMAT = "%d개 일치 (%,d원) - %d개\n";
+    private static final String SECOND_RESULT_FORMAT = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개\n";
 
     @Override
     public String toString() {

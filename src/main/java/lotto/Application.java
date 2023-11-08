0cae229e3a99f062c37f55e92758a5e6cb06a0e8
lg960214@naver.com <lg960214@naver.com>
2023-11-08 20:50:35 +0900

    feat : Input 에러 관리 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,47 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("구입금액을 입력해 주세요.");
-        String cashStr = Console.readLine();
-        int cash = Integer.parseInt(cashStr);
+        int cash = getInputFir();
+
+//        List<Integer> numbers = getInputSec();
+
 
 
     }
+
+    private static int getInputFir() {
+        int processedCash = 0;
+
+        while (true){
+            System.out.println("구입금액을 입력해 주세요.");
+            try{
+                String cashStr = Console.readLine();
+                processedCash = validInputOne(cashStr);
+                break;
+            }
+
+            catch(IllegalArgumentException e){
+                System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+            }
+        }
+
+        return processedCash;
+    }
+
+    private static int validInputOne(String cashStr){
+        try{
+            int tempCash = 0;
+            tempCash = Integer.parseInt(cashStr);
+
+            if (tempCash % 1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+
+            return tempCash;
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
+
 }

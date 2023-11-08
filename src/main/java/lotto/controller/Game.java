d0d17d24bf71d510d159266a8be5ecbeb373e1f5
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:18:02 +0900

    feat : 보너스 볼 입력 받은 후 검증
    보너스 볼 입력 후 예외 발생시 다시 입력 받는다

--- src/main/java/lotto/controller/Game.java
+++ src/main/java/lotto/controller/Game.java
@@ -12,12 +12,14 @@ public class Game {
     private int lottoCount; // 구입한 로또 장 수
     private List<Lotto> lottoList = new ArrayList<>(); // 구입한 로또 리스트
     private Lotto winningLotto; // 당첨 번호
+    private int winningBonusNumber; // 보너스 번호
 
     public Game() {}
 
     public static Game create(){
         return new Game();
     }
+
 
     public void setLottoCount() {
         String inputStringValue = InputView.inputBuyValue();
@@ -49,6 +51,19 @@ public class Game {
         catch (IllegalArgumentException e){
             System.out.println(e.getMessage());
             setWinningLotto();
+        }
+
+    }
+
+    public void setWinningBonusNumber(){
+
+        try{
+            String inputStringValue = InputView.inputBonusNumber();
+            ValidateWinningNumber.validateBonusNumber(inputStringValue);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            setWinningBonusNumber();
         }
 
     }

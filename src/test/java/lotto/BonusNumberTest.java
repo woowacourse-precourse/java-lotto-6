4ff3520a0e1d5f79d3c388949e114e65c124bb14
Seyeon <wjdtpdus828@naver.com>
2023-11-08 16:14:16 +0900

    test(BonusNumberTest): 보너스 번호의 범위가 정상일 경우 테스트하는 코드 추가

--- src/test/java/lotto/BonusNumberTest.java
+++ src/test/java/lotto/BonusNumberTest.java
@@ -3,12 +3,14 @@ package lotto;
 import lotto.controller.BonusNumberController;
 import lotto.domain.Bonus;
 import lotto.domain.Lotto;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BonusNumberTest {
 
@@ -35,5 +37,23 @@ public class BonusNumberTest {
         int actualResult = bonus.getBonusNumber();
         assertThatThrownBy(() -> lotto.validateBonusNumber(actualResult))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호의 범위가 정상이면 예외가 발생하지 않는다.")
+    @Test
+    void createBonusWhenRightRange() {
+        Bonus bonus = new Bonus(1);
+        int expectedResult = 1;
+        assertEquals(expectedResult, bonus.getBonusNumber());
+    }
+
+    @DisplayName("보너스 번호가 중복되지 않으면 예외가 발생하지 않는다.")
+    @Test
+    void createBonusWhenNotDuplicate() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 15));
+        lotto.validateBonusNumber(16);
+        Bonus bonus = new Bonus(16);
+        int expectedResult = 16;
+        assertEquals(expectedResult, bonus.getBonusNumber());
     }
 }

161adc57fc54f873f05a9f45b918e4e5cded884a
HYUNJUN\sajun <sajun28@naver.com>
2023-11-08 21:02:00 +0900

    Refactor: "enum 수정"

--- src/main/java/lotto/model/LottoResult.java
+++ src/main/java/lotto/model/LottoResult.java
@@ -5,26 +5,23 @@ import java.util.HashMap;
 public class LottoResult {
 
     private static final int NUMBER_OF_WINNING_CASE = 5;
-    private static final int FIRST_MONEY = 2000000000;
-    private static final int SECOND_MONEY = 30000000;
-    private static final int THIRD_MONEY = 1500000;
-    private static final int FOURTH_MONEY = 50000;
-    private static final int FIFTH_MONEY = 5000;
-    private static final int FIRST_NUMBER_OF_SAME = 6;
-    private static final int SECOND_NUMBER_OF_SAME = 5;
-    private static final int THIRD_NUMBER_OF_SAME = 5;
-    private static final int FORUTH_NUMBER_OF_SAME = 4;
-    private static final int FIFTH_NUMBER_OF_SAME = 3;
 
     private static HashMap<Integer, Integer> result = new HashMap<>();
 
     public enum PrizeMoney {
-        FIRST(FIRST_MONEY), SECOND(SECOND_MONEY), THIRD(THIRD_MONEY), FOURTH(FOURTH_MONEY), FIFTH(FIFTH_MONEY);
+        FIRST(6, 2000000000), SECOND(5, 30000000), THIRD(5, 1500000),
+        FOURTH(4, 50000), FIFTH(3, 5000);
 
+        private final int numberOfSame;
         private final int money;
 
-        PrizeMoney(int money) {
+        PrizeMoney(int numberOfSame, int money) {
+            this.numberOfSame = numberOfSame;
             this.money = money;
+        }
+
+        public int getNumberOfSame() {
+            return numberOfSame;
         }
 
         public int calculateFare(int count) {
@@ -33,38 +30,38 @@ public class LottoResult {
     }
 
     public static void initialize() {
-        for (int i = FIFTH_NUMBER_OF_SAME; i < NUMBER_OF_WINNING_CASE + FIFTH_NUMBER_OF_SAME; i++) {
+        for (int i = PrizeMoney.FIFTH.getNumberOfSame(); i < NUMBER_OF_WINNING_CASE + PrizeMoney.FIFTH.getNumberOfSame(); i++) {
             result.put(i, 0);
         }
     }
 
     public static HashMap statistics(int sameCount, Boolean bonusCheck) {
-        if (sameCount == FIFTH_NUMBER_OF_SAME) {
-            int num = result.get(FIFTH_NUMBER_OF_SAME);
+        if (sameCount == PrizeMoney.FIFTH.getNumberOfSame()) {
+            int num = result.get(PrizeMoney.FIFTH.getNumberOfSame());
             num++;
-            result.put(FIFTH_NUMBER_OF_SAME, num);
+            result.put(PrizeMoney.FIFTH.getNumberOfSame(), num);
         }
-        if (sameCount == FORUTH_NUMBER_OF_SAME) {
-            int num = result.get(FORUTH_NUMBER_OF_SAME);
+        if (sameCount == PrizeMoney.FOURTH.getNumberOfSame()) {
+            int num = result.get(PrizeMoney.FOURTH.getNumberOfSame());
             num++;
-            result.put(FORUTH_NUMBER_OF_SAME, num);
+            result.put(PrizeMoney.FOURTH.getNumberOfSame(), num);
         }
-        if (sameCount == THIRD_NUMBER_OF_SAME) {
+        if (sameCount == PrizeMoney.THIRD.getNumberOfSame()) {
             if (bonusCheck) {
-                int num = result.get(SECOND_NUMBER_OF_SAME + 1);
+                int num = result.get(PrizeMoney.SECOND.getNumberOfSame() + 1);
                 num++;
-                result.put(SECOND_NUMBER_OF_SAME + 1, num);
+                result.put(PrizeMoney.SECOND.getNumberOfSame() + 1, num);
             }
             if (!bonusCheck) {
-                int num = result.get(THIRD_NUMBER_OF_SAME);
+                int num = result.get(PrizeMoney.THIRD.getNumberOfSame());
                 num++;
-                result.put(THIRD_NUMBER_OF_SAME, num);
+                result.put(PrizeMoney.THIRD.getNumberOfSame(), num);
             }
         }
-        if (sameCount == FIRST_NUMBER_OF_SAME) {
-            int num = result.get(FIRST_NUMBER_OF_SAME + 1);
+        if (sameCount == PrizeMoney.FIRST.getNumberOfSame()) {
+            int num = result.get(PrizeMoney.FIRST.getNumberOfSame() + 1);
             num++;
-            result.put(FIRST_NUMBER_OF_SAME + 1, num);
+            result.put(PrizeMoney.FIRST.getNumberOfSame() + 1, num);
         }
         return result;
     }
@@ -72,7 +69,7 @@ public class LottoResult {
     public static double calculateProfit(int purchase) {
         double allMoney = 0;
         for (PrizeMoney p : PrizeMoney.values()) {
-            allMoney += p.calculateFare(result.get(FIRST_NUMBER_OF_SAME + 1 - p.ordinal()));
+            allMoney += p.calculateFare(result.get(PrizeMoney.FIRST.getNumberOfSame() + 1 - p.ordinal()));
         }
         return allMoney / (purchase * 10);
     }

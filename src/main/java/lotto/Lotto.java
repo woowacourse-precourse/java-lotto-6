c36ad731cd885322d1fa9dceb776452e093a417f
UnityChan3000 <lego0313@naver.com>
2023-11-08 21:20:11 +0900

    당첨번호 각 숫자 1~45 범위 밖에 있는지 확인 및, 에러메세지 보내는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,12 +8,25 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         checkSameNumber(numbers);
+        loopCheckNumberRange(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("당첨 번호는 숫자 6개를 입력해야 합니다.");
+        }
+    }
+
+    private void loopCheckNumberRange(List<Integer> numbers){
+        for(int number : numbers){
+            checkNumberRange(number);
+        }
+    }
+
+    private void checkNumberRange(int number){
+        if(number<1 || number>45){
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 

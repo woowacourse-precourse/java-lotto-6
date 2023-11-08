98070d65143e6b560ce9bfaeba80e0e6768e062b
ehtjsv2 <ehtjsv2@naver.com>
2023-11-08 16:09:31 +0900

    Refact: Output enum클래스 삭제 final static으로 대체

--- src/main/java/lotto/view/OutPutView.java
+++ src/main/java/lotto/view/OutPutView.java
@@ -1,12 +1,13 @@
 package lotto.view;
 
-import static lotto.configuration.OutputConfiguration.INSERT_BONUS_NUMBER;
-import static lotto.configuration.OutputConfiguration.INSERT_MONEY;
-import static lotto.configuration.OutputConfiguration.INSERT_WIN_NUMBER;
-import static lotto.configuration.OutputConfiguration.PURCHASED_LOTTO;
-import static lotto.configuration.OutputConfiguration.WIN_STATISTICS;
-
 public class OutPutView {
+    final static String INSERT_MONEY = "구입금액을 입력해 주세요.";
+    final static String INSERT_WIN_NUMBER = "당첨 번호를 입력해 주세요.";
+    final static String INSERT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    final static String PURCHASED_LOTTO = "개를 구매했습니다.";
+    final static String WIN_STATISTICS = "\n당첨 통계\n---\n";
+    final static String RETURN_RATE_OPENING = "총 수익률은 ";
+    final static String RETURN_RATE_CLOSING = "%%입니다.";
 
     public static void println(String out) {
         System.out.println(out);
@@ -16,11 +17,11 @@ public class OutPutView {
     }
 
     public static void printBuyTicket() {
-        println(INSERT_MONEY.getMessage());
+        println(INSERT_MONEY);
     }
 
     public static void printLottoCount(int lottoCount) {
-        println("\n" + lottoCount + PURCHASED_LOTTO.getMessage());
+        println("\n" + lottoCount + PURCHASED_LOTTO);
     }
 
     public static void printLottos(String lottos) {
@@ -28,18 +29,18 @@ public class OutPutView {
     }
 
     public static void printEnterWinNumber() {
-        println("\n" + INSERT_WIN_NUMBER.getMessage());
+        println("\n" + INSERT_WIN_NUMBER);
     }
 
     public static void printBonusNumber() {
-        println("\n" + INSERT_BONUS_NUMBER.getMessage());
+        println("\n" + INSERT_BONUS_NUMBER);
     }
 
     public static void printMatchResult(String matchResult) {
         print(WIN_STATISTICS + matchResult);
     }
     public static void printReturnRate(double returnRate){
-        System.out.printf("총 수익률은 %.1f%%입니다.",returnRate);
+        System.out.printf(RETURN_RATE_OPENING+"%.1f"+RETURN_RATE_CLOSING,returnRate);
     }
 
 }

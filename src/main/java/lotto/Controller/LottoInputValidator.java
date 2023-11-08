0bf9e2b43ca014387673400292594a96ed0f0cdc
Sanghee0820 <lsh277604@gmail.com>
2023-11-08 18:05:12 +0900

    Feat: add Exception -> Budget range
    Budget > 0 && Budget % 1000 = 0

--- src/main/java/lotto/Controller/LottoInputValidator.java
+++ src/main/java/lotto/Controller/LottoInputValidator.java
@@ -7,7 +7,10 @@ public class LottoInputValidator {
         try {
             budget = Long.parseLong(budgetStr);
         } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 숫자가아닙니다.");
+            throw new IllegalArgumentException("[ERROR] 구입 금액이 숫자가아닙니다.");
+        }
+        if (budget <= 0 || budget % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 예산은 0보다 크고 1000으로 나누어 떨어져야 합니다.\n");
         }
     }
 }

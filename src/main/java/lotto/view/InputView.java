8bcc5ea79e90cad350fae577df4885fdd2572596
juns1s <juns1s@naver.com>
2023-11-08 15:05:21 +0900

    feat: InputException 구현에 따른 처리 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,8 +3,8 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import lotto.message.InputExceptionMessage;
+import lotto.exception.InputException;
+import lotto.exception.InputExceptionMessage;
 import lotto.model.Lotto;
 
 /**
@@ -31,78 +31,81 @@ public class InputView {
 
     /**
      * 총 로또 구매 가격을 입력받는 메서드
+     *
      * @return totalPrice(총 구매 가격)
      */
     public int receiveTotalPrice() {
-        while (true) {
+        try {
             String input = Console.readLine();
 
             if (!input.matches(ONLY_NUMBER)) {
-                System.out.println(InputExceptionMessage.INVALID_PRICE_TYPE);
-                continue;
+                throw new InputException(InputExceptionMessage.INVALID_PRICE_TYPE);
             }
 
             int totalPrice = Integer.parseInt(input);
 
             if (totalPrice % LOTTO_PRICE != 0) {
-                System.out.println(InputExceptionMessage.INVALID_PRICE);
-                continue;
+                throw new InputException(InputExceptionMessage.INVALID_PRICE);
             }
+
             return totalPrice;
+        } catch (IllegalArgumentException e) {
+            return receiveTotalPrice();
         }
     }
 
     /**
      * 1등 로또 번호(당첨번호)를 입력받는 메서드
+     *
      * @return {@link Lotto} 1등 당첨번호
      */
     public Lotto receiveLottoWinning() {
-        while (true) {
+        try {
             String input = Console.readLine();
 
             if (!input.matches(VALID_LOTTO_LIST_PATTERN)) {
-                System.out.println(InputExceptionMessage.INVALID_LOTTO_NUMBERS);
-                continue;
+                throw new InputException(InputExceptionMessage.INVALID_LOTTO_NUMBERS);
             }
 
-            Optional<List<Integer>> optionalList = getValidWinningList(input);
-            if (optionalList.isEmpty()) {
-                continue;
-            }
+            List<Integer> validWinningList = getValidWinningList(input);
 
-            return new Lotto(optionalList.get());
+            return new Lotto(validWinningList);
+        } catch (IllegalArgumentException e) {
+            return receiveLottoWinning();
         }
     }
 
     /**
      * 보너스 번호를 입력받는 메서드
+     *
      * @param lotto {@link Lotto} 1등 당첨 번호와 중복 방지를 위해 확인
      * @return 보너스 번호
      */
     public int receiveBonusNumber(Lotto lotto) {
-        while (true) {
+        try {
             String input = Console.readLine();
             if (!input.matches(VALID_LOTTO_NUMBER_PATTERN)) {
-                System.out.println(InputExceptionMessage.INVALID_BONUS_NUMBER);
-                continue;
+                throw new InputException(InputExceptionMessage.INVALID_BONUS_NUMBER);
             }
             int num = Integer.parseInt(input);
 
             if (lotto.getNumbers().contains(num)) {
-                System.out.println(InputExceptionMessage.DUPLICATED_BONUS_NUMBER);
-                continue;
+                throw new InputException(InputExceptionMessage.DUPLICATED_BONUS_NUMBER);
             }
 
             return num;
+        } catch (IllegalArgumentException e) {
+            return receiveBonusNumber(lotto);
         }
     }
 
     /**
      * 로또 번호의 중복을 확인하는 메서드
+     *
      * @param input {@link lotto.view.InputView#VALID_LOTTO_LIST_PATTERN} 형식의 로또 번호
      * @return {@literal Optional<List<Integer>>} 형식의 유효한 로또 번호 리스트
      */
-    private static Optional<List<Integer>> getValidWinningList(String input) {
+    private static List<Integer> getValidWinningList(String input) {
         List<Integer> winningList = Arrays.stream(input.split(","))
                 .map(Integer::parseInt)
                 .sorted(Integer::compare)
@@ -111,9 +114,8 @@ public class InputView {
         long uniqueLength = winningList.stream().distinct().count();
 
         if (winningList.size() != uniqueLength) {
-            System.out.println(InputExceptionMessage.INVALID_LOTTO_DUPLICATED);
-            return Optional.empty();
+            throw new InputException(InputExceptionMessage.INVALID_LOTTO_DUPLICATED);
         }
-        return Optional.of(winningList);
+        return winningList;
     }
 }

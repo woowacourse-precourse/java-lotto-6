fe2bd912061196f75db251c3414142138e2f9bfe
JongMinCh0i <wonderland314592@gmail.com>
2023-11-08 21:32:21 +0900

    test(LottoRankTest): LottoRank 등수당 해당하는 금액과, 설명이 올바르게 출력되는지에 대해 단위 테스트 작성

--- src/test/java/lotto/constants/LottoRankTest.java
+++ src/test/java/lotto/constants/LottoRankTest.java
@@ -18,4 +18,25 @@ public class LottoRankTest {
         assertEquals(LottoRank.NONE, LottoRank.evaluateLottoRank(2, false));
     }
 
+    @DisplayName("등수당 금액이 맞는지 테스트 한다.")
+    @Test
+    public void testGetPrizeMoney() {
+        assertEquals(5000, LottoRank.FIFTH.getPrizeMoney());
+        assertEquals(50000, LottoRank.FOURTH.getPrizeMoney());
+        assertEquals(1500000, LottoRank.THIRD.getPrizeMoney());
+        assertEquals(30000000, LottoRank.SECOND.getPrizeMoney());
+        assertEquals(2000000000, LottoRank.FIRST.getPrizeMoney());
+        assertEquals(0, LottoRank.NONE.getPrizeMoney());
+    }
+
+    @DisplayName("해당하는 등수에 맞는 설명이 출력되는지 테스트한다.")
+    @Test
+    public void testGetDescription() {
+        assertEquals("3개 일치 (5,000원)", LottoRank.FIFTH.getDescription());
+        assertEquals("4개 일치 (50,000원)", LottoRank.FOURTH.getDescription());
+        assertEquals("5개 일치 (1,500,000원)", LottoRank.THIRD.getDescription());
+        assertEquals("5개 일치, 보너스 볼 일치 (30,000,000원)", LottoRank.SECOND.getDescription());
+        assertEquals("6개 일치 (2,000,000,000원)", LottoRank.FIRST.getDescription());
+        assertEquals("일치하는 숫자 없음", LottoRank.NONE.getDescription());
+    }
 }

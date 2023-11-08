d1ae8270c83b15df4b16b0f0eff20851bd772842
HwangSeokJin <ghkd3703@gmail.com>
2023-11-08 16:07:12 +0900

    style: 메세지 출력 형식 변경

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -7,8 +7,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String MESSAGE_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String MESSAGE_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String MESSAGE_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String MESSAGE_INPUT_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
     private static final String DELIMITER = ",";
 
     public int inputPurchaseAmount() throws IllegalArgumentException {
--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class OutputView {
     private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
-    private static final String LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    private static final String LOTTO_COUNT_FORMAT = "\n%d개를 구매했습니다.";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.1f%%입니다.";
-    private static final String RESULT_FORMAT = "당첨 통계\n---%s";
+    private static final String RESULT_FORMAT = "\n당첨 통계\n---%s";
 
     public void printErrorMessage(String message) {
         System.out.println(String.format(ERROR_MESSAGE_FORMAT, message));

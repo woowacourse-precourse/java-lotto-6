451c7559355c93e103c0a01c272e57ae677bcd2d
다운 <j_d_woon@naver.com>
2023-11-08 16:32:22 +0900

    refactor: 메서드 접근 지정자 변경

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -42,7 +42,7 @@ public class LottoService {
         return checkRankCounts(lottos, winningLottoNumbers,bonusLottoNumber);
     }
 
-    private void validateWinningNumbers(List<Integer> winningLottoNumbers) {
+    public void validateWinningNumbers(List<Integer> winningLottoNumbers) {
         if(!winningLottoNumbers.stream()
                 .allMatch(number -> number >= 1 && number <= 45)) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");

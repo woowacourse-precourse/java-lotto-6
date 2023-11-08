fa2b00be1c51a0eb5983f3daf343552ceac4ab1d
권장순 <jsoonworld@gmail.com>
2023-11-08 13:59:11 +0900

    feat( LottoYieldController ) : Yield view, service를 제어하는 controller 구현.

---
+++ src/main/java/lotto/v3/controller/LottoYieldController.java
@@ -0,0 +1,30 @@
+package lotto.v3.controller;
+
+import lotto.v3.model.LottoRank;
+import lotto.v3.model.LottoResult;
+import lotto.v3.service.LottoYieldCalculatorService;
+import lotto.v3.view.LottoYieldView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class LottoYieldController {
+    private final LottoYieldView view;
+    private final LottoYieldCalculatorService yieldCalculatorService;
+
+    public LottoYieldController(LottoYieldView view, LottoYieldCalculatorService yieldCalculatorService) {
+        this.view = view;
+        this.yieldCalculatorService = yieldCalculatorService;
+    }
+
+    public void processLottoYield(List<Set<Integer>> purchasedNumbers, Set<Integer> winningNumbers, int bonusNumber) {
+        LottoResult result = new LottoResult(purchasedNumbers, winningNumbers, bonusNumber);
+        Map<LottoRank, Integer> matchCounts = result.getMatchCounts();
+        double yield = yieldCalculatorService.calculateYield(matchCounts);
+        view.displayResults(matchCounts, yield);
+    }
+}
+
+
+

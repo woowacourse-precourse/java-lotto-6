2bebcfaddd188440dfa1766edfedae315184f468
Hoony0321 <eogns0321@gmail.com>
2023-11-08 17:15:21 +0900

    feat : ExceptionView 추가
    Exception 관련 출력을 담당하는 클래스

---
+++ src/main/java/lotto/view/ExceptionView.java
@@ -0,0 +1,10 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+public class ExceptionView {
+
+    public static void printErrorMessage(String message) {
+        System.out.println("[ERROR] " + message);
+    }
+}

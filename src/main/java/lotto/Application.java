51e0232ddeefbdebf45f65eb84a230a7bdef44ea
gyeommm <dkndob02@naver.com>
2023-11-08 15:45:07 +0900

    검증 항목별 분리, 예외처리 위치를 main함수가 아닌 PurchaseAmount 클래스 내로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,19 +4,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         PurchaseAmount purchaseAmount = new PurchaseAmount();
-
-        while(true) {
-            try {
-                purchaseAmount.printInputAmountMessage();
-                purchaseAmount.inputAmount();
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println("[Error] " + e.getMessage() + "\n");
-            }
-        }
+        purchaseAmount.printInputAmountMessage();
+        purchaseAmount.inputAmount();
 
         LottoList lottoList = new LottoList(purchaseAmount.getAmount());
-        lottoList.printNumberOfLottoList();
+        lottoList.printNumberOfLotto();
         lottoList.createLottoList();
         lottoList.printLottoList();
 
--- src/main/java/lotto/PurchaseAmount.java
+++ src/main/java/lotto/PurchaseAmount.java
@@ -13,19 +13,33 @@ public class PurchaseAmount {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void validate(String input_amount){
+    // 입력값이 숫자인지 확인하여 변환
+    private int convertToInt(String input_amount){
         try {
-            amount = Integer.parseInt(input_amount);
-        } catch (NumberFormatException e) {
+            return Integer.parseInt(input_amount);
+        } catch (NumberFormatException e){
             throw new IllegalArgumentException("입력값이 숫자가 아닙니다. 1000원 단위의 자연수를 입력해주세요.");
         }
+    }
 
+    // 구입금액이 1000원 단위인지 확인
+    public void checkUnit1000(int amount){
         if(amount < 0 || amount % 1000 != 0){
             throw new IllegalArgumentException("구입금액은 1000원 단위의 자연수로 입력해야 합니다.");
         }
     }
+
     public void inputAmount(){
-        String input_amount = readLine();
-        validate(input_amount);
+        while(true) {
+            String input_amount = readLine();
+            try {
+                int converted_amount = convertToInt(input_amount);
+                checkUnit1000(converted_amount);
+                amount = converted_amount;
+                break;
+            } catch (IllegalArgumentException e){
+                System.out.println("[Error] " + e.getMessage());
+            }
+        }
     }
 }

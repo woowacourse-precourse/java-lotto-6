1f7b4232bab5784f096da81ce74b6a1fc7797056
GyungA <GyungA@users.noreply.github.com>
2023-11-08 21:08:36 +0900

    feat: 사용자에게 보너스 번호를 입력받는 기능

--- src/main/java/lotto/View/WinningLottoGenerator.java
+++ src/main/java/lotto/View/WinningLottoGenerator.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class WinningLottoGenerator {
     public static List<Integer> WINNING_NUMBERS;
+    public static Integer BONUS_NUMBER;
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. (쉼표로 구분)";
+    private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "입력값 %s는 올바른 숫자 형식이 아닙니다. ";
     public static void generateWinningNumbers() {
         System.out.println(WINNING_NUMBER_MESSAGE);
@@ -27,5 +29,11 @@ public class WinningLottoGenerator {
         }
         WINNING_NUMBERS = winningNumbers;
         return winningNumbers;
+    }
+
+    public static Integer generateBonusNumber() {
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        BONUS_NUMBER = Integer.parseInt(Console.readLine());
+        return BONUS_NUMBER;
     }
 }

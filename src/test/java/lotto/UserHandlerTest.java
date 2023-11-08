6d514ec22899c7fa86dd77d215b48cdf377391ab
leedabin2 <12193152@inha.edu>
2023-11-08 19:07:02 +0900

    test(UserHandleTest): Add error handling test for validateWinningNumberInput method

--- src/test/java/lotto/UserHandlerTest.java
+++ src/test/java/lotto/UserHandlerTest.java
@@ -39,7 +39,60 @@ class UserHandlerTest {
                 .hasMessage("[ERROR] 1000원 단위로 입력해주세요.");
     }
 
+    // 당첨 번호 입력 에러 테스트
+    @DisplayName("validateWinningNumberInput 메서드 테스트: null 입력 시 Exception 발생")
+    @Test
+    void validateWinningNumberInput_null_숫자_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 번호를 입력해주세요.");
+    }
 
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 빈값 입력 시 Exception 발생")
+    @Test
+    void validateWinningNumberInput_빈값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 번호를 입력해주세요.");
+    }
 
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 쉼표(,)로 구분되지 않은 경우 Exception 발생")
+    @Test
+    void validateWinningNumberInput_쉼표가_아닌값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 쉼표(,)로 구분해주세요.");
+    }
 
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 당첨 번호가 6개가 아닌 경우 Exception 발생")
+    @Test
+    void validateWinningNumberInput_6자리가_아닌값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
+    }
+
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 당첨 번호가 중복된 경우 Exception 발생")
+    @Test
+    void validateWinningNumberInput_중복된값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput("1,1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+    }
+
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 당첨 번호가 숫자가 아닌 경우 Exception 발생")
+    @Test
+    void validateWinningNumberInput_숫자가_아닌값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput("1,2,c,3,e,f"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 숫자를 입력해주세요.");
+    }
+
+    @DisplayName("validateWinningNumberInput 메서드 테스트: 당첨 번호가 1~45 범위를 벗어난 경우 Exception 발생")
+    @Test
+    void validateWinningNumberInput_범위를_벗어난_값_입력시() {
+        assertThatThrownBy(() -> UserHandler.validateWinningNumberInput("1,43,48,49,50,51"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45까지의 숫자입니다.");
+    }
 }

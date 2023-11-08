5b79d292e201ab39854c22eba89aad9817f8ab9f
yeahdy <yejine98@naver.com>
2023-11-08 17:14:06 +0900

    Refector: 총 로또 당첨 금액 계산 메소드 수정
    하드 코딩 값 상수화, 반복되는 코드 for 문으로 수정

--- src/main/java/domain/LottoResult.java
+++ src/main/java/domain/LottoResult.java
@@ -1,6 +1,8 @@
 package domain;
 
+import constants.LottoWinningPrice;
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -28,12 +30,13 @@ public class LottoResult {
                 isBonusLotto = true;
                 winningCount++;
             }
-            lottoResult.setLottoResultStatistic(winningCount,isBonusLotto);
+            lottoResult.setLottoResultCount(winningCount,isBonusLotto);
         }
         BigDecimal totalProfit = lottoResult.createTotalProfit(lottoResult);
         lottoResult.setProfitRate(totalProfit ,lottoGame.getAmount());
         return lottoResult;
     }
+
 
     private int getContainsLottoCount(Set<Integer> lottoSet, List<Integer> lottoNumbers){
         int winningCount= 0;
@@ -46,7 +49,8 @@ public class LottoResult {
         return winningCount;
     }
 
-    private void setLottoResultStatistic(int winningCount, boolean isBonusLotto){
+
+    public void setLottoResultCount(int winningCount, boolean isBonusLotto){
         if(winningCount == 3){
             this.threeLottoWinning++;
             return;
@@ -72,6 +76,7 @@ public class LottoResult {
         this.fiveLottoWinning++;
     }
 
+
     public void setProfitRate(BigDecimal totalProfit, int amount){
         BigDecimal ratio = totalProfit.divide(new BigDecimal(amount), 3, BigDecimal.ROUND_HALF_UP);
         BigDecimal profitRate = ratio.multiply(new BigDecimal(100));
@@ -79,28 +84,33 @@ public class LottoResult {
         this.profitRate = profitRate.doubleValue();
     }
 
-    private BigDecimal createTotalProfit(LottoResult lottoResult){
-        BigDecimal threePrice = new BigDecimal("5000");
-        BigDecimal fourPrice = new BigDecimal("50000");
-        BigDecimal fivePrice = new BigDecimal("1500000");
-        BigDecimal fiveBonusPrice = new BigDecimal("30000000");
-        BigDecimal sixPrice = new BigDecimal("2000000000");
 
-        BigDecimal threePriceResult = threePrice.multiply(
-                new BigDecimal(lottoResult.getThreeLottoWinning()));
-        BigDecimal fourPriceResult = fourPrice.multiply(
-                new BigDecimal(lottoResult.getFourLottoWinning()));
-        BigDecimal fivePriceResult = fivePrice.multiply(
-                new BigDecimal(lottoResult.getFiveLottoWinning()));
-        BigDecimal fiveBonusPriceResult = fiveBonusPrice.multiply(
-                new BigDecimal(lottoResult.getFiveBonusLottoWinning()));
-        BigDecimal sixPriceResult = sixPrice.multiply(
-                new BigDecimal(lottoResult.getSixLottoWinning()));
+    public BigDecimal createTotalProfit(LottoResult lottoResult){
+        BigDecimal[] lottoPrices = getBigDecimalLottoPrices();
+        BigDecimal[] priceCounts = getBigDecimalPriceCounts(lottoResult);
+        BigDecimal totalProfit = BigDecimal.ZERO;
 
-        return threePriceResult.add(fourPriceResult)
-                .add(fivePriceResult)
-                .add(fiveBonusPriceResult)
-                .add(sixPriceResult);
+        for(int i=0; i<priceCounts.length; i++){
+            BigDecimal calculatedPrice = lottoPrices[i].multiply(priceCounts[i]);
+            totalProfit = totalProfit.add(calculatedPrice);
+        }
+
+        return totalProfit;
+    }
+
+    private BigDecimal[] getBigDecimalLottoPrices(){
+        return Arrays.stream(LottoWinningPrice.values())
+                .map(lottoPrice -> new BigDecimal(lottoPrice.getPrice()))
+                .toArray(BigDecimal[]::new);
+    }
+
+    private BigDecimal[] getBigDecimalPriceCounts(LottoResult lottoResult){
+        return new BigDecimal[] {
+                new BigDecimal(lottoResult.getThreeLottoWinning())
+                ,new BigDecimal(lottoResult.getFourLottoWinning())
+                ,new BigDecimal(lottoResult.getFiveLottoWinning())
+                ,new BigDecimal(lottoResult.getFiveBonusLottoWinning())
+                ,new BigDecimal(lottoResult.getSixLottoWinning())};
     }
 
     public int getThreeLottoWinning() {

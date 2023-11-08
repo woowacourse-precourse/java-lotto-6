e21070eaad1b41f1affd4173e09189c8e4100971
minsu20 <syt07100@gmail.com>
2023-11-08 20:47:25 +0900

    refactor: 로또 수익률 계산기 동적 상금 계산으로 수정

--- src/main/java/lotto/service/LottoProfitCalculator.java
+++ src/main/java/lotto/service/LottoProfitCalculator.java
@@ -1,23 +1,16 @@
 package lotto.service;
 
-import lotto.constants.LottoPrize;
-
-import static lotto.constants.LottoSetting.PRICE_PER_TICKET;
-
 public class LottoProfitCalculator {
 
-    private static final int TICKET_PRICE = PRICE_PER_TICKET.value();
-
-    public double calculateProfitRate(int[] matchCounts, int ticketsPurchased) {
-        long totalPrize = calculateTotalPrize(matchCounts);
-        return (double) totalPrize / (ticketsPurchased * TICKET_PRICE) * 100.0;
+    public double calculateProfitRate(int[] matchCounts, int payMoney, long[] prizeAmounts) {
+        long totalPrize = calculateTotalPrize(matchCounts, prizeAmounts);
+        return (double) totalPrize / (payMoney) * 100.0;
     }
 
-    private long calculateTotalPrize(int[] matchCounts) {
+    private long calculateTotalPrize(int[] matchCounts, long[] prizeAmounts) {
         long totalPrize = 0;
-        LottoPrize[] prizes = LottoPrize.values();
         for (int i = 0; i < matchCounts.length; i++) {
-            totalPrize += matchCounts[i] * prizes[i].getPrizeMoney();
+            totalPrize += matchCounts[i] * prizeAmounts[i];
         }
         return totalPrize;
     }

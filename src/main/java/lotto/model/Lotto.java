8302c02e7c4ec36a11e07570472a71d593cb5b94
hyerimmy <heyrim2010@naver.com>
2023-11-08 20:16:31 +0900

    fix: Lotto 클래스 에러 추가 - 중복 숫자일 시 에러 반환

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
+import lotto.constant.message.ErrorMessage;
+
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -8,12 +11,23 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int number : numbers){
+            if(hashSet.contains(number)){
+                throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER);
+            }
+            hashSet.add(number);
         }
     }
 

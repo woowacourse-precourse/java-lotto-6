9af3a22b9bea6487b19e0985a75c0142a8390d67
Seyeon <wjdtpdus828@naver.com>
2023-11-08 16:22:58 +0900

    add(Numbers): 등수 개수인 정수형 상수 추가

--- src/main/java/lotto/constant/Numbers.java
+++ src/main/java/lotto/constant/Numbers.java
@@ -3,7 +3,8 @@ package lotto.constant;
 public enum Numbers {
     INIT_NUMBER_START(1),
     INIT_NUMBER_END(45),
-    INIT_NUMBER_COUNT(6);
+    INIT_NUMBER_COUNT(6),
+    RANK_NUMBER(5);
 
     private final int number;
 

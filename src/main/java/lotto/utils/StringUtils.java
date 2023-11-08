c48a2a4baa2b5b3c8250637d9a60e231679e8d61
jjongwon7 <jjongwon7@naver.com>
2023-11-08 22:29:21 +0900

    feat: 문자열 배열을 정수 리스트로 변환하는 기능 추가 문자열 배열을 정수 리스트로 변환하여 리턴하고, 변환이 불가능한 경우 예외 발생시키는 메서드 작성

--- src/main/java/lotto/utils/StringUtils.java
+++ src/main/java/lotto/utils/StringUtils.java
@@ -4,6 +4,9 @@ import static lotto.constant.ErrorMessage.INPUT_CONTAINS_WHITE_SPACE;
 import static lotto.constant.ErrorMessage.INPUT_EMPTY;
 import static lotto.constant.ErrorMessage.INPUT_IS_NOT_INTEGER;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringUtils {
     public static void validEmptyString(String str) {
         if (str == null || str.isEmpty()) {
@@ -23,5 +26,15 @@ public class StringUtils {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INPUT_IS_NOT_INTEGER);
         }
+    }
+
+    public static List<Integer> getIntegerValueList(String[] strings) {
+        List<Integer> list = new ArrayList<>();
+
+        for (String str : strings) {
+            list.add(getIntegerValue(str));
+        }
+
+        return list;
     }
 }

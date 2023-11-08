ce0e23945527128fce621ca958f6cd589266c3d5
hsyoodev <hsyoodev@gmail.com>
2023-11-08 16:03:12 +0900

    refactor(lotto): 로또 번호는 중복되지 않는 6개의 숫자를 가진다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,9 +5,9 @@ import java.util.Set;
 
 public class Lotto {
 
-    private static final int NUMBERS_SIZE = 6;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+    private static final int SIZE = 6;
+    private static final int MIN = 1;
+    private static final int MAX = 45;
 
     private final List<Integer> numbers;
 
@@ -17,8 +17,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (isNotSizeSix(numbers) || isDuplicateNumbers(numbers)) {
-            throw new IllegalArgumentException(Message.INVALID_NUMBERS.getMessage());
+        if (isOutSize(numbers)) {
+            throw new IllegalArgumentException(Message.INVALID_SIZE.getMessage());
+        }
+        if (isNotUnique(numbers)) {
+            throw new IllegalArgumentException(Message.INVALID_UNIQUE.getMessage());
         }
         if (isOutRange(numbers)) {
             throw new IllegalArgumentException(Message.INVALID_RANGE.getMessage());
@@ -26,21 +29,23 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
-    private boolean isNotSizeSix(List<Integer> numbers) {
-        return numbers.size() != NUMBERS_SIZE;
+    private boolean isOutSize(List<Integer> numbers) {
+        return numbers.size() != SIZE;
     }
 
     private boolean isOutRange(List<Integer> numbers) {
-        return numbers.stream().anyMatch((number) -> (number < MIN_NUMBER || number > MAX_NUMBER));
+        return numbers.stream().anyMatch((number) -> (number < MIN || number > MAX));
     }
 
-    private boolean isDuplicateNumbers(List<Integer> numbers) {
-        return Set.copyOf(numbers).size() != NUMBERS_SIZE;
+    private boolean isNotUnique(List<Integer> numbers) {
+        return Set.copyOf(numbers).size() != SIZE;
     }
 
     enum Message {
-        INVALID_NUMBERS("로또는 중복되지 않는 6개의 숫자만 가능합니다."),
-        INVALID_RANGE("로또 번호의 숫자 범위는 1~45만 가능합니다.");
+        INVALID_SIZE("로또 번호는 6개의 숫자만 가능합니다."),
+        INVALID_UNIQUE("로또 번호는 중복되지 않는 숫자만 가능합니다."),
+        INVALID_RANGE("로또 번호의 숫자 범위는 1~45까지만 가능합니다.");
+
         private final String message;
 
         Message(String message) {

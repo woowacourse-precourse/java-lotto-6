d8ae3c329c0706c0ca74baf3ad83370945627a0e
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:53:41 +0900

    feat : getLotto 함수 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -18,5 +18,8 @@ public class WinningLotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
     }
-    
+
+    public Lotto getLotto() {
+        return lotto;
+    }
 }

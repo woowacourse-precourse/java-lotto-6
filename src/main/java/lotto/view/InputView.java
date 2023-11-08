c705416ba97904c912182d2c02cbc8a116bd814d
Subin <rla0010101@gmail.com>
2023-11-08 21:32:23 +0900

    Refactor: 입력 시 Console 라이브러리 이용하여 readLine()으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,28 +1,26 @@
 package lotto.view;
 
-import java.util.Scanner;
-
+import camp.nextstep.edu.missionutils.Console;
 import static lotto.util.Instruction.*;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
     private InputView() {
     }
 
     public static String inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE.getMessage());
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     public static String inputWinningNumbers() {
         System.out.println(INPUT_LOTTO_WINNING_NUMBERS_MESSAGE.getMessage());
-        return scanner.nextLine();
+        return Console.readLine();
     }
 
     public static String inputBonusNumber() {
         System.out.println();
         System.out.println(INPUT_LOTTO_BONUS_NUMBER_MESSAGE.getMessage());
-        return scanner.nextLine();
+        return Console.readLine();
     }
 }

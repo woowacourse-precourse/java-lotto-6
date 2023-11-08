3b2a9b98cfda9f947c23ab60ba12db08b7f630d0
DonghaOh <donghadarwin@naver.com>
2023-11-08 13:44:21 +0900

    feat: 로또 개수 계산
    lottoTicketsCount 메소드

--- src/main/java/model/LottoGenerate.java
+++ src/main/java/model/LottoGenerate.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoGenerate {
+    public static int lottoTicketsCount(int moneyNumber) {
+        return moneyNumber / 1000;
+    }
+
     public static List<Integer> generateLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         return numbers;

6ae8c2c7a6a408bd8faa4271e998f0835323c694
BINBEAN777 <peanutchan@naver.com>
2023-11-08 17:58:55 +0900

    Refactor : 출력 구문 상수화

--- src/main/java/lotto/LottoWinnerNum.java
+++ src/main/java/lotto/LottoWinnerNum.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Set;
 
 public class LottoWinnerNum {
+    private final static String ERROR_SIZE="[ERROR] 6개의 번호를 입력해 주세요.";
+    private final static String ERROR_NUM_LENGTH="[ERROR] 로또 번호는 1부터 45까지의 숫자만 가능합니다.";
+    private final static String ERROR_DUPLICATE="[ERROR] 입력하실 6개의 번호는 서로 중복되면 안됩니다.";
     private final List<Integer> winnerNumList;
     private final int winnerBonusNum;
 
@@ -24,7 +27,7 @@ public class LottoWinnerNum {
 
     private void checkNumSize(List<Integer> list) {
         if (list.size() != 6) {
-            System.out.println("[ERROR] 6개의 번호를 입력해 주세요.");
+            System.out.println(ERROR_SIZE);
             throw new IllegalArgumentException();
         }
     }
@@ -33,7 +36,7 @@ public class LottoWinnerNum {
         for (int i = 0; i < list.size(); i++) {
             int number = list.get(i);
             if (number < 1 || number > 45) {
-                System.out.println("[ERROR] 로또 번호는 1부터 45까지의 숫자만 가능합니다.");
+                System.out.println(ERROR_NUM_LENGTH);
                 throw new IllegalArgumentException();
             }
         }
@@ -42,7 +45,7 @@ public class LottoWinnerNum {
     private void checkNumDuplicate(List<Integer> list) {
         Set<Integer> checkList = new HashSet<>(list);
         if (checkList.size() != 6) {
-            System.out.println("[ERROR] 입력하실 6개의 번호는 서로 중복되면 안됩니다.");
+            System.out.println(ERROR_DUPLICATE);
             throw new IllegalArgumentException();
         }
     }

d51bd4a6e76289e1af51d0b80aed3b752a8860b3
HaLoomin <103578343+HaLoomin@users.noreply.github.com>
2023-11-08 17:33:46 +0900

    fix: Output 관련 수정

--- src/main/java/lotto/string/OutputMessage.java
+++ src/main/java/lotto/string/OutputMessage.java
@@ -7,12 +7,15 @@ public enum OutputMessage {
     REQUEST_BONUSNUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
     WININGSTATISTIC_START_MESSAGE("당첨 통계"),
     DIVISIONLINE_MESSAGE("---"),
-    STATISTIC_FRONT_MESSAGE("개 일치 ("),
-    STATISTIC_MIDDLE_MESSAGE("원) - "),
-    STATISTIC_LAST_MESSAGE("개"),
-    STATISTIC_RESULT_FRONT_MESSAGE("총 수익률은"),
-    STATISTIC_RESULT_LAST_MESSAGE("%입니다.");
-
+    RANK5_COUNT_MESSAGE("3개 일치 (5,000원) - "),
+    RANK4_COUNT_MESSAGE("4개 일치 (50,000원) - "),
+    RANK3_COUNT_MESSAGE("5개 일치 (1,500,000원) - "),
+    RANK2_COUNT_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    RANK1_COUNT_MESSAGE("6개 일치 (2,000,000,000원) - "),
+    RANK_COUNT_UNIT_MESSAGE("개"),
+    STATISTIC_RESULT_FRONT_MESSAGE("총 수익률은 "),
+    STATISTIC_RESULT_LAST_MESSAGE("%입니다."),
+    NEWLINE_MESSAGE("\n");
     private String message;
 
     OutputMessage(String message) {
--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,7 +1,16 @@
 package lotto.view;
 
+import static lotto.config.WiningRank.RANK1;
+import static lotto.config.WiningRank.RANK2;
+import static lotto.config.WiningRank.RANK3;
+import static lotto.config.WiningRank.RANK4;
+import static lotto.config.WiningRank.RANK5;
+import static lotto.string.OutputMessage.NEWLINE_MESSAGE;
+
 import java.util.List;
+import lotto.config.WiningRank;
 import lotto.domain.Lotto;
+import lotto.domain.RankBoard;
 import lotto.string.OutputMessage;
 
 public class Output {
@@ -10,6 +19,7 @@ public class Output {
     }
 
     public void printPurchaseCount(int purchaseCount) {
+        System.out.print(NEWLINE_MESSAGE.getMessage());
         System.out.print(purchaseCount);
         System.out.println(OutputMessage.PURCHASE_COUNT_MESSAGE.getMessage());
     }
@@ -21,37 +31,53 @@ public class Output {
     }
 
     public void requestWiningNumber() {
+        System.out.print(NEWLINE_MESSAGE.getMessage());
         System.out.println(OutputMessage.REQUEST_WININGNUMBER_MESSAGE.getMessage());
     }
 
     public void requestBonusNumber() {
+        System.out.print(NEWLINE_MESSAGE.getMessage());
         System.out.println(OutputMessage.REQUEST_BONUSNUMBER_MESSAGE.getMessage());
     }
 
-    public void printResultLotto() {
-        //추가 구현 필요
-        float num = 0;
-        printWiningStatisticsStart();
-        System.out.println(OutputMessage.DIVISIONLINE_MESSAGE.getMessage());
-        printWiningCount();
-        printReturnRate(num);
-
-    }
-
     public void printWiningStatisticsStart() {
+        System.out.print(NEWLINE_MESSAGE.getMessage());
         System.out.println(OutputMessage.WININGSTATISTIC_START_MESSAGE.getMessage());
+        System.out.println(OutputMessage.DIVISIONLINE_MESSAGE.getMessage());
     }
 
-    public void printWiningCount() {
-        //추가 구현 필요
-        System.out.print(OutputMessage.STATISTIC_FRONT_MESSAGE.getMessage());
-        System.out.print(OutputMessage.STATISTIC_MIDDLE_MESSAGE.getMessage());
-        System.out.print(OutputMessage.STATISTIC_LAST_MESSAGE.getMessage());
+    public void printWiningCount(WiningRank rank, RankBoard board) {
+        if(rank == RANK1) {
+            System.out.print(OutputMessage.RANK1_COUNT_MESSAGE.getMessage());
+            System.out.print(board.getRankCount(rank));
+            System.out.println(OutputMessage.RANK_COUNT_UNIT_MESSAGE.getMessage());
+        }
+        if(rank == RANK2) {
+            System.out.print(OutputMessage.RANK2_COUNT_MESSAGE.getMessage());
+            System.out.print(board.getRankCount(rank));
+            System.out.println(OutputMessage.RANK_COUNT_UNIT_MESSAGE.getMessage());
+        }
+        if(rank == RANK3) {
+            System.out.print(OutputMessage.RANK3_COUNT_MESSAGE.getMessage());
+            System.out.print(board.getRankCount(rank));
+            System.out.println(OutputMessage.RANK_COUNT_UNIT_MESSAGE.getMessage());
+        }
+        if(rank == RANK4) {
+            System.out.print(OutputMessage.RANK4_COUNT_MESSAGE.getMessage());
+            System.out.print(board.getRankCount(rank));
+            System.out.println(OutputMessage.RANK_COUNT_UNIT_MESSAGE.getMessage());
+        }
+        if(rank == RANK5) {
+            System.out.print(OutputMessage.RANK5_COUNT_MESSAGE.getMessage());
+            System.out.print(board.getRankCount(rank));
+            System.out.println(OutputMessage.RANK_COUNT_UNIT_MESSAGE.getMessage());
+        }
     }
 
-    public void printReturnRate(float returnRate) {
+    public void printReturnRate(double returnRate) {
         System.out.print(OutputMessage.STATISTIC_RESULT_FRONT_MESSAGE.getMessage());
-        System.out.print(returnRate);
+        String returnRateString = String.format("%1f", returnRate);
+        System.out.print(returnRateString);
         System.out.println(OutputMessage.STATISTIC_RESULT_LAST_MESSAGE.getMessage());
 
     }

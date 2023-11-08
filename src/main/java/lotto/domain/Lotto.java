422acf40eb6f67f47bdc36c69cef737637b9eab2
suhyun__ <thebbbb1206@gmail.com>
2023-11-08 16:38:42 +0900

    feat: WinningNumbersValidator를 이용해서 로또 번호 검증 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validator.WinningNumbersValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        WinningNumbersValidator.validateDuplicateNumber(numbers);
+        WinningNumbersValidator.validateMinNumber(numbers);
+        WinningNumbersValidator.validateMaxNumber(numbers);
         this.numbers = numbers;
     }
 

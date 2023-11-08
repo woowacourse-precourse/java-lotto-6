c5e8c5c6f875ededfbb7ed43f8e397bb3d66e34d
Jihyun3478 <leejihyun3478@gmail.com>
2023-11-08 16:35:46 +0900

    feat: 메인 함수 로직 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.GameManageController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameManageController game = new GameManageController();
+        game.start();
     }
 }

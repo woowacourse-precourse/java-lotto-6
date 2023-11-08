f6fa9d82dbca8627bb625b83354bb0d2863929d4
youjae <youjae0803@gmail.com>
2023-11-08 20:43:20 +0900

    test: lotto test

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -1,8 +1,8 @@
 package lotto;
 
 import lotto.application.LottoService;
-import lotto.domain.Lotto;
-import lotto.domain.LottoGenerator;
+import lotto.application.UserService;
+import lotto.domain.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -51,4 +51,79 @@ class LottoServiceTest {
         assertThat(lottos.size()).isEqualTo(cnt);
     }
 
+    @Test
+    void 로또_결과_테스트_1개() {
+        LottoService lottoService = new LottoService();
+
+        UserService userService = lottoService.getUserService();
+        userService.updateUser(List.of(new Lotto(List.of(1,2,3,4,5,6))), 1);
+
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        winningNumber.setBonusNumber(7);
+        lottoService.setWinningNumber(winningNumber);
+
+        lottoService.checkLottos();
+
+        User user = userService.getUser();
+
+        lottoService.showResult();
+        assertThat(user.getLottoCnt()).isEqualTo(1);
+        assertThat(user.getCntRank().get(Rank.FirstRank)).isEqualTo(1);
+        assertThat(user.getCntRank().get(Rank.SecondRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ThirdRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ForthRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.FifthRank)).isEqualTo(0);
+
+        assertThat(user.getTotalPrize()).isEqualTo(Rank.FirstRank.getPrize());
+    }
+
+    @Test
+    void 로또_결과_테스트_2개() {
+        LottoService lottoService = new LottoService();
+
+        UserService userService = lottoService.getUserService();
+        userService.updateUser(List.of(new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(1,2,3,4,5,6))), 2);
+
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        winningNumber.setBonusNumber(7);
+        lottoService.setWinningNumber(winningNumber);
+
+        lottoService.checkLottos();
+
+        User user = userService.getUser();
+
+        assertThat(user.getLottoCnt()).isEqualTo(2);
+        assertThat(user.getCntRank().get(Rank.FirstRank)).isEqualTo(2);
+        assertThat(user.getCntRank().get(Rank.SecondRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ThirdRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ForthRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.FifthRank)).isEqualTo(0);
+
+        assertThat(user.getTotalPrize()).isEqualTo(2 * Rank.FirstRank.getPrize());
+    }
+
+    @Test
+    void 로또_결과_테스트_3개() {
+        LottoService lottoService = new LottoService();
+
+        UserService userService = lottoService.getUserService();
+        userService.updateUser(List.of(new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(1,2,3,4,5,6)), new Lotto(List.of(45,7,3,4,5,6))), 3);
+
+        WinningNumber winningNumber = new WinningNumber(List.of(1, 2, 3, 4, 5, 6));
+        winningNumber.setBonusNumber(7);
+        lottoService.setWinningNumber(winningNumber);
+
+        lottoService.checkLottos();
+
+        User user = userService.getUser();
+
+        assertThat(user.getLottoCnt()).isEqualTo(3);
+        assertThat(user.getCntRank().get(Rank.FirstRank)).isEqualTo(2);
+        assertThat(user.getCntRank().get(Rank.SecondRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ThirdRank)).isEqualTo(0);
+        assertThat(user.getCntRank().get(Rank.ForthRank)).isEqualTo(1);
+        assertThat(user.getCntRank().get(Rank.FifthRank)).isEqualTo(0);
+
+        assertThat(user.getTotalPrize()).isEqualTo(2 * Rank.FirstRank.getPrize() + Rank.ForthRank.getPrize());
+    }
 }

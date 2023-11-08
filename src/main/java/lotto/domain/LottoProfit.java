a67aa5e82a0ba897244297d0ebfe64bf0621e054
SeongJunChun <csj991210@naver.com>
2023-11-08 09:14:08 +0900

    FEAT: 수익률 계산하는 기능추가

---
+++ src/main/java/lotto/domain/LottoProfit.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import java.util.Arrays;
+
+public class LottoProfit {
+
+    public double calculateProfitRate(LottoResult lottoResult, int purchaseAmount){
+        int profit = calculateProfit(lottoResult);
+        return  (double) profit/ (double) purchaseAmount *100;
+    }
+    public int calculateProfit(LottoResult lottoResult){
+        return Arrays.stream(LottoRank.values())
+                .mapToInt(lottoRank -> lottoRank.getPrize() * lottoResult.getCount(lottoRank))
+                .sum();
+    }
+
+}

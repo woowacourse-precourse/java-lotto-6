622f6bd5db2cb14468e6f12df17670d22e2d5f89
godee95 <50854903+godee95@users.noreply.github.com>
2023-11-08 17:35:50 +0900

    feat : 사용자 입력 기능 구현

--- src/main/java/lotto/InputManager.java
+++ src/main/java/lotto/InputManager.java
@@ -12,6 +12,7 @@ public class InputManager {
     private static final int MAX_NUMBER = 45;
 
     public int getPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
         int purchaseAmount = Integer.parseInt(Console.readLine());
         if (purchaseAmount % LOTTO_PRICE != 0) {
             System.out.println("[ERROR] 구입 금액은 1,000원 단위로 입력되어야 합니다.");
@@ -21,6 +22,7 @@ public class InputManager {
     }
 
     public List<Integer> getWinnigNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> winningNumbers = Arrays.stream(Console.readLine().split(","))
                                             .map(Integer::parseInt)
                                             .collect(Collectors.toList());
@@ -34,6 +36,7 @@ public class InputManager {
     }
 
     public int getBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
         if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
             System.out.println("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");

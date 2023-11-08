c26e4ebb0eb0d803158ed94c416f40c73b1474a3
박승민 <tmddudals369@naver.com>
2023-11-08 15:08:15 +0900

    feat(rank): 당첨 기준과 상금을 관리할 상수 구현

---
+++ src/main/java/lotto/utils/Rank.java
@@ -0,0 +1,20 @@
+package lotto.utils;
+
+import java.util.Arrays;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    NONE(0, 0);
+
+    private final int matchCount;
+    private final int prize;
+
+    Rank(int matchCount, int prize) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+    }
+}

482ea4b5813762353b7695daf2044fe7d115b782
bangjaeyoung <qkdwo4613@gmail.com>
2023-11-08 17:22:58 +0900

    refactor(print-statistics): 당첨 통계 출력 기능 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,16 +3,15 @@ package lotto.view;
 import lotto.domain.Lotto;
 import lotto.domain.Ranking;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class OutputView {
     private static final String PURCHASE_TICKET_COUNT_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STASTISTICS_MESSAGE = "당첨 통계";
     private static final String LINE = "---";
-    private static final String LINE_SEPARATOR = System.lineSeparator();
     
     public void printErrorMessage(Exception e) {
         System.out.println(e.getMessage());
@@ -35,16 +34,16 @@ public class OutputView {
         return ticket.getNumbers().stream().sorted().collect(Collectors.toCollection(LinkedList::new));
     }
     
-    public void printWinningstatistics() {
+    public void printWinningstatistics(Map<Ranking, Integer> winningResult) {
         System.out.println(WINNING_STASTISTICS_MESSAGE);
         System.out.println(LINE);
-        System.out.println(makeWinningStatisticsText());
+        System.out.println(makeWinningStatisticsText(winningResult));
     }
     
-    private String makeWinningStatisticsText() {
-        return Arrays.stream(Ranking.values())
-                .map(ranking -> String.format("%s - %d개", ranking.getMessage(), ranking.getCount()))
-                .collect(Collectors.joining(LINE_SEPARATOR));
+    private String makeWinningStatisticsText(Map<Ranking, Integer> winningResult) {
+        return winningResult.entrySet().stream()
+                .map(entry -> String.format("%s - %d개", entry.getKey().getMessage(), entry.getValue()))
+                .collect(Collectors.joining("\n"));
     }
     
     public void printNewLine() {

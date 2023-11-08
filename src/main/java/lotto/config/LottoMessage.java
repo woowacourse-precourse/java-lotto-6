90d68fd2377fbfab5faee90bb9661c78636c2629
gywns0417 <gywns0417@gmail.com>
2023-11-08 22:51:48 +0900

    refactor: 로또 게임 안내 메시지에 공백 추가

--- src/main/java/lotto/config/LottoMessage.java
+++ src/main/java/lotto/config/LottoMessage.java
@@ -2,10 +2,10 @@ package lotto.config;
 
 public enum LottoMessage {
     PURCHASE_AMOUNT_REQUEST_MESSAGE("구입금액을 입력해 주세요."),
-    PURCHASED_AMOUNT_RESULT_MESSAGE("%d개를 구매했습니다.\n"),
-    WINNING_NUMBER_REQUEST_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_REQUEST_MESSAGE("보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS("당첨 통계\n"
+    PURCHASED_AMOUNT_RESULT_MESSAGE("\n%d개를 구매했습니다.\n"),
+    WINNING_NUMBER_REQUEST_MESSAGE("\n당첨 번호를 입력해 주세요."),
+    BONUS_NUMBER_REQUEST_MESSAGE("\n보너스 번호를 입력해 주세요."),
+    WINNING_STATISTICS("\n당첨 통계\n"
             + "---\n"
             + "3개 일치 (5,000원) - %d개\n"
             + "4개 일치 (50,000원) - %d개\n"

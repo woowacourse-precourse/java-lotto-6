4d630e3fbf24ea8b96fb1d58ffdc2148cb4b3215
minju <minju9711@naver.com>
2023-11-08 16:56:09 +0900

    [feat] 계산된 랭킹을 출력해주는 함수 추가

--- src/main/java/lotto/IO/IO.java
+++ src/main/java/lotto/IO/IO.java
@@ -30,6 +30,21 @@ public class IO { //여긴 무조건 정적호출로 해보자
         return Console.readLine();
     }
 
+    public static void printRanking(LottoRank[] ranks) {
+        Arrays.sort(ranks, Comparator.comparingLong(LottoRank::getPrizeMoney));  // 당첨금액을 기준으로 오름차순 정렬
+        NumberFormat formatter = NumberFormat.getInstance();
+        for (LottoRank rank : ranks) {
+            Map<LottoRank, Integer> ranking = rank.getRanking();
+            String bonusBall = Optional.of(rank)
+                    .filter(LottoRank::isBonusBallMatch)
+                    .map(r -> ", 보너스 볼 일치")
+                    .orElse("");
+            String prizeMoney = formatter.format(rank.getPrizeMoney());
+            String rankDescription = rank.getMatchCount() + "개 일치" + bonusBall + " (" + prizeMoney + "원) - ";
+            System.out.println(rankDescription + ranking.getOrDefault(rank, 0) + "개");
+        }
+    }
+
     public static void PrintBuyLotto(LottoChecker lottoChecker){
         System.out.println(lottoChecker.toString());
     }

51018808b1c6e3856a00b69fe5b4a2fd2bd619e0
coh <och5405@naver.com>
2023-11-08 21:46:33 +0900

    (fix:lotto)에러 해결

--- src/main/java/lotto/Judgement.java
+++ src/main/java/lotto/Judgement.java
@@ -3,16 +3,6 @@ package lotto;
 import java.util.List;
 
 public class Judgement {
-	public int compareNumbers(List<Integer> correct, List<Integer> lotto) {
-		int result = 0;
-		for (int i = 0; i < correct.size(); i++) {
-			if (correct.contains(lotto.get(i))) {
-				result++;
-			}
-		}
-		return result;
-	}
-
 	public boolean isInRange(int number) {
 		return (Number.MIN_NUMBER.getNumber() <= number && number <= Number.MAX_NUMBER.getNumber());
 	}
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,10 +1,10 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
-    boolean isBonus = false;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,6 +14,13 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != Number.LOTTO_SIZE.getNumber()) {
             throw new IllegalArgumentException();
+        }
+        List<Integer> temp = new ArrayList<>(numbers);
+        for (int number : numbers) {
+            temp.remove((Integer) number);
+            if (temp.contains(number)) {
+                throw new IllegalArgumentException();
+            }
         }
     }
     // TODO: 추가 기능 구현

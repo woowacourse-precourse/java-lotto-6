beabec945805c73aca668515f67f58e23fe532e9
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 17:17:38 +0900

    feat(RandomUtil) : add verification() method

--- src/main/java/lotto/domain/PurchaseAmount.java
+++ src/main/java/lotto/domain/PurchaseAmount.java
@@ -5,7 +5,7 @@ public class PurchaseAmount {
     private final int amount;
 
     public PurchaseAmount(String amount) {
-//        verification(amount);
+        verification(amount);
         this.amount = Integer.parseInt(amount);
     }
 
@@ -15,5 +15,29 @@ public class PurchaseAmount {
 
     public int getLottoCount() {
         return amount / LOTTO_PRICE;
+    }
+
+    private void verification(String amount) {
+        isNullPurchaseAmount(amount);
+        isPurchaseAmount(amount);
+        isValidPurchaseAmount(amount);
+    }
+
+    private void isNullPurchaseAmount(String amount) {
+        if (amount.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 공백일 수 없습니다.\n");
+        }
+    }
+
+    private void isPurchaseAmount(String amount) {
+        if (amount.matches(".*[^0-9].*")) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자만 입력할 수 있습니다.\n");
+        }
+    }
+
+    private void isValidPurchaseAmount(String amount) {
+        if (Integer.parseInt(amount) % LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 천원 단위로 입력할 수 있습니다.\n");
+        }
     }
 }

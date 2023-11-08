a279abac80e727d8ef4d9b08943bae9f8a1c0f47
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-08 14:41:42 +0900

    feat(Lotto): 일치한 로또를 확인하는 기능 구현

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,10 +1,19 @@
 package lotto;
 
 import java.io.PrintStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import lib.constant.LottoConstants;
+import lib.enumeration.LottoWinnerRule;
 
 public class LottoGame {
     PrintStream printStream = System.out;
@@ -35,5 +44,33 @@ public class LottoGame {
         for (int i = 0; i < count; ++i) {
             this.lottos.add(this.createLotto());
         }
+    }
+
+    private void resultPut(Map<Integer, List<List<Integer>>> map, int key, List<Integer> value) {
+        if (map.containsKey(key)) {
+            map.get(key).add(value);
+            return;
+        }
+
+        List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+        results.add(value);
+        map.put(key, results);
+    }
+
+    public Map<Integer, List<List<Integer>>> getMatchedLottosResult() {
+        Map<Integer, List<List<Integer>>> matchedLottosResult = new HashMap<Integer, List<List<Integer>>>();
+
+        this.lottos.forEach(lotto -> {
+            List<Integer> matchedLotto = lotto.getMatchedLotto(this.winnerNumbers.getNumbers(), this.bonusWinnerNumber);
+
+            if (matchedLotto.size() == 6 && matchedLotto.contains(this.bonusWinnerNumber)) { this.resultPut(matchedLottosResult, LottoWinnerRule.SECOND.index(), matchedLotto); return; }
+            if (matchedLotto.size() == 6) { this.resultPut(matchedLottosResult, LottoWinnerRule.FIRST.index(), matchedLotto); return; }
+            if (matchedLotto.size() == 5) { this.resultPut(matchedLottosResult, LottoWinnerRule.THIRD.index(), matchedLotto); return; }
+            if (matchedLotto.size() == 4) { this.resultPut(matchedLottosResult, LottoWinnerRule.FOURTH.index(), matchedLotto); return; }
+            if (matchedLotto.size() == 3) { this.resultPut(matchedLottosResult, LottoWinnerRule.FIFTH.index(), matchedLotto); return; }
+        });
+
+        return matchedLottosResult;
     }
 }

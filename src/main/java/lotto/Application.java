9123d068129ddc1b6f5bb6de34ca79af0be42336
Hanbee Lee <bee1162@naver.com>
2023-11-08 12:41:20 +0900

    fix : application실행 되게 바꾸기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.controller.LottoStore;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoStore lottoStore = new LottoStore();
+        lottoStore.lottoGameStart();
     }
 }

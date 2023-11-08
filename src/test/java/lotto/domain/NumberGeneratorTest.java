61ff01dd4eeda26634bd7ea8614252c415d75ab7
wonchanjoo <wcj0911@naver.com>
2023-11-08 14:28:04 +0900

    test: 로또 숫자 생성 테스트 생성

---
+++ src/test/java/lotto/domain/NumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberGeneratorTest {
+
+    @Test
+    @DisplayName("중복되지 않는 6개의 숫자를 포함한 리스트를 생성한다.")
+    void createNumbers() {
+        List<Integer> numbers = NumberGenerator.generateNumbers();
+
+        assertThat(numbers.size()).isEqualTo(LottoConfiguration.NUMBERS_SIZE);
+        assertThat(containsNoDuplicates(numbers)).isTrue();
+    }
+
+    private boolean containsNoDuplicates(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == numbers.size();
+    }
+}

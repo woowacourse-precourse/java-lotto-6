4025e661611b1a2bf7e957b033a41c8c9102cb5f
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-08 12:27:45 +0900

    Style: Modify error message text

--- src/main/java/lotto/Bonus.java
+++ src/main/java/lotto/Bonus.java
@@ -18,11 +18,11 @@ public class Bonus {
         List<Integer> lottoNumbers = lotto.getNumbers();
 
         if (!(number >= 1 && number <= 45)) {
-            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다."); // 보너스 번호는 1부터 45 사이의 숫자여야 합니다.
+            throw new IllegalArgumentException("보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
 
         if (lottoNumbers.contains(number)) {
-            throw new IllegalArgumentException("보너스 번호는 중복이 아니여야 합니다."); // 보너스 번호는 중복이 아니여야 합니다.
+            throw new IllegalArgumentException("보너스 번호는 로또 번호와 중복이 아니여야 합니다.");
         }
     }
 }

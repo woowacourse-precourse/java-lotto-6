0a92f81dd378b130c95a18faf9dd71a0b5ce022a
witwint <witwint1234@gmail.com>
2023-11-08 17:26:38 +0900

    feat: 친근한 버전의 view 제작후 DI 확인

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,14 +8,21 @@ import lotto.start.LottoGame;
 import lotto.view.Exception;
 import lotto.view.ExceptionView;
 import lotto.view.LottoView;
+import lotto.view.LottoViewV2;
 import lotto.view.Output;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         Output view = new LottoView();
         Exception exception = new ExceptionView();
         Calculation lottoService = new LottoService();
+
+        // DI를 적용해서 친근한 버전의 view를 만들어 생성자의 매개변수만 바꾸면 적용되는 코드를 제작했습니다.
+//        Output viewFriendly = new LottoViewV2();
+//        Input controller = new LottoController(viewFriendly,exception,lottoService);
+
         Input controller = new LottoController(view,exception,lottoService);
         LottoGame lottoGame = new LottoGame(controller);
 

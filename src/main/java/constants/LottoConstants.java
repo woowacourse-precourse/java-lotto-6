5c86e6d7d5628baf9d09472a53e3d48de289e6b5
pjaehyun <show6756@gmail.com>
2023-11-08 15:05:39 +0900

    feat: 추가 등수별 메세지 상수

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -21,4 +21,14 @@ public class LottoConstants {
     public static final int LOTTO_RANK_COUNT = 5;
 
     public static final int LOTTO_PRICE = 1000;
+
+    public static final String LOTTO_WINNING_FIRST_MESSAGE = "6개 일치 (2,000,000,000원)";
+
+    public static final String LOTTO_WINNING_SECOND_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원)";
+
+    public static final String LOTTO_WINNING_THIRD_MESSAGE = "5개 일치 (1,500,000원)";
+
+    public static final String LOTTO_WINNING_FOURTH_MESSAGE = "4개 일치 (50,000원)";
+
+    public static final String LOTTO_WINNING_FIFTH_MESSAGE = "3개 일치 (5,000원)";
 }

31e8da8c9f7fc81d7b0a764f085cd1ea9eb270b8
sudhdkso <waga1009@gmail.com>
2023-11-08 22:24:01 +0900

    refactor: 예외클래스 메세지 상수명 변경

--- src/main/java/lotto/util/exception/InvalidLottoSizeException.java
+++ src/main/java/lotto/util/exception/InvalidLottoSizeException.java
@@ -3,7 +3,7 @@ package lotto.util.exception;
 import static lotto.domain.enums.LottoConstants.LOTTO_SIZE;
 
 public class InvalidLottoSizeException extends IllegalArgumentException {
-    private static final String INVALID_LOTTO_SIZE = "[ERROR] 로또의 숫자가 " + LOTTO_SIZE + "개 여야합니다.";
+    private static final String INVALID_LOTTO_SIZE = String.format("[ERROR] 로또의 숫자가 %d개 여야합니다.", LOTTO_SIZE);
 
     public InvalidLottoSizeException() {
         super(INVALID_LOTTO_SIZE);

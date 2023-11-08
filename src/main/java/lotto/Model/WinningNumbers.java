3421799f3981e3e4975021b0c8262c51d3a0e271
myoungjin-kim <kaudwls58@naver.com>
2023-11-08 22:11:26 +0900

    feat: WinningNumbers 클래스 추가
    당첨 로또와 보너스 번로를 이용해 객체를 생성합니다.
    당첨 로또를 반환합니다.
    보너스 로또를 반환합니다.

---
+++ src/main/java/lotto/Model/WinningNumbers.java
@@ -0,0 +1,19 @@
+package lotto.Model;
+
+public class WinningNumbers {
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningNumbers(Lotto winningLotto, int bonusNumber) {
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public Lotto getWinningLotto() {
+        return winningLotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}

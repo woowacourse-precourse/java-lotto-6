f25118be634d904ac92f2833fc8158b7ea50e9e6
qocksdlf123 <qocksdlf123@naver.com>
2023-11-08 21:49:07 +0900

    Fix(ExceptionMsg): 에외 메시지에 [ERROR] 추가

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,4 +1,13 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoGameController {
+    private static final int LOTTO_PRICE = 1000;
 }
--- src/main/java/lotto/exception/ExceptionMsg.java
+++ src/main/java/lotto/exception/ExceptionMsg.java
@@ -1,11 +1,11 @@
 package lotto.exception;
 
 public enum ExceptionMsg {
-    NULL_INPUT("아무런 입력이 없습니다.\n"),
-    NOT_INT_INPUT("입력이 숫자가 아닙니다.\n"),
-    NOT_MULTIPLE_THOUSAND("구입금액이 1000의 배수가 아닙니다\n"),
-    DUPLICATE_LOTTO_NUM("로또 번호가 중복될 수 없습니다.\n"),
-    NOT_SIX_INPUT_NUMBERS("당첨 번호가 6개가 아닙니다");
+    NULL_INPUT("[ERROR] 아무런 입력이 없습니다.\n"),
+    NOT_INT_INPUT("[ERROR] 입력이 숫자가 아닙니다.\n"),
+    NOT_MULTIPLE_THOUSAND("[ERROR] 구입금액이 1000의 배수가 아닙니다\n"),
+    DUPLICATE_LOTTO_NUM("[ERROR] 로또 번호가 중복될 수 없습니다.\n"),
+    NOT_SIX_INPUT_NUMBERS("[ERROR] 당첨 번호가 6개가 아닙니다");
 
     private final String message;
 

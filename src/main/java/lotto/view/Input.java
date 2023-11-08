b9fb844f488ab329c37bb84ee0641902c262740d
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 22:44:29 +0900

    Fix : 출력 포맷 수정

--- src/main/java/lotto/view/Input.java
+++ src/main/java/lotto/view/Input.java
@@ -5,14 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     public String buyAmount(){
         System.out.println("구입금액을 입력해 주세요.");
+        System.out.println();
         return Console.readLine();
     }
     public String pickSixNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println();
         return Console.readLine();
     }
     public String  pickBonusNumber(){
         System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println();
         return Console.readLine();
     }
 

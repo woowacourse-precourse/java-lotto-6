64eb80266eecee9b3c432fbc3dfb2588ceda663a
jookbooin <3670lsh@naver.com>
2023-11-08 16:05:23 +0900

    refactor : LottoMessage 출력 형태 수정

--- src/main/java/lotto/message/GameMessage.java
+++ src/main/java/lotto/message/GameMessage.java
@@ -5,8 +5,8 @@ public enum GameMessage implements Message {
     ASK_FOR_PURCHASE_COST("구입금액을 입력해 주세요."),
     ASK_FOR_WINNING_NUMBER("당첨 번호를 입력해 주세요."),
     ASK_FOR_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
-    WINNING_STATISTICS("당첨 통계\n---\n"),
-    ;
+    WINNING_STATISTICS("당첨 통계\n---");
+
     private final String message;
 
     GameMessage(String message) {
--- src/main/java/lotto/message/LottoMessage.java
+++ src/main/java/lotto/message/LottoMessage.java
@@ -4,11 +4,9 @@ public enum LottoMessage implements Message {
 
     LOTTO_COUNT("%d개를 구매했습니다.\n"),
     DISPLAY_LOTTO_NUMBER_FORMAT("[%s]"),
-    THREE_MATCH("3개 일치 (5,000원) - %d개"),
-    FOUR_MATCH("4개 일치 (50,000원) - %d개"),
-    FIVE_MATCH("5개 일치 (1,500,000원) - %d개"),
-    FIVE_BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
-    SIX_MATCH("6개 일치 (2,000,000,000원) - %d개");
+    NOT_EXIST_BONUS_MESSEAGE("%d개 일치 (%s원) - %d개"),
+    EXIST_BONUS_MESSAGE("%d개 일치, 보너스 볼 일치 (%s원) - %d개"),
+    EARNING_RESULT_FORMAT("총 수익률은 %.1f%%입니다.");
 
     private final String message;
 

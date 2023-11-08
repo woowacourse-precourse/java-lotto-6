f064b0d2ef2055e692ea8a137692c98542dc9887
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 21:12:12 +0900

    Feat : service에 정의된 함수들 호출하는 객체 생성

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.controller;
+
+import lotto.model.service.LottoService;
+
+public class LottoController {
+    private final LottoService lottoService;
+    public LottoController(LottoService lottoService){
+        this.lottoService = lottoService;
+    }
+    public void start(){
+        lottoService.startLotto();
+        lottoService.drawNumber();
+        lottoService.calculateProfit();
+        lottoService.resultOutput();
+    }
+}

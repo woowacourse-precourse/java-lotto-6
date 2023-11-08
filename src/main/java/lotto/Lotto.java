2282c85a281387c8c0af9592241b9370bc216884
suin <parksuuuun@gmail.com>
2023-11-08 19:01:16 +0900

    feat: 사용자로부터 로또 당첨번호 입력 받기 위한 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,46 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+
+        System.out.println("\n당첨번호를 입력해주세요.(쉼표(,)로 구분)");
+
+        Errors errors = new Errors();
+        errors.checkErrorsOfInputLottoNumbers(this.numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    private List<Integer> validateOfLottoNumbers() {
+
+        Errors errors = new Errors();
+
+        boolean validInputLottoNumber = false;
+        while (!validInputLottoNumber) {
+            try {
+                errors.checkErrorsOfInputLottoNumbers(numbers);
+                validInputLottoNumber = true;
+            } catch (NumberFormatException e) {
+                System.out.println("[Error] 숫자만 입력 가능합니다.");
+                System.out.println("다시 입력해주세요.");
+                numbers.clear();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("다시 입력해주세요.");
+                numbers.clear();
+            }
+
         }
+        numbers = this.numbers;
+        return this.numbers = getNumbers();
     }
 
-    // TODO: 추가 기능 구현
 }

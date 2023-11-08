58e1df3ff625ef8acb495c0d8e0b3ebf6ec23c1e
choiyewon <choiyewon1997@gmail.com>
2023-11-08 10:52:29 +0900

    feat: (v2)class Game 생성

---
+++ src/main/java/lotto/Game.java
@@ -0,0 +1,12 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    int money;
+    List<Lotto> buy = new ArrayList<>();
+    List<Integer> answer = new ArrayList<>();
+}

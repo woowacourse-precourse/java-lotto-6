e03ec216e189fc1b8416ac5473c3870491f601b9
takoyakimchi <c4nyou@gmail.com>
2023-11-08 21:01:11 +0900

    refactor(const): 요구사항에 맞게 개행 수정, 보너스 볼 반영

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -4,10 +4,11 @@ public enum GameMessage {
 
     INPUT_BUY_PRICE("구입금액을 입력해 주세요."),
     YOU_BOUGHT_N_LOTTOS("개를 구매했습니다."),
-    INPUT_WIN_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
-    WIN_STATISTICS("당첨 통계\n---"),
+    INPUT_WIN_NUMBERS("\n당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBERS("\n보너스 번호를 입력해 주세요."),
+    WIN_STATISTICS("\n당첨 통계\n---"),
     MATCH_N_NUMBERS("개 일치"),
+    MATCH_BONUS(", 보너스 볼 일치"),
     N_NUMBERS("개"),
     TOTAL_REVENUE_RATE_IS("총 수익률은 "),
     N_PERCENTS("%입니다.");

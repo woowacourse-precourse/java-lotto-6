68c1e5911b90997118cb46a3dfc977895cf57130
SungHoCha <tjdgh3725@gmail.com>
2023-11-08 17:07:54 +0900

    FIX- "fix LottoRank"
    LottoRank 의 객체들 순서 수정

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -3,9 +3,9 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoRank {
+    LOSE(0, "", 0),
     FIFTH(3, "3개 일치 (5,000원) - ", 5000),
     FOURTH(4, "4개 일치 (50,000원) - ", 50000),
-    LOSE(0, "", 0),
     THIRD(5, "5개 일치 (1,500,000원) - ", 1500000),
     SECOND(5, "5개 일치, 보너스 볼 일치 (30,000,000원) - ", 30000000),
     FIRST(6, "6개 일치 (2,000,000,000원) - ",2000000000);

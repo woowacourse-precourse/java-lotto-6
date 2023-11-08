f574b811f1b33cb57539d2838ad7fcf1eb4afb8c
ay-eonii <naayen@naver.com>
2023-11-08 14:33:04 +0900

    test(buyer): 구매금액이 1000원 단위가 아닌 경우 예외 발생

--- src/test/java/lotto/domain/BuyerTest.java
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -23,6 +23,14 @@ class BuyerTest {
                 .hasMessage("[ERROR] 구매금액은 1,000원 이상이어야 합니다.");
     }
 
+    @DisplayName("구매금액이 1,000원 단위가 아니라면 예외가 발생한다.")
+    @Test
+    void getCostNotPriceUnits() {
+        assertThatThrownBy(() -> new Buyer(1200))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매금액은 1,000원 단위여야 합니다.");
+    }
+
     @Test
     void getBuyCount() {
     }

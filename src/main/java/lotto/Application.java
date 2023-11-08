aa96684484625a795c55ef62d4bd4e69cd3fc83d
Min Seop Kim <k_ms1998@naver.com>
2023-11-08 20:08:26 +0900

    입력 받기 구현
    * InputUtils에서 입력 구현 및 입력에 대한 예외 처리
    * OutputUtils에서 출력 구현
    * MessageType은 상황에 맞는 각 매세지를 담고 있는 ENUM

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,78 @@
 package lotto;
 
+import lotto.enums.MessageType;
+import lotto.utils.InputUtils;
+import lotto.utils.LottoUtils;
+import lotto.utils.OutputUtils;
+
+import java.util.*;
+
 public class Application {
+
+    private static int amount = 0;
+    private static Lotto[] lottos;
+    private static List<Integer> winningNumbers;
+    private static int bonusNumber;
+
+    private static Result result = new Result();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        initGame();
+        startGame();
+        endGame();
     }
+
+    private static void initGame(){
+        OutputUtils.printBasicMessage(MessageType.INPUT_AMOUNT_MESSAGE);
+        try {
+            amount = InputUtils.inputAmount();
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            initGame();
+        }
+
+        int numberOfLottos = amount / 1000;
+        lottos = new Lotto[numberOfLottos];
+
+        lottos = LottoUtils.createLottos(numberOfLottos);
+        OutputUtils.printLottos(lottos, numberOfLottos);
+    }
+
+    private static void startGame(){
+        inputWiningNumbers();
+        inputBonusNumber();
+    }
+
+    private static void endGame(){
+        OutputUtils.printBasicMessage(MessageType.END_MSG);
+
+        for(Lotto lotto : lottos){
+            int currentMatchCount = lotto.compareAndGetCountOfMatchers(winningNumbers);
+            boolean bonusMatch = lotto.checkIfBonusNumberExists(bonusNumber);
+
+            result.updateResult(currentMatchCount, bonusMatch);
+        }
+
+        OutputUtils.printResult(result);
+        OutputUtils.printGain(result, amount);
+    }
+
+    private static void inputWiningNumbers(){
+        try {
+            OutputUtils.printBasicMessage(MessageType.INPUT_WINNING_MSG);
+            winningNumbers = InputUtils.inputWinningNumbers();
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            inputWiningNumbers();
+        }
+    }
+
+    private static void inputBonusNumber(){
+        try{
+            OutputUtils.printBasicMessage(MessageType.INPUT_BONUS_MSG);
+            bonusNumber = InputUtils.inputBonusNumber(winningNumbers);
+        }catch  (IllegalStateException | IllegalArgumentException e) {
+            inputBonusNumber();
+        }
+    }
+
 }

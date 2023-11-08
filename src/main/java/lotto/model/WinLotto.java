6fe79f0fe950ba7f25a4b94f6c567b0335565cee
Ojh <201833022@yiu.ac.kr>
2023-11-08 17:35:42 +0900

    feat: 당첨 번호 오류 메시지 관련 상수

--- src/main/java/lotto/model/WinLotto.java
+++ src/main/java/lotto/model/WinLotto.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class WinLotto {
 
+  private static final String REGEX_ALPHABET_PATTERN = ".*[a-zA-Z].*";
+  private static final String WINNING_NUMBER_FORMAT_ERROR_MESSAGE = "[ERROR] 당첨 번호는 문자를 포함하지 않고 '1,2,3,4,5,6' 형식으로 입력해 주세요.";
   private static final String BONUS_NUMBER_WITH_LETTER_MESSAGE = "[ERROR] 숫자 하나만 입력해 주세요.";
   private static final String INVALID_BONUS_NUMBER_RANGE_MESSAGE = "[ERROR] 입력 가능한 보너스 숫자의 범위는 1~45까지 입니다.";
   private static final String NON_OVERLAPPING_BONUS_NUMBER_MESSAGE = "[ERROR] 당첨 번호와 겹치지 않는 보너스 숫자를 입력해 주세요.";

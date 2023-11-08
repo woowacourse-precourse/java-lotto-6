02df06bfce9f25b24dbb7d48d6f293cc5b08d6bf
eora21 <eora21@naver.com>
2023-11-08 13:18:07 +0900

    test: 💍 수정 로또 결제 객체 사용하도록 변경, 구매 금액 정수로 입력, 반환값 Bill 적용

--- src/test/java/lotto/domain/LottoTicketsPurchasingMachineTest.java
+++ src/test/java/lotto/domain/LottoTicketsPurchasingMachineTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import domain.Amount;
 import domain.LottoTicketsPurchasingMachine;
+import domain.TicketSize;
+import dto.Bill;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,38 +15,37 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoTicketsPurchasingMachineTest {
+    private final LottoTicketsPurchasingMachine lottoTicketsPurchasingMachine = new LottoTicketsPurchasingMachine();
     @ParameterizedTest
     @DisplayName("로또 금액으로 양수가 아닌 수를 지불하면 예외가 발생한다.")
     @ValueSource(ints = {-10_000, -1_000, 0})
     void purchaseOfLottoTicketsNotPositiveAmount(int amount) {
-        LottoTicketsPurchasingMachine lottoTicketsPurchasingMachine = new LottoTicketsPurchasingMachine();
         assertThrows(IllegalArgumentException.class,
-                () -> lottoTicketsPurchasingMachine.purchaseOfLottoTickets(new Amount(amount)));
+                () -> lottoTicketsPurchasingMachine.purchaseOfLottoTickets(amount));
     }
 
     @ParameterizedTest
     @DisplayName("로또 금액으로 구매 금액 단위에 맞지 않는 수를 지불하면 예외가 발생한다.")
     @ValueSource(ints = {1, 11, 111, 1_111, 11_111})
     void purchaseOfLottoTicketsNotCorrectUnitAmount(int amount) {
-        LottoTicketsPurchasingMachine lottoTicketsPurchasingMachine = new LottoTicketsPurchasingMachine();
         assertThrows(IllegalArgumentException.class,
-                () -> lottoTicketsPurchasingMachine.purchaseOfLottoTickets(new Amount(amount)));
+                () -> lottoTicketsPurchasingMachine.purchaseOfLottoTickets(amount));
     }
 
     @ParameterizedTest
     @DisplayName("로또 금액 정상 지불")
     @MethodSource("purchaseAmountAndExpectLottoTicketsNumber")
-    void purchaseOfLottoTicketsSuccessTest(Amount purchaseAmount, int expectLottoTicketsNumber) {
-        LottoTicketsPurchasingMachine lottoTicketsPurchasingMachine = new LottoTicketsPurchasingMachine();
-        assertThat(lottoTicketsPurchasingMachine.purchaseOfLottoTickets(purchaseAmount))
-                .isEqualTo(expectLottoTicketsNumber);
+    void purchaseOfLottoTicketsSuccessTest(int purchaseAmount, int expectLottoTicketsNumber) {
+        Bill bill = lottoTicketsPurchasingMachine.purchaseOfLottoTickets(purchaseAmount);
+        TicketSize ticketSize = bill.getTicketSize();
+        assertThat(ticketSize.getTicketSize()).isEqualTo(expectLottoTicketsNumber);
     }
 
     public static Stream<Arguments> purchaseAmountAndExpectLottoTicketsNumber() {
         return Stream.of(
-                Arguments.of(new Amount(1_000), 1),
-                Arguments.of(new Amount(10_000), 10),
-                Arguments.of(new Amount(100_000), 100)
+                Arguments.of(1_000, 1),
+                Arguments.of(10_000, 10),
+                Arguments.of(100_000, 100)
         );
     }
 

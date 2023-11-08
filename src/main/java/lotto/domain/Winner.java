5607702664b82973bfe705bb12719dfbf59fbe4d
GiJung <weeds2577@gmail.com>
2023-11-08 15:14:54 +0900

    refactor(Winner): WinningCriteria 객체를 활용하는 방법으로 수정

--- src/main/java/lotto/domain/Winner.java
+++ src/main/java/lotto/domain/Winner.java
@@ -3,62 +3,39 @@ package lotto.domain;
 import java.util.HashMap;
 import java.util.Map;
 
+import static lotto.domain.WinningCriteria.FIFTH_PRIZE;
+import static lotto.domain.WinningCriteria.FIRST_PRIZE;
+import static lotto.domain.WinningCriteria.FOURTH_PRIZE;
+import static lotto.domain.WinningCriteria.SECOND_PRIZE;
+import static lotto.domain.WinningCriteria.THIRD_PRIZE;
+
 public class Winner {
-    private static final String FIRST_PLACE = "1stPlace";
-    private static final String SECOND_PLACE = "2ndPlace";
-    private static final String THIRD_PLACE = "3rdPlace";
-    private static final String FOURTH_PLACE = "4thPlace";
-    private static final String FIFTH_PLACE = "5thPlace";
 
-
-    private Map<String, Integer> winners;
+    private Map<Integer, Integer> winners;
 
     public Winner() {
+
         winners = new HashMap<>();
-        winners.put(FIRST_PLACE, 0);
-        winners.put(SECOND_PLACE, 0);
-        winners.put(THIRD_PLACE, 0);
-        winners.put(FOURTH_PLACE, 0);
-        winners.put(FIFTH_PLACE, 0);
+
+        for (WinningCriteria winningCriteria : WinningCriteria.values()) {
+            winners.put(winningCriteria.getRank(), 0);
+        }
     }
 
-    public void increase1stPlace() {
-        winners.put(FIRST_PLACE, winners.get(FIRST_PLACE) + 1);
+    public void increaseCount(int key) {
+        validate(key);
+        winners.put(key, winners.get(key) + 1);
     }
 
-    public void increase2ndPlace() {
-        winners.put(SECOND_PLACE, winners.get(SECOND_PLACE) + 1);
+    public int getWinningCount(int key) {
+        validate(key);
+        return winners.get(key);
     }
 
-    public void increase3rdPlace() {
-        winners.put(THIRD_PLACE, winners.get(THIRD_PLACE) + 1);
+    private void validate(int key) {
+        if(key < FIRST_PRIZE.getRank() || key > FIFTH_PRIZE.getRank()){
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void increase4thPlace() {
-        winners.put(FOURTH_PLACE, winners.get(FOURTH_PLACE) + 1);
-    }
-
-    public void increase5thPlace() {
-        winners.put(FIFTH_PLACE, winners.get(FIFTH_PLACE) + 1);
-    }
-
-    public int get1stPlace() {
-        return winners.get(FIRST_PLACE);
-    }
-
-    public int get2ndPlace() {
-        return winners.get(SECOND_PLACE);
-    }
-
-    public int get3rdPlace() {
-        return winners.get(THIRD_PLACE);
-    }
-
-    public int get4thPlace() {
-        return winners.get(FOURTH_PLACE);
-    }
-
-    public int get5thPlace() {
-        return winners.get(FIFTH_PLACE);
-    }
 }

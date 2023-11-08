9b78289c01177c2bc122f06a1360e129c707ce4c
zion0425 <zion0425@naver.com>
2023-11-08 19:15:33 +0900

    refactor: Enum 및 메소드로 분리

--- src/main/java/lotto/domain/LottoManager.java
+++ src/main/java/lotto/domain/LottoManager.java
@@ -2,9 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class LottoManager {
     private final List<Lotto> lottos;
@@ -36,63 +34,45 @@ public class LottoManager {
         }
     }
 
-    public void calculateMatch(Lotto winningLotto, Bonus bonus) {
-        final String threeMatchMessage = "3개 일치 (5,000원) - ";
-        final Integer threeMatchMoney = 5000;
-        final String fourMatchMessage = "4개 일치 (50,000원) - ";
-        final Integer fourMatchMoney = 50000;
-        final String fiveMatchMessage = "5개 일치 (1,500,000원) - ";
-        final Integer fiveMatchMoney = 1500000;
-        final String fiveBonusMatchMessage = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
-        final Integer fiveBonusMatchMoney = 30000000;
-        final String sixMatchMessage = "6개 일치 (2,000,000,000원) - ";
-        final Integer sixMatchMoney = 2000000000;
-
-        HashMap<Integer, Integer> matchNumbers = new HashMap<>();
-        for (int i = 1; i < 6; i++) {
-            matchNumbers.put(i, 0);
+    private WinningInfo isMatchBonus(Lotto lotto, Bonus bonus) {
+        if (lotto.isContain(bonus.getNumber())) {
+            return WinningInfo.FIVE_BONUS;
         }
-        int count = 0;
-        int money = 0;
+        return WinningInfo.FIVE_BONUS;
+    }
+
+    private void printResult(Map matchNumbers, Float resultNumbr) {
+        for (WinningInfo type : WinningInfo.values()) {
+            System.out.println(type.getMessage() + matchNumbers.getOrDefault(type, 0) + "개");
+        }
+
+        String format = String.format("%.1f", resultNumbr);
+        System.out.print("총 수익률은 " + format + "%입니다.");
+    }
+
+    public void calculateMatch(Lotto winningLotto, Bonus bonus) {
+        Map<WinningInfo, Integer> matchNumbers = new EnumMap<>(WinningInfo.class);
         int sum = 0;
+        int money = 0;
 
         for (Lotto lotto : lottos) {
             money += 1000;
-            for (int i = 0; i < 6; i++) {
-                count = lotto.compareLotto(winningLotto);
-            }
-            // 분리 로직
+            int count = lotto.compareLotto(winningLotto);
+            WinningInfo matchType = null;
             if (count == 3) {
-                matchNumbers.put(5, matchNumbers.getOrDefault(3, 0) + 1);
-                sum += threeMatchMoney;
+                matchType = WinningInfo.THREE;
             } else if (count == 4) {
-                matchNumbers.put(4, matchNumbers.getOrDefault(4, 0) + 1);
-                sum += fourMatchMoney;
+                matchType = WinningInfo.FOUR;
             } else if (count == 5) {
-                if (lotto.isContain(bonus.getNumber())) {
-                    matchNumbers.put(2, matchNumbers.getOrDefault(2, 0) + 1);
-                    sum += fiveBonusMatchMoney;
-                    continue;
-                }
-                sum += fiveMatchMoney;
-                matchNumbers.put(3, matchNumbers.getOrDefault(3, 0) + 1);
+                matchType = isMatchBonus(lotto, bonus);
             } else if (count == 6) {
-                sum += sixMatchMoney;
-                matchNumbers.put(1, matchNumbers.getOrDefault(1, 0) + 1);
+                matchType = WinningInfo.SIX;
+            }
+            if (matchType != null) {
+                matchNumbers.put(matchType, matchNumbers.getOrDefault(matchType, 0) + 1);
+                sum += matchType.getMoney();
             }
         }
-
-        System.out.println(threeMatchMessage + matchNumbers.get(5) + "개");
-        System.out.println(fourMatchMessage + matchNumbers.get(4) + "개");
-        System.out.println(fiveMatchMessage + matchNumbers.get(3) + "개");
-        System.out.println(fiveBonusMatchMessage + matchNumbers.get(2) + "개");
-        System.out.println(sixMatchMessage + matchNumbers.get(1) + "개");
-
-        for (Integer key : matchNumbers.keySet()) {
-            sum += matchNumbers.get(key);
-        }
-        String format = String.format("%.1f", ((float) sum / money) * 100);
-        System.out.print("총 수익률은 " + format + "%입니다.");
-
+        printResult(matchNumbers, ((float) sum / money) * 100);
     }
 }

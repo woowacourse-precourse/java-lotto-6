0351b1af3162eb325c91e10ca42ffd8a6e470885
djc06048 <gsafe1213@gmail.com>
2023-11-08 14:11:30 +0900

    refactor(model): TotalLotto 일급 컬렉션 수정 일급 컬렉션 적용시도, 추가적인 공부 필요

--- src/main/java/lotto/model/TotalLotto.java
+++ src/main/java/lotto/model/TotalLotto.java
@@ -4,10 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TotalLotto {
-    private List<Lotto> totalLotto = new ArrayList<>();
+    private final List<Lotto> totalLotto;
+
+    public TotalLotto() {
+        this.totalLotto = new ArrayList<>();
+    }
 
     public void generateTotalLotto(Lotto lotto) {
         totalLotto.add(lotto);
     }
+    public List<Lotto> getLottoList() {
+        return new ArrayList<>(totalLotto); // 복제된 리스트 반환
+    }
+    public int getTotalLottoCount() {
+        return totalLotto.size();
+    }
+
 
 }

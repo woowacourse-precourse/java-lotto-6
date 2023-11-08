92889843f873457d127697f49e474062d62052c7
박형균 <phk@baghyeong-gyun-ui-MacBookPro.local>
2023-11-08 09:19:16 +0900

    feat: 유틸성을 위해 Constant 분리

---
+++ src/main/java/lotto/constant/Constant.java
@@ -0,0 +1,12 @@
+package lotto.constant;
+
+public class Constant {
+
+    public static final int NUMBER_MIN = 1;
+    public static final int NUMBER_MAX = 45;
+    public static final int NUMBERS_SIZE = 6;
+    public static final int MONEY_UNIT = 1000;
+
+    private Constant() {
+    }
+}

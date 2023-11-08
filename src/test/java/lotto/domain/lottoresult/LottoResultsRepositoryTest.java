1cb9b1bae15e82a3717e400b46a10fe3a92d9edc
newh08 <newh08@gmail.com>
2023-11-08 11:02:55 +0900

    ️☑️ TEST: LottoResultsRepository 로직 변경에 의한 테스트 수정

--- src/test/java/lotto/domain/lottoresult/LottoResultsRepositoryTest.java
+++ src/test/java/lotto/domain/lottoresult/LottoResultsRepositoryTest.java
@@ -1,22 +1,28 @@
 package lotto.domain.lottoresult;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import java.util.List;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoBundle;
+import lotto.domain.player.Player;
+import lotto.domain.player.Profit;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LottoResultsRepositoryTest {
 
-    @DisplayName("3등, 4등으로 로또 결과를 업데이트 해 DTO 를 통해 총 상금을 받으면, 총 상금은 1,550,000 이다.")
+    @DisplayName("4등으로 로또 결과 업데이트 후 10,000원 사용한 플레이어로 이익률을 계산하면 500.0이 나온다.")
     @Test
     void lottoResultRepositoryTest_1() {
         //given
         LottoResultsRepository lottoResultsRepository = new LottoResultsRepository();
+        Player player = new Player(10000, new LottoBundle());
+        player.buyAndSaveRandomLottoWithAllTicket(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6)));
         //when
-        lottoResultsRepository.saveLottoResult(LottoResult.THIRD);
         lottoResultsRepository.saveLottoResult(LottoResult.FOURTH);
         //then
-        assertThat(lottoResultsRepository.getTotalLottoPrizeMoney()).isEqualTo(1550000);
+        Profit profit = lottoResultsRepository.calculateProfit(player);
+        Assertions.assertThat(profit.getProfit()).isEqualTo(500.0);
     }
 }

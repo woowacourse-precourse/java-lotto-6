db48d31819aaecf617b16f45c9abb3a79ce82e5a
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 21:09:10 +0900

    Feat : 당첨 금액 계산 해주고 수익률 저장해줄 객체 추가

---
+++ src/main/java/lotto/model/Profit.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+import lotto.entity.Prize;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+
+public class Profit {
+    private final Map<Prize, Integer> resultCount;
+    private Integer amount = 0;
+    private Integer profit = 0;
+
+    public Profit() {
+        this.resultCount = init();
+    }
+
+    public double getProfitRate() {
+        return (double) profit / amount*100;
+    }
+
+    public void updateProfitRate(Prize prize) {
+        this.amount += 1000;
+        this.profit += prize.getReward();
+        resultCount.put(prize,resultCount.get(prize)+1);
+    }
+
+    public Map<Prize, Integer> init() {
+        Map<Prize, Integer> result = new EnumMap<>(Prize.class);
+        Arrays.stream(Prize.values())
+                .forEach(prize -> result.put(prize, 0));
+        return result;
+    }
+    public Map<Prize,Integer> getResultCount(){
+        return resultCount;
+    }
+}

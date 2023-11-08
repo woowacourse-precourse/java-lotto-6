83514b8f83b7ed20ee14cd86c1725de7ac8661b5
yunseung <yseungun.21@gmail.com>
2023-11-08 20:01:07 +0900

    feat(Lotto): LottoTest 통과하는 코드 작성
    - creatLottoByDuplicateNumber() 테스트 코드 통과

--- src/main/java/lottoWinningNumber/Lotto.java
+++ src/main/java/lottoWinningNumber/Lotto.java
@@ -1,8 +1,11 @@
 package lottoWinningNumber;
 
+import static constant.ErrorMessage.DUPLICATE_VALUES_MESSAGE;
 import static constant.ErrorMessage.INVALID_NUMBER_COUNT;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,6 +18,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(INVALID_NUMBER_COUNT);
+        }
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() < numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATE_VALUES_MESSAGE);
         }
     }
 }

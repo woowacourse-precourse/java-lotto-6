d45e821cb8c6533fe992014a88457608e6cb4ea3
hansg931 <hansg0509@cau.ac.kr>
2023-11-08 22:18:38 +0900

    feat(PurchaseValidator): 구입 금액이 1000 보다 작은지 검사하는 메소드 작성

--- src/main/java/lotto/util/PurchaseValidator.java
+++ src/main/java/lotto/util/PurchaseValidator.java
@@ -3,6 +3,7 @@ package lotto.util;
 public class PurchaseValidator {
 	public void validatePurchaseAmount(String input) {
 		validateInteger(input);
+		validateRange(input);
 	}
 
 	private void validateInteger(String input) {
@@ -10,6 +11,12 @@ public class PurchaseValidator {
 			Integer.parseInt(input);
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_NOT_INTEGER_MESSAGE);
+		}
+	}
+
+	private void validateRange(String input) {
+		if (Integer.parseInt(input) < Constant.LOTTO_PRICE) {
+			throw new IllegalArgumentException(ExceptionMessage.PURCHASE_AMOUNT_OUT_OF_RANGE_MESSAGE);
 		}
 	}
 }

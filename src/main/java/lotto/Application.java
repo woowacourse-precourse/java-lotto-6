c8cc3a8da27a3f0225c29c939fbaaee2c5a6e278
seeunjang <wisjang1@naver.com>
2023-11-08 14:15:14 +0900

    feat : 로또 게임 실행 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import controller.LottoController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final LottoController lottoController = new LottoController();
+        lottoController.run();
+        Console.close();
     }
 }

f5df1520688b664571ce3877dda17b88b1709cdc
binary_ho <dfghcvb11@naver.com>
2023-11-08 18:43:31 +0900

    feat : 모든 model, view와 상호작용 하는 Controller LottoMachine 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
+import lotto.view.BillAcceptor;
+import lotto.view.LottoNumberInput;
+import lotto.view.Screen;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachine lottoMachine = new LottoMachine(new Screen(), new BillAcceptor(), new LottoNumberInput());
+        lottoMachine.start();
     }
 }
---
+++ src/main/java/lotto/LottoMachine.java
@@ -0,0 +1,73 @@
+package lotto;
+
+import java.util.List;
+import lotto.model.LottoTicket;
+import lotto.model.winninglotto.LottoWinningResult;
+import lotto.model.winninglotto.WinningLotto;
+import lotto.util.NumberConvertor;
+import lotto.util.PurchaseCountCalculator;
+import lotto.view.BillAcceptor;
+import lotto.view.LottoNumberInput;
+import lotto.view.Screen;
+
+public class LottoMachine {
+
+    private static final int LOTTO_PRICE = 1_000;
+    private final Screen screen;
+    private final BillAcceptor billAcceptor;
+    private final LottoNumberInput lottoNumberInput;
+
+    public LottoMachine(Screen screen, BillAcceptor billAcceptor, LottoNumberInput lottoNumberInput) {
+        this.screen = screen;
+        this.billAcceptor = billAcceptor;
+        this.lottoNumberInput = lottoNumberInput;
+    }
+
+    public void start() {
+        int userLottoPurchaseCount = getUserLottoPurchaseCount();
+        LottoTicket lottoTicket = buyLottoTicket(userLottoPurchaseCount);
+        WinningLotto winningLotto = getWinningLotto();
+        getLottoResult(winningLotto, lottoTicket, userLottoPurchaseCount);
+    }
+
+    private int getUserLottoPurchaseCount() {
+        try {
+            int userInsertAmount = billAcceptor.acceptBill();
+            return PurchaseCountCalculator.calculatePurchaseCount(userInsertAmount, LOTTO_PRICE);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return getUserLottoPurchaseCount();
+        }
+    }
+
+    private LottoTicket buyLottoTicket(int userLottoPurchaseCount) {
+        LottoTicket lottoTicket = LottoTicket.issueLottoTicket(userLottoPurchaseCount);
+        screen.printLottoTicket(userLottoPurchaseCount, lottoTicket);
+        return lottoTicket;
+    }
+
+    private WinningLotto getWinningLotto() {
+        try {
+            List<Integer> winningNumbers = getWinningNumbers();
+            Integer bonusNumber = getBonusNumber();
+            return WinningLotto.createWinningLotto(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return getWinningLotto();
+        }
+    }
+
+    private List<Integer> getWinningNumbers() {
+        String winningNumberInput = lottoNumberInput.requestWinningNumberInput();
+        return NumberConvertor.convertToNumbers(winningNumberInput);
+    }
+
+    private Integer getBonusNumber() {
+        String bonusNumberInput = lottoNumberInput.requestBonusNumberInput();
+        return NumberConvertor.convertToNumber(bonusNumberInput);
+    }
+
+    private void getLottoResult(WinningLotto winningLotto, LottoTicket lottoTicket, int purchaseCount) {
+        LottoWinningResult lottoResult = winningLotto.getLottoResult(lottoTicket.getLottoTicket());
+        double profitRate = lottoResult.getLottoProfitRate(purchaseCount * LOTTO_PRICE);
+        screen.printLottoResult(lottoResult, profitRate);
+    }
+}

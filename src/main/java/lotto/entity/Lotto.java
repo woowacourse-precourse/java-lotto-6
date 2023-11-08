6bd4fccc89bb4c324b62e3a2eeb3a57a849e8760
seonghoo1217 <seonghoo1217@naver.com>
2023-11-08 16:35:07 +0900

    refactor: 로또 유효성 검사

--- src/main/java/lotto/entity/Lotto.java
+++ src/main/java/lotto/entity/Lotto.java
@@ -4,9 +4,11 @@ import lotto.entity.mapper.FiledMapper;
 import lotto.property.MethodProperty;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 import static lotto.property.ErrorProperty.LOTTO_SIZE_IS_NOT_STANDARD;
+import static lotto.property.ErrorProperty.WINNING_NUMBER_IS_DUPLICATE;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -19,6 +21,10 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException(LOTTO_SIZE_IS_NOT_STANDARD.toString());
+        }
+
+        if (numbers.size() != new HashSet<>(numbers).size()) {
+            throw new IllegalArgumentException(WINNING_NUMBER_IS_DUPLICATE.toString());
         }
     }
 

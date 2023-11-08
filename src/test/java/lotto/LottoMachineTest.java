73ac4129e18a7be119872513200e5e292d5e7475
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 17:27:19 +0900

    당첨 번호 추출 및 로또 결과 결정 기능 검증
    LottoMachine 클래스의 drawWinningNumbers 메소드가 정확한 당첨 결과를 생성하는지 검증하는 테스트를 추가하였습니다.
    
    - Mockito를 활용하여 LottoGenerator의 행위를 모의하고, 결과가 1등 당첨인 경우를 테스트합니다.

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -24,7 +24,7 @@ public class LottoMachineTest {
     }
 
     @Test
-    @DisplayName("정확한 수의 로또 티켓을 구매해야 한다")
+    @DisplayName("정확한 수의 로또 티켓을 구매해야 하는지 테스트")
     public void testPurchaseLottos() {
         // 기대하는 로또 번호 설정
         Lotto expectedLotto1 = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
@@ -41,5 +41,23 @@ public class LottoMachineTest {
         // 구매된 로또가 기대한 로또와 일치하는지 확인
         assertEquals(expectedLotto1, purchasedLottos.get(0), "첫 번째 구매한 로또가 기대한 로또와 일치해야 한다");
         assertEquals(expectedLotto2, purchasedLottos.get(1), "두 번째 구매한 로또가 기대한 로또와 일치해야 한다");
+    }
+    @Test
+    @DisplayName("로또 결과를 정확하게 결정하는지를 테스트")
+    public void testDrawWinningNumbers() {
+        // 기대하는 로또 번호 설정
+        Lotto purchasedLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        when(generator.generate()).thenReturn(purchasedLotto);
+
+        // 로또 구매 및 당첨 번호 설정
+        machine.purchaseLottos(1);
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        // 당첨 번호 추출 및 결과 가져오기
+        LottoResults results = machine.drawWinningNumbers(winningNumbers, bonusNumber);
+
+        // 결과가 1등 당첨을 포함하는지 확인
+        assertEquals(1, results.getResults().get(LottoRank.FIRST).intValue(), "1등 당첨이 하나 있어야 한다");
     }
 }

c9267a5ffd8a588a2b44fcbc71f89d74109b63da
박상현 <shawnpark98@naver.com>
2023-11-08 17:53:52 +0900

    feat: 번호를 비교해서 로또 결과 계산하는 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -17,4 +17,32 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public LottoResult getResult(List<Integer> luckyNumbers, Integer bonusNumber) {
+        int hitCount = 0;
+
+        validate(luckyNumbers);
+
+        for (Integer luckyNumber : luckyNumbers) {
+            if (numbers.contains(luckyNumber)) {
+                hitCount++;
+            }
+        }
+
+        if(hitCount == 6) {
+            return LottoResult.FIRST;
+        }
+        if(hitCount == 5 && numbers.contains(bonusNumber)) {
+            return LottoResult.SECOND;
+        }
+        if(hitCount == 5) {
+            return LottoResult.THIRD;
+        }
+        if(hitCount == 4) {
+            return LottoResult.FOURTH;
+        }
+        if(hitCount == 3) {
+            return LottoResult.FIFTH;
+        }
+        return LottoResult.FAIL;
+    }
 }

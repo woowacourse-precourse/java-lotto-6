f9289439bdbae2ce9006b684b1d2229cd989aee4
c1ce <chalicenuclear@gmail.com>
2023-11-08 22:25:42 +0900

    Refactor : codestyle 적용

--- src/main/java/lotto/view/UserLottoView.java
+++ src/main/java/lotto/view/UserLottoView.java
@@ -6,7 +6,7 @@ public class UserLottoView {
     private static final String BUY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_LOTTO_MESSAGE = "보너스 번호를 입력해 주세요.";
-
+ 
     public String inputBuyPrice() {
         System.out.println(BUY_MESSAGE);
         String buyPrice = readLine();

7ef087f290a129a1ea18bab71c111d096d7d5f3a
taewan625 <taewan1997625@gmail.com>
2023-11-08 15:24:43 +0900

    Refactor: Add LottoTest 중복 테스드 검증 method 추가

--- src/main/java/lotto/model/domain/Lotto.java
+++ src/main/java/lotto/model/domain/Lotto.java
@@ -1,17 +1,27 @@
-package lotto.model;
+package lotto.model.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        checkDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDuplicate(List<Integer> numbers) {
+        Set<Integer> duplications = new HashSet<>(numbers);
+        if (duplications.size() != 6) {
             throw new IllegalArgumentException();
         }
     }

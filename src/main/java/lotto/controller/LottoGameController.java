57c62fd83f3f2e8fa03fb4332d1a67a30ae99676
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 21:11:29 +0900

    Feat : 로또 구매 컨트롤러 구현
    로또 구매와 관련된 것들을 하는 컨트롤러인
    LottoPurchaseController 클래스 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,4 +1,15 @@
 package lotto.controller;
 
 public class LottoGameController {
+    private LottoPurchaseController lottoPurchaseController;
+    private LottoWinningCalculationController lottoWinningCalculationController;
+
+    public LottoGameController() {
+        lottoPurchaseController = new LottoPurchaseController();
+        lottoWinningCalculationController = new LottoWinningCalculationController();
+    }
+
+    public void start() {
+        lottoPurchaseController.purchaseLottos();
+    }
 }
--- src/main/java/lotto/controller/LottoPurchaseController.java
+++ src/main/java/lotto/controller/LottoPurchaseController.java
@@ -1,4 +1,24 @@
 package lotto.controller;
 
+import lotto.domain.LottoPurchaseMachine;
+import lotto.domain.Lottos;
+import lotto.view.LottoPurchaseOutputView;
+
 public class LottoPurchaseController {
+    private LottoPurchaseMachine lottoPurchaseMachine;
+    private MoneyInputController moneyInputController;
+    private LottoPurchaseOutputView lottoPurchaseOutputView;
+
+    public LottoPurchaseController() {
+        lottoPurchaseMachine = new LottoPurchaseMachine();
+        moneyInputController = new MoneyInputController();
+        lottoPurchaseOutputView = new LottoPurchaseOutputView();
+    }
+
+    public void purchaseLottos() {
+        int money = moneyInputController.inputMoney();
+        int lottoCount = money / 1000;
+        Lottos lottos = lottoPurchaseMachine.purchaseLottos(lottoCount);
+        lottoPurchaseOutputView.outputLotto(lottos, lottoCount);
+    }
 }

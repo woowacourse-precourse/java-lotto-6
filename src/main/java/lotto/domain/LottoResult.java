e85444f2ab2bfb1cd7abb0fbf36b855a0fae10ff
HyunmoBae <tsi0520@naver.com>
2023-11-08 12:12:06 +0900

    feat(lotto) : add compare, Rank, ProfitMargin, Prize methods

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,17 +1,69 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoResult {
+    int first = 0;
+    int second = 0;
+    int third = 0;
+    int fourth = 0;
+    int fifth = 0;
 
-    public int compareLottoTicket() {
-        return 0;
+    public void compareLottoTicket(List<Integer> lottoNumbers, List<Integer> ticketNumbers) {
+        int lottoMatch = compareLottoNumber(lottoNumbers, ticketNumbers);
+        boolean bonusMatch = compareBonusNumber(lottoNumbers, ticketNumbers);
+
+        getLottoRank(lottoMatch, bonusMatch);
     }
 
-    public int lottoResult() {
-        return 0;
+    public int compareLottoNumber(List<Integer> lottoNumbers, List<Integer> ticketNumbers) {
+        int lottoMatch = 0;
+        for (int i = 0; i < lottoNumbers.size() - 1; i++) {
+            if (ticketNumbers.contains(lottoNumbers.get(i))) {
+                lottoMatch += 1;
+            }
+        }
+        return lottoMatch;
     }
 
-    public float rateOfReturn() {
-        return 0;
+    public boolean compareBonusNumber(List<Integer> lottoNumbers, List<Integer> ticketNumbers) {
+        if (ticketNumbers.contains(lottoNumbers.get(6))) {
+            return true;
+        }
+        return false;
+    }
+
+    public void compareAllLottoTicket(List<Integer> lottoNumbers, List<LottoTicket> lottoTickets) {
+        for (LottoTicket lottoTicket : lottoTickets) {
+            compareLottoTicket(lottoNumbers, lottoTicket.lottoNumber);
+        }
+    }
+
+    public void getLottoRank(int lottoMatch, boolean bonusMatch) {
+        if (lottoMatch == 3) {
+            this.fifth += 1;
+        } else if (lottoMatch == 4) {
+            this.fourth += 1;
+        } else if (lottoMatch == 5 && bonusMatch) {
+            this.second += 1;
+        } else if (lottoMatch == 5) {
+            this.third += 1;
+        } else if (lottoMatch == 6) {
+            this.first += 1;
+        }
+    }
+
+    public double getProfitMargin(int money) {
+        double prize = getPrize();
+        double profitMargin = (prize / money) * 100;
+        return profitMargin;
+    }
+
+    public double getPrize() {
+        double prize =
+                (first * 2000000000) + (second * 30000000) + (third * 1500000) + (fourth * 50000) + (fifth * 5000);
+        return prize;
     }
 
 }
+

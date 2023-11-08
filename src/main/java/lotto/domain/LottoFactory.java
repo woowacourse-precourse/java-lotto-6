00067df1c62ea043a8dcd1a0d6f674587158d452
LEEJUNGWOO <jungwoo1002@gmail.com>
2023-11-08 12:59:29 +0900

    refactor(LottoFactory): 메소드에 있는 상수 처리

--- src/main/java/lotto/domain/LottoFactory.java
+++ src/main/java/lotto/domain/LottoFactory.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 import lotto.domain.number.NumberGenerator;
 
 public class LottoFactory {
+    private static final int START_NUMBER = 0;
     private final NumberGenerator generator;
 
     public LottoFactory(final NumberGenerator generator) {
@@ -13,7 +14,7 @@ public class LottoFactory {
 
     public LottoBundle createBundle(final int count) {
         final List<Lotto> lottos = IntStream
-                .range(0, count)
+                .range(START_NUMBER, count)
                 .mapToObj(i -> new Lotto(generator.generate()))
                 .toList();
         return LottoBundle.from(lottos);

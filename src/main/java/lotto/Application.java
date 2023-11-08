deadd579475b6c64774dd8eb53c73d8a0b5d3391
g00hyun <rhkdgus1417@hufs.ac.kr>
2023-11-08 22:56:25 +0900

    [feat] main 동작부 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.DHLottery;
+import lotto.model.Member;
+import lotto.model.ScoreType;
+import lotto.view.Input;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Member member = new Member();
+        DHLottery dhLottery = new DHLottery();
+        LottoController lottoController = new LottoController(dhLottery, member);
+
+        lottoController.run();
     }
 }

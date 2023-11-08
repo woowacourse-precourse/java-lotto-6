595da2126385768e27e3b91ac001b0c58a814d5d
Dawin <dawin002@naver.com>
2023-11-08 16:36:32 +0900

    refactor(LottoManager): 필드 변수 수정
    필드 변수 수정:
    - private int ticketNumber
    - 생성할 로또 개수를 LottoManager 클래스에서 관리

--- src/main/java/lotto/model/LottoManager.java
+++ src/main/java/lotto/model/LottoManager.java
@@ -7,17 +7,23 @@ import java.util.List;
 public class LottoManager {
     private List<Lotto> lottos;
     private LottoGenerator generator;
+    private int ticketNumber;
 
-    public LottoManager(LottoGenerator generator) {
+    public LottoManager(LottoGenerator generator, int ticketNumber) {
         lottos = new ArrayList<>();
         this.generator = generator;
+        this.ticketNumber = ticketNumber;
     }
 
-    public void generateLottos(int ticketNumber) {
+    public void generateLottos() {
         for (int i = 0; i < ticketNumber; i++) {
             Lotto newLotto = generator.createSingleLotto();
             lottos.add(newLotto);
         }
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
     }
 
     public List<Lotto> getLottos() {

d4ab9aca6a9e6494f8c21fc0fd2e7d62d6a10c9e
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-08 18:14:55 +0900

    feat: 입력 view 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view;
+
+public class InputView {
+    public static void inputMoneyMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void inputAnswerMessage() {
+        System.out.println();
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void inputBonusNumberMessage() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+}

de3519e160d0da24a38ab697073914b0be214bf2
dmong <dt2980_hee@naver.com>
2023-11-08 10:27:34 +0900

    feat: Order에 buyAmount 상태변수, DTO 변환 메소드 추가

--- src/main/java/lotto/domain/Order.java
+++ src/main/java/lotto/domain/Order.java
@@ -1,15 +1,19 @@
 package lotto.domain;
 
-import static lotto.domain.LottoConstants.LOTTO_PRICE;
-import static lotto.domain.LottoConstants.MIN_LOTTO_BUY_AMOUNT;
-import static lotto.view.ErrorConstants.INDIVISIBLE_BUY_AMOUNT_ERROR_MESSAGE;
-import static lotto.view.ErrorConstants.MINIMUM_BUY_AMOUNT_ERROR_MESSAGE;
+import static lotto.constants.ErrorConstants.INDIVISIBLE_BUY_AMOUNT_ERROR_MESSAGE;
+import static lotto.constants.ErrorConstants.MINIMUM_BUY_AMOUNT_ERROR_MESSAGE;
+import static lotto.constants.LottoConstants.LOTTO_PRICE;
+import static lotto.constants.LottoConstants.MIN_LOTTO_BUY_QUANTITY;
+
+import lotto.domain.dto.OrderResultDto;
 
 public class Order {
     private final int quantity;
+    private final int buyAmount;
 
     private Order(int buyAmount) {
         validate(buyAmount);
+        this.buyAmount = buyAmount;
         this.quantity = buyAmount / LOTTO_PRICE.getValue();
     }
 
@@ -22,7 +26,7 @@ public class Order {
     }
 
     private boolean isQuantityLessThanMinimum(int amount) {
-        return amount / LOTTO_PRICE.getValue() < MIN_LOTTO_BUY_AMOUNT.getValue();
+        return amount / LOTTO_PRICE.getValue() < MIN_LOTTO_BUY_QUANTITY.getValue();
     }
 
     private void validate(int amount) {
@@ -32,11 +36,19 @@ public class Order {
         }
         if (isQuantityLessThanMinimum(amount)) {
             throw new IllegalArgumentException(
-                    String.format(MINIMUM_BUY_AMOUNT_ERROR_MESSAGE.getMessage(), MIN_LOTTO_BUY_AMOUNT.getValue()));
+                    String.format(MINIMUM_BUY_AMOUNT_ERROR_MESSAGE.getMessage(), MIN_LOTTO_BUY_QUANTITY.getValue()));
         }
+    }
+
+    public OrderResultDto getOrderResultDto(Lottos lottos) {
+        return new OrderResultDto(quantity, lottos.getLottos());
     }
 
     public int getQuantity() {
         return quantity;
+    }
+
+    public int getBuyAmount() {
+        return buyAmount;
     }
 }

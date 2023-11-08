2f1e8bbae1ee7d45af93edc10e162e4b7841bd6d
Donghun Won <wonhun1225@gmail.com>
2023-11-08 16:30:05 +0900

    feat(view) : outputView 클래스 기능 추가
    로또 추첨 결과 보여주는 메서드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,35 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
 import lotto.model.Lotto;
+import lotto.model.Prize;
+import lotto.model.Rank;
 
 public class OutputView {
-    public void showLottoNum( int num, List<Lotto> lottoGames){
-        System.out.println("\n"+num+"개를 구매했습니다.");
+    public void showLottoNum(int num, List<Lotto> lottoGames) {
+        System.out.println("\n" + num + "개를 구매했습니다.");
         for (int i = 0; i < lottoGames.size(); i++) {
             Lotto lotto = lottoGames.get(i);
             List<Integer> numbers = lotto.getLottoNumber();
             System.out.println(numbers);
         }
+    }
+
+    public void showLottoGameResult(int count, String lottoPrize, int lottoNum) {
+        System.out.println(count + "개 일치 (" + lottoPrize + "원) - " + lottoNum + "개");
+    }
+
+    public void showProfitResult(double profit) {
+        System.out.println("총 수익률은 " + profit + "%입니다.");
+    }
+
+    public void showBonusResult(int count, String lottoPrize, int lottoNum) {
+        System.out.println(count + "개 일치, 보너스 볼 일치 (" + lottoPrize + "원) - " + lottoNum + "개");
+    }
+
+    public void resultStatistics() {
+        System.out.println("당첨 통계");
+        System.out.println("---");
     }
 }

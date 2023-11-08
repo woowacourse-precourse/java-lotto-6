7ce6de1d37fe10f38a198582bb009bdc4411b6d1
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-08 14:49:51 +0900

    feat(Application): 로또 프로그램 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,16 @@
 package lotto;
 
 public class Application {
+    
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+
+        lottoGame.enterPurchaseAmount();
+        lottoGame.createLottos();
+        lottoGame.printLottos();
+        lottoGame.enterWinnerNumbers();
+        lottoGame.enterBonusWinnerNumber();
+        lottoGame.printMatchedLottosReport();
+        lottoGame.printReturnRate();
     }
 }

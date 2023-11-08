bf4e0a44aa598d054265925a66a4955a6a8bec9e
Kim mingyu <minq0302@naver.com>
2023-11-08 20:19:40 +0900

    refactor(domain): change magic number into constant variable

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import java.util.List;
 
 public class Lotto {
+    public static final int LOTTO_SIZE = 6;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,8 +12,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR]: 로또 숫자는 6개여야 합니다.");
         }
     }
 

eabbcc96d55f09a48dad2514d13e426968e71c2d
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 15:27:32 +0900

    feat(model) : lotto 모델에 중복 값 들어올 시 예외 발생 적용

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.model;
 
+import lotto.model.constant.LottoConstants;
+import lotto.util.validator.InputValidator;
+
 import java.util.List;
 
 public class Lotto {
@@ -7,14 +10,20 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LottoConstants.LOTTO_MAX_COUNT.getNumber()) {
             throw new IllegalArgumentException();
         }
     }
 
+    private void validateDuplicate(List<Integer> numbers) {
+        if(numbers.stream().distinct().count() != LottoConstants.LOTTO_MAX_COUNT.getNumber()) {
+            throw new IllegalArgumentException();
+        }
+    }
     // TODO: 추가 기능 구현
 }

07805be41110d4c5922c4d344b925df968cd965e
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 12:37:47 +0900

    feat(model) : LottoResult 모델 구현

---
+++ src/main/java/lotto/model/LottoResult.java
@@ -0,0 +1,10 @@
+package lotto.model;
+
+public class LottoResult {
+    private int firstPlace;
+    private int secondPlace;
+    private int thirdPlace;
+    private int fourthPlace;
+    private int fifthPlace;
+    private int totalWinningAmount;
+}

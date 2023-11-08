8e26b31e0b18e1daa0e33a4780b279865eb2af5f
dong2ast <kdh01132006@naver.com>
2023-11-08 20:04:18 +0900

    ✅ test: 당첨번호에 숫자가 아닌 문자를 입력할 경우 예외처리 테스트

--- src/test/java/lotto/service/InteractServiceTest.java
+++ src/test/java/lotto/service/InteractServiceTest.java
@@ -1,6 +1,6 @@
 package lotto.service;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
@@ -18,11 +18,9 @@ public class InteractServiceTest extends NsTest {
 
     @DisplayName("로또 구입 금액을 문자열로 입력할 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(
-        strings = {
-                "test", "purchase", "String"
-        }
-    )
+    @ValueSource(strings = {
+            "test", "purchase", "String"
+    })
     void purchaseByString(String input) {
         assertSimpleTest(() -> {
             runException(
@@ -34,11 +32,9 @@ public class InteractServiceTest extends NsTest {
 
     @DisplayName("로또 구입 금액을 기호로 입력할 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "@@@", ",,,", "!@#$!@$!"
-            }
-    )
+    @ValueSource(strings = {
+            "@@@", ",,,", "!@#$!@$!"
+    })
     void purchaseBySign(String input) {
         assertSimpleTest(() -> {
             runException(
@@ -50,11 +46,9 @@ public class InteractServiceTest extends NsTest {
 
     @DisplayName("로또 구입 금액을 공백로 입력할 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    " ", "\n"
-            }
-    )
+    @ValueSource(strings = {
+            " ", "\n"
+    })
     void purchaseByBlank(String input) {
         assertSimpleTest(() -> {
             runException(
@@ -66,11 +60,9 @@ public class InteractServiceTest extends NsTest {
 
     @DisplayName("로또 구입 금액을 음수로 입력할 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "-400", "-800", "-1200"
-            }
-    )
+    @ValueSource(strings = {
+            "-400", "-800", "-1200"
+    })
     void purchaseByNegative(String input) {
         assertSimpleTest(() -> {
             runException(
@@ -82,17 +74,30 @@ public class InteractServiceTest extends NsTest {
 
     @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않을 경우 예외가 발생한다.")
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "500", "1200", "9999"
-            }
-    )
+    @ValueSource(strings = {
+            "500", "1200", "9999"
+    })
     void purchaseNotDivideByThousand(String input) {
         assertSimpleTest(() -> {
             runException(
                     input
             );
             assertThat(output()).contains(ErrorStatus.NOT_DIVISION_THOUSAND_ERROR.getMessage());
+        });
+    }
+
+    @DisplayName("당첨 번호에 숫자가 아닌 문자를 입력할 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "1,2,3,4,ss,tt", "1,2,3, ,5,6", "1,2,@@,4,5,6", "1,2,3,4,\n,6"
+    })
+    void winningNumByString(String input) {
+        assertSimpleTest(() -> {
+            runException(
+                    PURCHASE_FEE,
+                    input
+            );
+            assertThat(output()).contains(ErrorStatus.PARSE_INT_ERROR.getMessage());
         });
     }
 

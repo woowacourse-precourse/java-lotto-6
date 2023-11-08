7bb81d6879110b8e1d05ef340926b65cd923c276
gmu1026 <curiouscomponent@gmail.com>
2023-11-08 19:36:15 +0900

    feat: 로또 게임 진행 관리 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,33 +1,11 @@
 package lotto;
 
-import lotto.model.Buyer;
-import lotto.model.Lotto;
-import lotto.model.WinningLotto;
-import lotto.util.StringUtils;
-import lotto.view.InputView;
-import lotto.view.OutputView;
+import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        OutputView outputView = new OutputView();
-        InputView inputView = new InputView();
-
-        outputView.printLottoPurchaseMessage();
-        String inputMoney = inputView.input();
-        outputView.printLineSeparator();
-
-        Buyer buyer = new Buyer(inputMoney);
-        outputView.printLottoCountMessage(buyer.getLottoCount());
-        buyer.printLottos();
-        outputView.printLineSeparator();
-
-        outputView.printWinningNumberMessage();
-        String inputWinningNumbers = inputView.input();
-        outputView.printBonusNumberMessage();
-        String inputBonusNumber = inputView.input();
-        Lotto inputLotto = new Lotto(StringUtils.convertStringToList(inputWinningNumbers));
-        WinningLotto winningLotto = new WinningLotto(inputLotto, Integer.parseInt(inputBonusNumber));
-
-        inputView.close();
+        LottoController lottoController = new LottoController();
+        lottoController.start();
+        lottoController.end();
     }
 }

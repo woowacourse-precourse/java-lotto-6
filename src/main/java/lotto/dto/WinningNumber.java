fed87db632b6dd8a148db2df9b0013c401bd615b
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-08 16:47:24 +0900

    feat(WinningNumber): 중복 숫자 예외 처리 구현

--- src/main/java/lotto/dto/WinningNumber.java
+++ src/main/java/lotto/dto/WinningNumber.java
@@ -1,6 +1,7 @@
 package lotto.dto;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,7 @@ public class WinningNumber {
     private static final String DELIMITER = ",";
     private static final String OUT_RANGE_ERROR_MESSAGE = "[ERROR] 범위를 벗어난 숫자 입니다.";
     private static final String NULL_INPUT_ERROR_MESSAGE = "[ERROR] 금액을 정확히 입력해 주세요.";
+    private static final String DUPLICATE_ERROR_MESSAGE = "[ERROR] 중복 숫자가 존재합니다";
     private static final String NOT_INTEGER_ERROR_MESSAGE = "[ERROR] 숫자를 입력해 주세요.";
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
@@ -21,6 +23,7 @@ public class WinningNumber {
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
             validInRangeNumber();
+            validateDuplicateNumber(winningNumber);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
@@ -41,6 +44,13 @@ public class WinningNumber {
 
     void validateNullValue(String value) {
         if (value.isBlank()) throw new IllegalArgumentException(NULL_INPUT_ERROR_MESSAGE);
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        HashSet<Integer> distinctNumbers = new HashSet<>(numbers);
+        if (distinctNumbers.size() != 6) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE);
+        }
     }
 
     public List<Integer> getWinningNumber(){

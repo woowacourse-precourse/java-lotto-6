c46ae2f1617990f2fc7e574074b97854c09716d9
ppusda <ppusda1234@gmail.com>
2023-11-08 14:17:13 +0900

    fix: 불필요한 코드 제거

--- src/main/java/lotto/Model/Domain/Raffle.java
+++ src/main/java/lotto/Model/Domain/Raffle.java
@@ -7,15 +7,8 @@ public class Raffle {
     private final int bonusNumber;
 
     public Raffle(List<Integer> winningNumbers, int bonusNumber) {
-        validate(winningNumbers);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
     }
 
     public List<Integer> getWinningNumbers() {

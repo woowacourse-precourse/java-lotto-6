600b3df3ca5a9d154b4afa18d9c197ddc81ab3e1
youjae <youjae0803@gmail.com>
2023-11-08 10:59:38 +0900

    test: LottoGenerator#generate

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -2,6 +2,8 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import lotto.application.LottoService;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -77,6 +80,13 @@ class ApplicationTest extends NsTest {
     void 구매_금액이_1000원_단위가_아닐때_검증(){
         boolean validated = LottoService.validatePurchaseAmount("10001");
         assertThat(validated).isFalse();
+    }
+
+    @Test
+    void 로또_생성기가_조건에_맞게_로또를_생성하는지_테스트(){
+        assertThatCode(()->{
+            Lotto lotto = LottoGenerator.generate();
+        }).doesNotThrowAnyException();
     }
 
     @Override

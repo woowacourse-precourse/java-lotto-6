8c2b39d8fc95cd49b9712bf45a45c585e3eb7ca4
bokyung <bbookng@gmail.com>
2023-11-08 16:01:35 +0900

    fix(InputView) : 올바른 값이 입력될 때 까지 반복하는 로직 추가 - Validator Class 를 이용하여 예외처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,36 +1,78 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.validator.Validator;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_INPUT = "구입금액을 입력해주세요.";
     private static final String WINNING_NUMBERS_INPUT = "당첨 번호를 입력해주세요.";
     private static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해주세요.";
     private static final String HASH = ",";
+    private final Validator validator = new Validator();
 
     public long getPurchaseAmount() {
-        System.out.println(PURCHASE_AMOUNT_INPUT);
-        long purchaseAmount = Long.parseLong(Console.readLine());
-        System.out.println();
+        long purchaseAmount = 0;
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                System.out.println(PURCHASE_AMOUNT_INPUT);
+                String inputString = Console.readLine();
+                purchaseAmount = validator.validateString(inputString);
+                isValidInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         return purchaseAmount;
     }
 
-    public String getWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_INPUT);
-        return Console.readLine();
+
+    public List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers = new ArrayList<>();
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                System.out.println(WINNING_NUMBERS_INPUT);
+                winningNumbers = splitWinningNumbersString(Console.readLine());
+                isValidInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return winningNumbers;
     }
 
-    public int getBonusNumber() {
-        System.out.println(BONUS_NUMBER_INPUT);
-        return Integer.parseInt(Console.readLine());
+    public int getBonusNumber(List<Integer> winningNumbers) {
+        String inputBonusNumber = null;
+        boolean isValidInput = false;
+
+        while (!isValidInput) {
+            try {
+                System.out.println(BONUS_NUMBER_INPUT);
+                inputBonusNumber = Console.readLine();
+                validator.validateBonusNumbers(winningNumbers, inputBonusNumber);
+                isValidInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return Integer.parseInt(inputBonusNumber);
     }
 
-    public List<Integer> splitNumbers(String inputNumbers) {
-        return Arrays.stream(inputNumbers.split(HASH))
+    public List<Integer> splitWinningNumbersString(String winningNumbersString) {
+        List<Integer> winningNumbers = Arrays.stream(winningNumbersString.split(HASH))
                 .map(number -> Integer.parseInt(number))
                 .collect(Collectors.toList());
+        Validator.validateLottoNumbers(winningNumbers);
+        return winningNumbers;
     }
+
 }

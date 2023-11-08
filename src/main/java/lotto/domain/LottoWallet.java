27e5758eecae9e4848193d066a391d82a63f2fd6
taekgyun <show9510@naver.com>
2023-11-08 22:40:57 +0900

    구매한 로또 관리

---
+++ src/main/java/lotto/domain/LottoWallet.java
@@ -0,0 +1,23 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoWallet {
+    private final List<Lotto> myLotto = new ArrayList<Lotto>();
+
+    public LottoWallet() {
+    }
+
+    public void addLotto(List<Integer> lottoNumbers) {
+        myLotto.add(new Lotto(lottoNumbers));
+    }
+
+    public void clearWallet() {
+        myLotto.clear();
+    }
+
+    public List<Lotto> getLotto() {
+        return myLotto;
+    }
+}

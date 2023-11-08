d5527a3c43ba65c356daa5343b0395b47a489c56
Juho <jeuslee99@naver.com>
2023-11-08 21:29:57 +0900

    add: BuyLotto and CheckAmountLotto

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,41 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //가격 입력
+        System.out.println("구입금액을 입력해 주세요.");
+        int amount = Integer.parseInt(Console.readLine());
+        BuyLotto(amount);
+
+    }
+
+    public static void BuyLotto(int amountLotto) {
+        CheckAmountLotto(amountLotto);
+
+        int realAmount = amountLotto / 1000;
+        System.out.println("\n" + realAmount+ "개를 구매했습니다.");
+
+        ArrayList<Lotto> lottoList = new ArrayList<>();
+
+        for(int i=0; i<realAmount; i++) {
+            //한 횟수당 랜덤하게 6개씩 뽑을 것
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1,45,6);
+            Lotto lottoInstance = new Lotto(numbers);
+            System.out.println(numbers);
+
+            lottoList.add(lottoInstance);
+        }
+    }
+
+    public static void CheckAmountLotto(int amountLotto) {
+        if(amountLotto % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 단위로 입력해야 합니다.");
+        }
     }
 }

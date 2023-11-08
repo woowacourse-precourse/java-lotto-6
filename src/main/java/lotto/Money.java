189b9c3bbe3189330033947d94c3cf065cc785e2
aconite <aconite65@gmail.com>
2023-11-08 18:28:44 +0900

    feat: 1000원 단위로 나누어 떨어지지 않을 경우 예외 처리 기능 추가

--- src/main/java/lotto/Money.java
+++ src/main/java/lotto/Money.java
@@ -6,7 +6,12 @@ public class Money {
 
     private final int money;
 
-    public Money(int money) {}
+    public Money(int money) {
+        if (money < 1000) {
+            throw new IllegalArgumentException("[ERROR] 금액은 1000원 이상이어야 합니다.");
+        }
+        this.money = money;
+    }
 
     public int geBuyTicket() {
         return money / TICKET_PRICE;

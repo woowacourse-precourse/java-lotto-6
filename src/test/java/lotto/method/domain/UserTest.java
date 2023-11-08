8d36255d6f7abf306f1fbd94287a624587ad6555
JP-company <wjsdj2008@gmail.com>
2023-11-08 22:13:48 +0900

    Test: [수정] userInput 메서드 UserTest 클래스에 포함시킴

--- src/test/java/lotto/method/domain/UserTest.java
+++ src/test/java/lotto/method/domain/UserTest.java
@@ -1,12 +1,13 @@
 package lotto.method.domain;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.domain.Lotto;
 import lotto.domain.LottoWinningNumber;
 import lotto.domain.User;
-import lotto.method.domain.uilogic.UserInterfaceLogic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,14 +18,14 @@ public class UserTest {
     @Test
     @DisplayName("로또 구입 금액을 입력하면 금액에 따라 로또를 반환한다.")
     void inputLottoPayment() {
-        UserInterfaceLogic.setUserInput("8000");
+        setUserInput("8000");
         assertThat(user.buyLotto()).size().isEqualTo(8);
     }
 
     @Test
     @DisplayName("잘못된 로또 구입 금액을 입력하면 정상적인 값을 입력할 때까지 다시 요청한다.")
     void requestAgainWhenInputWrongPayment() {
-        UserInterfaceLogic.setUserInput("8000q\n8111\n8000");
+        setUserInput("8000q\n8111\n8000");
         assertThat(user.buyLotto()).size().isEqualTo(8);
     }
 
@@ -35,7 +36,7 @@ public class UserTest {
         lottoWinningNumber.setLotto(new Lotto(List.of(1,2,3,4,5,6)));
         lottoWinningNumber.setBonusNumber(7);
 
-        UserInterfaceLogic.setUserInput("1,2,3,4,5,6\n7");
+        setUserInput("1,2,3,4,5,6\n7");
         assertThat(user.createWinningLottoNumber()).isInstanceOf(LottoWinningNumber.class);
     }
 
@@ -46,8 +47,13 @@ public class UserTest {
         lottoWinningNumber.setLotto(new Lotto(List.of(1,2,3,4,5,6)));
         lottoWinningNumber.setBonusNumber(7);
 
-        UserInterfaceLogic.setUserInput("1,2,3,4,5,5\n1,2,3,4,5,6f\n1,2,3,4,5,6" +  // 로또 당첨 번호 재입력
+        setUserInput("1,2,3,4,5,5\n1,2,3,4,5,6f\n1,2,3,4,5,6" +  // 로또 당첨 번호 재입력
                                         "\n6\n0\n7");                               // 보너스 번호 재입력
         assertThat(user.createWinningLottoNumber()).isInstanceOf(LottoWinningNumber.class);
+    }
+
+    private void setUserInput(String userInput) {
+        Console.close();
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
     }
 }

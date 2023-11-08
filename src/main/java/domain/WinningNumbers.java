bc2c2efcb299107f31cd7f34ed64bcf800a31b76
KwangHo <skh990427@naver.com>
2023-11-08 13:23:30 +0900

    feat: 당첨번호 및 보너스 번호 입력 기능 구현

---
+++ src/main/java/domain/WinningNumbers.java
@@ -0,0 +1,97 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import enums.LottoError;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class WinningNumbers {
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+
+    public WinningNumbers() {
+        this.winningNumbers = new ArrayList<>();
+    }
+
+    public void inputWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        while (true) {
+            String winningNumbersInput = Console.readLine();
+
+            try {
+                this.winningNumbers = parseWinningNumbersInput(winningNumbersInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void inputBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        while (true) {
+            String bonusNumberInput = Console.readLine();
+
+            try {
+                this.bonusNumber = parseBonusNumberInput(bonusNumberInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private List<Integer> parseWinningNumbersInput(String winningNumbersInput) {
+        StringTokenizer token = new StringTokenizer(winningNumbersInput, ",");
+        List<Integer> winNumber = new ArrayList<>();
+        if (token.countTokens() != 6)
+            throw new IllegalArgumentException(LottoError.NUMBER_COUNT_ERROR.getMessage());
+        for (int i = 0; i < 6; i++) {
+            inputLottoNumbersCheck(token, winNumber);
+        }
+        return winNumber;
+    }
+
+    private static void inputLottoNumbersCheck(StringTokenizer token, List<Integer> winNumber) {
+        try {
+            String inputToken = token.nextToken();
+            if (inputToken.equals(" "))
+                throw new IllegalArgumentException(LottoError.LOTTO_BLANK_ERROR.getMessage());
+            int num = Integer.parseInt(inputToken);
+            if (num < 1 || num > 45)
+                throw new IllegalArgumentException(LottoError.NUMBER_RANGE_ERROR.getMessage());
+            if(winNumber.contains(num))
+                throw new IllegalArgumentException(LottoError.NUMBER_DUPLICATE_ERROR.getMessage());
+            winNumber.add(num);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoError.NUMBER_FORMAT_ERROR.getMessage());
+        }
+    }
+
+    private int parseBonusNumberInput(String bonusNumberInput) {
+        int bonusNumber;
+        try {
+            bonusNumber = Integer.parseInt(bonusNumberInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(LottoError.NUMBER_FORMAT_ERROR.getMessage());
+        }
+
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException(LottoError.NUMBER_RANGE_ERROR.getMessage());
+        }
+        if (this.winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException(LottoError.NUMBER_DUPLICATE_ERROR.getMessage());
+        }
+        return bonusNumber;
+    }
+}
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,6 +3,7 @@ package lotto;
 import domain.LottoGenerator;
 import domain.LottoPrinter;
 import domain.PurchaseInput;
+import domain.WinningNumbers;
 
 import java.util.List;
 
@@ -10,11 +11,16 @@ public class Application {
     public static void main(String[] args) {
         PurchaseInput purchaseInput = new PurchaseInput();
         LottoGenerator lottoGenerator = new LottoGenerator();
+        WinningNumbers winningNumbers = new WinningNumbers();
 
         int purchaseAmount = purchaseInput.getPurchaseAmount();
         List<List<Integer>> lottoNumbers = lottoGenerator.generateLottoNumbers(purchaseAmount);
 
         System.out.println("\n" + lottoNumbers.size() + "개를 구매했습니다.");
         LottoPrinter.printFormattedLottoNumbers(lottoNumbers);
+
+        winningNumbers.inputWinningNumbers();
+        winningNumbers.inputBonusNumber();
+
     }
 }

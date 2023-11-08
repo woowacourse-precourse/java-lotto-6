5b19ad6652352b465924be0904e5f9aa826c5144
SuhyungK <dook4966@gmail.com>
2023-11-08 14:30:17 +0900

    add(StaticMessage): 문자열 출력 형식에 맞춰 \n 추가

--- src/main/java/lotto/constants/StaticMessage.java
+++ src/main/java/lotto/constants/StaticMessage.java
@@ -1,11 +1,11 @@
 package lotto.constants;
 
 public enum StaticMessage {
-    INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_WINNING_NUMBERS("\n당첨 번호를 입력해 주세요."),
     INPUT_PAYMENT("구입금액을 입력해 주세요."),
-    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("\n보너스 번호를 입력해 주세요."),
     PRINT_LOTTO_COUNT("개를 구매했습니다."),
-    PRINT_RESULT_NOTICE("당첨 통계%n---");
+    PRINT_RESULT_NOTICE("\n당첨 통계\n---");
 
     private final String message;
 

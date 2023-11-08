530b68e747c8593ebc1e07186f23ce4ed30e620a
mksystem98 <mksystem98@gmail.com>
2023-11-08 20:56:16 +0900

    feat: LottoErrorCodes에 중복된 숫자가 있음을 의미하는 상수 추가
    LottoErrorCodes.DUPLICATE_NUMBER_ERROR 는 중복이 있을 때 나타내는 오류메세지를 상수로 선언한 것

--- src/main/java/lotto/enums/LottoErrorCodes.java
+++ src/main/java/lotto/enums/LottoErrorCodes.java
@@ -5,10 +5,11 @@ public enum LottoErrorCodes {
     INVALID_INTEGER_INPUT("[ERROR] 정수형 숫자만 입력할 수 있습니다."),
     MINIMUM_AMOUNT_ERROR("[ERROR] 구매 금액은 최소 " + LottoConstants.LOTTO_TICKET_PRICE.getValue() + "원 입니다."),
     AMOUNT_UNIT_ERROR("[ERROR] 구매 금액은 " + LottoConstants.LOTTO_TICKET_PRICE.getValue() + "원 단위로 입력해야 합니다."),
-    DIFFERENT_BONUS_NUMBER_ERROR("[ERROR] 보너스 번호는 당첨 번호와 다른 번호여야 합니다: "),
+    DIFFERENT_BONUS_NUMBER_ERROR("[ERROR] 보너스 번호는 당첨 번호와 다른 번호여야 합니다."),
     NUMBERS_ONLY_INPUT("[ERROR] 숫자만 입력할 수 있습니다."),
     NUMBER_RANGE_ERROR("[ERROR] 숫자는 %d에서 %d 사이어야 합니다.", LottoConstants.MINIMUM_NUMBER.getValue(), LottoConstants.MAXIMUM_NUMBER.getValue()),
-    EXACT_NUMBER_REQUIRED("[ERROR] 정확히 " + LottoConstants.NUMBERS_PER_TICKET.getValue() + "개의 숫자를 입력해야 합니다.");
+    EXACT_NUMBER_REQUIRED("[ERROR] 정확히 " + LottoConstants.NUMBERS_PER_TICKET.getValue() + "개의 숫자를 입력해야 합니다."),
+    DUPLICATE_NUMBER_ERROR("[ERROR] 중복된 숫자가 있습니다.");
 
     private final String message;
     private final int minimumNumber;

b3770c97c868e07de74e9a06aad46237fb9724f6
gdakate <gdakate1215@gmail.com>
2023-11-08 15:32:19 +0900

    Feat : 상수 만들기
    - ErrorMessage : 에러 메세지 상수로 지정
    - Constants : 1000 (로또 최소값), 6(로또번호 수) 를 상수로 지정

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,10 @@
+package lotto.util;
+
+public class ErrorMessage {
+    public static final String NOT_THOUSAND_UNIT = "[ERROR] 구입 금액은 1000 단위여야 합니다.";
+    public static final String NOT_INTEGER ="[ERROR] 구입 금액은 정수여야 합니다.";
+    public static final String INVALID_INPUT ="[ERROR] 당첨 숫자는 6개여야 하며 쉼표로 구분되어야합니다.";
+    public static final String INVALID_NUMBER_RANGE ="[ERROR] 번호는 1에서 45 사이의 숫자여야합니다.";
+    public static final String DUPLICATE_NUMBER = "[ERROR] 로또 번호는 중복될 수 없습니다.";
+    public static final String OVER_SIZE ="[ERROR] 로또 번호는 6개여야 합니다.";
+}

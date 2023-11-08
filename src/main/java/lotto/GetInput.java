1668cbc0455cd923bd194a9a59654655b5f008be
ChanbyeongPark <coldbottle@korea.ac.kr>
2023-11-08 19:08:24 +0900

    feat: 로또 구입 금액 입력 예외 처리 구현

--- src/main/java/lotto/GetInput.java
+++ src/main/java/lotto/GetInput.java
@@ -5,7 +5,48 @@ import camp.nextstep.edu.missionutils.Console;
 public class GetInput {
     public static int askPurchaseMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        String purchaseMoney = Console.readLine();
-        return Integer.parseInt(purchaseMoney);
+        while (true) {
+            String purchaseMoney = Console.readLine();
+            if (!isPurchaseMoneyInteger(purchaseMoney)) {
+                continue;
+            }
+            if (!isPurchaseMoneyPositive(purchaseMoney)) {
+                continue;
+            }
+            if (!isPurchaseMoneyThousands(purchaseMoney)) {
+                continue;
+            }
+            return Integer.parseInt(purchaseMoney);
+        }
+    }
+
+    public static boolean isPurchaseMoneyInteger(String purchaseMoney) {
+        try {
+            CheckInputValidation.isItInteger(purchaseMoney);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 구입금액은 숫자로 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isPurchaseMoneyPositive(String purchaseMoney) {
+        try {
+            CheckInputValidation.isItPositive(purchaseMoney);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 구입금액은 양수로 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isPurchaseMoneyThousands(String purchaseMoney) {
+        try {
+            CheckInputValidation.isItThousands(purchaseMoney);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("[ERROR] 구입금액은 1000의 배수로 입력해주세요.");
+            return false;
+        }
+        return true;
     }
 }

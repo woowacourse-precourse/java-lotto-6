2bf9009e843c21da7eb4a3c0a5dbe765ebd6fe62
yookyung Choi <ykc131719@gmail.com>
2023-11-08 19:28:35 +0900

    feat: Lotto model에서 로또 번호 중 중복값이 있을 경우 예외처리

--- src/main/java/lotto/Model/Lotto.java
+++ src/main/java/lotto/Model/Lotto.java
@@ -1,6 +1,9 @@
 package lotto.Model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -12,8 +15,14 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(); //입력값이 6개가 아닐 경우
         }
+
+        //중복값 확인
+        Set<Integer> checkSet = new HashSet<Integer>(numbers);
+        List<Integer> newNumbers = new ArrayList<Integer>(checkSet);
+        if(newNumbers.size() != 6)
+            throw new IllegalArgumentException(); //중복값 존재할 경우
     }
 
     // TODO: 추가 기능 구현

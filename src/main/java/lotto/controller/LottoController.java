74dc2411c99c312b85a6c8f31a38accbeea19fc2
bokyung <bbookng@gmail.com>
2023-11-08 16:08:14 +0900

    fix(LottoController) : playLotto 외 Logic 다른 객체로 분리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,11 +1,9 @@
 package lotto.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import lotto.domain.Lotto;
 import lotto.domain.LottoList;
+import lotto.domain.LottoMaker;
 import lotto.domain.WinningNumbers;
-import lotto.utils.RandomNumbersGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -13,28 +11,15 @@ public class LottoController {
 
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
-    private final RandomNumbersGenerator randomNumbersGenerator = new RandomNumbersGenerator();
 
     public void playLotto() {
         long purchaseAmount = inputView.getPurchaseAmount();
-        LottoList lottoList = new LottoList(makeLottoList(purchaseAmount));
+        LottoList lottoList = new LottoList(LottoMaker.makeLottoList(purchaseAmount));
         outputView.printLottoList(lottoList);
 
-        WinningNumbers winningNumbers = generateWinningNumbers();
-    }
-
-    public List<Lotto> makeLottoList(long purchaseAmount) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < purchaseAmount / 1000; i++) {
-            List<Integer> lottoNumbers = randomNumbersGenerator.randomNumbersGenerate();
-            lottoList.add(new Lotto(lottoNumbers));
-        }
-        return lottoList;
-    }
-
-    public WinningNumbers generateWinningNumbers() {
-        String winningNumber = inputView.getWinningNumbers();
-        int bonusNumber = inputView.getBonusNumber();
-        return new WinningNumbers(winningNumber, bonusNumber);
+        List<Integer> winningNumber = inputView.getWinningNumbers();
+        int bonusNumber = inputView.getBonusNumber(winningNumber);
+        WinningNumbers winningNumbers = new WinningNumbers(winningNumber, bonusNumber);
+        outputView.printTotalResult(lottoList.getLottoResult(winningNumbers).toString());
     }
 }

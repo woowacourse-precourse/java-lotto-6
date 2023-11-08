22639fa3670845406cebb2f3c32ab56211d8945f
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:00:31 +0900

    [feat] Domain - BonusNumber 객체 관리 및 저장

--- src/main/java/lotto/Domain/BonusNumber.java
+++ src/main/java/lotto/Domain/BonusNumber.java
@@ -1,4 +1,13 @@
 package lotto.Domain;
 
 public class BonusNumber {
+    private int bonusNumber;
+
+    public BonusNumber(int bonusNumber) {
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
 }

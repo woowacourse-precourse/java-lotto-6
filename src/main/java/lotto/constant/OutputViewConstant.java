68b7e7e6d1217917edc5bcac30c3af2fc1229a3e
jay <wlguswhd0809@naver.com>
2023-11-08 21:08:32 +0900

    style: Output Constant 구분

---
+++ src/main/java/lotto/constant/OutputViewConstant.java
@@ -0,0 +1,15 @@
+package lotto.constant;
+
+public class OutputViewConstant {
+    public static final String PURCHASE_MESSAGE = "개를 구매했습니다.\n";
+    public static final String WINNING_STATS_MESSAGE = "당첨 통계\n"
+            + "---";
+    public static final String FIFTH_PRIZE_MESSAGE = "3개 일치 (5,000원) - ";
+    public static final String FOURTH_PRIZE_MESSAGE = "4개 일치 (50,000원) - ";
+    public static final String THIRD_PRIZE_MESSAGE = "5개 일치 (1,500,000원) - ";
+    public static final String SECOND_PRIZE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String FIRST_PRIZE_MESSAGE = "6개 일치 (2,000,000,000원) - ";
+    public static final String COUNT_POSTFIX_MESSAGE = "개";
+    public static final String RETURN_RATE_MESSAGE_PREFIX = "총 수익률은 ";
+    public static final String RETURN_RATE_MESSAGE_POSTFIX = "%%입니다.";
+}

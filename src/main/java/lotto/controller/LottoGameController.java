265bbca582736f28ccc6fc2dd496dd053afce245
qocksdlf123 <qocksdlf123@naver.com>
2023-11-08 22:55:54 +0900

    Feat(LottoGameController): 메인 게임 진행 로직인 play 구현

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,18 +1,41 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.GameData;
 import lotto.domain.Lotto;
+import lotto.verifier.LottoNumberVerifier;
+import lotto.verifier.LottoPurchaseVerifier;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class LottoGameController {
     private static final int LOTTO_PRICE = 1000;
+    public void play() {
+        String purchaseAmout = InputView.receiveInputWinningNumberMsg();
 
+        new LottoPurchaseVerifier(purchaseAmout);
+        int purchaseCount = Integer.parseInt(purchaseAmout);
+        List<Lotto> lottos = purchaseLottos(purchaseCount);
+        String winningnumbers = InputView.receiveInputWinningNumberMsg();
+        new LottoNumberVerifier(winningnumbers);
+        Lotto winningLotto = new Lotto(parseWinningNumbers(winningnumbers));
+        int bonusNumber = Integer.parseInt(InputView.receiveInputBonusNumberMsg());
+        GameData gameData = new GameData(winningLotto, bonusNumber, lottos);
+
+//        calculateResult(gameData);
+    }
+    public List<Integer> parseWinningNumbers(String winningnumbers){
+        return Arrays.stream(winningnumbers.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
     private List<Lotto> purchaseLottos(int purchaseAmount) {
         int lottoCount = purchaseAmount / LOTTO_PRICE;
         OutputView.printPurchaseMsg(lottoCount);
@@ -20,6 +43,8 @@ public class LottoGameController {
                 .mapToObj(i -> new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)))
                 .collect(Collectors.toList());
     }
+
+
 
 
 

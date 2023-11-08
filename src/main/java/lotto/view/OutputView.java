914768fc65a08521c760c4393d4a8579a9f3fbef
peeaaccee <wlgp8958@naver.com>
2023-11-08 23:19:52 +0900

    feat(OutputView): "당첨 번호를 입력해 주세요." 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class OutputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String CHECK_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_NUMBERS_MESSAGE = "당첨 번호를 입력해 주세요.";
+
     public static void printInputAmount() {
         System.out.println(INPUT_AMOUNT_MESSAGE);
     }
@@ -19,5 +21,9 @@ public class OutputView {
         for (Lotto lotto : purchasedNumbers) {
             System.out.println(lotto.getNumbers().toString());
         }
+    }
+
+    public static void printWinningNumbers() {
+        System.out.println(WINNING_NUMBERS_MESSAGE);
     }
 }

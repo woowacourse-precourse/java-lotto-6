eefb3a9115f8b94992de03cfe3ae3bb3cc4c309c
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-08 13:36:43 +0900

    [FEAT] 로또 발행 갯수 확인

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -9,8 +9,25 @@ public class LottoTicket {
         this.tickets = tickets;
     }
 
+    public void printTickets() {
+        for (Lotto lotto : tickets) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
     public static LottoTicket purchase(int amount) {
-        return null;
+        if (amount % 1000 != 0) {
+            throw new IllegalArgumentException("로또 한 장은 1,000원입니다.");
+        }
+
+        int numberOfTickets = amount / 1000;
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            tickets.add(Lotto.generateRandomLotto());
+        }
+
+        return new LottoTicket(tickets);
     }
 
 }

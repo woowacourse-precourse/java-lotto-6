c99f23adfd934b7b097d8c1274df2d3200020c70
ebang <dskhe8@gmail.com>
2023-11-08 21:24:50 +0900

    test : Game이 보너스 번호를 저장하는 로직 검증 테스트 구현

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -6,6 +6,8 @@ import java.util.List;
 import lotto.Validator.LottoValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GameTest {
     @DisplayName("게임서비스는 유저로부터 당첨번호를 입력받아 저장한다.")
@@ -17,4 +19,13 @@ public class GameTest {
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
         assertThat(lotto.getHowManySameNumbers(game.getWinningLotto())).isEqualTo(LottoValidator.getLottoSize());
     }
+    @DisplayName("게임서비스는 유저로부터 보너스 번호를 입력받아 저장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void setWinningNumbers(int n) {
+        Game game = new Game();
+        game.setBonusNumber(n);
+        assertThat(game.getBonusNmber()).isEqualTo(n);
+    }
+
 }

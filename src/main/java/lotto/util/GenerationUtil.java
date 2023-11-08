2e98c39c9cb9c1f34da206de4c359692f6794959
Sinorang <aodkrak@naver.com>
2023-11-08 18:41:53 +0900

    feat. 로또 번호 반복 생성 메소드 구현
    - 매개변수로 전달된 값만큼 로또 번호를 생성하는 기능 구현
    - 로또 번호 생성 메소드를 호출하여 리스트에 추가하여 리스트를 반환

--- src/main/java/lotto/util/GenerationUtil.java
+++ src/main/java/lotto/util/GenerationUtil.java
@@ -1,7 +1,9 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import lotto.domain.Lotto;
 import lotto.enums.ConstVariable;
 
 public class GenerationUtil {
@@ -12,5 +14,15 @@ public class GenerationUtil {
                 ConstVariable.MAX_NUMBER.getValue(),
                 ConstVariable.LOTTO_COUNT.getValue()
         );
+    }
+
+    public static List<Lotto> generateLottos(int count) {
+        List<Lotto> generatedLottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            Lotto lotto = Lotto.create();
+            System.out.println(lotto.printNumbers());
+            generatedLottos.add(lotto);
+        }
+        return generatedLottos;
     }
 }

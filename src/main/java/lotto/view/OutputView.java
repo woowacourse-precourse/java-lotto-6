1756ce099142f76bad90bf680498175b0c36ea80
JoonKyo <joonkyolee@gmail.com>
2023-11-08 17:32:14 +0900

    feat(OutputView): 로또 게임 수와 발행된 로또를 출력하는 메서드 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,9 @@ package lotto.view;
 
 import static lotto.view.OutputMessage.*;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public class OutputView {
     public void askPurchaseAmount() {
         System.out.println(ENTER_PURCHASE_AMOUNT.getMessage());
@@ -16,6 +19,24 @@ public class OutputView {
         printNewLine();
         System.out.println(ENTER_BONUS_NUMBER.getMessage());
     }
+
+    public void printPurchasedResult(List<Lotto> purchasedLotto, int purchasedGameCount) {
+        printNewLine();
+        printPurchasedGameCount(purchasedGameCount);
+        printPurchasedLottoNumbers(purchasedLotto);
+    }
+
+    private void printPurchasedGameCount(int purchasedGameCount) {
+        System.out.printf(SHOW_LOTTO_GAME_COUNT.getMessage(), purchasedGameCount);
+        printNewLine();
+    }
+
+    private void printPurchasedLottoNumbers(List<Lotto> purchasedLotto) {
+        for (Lotto eachLotto : purchasedLotto) {
+            System.out.println(eachLotto.getNumbers());
+        }
+    }
+
 
     public void printNewLine() {
         System.out.println();

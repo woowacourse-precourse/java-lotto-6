8cfd6afb9474fabc4d5bdb167ac884730b388369
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 18:33:32 +0900

    feat(LottoController): 컨트롤러 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,28 @@
 package lotto.controller;
 
+import lotto.LottoWinningCase;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
+import lotto.domain.Money;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static lotto.constant.ErrorMessage.ERROR_INTRO;
+
 public class LottoController {
-//    private LottoService lottoService = new LottoService();
+    private LottoService lottoService = new LottoService();
+    private Money mySpentAmount;
+    private List<Lotto> myLottoTickets;
+    private List<Integer> winningLottoNumber;
+    private BonusNumber bonusNumber;
+    private Map<LottoWinningCase, Integer> winStatisticMap = new HashMap<>();
+    private Integer totalIncome;
+    private String incomeRate;
 
     /*
      * run() {
@@ -15,16 +36,55 @@ public class LottoController {
      * */
 
     public void run() {
-
+        try{
+            savePurchaseAmount();
+            generateAndSaveRandomNumber();
+            saveWinningNumber();
+            saveBonusNumber();
+            performNumberComparisonLogic();
+            printWinningStatistics();
+        } catch (Exception e) {
+            System.out.println(ERROR_INTRO + e.getMessage());
+        }
     }
 
     private void savePurchaseAmount() {
-
+        mySpentAmount = new Money(InputView.receiveSpentAmount());
     }
 
-    private void generateAndSaveRandomNumber() {}
-    private void saveWinningNumber() {}
-    private void saveBonusNumber() {}
-    private void performNumberComparisonLogic() {}
-    private void printWinningStatistics() {}
+    private void generateAndSaveRandomNumber() {
+        int availableNumberOfLotteryTickets = lottoService.calculateAvailableNumberOfLotteryTickets(mySpentAmount.getSpendAmount());
+        myLottoTickets = lottoService.generateRandomLottoNums(availableNumberOfLotteryTickets);
+
+        OutputView.printTicketAmountAndEachRandomNumbers(availableNumberOfLotteryTickets, myLottoTickets);
+    }
+
+    private void saveWinningNumber() {
+        winningLottoNumber = lottoService.reshapeWinningNumber(InputView.receiveWinningNumber());
+    }
+    private void saveBonusNumber() {
+        bonusNumber = new BonusNumber(InputView.receiveBonusNumber(), winningLottoNumber);
+    }
+    private void performNumberComparisonLogic() {
+        initWinStatisticMap();
+        for (Lotto myLottoTicket : myLottoTickets) {
+            LottoWinningCase lottoWinningCase = lottoService.compareMyNumberWithWinningNumber(myLottoTicket, winningLottoNumber, bonusNumber.getBonusNumber());
+            Integer prevCaseCount = winStatisticMap.get(lottoWinningCase);
+            winStatisticMap.put(lottoWinningCase, prevCaseCount+1);
+        }
+        totalIncome = lottoService.calculateTotalIncome(winStatisticMap);
+        incomeRate = lottoService.calculateIncomeRate(totalIncome, mySpentAmount.getSpendAmount());
+    }
+
+    private void initWinStatisticMap() {
+        LottoWinningCase[] lottoWinningCases = LottoWinningCase.values();
+        for (LottoWinningCase lottoWinningCase : lottoWinningCases) {
+            winStatisticMap.put(lottoWinningCase, 0);
+        }
+    }
+
+    private void printWinningStatistics() {
+        OutputView.printWinningStatistics(winStatisticMap, incomeRate);
+    }
+
 }

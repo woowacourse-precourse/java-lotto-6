19756e7bce59f46981b1ec9c6cea935b6428ff4d
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 14:37:38 +0900

    refactor: ErrorMessage 변경

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -6,17 +6,17 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class User {
-//    사용자는 구입 금액과 당첨 번호, 보너스 번호를 각각 입력해야 한다.
+    //    사용자는 구입 금액과 당첨 번호, 보너스 번호를 각각 입력해야 한다.
     public int inputPurchaseAmount() {
         String input = Console.readLine();
         try {
             int money = Integer.parseInt(input);
             validatePurchaseAmount(money);
             return money;
-        }catch (NumberFormatException e){ // 숫자인지 확인 가능
-            System.out.println("[ERROR] 1000원 단위의 숫자를 입력해주세요");
-        }catch (IllegalArgumentException e){
-            System.out.println(e);
+        } catch (NumberFormatException e) { // 숫자인지 확인 가능
+            System.out.println(ErrorMessage.INVALID_PURCHASE_AMOUNT_CHARACTER.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
         return inputPurchaseAmount();
     }
@@ -27,18 +27,18 @@ public class User {
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
             return numbers;
-        }catch (NumberFormatException e){
-            System.out.println("[ERROR] 숫자로만 이루어진 6자리를 입력하세요");
+        } catch (NumberFormatException e) {//사용자가 당첨 번호를 숫자가 아닌 문자로 입력했을 경우
+            System.out.println(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
         }
         return inputLottoNumber();
     }
 
-    private void validatePurchaseAmount(int money){
-        if(money<1000||money>100000){// 1000원 미만, 100000원 초과의 값 입력시의 예외 처리
-            throw new IllegalArgumentException("[ERROR] 1000원 이상 100000원 이하의 값을 입력해주세요");
+    private void validatePurchaseAmount(int money) {
+        if (money < 1000 || money > 100000) {// 1000원 미만, 100000원 초과의 값 입력시의 예외 처리
+            throw new IllegalArgumentException(ErrorMessage.INVALID_PURCHASE_AMOUNT_NUMBER.getMessage());
         }
-        if(money%1000!=0){// 1000원 단위의 값 외의 값 입력시의 예외 처리
-            throw new IllegalArgumentException("[ERROR] 1000원 단위의 값을 입력해주세요");
+        if (money % 1000 != 0) {// 1000원 단위의 값 외의 값 입력시의 예외 처리
+            throw new IllegalArgumentException(ErrorMessage.INVALID_AMOUNT_UNIT.getMessage());
         }
     }
 }

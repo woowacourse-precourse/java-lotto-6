a4ef9d918de4ff09e10af9610101ebbc74b3e8cd
Park Eunhyeong <silbroeh@gmail.com>
2023-11-08 16:46:50 +0900

    test : Lotto 단위테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +25,32 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 1~45 범위 외의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("다른 로또와의 일치 개수")
+    @Test
+    void testCountMatches() {
+        Lotto lotto1 = new Lotto(List.of(5,10,15,20,25,30));
+        Lotto lotto2 = new Lotto(List.of(20,25,30,35,40,45));
+        int result = lotto1.countMatches(lotto2);
+        int answer = 3;
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @DisplayName("보너스 번호와의 일치 여부")
+    @Test
+    void testMatchWithBonus() {
+        Lotto lotto = new Lotto(List.of(7,15,22,29,37,45));
+        int bonusNumber = 29;
+        boolean result = lotto.matchWithBonus(bonusNumber);
+        boolean answer = true;
+
+        assertThat(result).isEqualTo(answer);
+    }
 }

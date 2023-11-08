5974cbea5ba0be5b2699dd47c6739314bd02d84e
KDT live <dnjsxo0616@gmail.com>
2023-11-08 16:16:53 +0900

    feat(Rank): Rank 열거형 추가
    - 로또 등수와 그에 따른 상금을 정의.
    - 열거형 상수는 매치된 숫자 개수, 상금, 보너스 번호 여부 및 결과 판단 함수 포함.

---
+++ src/main/java/lotto/domain/Rank.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.function.BiFunction;
+
+public enum Rank {
+    NONE(0, 0, false, (matchCount, bonus) -> matchCount <= 2),
+    FIFTH(3, 5_000, false, (matchCount, bonus) -> matchCount == 3),
+    FOURTH(4, 50_000, false, (matchCount, bonus) -> matchCount == 4),
+    THIRD(5, 1_500_000, false ,(matchCount, bonus) -> matchCount == 5 && !bonus),
+    SECOND(5, 30_000_000, true, (matchCount, bonus) -> matchCount == 5 && bonus),
+    FIRST(6, 2_000_000_000, false, (matchCount, bonus) -> matchCount == 6);
+
+    private final int matchCount;
+    private final int prize;
+    private final boolean bonus;
+    private final BiFunction<Integer, Boolean, Boolean> matchResult;
+
+    Rank(final int matchCount, final int prize, final boolean bonus, final BiFunction<Integer, Boolean, Boolean> matchResult){
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.bonus = bonus;
+        this.matchResult = matchResult;
+    }
+
+}

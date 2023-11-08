1097fc75cd524c0710a0783b663ae00f1c1425d9
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 18:09:21 +0900

    refactor(MakeLotto) : static 함수 올리고 코드 정렬

--- src/main/java/lotto/util/MakeLotto.java
+++ src/main/java/lotto/util/MakeLotto.java
@@ -6,18 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MakeLotto {
+    private static Lotto generateLotto() {
+        MakeRandomNumber makeRandomNumber = new MakeRandomNumber();
+        return new Lotto(makeRandomNumber.getRandomNumber());
+    }
 
     public List<Lotto> makeLottoList(int lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
-        while(lottos.size() < lottoCount){
+        while (lottos.size() < lottoCount) {
             lottos.add(generateLotto());
         }
         return lottos;
-    }
-
-    private static Lotto generateLotto(){
-        MakeRandomNumber makeRandomNumber = new MakeRandomNumber();
-        return new Lotto(makeRandomNumber.getRandomNumber());
     }
 
 

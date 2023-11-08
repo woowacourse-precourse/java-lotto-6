94937e075c411a6044d0f9af4d8cda4a0c15df51
ulsandonghun <donghoon141@naver.com>
2023-11-08 21:01:13 +0900

    [feat] : 당첨번호를 입력받는 기능

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -2,8 +2,13 @@ package lotto.service;
 
 public class InputService {
     public String price;
+    public String answer;
 
     public void readPrice(String input) {
         price=input;
+    }
+
+    public void readAnswer(String input) {
+        answer=input;
     }
 }
--- src/test/java/lotto/service/InputServiceTest.java
+++ src/test/java/lotto/service/InputServiceTest.java
@@ -1,20 +1,38 @@
 package lotto.service;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class InputServiceTest {
+    InputService inputService;
+
+
+    @BeforeEach
+    public void init() {
+        inputService=new InputService();
+    }
 
     @Test
     @DisplayName("로또 구입 금액을 입력받는 기능.")
     public void input1() {
-        InputService inputService = new InputService();
         String input = "14000";
 
         inputService.readPrice(input);
-        Assertions.assertThat(inputService.price).isEqualTo("14000");
+        assertThat(inputService.price).isEqualTo("14000");
+    }
+
+    @Test
+    @DisplayName("당첨번호를 입력받는기능.")
+    public void input2() {
+        String input = "1,2,3,4,5,6";
+        inputService.readAnswer(input);
+        assertThat(inputService.answer).isEqualTo("1,2,3,4,5,6");
+
     }
 
 }

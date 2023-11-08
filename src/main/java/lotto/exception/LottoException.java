cdaaa523d8acf2bbca6cfbff20325442fc955144
강승민 <rkdtmdals999@naver.com>
2023-11-08 18:20:35 +0900

    [style] 로또 번호 범위 계산하는 조건문 상수로 변경

--- src/main/java/lotto/exception/LottoException.java
+++ src/main/java/lotto/exception/LottoException.java
@@ -4,6 +4,8 @@ import static lotto.utility.Constants.ERROR_MESSAGE_1;
 import static lotto.utility.Constants.ERROR_MESSAGE_3;
 import static lotto.utility.Constants.ERROR_MESSAGE_4;
 import static lotto.utility.Constants.LOTTO_LENGTH;
+import static lotto.utility.Constants.MAX_LOTTO_NUMBER;
+import static lotto.utility.Constants.MIN_LOTTO_NUMBER;
 
 import java.util.HashSet;
 import java.util.List;
@@ -25,7 +27,7 @@ public class LottoException {
 
     private static void validateLottoNumberRange(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number <= 0 || number > 45) {
+            if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
                 System.out.println(ERROR_MESSAGE_1);
                 throw new IllegalArgumentException(ERROR_MESSAGE_1);
             }

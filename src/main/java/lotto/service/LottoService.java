c8ef9558679fc28ea7804e0b8da36f27b5458c79
Bae Hyeonseo <haulqogustj@naver.com>
2023-11-08 16:32:54 +0900

    당첨번호 입력 로직 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -15,6 +15,29 @@ public class LottoService {
             validateAmount(amountStr);
         }
     }
+    public void inputLottoNum(){
+        System.out.println("당첨 번호를 입력하세요.");
+        String inputStr = Console.readLine();
+        String[] input = inputStr.split(",");
+        List<Integer> numbers = new ArrayList<>();
+
+        for (String numberStr : input) {
+            validateLottoNum(numbers,numberStr);
+        }
+    }
+    private List<Integer> validateLottoNum(List<Integer> numbers, String numberStr){
+        try {
+            int number = Integer.parseInt(numberStr);
+            if(number>=1 && number<=45){
+                throw new IllegalArgumentException("[ERROR] 유효하지 로또 번호입니다.");
+            }
+            numbers.add(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return numbers;
+    }
     private void validateAmount(String amountStr){
         try {
             int amount = Integer.parseInt(amountStr);

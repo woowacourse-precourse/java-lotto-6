a26378389e7f7f94523e25098f3df0b69d047a65
leejuno <zhqkfxm0127@naver.com>
2023-11-08 22:55:08 +0900

    feat(Operator): 보너스번호를 비교하는 기능 구현

--- src/main/java/lotto/domain/Operator.java
+++ src/main/java/lotto/domain/Operator.java
@@ -4,17 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Operator {
-    public void matchNumbers(List<Lotto> lottos, Lotto winningLotto) {
+
+    private static final int LOTTO_SIZE = 6;
+
+    public List<Integer> matchNumbers(List<Lotto> lottos, Lotto winningLotto) {
         List<Integer> corrects = new ArrayList<>();
 
         for (Lotto candidateLotto : lottos) {
             int correctCount = 0;
-            for (int i = 0; i < candidateLotto.getNumbers().size(); i++) {
+            for (int i = 0; i < LOTTO_SIZE; i++) {
                 if (lottos.contains(candidateLotto.getNumbers().get(i))) {
                     correctCount++;
                 }
             }
             corrects.add(correctCount);
+        }
+        return corrects;
+    }
+
+    public void matchBonusNumber(List<Lotto> lottos, int bonusNumber) {
+        List<Integer> bonusCorrects = new ArrayList<>();
+
+        for (Lotto candidateLotto : lottos) {
+            int bonusCorrect = 0;
+            if (candidateLotto.getNumbers().contains(bonusNumber)) {
+                bonusCorrect++;
+            }
+            bonusCorrects.add(bonusCorrect);
         }
     }
 }

dbcd63269f3fd00586fd23a80db1d808198dd677
yohanii <dygks924@gmail.com>
2023-11-08 17:42:24 +0900

    feat: 1부터 45까지의 중복되지 않는 6개의 숫자를 뽑는다.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.List;
 
 public class LottoGame {
 
     public List<Integer> generateLottoNums() {
-        return null;
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 
     public int getLottoCounts(int price) {

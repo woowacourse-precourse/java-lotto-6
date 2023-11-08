40d20f3e5e5bcac32b4de4fdde5568d771ab8b58
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 16:27:51 +0900

    test: 로또 클래스에 대한 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -2,14 +2,19 @@ package lotto.model;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import lotto.enums.ErrorMessage;
 import lotto.model.Lotto;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
 
@@ -24,61 +29,50 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_NUMBERS_COUNT_ERROR.getMessage());
     }
 
     @DisplayName("로또 번호의 개수가 6개보다 작으면 예외가 발생한다.")
     @Test
     void createLottoByUnderSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
-            .isInstanceOf(IllegalArgumentException.class);
+            .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.INVALID_NUMBERS_COUNT_ERROR.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessage.DUPLICATE_NUMBER_ERROR.getMessage());
     }
 
     @DisplayName("로또 번호가 1부터 45 사이가 아니면 예외가 발생한다.")
-    @Test
-    void createExceedRangeNumber() {
-        /**
-         * List.of(1,2,3,4,5,0)
-         * List.of(1,2,3,4,5,100)
-         */
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
-            .isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @CsvSource({
+        "1, 2, 3, 4, 5, 0",
+        "1, 2, 3, 4, 5, 100",
+        "1, 2, 3, 4, 5 ,-6"
+    })
+    void createExceedRangeNumber(int num1, int num2, int num3, int num4, int num5, int num6) {
+        // given
+        List<Integer> numbers = List.of(num1, num2, num3, num4, num5, num6);
+        // when, then
+        assertThatThrownBy(() -> new Lotto(numbers))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.EXCEED_LOTTO_RANGE_ERROR.getMessage());
     }
 
-    @DisplayName("로또 번호에 숫자가 아닌 문자가 포함되어 있으면 예외가 발생한다.")
+    @DisplayName("로또 번호가 양식에 맞게 출력되는지 확인한다.")
     @Test
-    void createLottoByContainLetter() {
-        /**
-         * List.of(1,2,3,4,5,-1)
-         * List.of(1,2,3,4,5,a)
-         * List.of(1,2,3,4,5,0.1)
-         * List.of(1,2,3,4,5,*)
-         */
-        //TODO 테스트 수정
-        assertThatThrownBy(() -> new Lotto(List.of())).isInstanceOf(
-            IllegalArgumentException.class);
-    }
+    void lottoNumberFormatTest() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
+        // when
+        String expectedFormat = "[1, 2, 3, 4, 5, 6]";
+        String actualFormat = lotto.toString();
 
-    @DisplayName("로또 번호에 Empty 값이 있으면 예외가 발생한다. ")
-    @Test()
-    void createLottoByEmptyValue() {
-        //TODO 이 경우, 애초에 공백 문자가 들어갈 수 없는 구조라 예외 발생이 어려움. 기능명세서 수정.
-        String input = "1,2,3,4,,6";
-        List<Integer> lottoNumbers = Arrays.stream(input.split(","))
-            .map(String::trim)
-            .filter(s -> !s.isEmpty())
-            .map(Integer::parseInt)
-            .collect(Collectors.toList());
-
-        assertThatThrownBy(() -> new Lotto(lottoNumbers))
-            .isInstanceOf(IllegalArgumentException.class);
+        // then
+        assertEquals(expectedFormat, actualFormat);
     }
 }

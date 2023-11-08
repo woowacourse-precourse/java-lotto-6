0eb4d20e3b4c070142d76f7b3c7ebc3c72d801a1
kang jihyeong <her4386@gmail.com>
2023-11-08 12:48:50 +0900

    [refactor] 매지넘버로 작성된 메시지 수정

--- src/main/java/lotto/utils/ErrorPhrase.java
+++ src/main/java/lotto/utils/ErrorPhrase.java
@@ -1,7 +1,6 @@
 package lotto.utils;
 
-import static lotto.utils.LottoSystemUtils.MONEY_MAX_RANGE;
-import static lotto.utils.LottoSystemUtils.MONEY_MIN_RANGE;
+import static lotto.utils.LottoSystemUtils.*;
 
 public class ErrorPhrase {
 
@@ -9,7 +8,7 @@ public class ErrorPhrase {
 
     public static final String ERROR_PHRASE_MONEY_IS_OUT_OF_RANGE = "구입 금액은 "+MONEY_MIN_RANGE+" ~ "+MONEY_MAX_RANGE+" 사이 숫자만 가능합니다.";
 
-    public static final String ERROR_PHRASE_MONEY_IS_WRONG_UNIT = "구입 금액은 1000원 단위만 가능합니다.";
+    public static final String ERROR_PHRASE_MONEY_IS_WRONG_UNIT = "구입 금액은 "+MONEY_UNIT+"원 단위만 가능합니다.";
 
     public static final String ERROR_PHRASE_LOTTO_NUMBER_IS_NOT_DIGIT = "로또 번호는 숫자만 가능합니다.";
 

d1f03ae1bc96b2428bb8cdbbc9a921a37e537c19
wonndrive <nonyok812@gmail.com>
2023-11-08 23:51:28 +0900

    feat: 숫자 상수 입력

---
+++ src/main/java/lotto/constant/NumberConstants.java
@@ -0,0 +1,12 @@
+package lotto.constant;
+
+public final class NumberConstants {
+    public static final int LOTTO_NUMBER_SIZE = 6;
+    public static final int LOTTO_NUMBER_MIN = 1;
+    public static final int LOTTO_NUMBER_MAX = 45;
+    public static final int LOTTO_PRICE_UNIT = 1000;
+    public static final int ONE_HUNDRED = 100;
+    public static final int WINNING_NUMBER_MIN = 3;
+
+
+}

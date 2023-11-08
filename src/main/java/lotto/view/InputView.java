539ad145774687b40dd3ebbff15ad26b3d7847d2
KimKyuHwan <vkdls754@gmail.com>
2023-11-08 15:55:42 +0900

    fix(ViewClass): InputView, OutputView 클래스 공백, 필요없는 기능 이동 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -10,27 +10,20 @@ public class InputView {
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
-    public static int inputPurchasePrice() {
+    public static String inputPurchasePrice() {
         System.out.println(PURCHASE_PRICE_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+        return Console.readLine();
     }
 
-    public static List<Integer> inputWinningNumbers() {
-        System.out.println(WINNING_NUMBER_MESSAGE);
-        String input = Console.readLine();
-        return parseNumberList(input);
+    public static String inputWinningNumbers() {
+        System.out.println("\n" + WINNING_NUMBER_MESSAGE);
+        return Console.readLine();
     }
 
-    public static int inputBonusNumber() {
-        System.out.println(BONUS_NUMBER_MESSAGE);
-        return Integer.parseInt(Console.readLine());
+    public static String inputBonusNumber() {
+        System.out.println("\n" + BONUS_NUMBER_MESSAGE);
+        return Console.readLine();
     }
 
-    private static List<Integer> parseNumberList(String input) {
-        List<Integer> numbers = Stream.of(input.split(","))
-                .map(String::trim)
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        return numbers;
-    }
+
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -4,25 +4,31 @@ import java.util.List;
 import lotto.domain.Lotto;
 
 public class OutputView {
-    private static final String LOTTO_GENERATION_MESSAGE = "난수로 생성된 로또 번호 출력";
+    private static final String TICKET_SIZE_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
-    private static final String SEPARATOR = "--------------";
+    private static final String SEPARATOR = "---";
     private static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.2f%%입니다.";
+    private static final String ERROR_MESSAGE = "[ERROR] ";
+
 
     public static void printGeneratedLotto(List<Lotto> lottos) {
-        System.out.println(LOTTO_GENERATION_MESSAGE);
+        System.out.println("\n" + lottos.size() + TICKET_SIZE_MESSAGE);
+
         for (Lotto lotto : lottos) {
-            System.out.println(lotto.toString());
+            System.out.println(lotto);
         }
     }
 
-    public static void printWinningStatistics() {
-        System.out.println(WINNING_STATISTICS_MESSAGE);
+    public static void printWinningStatistics(List<Integer> winningCounts) {
+        System.out.println("\n" + WINNING_STATISTICS_MESSAGE);
         System.out.println(SEPARATOR);
-
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
         System.out.println(String.format(RATE_OF_RETURN_MESSAGE, rateOfReturn));
+    }
+
+    public static void printErrorMessage(String detailErrorMessage) {
+        System.out.println(ERROR_MESSAGE + detailErrorMessage);
     }
 }

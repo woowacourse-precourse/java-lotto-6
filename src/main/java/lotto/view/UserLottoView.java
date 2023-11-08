5b9645610688ae51f3b928841483e51521f4df22
c1ce <chalicenuclear@gmail.com>
2023-11-08 16:53:15 +0900

    fix : 출력문 오타 수정

--- src/main/java/lotto/view/UserLottoView.java
+++ src/main/java/lotto/view/UserLottoView.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class UserLottoView {
     private static final String BUY_MESSAGE="구입금액을 입력해 주세요.";
-    private static final String INPUT_LOTTO_MESSAGE="구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_MESSAGE="당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_LOTTO_MESSAGE="보너스 번호를 입력해 주세요.";
     public String inputBuyPrice(){
         System.out.println(BUY_MESSAGE);

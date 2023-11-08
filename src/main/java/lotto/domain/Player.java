e4004dd1687c38ac04e96d57fb97e28a293c8b3a
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 15:43:02 +0900

    feat(Player): 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,10 +1,24 @@
 package lotto.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
     private int purChaseAmount;
+    private List<Integer> winningNumbers;
 
     public void insertPurchaseAmount(int purChaseAmountInput) {
         this.purChaseAmount = purChaseAmountInput;
+    }
+
+    public void insertWinningNumbers(String numbers) {
+        this.winningNumbers = Arrays.stream(numbers.split(","))
+                .map(Integer::valueOf)
+                .toList();
+    }
+    
+    public List<Integer> getWinningNumbers() {
+        return winningNumbers;
     }
 
     public int getPurChaseAmount() {

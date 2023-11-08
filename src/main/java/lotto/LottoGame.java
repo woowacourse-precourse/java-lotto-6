8c4e0a07a5e6f6db796ae7973cb0d6bbb8fcda6a
noeyodeel <sko8044@gmail.com>
2023-11-08 21:43:54 +0900

    refator : 당첨티켓 확인 메소드 분리
    당첨 티켓 확인하는 메소드 분리함.

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -17,7 +17,7 @@ public class LottoGame {
       System.out.println(purchasedTicket.getNumbers());
     }
   }
-  
+
 
   public void setWinningTicket(Lotto winningTicket) {
     this.winningTicket = winningTicket;
@@ -28,24 +28,30 @@ public class LottoGame {
     int[] results = new int[5];
 
     for (Lotto ticket : purchasedTickets) {
-      int matchingNumbers = ticket.countMatchingNumbers(winningTicket);
-      if (matchingNumbers >= 3) {
-        results[matchingNumbers - 3]++;
-      }
-      if (results[2] > 0) {
-        int bonusMatching = ticket.countMatchingNumbers(bonusNumber);
-        if (bonusMatching > 0) {
-          results[4]++;
-          results[2]--;
-        }
+      calculateTicketResults(ticket, results, bonusNumber);
+    }
+
+    return results;
+  }
+
+  private void calculateTicketResults(Lotto ticket, int[] results, int bonusNumber) {
+    int matchingNumbers = ticket.countMatchingNumbers(winningTicket);
+    if (matchingNumbers >= 3) {
+      results[matchingNumbers - 3]++;
+    }
+
+    if (results[2] > 0) {
+      int bonusMatching = ticket.countMatchingNumbers(bonusNumber);
+      if (bonusMatching > 0) {
+        results[4]++;
+        results[2]--;
       }
     }
-    return results;
   }
 
   private Lotto generateRandomLotto() {
     List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    List<Integer> sortedNumbers = new ArrayList<>(numbers);  // 복제
+    List<Integer> sortedNumbers = new ArrayList<>(numbers); // 복제
     Collections.sort(sortedNumbers);
     return new Lotto(sortedNumbers);
   }

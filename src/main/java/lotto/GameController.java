c53b6d8f305b6bb62daff60be0d2fd8e4ebe8b63
mac <seunghune12@gmail.com>
2023-11-08 17:07:27 +0900

    feat: 당첨번호와 보너스번호 입력 기능

--- src/main/java/lotto/GameController.java
+++ src/main/java/lotto/GameController.java
@@ -1,8 +1,11 @@
 package lotto;
 
+import java.util.Arrays;
 import java.util.List;
 
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.domain.Money;
@@ -14,33 +17,41 @@ import lotto.Message.GameMessage;
 public class GameController {
 
     public void start() {
+
         Money count = insertMoney();
         List<Lotto> lottos = generateUserLotto(count);
-//        WinningLotto winningLotto = generateWinningLottto();
+        WinningLotto winningLotto = generateWinningLottto();
 
     }
 
     private Money insertMoney(){
+        System.out.println(GameMessage.OUT_REQUEST_MONEY_MESSAGE);
         Money count = new Money(Integer.parseInt(Input.consoleLine()));
         count.checkmoney();
         return count;
     }
 
     private List<Lotto> generateUserLotto(Money count){
+        System.out.println();
         List<Lotto> lottos =  Lottos.generateLotto(count);
         return lottos;
     }
 
     private WinningLotto generateWinningLottto(){
         Output.consoleLine(GameMessage.OUT_REQUEST_WINNING_LOTTO_MESSAGE);
-        String numbers = Input.consoleLine();
-        //validation 실행
-        Output.consoleLine(GameMessage.OUT_REQUEST_BONUS_NUMBER_MESSAGE);
-        String bonusNumbers = Input.consoleLine();
-        //validationtion실행
-//        WinningLotto winningLotto = new WinningLotto();
-        //생성
-    }
-    return
+        List<Integer> winningNumbers = splitNumber(Input.consoleLine());
 
+        Output.consoleLine(GameMessage.OUT_REQUEST_BONUS_NUMBER_MESSAGE);
+        int bonusNumbers = Integer.parseInt(Input.consoleLine());
+                WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumbers);
+
+                return winningLotto;
+    }
+
+    private List<Integer> splitNumber(String str){
+
+        return Arrays.stream(str.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
 }

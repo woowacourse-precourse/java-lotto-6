9aa7b65963bc2f73d6842576a0663087330821c0
nayonsoso <panda0329@naver.com>
2023-11-08 22:20:03 +0900

    refactor: 당첨 통계 시작 문구 전에 줄바꿈

--- src/main/java/lotto/constants/message/StatisticsMessage.java
+++ src/main/java/lotto/constants/message/StatisticsMessage.java
@@ -1,7 +1,7 @@
 package lotto.constants.message;
 
 public class StatisticsMessage {
-    public static final String START = "당첨 통계";
+    public static final String START = "\n당첨 통계";
     public static final String START_LINE = "---";
     public static final String CONTENT = "%d개 일치 (%s원) - %d개";
     public static final String CONTENT_INCLUDE_BONUS_NUMBER = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";

2e0daf70fbe234eaaa44acde77a6a978f54f914f
JoonKyo <joonkyolee@gmail.com>
2023-11-08 21:45:03 +0900

    feat(LottoGame): 로또 구매 전에 플레이어로부터 구입 금액을 입력받는 메서드 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,2 +1,38 @@
-package lotto.controller;public class LottoGame {
+package lotto.controller;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.Player;
+import lotto.domain.WinningLottoNumber;
+import lotto.exception.InputException;
+import lotto.util.ConvertInput;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGame {
+    private static Player player;
+    private static List<Lotto> playerLottos;
+    private static WinningLottoNumber winNumbers;
+
+    private LottoGame() {
+    }
+
+    public static void openStore() {
+        beforeGame();
+    }
+
+    private static void beforeGame() {
+        readPurchaseAmount();
+    }
+
+    private static void readPurchaseAmount() {
+        try {
+            OutputView.askPurchaseAmount();
+            int purchaseMoney = ConvertInput.makePlayerMoneyToInt(InputView.readInput());
+            player = Player.from(purchaseMoney);
+        } catch (InputException exception) {
+            OutputView.printErrorMessage(exception.getMessage());
+            readPurchaseAmount();
+        }
+    }
 }

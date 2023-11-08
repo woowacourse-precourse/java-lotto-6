91102ba6db1c9cd1f3de9aa0413fb454f1cf45f2
JeongHunHui <deca3248@gmail.com>
2023-11-08 22:06:59 +0900

    feat: Lottos에 당첨번호랑 보너스 번호를 줘서 LottoResult를 얻는 메소드 추가

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -1,6 +1,9 @@
 package lotto.model;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import lotto.constant.LottoPrize;
 import lotto.dto.LottoInfoResponse;
 
 public class Lottos {
@@ -16,5 +19,17 @@ public class Lottos {
             lottos.size(),
             lottos.stream().map(Lotto::getNumbers).toList()
         );
+    }
+
+    public LottoResult getLottoResult(WinningNumberAndBonusNumber winningNumberAndBonusNumber) {
+        Map<LottoPrize, Integer> prizesCount = new EnumMap<>(LottoPrize.class);
+        for (LottoPrize prize : LottoPrize.values()) {
+            prizesCount.put(prize, 0); // 초기값을 0으로 설정합니다.
+        }
+        for (Lotto lotto : lottos) {
+            final LottoPrize lottoPrize = lotto.findPrize(winningNumberAndBonusNumber);
+            prizesCount.put(lottoPrize, prizesCount.get(lottoPrize) + 1);
+        }
+        return new LottoResult(prizesCount);
     }
 }

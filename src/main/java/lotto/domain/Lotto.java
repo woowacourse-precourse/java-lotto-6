dc170352741de6531b8f96f191dbfc23c002cc3d
CJY <c0512jy@naver.com>
2023-11-08 22:10:43 +0900

    feat(Lotto.java): 로또 도메인 기능
    1. 로또 번호 개수, 최소, 최대, 가격 상수 정의
    2. 번호 정렬 기능
    3. contains 기능
    4. 다른 로또와 일치하는 번호 개수 카운팅 기능
    5. toString
    6. 생성시 개수, 범위, 중복 체크

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,70 @@
 package lotto.domain;
 
+import lotto.exception.ErrorStatus;
+import lotto.exception.LottoArgumentException;
+
 import java.util.List;
 
 public class Lotto {
+
+    public static final int SIZE = 6;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int PRICE = 1_000;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortAsc(numbers);
+    }
+
+    public boolean contains(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public int matchCount(Lotto lotto) {
+        return (int) numbers.stream()
+                .filter(lotto::contains)
+                .count();
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private List<Integer> sortAsc(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != Lotto.SIZE) {
+            throw new LottoArgumentException(ErrorStatus.LOTTO_SIZE_ERROR);
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        numbers.forEach(this::validateRange);
+    }
+
+    private void validateRange(Integer number) {
+        if (number < Lotto.MIN_NUMBER || number > Lotto.MAX_NUMBER) {
+            throw new LottoArgumentException(ErrorStatus.LOTTO_NUMBER_RANGE_ERROR);
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if (numbers.stream().distinct().count() != Lotto.SIZE) {
+            throw new LottoArgumentException(ErrorStatus.LOTTO_NUMBER_DUPLICATE_ERROR);
+        }
+    }
 }

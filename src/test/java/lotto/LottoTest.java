618997161508d0230c64ba09ad297503e7fbaf70
김서현 <kimseohyun@gimseohyeons-MacBook-Pro.local>
2023-11-08 15:24:25 +0900

    test: Modify test code

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -19,9 +19,7 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 class LottoTest {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
-    private static final int NUMBERS_PER_TICKET = 6;
+    UserController userController = new UserController();
 
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
@@ -36,7 +34,7 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호에 중복된 숫자가 존재합니다.");
+                .hasMessage("로또 번호에 중복된 숫자가 있습니다.");
     }
 
     // 아래에 추가 테스트 작성 가능
@@ -49,5 +47,35 @@ class LottoTest {
 
         List<Lotto> generatedLottos = lottoController.generateLottos(lottoMount);
         assertEquals(lottoMount, generatedLottos.size());
+    }
+
+    @DisplayName("로또 구입 가격을 입력하는 기능 테스트")
+    @Test
+    void testInvalidInputPurchasePrice() {
+        assertThatThrownBy(() -> userController.inputPriceHowManyLottos())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 구입 금액은 1,000원 단위로 입력해야 합니다.")
+                .hasMessage("로또 구입 금액은 0보다 커야 합니다.")
+                .hasMessage("로또 구입 금액은 숫자로만 입력해야 합니다.");
+    }
+
+    @DisplayName("사용자가 유효한 로또 번호를 입력하는 기능 테스트")
+    @Test
+    void testValidInput() {
+        assertThatThrownBy(() -> userController.generateUserLottoNumber())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이여야 합니다.")
+                .hasMessage("로또 번호는 숫자여야 합니다.")
+                .hasMessage("로또 번호는 중복 될 수 없습니다.");
+    }
+
+    @DisplayName("사용자가 유효한 로또 보너스 번호를 입력하는 기능 테스트")
+    @Test
+    void testValidInputBonus() {
+        assertThatThrownBy(() -> userController.inputBonusNumber())
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("로또 번호는 1부터 45 사이여야 합니다.")
+                .hasMessage("로또 번호는 숫자여야 합니다.")
+                .hasMessage("로또 번호는 중복 될 수 없습니다.");
     }
 }

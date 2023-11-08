a5780cf98f79fd2ccc8a50b5d7161baf6a240238
limzzum <chfhddl1234@naver.com>
2023-11-08 19:25:10 +0900

    :hammer: refactor : 게임 결과 dto 당첨 순위 별 개수 배열로 저장

--- src/main/java/lotto/dto/ResponseDto.java
+++ src/main/java/lotto/dto/ResponseDto.java
@@ -3,42 +3,22 @@ package lotto.dto;
 public class ResponseDto {
 
     public static class LottoGameResultDto{
-        private int rankingOneCnt;
-        private int rankingTwoCnt;
-        private int rankingThreeCnt;
-        private int rankingFourCnt;
-        private int rankingFiveCnt;
+        private int[] countWinning;
         private double rateOfReturn;
 
-        public LottoGameResultDto(){};
+        public LottoGameResultDto(){}
 
         public LottoGameResultDto(int[] countWinning, double rateOfReturn){
-            this.rankingOneCnt = countWinning[1];
-            this.rankingTwoCnt = countWinning[2];
-            this.rankingThreeCnt = countWinning[3];
-            this.rankingFourCnt = countWinning[4];
-            this.rankingFiveCnt = countWinning[5];
+            this.countWinning = countWinning;
             this.rateOfReturn = rateOfReturn;
         }
 
-        public int getRankingOneCnt() {
-            return rankingOneCnt;
+        public int[] getCountWinning() {
+            return countWinning;
         }
 
-        public int getRankingTwoCnt() {
-            return rankingTwoCnt;
-        }
-
-        public int getRankingThreeCnt() {
-            return rankingThreeCnt;
-        }
-
-        public int getRankingFourCnt() {
-            return rankingFourCnt;
-        }
-
-        public int getRankingFiveCnt() {
-            return rankingFiveCnt;
+        public double getRateOfReturn() {
+            return rateOfReturn;
         }
     }
 

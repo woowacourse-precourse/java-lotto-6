72baf7ef0bed7c40af74eba8da02e5f76ed80a7a
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 18:54:57 +0900

    feat: 로또 당첨 확인 메서드 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,2 +1,41 @@
-package lotto.service;public class LottoSevice {
+package lotto.service;
+
+import lotto.model.Lotto;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class LottoService {
+
+    public HashMap<Integer, Integer> calculateWinningResult(List<List<Integer>> randomSixNumbers, Lotto lotto, int bonusNumber) {
+        HashMap<Integer, Integer> winningResult = new HashMap<>(0);
+        for (List<Integer> randomSixNumber : randomSixNumbers) {
+            int matchingCount = winningCount(randomSixNumber, lotto.getNumbers());
+            boolean hasBonus = winningBonusNumber(randomSixNumber, bonusNumber);
+            if (!hasBonus) {
+                winningResult.put(matchingCount, winningResult.getOrDefault(matchingCount, 0) + 1);
+            }
+            if (hasBonus && matchingCount == 5) {
+                winningResult.put(matchingCount + 2, winningResult.getOrDefault(matchingCount + 2, 0) + 1);
+            }
+        }
+        return winningResult;
+    }
+
+    private int winningCount(List<Integer> randomSixNumber, List<Integer> correctNumber) {
+        int count = 0;
+        for (Integer correct : correctNumber) {
+            if (randomSixNumber.contains(correct)) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    private boolean winningBonusNumber(List<Integer> randomSixNumber, int bonusNumber) {
+        if (randomSixNumber.contains(bonusNumber)) {
+            return true;
+        }
+        return false;
+    }
 }

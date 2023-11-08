5dc5d24f2b9e0d0cb5df851845ac8e642618d336
정재웅 <jweidon0821@gmail.com>
2023-11-08 11:37:33 +0900

    feat(domain): 금액 입력값 예외 처리

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -3,14 +3,37 @@ package lotto.domain;
 import lotto.view.InputView;
 
 public class Money {
-    private final int amount;
-    private static final int moneyCondition = 1000;
+    private int amount;
+    public static final int DIVIDE_CONDITION = 1000;
 
     public Money() {
-        this.amount = InputView.inputMoney();
+        validate();
     }
 
     public int trial() {
-        return amount / moneyCondition;
+        return amount / DIVIDE_CONDITION;
+    }
+
+    public void validate() {
+        try {
+            amount = InputView.inputMoney();
+            validateDivide();
+            validateNegative();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            validate();
+        }
+    }
+
+    public void validateDivide() {
+        if (amount % DIVIDE_CONDITION != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+        }
+    }
+
+    public void validateNegative() {
+        if (amount < 0) {
+            throw new IllegalArgumentException("[ERROR] 양수 금액을 입력해주세요.");
+        }
     }
 }

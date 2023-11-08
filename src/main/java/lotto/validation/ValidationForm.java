ee58e1cf48175eb1036e5d67ea630dd1ea8ceeba
seonghoo1217 <seonghoo1217@naver.com>
2023-11-08 19:16:01 +0900

    style: 주석

--- src/main/java/lotto/validation/ValidationForm.java
+++ src/main/java/lotto/validation/ValidationForm.java
@@ -2,7 +2,6 @@ package lotto.validation;
 
 import lotto.property.ValidationProperty;
 
-import static lotto.property.ValidationProperty.*;
 import static lotto.validation.validator.CostValidator.costFormatIsCorrect;
 import static lotto.validation.validator.CostValidator.costIsStandardUnder;
 import static lotto.validation.validator.WinningValidator.*;
@@ -11,7 +10,7 @@ import static lotto.validation.validator.WinningValidator.*;
 public class ValidationForm {
 
     public static void verifyFormatForInputValue(ValidationProperty validatorType, String inputValue) {
-        if (validatorType.equals(WINNING)) {
+        /*if (validatorType.equals(WINNING)) {
             verifyForWinningNumber(inputValue);
         }
         if (validatorType.equals(WINNINGS)) {
@@ -22,7 +21,7 @@ public class ValidationForm {
         }
         if (validatorType.equals(BONUS)) {
             verifyForBonusNumber(inputValue);
-        }
+        }*/
     }
 
     static void verifyForWinningNumbers(String winningNumbers) {

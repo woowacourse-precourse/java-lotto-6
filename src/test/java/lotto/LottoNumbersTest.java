c6406e591aad335d04ae82f6d150b24fdab2bb27
K-KY <kyuyoungk@naver.com>
2023-11-08 09:10:53 +0900

    Test(LottoNumbersTest) : 번호출력 테스트 수정
    일급 컬렉션 적용한 테스트

--- src/test/java/lotto/LottoNumbersTest.java
+++ src/test/java/lotto/LottoNumbersTest.java
@@ -9,9 +9,10 @@ public class LottoNumbersTest {
 
     @Test
     void 번호_출력() {
-        List<Integer> lottoNumber = List.of(1,2,3,4,5,6);
-        List<Integer> lottoNumber2 = List.of(6,7,8,9,10,11);
-        LottoNumbers lotto = new LottoNumbers(List.of(lottoNumber, lottoNumber2));
+        LottoNumber lottoNumber = new LottoNumber(List.of(1,2,3,4,5,6));
+        LottoNumber lottoNumber2 = new LottoNumber(List.of(6,7,8,9,10,11));
+        List<LottoNumber> lottoNumbers = List.of(lottoNumber, lottoNumber2);
+        LottoNumbers lotto = new LottoNumbers(lottoNumbers);
         assertThat(lotto.toString()).contains("[1, 2, 3, 4, 5, 6]");
         assertThat(lotto.toString()).contains("[6, 7, 8, 9, 10, 11]");
         System.out.println(lotto);

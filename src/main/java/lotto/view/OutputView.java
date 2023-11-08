55e8affdf2b172969d51e5706ebf241f1b241f5f
choiejae <ejae8319@gmail.com>
2023-11-08 16:26:22 +0900

    feat(OutputView): 로또 결과를 출력하는 기능 구현
    - 출력과 관련한 책임을 OutputView에게 부여한다
    - 당첨 랭크 별 출력 순서를 정렬하는 책임을 OutputView에게 부여한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,14 +1,25 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 import lotto.Lotto;
+import lotto.LottoRank;
+import lotto.LottoResultBoard;
 import lotto.LottoTickets;
+import lotto.PurchaseAmount;
 
 public class OutputView {
 
     private static final String PURCHASED_MESSAGE = "%d개를 구매했습니다.";
     private static final String ERROR_MESSAGE = "[ERROR]";
+    private static final String RESULT_HEAD_LINE_MESSAGE = "당첨 통계";
+    private static final String RESULT_SEPARATE_LINE_MESSAGE = "---";
+    private static final String LOTTO_RESULT_MESSAGE = "%d개 일치 (%,d원) - %d개";
+    private static final String LOTTO_BONUS_RESULT_MESSAGE = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
+    private static final String RESULT_TOTAL_PROFITABILITY_PERCENT_MESSAGE = "총 수익률은 %,.1f%%입니다.";
 
     public void printAutoLottoTickets(final LottoTickets lottoTickets) {
         printNewLine();
@@ -26,6 +37,72 @@ public class OutputView {
             List<Integer> numbers = ascendingSort(lotto).toList();
             print(numbers.toString());
         }
+    }
+
+    public void printLottoResultStatistics(
+            final LottoResultBoard lottoResultBoard,
+            final PurchaseAmount purchaseAmount
+    ) {
+        printLottoResultHeadLine();
+        printLottoResultBoard(lottoResultBoard);
+        printProfitabilityPercent(lottoResultBoard, purchaseAmount);
+    }
+
+    private void printLottoResultBoard(LottoResultBoard lottoResultBoard) {
+        LottoRank[] sortedLottoRanks = sortLottoRank();
+        printMatchCountByLottoRank(lottoResultBoard, sortedLottoRanks);
+    }
+
+    private void printMatchCountByLottoRank(LottoResultBoard lottoResultBoard, LottoRank[] lottoRanks) {
+        for (LottoRank lottoRank : lottoRanks) {
+            int matchNumber = lottoRank.getMatchNumber();
+            long winningAmount = lottoRank.getWinningAmount();
+            long matchCount = lottoResultBoard.findMatchCount(lottoRank);
+
+            printMatchCountOfLottoRank(lottoRank, matchNumber, winningAmount, matchCount);
+        }
+    }
+
+    private void printMatchCountOfLottoRank(LottoRank lottoRank, int matchNumber, long winningAmount, long matchCount) {
+        if (lottoRank.equals(LottoRank.NONE)) {
+            return;
+        }
+        if (lottoRank.equals(LottoRank.SECOND)) {
+            print(String.format(LOTTO_BONUS_RESULT_MESSAGE, matchNumber, winningAmount, matchCount));
+            return;
+        }
+        print(String.format(LOTTO_RESULT_MESSAGE, matchNumber, winningAmount, matchCount));
+    }
+
+    private LottoRank[] sortLottoRank() {
+        LottoRank[] lottoRanks = LottoRank.values();
+        Arrays.sort(lottoRanks, Collections.reverseOrder());
+        return lottoRanks;
+    }
+
+    private void printProfitabilityPercent(
+            final LottoResultBoard lottoResultBoard,
+            final PurchaseAmount purchaseAmount
+    ) {
+        double roundedProfitabilityPercent = calculateRoundedProfitabilityPercent(lottoResultBoard, purchaseAmount);
+        print(String.format(RESULT_TOTAL_PROFITABILITY_PERCENT_MESSAGE, roundedProfitabilityPercent));
+    }
+
+    private static double calculateRoundedProfitabilityPercent(LottoResultBoard lottoResultBoard,
+                                                               PurchaseAmount purchaseAmount) {
+        double profitabilityPercent = lottoResultBoard.calculateProfitabilityPercent(purchaseAmount);
+        return roundProfitabilityPercent(profitabilityPercent);
+    }
+
+    private static double roundProfitabilityPercent(double profitabilityPercent) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        return Double.parseDouble(decimalFormat.format(profitabilityPercent));
+    }
+
+    private void printLottoResultHeadLine() {
+        printNewLine();
+        print(RESULT_HEAD_LINE_MESSAGE);
+        print(RESULT_SEPARATE_LINE_MESSAGE);
     }
 
     private Stream<Integer> ascendingSort(Lotto lotto) {

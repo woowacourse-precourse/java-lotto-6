e5c026eca192838369f59d38920ce07355a2a559
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 21:40:51 +0900

    test: 보너스 번호가 주어진 로또 내에 포함되어 있는지 확인 테스트

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
@@ -8,6 +9,8 @@ import static lotto.constant.ErrorMessage.DUPLICATED_BONUS;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BonusNumberTest {
+
+    private Lotto winningNumbers = new Lotto(List.of(1, 2, 3, 4, 5, 6));
 
     @DisplayName("보너스 번호와 당첨번호가 중복될 시 예외가 발생한다.")
     @Test
@@ -18,4 +21,11 @@ class BonusNumberTest {
                 .hasMessage(DUPLICATED_BONUS);
     }
 
+    @DisplayName("보너스 번호가 주어진 로또 내에 포함되어 있는지 확인할 수 있다.")
+    @Test
+    void bonusNumberIsInLotto() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 7));
+        BonusNumber bonusNumber = new BonusNumber(7, winningNumbers);
+        Assertions.assertThat(bonusNumber.isIn(lotto)).isTrue();
+    }
 }

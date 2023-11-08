71b9143684470520be9537833712716523169a87
ale783 <ale783@naver.com>
2023-11-08 14:06:42 +0900

    Add new code in Lotto class
    1. add random numbe range
    2. illegalexit code be included
    
     anyway, this is preparing for Application code and testing of git
     commit

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,14 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int LOTTO_PRICE = 1000;
+    private static final int LOTTO_NUMBERS = 6;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -11,10 +11,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != 6 || !areAllNumbersUnique(numbers) || !areNumbersInRange(numbers)) {
             throw new IllegalArgumentException();
         }
     }
-
     // TODO: 추가 기능 구현
+
+    private boolean areAllNumbersUnique(List<Integer> numbers) {
+        return numbers.stream().distinct().count() == numbers.size();
+    }
+    private boolean areNumbersInRange(List<Integer> numbers) {
+        return numbers.stream().allMatch(n -> n >= 1 && n <= 45);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
 }

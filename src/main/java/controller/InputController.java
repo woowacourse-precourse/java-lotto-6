56ba54d384a53c105b1935710cf38f21db62d295
DonghaOh <donghadarwin@naver.com>
2023-11-08 13:37:19 +0900

    feat: 금액 에러 메세지 출력 후 다시 입력 받기
    getMoneyInput 메소드

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -8,9 +8,16 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputController {
     public static int getMoneyInput() {
         moneyMessagePrint();
-        String money = Console.readLine();
-        moneyValidate(money);
-        int moneyNumber = Integer.parseInt(money);
-        return moneyNumber;
+        while (true) {
+            try {
+                String money = Console.readLine();
+                moneyValidate(money);
+                int moneyNumber = Integer.parseInt(money);
+                return moneyNumber;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 }

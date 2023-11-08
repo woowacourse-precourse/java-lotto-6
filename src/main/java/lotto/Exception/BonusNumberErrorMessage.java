8315975befb5f7a977db8f37c1703c9e3a8d2d04
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:19:55 +0900

    [feat] Validator - 보너스 넘버가 1~45 사이가 아닐 경우 검증 로직
    Signed-off-by: Hyunsoo Kim <sgn06221@gmail.com>

--- src/main/java/lotto/Exception/BonusNumberErrorMessage.java
+++ src/main/java/lotto/Exception/BonusNumberErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.Exception;
 
 public enum BonusNumberErrorMessage {
+    INVALID_BONUS_NUMBER_RANGE("[ERROR] 보너스 번호는 1~45 숫자만 입력이 가능합니다."),
     NO_BONUS_NUMBER_ENTERED("[ERROR] 보너스 번호를 입력해 주세요."),
     TOO_MANY_BONUS_NUMBERS("[ERROR] 보너스 번호는 1개만 입력해 주세요.");
 

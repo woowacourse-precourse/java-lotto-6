d0ddace697cd0966b921d533b939879feb685e71
ChoiseU <csw980928@gmail.com>
2023-11-08 14:30:42 +0900

    test : 1미만 45초과 시 예외처리 하는 부분 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 숫자 1미만 이면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 숫자 45를 초과하면 예외가 발생한다.")
+    @Test
+    void createLottoByOVerNumber() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(41, 42, 43, 44, 45, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

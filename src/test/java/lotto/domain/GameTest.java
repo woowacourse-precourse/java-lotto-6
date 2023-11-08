2e66a96961a6851af21393e110f0227ce386d690
mung96 <mung5079@gmail.com>
2023-11-08 12:03:05 +0900

    test: Game객체 생성시 validate메서드 테스트 3개

--- src/test/java/lotto/domain/GameTest.java
+++ src/test/java/lotto/domain/GameTest.java
@@ -3,6 +3,9 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +14,7 @@ class GameTest {
     @Test
     void countTickets(){
         //given
-        Game game = new Game();
+        Game game = new Game(List.of(1, 2, 3, 4, 5, 6),7);
         //when
         assertThat(16).isEqualTo(game.countTickets(16000));
         //then
@@ -21,23 +24,32 @@ class GameTest {
     @Test
     void countTicketsException(){
         //given
-        Game game = new Game();
+        Game game = new Game(List.of(1, 2, 3, 4, 5, 6),7);
         //when
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
                 () -> game.countTickets(13500));
 
         assertThat(e.getMessage()).isEqualTo("[ERROR]1,000원 단위로만 입력가능합니다.");
     }
-    @DisplayName("보너스 번호가 미리 입력 받은 6자리와 다르다면 통과")
+
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
-    void validateDuplicateBonusNumber(){
-        //given
-        Game game = new Game();
-        //when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-                () -> game.countTickets(13500));
-
-        assertThat(e.getMessage()).isEqualTo("[ERROR]1,000원 단위로만 입력가능합니다.");
+    void createGameByOverSize() {
+        assertThatThrownBy(() -> new Game(List.of(1, 2, 3, 4, 5, 6, 7),5))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createGameByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Game(List.of(1, 2, 3, 4, 5, 5),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 미리 입력 받은 6자리 중 포함된다면 예외가 발생한다.")
+    @Test
+    void validateDuplicateBonusNumber() {
+        assertThatThrownBy(() -> new Game(List.of(1, 2, 3, 4, 5, 6),6))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

4723cbf1a8dcb54fa7c99a40773bbf0a2bca3c93
dhl1924 <dhl1924@naver.com>
2023-11-08 15:46:53 +0900

    feat(inputView): 문자열로 표현된 숫자 목록을 파싱 후 정수 리스트 반환하는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,25 +1,32 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InputView {
     private static final String INPUT_LOTTO_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     private static final String INPUT_LOTTO_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_LOTTO_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
     private static final String INVALID_NUMBER_FORMAT = "[ERROR] 숫자만 입력이 가능해요";
 
-    public static int inputLottoAmount() {
+    private InputView() {
+    }
+
+    public static int inputLottoPurchaseAmount() {
         System.out.println(INPUT_LOTTO_PURCHASE_AMOUNT);
         return convertToIntegerValue(Console.readLine());
     }
 
-    public static int inputLottoWinningNumbers() {
+    public static List<Integer> inputWinningNumbers() {
         System.out.println(INPUT_LOTTO_WINNING_NUMBERS);
-        return convertToIntegerValue(Console.readLine());
-    }
-
-    public  static int inputLottoBonusNumber() {
+        List<Integer> numbers = parseNumbers(Console.readLine());
+        System.out.println();
         System.out.println(INPUT_LOTTO_BONUS_NUMBER);
-        return convertToIntegerValue(Console.readLine());
+        int bonusNumber = convertToIntegerValue(Console.readLine());
+        numbers.add(bonusNumber);
+        return numbers;
     }
 
     private static int convertToIntegerValue(String inputStringValue) {
@@ -28,5 +35,11 @@ public class InputView {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_NUMBER_FORMAT);
         }
+    }
+
+    private static List<Integer> parseNumbers(String inputStringValue) {
+        return Arrays.stream(inputStringValue.split(","))
+                .map(InputView::convertToIntegerValue)
+                .collect(Collectors.toList());
     }
 }

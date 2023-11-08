07ded9c1617e437756ef4532566c6f565f97bc55
ay-eonii <naayen@naver.com>
2023-11-08 17:11:50 +0900

    test(statistics): 당첨 순위별 통계 테스트

---
+++ src/test/java/lotto/domain/StatisticsTest.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class StatisticsTest {
+
+    @DisplayName("당첨 순위별 통계를 계산한다.")
+    @Test
+    public void statisticsCreation() {
+        List<Lotto> lottos = Arrays.asList(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new Lotto(List.of(1, 2, 3, 4, 5, 7))
+        );
+        WinningLotto winningLotto = new WinningLotto((List.of(1, 2, 3, 4, 5, 6)));
+        int bonus = 7;
+
+        Statistics statistics = new Statistics(lottos, winningLotto, bonus);
+        String actual = statistics.toString();
+
+        String expected = "3개 일치 (5,000원) - 0개\n" +
+                "4개 일치 (50,000원) - 0개\n" +
+                "5개 일치 (1,500,000원) - 0개\n" +
+                "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개\n" +
+                "6개 일치 (2,000,000,000원) - 1개\n";
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+}

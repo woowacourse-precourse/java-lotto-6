ae1143720e6a48644c1f914027e722e33166fd01
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-08 21:08:24 +0900

    feat: add request numbers
    Add process to request and input winning numbers and bonus numbers

--- src/main/java/lotto/LottoProcess.java
+++ src/main/java/lotto/LottoProcess.java
@@ -3,19 +3,28 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class LottoProcess {
+    Validation validate = new Validation();
     private static final int numLottoNum = 6;
     private int quantityLotto;
     private Lotto[] lottos;
+    private List<Integer> winningNumbers;
+    private int bonusNum;
+
+    private boolean checkValidBonusNum = true;
+    private boolean checkValidWinningNum = true;
     
     public void setNumLotto() {
         quantityLotto = new BuyLotto().BuyLotto();
         printLotto();
+        requestWinnigNumbers();
+        requsetBonnusNum();
     }
 
     public void printLotto() {
@@ -38,4 +47,46 @@ public class LottoProcess {
         return numbers;
     }
 
+    public void requestWinnigNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        while (checkValidWinningNum) {
+            try {
+                String str = readLine();
+                winningNumbers = transferWinningNumbers(str);
+                checkValidWinningNum = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                checkValidWinningNum = true;
+            }
+        }
+    }
+
+    public List<Integer> transferWinningNumbers(String str){
+        String[] strArr = str.split(",");
+        List<Integer> winnigNumbers = new ArrayList<>();
+        for (String s : strArr) {
+            validate.validateIsNum(s);
+            int winningNum = Integer.parseInt(s);
+            validate.validateIsLottoNumRange(winningNum);
+            winnigNumbers.add(winningNum);
+        }
+        validate.validateIsSIzeSix(winnigNumbers);
+        return winnigNumbers;
+    }
+
+    public void requsetBonnusNum() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        while (checkValidBonusNum) {
+            try{
+                String str = readLine();
+                validate.validateIsNum(str);
+                bonusNum = Integer.parseInt(str);
+                validate.validateIsLottoNumRange(bonusNum);
+                checkValidBonusNum = false;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                checkValidBonusNum = true;
+            }
+        }
+    }
 }

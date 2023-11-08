fa5e97d14209f92b8cf925af4779796d2a959459
nayonsoso <panda0329@naver.com>
2023-11-08 20:22:10 +0900

    refactor: 게임 컨트롤러 필요없는 코드 삭제

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -8,42 +8,17 @@ public class LottoGameController {
     private final PaymentController paymentController = new PaymentController();
     private final LottoPurchaseController lottoPurchaseController = new LottoPurchaseController();
     private final WinningsInitController winningsInitController = new WinningsInitController();
-    private final HitNumberCalculator hitNumberCalculator = new HitNumberCalculator();
-    private final RankStatistics rankStatistics = new RankStatistics();
+    private final StatisticsController statisticsController = new StatisticsController();
+    private final RankCalculator rankCalculator = new RankCalculator();
 
     public void run() {
         int userPayment = paymentController.getUserPayment();
         List<Lotto> purchasedLotto = lottoPurchaseController.purchase(userPayment);
         Lotto winningLotto = winningsInitController.initWinningLotto();
         int bonusNumber = winningsInitController.initBonusNumber(winningLotto);
-        List<Rank> ranks = purchasedLotto.stream()
-                .map(e -> calculateRank(e, winningLotto, bonusNumber))
-                .toList();
-        calculateStatistics(ranks);
-        double profitRate = calculateProfitRate(userPayment, rankStatistics.getTotalPrize());
-    }
-
-
-
-    private Rank calculateRank(Lotto purchasedLotto, Lotto winningLotto, int bonusNumber) {
-        List<Integer> purchasedLottoNumbers = purchasedLotto.getNumbers();
-        List<Integer> winningLottoNumbers = winningLotto.getNumbers();
-        int hitCount = hitNumberCalculator.hitNumberCounter(
-                purchasedLottoNumbers, winningLottoNumbers);
-        boolean isBonusIncluded = hitNumberCalculator.isBonusNumberIncluded(
-                purchasedLottoNumbers, bonusNumber);
-        return Rank.getRank(hitCount, isBonusIncluded);
-    }
-
-    private void calculateStatistics(List<Rank> ranks) {
-        ranks.forEach(rankStatistics::add);
-    }
-
-    private double calculateProfitRate(int userPayment, int totalPrize) {
-        return totalPrize / (double) userPayment;
-    }
-
-    private void printStatistics(){
-
+        List<Rank> ranks = rankCalculator.calculateAllRanks(purchasedLotto, winningLotto, bonusNumber);
+        RankStatistics rankStatistics = new RankStatistics(ranks);
+        statisticsController.printStatistics(rankStatistics);
+        statisticsController.printProfitRate(rankStatistics, userPayment);
     }
 }

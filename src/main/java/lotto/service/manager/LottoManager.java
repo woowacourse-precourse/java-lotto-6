9416d40c74e2b37fbf7e805baba5f045077989e2
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 18:55:09 +0900

    feat: 보너스 숫자의 범위 검증 코드 추가

--- src/main/java/lotto/service/manager/LottoManager.java
+++ src/main/java/lotto/service/manager/LottoManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import lotto.domain.entity.Lotto;
 import lotto.domain.entity.Lottos;
+import lotto.exception.ExceptionCode;
 
 public class LottoManager {
 
@@ -12,9 +13,19 @@ public class LottoManager {
     private final int bonusNumber;
 
     public LottoManager(final Lotto target, final int bonusNumber) {
+        validate(bonusNumber);
         this.target = target;
         this.bonusNumber = bonusNumber;
         rankBoard = new RankBoard();
+    }
+
+    private void validate(int bonusNumber) {
+        if (Lotto.START > bonusNumber || Lotto.END < bonusNumber)
+        {
+            throw new IllegalArgumentException(
+                    ExceptionCode.LOTTO_INVALID_RANGE.getDescription()
+            );
+        }
     }
 
     public void calculateTotalRanking(final Lottos lottos) {

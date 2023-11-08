cd33569a3ec05495fb1695af504620cc29d3c53a
wooni97 <jhw970714@gmail.com>
2023-11-08 20:07:08 +0900

    refactor: 상수명 수정
    - 이 커밋은 기존 클래스의 상수를 상수 전용 클래스로 옮기고, 상수명을 변경함에 따라 클래스 내의 상수명을 수정합니다.

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,21 +1,24 @@
 package lotto.model;
 
+import lotto.constant.ErrorMessage;
+import lotto.constant.Numbers;
+
 public class BonusNumber {
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 45;
+
     private final int bonusNumber;
 
     public BonusNumber(int bonusNumber) {
         checkValidateRange(bonusNumber);
         this.bonusNumber = bonusNumber;
     }
+
     public int getBonusNumber() {
         return bonusNumber;
     }
 
     private void checkValidateRange(int bonusNumber) {
-        if (bonusNumber < MIN_NUMBER || bonusNumber > MAX_NUMBER) {
-            throw new IllegalArgumentException();
+        if (bonusNumber < Numbers.RANGE_MIN_NUMBER || bonusNumber > Numbers.RANGE_MAX_NUMBER) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE);
         }
     }
 }

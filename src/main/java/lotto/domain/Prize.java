f55a326bbd977d443b239f586519a73da86a9d25
Lee9Bin <imagen33@daum.net>
2023-11-08 10:32:39 +0900

    feat : 당첨 등수 관리하는 기능 구현

---
+++ src/main/java/lotto/domain/Prize.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+
+public enum Prize {
+    LAST_PLACE(0, 0, (matchLottoNumber, containBonusNumber) -> matchLottoNumber < 3),
+    FIFTH_PLACE(5_000, 3, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 3),
+    FOURTH_PLACE(50_000, 4, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 4),
+    THIRD_PLACE(1_500_000, 5, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && !containBonusNumber),
+    SECOND_PLACE(30_000_000, 5, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 5 && containBonusNumber),
+    FIRST_PLACE(2_000_000_000, 6, (matchLottoNumber, containBonusNumber) -> matchLottoNumber == 6);
+
+    private final int money;
+    private final int matchNumber;
+    private final BiPredicate<Integer, Boolean> isMatch;
+
+    Prize(int money, int matchNumber, BiPredicate<Integer, Boolean> isMatch) {
+        this.money = money;
+        this.matchNumber = matchNumber;
+        this.isMatch = isMatch;
+    }
+
+    public static Prize getPrize(final int matchNumber, final boolean containBonusNumber) {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize.isMatch.test(matchNumber, containBonusNumber))
+                .findAny()
+                .orElse(LAST_PLACE);
+    }
+
+    public static List<Prize> getWinning() {
+        return Arrays.stream(Prize.values())
+                .filter(prize -> prize != LAST_PLACE)
+                .collect(Collectors.toList());
+    }
+}

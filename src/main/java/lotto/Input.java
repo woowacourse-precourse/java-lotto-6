d24092c03b8dae7fdfc1784388f8d59335e85858
yohanii <dygks924@gmail.com>
2023-11-08 12:46:38 +0900

    feat: 구매 금액을 입력 받는다.

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,19 +1,24 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Input {
 
     public static List<Integer> inputWinNums() {
-        return null;
+        System.out.println("당첨 번호를 입력해 주세요.");
+        return validateWinNumsInput(Console.readLine());
     }
 
     public static int inputBonusNum() {
-        return 0;
+        System.out.println("보너스 번호를 입력해 주세요.");
+        return validateBonusNumInput(Console.readLine());
     }
 
     public static int inputPrice() {
-        return 0;
+        System.out.println("구입금액을 입력해 주세요.");
+        return validatePriceInput(Console.readLine());
     }
 
     public static List<Integer> validateWinNumsInput(String winNumsInput) {
@@ -25,6 +30,14 @@ public class Input {
     }
 
     public static int validatePriceInput(String priceInput) {
-        return 0;
+        try {
+            Integer price = Integer.valueOf(priceInput);
+            if (price % 1000 != 0) {
+                throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000원 단위여야 합니다.");
+            }
+            return price;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 타입의 입력입니다.");
+        }
     }
 }

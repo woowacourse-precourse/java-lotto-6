072a1974eb7c802593ff86e9141be37ea970fe75
You <ytn1574@gmail.com>
2023-11-08 16:07:27 +0900

    chore: 불필요한 주석 삭제

--- src/main/java/domain/BonusDraw.java
+++ src/main/java/domain/BonusDraw.java
@@ -6,11 +6,6 @@ import java.util.List;
 
 public class BonusDraw {
 
-//    /* 보너스번호 validation 시작 */
-//    int bonusNumber = validation.validateBonusNumberInRangeOrIsNumber(bonusNumberInput);
-//            validation.validateBonusNumberByDuplicate(bonusNumber, winNumbers);
-//    /* 보너스번호 validation 종료 */
-
     public void bonusDraw(List<Integer> winNumbers, String bonusNumberInput) {
         int bonusNumber = Validation.validateBonusNumberInRangeOrIsNumber(bonusNumberInput);
         Validation.validateBonusNumberByDuplicate(bonusNumber, winNumbers);

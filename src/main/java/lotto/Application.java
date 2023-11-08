68d028089ed0b0c0b6d1f3b62a657dc6de3f10ef
syy0O <86238720+syy0O@users.noreply.github.com>
2023-11-08 17:28:20 +0900

    feat: lotto 시작 기능

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutPutView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutPutView outputView = new OutPutView();
+
+        LottoController lottoController = new LottoController(inputView, outputView);
+        lottoController.start();
     }
 }

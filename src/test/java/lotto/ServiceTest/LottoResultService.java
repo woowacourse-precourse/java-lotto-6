999bf2109604c6c8a776799c03a993afd41edd38
hongyongjae <developsvai5096@gmail.com>
2023-11-08 23:03:03 +0900

    Test : 서비스 테스트 코드 작성 실패 ,시간부족

--- src/test/java/lotto/ServiceTest/LottoResultService.java
+++ src/test/java/lotto/ServiceTest/LottoResultService.java
@@ -1,45 +1,35 @@
 package lotto.ServiceTest;
 
-import java.util.List;
-import lotto.Model.BonusNumber.BonusNumber;
 import lotto.Model.Lotto.Lotto;
 import lotto.Model.LottoSet.LottoSet;
-import lotto.Model.WinningNumber.WinningNumber;
-import lotto.Service.LottoTicketService.LottoTicketService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class LottoResultService {
-    private LottoResultService lottoResultService;
-    private WinningNumber winningNumber;
-
-    private LottoTicketService lottoTicketService;
-    private LottoSet lottoSet;
     private Lotto lotto;
+    private Lotto lotto2;
+    private LottoSet lottoSet;
 
-    @BeforeEach
-    void setUp() {
-        lottoResultService = new LottoResultService();
-        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        LottoSet lottoSet = new LottoTicketService();
-    }
-    @Test
-    void calculateResults_6개_일치_FIRST() {
-          = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = new BonusNumber(30);
+//    private LottoWinningResult lottoWinningResut;
+//
+//    @BeforeEach
+//    void setUp() {
+//
+//        lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+//        lotto2 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+//        lottoSet = new LottoSet(List.of(lotto));
+//
+//        lottoWinningResut.addMatchingLotto(3,lotto);
+//        lottoWinningResut.addMatchingLotto(4,lotto2);
+//
+//    }
+//    @Test
+//    void calculateResults_6개_일치_FIRST() {
+//
+//        LottoWinningResult lottoWinningResult = new LottoWinningResult();
+//        BonusNumber bonusNumber = new BonusNumber(30);
+//
+//        LottoResult lottoResult = lottoResultService.calculateResults(lottoBundle, winningNumbers, bonusNumber);
+//        Assertions.assertEquals(1, lottoResult.getResults().get(FIRST));
+//    }
 
-        LottoResult lottoResult = lottoResultService.calculateResults(lottoBundle, winningNumbers, bonusNumber);
-        Assertions.assertEquals(1, lottoResult.getResults().get(FIRST));
-    }
-
-    @Test
-    void calculateResults_5개_일치_보너스_일치_SECOND() {
-        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 5, 10));
-        BonusNumber bonusNumber = new BonusNumber(6);
-
-        LottoResult lottoResult = lottoResultService.calculateResults(lottoBundle, winningNumbers, bonusNumber);
-        Assertions.assertEquals(1, lottoResult.getResults().get(SECOND));
-    }
 
 }

fd11c95712a5cf16a5e9148b78b28001333c4161
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2023-11-08 21:53:05 +0900

    feat: matching 결과를 통계 데이터로 변환하는 메소드 구현

--- src/main/java/lotto/service/RateOfReturn.java
+++
@@ -1,7 +0,0 @@
-package lotto.service;
-
-import java.util.List;
-
-public interface RateOfReturn {
-    float compute(List<Integer> matchResult);
-}
--- src/main/java/lotto/service/SummaryMatchingImpl.java
+++ src/main/java/lotto/service/SummaryMatchingImpl.java
@@ -3,11 +3,43 @@ package lotto.service;
 import java.util.List;
 import lotto.domain.Lottos;
 import lotto.domain.Prize;
+import lotto.domain.dto.StatisticsDto;
 
-public class RateOfReturnImpl implements RateOfReturn {
+public class SummaryMatchingImpl implements SummaryMatching {
+    private final List<Integer> matchResult;
+
+    public SummaryMatchingImpl(List<Integer> matchResult) {
+        this.matchResult = matchResult;
+    }
 
     @Override
-    public float compute(List<Integer> matchResult) {
+    public StatisticsDto toStatisticsDto() {
+        int first = 0, second = 0, third = 0, forth = 0, fifth = 0;
+        float ror = computeRoR();
+        for (int result : matchResult) {
+            if (isFifthPrize(result)) {
+                fifth++;
+            } else if (isForthPrize(result)) {
+                forth++;
+            } else if (isThirdPrize(result)) {
+                third++;
+            } else if (isSecondPrize(result)) {
+                second++;
+            } else if (isFirstPrize(result)) {
+                first++;
+            }
+        }
+        return new StatisticsDto.Builder()
+                .first(first)
+                .second(second)
+                .third(third)
+                .forth(forth)
+                .fifth(fifth)
+                .ror(ror)
+                .build();
+    }
+
+    private float computeRoR() {
         int reward = 0;
         for (int result : matchResult) {
             if (isFifthPrize(result)) {
@@ -22,10 +54,10 @@ public class RateOfReturnImpl implements RateOfReturn {
                 reward += Prize.FIRST.getReward();
             }
         }
-        return computeRoR(reward, matchResult.size());
+        return computeRoRFormula(reward, matchResult.size());
     }
 
-    private float computeRoR(int reward, int LottoCount) {
+    private float computeRoRFormula(int reward, int LottoCount) {
         return (float) reward / (LottoCount * Lottos.LOTTO_PRICE) * 100;
     }
 

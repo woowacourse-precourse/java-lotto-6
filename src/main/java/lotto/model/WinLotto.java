3dbcc2c87cbb7147b2618f3288784a2ebe9a782e
Ojh <201833022@yiu.ac.kr>
2023-11-08 17:32:26 +0900

    refactor: 보너스 숫자 입력 시 오류 메시지 상수 수정

--- src/main/java/lotto/model/WinLotto.java
+++ src/main/java/lotto/model/WinLotto.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class WinLotto {
 
   private static final String BONUS_NUMBER_WITH_LETTER_MESSAGE = "[ERROR] 숫자 하나만 입력해 주세요.";
-  private static final String INVALID_BONUS_NUMBER_RANGE_MESSAGE = "[ERROR] 입력 가능한 숫자의 범위는 1~45까지 입니다.";
-  private static final String NON_OVERLAPPING_NUMBER_MESSAGE = "[ERROR] 당첨 번호와 겹치지 않는 숫자를 입력해 주세요.";
+  private static final String INVALID_BONUS_NUMBER_RANGE_MESSAGE = "[ERROR] 입력 가능한 보너스 숫자의 범위는 1~45까지 입니다.";
+  private static final String NON_OVERLAPPING_BONUS_NUMBER_MESSAGE = "[ERROR] 당첨 번호와 겹치지 않는 보너스 숫자를 입력해 주세요.";
 
   private final List<Integer> winLotto;
   private final int bonusNumber;

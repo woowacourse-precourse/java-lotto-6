80beefce8c44573a707892a0fcb11613b3ddc434
FutureCircuit <137764369+FutureCircuit@users.noreply.github.com>
2023-11-08 14:52:12 +0900

    feat+test: Lotto 클래스 생성자 인자로 중복된 숫자나 범위를 벗어난 숫자를 받을 시 예외 발생을 테스트하는 코드 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class Lotto {
@@ -11,12 +12,33 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateInRange(numbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> hs = new HashSet<>(numbers);
+        if (numbers.size() != hs.size()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInRange(List<Integer> numbers) {
+        for(int n : numbers) {
+            if(n < 1 || n > 45) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }

7c25486dde224ae7145b04b6c37ebca34627ceeb
SuhyungK <dook4966@gmail.com>
2023-11-08 11:27:26 +0900

    feat(ErrorMessage): 당첨 번호 입력 예외 처리에 대한 메세지 추가

--- src/main/java/lotto/constants/ErrorMessage.java
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -1,12 +1,15 @@
 package lotto.constants;
 
 public enum ErrorMessage {
-    NUMBER_FORMAT("숫자를 입력해야 합니다.");
+    NUMBER_FORMAT("로또 번호는 숫자만 입력 해야 합니다."),
+    NOT_VALID_SIZE("로또 번호는 6개의 숫자를 입력 해야 합니다."),
+    NOT_VALID_RANGE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NOT_DUPLICATED("로또 번호는 중복된 숫자가 존재할 수 없습니다.");
 
     private final String message;
 
     ErrorMessage(String message) {
-        this.message = message;
+        this.message = "[ERROR] " + message;
     }
 
     public String getMessage() {

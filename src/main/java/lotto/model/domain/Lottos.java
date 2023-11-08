c05aebb58f8e8dc638837dbcaa4b3cf3e8fedd17
hyeongjun im <limbs713@naver.com>
2023-11-08 10:12:29 +0900

    :sparkles: 로또 객체 생성 전에 로또 번호를 오름차순으로 정렬하는 기능 추가

--- src/main/java/lotto/model/domain/Lottos.java
+++ src/main/java/lotto/model/domain/Lottos.java
@@ -1,6 +1,7 @@
 package lotto.model.domain;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import lotto.constant.StateType;
@@ -10,7 +11,19 @@ public class Lottos {
     private final HashMap<StateType, Integer> countByState = new HashMap<>();
 
     public void addLotto(List<Integer> lottoNumbers) {
+        if(!isSortedList(lottoNumbers)){
+            lottoNumbers.sort(Comparator.naturalOrder());
+        }
         lottos.add(new Lotto(lottoNumbers));
+    }
+
+    private boolean isSortedList(List<Integer> lottoNumbers) {
+        for (int i = 0; i < lottoNumbers.size() - 1; i++) {
+            if (lottoNumbers.get(i) > lottoNumbers.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public List<Lotto> getAllLotto() {

ac4993eacf4a5bff0846ec9e4356e00748eeaccb
JeongHunHui <deca3248@gmail.com>
2023-11-08 22:13:48 +0900

    feat: LottoResult를 받아 출력할 형태로 만들어 주는 메서드 구현

--- src/main/java/lotto/constant/LottoMessage.java
+++ src/main/java/lotto/constant/LottoMessage.java
@@ -1,5 +1,8 @@
 package lotto.constant;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class LottoMessage {
 
     private static final String PURCHASE_AMOUNT_INPUT = "구입금액을 입력해 주세요.";
@@ -7,7 +10,10 @@ public class LottoMessage {
     private static final String WINNING_NUMBER_INPUT = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_INPUT = "보너스 번호를 입력해 주세요.";
     private static final String LOTTO_RESULT_WRITE_START = "당첨 통계\n---";
+    private static final String LOTTO_RESULT_FORMAT = "%d개 일치%s (%s원) - %d개";
+    private static final String BONUS_CORRECT = ", 보너스 볼 일치";
     private static final String EMPTY = "";
+    private static final NumberFormat formatter = NumberFormat.getInstance(Locale.US);
 
     public static String getPurchaseAmountInputMessage() {
         return PURCHASE_AMOUNT_INPUT;
@@ -27,6 +33,23 @@ public class LottoMessage {
 
     public static String getLottoResultWriteStartMessage() {
         return LOTTO_RESULT_WRITE_START;
+    }
+
+    public static String getLottoResultMessage(LottoPrize prize, Integer count) {
+        return String.format(
+            LOTTO_RESULT_FORMAT,
+            prize.getCorrectCount(),
+            getBonusCorrectMessage(prize.getIsBonusCorrect()),
+            formatter.format(prize.getPrize()),
+            count
+        );
+    }
+
+    private static String getBonusCorrectMessage(Boolean isBonusCorrect) {
+        if (isBonusCorrect) {
+            return BONUS_CORRECT;
+        }
+        return EMPTY;
     }
 
     public static String getEmpty() {
--- src/main/java/lotto/view/OutputMaker.java
+++ src/main/java/lotto/view/OutputMaker.java
@@ -1,7 +1,12 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map;
+import java.util.StringJoiner;
 import java.util.stream.Collectors;
+import lotto.constant.LottoMessage;
+import lotto.constant.LottoPrize;
+import lotto.model.LottoResult;
 
 public class OutputMaker {
 
@@ -14,6 +19,18 @@ public class OutputMaker {
         return lottosNumbers.stream()
             .map(this::makeLottoNumbersOutput)
             .collect(Collectors.joining(NEW_LINE));
+    }
+
+    public String makeLottoResultOutput(LottoResult lottoResult) {
+        StringJoiner sj = new StringJoiner(NEW_LINE);
+        Map<LottoPrize, Integer> prizesCount = lottoResult.getPrizesCount();
+        for (LottoPrize lottoPrize : prizesCount.keySet()) {
+            if (lottoPrize.equals(LottoPrize.NO_MATCH)) {
+                continue;
+            }
+            sj.add(LottoMessage.getLottoResultMessage(lottoPrize, prizesCount.get(lottoPrize)));
+        }
+        return sj.toString();
     }
 
     private String makeLottoNumbersOutput(List<Integer> numbers) {

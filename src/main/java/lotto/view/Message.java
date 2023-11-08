d96aebfbc5e9b59520de42d257469a4866c8c7ac
jeonseong-gwon <jeonsg9904@gmail.com>
2023-11-08 22:41:31 +0900

    🔨Refactor : 로또관련 메시지 추가

--- src/main/java/lotto/view/Message.java
+++ src/main/java/lotto/view/Message.java
@@ -1,8 +1,10 @@
 package lotto.view;
 
 public class Message {
+    public static String CHECK_AMOUNT = "로또는 1000원 단위로만 구매할 수 있습니다.";
     public static void displayStatistics() {
         System.out.println("당첨 통계");
         System.out.println("---");
     }
+
 }

1ea6fbd6b17c6a6d9699d6d0f4dd59880d6dc02e
Ojh <201833022@yiu.ac.kr>
2023-11-08 15:18:57 +0900

    refactor: 로또 길이 유효성 검사 수정

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,17 +4,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
+
+    private static final String INVALID_LOTTO_LENGTH_MESSAGE = "[ERROR] 로또 번호의 개수는 6개를 입력해야 합니다.";
+
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateLottoLength(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateLottoLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(INVALID_LOTTO_LENGTH_MESSAGE);
         }
     }
 
@@ -23,5 +27,11 @@ public class Lotto {
     @Override
     public String toString() {
         return numbers.toString();
+    }
+
+    private void validateNoDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+        if (numberSet.size() != numbers.size())
+            throw new IllegalArgumentException();
     }
 }

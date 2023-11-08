981c49ba99d8d0be1f192c38464526ff7c993654
Taehyoun Kim <kth9903@gmail.com>
2023-11-08 22:30:18 +0900

    refactor(lottoresultcalculator): implement result calculation

--- src/main/java/lotto/LottoResultCalculator.java
+++ src/main/java/lotto/LottoResultCalculator.java
@@ -1,46 +1,20 @@
 package lotto;
 
-import lotto.Lotto;
-
-import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 public class LottoResultCalculator {
 
-    private static final Map<Integer, Integer> prizeMap = new HashMap<>();
-    private static final int BOUNUS_MATCH = 7;
-
-    static {
-        prizeMap.put(3, 5_000);
-        prizeMap.put(4, 50_000);
-        prizeMap.put(5, 1_500_000);
-        prizeMap.put(BOUNUS_MATCH, 30_000_000);
-        prizeMap.put(6, 2_000_000_000);
-    }
-
-    public Map<Integer, Integer> calculateResults(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
-        Map<Integer, Integer> results = new HashMap<>();
-        lottos.forEach(lotto -> {
+    public Map<Prize, Integer> calculateResults(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
+        Map<Prize, Integer> results = new HashMap<>();
+        for (Lotto lotto : lottos) {
             int matchCount = countMatches(lotto.getNumbers(), winningNumbers);
-            if (matchCount == 5 && lotto.getNumbers().contains(bonusNumber)) {
-                matchCount++; // Increment to signify bonus number match
-            }
-            results.put(matchCount, results.getOrDefault(matchCount, 0) + 1);
-        });
+            boolean isBonusMatch = matchCount == 5 && lotto.getNumbers().contains(bonusNumber);
+            Prize prize = Prize.determinePrize(matchCount, isBonusMatch);
+            results.merge(prize, 1, Integer::sum);
+        }
         return results;
-    }
-
-    public static String getPrizeAmount(int matchCount) {
-        Integer prizeAmount = prizeMap.getOrDefault(matchCount, 0);
-        return formatNumber(prizeAmount);
-    }
-
-    private static String formatNumber(int number){
-        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
-        return numberFormat.format(number);
     }
 
     private int countMatches(List<Integer> lottoNumbers, List<Integer> winningNumbers) {

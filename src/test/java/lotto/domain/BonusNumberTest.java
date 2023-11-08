baef2a903cbb88a4a757b996eaed25c553cdc9ab
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-08 15:39:22 +0900

    refactor(BonusNumberTest): 메서드 파라미터 추가

--- src/test/java/lotto/domain/BonusNumberTest.java
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import lotto.util.message.ErrorMessage;
 
@@ -20,21 +21,24 @@ public class BonusNumberTest {
 	}
 
 	@DisplayName("보너스 번호가 당첨 로또 번호와 중복되면 예외가 발생한다.")
-	@Test
-	void createBonusNumberDuplicatedNumber() {
-		assertThatThrownBy(() -> new BonusNumber(answerLottoNumebers, 5))
+	@ValueSource(ints = {1, 2, 3, 4, 5, 6})
+	@ParameterizedTest
+	void createBonusNumberDuplicatedNumber(int bonusNumber) {
+		assertThatThrownBy(() -> new BonusNumber(answerLottoNumebers, bonusNumber))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessageContaining(ErrorMessage.LOTTO_BONUS_NUMBER_DUPLICATE_ERROR.getMessage());
 	}
 
 	@DisplayName("보너스 번호와 당첨 로또 번호가 중복이 아닐 경우 정상적으로 작동한다.")
-	@Test
-	void checkNomalOperation1() {
-		new BonusNumber(answerLottoNumebers, 7);
+	@ValueSource(ints = {10, 22, 33, 45})
+	@ParameterizedTest
+	void checkNomalOperation1(int bonusNumber) {
+		new BonusNumber(answerLottoNumebers, bonusNumber);
 	}
 
 	@DisplayName("보너스 번호의 범위가 MIN_NUMBER ~ MAX_NUMBER가 아니라면 예외가 발생한다.")
-	@Test
+	@ValueSource(ints = {0, 46, 47})
+	@ParameterizedTest
 	void createBonusNumberByNumberOutOfRange() {
 		assertThatThrownBy(() -> new BonusNumber(answerLottoNumebers, 46))
 				.isInstanceOf(IllegalArgumentException.class)
@@ -43,8 +47,9 @@ public class BonusNumberTest {
 	}
 
 	@DisplayName("보너스 번호의 범위가 MIN_NUMBER ~ MAX_NUMBER라면 정상적으로 작동한다.")
-	@Test
-	void checkNomalOperation2() {
-		new BonusNumber(answerLottoNumebers, 45);
+	@ValueSource(ints = {7, 12, 24, 45})
+	@ParameterizedTest
+	void checkNomalOperation2(int bonusNumber) {
+		new BonusNumber(answerLottoNumebers, bonusNumber);
 	}
 }

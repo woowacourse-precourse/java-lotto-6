cbe3a4abf4dae9c6c9e52ff4deec75dfd47af56b
hong <kevin0928@naver.com>
2023-11-08 17:23:35 +0900

    test : isMatchBonus 테스트 구현
    - 보너스 번호가 로또 번호들 중 있다면 True, 없으면 False 반환 하는 것을 테스트 해서 검사함

--- src/test/java/lotto/domain/BankTest.java
+++ src/test/java/lotto/domain/BankTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,39 @@ class BankTest {
     @DisplayName("당첨 번호와 로또 번호 일치하는 개수 테스트")
     @Test
     public void testCountCorrect() {
+        //given
         Bank bank = new Bank();
         bank.setWinningNumbers(List.of(1, 2, 3, 4, 5, 6));
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-
+        //when
         int correctCount = bank.countCorrect(lotto);
+        //then
         assertEquals(6, correctCount);
+    }
+
+    @DisplayName("로또 번호들 중 보너스 번호와 일치하는 값이 존재하면 true 반환")
+    @Test
+    public void testIsMatchBonusTrue() {
+        //given
+        Bank bank = new Bank();
+        bank.setExtraWinningNumber(6);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        Boolean matchBonus = bank.isMatchBonus(lotto);
+        //then
+        Assertions.assertEquals(true, matchBonus);
+    }
+
+    @DisplayName("로또 번호들 중 보너스 번호와 일치하는 값이 존재하면 false 반환")
+    @Test
+    public void testIsMatchBonusFalse() {
+        //given
+        Bank bank = new Bank();
+        bank.setExtraWinningNumber(7);
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        //when
+        Boolean matchBonus = bank.isMatchBonus(lotto);
+        //then
+        Assertions.assertEquals(false, matchBonus);
     }
 }

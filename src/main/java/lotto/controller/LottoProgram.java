a2f28eed2a5b1a9a11d806fbdeb952cae2242302
dustjq1004 <dustjq1005@naver.com>
2023-11-08 15:08:17 +0900

    yeonsup/refactor : 리팩토링으로인해 수정

--- src/main/java/lotto/controller/LottoProgram.java
+++ src/main/java/lotto/controller/LottoProgram.java
@@ -1,24 +1,25 @@
 package lotto.controller;
 
 import java.util.List;
-import lotto.LottoStore;
-import lotto.StatisticsService;
+import lotto.controller.service.LottoService;
+import lotto.controller.service.StatisticsService;
+import lotto.model.Bonus;
 import lotto.model.Lotto;
 import lotto.model.LottoMoney;
+import lotto.model.WiningNumbers;
 import lotto.model.WiningStatistics;
-import lotto.utils.StringUtils;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class LottoProgram {
     InputView inputView;
     OutputView outputView;
-    LottoStore lottoStore;
+    LottoService lottoService;
 
-    public LottoProgram(InputView inputView, OutputView outputView, LottoStore lottoStore) {
+    public LottoProgram(InputView inputView, OutputView outputView, LottoService lottoService) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.lottoStore = lottoStore;
+        this.lottoService = lottoService;
     }
 
     public void start() {
@@ -28,23 +29,22 @@ public class LottoProgram {
     }
 
     private List<Lotto> purchaseLottoNumbers(LottoMoney money) {
-        List<Lotto> lottos = lottoStore.receiveLottoNumbers(money);
+        List<Lotto> lottos = lottoService.createLottoNumbers(money);
         outputView.printBuyingLotto(lottos);
         return lottos;
     }
 
     private void drawLottery(List<Lotto> lottos) {
-        String winingNumbers = inputView.printWiningNumbers();
-        String bonusNumber = inputView.printBonusNumber();
-
-        result(lottos, winingNumbers, bonusNumber);
+        Lotto numbers = inputView.printWiningNumbers();
+        Bonus bonusNumber = inputView.printBonusNumber();
+        WiningNumbers winingNumbers = new WiningNumbers(numbers, bonusNumber);
+        result(lottos, winingNumbers);
     }
 
-    private void result(List<Lotto> lottos, String winingNumbers, String bonusNumber) {
+    private void result(List<Lotto> lottos, WiningNumbers winingNumbers) {
         StatisticsService statisticsService = new StatisticsService();
         WiningStatistics winingStatistics =
-                statisticsService.calculateWiningStatistics(StringUtils.asListByDelimiter(winingNumbers, ",")
-                        , Integer.parseInt(bonusNumber), lottos);
+                statisticsService.calculateWiningStatistics(winingNumbers, lottos);
         outputView.printWiningStatistics(winingStatistics);
     }
 }

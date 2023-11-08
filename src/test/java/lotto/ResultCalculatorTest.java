023c6c752e06e820185bf7e46035c03e22607023
MisaSohee <sohee5951@naver.com>
2023-11-08 23:15:30 +0900

    test(ResultCalculator): 총 상금 계산 테스트

--- src/test/java/lotto/ResultCalculatorTest.java
+++ src/test/java/lotto/ResultCalculatorTest.java
@@ -10,15 +10,23 @@ public class ResultCalculatorTest {
     @Test
     @DisplayName("당첨 결과와 로또 구입 개수를 이용해 수익률을 정확하게 계산한다")
     public void testCalculateEarningsRate() {
-        // Prepare data
         WinningResult result = new WinningResult();
         result.addWinningTicket(3, false);  // Suppose we have one ticket with 3 correct numbers
         int lottoCount = 1;
 
-        // Call method
         double earningsRate = calculator.calculateEarningsRate(result, lottoCount);
 
-        // Check result
         Assertions.assertTrue(earningsRate == 5000.0 / 1000);
+    }
+
+    @Test
+    @DisplayName("당첨 결과를 이용해 총 상금을 정확하게 계산한다")
+    public void testCalculateTotalPrize() {
+        WinningResult result = new WinningResult();
+        result.addWinningTicket(3, false);  // Suppose we have one ticket with 3 correct numbers
+
+        long totalPrize = calculator.calculateTotalPrize(result);
+
+        Assertions.assertTrue(totalPrize == 5000);
     }
 }

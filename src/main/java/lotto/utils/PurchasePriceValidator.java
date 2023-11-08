a7c99a8e65bcb811f004154c305c70caf8167749
BangMingGing <privatebmk802@naver.com>
2023-11-08 15:00:46 +0900

    feat(PurchasePriceValidator): Add purchase price validator

--- src/main/java/lotto/utils/PurchasePriceValidator.java
+++ src/main/java/lotto/utils/PurchasePriceValidator.java
@@ -1,2 +1,17 @@
-package lotto.utils;public class PurchasePriceValidator {
+package lotto.utils;
+
+public class PurchasePriceValidator {
+    public static void validIsInt(String purchasePrice) {
+        try {
+            Integer.parseInt(purchasePrice);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 숫자여야 합니다.");
+        }
+    }
+
+    public static void validIsMultiplesOfThousand(int purchasePrice) {
+        if (purchasePrice%1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구매금액은 1000으로 나누어 떨어져야 합니다.");
+        }
+    }
 }

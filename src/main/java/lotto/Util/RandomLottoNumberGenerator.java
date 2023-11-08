506599e3313ae29cdded86cacca6d9319f15d381
gemstoneyang <bennyws99@gmail.com>
2023-11-08 18:49:26 +0900

    feat: 랜덤한 로또 번호를 생성하는 클래스 구현

---
+++ src/main/java/lotto/Util/RandomLottoNumberGenerator.java
@@ -0,0 +1,19 @@
+package lotto.Util;
+
+import static lotto.config.LottoConfig.LOTTO_MAX_VALUE;
+import static lotto.config.LottoConfig.LOTTO_MIN_VALUE;
+import static lotto.config.LottoConfig.LOTTO_SIZE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomLottoNumberGenerator {
+    public static List<Integer> makeNumber() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LOTTO_MIN_VALUE.getValue(), LOTTO_MAX_VALUE.getValue(), LOTTO_SIZE.getValue());
+        Collections.sort(numbers);
+
+        return numbers;
+    }
+}

e518a3e3d1de8b89642bda99653a6d8bab8a88a1
yeop0740 <ssucgoo@gmail.com>
2023-11-08 14:30:06 +0900

    feat: add method to get winning numbers and bonus number from user

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,11 +6,30 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int price = getPrice();
+
+        Lottos lottos = new Lottos(price);
+        OutputConsole.print(lottos);
+
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber();
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        OutputConsole.printResult(lottos, winningLotto);
     }
 
     public static int getPrice() {
         OutputConsole.printMessage(OutputConsole.INPUT_PRICE_MESSAGE);
         return InputConsole.inputPrice();
+    }
+
+    public static List<Integer> getWinningNumbers() {
+        OutputConsole.printMessage(OutputConsole.INPUT_WINNING_NUMBERS_MESSAGE);
+        return InputConsole.inputWinningNumbers();
+    }
+
+    public static int getBonusNumber() {
+        OutputConsole.printMessage(OutputConsole.INPUT_BONUS_NUMBER_MESSAGE);
+        return InputConsole.inputBonusNumber();
     }
 
 }

af2c45723ac83502c94ede547314316ac308ff90
hunm719 <hunm719@naver.com>
2023-11-08 19:31:05 +0900

    요구 사항에 맞게 Result 클래스 수정

--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -3,18 +3,19 @@ package lotto;
 import java.text.NumberFormat;
 
 public class Result {
-    // 각 로또의 당첨 여부를 저장하고 결과를 집계하는 역할
-    private int[] matchCounts;
+    private static final int PRICE_PER_LOTTO = 1000;
+    private static final int[] PRIZE_MONEY = {0, 0, 0, 5000, 50000, 1500000, 0, 30000000, 2000000000};
+    private static final int MATCH_5_WITH_BONUS_PRIZE_INDEX = 7;
+
+    private final int[] matchCounts;
 
     public Result() {
-        this.matchCounts = new int[8];
+        this.matchCounts = new int[PRIZE_MONEY.length];
     }
 
     public void add(int matchedCount, boolean hasBonusNumber) {
-        if (matchedCount == 6) {
-            matchCounts[6]++;
-        } else if (matchedCount == 5 && hasBonusNumber) {
-            matchCounts[6]++;
+        if (matchedCount == 6 || (matchedCount == 5 && hasBonusNumber)) {
+            matchCounts[MATCH_5_WITH_BONUS_PRIZE_INDEX]++;
         } else {
             matchCounts[matchedCount]++;
         }
@@ -24,31 +25,43 @@ public class Result {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---");
-        System.out.printf("3개 일치 (5,000원) - %d개.\n", matchCounts[3]);
-        System.out.printf("4개 일치 (50,000원) - %d개.\n", matchCounts[4]);
-        System.out.printf("5개 일치 (1,500,000원) - %d개.\n", matchCounts[5]);
-        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개.\n", matchCounts[7]);
-        System.out.printf("6개 일치 (2,000,000,000원) - %d개.\n", matchCounts[6]);
+        printMatchCounts();
+        printTotalProfitRate();
+    }
 
-        double totalProfitRate = calculateTotalProfitRate();
+    private void printMatchCounts() {
+        for (int i = 3; i < matchCounts.length; i++) {
+            if (i != MATCH_5_WITH_BONUS_PRIZE_INDEX) {
+                System.out.printf("%d개 일치 (%d원) - %d개.\n", i, PRIZE_MONEY[i], matchCounts[i]);
+            } else {
+                System.out.printf("5개 일치, 보너스 볼 일치 (%d원) - %d개.\n", PRIZE_MONEY[i], matchCounts[i]);
+            }
+        }
+    }
+
+    private void printTotalProfitRate() {
+        double totalPrize = calculateTotalPrize();
+        double totalPurchase = getTotalPurchaseAmount();
+        double totalProfitRate = (totalPrize / totalPurchase) * 100;
+
         NumberFormat nf = NumberFormat.getInstance();
         nf.setMaximumFractionDigits(1);
         System.out.printf("총 수익률은 %s%%입니다.\n", nf.format(totalProfitRate));
     }
 
-    private double calculateTotalProfitRate() {
-        int totalPrize = matchCounts[3] * 5000 + matchCounts[4] * 50000 + matchCounts[5] * 1500000 +
-                matchCounts[7] * 30000000 + matchCounts[6] * 2000000000;
-        int totalPurchase = getTotalPurchaseAmount();
-        return ((double) totalPrize / totalPurchase) * 100;
+    private double calculateTotalPrize() {
+        double totalPrize = 0;
+        for (int i = 3; i < matchCounts.length; i++) {
+            totalPrize += matchCounts[i] * PRIZE_MONEY[i];
+        }
+        return totalPrize;
     }
 
     private int getTotalPurchaseAmount() {
-        int pricePerLotto = 1000;
         int totalLottos = 0;
         for (int count : matchCounts) {
             totalLottos += count;
         }
-        return totalLottos * pricePerLotto;
+        return totalLottos * PRICE_PER_LOTTO;
     }
 }

94bb52ad1bd660d9adbc83836c39cc9c3060224c
dltjdn <hana40004@naver.com>
2023-11-08 17:09:14 +0900

    feat(InputView): 잘못된 값 입력할 경우 에러 메시지 출력 후 다시 입력 기능

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import java.util.stream.Stream;
+import lotto.ErrorLogger;
 import lotto.model.Lotto;
 
 public class InputView {
@@ -11,11 +12,16 @@ public class InputView {
     private static final String BONUS_NUMBER_INPUT_PROMPT = "\n보너스 번호를 입력해 주세요.";
 
     public int readPurchaseAmount() {
-        System.out.println(PURCHASE_AMOUNT_INPUT_PROMPT);
-        int purchaseAmount = readLineAndParseInt();
-        InputValidator.greaterThanThousand(purchaseAmount);
-        InputValidator.divisibleByThousand(purchaseAmount);
-        return purchaseAmount;
+        try {
+            System.out.println(PURCHASE_AMOUNT_INPUT_PROMPT);
+            int purchaseAmount = readLineAndParseInt();
+            InputValidator.greaterThanThousand(purchaseAmount);
+            InputValidator.divisibleByThousand(purchaseAmount);
+            return purchaseAmount;
+        } catch (IllegalArgumentException e) {
+            ErrorLogger.log(e);
+            return readPurchaseAmount();
+        }
     }
 
     private int readLineAndParseInt() {
@@ -28,20 +34,31 @@ public class InputView {
     }
 
     public Lotto readWinningNumbers() {
-        System.out.println(WINNING_NUMBERS_INPUT_PROMPT);
-        String input = Console.readLine();
-        List<Integer> winningNumbers = Stream.of(input.split(","))
-                .map(Integer::parseInt)
-                .toList();
+        try {
+            System.out.println(WINNING_NUMBERS_INPUT_PROMPT);
+            String input = Console.readLine();
+            List<Integer> winningNumbers = Stream.of(input.split(","))
+                    .map(Integer::parseInt)
+                    .toList();
 
-        return new Lotto(winningNumbers);
+            return new Lotto(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            ErrorLogger.log(e);
+            return readWinningNumbers();
+        }
     }
 
     public int readBonusNumber() {
-        System.out.println(BONUS_NUMBER_INPUT_PROMPT);
-        int bonusNumber = readLineAndParseInt();
-        InputValidator.inRange(bonusNumber);
-        return bonusNumber;
+        try{
+            System.out.println(BONUS_NUMBER_INPUT_PROMPT);
+            int bonusNumber = readLineAndParseInt();
+            InputValidator.inRange(bonusNumber);
+            return bonusNumber;
+        }catch(IllegalArgumentException e ){
+            ErrorLogger.log(e);
+            return readBonusNumber();
+        }
+
     }
 
 

425c06b303ccdf736058f2b2cdbd33669ce60b48
cookingTorch <jhw7181@naver.com>
2023-11-08 18:44:05 +0900

    test(LottoTest): add unit test for calculating return rate in Result
    Added a new unit test `findReturnRate` to ensure the correct calculation of
    the return rate in the Result class. The test verifies that the return rate
    is accurately computed when a set of results and a bonus number are provided
    for a given input amount.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -100,4 +100,16 @@ class LottoTest {
                 .contains("3개 일치 (5,000원) - 1개", "4개 일치 (50,000원) - 0개", "5개 일치 (1,500,000원) - 1개",
                         "5개 일치, 보너스 볼 일치 (30,000,000원) - 2개", "6개 일치 (2,000,000,000원) - 3개");
     }
+
+    @DisplayName("수익률 계산")
+    @Test
+    void findReturnRate() {
+        int numberCount = 6;
+        Result result = new Result(numberCount);
+        result.addResult(1);
+        result.addResult(3);
+        result.addResult(numberCount - 1);
+        result.addBonus(numberCount);
+        assertThat(result.calculateReturnRate(8000)).isEqualTo(375062.5);
+    }
 }

f1daeb52e0110a3e4cb7717902d2362b2181f557
haebyun <bhk4392@gmail.com>
2023-11-08 16:18:54 +0900

    test(Converter): 숫자가 아닌 문자가 포함된 비정상 구매 금액 문자열을 예외 처리하는 기능 테스트

--- src/test/java/lotto/util/ConverterTest.java
+++ src/test/java/lotto/util/ConverterTest.java
@@ -1,9 +1,12 @@
 package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ConverterTest {
     @Test
@@ -18,4 +21,12 @@ class ConverterTest {
         assertThat(result).isEqualTo(amount);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1000a", "1 000", "####"})
+    @DisplayName("숫자가 아닌 문자가 포함된 구매 금액 문자열들을 예외 처리하는 테스트")
+    void convertPurchaseAmountExceptionTest(String input) {
+        //then
+        assertThatThrownBy(() -> Converter.convertPurchaseAmount(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

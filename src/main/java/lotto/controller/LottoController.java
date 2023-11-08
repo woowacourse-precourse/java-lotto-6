136834b26e480047224f7ef9492cb6b8d654f09b
Kim mingyu <minq0302@naver.com>
2023-11-08 23:15:44 +0900

    feat(controller): add run method

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,17 +4,106 @@ package lotto.controller;
 import static lotto.util.Util.changeStringToInt;
 import static lotto.validator.LottoInputValidator.validateBonusNumber;
 import static lotto.validator.LottoInputValidator.validatePurchaseMoney;
+import static lotto.view.InputView.showInputBonusNumberMessage;
+import static lotto.view.InputView.showInputPurchaseMoneyMessage;
+import static lotto.view.InputView.showInputWinningNumbersMessage;
+import static lotto.view.OutputBuyLottoCount.printBuyLotto;
 import static lotto.view.PrintView.printErrorMessage;
+import static lotto.view.PrintView.printLottoNumbers;
+import static lotto.view.PrintView.printStatistics;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.domain.BonusNumber;
+import lotto.domain.LottoGenerator;
+import lotto.domain.Lottos;
+import lotto.domain.Money;
+import lotto.domain.PrizeResult;
+import lotto.domain.Rate;
+import lotto.domain.Service;
+import lotto.domain.WinningLotto;
 import lotto.domain.WinningNumber;
 import lotto.util.Util;
 import lotto.validator.LottoInputValidator;
+import lotto.view.PrintView;
 
 public class LottoController {
-    public WinningNumber inputWinningNumbers() {
+    PrintView printView = new PrintView();
+    Service service = new Service();
+
+    public void run() {
+        Money money = getLottoMoney();
+        printBuyLotto(money);
+
+        Lottos lottos = getLottos(money);
+        printLottoNumbers(lottos);
+
+        WinningLotto winningLotto = getWinningLotto();
+        PrizeResult prizeResult = new PrizeResult();
+        calcLottoResult(prizeResult, winningLotto, lottos);
+
+        Rate rate = getRate(money, prizeResult);
+        printStatistics(prizeResult, rate);
+    }
+
+
+    private Money getLottoMoney() {
+        int money = getPurchaseMoney();
+        return new Money(money);
+    }
+
+    private Lottos getLottos(Money money) {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        return new Lottos(lottoGenerator.generateLottos(money.getTicket()));
+    }
+
+    private int getBonusNumber() {
+        showInputBonusNumberMessage();
+        return inputBonusNumber();
+    }
+
+
+    private int getPurchaseMoney() {
+        showInputPurchaseMoneyMessage();
+        return inputPurchaseMoney();
+    }
+
+    private void printLottos(int money) {
+        Lottos lottos = buyLottos(money);
+        printView.printNumberOfLottos(money);
+        printView.printLottoNumbers(lottos);
+    }
+
+    private List<Integer> getWinningNumbers() {
+        showInputWinningNumbersMessage();
+        return inputWinningNumbers();
+    }
+
+    private Lottos buyLottos(int money) {
+        int lottoSize = service.getNumberOfLottos(money);
+        LottoGenerator generator = new LottoGenerator();
+        Lottos lottos = new Lottos(generator.generateLottos(lottoSize));
+        return lottos;
+    }
+
+    private Rate getRate(Money money, PrizeResult prizeResult) {
+        return new Rate(money, prizeResult);
+    }
+
+    private WinningLotto getWinningLotto() {
+        showInputWinningNumbersMessage();
+        List<Integer> winningNumbers = inputWinningNumbers();
+        showInputBonusNumberMessage();
+        Integer bonusNumber = inputBonusNumber();
+
+        return new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    private void calcLottoResult(PrizeResult prizeResult, WinningLotto winningLotto, Lottos lottos) {
+        prizeResult.calcPrizeResult(winningLotto, lottos);
+    }
+
+    public List<Integer> inputWinningNumbers() {
         WinningNumber winningNumber = null;
 
         while (winningNumber == null) {
@@ -25,7 +114,7 @@ public class LottoController {
             }
         }
 
-        return winningNumber;
+        return winningNumber.getWinningNumbers();
     }
 
     private WinningNumber getValidWinningNumber() {
@@ -55,7 +144,7 @@ public class LottoController {
     public int inputPurchaseMoney() {
         boolean validInput = false;
         int purchaseMoney = 0;
-        
+
         while (!validInput) {
             try {
                 String input = Console.readLine();

7891d8dc0055e21381a80639691d983f6ad1bc4b
zoid79 <zidwkd00@naver.com>
2023-11-08 21:24:38 +0900

    feat: rounds

--- src/main/java/lotto/Counter.java
+++ src/main/java/lotto/Counter.java
@@ -21,11 +21,11 @@ public class Counter {
         System.out.println(ERating.eTHIRD.getValue()+counts.get(ERating.eTHIRD)+"개");
         System.out.println(ERating.eSECOND.getValue()+counts.get(ERating.eSECOND)+"개");
         System.out.println(ERating.eFIRST.getValue()+counts.get(ERating.eFIRST)+"개");
-        System.out.println("총 수익률은 "+(float)(counts.get(ERating.eFIFTH)*ERating.eFIFTH.getReward()
+        System.out.println("총 수익률은 "+(double)Math.round((counts.get(ERating.eFIFTH)*ERating.eFIFTH.getReward()
                 +counts.get(ERating.eFOURTH)*ERating.eFOURTH.getReward()
                 +counts.get(ERating.eTHIRD)*ERating.eTHIRD.getReward()
                 +counts.get(ERating.eSECOND)*ERating.eSECOND.getReward()
-                +counts.get(ERating.eFIRST)*ERating.eFIRST.getReward())/(ticketCount*10)+"%입니다.");
+                +counts.get(ERating.eFIRST)*ERating.eFIRST.getReward())/(ticketCount))/10+"%입니다.");
     }
     public void countRatings(ERating rating) {
         counts.put(rating, counts.getOrDefault(rating, 0) + 1);

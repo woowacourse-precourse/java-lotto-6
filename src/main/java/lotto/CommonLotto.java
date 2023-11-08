cf075b5f4e37b0648de759ffa2af56b43a901e6a
dong_hy <kimadonghyon99@gmail.com>
2023-11-08 19:43:30 +0900

    feat : 로또에서 사용하는 상수값 클래스 생성

---
+++ src/main/java/lotto/CommonLotto.java
@@ -0,0 +1,9 @@
+package lotto;
+
+public class CommonLotto {
+    public static final int LOTTO_PRICE = 1000;
+    public static final int LOTTO_COUNT = 6;
+    public static final int LOTTO_MIN = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final String SEPERATOR = ",";
+}

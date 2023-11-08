a34eb35be38f3f1c9dabdda8b9e9e3e1f598ed2c
HanSeongLEE <leehs98712@gmail.com>
2023-11-08 21:25:12 +0900

    Feat : lottoPrize 도메인 작성 및 테스트 실행
    1,2,3,4,5 등에 대한 구현 및 오름차순 정렬 반환 구현

--- src/main/java/lotto/domain/LottoPrize.java
+++ src/main/java/lotto/domain/LottoPrize.java
@@ -1,26 +1,50 @@
 package lotto.domain;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public enum LottoPrize {
-    FIRST("1등", 2_000_000_000),
-    SECOND("2등", 30_000_000),
-    THIRD("3등", 1_500_000),
-    FOURTH("4등", 50_000),
-    FIFTH("5등", 5_000);
-    private final String rank;
+    FIRST( 2_000_000_000,6,false),
+    SECOND( 30_000_000,5,true),
+    THIRD( 1_500_000,5,false),
+    FOURTH(50_000,4,false),
+    FIFTH( 5_000,3,false),
+    NONE(0,0,false);
     private final long prizeAmount;
+    private final int matchingNumber;
+    private final boolean isSecond;
 
-
-    LottoPrize(String rank, long prizeAmount) {
-        this.rank = rank;
+    LottoPrize(long prizeAmount,int matchingNumbers,boolean isSecond) {
         this.prizeAmount = prizeAmount;
-    }
-
-    public String getRank() {
-        return rank;
+        this.matchingNumber = matchingNumbers;
+        this.isSecond = isSecond;
     }
 
     public Long getPrizeAmount() {
         return prizeAmount;
     }
+
+    public int getMatchingNumber() {
+        return matchingNumber;
+    }
+
+    public static LottoPrize findRank(int matchingNumber, boolean isSecond){
+        for (LottoPrize lottoPrize : LottoPrize.values()) {
+            if (lottoPrize.matchingNumber == matchingNumber && lottoPrize.isSecond == isSecond) {
+                return lottoPrize;
+            }
+        }
+        return NONE;
+    }
+    public static List<LottoPrize> lowestPrizeOrder() {
+        List<LottoPrize> orderedPrizes = new ArrayList<>(Arrays.asList(LottoPrize.values()));
+        orderedPrizes.remove(NONE);
+        orderedPrizes.sort(Comparator.comparingLong(LottoPrize::getPrizeAmount));
+        return orderedPrizes;
+    }
+
 
 }

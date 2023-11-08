62623443ae63556bf2f9346d957f7328366d4ea9
cyPark95 <parkcy1007@gmail.com>
2023-11-08 12:23:27 +0900

    feat: 수익률 계산 로직 구현 & 테스트
    - 수익률 소수점 둘째 자리에서 반올림 기능 구현 목록 게임 진행으로 이동
    - 로또 당첨 금액 총합을 구해서 수익률을 계산
    - 수익률을 반환하기 전 소수점 둘째 자리에서 반올림한다.
    
    [테스트 케이스]
    - 로또 구매 금액에 따른 당첨 금액 수익률 계산 검증

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -10,6 +10,8 @@ public class LottoResult {
 
     public static final int INIT_BOARD_COUNT = 0;
     public static final int INCREASE_COUNT = 1;
+    public static final double DECIMAL_PLACE = 10.0;
+    public static final int PERCENTAGE = 100;
     private final Map<ResultType, Integer> board;
 
     public LottoResult(List<ResultType> resultTypes) {
@@ -21,6 +23,12 @@ public class LottoResult {
         return Collections.unmodifiableMap(board);
     }
 
+    public double getYieldRate(int amount) {
+        int totalReward = getTotalReward();
+        double yieldRate = getYieldRate(amount, totalReward);
+        return Math.round(yieldRate * DECIMAL_PLACE) / DECIMAL_PLACE;
+    }
+
     private void calculateResult(List<ResultType> resultTypes) {
         resultTypes
                 .forEach(type -> board.put(type, increaseCount(type)));
@@ -28,5 +36,19 @@ public class LottoResult {
 
     private int increaseCount(ResultType type) {
         return board.getOrDefault(type, INIT_BOARD_COUNT) + INCREASE_COUNT;
+    }
+
+    private int getTotalReward() {
+        return board.keySet().stream()
+                .mapToInt(this::getReward)
+                .sum();
+    }
+
+    private int getReward(ResultType key) {
+        return key.getReward() * board.get(key);
+    }
+
+    private double getYieldRate(int amount, int totalReward) {
+        return (double) totalReward / amount * PERCENTAGE;
     }
 }
--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -11,34 +11,45 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import common.enumtype.ResultType;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("로또 당첨 결과 검증")
 class LottoResultTest {
 
+    private LottoResult lottoResult;
+
+    @BeforeEach
+    void setUp() {
+        List<ResultType> resultTypes = List.of(FIFTH_PLACE, THIRD_PLACE);
+        lottoResult = new LottoResult(resultTypes);
+    }
+
     @Test
     void 로또_당첨_결과_통계_검증() {
-        // given
-        List<ResultType> resultTypes = List.of(
-                FIFTH_PLACE, FIFTH_PLACE,
-                FOURTH_PLACE, FOURTH_PLACE, FOURTH_PLACE,
-                THIRD_PLACE,
-                SECOND_PLACE, SECOND_PLACE,
-                FIRST_PLACE
-        );
-        LottoResult lottoResult = new LottoResult(resultTypes);
-
         // when
         Map<ResultType, Integer> result = lottoResult.getResult();
 
         // then
         assertAll(
-                () -> assertThat(result.getOrDefault(FIFTH_PLACE, 0)).isEqualTo(2),
-                () -> assertThat(result.getOrDefault(FOURTH_PLACE, 0)).isEqualTo(3),
+                () -> assertThat(result.getOrDefault(FIFTH_PLACE, 0)).isEqualTo(1),
+                () -> assertThat(result.getOrDefault(FOURTH_PLACE, 0)).isEqualTo(0),
                 () -> assertThat(result.getOrDefault(THIRD_PLACE, 0)).isEqualTo(1),
-                () -> assertThat(result.getOrDefault(SECOND_PLACE, 0)).isEqualTo(2),
-                () -> assertThat(result.getOrDefault(FIRST_PLACE, 0)).isEqualTo(1)
+                () -> assertThat(result.getOrDefault(SECOND_PLACE, 0)).isEqualTo(0),
+                () -> assertThat(result.getOrDefault(FIRST_PLACE, 0)).isEqualTo(0)
         );
+    }
+
+    @Test
+    void 로또_결과_수익률_검증() {
+        // given
+        int amount = 8000;
+
+        // when
+        double result = lottoResult.getYieldRate(amount);
+
+        // then
+        assertThat(result).isEqualTo(18812.5);
     }
 }

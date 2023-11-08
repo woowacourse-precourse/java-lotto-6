61441d7b2455e551572f80cb327494358597a7d3
hansg931 <hansg0509@cau.ac.kr>
2023-11-08 23:06:10 +0900

    feat(OutputView): 로또의 정보를 출력하는 메소드 작성

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import lotto.util.IOMessage;
 
+import java.util.List;
+
 public class OutputView {
 	public static void printInputPurchaseAmountMessage() {
 		System.out.println(IOMessage.INPUT_PURCHASE_AMOUNT_MESSAGE);
@@ -13,5 +15,13 @@ public class OutputView {
 
 	public static void printInputBonusNumberMessage() {
 		System.out.println(IOMessage.INPUT_BONUS_NUMBER_MESSAGE);
+	}
+
+	public static void printLotto(List<Integer> lotto) {
+		printMessage(lotto.toString());
+	}
+
+	public static void printMessage(String message) {
+		System.out.println(message);
 	}
 }

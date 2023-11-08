dd18e05ade2b5b9341348117ceec4b34241993ba
getbravelee <getbravelee@gmail.com>
2023-11-08 22:58:19 +0900

    test: 로또 구매 금액 입력 값 테스트

--- src/test/java/lotto/BuyAmountTest.java
+++ src/test/java/lotto/BuyAmountTest.java
@@ -11,7 +11,7 @@ public class BuyAmountTest {
     @DisplayName("로또 구매 금액 입력")
     @Test
     void createLottoByOverSize() {
-        int payment = 8000;
+        String payment = "8000";
         BuyAmount buyAmount = new BuyAmount(payment);
 
         assertThat(buyAmount.getPayment()).isEqualTo(8000);

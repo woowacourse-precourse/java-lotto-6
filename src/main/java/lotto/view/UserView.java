d58a49e5474b2d2dd363be54244eda2c0d61610f
mumpuris <hst990124@naver.com>
2023-11-08 21:15:16 +0900

    구매한 로또의 개수와 로또 번호 출력하는 기능 구현

--- src/main/java/lotto/view/UserView.java
+++ src/main/java/lotto/view/UserView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
+
+import java.util.List;
 
 public class UserView {
 
@@ -8,5 +12,15 @@ public class UserView {
         System.out.println("구입금액을 입력해 주세요.(천원 단위)");
         return Console.readLine();
     }
+
+
+    public static void outputBuyLotto(List<Lotto> lottos) {
+        System.out.println("\n"+ lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+        System.out.println();
+    }
+
 
 }

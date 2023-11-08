502a2a4c3641a4c500fd1a6fba60cd9a6a23b185
JDUB9823 <jeong98231@gmail.com>
2023-11-08 13:44:09 +0900

    test: 당첨 숫자 개수, 범위, 중복 예외테스트 추가

---
+++ src/test/java/lotto/WinningNumbersTest.java
@@ -0,0 +1,38 @@
+package lotto;
+
+import lotto.controller.InputController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WinningNumbersTest {
+    public InputController inputController = new InputController();
+
+    @DisplayName("당첨 숫자의 개수가 6이 아니면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByOverSize() {
+        assertThatThrownBy(() -> inputController.validateLength("1,2,3,4,5"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 숫자의 범위가 1 ~ 45를 벗어나면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 46})
+    void createWinningNumbersByInvalidRange(int invalidRangeNumber) {
+        assertThatThrownBy(() -> inputController.validateNumberRange(invalidRangeNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 숫자가 중복되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,10,20,30,40,45})
+    void createWinningNumbersByDuplicatedNumbers(int duplicatedNumber) {
+        assertThatThrownBy(() -> inputController.validateNumberDuplication(duplicatedNumber, List.of(1,10,20,30,40,45)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

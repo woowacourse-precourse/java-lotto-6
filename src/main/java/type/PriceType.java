7ce2a67d522b8aaa1e63855934def346aa061084
win9 <krrr883@gmail.com>
2023-11-08 18:21:38 +0900

    [Feat]: 텍스트타입 변경
    괄호, 금액 분리

--- src/main/java/type/PriceType.java
+++ src/main/java/type/PriceType.java
@@ -1,18 +1,20 @@
 package type;
 
 public enum PriceType {
-    FIRST("(2,000,000,000원)", 6),
-    SECOND("(30,000,000원)", 7),
-    THIRD("(1,500,000원)", 5),
-    FOURTH("(50,000원)", 4),
-    FIFTH("(5,000원)", 3),
-    BONUS("보너스 볼 일치", 0);
+    FIRST("(2,000,000,000원)", "2,000,000,000", 6),
+    SECOND("(30,000,000원)", "30,000,000", 7),
+    THIRD("(1,500,000원)", "1,500,000", 5),
+    FOURTH("(50,000원)", "50,000", 4),
+    FIFTH("(5,000원)", "5,000", 3),
+    BONUS("보너스 볼 일치", "", 0);
 
+    private final String text;
     private final String price;
     private final int count;
 
-    PriceType (String result, int count){
-        this.price = result;
+    PriceType (String text, String price, int count){
+        this.text = text;
+        this.price = price;
         this.count = count;
     }
 
@@ -22,5 +24,9 @@ public enum PriceType {
 
     public int getCount() {
         return count;
+    }
+
+    public String getText() {
+        return text;
     }
 }

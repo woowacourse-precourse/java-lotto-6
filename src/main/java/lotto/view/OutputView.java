588ac20972922b21d1b041baf8d9c9102cff98cf
K-KY <kyuyoungk@naver.com>
2023-11-08 18:20:36 +0900

    Style(OutputView) : 코드 정렬

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,7 +7,6 @@ public class OutputView {
     private static final String INPUT_WIN_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
-
     public static void printInputMoney() {
         System.out.println(INPUT_MONEY);
     }
@@ -19,12 +18,15 @@ public class OutputView {
     public static void printGeneratedLotto(String lottos) {
         System.out.println(lottos);
     }
+
     public static void printInputWinNumber() {
         System.out.println(INPUT_WIN_NUMBER);
     }
+
     public static void printInputBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
     }
+
     public static void printWinStatistics(String result) {
         System.out.println(result);
     }

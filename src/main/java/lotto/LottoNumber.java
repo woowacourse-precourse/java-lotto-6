0c38d3e878e1245dbb203754b2e3f02031b802fc
최진원 <etudent39@gmail.com>
2023-11-08 16:26:53 +0900

    feat : 당첨 번호와 보너스 번호의 공통 조상 클래스 구현

---
+++ src/main/java/lotto/LottoNumber.java
@@ -0,0 +1,39 @@
+package lotto;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+public class LottoNumber {
+
+    private final static Integer LOTTO_FIRST_NUMBER = 1;
+    private final static Integer LOTTO_FINAL_NUMBER = 45;
+
+    public LottoNumber(String[] winNumbers) {
+        for(String winNumber : winNumbers) {
+            validateInteger(winNumber);
+            validateRange(Integer.parseInt(winNumber));
+        }
+
+    }
+
+    private LottoNumber(String bonusNumber) {
+        validateInteger(bonusNumber);
+        validateRange(Integer.parseInt(bonusNumber));
+    }
+
+    private void validateInteger(String userInput) {
+        try {
+            Integer.parseInt(userInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력하신 번호는 정수로 변환할 수 없습니다");
+        }
+    }
+
+    private void validateRange(Integer number) {
+        if(!(LOTTO_FIRST_NUMBER <= number && number <= LOTTO_FINAL_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
+
+}

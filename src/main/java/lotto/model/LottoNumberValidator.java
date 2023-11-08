63084ac9b2e2c95cab2215d02d33d8317193c2a7
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 15:26:05 +0900

    feat: 공통적으로 사용되는 검증로직을 모아 클래스 구현

--- src/main/java/lotto/model/LottoNumberValidator.java
+++ src/main/java/lotto/model/LottoNumberValidator.java
@@ -1,2 +1,35 @@
-package lotto.model;public class LottoNumberValidator {
+package lotto.model;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lotto.enums.ErrorMessage;
+
+public class LottoNumberValidator {
+    public static void validateNumbersSize(List<Integer> numbers) throws IllegalArgumentException {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException(
+                ErrorMessage.INVALID_NUMBERS_COUNT_ERROR.getMessage());
+        }
+    }
+
+    public static void validateNumberRange(List<Integer> numbers) throws IllegalArgumentException {
+        boolean isOutOfRange = numbers.stream().anyMatch(number -> number < 1 || number > 45);
+        if (isOutOfRange) {
+            throw new IllegalArgumentException(ErrorMessage.EXCEED_LOTTO_RANGE_ERROR.getMessage());
+        }
+    }
+
+    public static void validateDuplicateNumbers(List<Integer> numbers) throws IllegalArgumentException {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        List<Integer> duplicateNumbers = numbers.stream()
+            .filter(number -> !uniqueNumbers.add(number))
+            .collect(Collectors.toList());
+
+        if (!duplicateNumbers.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NUMBER_ERROR.getMessage());
+        }
+    }
+
 }

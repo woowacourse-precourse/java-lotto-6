fbab5a69edc1977bb01d87a153cdb78cb4696dff
hanriverkitty <danieltls@naver.com>
2023-11-08 16:04:04 +0900

    Feat: 로뜨 랜덤 생성클래스 생성

--- src/main/java/domain/GenerateLotto.java
+++ src/main/java/domain/GenerateLotto.java
@@ -1,2 +1,12 @@
-package domain;public class GenerateLotto {
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class GenerateLotto {
+    public List<Integer> generatedNumbers;
+    public GenerateLotto(){
+        this.generatedNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
 }

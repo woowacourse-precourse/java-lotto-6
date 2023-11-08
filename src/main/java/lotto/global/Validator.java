9f479237cb46ef0de2e32ba3f0bcad77b3e02f0d
ckkim817 <ckkim817@konkuk.ac.kr>
2023-11-08 20:51:08 +0900

    [Refactor] Input과 Validator 클래스 분리 및 중복되는 문맥 삭제

---
+++ src/main/java/lotto/global/Validator.java
@@ -0,0 +1,65 @@
+package lotto.global;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Validator {
+
+    public static boolean purchasePriceFormat(String input) {
+        if (!input.matches("^(\\d+000)$")) {
+            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력받으며 1,000원으로 나누어떨어져야 합니다.\n");
+        }
+
+        return true;
+    }
+
+    public static boolean lottoNumbers(List<String> lottoNumbers) {
+        lottoNumbersCount(lottoNumbers);
+        lottoNumbersFormat(lottoNumbers);
+        lottoNumbersDuplicate(lottoNumbers);
+
+        return true;
+    }
+
+    public static boolean bonusNumber(String input, List<String> winningNumbers) {
+        lottoNumberFormat(input);
+        bonusNumberDuplicate(input, winningNumbers);
+
+        return true;
+    }
+
+    private static void lottoNumbersCount(List<String> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("로또 번호는 쉼표(,)를 기준으로 6개여야 합니다.\n");
+        }
+    }
+
+    private static void lottoNumbersFormat(List<String> numbers) {
+        for (String number : numbers) {
+            lottoNumberFormat(number);
+        }
+    }
+
+    private static void lottoNumbersDuplicate(List<String> numbers) {
+        Set<String> noDuplicateNumbers = new HashSet<>(numbers);
+
+        if (noDuplicateNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 중복되지 않는 숫자 6개여야 합니다.\n");
+        }
+    }
+
+    private static void lottoNumberFormat(String input) {
+        if (!input.matches("^([1-9]|[1-3][0-9]|4[0-5])$")) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
+        }
+    }
+
+    private static void bonusNumberDuplicate(String input, List<String> winningNumbers) {
+        for (String number : winningNumbers) {
+            if (input.equals(number)) {
+                throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 않아야 합니다.\n");
+            }
+        }
+    }
+}
--- src/main/java/lotto/ui/Input.java
+++ src/main/java/lotto/ui/Input.java
@@ -2,10 +2,9 @@ package lotto.ui;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
+import lotto.global.Validator;
 
 public class Input {
 
@@ -18,7 +17,7 @@ public class Input {
         while (!isValid) {
             try {
                 readPurchasePrice();
-                isValid = validatePurchasePriceFormat();
+                isValid = Validator.purchasePriceFormat(this.input);
 
             } catch (IllegalArgumentException e) {
                 System.out.print("[ERROR] " + e.getMessage());
@@ -34,7 +33,7 @@ public class Input {
         while (!isValid) {
             try {
                 readWinningNumbers();
-                isValid = validateWinningNumbers();
+                isValid = Validator.lottoNumbers(this.winningNumbers);
 
             } catch (IllegalArgumentException e) {
                 System.out.print("[ERROR] " + e.getMessage());
@@ -48,7 +47,7 @@ public class Input {
         while (!isValid) {
             try {
                 readBonusNumber();
-                isValid = validateBonusNumber();
+                isValid = Validator.bonusNumber(this.input, this.winningNumbers);
 
             } catch (IllegalArgumentException e) {
                 System.out.print("[ERROR] " + e.getMessage());
@@ -79,65 +78,6 @@ public class Input {
     private void readBonusNumber() {
         System.out.print("\n보너스 번호를 입력해 주세요.\n");
         this.input = Console.readLine().trim();
-    }
-
-    private boolean validatePurchasePriceFormat() {
-        if (!this.input.matches("^(\\d+000)$")) {
-            throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력받으며 1,000원으로 나누어떨어져야 합니다.\n");
-        }
-
-        return true;
-    }
-
-    private boolean validateWinningNumbers() {
-        validateWinningNumbersCount();
-        validateWinningNumbersFormat();
-        validateWinningNumbersDuplicate();
-
-        return true;
-    }
-
-    private boolean validateBonusNumber() {
-        validateBonusNumberFormat();
-        validateBonusNumberDuplicate();
-
-        return true;
-    }
-
-    private void validateWinningNumbersCount() {
-        if (this.winningNumbers.size() != 6) {
-            throw new IllegalArgumentException("당첨 번호는 쉼표(,)를 기준으로 6개여야 합니다.\n");
-        }
-    }
-
-    private void validateWinningNumbersFormat() {
-        for (String number : this.winningNumbers) {
-            if (!number.matches("^([1-9]|[1-3][0-9]|4[0-5])$")) {
-                throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
-            }
-        }
-    }
-
-    private void validateWinningNumbersDuplicate() {
-        Set<String> noDuplicateNumbers = new HashSet<>(this.winningNumbers);
-
-        if (noDuplicateNumbers.size() != this.winningNumbers.size()) {
-            throw new IllegalArgumentException("당첨 번호는 중복되지 않는 숫자 6개여야 합니다.\n");
-        }
-    }
-
-    private void validateBonusNumberFormat() {
-        if (!this.input.matches("^([1-9]|[1-3][0-9]|4[0-5])$")) {
-            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
-        }
-    }
-
-    private void validateBonusNumberDuplicate() {
-        for (String number : this.winningNumbers) {
-            if (this.input.equals(number)) {
-                throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되지 않아야 합니다.\n");
-            }
-        }
     }
 
     private int getLottoTicketsNumber() {

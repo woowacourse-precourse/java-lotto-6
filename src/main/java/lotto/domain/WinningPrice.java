13526b904c3636a87d5e996f970fb7709dd398bf
GiJung <weeds2577@gmail.com>
2023-11-08 16:32:43 +0900

    feat(WinningPrice): 총 우승 금액을 저장하는 객체 생성

---
+++ src/main/java/lotto/domain/WinningPrice.java
@@ -0,0 +1,14 @@
+package lotto.domain;
+
+public class WinningPrice {
+
+    private final long winningPrice;
+
+    public WinningPrice(long winningPrice) {
+        this.winningPrice = winningPrice;
+    }
+
+    public long getWinningPrice() {
+        return winningPrice;
+    }
+}

59f43363833d468ec6fab361eb2444f045995e5c
jjongwon7 <jjongwon7@naver.com>
2023-11-08 23:04:40 +0900

    test: 로또 당첨 번호 입력 예외 케이스 테스트 작성 입력한 로또 당첨 번호가 1~45 사이의 값이 아닌 경우

--- src/test/java/lotto/validators/WinningNumberValidatorTest.java
+++ src/test/java/lotto/validators/WinningNumberValidatorTest.java
@@ -1,8 +1,11 @@
 package lotto.validators;
 
 import static lotto.constant.ErrorMessage.INPUT_WINNING_NUMBER_COUNT_IS_INCORRECT;
+import static lotto.constant.ErrorMessage.INPUT_WINNING_NUMBER_VALUE_OUT_OF_RANGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,5 +21,18 @@ public class WinningNumberValidatorTest {
                 () -> WinningNumberValidator.validWinningNumberCount(winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INPUT_WINNING_NUMBER_COUNT_IS_INCORRECT);
+    }
+
+    @Test
+    @DisplayName("로또 당첨 번호가 범위 내의 값이 아니라면 예외가 발생한다.")
+    void validWinningNumberValueInRange() {
+        // given
+        List<Integer> winningNumbers = Arrays.asList(1, 2, 3, 4, 5, 50);
+
+        // when & then
+        assertThatThrownBy(
+                () -> WinningNumberValidator.validWinningNumberValueInRange(winningNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INPUT_WINNING_NUMBER_VALUE_OUT_OF_RANGE);
     }
 }

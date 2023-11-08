dd399426865510e10a58a09a1454a221dda32dd5
yoonjuho <dbswn990@naver.com>
2023-11-08 17:47:52 +0900

    refactor(Strategy): 클래스 명 변경

--- src/test/java/lotto/model/StrategyTest.java
+++ src/test/java/lotto/model/StrategyTest.java
@@ -1,6 +1,5 @@
 package lotto.model;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,27 +8,26 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class LottoCreateStrategyTest {
+class StrategyTest {
     private List<Integer> possibleNumbers;
 
     @BeforeEach
-    private void init(){
+    private void init() {
         possibleNumbers = new ArrayList<>();
-        for(int i = LottoNumber.MIN; i <= LottoNumber.MAX; i++){
+        for (int i = LottoNumber.MIN; i <= LottoNumber.MAX; i++) {
             possibleNumbers.add(i);
         }
     }
 
     @Test
     @DisplayName("1~45의 숫자 중 총 6개의 랜덤한 값이 추출된다")
-    void create(){
-        LottoCreateStrategy strategy = new LottoCreateStrategy();
-        List<Integer> numbers = strategy.create();
+    void create() {
+        List<Integer> numbers = Strategy.generateLotto();
         Assertions.assertAll(
                 () -> assertThat(possibleNumbers.containsAll(numbers)).isTrue(),
-                () -> assertThat(numbers.size()).isEqualTo(LottoNumber.SIZE)
+                () -> assertThat(numbers.size()).isEqualTo(Lotto.SIZE)
         );
     }
 }

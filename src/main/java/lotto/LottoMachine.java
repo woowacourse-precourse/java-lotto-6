6c34779543a61bbac3b59398f2f7765b39f70494
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 16:20:57 +0900

    로또 구매 기능 구현
    사용자가 지정한 수량만큼 로또를 구매하는 기능을 추가하였습니다.
    - 인자로 받은 숫자(numberOfLottos)만큼 반복하여 로또를 생성합니다.
    - 생성된 각 로또는 purchasedLottos 리스트에 추가됩니다.

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,9 +1,19 @@
 package lotto;
 
-public class LottoMachine {
-    private final LottoGenerator lottoGenerator;
-    public LottoMachine(LottoGenerator generator) {
-        this.lottoGenerator = generator;
-    }
+import java.util.ArrayList;
+import java.util.List;
 
+public class LottoMachine {
+    private final LottoGenerator generator;
+    private final List<Lotto> purchasedLottos;
+    public LottoMachine(LottoGenerator generator) {
+        this.generator = generator;
+        this.purchasedLottos = new ArrayList<>();
+    }
+    public void purchaseLottos(int numberOfLottos) {
+        for (int i = 0; i < numberOfLottos; i++) {
+            Lotto lotto = generator.generate();
+            purchasedLottos.add(lotto);
+        }
+    }
 }

8a7056553df78f71a2a19dc5bcdad87c9ec7bc8f
haebyun <bhk4392@gmail.com>
2023-11-08 17:17:06 +0900

    feat(Lotto): 로또를 생성할 때 필요한 과정들을 마치고 로또를 생성한다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,21 +1,19 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.validation.LottoNumbersValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
-
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        LottoNumbersValidator.validate(numbers);
+        ArrayList<Integer> validNumbers = new ArrayList<>(numbers);
+        Collections.sort(validNumbers);
+        this.numbers = validNumbers;
     }
 
     // TODO: 추가 기능 구현
-
 }

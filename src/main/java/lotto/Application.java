caa00e49118d4f0a2ef5045b6d83482a32a82e14
BH946 <kobh2895@gmail.com>
2023-11-08 22:08:51 +0900

    feat: 메인함수 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.util.LottoGenerator;
+import lotto.util.Referee;
+import lotto.view.InputLotto;
+import lotto.view.OutputLotto;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int purchaseCount = InputLotto.purchaseMoney();
+
+        List<Lotto> lottos = LottoGenerator.createLottos(purchaseCount);
+        OutputLotto.counts(purchaseCount);
+        OutputLotto.numbers(lottos);
+
+        Lotto answer = new Lotto(InputLotto.answerNumber());
+        int bonus = InputLotto.bonusNumber();
+
+        int[] winning = Referee.compareToLottos(lottos, answer, bonus);
+        double income = Referee.calculateIncome(winning, purchaseCount);
+        OutputLotto.winningHistory(winning);
+        OutputLotto.incomeRate(income);
     }
 }

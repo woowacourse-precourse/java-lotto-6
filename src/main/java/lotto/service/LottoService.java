7d0bd9c5f0a2fb48ec83caab9ff9d88cc4817ea7
haebyun <bhk4392@gmail.com>
2023-11-08 19:29:29 +0900

    feat(LottoService): 당첨 개수들을 등수에 따라 정리하여 문자열 리스트 형태로 반환하는 기능 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,17 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import lotto.domain.Lotto;
+import lotto.domain.LottoResult;
 import lotto.domain.WinningLotto;
 import lotto.util.Converter;
 
 public class LottoService {
     private final List<Lotto> lottos = new ArrayList<>();
     private WinningLotto winningLotto;
+    private LottoResult lottoResult;
     private int purchaseAmount;
 
     public void buyLotto(int purchaseAmount) {
         this.purchaseAmount = purchaseAmount;
-        int numberOfLottos = purchaseAmount/1000;
+        int numberOfLottos = purchaseAmount / 1000;
         generateLotto(numberOfLottos);
     }
 
@@ -39,5 +41,10 @@ public class LottoService {
 
     public void makeWinningLotto(List<Integer> winningNumbers, int bonusNumber) {
         winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+    }
+
+    public List<String> createWinningResults() {
+        lottoResult = new LottoResult(lottos, winningLotto);
+        return lottoResult.getFormalizedResult();
     }
 }

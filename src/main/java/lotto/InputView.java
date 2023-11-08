3364a0368f27d5fe99a4e2752184f048b8162fde
oosedus <seayeon0729@gmail.com>
2023-11-08 17:42:29 +0900

    fix: 타입 오류 해결

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -129,7 +129,7 @@ public class InputView {
         if(bonusNumber < LOTTO_MINIMUM_NUMBER || bonusNumber > LOTTO_MAXIMUM_NUMBER){
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
-        if(lottoNumbers.contains(input)) throw new IllegalArgumentException("[ERROR] 보너스 번호와 기존 당첨 번호는 중복될 수 없습니다.");
+        if(lottoNumbers.contains(bonusNumber)) throw new IllegalArgumentException("[ERROR] 보너스 번호와 기존 당첨 번호는 중복될 수 없습니다.");
         return bonusNumber;
     }
 }

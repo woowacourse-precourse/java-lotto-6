b64d539022ca5158f3d31a2c0e977beb2ac2dfdf
Seyeon <wjdtpdus828@naver.com>
2023-11-08 16:29:23 +0900

    style(Views): 띄어쓰기 오타 수정

--- src/main/java/lotto/constant/Views.java
+++ src/main/java/lotto/constant/Views.java
@@ -5,7 +5,7 @@ public enum Views {
     OUTPUT_PURCHASE_AMOUT_MESSAGE("개를 구매했습니다."),
     INPUT_WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
-    OUTPUT_RATE_RESULT_MESSAGE("총 수익률은 %s%% 입니다."),
+    OUTPUT_RATE_RESULT_MESSAGE("총 수익률은 %s%%입니다."),
     OUTPUT_WINNING_RESULT("당첨 통계"),
     OUTPUT_UNDER_BAR("---");
 

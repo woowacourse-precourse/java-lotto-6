002826be949fd142e6da0c8d40322be4c491d062
mini1115 <kmini9611@naver.com>
2023-11-08 14:02:13 +0900

    feat: 입력값 검증 및 예외처리 추가

--- src/main/java/lotto/PlayerInput.java
+++ src/main/java/lotto/PlayerInput.java
@@ -9,7 +9,14 @@ public class PlayerInput {
     //금액 입력받기
     public int InputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        int money = Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        try {
+            ErrorValidate.isNumberFormat(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.ERROR_IS_NUMBER.getErrorMessage());
+            InputMoney();
+        }
+        int money = Integer.parseInt(input);
         return money;
     }
 
@@ -18,11 +25,18 @@ public class PlayerInput {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         for (String number : input.split(",")) {
+            try {
+                ErrorValidate.inputNumberLimit(Integer.parseInt(number));
+            } catch (IllegalArgumentException e) {
+                System.out.println(ErrorMessage.ERROR_NUMBER_RANGE.getErrorMessage());
+                LottoNumber();
+            }
             winNumber.add(Integer.parseInt(number));
         }
         return winNumber;
     }
-    public int BonusNumber(){
+
+    public int BonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.");
         int bonusNumber = Integer.parseInt(Console.readLine());
         return bonusNumber;

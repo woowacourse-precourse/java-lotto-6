d60458bed95b009c5237612a8b60babbe8534352
dmong <dt2980_hee@naver.com>
2023-11-08 10:21:29 +0900

    test: PrizeTest에 보너스 번호 매칭 여부에 따라 다른 값 반환 검증 추가

--- src/test/java/lotto/domain/PrizeTest.java
+++ src/test/java/lotto/domain/PrizeTest.java
@@ -1,35 +1,48 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PrizeTest {
 
-    @Test
-    void 매칭되는_타입이_없으면_NONE_기본값_생성() {
-        int numOfMatches = 10;
-        boolean bonusNumberIncluded = true;
-
-        Assertions.assertThat(Prize.valueOf(numOfMatches, bonusNumberIncluded))
+    @ParameterizedTest
+    @CsvSource(value = {"10, true", "20, false", "1, false"})
+    void 매칭되는_타입이_없으면_NONE_기본값_생성(int numOfMatches, boolean bonusMatched) {
+        assertThat(Prize.valueOf(numOfMatches, bonusMatched))
                 .isEqualTo(Prize.NONE);
     }
 
     @Test
     void 여러_조건_매칭될_경우_제일_상금이_높은_값으로_생성() {
         int numOfMatches = Prize.THIRD.getNumberOfMatches();
-        boolean bonusNumberIncluded = Prize.SECOND.isBonusIncluded();
+        boolean bonusMatched = Prize.SECOND.isBonusIncluded();
 
-        Assertions.assertThat(Prize.valueOf(numOfMatches, bonusNumberIncluded))
+        assertThat(Prize.valueOf(numOfMatches, bonusMatched))
                 .isEqualTo(Prize.SECOND);
     }
 
     @Test
     void 보너스_번호_매칭_여부에_관계없이_같은_값_생성() {
         int numOfMatches = Prize.FOURTH.getNumberOfMatches();
-        boolean bonusNumberIncluded = Prize.FOURTH.isBonusIncluded();
+        boolean bonusMatched = Prize.FOURTH.isBonusIncluded();
 
-        Assertions.assertThat(Prize.valueOf(numOfMatches, bonusNumberIncluded))
-                .isEqualTo(Prize.valueOf(numOfMatches, !bonusNumberIncluded));
+        assertThat(Prize.valueOf(numOfMatches, bonusMatched))
+                .isEqualTo(Prize.valueOf(numOfMatches, !bonusMatched));
+    }
+
+    @Test
+    void 보너스_번호_매칭_여부에_따라_다른_반환값_생성() {
+        int numOfMatches = 5;
+        boolean bonusMatched = true;
+
+        Prize secondExpected = Prize.valueOf(numOfMatches, bonusMatched);
+        Prize thirdExpected = Prize.valueOf(numOfMatches, !bonusMatched);
+
+        assertThat(secondExpected).isEqualTo(Prize.SECOND);
+        assertThat(thirdExpected).isEqualTo(Prize.THIRD);
     }
 
 }

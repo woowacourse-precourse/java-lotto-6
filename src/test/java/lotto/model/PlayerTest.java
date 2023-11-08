153bccf8eb9c0f6e5255e996f52c20b3962059e8
leejh7 <777joonho@kookmin.ac.kr>
2023-11-08 21:36:20 +0900

    test: DateFormat 관련 메서드 테스트 작성

--- src/test/java/lotto/model/PlayerTest.java
+++ src/test/java/lotto/model/PlayerTest.java
@@ -1,5 +1,6 @@
 package lotto.model;
 
+import lotto.constant.ConsoleMessage;
 import lotto.constant.LotteryRank;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -16,20 +17,6 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
-    @DisplayName("둘째 자리에서 반올림 테스트")
-    @ParameterizedTest
-    @CsvSource(value = {"12.345:12.3", "123.456:123.5", "123.45:123.5", "10:10.0"}, delimiter = ':')
-    void decimalFormatTest(double value, double expected) {
-        // given
-        DecimalFormat df = new DecimalFormat("#.#");
-
-        // when
-        double result = Double.parseDouble(df.format(value));
-
-        // then
-        assertThat(result).isEqualTo(expected);
-    }
-
     @DisplayName("로또 게임 이용자의 당첨 내역 테스트")
     @ParameterizedTest
     @MethodSource("generateLotto")

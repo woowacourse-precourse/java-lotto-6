9b4eae273481120ef1bb19c01b93d62b43e3bfc5
Jihyun3478 <leejihyun3478@gmail.com>
2023-11-08 16:35:16 +0900

    feat(Constant): Constant 클래스 추가

---
+++ src/main/java/lotto/view/constatnt/Constant.java
@@ -0,0 +1,9 @@
+package lotto.view.constatnt;
+
+public class Constant {
+    public static final int MIN_LENGTH = 1;
+    public static final int MAX_LENGTH = 45;
+    public static final int COUNT = 6;
+    public static final int DIVIDE_AMOUNT = 1000;
+    public static final String COMMA = ",";
+}

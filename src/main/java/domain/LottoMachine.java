7adb4e156ddfb02b321af70e1ad233758e16e0d0
ddohyeong <dohyeong7026@gmail.com>
2023-11-08 16:09:49 +0900

    refactor(LottoMachine) : 가독성 및 유지 보수를 위해 상수 처리

--- src/main/java/domain/LottoMachine.java
+++ src/main/java/domain/LottoMachine.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoMachine {
+	private static final int LOTTO_SIZE = 6;
+	private static final int MIN_LOTTO_NUMBER = 1;
+	private static final int MAX_LOTTO_NUMBER = 45;
 	private List<Integer> randomNumbers;
 
 	public LottoMachine() {
@@ -17,7 +20,7 @@ public class LottoMachine {
 	}
 
 	private List<Integer> generateRandomNumbers() {
-		return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_SIZE);
 	}
 
 	private List<Integer> sortRandomNumbers(List<Integer> numbers) {

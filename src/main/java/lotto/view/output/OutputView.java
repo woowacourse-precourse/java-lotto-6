f6c2d13b90ad539e28e2f8e224b47bac8fed659d
gmelon <hsh1769@naver.com>
2023-11-08 19:08:48 +0900

    refactor: OutputView에서 StringBuilder를 사용하여 메시지를 만들도록 개선

--- src/main/java/lotto/view/output/OutputView.java
+++ src/main/java/lotto/view/output/OutputView.java
@@ -12,15 +12,15 @@ import lotto.domain.LottoRank;
 import lotto.domain.LottoResult;
 
 public class OutputView {
-    private static final String PURCHASED_LOTTO_SIZE_MESSAGE_FORMAT = "%d개를 구매했습니다.";
-    private static final String PURCHASED_LOTTO_NUMBERS_MESSAGE_FORMAT = "[%s]";
+    private static final String PURCHASED_LOTTO_SIZE_MESSAGE_FORMAT = "%d개를 구매했습니다.\n";
+    private static final String PURCHASED_LOTTO_NUMBERS_MESSAGE_FORMAT = "[%s]\n";
     private static final String LOTTO_NUMBERS_JOINING_DELIMETER = ", ";
     private static final String LOTTO_RESULT_TITLE_MESSAGE = "당첨 통계\n---";
     private static final String LOTTO_RANK_MATCH_COUNT_MESSAGE_FORMAT = "%d개 일치";
     private static final String BONUS_NUMBER_MATCHED_MESSAGE = ", 보너스 볼 일치";
     private static final String LOTTO_RANK_PRIZE_MESSAGE_FORMAT = " (%,d원)";
-    private static final String LOTTO_RESULT_MATCH_COUNT_MESSAGE_FORMAT = " - %d개";
-    private static final String PRIZE_RATE_MESSAGE_FORMAT = "총 수익률은 %.2f%%입니다.";
+    private static final String LOTTO_RESULT_MATCH_COUNT_MESSAGE_FORMAT = " - %d개\n";
+    private static final String PRIZE_RATE_MESSAGE_FORMAT = "총 수익률은 %.2f%%입니다.\n";
 
     private OutputView() {
     }
@@ -29,49 +29,48 @@ public class OutputView {
         List<Lotto> purchasedLottos = customer.purchasedLottos();
         System.out.printf(PURCHASED_LOTTO_SIZE_MESSAGE_FORMAT, purchasedLottos.size());
 
+        StringBuilder stringBuilder = new StringBuilder();
         for (Lotto purchasedLotto : purchasedLottos) {
-            String joinedLottoNumbers = joiningLottoNumbers(purchasedLotto.numbers());
-            System.out.printf(PURCHASED_LOTTO_NUMBERS_MESSAGE_FORMAT, joinedLottoNumbers);
+            buildPurchasedLottoNumbersMessage(purchasedLotto.numbers(), stringBuilder);
         }
+        System.out.print(stringBuilder);
     }
 
-    private static String joiningLottoNumbers(List<LottoNumber> lottoNumbers) {
-        return lottoNumbers.stream()
+    private static void buildPurchasedLottoNumbersMessage(List<LottoNumber> lottoNumbers, StringBuilder stringBuilder) {
+        String joinedLottoNumbers = lottoNumbers.stream()
                 .map(lottoNumber -> String.valueOf(lottoNumber.value()))
                 .collect(joining(LOTTO_NUMBERS_JOINING_DELIMETER));
+
+        stringBuilder.append(String.format(PURCHASED_LOTTO_NUMBERS_MESSAGE_FORMAT, joinedLottoNumbers));
     }
 
     public static void printLottoResult(LottoResult lottoResult) {
         System.out.println(LOTTO_RESULT_TITLE_MESSAGE);
 
-        for (String lottoRankResultMessage : lottoRankResultMessages(lottoResult)) {
-            System.out.println(lottoRankResultMessage);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (LottoRank lottoRank : printableLottoRanks()) {
+            buildLottoRankResultMessage(lottoRank, lottoResult.count(lottoRank), stringBuilder);
         }
+        System.out.print(stringBuilder);
     }
 
-    private static List<String> lottoRankResultMessages(LottoResult lottoResult) {
+    private static List<LottoRank> printableLottoRanks() {
         return Arrays.stream(LottoRank.values())
                 .filter(lottoRank -> lottoRank != LottoRank.NONE)
                 .sorted(Comparator.comparingInt(LottoRank::prize))
-                .map(lottoRank -> lottoRankResultMessage(lottoRank, lottoResult.count(lottoRank)))
                 .toList();
     }
 
-    private static String lottoRankResultMessage(LottoRank rank, int count) {
-        StringBuilder stringBuilder = new StringBuilder();
-
+    private static void buildLottoRankResultMessage(LottoRank rank, int count, StringBuilder stringBuilder) {
         stringBuilder.append(String.format(LOTTO_RANK_MATCH_COUNT_MESSAGE_FORMAT, rank.matchCount()));
         if (rank.hasBonusNumber()) {
             stringBuilder.append(BONUS_NUMBER_MATCHED_MESSAGE);
         }
         stringBuilder.append(String.format(LOTTO_RANK_PRIZE_MESSAGE_FORMAT, rank.prize()));
         stringBuilder.append(String.format(LOTTO_RESULT_MATCH_COUNT_MESSAGE_FORMAT, count));
-
-        return stringBuilder.toString();
     }
 
     public static void printPrizeRate(LottoResult lottoResult) {
         System.out.printf(PRIZE_RATE_MESSAGE_FORMAT, lottoResult.calculatePrizeRate());
     }
 }
-

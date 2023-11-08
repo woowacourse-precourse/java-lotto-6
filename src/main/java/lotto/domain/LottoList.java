3a5c96742bbbd2ec217fbd18073d10e44c74e276
bokyung <bbookng@gmail.com>
2023-11-08 16:08:49 +0900

    feat(LottoList) : Lotto 하나 당 결과에 따라 전체 LottoResult 반환 로직 구현

--- src/main/java/lotto/domain/LottoList.java
+++ src/main/java/lotto/domain/LottoList.java
@@ -1,6 +1,8 @@
 package lotto.domain;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoList {
     private final List<Lotto> lottoList;
@@ -11,5 +13,14 @@ public class LottoList {
 
     public List<Lotto> getLottoList() {
         return this.lottoList;
+    }
+
+    public LottoResultMap getLottoResult(WinningNumbers winningNumbers) {
+        Map<LottoResult, Integer> lottoResultCountMap = new HashMap<>();
+        lottoList.stream().forEach(lotto -> {
+            LottoResult lottoResult = lotto.getResult(winningNumbers);
+            lottoResultCountMap.put(lottoResult, lottoResultCountMap.getOrDefault(lottoResult, 0) + 1);
+        });
+        return new LottoResultMap(lottoResultCountMap);
     }
 }

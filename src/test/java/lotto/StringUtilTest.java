aff4177aec3c0af6e628c6979884d78b1eebdccc
taekgyun <show9510@naver.com>
2023-11-08 23:05:03 +0900

    StringUtilTest

---
+++ src/test/java/lotto/StringUtilTest.java
@@ -0,0 +1,28 @@
+package lotto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lotto.util.StringUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringUtilTest {
+    @DisplayName("splitWithComma 테스트")
+    @Test
+    void testStringSplitWithComma() {
+        assertEquals(List.of("1", "2", "3"), StringUtil.splitWithComma("1,2,3"));
+    }
+
+    @DisplayName("convertStringToInt 테스트")
+    @Test
+    void testStringtoInt() {
+        assertEquals(1, StringUtil.convertStringToInt("1"));
+    }
+
+    @DisplayName("convertListToString 테스트")
+    @Test
+    void testListToString() {
+        assertEquals("1, 2, 3", StringUtil.convertListToString(List.of(1, 2, 3)));
+    }
+}

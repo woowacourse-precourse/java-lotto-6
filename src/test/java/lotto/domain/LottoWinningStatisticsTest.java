e153ccfd8f028b0579a3292c710adfbdd206e2ec
eora21 <eora21@naver.com>
2023-11-08 13:20:13 +0900

    test: 💍 수정 로또 통계 객체 사용하도록 변경, 당첨 등급에 옵셔널 적용 해제

--- src/test/java/lotto/domain/LottoWinningStatisticsTest.java
+++ src/test/java/lotto/domain/LottoWinningStatisticsTest.java
@@ -18,20 +18,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class LottoWinningStatisticsTest {
+    private final LottoWinningStatistics lottoWinningStatistics = new LottoWinningStatistics();
     @Test
     @DisplayName("불가능한 금액을 입력할 시 예외가 발생한다.")
     void calculateStatisticsByImpossibleAmount() {
         assertThrows(IllegalArgumentException.class, () ->
-                LottoWinningStatistics.calculateStatistics(new Amount(-1_000),
-                        List.of(Optional.of(LottoWinningTier.FIRST_TIER))));
+                lottoWinningStatistics.calculateStatistics(new Amount(-1_000),
+                        List.of(LottoWinningTier.FIRST_TIER)));
     }
 
     @ParameterizedTest
     @DisplayName("통계 정상 반환")
     @MethodSource("AmountWinningTiersAndExpectPercentOfTotalWinningAmount")
-    void calculateStatistics(Amount amount, List<Optional<LottoWinningTier>> winningTiers, double expectPercent) {
-        LottoStatisticsResult lottoStatisticsResult = assertDoesNotThrow(
-                () -> LottoWinningStatistics.calculateStatistics(amount, winningTiers));
+    void calculateStatistics(Amount amount, List<LottoWinningTier> winningTiers, double expectPercent) {
+        LottoStatisticsResult lottoStatisticsResult = assertDoesNotThrow(() ->
+                lottoWinningStatistics.calculateStatistics(amount, winningTiers));
 
         assertThat(lottoStatisticsResult.getPercentOfTotalWinningAmount()).isEqualTo(expectPercent);
     }
@@ -39,39 +40,26 @@ public class LottoWinningStatisticsTest {
     public static Stream<Arguments> AmountWinningTiersAndExpectPercentOfTotalWinningAmount() {
         return Stream.of(
                 Arguments.of(new Amount(2_000),
-                        List.of(Optional.empty(),
-                                Optional.of(LottoWinningTier.FIRST_TIER)),
+                        List.of(LottoWinningTier.FIRST_TIER),
                         100_000_000),
 
                 Arguments.of(new Amount(1_000),
-                        List.of(Optional.of(LottoWinningTier.FIRST_TIER)),
+                        List.of(LottoWinningTier.FIRST_TIER),
                         200_000_000),
 
                 Arguments.of(new Amount(3_000),
                         List.of(
-                                Optional.of(LottoWinningTier.FIRST_TIER),
-                                Optional.of(LottoWinningTier.FIRST_TIER),
-                                Optional.of(LottoWinningTier.FIRST_TIER)),
+                                LottoWinningTier.FIRST_TIER,
+                                LottoWinningTier.FIRST_TIER,
+                                LottoWinningTier.FIRST_TIER),
                         200_000_000),
 
                 Arguments.of(new Amount(8_000),
-                        List.of(
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.of(LottoWinningTier.FIFTH_TIER)),
+                        List.of(LottoWinningTier.FIFTH_TIER),
                         62.5),
 
                 Arguments.of(new Amount(4_000),
-                        List.of(
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty(),
-                                Optional.empty()),
+                        List.of(),
                         0)
         );
     }

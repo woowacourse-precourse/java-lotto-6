564e08b596deb0b955dc2111677467322caf429f
mdgome <mdgome3@gmail.com>
2023-11-08 14:14:28 +0900

    test: 당첨 번호 예외 처리 추가
    1. 당첨 번호 구분자가 쉼표가 아닌 경우
    2. 당첨 번호를 입력하지 않는 경우
    3. 당첨 번호에 숫자가 아닌 값을 입력한 경우

---
+++ src/test/java/lotto/util/SplitGeneratorTest.java
@@ -0,0 +1,34 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SplitGeneratorTest {
+
+    @DisplayName("당첨 번호 입력 시 쉼표로 구분하지 않을 경우 예외가 발생한다.")
+    @Test
+    void inputWinNumbersIsNonContainsComma() {
+        String winNumbers = "1.2.3.4.5.6.7";
+
+        assertThatThrownBy(() -> new SplitGenerator(winNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void inputWinNumberIsNull() {
+        assertThatThrownBy(() -> new SplitGenerator(null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 숫자가 아닌 값을 입력하면 예외가 발생한다.")
+    @Test
+    void inputIsNumeric() {
+        String winNumbers = "1,2,3,4,5,seven";
+
+        assertThatThrownBy(() -> new SplitGenerator(winNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

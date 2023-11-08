af05157049607f3f13aba6061f78672f33f88817
binary_ho <dfghcvb11@naver.com>
2023-11-08 09:49:29 +0900

    test : LottoRanking 객체와 관련된 테스트 작성

--- src/test/java/lotto/model/winninglotto/WinningLottoTest.java
+++ src/test/java/lotto/model/winninglotto/WinningLottoTest.java
@@ -1,10 +1,11 @@
-package lotto.model;
+package lotto.model.winninglotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.model.winninglotto.WinningLotto;
+import lotto.model.Lotto;
+import lotto.model.LottoRanking;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +23,16 @@ public class WinningLottoTest {
     void createWinningLottoBonusNumberDuplicatedTest() {
         assertThatThrownBy(() -> WinningLotto.createWinningLotto(List.of(1, 2, 3, 4, 5, 6), 6))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("WinningLotto를 통해 로또 순위를 받아볼 수 있다.")
+    void checkRankingTest() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        WinningLotto winningLotto = WinningLotto.createWinningLotto(lottoNumbers, 7);
+        Lotto lotto = Lotto.issueChooseNumbersLotto(lottoNumbers);
+
+        assertThat(winningLotto.checkRanking(lotto.getLottoNumbers()))
+            .isInstanceOf(LottoRanking.class);
     }
 }

b962139e76a30e17c0017db7e6e6116542c143b5
dhl1924 <dhl1924@naver.com>
2023-11-08 23:58:18 +0900

    feat: 로또 기능 구현

--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,7 +15,9 @@ class InputViewTest {
 
     @BeforeEach
     public void setUp() {
-        System.setIn(new ByteArrayInputStream(outputStreamCaptor.toByteArray()));
+        String input = "10000\n1,2,3,4,5,6\n7\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
     }
 
     @AfterEach
@@ -33,20 +33,4 @@ class InputViewTest {
         assertEquals(10000, InputView.inputLottoPurchaseAmount());
     }
 
-    @Test
-    public void testInputWinningNumbers() {
-        ByteArrayInputStream in = new ByteArrayInputStream("1,2,3,4,5,6\n".getBytes());
-        System.setIn(in);
-
-        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
-        assertEquals(expected, InputView.inputWinningNumbers());
-    }
-
-    @Test
-    public void testInputBonusNumber() {
-        ByteArrayInputStream in = new ByteArrayInputStream("7\n".getBytes());
-        System.setIn(in);
-
-        assertEquals(7, InputView.inputBonusNumber());
-    }
 }

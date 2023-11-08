652ddea93d310af5317ace695cc7ebb27c31e915
TaeDongUm <etd93@naver.com>
2023-11-08 21:49:36 +0900

    [feat] inputLottoNumbers 기능 추가

--- src/main/java/lotto/service/InputLottoInfoService.java
+++ src/main/java/lotto/service/InputLottoInfoService.java
@@ -3,9 +3,11 @@ package lotto.service;
 import java.util.List;
 
 import lotto.domain.Lotto;
+import lotto.view.OutputView;
 
 
 public class InputLottoInfoService {
+	private OutputView outputView = new OutputView();
 
 	public Lotto inputLottoInfo() {
 		List<Integer> lottoNumbersList = inputLottoNumbers();
@@ -18,6 +20,12 @@ public class InputLottoInfoService {
 		lotto.setBonusNumber(bonusNumber);
 		return lotto;
 		
+	}
+	
+	public List<Integer> inputLottoNumbers(){
+		outputView.printAskingLottoNumbers();
+		List<Integer> lottoNumbers = lottoUtil();
+		return lottoNumbers;
 	}
 
 }

886ccd224532540ab8483eaa386b2e88ed52d94e
KDT live <dnjsxo0616@gmail.com>
2023-11-08 19:43:23 +0900

    fix(Application): 예외 처리 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,7 +4,11 @@ import lotto.controller.LottoController;
 
 public class Application {
     public static void main(String[] args) {
-        LottoController lottoController = new LottoController();
-        lottoController.play();
+        try{
+            LottoController lottoController = new LottoController();
+            lottoController.play();
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }

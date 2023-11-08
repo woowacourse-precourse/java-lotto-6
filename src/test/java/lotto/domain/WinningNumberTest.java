153fc48cae057d832008cf310017686a8eededd1
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 23:14:39 +0900

    [TEST] winningNumber와 bonusNumber 생성 시의 테스트 코드 작성
    .

---
+++ src/test/java/lotto/domain/WinningNumberTest.java
@@ -0,0 +1,38 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WinningNumberTest {
+    @DisplayName("당첨 번호와 보너스 번호가 중복되면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByDuplicate() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6, 7), 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverRangeBonusNumber() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6, 7), 50))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverRangeWinningNumber() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6, 50), 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호의 size가 6 이상이면 예외가 발생한다.")
+    @Test
+    void createWinningNumberByOverSize() {
+        assertThatThrownBy(() -> new WinningNumber(List.of(1, 2, 3, 4, 5, 6, 7, 8), 10))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

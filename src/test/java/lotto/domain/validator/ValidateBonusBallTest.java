16ca7eb71568423518852fde903d11210390347f
K-KY <kyuyoungk@naver.com>
2023-11-08 23:07:22 +0900

    Test(ValidateBonusBallTest) : 줄바꿈 추가

--- src/test/java/lotto/domain/validator/ValidateBonusBallTest.java
+++ src/test/java/lotto/domain/validator/ValidateBonusBallTest.java
@@ -12,25 +12,29 @@ public class ValidateBonusBallTest {
     @Test
     void 범위_내의_숫자_큼() {
         assertThatThrownBy(() -> new ValidateBonusBall().isInRange(46))
-                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 45보다 큰 숫자는 입력 할 수 없습니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 45보다 큰 숫자는 입력 할 수 없습니다.");
     }
 
     @Test
     void 범위_내의_숫자_작음() {
         assertThatThrownBy(() -> new ValidateBonusBall().isInRange(0))
-                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 1보다 작은 숫자는 입력 할 수 없습니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 1보다 작은 숫자는 입력 할 수 없습니다.");
     }
 
     @Test
     void 보너스볼_중복() {
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
         assertThatThrownBy(() -> new BonusBall(1, lotto))
-                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 보너스볼의 숫자와 같은 숫자가 존재합니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스볼의 숫자와 같은 숫자가 존재합니다.");
     }
     @Test
     void 보너스볼_중복_검증() {
         Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
         assertThatThrownBy(() -> ValidateBonusBall.isDuplicate(1, lotto))
-                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR] 보너스볼의 숫자와 같은 숫자가 존재합니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스볼의 숫자와 같은 숫자가 존재합니다.");
     }
 }

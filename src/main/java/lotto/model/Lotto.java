aa95becda20d913095add6ef7d71c4fff5dcc033
myonjin <kjinabc@gmail.com>
2023-11-08 20:50:03 +0900

    Feat : Lotto 객체

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -3,20 +3,18 @@ package lotto.model;
 import java.util.List;
 
 public class Lotto {
-    public static class Lotto {
-        private final List<Integer> numbers;
+    private final List<Integer> numbers;
 
-        public Lotto(List<Integer> numbers) {
-            validate(numbers);
-            this.numbers = numbers;
-        }
-
-        private void validate(List<Integer> numbers) {
-            if (numbers.size() != 6) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 입력 가능합니다.");
-            }
-        }
-
-        // TODO: 추가 기능 구현
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
     }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 입력 가능합니다.");
+        }
+    }
+
+    // TODO: 추가 기능 구현
 }

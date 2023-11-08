6deda454460464c6d7ba9e915f2d3550411105e5
KellyKimHyeJin <kelly317kim@naver.com>
2023-11-08 17:21:57 +0900

    feat: 당첨 번호 저장 기능 구현

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,5 +1,20 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LottoService {
+
+    public void saveWinNumber(String winNumber){
+        List<String> tempNumbers = Arrays.asList(winNumber.split(","));
+        List<Integer> numbers = new ArrayList<>();
+        for(String number: tempNumbers){
+            numbers.add(Integer.parseInt(number));
+        }
+        Lotto lotto = new Lotto(numbers);
+    }
+
+
 
 }

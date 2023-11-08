a225b2e5603c0e624ba1243bebe854042f9e64bd
junseoks-99 <angec5645@naver.com>
2023-11-08 21:40:00 +0900

    refactor: 등수 계산 로직 변경

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -30,20 +30,46 @@ public class Lotto {
 
     public int getRank(List<Integer> otherList, Integer bonusNumber) {
         Integer sameCount = calcSameNumberCount(otherList);
-        if (sameCount == LottoPrinciples.SIXTH_MATCHES.getNumber()) {
-            return WinnerRank.FIFTH_PLACE.getRank();
-        } else if (sameCount == LottoPrinciples.FIFTH_MATCHES.getNumber()) {
-            if (containsBonusNumber(bonusNumber)) {
-                return WinnerRank.FIFTH_PLACE.getRank();
-            }
-            return WinnerRank.THIRD_PLACE.getRank();
-        } else if (sameCount == LottoPrinciples.FOURTH_MATCHES.getNumber()) {
-            return WinnerRank.FOURTH_PLACE.getRank();
-        }
-        return WinnerRank.FIFTH_PLACE.getRank();
+        Integer rank = Rank.LOSE_PLACE.getRank();
+        rank = calcFirstPlaceNumber(sameCount, rank);
+        rank = calcSecondOrThirdPlaceNumber(sameCount, bonusNumber, rank);
+        rank = calcFourthPlaceNumber(sameCount, rank);
+        rank = calcFifthPlaceNumber(sameCount, rank);
+        return rank;
     }
 
     public boolean containsBonusNumber(Integer bonusNumber) {
         return this.numbers.contains(bonusNumber);
+    }
+
+    private int calcFirstPlaceNumber(Integer sameCount, Integer rank) {
+        if (sameCount == LottoPrinciples.SIXTH_MATCHES.getNumber()) {
+            return Rank.FIRST_PLACE.getRank();
+        }
+        return rank;
+    }
+
+    private int calcSecondOrThirdPlaceNumber(Integer sameCount, Integer bonusNumber, Integer rank) {
+        if (sameCount == LottoPrinciples.FIFTH_MATCHES.getNumber()) {
+            if (containsBonusNumber(bonusNumber)) {
+                return Rank.SECOND_PLACE.getRank();
+            }
+            return Rank.THIRD_PLACE.getRank();
+        }
+        return rank;
+    }
+
+    private int calcFourthPlaceNumber(Integer sameCount, Integer rank) {
+        if (sameCount == LottoPrinciples.FOURTH_MATCHES.getNumber()) {
+            return Rank.FOURTH_PLACE.getRank();
+        }
+        return rank;
+    }
+
+    private int calcFifthPlaceNumber(Integer sameCount, Integer rank) {
+        if (sameCount == LottoPrinciples.FIFTH_MATCHES.getNumber()) {
+            return Rank.FIFTH_PLACE.getRank();
+        }
+        return rank;
     }
 }

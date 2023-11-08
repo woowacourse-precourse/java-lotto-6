f25651de6ea78c78e09c7a6897b114a0735cd06c
i3kae <snoger1234@gmail.com>
2023-11-08 12:24:32 +0900

    refactor : 구매 금액, 보너스 번호, 당첨 번호에 대한 테스트 분리

--- src/test/java/lotto/InputTest.java
+++ src/test/java/lotto/InputTest.java
@@ -11,19 +11,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InputTest {
-    @DisplayName("구매 금액 입력시 비정상적인 문자열을 받는 경우")
+    @DisplayName("구매 금액 입력시 정상적인 문자열의 입력")
     @Test
-    void illigalPurchaseAmountInput() {
+    void normalPurchaseAmountInput() {
         Input input = new Input();
 
-        // 정상적인 경우 테스트
         assertThat(input.purchaseAmountInput("11000"))
                 .isEqualTo(new ArrayList<>(Arrays.asList(11000)));
         assertThat(input.purchaseAmountInput("11234000"))
                 .isEqualTo(new ArrayList<>(Arrays.asList(11234000)));
+    }
 
-        // 비정상적인 경우 테스트
-        // 문자열에 숫자를 제외한 다른 문자가 있을 경우
+    @DisplayName("구매 금액 입력시 숫자 이외의 문자를 포함한 문자열의 입력")
+    @Test
+    void nonNumericPurchaseAmountInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.purchaseAmountInput("!110000"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.purchaseAmountInput("+110000"))
@@ -32,27 +35,41 @@ public class InputTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.purchaseAmountInput("110000@"))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 문자열이 비어있는 경우
+    }
+    @DisplayName("구매 금액 입력시 비어있는 문자열 입력")
+    @Test
+    void emptyPurchaseAmountInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.purchaseAmountInput(""))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 구매 금액이 1000으로 나누어 떨어지지 않는 경우
+    }
+    @DisplayName("구매 금액 입력시 1000으로 나눠 떨어지지 않는 문자열의 입력")
+    @Test
+    void notDivide1000PurchaseAmountInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.purchaseAmountInput("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> input.purchaseAmountInput("123400"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("보너스 번호 입력시 비정상적인 문자열을 받는 경우")
+    @DisplayName("보너스 번호 입력시 정상적인 문자열의 입력")
     @Test
-    void illigalBonusNumberInput() {
+    void normalBonusNumberInput() {
         Input input = new Input();
 
-        // 정상적인 경우 테스트
         assertThat(input.bonusNumberInput("11"))
                 .isEqualTo(new ArrayList<>(Arrays.asList(11)));
         assertThat(input.bonusNumberInput("45"))
                 .isEqualTo(new ArrayList<>(Arrays.asList(45)));
+    }
+    @DisplayName("보너스 번호 입력시 숫자 이외의 문자를 포함한 문자열의 입력")
+    @Test
+    void nonNumericBonusNumberInput() {
+        Input input = new Input();
 
-        // 비정상적인 경우 테스트
-        // 문자열에 숫자를 제외한 다른 문자가 있을 경우
         assertThatThrownBy(() -> input.bonusNumberInput("!1"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.bonusNumberInput("1#"))
@@ -61,42 +78,75 @@ public class InputTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.bonusNumberInput("+5"))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 문자열이 비어있는 경우
+    }
+    @DisplayName("보너스 번호 입력시 비어있는 문자열의 입력")
+    @Test
+    void emptyBonusNumberInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.bonusNumberInput(""))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 보너스 번호가 1 ~ 45 사이가 아닐 경우
-        assertThatThrownBy(() -> input.bonusNumberInput("1234"))
+    }
+    @DisplayName("보너스 번호 입력시 1 ~ 45 사이의 숫자가 아닌 문자열의 입력")
+    @Test
+    void notIn1To45BonusNumberInput() {
+        Input input = new Input();
+
+        assertThatThrownBy(() -> input.bonusNumberInput("46"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.bonusNumberInput("0"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> input.bonusNumberInput("100"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("당첨 번호 입력시 비정상적인 문자열을 받는 경우")
+    @DisplayName("당첨 번호 입력시 정상적인 문자열의 입력")
     @Test
-    void illigalWinningNumberInput() {
+    void normalWinningNumberInput() {
         Input input = new Input();
 
-        // 정상적인 경우 테스트
         assertThat(input.winningNumbersInput("1,2,3,4,5,6"))
-                .isEqualTo(new ArrayList<>(Arrays.asList(1,2,3,4,5,6)));
+                .isEqualTo(new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6)));
         assertThat(input.winningNumbersInput("10,14,18,22,26,30"))
-                .isEqualTo(new ArrayList<>(Arrays.asList(10,14,18,22,26,30)));
+                .isEqualTo(new ArrayList<>(Arrays.asList(10, 14, 18, 22, 26, 30)));
+    }
+    @DisplayName("당첨 번호 입력시 숫지 이외의 문자를 포함한 문자열의 입력")
+    @Test
+    void nonNumericWinningNumberInput() {
+        Input input = new Input();
 
-        // 비정상적인 경우 테스트
-        // 문자열에 숫자와 쉼표를 제외한 다른 문자가 있을 경우
         assertThatThrownBy(() -> input.winningNumbersInput("!,2,3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.winningNumbersInput("@,2,3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 쉼표 구분이 적절하게 이뤄지지 않은 경우
+    }
+    @DisplayName("당첨 번호 입력시 쉼표의 구분이 적절하지 않은 문자열의 입력")
+    @Test
+    void illigalCommaWinningNumberInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.winningNumbersInput("1,2,3,4,5,6,"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.winningNumbersInput(",1,2,3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.winningNumbersInput("1,,2,3,4,5,6"))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 문자열이 비어있는 경우
+    }
+    @DisplayName("당첨 번호 입력시 비어있는 문자열의 입력")
+    @Test
+    void emptyWinningNumberInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.winningNumbersInput(""))
                 .isInstanceOf(IllegalArgumentException.class);
-        // 당첨 번호가 1 ~ 45 사이가 아닐 경우
+    }
+    @DisplayName("당첨 번호 입력시 1 ~ 45 사이의 숫자가 아닌 문자열의 입력")
+    @Test
+    void illigalWinningNumberInput() {
+        Input input = new Input();
+
         assertThatThrownBy(() -> input.winningNumbersInput("0,1,2,3,4,5"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> input.winningNumbersInput("41,42,43,44,45,46"))

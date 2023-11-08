9252c0f8622609ff7c6edfa84cbfda0e2799c8f4
Woo-Shinyoung <tlsdud8205@naver.com>
2023-11-08 20:53:24 +0900

    Feat[domain] 사용자 필드 생성

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -1,4 +1,14 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private final List<Lotto> lottos;
+    private final int purchaseAmount;
+
+    public Player(int purchaseAmount) {
+        this.lottos = new ArrayList<>();
+        this.purchaseAmount = purchaseAmount;
+    }
 }

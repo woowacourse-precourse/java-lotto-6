077579ce8439788e29966a82ab9310f510804ad0
LooanCheong <jjyejun@naver.com>
2023-11-08 16:50:08 +0900

    <feat>로또 클래스 기능 추가
    1. 입력된 로또 범위 확인
    2. 정렬 기능

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,12 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.exception.LottoRangeException;
 
 public class Lotto {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +15,29 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        sizeValidate(numbers);
+        rangeValidate(numbers);
+    }
+
+    private void sizeValidate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void rangeValidate(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new LottoRangeException();
+        }
+    }
+
+    private void rangeValidate(List<Integer> numbers) {
+        numbers.forEach(this::rangeValidate);
+    }
+
+    public List<Integer> sortLotto(List<Integer> numbers) {
+        return numbers.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 }

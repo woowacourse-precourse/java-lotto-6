38c5fc0c7ae1a411828d8311a5fdd89b5f50b3c8
최진원 <etudent39@gmail.com>
2023-11-08 22:24:48 +0900

    feat : 당첨 결과 집계하는 클래스 구현

--- src/main/java/lotto/WinResult.java
+++ src/main/java/lotto/WinResult.java
@@ -1,2 +1,26 @@
-package lotto;public class winResult {
+package lotto;
+
+import java.util.HashMap;
+import java.util.Map;
+public class WinResult {
+
+    private final static Integer WIN_RESULT_INITIALIZATION = 0;
+    private final static Integer WIN_RESULT_INCREMENT = 1;
+    private Map<Rankings, Integer> winResult;
+
+    public WinResult() {
+        winResult = new HashMap<>();
+        winResult.put(Rankings.FIRST, WIN_RESULT_INITIALIZATION);
+        winResult.put(Rankings.SECOND, WIN_RESULT_INITIALIZATION);
+        winResult.put(Rankings.THIRD, WIN_RESULT_INITIALIZATION);
+        winResult.put(Rankings.FOURTH, WIN_RESULT_INITIALIZATION);
+        winResult.put(Rankings.FIFTH, WIN_RESULT_INITIALIZATION);
+    }
+
+    public void setWinresult(Rankings rankings) {
+        if(!(rankings == Rankings.NOTHING)) {
+            this.winResult.replace(rankings, this.winResult.get(rankings) + WIN_RESULT_INCREMENT);
+        }
+    }
+
 }

ed575d8c1dcc3cbd879d0562a6f865ee90b28e98
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-08 21:09:53 +0900

    refactor: User 클래스 validate 로직 분리

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -5,11 +5,15 @@ public class User {
 	private LottoPaper lottoPaper;
 
 	public User(Integer money) {
-		if (money % 1000 != 0) {
+		this.money = money;
+		validate();
+		this.lottoPaper = new LottoPaper(money);
+	}
+
+	public void validate() {
+		if (this.money % 1000 != 0) {
 			throw new IllegalArgumentException("[ERROR] 구입금액은 1000원 단위로 입력 가능합니다!");
 		}
-		this.money = money;
-		this.lottoPaper = new LottoPaper(money);
 	}
 
 	public LottoPaper getLottoPaper() {

ea7e48e5868f1987d682cc2e6dad08460a66c763
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 13:12:41 +0900

    chore : 패키지 변경

--- src/test/java/lotto/LottoTicketTest.java
+++ src/test/java/lotto/LottoTicketTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.List;
 import lotto.model.Budget;
+import lotto.model.machine.FixedLottoMachine;
 import lotto.model.machine.LottoMachine;
 import lotto.model.lotto.LottoTicket;
 import org.junit.jupiter.api.BeforeEach;

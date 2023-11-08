bb8145187d2d8d24c8ceb5b2ed135c698cfda53d
giljin <68088970+giljin@users.noreply.github.com>
2023-11-08 12:24:23 +0900

    feat(ErrorMessage): Exception 공통 포멧 추가

--- src/main/java/lotto/ErrorMessage.java
+++ src/main/java/lotto/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto;
 
 public class ErrorMessage {
+    public static final String ERROR_FORMAT = "[ERROR] %s";
     public static final String MISMATCH_LOTTO_INPUT_FORMAT = "숫자 %d개를 입력해 주세요.";
     public static final String MISMATCH_LOTTO_NUMBER_RANGE_FORMAT = "숫자가 %d ~ %d내의 값이 아닙니다.";
     public static final String GENERATE_CHANGES_FORMAT = "%d원 단위로 구입 가능합니다.";

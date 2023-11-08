bf77d4f1831d653905778ccc8f4853a5a606686a
dy102 <132787602+dy102@users.noreply.github.com>
2023-11-08 20:51:58 +0900

    test: LottoResult 관련 테스트 추가

--- src/test/java/lotto/domain/result/LottoResultTest.java
+++ src/test/java/lotto/domain/result/LottoResultTest.java
@@ -1,42 +1,25 @@
 package lotto.domain.result;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
-import java.util.stream.Stream;
-import lotto.Option.GameStatus;
+import lotto.generator.LottoResultGenerator;
+import lotto.option.EqualStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-public class LottoResultTest {
+class LottoResultTest {
 
     @DisplayName("서로 다른 LottoResult의 GameStatus가 같으면 같은 객체로 간주한다.")
     @Test
     void checkLottoResultEqual() {
-        LottoResult lottoResult = new LottoResult(GameStatus.SIX_EQUAL);
-        LottoResult lottoResult1 = new LottoResult(GameStatus.SIX_EQUAL);
-        LottoResult lottoResult2 = new LottoResult(6, 0);
+        LottoResult lottoResult = new LottoResult(EqualStatus.SIX_EQUAL);
+        LottoResult lottoResult1 = new LottoResult(EqualStatus.SIX_EQUAL);
+        LottoResult lottoResult2 = LottoResultGenerator.setEqualStatus(6, 0);
 
-        assertThat(lottoResult).isEqualTo(lottoResult1);
-        assertThat(lottoResult).isEqualTo(lottoResult2);
-    }
-
-    @DisplayName("LottoResult의 GameStatus를 주어진 수에 맞게 설정하는지 확인한다.")
-    @ParameterizedTest
-    @MethodSource("validParameters")
-    void setGameStatusByEqual(int equalCount, int bonusCount, GameStatus gameStatus) {
-        LottoResult lottoResult = new LottoResult(equalCount, bonusCount);
-        lottoResult.setGameStatus();
-
-        assertThat(lottoResult).isEqualTo(new LottoResult(gameStatus));
-    }
-
-    static Stream<Arguments> validParameters() {
-        return Stream.of(
-                Arguments.of(2, 1, GameStatus.THREE_EQUAL),
-                Arguments.of(5, 1, GameStatus.FIVE_AND_BONUS_EQUAL)
-        );
+        assertSoftly(softAssertions -> {
+            assertThat(lottoResult).isEqualTo(lottoResult1);
+            assertThat(lottoResult).isEqualTo(lottoResult2);
+        });
     }
 }

8b903fd27addf3e410bafa7a0126a927ec1dbd0a
S-DPR <thankyouformuch@naver.com>
2023-11-08 22:01:35 +0900

    feat: Application - 메인 run

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import java.text.DecimalFormat;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoMachine tryIt = new LottoMachine(new DecimalFormat("#.##"));
+        tryIt.run();
     }
 }

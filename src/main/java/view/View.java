801a33fec09c4fb57a8e10335f62970aee9502b7
DonghaOh <donghadarwin@naver.com>
2023-11-08 14:16:58 +0900

    feat: 보너스 번호 입력 문구 출력
    bonusNumberMessagePrint 메소드

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -23,4 +23,9 @@ public class View {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    public static void bonusNumbersMessagePrint() {
+        System.out.println();
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
 }

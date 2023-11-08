6c5b5e4778b75366b42998e51b3c7cdb550a071b
DonghaOh <donghadarwin@naver.com>
2023-11-08 14:56:29 +0900

    feat: 당첨 번호 쉼표 기준으로 구분
    winningNumbersComma 메소드

--- src/main/java/validator/WinningNumbersValidator.java
+++ src/main/java/validator/WinningNumbersValidator.java
@@ -3,11 +3,18 @@ package validator;
 public class WinningNumbersValidator {
     public static void winningNumbersValidate(String winningNumbers) {
         winningNumbersNotNull(winningNumbers);
+        winningNumbersComma(winningNumbers);
     }
 
     private static void winningNumbersNotNull(String winningNumbers) {
         if (winningNumbers.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
+        }
+    }
+
+    private static void winningNumbersComma(String money) {
+        if (!money.matches("^[0-9,]*$")) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 쉼표로 구분해주세요.");
         }
     }
 }

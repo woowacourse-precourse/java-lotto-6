94a9126590f9b576c840a9a822afa5658027d81b
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-08 19:37:42 +0900

    feat : 로또 구입 장수 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,7 +9,11 @@ public class OutputView {
     public static void printPurchaseAmountInputMessage(){
         System.out.println("구입금액을 입력해 주세요.");
     }
-    
 
+    public static void printLottoCountMessage(Integer lottoCount){
+        System.out.printf("\n%d개를 구매했습니다.\n",lottoCount);
+    }
+
+    
 
 }

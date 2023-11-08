8ee4c6178eada4896752c1d4a51fab2e2d2e0b60
sunwoong <sunwoong1122@gmail.com>
2023-11-08 21:38:28 +0900

    feat: 로또 수익률 계산 기능 구현

--- src/main/java/lotto/domain/service/LottoService.java
+++ src/main/java/lotto/domain/service/LottoService.java
@@ -2,6 +2,7 @@ package lotto.domain.service;
 
 import lotto.domain.model.Bonus;
 import lotto.domain.model.Lotto;
+import lotto.domain.model.Receipt;
 import lotto.domain.model.Result;
 
 import java.util.List;
@@ -19,6 +20,37 @@ public class LottoService {
             }
         }
         return new Result(count, false);
+    }
+
+    public Receipt calculateLottoRate(int price, List<Result> results) {
+        int profit = 0;
+        Receipt receipt = new Receipt();
+        List<Integer> counts = receipt.getCounts();
+        for (Result result : results) {
+            int count = result.getCount();
+            boolean isIncludeBonus = result.getIsIncludeBonus();
+            if (count == 3) {
+                profit += 5000;
+                counts.set(0, counts.get(0) + 1);
+            } else if (count == 4) {
+                profit += 50000;
+                counts.set(1, counts.get(1) + 1);
+            } else if (count == 5) {
+                profit += 1500000;
+                counts.set(2, counts.get(2) + 1);
+            } else if (count == 6) {
+                if (isIncludeBonus) {
+                    profit += 30000000;
+                    counts.set(3, counts.get(3) + 1);
+                } else {
+                    profit += 2000000000;
+                    counts.set(4, counts.get(4) + 1);
+                }
+            }
+        }
+        double rate = Math.round(((double) profit / price) * 1000) / 10.0;
+        receipt.setRate(rate);
+        return receipt;
     }
 
     private int matchGeneratedNumbersAndNumbers(int count, List<Integer> generatedNumbers, List<Integer> numbers) {

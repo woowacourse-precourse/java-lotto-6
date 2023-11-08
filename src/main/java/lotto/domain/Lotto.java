8679a04f7dd4448707c1061ec1f1b99df3d70c1d
GumaLee <rkdxhd97@naver.com>
2023-11-08 12:44:05 +0900

    Refactor: 로또기능_validate() 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import lotto.verification.InputVerification;
+
 import java.util.List;
 
 public class Lotto {
@@ -11,9 +13,15 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        InputVerification inputVerification = new InputVerification();
+
+        if (!inputVerification.isInputSizeCorrect(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또번호 개수 에러");
         }
+        if (inputVerification.isNumbersDuplicated(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또번로 중복");
+        }
+
     }
 
     // TODO: 추가 기능 구현

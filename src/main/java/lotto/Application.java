80f81922abd3b66b79c7558a56411beb79de1e20
jihyuk <kwlgur0416@gmail.com>
2023-11-08 16:32:51 +0900

    docs : 기능 구현 목록 최신화 및 요구사항 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,7 +5,6 @@ import lotto.controller.LottoGameController;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         LottoGameController gameController = new LottoGameController();
         gameController.run();
     }

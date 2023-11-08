947ca59beab866b7da01d456784499ccde4b05c4
Junho Lee <ghzm888@gmail.com>
2023-11-08 19:54:13 +0900

    refactor : 변경된 InputView 에서 받아와 사용하도록 변경

--- src/main/java/lotto/domain/service/ClientService.java
+++ src/main/java/lotto/domain/service/ClientService.java
@@ -2,8 +2,10 @@ package lotto.domain.service;
 
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
-import lotto.global.utils.console.ConsoleUtil;
+import lotto.global.utils.constant.LottoNumberType;
 import lotto.global.utils.constant.OutputType;
+import lotto.global.utils.view.InputView;
+import lotto.global.utils.view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,35 +16,47 @@ public class ClientService {
     private static final String SPLITTER = ",";
 
     public Lotto getLottoNumbers() {
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_WINNING_NUMBERS.getComment());
-        String input = ConsoleUtil.input();
-        List<Integer> inputLottoList = Arrays.stream(input.split(SPLITTER))
-                .map(Integer::parseInt)
-                .toList();
-        return new Lotto(inputLottoList);
+        OutputView.commonOutputLine(OutputType.OUTPUT_WINNING_NUMBERS.getComment());
+        while (true) {
+            String input = InputView.input();
+            try {
+                List<Integer> inputLottoList = Arrays.stream(input.split(SPLITTER))
+                        .map(Integer::parseInt)
+                        .toList();
+                return new Lotto(inputLottoList);
+            } catch (IllegalArgumentException e) {
+                OutputView.commonOutputLine(OutputType.EXCEPTION.getComment());
+            }
+        }
     }
 
     public BonusNumber getBonusNumber(Lotto inputLottoList) {
-        ConsoleUtil.commonOutputLine(OutputType.OUTPUT_BONUS_NUMBER.getComment());
-        String input = ConsoleUtil.input();
-        return new BonusNumber(inputLottoList, Integer.parseInt(input));
+        OutputView.commonOutputLine(OutputType.OUTPUT_BONUS_NUMBER.getComment());
+        while (true) {
+            String input = InputView.input();
+            try {
+                return new BonusNumber(inputLottoList, Integer.parseInt(input));
+            } catch (IllegalArgumentException e) {
+                OutputView.commonOutputLine(OutputType.EXCEPTION.getComment());
+            }
+        }
     }
 
     public int getLottoCount() {
         while (true) {
-            String input = ConsoleUtil.input();
+            String input = InputView.input();
             try {
                 int priceOfLotto = Integer.parseInt(input);
                 validatePriceOfLotto(priceOfLotto);
-                return priceOfLotto / 1000;
+                return priceOfLotto / LottoNumberType.LOTTO_AMOUNT_UNIT.getValue();
             } catch (IllegalArgumentException e) {
-                ConsoleUtil.commonOutputLine(OutputType.EXCEPTION_INPUT_PRICE.getComment());
+                OutputView.commonOutputLine(OutputType.EXCEPTION_INPUT_PRICE.getComment());
             }
         }
     }
 
     private void validatePriceOfLotto(int priceOfLotto) {
-        if (priceOfLotto < 0 || priceOfLotto % 1000 != 0) {
+        if (priceOfLotto < 0 || priceOfLotto % LottoNumberType.LOTTO_AMOUNT_UNIT.getValue() != 0) {
             throw new IllegalArgumentException(EXCEPTION_INPUT_PRICE.getComment());
         }
     }

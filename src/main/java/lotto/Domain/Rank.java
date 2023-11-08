83bc364f3277b276cf6009018e32471f914de63d
ulsandonghun <donghoon141@naver.com>
2023-11-08 21:54:20 +0900

    [feat] : 당첨내역을 출력하는 기능

---
+++ src/main/java/lotto/Domain/Rank.java
@@ -0,0 +1,34 @@
+package lotto.Domain;
+
+public enum Rank {
+    FIRST(2000000000, 6, false, 0),
+    SECOND(30000000, 5, true, 0),
+    THIRD(1500000, 5, false, 0),
+    FORTH(50000, 4, false, 0),
+    FIFTH(5000, 3, false, 0);
+
+    private int price;
+    private int lotto;
+    private boolean bonus;
+    private int amount;
+
+    Rank(int price, int lotto, boolean bonus, int amount) {
+        this.price = price;
+        this.lotto = lotto;
+        this.bonus = bonus;
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int checkPrice(int amountOfLotto, boolean isBonus) {
+        int canGetPrice = 0;
+        if (lotto == amountOfLotto && bonus == isBonus) {
+            amount++;
+            canGetPrice = price;
+        }
+        return canGetPrice;
+    }
+}

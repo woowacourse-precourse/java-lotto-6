2d5960c040144667cb4908f3a1ae874bbace0e30
eora21 <eora21@naver.com>
2023-11-08 11:48:20 +0900

    test: 💍 수정 로또 티켓 생성 객체 사용, 파라미터 티켓사이즈 이용하도록 변경

--- src/test/java/lotto/domain/LottoTicketsGeneratingMachineTest.java
+++ src/test/java/lotto/domain/LottoTicketsGeneratingMachineTest.java
@@ -5,28 +5,29 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import domain.LottoTicketsGeneratingMachine;
+import domain.TicketSize;
 import dto.LottoTickets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LottoTicketsGeneratingMachineTest {
+    private final LottoTicketsGeneratingMachine lottoTicketsGeneratingMachine = new LottoTicketsGeneratingMachine();
+
     @ParameterizedTest
     @DisplayName("로또 티켓 개수가 양수가 아닐 경우 예외가 발생한다.")
     @ValueSource(ints = {-1, 0})
     void createLottoTicketsNotPositiveInitSize(int initSize) {
-        LottoTicketsGeneratingMachine lottoTicketsGeneratingMachine = new LottoTicketsGeneratingMachine();
         assertThrows(IllegalArgumentException.class,
-                () -> lottoTicketsGeneratingMachine.generateRandomLottoTickets(initSize));
+                () -> lottoTicketsGeneratingMachine.generateRandomLottoTickets(new TicketSize(initSize)));
     }
 
     @ParameterizedTest
     @DisplayName("로또 티켓 정상 생성")
     @ValueSource(ints = {1, 10, 100})
     void createLottoTickets(int initSize) {
-        LottoTicketsGeneratingMachine lottoTicketsGeneratingMachine = new LottoTicketsGeneratingMachine();
         LottoTickets lottoTickets = assertDoesNotThrow(
-                () -> lottoTicketsGeneratingMachine.generateRandomLottoTickets(initSize));
+                () ->lottoTicketsGeneratingMachine.generateRandomLottoTickets(new TicketSize(initSize)));
         int size = lottoTickets.getLottoTickets()
                 .size();
         assertThat(size).isEqualTo(initSize);

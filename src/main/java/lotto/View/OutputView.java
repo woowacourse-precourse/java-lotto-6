946a9ff911866cd96a2cc8e46daa9ad02b0b97ec
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-08 17:54:30 +0900

    feat: 데이터를 프롬프트에 출력하는 기능 추가

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -1,39 +1,40 @@
 package lotto.View;
 
-import java.util.List;
-import java.util.Map;
-import lotto.Model.Lotto;
-import lotto.Constants.MatchTypes;
-
-import static lotto.Message.OutputPrompt.WHITE_SPACE;
+import static lotto.Constants.BudgetConstants.BUDGET_UNIT;
+import static lotto.Constants.TurnOuts.TURN_OUT_RATE;
 import static lotto.Message.OutputPrompt.HOW_MANY_LOTTERY_ARE_BOUGHT;
 import static lotto.Message.OutputPrompt.LOTTO_GAME_RESULT_NOTICE;
-import static lotto.Constants.TurnOuts.TURN_OUT_RATE;
+
+import java.util.List;
+import java.util.Map;
+import lotto.Constants.MatchTypes;
+import lotto.Model.Lotto;
 
 public class OutputView {
 
-    public void printHowManyLotteryBought(Integer numOfLottery){
+    public void printHowManyLotteryBought(Integer lotteryBudget) {
+        Integer numOfLottery = lotteryBudget / BUDGET_UNIT.getPrice();
         System.out.println(String.format(HOW_MANY_LOTTERY_ARE_BOUGHT.getMessage(), numOfLottery));
     }
 
-    public void printRegisteredLottery(List<Lotto> lottoGroup){
-        for(Lotto lotto : lottoGroup){
+    public void printRegisteredLottery(List<Lotto> lottoGroup) {
+        for (Lotto lotto : lottoGroup) {
             System.out.println(lotto.getLottoNumbers().toString());
         }
     }
 
-    public void printWholeResult(Map<MatchTypes, Integer> gameResult, Integer turnOutRate){
-        System.out.println(WHITE_SPACE.getMessage());
+    public void printGameResult(Map<MatchTypes, Integer> gameResult) {
         System.out.println(LOTTO_GAME_RESULT_NOTICE.getMessage());
         printLottoResult(gameResult);
-        printTurnOutRate(turnOutRate);
     }
-    public void printLottoResult(Map<MatchTypes, Integer> gameResult){
+
+    public void printLottoResult(Map<MatchTypes, Integer> gameResult) {
         for (MatchTypes matchType : gameResult.keySet()) {
-            System.out.println(String.format(matchType.getMessage(), gameResult.get(matchType)));
+            System.out.print(String.format(matchType.getMessage(), gameResult.get(matchType)));
         }
     }
-    public void printTurnOutRate(Integer turnOutRate){
-        System.out.println(String.format(TURN_OUT_RATE.getMessage(), turnOutRate));
+
+    public void printTurnOutRate(Double turnOutRate) {
+        System.out.print(String.format(TURN_OUT_RATE.getMessage(), turnOutRate));
     }
 }

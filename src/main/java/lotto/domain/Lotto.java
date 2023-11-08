ce1bb64f7cfa914e255dff4f0337ee37e1b2d4e0
grax <grax@192.168.35.119>
2023-11-08 23:27:44 +0900

    refactor(lotto): make codes align clean

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -7,43 +7,40 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Lotto {
-    private final List<Integer> numbers;
+	private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        //validate(numbers);
-        this.numbers = numbers;
-    }
+	public Lotto(List<Integer> numbers) {
+		// validate(numbers);
+		this.numbers = numbers;
+	}
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-    
-    public List<Integer> getNumbers() {
-    	return numbers;
-    }
-
-    public List<Integer> generateLottoNumbers() {
-    	List<Integer> generateNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
-    	Collections.sort(generateNumbers); // 오름차순 정리
-    	return generateNumbers;
-    }
-    
-    public int countLottoTickets(int money) {
-    	int ticketQty = money / 1000;
-    	return ticketQty;
-    }
-    
-    public List<List<Integer>> createLottoTickets(int ticket) {
-    	List<List<Integer>> totalLottoTickets = new ArrayList<>();
-    	for (int i = 0; i < ticket; i++) {
-    		totalLottoTickets.add(generateLottoNumbers());
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != 6) {
+			throw new IllegalArgumentException();
 		}
-    	return totalLottoTickets;
-    }
-    
-    
-    
-  
+	}
+
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
+
+	public List<Integer> generateLottoNumbers() {
+		List<Integer> generateNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+		Collections.sort(generateNumbers); // 오름차순 정리
+		return generateNumbers;
+	}
+
+	public int countLottoTickets(int money) {
+		int ticketQty = money / 1000;
+		return ticketQty;
+	}
+
+	public List<List<Integer>> createLottoTickets(int ticket) {
+		List<List<Integer>> totalLottoTickets = new ArrayList<>();
+		for (int i = 0; i < ticket; i++) {
+			totalLottoTickets.add(generateLottoNumbers());
+		}
+		return totalLottoTickets;
+	}
+
 }

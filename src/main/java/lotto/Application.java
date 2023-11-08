dfc0b8822c71e2db771fc914cc89a12ffa6202f8
pmh5574 <g_a_m_e_@naver.com>
2023-11-08 15:14:36 +0900

    feat: 오류 처리 후 다시 시작

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -8,6 +8,12 @@ import lotto.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         LottoController lottoController = new LottoController(new InputView(), new OutputView(), new LottoService());
-        lottoController.run();
+        try {
+            lottoController.run();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            lottoController.run();
+        }
+
     }
 }

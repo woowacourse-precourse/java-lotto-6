69aa8a83fbcb0375d87a9513b35d255ed89ca269
honi20 <1594cyh@gmail.com>
2023-11-08 15:41:11 +0900

    feat: 당첨 번호 입력 예외 처리
    - 정수가 아닌 값이 포함된 경우
    - 6개 미만 또는 초과된 값을 입력한 경우
    - 1~45 범위가 아닌 숫자를 포함하는 경우
    - 중복된 번호가 있는 경우

--- src/main/java/lotto/User.java
+++ src/main/java/lotto/User.java
@@ -3,7 +3,9 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
 
@@ -39,8 +41,13 @@ public class User {
     }
 
     public List<Integer> inputAndGetWinningNumbers() {
-        String winningLotto = input("\n당첨 번호를 입력해 주세요.");
-        return getWinningNumbers(winningLotto);
+        try {
+            String winningLotto = input("\n당첨 번호를 입력해 주세요.");
+            return getWinningNumbers(winningLotto);
+        } catch (IllegalArgumentException e) {
+            printError(e.getMessage());
+            return inputAndGetWinningNumbers();
+        }
     }
 
     public int inputAndGetBonusNumber() {
@@ -57,8 +64,40 @@ public class User {
         System.out.println("[ERROR] " + message);
     }
 
-    private static List<Integer> getWinningNumbers(String winningNumber) {
-        return convertStringToIntegerList(winningNumber);
+    private static List<Integer> getWinningNumbers(String winningLotto) {
+        try {
+            List<Integer> winnerNumbers = convertStringToIntegerList(winningLotto);
+            validateNumbers(winnerNumbers);
+            return winnerNumbers;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("정수만 입력해야 합니다.");
+        }
+    }
+
+    private static void validateNumbers(List<Integer> winnerNumbers) {
+        if (winnerNumbers.size() != 6) {
+            throw new IllegalArgumentException("6개의 당첨 번호를 입력해야 합니다.");
+        }
+
+        int idx = 0;
+        while (idx < winnerNumbers.size()) {
+            if (winnerNumbers.get(idx) < 1)
+                break;
+
+            if (winnerNumbers.get(idx) > 45)
+                break;
+
+            idx++;
+        }
+
+        if (idx != winnerNumbers.size()) {
+            throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+
+        Set<Integer> distinctNumbers = new HashSet<>(winnerNumbers);
+        if (winnerNumbers.size() != distinctNumbers.size()) {
+            throw new IllegalArgumentException("당첨 번호에 중복이 없어야 합니다.");
+        }
     }
 
     private static int getBonusNumber(String bonusNumber) {

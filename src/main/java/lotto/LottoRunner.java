e819c64a852b8d93dda84b8476cc49e8fb9cf107
jyoooooung <jyooooounh@naver.com>
2023-11-08 21:16:37 +0900

    createLotto() : 입력 금액에 따라 로또 객체 생성

--- src/main/java/lotto/LottoRunner.java
+++ src/main/java/lotto/LottoRunner.java
@@ -1,4 +1,11 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoRunner {
+    private final static LottoService lottoService = new LottoService();
+
+    static void run() {
+        List<Lotto> lottoList = lottoService.createLotto();
+    }
 }
--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,4 +1,19 @@
 package lotto;
 
+import lotto.util.InputHandler;
+
+import java.util.List;
+
 public class LottoService {
+    private final static int lottoPrice = 1000;
+    private final static String totalPriceMessage = "구입금액을 입력해 주세요.";
+    public List<Lotto> createLotto() {
+        int totalPrice = Integer.parseInt(InputHandler.printMessageAndReceiveInput(totalPriceMessage));
+        int lottoCount = calculateLottoCount(totalPrice);
+        return LottoFactory.createLotto(lottoCount);
+    }
+
+    private int calculateLottoCount(int totalPrice) {
+        return totalPrice / lottoPrice;
+    }
 }

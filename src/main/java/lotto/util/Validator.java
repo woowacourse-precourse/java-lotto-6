235175d7dfd47117a238c8e2eeac13d11569c6b6
jjw0071 <jjw0071@naver.com>
2023-11-08 20:39:20 +0900

    style(Validator): Error 메시지에 줄바꿈 추가

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -22,19 +22,19 @@ public class Validator {
         try{
             Integer.parseInt(input);
         }catch(NumberFormatException e){
-            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 정수를 입력해야 합니다.\n");
         }
     }
 
     private void validatePositiveInteger(int lottoPurchaseCost) {
         if(lottoPurchaseCost<1){
-            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해야 합니다.\n");
         }
     }
 
     private void validateAmountInThousands(int lottoPurchaseCost) {
         if(lottoPurchaseCost%1000!=0){
-            throw new IllegalArgumentException("[ERROR] 1000의 배수인 정수를 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 1000의 배수인 정수를 입력해야 합니다.\n");
         }
     }
 
@@ -57,7 +57,7 @@ public class Validator {
 
     private void validateLottoNumberRange(int lottoNumber) {
         if(lottoNumber<1 || lottoNumber>45){
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n");
         }
     }
 
@@ -86,7 +86,7 @@ public class Validator {
         Set<String> uniqueInputs = new HashSet<>();
         for(String element:trimmedElements){
             if(!uniqueInputs.add(element)){
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.\n");
             }
         }
     }
@@ -94,7 +94,7 @@ public class Validator {
     private String[] validateSixNumbers(String input) {
         String[] inputInArray = input.split(",");
         if(inputInArray.length != 6){
-            throw new IllegalArgumentException("[ERROR] 6개의 값을 입력해야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 6개의 값을 입력해야 합니다.\n");
         }
         return inputInArray;
     }
@@ -108,7 +108,7 @@ public class Validator {
 
     public void compareWithLottoNumbers(List<Integer> winningLottoNumbers, int bonusNumber){
         if(winningLottoNumbers.contains(bonusNumber)){
-            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복되면 안됩니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호와 중복되면 안됩니다.\n");
         }
 
     }

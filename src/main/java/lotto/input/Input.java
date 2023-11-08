10548d0dd2327ebf6face8fba38843f6af2dc052
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-08 13:49:46 +0900

    refactor: 중복 제거

--- src/main/java/lotto/input/Input.java
+++ src/main/java/lotto/input/Input.java
@@ -7,7 +7,7 @@ public class Input {
 
     public static Integer getInteger() {
         try {
-            String readLine = Console.readLine();
+            String readLine = getLine();
             return Integer.parseInt(readLine);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 정수만 입력해야 합니다.");

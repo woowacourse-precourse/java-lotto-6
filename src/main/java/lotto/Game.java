0d5d2bbe394db827bd0a443b0d1e0a69bd8779a7
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-11-08 10:36:42 +0900

    feat(lotto): 로또 일치 판별 로직 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -2,27 +2,55 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import ui.Input;
+import ui.Output;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Game {
     private final Input input = new Input();
+    private final Output output = new Output();
     private int price;
     private int bonusNumber;
     private List<Integer> winningNumbers;
     private List<Lotto> lottos = new ArrayList<>();
+    private Map<MatchState, Integer> totalMatchCounts = new LinkedHashMap<>();
     private Error errorState = Error.NO_PROBLEM;
 
-
+//MatchState 활용해서 구현하기
+    private void printMatch() {
+        sortTotalMatchCounts();
+        totalMatchCounts.forEach(output::printResult);
+    }
     private void generateLotto() {
         int numberOfLotto = price/1000;
         int index = 0;
         while (lottos.size() < numberOfLotto){
             lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
             lottos.get(index).printLottoNumbers();
+            setTotalMatchCounts(lottos.get(index).getMatchCount(winningNumbers), lottos.get(index).checkBonusNumber(bonusNumber));
             index++;
         }
+    }
+
+    private void setTotalMatchCounts(int numberOfMatch, boolean isMatchBonus) {
+        MatchState matchState = MatchState.getMatchState(numberOfMatch, isMatchBonus);
+        if (totalMatchCounts.containsKey(matchState)) {
+            totalMatchCounts.put(matchState, matchState.getPrize());
+        }
+        totalMatchCounts.put(matchState, 1);
+    }
+
+    private void sortTotalMatchCounts() {
+        totalMatchCounts = totalMatchCounts.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.comparingInt(MatchState::getMatchCount)))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1,
+                        LinkedHashMap::new
+                ));
     }
 
     private void setPrice() {

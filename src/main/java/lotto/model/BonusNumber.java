38f909e5ee256239f9ee13cd805adf88ae350270
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:32:40 +0900

    refactor(BonusNumber): 보너스 번호 검증 로직을 BonusNumber 책임으로 변경

--- src/main/java/lotto/model/BonusNumber.java
+++ src/main/java/lotto/model/BonusNumber.java
@@ -1,9 +1,13 @@
 package lotto.model;
 
+import java.util.List;
+import lotto.validation.BonusNumberValidation;
+
 public class BonusNumber {
     private final int number;
 
-    public BonusNumber(int number) {
+    public BonusNumber(int number, List<Integer> winningNumber) {
+        BonusNumberValidation.validateBonusNumber(number, winningNumber);
         this.number = number;
     }
 

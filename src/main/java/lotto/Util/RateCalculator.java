a267a95d3450ec4f92a8fc2ee7402b65ab5d8708
minju <minju9711@naver.com>
2023-11-08 16:46:12 +0900

    [feat] 수익률 계산하는 클래스에 해당 기능이 들어간 함수 추가

--- src/main/java/lotto/Util/RateCalculator.java
+++ src/main/java/lotto/Util/RateCalculator.java
@@ -1,4 +1,14 @@
 package lotto.Util;
 
-public class RateCalculator { //수익률 계산 클래스
+public class RateCalculator {
+
+    public static double returnRate(int getPrize, int Cash) {
+        //수익률(%) = (총 당첨금 / 총 구입금액) * 100
+        double rate = (double) getPrize / Cash * 100;
+
+        // 소수점 둘째 자리에서 반올림
+        rate = Math.round(rate * 10) / 10.0;
+
+        return rate;
+    }
 }

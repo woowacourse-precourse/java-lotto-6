59e1586caa949d7aafcb79cd8c0eb845c29f106f
BH946 <kobh2895@gmail.com>
2023-11-08 22:05:18 +0900

    feat: 수익률 출력 함수

--- src/main/java/lotto/view/OutputLotto.java
+++ src/main/java/lotto/view/OutputLotto.java
@@ -4,7 +4,7 @@ import lotto.domain.Lotto;
 
 import java.util.List;
 
-import static lotto.util.message.Output.PURCHASE_COUNT;
+import static lotto.util.message.Output.*;
 
 public class OutputLotto {
     public static void counts(int purchaseCount) {
@@ -15,5 +15,18 @@ public class OutputLotto {
         for (Lotto lotto : lottos) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public static void winningHistory(int[] winning) {
+        System.out.println(WINNING_STATISTICS);
+        System.out.println(WINNER_FIRST+winning[0]+"개");
+        System.out.println(WINNER_SECOND+winning[1]+"개");
+        System.out.println(WINNER_THIRD+winning[2]+"개");
+        System.out.println(WINNER_FOURTH+winning[3]+"개");
+        System.out.println(WINNER_FIFTH+winning[4]+"개");
+    }
+
+    public static void incomeRate(double income) {
+        System.out.println(INCOME+income+"%입니다.");
     }
 }

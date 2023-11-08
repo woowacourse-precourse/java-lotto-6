221043a77cb51165fed0d22b17e185f5132c60e4
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:02:54 +0900

    refactor : 메소드 추출 후 List 반환 메소드 작성

--- src/main/java/lotto/util/ValidateWinningNumber.java
+++ src/main/java/lotto/util/ValidateWinningNumber.java
@@ -1,12 +1,24 @@
 package lotto.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidateWinningNumber {
 
-    private static void isSixNumber(String inputValue) {
+    private static List<Integer> winningNumberList = new ArrayList<>();
+
+    public static List<Integer> validateWinningNumber(String inputValue) {
 
         String[] winningNumberStringArray = inputValue.split(",");
+        isSixNumber(winningNumberStringArray);
 
-        if (winningNumberStringArray.length != 6) {
+        return winningNumberList;
+    }
+
+
+    private static void isSixNumber(String[] winningNumber) {
+
+        if (winningNumber.length != 6) {
             throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         }
 

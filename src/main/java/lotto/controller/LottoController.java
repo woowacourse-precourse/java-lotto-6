f94cebe99309cac175d859446dee4d30622b4655
BangMingGing <privatebmk802@naver.com>
2023-11-08 14:58:45 +0900

    feat(LottoController): Add start function and get purchase price function

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,2 +1,27 @@
-package lotto.controller;public class LottoController {
+package lotto.controller;
+
+import lotto.utils.PurchasePriceValidator;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        int purchasePrice = getPurchasePrice();
+    }
+
+    private int getPurchasePrice() {
+        String input = inputView.getPurchasePrice();
+        PurchasePriceValidator.validIsInt(input);
+        return Integer.parseInt(input);
+    }
+
 }

1599546f52d0f68a082c1f80d5e91427655c25f0
minsu20 <syt07100@gmail.com>
2023-11-08 23:19:45 +0900

    feat: 로또 리스트를 발행하는 메서드 추가

--- src/main/java/lotto/service/LottoGenerator.java
+++ src/main/java/lotto/service/LottoGenerator.java
@@ -3,11 +3,23 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.model.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static lotto.constants.LottoSetting.*;
 
 public class LottoGenerator {
+
+    public List<Lotto> issueTickets(int purchaseAmount) {
+        int numberOfTickets = purchaseAmount / PRICE_PER_TICKET.value();
+        List<Lotto> tickets = new ArrayList<>();
+
+        for (int i = 0; i < numberOfTickets; i++) {
+            tickets.add(issueLottoTicket());
+        }
+
+        return tickets;
+    }
 
     public Lotto issueLottoTicket() {
         return new Lotto(generateUniqueRandomNumbers());

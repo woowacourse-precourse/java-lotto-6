3c4c714d7e3f4699ad7050a95a5c46a1b4b339e3
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 20:22:45 +0900

    add: calculateAllRankCounts, calculateRofitRate in Calculation

--- src/main/java/lotto/Calculation.java
+++ src/main/java/lotto/Calculation.java
@@ -3,6 +3,9 @@ package lotto;
 import lotto.utils.Rank;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Calculation {
     public int calculateRankCount(List<Lotto> lottos, WinningNumber winningNumber, Rank rank) {
@@ -27,5 +30,18 @@ public class Calculation {
                 .count();
     }
 
+    // 모든 등수의 개수를 계산하는 메서드
+    public Map<Rank, Integer> calculateAllRankCounts(List<Lotto> lottos, WinningNumber winningNumber) {
+        return lottos.stream()
+                .map(lotto -> calculateRank(lotto, winningNumber))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+    }
 
+    // 수익률을 계산하는 메서드
+    public double calculateProfitRate(Map<Rank, Integer> rankCounts, int purchaseAmount) {
+        long totalPrize = rankCounts.entrySet().stream()
+                .mapToLong(entry -> (long) entry.getKey().getPrize() * entry.getValue())
+                .sum();
+        return (totalPrize / (double) purchaseAmount) * 100;
+    }
 }

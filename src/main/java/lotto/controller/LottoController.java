cd2f9eadfe36e5d85924dfdd9784ddfa40178fa5
Terry <t3rryahn.dev@gmail.com>
2023-11-08 13:09:54 +0900

    feat: 구매 및 금액 관련 기능 추가
    LottoController 클래스에 purchase메서드로 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,17 +1,36 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
+import lotto.model.LottoNumberGenerator;
 import lotto.model.WinningNumbers;
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoController {
+    private int purchaseAmount;
+    private final List<Lotto> lottoTickets = new ArrayList<>();
 
-    public void start() {
+    public void setWinningNumbers() {
 
         //당첨 번호, 보너스 번호 입력 단계
         List<Integer> numbersInput = InputView.inputWinningNumbers();
         int numberInput = InputView.inputBonusNumber(numbersInput);
-        WinningNumbers.getInstance(numbersInput, numberInput);
+        WinningNumbers winningNumbers = WinningNumbers.getInstance(numbersInput, numberInput);
+
+    }
+
+    public void purchase() {
+        purchaseAmount = InputView.inputPurchaseAmount();
+        int tickets = purchaseAmount / 1000;
+        LottoNumberGenerator lottoNumberGenerator = new LottoNumberGenerator();
+
+        for (int i = 0; i < tickets; i++) {
+            lottoTickets.add(new Lotto(lottoNumberGenerator.generate()));
+        }
+
+        OutputView.printPurchaseResult(tickets, lottoTickets);
     }
 }

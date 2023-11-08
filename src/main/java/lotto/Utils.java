66feba6ca5509486f7fae68a01cf3b3b33091ef3
i3kae <snoger1234@gmail.com>
2023-11-08 21:31:13 +0900

    style : Utils 클래스 변수명 재정의 1. inNumeric 메소드의 인자 str -> testValue 2. 제외문자를 포함하는 inNumeric 메소드 인자 str -> testValue, toExclude -> excludedWords

--- src/main/java/lotto/Utils.java
+++ src/main/java/lotto/Utils.java
@@ -8,24 +8,24 @@ public class Utils {
     public static String input(){
         return Console.readLine();
     }
-    public static boolean isNumeric(String str){
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) < '0' || '9' < str.charAt(i)){
+    public static boolean isNumeric(String testValue){
+        for (int i = 0; i < testValue.length(); i++){
+            if (testValue.charAt(i) < '0' || '9' < testValue.charAt(i)){
                 return false;
             }
         }
-        return !(str.isEmpty());
+        return !(testValue.isEmpty());
     }
-    public static boolean isNumeric(String str, List<String> toExclude){
-        for (int i = 0; i < str.length(); i++){
-            if (toExclude.contains(str.substring(i, i + 1))){
+    public static boolean isNumeric(String testValue, List<String> excludedWords){
+        for (int i = 0; i < testValue.length(); i++){
+            if (excludedWords.contains(testValue.substring(i, i + 1))){
                 continue;
             }
-            if (str.charAt(i) < '0' || '9' < str.charAt(i)){
+            if (testValue.charAt(i) < '0' || '9' < testValue.charAt(i)){
                 return false;
             }
         }
-        return !(str.isEmpty());
+        return !(testValue.isEmpty());
     }
     public static List<Integer> makeRandomNumbers(int start, int end, int cnt){
         List<Integer> makedNumbers;

de564fed63a2229efa7e934550ba72cc54ce7df0
kwondongwook <kwondongwook1@gmail.com>
2023-11-08 19:30:30 +0900

    style(ConsoleOutputWriter): use static import for OutputMessage

--- src/main/java/lotto/ui/ConsoleOutputWriter.java
+++ src/main/java/lotto/ui/ConsoleOutputWriter.java
@@ -5,6 +5,19 @@ import static lotto.domain.winning.Winning.FIRST_PLACE;
 import static lotto.domain.winning.Winning.FOURTH_PLACE;
 import static lotto.domain.winning.Winning.SECOND_PLACE;
 import static lotto.domain.winning.Winning.THIRD_PLACE;
+import static lotto.ui.OutputMessage.BONUS_NUMBER_INPUT;
+import static lotto.ui.OutputMessage.ERROR_FORMAT;
+import static lotto.ui.OutputMessage.FIFTH_PLACE_FORMAT;
+import static lotto.ui.OutputMessage.FIRST_PLACE_FORMAT;
+import static lotto.ui.OutputMessage.FOURTH_PLACE_FORMAT;
+import static lotto.ui.OutputMessage.LOTTO_FORMAT;
+import static lotto.ui.OutputMessage.PURCHASE_AMOUNT_INPUT;
+import static lotto.ui.OutputMessage.PURCHASE_QUANTITY_FORMAT;
+import static lotto.ui.OutputMessage.RATE_OF_RETURN_FORMAT;
+import static lotto.ui.OutputMessage.SECOND_PLACE_FORMAT;
+import static lotto.ui.OutputMessage.THIRD_PLACE_FORMAT;
+import static lotto.ui.OutputMessage.WINNING_NUMBERS_INPUT;
+import static lotto.ui.OutputMessage.WINNING_STATISTICS_DELIMITER;
 
 import java.util.List;
 import lotto.domain.number.Lottos;
@@ -14,48 +27,48 @@ import lotto.domain.winning.WinningStatistics;
 public class ConsoleOutputWriter implements OutputWriter {
     @Override
     public void writePurchaseAmountInput() {
-        write(OutputMessage.PURCHASE_AMOUNT_INPUT);
+        write(PURCHASE_AMOUNT_INPUT);
     }
 
     @Override
     public void writeLottos(Lottos lottos) {
-        write(OutputMessage.PURCHASE_QUANTITY_FORMAT, lottos.countLottos());
+        write(PURCHASE_QUANTITY_FORMAT, lottos.countLottos());
 
         lottos.forEach(lotto -> {
             List<Number> sortedNumbers = lotto.createSortedNumbers();
             Object[] integerArray = sortedNumbers.stream()
                     .map(Number::getValue)
                     .toArray();
-            write(OutputMessage.LOTTO_FORMAT, integerArray);
+            write(LOTTO_FORMAT, integerArray);
         });
     }
 
     @Override
     public void writeWinningNumbersInput() {
-        write(OutputMessage.WINNING_NUMBERS_INPUT);
+        write(WINNING_NUMBERS_INPUT);
     }
 
     @Override
     public void writeBonusNumberInput() {
-        write(OutputMessage.BONUS_NUMBER_INPUT);
+        write(BONUS_NUMBER_INPUT);
     }
 
     @Override
     public void writeWinningStatistics(WinningStatistics winningStatistics) {
-        write(OutputMessage.WINNING_STATISTICS_DELIMITER);
+        write(WINNING_STATISTICS_DELIMITER);
 
-        write(OutputMessage.FIFTH_PLACE_FORMAT, winningStatistics.count(FIFTH_PLACE));
-        write(OutputMessage.FOURTH_PLACE_FORMAT, winningStatistics.count(FOURTH_PLACE));
-        write(OutputMessage.THIRD_PLACE_FORMAT, winningStatistics.count(THIRD_PLACE));
-        write(OutputMessage.SECOND_PLACE_FORMAT, winningStatistics.count(SECOND_PLACE));
-        write(OutputMessage.FIRST_PLACE_FORMAT, winningStatistics.count(FIRST_PLACE));
+        write(FIFTH_PLACE_FORMAT, winningStatistics.count(FIFTH_PLACE));
+        write(FOURTH_PLACE_FORMAT, winningStatistics.count(FOURTH_PLACE));
+        write(THIRD_PLACE_FORMAT, winningStatistics.count(THIRD_PLACE));
+        write(SECOND_PLACE_FORMAT, winningStatistics.count(SECOND_PLACE));
+        write(FIRST_PLACE_FORMAT, winningStatistics.count(FIRST_PLACE));
 
-        write(OutputMessage.RATE_OF_RETURN_FORMAT, winningStatistics.calculateRateOfReturn());
+        write(RATE_OF_RETURN_FORMAT, winningStatistics.calculateRateOfReturn());
     }
 
     @Override
     public void writeError(Exception e) {
-        write(OutputMessage.ERROR_FORMAT, e.getMessage());
+        write(ERROR_FORMAT, e.getMessage());
     }
 
     private void write(Object format, Object... args) {

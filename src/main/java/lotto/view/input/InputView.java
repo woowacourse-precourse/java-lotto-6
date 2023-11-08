7647cd0789d92e5587eb7164f0f276a134ac9147
fromitive <fromitive@gmail.com>
2023-11-08 12:31:54 +0900

    feat: 입력 기능 구현

---
+++ src/main/java/lotto/view/input/InputView.java
@@ -0,0 +1,17 @@
+package lotto.view.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String getBuyingLottoNumber() {
+        return Console.readLine();
+    }
+
+    public static String getLottoWinningNumber() {
+        return Console.readLine();
+    }
+
+    public static String getBonusLottorNumber() {
+        return Console.readLine();
+    }
+}

d6775cad954b6c0b290879617b8c8649221fb44d
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-08 19:07:53 +0900

    style: 상수 이름명 범용성 높게 변경

--- src/main/java/lotto/Constants/BudgetConstants.java
+++ src/main/java/lotto/Constants/BudgetConstants.java
@@ -1,13 +1,14 @@
 package lotto.Constants;
 
 public enum BudgetConstants {
-    ZERO_BUDGET(0),
+    ZERO(0),
     MINIMUM_BUDGET(1000),
     MAXIMUM_BUDGET(1000000000),
     BUDGET_UNIT(1000);
 
     private int price;
-    BudgetConstants(int price){
+
+    BudgetConstants(int price) {
         this.price = price;
     }
 

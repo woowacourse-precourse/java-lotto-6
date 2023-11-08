2f182d8ae40e33bd19480613e220ff2c7c2bf4ef
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-11-08 13:05:03 +0900

    refactor(ui): 당첨이 되지 않더라도 모든 상태 출력하도록 변경

--- src/main/java/ui/Output.java
+++ src/main/java/ui/Output.java
@@ -3,16 +3,22 @@ package ui;
 import lotto.MatchState;
 
 import java.text.DecimalFormat;
+import java.util.Map;
 
 public class Output {
-    public void printResult(MatchState matchState, int totalNumberOfMatch) {
-        if (matchState.equals(MatchState.MATCH_COUNT_BONUS))
-        {
-            System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + totalNumberOfMatch);
-            return;
+    public void printResult(Map<MatchState, Integer> totalMatchCounts) {
+        for(MatchState matchState : MatchState.values()) {
+            if (matchState.equals(MatchState.MATCH_COUNT_BONUS)) {
+                System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + totalMatchCounts.getOrDefault(matchState, 0) + "개");
+                continue;
+            }
+            System.out.println(matchState.getMatchCount() + "개 일치 " +
+                    "(" + convertIntegerToPrize(matchState.getPrize()) + ") - " + totalMatchCounts.getOrDefault(matchState, 0) + "개");
         }
-        System.out.println(matchState.getMatchCount() + "개 일치 " +
-                "(" + convertIntegerToPrize(matchState.getPrize()) + ") - " + totalNumberOfMatch);
+    }
+
+    public void printRateOfReturn(float rateOfReturn) {
+        System.out.printf("총 수익률은 %.2f%%입니다.%n", rateOfReturn);
     }
 
     private String convertIntegerToPrize(int prize) {

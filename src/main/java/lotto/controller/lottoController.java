4eab0a937aa2e097bf38c46ab506688cbeae9ff7
ParkKyuIl <hakim@nex.market>
2023-11-08 15:38:40 +0900

    fix:controller run 함수 분

--- src/main/java/lotto/controller/lottoController.java
+++ src/main/java/lotto/controller/lottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import static lotto.view.messagePrinter.printLottoResults;
 import static lotto.view.messagePrinter.printPurchasedLottos;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -8,35 +9,45 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lotto.model.Lotto;
+import lotto.model.WinningLotto;
 import lotto.view.userInput;
 import lotto.view.messagePrinter;
 import lotto.model.Cash;
 import lotto.service.lottoService;
 
 public class lottoController {
-
     private final lottoService service;
-
 
     public lottoController(){
         this.service = new lottoService();
-
     }
 
-    final int lottoNumberLimit = 6;
-    final int lottoNumberStart = 1;
-    final int lottoNumberEnd = 45;
-
     public void run(){
-        messagePrinter.printPurchaseAmount();
+        buyLotto();
+        generateAnswerLotto();
+        showResult();
+    }
+
+    private void buyLotto(){
+        messagePrinter.printPurchaseAmountInstruction();
         Cash cash = new Cash(userInput.getInteger());
-
         messagePrinter.printPurchaseAmount(cash.getCount());
-
         lottoService.publishLottos(cash.getCount());
-
         messagePrinter.printPurchasedLottos();
+    }
 
+    private void generateAnswerLotto(){
+        messagePrinter.printWinningNumInstruction();
+        List<Integer> winningNumber = userInput.getIntegerList();
+        messagePrinter.printBonusNumInstruction();
+        int bonusNum = userInput.getInteger();
+        WinningLotto winningLotto = new WinningLotto(winningNumber,bonusNum);
+        lottoService.compareLottos(winningLotto);
+    }
+
+    private void showResult(){
+        printLottoResults(lottoService.getLottoHashMap());
+        messagePrinter.printProfitRate();
     }
 
 

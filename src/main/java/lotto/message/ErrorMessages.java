b44cc4c0dda0e06b14ebb80dede5891852ca3ad1
COBI-98 <tkdgus968@naver.com>
2023-11-08 20:07:50 +0900

    feat: 로또 볼 검증 기능 구현

--- src/main/java/lotto/message/ErrorMessages.java
+++ src/main/java/lotto/message/ErrorMessages.java
@@ -2,9 +2,13 @@ package lotto.message;
 
 public enum ErrorMessages {
 
+    INVALID_AMOUNT_EMPTY("[ERROR] 구입 금액이 비어있습니다."),
     INVALID_AMOUNT_FORMAT("[ERROR] 구입 금액에 문자가 들어갈 수 없습니다. 구입 금액은 숫자만 입력해주세요."),
     INVALID_AMOUNT_RANGE("[ERROR] 구입 금액 범위를 벗어났습니다. 1,000원 ~ 10,000,000원 내 금액을 입력해주세요."),
-    INVALID_AMOUNT_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요.");
+    INVALID_AMOUNT_UNIT("[ERROR] 구입 금액은 1,000원 단위로 입력해주세요."),
+    INVALID_LOTTO_BALL_FORMAT("[ERROR] 로또 공에는 숫자만 입력 가능합니다."),
+    INVALID_LOTTO_BALL_RANGE("[ERROR] 로또 공은 1~45 사이의 숫자만 입력해주세요."),
+    INVALID_LOTTO_BALL_EMPTY("[ERROR] 로또 공에 숫자를 입력해주세요.");
 
     private String message;
 

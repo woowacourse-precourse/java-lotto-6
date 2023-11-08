07de71a1519f8cd4ad3ea1d0ac672eea38bc93d6
pjaehyun <show6756@gmail.com>
2023-11-08 17:08:54 +0900

    feat: 추가 LottoCollectionTest 클래스 및 구매한 로또 번호 출력 기능 테스트

---
+++ src/test/java/lotto/LottoCollectionTest.java
@@ -0,0 +1,48 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoCollectionTest {
+
+    @DisplayName("구매한 로또 전체를 출력한다.")
+    @Test
+    void printLottoNumbers() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    int purchaseAmount = 5000;
+                    LottoCollection lottoCollection = new LottoCollection(purchaseAmount);
+
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    System.setOut(new PrintStream(outputStream));
+
+                    lottoCollection.printLottoNumbers();
+
+                    String captured = outputStream.toString().trim();
+
+                    assertThat(captured).contains(
+                            "5개를 구매했습니다.",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "[7, 8, 9, 10, 11, 12]",
+                            "[13, 14, 15, 16, 17, 18]",
+                            "[19, 20, 21, 22, 23, 24]",
+                            "[25, 26, 27, 28, 29, 30]"
+                    );
+
+
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(7, 8, 9, 10, 11, 12),
+                List.of(13, 14, 15, 16, 17, 18),
+                List.of(19, 20, 21, 22, 23, 24),
+                List.of(25, 26, 27, 28, 29, 30)
+        );
+    }
+}

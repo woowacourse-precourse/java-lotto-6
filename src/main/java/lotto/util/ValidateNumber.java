2d3a568f39ead44e39729c6b010fc81307e3dd68
KongJunWoo <joonoo3@naver.com>
2023-11-08 22:31:17 +0900

    feat : 중복된 수 있는 지 검증
    - Set을 사용해 중복을 제거하고 List 크기와 비교한다
    - 다를 시 중복이 있다는 것이므로 IllegalArgumentException 발생

--- src/main/java/lotto/util/ValidateNumber.java
+++ src/main/java/lotto/util/ValidateNumber.java
@@ -1,5 +1,8 @@
 package lotto.util;
 
+import java.util.List;
+import java.util.Set;
+
 public class ValidateNumber {
 
     public static int isPositiveInteger(String inputValue) {
@@ -24,6 +27,16 @@ public class ValidateNumber {
 
         if(inputValue <= 0){
             throw new IllegalArgumentException("[ERROR] 0보다 큰 정수를 입력해주세요.");
+        }
+
+    }
+
+    public static void isDuplicate(List<Integer> NumberList) {
+
+        Set<Integer> set = Set.copyOf(NumberList);
+
+        if(set.size() != NumberList.size()){
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
         }
 
     }

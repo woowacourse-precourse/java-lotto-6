ed567c9de9a586af02e713380236d6b445565256
yeahdy <yejine98@naver.com>
2023-11-08 17:14:31 +0900

    test: 총 로또 당첨 금액 계산 메소드 테스트 코드 작성

--- src/test/java/domain/LottoResultTest.java
+++ src/test/java/domain/LottoResultTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,11 +20,26 @@ public class LottoResultTest {
         lottoResult = new LottoResult();
     }
 
+    @Test
+    @DisplayName("3등 1장 + 4등 2장 당첨 = 총 당첨금액 105,000원")
+    void createTotalProfitTest(){
+        //given
+        lottoResult.setLottoResultCount(3,false);
+        lottoResult.setLottoResultCount(4,false);
+        lottoResult.setLottoResultCount(4,false);
+
+        //when
+        BigDecimal totalProfit = lottoResult.createTotalProfit(lottoResult);
+
+        //then
+        assertThat(totalProfit.toString()).isEqualTo("105000");
+    }
+
 
     @DisplayName("배열에 담긴 값은 로또 당첨 금액과 같다")
     @ParameterizedTest
     @ValueSource(strings = {"5000","50000","1500000","30000000","2000000000"})
-    void purchasedLottoCountTest(String price) {
+    void getBigDecimalLottoPricesTest(String price) {
         // given
         BigDecimal[] lottoPrices = Arrays.stream(LottoWinningPrice.values())
                 .map(winningPrice -> new BigDecimal(winningPrice.getPrice()))

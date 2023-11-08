c1fd1ccd055335bacc1bded00fc5733c60caba82
nhn1030 <shgusska12@gmail.com>
2023-11-08 16:47:24 +0900

    feat: 당첨번호와 보너스번호를 하나의 리스트로 반환

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -3,6 +3,19 @@ package lotto.domain;
 import java.util.List;
 import java.util.ArrayList;
 public class WinningNumbers {
+    private final List<Integer> winningNumbers;
+    private final int bonusNumber;
 
+    public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public List<Integer> getWinningNumbersWithBonus() {
+        List<Integer> winningNumbersWithBonus = new ArrayList<>(winningNumbers);
+        winningNumbersWithBonus.add(bonusNumber);
+
+        return winningNumbersWithBonus;
+    }
 }
 

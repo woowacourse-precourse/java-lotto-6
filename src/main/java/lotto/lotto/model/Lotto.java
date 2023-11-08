832c8b97de593687f7bcee51ecc31f21d32ea758
황준혁 <hyug0819@gmail.com>
2023-11-08 19:57:33 +0900

    feat : Lotto 중복 예외사항

--- src/main/java/lotto/lotto/model/Lotto.java
+++ src/main/java/lotto/lotto/model/Lotto.java
@@ -1,7 +1,12 @@
 package lotto.lotto.model;
 
+import static lotto.lotto.model.LottoConstant.*;
+
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import lotto.util.error.CustomError;
 
@@ -10,6 +15,7 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
         Collections.sort(numbers);
         this.numbers = numbers;
     }
@@ -21,6 +27,15 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    private void validateDuplicate(List<Integer> numbers){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int number : numbers){
+            map.put(number, 1);
+        }
+        Set<Integer> keySet = map.keySet();
+        if(keySet.size() != MAX_INPUT_NUMBER)
+            throw new IllegalArgumentException(CustomError.NUMBER_DUPLICATE.getError());
+    }
     public String toString(){
         return numbers.toString();
     }

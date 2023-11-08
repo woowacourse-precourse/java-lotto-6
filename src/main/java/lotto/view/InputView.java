e325bd74238d75a80f01f90d2b6a0d81c75b255a
shining013 <nevertheless113@naver.com>
2023-11-08 20:53:03 +0900

    modify: 한 줄 띄어쓰는 출력문 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,12 +8,13 @@ public class InputView {
                 return Console.readLine();
         }
 
-        public static String winningNumerInput() {
+        public static String winningNumberInput() {
                 System.out.println("당첨 번호를 입력해 주세요.");
                 return Console.readLine();
         }
 
         public static String bonusNumberInput() {
+                System.out.println();
                 System.out.println("보너스 번호를 입력해 주세요.");
                 return Console.readLine();
         }

b7cc601359bbf5067f84b159fe65ff5417182597
KimSehoon <shggm2000@gmail.com>
2023-11-08 15:26:21 +0900

    refactor: getValidPurchaseAmount_test method 수정

--- src/test/java/lotto/InputPurchaseAmountTest.java
+++ src/test/java/lotto/InputPurchaseAmountTest.java
@@ -6,6 +6,7 @@ import static lotto.message.ErrorMessage.NUMBER_FORMAT_MONEY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.io.ByteArrayInputStream;
 import lotto.view.Input;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,10 +17,10 @@ public class InputPurchaseAmountTest extends NsTest {
     @Test
     @DisplayName("유효한 구입금액 입력 테스트")
     void getValidPurchaseAmount_test() {
-        assertSimpleTest(() -> {
-            run("8000");
-            assertThat(input.getValidPurchaseAmount()).isEqualTo(8000);
-        });
+        command("8000");
+
+        assertThat(input.getValidPurchaseAmount()).isEqualTo(8000);
+
     }
 
     @Test
@@ -33,11 +34,16 @@ public class InputPurchaseAmountTest extends NsTest {
 
     @Test
     @DisplayName("1000원 단위가 아닌 구입금액 입력 시 예외 테스트")
-    void validatePurchaseAmountDivisibleBy1000_test(){
+    void validatePurchaseAmountDivisibleBy1000_test() {
         assertSimpleTest(() -> {
             runException("2500");
             assertThat(output()).contains(DIVISIBLE_BY_1000.errorMessage());
         });
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 
     @Override

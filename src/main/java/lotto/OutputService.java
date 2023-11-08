eebc370adbed87d5be0f15eb492d087667224291
hwangkyojune <shane5969@naver.com>
2023-11-08 17:39:08 +0900

    모든 출력을 담당하도록 리팩토링

--- src/main/java/lotto/OutputService.java
+++ src/main/java/lotto/OutputService.java
@@ -5,16 +5,32 @@ import java.util.List;
 import java.util.Map;
 
 public class OutputService {
+    static private final String requestMoney ="구입금액을 입력해 주세요.";
+    static private final String requestWinningNums ="당첨 번호를 입력해 주세요.";
+    static private final String requestBonusNum ="보너스 번호를 입력해 주세요.";
     private static final String informIssuedLottoCount = "%d개를 구매했습니다.";
     private static final String informRankCount =  " - %d개";
+    private static final String informReturnOfRate = "총 수익률은 %.1f%%입니다.";
+    private static final String informWinRecord ="당첨 통계\n---";
+    public static void requestMoney(){
+        System.out.println(requestMoney);
+    }
+    public static void requestWinningNums(){
+        System.out.println(requestWinningNums);
+    }
+    public static void requestBonusNum(){
+        System.out.println(requestBonusNum);
+    }
     public static void printLottoCount(int lottoCount){
         System.out.println(String.format(informIssuedLottoCount,lottoCount));
+        System.out.println();
     }
 
     public static void printIssuedLotto(List<Lotto> lottoes){
         lottoes.stream().forEach((lotto) ->{
             System.out.println(Arrays.toString(lotto.getNumbers().toArray()));
         });
+        System.out.println();
     }
 
     public static void printRankStatus(Rank rank){
@@ -26,13 +42,15 @@ public class OutputService {
     }
 
     public static void printAllWinRecord(Map<Rank,Integer> rankingResult){
+        System.out.println(informWinRecord);
         rankingResult.keySet().stream().forEach((rank)->{
             printRankStatus(rank);
             printRankCount(rankingResult.get(rank));
                 });
+        System.out.println();
     }
 
     public static void printReturnOfRate(double profitRate){
-        System.out.println(String.format("총 수익률은 %.1f%%입니다.",profitRate));
+        System.out.println(String.format(informReturnOfRate,profitRate));
     }
 }

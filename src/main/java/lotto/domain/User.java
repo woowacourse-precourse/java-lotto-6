d6a7324d5c8323c06d9368a363f362e2d0efc726
taekgyun <show9510@naver.com>
2023-11-08 22:39:39 +0900

    User 로또 구매

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,4 +1,57 @@
 package lotto.domain;
 
+import static lotto.constant.Constant.LOTTO_PRICE;
+import static lotto.constant.Constant.MAX_NUMBER;
+import static lotto.constant.Constant.MIN_NUMBER;
+import static lotto.constant.Constant.NUMBERS_SIZE;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.util.MoneyValidate;
+
 public class User {
+    private final LottoWallet lottoWallet;
+    private final UserPrize userPrize;
+
+    public User(LottoWallet lottoWallet, UserPrize userPrize) {
+        this.lottoWallet = lottoWallet;
+        this.userPrize = userPrize;
+    }
+
+    public void purchaseLotto(int money) {
+        MoneyValidate.validateMoney(money);
+        userPrize.setCost(money);
+        lottoWallet.clearWallet();
+        addLottos(money / LOTTO_PRICE);
+    }
+
+    private void addLottos(int amount) {
+        for (int i = 0; i < amount; i++) {
+            addLotto();
+        }
+    }
+
+    private void addLotto() {
+        lottoWallet.addLotto(pickNumbers());
+    }
+
+    private List<Integer> pickNumbers() {
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBERS_SIZE);
+    }
+
+    public void winningTheLotto(int rank) {
+        userPrize.plusPrizeCount(rank);
+    }
+
+    public double getRateOfReturn() {
+        return userPrize.getRateOfReturn();
+    }
+
+    public List<Lotto> getMyLotto() {
+        return lottoWallet.getLotto();
+    }
+
+    public List<Integer> getPrizeCounts() {
+        return userPrize.getPrizeCounts();
+    }
 }

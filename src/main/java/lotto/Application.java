5be6b3c8b45e3a6e24b6b4948e1e92e498f128bf
yohanii <dygks924@gmail.com>
2023-11-08 19:22:19 +0900

    feat: Application 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+
+        int price = Input.inputPrice();
+        List<Lotto> lottos = lottoGame.buyLottos(lottoGame.getLottoCounts(price));
+
+        List<Integer> winNums = Input.inputWinNums();
+        int bonusNum = Input.inputBonusNum(winNums);
+
+        List<Rank> ranks = lottos.stream().map(lotto -> lotto.getRank(winNums, bonusNum)).toList();
+
+        lottoGame.printResult(ranks);
+        lottoGame.printEarningRate(ranks, price);
     }
 }

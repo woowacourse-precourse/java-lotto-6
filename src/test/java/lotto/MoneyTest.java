c2f6f015adf5b1424d2988e9139dff59c32d8685
jihun-24k <gi020477@gmail.com>
2023-11-08 09:28:44 +0900

    test: 구매 금액 0원 테스트

--- src/test/java/lotto/MoneyTest.java
+++ src/test/java/lotto/MoneyTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -16,6 +17,12 @@ class MoneyTest {
     @CsvSource({"1100", "10", "13040", "11111"})
     @ParameterizedTest
     public void 구매_금액은_올바른_단위만_가능(String amount) {
+        assertThatThrownBy(() -> new Money(amount)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 구매_금액은_0원_이상만_가능() {
+        String amount = "0";
         assertThatThrownBy(() -> new Money(amount)).isInstanceOf(IllegalArgumentException.class);
     }
 }

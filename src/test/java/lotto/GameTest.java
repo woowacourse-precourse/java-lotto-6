ae647bf58d2ac3f124be95d0daa4a5d38a34f5de
ebang <dskhe8@gmail.com>
2023-11-08 22:10:11 +0900

    test(lotto): Game이 로또 금액을 입력받는 테스트 구현

--- src/test/java/lotto/GameTest.java
+++ src/test/java/lotto/GameTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.Validator.LottoValidator;
@@ -12,20 +13,53 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class GameTest {
     @DisplayName("게임서비스는 유저로부터 당첨번호를 입력받아 저장한다.")
     @Test
-    void askWinningNumbers(){
+    void askWinningNumbers() {
         Game game = new Game();
-        game.setWinningLotto(List.of(1,2,3,4,5,6));
+        game.setWinningLotto(List.of(1, 2, 3, 4, 5, 6));
 
-        Lotto lotto = new Lotto(List.of(1,2,3,4,5,6));
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
         assertThat(lotto.getHowManySameNumbers(game.getWinningLotto())).isEqualTo(LottoValidator.getLottoSize());
     }
+
     @DisplayName("게임서비스는 유저로부터 보너스 번호를 입력받아 저장한다.")
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3,4,5})
+    @ValueSource(ints = {1, 2, 3, 4, 5})
     void setWinningNumbers(int n) {
         Game game = new Game();
         game.setBonusNumber(n);
         assertThat(game.getBonusNumber()).isEqualTo(n);
     }
+
+    @DisplayName("게임서비스는 유저로부터 금액을 입력받아 저장한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {14000, 1000})
+    void setMoney(int money) {
+        Game game = new Game();
+        game.setMoney(money);
+        assertThat(game.getMoney()).isEqualTo(money);
+    }
+
+    @DisplayName("게임서비스는 유저로부터 받는 금액이 1000원 이상 21 * 10^8원 이하이다 ")
+    @ParameterizedTest
+    @ValueSource(ints = {0, Integer.MIN_VALUE, -1000})
+    void setMoneyWithExceptionMinMax(int money) {
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.setMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @DisplayName("게임서비스는 유저로부터 받는 금액이 1000원 단위이다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1001, 1003, 1500})
+    void setMoneyWithExceptionThousand(int money) {
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.setMoney(money))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
 
 }

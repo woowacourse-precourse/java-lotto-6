c0ec894bcac4a148d5334f8986e239bd859ce168
GyungA <GyungA@users.noreply.github.com>
2023-11-08 22:18:53 +0900

    chore: 로또 당첨 번호와 보너스 번호의 상수 표시를 해제

--- src/main/java/lotto/View/WinningLottoGenerator.java
+++ src/main/java/lotto/View/WinningLottoGenerator.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WinningLottoGenerator {
-    public static List<Integer> WINNING_NUMBERS;
-    public static Integer BONUS_NUMBER;
+    public static List<Integer> winningNumbers;
+    public static Integer bonusNumber;
     private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. (쉼표로 구분)";
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String NUMBER_FORMAT_ERROR_MESSAGE = "%s 입력값 %s는 올바른 숫자 형식이 아닙니다. ";
@@ -28,13 +28,13 @@ public class WinningLottoGenerator {
                 System.out.println(String.format(NUMBER_FORMAT_ERROR_MESSAGE, Lotto.ERROR_NOTICE, numberString));
             }
         }
-        WINNING_NUMBERS = winningNumbers;
+        WinningLottoGenerator.winningNumbers = winningNumbers;
         return winningNumbers;
     }
 
     public static Integer generateBonusNumber() {
         System.out.println(BONUS_NUMBER_MESSAGE);
-        BONUS_NUMBER = Integer.parseInt(Console.readLine());
-        return BONUS_NUMBER;
+        bonusNumber = Integer.parseInt(Console.readLine());
+        return bonusNumber;
     }
 }

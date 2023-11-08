7fc7460c5a4244eaf3e2ebeaf89a12d4b68c368d
yang <hjyang@epience.com>
2023-11-08 17:19:15 +0900

    🚀refector(domain / Result) : 수익률 계산 로직 수정

--- src/main/java/lotto/domain/Rsult.java
+++ src/main/java/lotto/domain/Rsult.java
@@ -6,8 +6,9 @@ public class Rsult {
 //    구입 금액 / 당첨금액 * 100
 //   소수 둘째 자리에서 반올림
     // 수익률
-    public static String yield(int purchase, List<Rank> rank) {
-        return String.format("%.2f", purchase / getYield(rank) * 100);
+    public static double yield(int purchase, List<Rank> rank) {
+        double result = (double) getYield(rank) / purchase * 100;
+        return Math.round(result * 100.0) / 100.0;
     }
     public static double getYield(List<Rank> rank) {
         return rank.stream()

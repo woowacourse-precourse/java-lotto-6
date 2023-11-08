d27d9eb9574409f1d3ab51537b3724aec7448497
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 11:11:38 +0900

    feat(LottoController) : 로또 컨트롤러 구현 - lotto() - setPurchaseAmount() - setWinnerNum() - setBonusNum() - setLottoResult()

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,60 @@
+package lotto.controller;
+
+import lotto.domain.*;
+import lotto.util.CalculateRate;
+import lotto.util.MakeLotto;
+import lotto.util.MakeLottoCount;
+import lotto.view.OutputView;
+import lotto.view.OutputViewLotto;
+import lotto.view.OutputViewLottoCount;
+import lotto.view.OutputViewResult;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+    InputController inputController = new InputController();
+    MakeLotto makeLotto = new MakeLotto();
+
+    public void Lotto(){
+        LottoPurchaseAmount lottoPurchaseAmount = setPurchaseAmount();
+
+        int lottoCount = MakeLottoCount.getLottoCount(lottoPurchaseAmount.getCount());
+        List<Lotto> lottos = makeLotto.makeLottoList(lottoCount);
+
+        OutputViewLottoCount.printLottoCount(lottoCount);
+        OutputViewLotto.printLotto(lottos);
+
+        List<Integer> winnerNum = setWinnerNum();
+
+        int bonusNumber = setBonusNum();
+
+        LottoMoneyPrize lottoMoneyPrize = new LottoMoneyPrize(lottos,winnerNum,bonusNumber);
+        double rate = CalculateRate.getRate(lottoPurchaseAmount.getCount(),lottoMoneyPrize.getTotalReward());
+
+        setLottoResult(lottoMoneyPrize,rate);
+
+
+    }
+
+    private LottoPurchaseAmount setPurchaseAmount() {
+        return inputController.getPurchaseAmount();
+    }
+    private List<Integer> setWinnerNum() {
+        return inputController.getWinnerNumber();
+    }
+    private int setBonusNum() {
+        return inputController.getBonusNumber();
+    }
+
+    private void setLottoResult(LottoMoneyPrize lottoMoneyPrize, double rate) {
+        OutputView.printEnter();
+        OutputViewResult.printResultStart();
+        OutputViewResult.printResultLotto(lottoMoneyPrize.getLottoMoneyPrize());
+        OutputViewResult.printResultRate(rate);
+    }
+
+
+
+
+}

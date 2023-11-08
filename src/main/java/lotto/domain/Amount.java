b7e8de3757643dfd22d4d1b494dcaab9d38d1980
rjsdn9025 <rjsdn9025@naver.com>
2023-11-08 12:11:41 +0900

    ref: Amount클래스 수정

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,31 +1,16 @@
 package lotto.domain;
 
 public class Amount {
-    private final int originalAmount;
-    private int amount;
+    private final int amount;
 
-    public Amount(int amount) {
-        this.amount = amount;
-        this.originalAmount = amount;
+    public Amount(String amount) {
+        this.amount = Integer.parseInt(amount);
     }
-
-    public int getAmountDividedBy(int amount) {
-        return this.amount / amount;
-    }
-
-    public void decrease(int amount) {
-        this.amount -= amount;
-    }
-
-    public boolean isPurchasableBy(int amount) {
-        return amount <= this.amount;
-    }
-
-    public int getOriginalAmount() {
-        return originalAmount;
-    }
-
     public int calculateLottoPurchaseSize() {
         return amount / 1000;
+    }
+
+    public double calculateProfitRate(Long reward) {
+        return (reward / (float) amount) * 100;
     }
 }

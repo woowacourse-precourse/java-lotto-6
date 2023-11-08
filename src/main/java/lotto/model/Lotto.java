47097298f81ca38c0288cd3afc791dfa48ffcfea
chanwoo <mdsoo55828@gmail.com>
2023-11-08 13:10:50 +0900

    feat: Lotto 정렬 기능 추가

---
+++ src/main/java/lotto/model/Lotto.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        sortNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private void sortNumbers(List<Integer> numbers){
+        Collections.sort(numbers);
+    }
+
+}

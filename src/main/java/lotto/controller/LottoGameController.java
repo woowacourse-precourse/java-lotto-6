9534d7f4d3528105460d91fe74ef1f00efff87f8
inpink <inpink@kakao.com>
2023-11-08 21:45:32 +0900

    refactor: Controller에서 게임 흐름을  3가지로 분리. 1) 구매, 2) 당첨 번호 생성, 3) 당첨 결과 계산

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -2,15 +2,17 @@ package lotto.controller;
 
 import static lotto.configuration.IntegerConstants.COST_OF_LOTTO;
 
-import lotto.domain.Lotto;
-import lotto.domain.LottoGame;
-import lotto.domain.LottoNumber;
-import lotto.domain.Lottos;
-import lotto.domain.ThousandUnitMoney;
-import lotto.domain.WinningResult;
-import lotto.domain.dto.LottoGameMapper;
-import lotto.domain.dto.LottoGamePurchaseDto;
-import lotto.domain.dto.LottoGameWinningResultDto;
+import lotto.domain.entity.Lotto;
+import lotto.domain.entity.LottoGame;
+import lotto.domain.entity.LottoNumber;
+import lotto.domain.entity.Lottos;
+import lotto.domain.entity.ThousandUnitMoney;
+import lotto.domain.entity.WinningResult;
+import lotto.domain.dto.DtoMapper;
+import lotto.domain.dto.PurchaseDto;
+import lotto.domain.dto.WinningResultDto;
+import lotto.domain.entity.Purchase;
+import lotto.domain.entity.WinningTicket;
 import lotto.service.LottoGameService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -30,54 +32,41 @@ public class LottoGameController {
     }
 
     public void playGame() {
-        //구입 금액입력받기
+        Purchase purchase = purchaseLotto();
+
+        outputView.outputPurchaseLottoList(DtoMapper.toPurchaseDtofrom(purchase.getLottos()));
+
+        WinningTicket winningTicket = generateWinningTicket();
+
+        WinningResult winningResult = lottoGameService.calcWinningResult(purchase, winningTicket);
+
+        outputView.outputWinningStatistics(
+                DtoMapper.toWinningResultDtofrom(
+                        winningResult.getRankings(), winningResult.getRateOfReturn()));
+
+        LottoGame lottoGame = LottoGame.create(purchase, winningTicket, winningResult);
+        Long lottoGameId = lottoGameService.saveLottoGame(lottoGame);
+
+        replayGame(false);
+    }
+
+    private Purchase purchaseLotto() {
         String purchaseAmountInput = inputView.inputPurchaseAmount();
 
-        //purchaseAmount를 Money로 변환
         ThousandUnitMoney purchasePrice = ThousandUnitMoney.create(purchaseAmountInput);
-        //Money만큼 로또담아 Lottos 생성
         Lottos automaticLottos = Lottos.createAutomatic(purchasePrice, COST_OF_LOTTO.getValue());
 
-        //Lottos출력
-        outputView.outputPurchaseLottoList(automaticLottos);
+        return Purchase.create(purchasePrice, automaticLottos);
+    }
 
-        //당첨 번호 입력받기
-        //보너스 번호 입력받기
+    private WinningTicket generateWinningTicket() {
         String winningNumbersInput = inputView.inputWinningLotto();
         String bonusNumberInput = inputView.inputBonusNumber();
 
-        //Lotto로 변환
-        //로또 넘버로 변환
         Lotto winningNumbers = Lotto.create(winningNumbersInput);
         LottoNumber bonusNumber = LottoNumber.create(bonusNumberInput);
 
-        //당첨 개수, 총수익률 계산
-        WinningResult winningResult = lottoGameService.calcWinningResult(
-                winningNumbers,
-                bonusNumber,
-                automaticLottos,
-                purchasePrice);
-
-        //당첨 통계 출력
-        outputView.outputWinningStatistics(winningResult);
-
-        //데이터를 lottoGame에 담아 보관
-        LottoGame lottoGame = LottoGame.create(
-                purchasePrice,
-                automaticLottos,
-                winningNumbers,
-                bonusNumber,
-                winningResult);
-
-        //service를 통해 게임 저장
-        Long lottoGameId = lottoGameService.saveLottoGame(lottoGame);
-
-        //구매 정보 데이터만을 담아 View로 전달하기 위해 DTO 사용
-        LottoGamePurchaseDto lottoGamePurchaseDto = LottoGameMapper.toPurchaseDtofrom(lottoGame);
-        LottoGameWinningResultDto lottoGameWinningResultDto = LottoGameMapper.toWinningResultDtofrom(lottoGame);
-
-        //재시작기능
-        replayGame(false);
+        return WinningTicket.create(winningNumbers, bonusNumber);
     }
 
     private void replayGame(boolean isContinue) {

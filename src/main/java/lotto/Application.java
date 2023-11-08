db64c6aef73e35f174e1df70bcab3dccce8b9ef5
ogi <ogi@naver.com>
2023-11-08 23:22:08 +0900

    [refacter] 입력 UI 분리

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,16 +1,8 @@
 package lotto;
 
-import lotto.controller.LottoController;
-import lotto.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        try {
-            LottoController lottoController = LottoController.lottoPlayController();
-            lottoController.buyLotto();
-            lottoController.checkLottoResult();
-        } catch (RuntimeException e) {
-            OutputView.showErrorMessage(String.valueOf(e));
-        }
+        LottoController lottoController = new LottoController();
+        lottoController.run();
     }
 }

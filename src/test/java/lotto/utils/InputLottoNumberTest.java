3fcf52a270386c84ee77cf4db5d2a5e6e09d6a87
sgo722 <sgo722@naver.com>
2023-11-08 20:41:56 +0900

    test : 당첨번호 입력기능 테스트

--- src/test/java/lotto/utils/InputLottoNumberTest.java
+++ src/test/java/lotto/utils/InputLottoNumberTest.java
@@ -1,8 +1,12 @@
 package lotto.utils;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -34,6 +38,38 @@ public class InputLottoNumberTest {
         assertThatThrownBy(() -> Validator.checkWinningNumberForm(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 입력 형식에 올바르게 입력되지 않았습니다.");
+    }
+
+    @Test
+    void 당첨번호_범위_내_포함되지_않는_경우() {
+        List<Integer> invalidLottoNumbers = Arrays.asList(1, 2, 3, 4, 5, 55);
+        assertThatThrownBy(() -> Validator.checkWinningNumber(invalidLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 각 로또 번호는 1과 45 사이의 값이어야 합니다.");
+    }
+
+    @Test
+    void 당첨번호_6개_이하인_아닌경우() {
+        List<Integer> invalidLottoNumbers = Arrays.asList(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> Validator.checkWinningNumber(invalidLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호 개수가 6개가 아닙니다.");
+    }
+
+    @Test
+    void 당첨번호_6개_초과인_아닌경우() {
+        List<Integer> invalidLottoNumbers = Arrays.asList(1, 2, 3, 4, 5,6,7);
+        assertThatThrownBy(() -> Validator.checkWinningNumber(invalidLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호 개수가 6개가 아닙니다.");
+    }
+
+    @Test
+    void 당첨번호_중복된_숫자가_있는경우() {
+        List<Integer> invalidLottoNumbers = Arrays.asList(1, 2, 3, 4, 6,6);
+        assertThatThrownBy(() -> Validator.checkWinningNumber(invalidLottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 당첨 번호에 중복된 번호가 존재합니다.");
     }
 
 

8b1a1d25b016765c6cca34e34f16f1fbb9557354
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-08 13:32:09 +0900

    OutputView에 controller 연결

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,9 +1,18 @@
 package View;
 
+import Controller.ModelHandler;
 import Model.Lotto;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutputView {
+    private ModelHandler moder = ModelHandler.getInstance();
 
+    public void printMyLotto() {
+        moder.printMyLotto();
+    }
+
+    public void printStatistics() {
+        moder.printStatistics();
+    }
 }

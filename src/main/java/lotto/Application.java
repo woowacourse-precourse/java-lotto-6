0f8849b1eec56e99267cce42b597d7984cb6a991
penrose15 <seongju99@naver.com>
2023-11-08 22:35:34 +0900

    Refactor: Dependency로 DI

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,6 +7,7 @@ import lotto.domain.lotto.WinningNormalNumbers;
 import lotto.domain.money.Money;
 import lotto.domain.number.NumberGenerator;
 import lotto.domain.lotto.WinningNumbers;
+import lotto.global.config.Dependency;
 import lotto.global.view.io.LottoInputProcessor;
 import lotto.global.view.io.LottoInputView;
 import lotto.global.view.output.OutputView;
@@ -17,17 +18,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         try {
-            WinningNormalNumbers winningNormalNumbers = new WinningNormalNumbers();
-            BonusNumber bonusNumber = new BonusNumber();
-
-            WinningNumbers winningNumbers = new WinningNumbers(winningNormalNumbers, bonusNumber);
-
-            LottoInputProcessor lottoInputProcessor = new LottoInputProcessor(winningNumbers);
-
-            LottoGame lottoGame = new LottoGame();
-
-            LottoInputView lottoInputView = new LottoInputView(lottoInputProcessor);
-            GameController gameController = new GameController(lottoInputView , new NumberGenerator(), lottoGame);
+            Dependency dependency = new Dependency();
+            GameController gameController = dependency.gameController();
             gameController.play();
         } catch (Exception e) {
             OutputView.printMessageLine(EXCEPTION_PREFIX.getMessage() + e.getMessage());

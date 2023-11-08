459bc6981eecf99f2f59aa9494b7c99357e00800
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2023-11-08 19:37:11 +0900

    feat : 구입금액입력 안내메시지 출력기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,15 @@
+package lotto.view;
+
+
+import lotto.model.LottoResult;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class OutputView {
+    public static void printPurchaseAmountInputMessage(){
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+    
+
+
+}

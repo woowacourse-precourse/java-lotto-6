03b1d0b3e0e3f4db508e2aca2612e919fde88915
Concepty <khs1903b@gmail.com>
2023-11-08 17:08:34 +0900

    test(LottoWinning): 예외 처리에 대한 테스트 추가

--- src/test/java/lotto/LottoWinningTest.java
+++ src/test/java/lotto/LottoWinningTest.java
@@ -1,12 +1,12 @@
 package lotto;
 
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,4 +31,27 @@ class LottoWinningTest {
                 Arguments.of(new Lotto(List.of(5,10,15,20,25,30)), LottoRank.FIRST_PLACE)
         );
     }
+
+    @DisplayName("잘못된 로또 당첨 기준에 대해 예외 처리한다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidLottoWinningParams")
+    void checkExceptionOnWrongInput(List<Integer> winningNumbers, Integer bonusNumber) {
+        assertThatThrownBy(() -> {
+            new LottoWinning(winningNumbers).setBonusNumber(bonusNumber);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
+
+    static Stream<Arguments> provideInvalidLottoWinningParams() {
+        return Stream.of(
+                Arguments.of(List.of(1,2,3,4,5,6,7), 7),
+                Arguments.of(List.of(1,2,3,4,5,55), 7),
+                Arguments.of(List.of(1,2,3,4,5,5), 7),
+                Arguments.of(List.of(1,2,3,4,5,6), 55),
+                Arguments.of(List.of(1,2,3,4,5,6), 6),
+                Arguments.of(List.of(1,2,3,4,5,6,7), 0)
+        );
+    }
+
 }

0b36f0e72c154e86e9d86e459f73e0afae4753f6
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 21:05:01 +0900

    feat: 문자열을 정수로 바꿔주는 클래스 구현

--- src/main/java/lotto/parser/Parser.java
+++ src/main/java/lotto/parser/Parser.java
@@ -1,9 +1,20 @@
 package lotto.parser;
 
-public class Parser {
-    public int parse(String str) {
-        int strToInt = Integer.parseInt(str);
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-        return strToInt;
+public class Parser {
+
+    private static final String DELIMITER = ",";
+    public static int parseInt(String input){
+        return Integer.parseInt(input);
+    }
+
+    public static List<Integer> parseWinningNumbers(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }

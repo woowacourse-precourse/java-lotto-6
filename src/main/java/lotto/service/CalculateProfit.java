f091f69ca13488e35790e338af0b32a2c8fb4321
banxzxx <jwbang1643@gmail.com>
2023-11-08 19:46:10 +0900

    Refactor : 수익률 계산 수정

--- src/main/java/lotto/service/CalculateProfit.java
+++ src/main/java/lotto/service/CalculateProfit.java
@@ -6,15 +6,16 @@ public class CalculateProfit {
     double profit = 0;
 
     public void calcProfit(){
-        profit += 5000* Bingo.getBingo3().correctLotto;
+        profit += 5000*Bingo.getBingo3().correctLotto;
         profit += 50000*Bingo.getBingo4().correctLotto;
         profit += 1500000*Bingo.getBingo5().correctLotto;
         profit += 30000000*Bingo.getBingo5wB().correctLotto;
         profit += 2000000000*Bingo.getBingo6().correctLotto;
     }
 
-    public double calcProfitPercent(int ticket){
-        return Math.round(profit/(double)ticket*100)/1000.0;
+    public double calcProfitPercent(int money){
+        double percent = (profit/(double)money)*100;
+        return Math.round(percent*100)/100.0;
     }
 
 }

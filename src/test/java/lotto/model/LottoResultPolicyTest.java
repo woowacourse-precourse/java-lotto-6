b67aaca17796a9473a5bfa333f75e821d5d5248e
seona-moon <fhflwhwl5@gmail.com>
2023-11-08 20:30:10 +0900

    test: 로또 결과가 제대로 계산되는지 정상 테스트 구현

--- src/test/java/lotto/model/LottoResultPolicyTest.java
+++ src/test/java/lotto/model/LottoResultPolicyTest.java
@@ -1,6 +1,7 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import lotto.enums.ErrorMessage;
@@ -10,8 +11,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoResultPolicyTest {
-    //사용자에게 winning number, bonus number를 받아와서 검증, 로또 넘버와 계산 결과를 비교한 결과를 리턴한다.
     private LottoResultPolicy lottoRankPolicy = new LottoResultPolicy();
+
+    @DisplayName("로또 결과를 제대로 검정하는지 테스트")
+    @Test
+    void calculateResultTest(){
+        // given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        lottoRankPolicy.setWinningNumbers(List.of(1,2,3,4,10,11));
+        lottoRankPolicy.setBonusNumber(20);
+        // when
+        LottoResult lottoResult = lottoRankPolicy.calculateResult(lotto);
+        int winningCount = lottoResult.getWinningCount();
+        boolean bonusMatch = lottoResult.isBonusMatch();
+        // then
+        assertEquals(4, winningCount);
+        assertEquals(false, bonusMatch);
+    }
 
     @DisplayName("당첨 번호의 개수가 6개를 넘으면 예외가 발생한다.")
     @Test

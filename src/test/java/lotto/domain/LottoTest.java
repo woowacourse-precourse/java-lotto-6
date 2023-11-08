2d224f68aa8a397681461b0344354ef7d33dbd96
Jiyun <alex68566856@gmail.com>
2023-11-08 19:17:15 +0900

    refacor: import 정리, 불필요 class 삭제 - import 정리: LottoTest, BudgetTest - 삭제: StatisticTest

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,10 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 

1f43e565360d43b0e8ad45ad939fb67a47b1912b
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-08 13:33:56 +0900

    [FEAT] 1부터 45까지의 6가지 수로 구성된 로또 발행

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.List;
 
 public class Lotto {
@@ -16,5 +20,17 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean isValidNumbers(List<Integer> numbers) {
+        return numbers.stream().allMatch(num -> num >= 1 && num <= 45);
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public static Lotto generateRandomLotto() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return new Lotto(numbers);
+    }
 }

8bc02aeaa7e2e9ff58292bdab98d8b76ba94938c
jjaegii <hn06038@gmail.com>
2023-11-08 22:20:59 +0900

    refactor: 띄어쓰기

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Output {
     public static void printLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
-        for(Lotto lotto: lottos) {
+        for (Lotto lotto : lottos) {
             List<Integer> numbers = lotto.getNumbers();
             Collections.sort(numbers);
             System.out.println(numbers);

5a3dd7c3d8b2f73b7c2b3379fd700b57b240a885
takoyakimchi <c4nyou@gmail.com>
2023-11-08 20:38:43 +0900

    refactor(const): 게임 결과 출력 문자열 상수로 정의

--- src/main/java/lotto/constant/GameMessage.java
+++ src/main/java/lotto/constant/GameMessage.java
@@ -6,7 +6,11 @@ public enum GameMessage {
     YOU_BOUGHT_N_LOTTOS("개를 구매했습니다."),
     INPUT_WIN_NUMBERS("당첨 번호를 입력해 주세요."),
     INPUT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
-    WIN_STATISTICS("당첨 통계\n---");
+    WIN_STATISTICS("당첨 통계\n---"),
+    MATCH_N_NUMBERS("개 일치"),
+    N_NUMBERS("개"),
+    TOTAL_REVENUE_RATE_IS("총 수익률은 "),
+    N_PERCENTS("%입니다.");
 
     private final String message;
 

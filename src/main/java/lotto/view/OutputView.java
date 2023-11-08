79d58b822f2d49b303a66c4326406f8b8ab3dcc7
peeaaccee <wlgp8958@naver.com>
2023-11-08 23:19:09 +0900

    feat(OutputView): 구입한 로또의 번호들 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
     private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String CHECK_AMOUNT_MESSAGE = "%d개를 구매했습니다.";
@@ -9,5 +13,11 @@ public class OutputView {
 
     public static void printCheckAmount(int amount) {
         System.out.printf(CHECK_AMOUNT_MESSAGE + "%n", amount);
+    }
+
+    public static void printPurchasedNumbers(List<Lotto> purchasedNumbers) {
+        for (Lotto lotto : purchasedNumbers) {
+            System.out.println(lotto.getNumbers().toString());
+        }
     }
 }

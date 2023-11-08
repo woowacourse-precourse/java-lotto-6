cc973e4d735c2c51adacbe3ba5eed192a838fc7c
cheolhyeon <tghy4@naver.com>
2023-11-08 17:01:28 +0900

    refacetor : 코드 구조 개선

--- src/main/java/lottogame/LottoGamePlayer.java
+++ src/main/java/lottogame/LottoGamePlayer.java
@@ -8,9 +8,9 @@ import print.LottoGameMessagePrinter;
 import supervisor.LottoSupervisor;
 
 public class LottoGamePlayer {
-    private LottoManager lottoManager;
-    private LottoGenerator generator;
-    private LottoSupervisor lottoSupervisor;
+    private final LottoManager lottoManager;
+    private final LottoGenerator generator;
+    private final LottoSupervisor lottoSupervisor;
     public LottoGamePlayer() {
         this.generator = new LottoGenerator();
         this.lottoManager = new LottoManager();
@@ -18,33 +18,47 @@ public class LottoGamePlayer {
     }
 
     public void gamePlay() {
-        //티켓 값 입력 및 객체 생성
-        LottoGameMessagePrinter.InputBuyLottoTicketMessage();
-        lottoManager.setInputLottoTicketPrice();
-        int lottoTickets = lottoManager.setLottoTicket();
-
-        // 해당 메서드가 호출이 되어야 Lotto 객체가 생성됨.
+        int lottoTickets = settingInputTicket();
         generator.generateLotto(lottoTickets);
-        // 로또 객체가 생성 되었으니 로또 리스트를 가지고옴
+        printLottoInfo();
+        int[] winningNumber = settingInputWinningNum();
+        int bonusNumber = settingInputBonusNum();
+        printLottoResult(winningNumber, bonusNumber);
+    }
+
+    private void printLottoResult(int[] winningNumber, int bonusNumber) {
+        // 당첨 통계 및 결과
+        LottoGameMessagePrinter.printResultMessage();
+        lottoSupervisor.printResultComparingToWinningNumber(winningNumber, bonusNumber);
+    }
+
+    private int settingInputBonusNum() {
+        // 보너스 번호 입력 및 객체 생성
+        LottoGameMessagePrinter.InputBonusNumber();
+        int bonusNumber = lottoManager.setInputBonusNumber();
+        return bonusNumber;
+    }
+
+    private int[] settingInputWinningNum() {
+        // 당첨번호 입력 및 객체 생성
+        LottoGameMessagePrinter.InputWinningNumber();
+        int[] winningNumber = lottoManager.setInputWinningNumbers();
+        return winningNumber;
+    }
+
+    private void printLottoInfo() {
         List<Lotto> lotto = generator.getLottoList();
 
         //몇개를 구매했는지 출력 후, 해당 로또의 숫자들을 출력
         LottoGameMessagePrinter.printPurchasedTickets(lotto);
         LottoGameMessagePrinter.printLottoNumbers(lotto);
+    }
 
-
-        // 당첨번호 입력 및 객체 생성
-        LottoGameMessagePrinter.InputWinningNumber();
-        int[] winningNumber = lottoManager.setInputWinningNumbers();
-
-
-        // 보너스 번호 입력 및 객체 생성
-        LottoGameMessagePrinter.InputBonusNumber();
-        int bonusNumber = lottoManager.setInputBonusNumber();
-
-        // 당첨 통계 및 결과
-        LottoGameMessagePrinter.printResultMessage();
-        lottoSupervisor.compareToWinningNumber(winningNumber, bonusNumber);
-
+    private int settingInputTicket() {
+        //티켓 값 입력 및 객체 생성
+        LottoGameMessagePrinter.InputBuyLottoTicketMessage();
+        lottoManager.setInputLottoTicketPrice();
+        int lottoTickets = lottoManager.setLottoTicket();
+        return lottoTickets;
     }
 }

7d28b55d6dee8da1850bfdb875869b5ed9d9fb01
San Kim <rlatks1217@gmail.com>
2023-11-08 16:36:06 +0900

    refactor: 클래스 구조 변경

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,10 @@
 package lotto;
 
 import lotto.GameProcess.LottoProcess;
-import lotto.GameProcess.LottoProcessImpl;
 
 public class Application {
     public static void main(String[] args) {
-        LottoProcess lottoProcess = new LottoProcessImpl();
+        LottoProcess lottoProcess = new LottoProcess();
         lottoProcess.playGame();
     }
 }

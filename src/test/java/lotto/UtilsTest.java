672bc8659640fd211bc15eb80d198518f9d58f55
i3kae <snoger1234@gmail.com>
2023-11-08 19:44:43 +0900

    test : 제외 문자가 포함된 숫자 검증 함수 테스트 코드 추가 1. 제외 문자가 포함되었을 때 해당 문자는 제외하고 검증을 진행하는지 확인하는 테스트 2. 제외 문자가 비어 있을 때 기존과 똑같이 검증을 진행하는지 확인하는 테스트

--- src/test/java/lotto/UtilsTest.java
+++ src/test/java/lotto/UtilsTest.java
@@ -1,10 +1,9 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UtilsTest {
@@ -25,5 +24,18 @@ class UtilsTest {
         assertThat(Utils.isNumeric("!1234")).isEqualTo(false);
         assertThat(Utils.isNumeric("12@34")).isEqualTo(false);
         assertThat(Utils.isNumeric("1234#")).isEqualTo(false);
+    }
+
+    @DisplayName("제외 문자가 포함되었을 때 해당 문자는 제외하고 검증을 진행한다.")
+    @Test
+    void normalNumberStringWithExcludeWord() {
+        assertThat(Utils.isNumeric("12,34,56", List.of(","))).isEqualTo(true);
+        assertThat(Utils.isNumeric("1!2@3#", Arrays.asList("!", "@", "#"))).isEqualTo(true);
+    }
+    @DisplayName("제외 문자가 비어 있을 때 기존과 똑같이 검증을 진행한다.")
+    @Test
+    void normalNumberStringWithEmptyExcludeWord() {
+        assertThat(Utils.isNumeric("123", List.of())).isEqualTo(true);
+        assertThat(Utils.isNumeric("12,34,56", List.of())).isEqualTo(false);
     }
 }

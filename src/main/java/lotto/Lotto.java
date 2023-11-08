f7f675d73101ff63d0f4c63df656bb333388b0cb
jiwon1217 <koagjw@gmail.com>
2023-11-08 15:18:53 +0900

    :sparkles: feat: 당첨 번호는 중복될 수 없다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Lotto {
     private static final String INVALID_LOTTO_SIZE = "[ERROR] 로또는 6개의 숫자로 이루어져야 합니다.";
     private static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호의 범위는 1 ~ 45까지 입니다.";
+    private static final String DUPLICATE_LOTTO_NUMBER = "[ERROR] 로또의 6개의 수는 중복될 수 없습니다.";
     private static final int LOTTO_SIZE = 6;
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 45;
@@ -23,6 +24,7 @@ public class Lotto {
     private void validate(List<Integer> numbers) {
         validateLottoSize(numbers);
         validateNumberRange(numbers);
+        validateDuplicateNumber(numbers);
     }
 
     private void validateLottoSize(List<Integer> numbers) {
@@ -40,5 +42,15 @@ public class Lotto {
     private boolean isValidRange(List<Integer> numbers) {
         return numbers.stream()
                 .anyMatch(number -> number < MIN_RANGE || number > MAX_RANGE);
+    }
+
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        if (isDuplicate(numbers)) {
+            throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBER);
+        }
+    }
+
+    private boolean isDuplicate(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
     }
 }

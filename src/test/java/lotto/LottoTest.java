7dec560ee709a78054d16d1ac6c4b222cb913e15
hyunjin <guswlsdl0121@gmail.com>
2023-11-08 23:17:51 +0900

    test: 로또 객체 생성 Test
    Parameterized Test로 리팩토링

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
@@ -8,6 +9,8 @@ import lotto.vo.WinningNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -49,63 +52,45 @@ class LottoTest {
         Assertions.assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))).isInstanceOf(Lotto.class);
     }
 
-    @DisplayName("일치하는 번호가 없을 경우 Rank.NONE을 반환한다.")
-    @Test
-    void matchRankWithNoMatches() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(7, 8, 9, 10, 11, 12));
-        BonusNumber bonusNumber = BonusNumber.from(13);
+    @DisplayName("다양한 번호 일치 경우에 대한 Rank 반환 테스트")
+    @ParameterizedTest(name = "번호가 {0}개 일치하고, 보너스 번호가 일치하는 경우 {1}, Rank.{2} 반환")
+    @CsvSource({
+            "0, false, NONE",
+            "3, false, FIFTH",
+            "4, false, FOURTH",
+            "5, false, THIRD",
+            "5, true, SECOND", // 5개의 번호가 일치하고 보너스 번호도 일치함
+            "6, false, FIRST"
+    })
+    void matchRankWithVariousMatches(int matchingNumbers, boolean matchBonus, LottoRank expectedRank) {
+        // Given: 유효한 로또 번호 리스트 준비
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
 
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.NONE);
-    }
+        // 매칭될 번호와 매칭되지 않을 번호를 설정합니다.
+        List<Integer> matchedNumbers = numbers.subList(0, matchingNumbers);
+        List<Integer> unmatchedNumbers = List.of(7, 8, 9, 10, 11, 12).subList(0, 6 - matchingNumbers);
+        List<Integer> winningNumbersList = new ArrayList<>();
+        winningNumbersList.addAll(matchedNumbers);
+        winningNumbersList.addAll(unmatchedNumbers);
+        WinningNumbers winningNumbers = WinningNumbers.from(winningNumbersList);
 
-    @DisplayName("일치하는 번호가 3개일 경우 Rank.FIFTH를 반환한다.")
-    @Test
-    void matchRankWithThreeMatches() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 7, 8, 9));
-        BonusNumber bonusNumber = BonusNumber.from(45);
+        // 보너스 번호 설정
+        int bonusNumberValue = 0;
+        if (matchBonus) {
+            bonusNumberValue = numbers.get(4); // 보너스 번호가 일치해야 하는 경우
+        }
 
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.FIFTH);
-    }
+        if (!matchBonus) {
+            bonusNumberValue = 45; // 보너스 번호가 일치하지 않아야 하는 경우
+        }
 
-    @DisplayName("일치하는 번호가 4개일 경우 Rank.FOURTH를 반환한다.")
-    @Test
-    void matchRankWithFourMatches() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 11, 12));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 8, 9));
-        BonusNumber bonusNumber = BonusNumber.from(13);
+        BonusNumber bonusNumber = BonusNumber.from(bonusNumberValue);
 
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.FOURTH);
-    }
+        // When: Lotto 객체를 생성하고 matchRank 메서드를 호출
+        Lotto lotto = new Lotto(numbers);
+        LottoRank resultRank = lotto.matchRank(winningNumbers, bonusNumber);
 
-    @DisplayName("일치하는 번호가 5개일 경우 Rank.THIRD를 반환한다.")
-    @Test
-    void matchRankWithFiveMatches() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 12));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 9));
-        BonusNumber bonusNumber = BonusNumber.from(13);
-
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.THIRD);
-    }
-
-    @DisplayName("일치하는 번호가 5개이고 보너스 번호도 일치할 경우 Rank.SECOND를 반환한다.")
-    @Test
-    void matchRankWithFiveMatchesAndBonus() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 12));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 9));
-        BonusNumber bonusNumber = BonusNumber.from(12);
-
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.SECOND);
-    }
-
-    @DisplayName("일치하는 번호가 6개일 경우 Rank.FIRST를 반환한다.")
-    @Test
-    void matchRankWithSixMatches() {
-        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-        WinningNumbers winningNumbers = WinningNumbers.from(List.of(1, 2, 3, 4, 5, 6));
-        BonusNumber bonusNumber = BonusNumber.from(12);
-
-        Assertions.assertThat(lotto.matchRank(winningNumbers, bonusNumber)).isEqualTo(LottoRank.FIRST);
+        // Then: 반환된 Rank가 예상 Rank와 일치하는지 검증
+        Assertions.assertThat(resultRank).isEqualTo(expectedRank);
     }
 }

1a87905797e558a097d2c1312cdee0953184c6c7
kmw2378 <rlarla677@gmail.com>
2023-11-08 12:58:03 +0900

    feat(lotto): 콘솔 입력시 사용되는 View 클래스 추가 콘솔을 통해 입력 받을 때 사용되는 View 클래스를 추가하였습니다. InputView를 구현하며 별도의 검증 로직이 포함되있습니다.

---
+++ src/main/java/lotto/view/ConsoleInputView.java
@@ -0,0 +1,54 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.dto.request.BonusNumberRequest;
+import lotto.dto.request.PurchasePriceRequest;
+import lotto.dto.request.WinningNumberRequest;
+import lotto.exception.InputException;
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.configuration.GameConfiguration.INPUT_SEPARATOR;
+import static lotto.exception.errorcode.InputErrorCode.INVALID_BONUS_NUMBER;
+
+public class ConsoleInputView implements InputView {
+    @Override
+    public PurchasePriceRequest requestPurchasePrice() {
+        final String input = Console.readLine();
+        return new PurchasePriceRequest(Long.parseLong(input));
+    }
+
+    @Override
+    public WinningNumberRequest requestWinningNumber() {
+        final String winningNumber = Console.readLine();
+        validateSplitEachNumeric(winningNumber);
+        return new WinningNumberRequest(getWinningNumber(winningNumber));
+    }
+
+    @Override
+    public BonusNumberRequest requestBonusNumber() {
+        final String bonusNumber = Console.readLine();
+        validateNumeric(bonusNumber);
+        return new BonusNumberRequest(Integer.parseInt(bonusNumber));
+    }
+
+    private List<Integer> getWinningNumber(final String winningNumber) {
+        return Arrays.stream(winningNumber.split(INPUT_SEPARATOR))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .toList();
+    }
+
+    private void validateNumeric(final String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InputException(INVALID_BONUS_NUMBER);
+        }
+    }
+
+    private void validateSplitEachNumeric(final String input) {
+        Arrays.stream(input.split(INPUT_SEPARATOR))
+                .forEach(this::validateNumeric);
+    }
+}

29e0ced7a394b4cd472da2ca62cd2a6fdddc2111
gunwoong <gwj0421@gmail.com>
2023-11-08 12:40:22 +0900

    test : 당첨 번호를 검증하고 WinningNumbers 객체의 필드에 저장하는 기능 테스트 메서드 추가

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -1,36 +1,59 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import static lotto.config.ConstantNum.*;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class WinningNumbersTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
+    private List<Integer> winning;
+    private List<Integer> wrong;
+    private WinningNumbers winningNumbers;
 
-    @Test
-    @DisplayName("주어진 당첨 번호의 개수가 일치하지 않을 경우 예외 발생")
-    void testValidate() {
+    @BeforeEach
+    void setUp() {
         // given
-        List<Integer> winning = new ArrayList<>();
-        for (int i = LOTTO_NUMBER_MIN.getNum(); i < LOTTO_NUMBER_MIN.getNum() + LOTTO_BONUS_NUMBER_CNT.getNum(); i++) {
+        winning = new ArrayList<>();
+        for (int i = LOTTO_NUMBER_MIN.getNum(); i < LOTTO_NUMBER_MIN.getNum() + WINNING_NUMBER_CNT.getNum(); i++) {
             winning.add(i);
         }
-        List<Integer> wrong = new ArrayList<>(winning);
-        wrong.add(LOTTO_NUMBER_MAX.getNum());
+        wrong = new ArrayList<>(winning);
+    }
 
+    @Test
+    @DisplayName("주어진 번호의 개수, 범위가 일치할 경우 정상 동작")
+    void Should_Success_When_ValidWinningNumberCntAndRange() {
         // when
-        BonusNumbers winningNumbers = new BonusNumbers(winning);
+        ThrowableAssert.ThrowingCallable throwingCallable = () -> new WinningNumbers(winning);
 
         // then
-        assertThatThrownBy(() -> winningNumbers.validate(wrong))
-                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(ERROR_MESSAGE);
-        assertThatCode(() -> winningNumbers.validate(winning)).doesNotThrowAnyException();
+        assertThatCode(throwingCallable).doesNotThrowAnyException();
+    }
 
+    @Test
+    @DisplayName("주어진 당첨 번호의 개수가 일치하지 않을 경우 예외 처리 후 재입력 동작")
+    void Should_ReEnter_When_WinningNumberCntIsNotMatch() {
+        // given
+        wrong.add(LOTTO_NUMBER_MIN.getNum());
+
+        // when
+        try (final MockedStatic<Console> consoleMock = mockStatic(Console.class)) {
+            consoleMock.when(() -> readLine()).thenReturn(
+                    winning.toString().substring(1, winning.toString().length() - 1).replaceAll("\\s", ""));
+            winningNumbers = new WinningNumbers(winning);
+        }
+
+        // then
+        assertThat(winningNumbers.getNumbers()).usingRecursiveComparison().isEqualTo(winning);
     }
 }

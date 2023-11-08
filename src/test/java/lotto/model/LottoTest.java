63bc71d0addad6e759dbca8852a3b28c7a880807
insu <is23193160@gmail.com>
2023-11-08 20:56:11 +0900

    test: 예외가 어떤 메세지인지 확인하는 메서드 추가 작성

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -13,14 +13,16 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 숫자 6개를 입력해주세요.");
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 중복되지 않은 숫자를 입력해주세요.");
     }
 
     @Test

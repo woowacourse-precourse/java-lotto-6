f6bcc9b726c1fa6811d87a3cb13e275fa15a79df
chanwoo <mdsoo55828@gmail.com>
2023-11-08 14:02:45 +0900

    feat: Lotto 개수 예외처리 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,7 +1,12 @@
 package lotto.model;
 
+import lotto.constant.ErrorConstants;
+import lotto.constant.GameConstants;
+
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -13,12 +18,16 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        validateLottoSize(numbers);
     }
+
     private void sortNumbers(List<Integer> numbers){
         Collections.sort(numbers);
     }
 
+    private void validateLottoSize(List<Integer> numbers) {
+        if (numbers.size() != GameConstants.LOTTO_COUNT) {
+            throw new IllegalArgumentException(ErrorConstants.OVER_LOTTO_SIZE);
+        }
+    }
 }

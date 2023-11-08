0234be4184614df9cd47cb5e4b00e2cf76d66a58
oosedus <seayeon0729@gmail.com>
2023-11-08 16:33:50 +0900

    refactor: 등수별 개수 계산하는 함수들 PrizeCalculator 클래스로 이동

--- src/main/java/lotto/PrizeCalculaor.java
+++ src/main/java/lotto/PrizeCalculaor.java
@@ -10,6 +10,82 @@ public class PrizeCalculaor {
     private final static int FIVE_BONUS_WINNING_PRIZE = 30000000;
     private final static int SIX_WINNING_PRIZE = 2000000000;
 
+    int countThreeMatches(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbers){
+        int count = 0;
+        for(List<Integer> lottoNumbers : lottoNumbersCollection){
+            List<Integer> copyOfLottoNumbers = new ArrayList<>(lottoNumbers);
+            copyOfLottoNumbers.retainAll(winningNumbers);
+            if(copyOfLottoNumbers.size() == 3){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countFourMatches(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbers){
+        int count = 0;
+        for(List<Integer> lottoNumbers : lottoNumbersCollection){
+            List<Integer> copyOfLottoNumbers = new ArrayList<>(lottoNumbers);
+            copyOfLottoNumbers.retainAll(winningNumbers);
+            if(copyOfLottoNumbers.size() == 4){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countFiveMatches(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbers){
+        int count = 0;
+        for(List<Integer> lottoNumbers : lottoNumbersCollection){
+            List<Integer> copyOfLottoNumbers = new ArrayList<>(lottoNumbers);
+            copyOfLottoNumbers.retainAll(winningNumbers);
+            if(copyOfLottoNumbers.size() == 5){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countSixMatches(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbersExceptBonus){
+        int count = 0;
+        for(List<Integer> lottoNumbers : lottoNumbersCollection){
+            List<Integer> copyOfLottoNumbers = new ArrayList<>(lottoNumbers);
+            copyOfLottoNumbers.retainAll(winningNumbersExceptBonus);
+            if(copyOfLottoNumbers.size() == 6){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countFiveBonusMatches(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbersExecptBonus, int bonusNumber){
+        int count = 0;
+        for(List<Integer> lottoNumbers : lottoNumbersCollection){
+            List<Integer> copyOfLottoNumbers = new ArrayList<>(lottoNumbers);
+            copyOfLottoNumbers.retainAll(winningNumbersExecptBonus);
+            if(copyOfLottoNumbers.size() == 5 && copyOfLottoNumbers.contains(bonusNumber)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    List<Integer> makeWinningCountCollection(List<List<Integer>> lottoNumbersCollection, List<Integer> winningNumbers,List<Integer> winningNumbersExecptBonus, int bonusNumber){
+        List<Integer> winningCount = new ArrayList<>();
+        winningCount.add(countThreeMatches(lottoNumbersCollection, winningNumbers));
+        winningCount.add(countFourMatches(lottoNumbersCollection,winningNumbers));
+        winningCount.add(countFiveMatches(lottoNumbersCollection,winningNumbers));
+        winningCount.add(countFiveBonusMatches(lottoNumbersCollection,winningNumbersExecptBonus,bonusNumber));
+        winningCount.add(countSixMatches(lottoNumbersCollection,winningNumbersExecptBonus));
+        return winningCount;
+    }
+
+    List<Integer> combineWinningNumbersAndBonusNumber(List<Integer> winningNumbers, int BonusNumber){
+        List<Integer> winningNumbersWithBonus = winningNumbers;
+        winningNumbersWithBonus.add(BonusNumber);
+        return winningNumbersWithBonus;
+    }
+
     List<Integer> calculatePrize(List<Integer> countsOfWonLotto){
         List<Integer> prizes = new ArrayList<>();
         prizes.add(countsOfWonLotto.get(0)*THREE_WINNING_PRIZE);

f4b32f22f66e70cba67b9922e79103ba3e36f3c4
lsj5657 <lsj56575@naver.com>
2023-11-08 12:26:59 +0900

    입력에 대한 정보 메시지 출력, 숫자 하드 코딩된 부분을 상수 이용

--- src/main/java/lotto/LottoManagement.java
+++ src/main/java/lotto/LottoManagement.java
@@ -10,13 +10,16 @@ public class LottoManagement {
     LottoManagement(){}
 
     void initMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
         String moneyInput = Console.readLine();
         InputMoneyHandler inputMoneyHandler = new InputMoneyHandler(moneyInput);
         verifiedMoney=inputMoneyHandler.exceptionHandledMoney();
     }
 
     void initWinningLottoNumbers(){
+        System.out.println("당첨 번호를 입력해 주세요.");
         String numbersInput = Console.readLine();
+        System.out.println("보너스 번호를 입력해주세요:");
         String bonusNumberInput=Console.readLine();
         NumbersHandler numbersHandler = new NumbersHandler(numbersInput,bonusNumberInput);
         winningLottoNumbers = numbersHandler.convertedWinningNumbers();
--- src/main/java/lotto/NumbersHandler.java
+++ src/main/java/lotto/NumbersHandler.java
@@ -1,48 +1,54 @@
 package lotto;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class NumbersHandler {
-    static final int maxNumber=45;
+    static final int minNumber = 1;
+    static final int maxNumber = 45;
+    static final int duplication = 2;
     private final List<Integer> winningNumbers = new ArrayList<>();
     private final int bonusNumber;
 
-    NumbersHandler(String numbersInput, String bonusNumberInput){
+    NumbersHandler(String numbersInput, String bonusNumberInput) {
         makeList(numbersInput);
-        bonusNumber=convert(bonusNumberInput);
+        bonusNumber = convert(bonusNumberInput);
     }
 
-    public void makeList(String numbersInput){
+    public void makeList(String numbersInput) {
         String[] numbers = numbersInput.split(",");
-        for(String number:numbers){
+        for (String number : numbers) {
             winningNumbers.add(convert(number));
         }
     }
 
-    public int convert(String number){
+    public int convert(String number) {
         int convertedNumber;
         try {
             convertedNumber = Integer.parseInt(number);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
         }
         return convertedNumber;
     }
 
-    public boolean hasDuplicateOrNotValidNumber(){
-        List<Integer> numberCount= new ArrayList<>(maxNumber+1);
-        for (int number=0; number<=maxNumber; number++) numberCount.add(0);
-        
-        for (int number:winningNumbers){
-            if(number<1||number>45)return false;
-            numberCount.set(number,numberCount.get(number)+1);
-            if(numberCount.get(number)>=2)return true;
+    public boolean hasDuplicateOrNotValidNumber() {
+        List<Integer> numberCount = new ArrayList<>(maxNumber + 1);
+        for (int number = minNumber - 1; number <= maxNumber; number++) numberCount.add(0);
+
+        if (bonusNumber < minNumber || bonusNumber > maxNumber) return false;
+        numberCount.set(bonusNumber, numberCount.get(bonusNumber) + 1);
+
+        for (int number : winningNumbers) {
+            if (number < minNumber || number > maxNumber) return false;
+            numberCount.set(number, numberCount.get(number) + 1);
+            if (numberCount.get(number) >= duplication) return true;
         }
         return false;
     }
 
-    public List<Integer> convertedWinningNumbers(){
-        if(hasDuplicateOrNotValidNumber())throw new IllegalArgumentException();
+    public List<Integer> convertedWinningNumbers() {
+        if (hasDuplicateOrNotValidNumber()) throw new IllegalArgumentException();
         return winningNumbers;
     }
 

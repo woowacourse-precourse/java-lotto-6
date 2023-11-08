7fd53b6f5762178806ba3dc1cfc0ba1f1b73e530
flown222 <jangu3384@gmail.com>
2023-11-08 14:20:53 +0900

    test: 금액 정규식 테스트

--- src/test/java/lotto/utils/AmountValidatorTest.java
+++ src/test/java/lotto/utils/AmountValidatorTest.java
@@ -10,8 +10,15 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 class AmountValidatorTest {
     @Test
+    void validateMoney() {
+        assertThatThrownBy(() -> AmountValidator.validateMoney("01244"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("금액을 올바르게 입력하세요.");
+    }
+
+    @Test
     void validateAmountByZero() {
-        assertThatThrownBy(() -> AmountValidator.validateAmount(BigDecimal.ZERO))
+        assertThatThrownBy(() -> AmountValidator.validatePurchasingAmount(BigDecimal.ZERO))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("금액은 0을 입력할 수 없습니다.");
     }
@@ -20,7 +27,7 @@ class AmountValidatorTest {
     @ParameterizedTest(name = "{index} ==> 투입금액 : {0}")
     @CsvSource(value = {"1001", "10005", "21239"})
     void validateAmountByImproperNumber(int inputMoney) {
-        assertThatThrownBy(() -> AmountValidator.validateAmount(new BigDecimal(inputMoney)))
+        assertThatThrownBy(() -> AmountValidator.validatePurchasingAmount(new BigDecimal(inputMoney)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("1000 단위로만 입력 가능합니다.");
     }

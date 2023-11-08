f88d217726564ccadbbf1e531164ec05ba5c1f2a
minjumost <95904729+minjumost@users.noreply.github.com>
2023-11-08 16:13:30 +0900

    fix: 불변값에 대한 수정
    pickUniqueNumbersInRange가 final, 불변값을 리턴하는 이슈가 있어서 새로운 배열에 담아 반환하도록 수정, 이에 따라 다른 일급콜렉션도 불변을 유지할 수 있게 복사본을 return하도록 수정

--- src/main/java/lotto/util/Generator.java
+++ src/main/java/lotto/util/Generator.java
@@ -1,15 +1,18 @@
 package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 import lotto.constant.ConfigurationNumbers;
 
 public class Generator {
     public static List<Integer> generateRandomNumbers() {
-        return Randoms.pickUniqueNumbersInRange(
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
                 ConfigurationNumbers.MIN_NUMBER.getNumber(),
                 ConfigurationNumbers.MAX_NUMBER.getNumber(),
                 ConfigurationNumbers.LOTTO_LENGTH.getNumber()
         );
+        List<Integer> randomNumbersCopy = new ArrayList<>(randomNumbers);
+        return randomNumbersCopy;
     }
 }

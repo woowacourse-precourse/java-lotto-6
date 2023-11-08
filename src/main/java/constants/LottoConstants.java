7378ef4e1868e2725355d00ee99c96f2bb611bc3
pjaehyun <show6756@gmail.com>
2023-11-08 15:09:59 +0900

    refactor: 오타 수정

--- src/main/java/constants/LottoConstants.java
+++ src/main/java/constants/LottoConstants.java
@@ -1,7 +1,7 @@
 package constants;
 
 public class LottoConstants {
-    public static final String PURCHASE_MESSAGE = "개를 구매했습니다";
+    public static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
 
     public static final String WINNING_STATISTICS_MESSAGE = "당첨 통계";
 

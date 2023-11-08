27a97f823afb2bde38b37d14f90e6c1ba995e19b
jh <pv104@naver.com>
2023-11-08 19:12:06 +0900

    feat : 기능 추가 로또 당첨 결과를 출력하는 기능 추가

--- src/main/java/lotto/view/PrintResult.java
+++ src/main/java/lotto/view/PrintResult.java
@@ -1,10 +1,35 @@
 package lotto.view;
 
+import lotto.controller.CalculateController;
+import lotto.controller.LottoController;
+import lotto.model.Rankings;
+
+import java.util.Arrays;
+
 public class PrintResult {
 
+
+    private LottoController lottoController = new LottoController();
+    int[] resultsOfPrize;
+    double resultOfProfitRate;
+    public PrintResult(int[] resultsOfPrize,double resultOfProfitRate) {
+        this.resultsOfPrize = resultsOfPrize;
+        this.resultOfProfitRate = resultOfProfitRate;
+    }
 
     public String printStartText() {
         return "당첨 통계\n" +
                 "---";
     }
+
+    public String printResults() {
+        Rankings.RankingData[] rankingData = Rankings.RankingData.values();
+        StringBuilder results = new StringBuilder();
+        for (Rankings.RankingData values:
+             rankingData) {
+            results.append(values.getRankings() + resultsOfPrize[values.ordinal()] + " \n");
+        }
+        return results.toString();
+    }
+
 }

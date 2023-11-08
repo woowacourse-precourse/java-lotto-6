6e61b63e48c06d1527809f0ae6155207fc635f43
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 22:32:28 +0900

    refactor: change method name
    자료형 포함한 메서드 이름 변경에 따른 커밋

--- src/main/java/lotto/service/UserRequestService.java
+++ src/main/java/lotto/service/UserRequestService.java
@@ -7,10 +7,10 @@ import lotto.util.Exception;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
-// 유저로부터 입력값을 받아 예외를 처리해주는 클래스
-// 1. 로또 금액에 대해 입력 받고 예외를 불러와 검증을 시작하는 메서드
-// 2. 당첨 로또 번호를 입력 받고 예외를 불러와 검증을 시작하는 메서드
-// 3. 당첨 보너스 번호를 입력 받고 예외를 불러와 검증을 시작하는 메서드
+/* 유저로부터 입력값을 받아 예외를 처리해주는 클래스
+1. 로또 금액에 대해 입력 받고 예외를 불러와 검증을 시작하는 메서드
+2. 당첨 로또 번호를 입력 받고 예외를 불러와 검증을 시작하는 메서드
+3. 당첨 보너스 번호를 입력 받고 예외를 불러와 검증을 시작하는 메서드*/
 
 public class UserRequestService {
     public static int requestLottoCostWithException() {
@@ -21,7 +21,7 @@ public class UserRequestService {
 
     public static Lotto requestWinningLottoWithException() {
         OutputView.printRequestWinningNumbers();
-        List<Integer> winningNumbers = Converter.toIntegerList(InputView.requestWinningNumber());
+        List<Integer> winningNumbers = Converter.convertToNumberList(InputView.requestWinningNumber());
         return Exception.winningLottoException(winningNumbers);
     }
 

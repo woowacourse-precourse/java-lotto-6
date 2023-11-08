c67134f3cf7f4a47f97bdbd63f27dee21e71758f
dmdwns2 <ssreki1@naver.com>
2023-11-08 15:27:48 +0900

    feat: create output message for purchase history

--- src/main/java/lotto/Output.java
+++ src/main/java/lotto/Output.java
@@ -1,7 +1,16 @@
 package lotto;
 
+import java.util.List;
+
 public class Output {
-    public void outputPurchaseAmount(){
+    public void outputPurchaseAmount() {
         System.out.println("구입금액을 입력 주세요.");
+    }
+
+    public void outputTicketPurchaseHistory(int ticket, List<List<Integer>> allTicketNumbers) {
+        System.out.println(ticket + "개를 구했습니다.");
+        for (int i = 0; i < ticket; i++) {
+            System.out.println(allTicketNumbers.get(i));
+        }
     }
 }

098039eddb3c3475bbe9857a466ee1b47c5334ff
Jihyeon Park <mmmszip@gmail.com>
2023-11-08 23:54:26 +0900

    feat: 예외처리 메시지 기능 구현

---
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -0,0 +1,12 @@
+package lotto.constants;
+
+public class ErrorMessage {
+    public static final String INVALID_NUMBER_RANGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALID_LOTTO_LENGTH = "[ERROR] 6자리의 로또 번호를 입력하세요.";
+    public static final String INVALID_DUPLICATE_NUMBERS = "[ERROR] 로또 번호는 중복되면 안됩니다.";
+    public static final String INVALID_DUPLICATE_BONUS_NUMBER = "[ERROR] 로또 번호에 존재하는 번호와 중복되면 안됩니다.";
+    public static final String INVALID_NUMBER_FORMAT = "[ERROR] 숫자 형식이 올바르지 않습니다.";
+    public static final String INVALID_POSITIVE_NUMBER = "[ERROR] 0보다 큰 숫자여야 합니다.";
+    public static final String INVALID_HUNDRED_UNIT_AMOUNT = "[ERROR] 1,000원 단위의 숫자여야 합니다.";
+
+}

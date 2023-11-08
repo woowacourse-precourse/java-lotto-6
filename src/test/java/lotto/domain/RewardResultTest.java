c27f7d717e6016ec4868dd3959cdbd569fa233e9
Aeraekun <lsch3374@naver.com>
2023-11-08 19:55:04 +0900

    test: resultReward 테스트 추가 ################
    ################
    
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/test/java/lotto/domain/RewardResultTest.java
+++ src/test/java/lotto/domain/RewardResultTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;

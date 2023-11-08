fab63314e79ac6ede6092541ac6df76d79977634
sangxxjin <sj_0368@naver.com>
2023-11-08 23:06:31 +0900

    refactor: controller 예외 입력 재입력 메소드 적용

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -18,7 +18,7 @@ public class Application {
     for (Lotto lotto : lottos.getLottos()) {
       OutputView.displayLottoNumbers(lotto.getNumbers());
     }
-    lottos.addLottos(InputView.getWinningNumber());
+    lottos.addLottos(ControllerService.isValidWinningNumber(InputView.getWinningNumber()));
     Lotto lastLotto = lottos.getLottos().get(lottos.getLottos().size() - 1);
     List<Integer> lastLottoNumbers = lastLotto.getNumbers();
     BonusNumber bonusNumber = new BonusNumber(InputView.getBonusNumber(), lastLottoNumbers);
@@ -29,15 +29,4 @@ public class Application {
             lottoTicketQuantity.getLottoTicketQuantity()));
 
   }
-//  private static String checkValidPurchaseAmount(String purchaseAmount) {
-//    String canPasrse = purchaseAmount;
-//    try {
-//      // 문자열을 숫자로 변환하여 예외가 발생하지 않으면 숫자로 판단
-//      double d = Double.parseDouble(canPasrse);
-//    } catch (IllegalArgumentException e) {
-//      System.out.println("[ERROR] 구입 금액이 유효하지 않습니다.");
-//      checkValidPurchaseAmount(InputView.getPurchaseAmount());
-//    }
-//    return canPasrse;
-//  }
 }

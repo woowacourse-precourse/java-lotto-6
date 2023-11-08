e663141a746e5a8467eb98c2ca294bbd91e6895e
DonghaOh <donghadarwin@naver.com>
2023-11-08 17:12:12 +0900

    feat: 당첨 결과 일치 개수 출력
    lottoResultMessagePrint 메소드

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -28,4 +28,12 @@ public class View {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
     }
+
+    public static void lottoResultMessagePrint(List<Integer> lottoResult) {
+        System.out.printf("3개 일치 (5,000원) - %d개", lottoResult.get(0));
+        System.out.printf("4개 일치 (50,000원) - %d개", lottoResult.get(1));
+        System.out.printf("5개 일치 (1,500,000원) - %d개", lottoResult.get(2));
+        System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개", lottoResult.get(3));
+        System.out.printf("6개 일치 (2,000,000,000원) - %d개", lottoResult.get(4));
+    }
 }

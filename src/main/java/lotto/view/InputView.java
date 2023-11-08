8ce10f42fc052155d1f68d9b228ec2fb0a5bfdeb
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:11:18 +0900

    feat : inputMoney 함수 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,4 +7,22 @@ public class InputView {
     private static final int MAX_LOTTO_NUMBER = 45;
     private static final int LOTTO_NUMBERS_SIZE = 6;
 
+    public static int inputMoney() {
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요. (1000원 단위)");
+                String input = scanner.nextLine();
+                int purchaseAmount = Integer.parseInt(input);
+                if (purchaseAmount <= 0 || purchaseAmount % 1000 != 0) {
+                    throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위의 양수여야 합니다.");
+                }
+                return purchaseAmount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }

9ef60791d158cebd8e3f70972a58b5d6541397a6
깃wooqqq <sge0225@naver.com>
2023-11-08 15:26:07 +0900

    refactor: 구입 금액 예외처리 메서드 다시 추가

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -1,11 +1,23 @@
 package lotto.domain;
 
 public class Money {
+    private static final int ZERO = 0;
     private static final int THOUSAND = 1000;
     private final int money;
 
     public Money(int money) {
+        validate(money);
         this.money = money;
+    }
+
+    private void validate(int input) {
+        validateRemainder(input);
+    }
+
+    private void validateRemainder(int input) {
+        if (input % THOUSAND != ZERO) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public int getTicket() {

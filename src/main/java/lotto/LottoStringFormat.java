92eb9b935fe5325413ffecdb63281a8fc9d7462a
giljin <68088970+giljin@users.noreply.github.com>
2023-11-08 12:23:54 +0900

    feat(LottoStringFormat): 로또 안내 멘트 포맷

---
+++ src/main/java/lotto/LottoStringFormat.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class LottoStringFormat {
+    public static final String LOTTO_RESULT_FORMAT = "%s %s - %d개";
+    public static final String LOTTO_RATE_OF_RETURN_RESULT = "총 수익률은 %s%%입니다.";
+    public static final String LOTTO_RATE_OF_RETURN = "%,.1f";
+    public static final String LOTTO_RULE_FORMAT = "%d개 일치%s";
+    public static final String LOTTO_BUY_COUNT_FORMAT = "\n%d개를 구매했습니다.";
+    public static final String LOTTO_RESULT_TO_STRING_FORMAT = "(%s원)";
+}

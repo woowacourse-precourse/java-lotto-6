7a330400cb922a1718aa08f5a929395075e67f14
Hyeon-hak Kim <07ily@naver.com>
2023-11-08 14:28:46 +0900

    feat(Application): `lottoGame`을 활용한 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+        List<Lotto> purchasedLotto = lottoGame.purchaseLotto();
+
+        System.out.println(purchasedLotto.size() + "개를 구매했습니다.");
+        for (Lotto lotto : purchasedLotto) {
+            System.out.println(lotto.getNumbers().toString());
+        }
+
+        LottoChecker lottoChecker = lottoGame.getResult();
+        lottoGame.printScores(lottoChecker.getTotalScores(purchasedLotto));
+        lottoGame.printProfitRate(lottoChecker.getTotalPrizes(purchasedLotto));
     }
 }

524e2cf87a96e93b5902c2fa304028d2cef575ad
mini1115 <kmini9611@naver.com>
2023-11-08 14:03:33 +0900

    refactor : 컨벤션 정리

--- src/main/java/lotto/ErrorValidate.java
+++ src/main/java/lotto/ErrorValidate.java
@@ -2,13 +2,14 @@ package lotto;
 
 public class ErrorValidate {
     public static void inputNumberLimit(int number) {
-        if (number<1 || number>46) {
+        if (number < 1 || number > 46) {
             throw new IllegalArgumentException();
         }
     }
-    public static void isNumberFormat(String input){
+
+    public static void isNumberFormat(String input) {
         final String REGEX = "[0-9]+";
-        if(!input.matches(REGEX)){
+        if (!input.matches(REGEX)) {
             throw new IllegalArgumentException();
         }
     }

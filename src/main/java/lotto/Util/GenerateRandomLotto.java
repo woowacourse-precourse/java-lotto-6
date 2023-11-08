51f0b2ef503c195ae113671d97c73a1b583e0ebe
Choijake <shameless8@naver.com>
2023-11-08 18:22:02 +0900

    Refactor : GenerateRandomLotto 클래스의 amount 어트리뷰트 삭제 -> 일회성 값으로 사용

--- src/main/java/lotto/Util/GenerateRandomLotto.java
+++ src/main/java/lotto/Util/GenerateRandomLotto.java
@@ -5,13 +5,21 @@ import java.util.List;
 import java.util.stream.IntStream;
 import lotto.domain.InputHandler;
 import lotto.domain.Lotto;
+import lotto.view.OutputView;
 
 public class GenerateRandomLotto {
+    //TODO : 입력 값에 대한 검증 필요할 듯
     private static final int LOTTO_PRICE = 1000;
+    private List<Lotto> autoLottoList;
 
-    public static List<Lotto> AutoGenerateLottoList() {
+    public GenerateRandomLotto() {
+        this.autoLottoList = AutoLottoList();
+    }
+
+    public List<Lotto> AutoLottoList() {
         List<Lotto> LottoList = new ArrayList<>();
-        IntStream.range(0, provideAmountNumber())
+        int amount = InputHandler.inputAmountNumber() / LOTTO_PRICE;
+        IntStream.range(0, amount)
                 .forEach(i -> LottoList.add(Lotto.of(generateAutoLottoNumberList())));
 
         return LottoList;
@@ -23,8 +31,11 @@ public class GenerateRandomLotto {
         return lottoNumberList;
     }
 
-    private static int provideAmountNumber() {
-        int amountNumber = InputHandler.inputAmountNumber() / LOTTO_PRICE;
-        return amountNumber;
+    public void printAmount() {
+        System.out.println(String.format(OutputView.BUYING_COUNT, autoLottoList.size()));
+    }
+
+    public List<Lotto> getLottoList() {
+        return autoLottoList;
     }
 }

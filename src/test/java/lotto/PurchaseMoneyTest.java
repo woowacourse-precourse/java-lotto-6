4322d61d9cfb18d289f1ac2176376d7926af1d8e
ryj913 <ryj913@naver.com>
2023-11-08 14:00:43 +0900

    add domain/Exception_Test

---
+++ src/test/java/lotto/PurchaseMoneyTest.java
@@ -0,0 +1,23 @@
+package lotto;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.domain.Money;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PurchaseMoneyTest {
+
+    @DisplayName("구매 금액이 1000원단위가 아닐 경우 예외 발생")
+    @Test
+    void formatException() {
+        assertThatThrownBy(() -> new Money(1234))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    void format_amountException_2() {
+        assertThatThrownBy(() -> new Money(300))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}

0d73a190220b1d9f6b4d04723213b31e744b5c85
minoooo119 <minoth0119@naver.com>
2023-11-08 20:57:35 +0900

    [ADD] MoneyCalculateModel.java: 당첨 금액, 수익률 계산 Model

--- src/main/java/Model/MoneyCalculateModel.java
+++ src/main/java/Model/MoneyCalculateModel.java
@@ -1,2 +1,33 @@
-package Model;public class MoneyCalculateModel {
+package Model;
+
+import lotto.Rank;
+
+public class MoneyCalculateModel {
+    private long totalMoney;
+    private Integer investMoney;
+    private double niceMoneyPercentage;
+    public MoneyCalculateModel(Integer investMoney,RankCalculateModel rankCalculateModel) {
+        totalMoney=sumAll(rankCalculateModel.getFirstRank()*200000000,
+                rankCalculateModel.getSecondRank()*30000000,
+                rankCalculateModel.getThirdRank()*1500000,
+                rankCalculateModel.getFourthRank()*50000,
+                rankCalculateModel.getFifthRank()*5000);
+        this.investMoney=investMoney;
+        niceMoneyPercentage=calculateNiceMoney();
+    }
+    private long sumAll(Integer a, Integer b, Integer c, Integer d, Integer e){
+        return a+b+c+d+e;
+    }
+    private double calculateNiceMoney(){
+        return ((double)totalMoney/(double)investMoney)*100;
+    }
+    public double getNiceMoneyPercentage(){
+        return niceMoneyPercentage;
+    }
+    public void printNiceMoneyPercentage(){
+        System.out.printf("총 수익률은 %.1f%%입니다.\n",getNiceMoneyPercentage());
+    }
+
+
+
 }

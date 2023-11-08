6b490e9350dfe55e3c363fef974b03690d963ca1
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:36:45 +0900

    refactor(PaymentAmount): 구입 금액 유효성 검증 책임 변경

--- src/main/java/lotto/model/PaymentAmount.java
+++ src/main/java/lotto/model/PaymentAmount.java
@@ -1,9 +1,12 @@
 package lotto.model;
 
+import lotto.validation.PaymentAmountValidation;
+
 public class PaymentAmount {
     private final int price;
 
     public PaymentAmount(int price) {
+        PaymentAmountValidation.validatePaymentAmount(price);
         this.price = price;
     }
 

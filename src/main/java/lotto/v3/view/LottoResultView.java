e3af24a79d6b93006677bb45fad77060b94e8fb9
권장순 <jsoonworld@gmail.com>
2023-11-08 10:24:46 +0900

    refactoring( LottoResultView ) : Enum으로 받은 값을 순서를 보장해서 출력할 수 있도록 수정.

--- src/main/java/lotto/v3/view/LottoResultView.java
+++ src/main/java/lotto/v3/view/LottoResultView.java
@@ -2,13 +2,25 @@ package lotto.v3.view;
 
 import lotto.v3.model.LottoRank;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class LottoResultView {
     public void displayResults(Map<LottoRank, Integer> matchCounts) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        for (LottoRank rank : LottoRank.values()) {
+
+        // 원하는 출력 순서로 LottoRank 값들을 List에 추가
+        List<LottoRank> sortedRanks = new ArrayList<>();
+        sortedRanks.add(LottoRank.FIFTH);
+        sortedRanks.add(LottoRank.FOURTH);
+        sortedRanks.add(LottoRank.THIRD);
+        sortedRanks.add(LottoRank.SECOND);
+        sortedRanks.add(LottoRank.FIRST);
+
+        // 정렬된 순서대로 결과 출력
+        for (LottoRank rank : sortedRanks) {
             String result = String.format("%d개 일치", rank.getMatchCount());
             if(rank.isBonusMatch()) {
                 result += ", 보너스 볼 일치";
@@ -21,5 +33,5 @@ public class LottoResultView {
     private String formatCurrency(long amount) {
         return String.format("%,d", amount);
     }
-
 }
+

00cd0a3932385bd222ce226af058deaa434dc217
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 14:45:27 +0900

    feat(LottoCalculateService) : getYieldOfLotto 매서드 생성

--- src/main/java/lotto/service/LottoCalculateService.java
+++ src/main/java/lotto/service/LottoCalculateService.java
@@ -1,11 +1,13 @@
 package lotto.service;
 
+import lotto.Constants;
 import lotto.domain.Lotto;
 import lotto.domain.User;
 import lotto.domain.UserLotto;
 import lotto.domain.Winning;
 import lotto.view.OutputView;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,6 +25,14 @@ public class LottoCalculateService {
         List<UserLotto> userLottos = user.getLottos();
         userLottos.stream()
                 .forEach(userLotto -> countLottoWinning(user, userLotto.getNumbers(), lotto));
+    }
+
+    public String getYieldOfLotto(User user, int totalPrice) {
+        if (user.getBuyingPrice() == 0) {
+            return "0.0";
+        }
+        DecimalFormat decimalFormat = new DecimalFormat(Constants.DECIMAL_FORMAT);
+        return decimalFormat.format((double) totalPrice / user.getBuyingPrice() * 100);
     }
 
 }

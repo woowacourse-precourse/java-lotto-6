3e3597b0fe177fb496ad51e92a156561dd0dbaf6
mdgome <mdgome3@gmail.com>
2023-11-08 16:08:35 +0900

    feat: 결과 출력 - 고정 메시지

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -1,12 +1,16 @@
 package lotto.view;
 
+import lotto.constant.OutputMessage;
 import lotto.model.LottoTicket;
 import lotto.model.Order;
 import lotto.model.WinStatistics;
 
 public class Output {
     public void buyLottoNumbers(LottoTicket lottoTicket, Order order) {
+        // 구매한 티켓 수 출력
+        System.out.println(OutputMessage.PURCHASED_COUNT_MESSAGE);
 
+        // 구매한 티켓 번호 출력
     }
 
     public void printProfitRate(Order order, WinStatistics winStatistics) {
@@ -14,7 +18,8 @@ public class Output {
     }
 
     public void winStatistics(WinStatistics winStatistics) {
-
+        System.out.println(OutputMessage.WINNING_STATISTICS_TITLE);
+        // 등수별 개수 출력
     }
 
 }

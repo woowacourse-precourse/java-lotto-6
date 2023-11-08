96bde1bf133ac0476d02360ad642087e64fa21dc
Yoonseol Lee <thisyoon97@gmail.com>
2023-11-08 14:01:57 +0900

    refactor: 구매가능금액 최대 2억으로 수정 int 자료형의 최대범위를 초과하지 않도록 2억으로 제한

--- src/main/java/lotto/utils/ErrorMessage.java
+++ src/main/java/lotto/utils/ErrorMessage.java
@@ -2,7 +2,7 @@ package lotto.utils;
 
 public enum ErrorMessage {
     INPUT_MONEY_BELOW_MIN_AMOUNT_ERROR("[ERROR] 최소 1000원을 입력해야합니다."),
-    INPUT_MONEY_ABOVE_MAX_AMOUNT_ERROR("[ERROR] 최대 100,000,000원을 입력해야합니다."),
+    INPUT_MONEY_ABOVE_MAX_AMOUNT_ERROR("[ERROR] 최대 200,000,000원을 입력해야합니다."),
     INPUT_MONEY_NOT_DIVISIBLE_ERROR("[ERROR] 1000원 단위의 값을 입력해야합니다."),
     INPUT_MONEY_NOT_INTEGER_ERROR("[ERROR] 정수만 입력 가능합니다."),
 

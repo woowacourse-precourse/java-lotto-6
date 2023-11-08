1527d895d16c119eefde2aaf6ec10561750bc21c
JGM <cnxw4570123@gmail.com>
2023-11-08 14:24:41 +0900

    feat(Lotto) : add Lotto default constructor and sortNumbersAscending 기본 생성자와 순서 정렬 메소드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,31 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.constant.Condition;
 
 public class Lotto {
     private final List<Integer> numbers;
 
+    public Lotto() {
+        this.numbers = Randoms.pickUniqueNumbersInRange(Condition.MIN_DRAW_NUMBER, Condition.MAX_DRAW_NUMBER,
+                Condition.SIX_WINNING_NUMBERS);
+        sortNumbersAscending();
+    }
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        sortNumbersAscending();
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
+        Procedure.DRAW_WINNING_NUMBERS.checkPossibleError(numbers);
     }
 
     // TODO: 추가 기능 구현
+    private void sortNumbersAscending(){
+        this.numbers.sort(Integer::compareTo);
+    }
+
 }

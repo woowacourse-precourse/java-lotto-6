f57cf33d4710736f19dc318a92aca7a9a01118f4
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 16:00:08 +0900

    [FEAT] OutputView에 pringLottos 함수 구현
    뽑은 로또를 프린트해주는 기능

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,7 +2,6 @@ package lotto.view;
 
 import java.util.List;
 import lotto.domain.Lotto;
-import lotto.domain.Lottos;
 
 public class OutputView {
     private static final String LOTTO_COUNT = "개를 구매했습니다.";
@@ -11,5 +10,12 @@ public class OutputView {
     public static void printLottoCount(int lottoCount) {
         System.out.println();
         System.out.println(lottoCount + LOTTO_COUNT);
+    }
+
+    public static void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+        System.out.println();
     }
 }

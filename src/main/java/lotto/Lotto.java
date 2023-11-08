28a58307d9fba25c4104db31cb24b6e4dd886464
JunRain <sa2313@tukorea.ac.kr>
2023-11-08 10:34:48 +0900

    오탈자 수정

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -10,7 +10,7 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> nㅈumbers) {
+    private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }

b814293c882dec23cfcaae1e5f84cd39df4d513f
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2023-11-08 14:07:50 +0900

    feat: 오름차순 정렬된 로또번호 얻는 기능 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class LottoNumber {
-    
+public class LottoNumber implements Comparable<LottoNumber> {
+
     private static final int MIN = 1;
     private static final int MAX = 45;
     private static final Map<Integer, LottoNumber> CACHE = new HashMap<>();
@@ -27,12 +29,19 @@ public class LottoNumber {
         return CACHE.get(rawValue);
     }
 
+
+    public static List<LottoNumber> getLottoNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(LottoNumber::of)
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
     private static void validate(int value) {
         if (value < MIN || value > MAX) {
             throw new IllegalArgumentException("[ERROR] 숫자의 범위는 1이상 45이하여야 합니다");
         }
     }
-
 
     @Override
     public boolean equals(Object object) {
@@ -48,6 +57,11 @@ public class LottoNumber {
     @Override
     public int hashCode() {
         return Objects.hash(value);
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return Integer.compare(value, o.value);
     }
 
 }
--- src/test/java/lotto/domain/LottoNumberTest.java
+++ src/test/java/lotto/domain/LottoNumberTest.java
@@ -3,28 +3,29 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoNumberTest {
 
-
-
-
-
     @DisplayName("LottoNumber 정상 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1,45,25})
-    void ofTest(int input){
+    @ValueSource(ints = {1, 45, 25})
+    void ofTest(int input) {
         assertThatNoException().isThrownBy(() -> LottoNumber.of(input));
     }
 
     @DisplayName("동등 객체 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {1,45,25})
-    void equalsTest(int input){
+    @ValueSource(ints = {1, 45, 25})
+    void equalsTest(int input) {
         LottoNumber lottoNumber1 = LottoNumber.of(input);
         LottoNumber lottoNumber2 = LottoNumber.of(input);
 
@@ -33,11 +34,32 @@ class LottoNumberTest {
 
     @DisplayName("범위 예외 테스트")
     @ParameterizedTest
-    @ValueSource(ints = {0,46,-1})
-    void validateTest(int input){
-        assertThatThrownBy(() ->LottoNumber.of(input))
+    @ValueSource(ints = {0, 46, -1})
+    void validateTest(int input) {
+        assertThatThrownBy(() -> LottoNumber.of(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 숫자의 범위는 1이상 45이하여야 합니다");
+    }
+
+    @DisplayName("로또 번호들 정상 테스트")
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void getLottoNumbersTest(List<Integer> numbers) {
+        assertThat(LottoNumber.getLottoNumbers(numbers).size()).isEqualTo(6);
+    }
+
+    static Stream<List<Integer>> dataProvider() {
+        return Stream.of(
+                List.of(4, 2, 3, 1, 5, 6),
+                List.of(11, 14, 15, 13, 16, 12)
+        );
+    }
+
+    @DisplayName("오름차순 정렬 테스트")
+    @Test
+    void ascendingSortTest() { //오름차순 정렬되어 동등객체 리스트이다
+        assertThat(LottoNumber.getLottoNumbers(List.of(4,2,3,1,6,5)))
+                .isEqualTo(LottoNumber.getLottoNumbers(List.of(1,2,3,4,5,6)));
     }
 
 }

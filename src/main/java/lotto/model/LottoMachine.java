92fbb5128c832edd1daff1d4838f2526f1be7b8a
jookbooin <3670lsh@naver.com>
2023-11-08 16:41:14 +0900

    refeat : lottoMachine 당첨 결과 출력 기능 추가

--- src/main/java/lotto/model/LottoMachine.java
+++ src/main/java/lotto/model/LottoMachine.java
@@ -4,19 +4,27 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
+import static lotto.message.LottoMessage.EXIST_BONUS_MESSAGE;
+import static lotto.message.LottoMessage.NOT_EXIST_BONUS_MESSEAGE;
+
 public class LottoMachine {
     private final Map<Rank, Integer> winningRecord;
 
     public LottoMachine() {
-        winningRecord = new EnumMap<Rank, Integer>(Rank.class);
+        winningRecord = new EnumMap<>(Rank.class);
         for (Rank value : Rank.values()) {
             winningRecord.put(value, 0);
         }
-
     }
 
-    public void winningResult(List<Lotto> lottos, WinningNumbers winningNumbers) {
+    public void showWinningResult(List<Lotto> lottos, WinningNumbers winningNumbers) {
 
+        calculateResult(lottos, winningNumbers);
+
+        printResult();
+    }
+
+    private void calculateResult(List<Lotto> lottos, WinningNumbers winningNumbers) {
         for (Lotto lotto : lottos) {
             Rank rank = winningNumbers.determineRank(lotto);
             updateWinningRecord(rank);
@@ -29,17 +37,28 @@ public class LottoMachine {
         winningRecord.put(rank, getRankValue(rank) + 1);
     }
 
-    public void print() {
-        for (Map.Entry<Rank, Integer> entry : winningRecord.entrySet()) {
-            Rank rank = entry.getKey();
-            int count = entry.getValue();
-            System.out.println(rank + ": " + count);
-        }
+    private void printResult() {
+        winningRecord.entrySet().stream().filter(entry -> entry.getKey() != Rank.PASS)
+                .forEach(entry -> {
+                    int winningCount = entry.getKey().getWinningCount();
+                    String reward = entry.getKey().rewardNumberFormat();
+                    int myWinningCount = entry.getValue();
+
+                    if (entry.getKey().isBonus()) {
+                        System.out.printf(EXIST_BONUS_MESSAGE.getMessage(), winningCount, reward, myWinningCount);
+                        System.out.println();
+                    }
+
+                    if (!entry.getKey().isBonus()) {
+                        System.out.printf(NOT_EXIST_BONUS_MESSEAGE.getMessage(), winningCount, reward, myWinningCount);
+                        System.out.println();
+                    }
+
+                });
     }
 
-    public int getRankValue(Rank rank){
+    public int getRankValue(Rank rank) {
         return winningRecord.get(rank);
     }
-
 
 }

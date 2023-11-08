ed0f26964c969d2c9f99d951c92b616583c81e3d
AidenRoh <colohrby@gmail.com>
2023-11-08 20:04:46 +0900

    feat: add main logic

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,17 @@
 package lotto;
 
+import lotto.controller.GameController;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		InputView inputView = new InputView();
+		OutputView outputView = new OutputView();
+		LottoService lottoService = new LottoService();
+
+		GameController gameController = new GameController(inputView, outputView, lottoService);
+		gameController.runGame();
+	}
 }

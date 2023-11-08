e55cc710f4f94e7fe5dafae73973652a64f412ff
JeongHunHui <deca3248@gmail.com>
2023-11-08 21:19:32 +0900

    feat: 로또의 상금의 정보를 가지고 있는 LottoPrize Enum 클래스 생성

---
+++ src/main/java/lotto/constant/LottoPrize.java
@@ -0,0 +1,43 @@
+package lotto.constant;
+
+public enum LottoPrize {
+    MATCH_3(3, false, 5000),
+    MATCH_4(4, false, 50000),
+    MATCH_5(5, false, 1500000),
+    MATCH_5_BONUS(5, true, 30000000),
+    MATCH_6(6, false, 2000000000);
+
+    private final Integer correctCount;
+    private final Boolean isBonusCorrect;
+    private final Integer prize;
+
+    LottoPrize(Integer correctCount, Boolean isBonusCorrect, Integer prize) {
+        this.correctCount = correctCount;
+        this.isBonusCorrect = isBonusCorrect;
+        this.prize = prize;
+    }
+
+    public Integer getCorrectCount() {
+        return correctCount;
+    }
+
+    public Boolean getIsBonusCorrect() {
+        return isBonusCorrect;
+    }
+
+    public Integer getPrize() {
+        return prize;
+    }
+
+    public Boolean canGetPrize(Integer correctCount, Boolean isBonusCorrect) {
+        if (this.isBonusCorrect) {
+            return isCorrectCountEqual(correctCount) && isBonusCorrect;
+        }
+        return isCorrectCountEqual(correctCount);
+    }
+
+    private Boolean isCorrectCountEqual(Integer correctCount) {
+        return this.correctCount == correctCount;
+    }
+}
+

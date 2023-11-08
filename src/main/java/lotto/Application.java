049477cfdf196266ca6b43d0957dc114bfc93df3
Henry-Cha <sungwon4728@naver.com>
2023-11-08 10:20:42 +0900

    feat: main 프로그램 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = InputView.getInstance();
+        OutputView outputView = OutputView.getInstance();
+        LottoController lottoController = LottoController.getInstance(inputView,outputView);
+        lottoController.run();
     }
 }

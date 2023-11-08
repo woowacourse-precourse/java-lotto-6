51d45cd6fa52f1f2433c32d93dae7c7465479466
MyStraw <kikongdosa@gmail.com>
2023-11-08 09:28:12 +0900

    Feat: 추가 보너스 번호의 범위 검사
    게터도 추가

--- src/main/java/lotto/WinningLotto.java
+++ src/main/java/lotto/WinningLotto.java
@@ -13,5 +13,17 @@ public class WinningLotto {
         if (lotto.getNumbers().contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 번호가 당첨 번호와 중복이에요");
         }
+
+        if (bonusNumber <1 || bonusNumber > 45){
+            throw new IllegalArgumentException("보너스 번호가 범위를 벗어났습니다.");
+        }
+    }
+
+    public Lotto getLotto() {
+        return lotto;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
     }
 }

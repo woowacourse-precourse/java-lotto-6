912f7dc74799d1129aa5e99506fde39f0eabcac3
ASak1104 <khw56184@gmail.com>
2023-11-08 09:33:43 +0900

    feat: 로또 게임 초기 설정 기능 추가

--- src/main/java/lotto/game/LottoGame.java
+++ src/main/java/lotto/game/LottoGame.java
@@ -1,23 +1,53 @@
 package lotto.game;
 
 import java.util.List;
+import java.util.Objects;
 import lotto.model.Lotto;
 import lotto.util.LottoGameConsole;
 
 public class LottoGame {
     private int cost = Integer.MIN_VALUE;
-    private LottoGame target;
+    private int bonusNumber = Integer.MIN_VALUE;
+    private Lotto target;
     private List<Lotto> lottos;
 
     public void init() {
         while (cost == Integer.MIN_VALUE) {
             initCost();
         }
+
+        while (Objects.isNull(target)) {
+            initTarget();
+        }
+
+        while (bonusNumber == Integer.MIN_VALUE) {
+            initBonusNumber();
+        }
     }
 
     private void initCost() {
         try {
             cost = LottoGameConsole.readCost();
+        } catch (NumberFormatException e) {
+            LottoGameConsole.print(LottoGameMessage.NON_NUMBER);
+        } catch (IllegalArgumentException e) {
+            LottoGameConsole.print(LottoGameMessage.INVALID_COST_UNIT);
+        }
+    }
+
+    private void initTarget() {
+        try {
+            target = LottoGameConsole.readTargetLotto();
+        } catch (NumberFormatException e) {
+            LottoGameConsole.print(LottoGameMessage.NON_NUMBER);
+        } catch (IllegalArgumentException e) {
+            LottoGameConsole.print(LottoGameMessage.INVALID_LOTTO_NUMBERS);
+        }
+    }
+
+    private void initBonusNumber() {
+        try {
+            bonusNumber = LottoGameConsole.readBonusNumber();
         } catch (NumberFormatException e) {
             LottoGameConsole.print(LottoGameMessage.NON_NUMBER);
         } catch (IllegalArgumentException e) {

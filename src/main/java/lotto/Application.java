64ff884f3908405d8095ed082f027eb377b5a0fd
flame-possible <werq2003@naver.com>
2023-11-08 22:30:17 +0900

    feat : Main이 Controller 기능을 수행하도록 최종 완성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,38 @@
 package lotto;
 
+import static lotto.domain.CalEarn.CalEarn;
+import static lotto.domain.CompareNumber.CompareNumbers;
+import static lotto.domain.MakeNumber.MakeRandom;
+
+import java.util.HashMap;
+import java.util.Map;
+import lotto.domain.Input;
+import lotto.domain.MakeNumber;
+import lotto.domain.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Ranking;
+
 public class Application {
+    private static Long price = 0L;
+    private static int bonus_number = 0;
+    private static List<Lotto> lottos = new ArrayList<>();
+    private static Map<Ranking, Integer> result = new HashMap<>();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Output.PrintInputPrice();
+        price = Input.InputPrice();
+        MakeRandom(lottos, price);
+
+        Output.PrintInputWinnerNumber();
+        Lotto winner_lotto = new Lotto(Input.InputWinnerNumber());
+
+        Output.PrintInputBonusNumber();
+        bonus_number = Input.InputBonusNumber(winner_lotto.getNumbers());
+
+        result = CompareNumbers(lottos, winner_lotto, bonus_number);
+        Output.PrintResult(result);
+
+        CalEarn(result, price);
     }
 }

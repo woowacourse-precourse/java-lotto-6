1f6d6f16006b8564af40d0a5194e8c4dae24d683
styughjvbn <3338761@naver.com>
2023-11-08 17:35:17 +0900

    <refact> : 출력 안내 메시지 상수화 ------------------------------ 1. 출력 안내 메시지 상수화

--- src/main/java/Constant/ViewMessage.java
+++ src/main/java/Constant/ViewMessage.java
@@ -3,14 +3,15 @@ package Constant;
 public enum ViewMessage {
     PRINT_PURCHASE_RESULT("%d개를 구매했습니다.\n"),
     PRINT_WINNING_STATISTICS("당첨 통계\n---\n"),
+    PRINT_RATE_OF_RETURN("총 수익률은 %s%%입니다."),
     ASK_PURCHASE_AMOUNT("구입금액을 입력해 주세요."),
     ASK_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
     ASK_BONUS_NUMBER("보너스 번호를 입력해 주세요.");
 
     private final String message;
 
-    ViewMessage(String message){
-        this.message=message;
+    ViewMessage(String message) {
+        this.message = message;
     }
 
     public String getMessage() {
--- src/main/java/view/OutPutView.java
+++ src/main/java/view/OutPutView.java
@@ -1,20 +1,17 @@
 package view;
 
 import Constant.Rank;
+import Constant.ViewMessage;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Map;
 import model.Lotto;
 import model.MyLotto;
 
 public class OutPutView {
 
-    private static final String PRINT_PURCHASE_RESULT = "%d개를 구매했습니다.\n";
-    private static final String PRINT_WINNING_STATISTICS = "당첨 통계\n---\n";
-    private static final String PRINT_RATE_OF_RETURN = "총 수익률은 %s%%입니다.";
 
     public static void printPurchaseResult(MyLotto myLotto) {
-        System.out.printf(PRINT_PURCHASE_RESULT, myLotto.getMyLotto().size());
+        System.out.printf(ViewMessage.PRINT_PURCHASE_RESULT.getMessage(), myLotto.getMyLotto().size());
         for (Lotto lotto : myLotto.getMyLotto()) {
             System.out.println(lotto.getSortedNumbers());
         }
@@ -22,7 +19,7 @@ public class OutPutView {
     }
 
     public static void printWinningStatistics(Map<Rank, Integer> winningDetails) {
-        System.out.print(PRINT_WINNING_STATISTICS);
+        System.out.print(ViewMessage.PRINT_WINNING_STATISTICS.getMessage());
         for (Rank rank : Rank.values()) {
             System.out.printf(rank.getDescription(), winningDetails.get(rank));
             System.out.println();
@@ -30,8 +27,7 @@ public class OutPutView {
     }
 
     public static void printRateOfReturn(double rateOfReturn) {
-        NumberFormat numberFormat = new DecimalFormat("###,###.0");
-        String str = numberFormat.format(rateOfReturn);
-        System.out.printf(PRINT_RATE_OF_RETURN, str);
+        String fomattedString = new DecimalFormat("###,###.0").format(rateOfReturn);
+        System.out.printf(ViewMessage.PRINT_RATE_OF_RETURN.getMessage(), fomattedString);
     }
 }

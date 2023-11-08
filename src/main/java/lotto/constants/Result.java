cab3b7c7ffc138bd9c38c804180132f2edd638e3
dj <fjzl12@naver.com>
2023-11-08 17:02:10 +0900

    refactor(Result): 패키지 변경

--- src/main/java/lotto/constants/Result.java
+++ src/main/java/lotto/constants/Result.java
@@ -1,15 +1,17 @@
-package lotto.domain;
+package lotto.constants;
 
-public enum ResultMessage {
+public enum Result {
     THREE_MATCH("3개 일치 (5,000원) - %d개\n"),
     FOUR_MATCH("4개 일치 (50,000원) - %d개\n"),
     FIVE_MATCH("5개 일치 (1,500,000원) - %d개\n"),
     BONUS_MATCH("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"),
     SIX_MATCH("6개 일치 (2,000,000,000원) - %d개\n");
-    private String message;
-    ResultMessage(String message) {
+    private final String message;
+
+    Result(String message) {
         this.message = message;
     }
+
     public String getMessage() {
         return message;
     }

f1e8af3774737299a297aac6d7387950fd52628f
GiJung <weeds2577@gmail.com>
2023-11-08 19:06:32 +0900

    test(LottoTest): 책임의 변경으로 필요없는 테스트 삭제

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -52,17 +52,4 @@ public class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ErrorCode.INVALID_NUMBER_RANGE.getMessage());
     }
-
-    @DisplayName("로또 번호는 오름차순으로 정렬되어야 한다.")
-    @Test
-    void sortedLottoNumber() {
-
-        List<Integer> lottoNumbers = new ArrayList<>(List.of(6, 4, 5, 1, 2, 3));
-        List<Integer> sortedLottoNumbers = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
-
-        Lotto lotto = new Lotto(lottoNumbers);
-
-        assertThat(lotto.getLotto()).isEqualTo(sortedLottoNumbers);
-    }
-
 }

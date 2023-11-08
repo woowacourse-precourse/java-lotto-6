4d91456428821460041c330cb60ff01e989c348c
PeterChoi <94peterchoi@gmail.com>
2023-11-08 21:49:24 +0900

    feat: Lotto 클래스 단위테스트 추가로 작성
    - 로또 번호의 유효 범위 테스트
    - 당첨된 로또 번호 개수 테스트
    - 특정 숫자가 로또 번호에 포함되어 있는지 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -19,10 +20,32 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 유효 범위를 벗어나면 예외가 발생한다.")
+    @Test
+    void createLottoWithInvalidNumberRange() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 49)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호와 일치하는 로또 번호의 개수를 계산한다.")
+    @Test
+    void countMatchingNumbers() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int matchCount = lotto.countMatchingNumbers(List.of(4, 5, 6, 7, 8, 9));
+        assertThat(matchCount).isEqualTo(3);
+    }
+
+    @DisplayName("특정 숫자가 로또 번호 리스트에 포함되어 있는지 확인한다.")
+    @Test
+    void containNumber() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.containNumber(3)).isTrue();
+        assertThat(lotto.containNumber(7)).isFalse();
+    }
 }

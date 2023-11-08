ddefc1a99cbc32999a5e19a90fdb9c05013f2d19
20191777 <20191777@o365.hanbat.ac.kr>
2023-11-08 23:20:12 +0900

    FIX: 확률연산 오류 수정 - 확률이 잘못연산되는 것을 수정

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import lotto.domain.Buyer;
 import lotto.domain.Lotto;
 import lotto.domain.LottoResult;
+import lotto.domain.Rank;
 import lotto.service.Service;
 import lotto.view.View;
 
@@ -18,6 +19,7 @@ public class Controller {
         watchPurchasedLotteries();
         LottoResult lottoResult = new LottoResult();
         List<Integer> count = new ArrayList<>();
+
         for (int i = 0; i < buyer.getCount(); i++) {
             int cnt = 0;
             for (int number : lottoResult.getWinningNumbers()) {
@@ -36,7 +38,12 @@ public class Controller {
         System.out.printf("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개 \n", Collections.frequency(count, 5));
         System.out.printf("6개 일치 (2,000,000,000원) - %d개 \n", Collections.frequency(count, 6));
         System.out.println("---");
-        System.out.printf("총 수익률은 %.1f", 62.5);
+        System.out.println(buyer.getPrice());
+        double num = (double)(Collections.frequency(count, 3)*5000 + Collections.frequency(count, 4)*50000 + Collections.frequency(count, 5) * 1500000
+                + Collections.frequency(count, 5) * 30000000 + Collections.frequency(count, 6)*2000000000) / buyer.getPrice() * 100;
+        String p = String.format("%.1f", num);
+        System.out.print("총 수익률은 ");
+        System.out.print(p);
         System.out.println("%입니다.");
         }
 

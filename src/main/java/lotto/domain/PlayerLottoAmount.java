8c8c47daf4d4985ee5ef5b4c8b1f89376e960a97
noah <jeonghogim318@gmail.com>
2023-11-08 23:56:47 +0900

    feat: 게임실행횟수만큼 실행

--- src/main/java/lotto/domain/PlayerLottoAmount.java
+++ src/main/java/lotto/domain/PlayerLottoAmount.java
@@ -1,2 +1,19 @@
-package lotto.domain;public class PlayerLottoAmount {
+package lotto.domain;
+
+import lotto.view.ExceptionMessage;
+
+public class PlayerLottoAmount {
+    private static final int LOTTO_MIN_AMOUNT = 1000;
+    private final int amount;
+
+    public PlayerLottoAmount(String amount) {
+        int amountNum = validateNumber(amount);
+        validateAmount(amountNum);
+        this.amount = amountNum;
+    }
+
+    public int calculateLottoCount() {
+        return amount / LOTTO_MIN_AMOUNT;
+    }
+
 }

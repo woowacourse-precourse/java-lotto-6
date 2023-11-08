a53d0920bd9147349c86b8c3d00e780b21c762f9
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:53:23 +0900

    feat : validateBonusNumber 함수 추가
    lotto와 달리 bonusNumber는 따로 유효성 검사를 하지 않았기 때문에 도메인 유효성 검사를 위해 해당 함수를 추가한다.

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -9,4 +9,14 @@ public class WinningLotto {
         this.bonusNumber = bonusNumber;
         validateBonusNumber(bonusNumber);
     }
+
+    private void validateBonusNumber(int bonusNumber) {
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다");
+        }
+        if (lotto.getNumbers().contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+    }
+    
 }

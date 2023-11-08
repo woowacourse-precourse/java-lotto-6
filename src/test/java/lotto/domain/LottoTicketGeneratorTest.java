0d76feeff111b4d3dc11ae208c40e05d92a0710b
HYOAN <aspqll@naver.com>
2023-11-08 23:29:21 +0900

    feat(lotto): 로또  티켓 메서드 테스트 코드 구현

--- src/test/java/lotto/domain/LottoTicketGeneratorTest.java
+++ src/test/java/lotto/domain/LottoTicketGeneratorTest.java
@@ -1,4 +1,42 @@
+package lotto.domain;
+
+import lotto.LottoTicket;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LottoTicketGeneratorTest {
-  
+    private LottoTicketGenerator lottoTicketGenerator;
+
+    @BeforeEach
+    void setUp() {
+        lottoTicketGenerator = new LottoTicketGenerator();
+    }
+
+    @Test
+    void 티켓_수_확인() {
+        List<LottoTicket> tickets = lottoTicketGenerator.generateLottoTicket(6000);
+        assertEquals(6, tickets.size()); // 티켓 수 확인
+
+    }
+
+    @Test
+    void 랜덤_로또_번호_테스트() {
+        List<Integer> numbers = lottoTicketGenerator.generateLottoNumbers();
+        assertEquals(6, numbers.size()); // 발행된 로또 번호가 6개인지 확인
+
+        for (int number : numbers) { // 1~45 사이의 숫자인지 확인
+            assertTrue(number >= 1 && number <= 45);
+        }
+
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers); // 중복된 숫자가 있는지 확인
+        assertEquals(numbers.size(), uniqueNumbers.size());
+
+    }
 }

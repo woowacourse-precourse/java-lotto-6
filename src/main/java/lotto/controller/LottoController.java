e06440ff7a8a5116387379363b5d07f71d9433de
shining013 <nevertheless113@naver.com>
2023-11-08 15:17:21 +0900

    feat: LottoController 컨트롤러
    -당첨 번호 인풋 세팅
    -보너스 번호 인풋 세팅
    -당첨 통계 산출
    -수익률 계산
    
    기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,10 +1,14 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.Random;
+import lotto.Constants;
 import lotto.model.Lotto;
 import lotto.view.OutputView;
 import lotto.model.Quantity;
 import lotto.model.RandomLotto;
+
+import lotto.Prize;
 
 import lotto.view.OutputView;
 
@@ -14,27 +18,35 @@ public class LottoController {
         private int quantity;
         private List<List<Integer>> randomNumbers;
         private List<Integer> lottoNumbers;
+        private int bonusNumber;
 
         public LottoController() {
+                setStart();
+                showStart();
                 set();
-                conduct();
-                showResult();
+                calculate();
         }
 
-        public void set() {
+        public void setStart() {
                 quantity = InputController.setQuantity();
                 randomNumbers = InputController.setRandomNumbers(quantity);
         }
 
-        public void conduct() {
+        public void showStart() {
                 OutputView.printQuantity(quantity);
                 OutputView.printRandomLotto(randomNumbers);
         }
 
-        public void showResult() {
-
+        public void set() {
+                lottoNumbers = InputController.setLottoNumbers();
+                bonusNumber = InputController.setBonusNumber(lottoNumbers);
+                Lotto lotto = new Lotto(lottoNumbers);
         }
 
-
-
+        public void calculate() {
+                lotto.getWinningData(randomNumbers,bonusNumber);
+                lotto.calculatePrize();
+                lotto.calculateCost();
+                lotto.calculateReturn();
+        }
 }

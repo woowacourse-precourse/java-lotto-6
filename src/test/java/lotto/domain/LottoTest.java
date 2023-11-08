2912610283e90582a9f2dc0b27fef272264fa407
kmw2378 <rlarla677@gmail.com>
2023-11-08 20:38:48 +0900

    rename(lotto): LottoTest 패키지 이동 기존 Lotto 패키지가 이동됨에 따라 Test 메서드도 이동하였습니다.

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,11 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
+import static lotto.exception.errorcode.InputErrorCode.DUPLICATED_LOTTO_NUMBER;
+import static lotto.exception.errorcode.InputErrorCode.INVALID_LOTTO_NUMBER_SIZE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -13,16 +13,15 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_LOTTO_NUMBER_SIZE.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DUPLICATED_LOTTO_NUMBER.getMessage());
     }
-
-    // 아래에 추가 테스트 작성 가능
 }

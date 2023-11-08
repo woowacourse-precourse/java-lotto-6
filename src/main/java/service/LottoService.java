50e4e48696bb467b300c3f5b8bf0852d83d493e9
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 18:17:05 +0900

    feat : '서비스 계층 추가'

---
+++ src/main/java/service/LottoService.java
@@ -0,0 +1,25 @@
+package service;
+
+import domain.Lotto;
+import domain.Place;
+import repository.LottoRepository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoService {
+    private LottoRepository lottoRepository;
+
+    public LottoService(int size){
+        lottoRepository = new LottoRepository(size);
+    }
+    public void addLotto(Lotto lotto){
+        lottoRepository.add(lotto);
+    }
+
+    public Map<Place,Integer> numberOfWins(Lotto winningLotto,int bonusNumber){
+        lottoRepository.updateAllLottoPlace(winningLotto,bonusNumber);
+        return lottoRepository.findAllPlace();
+    }
+}

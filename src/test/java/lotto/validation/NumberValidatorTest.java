c4b8b9e1d3f8584a9a03e37e6d76629ca078a769
박승민 <tmddudals369@naver.com>
2023-11-08 14:20:59 +0900

    feat(numberValidator): 번호의 값이 정상 범위인지 검증하는 기능 테스트

--- src/test/java/lotto/validation/NumberValidatorTest.java
+++ src/test/java/lotto/validation/NumberValidatorTest.java
@@ -50,4 +50,36 @@ class NumberValidatorTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("[ERROR] 번호는 공백이 포함될 수 없습니다.");
     }
+
+    @Test
+    @DisplayName("번호의 값이 1보다 작으면 예외를 발생시킨다.")
+    void throwExceptionNumberLess1() {
+        //given
+        int number = 0;
+        //when //then
+        assertThatThrownBy(() -> NumberValidator.validateInRangeNumber(number))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 번호는 1이상 45이하의 수만 입력할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("번호의 값이 45보다 크면 예외를 발생시킨다.")
+    void throwExceptionNumberGreater45() {
+        //given
+        int number = 46;
+        //when //then
+        assertThatThrownBy(() -> NumberValidator.validateInRangeNumber(number))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 번호는 1이상 45이하의 수만 입력할 수 있습니다.");
+    }
+
+    @Test
+    @DisplayName("번호의 값이 1이상 45이하이면 예외를 발생시키지 않는다.")
+    void notThrowExceptionNumberGreaterThanEqual1AndLessThanEqual45() {
+        //given
+        int number = 1;
+        //when //then
+        assertThatCode(() -> NumberValidator.validateInRangeNumber(number))
+            .doesNotThrowAnyException();
+    }
 }

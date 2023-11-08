e0ecdb68b3137a2a599dbdf571ba9ec4c941636e
seondays <agseon9@gmail.com>
2023-11-08 13:31:45 +0900

    refactor(InformationMessage): 결과 메시지 안내 상수 enum으로 분리

---
+++ src/main/java/lotto/utils/InformationMessage.java
@@ -0,0 +1,18 @@
+package lotto.utils;
+
+public enum InformationMessage {
+    BUY("%n%d개를 구매했습니다.%n"),
+    PROFIT("총 수익률은 %.1f%%입니다."),
+    MATCH_NUMBER("%d개 일치 (%s원) - %d개%n"),
+    MATCH_NUMBER_BONUS("%d개 일치, 보너스 볼 일치 (%s원) - %d개%n");
+
+    private final String message;
+
+    InformationMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}

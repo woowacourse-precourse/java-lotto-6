5a701a3b17c54e916ed0c3f679166b6a3a07f02b
Hoony0321 <eogns0321@gmail.com>
2023-11-08 14:48:51 +0900

    refactor : Rank message 필드 삭제
    추후에 당첨 통계를 출력하는 과정에서 사용하려고 설정했지만, 해당 필드 없이 출력이 가능해서 삭제함.

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,21 +1,19 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)"),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
-    FOURTH(4, 50000, "4개 일치 (50,000원)"),
-    FIFTH(3, 5000, "3개 일치 (5,000원)"),
-    NONE(0, 0, "");
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000),
+    NONE(0, 0);
 
     private final int matchCount;
     private final int winningMoney;
-    private final String message;
 
-    Rank(int matchCount, int winningMoney, String message) {
+    Rank(int matchCount, int winningMoney) {
         this.matchCount = matchCount;
         this.winningMoney = winningMoney;
-        this.message = message;
     }
 
     public static Rank valueOf(int matchCount, boolean isMatchBonus) {

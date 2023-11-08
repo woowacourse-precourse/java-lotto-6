3c59a16d87e6e78895e8dc9d8996a1257dbd0e46
Bosub Kim <grapeflavoredapple@gmail.com>
2023-11-08 14:09:07 +0900

    feat: Lotto 클래스 기능 구현
    - 생성시 검증 로직 추가
    - 불별 데이터를 제공하는 함수 구현
    - 받은 리스트에 대해 본인에 일치하는 숫자가 존재하는지 알려주는 함수 구현
    - 넘겨 받은 리스트와 일치하는 숫자 개수를 리턴하는 함수 추가
    - 본인인의 정보를 출력해주는 메서드 추가

--- src/main/java/lotto/model/lotto/Lotto.java
+++ src/main/java/lotto/model/lotto/Lotto.java
@@ -3,9 +3,13 @@ package lotto.model.lotto;
 import static lotto.exception.ExceptionMessage.LOTTO_NUMBER_COUNT_ERROR;
 import static lotto.exception.ExceptionMessage.LOTTO_NUMBER_OUT_OF_RANGE;
 import static lotto.exception.ExceptionMessage.WINNING_NUMBER_DUPLICATION;
+import static lotto.utils.constant.IOConstant.LOTTO_POSTFIX;
+import static lotto.utils.constant.IOConstant.LOTTO_PREFIX;
+import static lotto.utils.constant.IOConstant.OUTPUT_DELIMITER;
 import static lotto.utils.constant.LottoConstant.LOTTO_NUMBER_MAX;
 import static lotto.utils.constant.LottoConstant.LOTTO_NUMBER_MIN;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -28,8 +32,25 @@ public class Lotto {
         numbers.forEach(this::checkNumberInRange);
     }
 
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
     public boolean isNumberExist(int number) {
         return numbers.contains(number);
+    }
+
+    public String printLottoInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LOTTO_PREFIX.getValue());
+        for (int number : numbers) {
+            sb.append(number)
+                    .append(OUTPUT_DELIMITER.getValue());
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(LOTTO_POSTFIX.getValue());
+        return sb.toString();
     }
 
     private void checkNumberInRange(int number) {

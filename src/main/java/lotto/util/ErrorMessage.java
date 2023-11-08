b8d7f272aa7cba9f7e267ae7dbe2daea6bafe188
Jaewon <choijawon99@gmail.com>
2023-11-08 14:36:20 +0900

    feat(validator): 당첨 번호 입력 예외 추가
    - 문자 입력시 예외처리

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -3,24 +3,23 @@ package lotto.util;
 public enum ErrorMessage {
 
     IS_EMPTY("값을 입력해 주세요."),
-    IS_NOT_NUMBER("투입 금액은 숫자여야 합니다."),
-    IS_NOT_DIVIDED_BY_PRICE("로또 금액 단위로 금액을 투입해 주세요!"),
+    IS_NOT_NUMBER("숫자를 입력해 주세요."),
+    IS_NOT_DIVIDED_BY_PRICE("로또 금액 단위로 금액을 투입해 주세요."),
     IS_NOT_POSITIVE_NUMBER("투입 금액은 음수가 될 수 없습니다."),
     IS_NOT_OVER_MIN_PRICE("로또를 구매할 돈이 부족합니다."),
     IS_NOT_LAST_INDEX("입력 형태를 확인해 주세요 [입력 예시 : 1,2,3,4,5,6]"),
     IS_NOT_LOTTO_SIZE("로또는 6개의 숫자가 필요합니다."),
     IS_NOT_LOTTO_NUMBER_RANGE("로또 번호는 1 ~ 45 사이의 숫자 이어야 합니다."),
-    IS_NOT_UNIQUE_LOTTO_NUMBER("로또 번호는 서로 중복될 수 없습니다.")
-    ;
+    IS_NOT_UNIQUE_LOTTO_NUMBER("로또 번호는 서로 중복될 수 없습니다.");
 
     private static final String ERROR_MESSAGE = "[ERROR] ";
     private final String message;
 
-    ErrorMessage(String message){
+    ErrorMessage(String message) {
         this.message = message;
     }
 
-    public String getMessage(){
+    public String getMessage() {
         return ERROR_MESSAGE + message;
     }
 }
--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -10,7 +10,7 @@ public class Validator {
     private final static int MIN_LOTTO_NUM = 1;
     private final static int MAX_LOTTO_NUM = 45;
 
-    public void check_InputMoney(String input) throws IllegalArgumentException{
+    public void check_InputMoney(String input) throws IllegalArgumentException {
         check_Empty(input);
         check_NotNumber(input);
 
@@ -21,44 +21,44 @@ public class Validator {
         check_NotDividedByPrice(money);
     }
 
-    private void check_Empty(String input){
-        if(input.isEmpty()){
+    private void check_Empty(String input) {
+        if (input.isEmpty()) {
             throw new IllegalArgumentException(ErrorMessage.IS_EMPTY.getMessage());
         }
     }
 
-    private void check_NotNumber(String input){
-        try{
+    private void check_NotNumber(String input) {
+        try {
             Integer.parseInt(input);
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMessage());
         }
     }
 
-    private void check_NotDividedByPrice(int money){
-        if(money % LOTTO_PRICE != LOTTO_PRICE_MIN){
+    private void check_NotDividedByPrice(int money) {
+        if (money % LOTTO_PRICE != LOTTO_PRICE_MIN) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_DIVIDED_BY_PRICE.getMessage());
         }
     }
 
-    private void check_PositiveNumber(int money){
-        if(money < LOTTO_PRICE_MIN){
+    private void check_PositiveNumber(int money) {
+        if (money < LOTTO_PRICE_MIN) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_POSITIVE_NUMBER.getMessage());
         }
     }
 
-    private void check_OverMinLottoPrice(int money){
-        if(money < LOTTO_PRICE){
+    private void check_OverMinLottoPrice(int money) {
+        if (money < LOTTO_PRICE) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_OVER_MIN_PRICE.getMessage());
         }
     }
 
-    public void check_InputWinLotto(String input) throws IllegalArgumentException{
+    public void check_InputWinLotto(String input) throws IllegalArgumentException {
         check_Empty(input);
         check_LastIndex(input);
 
         List<String> stringList = GameUtil.converseStringToStringList(input);
-        //문자 입력시
+        check_LottoNumberInteger(stringList);
 
         List<Integer> integerList = GameUtil.converseStringListToIntegerList(stringList);
         check_LottoNumberSize(integerList);
@@ -66,45 +66,62 @@ public class Validator {
         check_LottoNumberUnique(integerList);
     }
 
-    private void check_LastIndex(String input){
-        if(input.lastIndexOf(",") == input.length() - 1){
+    private void check_LastIndex(String input) {
+        if (input.lastIndexOf(",") == input.length() - 1) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_LAST_INDEX.getMessage());
         }
     }
 
-    private void check_LottoNumberSize(List<Integer> input){
-        if(input.size() != LOTTO_NUM){
+    private void check_LottoNumberInteger(List<String> input){
+        if(!isInteger(input)){
+            throw new IllegalArgumentException(ErrorMessage.IS_NOT_NUMBER.getMessage());
+        }
+    }
+
+    private boolean isInteger(List<String> input){
+        for(String num : input){
+            try{
+                Integer.parseInt(num);
+            }catch(NumberFormatException e){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void check_LottoNumberSize(List<Integer> input) {
+        if (input.size() != LOTTO_NUM) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_LOTTO_SIZE.getMessage());
         }
     }
 
-    private void check_LottoNumberRange(List<Integer> input){
-        if(!isAllValidLottoNumberRange(input)){
+    private void check_LottoNumberRange(List<Integer> input) {
+        if (!isAllValidLottoNumberRange(input)) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_LOTTO_NUMBER_RANGE.getMessage());
         }
     }
 
-    private static boolean isAllValidLottoNumberRange(List<Integer> input){
+    private static boolean isAllValidLottoNumberRange(List<Integer> input) {
         return input.stream()
                 .allMatch(Validator::isValidLottoNumberRange);
     }
 
-    private static boolean isValidLottoNumberRange(int number){
+    private static boolean isValidLottoNumberRange(int number) {
         return (number >= MIN_LOTTO_NUM) && (number <= MAX_LOTTO_NUM);
     }
 
-    private void check_LottoNumberUnique(List<Integer>input){
-        if(isDuplicate(input)){
+    private void check_LottoNumberUnique(List<Integer> input) {
+        if (isDuplicate(input)) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_UNIQUE_LOTTO_NUMBER.getMessage());
         }
     }
 
-    private static boolean isDuplicate(List<Integer> input){
+    private static boolean isDuplicate(List<Integer> input) {
         HashSet<Integer> hashSet = new HashSet<>(input);
         return hashSet.size() != input.size();
     }
 
-    public void check_BonusNum(String input) throws IllegalArgumentException{
+    public void check_BonusNum(String input) throws IllegalArgumentException {
         check_Empty(input);
         check_NotNumber(input);
 
@@ -113,8 +130,8 @@ public class Validator {
         check_BonusNumRange(bonusNum);
     }
 
-    private void check_BonusNumRange(int input){
-        if(!isValidLottoNumberRange(input)){
+    private void check_BonusNumRange(int input) {
+        if (!isValidLottoNumberRange(input)) {
             throw new IllegalArgumentException(ErrorMessage.IS_NOT_LOTTO_NUMBER_RANGE.getMessage());
         }
     }

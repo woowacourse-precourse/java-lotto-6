39819e19f13ff163398ca9e097a5e329ce15cb84
S-DPR <thankyouformuch@naver.com>
2023-11-08 19:36:24 +0900

    feat: validateNaturalNumber 메소드

--- src/main/java/lotto/Validation.java
+++ src/main/java/lotto/Validation.java
@@ -10,4 +10,15 @@ public class Validation {
             throw new IllegalArgumentException(ErrorMessage.NULL_ERROR.getMessage());
         }
     }
+
+    public static void validateNaturalNumber(String input) {
+        boolean isInteger = input.trim().matches("^[0-9]+$");
+        if (!isInteger || Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NATURAL_NUMBER.getMessage());
+        }
+    }
+
+    public static void validateNaturalNumber(List<String> input) {
+        input.forEach(Validation::validateNaturalNumber);
+    }
 }

89ae229e2309c25062fa25eec1bdf2fac4ad9443
leeseobin00 <leeseobin000709@gmail.com>
2023-11-08 17:04:18 +0900

    feat(BonusNumberValidator): Bonus Number 범위 검증 구현

--- src/main/java/lotto/util/validator/BonusNumberValidator.java
+++ src/main/java/lotto/util/validator/BonusNumberValidator.java
@@ -3,6 +3,9 @@ package lotto.util.validator;
 import lotto.util.exception.ErrorCode;
 
 public class BonusNumberValidator {
+    public static final int MIN_RANDOM_BOUND = 1;
+    public static final int MAX_RANDOM_BOUND = 45;
+
     private void validateNullorEmpty(String inputBonusNumber) {
         if (inputBonusNumber == null || inputBonusNumber.trim().isEmpty()) {
             throw new IllegalArgumentException(ErrorCode.NULL_OR_EMPTY_BONUS_NUMBER.getMessage());
@@ -14,6 +17,12 @@ public class BonusNumberValidator {
             return Integer.parseInt(inputBonusNumber);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorCode.INVALID_BONUS_NUMBER_TYPE.getMessage());
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < MIN_RANDOM_BOUND || number > MAX_RANDOM_BOUND) {
+            throw new IllegalArgumentException(ErrorCode.INVALID_BONUS_NUMBER_RANGE.getMessage());
         }
     }
 }

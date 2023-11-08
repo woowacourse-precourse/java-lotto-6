8ac111dce8801cf5be4e25a76f49fe275237d4d3
MinjuJi <hinote444@gmail.com>
2023-11-08 09:38:33 +0900

    test(inputvalidatortest): 구입금액이 천원 단위로 입력되었는지 검사하는 테스트 구현

---
+++ src/test/java/lotto/InputValidatorTest.java
@@ -0,0 +1,30 @@
+package lotto;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import validator.InputValidator;
+
+public class InputValidatorTest {
+    @DisplayName("구입금액이 1,000원 단위로 입력되었는지 검사")
+    @Test
+    void 구입금액_천_단위_테스트() {
+        // given
+        int case1 = 5_500;
+        int case2 = 5_000;
+
+        // when
+        Throwable result1 = catchThrowable(() -> {
+            InputValidator.validatePurchaseAmount(case1);
+        });
+        Throwable result2 = catchThrowable(() -> {
+            InputValidator.validatePurchaseAmount(case2);
+        });
+
+        // then
+        assertThat(result1).isInstanceOf(IllegalArgumentException.class);
+        assertThat(result2).doesNotThrowAnyException();
+    }
+}

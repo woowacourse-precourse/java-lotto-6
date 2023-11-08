49efac15eed028df06d47cfa1376fcb5243fa97f
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-08 13:33:44 +0900

    ViewProcess 구현

--- src/main/java/lotto/ExecProcess.java
+++ src/main/java/lotto/ExecProcess.java
@@ -9,6 +9,13 @@ public class ExecProcess {
 
     public void ViewProcess() {
         INPUT_VIEW.inputPrice();
+        System.out.println();
         OUTPUT_VIEW.printMyLotto();
+        System.out.println();
+        INPUT_VIEW.inputWinningNumber();
+        System.out.println();
+        INPUT_VIEW.inputWinningNumber();
+        System.out.println();
+        OUTPUT_VIEW.printStatistics();
     }
 }

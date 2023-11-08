9a6cc8da7e87ed9147558f6fef4fb4da2f03fe60
minseokKim6823 <alstjr6823@naver.com>
2023-11-08 21:28:23 +0900

    BubbleSortService 버블 정렬 테스트

--- src/test/java/lotto/generator/CalculateWinCountsTest.java
+++ src/test/java/lotto/generator/CalculateWinCountsTest.java
@@ -11,7 +11,6 @@ public class CalculateWinCountsTest {
     @DisplayName("로또 번호 조합의 당첨 번호 개수를 올바르게 계산한다.")
     @Test
     void testCalculateWinCounts() {
-        // Arrange
         List<List<Integer>> lottoNumberList = List.of(
                 List.of(1, 2, 3, 4, 5, 6),
                 List.of(7, 8, 9, 10, 11, 12),
@@ -20,10 +19,8 @@ public class CalculateWinCountsTest {
         List<Integer> lottoWinNumber = List.of(1, 2, 3, 4, 5, 6); // 당첨 번호
         int bonusNumber = 7; // 보너스 번호
 
-        // Act
         List<Integer> winCounts = CalculateWinCounts.calculateWinCounts(lottoNumberList, lottoWinNumber, bonusNumber);
 
-        // Assert
         assertThat(winCounts).containsExactly(6, 0, 6); // 당첨 번호 개수 확인
     }
 }

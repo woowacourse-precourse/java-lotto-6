18ede4fa7168336a0b80231faf7db499647b7f68
wschoe <chldntjr0425@naver.com>
2023-11-08 20:41:48 +0900

    feat: add issueTickets in LottoService

---
+++ src/main/java/lotto/lottosystem/bussiness/LottoService.java
@@ -0,0 +1,38 @@
+package lotto.lottosystem.bussiness;
+
+import lotto.lottosystem.presentation.printer.TicketsVO;
+import lotto.lottosystem.presentation.reader.MoneyVO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoService {
+
+    private static final int ONE_LOTTO_EXPENCE = 1000;
+    private final LottoGenerator lottoGenerator;
+
+    public LottoService(LottoGenerator lottoGenerator) {
+        this.lottoGenerator = lottoGenerator;
+    }
+
+    public TicketsVO issueTickets(MoneyVO moneyVO) {
+        List<List<Integer>> tickets = new ArrayList<>();
+        int money = moneyVO.money();
+        validateMoney(money);
+        int count = calcCount(money);
+        for(int i = 0; i < count; i++) {
+            tickets.add(lottoGenerator.generateLotto());
+        }
+        return new TicketsVO(tickets);
+    }
+
+    private void validateMoney(int money) {
+        if ((money % ONE_LOTTO_EXPENCE) != 0) {
+            throw new IllegalArgumentException("구입 금액은 1000원 단위여야 합니다.");
+        }
+    }
+
+    private int calcCount(int money) {
+        return (money / ONE_LOTTO_EXPENCE);
+    }
+}

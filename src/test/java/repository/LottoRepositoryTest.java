5635b0907fcd3e06eb897b1b1869fc68948dc883
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 18:10:03 +0900

    test: '로또 등수가 올바르게 업데이트 되는지 테스트'

--- src/test/java/repository/LottoRepositoryTest.java
+++ src/test/java/repository/LottoRepositoryTest.java
@@ -1,12 +1,15 @@
 package repository;
 
 import domain.Lotto;
+import domain.Place;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class LottoRepositoryTest {
     LottoRepository lottoRepository = new LottoRepository(3);
@@ -53,6 +56,35 @@ class LottoRepositoryTest {
 
         Assertions.assertThat(lottoRepository.findAll()).containsExactly(lotto1,lotto2,lotto3);
 
+    }
+
+    @Test
+    void 로또등수업데이트(){
+        Lotto correctNumber = new Lotto(List.of(1,2,3,4,5,6));
+        int bonusNumber = 7;
+
+        Lotto lotto1 = new Lotto(new ArrayList<>(List.of(1,2,3,4,5,6)));
+        Lotto lotto2 = new Lotto(new ArrayList<>(List.of(1,2,3,4,5,7)));
+        Lotto lotto3 = new Lotto(new ArrayList<>(List.of(1,2,3,6,7,8)));
+
+        lottoRepository.add(lotto1);
+        lottoRepository.add(lotto2);
+        lottoRepository.add(lotto3);
+
+        lottoRepository.updateAllLottoPlace(correctNumber,bonusNumber);
+
+        Map<Place,Integer> numberOfPlaceMap = new HashMap<>(){{
+            put(Place.FIRST,1);
+            put(Place.SECOND,1);
+            put(Place.THIRD,0);
+            put(Place.FORTH,0);
+            put(Place.FIFTH,1);
+            put(Place.NONE,0);
+        }};
+
+        Map<Place, Integer> checkingMap = lottoRepository.findAllPlace();
+
+        Assertions.assertThat(checkingMap).containsExactlyInAnyOrderEntriesOf(numberOfPlaceMap);
     }
 
 }

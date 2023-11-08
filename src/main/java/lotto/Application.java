1f2fbe42c5fccd88982730879fc0b2a0f22f1278
SWtheWhite <swthewhite@gmail.com>
2023-11-08 19:58:52 +0900

    feat: 기본 설정 - 로또 번호 범위 상수 설정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,12 @@
 package lotto;
 
 public class Application {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+
+
     }
 }

ad75962b37e5ed6cc067c43c5ffb864ea2f8b93c
limzzum <chfhddl1234@naver.com>
2023-11-08 21:55:32 +0900

    :sparkles: feat : 에러 메시지 추가

--- src/main/java/lotto/constant/ErrorMessage.java
+++ src/main/java/lotto/constant/ErrorMessage.java
@@ -2,6 +2,7 @@ package lotto.constant;
 
 public class ErrorMessage {
     public static final String INVALID_RANGE_LOTTO_NUM = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+    public static final String INVALID_REQUIRED_LOTTO_NUM = "[ERROR] 로또 당첨 번호는 6개 입력해야 합니다.";
     public static final String INVALID_DUPLICATE_LOTTO_NUM = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     public static final String INVALID_IS_NOT_INPUT = "[ERROR] 입력값이 존재하지 않습니다.";
     public static final String INVALID_NUMBER = "[ERROR] 1이상 2,147,483,647이하의 숫자만 입력 가능합니다.";

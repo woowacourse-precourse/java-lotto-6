c676baf29074fdd5101a1f56d00a2d85b2c3d487
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-08 17:14:36 +0900

    refactor($Applcation): UI와 매인 로직 상호작용 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,9 +9,12 @@ public class Application {
         LottoResultChecker lottoResultChecker = new LottoResultChecker();
         Buyer buyer = new Buyer(lottoMachine, lottoResultChecker);
 
-        buyer.buyLotto();
+        int price = InputProcessor.parsePrice(LottoUI.readLine("구매금액을 입력해 주세요."));
+        buyer.buyLotto(price);
 
-        buyer.drawWinningNumbersAndBonusNumber();
+        List<Integer> winningNumbers = InputProcessor.getWinningNumbers(LottoUI.readLine("구매금액을 입력해 주세요."));
+        Integer bonusNumber = InputProcessor.getBonusNumber(LottoUI.readLine("보너스 번호를 입력해 주세요."));
+        buyer.drawWinningNumbersAndBonusNumber(winningNumbers, bonusNumber);
         buyer.aggregateLotto();
     }
 }

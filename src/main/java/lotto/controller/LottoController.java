333605a36174eae7cb4aa9b1bbd53d400c2abd6b
Yoon Jiho <95673046+YoonJiho96@users.noreply.github.com>
2023-11-08 11:09:49 +0900

    refactor : Controller 동작 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -19,29 +19,34 @@ public class LottoController {
     LottoResultView lottoResultView = new LottoResultView();
 
     public void runLotto() {
-        // 1. 구입 금액 입력
         int price = lottoInputView.purchasePriceInput();
+        lottoModelProcess(price);
+
+        List<Lotto> purchasedLotto = purchaseLotto();
+        List<Integer> winNumbers = lottoWinInputView.winNumberInput();
+        int bonus = lottoBonusInputView.getBonusInput(winNumbers);
+
+        List<LottoPrize> lottoPrizes = calculatePrizes(purchasedLotto, winNumbers, bonus);
+        int[] prizeLog = lottoResultModel.getPrizeLog();
+
+        lottoResultView.printResultLog(prizeLog);
+        lottoResultView.printRateOfReturn(lottoPrizes, price);
+    }
+
+    private void lottoModelProcess(int price) {
         lottoModel.setInputPrice(price);
         lottoModel.calcPurchasedCount();
+    }
 
-        // 2. 로또 구매
+    private List<Lotto> purchaseLotto() {
         int count = lottoModel.getPurchasedCount();
         List<Lotto> purchasedLotto = lottoModel.getPurchasedLotto(count);
         lottoBuyView.printPurchaseLog(purchasedLotto, count);
+        return purchasedLotto;
+    }
 
-        // 3. 당첨 번호 입력
-        List<Integer> winNumbers = lottoWinInputView.winNumberInput();
-
-        // 4. 보너스 번호 입력
-        int bonus = lottoBonusInputView.getBonusInput(winNumbers);
-
-        // 5. 당첨 통계
+    private List<LottoPrize> calculatePrizes(List<Lotto> purchasedLotto, List<Integer> winNumbers, int bonus) {
         lottoResultModel.checkAllLottoMatch(purchasedLotto, winNumbers, bonus);
-        List<LottoPrize> lottoPrizes = lottoResultModel.getLottoPrizes();
-        int[] prizeLog = lottoResultModel.getPrizeLog();
-        lottoResultView.printResultLog(prizeLog);
-
-        // 6.총 수익률 출력
-        lottoResultView.printRateOfReturn(lottoPrizes, price);
+        return lottoResultModel.getLottoPrizes();
     }
 }

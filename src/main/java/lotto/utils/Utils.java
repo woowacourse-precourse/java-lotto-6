3904da754b9c15455e2a6488072278bafe6ebe41
snaag <snaag.dev@gmail.com>
2023-11-08 20:44:54 +0900

    feat: (Utils) 수익률 구하는 함수 구현

--- src/main/java/lotto/utils/Utils.java
+++ src/main/java/lotto/utils/Utils.java
@@ -11,4 +11,20 @@ public class Utils {
         NumberFormat formatter = NumberFormat.getNumberInstance(Locale.US);
         return formatter.format(money);
     }
+
+    public static String getPercentage(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException(LottoMessages.DIVIDE_BY_ZERO.getKr());
+        }
+
+        double result = (double) a / b;
+        result = Math.round(result * 10000) / 100.0;
+
+        String percentage = String.format("%.2f%%", result);
+        if(percentage.endsWith("0%")) {
+            percentage = percentage.replace("0%", "%");
+        }
+
+        return percentage;
+    }
 }

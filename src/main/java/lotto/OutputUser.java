f2eaeecda9fead6d2ff44dc88ee8bb2d705de15b
dyeongkim <dyeongkim98@gmail.com>
2023-11-08 15:57:49 +0900

    style(OutputUser): Modify 구매한 로또 개수 출력 양식 변경

--- src/main/java/lotto/OutputUser.java
+++ src/main/java/lotto/OutputUser.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class OutputUser {
     public void outputBuyLotto(Integer totalLottoCount){
-        System.out.printf("%d개를 구매했습니다.", totalLottoCount);
+        System.out.printf("%d개를 구매했습니다.\n", totalLottoCount);
     }
     public void outputBuyLottoNumbers(List<Lotto> numberTickets){
         for (Lotto lotto : numberTickets){

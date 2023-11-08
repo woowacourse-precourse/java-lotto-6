70f6cbe2a63ab387b1b6953171190a0c44e207f6
1015s <1015_s@naver.com>
2023-11-08 03:07:52 -0800

    feat(LottoTest) : 로또 숫자 범위 관련 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +24,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1에서 45 사이의 숫자가 아닌 경우 예외가 발생한다.")
+    @Test
+    void createLottoByWrongRangeNumber(){
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 60)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

99e62f1381ca2538e70a91bbeb9eef5074bae695
kangyuseok <kingyuseok@naver.com>
2023-11-08 15:54:34 +0900

    Feat: lotto 번호 유효성 검사 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,26 +1,42 @@
 package lotto.domain;
 
+import static lotto.domain.constants.LottoConstatants.LOTTO_MAX_NUMBER;
+import static lotto.domain.constants.LottoConstatants.LOTTO_MIN_NUMBER;
+
+import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateSize(numbers);
+        validateDuplicated(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 가능합니다.");
         }
+    }
+    private void validateDuplicated(List<Integer> numbers){
+        Set<Integer> lotto = new HashSet<>(numbers);
+        if(lotto.size() != 6)
+            throw new IllegalArgumentException("[ERROR] 중복된 로또 번호가 있습니다.");
+    }
+
+    private void validateRange(List<Integer>numbers){
+        for(Integer i : numbers)
+            if(i < LOTTO_MIN_NUMBER.getValue() || i > LOTTO_MAX_NUMBER.getValue())
+                throw new IllegalArgumentException("[ERROR] 로또 숫자 범위는 1 ~ 45 입니다.");
     }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
-// TODO: 추가 기능 구현
 
     @Override
     public String toString() {

6deacc150648a84e8f2c76f76b72ad8d6696d3f0
ASak1104 <khw56184@gmail.com>
2023-11-08 12:25:16 +0900

    test: 로또 번호 포함 테스트 추가

--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -19,15 +20,13 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5))).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호가 최소 값 미만이면 예외가 발생한다.")
@@ -100,10 +99,24 @@ class LottoTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    @DisplayName("로또 번호 포함 여부 테스트")
+    @ParameterizedTest(name = "로또 [1, 2, 3, 4, 5, 6]에서 {0}을 조회하면 {1}이다.")
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:true", "5:true", "6:true",
+            "7:false", "8:false", "9:false", "10:false"}, delimiterString = ":")
+    void contains(int number, boolean expected) {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(numbers);
+
+        // when
+        boolean actual = lotto.contains(number);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
     static Stream<Arguments> generateOtherLotto() {
-        return Stream.of(
-                Arguments.of(new Lotto(List.of(1, 2, 3, 10, 11, 12)), 3),
-                Arguments.of(new Lotto(List.of(6, 5, 4, 3, 2, 1)), 6)
-        );
+        return Stream.of(Arguments.of(new Lotto(List.of(1, 2, 3, 10, 11, 12)), 3),
+                Arguments.of(new Lotto(List.of(6, 5, 4, 3, 2, 1)), 6));
     }
 }

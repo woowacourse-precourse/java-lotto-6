a7fb0d1145c6aa16b197b31d11c1c7bbf1cf2557
seastamp <seastamp10@gmail.com>
2023-11-08 17:17:35 +0900

    feat: Lotto 티켓 생성 구현

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,4 +1,19 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import lotto.utils.NumbersGenerator;
+
 public class LottoTicket {
+    private LottoTicket() {
+    }
+
+    public static List<Lotto> generate(int count) {
+        List<Lotto> lottoTicket = new ArrayList<>();
+        while (count > 0) {
+            lottoTicket.add(new Lotto(NumbersGenerator.generate()));
+            count--;
+        }
+        return lottoTicket;
+    }
 }

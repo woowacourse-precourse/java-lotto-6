b768699a17da9ff10b70e5f1a38abe3da3e27e64
권태현 <kygkth2011@gmail.com>
2023-11-08 23:39:36 +0900

    feat(Lotto) : 로또 번호 내 보너스번호, 로또간 일치 숫자 확인 메소드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Lotto {
     static final int SMALLEST_LOTTO_NUMBER = 1;
@@ -24,6 +25,10 @@ public class Lotto {
         }
     }
 
+    private List<Integer> getNumbers() {
+        return numbers;
+    }
+
     private void duplicateValidate(List<Integer> numbers) {
         if (numbers.size() != numbers.stream().distinct().count()) {
             throw new IllegalArgumentException("당첨 번호는 중복될 수 없습니다.");
@@ -39,7 +44,17 @@ public class Lotto {
     }
 
     public boolean isLottoNumberDuplicate(int bonusNumber) {
-        return !numbers.contains(bonusNumber);
+        return (numbers.contains(bonusNumber));
+    }
+
+    public boolean hasBonus(int bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public int matchedNumberAmount(Lotto winningLotto) {
+        return this.numbers.stream()
+                .filter(target -> winningLotto.getNumbers().stream()
+                        .anyMatch(Predicate.isEqual(target))).toList().size();
     }
 
     @Override

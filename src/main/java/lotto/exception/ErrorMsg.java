91cecc2a57e7575a49f03b06a871b9750d96d25d
jhva <junghun8158@naver.com>
2023-11-08 19:03:56 +0900

    :memo: docs: 로또 당첨 번호 메시지 수정

--- src/main/java/lotto/exception/ErrorMsg.java
+++ src/main/java/lotto/exception/ErrorMsg.java
@@ -1,7 +1,7 @@
 package lotto.exception;
 
 public enum ErrorMsg {
-    ERROR_LOTTO_NUMBERS_SIZE_MAX("[ERROR] 당첨 번호는 6개 이하로 입력해 주세요."),
+    ERROR_LOTTO_NUMBERS_SIZE_MAX("[ERROR] 당첨 번호는 6개를 입력해 주세요."),
     ERROR_LOTTO_DUPLICATES_NUMBERS("[ERROR] 중복된 숫자가 포함 되어 있습니다."),
     ERROR_LOTTO_NUMBER_OVER_MAX("[ERROR] 1~45 이하 숫자를 입력해 주세요."),
     ERROR_INPUT_NOT_NUMBER("[ERROR] 숫자로 입력 해주세요."),

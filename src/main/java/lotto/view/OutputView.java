60f6ec98cf3d40e147ebef4f5481ad5e7fc52d58
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:43:06 +0900

    feat : OutputView 상수 선언

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
 public class OutputView {
+    private static final String PURCHASED_TICKETS_MESSAGE = "개를 구매했습니다.";
+    private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
+    private static final String WINNING_STATISTICS_LINE = "---";
+    private static final String RESULT_FORMAT = "%d개 일치 (%d원) - %d개";
+    private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.2f%%입니다.";
+
 }

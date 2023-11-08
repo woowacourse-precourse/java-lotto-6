811a3033958fc0af22cfd3ed6417889afe812456
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 15:43:22 +0900

    refactor(LottoServiceTest): compareMyNumberWithWinningNumberTest() 수정
    추가적으로 보너스 점수를 가져오는 메소드는 사라져서 해당되는 테스트
    케이스도 삭제

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,5 +1,6 @@
 package lotto.service;
 
+import lotto.LottoWinningCase;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +13,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static lotto.ErrorMessage.CONTAINS_EMPTY_NUMBER;
+import static lotto.LottoWinningCase.FIFTH_PLACE;
+import static lotto.LottoWinningCase.UNRANK;
 import static org.assertj.core.api.Assertions.*;
 
 public class LottoServiceTest {
@@ -19,13 +23,12 @@ public class LottoServiceTest {
     private LottoService service = new LottoService();
 
     @ParameterizedTest
-    @ValueSource(strings = {"1,2,3,4,5", "6,7,8,9,10,11"})
+    @ValueSource(strings = {"1,2,3,4,5,6", "6,7,8,9,10,11"})
     @DisplayName("콤마로 구분된 입력 문자열을 List으로 변환")
     void convertToDifferentType(String input) {
         List<Integer> winningNumber = service.reshapeWinningNumber(input);
-        assertThat(winningNumber).hasSize(input.length() / 2 + 1)
-                .contains(input.charAt(0) - '0')
-                .contains(input.charAt(input.length() - 1) - '0');
+        assertThat(winningNumber).hasSize(6)
+                .contains(input.charAt(0) - '0');
     }
 
     @ParameterizedTest
@@ -41,8 +44,7 @@ public class LottoServiceTest {
     @DisplayName("로또 번호중 연속된 쉼표 존재할 경우의 테스트")
     void consequentCommaInLottoNumber(String input) {
         assertThatThrownBy(() -> service.reshapeWinningNumber(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR]");
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @ParameterizedTest
@@ -59,38 +61,21 @@ public class LottoServiceTest {
     void generateRandomLottoNumsByNTime(int time) {
         List<Lotto> myLottoNums = service.generateRandomLottoNums(time);
         assertThat(myLottoNums.size()).isEqualTo(time);
-        assertThat(myLottoNums.getNumbers().size()).isEqualTo(6);
+        assertThat(myLottoNums.get(0).getNumbers().size()).isEqualTo(6);
     }
 
     @ParameterizedTest
     @MethodSource("generateListData")
     @DisplayName("내 번호와 당첨 번호를 비교, 몇개의 숫자와 같은지 결과 보여줌")
-    void compareMyNumberWithWinningNumberTest(List<Integer> myNumber, List<Integer> winningNumber, int expected) {
-        int result = service.compareMyNumberWithWinningNumber(myNumber, winningNumber);
-        // 이거 지금은 enum 잘 몰라서 이렇게 했는데, 상수 enum으로 표현하는거 배우면 enum 클래스로 받아올 수 있음.
-        // 그래서 밑에 있는 Assertion도 수정 될 수 있음. + 매개변수도
+    void compareMyNumberWithWinningNumberTest(Lotto myLottoNumbers, List<Integer> winningNumber, int bonusNumber, LottoWinningCase expected) {
+        LottoWinningCase result = service.compareMyNumberWithWinningNumber(myLottoNumbers, winningNumber, bonusNumber);
         assertThat(result).isEqualTo(expected);
     }
 
     static Stream<Arguments> generateListData() {
         return Stream.of(
-                Arguments.of(Arrays.asList(3, 8, 10, 23, 33, 42), Arrays.asList(1, 2, 3, 4, 5, 6), 0),
-                Arguments.of(Arrays.asList(7, 9, 10, 11, 20, 23), Arrays.asList(11, 12, 13, 14, 20, 23), 1)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("generateMyNumberAndBonusNumber")
-    @DisplayName("보너스 번호 확인 하기")
-    void checkBonusPointTest(List<Integer> myNumber, int bonusNumber, boolean expectResult) {
-        boolean result = service.checkBonusPoint(myNumber, bonusNumber);
-        assertThat(result).isEqualTo(expectResult);
-    }
-
-    static Stream<Arguments> generateMyNumberAndBonusNumber() {
-        return Stream.of(
-                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 6), 7, false),
-                Arguments.of(Arrays.asList(1, 2, 3, 4, 5, 7), 7, true)
+                Arguments.of(new Lotto(Arrays.asList(3, 8, 10, 23, 33, 42)), Arrays.asList(1, 2, 3, 4, 5, 6), 10, UNRANK),
+                Arguments.of(new Lotto(Arrays.asList(7, 9, 10, 11, 20, 23)), Arrays.asList(11, 12, 13, 14, 20, 23), 10, FIFTH_PLACE)
         );
     }
 

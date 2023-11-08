7bc870d32609732e7d10c76e4a8c12c66ee02bdb
choiejae <ejae8319@gmail.com>
2023-11-08 16:41:20 +0900

    feat(LottoGameController): 분리된 핵심 로직과 UI 로직을 관리하는 컨트롤러 기능 구현
    - UI 로직을 요청 또는 응답한다
    - Service 핵심 로직을 통해 원하는 값을 받는다

---
+++ src/main/java/lotto/LottoGameController.java
@@ -0,0 +1,73 @@
+package lotto;
+
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final LottoGameService lottoGameService;
+
+    public LottoGameController(InputView inputView, OutputView outputView, LottoGameService lottoGameService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoGameService = lottoGameService;
+    }
+
+    public void run() {
+        PurchaseAmount purchaseAmount = inputPurchaseAmount();
+        LottoTickets autoLottoTickets = generateAutoLottoTickets(purchaseAmount);
+
+        outputView.printAutoLottoTickets(autoLottoTickets);
+
+        Lotto winningNumbers = inputWinningNumbers();
+        WinningLotto winningLotto = generateWinningLotto(winningNumbers);
+        LottoResultBoard lottoResultBoard = lottoGameService.generateLottoResultStatistics(
+                autoLottoTickets,
+                winningLotto
+        );
+
+        outputView.printLottoResultStatistics(lottoResultBoard, purchaseAmount);
+    }
+
+    private WinningLotto generateWinningLotto(Lotto winningNumbers) {
+        try {
+            BonusNumber bonusNumber = inputBonusNumber();
+            return lottoGameService.generateWinningLotto(winningNumbers, bonusNumber);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            return generateWinningLotto(winningNumbers);
+        }
+    }
+
+    private BonusNumber inputBonusNumber() {
+        String inputValue = inputView.inputBonusNumber();
+        return lottoGameService.generateBonusNumber(inputValue);
+    }
+
+    private Lotto inputWinningNumbers() {
+        try {
+            String inputValue = inputView.inputWinningLotto();
+            return lottoGameService.generateWinningNumbers(inputValue);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            return inputWinningNumbers();
+        }
+    }
+
+    private LottoTickets generateAutoLottoTickets(PurchaseAmount purchaseAmount) {
+        int lottoTicketQuantity = purchaseAmount.getPurchaseQuantity();
+        return lottoGameService.generateLottoTickets(lottoTicketQuantity);
+    }
+
+    private PurchaseAmount inputPurchaseAmount() {
+        try {
+            String inputValue = inputView.inputPurchaseAmount();
+            return lottoGameService.generatePurchaseAmount(inputValue);
+        } catch (IllegalArgumentException e) {
+            outputView.printErrorMessage(e);
+            return inputPurchaseAmount();
+        }
+    }
+}

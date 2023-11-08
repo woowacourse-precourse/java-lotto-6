6c6417680190d2ffeb3f261ea9cd2298d11b0dfc
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:14:57 +0900

    feat : inputWinningNumber 함수 생성

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,10 @@
 package lotto.view;
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
@@ -25,4 +29,31 @@ public class InputView {
         }
     }
 
+    public static List<Integer> inputWinningNumber() {
+        while (true) {
+            try {
+                System.out.println("당첨 번호를 입력해 주세요. (쉼표로 구분, 1~45 사이의 숫자)");
+                String[] numbers = scanner.nextLine().split(",");
+                if (numbers.length != LOTTO_NUMBERS_SIZE) {
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+                }
+                List<Integer> winningNumbers = Stream.of(numbers)
+                        .map(String::trim)
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+                if (winningNumbers.stream().anyMatch(number -> number < 1 || number > MAX_LOTTO_NUMBER)) {
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.");
+                }
+                if (winningNumbers.size() != new HashSet<>(winningNumbers).size()) {
+                    throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+                }
+                return winningNumbers;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자만 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }

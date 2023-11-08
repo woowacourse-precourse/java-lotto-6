9ae8004a40445f2bea64b8088494d03aff46e4f5
penrose15 <seongju99@naver.com>
2023-11-08 23:20:35 +0900

    Rename: 메서드 이름 변경

--- src/main/java/lotto/global/view/io/LottoInputView.java
+++ src/main/java/lotto/global/view/io/LottoInputView.java
@@ -5,7 +5,6 @@ import lotto.global.view.output.OutputView;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.NoSuchElementException;
 
 import static lotto.global.constant.exception.ExceptionMessage.EXCEPTION_PREFIX;
 
@@ -23,13 +22,13 @@ public class LottoInputView implements InvocationHandler {
             try {
                 return method.invoke(inputProcessor, args);
             } catch (InvocationTargetException e) {
-                checkIllegalArgumentException(e);
+                determineExceptionIsDueToPlayerInput(e);
                 OutputView.printMessageLine(EXCEPTION_PREFIX.getMessage() + e.getTargetException().getMessage());
             }
         }
     }
 
-    private void checkIllegalArgumentException(InvocationTargetException e) throws Throwable {
+    private void determineExceptionIsDueToPlayerInput(InvocationTargetException e) throws Throwable {
         if(!(e.getTargetException() instanceof IllegalArgumentException)) {
             throw e.getTargetException();
         }

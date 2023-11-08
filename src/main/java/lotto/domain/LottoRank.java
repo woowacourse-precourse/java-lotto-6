cddcf7324a8c1666569dfb13aaf43130643163f2
toychip <dlawnsgud427@naver.com>
2023-11-08 10:10:02 +0900

    feat: LottoRank description 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.List;
+
 public enum LottoRank {
 
     /*
@@ -9,21 +11,23 @@ public enum LottoRank {
         5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
         6개 일치 (2,000,000,000원) - 0개
     */
-    FIRST(2000000000, 6, false),
-    SECOND(30000000, 5, true),
-    THIRD(1500000, 5, false),
-    FOURTH(50000, 4, false),
-    FIFTH(5000, 3, false),
-    NONE(0, 0, false);
+    NONE(0, 0, false, null),
+    FIFTH(5000, 3, false, "3개 일치 (5,000원) - "),
+    FOURTH(50000, 4, false, "4개 일치 (50,000원) - "),
+    THIRD(1500000, 5, false, "5개 일치 (1,500,000원) - "),
+    SECOND(30000000, 5, true, "5개 일치, 보너스 볼 일치 (30,000,000원) - "),
+    FIRST(2000000000, 6, false, "6개 일치 (2,000,000,000원) - ");
     private static final int FIVE = 5;
     private final long amount;
     private final int count;
     private final boolean hasBonus;
+    private final String description;
 
-    LottoRank(final long amount, final int count, final boolean hasBonus) {
+    LottoRank(final long amount, final int count, final boolean hasBonus, final String description) {
         this.amount = amount;
         this.count = count;
         this.hasBonus = hasBonus;
+        this.description = description;
     }
 
     public static LottoRank of(final int count, final boolean hasBonus) {
@@ -40,7 +44,6 @@ public enum LottoRank {
                 return rank;
             }
         }
-
         return NONE;
     }
 
@@ -59,4 +62,7 @@ public enum LottoRank {
         return amount;
     }
 
+    public String getDescription() {
+        return description;
+    }
 }

138b5ec3593adca7e2c787bbe44c0a3d10c98077
jyeun722 <skqhs7276@gamil.com>
2023-11-08 23:13:58 +0900

    test: Change test file to application test

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -1,48 +1,98 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    @DisplayName("로또 구입 금액에 숫자가 아닌 다른 문자열을 입력하면 예외가 발생한다.")
     @Test
-    void 기능_테스트() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    run("8000", "1,2,3,4,5,6", "7");
-                    assertThat(output()).contains(
-                            "8개를 구매했습니다.",
-                            "[8, 21, 23, 41, 42, 43]",
-                            "[3, 5, 11, 16, 32, 38]",
-                            "[7, 11, 16, 35, 36, 44]",
-                            "[1, 8, 11, 31, 41, 42]",
-                            "[13, 14, 16, 38, 42, 45]",
-                            "[7, 11, 30, 40, 42, 43]",
-                            "[2, 13, 22, 32, 38, 45]",
-                            "[1, 3, 5, 14, 22, 45]",
-                            "3개 일치 (5,000원) - 1개",
-                            "4개 일치 (50,000원) - 0개",
-                            "5개 일치 (1,500,000원) - 0개",
-                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개",
-                            "6개 일치 (2,000,000,000원) - 0개",
-                            "총 수익률은 62.5%입니다."
-                    );
-                },
-                List.of(8, 21, 23, 41, 42, 43),
-                List.of(3, 5, 11, 16, 32, 38),
-                List.of(7, 11, 16, 35, 36, 44),
-                List.of(1, 8, 11, 31, 41, 42),
-                List.of(13, 14, 16, 38, 42, 45),
-                List.of(7, 11, 30, 40, 42, 43),
-                List.of(2, 13, 22, 32, 38, 45),
-                List.of(1, 3, 5, 14, 22, 45)
-        );
+    void InputLottoMoneyByNotNumber() {
+        assertSimpleTest(() -> {
+            runException("1000j");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 구입 금액 단위가 1000으로 나누어 떨어지지 않는 경우 예외가 발생한다.")
+    @Test
+    void InputWrongLottoMoneyUnit() {
+        assertSimpleTest(() -> {
+            runException("1100");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void createLottoByUnderSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 숫자가 1 ~ 45 범위를 만족하지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 숫자와 구분자 쉼표(,)를 제외하고 다른 문자열을 입력하면 예외가 발생한다.")
+    @Test
+    void InputLottoByNotNumber() {
+        assertSimpleTest(() -> {
+            runException("1000", "#");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("보너스 번호에 숫자가 아닌 다른 문자열을 입력하면 예외가 발생한다.")
+    @Test
+    void InputBonusByNotNumber() {
+        assertSimpleTest(() -> {
+            runException("1000", "1, 2, 3, 4, 5, 6", "40,");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("보너스 번호의 숫자가 1 ~ 45 범위를 만족하지 않으면 예외가 발생한다.")
+    @Test
+    void InputBonusByOutOfRangeNumber() {
+        assertSimpleTest(() -> {
+            runException("1000", "1, 2, 3, 4, 5, 6", "50");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @DisplayName("보너스 번호가 로또 번호와 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void InputBonusByDuplicatedNumber() {
+        assertSimpleTest(() -> {
+            runException("1000", "1, 2, 3, 4, 5, 6", "6");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
     }
 
     @Override

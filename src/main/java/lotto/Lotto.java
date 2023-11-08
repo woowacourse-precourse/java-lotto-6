c6a7ac5f39089cfa3323c42ed9703820d60fff59
kkkkimtaehyeon <kth1927@naver.com>
2023-11-08 12:13:18 +0900

    feat : 로또 숫자 범위 초과 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -8,7 +8,8 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validateLength(numbers);
-        validateDuplication(numbers);
+        validateDuplicateNumbers(numbers);
+        validateOutOfRange(numbers);
         this.numbers = numbers;
     }
 
@@ -17,10 +18,17 @@ public class Lotto {
             throw new IllegalArgumentException();
         }
     }
-    private void validateDuplication(List<Integer> numbers) {
+    private void validateDuplicateNumbers(List<Integer> numbers) {
         HashSet<Integer> hashNumbers = new HashSet<>(numbers);
         if(hashNumbers.size() != numbers.size()){
             throw new IllegalArgumentException();
+        }
+    }
+    private void validateOutOfRange(List<Integer> numbers){
+        for(Integer number : numbers){
+            if(!(number >= 1 && number <= 45)){
+                throw  new IllegalArgumentException();
+            }
         }
     }
     // TODO: 추가 기능 구현

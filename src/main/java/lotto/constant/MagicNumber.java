3a3df422b81b141d046ed6def02cfff6af73e25b
lee jihyeon <lejinhy@gmail.com>
2023-11-08 14:31:25 +0900

    feat: 매직 넘버 상수화 용 enum

---
+++ src/main/java/lotto/constant/MagicNumber.java
@@ -0,0 +1,21 @@
+package lotto.constant;
+
+public enum MagicNumber {
+    MIN_NUMBER(1),
+    MAX_NUMBER(45),
+    LOTTO_COUNT(6),
+    RANK_COUNT(5),
+    PRICE_UNIT(1000)
+    ;
+
+
+    private int number;
+
+    MagicNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}

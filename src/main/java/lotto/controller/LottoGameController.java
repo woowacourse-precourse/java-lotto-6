913bab521d19f1c3fab161846163eeb2afae7803
GaHee99 <u1879@sookmyung.ac.kr>
2023-11-08 10:32:30 +0900

    feat(WinningLotto): 당첨 로또 생성

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,9 +1,16 @@
 package lotto.controller;
 
+import static lotto.domain.NumberType.ORIGINAL;
+
+import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGenerator;
 import lotto.domain.Lottos;
+import lotto.domain.NumberType;
 import lotto.domain.PurchaseMoney;
+import lotto.domain.WinningLotto;
+import lotto.domain.WinningNumber;
+import lotto.domain.validator.WinningNumberValidator;
 import lotto.util.LottoNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -19,8 +26,31 @@ public class LottoGameController {
         PurchaseMoney purchaseMoney = getPurchaseMoney();
         LottoGenerator lottoGenerator =  LottoGenerator.of(lottoNumberGenerator, purchaseMoney);
         Lottos lottos = createLottos(purchaseMoney, lottoGenerator);
-
+        List<WinningNumber> onlyWinningNumber = createWinningLotto();
     }
+
+
+    private List<WinningNumber> createWinningLotto() {
+        String winningNumber = InputView.getWinningNumbers();
+        try {
+            List<Integer> winnings = WinningNumberValidator.validateWinningNumber(winningNumber);
+            return createWinningNumber(winnings);
+        } catch(IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+
+            return createWinningLotto();
+        }
+    }
+
+
+    private List<WinningNumber> createWinningNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> WinningNumber.of(number, ORIGINAL)).toList();
+    }
+
+
+
+
 
     private static PurchaseMoney getPurchaseMoney() {
         try {
--- src/main/java/lotto/message/ConsoleMessage.java
+++ src/main/java/lotto/message/ConsoleMessage.java
@@ -5,6 +5,8 @@ public enum ConsoleMessage {
 
     //Input Message
     GET_PURCHASE_MONEY("구입금액을 입력해 주세요."),
+    GET_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
+    GET_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
 
     //Output Message
     CREATE_LOTTOS_COUNT("개를 구매했습니다."),
@@ -13,6 +15,7 @@ public enum ConsoleMessage {
     ERROR_STRING("[ERROR] "),
     LOTTO_NUMBER_RANGE_ERROR("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
     LOTTO_NUMBER_DUPLICATE_ERROR("로또 넘버는 중복이 될 수 없습니다."),
+    LOTTO_NUMBER_SIZE_ERROR("당첨 번호는 6자리 수 여야 합니다."),
     PURCHASE_MONEY_ERROR("로또 구입 금액은 1,000원 단위로 나누어 떨어져야 합니다.");
     private ConsoleMessage(String message) {
         this.message = message;

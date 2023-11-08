3df770869c742e0f9b53dcfcb085d8355ea18c9b
usnim <sms897@naver.com>
2023-11-08 15:02:16 +0900

    [refactor] Lotto 클래스 불필요한 import 제거

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,8 +2,6 @@ package lotto.domain;
 
 import lotto.exception.InvalidDuplicatedNumberException;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {

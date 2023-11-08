794cde3245b63d29ac8e4614fe134918a6fc4d50
Kim mingyu <minq0302@naver.com>
2023-11-08 21:25:28 +0900

    Revert "feat(exception): add checking lotto number duplication exception"
    This reverts commit e429cb76d260d5ebb0c61ec29eb7e41a3716b72c.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,9 +1,6 @@
 package lotto.domain;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import lotto.exception.DuplicateLottoNumberException;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -15,20 +12,8 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        validateSize(numbers);
-        validateDuplicate(numbers);
-    }
-
-    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException("[ERROR]: 로또 숫자는 6개여야 합니다.");
-        }
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
-        if (nonDuplicateNumbers.size() != LOTTO_SIZE) {
-            throw new DuplicateLottoNumberException();
         }
     }
 

c00d8d2ba0d9ef72fc9e407d705e286488485165
hansg931 <hansg0509@cau.ac.kr>
2023-11-08 21:46:22 +0900

    feat(LottoNumberValidator): 입력받은 로또 숫자의 갯수를 검사하는 메소드 작성

--- src/main/java/lotto/util/LottoNumberValidator.java
+++ src/main/java/lotto/util/LottoNumberValidator.java
@@ -2,14 +2,12 @@ package lotto.util;
 
 public class LottoNumberValidator {
 	public void validateLottoNumber(String input) {
-
+		validateNumberCount(input);
 	}
 
-	private void checkInteger(String input) {
-		try {
-			Integer.parseInt(input);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUM_NOT_INTEGER_MESSAGE);
+	private void validateNumberCount(String input) {
+		if (input.split(",").length != Constant.LOTTO_SIZE) {
+			throw new IllegalArgumentException(ExceptionMessage.LOTTO_NUM_SIZE_MESSAGE);
 		}
 	}
 }

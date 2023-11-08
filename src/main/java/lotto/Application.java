dada8cd77790217094337672f00ad0138ee5114a
sunkong25 <admin@Hyebinui-MacBookPro.local>
2023-11-08 20:30:04 +0900

    feat(Ask): 중복되지 않은 숫자 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,16 @@
 package lotto;
 
+import lotto.domain.Lotto;
+import lotto.view.Ask;
 import lotto.view.Guest;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Ask ask = new Ask();
+        ask.winningException();
+
 
     }
+
 }
--- src/main/java/lotto/view/Ask.java
+++ src/main/java/lotto/view/Ask.java
@@ -1,10 +1,35 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.domain.Lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class Ask {
+    List<Integer> winningNumbers = new ArrayList<>();
     public List<Integer> winningNumber() {
-        return null;
+        winningNumbers.clear();
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winning = Console.readLine();
+        String[] number = winning.split(",");
+
+        for (int i = 0; i < number.length; i++) {
+            int num = Integer.parseInt(number[i]);
+            winningNumbers.add(num);
+        }
+        return winningNumbers;
+    }
+
+    public void winningException(){
+        try{
+            Lotto lotto = new Lotto(winningNumber());
+        }
+        catch(IllegalArgumentException e){
+            winningException();
+        }
     }
 
     public int bonusNumber() {

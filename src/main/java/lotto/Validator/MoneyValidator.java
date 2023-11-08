796921ce25e1997999b5907e7bd86bbf8e50c8a0
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 11:59:26 +0900

    [feat] Validator - 구입 금액을 숫자로 입력했는지에 대한 검증 로직

--- src/main/java/lotto/Validator/MoneyValidator.java
+++ src/main/java/lotto/Validator/MoneyValidator.java
@@ -6,12 +6,19 @@ public class MoneyValidator {
 
     public static String validatePurchaseAmount(String input) {
         validateNotEmpty(input);
+        validateNotNumber(input);
 
         return input;
     }
 
     private static void validateNotEmpty(String input) {
         if (input.trim().isEmpty()) {
+            throw new IllegalArgumentException(MoneyErrorMessage.INVALID_MONEY_INPUT.getMessage());
+        }
+    }
+
+    private static void validateNotNumber(String input) {
+        if (!input.matches("\\d+")) {
             throw new IllegalArgumentException(MoneyErrorMessage.INVALID_MONEY_INPUT.getMessage());
         }
     }

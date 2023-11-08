0d3dbaa575c0f54ec798024acc695bc67d187e7e
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-08 13:18:15 +0900

    refactor: Validator 내 메서드 분리

--- src/main/java/lotto/utils/Validator.java
+++ src/main/java/lotto/utils/Validator.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static java.lang.Integer.parseInt;
 import static java.lang.Long.parseLong;
 import static lotto.domain.constant.NumberConstant.*;
 
@@ -19,13 +20,13 @@ public class Validator {
     }
 
 
-    public static void validateSize(List<Integer> numbers) {
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE)
             throw new InvalidLottoSizeException();
     }
 
 
-    public static void validateDuplicate(List<Integer> numbers) {
+    private static void validateDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>();
 
         for (Integer n : numbers)
@@ -34,23 +35,14 @@ public class Validator {
     }
 
 
-    public static void validateNumberInRange(List<Integer> numbers) {
+    private static void validateNumberInRange(List<Integer> numbers) {
         if (numbers.stream().anyMatch(n -> n < LOTTO_NUMBER_START || n > LOTTO_NUMBER_END))
             throw new LottoNumOutOfRangeException();
     }
 
 
     // InputService SERVICE
-    public static void validateExpenseValue(Long expense) {
-        if (expense < MIN_EXPENSE_VALUE)
-            throw new InvalidExpenseValueException();
-
-        if (expense % LOTTO_PRICE > ZERO)
-            throw new NonDividableExpenseValueException();
-    }
-
-
-    public static void validateNumberType(String input) {
+    private static void validateNumberType(String input) {
         try {
             parseLong(input);
         } catch (NumberFormatException e) {
@@ -59,13 +51,52 @@ public class Validator {
     }
 
 
-    public static void validateBonusNumInRange(int bonusNum) {
+    // expense
+    public static void validateExpense(String input) {
+        validateNumberType(input);
+
+        long expense = parseLong(input);
+        validateExpenseMin(expense);
+        validateExpenseDividable(expense);
+    }
+
+    private static void validateExpenseMin(Long expense) {
+        if (expense <= MIN_EXPENSE_VALUE)
+            throw new InvalidExpenseValueException();
+    }
+
+    private static void validateExpenseDividable(Long expense) {
+        if (expense % LOTTO_PRICE > ZERO)
+            throw new NonDividableExpenseValueException();
+    }
+
+
+    // winningLotto
+    public static void validateLottoNumType(String[] input) {
+        for (String s : input)
+            try {
+                parseInt(s);
+            } catch (NumberFormatException e) {
+                throw new InvalidNumberTypeException();
+            }
+    }
+
+
+    // bonusNum
+    public static void validateBonusNum(String input, Lotto winningLotto) {
+        validateNumberType(input);
+
+        int bonusNum = parseInt(input);
+        validateBonusNumInRange(bonusNum);
+        validateBonusNumDuplicate(bonusNum, winningLotto);
+    }
+
+    private static void validateBonusNumInRange(int bonusNum) {
         if (bonusNum < LOTTO_NUMBER_START || bonusNum > LOTTO_NUMBER_END)
             throw new LottoNumOutOfRangeException();
     }
 
-
-    public static void validateBonusNumDuplicate(int bonusNum, Lotto winningLotto) {
+    private static void validateBonusNumDuplicate(int bonusNum, Lotto winningLotto) {
         if (winningLotto.getNumbers().contains(bonusNum))
             throw new DuplicateBonusNumException();
     }

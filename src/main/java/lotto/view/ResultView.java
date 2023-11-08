2967e761f45ecab61d5c348e7708ea4e1802a287
leedabin2 <12193152@inha.edu>
2023-11-08 18:41:40 +0900

    feat(ResultView): Implement result printing methods

--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,8 +1,9 @@
 package lotto.view;
 
-import lotto.handler.UserHandler;
+import lotto.model.Rank;
 
 import java.util.List;
+import java.util.Map;
 
 public class ResultView {
 
@@ -16,5 +17,22 @@ public class ResultView {
 
     public static void printLottoNumber(List<Integer> numbers){
         System.out.println(numbers);
+    }
+
+    public static void printResult(Map<Rank, Integer> result) {
+        printNewLine();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        printResultForRank(result, Rank.FIFTH, 5_000);
+        printResultForRank(result, Rank.FOURTH, 50_000);
+        printResultForRank(result, Rank.THIRD, 1_500_000);
+        printResultForRank(result, Rank.SECOND, 30_000_000);
+        printResultForRank(result, Rank.FIRST, 2_000_000_000);
+    }
+
+    private static void printResultForRank(Map<Rank, Integer> result, Rank rank, int prizeMoney) {
+        System.out.println(rank.getMatchCount() + "개 일치" + (rank == Rank.SECOND ? ", 보너스 볼 일치 " : " ")
+                + "(" + String.format("%,d", prizeMoney) + "원) - " + result.getOrDefault(rank, 0) + "개");
     }
 }

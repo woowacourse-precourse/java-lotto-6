fa70d67b6514e845c566d38fab03b672caf2de1f
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 18:08:25 +0900

    refactor(LottoMoneyPrize) : 함수 순서 정리 및 final 붙여주기

--- src/main/java/lotto/domain/LottoMoneyPrize.java
+++ src/main/java/lotto/domain/LottoMoneyPrize.java
@@ -7,28 +7,28 @@ import java.util.List;
 import java.util.Map;
 
 public class LottoMoneyPrize {
-    private final Map<LottoMoney,Integer> lottoResult = new EnumMap<>(LottoMoney.class);
-    private CompareNumber compareNumber = new CompareNumber();
-
-    private void set() {
-        for(LottoMoney lottoMoney : LottoMoney.values()){
-            lottoResult.put(lottoMoney, 0);
-        }
-    }
+    private final Map<LottoMoney, Integer> lottoResult = new EnumMap<>(LottoMoney.class);
+    private final CompareNumber compareNumber = new CompareNumber();
 
     public LottoMoneyPrize(List<Lotto> lottos, List<Integer> randomWin, int bonusNum) {
         set();
         for (Lotto lotto : lottos) {
             int winnerNum = compareNumber.compareWinnerNum(lotto.getNumber(), randomWin);
-            boolean isBonusNumber = compareNumber.compareBonusNum(bonusNum,lotto.getNumber());
+            boolean isBonusNumber = compareNumber.compareBonusNum(bonusNum, lotto.getNumber());
             LottoMoney lottoMoney = LottoMoney.getLotto(winnerNum, isBonusNumber);
             updateLottoPrize(lottoMoney);
         }
     }
 
-    public int getTotalReward(){
+    private void set() {
+        for (LottoMoney lottoMoney : LottoMoney.values()) {
+            lottoResult.put(lottoMoney, 0);
+        }
+    }
+
+    public int getTotalReward() {
         int total = 0;
-        for(Map.Entry<LottoMoney, Integer> entry : lottoResult.entrySet()){
+        for (Map.Entry<LottoMoney, Integer> entry : lottoResult.entrySet()) {
             LottoMoney lottoMoney = entry.getKey();
             int count = entry.getValue();
             total += lottoMoney.getMoney() * count;
@@ -37,11 +37,10 @@ public class LottoMoneyPrize {
     }
 
     private void updateLottoPrize(LottoMoney lottoMoney) {
-        lottoResult.put(lottoMoney,lottoResult.get(lottoMoney) + 1);
+        lottoResult.put(lottoMoney, lottoResult.get(lottoMoney) + 1);
     }
 
-
-    public Map<LottoMoney, Integer> getLottoMoneyPrize(){
+    public Map<LottoMoney, Integer> getLottoMoneyPrize() {
         return lottoResult;
     }
 

19e6a0f0e3947ac4c195984f0f39941f32bf76aa
2018009107 <2018009107@hanyang.ac.kr>
2023-11-08 22:58:51 +0900

    fix: 리스트 정렬 방식 수정
    - immutable collection 예외 발생
    - API를 통한 랜덤 값 리스트 생성 이후 mutable list에 할당하여 sort 진행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,7 @@ package lotto;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -78,7 +79,7 @@ public class Application {
 
         List<Integer> numbers;
         for (int i = 0; i < lottoCount; i++){
-            numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
             numbers.sort(Comparator.naturalOrder());
             lottos[i] = new Lotto(numbers);
             System.out.println(numbers);

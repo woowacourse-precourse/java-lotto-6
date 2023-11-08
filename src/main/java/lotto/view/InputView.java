c3c5d1c75f100ef8b688875bf9b5af7b6e79d89a
cwkim <101470043+qwertyuiop4m@users.noreply.github.com>
2023-11-08 18:50:23 +0900

    refator: 함수 분리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,73 +7,79 @@ import java.util.List;
 
 public class InputView {
     public int getPurchase() {
-        System.out.println("구입금액을 입력해주세요");
-        String input = Console.readLine();
-
-        try {
-            int purchase = Integer.parseInt(input);
+        while (true) {
+            String input = readInput("구입금액을 입력해주세요");
+            int purchase = parseInt(input);
 
             if (purchase % 1000 != 0) {
-                throw new IllegalArgumentException("구입 금액은 1,000원 단위로 입력해주세요");
+                System.out.println("[ERROR] 1000단위로 입력해주세요");
+                return getPurchase();
             }
-
             return purchase;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR]");
-            return getPurchase();
         }
     }
 
-
     public List<Integer> getWinNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요");
-        String input = Console.readLine();
-        List<Integer> winNumbers = parseNumbers(input);
+        while (true) {
+            String input = readInput("당첨 번호를 입력해 주세요");
+            List<Integer> winNumbers = parseNumbers(input);
 
-        if (winNumbers.size() != 6 || !areValidNumbers(winNumbers)) {
-            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개의 1부터 45 사이의 유효한 숫자여야 합니다.");
+            if (isValidWinNumbers(winNumbers)) {
+                return winNumbers;
+            }
         }
-
-        return winNumbers;
     }
 
     public int getBonusNumbers() {
-        System.out.println("보너스 번호를 입력해 주세요");
+        while (true) {
+            String input = readInput("보너스 번호를 입력해 주세요");
+            int bonusNumber = parseInt(input);
 
+            if (isValidBonusNumber(bonusNumber)) {
+                return bonusNumber;
+            }
+        }
+    }
+
+    private String readInput(String message) {
+        System.out.println(message);
+        return Console.readLine();
+    }
+
+    private int parseInt(String input) {
         try {
-            String input = Console.readLine();
-            int bonusNumber = Integer.parseInt(input);
-
-            if (bonusNumber < 1 || bonusNumber > 45) {
-                throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-
-            return bonusNumber;
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 보너스 번호는 숫자로 입력해주세요");
+            System.out.println("[ERROR] 숫자로 입력해주세요");
+            return parseInt(readInput("다시 입력해주세요"));
         }
     }
 
-
-    private List<Integer> parseNumbers(String winNumbers) {
-        String [] tokens=winNumbers.split(",");
-        List<Integer> numbers=new ArrayList<>();
+    private List<Integer> parseNumbers(String input) {
+        String[] tokens = input.split(",");
+        List<Integer> numbers = new ArrayList<>();
         for (String token : tokens) {
-            try {
-                numbers.add(Integer.parseInt(token));
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 올바른 숫자를 입력하세요.");
+            int number = parseInt(token);
+            if (number >= 1 && number <= 45) {
+                numbers.add(number);
+            } else {
+                System.out.println("[ERROR] 1부터 45 사이의 유효한 숫자를 입력하세요.");
+                return parseNumbers(readInput("다시 입력해주세요"));
             }
         }
-        return numbers;
+        if (numbers.size() == 6) {
+            return numbers;
+        } else {
+            System.out.println("[ERROR] 6개의 숫자를 입력하세요.");
+            return parseNumbers(readInput("다시 입력해주세요"));
+        }
     }
 
-    private boolean areValidNumbers(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                return false;
-            }
-        }
-        return true;
+    private boolean isValidWinNumbers(List<Integer> winNumbers) {
+        return winNumbers.size() == 6 && winNumbers.stream().allMatch(n -> n >= 1 && n <= 45) && winNumbers.size() == winNumbers.stream().distinct().count();
+    }
+
+    private boolean isValidBonusNumber(int bonusNumber) {
+        return bonusNumber >= 1 && bonusNumber <= 45;
     }
 }

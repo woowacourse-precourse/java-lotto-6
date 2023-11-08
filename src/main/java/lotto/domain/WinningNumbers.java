ea140b4455cfe8a031a1d81800e65901fdb6a4ff
peeaaccee <wlgp8958@naver.com>
2023-11-08 16:17:06 +0900

    feat(WinningNumbers): 당첨번호 입력 시 컴마가 연속되는지 검사

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class WinningNumbers {
     private final List<Integer> numbers;
+    private static final String CONSECUTIVE_COMMA_ERROR_MESSAGE = "[ERROR] 입력에 쉼표가 연속해서 있습니다.";
 
     public WinningNumbers(List<Integer> numbers) {
         validate(numbers);
@@ -11,10 +12,19 @@ public class WinningNumbers {
     }
 
     private void validate(List<Integer> numbers) {
-        validateLotto(numbers);}
+        validateLotto(numbers);
+        validateConsecutiveCommas(numbers);
+    }
 
     private void validateLotto(List<Integer> numbers) {
         Lotto lotto = new Lotto(numbers);
         lotto.validate(numbers);
+    }
+
+    private void validateConsecutiveCommas(List<Integer> numbers) {
+        String winningNumbersString = numbers.toString().replaceAll("\\s*,\\s*", ",");
+        if (winningNumbersString.contains(",,") || winningNumbersString.startsWith(",") || winningNumbersString.endsWith(",")) {
+            throw new IllegalArgumentException(CONSECUTIVE_COMMA_ERROR_MESSAGE);
+        }
     }
 }

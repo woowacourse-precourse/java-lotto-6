aaa84089098d00c0100f8274d6a0aae24083a023
DrivenByCode <pjk146195@gmail.com>
2023-11-08 17:11:06 +0900

    feat(LottoCounter): 모든 로또 출력 기능 추가

--- src/main/java/output/LottoCounter.java
+++ src/main/java/output/LottoCounter.java
@@ -1,6 +1,8 @@
 package output;
 
 import constant.ServiceMessage;
+import java.util.List;
+import lotto.Lotto;
 
 public class LottoCounter {
     public static int countLotto(final int money) {
@@ -8,5 +10,12 @@ public class LottoCounter {
         System.out.println();
         System.out.print(String.valueOf(n).concat(ServiceMessage.getMessageByCode(102)));
         return n;
+    }
+
+    public static void printLottos(final int money) {
+        List<List<Integer>> lottos = Lotto.getLottos(money);
+        for(List<Integer> lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
     }
 }

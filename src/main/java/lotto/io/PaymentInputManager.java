79b3661f7dffcefda63792cb2c80da706aa649e4
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:18:21 +0900

    [FIX] 검증 분리

--- src/main/java/lotto/io/PaymentInputManager.java
+++ src/main/java/lotto/io/PaymentInputManager.java
@@ -1,6 +1,6 @@
 package lotto.io;
 
-import java.util.NoSuchElementException;
+import static java.text.MessageFormat.format;
 
 public class PaymentInputManager extends InputManager<Integer> {
     private static PaymentInputManager INSTANCE;
@@ -15,25 +15,30 @@ public class PaymentInputManager extends InputManager<Integer> {
 
 
     @Override
-    public Integer input() {
+    public Integer input() throws IllegalArgumentException{
         while (true) {
             try {
+                System.out.println("구입금액을 입력해 주세요.");
                 String inputLine = consoleAdapter.readLine();
-                return validation(inputLine);
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+                return validation(inputLine) / 1000;
             } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
-            } catch (NoSuchElementException e) {
-                throw new IllegalArgumentException("[ERROR] 입력이 존재하지 않습니다.");
+                System.out.println(e.getMessage());
             }
         }
     }
 
     @Override
-    public Integer validation(String inputLine) throws NumberFormatException {
-        int money = Integer.parseInt(inputLine);
-        if (money % 1000 != 0 || money / 1000 == 0) throw new IllegalArgumentException();
+    public Integer validation(String inputLine) throws IllegalArgumentException {
+        int money;
+        try {
+            money = Integer.parseInt(inputLine);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
+        }
+        if (money % 1000 != 0 || money / 1000 == 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+        }
+        System.out.println(format("{0}개를 구매했습니다.", money / 1000));
         return money;
     }
 }

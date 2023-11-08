703b2a92b7844710dcee0e32ec26d19e3c3e7921
realisshomyang <realisshoman@kakao.com>
2023-11-08 16:04:54 +0900

    feat : 사용자가 구매한 로또 정보를 담은 PurchasedLottos 구현

---
+++ src/main/java/lotto/PurchasedLottos.java
@@ -0,0 +1,26 @@
+package lotto;
+
+import static lotto.LottoGenerator.makeRandomNumbersInLotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchasedLottos {
+    private List<Lotto> lottos = new ArrayList<>();
+    private final Purchase purchase;
+    public PurchasedLottos(Purchase purchase) {
+        this.purchase = purchase;
+        for (int i = 0; i < purchase.getTicketCount(); i++) {
+            Lotto lotto = new Lotto(makeRandomNumbersInLotto());
+            lottos.add(lotto);
+        }
+    }
+    public String LottosToString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            stringBuilder.append(lotto.toString());
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}

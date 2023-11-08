0e46f2866bf42a872755d34cb6e597669767cd43
JJangcoding <bymyself103@naver.com>
2023-11-08 13:18:35 +0900

    로또 도메인의 보너스 번호 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,36 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        compare(numbers);
+        range(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("[ERROR] 6개의 숫자를 입력해주세요.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void compare(List<Integer> numbers) {
+        Set<Integer> numset = new HashSet<>(numbers);
+        if (numset.size() != numbers.size()) throw new IllegalArgumentException("[ERROR] 중복 요소가 있습니다.");
+    }
+
+    private void range(List<Integer> numbers) {
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < 1 || numbers.get(i) > 45)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
+    }
 }

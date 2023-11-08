3334116a50bba3d7defa488799a79c87db3e634b
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 13:38:43 +0900

    feat(util) : 로또 넘버의 범위가 1~45 사이인지 검증

--- src/main/java/lotto/util/validator/LottoInputValidator.java
+++ src/main/java/lotto/util/validator/LottoInputValidator.java
@@ -4,6 +4,8 @@ import lotto.util.validator.constant.ErrorMessageConstant;
 
 import java.util.Arrays;
 
+import static lotto.util.validator.constant.ErrorMessageConstant.*;
+
 public class LottoInputValidator implements InputValidator{
     @Override
     public void validate(String checkValue) {
@@ -12,16 +14,26 @@ public class LottoInputValidator implements InputValidator{
 
     private static void validateCount(String[] checkValues) {
         if(checkValues.length != 6) {
-            throw new IllegalArgumentException(ErrorMessageConstant.LOTTO_LENGTH_NOT_SIX_MESSAGE.getMessage());
+            throw new IllegalArgumentException(LOTTO_LENGTH_NOT_SIX_MESSAGE.getMessage());
         }
     }
     private static void validateConsistingNumber(String[] checkValues) {
         try {
-            for(int i = 0; i < checkValues.length; i++) {
-                int number = Integer.parseInt(checkValues[i]);
-            }
+            Arrays.stream(checkValues).forEach(Integer::parseInt);
         }catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessageConstant.LOTTO_NUMBER_IS_NOT_NUMERIC.getMessage());
+            throw new IllegalArgumentException(LOTTO_NUMBER_IS_NOT_NUMERIC.getMessage());
         }
+    }
+
+    private static void validateNumberRange(String[] checkValues) {
+        if(Arrays.stream(checkValues)
+                .mapToInt(Integer::parseInt)
+                .anyMatch(LottoInputValidator::isValidRange)) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_RANGE_ERROR_MESSAGE.getMessage());
+        }
+    }
+
+    private static boolean isValidRange(int number) {
+        return number < 1 || number > 45;
     }
 }

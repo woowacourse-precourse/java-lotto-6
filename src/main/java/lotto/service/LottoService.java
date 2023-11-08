b998f475bf28ad3c132cfe7412bf574f0362b96b
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:01:38 +0900

    refactor(LottoService): 매직 넘버 상수로 변경

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,15 @@
 package lotto.service;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+import static lotto.constants.Constants.INCREMENT_BY_ONE;
+import static lotto.constants.Constants.INITIAL_COUNT;
+import static lotto.constants.Constants.INITIAL_VALUE_ZERO;
+import static lotto.constants.Constants.LOTTO_NUMBER_COUNT;
+import static lotto.constants.Constants.MAX_LOTTO_NUMBER;
+import static lotto.constants.Constants.MIN_LOTTO_NUMBER;
+import static lotto.constants.Constants.MIN_PURCHASE_AMOUNT;
+import static lotto.constants.Constants.PERCENTAGE_MULTIPLIER;
+import static lotto.constants.Constants.START_INDEX;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,9 +26,9 @@ public class LottoService {
     public ArrayList<Lotto> generateLotto(PurchaseAmount purchaseAmount) {
         ArrayList<Lotto> lottos = new ArrayList<>();
 
-        int count = purchaseAmount.getMoney() / 1000;
+        int count = purchaseAmount.getMoney() / MIN_PURCHASE_AMOUNT;
 
-        for (int i = 0; i < count; i++) {
+        for (int i = START_INDEX; i < count; i++) {
             Lotto lotto = new Lotto(generateLottoNumbers());
             lottos.add(lotto);
         }
@@ -28,7 +37,7 @@ public class LottoService {
     }
 
     public List<Integer> generateLottoNumbers() {
-        ArrayList<Integer> numbers = new ArrayList<>(pickUniqueNumbersInRange(1, 45, 6));
+        ArrayList<Integer> numbers = new ArrayList<>(pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT));
         Collections.sort(numbers);
         return numbers;
     }
@@ -38,19 +47,19 @@ public class LottoService {
 
         for (Lotto lotto : lottos) {
             Rank rank = winningLotto.calculateRank(lotto);
-            rankCount.put(rank, rankCount.getOrDefault(rank, 0) + 1);
+            rankCount.put(rank, rankCount.getOrDefault(rank, INITIAL_COUNT) + INCREMENT_BY_ONE);
         }
 
         return rankCount;
     }
 
     public double calculateProfitRate(Map<Rank, Integer> rankCount, PurchaseAmount purchaseAmount) {
-        int totalWinningAmount = 0;
+        int totalWinningAmount = INITIAL_VALUE_ZERO;
 
         for (Rank rank : Rank.values()) {
-            totalWinningAmount += rank.getWinningAmount() * rankCount.getOrDefault(rank, 0);
+            totalWinningAmount += rank.getWinningAmount() * rankCount.getOrDefault(rank, INITIAL_COUNT);
         }
 
-        return (double) totalWinningAmount / (double) purchaseAmount.getMoney() * 100.0;
+        return (double) totalWinningAmount / (double) purchaseAmount.getMoney() * PERCENTAGE_MULTIPLIER;
     }
 }

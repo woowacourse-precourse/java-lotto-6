80ba63450a907f59cdb844546122556ade6dec49
icebear0111 <wltjq0901@naver.com>
2023-11-08 11:33:07 +0900

    feat: determinging each lotto and counting ranks

--- src/main/java/lotto/GameManager.java
+++ src/main/java/lotto/GameManager.java
@@ -11,6 +11,9 @@ public class GameManager {
     User user = new User();
     Output output = new Output();
     List<Lotto> lotto = new ArrayList<>();
+    List<Integer> winningNumber;
+    int bonusNumber;
+    int[] countRankArray = {0, 0, 0, 0, 0};
     public void start() {
         int inputPrice = user.inputPrice();
         System.out.println();
@@ -22,11 +25,12 @@ public class GameManager {
             lotto.add(new Lotto(lottoNumber));
         }
         System.out.println();
-        List<Integer> winningNumber = user.winningNumbers();
+        winningNumber = user.winningNumbers();
         System.out.println();
-        int bonusNumber = user.bonusNumber();
+        bonusNumber = user.bonusNumber();
         System.out.println();
-        output.printResult(winningNumber, bonusNumber);
+        checkEachLotto();
+        printResult();
     }
 
     public int buyLotto(int inputPrice) {
@@ -37,5 +41,60 @@ public class GameManager {
         List<Integer> lottoNumber = Randoms.pickUniqueNumbersInRange(1, 45, 6);
         sort(lottoNumber);
         return lottoNumber;
+    }
+
+    public void checkEachLotto() {
+        for (int i = 0; i < lotto.size(); i++) {
+            List<Integer> userLotto = lotto.get(i).getNumbers();
+            List<Integer> winningLotto = winningNumber;
+
+            int matchCnt = countMatchingNumber(userLotto, winningLotto);
+            Rank rank = getRank(lotto, matchCnt);
+
+            countEachRank(rank, countRankArray);
+        }
+    }
+
+    public int countMatchingNumber(List<Integer> userLotto, List<Integer> winningLotto) {
+        int matchCnt = 0;
+
+        for (int num : userLotto) {
+            if (winningLotto.contains(num))
+                matchCnt++;
+        }
+
+        return matchCnt;
+    }
+
+    public Rank getRank(List<Lotto> lotto, int matchCnt) {
+        if (matchCnt == 6)
+            return Rank.FIRST;
+        if (matchCnt == 5 && lotto.contains(bonusNumber))
+            return Rank.SECOND;
+        if (matchCnt == 5 && !lotto.contains(bonusNumber))
+            return Rank.THIRD;
+        if (matchCnt == 4)
+            return Rank.FOURTH;
+        if (matchCnt == 3)
+            return Rank.FIFTH;
+
+        return Rank.NONE;
+    }
+
+    public void countEachRank(Rank rank, int[] countRankArray) {
+        if (rank == Rank.FIFTH)
+            countRankArray[0]++;
+        if (rank == Rank.FOURTH)
+            countRankArray[1]++;
+        if (rank == Rank.THIRD)
+            countRankArray[2]++;
+        if (rank == Rank.SECOND)
+            countRankArray[3]++;
+        if (rank == Rank.FIRST)
+            countRankArray[4]++;
+    }
+
+    public void printResult() {
+        output.printResult(countRankArray);
     }
 }

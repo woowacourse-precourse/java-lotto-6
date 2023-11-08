8a65eaa7b4b2f421f83591be3093e1ad62c3b7d4
JW0321 <digneotn123@gmail.com>
2023-11-08 23:37:19 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/lotto/model/Purchase.java
+++ src/main/java/lotto/model/Purchase.java
@@ -4,17 +4,18 @@ import static lotto.util.ErrorMessage.*;
 
 public class Purchase {
 
+    private final int LOTTO_PRICE = 1000;
     private int purchaseAmount;
     private int purchaseNumber;
 
     public void setPurchase(int purchaseMoney) {
         validatePurchaseAmount(purchaseMoney);
         this.purchaseAmount = purchaseMoney;
-        this.purchaseNumber = purchaseMoney / 1000;
+        this.purchaseNumber = purchaseMoney / LOTTO_PRICE;
     }
 
     private void validatePurchaseAmount(int purchaseMoney) {
-        if (purchaseMoney % 1000 != 0) {
+        if (purchaseMoney % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE_PURCHASE_AMOUNT_CANT_DIVIDE_BY_THOUSAND);
         }
         if (purchaseMoney == 0) {

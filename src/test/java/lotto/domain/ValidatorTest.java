e684acd1e1755e381dc943c579feeb2d7c1df66a
jd99iam <96513365+jd99iam@users.noreply.github.com>
2023-11-08 13:38:53 +0900

    test: ValidatorTest 추가
    당첨 숫자 검증 기능 테스트
    - 당첨 숫자가 6자리가 아닐 경우 예외 발생
    - 당첨 숫자가 1~45 범위를 벗어날 경우 예외 발생
    - 당첨 숫자가 중복될 경우 예외 발생
    - 당첨 숫자가 조건을 만족할 경우 정상 실행

--- src/test/java/lotto/domain/ValidatorTest.java
+++ src/test/java/lotto/domain/ValidatorTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +28,23 @@ class ValidatorTest {
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessageContaining(NOT_DIVIDED_PURCHASE_AMOUNT),
                 () -> assertThatCode(() -> validator.validatePurchaseAmount(1000))
+                        .doesNotThrowAnyException()
+        );
+    }
+
+    @Test
+    void 당첨_숫자_검증이_가능하다() {
+        assertAll(
+                () -> assertThatThrownBy(() -> validator.validateWinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(INVALID_WINNING_NUMBER_LENGTH),
+                () -> assertThatThrownBy(() -> validator.validateWinningNumbers(List.of(1, 2, 3, 4, 5, 46)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(WINNING_NUMBER_OUT_OF_RANGE),
+                () -> assertThatThrownBy(() -> validator.validateWinningNumbers(List.of(1, 2, 3, 4, 6, 6)))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(DUPLICATE_WINNING_NUMBER),
+                () -> assertThatCode(() -> validator.validateWinningNumbers(List.of(1, 2, 3, 4, 5, 6)))
                         .doesNotThrowAnyException()
         );
     }

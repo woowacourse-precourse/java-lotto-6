f1d97dafd24f6a8bef95f67d8ec0106cca40148f
SWtheWhite <swthewhite@gmail.com>
2023-11-08 20:50:22 +0900

    feat: 로또 구매 - 로또 번호 생성 및 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,11 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Application {
     private static final int LOTTO_MIN_NUMBER = 1;
@@ -11,6 +16,9 @@ public class Application {
     public static void main(String[] args) {
         int purchaseAmount = inputPurchaseAmount();
         int lottoCount = purchaseAmount / LOTTO_ONE_PRICE;
+
+        List<Lotto> lottos = generateLottos(lottoCount);
+        printLottos(lottos);
     }
 
     private static int inputPurchaseAmount() {
@@ -36,4 +44,22 @@ public class Application {
         }
         return true;
     }
+    private static List<Lotto> generateLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, 6);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
+
+    private static void printLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            List<Integer> numbers = new ArrayList<>(lotto.getNumbers());
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
+    }
+
 }

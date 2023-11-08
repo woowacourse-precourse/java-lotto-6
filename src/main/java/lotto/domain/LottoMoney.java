1be73d5b5feac53204054d9bbf50ee7b040f8138
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 18:06:34 +0900

    refactor(LottoMoney) : final 붙이기 - mathchNumber, money, matchBonus에 final 붙여주기

--- src/main/java/lotto/domain/LottoMoney.java
+++ src/main/java/lotto/domain/LottoMoney.java
@@ -1,17 +1,17 @@
 package lotto.domain;
 
 public enum LottoMoney {
-    ZERO(0,0,false),
-    FIFTH(3,5_000,false),
-    FOURTH(4,50_000,false),
-    THIRD(5,150_0000,false),
-    SECOND(5,30_000_000, true),
-    FIRST(6,2_000_000_000,false);
+    ZERO(0, 0, false),
+    FIFTH(3, 5_000, false),
+    FOURTH(4, 50_000, false),
+    THIRD(5, 150_0000, false),
+    SECOND(5, 30_000_000, true),
+    FIRST(6, 2_000_000_000, false);
 
 
-    private int matchNumber;
-    private int money;
-    private boolean matchBonus;
+    private final int matchNumber;
+    private final int money;
+    private final boolean matchBonus;
 
     LottoMoney(int matchNumber, int money, boolean matchBonus) {
         this.matchNumber = matchNumber;
@@ -19,9 +19,9 @@ public enum LottoMoney {
         this.matchBonus = matchBonus;
     }
 
-    public static LottoMoney getLotto(int number, boolean bonus){
-        for(LottoMoney lottoMoney : LottoMoney.values()){
-            if(lottoMoney.matchNumber == number && lottoMoney.matchBonus == bonus){
+    public static LottoMoney getLotto(int number, boolean bonus) {
+        for (LottoMoney lottoMoney : LottoMoney.values()) {
+            if (lottoMoney.matchNumber == number && lottoMoney.matchBonus == bonus) {
                 return lottoMoney;
             }
         }
@@ -31,7 +31,6 @@ public enum LottoMoney {
     public int getMatchCount() {
         return matchNumber;
     }
-
 
     public int getMoney() {
         return money;

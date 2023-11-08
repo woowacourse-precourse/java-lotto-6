0b7996cf84994a359605bb390fcde169caf9aee7
jiwoo <zbqmgldjfh@gmail.com>
2023-11-08 14:04:41 +0900

    feat(WinningResult): WinningResult에 rewardMoney 상태값 정의

--- src/main/java/lotto/dto/WinningResult.java
+++ src/main/java/lotto/dto/WinningResult.java
@@ -1,16 +1,39 @@
 package lotto.dto;
 
+import lotto.domain.Money;
 import lotto.domain.Rank;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class WinningResult {
 
     private final Map<Rank, Integer> ranks;
+    private final Money rewardMoney;
 
     public WinningResult(Map<Rank, Integer> ranks) {
         this.ranks = ranks;
+        this.rewardMoney = calculateRewardMoney();
+    }
+
+    private Money calculateRewardMoney() {
+        List<Money> winningMoney = new ArrayList<>();
+        for (Rank rank : ranks.keySet()) {
+            for (Integer i = 0; i < ranks.get(rank); i++) {
+                winningMoney.add(rank.getReward());
+            }
+        }
+        return calculateEarning(winningMoney);
+    }
+
+    private static Money calculateEarning(List<Money> winningMoney) {
+        Money earning = new Money(0);
+        for (Money money : winningMoney) {
+            earning = earning.plus(money);
+        }
+        return earning;
     }
 
     public Map<Rank, Integer> getRanks() {

dfa514638183d42af4bcf9b01f16017720827c34
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-08 15:15:04 +0900

    recator(view): View 계층 상수값 enum클래스로 관리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,22 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import static lotto.view.ViewMessage.*;
 
 public class InputView {
-    static final String INPUT_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
-    static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 
     public static String inputPayment() {
-        System.out.println(INPUT_PURCHASE_MESSAGE);
+        System.out.println(INPUT_PURCHASE_MESSAGE.getMessage());
         return Console.readLine();
     }
 
     public static String inputWinningNumber() {
-        System.out.println("\n" + INPUT_WINNING_NUMBER_MESSAGE);
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE.getMessage());
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
-        System.out.println("\n" + INPUT_BONUS_NUMBER_MESSAGE);
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE.getMessage());
         return Console.readLine();
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,27 +3,21 @@ package lotto.view;
 import lotto.domain.Result;
 
 import java.util.List;
+import static lotto.view.ViewMessage.*;
 
 public class OutputView {
-    static final String PURCHASE_OUTPUT_MESSAGE = "개를 구매했습니다.";
 
     public static void printNumberOfLotto(int count) {
-        System.out.println("\n" + count + PURCHASE_OUTPUT_MESSAGE);
+        System.out.printf(PRINT_PURCHASE_MESSAGE.getMessage(), count);
     }
 
     public static void printResult(Result result) {
         List<Integer> ranks = result.getRanks();
-        System.out.printf("\n당첨 통계\n"
-                        + "---\n"
-                        + "3개 일치 (5,000원) - %d개\n"
-                        + "4개 일치 (50,000원) - %d개\n"
-                        + "5개 일치 (1,500,000원) - %d개\n"
-                        + "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n"
-                        + "6개 일치 (2,000,000,000원) - %d개\n"
+        System.out.printf(String.valueOf(PRINT_WINNING_STATISTICS_MESSAGE.getMessage())
                 , ranks.get(5), ranks.get(4), ranks.get(3), ranks.get(2), ranks.get(1));
     }
 
     public static void printIncomeRate(double incomeRate) {
-        System.out.printf("총 수익률은 %.1f%%입니다.", incomeRate);
+        System.out.printf(PRINT_INCOME_RATE_MESSAGE.getMessage(), incomeRate);
     }
 }

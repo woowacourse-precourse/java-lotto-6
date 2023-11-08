7586dd6b7e74d13fdd5c6d413753b7cd17619125
lee-JunR <wns2610@icloud.com>
2023-11-08 23:28:43 +0900

    refactor : "매직 넘버 적용"

--- src/main/java/lotto/service/ProfitCalculationService.java
+++ src/main/java/lotto/service/ProfitCalculationService.java
@@ -1,9 +1,10 @@
 package lotto.service;
+
 import java.util.List;
 import lotto.constant.Prize;
 
 public class ProfitCalculationService {
-
+  public static final int TICKET_PRICE = 10;
   public double calculateProfitRate(int purchaseAmount, List<Integer> winningRanks) {
     int totalPrize = calculateTotalPrize(winningRanks);
     return calculateProfit(purchaseAmount, totalPrize);
@@ -11,13 +12,16 @@ public class ProfitCalculationService {
 
   private int calculateTotalPrize(List<Integer> winningRanks) {
     int totalPrize = 0;
+    int i = 0;
+    winningRanks.remove(winningRanks.size() - 1); // 꽝인 복권들 제외
     for (int rank : winningRanks) {
-      totalPrize += Prize.values()[rank - 1].getPrizeMoney();
+      totalPrize += Prize.values()[i].getPrizeMoney() * rank;
+      i++;
     }
     return totalPrize;
   }
 
   private double calculateProfit(int purchaseAmount, int totalPrize) {
-    return (double) totalPrize / purchaseAmount;
+    return (double) totalPrize / (purchaseAmount * TICKET_PRICE);
   }
 }

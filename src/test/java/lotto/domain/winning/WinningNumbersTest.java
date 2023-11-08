7af81f269389984bc00ebbff354a8f835455a3c4
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-08 19:29:54 +0900

    test(WinningNumbersTest): 당첨 번호가 6개의 수이고, 중복이 없으며, 1이상 45이하인지 검증하는 테스트 구현

---
+++ src/test/java/lotto/domain/winning/WinningNumbersTest.java
@@ -0,0 +1,33 @@
+package lotto.domain.winning;
+
+import lotto.Lotto;
+import lotto.domain.BonusNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class WinningNumbersTest {
+    @DisplayName("당첨 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByOverSize() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByDuplicatedNumber() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("당첨 번호가 1미만 45초과인 경우 예외가 발생한다.")
+    @Test
+    void createWinningNumbersByOverRange() {
+        assertThatThrownBy(() -> new WinningNumbers(List.of(0, 1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

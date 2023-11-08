7af21a6db882ef3b000391ead6ec2b77a21b4392
K-KY <kyuyoungk@naver.com>
2023-11-08 17:07:36 +0900

    Refactor(ValidateMoney) : 매직넘버 상수화, 불필요한 리턴 제거

--- src/main/java/lotto/domain/validator/ValidateMoney.java
+++ src/main/java/lotto/domain/validator/ValidateMoney.java
@@ -1,27 +1,27 @@
 package lotto.domain.validator;
 
-import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
-
 public class ValidateMoney {
     private final static int MAX_VALUE = 1000000;
+    private static final int UNIT = 1000;
+    private static final int ZERO = 0;
 
-    public static ThrowingCallable isOverThousand(int money) {
-        if (money >= 1000) {
-            return null;
+    public static void isOverThousand(int money) {
+        if (money >= UNIT) {
+            return;
         }
         throw new IllegalArgumentException("[ERROR] 천보다 큰 금액을 입력해주세요.");
     }
 
-    public static ThrowingCallable isUnitFollowed(int money) {
-        if (money % 1000 == 0) {
-            return null;
+    public static void isUnitFollowed(int money) {
+        if (money % UNIT == ZERO) {
+            return;
         }
         throw new IllegalArgumentException("[ERROR] 금액은 천원 단위로 입력 해주세요.");
     }
 
-    public static ThrowingCallable isMoneyInRange(int money) {
+    public static void isMoneyInRange(int money) {
         if (money < MAX_VALUE) {
-            return null;
+            return;
         }
         throw new IllegalArgumentException("[ERROR] 100만을 넘는 금액은 입력 할 수 없습니다.");
     }

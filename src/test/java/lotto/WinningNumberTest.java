d446be28ecfe682350df17c4e5d981f584e2599d
Hajuuu <hpt0127@naver.com>
2023-11-08 17:33:06 +0900

    test(WinningNumberTest): 당첨 번호 입력 과정에서 발생하는 모든 예외 테스트

--- src/test/java/lotto/WinningNumberTest.java
+++ src/test/java/lotto/WinningNumberTest.java
@@ -1,35 +1,34 @@
 package lotto;
 
 import java.util.List;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class WinningNumberTest {
 
-    @DisplayName("당첨 번호의 개수가 6개가 아니면 예외 발생")
-    @Test
-    void checkWinningNumberLength() {
-        //given
+    @DisplayName("당첨 번호에 따른 예외 테스트")
+    @ParameterizedTest
+    @MethodSource("parameterProvider")
+    void checkWinningNumber(List<Integer> winningNumbers, String message) {
+
         WinningNumber winningNumber = new WinningNumber();
 
-        //when
-        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6, 7);
-
-        //then
         Assertions.assertThatThrownBy(() -> winningNumber.checkWinningNumber(winningNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("[ERROR] 당첨 번호는 6개를 입력해야 합니다.");
+                .hasMessageContaining(message);
     }
 
-    @DisplayName("당첨 번호가 1~45사이의 값이 아니면 예외 발생")
-    @Test
-    void checkWinningNumberRange() {
-        //given
-
-        //when
-
-        //then
+    private static Stream<Arguments> parameterProvider() {
+        return Stream.of(
+                Arguments.arguments(List.of(1, 2, 3, 4, 5, 6, 7), "[ERROR] 당첨 번호는 6개를 입력해야 합니다."),
+                Arguments.arguments(List.of(1, 1, 2, 3, 4, 5), "[ERROR] 당첨 번호는 서로 다른 번호를 입력해야 합니다."),
+                Arguments.arguments(List.of(1, 2, 3, 4, 5, 56), "[ERROR] 당첨 번호는 1부터 45 사이의 숫자여야 합니다.")
+        );
     }
 
     @Test

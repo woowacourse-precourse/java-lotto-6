515d761876a332457df721b0c37a1392750357a9
jiwon1217 <koagjw@gmail.com>
2023-11-08 14:09:57 +0900

    :sparkles: feat: 구입 금액은 음수일 수 없다.

--- src/main/java/lotto/util/Validator.java
+++ src/main/java/lotto/util/Validator.java
@@ -2,9 +2,11 @@ package lotto.util;
 
 public class Validator {
     private static final String INVALID_MONEY_TYPE = "[ERROR] 금액은 숫자여야 합니다.";
+    private static final String MONEY_MUST_NOT_NEGATIVE = "[ERROR] 금액은 음수일 수 없습니다.";
 
     public static void validateMoney(String money) {
         validateMoneyType(money);
+        validateMoneyRange(money);
     }
 
     public static void validateMoneyType(String money) {
@@ -12,6 +14,14 @@ public class Validator {
             Integer.parseInt(money);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_MONEY_TYPE);
+        }
+    }
+
+    public static void validateMoneyRange(String money) {
+        int amount = Integer.parseInt(money);
+
+        if (amount < 0) {
+            throw new IllegalArgumentException(MONEY_MUST_NOT_NEGATIVE);
         }
     }
 }

93808deb397c7bc3ba9cc07aae828cecc7908afd
Yonghyun Yoon <dragonappear1@gmail.com>
2023-11-08 17:44:48 +0900

    fix(OutputMessage): 수익률에서 딜리미터를 삭제한다

--- src/main/java/lotto/io/write/OutputMessage.java
+++ src/main/java/lotto/io/write/OutputMessage.java
@@ -3,7 +3,7 @@ package lotto.io.write;
 public enum OutputMessage {
 
     LOTTO_PURCHASE_AMOUNT_MESSAGE("\n%d개를 구매했습니다."),
-    LOTTO_PROFIT_MESSAGE("총 수익률은 %,.1f%%입니다."),
+    LOTTO_PROFIT_MESSAGE("총 수익률은 %.1f%%입니다."),
     LOTTO_STATISTICS_MESSAGE("%s - %d개"),
     LOTTO_RESULT_MESSAGE("\n당첨 통계\n---");
 

84e22a508312027397dfefeaf9af47e880acc1aa
706com <ehdgusjdbswl@gmail.com>
2023-11-08 21:29:33 +0900

    refactor : enum 을 활용한 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,34 +1,28 @@
 package lotto.view;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import lotto.domain.Lotto;
 
 public class OutputView {
-    private static final String OUTPUT_LOTTO_AMOUNT = "개를 구매했습니다.";
-
+    private static final String OUTPUT_LOTTO_AMOUNT = "를 구매했습니다.";
     private static final String OUTPUT_WINNING_STATISITCS = "당첨 통계";
+
+    private static final String BAR = "---";
+    private static final String AMOUNT = "개";
 
     public static void outputLottoAmountMessage(int amount){
         System.out.println();
-        System.out.println(amount+OUTPUT_LOTTO_AMOUNT);
+        System.out.println(amount+AMOUNT+OUTPUT_LOTTO_AMOUNT);
     }
-
     public static void outputSortingLottoNumber(List<Integer> number){
         System.out.println(number.toString());
     }
 
     public static void outputWinningStatisitcs(HashMap<Integer,Integer> hash, double winningRate){
-        statisitcsView[] views = statisitcsView.values();
         System.out.println('\n'+OUTPUT_WINNING_STATISITCS);
-        System.out.println("---");
-        for(int i=3; i<=6; i++){
-            System.out.println(views[i-3].getMessage()+hash.get(i)+"개");
-            if(i==5){
-                System.out.println(views[4].getMessage()+hash.get(i+2)+"개");
-            }
+        System.out.println(BAR);
+        for(int i=1; i<=5; i++){
+            System.out.println(StatisitcsView.getStatistics(i)+hash.get(i)+AMOUNT);
         }
         System.out.printf("총 수익률은 %.1f%%입니다.",winningRate);
     }

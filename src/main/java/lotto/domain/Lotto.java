6b065a3b1ef44d2e675bb46828630a8878bd9ba7
Jihyun3478 <leejihyun3478@gmail.com>
2023-11-08 16:32:19 +0900

    feat(Lotto): 사용자로부터 당첨 번호를 입력받는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,13 +5,42 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
+//    public Lotto(List<Integer> numbers) {
+//        validate(numbers);
+//        this.numbers = numbers;
+//    }
+//
+//    private void validate(List<Integer> numbers) {
+//        if (numbers.size() != 6) {
+//            throw new IllegalArgumentException();
+//        }
+//    }
+
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        validateAll(numbers);
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
+    private void validateAll(List<Integer> numbers) {
+        validateCount(numbers);
+        validateLengthAndType(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateCount(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateLengthAndType(List<Integer> numbers) {
+        if(!numbers.stream().allMatch(n -> n >=1 && n <= 45)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        if(!(numbers.size() == numbers.stream().distinct().count())) {
             throw new IllegalArgumentException();
         }
     }

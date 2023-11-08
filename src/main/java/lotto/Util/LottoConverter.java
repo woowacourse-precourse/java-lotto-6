e8c2f8b5966b0001e18ce350be9fa60c287e6866
Hyunsoo Kim <sgn06221@gmail.com>
2023-11-08 12:08:43 +0900

    [feat] Util - String으로 입력된 로또 번호 List<Integer>로 변환
    Signed-off-by: Hyunsoo Kim <sgn06221@gmail.com>

--- src/main/java/lotto/Util/LottoConverter.java
+++ src/main/java/lotto/Util/LottoConverter.java
@@ -1,4 +1,19 @@
 package lotto.Util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LottoConverter {
+    private static final String COMMA_SEPARATOR = ",";
+
+    public static List<Integer> convertStringToNumbers(String input) {
+        String[] numberStrings = input.split(COMMA_SEPARATOR);
+
+        List<Integer> numbers = Arrays.stream(numberStrings)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        return numbers;
+    }
 }

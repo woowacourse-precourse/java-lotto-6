15657686c7af1ebfa230682cf7df2fe197c34640
LEE JIEUN <jieun3595@soongsil.ac.kr>
2023-11-08 15:34:03 +0900

    [FEAT] inputView에 공백, 0, String 예외처리 기능 구현 후 재구성
    .

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,24 +1,59 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import lotto.domain.Lotto;
 
 public class InputView {
     private static final String TOTAL_COST = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String ZERO = "0";
 
-    public static String totalCost() {
+    public static int totalCost() {
         System.out.println(TOTAL_COST);
-        return Console.readLine();
+        return parseToInt(input());
     }
 
     public static String winningNumber() {
         System.out.println(WINNING_NUMBER);
-        return Console.readLine();
+        return input();
     }
 
-    public static String bonusNumber() {
+    public static int bonusNumber() {
+        System.out.println();
         System.out.println(BONUS_NUMBER);
-        return Console.readLine();
+        return parseToInt(input());
+    }
+
+    private static String input() {
+        String input = Console.readLine();
+        isBlank(input);
+        return input;
+    }
+
+    private static int parseToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+
+    private static void isBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("입력값을 입력해주세요.");
+        }
+    }
+
+    private void isZero(String input) {
+        if (Objects.equals(input, ZERO)) {
+            throw new IllegalArgumentException("0을 제외한 정확한 입력값을 입력해주세요.");
+        }
     }
 }

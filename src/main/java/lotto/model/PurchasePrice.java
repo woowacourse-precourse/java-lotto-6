92b6b7d0df1a4d6f5c8e58173fa958f2f463a957
kkkkimtaehyeon <kth1927@naver.com>
2023-11-08 12:24:39 +0900

    refactor : 로또 구매 금액 자료형 int -> long

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -3,7 +3,7 @@ package lotto.model;
 public class PurchasePrice {
 
     private final int PIECE_OF_LOTTO;
-    private static final int LOTTO_PRICE_EACH = 1000;
+    private static final int LOTTO_PRICE = 1000;
 
     public PurchasePrice(String priceFromUser){
         isNotDigits(priceFromUser);
@@ -22,13 +22,13 @@ public class PurchasePrice {
     }
 
     private void isUnder1000(String purchasePrice) throws IllegalArgumentException{
-        if(Integer.parseInt(purchasePrice) < LOTTO_PRICE_EACH){
+        if(Long.parseLong(purchasePrice) < LOTTO_PRICE){
             throw new IllegalArgumentException("[ERROR] 로또 한장은 1000원입니다.");
         }
     }
 
     private void isNotDivisibleBy1000(String purchasePrice) throws IllegalArgumentException{
-        if(Integer.parseInt(purchasePrice) % 1000 != 0){
+        if(Long.parseLong(purchasePrice) % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");
         }
     }

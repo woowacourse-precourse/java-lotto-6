82fc952bffd29efe4b0bce5cf23820df97a32b10
DrivenByCode <pjk146195@gmail.com>
2023-11-08 20:47:20 +0900

    feat(BonusNumber): 보너스 번호 입력기능 추가

---
+++ src/main/java/input/BonusNumber.java
@@ -0,0 +1,65 @@
+package input;
+
+import static constant.ErrorMessage.buildErrorMessage;
+
+import constant.ServiceMessage;
+import java.util.List;
+import java.util.Scanner;
+
+public class BonusNumber {
+    private String input;
+    private boolean isValidInput = false;
+
+    public BonusNumber(List<Integer> winningNumbers) {
+        while(!isValidInput) {
+            setBonnusNumber(winningNumbers);
+        }
+    }
+
+    private void setBonnusNumber(List<Integer> winningNumbers){
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println(ServiceMessage.getMessageByCode(104));
+            this.input = scanner.nextLine();
+            isValidInput = isValidate(input, winningNumbers);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private boolean isValidate(final String input, List<Integer> winningNumbers) {
+        isValidCharacters(input);
+        isValidLength(input);
+        isDuplicated(winningNumbers, input);
+        isValidBounds(input);
+        return true;
+    }
+
+    private void isValidBounds(final String input) {
+        int number = Integer.parseInt(input);
+        if(number < 1 || 45 < number) {
+            throw new IllegalArgumentException(buildErrorMessage(904));
+        }
+    }
+    private void isValidLength(final String input) {
+        if(input.length() != 1){
+            throw new IllegalArgumentException(buildErrorMessage(905));
+        }
+    }
+
+    private void isValidCharacters(final String input) {
+        if(!input.matches("[0-9]+")) {
+            throw new IllegalArgumentException(buildErrorMessage(901));
+        }
+    }
+
+    private void isDuplicated(List<Integer> winningNumbers, final String input) {
+         if(winningNumbers.contains(Integer.parseInt(input))) {
+             throw new IllegalArgumentException(buildErrorMessage(908));
+         }
+    }
+
+    public int getBonnusNumber() {
+        return Integer.parseInt(input);
+    }
+}
+
--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,11 +1,10 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import constant.ErrorMessage;
-import constant.ServiceMessage;
+import input.BonusNumber;
 import input.Purchase;
+import input.WinningNumber;
 import java.util.List;
-import output.LottoCounter;
+import output.LottoPrinter;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,6 +12,15 @@ public class Application {
         Purchase purchase = new Purchase();
         int money = purchase.getMoney();
 
-        LottoCounter.printLottos(money);
+        List<List<Integer>> lottos = Lotto.getLottos(money);
+        LottoPrinter.printLottos(lottos);
+
+        WinningNumber wn = new WinningNumber();
+        List<Integer> winningNumbers = wn.getWinningNumbers();
+        System.out.println("winningNumbers = " + winningNumbers);
+
+        BonusNumber bn = new BonusNumber(winningNumbers);
+        int bonnusNumber = bn.getBonnusNumber();
+        System.out.println("bonnusNumber = " + bonnusNumber);
     }
 }

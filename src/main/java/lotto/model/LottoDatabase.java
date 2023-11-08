a1814d706ba91e7849ae697d7580c0e49f0c69e2
hansg931 <hansg0509@cau.ac.kr>
2023-11-08 23:15:55 +0900

    feat(Lotto): 로또의 정보를 모두 처리하는 클래스 작성

---
+++ src/main/java/lotto/model/LottoDatabase.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoDatabase {
+	private List<Lotto> lottoDatabase = new ArrayList<>();
+
+	public void add(Lotto lotto) {
+		lottoDatabase.add(lotto);
+	}
+
+	public int size() {
+		return lottoDatabase.size();
+	}
+
+	public Lotto getIndex(int index) {
+		return lottoDatabase.get(index);
+	}
+
+	public void printLotto() {
+		lottoDatabase.forEach(Lotto::printLotto);
+	}
+}

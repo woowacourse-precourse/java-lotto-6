ad197a9270f21312acc63be00dbf895c07216142
KongJunWoo <joonoo3@naver.com>
2023-11-08 22:52:43 +0900

    feat : 구입한 모든 로또 숫자 출력

--- src/main/java/lotto/view/OutputLotto.java
+++ src/main/java/lotto/view/OutputLotto.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
 import lotto.model.Lotto;
+import java.util.List;
 
 public class OutputLotto {
+
+    public static void printLottoList(List<Lotto> lottoList) {
+        System.out.println("\n" + lottoList.size() + "개를 구매했습니다.");
+
+        for (Lotto lotto : lottoList) {
+            printLotto(lotto);
+        }
+
+    }
 
     //[8, 21, 23, 41, 42, 43] 형식으로 출력
     public static void printLotto(Lotto lotto) {

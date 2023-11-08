96b50ad3f625dcb4099c732ab70344b6145222ee
JunRain <sa2313@tukorea.ac.kr>
2023-11-08 21:18:23 +0900

    Rank count 변수 삭제

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -1,27 +1,21 @@
 package lotto;
 
 public enum Rank {
-	FIRST_PLACE(2000000000, 0),
-	SECOND_PLACE(30000000, 0),
-	THIRD_PLACE(1500000, 0),
-	FOURTH_PLACE(50000, 0),
-	FIFTH_PLACE(5000, 0),
+	FIRST_PLACE(2000000000),
+	SECOND_PLACE(30000000),
+	THIRD_PLACE(1500000),
+	FOURTH_PLACE(50000),
+	FIFTH_PLACE(5000),
 	;
 
 	private final int prizeMoney;
-	private int count;
 
-	Rank(int prizeMoney, int count) {
+	Rank(int prizeMoney) {
 		this.prizeMoney = prizeMoney;
-		this.count = count;
 	}
 
-	public int bePicked() {
-		count++;
+	public int getPrizeMoney() {
 		return prizeMoney;
 	}
 
-	public int getCount() {
-		return count;
-	}
 }
--- src/main/java/lotto/Result.java
+++ src/main/java/lotto/Result.java
@@ -1,22 +1,35 @@
 package lotto;
 
 import static lotto.LottoGame.*;
+import static lotto.Rank.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Result {
-	List<Lotto> lottos;
-	Winner winner;
-	int money;
-	int prizeMoney = 0;
+	private List<Lotto> lottos;
+	private Winner winner;
+	private int money;
+	private int prizeMoney = 0;
+	private Map<Rank, Integer> countMap;
 
 	public Result(List<Lotto> lottos, Winner winner, int money) {
 		this.lottos = lottos;
 		this.winner = winner;
 		this.money = money;
+		this.countMap = createCountMap();
 		calculatePrizeMoney(lottos, winner);
 		show();
+	}
+
+	private Map<Rank, Integer> createCountMap() {
+		Map<Rank, Integer> map = new HashMap<>();
+		for (Rank rank : Rank.values()) {
+			map.put(rank, 0);
+		}
+		return map;
 	}
 
 	private void calculatePrizeMoney(List<Lotto> lottos, Winner winner) {
@@ -30,31 +43,36 @@ public class Result {
 		matchList.retainAll(winner.getNumbers());
 
 		if (matchList.size() == 3)
-			return Rank.FIFTH_PLACE.bePicked();
+			return bePicked(Rank.FIFTH_PLACE);
 		if (matchList.size() == 4)
-			return Rank.FOURTH_PLACE.bePicked();
+			return bePicked(FOURTH_PLACE);
 		if (matchList.size() == 5 && lotto.getNumbers().contains(winner.getNumbers()))
-			return Rank.SECOND_PLACE.bePicked();
+			return bePicked(SECOND_PLACE);
 		if (matchList.size() == 5)
-			return Rank.THIRD_PLACE.bePicked();
+			return bePicked(THIRD_PLACE);
 		if (matchList.size() == 6)
-			return Rank.FIRST_PLACE.bePicked();
+			return bePicked(Rank.FIRST_PLACE);
 		return 0;
 	}
 
-	void show() {
+	private void show() {
 		System.out.println("당첨 통계");
 		System.out.println("---");
-		System.out.println("3개 일치 (5,000원) - " + Rank.FIFTH_PLACE.getCount() + "개");
-		System.out.println("4개 일치 (50,000원) - " + Rank.FOURTH_PLACE.getCount() + "개");
-		System.out.println("5개 일치 (1,500,000원) - " + Rank.THIRD_PLACE.getCount() + "개");
-		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Rank.SECOND_PLACE.getCount() + "개");
-		System.out.println("6개 일치 (2,000,000,000원) - " + Rank.FIRST_PLACE.getCount() + "개");
+		System.out.println("3개 일치 (5,000원) - " + countMap.get(FIFTH_PLACE) + "개");
+		System.out.println("4개 일치 (50,000원) - " + countMap.get(FOURTH_PLACE) + "개");
+		System.out.println("5개 일치 (1,500,000원) - " + countMap.get(THIRD_PLACE) + "개");
+		System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + countMap.get(SECOND_PLACE) + "개");
+		System.out.println("6개 일치 (2,000,000,000원) - " + countMap.get(FIRST_PLACE) + "개");
 		System.out.println("총 수익률은 " + calculateRateOfReturn() + "%입니다.");
 	}
 
 	private String calculateRateOfReturn() {
-		double rateOfReturn = ((double)prizeMoney /(double) (money * MONEY_UNIT)) * 100;
+		double rateOfReturn = ((double)prizeMoney / (double)(money * MONEY_UNIT)) * 100;
 		return String.format("%.1f", rateOfReturn);
+	}
+
+	private int bePicked(Rank rank) {
+		countMap.put(rank, countMap.get(rank) + 1);
+		return rank.getPrizeMoney();
 	}
 }

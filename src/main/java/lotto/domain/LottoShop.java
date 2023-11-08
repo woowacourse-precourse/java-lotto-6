b04902a3903b260d48e8a7b4a30e9d89e05df084
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:12:22 +0900

    [FIX] 출력을 위한 메서드 추가

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,13 +1,9 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import lotto.domain.Lotto;
-import lotto.io.InputManager;
-import lotto.io.PaymentInputManager;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class LottoShop {
     private final Integer lottoCount;
@@ -20,10 +16,14 @@ public class LottoShop {
 
     public void generateLotto(){
         for (int i = 0; i < lottoCount; i++)
-            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1,45,7)));
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(1,45,6)));
     }
 
     public List<Lotto> getLottos() {
         return lottos;
+    }
+
+    public void displayLottos() {
+        lottos.forEach(Lotto::displayLotto);
     }
 }

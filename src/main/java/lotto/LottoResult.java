0f12af6dc2b45d8682d8869a5c6a6242c69e5ce1
leorb159 <ldgsocial159@naver.com>
2023-11-08 17:40:23 +0900

    feat: LottoResult 생성, 로또 당첨 결과 메소드 작성

--- src/main/java/lotto/LottoResult.java
+++ src/main/java/lotto/LottoResult.java
@@ -1,2 +1,23 @@
-package lotto;public class LottoResult {
+package lotto;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoResult {
+    private Map<LottoRank, Integer> rankResult = new EnumMap<LottoRank, Integer>(LottoRank.class);
+
+    public LottoResult(List<Lotto> lottery, WinningLotto winningLotto) {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            rankResult.put(lottoRank, 0);
+        }
+        checkLottery(lottery, winningLotto);
+    }
+
+    private void checkLottery(List<Lotto> lottery, WinningLotto winningLotto) {
+        for (Lotto lotto : lottery) {
+            LottoRank lottoRank = winningLotto.makeRank(lotto);
+            rankResult.put(lottoRank, rankResult.get(lottoRank) + 1);
+        }
+    }
 }

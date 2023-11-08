1fb0f1e8c814ec64afea6479520aa1bdcf492ace
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:16:01 +0900

    [FIX] 테스트 케이스 수정

--- src/test/java/lotto/io/WinningNumberInputManagerTest.java
+++ src/test/java/lotto/io/WinningNumberInputManagerTest.java
@@ -3,10 +3,9 @@ package lotto.io;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningNumberInputManagerTest {
     private WinningNumberInputManager winningNumberInputManager;
@@ -33,27 +32,5 @@ class WinningNumberInputManagerTest {
         consoleAdapter.setInput("1,2,3,4,5,6");
         List<Integer> result = winningNumberInputManager.input();
         assertThat(result).isEqualTo(List.of(1,2,3,4,5,6));
-    }
-    @Test
-    void testInvalidInput() {
-        consoleAdapter.setInput("1.2.3,4,5,6");
-        assertThatThrownBy(() -> winningNumberInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("숫자를 입력해주세요.");
-    }
-    @Test
-    void testNoInput() {
-        consoleAdapter.setInput(null);
-        assertThatThrownBy(() -> winningNumberInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력이 존재하지 않습니다.");
-    }
-
-    @Test
-    void testInvalidRange() {
-        consoleAdapter.setInput("1,2,3,4,5,55");
-        assertThatThrownBy(() -> winningNumberInputManager.input())
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("1~45 범위 내의 값을 입력해주세요.");
     }
 }

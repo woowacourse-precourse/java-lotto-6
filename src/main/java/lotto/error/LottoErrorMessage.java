a7ff0ab30f3c1c2cb7fb6a8e2c8259f26a65c8b5
fromitive <fromitive@gmail.com>
2023-11-08 12:57:28 +0900

    feat: 로또 구입 검증 기능 구현

--- src/main/java/lotto/error/LottoErrorMessage.java
+++ src/main/java/lotto/error/LottoErrorMessage.java
@@ -1,18 +1,20 @@
 package lotto.error;
 
-import static lotto.common.LottoNumberRange.LOTTO_END_NUMBER;
-import static lotto.common.LottoNumberRange.LOTTO_START_NUMBER;
-
 public enum LottoErrorMessage {
-    LOTTO_NUMBER_OUT_OF_RANGE(
-            String.format("로또 범위 이외의 숫자가 생성되었습니다. %d 부터 %d 숫자를 입력해주세요.", 
-                    LOTTO_START_NUMBER, LOTTO_END_NUMBER));
+    LOTTO_NUMBER_OUT_OF_RANGE("로또 범위 이외의 숫자가 생성되었습니다. 1 부터 45 숫자를 입력해주세요."),
+    WRONG_LOTTO_PRICE("잘못된 복권 구입 금액을 입력하셨습니다. 복권 가격(1,000원) 단위로 다시 입력해 주세요."),
+    WRONG_LOTTO_NUMBER("잘못된 로또 번호를 입력하셨습니다. 1 부터 45 숫자를 입력해주세요."),
+    NOT_ENOUGH_MONEY("복권 가격(1,000원) 보다 적은 금액을 입력하셨습니다. 다시 입력해 주세요."),
+    PRICE_DOES_NOT_ZERO("복권 가격은 0이 될 수 없습니다 0 이상의 수로 설정해주세요."),
+    BUYING_PRICE_MUST_PRICE_UNIT("복권의 구입 가격은 복권 가격(1,000원)단위로 입력하셔야 합니다."),
+    DUPLICATED_LOTTO_NUMBERS("로또 번호 중 중복된 번호가 존재합니다."),
+    ALREADY_WINNING_NUMBER("이미 당첨번호에 포함되어 있는 번호입니다. 다른 번호를 입력해주세요."),
+    INVALID_LOTTO_SIZE("로또 번호개수가 잘못되었습니다. 다시 입력해 주세요.");
 
-    private final String message;
+    private String message;
 
     private LottoErrorMessage(String message) {
         this.message = message;
-
     }
 
     public String getMessage() {

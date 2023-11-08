b795d3fd6cf92dffff1c937d30c50decb78da366
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-08 20:12:50 +0900

    test: 테스트 오류 수정

--- src/test/java/lotto/domain/CalculateTotalRankCountArgumentsProvider.java
+++ src/test/java/lotto/domain/CalculateTotalRankCountArgumentsProvider.java
@@ -13,15 +13,24 @@ public class CalculateTotalRankCountArgumentsProvider implements ArgumentsProvid
     @Override
     public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
         return Stream.of(
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 45), 7, Map.of(Rank.FIRST, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 23), 45, Map.of(Rank.SECOND, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 23), 24, Map.of(Rank.THIRD, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 15, 17), 18, Map.of(Rank.FOURTH, 1)),
-                Arguments.arguments(givenNumbers(1, 2, 3, 4, 5, 6), 7, Map.of(Rank.FIFTH, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 15, 16), 17, Map.of(Rank.FOURTH, 1, Rank.FIFTH, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 14, 16, 32), 33, Map.of(Rank.FOURTH, 2)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 11, 14, 22), 24, Map.of(Rank.THIRD, 1, Rank.FIFTH, 1)),
-                Arguments.arguments(givenNumbers(1, 3, 5, 11, 14, 22), 45, Map.of(Rank.SECOND, 1, Rank.FIFTH, 1))
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 45), 7,
+                        Map.of(Rank.FIFTH, 0, Rank.FOURTH, 0, Rank.THIRD, 0, Rank.SECOND, 0, Rank.FIRST, 1)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 23), 45,
+                        Map.of(Rank.FIFTH, 0, Rank.FOURTH, 0, Rank.THIRD, 0, Rank.SECOND, 1, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 22, 23), 24,
+                        Map.of(Rank.FIFTH, 0, Rank.FOURTH, 0, Rank.THIRD, 1, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 15, 17), 18,
+                        Map.of(Rank.FIFTH, 0, Rank.FOURTH, 1, Rank.THIRD, 0, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 2, 3, 4, 5, 6), 7,
+                        Map.of(Rank.FIFTH, 1, Rank.FOURTH, 0, Rank.THIRD, 0, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 15, 16), 17,
+                        Map.of(Rank.FIFTH, 1, Rank.FOURTH, 1, Rank.THIRD, 0, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 14, 16, 32), 33,
+                        Map.of(Rank.FIFTH, 0, Rank.FOURTH, 2, Rank.THIRD, 0, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 11, 14, 22), 24,
+                        Map.of(Rank.FIFTH, 1, Rank.FOURTH, 0, Rank.THIRD, 1, Rank.SECOND, 0, Rank.FIRST, 0)),
+                Arguments.arguments(givenNumbers(1, 3, 5, 11, 14, 22), 45,
+                        Map.of(Rank.FIFTH, 1, Rank.FOURTH, 0, Rank.THIRD, 0, Rank.SECOND, 1, Rank.FIRST, 0))
 
         );
     }

9858b0451a9787410748ddf9fd5c5dfeef844370
jeondui <jeondui@gmail.com>
2023-11-08 22:33:42 +0900

    feat(WinningResult): 사용자의 로또 당첨 순위를 구하는 기능

---
+++ src/main/java/lotto/model/WinningResult.java
@@ -0,0 +1,46 @@
+package lotto.model;
+
+import java.util.EnumMap;
+import java.util.Map;
+import lotto.common.Ranking;
+
+public class WinningResult {
+
+    private final WinningNumbers winningNumbers;
+    private final LottoTickets lottoTickets;
+    private final Lotto lotto;
+    private static Map<Ranking, Integer> result = new EnumMap<>(Ranking.class);
+
+    private WinningResult(WinningNumbers winningNumbers, LottoTickets lottoTickets, Lotto lotto) {
+        this.winningNumbers = winningNumbers;
+        this.lottoTickets = lottoTickets;
+        this.lotto = lotto;
+    }
+
+    private static int determineWinningRank(Lotto lotto, LottoTicket lottoTicket, WinningNumbers winningNumbers) {
+        int count = getMatchingCount(lotto, lottoTicket);
+        if (isSecondRank(lottoTicket, winningNumbers.getBonusNumber(), count)) {
+            return Ranking.SECOND.getRank();
+        }
+        return rankMatchingNumbers(count);
+
+    }
+
+    private static boolean isSecondRank(LottoTicket lottoTicket, Number bonusNumber, int count) {
+        return lottoTicket.hasBonusNumber(bonusNumber) && count == 5;
+    }
+
+    private static int rankMatchingNumbers(int count) {
+        for (Ranking ranking : Ranking.values()) {
+            if (ranking.getMatchCount() == count) {
+                return ranking.getRank();
+            }
+        }
+        return 0;
+    }
+
+    private static int getMatchingCount(Lotto lotto, LottoTicket lottoTicket) {
+        return lotto.countMatchingNumbers(lottoTicket);
+    }
+
+}

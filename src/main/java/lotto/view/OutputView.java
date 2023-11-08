21e6a6d76fb90e070fcc20c06505f03bcb30ded4
kyejin0412 <kyejin0412@kookmin.ac.kr>
2023-11-08 21:06:01 +0900

    feat: 출력 view 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,30 +1,31 @@
-package lotto;
+package lotto.view;
 
-import lotto.Lotto;
+import java.util.List;
+import java.util.Map;
 
 public class OutputView {
-    public void printLottoCount(int count) {
+
+    public static void responseCount(int count) {
         System.out.println(count + "개를 구매했습니다.");
     }
 
-    public void printLottoNumbers(int count, Lotto randomLottoNumbers) {
-        System.out.println(count + "개를 구매했습니다.");
-        
-        for (int i = 0; i < count; i++)
-            System.out.println(randomLottoNumbers);
+    public static void responseLottoNumber(List<Integer> lotto) {
+        System.out.println(lotto);
     }
 
-    public void printResult(enum Result) {
-        System.out.println("당첨 통계 \n---");
-        System.out.println("3개 일치 (5,000원) - " + Result.COUNTTHREE.getValue() + "개");
-        System.out.println("4개 일치 (50,000원) - " + Result.COUNTFOUR.getValue() + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + Result.COUNTFIVE.getValue() + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + Result.COUNTFIVEBONUS.getValue() + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + Result.COUNTSIX.getValue() + "개");
+    public static void responseResult(Map<String, Object> result) {
+        double profitPercentage = (double) result.get("총 수익률");
+
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + result.get("5등") + "개");
+        System.out.println("4개 일치 (50,000원) - " + result.get("4등") + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + result.get("3등") + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + result.get("2등") + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + result.get("1등") + "개");
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitPercentage);
     }
 
-    public void printProfit(int profit) {
-        //double profit = Math.round((prize/purchaseAmount*100)/100.0);
-        System.out.println("총 수익률은 " + profit + "%입니다.");
-    }
+
 }

81cc047e8afc8ad80204bb54a8498a62c32586b0
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-11-08 20:37:37 +0900

    feat: add process to check how many numbers were winning
    Add process to check how many numbers were winning
    - compare values ​​using the method in class Lotto  and output the number of overlaps

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -3,6 +3,8 @@ package lotto;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -20,6 +22,18 @@ public class Lotto {
 
     public List<Integer> getNumbers() {
         return numbers;
+    }
+
+    public int checkWinningNum(List<Integer> winnigNumbers) {
+        return numbers.stream()
+                .filter(old -> winnigNumbers.stream()
+                        .noneMatch(Predicate.isEqual(old)))
+                .toList()
+                .size();
+    }
+
+    public boolean checkBonnusNum(int bonnusNum) {
+        return numbers.contains(bonnusNum);
     }
 
 }

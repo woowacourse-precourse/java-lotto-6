f29021d4bb4db9a388c6fee08d7f21b7524d39c6
jum0624 <jum0624@naver.com>
2023-11-08 20:40:23 +0900

    refactor: 동일한 메서드명으로 인한 코드 리펙터링

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -24,29 +24,26 @@ public class LottoGameController {
     }
 
     public void run() {
-        LottoPurchase lottoPurchase = buyLottos();
+        LottoPurchase lottoPurchase = getMoney();
         showLottoCount(lottoPurchase);
-        // 구매한 금액으로 로또 발행
+
         Lottos lottos = lottoGameService.buyLottos(lottoPurchase);
-        // 발급된 로또 정보 출력
         showLottosNumber(lottos);
-        // 당첨번호, 보너스 번호 입력받기
+
         Lotto winningNumber = getWinningNumber();
         WinningLotto winningLotto = getBonusNumber(winningNumber);
-        // 결과 계산
         Map<RouletteResult, Integer> result = lottoGameService.match(winningLotto, lottos);
-        // 결과 출력하기
         showResult(result);
         showRate(lottoPurchase);
     }
 
-    public LottoPurchase buyLottos() {
+    public LottoPurchase getMoney() {
         try {
             int money = inputView.inputMoney();
             return LottoPurchase.of(money);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return buyLottos();
+            return getMoney();
         }
     }
 

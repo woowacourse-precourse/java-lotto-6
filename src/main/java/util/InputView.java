742da27a963efbfaf8eb581b60830f9e22f2fd0f
kmh0601 <pongkmh@naver.com>
2023-11-08 19:32:40 +0900

    chore: 구입금액 입력을 요구하는 기능 추가

--- src/main/java/util/InputView.java
+++ src/main/java/util/InputView.java
@@ -1,13 +1,16 @@
 package util;
 
 public class InputView {
-    public static final String INPUT_LOTTO = "당첨 번호를 입력해주세요.";
-    public static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요";
-
-    public void printUserInputLotto() {
+    private static final String INPUT_LOTTO = "당첨 번호를 입력해주세요.";
+    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해주세요";
+    private static final String INPUT_USER_MONEY = "구입금액을 입력해주세요.";
+    public static void printUserInputLotto() {
         System.out.println(INPUT_LOTTO);
     }
-    public void printUserBonusNumber() {
+    public static void printUserBonusNumber() {
         System.out.println(INPUT_BONUS_NUMBER);
+    }
+    public static void printUserMoney() {
+        System.out.println(INPUT_USER_MONEY);
     }
 }

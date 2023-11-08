43236fef360e8de3f64b7c74a308e384eb0929dc
yangyanghyunjung <hyunjungyang0922@gmail.com>
2023-11-08 23:49:03 +0900

    🎯feat(util) : 상수 message 관리 클래스 생성

---
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -0,0 +1,9 @@
+package lotto.util;
+
+public class ErrorMessage {
+    public static final String ONLY_1_45_NUMBER = "[ERROR] 1-45 사이의 숫자만 입력하세요.";
+    public static final String ONLY_DIVIDED_THOUSAND_NUMBER = "[ERROR] 금액은 1000단위의 숫자만 입력하세요.";
+    public static final String NO_BLANK = "[ERROR] 빈 값을 입력할 수 없습니다.";
+    public static final String NO_DUPLICATE = "[ERROR] 중복된 번호를 입력할 수 없습니다.";
+    public static final String ONLY_DIVIDED_COMMA_NUMBERS = "[ERROR] ,로 구분된 6자리 숫자를 입력해 주세요.";
+}
---
+++ src/main/java/lotto/util/OutputMessage.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+public class OutputMessage {
+    public static final String PURCHASE_QUANTITY_MESSAGE = "개를 구매했습니다.";
+    public static final String WINNING_RESULT_MESSAGE = "당첨 통계\n---";
+    public static final String CORRECT_THREE = "3개 일치 (5,000원) - ";
+    public static final String CORRECT_FOURE = "4개 일치 (50,000원) - ";
+    public static final String CORRECT_FIVE = "5개 일치 (1,500,000원) - ";
+    public static final String CORRECT_FIVE_AND_BONU = "5개 일치, 보너스 볼 일치 (30,000,000원) - ";
+    public static final String CORRECT_SIX = "6개 일치 (2,000,000,000원) - ";
+    public static final String TOTAL_RATE = "총 수익률은 ";
+    public static final String GAE = "개\n";
+    public static final String IS_PERCENT = "%입니다.";
+}

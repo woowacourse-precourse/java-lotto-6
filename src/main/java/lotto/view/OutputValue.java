e9b3eda00283532ca07a9ebd5e1d7944c809dba9
GiJung <weeds2577@gmail.com>
2023-11-08 17:14:41 +0900

    refactor(OutputValue): 메세지 출력 형식을 간단하게 변경

--- src/main/java/lotto/view/OutputValue.java
+++ src/main/java/lotto/view/OutputValue.java
@@ -10,11 +10,8 @@ public class OutputValue {
     private static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     private static final String WIN_STATISTICS_MESSAGE = "당첨 통계";
     private static final String LINE_BREAK = "---";
-    private static final String FIFTH_PLACE_MESSAGE = "3개 일치 (5,000원) - %d개";
-    private static final String FOURTH_PLACE_MESSAGE = "4개 일치 (50,000원) - %d개";
-    private static final String THIRD_PLACE_MESSAGE = "5개 일치 (1,500,000원) - %d개";
-    private static final String SECOND_PLACE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
-    private static final String FIRST_PLACE_MESSAGE = "6개 일치 (2,000,000,000원) - %d개";
+    private static final String WINNING_SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String WINNING_RESULT_MESSAGE = "%d개 일치 (%s원) - %d개";
     private static final String REVENUE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public static void purchaseMessage() {
@@ -46,25 +43,14 @@ public class OutputValue {
         System.out.println(LINE_BREAK);
     }
 
-    public static void firstPlaceMessage(int count) {
-        System.out.println(String.format(FIRST_PLACE_MESSAGE, count));
+    public static void winningLottoResultMessage(int matchCount, String price, int count) {
+        System.out.println(String.format(WINNING_RESULT_MESSAGE, matchCount, price, count));
     }
 
-    public static void secondPlaceMessage(int count) {
-        System.out.println(String.format(SECOND_PLACE_MESSAGE, count));
+    public static void winningLottoSecondPrizeMessage (int matchCount, String price, int count) {
+        System.out.println(String.format(WINNING_SECOND_MESSAGE, matchCount, price, count));
     }
 
-    public static void thirdPlaceMessage(int count) {
-        System.out.println(String.format(THIRD_PLACE_MESSAGE, count));
-    }
-
-    public static void fourthPlaceMessage(int count) {
-        System.out.println(String.format(FOURTH_PLACE_MESSAGE, count));
-    }
-
-    public static void fifthPlaceMessage(int count) {
-        System.out.println(String.format(FIFTH_PLACE_MESSAGE, count));
-    }
 
     public static void revenueMessage(float revenue) {
         System.out.println(String.format(REVENUE_MESSAGE, revenue));

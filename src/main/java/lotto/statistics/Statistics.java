d9a85aa40c7005b104b68a884183b88d41decb2b
kimminsu <xjfcnfw3@naver.com>
2023-11-08 12:02:00 +0900

    refactor: 로또 통계에서 결과 객체 분리

--- src/main/java/lotto/statistics/Statistics.java
+++ src/main/java/lotto/statistics/Statistics.java
@@ -1,7 +1,5 @@
 package lotto.statistics;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lotto.money.Money;
@@ -9,29 +7,17 @@ import lotto.ranking.Ranking;
 
 public class Statistics {
 
-    private static final int INITIAL_COUNT = 0;
     private static final int INITIAL_TOTAL_PRIZE = 0;
 
-    private final Map<Ranking, Integer> statistics;
+    private final LottoResult statistics;
 
     public Statistics(List<Ranking> rankings) {
-        this.statistics = new HashMap<>();
-        initStatistics();
-        calculateDistribution(rankings);
+        this.statistics = new LottoResult(rankings);
     }
 
-    private void initStatistics() {
-        for (Ranking ranking : Ranking.values()) {
-            statistics.put(ranking, INITIAL_COUNT);
-        }
-    }
-
-    private void calculateDistribution(List<Ranking> rankings) {
-        rankings.forEach(ranking -> statistics.computeIfPresent(ranking, (key, count) -> ++count));
-    }
 
     public Map<Ranking, Integer> getStatistics() {
-        return Collections.unmodifiableMap(statistics);
+        return statistics.getLottoResult();
     }
 
     public Double calculateProfit(Money money) {
@@ -41,7 +27,7 @@ public class Statistics {
     private Integer getTotalPrize() {
         int prize = INITIAL_TOTAL_PRIZE;
         for (Ranking ranking : Ranking.values()) {
-            prize += ranking.calculatePrize(statistics.get(ranking));
+            prize += ranking.calculatePrize(statistics.getRankingCount(ranking));
         }
         return prize;
     }

452dc042f8bb5ddf425abe24f2a17c030a44d53d
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 15:46:06 +0900

    feat(LottoGameController): 로또 게임 정보 제공 로직 작성

---
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -0,0 +1,32 @@
+package lotto.controller;
+
+import lotto.model.Player;
+import lotto.model.WinningDetail;
+import lotto.model.WinningNumbers;
+import lotto.service.LottoGameService;
+import lotto.util.RandomUtil;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoGameController {
+    private final LottoGameService lottoGameService;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomUtil randomUtil;
+
+    public LottoGameController() {
+        inputView = new InputView();
+        outputView = new OutputView();
+        randomUtil = new RandomUtil();
+        lottoGameService = new LottoGameService(inputView, outputView, randomUtil);
+    }
+
+    public void startGame() {
+        final Player player = lottoGameService.createPlayer();
+        outputView.printLottoInfo(player.getLottos());
+        WinningNumbers winningNumbers = lottoGameService.createWinningNumber();
+        WinningDetail winningDetail = new WinningDetail(player.getLottos(), winningNumbers);
+        outputView.printWinningDetail(winningDetail.getWinningCount());
+        outputView.printProfitRate(player.calculateProfitRate(winningNumbers));
+    }
+}

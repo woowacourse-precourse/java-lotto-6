b2a577619b907b45fb1b815c01757f32c61f0683
kimdongyeon <do121712@naver.com>
2023-11-08 22:17:58 +0900

    feat : 로또를 사고 거스름돈 출력하기 위한 change 변수 추가

--- src/main/java/lotto/domain/Player.java
+++ src/main/java/lotto/domain/Player.java
@@ -5,11 +5,14 @@ public class Player {
     private int quantity;
     private double prize;
 
+    private int change;
+
     public Player(String money) {
         int parsingMoney = checkMoney(money);
         shortCheckMoney(parsingMoney);
         this.money = parsingMoney;
         this.quantity = parsingMoney / 1000;
+        this.change = parsingMoney % 1000;
         this.prize = 0;
     }
 
@@ -26,15 +29,19 @@ public class Player {
             throw new IllegalArgumentException("[ERROR] 1000원 이상의 금액을 입력해 주세요.");
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
     public void sumPrize(int prize) {
         this.prize = this.prize + prize;
     }
 
     public double getPercent() {
         return 100.0 * prize / money;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getChange() {
+        return change;
     }
 }

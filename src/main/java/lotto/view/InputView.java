adc69cc23b40e418c8332927526224aae4fe1661
hyeonwoo <ziczin7176@gmail.com>
2023-11-08 17:17:28 +0900

    feat: 당첨 금액 입력 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import lotto.validation.InputValidation;
 
 public class InputView {
@@ -18,6 +20,19 @@ public class InputView {
                 inputValidation.isNumber(input);
                 inputValidation.isThousandMultiple(input);
                 return Integer.parseInt(input);
+            } catch (IllegalArgumentException error) {
+                System.out.println(error);
+            }
+        }
+    }
+
+    public List<Integer> inputWinningNumbers() {
+        while (true) {
+            try {
+                String input = Console.readLine();
+                inputValidation.validateWinningNumbers(input);
+                String[] inputNumbers = input.split(",");
+                return Arrays.stream(inputNumbers).map(Integer::parseInt).toList();
             } catch (IllegalArgumentException error) {
                 System.out.println(error);
             }

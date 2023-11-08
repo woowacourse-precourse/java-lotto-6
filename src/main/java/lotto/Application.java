b5b399f7b6419a31016517764d2de5382f6abb36
Terry <t3rryahn.dev@gmail.com>
2023-11-08 18:24:36 +0900

    feat: Application 클래스 구현
    LottoController 메서드들 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.model.WinningNumbers;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoController lottoController = new LottoController();
+        lottoController.purchase();
+        lottoController.setWinningNumbers();
+        lottoController.makeResult();
     }
 }

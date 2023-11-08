801bc0d0ddc21b14916d911215a5f5d9ca7e9c10
minju <minju9711@naver.com>
2023-11-08 19:39:09 +0900

    [feat] 공백 삭제

--- src/test/java/lotto/Util/ApplicationTest.java
+++ src/test/java/lotto/Util/ApplicationTest.java
@@ -1,10 +1,9 @@
-package lotto;
+package lotto.Util;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import lotto.Application;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;

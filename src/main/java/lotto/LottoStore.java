438c29108ac2b635eb3835cefcbfff2d0132b221
최휘용 <chy2581@naver.com>
2023-11-08 20:32:50 +0900

    feat: 금액 예외 처리 추가
    0 이하인 금액이 들어올 경우 예외처리하도록 추가함

--- src/main/java/lotto/LottoStore.java
+++ src/main/java/lotto/LottoStore.java
@@ -50,6 +50,9 @@ public class LottoStore {
                 throw new IllegalArgumentException("[ERROR] 숫자로 된 금액을 입력해야 합니다.");
             }
         }
+        if (Integer.parseInt(number) <= Value.ZERO.get()) {
+            throw new IllegalArgumentException("[ERROR] 금액은 양수를 입력해야 합니다")
+        }
         if (Integer.parseInt(number) % Value.LOTTO_PRICE.get() != Value.ZERO.get()) {
             throw new IllegalArgumentException("[ERROR] 1,000원 단위로 금액을 입력해야 합니다.");
         }

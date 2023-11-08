14cb2935050f18915c44c052d97c71e2e165abdc
Sully <2dcoder@naver.com>
2023-11-08 16:23:02 +0900

    test(LottoTest): Lotto 도메인 관련 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,12 +1,11 @@
 package lotto.domain;
 
-import lotto.domain.Lotto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -24,5 +23,42 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1 ~ 45 범위가 아니라면 예외가 발생한다.")
+    @Test
+    void createLottoOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 ~ 45 사이의 숫자만 가능합니다.");
+    }
+
+    @DisplayName("숫자가 올바른 로또라면 true를 반환한다")
+    @Test
+    void checkMatchedNumber() {
+        // given
+        int validNumber = 6;
+        boolean expected = true;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        boolean actual = lotto.hasMatchedNumber(validNumber);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("숫자가 올바르지 않은 로또라면 false를 반환한다")
+    @Test
+    void checkUnmatchedNumber() {
+        // given
+        int invalidNumber = 7;
+        boolean expected = false;
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        // when
+        boolean actual = lotto.hasMatchedNumber(invalidNumber);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
 }

759301bae98598f2623601010cc01af428bdf159
GaShine <100428958+Ga-Long@users.noreply.github.com>
2023-11-08 20:18:55 +0900

    Feat : 발행한 로또 번호와 우승번호 비교

--- src/main/java/lotto/model/Player.java
+++ src/main/java/lotto/model/Player.java
@@ -1,6 +1,11 @@
 package lotto.model;
 
 public class Player {
+    private static int THREE_MATCH = 0;
+    private static int FOUR_MATCH = 0;
+    private static int FIVE_MATCH = 0;
+    private static int FIVE_MATCH_WITH_BONUS = 0;
+    private static int SIX_MATCH = 0;
     private int payment;
     private Lotto lotto;
     private int bonusNumber;
@@ -25,5 +30,45 @@ public class Player {
 
     public void setBonusNumber(int bonusNumber) {
         this.bonusNumber = bonusNumber;
+    }
+
+    public int getThreeMatch() {
+        return THREE_MATCH;
+    }
+
+    public int getFourMatch() {
+        return FOUR_MATCH;
+    }
+
+    public int getFiveMatch() {
+        return FIVE_MATCH;
+    }
+
+    public int getFiveMatchWithBonus() {
+        return FIVE_MATCH_WITH_BONUS;
+    }
+
+    public int getSixMatch() {
+        return SIX_MATCH;
+    }
+
+    public void addThreeMatch() {
+        THREE_MATCH++;
+    }
+
+    public void addFourMatch() {
+        FOUR_MATCH++;
+    }
+
+    public void addFiveMatch() {
+        FIVE_MATCH++;
+    }
+
+    public void addFiveMatchWithBonus() {
+        FIVE_MATCH_WITH_BONUS++;
+    }
+
+    public void addSixMatch() {
+        SIX_MATCH++;
     }
 }
--- src/main/java/lotto/service/StatsService.java
+++ src/main/java/lotto/service/StatsService.java
@@ -1,5 +1,7 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.model.Lotto;
 import lotto.model.Player;
 
 public class StatsService {
@@ -9,4 +11,46 @@ public class StatsService {
         this.player = player;
     }
 
+    public void calculateWinnings(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            int matchCount = countMatchingNumbers(lotto, player.getLotto());
+            addMatch(lotto, matchCount);
+        }
+
+    }
+
+    public int countMatchingNumbers(Lotto lotto, Lotto winningNumber) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        List<Integer> winningNumbers = winningNumber.getNumbers();
+
+        int count = 0;
+        for (int number : lottoNumbers) {
+            if (winningNumbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isContainBonus(Lotto lotto, int bonusNumber) {
+        List<Integer> numbers = lotto.getNumbers();
+        return numbers.contains(bonusNumber);
+    }
+
+    public void addMatch(Lotto lotto, int matchCount) {
+        if (matchCount == 3) {
+            player.addThreeMatch();
+        } else if (matchCount == 4) {
+            player.addFourMatch();
+        } else if (matchCount == 5) {
+            boolean hasBonus = isContainBonus(lotto, player.getBonusNumber());
+            if (hasBonus) {
+                player.addFiveMatchWithBonus();
+            } else {
+                player.addFiveMatch();
+            }
+        } else if (matchCount == 6) {
+            player.addSixMatch();
+        }
+    }
 }

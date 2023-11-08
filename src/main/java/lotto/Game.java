6bcd23a2854c96a02dd7b3c287686e41f56bbdea
dyeongkim <dyeongkim98@gmail.com>
2023-11-08 17:32:37 +0900

    Feat(Game): Add 로또 게임 결과 출력
    게임결과 계산 후 출력 부분 추가

--- src/main/java/lotto/Game.java
+++ src/main/java/lotto/Game.java
@@ -3,47 +3,65 @@ package lotto;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Game {
     enum LottoRank{
-        FIRST(6, false, 2000000000),
-        SECOND(5, true, 30000000),
-        THIRD(5, false, 1500000),
-        FOURTH(4, false, 50000),
-        FIFTH(3, false, 5000),
-        NO_MATCHED(0, false, 0);
+        FIRST(6, false, 2000000000, "6개 일치 (2,000,000,000원)"),
+        SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
+        THIRD(5, false, 1500000, "5개 일치 (1,500,000원)"),
+        FOURTH(4, false, 50000, "4개 일치 (50,000원)"),
+        FIFTH(3, false, 5000, "3개 일치 (5,000원)"),
+        NONE(0,false,0,"");
 
         private final Integer matchedNumbersCount;
         private final boolean isBonusNumberMatched;
         private final Integer prizeMoney;
+        private final String maseege;
 
-        LottoRank(Integer matchedNumbersCount, boolean isBonusNumberMatched, final Integer prizeMoney){
+        LottoRank(Integer matchedNumbersCount, boolean isBonusNumberMatched, Integer prizeMoney, String maseege){
             this.matchedNumbersCount = matchedNumbersCount;
             this.isBonusNumberMatched = isBonusNumberMatched;
             this.prizeMoney = prizeMoney;
+            this.maseege = maseege;
         }
         private static LottoRank getRank(Integer matchedNumbersCount, boolean isBonusNumberMatched){
             for (LottoRank rank : LottoRank.values()){
-                if (Objects.equals(rank.matchedNumbersCount, matchedNumbersCount) && (rank.isBonusNumberMatched && isBonusNumberMatched)){
+                if (Objects.equals(rank.matchedNumbersCount, matchedNumbersCount) && (!rank.isBonusNumberMatched || isBonusNumberMatched)){
                     return rank;
                 }
             }
-            return NO_MATCHED;
+            return NONE;
+        }
+        public Integer getPrizeMoney() {
+            return prizeMoney;
+        }
+        public String getMaseege() {
+            return maseege;
         }
     }
     private InputUser inputUser;
     private OutputUser outputUser;
     private List<Lotto> numberTickets;
     private WinningLotto winningLotto;
+    private final Map<LottoRank, Integer> lottoResult;
     private Integer lottoMoney;
     private Integer totalLottoCount;
+    private double totalPrice;
+    private double profitRate;
 
     public Game(){
         this.inputUser = new InputUser();
         this.outputUser = new OutputUser();
         this.numberTickets = new ArrayList<>();
+        lottoResult = new EnumMap<>(LottoRank.class);
+        for (LottoRank rank : LottoRank.values()){
+            lottoResult.put(rank, 0);
+        }
     }
     public void play(){
         this.lottoMoney = inputUser.inputLottoMoney();
@@ -56,6 +74,7 @@ public class Game {
         this.winningLotto = new WinningLotto(winningNumbers, bonusNumber);
 
         resultLottoGame(this.numberTickets, this.winningLotto);
+        outputUser.outputLottoGameResult(this.lottoResult, this.profitRate);
     }
     private void makeNumberTicket(Integer lottoMoney){
         this.totalLottoCount = lottoMoney/1000;
@@ -78,6 +97,7 @@ public class Game {
             Boolean isBonusNumberMatched = calculateLottoGameBonus(lotto, winningLotto);
             calculateLottoGameResult(matchedNumbersCount, isBonusNumberMatched);
         }
+        this.profitRate = this.totalPrice/this.lottoMoney;
     }
     private int calculateLottoGame(Lotto lotto, WinningLotto winningLotto){
         List<Integer> resultLotto = new ArrayList<>(lotto.getNumbers());
@@ -89,6 +109,8 @@ public class Game {
     }
     private void calculateLottoGameResult(Integer matchedNumbersCount, Boolean isBonusNumberMatched){
         LottoRank rank = LottoRank.getRank(matchedNumbersCount, isBonusNumberMatched);
+        this.totalPrice += rank.prizeMoney;
+        lottoResult.merge(rank, 1, Integer::sum);
     }
 
 }

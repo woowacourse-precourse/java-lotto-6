d528658e95d7fa7c1f76b8d56967957f2b926b01
binary_ho <dfghcvb11@naver.com>
2023-11-08 17:22:24 +0900

    refactor : 출력 순서를 고려해 LottoRanking 상수 선언 순서를 변경

--- src/main/java/lotto/model/LottoRanking.java
+++ src/main/java/lotto/model/LottoRanking.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 
 public enum LottoRanking {
 
-    FIRST(6, 2_000_000_000), SECOND(5, 30_000_000), THIRD(5, 1_500_000),
-    FOURTH(4, 50_000), FIFTH(3, 5_000), UNRANKED(0, 0);
+    FIFTH(3, 5_000),
+    FOURTH(4, 50_000),
+    THIRD(5, 1_500_000),
+    SECOND(5, 30_000_000),
+    FIRST(6, 2_000_000_000),
+    UNRANKED(0, 0);
 
     private final int hitCount;
     private final int winningAmount;
@@ -25,6 +29,10 @@ public enum LottoRanking {
 
     public int getWinningAmount() {
         return winningAmount;
+    }
+
+    public int getHitCount() {
+        return hitCount;
     }
 
     private static LottoRanking getRanking(int hitCount) {

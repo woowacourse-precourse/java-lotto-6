1f788edd8187fb2b6ed3ed1ce3fac95fab7259d6
morenow <jjang9877@gmail.com>
2023-11-08 19:48:22 +0900

    feat: 로또 랜덤 구매 후 저장 및 출력 기능 추가
    로또 컬렉션을 가지는 일급 컬렉션 Lottos, 랜덤 구매하는 LottoGenerator 클래스 추가
    관련 기능 명세 완료 처리

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.domain.Lottos;
 import lotto.domain.Money;
 import lotto.domain.WinningNumbers;
 
@@ -7,6 +8,7 @@ public class LottoController {
 
     public void playLotto() {
         Money money = Money.createMoney();
+        Lottos lottos = Lottos.createLottos(money);
         WinningNumbers winningNumbers = WinningNumbers.createWinningNumbers();
     }
 }
--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,11 +1,21 @@
 package lotto.domain;
 
+import static lotto.domain.constant.CommonMessage.*;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lotto.domain.generator.LottoGenerator;
+import lotto.view.OutputView;
 
 public class Lotto {
     public static final int LOTTO_NUMBER = 6;
     public static final int LOTTO_RANGE_BEGIN = 1;
     public static final int LOTTO_RANGE_END = 45;
+    public static final String LOTTO_OPEN_BRACKET = "[";
+    public static final String LOTTO_CLOSE_BRACKET = "]";
+    public static final String LOTTO_DELIMITER = ", ";
 
     private final List<Integer> numbers;
 
@@ -15,10 +25,53 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_NUMBER) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(SIZE_ERROR_MESSAGE.getValue());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE.getValue());
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        HashSet<Integer> duplicateCheckSet = new HashSet<>();
+        numbers.forEach(number -> checkDuplicate(duplicateCheckSet, number));
+    }
+
+    private void checkDuplicate(HashSet<Integer> duplicateCheckSet, Integer number) {
+        if (duplicateCheckSet.contains(number)) {
+            throw new IllegalArgumentException(DUPLICATE_ERROR_MESSAGE.getValue());
+        }
+        duplicateCheckSet.add(number);
+    }
+
+    public static Lotto createLotto() {
+        LottoGenerator lottoGenerator = new LottoGenerator();
+        List<Integer> numbers = lottoGenerator.generateLottoNumbers();
+        Lotto lotto = new Lotto(numbers);
+        OutputView.printMessage(lotto.toString());
+        return lotto;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    @Override
+    public String toString() {
+        return LOTTO_OPEN_BRACKET +
+                numbers.stream()
+                        .map(Objects::toString)
+                        .collect(Collectors.joining(LOTTO_DELIMITER)) +
+                LOTTO_CLOSE_BRACKET;
+    }
 }

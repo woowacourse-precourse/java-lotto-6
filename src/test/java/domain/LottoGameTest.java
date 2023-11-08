62726c3618dd99aa7bbef7646dc7522f13cafac1
ssuzyn <asj7090@gmail.com>
2023-11-08 20:27:47 +0900

    :white_check_mark: [test] : 로또 번호 일치 여부 검증 테스트 작성

--- src/test/java/domain/LottoGameTest.java
+++ src/test/java/domain/LottoGameTest.java
@@ -18,4 +18,22 @@ public class LottoGameTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("보너스 번호가 일치하는 숫자가 있는지 확인하는 기능 테스트")
+    @Test
+    void matchBonus(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,8));
+        LottoGame lottoGame = new LottoGame(8, new Lotto(List.of(1,2,3,4,5,6)));
+        boolean result = lottoGame.matchBonus(lotto);
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("당첨 번호와 일치한 숫자가 몇개인지 반환하는 기능 테스트")
+    @Test
+    void matchWinningLotto(){
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,8));
+        LottoGame lottoGame = new LottoGame(8, new Lotto(List.of(1,2,3,4,5,6)));
+        int result = lottoGame.countMatchLotto(lotto);
+        assertThat(result).isEqualTo(5);
+    }
+
 }

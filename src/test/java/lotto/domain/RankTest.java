30c663eb3ce1bac5bfb80e98497ba94a5f640244
ay-eonii <naayen@naver.com>
2023-11-08 15:19:17 +0900

    test(rank): 당첨 순위 상금 테스트

--- src/test/java/lotto/domain/RankTest.java
+++ src/test/java/lotto/domain/RankTest.java
@@ -25,8 +25,14 @@ class RankTest {
         Assertions.assertThat(actual).isEqualTo(Rank.FIRST);
     }
 
+    @DisplayName("당첨 순위의 상금을 가져온다.")
     @Test
     void getPrize() {
+        Assertions.assertThat(5_000).isEqualTo(Rank.FIFTH.getPrize());
+        Assertions.assertThat(50_000).isEqualTo(Rank.FOURTH.getPrize());
+        Assertions.assertThat(1_500_000).isEqualTo(Rank.THIRD.getPrize());
+        Assertions.assertThat(30_000_000).isEqualTo(Rank.SECOND.getPrize());
+        Assertions.assertThat(2_000_000_000).isEqualTo(Rank.FIRST.getPrize());
     }
 
     @Test

cb614aa496299f7c9248f23e7213d12fb7e6baf0
giljin <68088970+giljin@users.noreply.github.com>
2023-11-08 12:21:10 +0900

    feat(ErrorMessage): 에러 메세지 관리

---
+++ src/main/java/lotto/ErrorMessage.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class ErrorMessage {
+    public static final String MISMATCH_LOTTO_INPUT_FORMAT = "숫자 %d개를 입력해 주세요.";
+    public static final String MISMATCH_LOTTO_NUMBER_RANGE_FORMAT = "숫자가 %d ~ %d내의 값이 아닙니다.";
+    public static final String GENERATE_CHANGES_FORMAT = "%d원 단위로 구입 가능합니다.";
+    public static final String MAXIMUM_PURCHASE_AMOUNT_FORMAT = "한번 최대 구입 금액은 %d원 입니다.";
+
+    public static final String DUPLICATE_NUMBER = "중복된 번호입니다.";
+    public static final String NO_NUMBER_FORMAT = "숫자만 입력 가능합니다.";
+}

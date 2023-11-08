f3070ae8403d4778b166c0830abbb1531f6df4c0
yyeennyy <kye3314@gmail.com>
2023-11-08 15:45:29 +0900

    test: User

--- src/test/java/lotto/domain/UserTest.java
+++ src/test/java/lotto/domain/UserTest.java
@@ -1,20 +1,30 @@
 package lotto.domain;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UserTest {
 
-    @DisplayName("유저__전체_금액_지불")
+    @DisplayName("[기능]User_지불하기__전체_예산이_지불된다")
     @Test
     void userPayAllTest() {
         int initBudget = 4000;
         User user = User.createByBudget(initBudget);
 
         user.pay();
-
-        Assertions.assertThat(user.getBudget()).isEqualTo(0);
+        assertThat(user.getBudget()).isEqualTo(0);
     }
 
+    @DisplayName("[검증]User_불변검증__갖고있는_로또는_셀러가_준_로또와_다른_인스턴스")
+    @Test
+    void notSameLottoPaper() {
+        LottoSeller lottoSeller = LottoSeller.create();
+        User user = User.createByBudget(5000);
+        LottoPaper lottoPaper = lottoSeller.sellLottos(user.getBudget());
+        user.setLottoPaper(lottoPaper);
+
+        assertThat(user.getLottoPaper()).isNotSameAs(lottoPaper);
+    }
 }

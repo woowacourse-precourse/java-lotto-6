5884e3c33183330eba89965358756654984231ec
ay-eonii <naayen@naver.com>
2023-11-08 14:28:49 +0900

    test(buyer): 구매금액이 음수인 경우 예외 발생

--- src/test/java/lotto/domain/BuyerTest.java
+++ src/test/java/lotto/domain/BuyerTest.java
@@ -9,8 +9,16 @@ class BuyerTest {
 
     @DisplayName("구매금액이 1,000원 이하라면 예외가 발생한다.")
     @Test
-    void getCost() {
+    void getCostUnderPrice() {
         assertThatThrownBy(() -> new Buyer(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구매금액은 1,000원 이상이어야 합니다.");
+    }
+
+    @DisplayName("구매금액이 음수라면 예외가 발생한다.")
+    @Test
+    void getNegativeCost() {
+        assertThatThrownBy(() -> new Buyer(-2000))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 구매금액은 1,000원 이상이어야 합니다.");
     }

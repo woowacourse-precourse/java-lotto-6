0bc25eea50a80b034dd5780af5fc18add9ba705b
BH946 <kobh2895@gmail.com>
2023-11-08 20:07:27 +0900

    feat: 당첨번호 입력, 보너스번호 입력, 숫자범위 예외처리 함수 개발

--- src/main/java/lotto/view/InputLotto.java
+++ src/main/java/lotto/view/InputLotto.java
@@ -6,11 +6,36 @@ import static lotto.util.message.Error.VALID_INPUT;
 
 public class InputLotto {
     static final int MOD_NUM = 1000;
+    static final int LOTTO_NUM_LENGTH = 6;
 
     public static int purchaseMoney() {
         while (true) {
             try {
                 return readMoney();
+            } catch (NumberFormatException e) {
+                System.out.println(VALID_INPUT);
+            } catch (IllegalArgumentException e) {
+                System.out.println(VALID_INPUT);
+            }
+        }
+    }
+
+    public static int[] answerNumber() {
+        while (true) {
+            try {
+                return readAnswer();
+            } catch (NumberFormatException e) {
+                System.out.println(VALID_INPUT);
+            } catch (IllegalArgumentException e) {
+                System.out.println(VALID_INPUT);
+            }
+        }
+    }
+
+    public static int bonusNumber() {
+        while (true) {
+            try {
+                return readBonus();
             } catch (NumberFormatException e) {
                 System.out.println(VALID_INPUT);
             } catch (IllegalArgumentException e) {
@@ -25,5 +50,30 @@ public class InputLotto {
             throw new IllegalArgumentException(VALID_INPUT);
         }
         return input / MOD_NUM;
+    }
+
+    private static int[] readAnswer() {
+        String[] input = readLine().split(",");
+        if (input.length != LOTTO_NUM_LENGTH) {
+            throw new IllegalArgumentException(VALID_INPUT);
+        }
+        int[] result = new int[LOTTO_NUM_LENGTH];
+        for (int i = 0; i < LOTTO_NUM_LENGTH; i++) {
+            result[i] = Integer.parseInt(input[i]);
+            exceptNumberRange(result[i]); // 범위 체크
+        }
+        return result;
+    }
+
+    private static int readBonus() {
+        Integer input = Integer.parseInt(readLine());
+        exceptNumberRange(input); // 범위 체크
+        return input;
+    }
+
+    private static void exceptNumberRange(int num) {
+        if(num < 1 || num > 45) {
+            throw new IllegalArgumentException(VALID_INPUT);
+        }
     }
 }

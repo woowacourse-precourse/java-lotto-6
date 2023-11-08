9d943e61b940ace8d20030aaf7a92b5a985df918
Suyoung Oh <ohsusu515@gmail.com>
2023-11-08 21:48:11 +0900

    test: 보너스 번호 유효성 검사 테스트

---
+++ src/test/java/lotto/domain/BonusNumberTest.java
@@ -0,0 +1,19 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.Test;
+
+class BonusNumberTest {
+
+    @Test
+    void consistOf() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBonusNumber() {
+    }
+}

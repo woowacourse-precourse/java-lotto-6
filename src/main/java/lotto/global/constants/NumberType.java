765329531e69912bd4309b2c41f92e595bbfb96c
MingyeomKim <mingyum119@naver.com>
2023-11-08 17:05:59 +0900

    feat: 프로그램 내에서 사용되는 상수를 enum class로 분리

--- src/main/java/lotto/global/constants/NumberType.java
+++ src/main/java/lotto/global/constants/NumberType.java
@@ -3,7 +3,8 @@ package lotto.global.constants;
 public enum NumberType {
     MIN_LOTTO_NUMBER(1),
     MAX_LOTTO_NUMBER(45),
-    COST_UNIT(1000);
+    COST_UNIT(1000),
+    LOTTO_SIZE(6);
 
     private final Integer value;
 
--- src/main/java/lotto/global/exception/ErrorMessage.java
+++ src/main/java/lotto/global/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package lotto.global.exception;
 
 import static lotto.global.constants.NumberType.COST_UNIT;
+import static lotto.global.constants.NumberType.LOTTO_SIZE;
 
 public enum ErrorMessage {
     BLANK_INPUT_ERROR("빈 문자열이 입력되었습니다."),
@@ -8,7 +9,8 @@ public enum ErrorMessage {
     INVALID_UNIT_ERROR(String.format("%d원 단위의 구입 금액을 입력해주세요.", COST_UNIT.getValue())),
     INVALID_SEPARATOR_ERROR("올바르지 않은 구분자 입력입니다."),
     INVALID_RANGE_ERROR("올바르지 않은 범위의 입력입니다."),
-    DUPLICATED_NUMBER_ERROR("중복되지 않은 숫자를 입력해주세요.");
+    DUPLICATED_NUMBER_ERROR("중복되지 않은 숫자를 입력해주세요."),
+    INVALID_LOTTO_SIZE_ERROR(String.format("%d개의 로또 번호를 입력해주세요.", LOTTO_SIZE.getValue()));
 
     private static final String PREFIX = "[ERROR]";
     private final String message;

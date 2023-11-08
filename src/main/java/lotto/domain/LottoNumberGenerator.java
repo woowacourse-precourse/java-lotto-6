8a914302cebe208becff5a12a9b13c3955509575
박준영 <wnsdud6969@naver.com>
2023-11-08 21:54:52 +0900

    refactor(LottoNumberGenerator): add constant and separate function
    Add constant
     - MINIMUM_NUMBER_OF_LOTTO_NUMBER
     - MAXIMUM_NUMBER_OF_LOTTO_NUMBER
     - ZERO, LOTTO_SIZE
    
     Separate function
     - add pickUniqueNumbersInRange() to separate generateRandomNumbers()

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -7,18 +7,28 @@ import java.util.stream.LongStream;
 
 public class LottoNumberGenerator {
 
-    public static List<List<Integer>> generateRandomNumbers(long count) {
+    private static final int MINIMUM_NUMBER_OF_LOTTO_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER_OF_LOTTO_NUMBER = 45;
+    private static final int LOTTO_SIZE = 6;
+    private static final int ZERO = 0;
 
+    public static List<List<Integer>> generateRandomNumbers(long count) {
         List<List<Integer>> randomLottoNumbers = new ArrayList<>();
 
-        LongStream.range(0, count)
-            .forEach(i -> {
-                List<Integer> randomNumbers = new ArrayList<>(
-                    Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        LongStream.range(ZERO, count)
+            .forEach(index -> {
+                List<Integer> randomNumbers = new ArrayList<>(pickUniqueNumbersInRange());
                 randomNumbers.sort(Integer::compareTo);
                 randomLottoNumbers.add(randomNumbers);
             });
 
         return randomLottoNumbers;
+    }
+
+    private static List<Integer> pickUniqueNumbersInRange() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(
+            MINIMUM_NUMBER_OF_LOTTO_NUMBER,
+            MAXIMUM_NUMBER_OF_LOTTO_NUMBER, LOTTO_SIZE);
+        return randomNumbers;
     }
 }

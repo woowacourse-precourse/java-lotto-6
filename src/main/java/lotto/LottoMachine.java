011da60e545a851276635562130310b20361831e
uwoobeat <desktop3711@gmail.com>
2023-11-08 23:46:09 +0900

    feat: 로또 머신 구현

--- src/main/java/lotto/LottoMachine.java
+++ src/main/java/lotto/LottoMachine.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import lotto.dto.LottoResult;
+
 public class LottoMachine {
     private final LottoSeller lottoSeller;
     private final LottoJudge lottoJudge;
@@ -7,5 +9,17 @@ public class LottoMachine {
     public LottoMachine(LottoSeller lottoSeller, LottoJudge lottoJudge) {
         this.lottoSeller = lottoSeller;
         this.lottoJudge = lottoJudge;
+    }
+
+    public LottoBundle buyLotto(Money money) {
+        return lottoSeller.sell(money);
+    }
+
+    public void configureJudge(WinningLotto winningLotto) {
+        lottoJudge.configure(winningLotto);
+    }
+
+    public LottoResult judge(LottoBundle bundle) {
+        return lottoJudge.judge(bundle);
     }
 }

8c782d3f3cabf383c0416bc0fcda430bb83eef2e
김현재 <khjmr@naver.com>
2023-11-08 14:11:23 +0900

    test(contain): 포함하는지 여부를 확인하는 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import lotto.model.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -23,5 +25,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByOutRange() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 456)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또에 포함된 수를 제대로 포함하는지 확인한다.")
+    @Test
+    public void contain() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contain(1)).isTrue();
+        assertThat(lotto.contain(7)).isFalse();
+    }
 }

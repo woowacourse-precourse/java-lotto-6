fde59d54d5e45df70e4fcf02bda7db2ce0149246
fromitive <fromitive@gmail.com>
2023-11-08 16:25:12 +0900

    refactor: LottoNumberException 추가

--- src/main/java/lotto/exception/LottoNumberException.java
+++ src/main/java/lotto/exception/LottoNumberException.java
@@ -3,16 +3,14 @@ package lotto.exception;
 import lotto.error.LottoErrorMessage;
 
 public class LottoNumberException extends IllegalArgumentException {
-    
-    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
-    private LottoErrorMessage errorMessage;
 
-    private LottoNumberException(final LottoErrorMessage errorMessage) {
-        super(String.format(ERROR_MESSAGE_FORMAT,errorMessage.getMessage()));
-        this.errorMessage = errorMessage;
+    private static final String ERROR_MESSAGE_FORMAT = "[ERROR] %s";
+
+    public LottoNumberException(LottoErrorMessage errorMessage) {
+        super(String.format(ERROR_MESSAGE_FORMAT, errorMessage.getMessage()));
     }
-    
-    public static LottoNumberException of(LottoErrorMessage errorMessage){
+
+    public static LottoNumberException of(LottoErrorMessage errorMessage) {
         return new LottoNumberException(errorMessage);
     }
 }

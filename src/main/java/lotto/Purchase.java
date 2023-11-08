4f7745911c8f79c8ed9f9e50276d17dbd7bd936f
Seo Young, Ha <59078585+82Hasheng@users.noreply.github.com>
2023-11-08 17:14:54 +0900

    feat: 구입 금액이 나누어 1000원으로 떨어지지 않는 경우 예외 처리 기능 구현

--- src/main/java/lotto/Purchase.java
+++ src/main/java/lotto/Purchase.java
@@ -2,11 +2,24 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Purchase {
-    public List[] automaticNum(List[] allTickets, int ticketNumber) {
+    String purchaseMoney;
+    int purchaseM;
+
+    public Purchase(String purchaseMoney) {
+        notNumber(purchaseMoney);
+        this.purchaseMoney = purchaseMoney;
+    }
+
+    public int getPurchaseMoney() {
+        this.purchaseM = Integer.parseInt(purchaseMoney);
+        return purchaseM;
+    }
+
+    public List[] automaticNum(int ticketNumber) {
+        List[] allTickets = new List[ticketNumber];
         for (int i = 0; i < ticketNumber; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             numbers.sort(Comparator.naturalOrder());
@@ -14,5 +27,20 @@ public class Purchase {
             System.out.println(numbers);
         }
         return allTickets;
+    }
+
+    public void notNumber(String purchaseMoney) {
+        for (int i = 0; i < purchaseMoney.length(); i++) {
+            char ch = purchaseMoney.charAt(i);
+            if (ch < 48 || ch > 57) {
+                throw new IllegalArgumentException("[ERROR]");
+            }
+        }
+    }
+
+    public int numberOfTickets(int money) {
+        int ticketNumber = money / 1000;
+        System.out.printf("\n%d개를 구매했습니다.%n", ticketNumber);
+        return ticketNumber;
     }
 }

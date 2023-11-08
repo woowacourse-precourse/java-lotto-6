72215648232ec58232ec7d45d6326f85bf1819a2
alxqkekrroqkfwk <fexxet@icloud.com>
2023-11-08 19:13:00 +0900

    feat(exception) : add InvalidPurchaseAmountUnitException class

---
+++ src/main/java/lotto/exception/InvalidPurchaseAmountUnitException.java
@@ -0,0 +1,7 @@
+package lotto.exception;
+
+public class InvalidPurchaseAmountUnitException extends IllegalArgumentException {
+    public InvalidPurchaseAmountUnitException() {
+        super("[ERROR] 구입 금액은 천원 단위로 입력할 수 있습니다.");
+    }
+}

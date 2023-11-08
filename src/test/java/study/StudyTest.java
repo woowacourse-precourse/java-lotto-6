0d2e8016616ec6fe059a26c4c542608f47323a7d
parkchangeon <jg9870@naver.com>
2023-11-08 22:06:02 +0900

    refactor(lotto): 코드 라인 정리

--- src/test/java/study/StudyTest.java
+++ src/test/java/study/StudyTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StudyTest {
     @DisplayName("반올림 테스트")
     @Test
-    void roundsTest(){
+    void roundsTest() {
         assertThat(rounds(1.23, 1)).isEqualTo(1.2);
         assertThat(rounds(1.25, 1)).isEqualTo(1.3);
         assertThat(rounds(1.254, 2)).isEqualTo(1.25);

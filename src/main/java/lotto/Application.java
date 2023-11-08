48b6755e5843d9673a83e470cfaa0a4ddfa1c86e
wooteco-daram <wooteco.daram@gmail.com>
2023-11-08 18:22:46 +0900

    feat: 로또 게임을 진행하는 `LottoGameController` 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,19 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.controller.LottoGameController;
+import lotto.view.input.reader.ConsoleReader;
+import lotto.view.output.writer.ConsoleWriter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleReader consoleReader = new ConsoleReader();
+        ConsoleWriter consoleWriter = new ConsoleWriter();
+        LottoGameController lottoGameController = new LottoGameController(
+                consoleReader,
+                consoleWriter
+        );
+        lottoGameController.start();
+        Console.close();
     }
 }

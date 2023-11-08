928874a3a8ea631b6a348c26426403ba53d30422
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-08 17:48:29 +0900

    feat($UI): LottoChecker의 집계 결과 출력을 UI에게 전가

--- src/main/java/lotto/LottoUI.java
+++ src/main/java/lotto/LottoUI.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.text.NumberFormat;
+import java.util.HashMap;
 import java.util.List;
 
 public class LottoUI {
@@ -18,5 +20,31 @@ public class LottoUI {
         lottos.stream()
                 .map(Lotto::getNumbers)
                 .forEach(System.out::println);
+    }
+
+    public static void printTotalPrize(HashMap<LottoRank, Integer> lottoResult) {
+        for (LottoRank lottoRank : LottoRank.values()) {
+            System.out.println(printPrize(lottoRank, lottoResult.get(lottoRank)));
+        }
+    }
+
+    public static String printPrize(LottoRank lottoRank, int count) {
+        String commaPrize = NumberFormat.getInstance().format(lottoRank.getPrize());
+
+        if (lottoRank.isCorrectBonus()) {
+            return String.format("%d개 일치, 보너스 볼 일치 (%s원) - %d개",
+                    lottoRank.getCorrectCount(),
+                    commaPrize,
+                    count);
+        }
+
+        return String.format("%d개 일치 (%s원) - %d개",
+                lottoRank.getCorrectCount(),
+                commaPrize,
+                count);
+    }
+
+    public static void printProfit(double profitRatio) {
+        System.out.println("총 수익률은 " + profitRatio + "%입니다.");
     }
 }

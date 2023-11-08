996bef67f1648ec4179428614919a3db8a63d1af
dmdwns2 <ssreki1@naver.com>
2023-11-08 18:05:18 +0900

    test : delete generate lotto test

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,11 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static lotto.Error.*;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -41,15 +39,5 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(-5, 2, 3, 4, 5, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_NUMBERS_VALUE.getErrMsg());
-    }
-
-    @DisplayName("로또가 정상적으로 생성")
-    @Test
-    public void testGenerateLotto() {
-        Lotto lotto = new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        List<Integer> numbers = lotto.generateLotto();
-        assertThat(numbers).hasSize(6)
-                .allMatch(number -> number >= 1 && number <= 45)
-                .doesNotHaveDuplicates();
     }
 }

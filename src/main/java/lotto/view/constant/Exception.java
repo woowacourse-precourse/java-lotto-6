106ee39686c330c8c95e57d98d6188dc80ceb2cd
ay-eonii <naayen@naver.com>
2023-11-08 18:16:53 +0900

    refactor(exception): 예외 메시지 재정의

--- src/main/java/lotto/view/constant/Exception.java
+++ src/main/java/lotto/view/constant/Exception.java
@@ -7,6 +7,6 @@ public class Exception {
     public static final String LOTTO_FORMAT = "[ERROR] 로또 번호는 쉼표로 구분되는 1에서 45 사이의 숫자여야 합니다.";
     public static final String LOTTO_DUPLICATED = "[ERROR] 로또 번호는 중복될 수 없습니다.";
     public static final String LOTTO_SIZE = "[ERROR] 로또 번호는 6자리여야 합니다.";
-    public static final String BONUS_FORMAT = "[ERROR] 보너스 번호는 하나의 숫자여야 합니다.";
+    public static final String BONUS_FORMAT = "[ERROR] 보너스 번호는 1에서 45 사이인 하나의 숫자여야 합니다.";
     public static final String BONUS_DUPLICATED = "[ERROR] 보너스 번호는 중복될 수 없습니다.";
 }

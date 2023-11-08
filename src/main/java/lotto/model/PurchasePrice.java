3d6f6bb4d66fcb3661612dd42c4db20529a8740a
kkkkimtaehyeon <kth1927@naver.com>
2023-11-08 12:27:43 +0900

    refactor : 공백제거

--- src/main/java/lotto/model/PurchasePrice.java
+++ src/main/java/lotto/model/PurchasePrice.java
@@ -1,7 +1,6 @@
 package lotto.model;
 
 public class PurchasePrice {
-
     private final int PIECE_OF_LOTTO;
     private static final int LOTTO_PRICE = 1000;
 
@@ -11,7 +10,6 @@ public class PurchasePrice {
         isNotDivisibleBy1000(priceFromUser);
         this.PIECE_OF_LOTTO = Integer.parseInt(priceFromUser) / 1000;
     }
-
     public int getCountPieceOfLotto(){
         return this.PIECE_OF_LOTTO;
     }
@@ -20,13 +18,11 @@ public class PurchasePrice {
             throw new IllegalArgumentException("[ERROR] 올바른 금액을 입력해주세요.");
         }
     }
-
     private void isUnder1000(String purchasePrice) throws IllegalArgumentException{
         if(Long.parseLong(purchasePrice) < LOTTO_PRICE){
             throw new IllegalArgumentException("[ERROR] 로또 한장은 1000원입니다.");
         }
     }
-
     private void isNotDivisibleBy1000(String purchasePrice) throws IllegalArgumentException{
         if(Long.parseLong(purchasePrice) % 1000 != 0){
             throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000으로 나누어 떨어져야 합니다.");

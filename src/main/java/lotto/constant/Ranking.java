00cad1daf1b5398c52a92f62c5cdd0acb63b6817
Laniel Park <Laniel8177@gmail.com>
2023-11-08 19:37:02 +0900

    feat : Ranking (Constant Class)

---
+++ src/main/java/lotto/constant/Ranking.java
@@ -0,0 +1,31 @@
+package lotto.constant;
+
+public enum Ranking {
+    FIRST(6, false, "2,000,000,000원"),
+    SECOND(5, true, "30,000,000원"),
+    THIRD(5, false, "1,500,000원"),
+    FOURTH(4, false, "50,000원"),
+    FIFTH(3, false, "5,000원");
+
+    private final int matchingNumbers;
+    private final boolean shouldBonusBallMatch;
+    private final String reward;
+
+    Ranking(int matchingNumbers, boolean shouldBonusBallMatch, String reward) {
+        this.matchingNumbers = matchingNumbers;
+        this.shouldBonusBallMatch = shouldBonusBallMatch;
+        this.reward = reward;
+    }
+
+    public int getMatchingNumbers() {
+        return matchingNumbers;
+    }
+
+    public boolean isShouldBonusBallMatch() {
+        return shouldBonusBallMatch;
+    }
+
+    public String getReward() {
+        return reward;
+    }
+}

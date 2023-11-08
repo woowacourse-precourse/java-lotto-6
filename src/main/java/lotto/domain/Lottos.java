39ba63cb077b991915dd313249da448126627d9b
박승민 <tmddudals369@naver.com>
2023-11-08 15:53:27 +0900

    feat(lottos): 로또의 당첨 결과를 구하는 기능 구현

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -8,4 +8,16 @@ public class Lottos {
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
     }
+
+    public Result getResult(WinningLotto winningLotto) {
+        Result result = new Result();
+
+        lottos.forEach(lotto -> {
+                int matchCount = lotto.getMatchCount(winningLotto.getLotto());
+                boolean containBonusNumber = lotto.isContainBonusNumber(winningLotto.getBonusNumber());
+                result.increaseRankCount(matchCount, containBonusNumber);
+            });
+
+        return result;
+    }
 }

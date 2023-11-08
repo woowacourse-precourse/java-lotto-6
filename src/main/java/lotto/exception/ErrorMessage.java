8938578d653a6549fd48077237d566c40769dd29
Hyyena <sjtxm0320@gmail.com>
2023-11-08 18:43:08 +0900

    ✨ feat: 로또 구입 기능
    - close #3

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -2,10 +2,14 @@ package lotto.exception;
 
 public enum ErrorMessage {
 
+    // Lotto
     EMPTY("빈 값은 입력할 수 없습니다."),
     MISMATCH_LOTTO_NUMBER("6개의 로또 번호를 입력해야 합니다."),
     DUPLICATED_LOTTO_NUMBER("중복된 로또 번호를 입력했습니다."),
-    OUT_OF_RANGE_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    OUT_OF_RANGE_LOTTO_NUMBER("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+
+    // LottoMachine
+    INVALID_PURCHASE_AMOUNT("로또 구입 금액은 1,000원 단위로 입력해야 합니다.");
 
     private static final String ERROR = "[ERROR] ";
     private final String message;

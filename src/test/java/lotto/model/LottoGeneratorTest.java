2a2e37ea8cfc8738af00da8f8a187f4a1c6badb8
heiler <threepebbles@naver.com>
2023-11-08 22:52:54 +0900

    Test: LottoGenerator 테스트 코드 작성

---
+++ src/test/java/lotto/model/LottoGeneratorTest.java
@@ -0,0 +1,16 @@
+package lotto.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoGeneratorTest {
+    @DisplayName("로또 생성기가 로또 객체를 생성하는지 테스트")
+    @Test
+    void 로또_생성_테스트() {
+        LottoRandomGenerator lottoRandomGenerator = new LottoRandomGenerator();
+        assertThat(lottoRandomGenerator.generate())
+                .isInstanceOf(Lotto.class);
+    }
+}

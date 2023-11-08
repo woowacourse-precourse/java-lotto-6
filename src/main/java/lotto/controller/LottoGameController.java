fa623965e3db658536ce3adb93cbabd5c2df5a90
minkyole <7719s@nave.com>
2023-11-08 20:29:57 +0900

    feat(LottoGameController.java) : 로또당첨번호를 입력받는 기능 구현
    - 로또 당첨번호 / 보너스번호를 입력받는 기능 구현완료

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,5 +1,6 @@
 package lotto.controller;
 
+import lotto.model.Lotto;
 import lotto.model.User;
 import lotto.model.Wallet;
 import lotto.model.WinningLotto;
@@ -14,6 +15,7 @@ public class LottoGameController {
     public void start() {
         checkPurchaseAmount();
         buyLottoes();
+        checkWinningLottoNumbers();
     }
 
     private void checkPurchaseAmount() {
@@ -35,5 +37,38 @@ public class LottoGameController {
             user.pickLottoNumber();
         }
         OutputView.printPurchaseLottoesMessage(user.getLottoes());
+    }
+
+    private void checkWinningLottoNumbers() {
+        String lottoNumber = checkLottoNumber();
+        String bonusNumber = checkBonusNumber(lottoNumber);
+        winningLotto = new WinningLotto(lottoNumber, bonusNumber);
+    }
+
+
+    private String checkLottoNumber() {
+        while (true) {
+            try {
+                OutputView.printInputWinningLottoNumberMessage();
+                String lottoNumber = InputView.getUserInput();
+                Lotto lotto = new Lotto(lottoNumber);
+                return lottoNumber;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                OutputView.printErrorMessage(illegalArgumentException.getMessage());
+            }
+        }
+    }
+
+    private String checkBonusNumber(String lottoNumber) {
+        while (true) {
+            try {
+                OutputView.printInputWinningBonusNumberMessage();
+                String bonusNumber = InputView.getUserInput();
+                WinningLotto winningLotto = new WinningLotto(lottoNumber, bonusNumber);
+                return bonusNumber;
+            } catch (IllegalArgumentException illegalArgumentException) {
+                OutputView.printErrorMessage(illegalArgumentException.getMessage());
+            }
+        }
     }
 }

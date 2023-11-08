c878668ef6c32d93f3dd36c22ffd5ba19fdd85c5
Sim-GyuBin <skb0606@gmail.com>
2023-11-08 09:06:41 +0900

    feat: 로또 당첨 내역 결과값 출력

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -1,21 +1,38 @@
 package lotto;
 
-import java.util.*;
+import lotto.interactor.Buyer;
+import lotto.interactor.LottoMachine;
+
+import java.util.List;
 
 public class LottoManager {
-    private Lottos lottos;
-    private static Buyer buyer;
-    public static void run() {
-        getInputMoney();
+    public void run() {
+        Buyer buyer = new Buyer();
+        LottoMachine lottoMachine = new LottoMachine();
+        Lottos lottos = lottoMachine.generateLottos(buyer.getTickets());
+        Jackpot jackpot = new Jackpot();
 
+        List<MatchResult> matchResults = lottos.calculateResults(jackpot);
+
+        ProfitCalculator profitCalculator = new ProfitCalculator();
+        double profitRate = profitCalculator.calculateProfit(matchResults, buyer.getTickets() * LottoMachine.TICKET_PRICE);
+
+        printResults(matchResults, profitRate);
     }
+    public void printResults(List<MatchResult> matchResults, double profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        for (Rank rank : Rank.values()) {
+            if (rank == Rank.NONE) {
+                continue; // NONE 등수는 출력하지 않음
+            }
 
-    private static void getInputMoney() {
-        buyer.pay();
+            long count = matchResults.stream()
+                    .filter(result -> result.getRank() == rank)
+                    .count();
 
-    }
-
-    private void buyLotto(int ticketNum) {
-
+            System.out.printf("%s - %d개%n", rank.getDescription(), count);
+        }
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", profitRate);
     }
 }

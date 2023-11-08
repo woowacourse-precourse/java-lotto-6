f4bdaf4f52cb56dab855554a13eb6e351a12baaa
potatomoong <star901210@hanmail.net>
2023-11-08 16:50:03 +0900

    Feat: 당첨번호가 아닌 다른 내용이 입력되면 오류처리하는 기능 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -15,11 +15,14 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
         }
 
-        for (int number : numbers) {
-            if (number < LottoGenerator.MIN_NUMBER || number > LottoGenerator.MAX_NUMBER) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
-            }
+        if (numbers.stream().anyMatch(number -> number < LottoGenerator.MIN_NUMBER || number > LottoGenerator.MAX_NUMBER)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
         }
+
+        if (numbers.stream().anyMatch(number -> !String.valueOf(number).matches("[0-9]+"))) {
+            throw new IllegalArgumentException("[ERROR] 숫자와 ','만 입력가능합니다.");
+        }
+
     }
 
     // TODO: 추가 기능 구현

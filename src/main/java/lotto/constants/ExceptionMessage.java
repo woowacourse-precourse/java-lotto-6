94680e44e4ae27190a5104f62532bda6961bda05
minsu20 <syt07100@gmail.com>
2023-11-08 23:09:40 +0900

    refactor: 구매 최대 금액 수정

--- src/main/java/lotto/constants/ExceptionMessage.java
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -2,9 +2,10 @@ package lotto.constants;
 
 public enum ExceptionMessage {
     LOTTO_NUMBERS_INVALID_ERROR("[ERROR] 로또 숫자는 1에서 45사이의 중복되지 않은 6개의 숫자로 이루어져야 합니다."),
+    BLANK_ERROR("[ERROR] 입력 값이 비어있습니다."),
     NOT_NUMERIC_ERROR("[ERROR] 숫자만 입력해 주세요."),
     AMOUNT_NOT_THOUSAND_UNIT_ERROR("[ERROR] 구매 금액은 1,000원 단위로만 입력해 주세요."),
-    AMOUNT_MAX_LIMIT_ERROR("[ERROR] 구매 금액은 최대 2,147,483,000 원까지만 입력해 주세요."),
+    AMOUNT_MAX_LIMIT_ERROR("[ERROR] 구매 금액은 최대 20억까지만 입력해 주세요."),
     WINNING_NUMBERS_FORMAT_ERROR("[ERROR] 당첨 번호는 숫자를 ','로 구분하여 6개의 숫자를 공백없이 입력해 주세요. ex)1,2,3,4,5,6"),
     WINNING_NUMBERS_RANGE_ERROR("[ERROR] 당첨 번호는 1에서 45사이의 숫자로 입력해 주세요."),
     WINNING_NUMBERS_DUPLICATE_ERROR("[ERROR] 당첨 번호는 서로 중복되지 않게 입력해 주세요."),

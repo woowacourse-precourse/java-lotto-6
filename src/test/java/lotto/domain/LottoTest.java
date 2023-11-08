a4df0604f4132fc5165ae1027fb013cb383b59a4
MingyeomKim <mingyum119@naver.com>
2023-11-08 22:18:12 +0900

    style(LottoTest): 패키지 이동으로 인한 import 제거

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -3,8 +3,6 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
-import lotto.domain.Lotto;
-import lotto.domain.Number;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 

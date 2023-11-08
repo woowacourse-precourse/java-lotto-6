ab42f56e7ed8fce6b012d7a76a9c17b62844fb7e
nahyeon99 <nahyeonee99@gmail.com>
2023-11-08 12:32:28 +0900

    refactor(domain/LottoNumber): BonusNumber -> LottoNumber 클래스 네이밍 변경
    1. 보너스 번호(BonusNumber)는 로또 숫자 1개(LottoNumber)로 관리
    2. Lotto 클래스도 List<LottoNumber>로 관리하도록 수정 필요

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,13 +1,16 @@
 package lotto.domain;
 
-public class BonusNumber {
+import java.util.Objects;
+
+public class LottoNumber {
     private static final String RANGE_ERROR_MESSAGE = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
 
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+
     private final int number;
 
-    public BonusNumber(int number) {
+    public LottoNumber(int number) {
         validateRange(number);
         this.number = number;
     }
@@ -20,5 +23,22 @@ public class BonusNumber {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) other;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }

930e7d8e254d5144ee9f4a3d9c88ede9cb8048a8
c1ce <chalicenuclear@gmail.com>
2023-11-08 13:38:25 +0900

    feat : 금액에 따른 로또구매장수를 계산하는 코드 구현

--- src/main/java/lotto/model/UserLottoPrice.java
+++ src/main/java/lotto/model/UserLottoPrice.java
@@ -4,16 +4,21 @@ public class UserLottoPrice {
     private static final String BUY_PRICE_ERROR="[ERROR] 금액은 1000원 단위로 입력해주세요";
     private static final int LOTTO_ONE_PRICE=1000;
     private int price;
+    private int lottoAmount;
     private UserLottoUtil userLottoUtil = new UserLottoUtil();
     public UserLottoPrice(String buyPrice){
         setPrice(buyPrice);
         validateUserPriceLotto(price);
+        setLottoAmount(price);
     }
     public void setPrice(String buyPrice){
         price = userLottoUtil.validateNumber(buyPrice);
     }
-    public int getPrice(){
-        return price;
+    public void setLottoAmount(int price){
+        lottoAmount = price / LOTTO_ONE_PRICE;
+    }
+    public int getLottoAmount(){
+        return lottoAmount;
     }
     private void validateUserPriceLotto(int buyPrice){
         if (buyPrice % LOTTO_ONE_PRICE != 0){

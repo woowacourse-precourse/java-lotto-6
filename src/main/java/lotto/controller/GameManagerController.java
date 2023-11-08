899206e6c87549014a0d5b40050049c39aaad777
kmw2378 <rlarla677@gmail.com>
2023-11-08 13:00:16 +0900

    feat(lotto): 요청에 따라 필요한 객체를 호출하여 비지니스 로직을 수행하는 클래스 추가 요청의 흐름에 따라 적절한 객체를 호출하여 비지니스 로직을 수행하는 클래스를 추가하였습니다.

---
+++ src/main/java/lotto/controller/GameManagerController.java
@@ -0,0 +1,69 @@
+package lotto.controller;
+
+import lotto.domain.User;
+import lotto.domain.Winning;
+import lotto.dto.request.BonusNumberRequest;
+import lotto.dto.request.PurchasePriceRequest;
+import lotto.dto.request.WinningNumberRequest;
+import lotto.dto.response.PurchasePriceResponse;
+import lotto.dto.response.WinningResponse;
+import lotto.service.GameManagerService;
+import lotto.service.LottoService;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class GameManagerController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final GameManagerService gameManagerService;
+    private final LottoService lottoService;
+
+    public GameManagerController(final InputView inputView,
+                                 final OutputView outputView,
+                                 final GameManagerService gameManagerService,
+                                 final LottoService lottoService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.gameManagerService = gameManagerService;
+        this.lottoService = lottoService;
+    }
+
+    public void start() {
+        final User user = createUser();
+        final Winning winning = createWinning();
+        settle(user, winning);
+    }
+
+    private void settle(final User user, final Winning winning) {
+        final WinningResponse winningResponse = gameManagerService.settleWinningResult(user, winning);
+        outputView.printWinningStatistic(winningResponse);
+    }
+
+    private Winning createWinning() {
+        final WinningNumberRequest winningNumberRequest = inputWinningNumber();
+        final BonusNumberRequest bonusNumberRequest = inputBonusNumber();
+        return lottoService.createWinning(winningNumberRequest, bonusNumberRequest);
+    }
+
+    private User createUser() {
+        final PurchasePriceRequest purchasePriceRequest = inputPurchase();
+        final User user = lottoService.createUser(purchasePriceRequest);
+        outputView.printPurchaseLottos(PurchasePriceResponse.createResponsesFromUser(user));
+        return user;
+    }
+
+    private PurchasePriceRequest inputPurchase() {
+        outputView.printPurchasePriceQuestionMessage();
+        return inputView.requestPurchasePrice();
+    }
+
+    private WinningNumberRequest inputWinningNumber() {
+        outputView.printWinningNumberQuestionMessage();
+        return inputView.requestWinningNumber();
+    }
+
+    private BonusNumberRequest inputBonusNumber() {
+        outputView.printBonusNumberQuestionMessage();
+        return inputView.requestBonusNumber();
+    }
+}

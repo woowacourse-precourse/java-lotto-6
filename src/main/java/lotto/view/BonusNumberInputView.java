95be42273ac883f1b10bbb37c94f469f65c145cf
sangdol <priceelees@gmail.com>
2023-11-08 13:06:39 +0900

    feat(BonusNumberInputView): 보너스 번호를 입력받는 InputViewable의 구현

---
+++ src/main/java/lotto/view/BonusNumberInputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import static lotto.view.constant.ViewConstant.InputViewConstant.InputErrorMessage.DUPLICATED_BONUS_NUMBER;
+import static lotto.view.constant.ViewConstant.InputViewConstant.InputErrorMessage.NOT_EXIST_INPUT_ERROR;
+
+import java.util.function.Supplier;
+
+public class BonusNumberInputView implements InputViewable, Supplier<Integer> {
+    private final String winningNumber;
+
+    public BonusNumberInputView(String winningNumber) {
+        this.winningNumber = winningNumber;
+    }
+
+    @Override
+    public Integer get() {
+        return Integer.parseInt(receiveInput());
+    }
+
+    @Override
+    public void validate(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(NOT_EXIST_INPUT_ERROR.getErrorMessage());
+        }
+        validateLottoNumber(input);
+        if (winningNumber.contains(input)) {
+            throw new IllegalArgumentException(DUPLICATED_BONUS_NUMBER.getErrorMessage());
+        }
+    }
+}

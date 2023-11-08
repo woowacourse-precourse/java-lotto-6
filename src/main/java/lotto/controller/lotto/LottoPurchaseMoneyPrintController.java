4abee98b7320805fc20792751e345b9668f39f6e
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-08 23:32:20 +0900

    feat : 구입 금액 출력 객체 생성

---
+++ src/main/java/lotto/controller/lotto/LottoPurchaseMoneyPrintController.java
@@ -0,0 +1,11 @@
+package lotto.controller.lotto;
+
+import lotto.model.lotto.LottoPurchaseMoney;
+import lotto.view.ProcessedValueOutputView;
+
+public class LottoPurchaseMoneyPrintController {
+	ProcessedValueOutputView processedValueOutputView = new ProcessedValueOutputView();
+	public void printLottoPurchaseMoney(LottoPurchaseMoney lottoPurchaseMoney) {
+		processedValueOutputView.printLottoPurchaseMoney(lottoPurchaseMoney.getLottoPurchaseMoney());
+	}
+}

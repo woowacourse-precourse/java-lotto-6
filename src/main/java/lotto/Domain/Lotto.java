6f2d14481a8b5e92ce14e8426a934b4e5366c843
lee-JunR <wns2610@icloud.com>
2023-11-08 14:58:12 +0900

    feat : "로또 클래스 생성 및 검증 추가"

---
+++ src/main/java/lotto/Domain/Lotto.java
@@ -0,0 +1,27 @@
+package lotto.Domain;
+
+import java.util.List;
+
+public class Lotto {
+
+  private final List<Integer> numbers;
+  private final int MIN_NUMBER = 1;
+  private final int MAX_NUMBER = 45;
+
+  public Lotto(List<Integer> numbers) {
+    validate(numbers);
+    this.numbers = numbers;
+  }
+
+  private void validate(List<Integer> numbers) {
+    if (numbers.size() != 6) {
+      throw new IllegalArgumentException("로또 넘버는 6개의 수를 포함해야 합니다.");
+    }
+
+    for (int number : numbers) {
+      if (number < MIN_NUMBER || number > MAX_NUMBER) {
+        throw new IllegalArgumentException("로또 넘버는 1부터 45 사이의 수이어야 합니다.");
+      }
+    }
+  }
+}

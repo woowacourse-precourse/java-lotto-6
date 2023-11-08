d15bcf1b7f8b7b9c6d0aa8237c10293f9682f81f
Identityless <rlandwnd555@naver.com>
2023-11-08 17:59:22 +0900

    test : 통계 반영에 대한 테스트 메서드 추가

--- src/test/java/lotto/InfoTest.java
+++ src/test/java/lotto/InfoTest.java
@@ -1,7 +1,22 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lotto.constant.Grade;
+import lotto.model.Info;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class InfoTest {
+    @DisplayName("당첨 통계 반영 테스트")
+    @Test
+    void 통계_반영에_대한_테스트() {
+        Info info = new Info();
+        info.addStatistic(Grade.FIRST_GRADE);
+        info.addStatistic(Grade.SECCOND_GRADE);
+        info.addStatistic(Grade.FIFTH_GRADE);
+        info.addStatistic(Grade.FIRST_GRADE);
 
+        assertThat(info.getStatistic()).isEqualTo(new int[]{0, 2, 1, 0, 0, 1});
+    }
 }

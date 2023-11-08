ab6a1a1479b6a31258b6bf918e8d977b2f96f200
jonghyeok-97 <dkswhdgur1209@naver.com>
2023-11-08 15:16:18 +0900

    style: 적절한 네이밍으로 변경

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -3,24 +3,24 @@ package lotto.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_PRICE_FOR_PURCHASING_LOTTO = "구입금액을 입력해 주세요.";
-    private static final String INPUT_WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
-    private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String PRICE_FOR_PURCHASING_LOTTO = "구입금액을 입력해 주세요.";
+    private static final String WINNING_LOTTO = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
     public static String inputPriceForPurchasingLotto() {
-        System.out.println(INPUT_PRICE_FOR_PURCHASING_LOTTO);
+        System.out.println(PRICE_FOR_PURCHASING_LOTTO);
         return Console.readLine();
     }
 
     public static String inputWinningLotto() {
         OutputView.newLine();
-        System.out.println(INPUT_WINNING_LOTTO);
+        System.out.println(WINNING_LOTTO);
         return Console.readLine();
     }
 
     public static String inputBonusNumber() {
         OutputView.newLine();
-        System.out.println(INPUT_BONUS_NUMBER);
+        System.out.println(BONUS_NUMBER);
         return Console.readLine();
     }
 }

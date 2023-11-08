441e78fba5b15e66202265ebf87f4fd7211d2eb2
HyunmoBae <tsi0520@naver.com>
2023-11-08 19:53:14 +0900

    fix(lotto) : fix error process

--- src/main/java/lotto/domain/LottoTicketGenerator.java
+++ src/main/java/lotto/domain/LottoTicketGenerator.java
@@ -3,7 +3,6 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
-import lotto.error.Error;
 import lotto.error.Error.ErrorType;
 
 public class LottoTicketGenerator {
@@ -27,13 +26,12 @@ public class LottoTicketGenerator {
     }
 
     public void insertMoney(int money) {
-        validate(money);
         this.ticketCount = money / 1000;
     }
 
-    private void validate(int money) {
+    public void validate(int money) {
         if (!(money % 1000 == 0) || money <= 0) {
-            Error.errorMessage(ErrorType.INVALID_AMOUNT);
+            throw new IllegalArgumentException(ErrorType.INVALID_MONEY_VALUE.getMessage());
         }
     }
 

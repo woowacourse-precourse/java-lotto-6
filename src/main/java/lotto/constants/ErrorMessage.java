407f6d9683d1a5d943399ac5b16dcdfc0dafdccf
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-11-08 16:11:03 +0900

    feat: 에러메시지 등록

---
+++ src/main/java/lotto/constants/ErrorMessage.java
@@ -0,0 +1,22 @@
+package lotto.constants;
+
+public enum ErrorMessage {
+    ERROR_AMOUNT_LESS_1000("로또 구입 금액을 1000원 이상 입력해주세요."),
+    ERROR_AMOUNT_UNDIVIDED_1000("로또 구입 금액을 1000원 단위로 입력해주세요."),
+    ERROR_AMOUNT_OTHER_STRING("로또 구입 금액을 1000원 단위로 숫자만 입력해주세요."),
+    ERROR_PRIZE_NUM_LESS_1_OVER_45("당첨 번호는 1부터 45 사이의 숫자로 입력해주세요."),
+    ERROR_PRIZE_NUM_SIZE_LESS_OR_OVER_6("당첨 번호는 6개만 입력해주세요."),
+    ERROR_PRIZE_NUM_OVERLAP("당첨 번호는(1-45) 6개를 중복되지 않게 입력해주세요."),
+    ERROR_PRIZE_NUM_OTHER_STRING("당첨 번호는(1-45) 6개를 쉼표(,)로 구분해 입력해주세요."),
+    ERROR_BONUS_NUM_OVERLAP("보너스 번호는(1-45) 당첨 번호 6개와 중복되지 않는 숫자로 입력해주세요.\n");
+
+    private String errorMessage;
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    // Getter
+    public String getErrorMessage() {
+        return "[ERROR] " + errorMessage;
+    }
+}

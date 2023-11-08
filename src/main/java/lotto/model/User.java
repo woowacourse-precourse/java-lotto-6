ef9f156fc7224bb63371ffba7ad0d0c9d2e0abb8
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-11-08 16:25:18 +0900

    feat: user model 설정

--- src/main/java/lotto/model/User.java
+++ src/main/java/lotto/model/User.java
@@ -1,4 +1,11 @@
 package lotto.model;
 
+import static lotto.view.InputView.lottoBoughtPrice;
+
 public class User {
+    public static int lottoBoughtNum;
+
+    public static void lottoBoughtNum() {
+        lottoBoughtNum = lottoBoughtPrice / 1000;
+    }
 }

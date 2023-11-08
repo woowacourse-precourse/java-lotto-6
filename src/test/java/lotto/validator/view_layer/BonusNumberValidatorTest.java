db855c2f39cd85c453ce2ad83b3ffcaddd4d5682
gomudayya <gomudayya@gmail.com>
2023-11-08 14:28:59 +0900

    Test : 보너스번호 테스트 작성

---
+++ src/test/java/lotto/validator/view_layer/BonusNumberValidatorTest.java
@@ -0,0 +1,25 @@
+package lotto.validator.view_layer;
+
+import lotto.constant.ErrorMessage;
+import lotto.domain.Lotto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static lotto.constant.ErrorMessage.WINNING_LOTTO_CONTAIN_BONUS_NUMBER;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusNumberValidatorTest {
+
+    BonusNumberValidator bonusNumberValidator = new BonusNumberValidator(new Lotto(List.of(21,22,23,24,25,26)));
+    @Test
+    @DisplayName("보너스 번호가 당첨 번호와 일치하는것이 있으면 안된다")
+    void validate() {
+        assertThatThrownBy(() -> bonusNumberValidator.validate("23"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(WINNING_LOTTO_CONTAIN_BONUS_NUMBER);
+    }
+}

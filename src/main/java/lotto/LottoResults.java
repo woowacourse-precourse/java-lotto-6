8078d8a6f9b557f78b5bf45fe6772b064e9853fb
WiFros <39113029+WiFros@users.noreply.github.com>
2023-11-08 16:28:30 +0900

    addResult 메서드 추가
    새로운 당첨 결과를 추가하는 메서드입니다.
     이 메서드는 다음과 같은 역할을 합니다.
     - 결과를 기록합니다. 객체 내에 기록됩니다.
     - 집계를 업데이트 합니다. 1등,2등,3등,...의 수를 집계합니다.
     - 결과를 분석합니다. 어떤 등수가 몇번이 나왔는지를 파악합니다.

--- src/main/java/lotto/LottoResults.java
+++ src/main/java/lotto/LottoResults.java
@@ -4,9 +4,14 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class LottoResults {
-    private final Map<LottoRank, Long> results;
-
-    public LottoResults(Map<LottoRank, Long> results) {
-        this.results = new EnumMap<>(results);
+    private final Map<LottoRank, Integer> results;
+    public LottoResults() {
+        this.results = new EnumMap<>(LottoRank.class);
+        for (LottoRank rank : LottoRank.values()) {
+            results.put(rank, 0);
+        }
+    }
+    public void addResult(LottoRank rank) {
+        results.merge(rank, 1, Integer::sum);
     }
 }

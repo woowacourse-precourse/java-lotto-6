faf86ab666921bda5e8d47ba846841485784a1a9
baejeong-eun <lovelyeun0914@gmail.com>
2023-11-08 19:05:15 +0900

    fix: 오름정렬 lotto클래스 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,20 +2,20 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.util.Utils.ascendingOrder;
+
 public class Lotto {
     public final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = ascendingOrder(numbers);
     }
-
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
     }
-
     // TODO: 추가 기능 구현
 
 }

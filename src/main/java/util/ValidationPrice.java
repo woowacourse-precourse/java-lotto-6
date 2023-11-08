02894ee5898cbe4155018c5495550a15590aff86
junseoks-99 <angec5645@naver.com>
2023-11-08 19:43:02 +0900

    feat: 구입 금액 음수 여부 검증 기능 추가

--- src/main/java/util/ValidationPrice.java
+++ src/main/java/util/ValidationPrice.java
@@ -4,6 +4,7 @@ public class ValidationPrice {
 
     private static final Integer UNIT_NUMBER = 1000;
     private static final Integer VALID_REMAINDER = 0;
+    private static final Integer ZERO = 0;
 
     private ValidationPrice() {
 
@@ -11,10 +12,11 @@ public class ValidationPrice {
 
     public static void validatePrice(String priceString) {
         validateNumber(priceString);
+        validateNegativeNumber(priceString);
         validateRemainder(priceString);
     }
 
-    public static void validateNumber(String priceString) {
+    private static void validateNumber(String priceString) {
         try {
             Integer price = Integer.parseInt(priceString);
         } catch (NumberFormatException notANumber) {
@@ -22,10 +24,18 @@ public class ValidationPrice {
         }
     }
 
-    public static void validateRemainder(String priceString) {
+    private static void validateRemainder(String priceString) {
         Integer price = Integer.parseInt(priceString);
+        System.out.println(price);
         if (price % UNIT_NUMBER != VALID_REMAINDER) {
             throw new IllegalArgumentException(ValidationPriceMessages.INVALID_REMAINDER.getMessage());
+        }
+    }
+
+    private static void validateNegativeNumber(String priceString) {
+        Integer price = Integer.parseInt(priceString);
+        if (price < ZERO) {
+            throw new IllegalArgumentException(ValidationPriceMessages.NEGATIVE_NUMBER.getMessage());
         }
     }
 }

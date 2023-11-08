e29690b04492a79d952289b4bd7563394aa87b44
jjongwon7 <jjongwon7@naver.com>
2023-11-08 21:35:53 +0900

    feat: 로또 구입 금액 입력 기능 추가 입력을 담당하는 UserInput 클래스를 만들고, 로또 구입 금액을 입력받는 메서드 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import static lotto.views.UserInput.readPurchasePrice;
 import static lotto.views.UserOutput.askPurchasePrice;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         askPurchasePrice();
+        Integer purchasePrice = readPurchasePrice();
     }
 }
---
+++ src/main/java/lotto/views/UserInput.java
@@ -0,0 +1,20 @@
+package lotto.views;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.utils.StringUtils.getIntegerValue;
+import static lotto.utils.StringUtils.validContainsWhiteSpace;
+import static lotto.utils.StringUtils.validEmptyString;
+import static lotto.validators.PurchaseValidator.validPurchasePrice;
+
+public class UserInput {
+    public static Integer readPurchasePrice() {
+        String inputLottoPurchasePrice = readLine();
+        validEmptyString(inputLottoPurchasePrice);
+        validContainsWhiteSpace(inputLottoPurchasePrice);
+
+        Integer purchasePrice = getIntegerValue(inputLottoPurchasePrice);
+        validPurchasePrice(purchasePrice);
+
+        return purchasePrice;
+    }
+}

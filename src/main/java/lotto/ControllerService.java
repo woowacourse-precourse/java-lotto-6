1cebe4c71b4851cc48107674be9b5fcc295dc99e
sangxxjin <sj_0368@naver.com>
2023-11-08 23:06:59 +0900

    feat: 당첨 번호 잘못 입력시 재입력 메소드 구현

--- src/main/java/lotto/ControllerService.java
+++ src/main/java/lotto/ControllerService.java
@@ -1,14 +1,18 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class ControllerService {
   private static final String PURCHASE_AMOUNT_ERROR_MESSAGE = "[ERROR] 구입 금액이 유효하지 않습니다.";
+  private static final String INVALID_WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 로또 번호가 유효하지 않습니다.";
+
   public static String isValidPurchaseAmount(String purchaseAmount) {
     try {
       // 문자열을 숫자로 변환하여 예외가 발생하지 않으면 숫자로 판단
-      double d = Double.parseDouble(purchaseAmount);
+      int canInteger = Integer.parseInt(purchaseAmount);
     } catch (NumberFormatException e) {
       OutputView.printErrorMessage(PURCHASE_AMOUNT_ERROR_MESSAGE);
       isValidPurchaseAmount(InputView.getPurchaseAmount()); // 재귀 호출
@@ -17,8 +21,18 @@ public class ControllerService {
     return purchaseAmount;
   }
 
-  public static String isValidWinningLotto(String winningLotto){
-  return "a";
+  public static String isValidWinningNumber(String winningNumbers) {
+    try {
+      List<Integer> winningNumber = Arrays.stream(winningNumbers.split(","))
+          .map(String::trim)
+          .map(Integer::parseInt)
+          .toList();
+
+    } catch (NumberFormatException e) {
+      OutputView.printErrorMessage(INVALID_WINNING_NUMBER_ERROR_MESSAGE);
+      return isValidWinningNumber(InputView.getWinningNumber()); // 재귀 호출
+    }
+    return winningNumbers;
   }
 
 }

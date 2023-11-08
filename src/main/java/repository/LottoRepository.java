9ed1cc7c9379195c5a82ab4f469f7dd0c111daee
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 18:08:43 +0900

    feat: '로또들의 등수를 업데이트하는 메서드와 반환하는 메서드 생성'

--- src/main/java/repository/LottoRepository.java
+++ src/main/java/repository/LottoRepository.java
@@ -1,9 +1,12 @@
 package repository;
 
 import domain.Lotto;
+import domain.Place;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoRepository {
     public List<Lotto> lottoList;
@@ -23,6 +26,30 @@ public class LottoRepository {
     }
     public void clear(){
         lottoList.clear();
+    }
+
+    public void updateAllLottoPlace(Lotto winningNumbers,int bounsNumber){
+        for(Lotto lotto : lottoList){
+            lotto.whichPlace(winningNumbers,bounsNumber);
+        }
+    }
+
+    public Map<Place,Integer> findAllPlace(){
+        Map<Place,Integer> numberOfPlaceMap = new HashMap<>(){{
+            put(Place.FIRST,0);
+            put(Place.SECOND,0);
+            put(Place.THIRD,0);
+            put(Place.FORTH,0);
+            put(Place.FIFTH,0);
+            put(Place.NONE,0);
+        }};
+
+        for(Lotto lotto : lottoList){
+            int numberOfPlace = numberOfPlaceMap.get(lotto.getPlace());
+            numberOfPlaceMap.put(lotto.getPlace(),numberOfPlace+1);
+        }
+
+        return numberOfPlaceMap;
     }
 
 }

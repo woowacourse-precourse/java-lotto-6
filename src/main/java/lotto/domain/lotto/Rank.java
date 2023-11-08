fc68079b34977c1d0bd318cbe0c8978c50182243
김강년 <64246800+ekek54@users.noreply.github.com>
2023-11-08 15:44:44 +0900

    refactor: 구현 간단화
    변경 전
      - Map을 통해 랭킹 조회
    변경 후
      - 직접 순회하여 랭킹 조회

--- src/main/java/lotto/domain/lotto/Rank.java
+++ src/main/java/lotto/domain/lotto/Rank.java
@@ -1,59 +1,48 @@
 package lotto.domain.lotto;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public enum Rank {
-    FIRST(2_000_000_000),
-    SECOND(30_000_000),
-    THIRD(1_500_000),
-    FOURTH(50_000),
-    FIFTH(5_000),
-    MISS(0);
-
-    private static final Map<MatchResult, Rank> RANK_MAP = new HashMap<>();
-
-    static {
-        RANK_MAP.put(new MatchResult(6, false), FIRST);
-        RANK_MAP.put(new MatchResult(5, true), SECOND);
-        RANK_MAP.put(new MatchResult(5, false), THIRD);
-        RANK_MAP.put(new MatchResult(4, false), FOURTH);
-        RANK_MAP.put(new MatchResult(3, false), FIFTH);
-    }
+    FIRST(2_000_000_000L, 6, false),
+    SECOND(30_000_000L, 5, true),
+    THIRD(1_500_000L, 5, false),
+    FOURTH(50_000L, 4, false),
+    FIFTH(5_000L, 3, false),
+    MISS(0L, 0, false);
 
     private final long prize;
+    private final int matchCount;
+    private final boolean matchBonus;
 
-    Rank(int prize) {
+    Rank(long prize, int matchCount, boolean matchBonus) {
         this.prize = prize;
+        this.matchCount = matchCount;
+        this.matchBonus = matchBonus;
     }
 
     public static Rank of(int matchCount, boolean matchBonus) {
-        return RANK_MAP.getOrDefault(new MatchResult(matchCount, matchBonus), MISS);
+        return Arrays.stream(Rank.values())
+                .filter(rank -> isSameMatchPoint(matchCount, matchBonus, rank))
+                .findFirst()
+                .orElse(MISS);
     }
 
-    private static class MatchResult {
-        private final int matchCount;
-        private final boolean matchBonus;
+    private static boolean isSameMatchPoint(int matchCount, boolean matchBonus, Rank rank) {
+        return rank.matchCount == matchCount && rank.matchBonus == matchBonus;
+    }
 
-        public MatchResult(int matchCount, boolean matchBonus) {
-            this.matchCount = matchCount;
-            this.matchBonus = matchBonus;
-        }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof MatchResult matchResult) {
-                return matchCount == matchResult.matchCount && matchBonus == matchResult.matchBonus;
-            }
-            return false;
-        }
+    public long getPrize() {
+        return prize;
+    }
 
-        @Override
-        public int hashCode() {
-            if (matchBonus) {
-                return matchCount * 2 + 1;
-            }
-            return matchCount * 2;
-        }
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public boolean getMatchBonus() {
+        return matchBonus;
     }
 }

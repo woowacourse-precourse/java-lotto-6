91b8d83a3046a85d4e4d75bbf8a181a9d6cc6c4c
yoonion <yoonion97@gmail.com>
2023-11-08 16:57:25 +0900

    test: 로또 구입 금액이 1000원으로 나누어떨지지않는 예외처리 테스트 코드 작성

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -11,8 +11,18 @@ class LottoServiceTest {
 
     @DisplayName("로또 구입 금액이 숫자가 아닌 문자열로 이루어져있을 때 예외 처리")
     @Test
-    void inputButLottoAmountNotNumber() {
+    void inputBuyLottoAmountNotNumber() {
         String buyAmount = "1000abc";
+
+        assertThatThrownBy(() -> lottoService.buyLottoAmountValidate(buyAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+
+    @DisplayName("로또 구입 금액이 1000원 단위로 나누어 떨어지지 않을 때 예외 처리")
+    @Test
+    void inputBuyLottoAmountIndivisible() {
+        String buyAmount = "900";
 
         assertThatThrownBy(() -> lottoService.buyLottoAmountValidate(buyAmount))
                 .isInstanceOf(IllegalArgumentException.class)

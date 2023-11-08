1be7e0f8a5b76faabf95f85c549fa506847ab8dc
kimds777 <77198242+kimds777@users.noreply.github.com>
2023-11-08 16:15:11 +0900

    로또 안내 멘트 및 로또 번호 생성, 정렬하기

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -1,13 +1,22 @@
 package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Input {
 
     private int amount;
+    List<List<Integer>> lottos;
+    private String inputSpeech = "구입금액을 입력해 주세요.";
+    private String amountSpeech = "개를 구매했습니다.";
     private String amountError = "[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.";
 
     public Input() {
+        System.out.println(inputSpeech);
         start();
     }
 
@@ -21,12 +30,28 @@ public class Input {
             throw new IllegalArgumentException(amountError);
         }
 
-        makeLottoNumber(amount / 1000);
+        lottoCount(amount / 1000);
     }
 
-    private void makeLottoNumber(int divisionAmount) {
-        for (int i = 0; i < divisionAmount; i++) {
+    private void lottoCount(int divisionAmount){
+        System.out.println(divisionAmount+amountSpeech);
+        makeLottoNumber(divisionAmount);
+    }
 
+    private void makeLottoNumber(int lottoAmount) {
+        lottos = new ArrayList<>();
+        for (int i = 0; i < lottoAmount; i++) {
+            lottos.add(getNumber());
         }
+    }
+
+    private List<Integer> getNumber(){
+        List<Integer> lottoNumber = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
+        return numberSort(lottoNumber);
+    }
+
+    private List<Integer> numberSort(List<Integer> lottoNumber){
+        Collections.sort(lottoNumber);
+        return lottoNumber;
     }
 }

af025cedf31cb7ab631904a8992e9d03bd45c16d
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-11-08 22:44:19 +0900

    feat: 작동을 위한 세부 작동 기능 구현

--- src/main/java/lotto/controller/RunLottoGame.java
+++ src/main/java/lotto/controller/RunLottoGame.java
@@ -2,13 +2,16 @@ package lotto.controller;
 
 import lotto.model.Lotto;
 import lotto.model.LottoTicket;
-
-import java.util.List;
+import lotto.model.Prize;
+import lotto.view.Statement;
 
 public class RunLottoGame {
     private SetUserLotto userLotto;
     private SetLottoTicket setLottoTicket;
+    private Calculation calculation;
+
     LottoTicket lottoTicket = setLottoTicket.lottoTickets();
+    Lotto lotto = userLotto.userLotto();
 
     public void gameIntro() {
         Printer.printPurchaseAmount();
@@ -32,8 +35,21 @@ public class RunLottoGame {
         System.out.println();
     }
 
-    public void endGame() {
+    public void terminateGame(int[] counts, double rate) {
+        counts = calculation.countMatchingNumbers(lotto, lottoTicket);
+        int start = 0;
         Printer.printResult();
         System.out.println("---");
+
+        for(Prize prize : Prize.values()) {
+            if(!prize.getHasBonus()) {
+                System.out.println(prize.getMatchingNumbers() + "개 일치, 보너스 볼 일치 (" + prize.getPrizeAmount() + "원) - " + counts[5] + "개");
+            } else {
+                System.out.println(prize.getMatchingNumbers() + "개 일치 (" + prize.getPrizeAmount() + "원) - " + counts[start] + "개");
+            }
+        }
+
+        System.out.println(Statement.SHOW_PROFIT_RATE + calculation.calculateProfitRate(lotto,lottoTicket,userLotto) + "%입니다.");
     }
+
 }

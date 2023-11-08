afb8eabb779055c5769eda6fb7beae8b28f93562
706com <ehdgusjdbswl@gmail.com>
2023-11-08 19:35:05 +0900

    refactor : 서비스 생성이 아닌 객체 내에서 중복 확인

--- src/main/java/lotto/domain/LotteryGenerator.java
+++ src/main/java/lotto/domain/LotteryGenerator.java
@@ -2,8 +2,12 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import lotto.service.WinningNumberService;
+import lotto.service.exception.IllegalArgumentExceptionHandler;
+import lotto.service.exception.InputErrorMessage;
 
 public class LotteryGenerator {
     private static final int START_NUMBER = 1;
@@ -13,14 +17,23 @@ public class LotteryGenerator {
     private Lotto lotto;
 
     private void generateLotto() {
-        List<Integer> numbers = new ArrayList<>();
-        WinningNumberService winningNumberService = new WinningNumberService();
+        List<Integer> numbers;
         do {
             numbers = Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER,NUMBER_SIZE);
 
-        } while (!winningNumberService.verifyDuplication(numbers));
-
+        } while (!verifyDuplication(numbers));
         this.lotto = new Lotto(numbers);
+    }
+
+    private boolean verifyDuplication(List<Integer> winningNumber){
+        Set<Integer> checkDuplication = new HashSet<>();
+        for(int number : winningNumber){
+            if(checkDuplication.contains(number)){
+                throw new IllegalArgumentExceptionHandler(InputErrorMessage.DUPLICATE_NUMBER);
+            }
+            checkDuplication.add(number);
+        }
+        return true;
     }
 
     public Lotto getLotto() {

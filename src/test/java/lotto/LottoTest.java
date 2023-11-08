8a3c2a048140c9562e28b60c4e2e306041a4e4d0
yeon2lee <md04254@naver.com>
2023-11-08 21:19:16 +0900

    test: 로또 번호 1부터 45까지의 숫자인지 확인

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -23,5 +23,12 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1부터 45까지의 숫자가 아니면 예외가 발생한다.")
+    @Test
+    void createLottoByOverRange() {
+        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

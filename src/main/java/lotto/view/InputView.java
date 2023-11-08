cfc4f2e7b7d80c4c74d7d399a4ace3f070e50140
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-11-08 15:24:53 +0900

    feat(view): 입력 view 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import lotto.utils.StringUtils;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static lotto.utils.Constants.*;
+
+public class InputView {
+
+    public static int inputBuyLotto() {
+        System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return readInt();
+    }
+
+    private static int readInt() {
+        String inputInt = readLine();
+        return StringUtils.stringToInt(inputInt);
+    }
+
+    public static String inputWinningNumbers() {
+        System.out.println();
+        System.out.println(WINNING_NUMBER_MESSAGE);
+        return readLine();
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println();
+        System.out.println(BONUS_NUMBER_MESSAGE);
+        return readInt();
+    }
+}

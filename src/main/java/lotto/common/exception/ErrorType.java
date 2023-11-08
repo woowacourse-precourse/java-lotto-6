87eb27a430654ff576684c47317c18cdcdadfaec
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-08 22:20:27 +0900

    feature: read valid winning lotto numbers

--- src/main/java/lotto/common/exception/ErrorType.java
+++ src/main/java/lotto/common/exception/ErrorType.java
@@ -2,6 +2,7 @@ package lotto.common.exception;
 
 public enum ErrorType {
     INVALID_PURCHASE_AMOUNT("로또 구입 금액은 1,000원 단위이어야 합니다."),
+    INVALID_LOTTO_SIZE("로또 번호의 개수는 6개여야 합니다."),
     INVALID_NUMBERS("로또 번호와 보너스 번호는 1부터 45 사이의 숫자여야 합니다."),
     DUPLICATE_NUMBERS("로또 번호와 보너스 번호는 모두 서로 중복되지 않는 숫자여야 합니다."),
     ;
--- src/main/java/lotto/core/domain/DrawLottoMachine.java
+++ src/main/java/lotto/core/domain/DrawLottoMachine.java
@@ -5,16 +5,64 @@ import lotto.common.console.Output;
 import lotto.common.exception.ErrorType;
 import lotto.common.exception.LottoGameException;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static lotto.core.domain.Lotto.LOTTO_SIZE;
 
 public class DrawLottoMachine {
     private final Lotto winningLotto;
     private final int bonusNumber;
 
-    public DrawLottoMachine(Lotto winningLotto) {
-        this.winningLotto = winningLotto;
+    public DrawLottoMachine() {
+        this.winningLotto = new Lotto(readWinningLottoNumbers());
         this.bonusNumber = readBonusNumber();
     }
+
+    private List<Integer> readWinningLottoNumbers() {
+        String winningLottoNumbers;
+
+        do {
+            winningLottoNumbers = Input.readLottoNumbers();
+        } while (!isValidWinningLottoNumbers(winningLottoNumbers));
+
+        return Arrays.stream(winningLottoNumbers.split(","))
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private boolean isValidWinningLottoNumbers(String winningLottoNumbersInput) {
+        try {
+            List<Integer> winningLottoNumbers = convertWinningLottoNumbers(winningLottoNumbersInput);
+            validateWinningLottoNumbers(winningLottoNumbers);
+            return true;
+        } catch (IllegalArgumentException e) {
+            Output.writeMessage(e.getMessage());
+            return false;
+        }
+    }
+
+    private List<Integer> convertWinningLottoNumbers(String winningLottoNumbersInput) {
+        List<Integer> winningLottoNumbers = Arrays.stream(winningLottoNumbersInput.split(","))
+                .map(Integer::parseInt)
+                .toList();
+
+        if(winningLottoNumbers.size() != LOTTO_SIZE) {
+            throw LottoGameException.withType(ErrorType.INVALID_LOTTO_SIZE);
+        }
+
+        return winningLottoNumbers;
+    }
+    private void validateWinningLottoNumbers(List<Integer> winningLottoNumbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(winningLottoNumbers);
+
+        if (uniqueNumbers.size() != winningLottoNumbers.size()) {
+            throw LottoGameException.withType(ErrorType.DUPLICATE_NUMBERS);
+        }
+    }
+
     private int readBonusNumber() {
         String bonusNumber;
 

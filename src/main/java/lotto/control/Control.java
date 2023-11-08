ba138ea92d35fd2da0622d31366466cbc1553fb6
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 14:18:19 +0900

    refactor: change variable name
    가독성을 위해 변수명 변경

--- src/main/java/lotto/control/Control.java
+++ src/main/java/lotto/control/Control.java
@@ -6,14 +6,14 @@ import lotto.domain.Lotto;
 import lotto.domain.LottoTicket;
 import lotto.domain.Prize;
 import lotto.service.LottoTicketGenerator;
-import lotto.service.PrizeStaticService;
+import lotto.service.PrizeStatisticService;
 import lotto.service.UserRequestService;
 import lotto.view.OutputView;
 
 public class Control {
     public static void playLotto() {
         int lottoCost = startLotto();
-        LottoTicket ticket = generateLotto(lottoCost);
+        LottoTicket ticket = generateLotto(calculateTicketCount(lottoCost));
         resultLotto(ticket, lottoCost);
     }
 
@@ -21,7 +21,7 @@ public class Control {
         int lottoCost = UserRequestService.requestLottoCost();
         int lottoTicketCount = calculateTicketCount(lottoCost);
         OutputView.printLottoTicketCount(lottoTicketCount);
-        return lottoTicketCount;
+        return lottoCost;
     }
 
     private static LottoTicket generateLotto(int lottoTicketCount) {
@@ -35,9 +35,9 @@ public class Control {
         int bonusNumber = UserRequestService.requestBonusNumber(winningLotto.getNumbers());
 
         Map<Prize, Integer> matchNumberCount = ticket.compilePrizeStatistics(winningLotto, bonusNumber);
-        int totalWinnings = PrizeStaticService.calculateAndDisplayPrizeStatistics(matchNumberCount);
+        int totalPrize = PrizeStatisticService.processAndReportPrizeResults(matchNumberCount);
 
-        OutputView.printProfitRate(totalWinnings, lottoCost);
+        OutputView.printProfitRate(totalPrize, lottoCost);
     }
 
     public static int calculateTicketCount(int lottoCost) {

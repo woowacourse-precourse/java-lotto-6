b45a71b027b81ce0b8b88b336e27a4c3acf34d5a
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 13:49:48 +0900

    feat(InputView): 당첨 번호 입력받는 view 메서드 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,8 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INSERT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String INSERT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
+
     public String readPurchaseAmount() {
         System.out.println(INSERT_PURCHASE_AMOUNT);
+        return Console.readLine();
+    }
+
+    public String readWinningNumbers() {
+        System.out.println(INSERT_WINNING_NUMBERS);
         return Console.readLine();
     }
 }

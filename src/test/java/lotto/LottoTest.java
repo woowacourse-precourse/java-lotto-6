e3d6bebbb486e49d17d001596784008d640d3c80
pjaehyun <show6756@gmail.com>
2023-11-08 16:14:00 +0900

    feat: 추가 로또 번호 출력 기능 함수 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,12 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,4 +28,22 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @DisplayName("로또 번호를 출력한다.")
+    @Test
+    void printLottoNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 7; i++) {
+            numbers.add(i);
+        }
+        Lotto lotto = new Lotto(numbers);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        lotto.printLottoNumbers();
+
+        String captured = outputStream.toString().trim();
+        assertThat(captured).isEqualTo("[1, 2, 3, 4, 5, 6]");
+     }
 }

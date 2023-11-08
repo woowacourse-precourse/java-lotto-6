a3577a3b09e40567449109902d5f3a1e484d910e
“sangjun121” <juncho12011201@gmail.com>
2023-11-08 22:03:48 +0900

    feat: 당첨 통계 출력 함수 구현
    OutputView.printLottosResult()에서 WinningCountDto를 받아 당첨 종류별 당첨 통계 출력 구현 완료

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.dto.WinningCountDto;
 import lotto.utility.enums.Messages;
 
 import java.util.List;
@@ -14,6 +15,15 @@ public class OutputView {
 
     public static void printLottoNumbers(List<Integer> lottoNumbers){
         System.out.println(lottoNumbers.toString());
+    }
+
+    public static void printLottosResult(WinningCountDto winningCountDto){
+        System.out.println(Messages.WINNING_STATISTICS_MESSAGE.getMessage());
+        System.out.println(String.format(Messages.THREE_WINNING_MESSAGE.getMessage(),winningCountDto.getMatchThreeWinningCount()));
+        System.out.println(String.format(Messages.FOUR_WINNING_MESSAGE.getMessage(),winningCountDto.getMatchFourWinningCount()));
+        System.out.println(String.format(Messages.FIVE_WINNING_MESSAGE.getMessage(),winningCountDto.getMatchFiveWinningCount()));
+        System.out.println(String.format(Messages.FIVE_WINNING_AND_BONUS_MESSAGE.getMessage(),winningCountDto.getMatchFiveWinningAndBonusCount()));
+        System.out.println(String.format(Messages.SIX_WINNING_MESSAGE.getMessage(),winningCountDto.getMatchSixWinningCount()));
     }
 
 }

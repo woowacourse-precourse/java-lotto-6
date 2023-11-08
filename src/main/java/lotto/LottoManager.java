01da1a4b03ab89341a268d94184771db21fddbab
choish19 <100904838+choish19@users.noreply.github.com>
2023-11-08 22:02:09 +0900

    Feat: "추가 createLotties() 함수"
    LottoManager 클래스
    - createLotties() 함수 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -2,10 +2,25 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoManager {
     private int purchaseAmount;
+    private List<Lotto> lotties;
+
     void inputLottoPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         purchaseAmount = Integer.getInteger(Console.readLine());
+    }
+
+    void createLotties() {
+        lotties = new ArrayList<>();
+        NumberGenerator numberGenerator = new NumberGenerator();
+        int lottoPrice = 1000;
+        for(int i = 0; i < this.purchaseAmount/lottoPrice; i++) {
+            List<Integer> numbers = numberGenerator.createRandomNumbers();
+            lotties.add(new Lotto(numbers));
+        }
     }
 }

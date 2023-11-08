862ab926ca8f5f51289079ffec9847ebcd738b0a
yeahdy <yejine98@naver.com>
2023-11-08 23:04:03 +0900

    Feat: 스캐너 입력 종료

--- src/main/java/service/LottoGameService.java
+++ src/main/java/service/LottoGameService.java
@@ -29,6 +29,8 @@ public class LottoGameService {
         setInputBonusLottoGame();
 
         printLottoResultStatistic();
+
+        Console.close();
     }
 
     private void purchaseLottoGame(){

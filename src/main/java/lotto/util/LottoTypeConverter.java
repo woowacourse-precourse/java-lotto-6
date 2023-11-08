cc2e6396aa76b21fa494d47038f75877158e3a67
taewan625 <taewan1997625@gmail.com>
2023-11-08 15:29:30 +0900

    Refacotr: method 분리

--- src/main/java/lotto/util/LottoTypeConverter.java
+++ src/main/java/lotto/util/LottoTypeConverter.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 public class LottoTypeConverter {
     private static final String COMMA = ",";
-    public static List<Integer> toList(String winningNumber) {
-        String[] split = winningNumber.split(COMMA);
-        return Arrays.stream(split).map(Integer::parseInt).toList();
+
+    public static String[] toArray(String winningNumber) {
+        return winningNumber.split(COMMA);
+    }
+
+    public static List<Integer> toList(String[] winningNumber) {
+        return Arrays.stream(winningNumber).map(Integer::parseInt).toList();
     }
 
     public static String toWon(int money) {

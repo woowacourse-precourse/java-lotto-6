6f7178a4cd037a1f08c5259e2c9d50a511101f4d
강승민 <rkdtmdals999@naver.com>
2023-11-08 21:23:22 +0900

    [test] 당첨 번호와 중복된 보너스 번호 테스트 코드 수정

--- src/test/java/lotto/controller/LottoManagerTest.java
+++ src/test/java/lotto/controller/LottoManagerTest.java
@@ -1,12 +1,15 @@
 package lotto.controller;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static lotto.exception.LottoException.validateBonusAll;
+import static lotto.utility.Constants.ERROR_MESSAGE_3;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Arrays;
 import java.util.List;
 import lotto.Application;
 import lotto.model.Lotto;
+import lotto.model.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -37,11 +40,11 @@ class LottoManagerTest extends NsTest {
     @DisplayName("보너스 번호가 당첨 번호와 중복되는 경우 예외가 발생한다.")
     @Test
     void createBonusLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("8000", "1,2,3,4,5,6", "1"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        User.setWinningNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)); // 예를 들어, 여기에 중복된 번호 설정
+
+        assertThatThrownBy(() -> validateBonusAll("1")) // 중복된 번호를 사용해서 테스트
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE_3);
     }
 
     @Override

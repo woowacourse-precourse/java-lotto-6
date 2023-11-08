527631a572365e2e3919939f5b44daf62bcb6a71
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 16:33:50 +0900

    feat : lotto 유효성 검사 함수 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,15 +5,21 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
     private static final int LOTTO_NUMBER_COUNT = 6;
-    
+
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers == null || numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 정확히 " + LOTTO_NUMBER_COUNT + "개 이어야 합니다.");
+        }
+        if (numbers.stream().distinct().count() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
         }
     }
 

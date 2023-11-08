98d1123f2f641511b0cb7fa73c4dfe9167dc2a0c
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:11:50 +0900

    feat : LottoResult 생성자 생성

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -1,5 +1,15 @@
 package lotto.domain;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class LottoResult {
     private final Map<Rank, Integer> rankCount;
+
+    public LottoResult() {
+        rankCount = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            rankCount.put(rank, 0);
+        }
+    }
 }

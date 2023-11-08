34b0e512f281b6e27849f29454bc4c08fdcbbc45
oxdjww <oxdjww@gmail.com>
2023-11-08 22:47:03 +0900

    test : 로또 사이즈 예외 테스트 구현

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,15 @@
 package lotto;
 
 import lotto.domain.Lotto;
+import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.exception.ErrorMessage.DUPLICATE_LOTTO_NUMBERS;
+import static lotto.exception.ErrorMessage.INVALID_LOTTO_FORMAT;
+import static lotto.exception.ErrorMessage.OUT_OF_RANGE_LOTTO_NUMBERS;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -13,7 +17,8 @@ class LottoTest {
     @Test
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(LottoException.class)
+                .hasMessageContaining(INVALID_LOTTO_FORMAT.getMessage());
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -21,8 +26,17 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(LottoException.class)
+                .hasMessageContaining(DUPLICATE_LOTTO_NUMBERS.getMessage());
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @Test
+    @DisplayName("로또 번호의 개수가 6개보다 작으면 예외가 발생한다.")
+    void createLottoByInvalidSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(LottoException.class)
+                .hasMessageContaining(INVALID_LOTTO_FORMAT.getMessage());
+    }
+
+
 }

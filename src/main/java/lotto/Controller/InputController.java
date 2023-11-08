9859411bc850ac8c0f0b98b8b9b2688c555b8f71
ekeprl <101682398+ekeprl@users.noreply.github.com>
2023-11-08 22:52:51 +0900

    Update InputController.java
    필요없는  InputController 내용 비우기

--- src/main/java/lotto/Controller/InputController.java
+++ src/main/java/lotto/Controller/InputController.java
@@ -1,28 +1,8 @@
 package lotto.Controller;
 
-import lotto.Util.LottoNumberValidator;
-import lotto.View.InputView;
+
 
 public class InputController {
 
-    public static Lottonumbers setLottonumbers() {
-        try {
-            LottoNumberValidator validation = new LottoNumberValidator(InputView.RightNumber());
-            return new Lottonumbers(validation.NUMBERS);
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return setLottonumbers();
-        }
-    }
-
-    public static int setRoundNum() {
-        try {
-            LottoNumberValidator validation = new LottoNumberValidator(InputView.RightNumber());
-            return Integer.parseInt(String.valueOf(validation.LOTTOS));
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return setRoundNum();
-        }
-    }
+   
 }

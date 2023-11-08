f32e3eecc393ed87217a3ca611436413473fff89
hoyeon <highoption@naver.com>
2023-11-08 15:25:31 +0900

    test(userTest) : 올바른 등수가 계산되는지 테스트

--- src/test/java/lotto/model/UserTest.java
+++ src/test/java/lotto/model/UserTest.java
@@ -1,10 +1,13 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class UserTest {
 
@@ -29,5 +32,17 @@ public class UserTest {
                 new Lotto(new ArrayList<>(List.of(1, 2, 3, 4, 5, 6))),
                 new BonusNumber("7")
         );
+    }
+
+    @Test
+    @DisplayName("초기 값을 바탕으로 올바른 등수가 나오는지 테스트")
+    void testCompareWinningLotto() {
+        user.compareWinningLotto(winningLotto);
+
+        assertThat(WinningResult.FIRST.getCountResult()).isEqualTo(0);
+        assertThat(WinningResult.SECOND.getCountResult()).isEqualTo(1);
+        assertThat(WinningResult.THIRD.getCountResult()).isEqualTo(1);
+        assertThat(WinningResult.FOURTH.getCountResult()).isEqualTo(1);
+        assertThat(WinningResult.FIFTH.getCountResult()).isEqualTo(0);
     }
 }

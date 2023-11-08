3f7bc846637cfea5294e98a8d6e8f7099314b488
DooDuZ <sin9158@naver.com>
2023-11-08 09:03:02 +0900

    refactor: 메시지 변수 명 수정

--- src/main/java/lotto/exception/ErrorMessage.java
+++ src/main/java/lotto/exception/ErrorMessage.java
@@ -4,7 +4,7 @@ import lotto.validator.LottoNumberValidator;
 import lotto.validator.PriceValidator;
 
 public enum ErrorMessage {
-    NOT_INTEGER_MESSAGE("숫자만 입력 가능합니다."),
+    NOT_NUMBER_MESSAGE("숫자만 입력 가능합니다."),
     NOT_THOUSAND_MESSAGE(String.format("%d 배수만 입력 가능합니다.", PriceValidator.PURCHASE_COST)),
     PRICE_OUT_OF_RANGE_MESSAGE(String.format("최소 주문 금액 : %,d / 최대 주문 금액 : %,d", PriceValidator.PURCHASE_COST,
             PriceValidator.MAX_PURCHASE_COST)),

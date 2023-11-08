1c6abc32867a8bca1b4da3ee815feda6823af2ea
wonindev <sangwonparkdev@gmail.com>
2023-11-08 22:30:09 +0900

    Feat: 같은 번호가 몇 개 있는지 return하는 기능 추가

--- src/main/java/lotto/utils/CommonHelper.java
+++ src/main/java/lotto/utils/CommonHelper.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import lotto.constants.LottoEnum;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CommonHelper {
     public static int strToInt(String input) {
@@ -30,5 +32,16 @@ public class CommonHelper {
         return splited.stream()
                 .map(Integer::parseInt)
                 .toList();
+    }
+
+    public static int countSameNumber(List<Integer> userLottoNumbers, List<Integer> lottoNumbers) {
+        int count = 0;
+        Set<Integer> set = new HashSet<>(userLottoNumbers);
+        for (Integer num : lottoNumbers) {
+            if (set.contains(num)) {
+                count++;
+            }
+        }
+        return count;
     }
 }

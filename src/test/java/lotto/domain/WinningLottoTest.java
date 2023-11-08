e4c78fabc4dd639f1c676fa529342e08323d2122
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:31:39 +0900

    test(WinningLottoTest): 에러 메시지를 상수로 변경

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import static lotto.constants.ErrorMessage.BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_COUNT_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -23,7 +27,7 @@ class WinningLottoTest {
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 6, 7), 8))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호의 개수는 6개여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
     }
 
     @DisplayName("당첨 번호의 개수가 6개 미만이면 예외가 발생한다.")
@@ -31,7 +35,7 @@ class WinningLottoTest {
     void createLottoByUnderSize() {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5), 8))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호의 개수는 6개여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
     }
 
     @DisplayName("당첨 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -39,7 +43,7 @@ class WinningLottoTest {
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new WinningLotto(List.of(1, 2, 3, 4, 5, 3), 8))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 중복될 수 없습니다.");
+                .hasMessage(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호와 당첨 번호가 중복되면 예외를 발생한다.")
@@ -47,7 +51,7 @@ class WinningLottoTest {
     void duplicateBonusAndWinningNumbers() {
         assertThatThrownBy(() -> new WinningLotto(lottoNumbers, 1))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("당첨 번호와 보너스 번호는 중복될 수 없습니다.");
+                .hasMessage(BONUS_NUMBER_DUPLICATE_ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호와 당첨 번호가 서로 다르면 정상 생성한다.")
@@ -63,6 +67,6 @@ class WinningLottoTest {
     void bonusNumberNotInRange1To45(int bonusNumber) {
         assertThatThrownBy(() -> new WinningLotto(lottoNumbers, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
     }
 }

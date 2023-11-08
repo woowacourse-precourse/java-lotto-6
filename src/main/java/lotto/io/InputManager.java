c50859ffcb0a026829d72ddedcd42b1c3e08ac7c
YeaChan05 <qkenrdl05@gmail.com>
2023-11-08 21:18:38 +0900

    [FIX] 예외 처리 변경

--- src/main/java/lotto/io/InputManager.java
+++ src/main/java/lotto/io/InputManager.java
@@ -1,15 +1,19 @@
 package lotto.io;
 
-public abstract class InputManager<T>  {
-    ConsoleAdapter consoleAdapter= new ConsoleAdapter();
-    public abstract T input();
-    public abstract T validation(String  input);
+public abstract class InputManager<T> {
+    ConsoleAdapter consoleAdapter = new ConsoleAdapter();
 
-    protected void setConsoleAdapter(ConsoleAdapter consoleAdapter){
-        this.consoleAdapter=consoleAdapter;
-    };
+    public abstract T input();
+
+    public abstract T validation(String input);
+
+    protected void setConsoleAdapter(ConsoleAdapter consoleAdapter) {
+        this.consoleAdapter = consoleAdapter;
+    }
 
     public static void validateNumberRange(Integer number) {
-        if (number > 45 || number < 1) throw new IndexOutOfBoundsException();
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException("[ERROR] 1~45 범위 내의 값을 입력해주세요.");
+        }
     }
 }

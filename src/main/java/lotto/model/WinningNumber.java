4cad54f917b91cb1f41c4b5085fc2c81b988be90
OhTaeHo-97 <57743934+OhTaeHo-97@users.noreply.github.com>
2023-11-08 21:39:46 +0900

    refactor(WinningNumber): 당첨 번호 유효성 검증의 책임 변경

--- src/main/java/lotto/model/WinningNumber.java
+++ src/main/java/lotto/model/WinningNumber.java
@@ -1,13 +1,20 @@
 package lotto.model;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import lotto.validation.WinningNumberValidation;
 
 public class WinningNumber {
     private final List<Integer> numbers;
 
-    public WinningNumber(List<Integer> numbers) {
-        this.numbers = numbers;
+    public WinningNumber(int[] numbers) {
+        WinningNumberValidation.validateWinningNumber(numbers);
+        this.numbers = Arrays.stream(numbers).boxed().toList();
+    }
+
+    public boolean isDuplicated(int number) {
+        return numbers.contains(number);
     }
 
     public List<Integer> getNumbers() {

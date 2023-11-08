ecf3330976cd481caf1005284e5ce084042df02f
Woongbae Yun <devbelly@naver.com>
2023-11-08 09:48:55 +0900

    feat : 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,10 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoMoney;
 import lotto.domain.Lottos;
+import lotto.util.StringUtils;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -13,6 +16,8 @@ public class LottoController {
         Lottos newlottos = new Lottos(lottoMoney.buyLottos());
         OutputView.printLottoCountMessage(newlottos.getLottoCount());
         OutputView.printLottos(newlottos);
+
+        Lotto lotto = getWinningLotto();
     }
 
     public static LottoMoney getLottoMoney() {
@@ -24,6 +29,19 @@ public class LottoController {
         } catch (IllegalArgumentException e) {
             OutputView.printExceptionMessage(e);
             return getLottoMoney();
+        }
+    }
+
+    public static Lotto getWinningLotto() {
+        try {
+            OutputView.printWinningLottoMessage();
+            String input = InputView.readLine();
+            OutputView.printEmptyLine();
+            List<Integer> numbers = StringUtils.splitStringToLottoNumbers(input);
+            return new Lotto(numbers);
+        } catch (IllegalArgumentException e) {
+            OutputView.printExceptionMessage(e);
+            return getWinningLotto();
         }
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ public class OutputView {
 
     private static final String LOTTO_PURCHASE_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String LOTTO_COUNT_MESSAGE = "%d개를 구매했습니다.";
+    private static final String WINNING_LOTTO_MESSAGE = "당첨 번호를 입력해 주세요.";
 
     public static void printLottoPurchaseMoneyMessage() {
         System.out.println(LOTTO_PURCHASE_MONEY_MESSAGE);
@@ -27,5 +28,9 @@ public class OutputView {
 
     public static void printLottoCountMessage(int lottoCount) {
         System.out.println(LOTTO_COUNT_MESSAGE.formatted(lottoCount));
+    }
+
+    public static void printWinningLottoMessage() {
+        System.out.println(WINNING_LOTTO_MESSAGE);
     }
 }

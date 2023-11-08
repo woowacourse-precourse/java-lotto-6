f1a147e49e0f21cb3405cbd3f01b5fabe1a7a329
CHANG MIN PARK <98qkrckdals@naver.com>
2023-11-08 19:34:23 +0900

    feat(Lotto):로또를 생성할 때 예외 상황에 대하여 테스트코드입니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,8 +3,9 @@ package lotto;
 import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,10 +20,36 @@ class LottoTest {
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 6자리를 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void createLottoBySizeIsSix() {
+        assertThatThrownBy(() -> new Lotto(List.of(1)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또의 범위가 1~45를 벗어나면 예외가 발생한다..")
+    @Test
+    void createLottoBySsizeIsSix() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 49, 50)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 숫자가 아닌 문자를 입력하면 예외가 발생한다.")
+    @Test
+    void createLottoByNotDigit() {
+        String input = "1, 2, 3, a, a, 6";
+
+        assertThatThrownBy(() -> {
+            List<Integer> numbers = Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            new Lotto(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

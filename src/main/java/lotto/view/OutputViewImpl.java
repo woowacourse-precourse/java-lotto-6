223c40963ccd9f81aca07eb8178f4c82d87e85fc
Ojh <201833022@yiu.ac.kr>
2023-11-08 22:32:26 +0900

    feat: 에러 메시지, 기타 출력, 결과 출력 상세 구현 기능

--- src/main/java/lotto/view/OutputViewImpl.java
+++ src/main/java/lotto/view/OutputViewImpl.java
@@ -1,13 +1,22 @@
 package lotto.view;
 
+import lotto.dto.Result;
 import lotto.model.MyLotto;
 
 public class OutputViewImpl implements OutputView {
   private static final String PURCHASE_MESSAGE = "개를 구매했습니다.";
+  private static final String LOTTO_WINNING_STATS = "당첨 통계";
+  private static final String SEPARATOR = "---";
+
 
   @Override
   public void printEmptyLine() {
     System.out.println();
+  }
+
+  @Override
+  public void printErrorMessage(IllegalArgumentException exception) {
+    System.out.println(exception.getMessage());
   }
 
   @Override
@@ -18,5 +27,20 @@ public class OutputViewImpl implements OutputView {
   @Override
   public void printPurchasedMyLottoList() {
     System.out.println(MyLotto.getAllMyLottoAsString());
+  }
+
+  @Override
+  public void printWinningStatistics() {
+    System.out.println(LOTTO_WINNING_STATS);
+  }
+
+  @Override
+  public void printSeparator() {
+    System.out.println(SEPARATOR);
+  }
+
+  @Override
+  public void printWinningStatisticsSummary(Result result) {
+    System.out.println(result);
   }
 }

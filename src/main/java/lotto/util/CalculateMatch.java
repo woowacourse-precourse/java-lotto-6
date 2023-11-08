ef82b636955b6593c71db9f128b8ccd4e5649a30
KongJunWoo <joonoo3@naver.com>
2023-11-08 23:28:05 +0900

    feat : 모든 로또 계산하여 PrizeResultDto 반환

--- src/main/java/lotto/util/CalculateMatch.java
+++ src/main/java/lotto/util/CalculateMatch.java
@@ -1,5 +1,7 @@
 package lotto.util;
 
+import lotto.model.PrizeResultDto;
+import lotto.enumration.Prize;
 import lotto.model.Lotto;
 import java.util.List;
 
@@ -7,6 +9,26 @@ public class CalculateMatch {
 
     private static int matchCount;
     private static boolean matchBonus;
+    private static int totalPrize = 0;
+    private static int[] totalPrizeCount = new int[6];
+
+    //등수와 총금액 반환
+    public static PrizeResultDto calculate(List<Lotto> lottoList, Lotto winningLotto, int winningBonusNumber) {
+
+        for (Lotto lotto : lottoList) {
+            dataReset();
+
+            calculateMatchCount(lotto, winningLotto.getNumbers());
+            calculateMatchBonus(lotto, winningBonusNumber);
+
+            Prize prize = Prize.of(matchCount, matchBonus);
+
+            totalPrize += prize.getWinningMoney();
+            totalPrizeCount[prize.getRank()]++;
+        }
+
+        return PrizeResultDto.of(totalPrizeCount, totalPrize);
+    }
 
     public static void dataReset(){
         matchCount = 0;

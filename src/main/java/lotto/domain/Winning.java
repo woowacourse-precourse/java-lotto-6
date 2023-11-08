0ca9203396715078b3f3f6962b64ea843c648c28
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 12:40:32 +0900

    feat(Winning) : enum Winning Constructor 생성

---
+++ src/main/java/lotto/domain/Winning.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+public enum Winning {
+    FIND(1, 0),
+    THIRD(3, 5000)
+    ;
+
+    private final int label;
+    private final int price;
+
+    Winning(int label, int price) {
+        this.label = label;
+        this.price = price;
+    }
+}

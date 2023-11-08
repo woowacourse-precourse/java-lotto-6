dc6478df4b10a61a7b52e71891ef0ee84cb61022
minoooo119 <minoth0119@naver.com>
2023-11-08 09:06:10 +0900

    [FIX] input 수정 내용 controller에 반영 : LottoGame.java

--- src/main/java/Controller/LottoGame.java
+++ src/main/java/Controller/LottoGame.java
@@ -1,13 +1,21 @@
 package Controller;
 
 import View.UserInput;
+import lotto.Lotto;
 
 import java.util.List;
 
 public class LottoGame {
+    public LottoGame(){
+
+    }
+    private static UserInput userInput=new UserInput();
+    private static List<Integer> winningLottoNum;
+    private static Integer winningBonusNum;
     public void gameStart(){
-        UserInput userInput=new UserInput();
-        List<Integer> winningNum=userInput.getWinningNum();
+        //1. User에게 입력을 받는다.
         Integer purchaseAmount= userInput.getPurchaseAmount();
+        Lotto lotto =userInput.getWinningNum();
+        winningBonusNum= userInput.getBonusNum();
     }
 }

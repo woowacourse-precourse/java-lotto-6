20a922fe62d755455804c1a7984472c1ec2f9cd2
minju <minju9711@naver.com>
2023-11-08 16:58:40 +0900

    [feat] 사용자의 금액에 맞게 컴퓨터에서 랜덤으로 로또를 생성해주는 purchaseLotto 함수의 기능추가

--- src/main/java/lotto/Controller/Controller.java
+++ src/main/java/lotto/Controller/Controller.java
@@ -1,36 +1,50 @@
 package lotto.Controller;
 
-import lotto.Factory.LottoContainer;
+import lotto.LottoFactory.*;
 import lotto.IO.IO;
+import lotto.Util.RateCalculator;
 import lotto.Util.Validate;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Controller {
     private static LottoContainer lottoContainer;
+    private static LottoController lottoController;
     private int count;
     private int bonus;
-    public Controller(LottoContainer lottoContainer) {
+    public Controller(LottoContainer lottoContainer, LottoController lottoController) {
         this.lottoContainer = lottoContainer;
+        this.lottoController = lottoController;
     }
 
-    public void SettingLotto() { //사용자로부터 로또와 정답로또 입력받고 생성까지 끝내놓기
+    public void SettingLotto() {
         String WinningLotto;
         List<Integer> WinningNum;
 
         purchaseLotto();
         lottoContainer.MakeContainer(count);
+        IO.PrintBuyLotto(lottoContainer);
+
         WinningLotto = IO.InputWinningLotto();
         WinningNum = Validate.WinningLottoInput(WinningLotto);
         lottoContainer.MakeWinningLotto(WinningNum);
+
         settingBonus();
     }
 
     private void purchaseLotto() {
-        String cash = IO.InputCash();
-        Validate.CashIsInteger(cash);
-        count = Integer.parseInt(cash) / 1000;
-        IO.PrintCount(count);
+        try{
+            String cash = IO.InputCash();
+            Validate.CashForm(cash);
+            count = Integer.parseInt(cash) / 1000;
+            IO.PrintCount(count);
+
+        }catch (NoSuchElementException e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
+
 
     private void settingBonus(){
         String bonusStr = IO.InputBonus();
@@ -38,11 +52,10 @@ public class Controller {
         Validate.isDuplicateBonus(lottoContainer, bonus);
     }
 
-    public void RunLotto(){ //사용자의 로또와 정답로또 비교 후 rank 매기기
-
+    public void RunLotto() {
     }
 
-    public void PrintResult(){ //매겨진 랭크 출력
-
+    public void PrintResult(double rate){
     }
+
 }

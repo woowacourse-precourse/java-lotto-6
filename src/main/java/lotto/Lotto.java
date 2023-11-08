3e30b911320430b349c21991a471f6c8afe075db
Dalliaaaaa <ajswlsoato@naver.com>
2023-11-08 22:35:06 +0900

    feat: 당첨 번호 범위 안의 숫자가 아니면 예외 처리

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -29,4 +29,12 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 0이 포함되어 있습니다.");
         }
     }
+
+    private void exceeding(List<Integer> numbers){
+        for (int n : numbers){
+            if (n > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.");
+            }
+        }
+    }
 }

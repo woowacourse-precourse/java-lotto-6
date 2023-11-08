12e6371f7f1afd2730b6d7db169ce1ab60650d8a
jihun-24k <gi020477@gmail.com>
2023-11-08 19:08:30 +0900

    refactor: main 리팩토링

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,5 @@
 package lotto;
 
-import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.LottoGame;
 import lotto.domain.LottoTickets;
@@ -12,18 +11,15 @@ import lotto.view.OutputView;
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        Integer purchaseAmount = inputView.readPurchaseAmount();
-        Money money = new Money(purchaseAmount);
+        Money money = new Money(inputView.readPurchaseAmount());
         LottoTickets lottoTickets = new LottoTickets(money);
 
         OutputView outputView = new OutputView();
         outputView.printLottoTickets(lottoTickets.getLottoTickets());
 
-        List<Integer> winnerNumbers = inputView.readWinnerNumbers();
-        Lotto winnerLotto = new Lotto(winnerNumbers);
-        Integer BonusNumber = inputView.readBonusNumber();
+        Lotto winnerLotto = new Lotto(inputView.readWinnerNumbers());
+        WinningLotto winningLotto = new WinningLotto(winnerLotto, inputView.readBonusNumber());
 
-        WinningLotto winningLotto = new WinningLotto(winnerLotto, BonusNumber);
         LottoGame lottoGame = new LottoGame();
         lottoGame.play(winningLotto, lottoTickets);
 

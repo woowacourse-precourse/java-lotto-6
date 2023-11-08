5ea3ab0839b19c29321a353ab6163ecb2a372bfa
soddong <aledjdygus9300@gmail.com>
2023-11-09 00:10:14 +0900

    fix: 중복 검증으로 인한 삭제

--- src/main/java/lotto/dto/WinningLotto.java
+++ src/main/java/lotto/dto/WinningLotto.java
@@ -1,13 +1,8 @@
 package lotto.dto;
 
 import java.util.List;
-import lotto.validator.LottoNumbersValidator;
 
 public record WinningLotto(Lotto winningLotto, int bonusNumber) {
-
-    public WinningLotto {
-        LottoNumbersValidator.validateBonus(winningLotto.getNumbers(), bonusNumber);
-    }
 
     public int getBonusNumber() {
         return bonusNumber;

285b233c70985b70517bab0fbe88e6095534eb0d
parkchangeon <jg9870@naver.com>
2023-11-08 14:30:52 +0900

    refactor(lotto): 예외 처리 문구에 단위 콤마 추가

--- src/test/java/lotto/LottoHandlerTest.java
+++ src/test/java/lotto/LottoHandlerTest.java
@@ -43,7 +43,7 @@ class LottoHandlerTest {
         // when // then
         assertThatThrownBy(() -> lottoHandler.calculateLottoTicketCount(receivedPurchasePrice))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 1000원 단위로만 입력해 주세요.");
+                .hasMessage("[ERROR] 1,000원 단위로만 입력해 주세요.");
     }
 
     @DisplayName("구매 금액에 문자를 입력하면 예외가 발생한다.")

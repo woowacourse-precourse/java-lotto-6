77ec419305f74332ffd6a7c6aa67ec6b4b3fcfa5
IDpluto <d0013159@gmail.com>
2023-11-08 17:47:42 +0900

    feat(Lotto): 숫자를 6개 이하나 이상으로 입력했을 경우 에러처리

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -6,6 +6,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputView {
+
+    private static final int WINNING_NUMBER_SIZE = 6;
+
+
     public int readPurchaseAmount() {
         while (true) {
             try {
@@ -14,6 +18,8 @@ public class InputView {
                 int purchaseAmount = Integer.parseInt(input);
                 validatePurchaseAmount(purchaseAmount);
                 return purchaseAmount;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해주세요." );
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
@@ -27,15 +33,29 @@ public class InputView {
     }
 
     public List<Integer> readWinningNumbers() {
-        String[] inputWinningNumbers = Console.readLine().split(",");
-        return checkWinningNumbersValidity(inputWinningNumbers);
+        while (true) {
+            try {
+                System.out.print("당첨 번호를 입력해 주세요: ");
+                String[] inputWinningNumbers = Console.readLine().split(",");
+                return checkWinningNumbersValidity(inputWinningNumbers);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<Integer> checkWinningNumbersValidity(String[] inputWinningNumbers) {
+        validateWinningNumbersLength(inputWinningNumbers);
         return Arrays.stream(inputWinningNumbers)
                 .map(String::trim)
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    public void validateWinningNumbersLength(String[] inputWinningNumbers) {
+        if (inputWinningNumbers.length != WINNING_NUMBER_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 숫자 6개를 입력하세요.");
+        }
     }
 
     public int readBonusNumber() {

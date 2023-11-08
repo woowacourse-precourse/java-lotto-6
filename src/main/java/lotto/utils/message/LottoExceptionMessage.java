d88fdf237db15e4c29285a0f3e27a689a445398e
june-777 <wlwhswnsrl96@gmail.com>
2023-11-08 16:37:25 +0900

    refactor(LottoExceptionMessage): 로또 관련 예외 메시지 추가

--- src/main/java/lotto/utils/message/LottoExceptionMessage.java
+++ src/main/java/lotto/utils/message/LottoExceptionMessage.java
@@ -3,8 +3,9 @@ package lotto.utils.message;
 public enum LottoExceptionMessage {
 
     NOT_SATISFY_REQUIRED_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
-    DUPLICATE_EXISTS("[ERROR] 로또 번호가 중복 입니다."),
-    INVALID_NUMBER("[ERROR] 로또 번호가 유효하지 않습니다.");
+    NOT_SATISFY_LOTTO_NUMBER_UNIQUE("[ERROR] 로또 번호가 중복 입니다."),
+    NOT_SATISFY_LOTTO_NUMBER_RANGE("[ERROR] 로또 번호는 1 ~ 45 범위의 수만 가능 합니다."),
+    WINNING_AND_BONUS_DUPLICATE_EXISTS("[ERROR] 보너스 번호는 당첨 번호와 중복일 수 없습니다.");;
 
     private final String error;
 

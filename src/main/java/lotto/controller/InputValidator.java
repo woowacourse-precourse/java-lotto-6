7f12ab888c9b5ff5ddbd93961ab76cd352e8ed10
Hyunjun KIM <hihj070914@icloud.com>
2023-11-08 19:46:42 +0900

    feat: utils in InputValidator - 사용자 입력을 검증하기 위한 private 메서드들을 구현하였습니다.

---
+++ src/main/java/lotto/controller/InputValidator.java
@@ -0,0 +1,38 @@
+package lotto.controller;
+
+import static lotto.constants.ErrorMessage.DUPLICATED_NUMBER_MESSAGE;
+import static lotto.constants.ErrorMessage.INVALID_INPUT_LENGTH_FORMAT;
+import static lotto.constants.ErrorMessage.INVALID_INPUT_MESSAGE;
+import static lotto.constants.ErrorMessage.NUMBER_NOT_IN_RANGE_FORMAT;
+import static lotto.constants.Common.DELIMITER;
+import static lotto.constants.Common.LOTTO_TICKET_LENGTH;
+import static lotto.constants.Common.LOTTO_TICKET_PRICE;
+import static lotto.constants.Common.MAXIMUM_LOTTO_NUMBER;
+import static lotto.constants.Common.MINIMUM_LOTTO_NUMBER;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class InputValidator {
+    private static boolean checkNumberNotInRange(int number) {
+        return !(MINIMUM_LOTTO_NUMBER <= number && number <= MAXIMUM_LOTTO_NUMBER);
+    }
+
+    private static boolean checkInvalidPrice(long price) {
+        return price < LOTTO_TICKET_PRICE || price % LOTTO_TICKET_PRICE != 0;
+    }
+
+    private static boolean checkContainsDuplicatedNumber(List<Integer> inputSequence) {
+        HashSet<Integer> checkDuplicate = new HashSet<>(inputSequence);
+        return checkDuplicate.size() != LOTTO_TICKET_LENGTH;
+    }
+
+    private static List<Integer> parseRawWinningNumber(String rawInput) {
+        List<Integer> parsed = new ArrayList<>();
+        for (String rawNumber : rawInput.split(DELIMITER)) {
+            parsed.add(Integer.parseInt(rawNumber));
+        }
+        return parsed;
+    }
+}

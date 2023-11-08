9132db70ced1bbfa3ff426fc091fc63c9e12b659
whereisrmsqhs <whereisrmsqhs@gmail.com>
2023-11-08 17:42:57 +0900

    feat(BonusNumber): 검증 코드 구현

--- src/main/java/lotto/domain/BonusNumber.java
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.List;
 
+import static lotto.constant.ErrorMessage.*;
+
 public class BonusNumber {
 
     private final int bonusNumber;
@@ -18,12 +20,24 @@ public class BonusNumber {
     }
 
     private void validateDuplication(String bonusNumber, List<Integer> lottoNumber) {
+        int convertedBonusNumber = Integer.parseInt(bonusNumber);
+        if(lottoNumber.contains(convertedBonusNumber)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_AND_LOTTO_NUMBER_DUPLICATED_WARNING);
+        }
     }
 
     private void validateNumberRange(String bonusNumber) {
+        int convertedBonusNumber = Integer.parseInt(bonusNumber);
+        if(convertedBonusNumber < 1 || convertedBonusNumber > 45) {
+            throw new IllegalArgumentException(BONUS_NUMBER_RANGE_WARNING);
+        }
     }
 
     private void validateIsDigit(String bonusNumber) {
-        
+        try{
+            Integer.parseInt(bonusNumber);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(BONUS_NUMBER_INCLUDE_STRING_WARNING);
+        }
     }
 }

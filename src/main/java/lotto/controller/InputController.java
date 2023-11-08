6c9421941a8f0f77a9161169640ce9f42aef63db
jeongjiyeon315 <jung_j_yeon@naver.com>
2023-11-08 18:59:34 +0900

    refactor(InputController): 스택 오버 플로우를 피하기 위해 재귀에서 반복문 형식으로 변경

--- src/main/java/lotto/controller/InputController.java
+++ src/main/java/lotto/controller/InputController.java
@@ -3,36 +3,41 @@ package lotto.controller;
 import lotto.domain.TicketAmount;
 import lotto.domain.WinningNumber;
 import lotto.domain.BonusNumber;
-import lotto.util.Validator;
 import lotto.view.InputView;
 
 public class InputController {
     private InputController() {
     }
     public static TicketAmount setTicketAmount() {
-        try {
-            String input = InputView.inputMoney();
-            return new TicketAmount(input);
-        } catch (IllegalArgumentException e) {
-            return setTicketAmount();
+        while (true) {
+            try {
+                String input = InputView.inputMoney();
+                return new TicketAmount(input);
+            } catch (IllegalArgumentException e) {
+                e.getMessage();
+            }
         }
     }
 
     public static WinningNumber setWinningNumber() {
-        try {
-            String input = InputView.inputLottoNumbers();
-            return new WinningNumber(input);
-        } catch (IllegalArgumentException e) {
-            return setWinningNumber();
+        while (true) {
+            try {
+                String input = InputView.inputLottoNumbers();
+                return new WinningNumber(input);
+            } catch (IllegalArgumentException e) {
+                e.getMessage();
+            }
         }
     }
 
     public static BonusNumber setBonusNumber(WinningNumber winningNumber) {
-        try {
-            String input = InputView.inputBonusNumber();
-            return new BonusNumber(winningNumber, input);
-        } catch (IllegalArgumentException e) {
-            return setBonusNumber(winningNumber);
+        while (true) {
+            try {
+                String input = InputView.inputBonusNumber();
+                return new BonusNumber(winningNumber, input);
+            } catch (IllegalArgumentException e) {
+                e.getMessage();
+            }
         }
     }
 }

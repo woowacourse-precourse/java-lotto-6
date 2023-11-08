6ba588ab37d945b5600fce6ded11d94aee98124f
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-08 15:48:23 +0900

    refactor(domain): 수익률 계산 시 안정성을 위해 유효성 검증 추가

--- src/main/java/lotto/domain/WinningResult.java
+++ src/main/java/lotto/domain/WinningResult.java
@@ -2,6 +2,8 @@ package lotto.domain;
 
 import java.util.Arrays;
 import java.util.EnumMap;
+import lotto.constant.Constant;
+import lotto.constant.Message;
 
 public class WinningResult {
     private final EnumMap<Rank, Integer> results;
@@ -28,6 +30,15 @@ public class WinningResult {
     }
 
     public double calculateYield(int purchaseAmount) {
+        if (purchaseAmount < Constant.LOTTO_PRICE.getValue()) {
+            throw new IllegalArgumentException(Message.ERROR_PURCHASE_AMOUNT_UNDER_MINIMUM.getMessage());
+        }
+        if (purchaseAmount > Constant.MAXIMUM_PURCHASE_AMOUNT.getValue()) {
+            throw new IllegalArgumentException(Message.ERROR_PURCHASE_AMOUNT_OVER_MAXIMUM.getMessage());
+        }
+        if (purchaseAmount % Constant.LOTTO_PRICE.getValue() != 0) {
+            throw new IllegalArgumentException(Message.ERROR_PURCHASE_AMOUNT_INVALID_UNIT.getMessage());
+        }
         return 100. * (double) calculateTotalPrizeMoney() / purchaseAmount;
     }
 }

184390c29a2f49c90cb35155e0d4d687b96c3c58
junseoks-99 <angec5645@naver.com>
2023-11-08 20:02:00 +0900

    feat: 당첨번호 입력 개수 유효 여부 검증 기능 추가

--- src/main/java/util/ValidationNumbers.java
+++ src/main/java/util/ValidationNumbers.java
@@ -3,17 +3,20 @@ package util;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import lotto.LottoPrinciples;
 
 public class ValidationNumbers {
 
     private static final String COMMA = ",";
     private static final Integer REMOVE_LAST_SPACE_NUMBER = 0;
+
     private ValidationNumbers() {
 
     }
 
     public static void validateNumbers(String numbers) {
         validateNumber(numbers);
+        validateLength(numbers);
     }
 
     private static void validateNumber(String numbers) {
@@ -22,6 +25,13 @@ public class ValidationNumbers {
             Arrays.stream(numbers.split(COMMA, REMOVE_LAST_SPACE_NUMBER)).forEach(Integer::parseInt);
         } catch (NumberFormatException notANumberException) {
             throw new IllegalArgumentException(ValidationNumbersMessages.NOT_A_NUMBER.getMessage());
+        }
+    }
+
+    private static void validateLength(String numbers) {
+        List<String> numbersList = List.of(numbers.split(COMMA, REMOVE_LAST_SPACE_NUMBER));
+        if (numbersList.size() != LottoPrinciples.LIMIT_NUMBER.getNumber()) {
+            throw new IllegalArgumentException(ValidationNumbersMessages.INVALID_LENGTH.getMessage());
         }
     }
 }
--- src/main/java/util/ValidationNumbersMessages.java
+++ src/main/java/util/ValidationNumbersMessages.java
@@ -2,7 +2,8 @@ package util;
 
 public enum ValidationNumbersMessages {
 
-    NOT_A_NUMBER("[ERROR] 숫자만 입력해주세요.");
+    NOT_A_NUMBER("[ERROR] 숫자만 입력해주세요."),
+    INVALID_LENGTH("[ERROR] , 기준으로 6개만 입력주세요.");
 
     private String message;
 

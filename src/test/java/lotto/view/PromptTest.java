0c64384a9bc82385c2e827139d8dbbc724649913
readable-ko <21700026@handong.edu>
2023-11-08 16:24:59 +0900

    refactor(Prompt): updated other static messages
    변경 내용 상세
    - LottoController에서 사용될 메세지 추가 구성

---
+++ src/test/java/lotto/view/PromptTest.java
@@ -0,0 +1,31 @@
+package lotto.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+public class PromptTest extends NsTest {
+    @Test
+    void checkPromptReturnValue() {
+        init();
+        for (Prompt prompt : Prompt.values()) {
+            System.out.println(prompt.getMessage());
+        }
+        assertThat(output()).contains(
+                "구입금액을 입력해 주세요.",
+                "당첨 번호를 입력해 주세요.",
+                "보너스 번호를 입력해 주세요.",
+                "%d개를 구매했습니다.\n",
+                "당첨 통계\n---",
+                "%d개 일치%s - %d개",
+                "총 수익률은 %.1f%%입니다."
+        );
+
+    }
+
+    @Override
+    protected void runMain() {
+
+    }
+}

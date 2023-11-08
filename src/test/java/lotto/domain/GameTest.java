9c6685740e030a1d64489fcf153e90125698ba79
seunghye218 <seunghye@student.42seoul.kr>
2023-11-08 11:28:04 +0900

    test: add WinningLottoTest, LottoMachineTest
    - remove GameTest

---
+++ src/test/java/lotto/domain/GameTest.java
@@ -0,0 +1,12 @@
+package lotto.domain;
+
+import lotto.domain.Game;
+import org.junit.jupiter.api.DisplayName;
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameTest {
+
+}
---
+++ src/test/java/lotto/domain/LottoMachineTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LottoMachineTest {
+
+    @DisplayName("구매 금액이 로또 가격에 나누어 떨지지 않거나 0원이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1234, 0})
+    void createLottoByOverSize(int purchaseAmount) {
+        assertThatThrownBy(() -> new LottoMachine().issueLottos(purchaseAmount))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("[ERROR]");
+    }
+}

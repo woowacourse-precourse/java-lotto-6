631c98face7132d4b25d0ccededade706895c65f
SangCheol <scpark0698@gmail.com>
2023-11-08 21:15:46 +0900

    feat: 구입금액 검증 - 숫자?, 음수?, 1000단위?

--- src/main/java/lotto/LottoGameValidator.java
+++ src/main/java/lotto/LottoGameValidator.java
@@ -2,5 +2,24 @@ package lotto;
 
 public class LottoGameValidator {
     public static void validateInputAmount(String inputBuyAmount) {
+        validateNumericInput(inputBuyAmount);
+        validateBuyAmountFormat(inputBuyAmount);
+    }
+
+    private static void validateNumericInput(String inputBuyAmount) {
+        try {
+            int buyAmount = Integer.parseInt(inputBuyAmount);
+
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력하시오.");
+        }
+    }
+
+    private static void validateBuyAmountFormat(String inputBuyAmount) {
+        int buyAmount = Integer.parseInt(inputBuyAmount);
+
+        if (buyAmount <= 0 || buyAmount % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 0보다 큰 1000원 단위의 숫자로 입력해야 합니다");
+        }
     }
 }

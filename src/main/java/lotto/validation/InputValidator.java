74390c22b24458c090ac2d8fdbb0105897756332
CJ <choijun0627@konkuk.ac.kr>
2023-11-08 15:04:26 +0900

    Feat : 입력이 숫자인지 검증하는 기능 구현
    입력이 숫자인지 검증하는 기능 구현

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -3,6 +3,12 @@ package lotto.validation;
 public class InputValidator {
 
     public boolean isNumber(String input) {
-        return false;
+        for (int i = 0; i < input.length(); i++) {
+            char oneCharacter = input.charAt(i);
+            if (!Character.isDigit(oneCharacter)) {
+                return false;
+            }
+        }
+        return true;
     }
 }

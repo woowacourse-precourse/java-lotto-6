8d52730b66258ff251284b195a6264dba0c57481
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-11-08 12:55:09 +0900

    feat(lotto): 입력 에러 추가 및 에러 출력 형식 변경

--- src/main/java/lotto/Error.java
+++ src/main/java/lotto/Error.java
@@ -2,6 +2,7 @@ package lotto;
 
 public enum Error {
     NO_PROBLEM,
+    INPUT_ERROR("[ERROR] 입력 양식이 잘못되었습니다. 다시 입력해주세요."),
     PRICE_ERROR("[ERROR] 구입 금액은 1000단위의 숫자여야 합니다."),
     LOTTO_NUMBER_DUPLICATE_ERROR("[ERROR] 로또 번호는 중복이 없어야 합니다."),
     LOTTO_NUMBER_OUT_OF_COUNT_ERROR("[ERROR] 로또 번호는 6개의 숫자로 구성되어야 합니다."),
@@ -21,11 +22,17 @@ public enum Error {
         this.message = message;
     }
 
-    public void printERROR(Error error) {
+    public static Error printERROR(Error error) {
+        if (error == NO_PROBLEM)
+        {
+            System.out.println(INPUT_ERROR.getMessage());
+            return INPUT_ERROR;
+        }
         System.out.println(error.getMessage());
+        return error;
     }
 
-    private String getMessage() {
+    public String getMessage() {
         return message;
     }
 }

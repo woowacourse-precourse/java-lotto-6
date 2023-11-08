f5ca0542638e58609f45cce33b640528757b9f86
peeaaccee <wlgp8958@naver.com>
2023-11-08 15:31:52 +0900

    test(Grade): 유효하지 않은 Rank 값이면 예외가 발생하는지 테스트

--- src/test/java/lotto/domain/GradeTest.java
+++ src/test/java/lotto/domain/GradeTest.java
@@ -1,7 +1,6 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,6 +8,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class GradeTest {
+    private static final String INVALID_KEY_MESSAGE = "[ERROR] 유효하지 않은 등급 이름입니다.";
     @ParameterizedTest
     @CsvSource({
             "6, 0, FIRST",
@@ -20,5 +20,14 @@ class GradeTest {
     @DisplayName("당첨 번호, 보너스 번호 일치 여부에 따른 등수를 올바르게 판별한다.")
     void findingRank(int matchingNumbers, int matchingBonus, String expectedGrade) {
         assertThat(Grade.findGrade(matchingNumbers, matchingBonus).name()).isEqualTo(expectedGrade);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"ab", "-", "7"})
+    @DisplayName("유효하지 않은 Rank 값이면 예외가 발생한다.")
+    void findGradeByInvalidKeyThrowError(String rank) {
+        assertThatThrownBy(() -> Grade.findGradeByRank(rank))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_KEY_MESSAGE);
     }
 }

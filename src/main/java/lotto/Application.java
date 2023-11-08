fdc83385c108d074fc50cb2b932e2828c48f3c05
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 16:27:59 +0900

    :sparkles: Feat: 애플리케이션 종료시 Console 종료
    - Console close

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 import lotto.controller.LottoGame;
 
 public class Application {
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame();
         lottoGame.run();
+        Console.close();
     }
 }

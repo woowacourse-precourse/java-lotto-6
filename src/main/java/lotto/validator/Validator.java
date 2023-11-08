3bc366d896dd88e4ad762c8b49db044476633315
DooDuZ <sin9158@naver.com>
2023-11-08 09:01:30 +0900

    feat: 숫자 판별 기능 추가
    매우 큰 수 입력 시 필요 - BigInteger로 판별

--- src/main/java/lotto/validator/Validator.java
+++ src/main/java/lotto/validator/Validator.java
@@ -1,13 +1,23 @@
 package lotto.validator;
 
+import java.math.BigInteger;
 import lotto.exception.ErrorMessage;
 
 public class Validator {
+    private static boolean isNumber(String inputMessage) {
+        try {
+            new BigInteger(inputMessage);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_NUMBER_MESSAGE.getMessage());
+        }
+        return true;
+    }
     public static boolean isInteger(String inputMessage) {
+        isNumber(inputMessage);
         try {
             Integer.parseInt(inputMessage);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorMessage.NOT_INTEGER_MESSAGE.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.PRICE_OUT_OF_RANGE_MESSAGE.getMessage());
         }
         return true;
     }
--- src/test/java/lotto/validator/PriceValidatorTest.java
+++ src/test/java/lotto/validator/PriceValidatorTest.java
@@ -2,6 +2,7 @@ package lotto.validator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.math.BigInteger;
 import lotto.config.IntegrationTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -11,13 +12,14 @@ public class PriceValidatorTest {
 
     int max = 100000;
     int min = 1000;
+
     @Test
     @DisplayName("1000배수 테스트")
     void validateThousandTest() {
         // given
         int price = 1001;
         // when then
-        assertThat(price%min==0).isEqualTo(false);
+        assertThat(price % min == 0).isEqualTo(false);
     }
 
     @Test
@@ -28,13 +30,23 @@ public class PriceValidatorTest {
         int price2 = 100001;
 
         // when
-        boolean result = price>=min && price<=max;
-        boolean result2 = price2>=min && price2<=max;
+        boolean result = price >= min && price <= max;
+        boolean result2 = price2 >= min && price2 <= max;
 
         // then
         assertThat(result).isEqualTo(false);
         assertThat(result2).isEqualTo(false);
     }
 
+    @Test
+    @DisplayName("매우 큰 수 테스트")
+    void isNumber(){
+        // given
+        String number = "999999999999999999999999999999999999999999j";
 
+        // when then
+        assertThatThrownBy( ()->{
+            new BigInteger(number);
+        }).isInstanceOf(NumberFormatException.class);
+    }
 }

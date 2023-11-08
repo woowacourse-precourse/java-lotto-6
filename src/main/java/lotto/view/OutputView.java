64069e35541b573276170e5a6db736bf3db69c67
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-08 17:45:11 +0900

    feat : printPurchasedTickets 함수 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,9 @@
 package lotto.view;
 
+import lotto.domain.LottoTicket;
+
+import java.util.List;
+
 public class OutputView {
     private static final String PURCHASED_TICKETS_MESSAGE = "개를 구매했습니다.";
     private static final String WINNING_STATISTICS_HEADER = "당첨 통계";
@@ -7,4 +11,11 @@ public class OutputView {
     private static final String RESULT_FORMAT = "%d개 일치 (%d원) - %d개";
     private static final String PROFIT_RATE_FORMAT = "총 수익률은 %.2f%%입니다.";
 
+    public static void printPurchasedTickets(List<LottoTicket> tickets) {
+        System.out.println(tickets.size() + PURCHASED_TICKETS_MESSAGE);
+        for (LottoTicket ticket : tickets) {
+            System.out.println(ticket.getNumbers().toString());
+        }
+    }
+    
 }

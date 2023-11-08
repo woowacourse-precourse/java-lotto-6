d8f016dc9b6f36c1a8a244416e5dbfbaddaf426d
HYUNJUN\sajun <sajun28@naver.com>
2023-11-08 16:06:28 +0900

    Refactor: "상수 변수화 "

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Lotto {
 
+    private static final int LOTTO_NUMBER_AMOUNT = 6;
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_END_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,21 +19,21 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호는 6자리 입니다.");
         }
     }
 
     private void duplicateCheck(List<Integer> numbers) {
         HashSet<Integer> numberSet = new HashSet<>(numbers);
-        if (numberSet.size() != 6) {
+        if (numberSet.size() != LOTTO_NUMBER_AMOUNT) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호가 중복입니다.");
         }
     }
 
     private void rangeCheck(List<Integer> numbers) {
         for (Integer Num : numbers) {
-            if (Num < 1 || Num > 45) {
+            if (Num < LOTTO_START_NUMBER || Num > LOTTO_END_NUMBER) {
                 throw new IllegalArgumentException("[ERROR] 숫자 범위 오류입니다.");
             }
         }

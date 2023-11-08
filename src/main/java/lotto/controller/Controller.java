cbc7317f66f7070ab0e429ed0f9bca948e561b9c
rkf06 <rkf06@DESKTOP-DUIQUNE>
2023-11-08 17:46:59 +0900

    공백 에러 체크

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,11 +1,20 @@
 package lotto.controller;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.setting.Lotto;
 
 public class Controller {
 	String money;
 	boolean tfCheck;
 	int checkMoney;
+	int countMoney;
+	List<Integer> autoNum;
+	List<Integer> numbers;
+
+	Lotto lotto;
 
 	public Controller() {
 	}
@@ -26,6 +35,12 @@ public class Controller {
 	// 금액 입력
 	public void inputMoney() {
 		money = Console.readLine();
+
+		//공백 에러 체크
+		if (money == "") {
+			throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+		}
+
 		for (int i = 0; i < money.length(); i++) {
 			checkMoney();
 		}
@@ -52,7 +67,19 @@ public class Controller {
 
 	// 몇 장의 로또를 구매했는 지 출력
 	public void countMoney() {
-		System.out.println(checkMoney / 1000 + "개를 구매했습니다.");
+		countMoney = checkMoney / 1000;
+		System.out.println("\n" + countMoney + "개를 구매했습니다.");
+		lottoNum(countMoney);
+	}
+
+	public void lottoNum(int countMoney) {
+		countMoney = this.countMoney;
+
+		for (int i = 0; i < countMoney; i++) {
+			autoNum = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+			System.out.println(autoNum);
+		}
+//		lotto.lottoNum(countMoney);
 	}
 
 }

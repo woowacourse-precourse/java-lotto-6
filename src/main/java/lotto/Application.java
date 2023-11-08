7de81977a93fcdae2917b882060e2d0ce37d2570
alenjb <alenjb@naver.com>
2023-11-08 17:03:07 +0900

    ✨feat(Application, Lotto): 사용자가 구입금액을 입력했을 때 에러처리 후 다시 입력받도록 수정
    사용자에게 구입금액을 입력받고 에러가 발생한 경우 에러 메시지를 출력한 뒤 다시 그 부분부터 입력 받도록 수정함.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -26,49 +26,51 @@ public class Application {
     // 입력 값이 1,000 단위로 나누어 떨어지는지 체크하는 메서드
     public static double isDivisibleByOneThousand(double number) {
         if (number < ONE_LOTTO_PRICE)
-            throw new IllegalArgumentException("[ERROR] 구매금액은 1,000원 이상으로 입력하셔야 합니다."); // 예외처리: 1,000원보다 작은 경우
+            throw new IllegalArgumentException();// 예외처리: 1,000원보다 작은 경우
         if (number % ONE_LOTTO_PRICE != 0)
-            throw new IllegalArgumentException("[ERROR] 구매금액은 1,000원 단위로 입력하셔야 합니다."); // 예외처리: 1,000으로 나누어떨어지지 않는 경우
+            throw new IllegalArgumentException(); // 예외처리: 1,000으로 나누어떨어지지 않는 경우
         return number / ONE_LOTTO_PRICE;
     }
 
     private static int askPurchaseNumber() {
-        try {
-            System.out.println("구입금액을 입력해 주세요.");
-            double purchaseAmount = Double.parseDouble(camp.nextstep.edu.missionutils.Console.readLine());
-            return (int) isDivisibleByOneThousand(purchaseAmount); // 구매 금액이 1,000으로 나누어지는지 확인 후 정수로 바꾸어 리턴
-        } catch (NullPointerException e) {
-            System.out.println("[ERROR] 구매금액을 입력하셔야 합니다.");
-            throw e;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 구매금액은 숫자형식이어야 합니다.");
-            throw e;
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                double purchaseAmount = Double.parseDouble(camp.nextstep.edu.missionutils.Console.readLine());
+                return (int) isDivisibleByOneThousand(purchaseAmount); // 구매 금액이 1,000으로 나누어지는지 확인 후 정수로 바꾸어 리턴
+            } catch (NullPointerException e) {
+                System.out.println("[ERROR] 구매금액을 입력하셔야 합니다.\n");
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 구매금액은 숫자형식이어야 합니다.\n");
+            }catch (IllegalArgumentException e){
+                System.out.println("[ERROR] 구매금액은 1,000원 단위로 입력하셔야 합니다.\n");
+            }
         }
     }
 
     private static Lotto askWiningNumber() {
-        System.out.println("\n당첨 번호를 입력해 주세요.");
-        try {
-            return new Lotto(parseNumberList(camp.nextstep.edu.missionutils.Console.readLine()));
-        } catch (NullPointerException e) {
-            System.out.println("[ERROR] 당첨번호를 입력하셔야 합니다.");
-            throw e;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 당첨번호들은 숫자형식이어야 합니다.");
-            throw e;
+        while (true) {
+            System.out.println("\n당첨 번호를 입력해 주세요.");
+            try {
+                return new Lotto(parseNumberList(camp.nextstep.edu.missionutils.Console.readLine()));
+            } catch (NullPointerException e) {
+                System.out.println("[ERROR] 당첨번호를 입력하셔야 합니다.\n");
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 당첨번호들은 숫자형식이어야 합니다.\n");
+            }
         }
     }
 
     private static int askBonusNumber() {
-        System.out.println("\n보너스 번호를 입력해 주세요.");
-        try {
-            return Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
-        } catch (NullPointerException e) {
-            System.out.println("[ERROR] 당첨번호를 입력하셔야 합니다.");
-            throw e;
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 당첨번호는 숫자형식이어야 합니다.");
-            throw e;
+        while (true) {
+            System.out.println("\n보너스 번호를 입력해 주세요.");
+            try {
+                return Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+            } catch (NullPointerException e) {
+                System.out.println("[ERROR] 보너스 번호를 입력하셔야 합니다.\n");
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 보너스 번호는 숫자형식이어야 합니다.\n");
+            }
         }
     }
 
@@ -82,7 +84,7 @@ public class Application {
                 int number = Integer.parseInt(numStr);
                 numberList.add(number);
             } catch (NumberFormatException e) {
-                System.out.println("[ERROR] 올바른 숫자 형식이 아닙니다: " + numStr);
+                System.out.println("[ERROR] 올바른 숫자 형식이 아닙니다: " + numStr+"\n");
             }
         }
         return numberList;

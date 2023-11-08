4e57064b8489763c0a95a5a5f3125dfd65b14f12
Geunwoo Kim <9eunwoo.kim@gmail.com>
2023-11-08 09:05:25 +0900

    feat(view): 사용자로부터 당첨 번호를 입력받는 기능 및 유효성 검증 구현

--- src/main/java/lotto/view/InputValidator.java
+++ src/main/java/lotto/view/InputValidator.java
@@ -4,7 +4,10 @@ import java.util.regex.Pattern;
 
 final class InputValidator {
     private static final Pattern NON_SPACED = Pattern.compile("^[\\S]+$");
-    private static final Pattern NON_ZERO_LEADING_DIGITS = Pattern.compile("^[1-9][0-9]*$");
+    private static final Pattern ONLY_NUMBERS = Pattern.compile("^[0-9]+$");
+    private static final Pattern ZERO_LEADING_NUMBERS = Pattern.compile("^[0][0-9]*$");
+    private static final Pattern NON_PARSABLE_NUMBERS = Pattern.compile("^[1-9][0-9]{9,}$");
+    private static final Pattern LOTTO_NUMBERS = Pattern.compile("^([1-9][0-9]{0,1})(,[1-9][0-9]{0,1}){5}$");
 
     private InputValidator() {
     }
@@ -15,9 +18,39 @@ final class InputValidator {
         }
     }
 
-    static void validateNonZeroLeadingDigits(String userInput) {
-        if (!NON_ZERO_LEADING_DIGITS.matcher(userInput).matches()) {
-            throw new IllegalArgumentException("[ERROR] 0으로 시작하지 않는 숫자만 입력할 수 있습니다.");
+    static void validateNonZeroLeadingParsableNumber(String userInput) {
+        if (!ONLY_NUMBERS.matcher(userInput).matches()) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력해야 합니다.");
+        }
+        if (ZERO_LEADING_NUMBERS.matcher(userInput).matches()) {
+            throw new IllegalArgumentException("[ERROR] 0으로 시작하는 숫자는 입력할 수 없습니다.");
+        }
+        if (NON_PARSABLE_NUMBERS.matcher(userInput).matches()) {
+            throw new IllegalArgumentException("[ERROR] 10자리 이상의 숫자는 입력할 수 없습니다.");
+        }
+    }
+
+    static void validateLottoNumbersPattern(String userInput) {
+        if (!LOTTO_NUMBERS.matcher(userInput).matches()) {
+            throw new IllegalArgumentException("[ERROR] 1에서 45 사이의 숫자를 쉼표로 구분해서 6개 입력해야 합니다.");
+        }
+    }
+
+    static void validatePurchaseAmount(int purchaseAmount) {
+        if (purchaseAmount < 1_000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
+        }
+        if (purchaseAmount > 100_000) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 100,000원을 초과할 수 없습니다.");
+        }
+        if (purchaseAmount % 1_000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
+        }
+    }
+
+    static void validateLottoNumber(int winningNumber) {
+        if (winningNumber < 1 || winningNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자만 가능합니다.");
         }
     }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,31 +1,31 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputView {
     public int getUserInputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String userInput = Console.readLine();
+
         InputValidator.validateNonSpaced(userInput);
-        InputValidator.validateNonZeroLeadingDigits(userInput);
-        try {
-            int purchaseAmount = Integer.parseInt(userInput);
-            validatePurchaseAmount(purchaseAmount);
-            return purchaseAmount;
-        } catch (NumberFormatException exception) {
-            throw new IllegalArgumentException("[ERROR] 입력된 구입 금액이 너무 큽니다.");
-        }
+        InputValidator.validateNonZeroLeadingParsableNumber(userInput);
+        int purchaseAmount = Integer.parseInt(userInput);
+        InputValidator.validatePurchaseAmount(purchaseAmount);
+        return purchaseAmount;
     }
 
-    private void validatePurchaseAmount(int purchaseAmount) {
-        if (purchaseAmount < 1_000) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 이상이어야 합니다.");
-        }
-        if (purchaseAmount > 100_000) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 100,000원을 초과할 수 없습니다.");
-        }
-        if (purchaseAmount % 1_000 != 0) {
-            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력해야 합니다.");
-        }
+    public List<Integer> getUserInputWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String userInput = Console.readLine();
+
+        InputValidator.validateNonSpaced(userInput);
+        InputValidator.validateLottoNumbersPattern(userInput);
+        return Arrays.stream(userInput.split(","))
+                .map(Integer::parseInt)
+                .peek(InputValidator::validateLottoNumber)
+                .collect(Collectors.toList());
     }
 }

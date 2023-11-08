f90b4b4540d9c9ed77430a635d3f5481a848112d
simjaeyoun <spqjfgogo@gmail.com>
2023-11-08 22:46:55 +0900

    refactor: '안쓰이는 import삭제'

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -4,8 +4,6 @@ import domain.Lotto;
 import domain.Place;
 import repository.LottoRepository;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class LottoService {
--- src/test/java/domain/PlaceAndCorrectNumberMapTest.java
+++ src/test/java/domain/PlaceAndCorrectNumberMapTest.java
@@ -1,13 +1,8 @@
 package domain;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class PlaceAndCorrectNumberMapTest {
 

d19fbdf3771136071be1ad398bde172f6ea1be73
dongho <dons97@naver.com>
2023-11-08 21:19:51 +0900

    refactor: extracted UI method in Application class
    seperated method
    - getPurchasedLottoAmount
    - getWinningNumbers
    - getBonusNumber

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -10,6 +10,51 @@ import lotto.view.Output;
 
 public class Application {
     public static void main(String[] args) {
+        int purcahsedLottoAmount = getPurcahsedLottoAmount();
+        List<Lotto> lottos = LottoController.generateLottos(purcahsedLottoAmount);
+        Output.printPurchasedDetails(lottos);
+        List<Integer> winningNumbers = getWinningNumbers();
+        int bonusNumber = getBonusNumber(winningNumbers);
+
+        List<Integer> counts = LottoController.makeLottoResult(winningNumbers, bonusNumber,
+                lottos);
+        ArrayList<Integer> rewards = new ArrayList<>(
+                List.of(5000, 50000, 1500000, 30000000, 2000000000));
+        double earningRate = LottoController.calculateEarningRate(purcahsedLottoAmount, rewards,
+                counts);
+        Output.printWinningStatistics(rewards, counts, earningRate);
+    }
+
+    private static int getBonusNumber(List<Integer> winningNumbers) {
+        int bonusNumber;
+        while (true) {
+            try {
+                Output.printPromptForBonusNumber();
+                bonusNumber = InputProcessor.processBonusNumberInput(Input.getInput(),
+                        winningNumbers);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+        return bonusNumber;
+    }
+
+    private static List<Integer> getWinningNumbers() {
+        List<Integer> winningNumbers;
+        while (true) {
+            try {
+                Output.printPromptForWinningNumbers();
+                winningNumbers = InputProcessor.processWinningNumbersInput(Input.getInput());
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+        return winningNumbers;
+    }
+
+    private static int getPurcahsedLottoAmount() {
         int purcahsedLottoAmount;
         while (true) {
             try {
@@ -22,37 +67,7 @@ public class Application {
                 System.out.println();
             }
         }
-
-        List<Lotto> lottos = LottoController.generateLottos(purcahsedLottoAmount);
-        Output.printPurchasedDetails(lottos);
-        List<Integer> winningNumbers;
-        while (true) {
-            try {
-                Output.printPromptForWinningNumbers();
-                winningNumbers = InputProcessor.processWinningNumbersInput(Input.getInput());
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e);
-            }
-        }
-        int bonusNumber;
-        while (true) {
-            try {
-                Output.printPromptForBonusNumber();
-                bonusNumber = InputProcessor.processBonusNumberInput(Input.getInput(), winningNumbers);
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e);
-            }
-        }
-
-        List<Integer> counts = LottoController.makeLottoResult(winningNumbers, bonusNumber,
-                lottos);
-        ArrayList<Integer> rewards = new ArrayList<>(
-                List.of(5000, 50000, 1500000, 30000000, 2000000000));
-        double earningRate = LottoController.calculateEarningRate(purcahsedLottoAmount, rewards,
-                counts);
-        Output.printWinningStatistics(rewards, counts, earningRate);
-
+        return purcahsedLottoAmount;
     }
+
 }

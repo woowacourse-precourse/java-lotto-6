2abb42c3c098df80a218e511227e31f2feb3453f
Dowon <3120kang@gmail.com>
2023-11-08 23:37:40 +0900

    Refactor(InputView): 하드코딩 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,36 +4,37 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NUMBER_ERROR = "[ERROR] 금액은 숫자를 입력해야 합니다.";
+    private static final String LUCKY_REQUEST_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static final String BONUS_REQUEST_MESSAGE = "보너스 번호를 입력해 주세요.";
+
     public int inputMoney() {
         while (true) {
             try {
-                System.out.println("구입금액을 입력해 주세요.");
+                System.out.println(START_MESSAGE);
                 return Integer.parseInt(Console.readLine());
 
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 금액은 숫자를 입력해야 합니다.");
+                System.out.println(NUMBER_ERROR);
             }
         }
     }
 
     public String inputLucky() {
-
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(LUCKY_REQUEST_MESSAGE);
         return Console.readLine();
     }
 
     public int inputBonus() {
         while (true) {
             try {
-                //System.out.println();
-                System.out.println("보너스 번호를 입력해 주세요.");
+                System.out.println(BONUS_REQUEST_MESSAGE);
                 String bonus = Console.readLine();
                 return Integer.parseInt(bonus);
             } catch (IllegalArgumentException e) {
-                System.out.println("[ERROR] 금액은 숫자를 입력해야 합니다.");
+                System.out.println(NUMBER_ERROR);
             }
         }
     }
-
-
 }

f16da6bd11ca08b38fbd95d7fe956448c588d651
JayM-Oh <277ipjk53@gmail.com>
2023-11-08 15:24:21 +0900

    refactor: validate in Lotto add: hasUniqueNumbers in Lotto

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,11 +13,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        if (numbers == null || numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (!hasUniqueNumbers(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복을 허용하지 않습니다.");
+        }
+        if (numbers.stream().anyMatch(n -> n < 1 || n > 45)) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1부터 45 사이의 자연수여야 합니다.");
         }
     }
-
+    private boolean hasUniqueNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        return uniqueNumbers.size() == numbers.size();
+    }
     public List<Integer> getNumbers(){
         return numbers;
     }

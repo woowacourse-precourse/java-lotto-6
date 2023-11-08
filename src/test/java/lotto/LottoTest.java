1e22b76a191fe486055313af4cc7ca8c0170c267
KAN-JUNHO <junho6667@gmail.com>
2023-11-08 19:53:04 +0900

    test : 로또번호가 1~45 아니면 예외 처리

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import domain.LottoTicket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,4 +25,11 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호가 1~45 사이가 아니면 예외가 발생한다/")
+    @Test
+    void createLottoWithLessThanSixNumbers() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5,46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }

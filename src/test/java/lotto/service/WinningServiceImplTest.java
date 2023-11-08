41068148f327f02ab480812a911cf5287d97d611
lesw <lesw1216@gmail.com>
2023-11-08 13:03:40 +0900

    test: 당첨 번호 6개 이하 입력 시 예외 발생 검증 테스트

--- src/test/java/lotto/service/WinningServiceImplTest.java
+++ src/test/java/lotto/service/WinningServiceImplTest.java
@@ -1,16 +1,13 @@
 package lotto.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningServiceImplTest {
-    //    [X] 각 번호는 1 ~ 45 범위 이외 입력 시 예외 발생
 //    - [X] 6개 이하 또는 6개 초과 입력 시 예외 발생 - 이미 Lotto의 validate가 구현되어 있음.
 //    - [X] 중복된 숫자 입력 시 예외 발생 - 이미 Lotto의 validateDuplicate으로 구현함.
 //            - [X] 숫자 입력 중 공백이 있다면 예외 발생 - 이미 Lotto의 validate() 가 역할을 하고 있다.
@@ -21,6 +18,18 @@ class WinningServiceImplTest {
     WinningServiceImpl winningService = new WinningServiceImpl();
 
     @Test
+    @DisplayName("6개 이하 입력 시 예외 발생")
+    void winningNumberCount() {
+        // Given
+        List<Integer> numbers = Arrays.asList(4, 34, 9, 3, 1);
+
+        // When
+        // Then
+        assertThatThrownBy(() -> winningService.createWinningLotto(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
     @DisplayName("각 번호가 1 ~ 45 범위가 아니면 예외 발생")
     void winningNumberRange() {
         // Given
@@ -29,7 +38,7 @@ class WinningServiceImplTest {
         // When
         // Then
 
-        Assertions.assertThatThrownBy(() -> winningService.createWinningLotto(numbers))
+        assertThatThrownBy(() -> winningService.createWinningLotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

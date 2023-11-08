8f30b361ee528ae7d63d52864c333a0e76feb752
jyeun722 <skqhs7276@gamil.com>
2023-11-08 17:45:32 +0900

    feat: Check input lotto numbers for duplicate

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,6 @@
 package lotto;
 
-import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private static final String ERROR_MESSAGE = "[ERROR]";
@@ -10,6 +10,7 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validateSize(numbers);
         validateRange(numbers);
+        validateDuplication(numbers);
         this.numbers = numbers;
     }
 
@@ -24,6 +25,14 @@ public class Lotto {
         boolean rangeCheck = numbers.stream().anyMatch(number -> number > 45 || number < 1);
         if (rangeCheck) {
             System.out.println(ERROR_MESSAGE + " 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateDuplication(List<Integer> numbers) {
+        HashSet<Integer> duplicateCheck = new HashSet<>(numbers);
+        if (duplicateCheck.size() != 6) {
+            System.out.println(ERROR_MESSAGE + " 로또 번호는 중복이 없어야 합니다.");
             throw new IllegalArgumentException();
         }
     }

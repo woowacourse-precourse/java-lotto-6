ac84f81f954f8c5cfe1e9426146600c13d910e1f
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 14:36:09 +0900

    feat: 로또번호 검증 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -11,8 +11,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        //사용자가 당첨 번호를 6개 미만 혹은 초과해서 입력했을 경우
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER.getMessage());
+        }
+
+        //사용자가 당첨 번호를 중복으로 입력했을 경우
+        if (numbers.stream().distinct().count() != 6) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_DUPLICATION.getMessage());
+        }
+        //사용자가 당첨 번호의 범위를 1부터 45이하로 입력하지 않았을 경우
+        for (Integer number : numbers) {
+            if (number < 1 || number > 45) {
+                throw new IllegalArgumentException(ErrorMessage.INVALID_LOTTO_NUMBER_RANGE.getMessage());
+            }
         }
     }
 

d0d24780c110f615c4975dbd07300d2d69fb1639
Haaae <101785628+Haaae@users.noreply.github.com>
2023-11-08 13:36:43 +0900

    feat: 숫자를 세는 Count 클래스 구현
    - RankBoard에서 Map<Rank, Long>을 사용하면 Rank count를 증가할 때 map에 대해 get(), put() 메서드를 사용하면서 Long객체를 변경해야 하는 상황을 해결하기 위해 구현
    
    - count 증가 메서드
    - count 반환 메서드

---
+++ src/main/java/lotto/domain/entity/Count.java
@@ -0,0 +1,23 @@
+package lotto.domain.entity;
+
+public class Count {
+
+    private long count;
+
+    public Count() {
+        this(0L);
+    }
+
+    public Count(long count) {
+        this.count = count;
+    }
+
+
+    public void plus() {
+        count++;
+    }
+
+    public long getCount() {
+        return count;
+    }
+}

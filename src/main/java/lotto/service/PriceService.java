c8188f3d8824a51f435983e18f912301ff078d72
dabin <db021129@naver.com>
2023-11-08 15:10:52 +0900

    feat : add LottoController

---
+++ src/main/java/lotto/service/PriceService.java
@@ -0,0 +1,12 @@
+package lotto.service;
+
+import lotto.domain.Price;
+
+import java.util.List;
+
+public class PriceService {
+
+    public int setPrice(Price price, int inputPrice) {
+        price.
+    }
+}

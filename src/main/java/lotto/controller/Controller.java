7f9375820a3f00d6aee9c9106fe51277e4df897f
hajungIm <ihj_ewha@ewhain.net>
2023-11-08 11:13:50 +0900

    Feat(Controller): 게임 시작 `run( )` 작성
    앞서 작성한
    구입 금액 입력 기능 (`getBudget`) 과
    랜덤 로또 발행 기능 (`getLottos`) 를 연결

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,5 +1,23 @@
 package lotto.controller;
 
-public class Controller {
+import lotto.repository.LottoRepository;
+import lotto.service.LottoService;
+import lotto.view.InputView;
 
+public class Controller {
+    static final LottoService lottoService = new LottoService();
+
+    public void run() {
+        getBudget();
+        getLottos();
+
+    private void getBudget() {
+        LottoRepository.setBudget(InputView.inputBudget());
+    }
+
+    private void getLottos() {
+        lottoService.issueLottos();
+    }
+
+    }
 }

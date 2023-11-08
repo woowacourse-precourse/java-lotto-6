8041a5e0b03c546fc04c9c85e10b99fe4a55589a
takoyakimchi <c4nyou@gmail.com>
2023-11-08 15:49:52 +0900

    feat(const): 게임 진행 관련 메시지 상수 enum 정의

---
+++ src/main/java/lotto/constant/GameMessage.java
@@ -0,0 +1,16 @@
+package lotto.constant;
+
+public enum GameMessage {
+
+    INPUT_BUY_PRICE("구입금액을 입력해 주세요."),
+    YOU_BOUGHT_N_LOTTOS("개를 구매했습니다."),
+    INPUT_WIN_NUMBERS("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBERS("보너스 번호를 입력해 주세요."),
+    WIN_STATISTICS("당첨 통계\n---");
+
+    private final String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+}

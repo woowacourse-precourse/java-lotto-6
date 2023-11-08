dd2837437956384d56244127d2333e0a8807e628
ihun-il <hunil9978@gmail.com>
2023-11-08 20:34:57 +0900

    feat: 상수 생성

---
+++ src/main/java/lotto/utils/Constant.java
@@ -0,0 +1,25 @@
+package lotto.utils;
+
+public class Constant {
+    //Lotto
+    public static final int LOTTO_NUMBERS_SIZE = 6;
+    public static final int LOTTO_NUMBERS_MIN_VALUE = 1;
+    public static final int LOTTO_NUMBERS_MAX_VALUE = 45;
+
+    //PurchaseAmount
+    public static final int PURCHASE_AMOUNT_UNIT = 1000;
+    public static final int PURCHASE_AMOUNT_REMAINDER = 1000;
+
+    //BonusNumber
+    public static final int BONUS_NUMBER_MIN_VALUE = 1;
+    public static final int BONUS_NUMBER_MAX_VALUE = 45;
+
+    //inputView
+    public static final String INPUT_VIEW_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_VIEW_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_VIEW_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    //outputView
+    public static final String OUTPUT_VIEW_LOTTO_COUNT = "개를 구매했습니다.";
+
+}

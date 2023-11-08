167dd08f5586ace45aed92d649721817adb300e1
potatomoong <star901210@hanmail.net>
2023-11-08 19:16:04 +0900

    Feat: 로또 번호에 중복이 있으면 에러 발생하는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -11,21 +11,35 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LottoGenerator.PICK_NUMBER) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
-        }
+        try {
+            if (numbers.size() != LottoGenerator.PICK_NUMBER) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 6개의 숫자여야 합니다.");
+            }
 
-        if (numbers.stream().anyMatch(number -> number < LottoGenerator.MIN_NUMBER || number > LottoGenerator.MAX_NUMBER)) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
-        }
+            if (numbers.stream().anyMatch(number -> number < LottoGenerator.MIN_NUMBER || number > LottoGenerator.MAX_NUMBER)) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+            }
 
-        if (numbers.stream().anyMatch(number -> !String.valueOf(number).matches("[0-9]+"))) {
-            throw new IllegalArgumentException("[ERROR] 숫자와 ','만 입력가능합니다.");
+            if (numbers.stream().anyMatch(number -> !String.valueOf(number).matches("[0-9]+"))) {
+                throw new IllegalArgumentException("[ERROR] 숫자와 ','만 입력가능합니다.");
+            }
+
+            if (hasDuplicate(numbers)) {
+                throw new IllegalArgumentException("[ERROR] 중복된 숫자는 입력할 수 없습니다.");
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
         }
 
     }
 
     // TODO: 추가 기능 구현
+    private boolean hasDuplicate(List<Integer> numbers) {
+        return numbers.stream().distinct().count() != numbers.size();
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }

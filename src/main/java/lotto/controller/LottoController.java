b08af3a42caeb6e7a0748e6aa6c3f9ff482fac1d
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 22:07:55 +0900

    feat: controller 추가 구현 및 수정

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -12,31 +12,66 @@ import lotto.view.OutputView;
 public class LottoController {
 
 	private static final int LOTTO_SIZE = 6;
-	private static List<Lotto> lottoNumbers = new ArrayList<>();
+	private static final int MIN_NUM = 1;
+	private static final int MAX_NUM = 45;
 
 	public void startGame() {
 		int amount = LottoAmount.inputAmount();
 		OutputView.printLottoAmount(amount);
 
-		setLottoList(amount);
-		OutputView.printLottoNumbers(lottoNumbers);
+		List<Lotto> lottos = setLottos(amount);
 
-		List<Integer> lottoPrizeNumber = InputView.inputLottoPrizeNumber();
+		List<Integer> lottoPrizeNumber = setPrizeNumber();
+		int bonusNumber = setBonusNumber();
+
+		LottoGame.playGame(lottos, lottoPrizeNumber, bonusNumber, amount);
+
+	}
+
+	private int setBonusNumber() {
 		int bonusNumber = InputView.inputBonusNumber();
+		if (bonusNumber > MAX_NUM || bonusNumber < MIN_NUM) {
+			System.out.println("[ERROR] 보너스 번호는 1에서 45사이의 수여야 합니다.");
+			throw new IllegalArgumentException();
+		}
+		return bonusNumber;
+	}
 
-		LottoGame.playGame(lottoNumbers, lottoPrizeNumber, bonusNumber);
+	private List<Integer> setPrizeNumber() {
+		List<Integer> prizeNumber = InputView.inputLottoPrizeNumber();
+		validatePrizeNumber(prizeNumber);
+		return prizeNumber;
+	}
 
-		OutputView.printPrizeMessage();
-		OutputView.printPrizeResult();
+	private void validatePrizeNumber(List<Integer> prizeNumber) {
+		if (prizeNumber.size() != LOTTO_SIZE) {
+			System.out.println("[ERROR] 당첨 번호는 6개여야 합니다.");
+			throw new IllegalArgumentException();
+		}
+		if (prizeNumber.stream()
+				.distinct()
+				.count() != prizeNumber.size()) {
+			System.out.println("[ERROR] 중복된 당첨 번호는 존재할 수 없습니다.");
+			throw new IllegalArgumentException();
+		}
+		for (Integer it : prizeNumber) {
+			if (it > MAX_NUM || it < MIN_NUM) {
+				System.out.println("[ERROR] 당첨 번호는 1에서 45 사이의 수여야 합니다.");
+				throw new IllegalArgumentException();
+			}
+		}
 
 	}
 
 
-	private void setLottoList(int amount) {
+	private List<Lotto> setLottos(int amount) {
+		List<Lotto> lottos = new ArrayList<>();
 		for (int i = 0; i < amount; i++) {
-			List<Integer> lotto = new ArrayList<>();
-			lotto = LottoGenerator.makeLottoList();
-			lottoNumbers.add(new Lotto(lotto));
+			List<Integer> lottoNumber = LottoGenerator.makeLottoList();
+			Lotto lotto = new Lotto(lottoNumber);
+			lottos.add(lotto);
+			OutputView.printLottoNumbers(lotto);
 		}
+		return lottos;
 	}
 }

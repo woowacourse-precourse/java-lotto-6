51fc9c70352c17449cef015847767e01463fbeda
gavin-ko <kas6610@gmail.com>
2023-11-08 22:12:37 +0900

    refactor : 클래스 기반 인스턴스 변경
     -LottoException 객체를 메서드 내에서 생성하고 사용하는 방식으로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lotto.exception.LottoException;
 
 public class Lotto {
-    private final LottoException lottoException = new LottoException();
+
     private final static int BONUS_INDEX = 6;
     private final List<Integer> numbers;
 
@@ -15,10 +15,12 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        LottoException lottoException = new LottoException();
         lottoException.validateLottoNumbers(numbers);
     }
 
     public void setBonusNumber(int inputBonusNumber) {
+        LottoException lottoException = new LottoException();
         lottoException.validateBonusNumbers(numbers, inputBonusNumber);
         numbers.add(inputBonusNumber);
     }

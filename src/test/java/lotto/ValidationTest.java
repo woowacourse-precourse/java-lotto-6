87307610f7d477c0082f285c296a4992e92924b1
“EvelynKimm” <evekimmy09@gmail.com>
2023-11-08 22:33:54 +0900

    test: add test for exception message
    예외 메세지([ERROR]) 포함하는지 확인하는 기능 추가

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
@@ -8,15 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ValidationTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
 
     // 입력 받은 로또 번호에 대한 테스트
     @DisplayName("로또 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
     @Test
     void createLottoByOutOfRangeNumber() {
-        List<Integer> outOfRangeNumbers = List.of(1, 2, 3, 4, 5, 46); // '46'이 범위를 벗어남
+        List<Integer> lottoNumber = List.of(1, 2, 3, 4, 5, 46);
 
-        assertThrows(IllegalArgumentException.class, () ->
-                Validation.validateLottoNumbers(outOfRangeNumbers));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
+                Validation.validateLottoNumbers(lottoNumber));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     // 입력 받은 로또 구입 가격에 대한 테스트
@@ -25,8 +28,9 @@ public class ValidationTest {
     void createLottoCostByNotIntegerFormat() {
         String lottoCost = "not number";
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validatePrice(lottoCost));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     @DisplayName("입력값에 공백이 포함되어 있을 경우 예외가 발생한다.")
@@ -34,8 +38,9 @@ public class ValidationTest {
     void createLottoCostByContainsSpace() {
         String lottoCost = "1 2";
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validatePrice(lottoCost));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     @DisplayName("로또 구입 금액이 1000으로 나누어 떨어지지 않을 경우 예외가 발생한다.")
@@ -43,8 +48,9 @@ public class ValidationTest {
     void createLottoCostByNotDivisibleThousand() {
         String lottoCost = "2500"; // 1000으로 나누어 떨어지지 않음
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validatePrice(lottoCost));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     // 입력 받은 보너스 넘버에 대한 테스트
@@ -54,8 +60,9 @@ public class ValidationTest {
         String bonusNumber = "abc";
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validateBonusNumber(bonusNumber, winningNumbers));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호가 1부터 45 사이의 숫자가 아니면 예외가 발생한다.")
@@ -64,8 +71,9 @@ public class ValidationTest {
         String bonusNumber = "46";
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validateBonusNumber(bonusNumber, winningNumbers));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
@@ -74,8 +82,9 @@ public class ValidationTest {
         String bonusNumber = "5"; // 당첨 번호와 중복되는 숫자
         List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
 
-        assertThrows(IllegalArgumentException.class, () ->
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
                 Validation.validateBonusNumber(bonusNumber, winningNumbers));
+        assertThat(exception.getMessage()).contains(ERROR_MESSAGE);
     }
 }
 

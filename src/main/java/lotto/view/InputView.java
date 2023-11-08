2df2fde357a0724e79b8538d03be6dacf9a43a32
devhoya97 <diajbh123@gmail.com>
2023-11-08 10:50:06 +0900

    refactor(view): InputView가 어느 정도의 파싱 로직을 갖도록 리팩토링
    입력 소스가 바뀌거나 입력 정책이 바뀐다고 했을 때, 핵심 로직은 영향을 받지 않고
    UI로직만 변경하려면 InputView에서 어느정도의 파싱을 가져야 한다고 판단함

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,10 +1,40 @@
 package lotto.view;
 
+import static lotto.utils.ErrorMessages.NOT_INTEGER_INPUT;
+import static lotto.utils.ConstantString.DELIMITER;
+
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
-    public String getInputFromConsole() {
+
+    public int getNumber() {
+        String input = getTrimInputFromConsole();
+        validateInteger(input);
+        return Integer.parseInt(input);
+    }
+
+    public List<Integer> getNumbers() {
+        String input = getTrimInputFromConsole();
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String splittedInput : input.split(DELIMITER)) {
+            validateInteger(splittedInput);
+            winningNumbers.add(Integer.parseInt(splittedInput));
+        }
+        return winningNumbers;
+    }
+
+    private String getTrimInputFromConsole() {
         String input = Console.readLine();
         return input.trim();
+    }
+
+    private void validateInteger(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException numberFormatException) {
+            throw new IllegalArgumentException(NOT_INTEGER_INPUT);
+        }
     }
 }

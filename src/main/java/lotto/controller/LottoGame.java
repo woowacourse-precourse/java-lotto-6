5732e9ff474a1573e6b306952995214713f2ebd5
이정우 <acg6138@naver.com>
2023-11-08 11:53:24 +0900

    feat(LottoGame, OutputView): 로또 게임 컨트롤러 및 결과 출력 클래스 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,4 +1,70 @@
 package lotto.controller;
 
+import lotto.domain.*;
+import lotto.dto.LottoTicketsDto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.view.InputView.inputBonusNumber;
+import static lotto.view.InputView.inputWinningNumbers;
+import static lotto.view.OutputView.printErrorMessage;
+
 public class LottoGame {
+
+    public void run() {
+        LottoPurchaseMoney lottoPurchaseMoney = getLottoPurchaseMoney();
+
+        LottoTickets lottoTickets = purchaseLottoTickets(lottoPurchaseMoney.getAvailablePurchaseCount());
+
+        OutputView.printLottoTickets(new LottoTicketsDto(lottoTickets));
+
+        WinningNumbers winningNumbers = getWinningNumbers();
+
+        LottoResult lottoResult = getLottoResult(winningNumbers, lottoTickets);
+
+        OutputView.printLottoResult(lottoResult, lottoPurchaseMoney);
+    }
+
+    public LottoPurchaseMoney getLottoPurchaseMoney() {
+        try {
+            int money = InputView.inputPurchaseMoney();
+            return new LottoPurchaseMoney(money);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e.getMessage());
+            return getLottoPurchaseMoney();
+        }
+    }
+
+    public LottoTickets purchaseLottoTickets(int numberOfTicket) {
+        try {
+            List<LottoTicket> lottoTickets = new ArrayList<>();
+            for (int count = 0; count < numberOfTicket; count++) {
+                lottoTickets.add(generateLottoTicket());
+            }
+            return new LottoTickets(lottoTickets);
+        } catch (IllegalArgumentException e) {
+            printErrorMessage(e.getMessage());
+            return purchaseLottoTickets(numberOfTicket);
+        }
+    }
+
+    private LottoTicket generateLottoTicket() {
+        return new LottoTicket(NumberGenerator.generate());
+    }
+
+    public WinningNumbers getWinningNumbers() {
+        return new WinningNumbers(inputWinningNumbers(), inputBonusNumber());
+    }
+
+    public LottoResult getLottoResult(WinningNumbers winningNumbers, LottoTickets lottoTickets) {
+        List<Rank> winningRanks = new ArrayList<>();
+        for (LottoTicket lottoTicket : lottoTickets.getLottoTickets()) {
+            winningRanks.add(winningNumbers.getRank(lottoTicket));
+        }
+        return new LottoResult(winningRanks);
+    }
+
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,49 @@
 package lotto.view;
 
+import lotto.domain.LottoPurchaseMoney;
+import lotto.domain.LottoResult;
+import lotto.domain.Rank;
+import lotto.dto.LottoTicketDto;
+import lotto.dto.LottoTicketsDto;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class OutputView {
+    private static final String EARNINGS_RATE_FORMAT = "#,###.#";
+
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println(errorMessage);
+    }
+
+    public static void printLottoTickets(LottoTicketsDto lottoTicketsDto) {
+        System.out.println(System.lineSeparator() + lottoTicketsDto.getTotalCount() + "개를 구매했습니다.");
+        for (LottoTicketDto lottoTicketDto : lottoTicketsDto.getLottoTickets()) {
+            System.out.println(lottoTicketDto.getLottoNumbers());
+        }
+    }
+
+    public static void printLottoResult(LottoResult lottoResult, LottoPurchaseMoney lottoPurchaseMoney) {
+        System.out.println(System.lineSeparator() + "당첨 통계\n" + "---");
+        for (Rank rank : getReversedRanks()) {
+            if (rank.equals(Rank.BLANK)) continue;
+            System.out.println(rank.getWinningMessage() + lottoResult.getRankCount(rank) + "개");
+        }
+        String earningsRate = earningsRateFormatter(lottoResult.getTotalEarningsRate(lottoPurchaseMoney));
+        System.out.println("총 수익률은 " + earningsRate + "%입니다.");
+    }
+
+    private static List<Rank> getReversedRanks() {
+        List<Rank> ranks = Arrays.asList(Rank.values());
+        Collections.reverse(ranks);
+        return ranks;
+    }
+
+    private static String earningsRateFormatter(double earningsRate) {
+        DecimalFormat format = new DecimalFormat(EARNINGS_RATE_FORMAT);
+        return format.format(earningsRate);
+    }
+
 }

13f91b51ad279ea279e83844bcec999af1667012
jiwon1217 <koagjw@gmail.com>
2023-11-08 15:43:20 +0900

    :sparkles: feat: 보너스 번호의 범위는 1 ~ 45까지이다.

--- src/main/java/lotto/model/WinningLotto.java
+++ src/main/java/lotto/model/WinningLotto.java
@@ -4,6 +4,9 @@ import lotto.Lotto;
 
 public class WinningLotto {
     private static final String BONUS_NUMBER_DOES_NOT_DUPLICATED_WITH_WINNING_NUMBER = "[ERROR] 보너스 번호는 당첨 번호와 중복되지 않아야 합니다.";
+    private static final String INVALID_NUMBER_RANGE = "[ERROR] 보너스 번호의 범위는 1 ~ 45까지 입니다.";
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 45;
     private final Lotto winningLotto;
     private final int bonusNumber;
 
@@ -23,11 +26,18 @@ public class WinningLotto {
 
     public void validate(Lotto winningLotto, int bonusNumber) {
         validateDuplicateCheck(winningLotto, bonusNumber);
+        validateNumberRange(bonusNumber);
     }
 
     public static void validateDuplicateCheck(Lotto winningLotto, int bonusNumber) {
         if (winningLotto.isContain(bonusNumber)) {
             throw new IllegalArgumentException(BONUS_NUMBER_DOES_NOT_DUPLICATED_WITH_WINNING_NUMBER);
+        }
+    }
+
+    private void validateNumberRange(int bonusNumber) {
+        if (bonusNumber < MIN_RANGE || bonusNumber > MAX_RANGE) {
+            throw new IllegalArgumentException(INVALID_NUMBER_RANGE);
         }
     }
 }

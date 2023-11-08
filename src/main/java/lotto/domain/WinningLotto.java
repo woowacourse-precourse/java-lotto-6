86097fb3d2b9af5f04cf9826e179d7ab5e00005c
haebyun <bhk4392@gmail.com>
2023-11-08 18:30:38 +0900

    feat(WinningLotto): 인자로 받은 로또와 당첨 로또를 비교하여 Rank를 반환하는 기능 구현

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class WinningLotto {
@@ -9,5 +10,20 @@ public class WinningLotto {
     public WinningLotto(List<Integer> numbers, int bonusNumber) {
         this.winningLottoNumbers = numbers;
         this.bonusNumber = bonusNumber;
+    }
+
+    public Rank matchingNumbers(Lotto lotto) {
+        int count = getMatchingCount(lotto);
+        boolean bonus = lotto.hasNumber(bonusNumber);
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.matchRank(count, bonus)&&rank.getPrize()!=0)
+                .findFirst()
+                .orElse(Rank.NOTHING_ZERO);
+    }
+
+    private int getMatchingCount(Lotto lotto) {
+        return (int) winningLottoNumbers.stream()
+                .filter(lotto::hasNumber)
+                .count();
     }
 }

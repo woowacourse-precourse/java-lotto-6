e2b106f89ad75ee7aa53dcc9e9adc59bd5e2f1a4
Donghun Won <wonhun1225@gmail.com>
2023-11-08 16:24:15 +0900

    feat(model) : lotto 클래스 예외처리 추가
    중복숫자 나올 시 예외처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,17 +1,28 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        duplicateValidate(numbers);
         this.numbers = numbers;
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void duplicateValidate(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+
+        if (numSet.size() != numbers.size()) {
             throw new IllegalArgumentException();
         }
     }

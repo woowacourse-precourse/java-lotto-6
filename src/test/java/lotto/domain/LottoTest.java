68a6ca150b5bbdbc1026029c824fad1ce9d02182
apuhapu <laks852@naver.com>
2023-11-08 22:03:41 +0900

    도메인 로직 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -4,9 +4,12 @@ import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -31,4 +34,18 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+    @DisplayName("List to arr")
+    @Test
+    void toArrTest() {
+        // given
+        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(nums);
+
+        // when
+        int[] arr = lotto.toArr();
+
+        // then
+        assertArrayEquals(nums.stream().mapToInt(Integer::intValue).toArray(), arr);
+    }
+
 }

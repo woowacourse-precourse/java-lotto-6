e8a7dba1ba100cbe81b610c48bb96939ac05e21c
formoin <alsrud869@gmail.com>
2023-11-08 16:56:00 +0900

    결과 출력하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -54,7 +54,8 @@ public class Application {
             totalprize += prize.prize;
             prizes[prize.count]++;
         }
-        
+        float percent = totalprize/price;
+        printResult(percent, prizes);
 
     }
     private static int inputPrice(){
@@ -108,6 +109,15 @@ public class Application {
             return Statistic.FAIL;
 
         return Statistic.getRank(match, bonusYes);
+    }
+    private static void printResult(float percent, int[] prizes){
+        System.out.println("3개 일치 (5,000원) - "+prizes[1]+"개");
+        System.out.println("4개 일치 (50,000원) - "+prizes[2]+"개");
+        System.out.println("5개 일치 (1,500,000원) - "+prizes[3]+"개");
+        System.out.println("5개 일치 , 보너스 볼 일치 (30,000,000원) - "+prizes[4]+"개");
+        System.out.println("6개 일치 (2,000,000,000원) - "+prizes[5]+"개");
+        System.out.println("총 수익률은 "+percent+"%입니다.");
+
     }
 
 

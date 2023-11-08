38e417c38c4d56d3bbc29e86f2282ccbae8d078a
ajou20658 <kwy1379@ajou.ac.kr>
2023-11-08 21:40:28 +0900

    Feat : 테스트 코드 추가

---
+++ src/test/java/lotto/model/ProfitTest.java
@@ -0,0 +1,24 @@
+package lotto.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfitTest {
+
+    @Test
+    void getProfitRate() {
+    }
+
+    @Test
+    void updateProfitRate() {
+    }
+
+    @Test
+    void init() {
+    }
+
+    @Test
+    void getResultCount() {
+    }
+}
---
+++ src/test/java/lotto/model/ValidatorTest.java
@@ -0,0 +1,48 @@
+package lotto.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    @Test
+    void validatePickSixNumber() {
+    }
+
+    @Test
+    void validatePickBonusNumber() {
+    }
+
+    @Test
+    void validateBuyAmount() {
+    }
+
+    @Test
+    void validateNumberList() {
+    }
+
+    @Test
+    void validateDuplicateSix() {
+    }
+
+    @Test
+    void validateDuplicateSeven() {
+    }
+
+    @Test
+    void validateCount() {
+    }
+
+    @Test
+    void validateNumber() {
+    }
+
+    @Test
+    void validateRange() {
+    }
+
+    @Test
+    void validateModuler() {
+    }
+}

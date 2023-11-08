0a6e0465a6fd0041c9fc32112bf3a61263b87e86
mingtorr <mingtorr@nave.com>
2023-11-08 22:15:43 +0900

    test: lotto 생성결과 비교 및 일치 갯수 비교 로직 테스트 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -24,5 +25,22 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("입력한 값대로 정상적으로 출력되는지 확인한다.")
+    @Test
+    void createLottoCompareInputWithOutput() {
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
+        Lotto lotto = new Lotto(input);
+        List<Integer> output = lotto.getLotto();
+        assertThat(output).isEqualTo(input);
+    }
+
+    @DisplayName("리스트에서 일치하는 값의 갯수가 정상적으로 나오는지 테스트")
+    @Test
+    void testCountMatches() {
+        List<Integer> winningList = List.of(1, 3, 5, 8, 10, 12);
+        List<Integer> numbers = List.of(1, 3, 5, 7, 9, 11);
+        Lotto lotto = new Lotto(numbers);
+        int matches = lotto.countMatches(winningList);
+        assertThat(matches).isEqualTo(3);
+    }
 }

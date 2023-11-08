880787a65403779b27bab83505959cadb26d416d
jeonseong-gwon <jeonsg9904@gmail.com>
2023-11-08 21:33:33 +0900

    ✨feat : 당첨 정보 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,11 @@
 package lotto.view;
 
+import lotto.domain.WinningPrize;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -11,4 +16,28 @@ public class OutputView {
     public void printInfo(List<Integer> numbers) {
         System.out.println(numbers);
     }
+
+    public void displayWinningResults(Map<Long, Long> winningInfo) {
+        LinkedHashMap<Integer, String> lottoPrizes = WinningPrize.getPrizeMap();
+
+        for (Map.Entry<Integer, String> prizeEntry : lottoPrizes.entrySet()) {
+            int prizeMatchingCount = prizeEntry.getKey();
+            String prizeAmount = prizeEntry.getValue();
+            Long winningCount = winningInfo.get((long) prizeMatchingCount);
+
+            if (winningCount != null) {
+                displayBonusNumber(prizeMatchingCount, prizeAmount, winningCount);
+            }
+        }
+    }
+
+    public void displayBonusNumber(int prizeMatchingCount, String prizeAmount , Long winningCount){
+            if(prizeMatchingCount != 7){
+                System.out.println(prizeMatchingCount + "개 일치" + "(" + prizeAmount + "원) - " + winningCount + "개");
+            }
+            else {
+                System.out.println("5개 일치 보너스 번호 일치 (" + prizeAmount + "원) - " + winningCount + "개");
+            }
+    }
+
 }

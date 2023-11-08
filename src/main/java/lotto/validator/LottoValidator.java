0db7c6d445ac861380d30199e20b0f921f51baa3
Sully <2dcoder@naver.com>
2023-11-08 14:50:04 +0900

    refactor(LottoValidator): 필요 없는 부분 변경

--- src/main/java/lotto/validator/LottoValidator.java
+++ src/main/java/lotto/validator/LottoValidator.java
@@ -13,28 +13,28 @@ public class LottoValidator {
     public void validateAll() {
         validateSize();
         validateNumberRange();
-        validateCheckUnique();
+        validateUnique();
     }
 
-    public void validateSize() throws IllegalArgumentException {
+    private void validateSize() {
         if (lottoList.size() != 6) {
             throw new IllegalArgumentException("로또는 6개여야 합니다.");
         }
     }
 
-    public void validateCheckUnique() throws IllegalArgumentException {
-        if (getUniqueCount() != 6){
+    private void validateUnique() {
+        if (hasDuplicateNumber()){
             throw new IllegalArgumentException("로또 숫자는 중복을 허락하지 않습니다.");
         }
     }
 
-    private long getUniqueCount() {
+    private boolean hasDuplicateNumber() {
         return lottoList.stream()
                 .distinct()
-                .count();
+                .count() != 6;
     }
 
-    public void validateNumberRange() {
+    private void validateNumberRange() {
         lottoList.stream()
                 .filter(this::isNumberWrongRange)
                 .forEach(number -> {
@@ -42,14 +42,18 @@ public class LottoValidator {
                 });
     }
 
-    private boolean isNumberWrongRange(int lotto) {
-        return (lotto < 1 || lotto > 45);
-    }
+    public void validateBonusNumber(int bonusNumber) {
+        if (isNumberWrongRange(bonusNumber)) {
+            throw new IllegalArgumentException("1 ~ 45 사이의 숫자만 가능합니다.");
+        }
 
-    public void checkBonusNumber(int bonusNumber) {
         if (lottoList.contains(bonusNumber)) {
             throw new IllegalArgumentException("보너스 숫자는 로또 정답 숫자와 중복될 수 없습니다.");
         }
+    }
+
+    private boolean isNumberWrongRange(int lotto) {
+        return (lotto < 1 || lotto > 45);
     }
 
 }

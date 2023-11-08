b28266936631560afa13f80e1faa37ac0e8a85bb
yy8305 <yy8305@kakao.com>
2023-11-08 14:42:03 +0900

    test(NumberValidator): 로또 번호 유효성 검사 기능 테스트 코드 추가

--- src/test/java/lotto/util/NumberValidatorTest.java
+++ src/test/java/lotto/util/NumberValidatorTest.java
@@ -3,7 +3,9 @@ package lotto.util;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -25,6 +27,24 @@ class NumberValidatorTest {
     void testVerifyPurchaseAmountFail(Integer amount) {
         assertThatIllegalArgumentException().isThrownBy(() -> {
             NumberValidator.verifyPurchaseAmount(amount);
+        });
+    }
+
+    @DisplayName("로또 번호가 정상적인 값으로 전달 됬을 경우 에러가 발생하지 않는다.")
+    @Test
+    void testVerifyNumbersSuccess() {
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatCode(() -> {
+            NumberValidator.verifyLottoNumbers(lottoNumbers);
+        }).doesNotThrowAnyException();
+    }
+
+    @DisplayName("로또 번호로 null 이 전달 됬을 경우 에러가 발생한다.")
+    @ParameterizedTest
+    @NullSource
+    void testVerifyNumbersNullExceptionCheck(List<Integer> lotto) {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            NumberValidator.verifyLottoNumbers(lotto);
         });
     }
 }

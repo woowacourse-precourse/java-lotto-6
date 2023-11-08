beff83585ea799038f80bcd97c571a8e5f152557
권장순 <jsoonworld@gmail.com>
2023-11-08 19:36:07 +0900

    test( LottoYieldCalculatorServiceTest#testCalculateYield() ) : 당첨 수와 구매한 번호를 기반으로 정확한 수익률 계산.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -13,9 +13,12 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
+
+        /** 로또 구입금액 */
         LottoPurchaseView purchaseView = new LottoPurchaseView();
         LottoPurchaseController numberOfLottoTickets = new LottoPurchaseController(purchaseView);
 
+        /** 로또 생산 */
         LottoGenerateView generateView = new LottoGenerateView();
         LottoMachine machine = new LottoMachine();
         LottoGenerationController  lottoGenerationController = new LottoGenerationController(generateView, machine);
@@ -26,17 +29,21 @@ public class Application {
                 .map(Lotto::getNumbers)
                 .collect(Collectors.toList());
 
+        /** 로또 당첨번호 */
         LottoWinningNumbersView LottoWinningNumbersView = new LottoWinningNumbersView();
         LottoWinningNumberController winningNumberController = new LottoWinningNumberController(LottoWinningNumbersView);
         LottoWinningNumber winningNumber = winningNumberController.createWinningNumber();
 
+        /** 로또 보너스번호 */
         LottoBonusNumberView bonusNumberView = new LottoBonusNumberView();
         LottoBonusNumberController bonusNumberController = new LottoBonusNumberController(bonusNumberView, winningNumber);
         LottoBonusNumber bonusNumber = bonusNumberController.createBonusNumber();
 
+        /** 로또 당첨 결과 */
         LottoResultController resultController = new LottoResultController(new LottoResultView());
         resultController.showResults(lottoTickets, winningNumber.getNumbers(), bonusNumber.getBonusNumber());
 
+        /** 로또 수익률 계산 */
         LottoYieldView yieldView = new LottoYieldView();
         LottoYieldCalculatorService yieldCalculatorService = new LottoYieldCalculatorService();
         LottoYieldController yieldController = new LottoYieldController(yieldView, yieldCalculatorService);

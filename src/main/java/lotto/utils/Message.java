3c40f7d27740c6e042086156cfdd26eb58ced168
다운 <j_d_woon@naver.com>
2023-11-08 12:14:03 +0900

    fix: message 클래스 삭제

--- src/main/java/lotto/utils/Message.java
+++
@@ -1,8 +0,0 @@
-package lotto.utils;
-
-public class Message {
-    public static String PURCAHSE_AMOUNT_LOTTO = "구입금액을 입력해 주세요.";
-    public static String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-    public static String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
-
-}

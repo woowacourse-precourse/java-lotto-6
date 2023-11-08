30da6addf0c29a36ca84ab6305398418b99cb5c1
bykbyk0401 <bykbyk0401@gmail.com>
2023-11-08 13:46:17 +0900

    [FEAT] 게임 로직

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,6 +2,19 @@ package lotto;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int purchaseAmount = Integer.parseInt(Console.readLine("구입금액을 입력해 주세요."));
+            LottoTicket lottoTicket = LottoTicket.purchase(purchaseAmount);
+
+            System.out.println(lottoTicket.getTickets().size() + "개를 구매했습니다.");
+            lottoTicket.printTickets();
+
+            WinningNumbers winningNumbers = WinningNumbers.inputWinningNumbers();
+            int bonusNumber = Integer.parseInt(Console.readLine("보너스 번호를 입력해 주세요."));
+            lottoTicket.calculateAndPrintResult(winningNumbers, bonusNumber);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
     }
 }

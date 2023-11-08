22c1a01ee9701efca09ad3f4cda6b1185d586006
prefer52 <tjsgh151@naver.com>
2023-11-08 21:58:01 +0900

    test(LottoTest): 로또 번호 리스트 반환 테스트

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,19 +1,33 @@
 package lotto.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoTest {
+    private final List<Integer> numbers = Arrays.asList(new Integer[] {1, 2, 3, 4, 5, 6});
+    private Lotto lotto;
+
+    @BeforeEach
+    void init() {
+        lotto = new Lotto(numbers);
+    }
 
     @Test
-    @DisplayName("입력한 로또 번호와 다른 양식의 문자열이 나올 시 예외 발생")
+    @DisplayName("입력한 로또 번호와 다른 양식의 문자열 반환 시 예외 발생")
     void getLottoString() {
-        Lotto lotto = new Lotto(Arrays.asList(new Integer[] {1, 2, 3, 4, 5, 6}));
         Assertions.assertEquals(lotto.getLottoString(), "[1, 2, 3, 4, 5, 6]");
+    }
+
+    @Test
+    @DisplayName("입력한 로또 번호와 다른 리스트 반환 시 예외 발생")
+    void getNumbers() {
+        Assertions.assertEquals(lotto.getNumbers(), numbers);
     }
 }

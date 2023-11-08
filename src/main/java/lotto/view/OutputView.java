2019f61d864861756b69204adab87322dc48ce8a
peeaaccee <wlgp8958@naver.com>
2023-11-08 23:17:55 +0900

    feat(OutputView): 로또 구입 금액 안내 문구 출력

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,9 @@
+package lotto.view;
+
+public class OutputView {
+    private static final String INPUT_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    public static void printInputAmount() {
+        System.out.println(INPUT_AMOUNT_MESSAGE);
+    }
+
+}

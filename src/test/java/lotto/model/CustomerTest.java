e758413595b6b4d9c9ec061a7c0c89c0b91be17d
Kite_Z <duswl339@ajou.ac.kr>
2023-11-08 19:26:47 +0900

    test: delete throwLastLottoByCustomer

--- src/test/java/lotto/model/CustomerTest.java
+++ src/test/java/lotto/model/CustomerTest.java
@@ -58,30 +58,6 @@ public class CustomerTest extends NsTest {
         );
     }
 
-    @DisplayName("throwLastLotto 메소드 실행시 마지막에 구매한 로또가 버려져야 한다.")
-    @Test
-    void throwLastLottoByCustomer() {
-        assertRandomUniqueNumbersInRangeTest(
-                () -> {
-                    Customer customer = new Customer("2000");
-                    customer.buyLotto();
-                    customer.buyLotto();
-                    customer.throwLastLotto();
-                    List<Lotto> lottos = customer.getLottos();
-
-                    assertThat(lottos.size())
-                            .isEqualTo(1);
-
-                    assertThat(lottos.get(0).getNumbers().toString())
-                            .contains("[6, 43, 23, 41, 42, 35]");
-                    },
-                List.of(6, 43, 23, 41, 42, 35),
-                List.of(8, 21, 23, 41, 42, 43)
-
-        );
-    }
-
-
     @Override
     public void runMain() {
         Application.main(new String[]{});

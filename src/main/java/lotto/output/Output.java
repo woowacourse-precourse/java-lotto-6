a3fd31aa975a50498e305b70427c3801b8b7a390
seung-won-kim <hesjs5@skuniv.ac.kr>
2023-11-08 13:37:45 +0900

    feat: 로또 구매 금액을 입력받는 기능 구현

---
+++ src/main/java/lotto/output/Output.java
@@ -0,0 +1,12 @@
+package lotto.output;
+
+public class Output {
+
+    public static void printPurchaseAmountInputMessage() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public static void printErrorMessage(final String message) {
+        System.out.println(message);
+    }
+}

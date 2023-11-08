c9bbe20e1e5ab2b0e4cd0fcec9528026cee6cf2d
Silver <leeeun5423@naver.com>
2023-11-08 17:01:32 +0900

    feat : 로또 번호 개수가 6개보다 작을 때 예외 발생 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -68,4 +68,11 @@ class LottoTest {
         assertThatThrownBy(() -> new User(1500))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("로또 번호의 개수가 6개보다 작을 때 예외가 발생한다.")
+    @Test
+    void createLottoWithSmallerSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

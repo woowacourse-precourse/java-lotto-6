443de671d9ba9c569d7adc9f19df1d7d5829faad
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-08 19:24:43 +0900

    feat: write entire lotto draw result

--- src/main/java/lotto/common/console/Output.java
+++ src/main/java/lotto/common/console/Output.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Output {
     public static final String WRITE_LOTTO_COUNT_FORMAT = "%d개를 구매했습니다.";
+    public static final String WRITE_ENTIRE_LOTTO_DRAW_RESULT = "당첨 통계";
+    public static final String DIVIDING_LINE = "---";
 
     public static void writeMessage(String message) {
         System.out.println(message);
@@ -20,5 +22,10 @@ public class Output {
         List<Integer> numbers = lotto.getNumbers();
 
         System.out.println(Arrays.toString(numbers.toArray()));
+    }
+
+    public static void writeEntireLottoDrawResult() {
+        System.out.println(WRITE_ENTIRE_LOTTO_DRAW_RESULT);
+        System.out.println(DIVIDING_LINE);
     }
 }

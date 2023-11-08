db8332012cec76483331018015dea008cc44de74
Dongjun Shin <tlsehdwns239@gmail.com>
2023-11-08 14:30:12 +0900

    feat(Lotto): 로또에 현재 번호가 중복되었는지 확인하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,11 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import lib.constant.LottoConstants;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -10,6 +15,7 @@ public class Lotto {
         this.validateNumbers(numbers);
     private void validateNumbers(List<Integer> numbers) {
         this.validateNumberSize(numbers);
+        this.validateNumberDuplicated(numbers);
     }
 
     private void validateNumberSize(List<Integer> numbers) {
@@ -17,6 +23,15 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개만 입력할 수 있습니다.");
         }
     }
+
+    private void validateNumberDuplicated(List<Integer> numbers) {
+        Set<Integer> uniqueNumber = new HashSet<Integer>();
+
+        numbers.forEach(number -> uniqueNumber.add(number));
+
+        if (uniqueNumber.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 로또 번호를 입력하였습니다.");
+        }
     }
 
     private void validate(List<Integer> numbers) {

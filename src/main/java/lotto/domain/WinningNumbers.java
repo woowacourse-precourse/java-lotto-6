1b9f1c604a258be14698e54938b6e10f791a0f37
bokyung <bbookng@gmail.com>
2023-11-08 16:52:58 +0900

    feat(WinningNumbers) : WinningNumbers 생성 시 BonusNumber 중복 확인 추가

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -1,12 +1,14 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.validator.Validator;
 
 public class WinningNumbers {
     private final List<Integer> winningNumbers;
     private final int bonusNumber;
 
     public WinningNumbers(List<Integer> winningNumbers, int bonusNumber) {
+        Validator.validateContainsBonusNumbers(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.bonusNumber = bonusNumber;
     }

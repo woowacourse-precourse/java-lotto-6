3ecb46c95df1773c9f3fa3563186eb807d62a6d6
oxdjww <oxdjww@gmail.com>
2023-11-08 20:41:01 +0900

    feat : `2.3` 구입 개수 안내문 출력 기능 구현 완료

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,8 @@
 package lotto.view;
 
+import static lotto.domain.constants.LottoConfig.LOTTO_COUNT;
+import static lotto.view.constants.ViewMessage.LOTTO_PURCHASE_NOTICE_MESSAGE;
+
 import lotto.domain.Lotto;
 import lotto.domain.Lottos;
 import lotto.view.constants.ViewMessage;
@@ -14,6 +17,7 @@ public class OutputView {
     }
 
     public static void printMessage(final Lottos lottoTicket) {
+        printMessage(LOTTO_PURCHASE_NOTICE_MESSAGE);
         for (Lotto lotto : lottoTicket.getLottoTicket()) {
             printMessage(lotto);
         }

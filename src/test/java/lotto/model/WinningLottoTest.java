22b010f2e05ab57a14742c2b03569f85eb5824a2
dltjdn <hana40004@naver.com>
2023-11-08 18:10:15 +0900

    test(WinningLottoTest): compare 함수의 단위테스트 추가

--- src/test/java/lotto/model/WinningLottoTest.java
+++ src/test/java/lotto/model/WinningLottoTest.java
@@ -19,5 +19,18 @@ class WinningLottoTest {
         assertThat(winningLotto).isNotNull();
     }
 
+    @DisplayName("로또와 당첨 번호 비교")
+    @Test
+    void compareLottoWithWinningNumbers() {
+        Lotto winningNumber = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 9;
+        WinningLotto winningLotto = WinningLotto.of(winningNumber, bonusNumber);
+        Lotto userLotto = new Lotto(List.of(1, 2, 3, 4, 5, 9));
+
+        WinningInfo winningInfo = winningLotto.compare(userLotto);
+
+        assertThat(winningInfo).isEqualTo(WinningInfo.FIVE_AND_BONUS_MATCH);
+    }
+
 
 }

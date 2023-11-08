a1f5e48d254e8639d0cf51095e4974c5bbdbd079
박승민 <tmddudals369@naver.com>
2023-11-08 14:48:19 +0900

    feat(lottoService): 로또를 발행하는 기능 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,21 @@
+package lotto.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import lotto.domain.Amount;
+import lotto.domain.Lotto;
+import lotto.domain.Lottos;
+import lotto.utils.LottoGenerator;
+
+public class LottoService {
+
+    public Lottos createLottos(Amount amount) {
+        int lottoCount = amount.getLottoCount();
+        List<Lotto> lottos = IntStream.range(0, lottoCount)
+            .mapToObj(i -> new Lotto(LottoGenerator.generate()))
+            .collect(Collectors.toList());
+
+        return new Lottos(lottos);
+    }
+}

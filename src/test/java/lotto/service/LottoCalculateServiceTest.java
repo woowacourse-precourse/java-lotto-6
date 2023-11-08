68490902d224e2c989ae04835a93e3dbf27e948d
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-11-08 14:08:55 +0900

    test(LottoCalculateServiceTest) : 로또 번호 몇개 맞았는지 테스트 코드 작성

--- src/test/java/lotto/service/LottoCalculateServiceTest.java
+++ src/test/java/lotto/service/LottoCalculateServiceTest.java
@@ -4,6 +4,8 @@ import lotto.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoCalculateServiceTest {
@@ -20,4 +22,17 @@ public class LottoCalculateServiceTest {
         // then
         assertThat(result).isEqualTo("62.5");
     }
+
+    @DisplayName("로또 번호 몇개 맞았는지 테스트")
+    @Test
+    public void countUserNumbersContainLottoTest() {
+        // given
+        List<Integer> userNumbers = List.of(1, 2, 3, 7, 8, 9);
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+        // when
+        int count = lottoCalculateService.countUserNumbersContainLotto(userNumbers, lottoNumbers);
+        // then
+        assertThat(count).isEqualTo(3);
+    }
+
 }

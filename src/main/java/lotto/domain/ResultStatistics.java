609b225d5b137758e5c5565897eafad2eb1902d3
Sinorang <aodkrak@naver.com>
2023-11-08 21:39:54 +0900

    feat. 게임 결과 통계를 나타내는 클래스 생성
    - 로또 게임의 결과를 통계적으로 분석하는 기능 구현
    - 통계 정보 출력 기능 구현

---
+++ src/main/java/lotto/domain/ResultStatistics.java
@@ -0,0 +1,59 @@
+package lotto.domain;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.enums.ResultMessage;
+import lotto.enums.ResultStatus;
+
+public class ResultStatistics {
+    private final List<Result> results;
+    private final Map<ResultStatus, Integer> map = new HashMap<>();
+    private int totalProfit = 0;
+
+    public ResultStatistics(List<Result> results) {
+        this.results = results;
+    }
+
+    private void initMap() {
+        map.put(ResultStatus.MATCH6, 0);
+        map.put(ResultStatus.MATCH5_WITH_BONUS_BALL, 0);
+        map.put(ResultStatus.MATCH5, 0);
+        map.put(ResultStatus.MATCH4, 0);
+        map.put(ResultStatus.MATCH3, 0);
+        map.put(ResultStatus.NONE2, 0);
+        map.put(ResultStatus.NONE1, 0);
+        map.put(ResultStatus.NONE0, 0);
+    }
+
+    private void makeMapAndTotalProfit(List<Result> results) {
+        for (Result result : results) {
+            addTotalProfit(result);
+            map.put(result.getResultStatus(), map.get(result.getResultStatus()) + 1);
+        }
+    }
+
+    private void addTotalProfit(Result result) {
+        totalProfit += result.getResultStatus().getValue();
+    }
+
+    public String printResultStatistics() {
+        StringBuilder result = new StringBuilder();
+        result.append(MessageFormat.format(ResultMessage.MATCH3.getValue(), map.get(ResultStatus.MATCH3))).append("\n");
+        result.append(MessageFormat.format(ResultMessage.MATCH4.getValue(), map.get(ResultStatus.MATCH4))).append("\n");
+        result.append(MessageFormat.format(ResultMessage.MATCH5.getValue(), map.get(ResultStatus.MATCH5))).append("\n");
+        result.append(MessageFormat.format(ResultMessage.MATCH5_WITH_BONUS_BALL.getValue(),
+                map.get(ResultStatus.MATCH5_WITH_BONUS_BALL))).append("\n");
+        result.append(MessageFormat.format(ResultMessage.MATCH6.getValue(), map.get(ResultStatus.MATCH6))).append("\n");
+        return result.toString();
+    }
+
+    public int getTotalProfit() {
+        return totalProfit;
+    }
+
+    public int getMatchCount(ResultStatus status) {
+        return map.get(status);
+    }
+}

2781f9639dbc62b863c2cb886a994ca2fb206ad8
toychip <dlawnsgud427@naver.com>
2023-11-08 19:16:54 +0900

    test: 원소가 6개가 아닐 때 테스트

--- src/test/java/lotto/input/WinningLottoTest.java
+++ src/test/java/lotto/input/WinningLottoTest.java
@@ -1,12 +1,12 @@
 package lotto.input;
 
 import static lotto.exception.ErrorType.InsideListDuplicatedException;
+import static lotto.exception.ErrorType.InvalidNumberSizeException;
 import static lotto.exception.ErrorType.OverValueException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.Lotto;
-import lotto.exception.ErrorType;
 import lotto.exception.LottoException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,7 +28,7 @@ public class WinningLottoTest {
     }
 
     @Test
-    @DisplayName("원소 중 범위를 넘어가는 값이 있을 때 InsideListDuplicatedException 발생")
+    @DisplayName("원소 중 범위를 넘어가는 값이 있을 때 OverValueException 발생")
     void invalidNumberTest(){
 
         //given
@@ -39,5 +39,24 @@ public class WinningLottoTest {
         assertThatThrownBy(() -> new Lotto(lottoNumbers))
                 .isInstanceOf(LottoException.class)
                 .hasFieldOrPropertyWithValue("exceptionType", OverValueException);
+    }
+
+    @Test
+    @DisplayName("원소가 6개가 아닐때 때 InsideListDuplicatedException 발생")
+    void invalidSizeTest(){
+
+        //given
+        List<Integer> lottoNumbers1 = List.of(1, 2, 3, 4, 5);
+        List<Integer> lottoNumbers2 = List.of(1, 2, 3, 4, 5, 6, 7);
+
+        //when & then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers1))
+                .isInstanceOf(LottoException.class)
+                .hasFieldOrPropertyWithValue("exceptionType", InvalidNumberSizeException);
+
+        //when & then
+        assertThatThrownBy(() -> new Lotto(lottoNumbers2))
+                .isInstanceOf(LottoException.class)
+                .hasFieldOrPropertyWithValue("exceptionType", InvalidNumberSizeException);
     }
 }

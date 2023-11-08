f76fad8a8b3b86326c5a7dc5b56a1028c09cd439
psionic0324 <psionic0324@naver.com>
2023-11-08 12:14:36 +0900

    Feat: 공백제거 같은 util을 따로 util 클래스로 분리.
    입력된 문자열에 있는 모든 공백 제거와 콤마(,)를 기준으로 문자열을 분리하는 기능을 포함한다.

---
+++ src/main/java/lotto/util/Util.java
@@ -0,0 +1,18 @@
+package lotto.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+    private static final String SPACE = " ";
+    private static final String NO_SPACE = "";
+    private static final String COMMA = ",";
+
+    public static String removeSpace(String input) {
+        return input.replaceAll(SPACE, NO_SPACE);
+    }
+
+    public static List<String> splitByComma(String input) {
+        return Arrays.asList(input.split(COMMA));
+    }
+}

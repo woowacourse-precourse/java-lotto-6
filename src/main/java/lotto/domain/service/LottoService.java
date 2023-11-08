1355075f0ff8cb52b7729e8e41c359a57e472a55
sunwoong <sunwoong1122@gmail.com>
2023-11-08 19:50:32 +0900

    feat: 로또 당첨 확인 기능 구현

---
+++ src/main/java/lotto/domain/service/LottoService.java
@@ -0,0 +1,50 @@
+package lotto.domain.service;
+
+import lotto.domain.model.Bonus;
+import lotto.domain.model.Lotto;
+import lotto.domain.model.Result;
+
+import java.util.List;
+
+public class LottoService {
+    public Result checkLotto(Lotto generatedLotto, Lotto lotto, Bonus bonus) {
+        int count = 0;
+        List<Integer> generatedNumbers = generatedLotto.getNumbers();
+        List<Integer> numbers = lotto.getNumbers();
+        count = matchGeneratedNumbersAndNumbers(count, generatedNumbers, numbers);
+        if (count != 6) {
+            count = matchGeneratedNumbersAndBonusNumber(bonus, count, generatedNumbers);
+            if (count == 6) {
+                return new Result(count, true);
+            }
+        }
+        return new Result(count, false);
+    }
+
+    private int matchGeneratedNumbersAndNumbers(int count, List<Integer> generatedNumbers, List<Integer> numbers) {
+        for (Integer generatedNumber : generatedNumbers) {
+            count = matchGeneratedNumberAndNumber(count, numbers, generatedNumber);
+        }
+        return count;
+    }
+
+    private int matchGeneratedNumbersAndBonusNumber(Bonus bonus, int count, List<Integer> generatedNumbers) {
+        Integer number = bonus.getNumber();
+        for (Integer generatedNumber : generatedNumbers) {
+            if (generatedNumber.equals(number)) {
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+
+    private int matchGeneratedNumberAndNumber(int count, List<Integer> numbers, Integer generatedNumber) {
+        for (Integer number : numbers) {
+            if (generatedNumber.equals(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

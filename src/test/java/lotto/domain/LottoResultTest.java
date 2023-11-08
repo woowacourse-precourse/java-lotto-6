9233a2a9d9688b760c8e1ee581ecd5292568a0d4
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 11:16:33 +0900

    test(LottoResultTest) : checkLottoResult() 변경 - 각각 로또 객체 채워주고 결과 값 확인 로직 구현

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -3,8 +3,6 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,25 +10,36 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottoResultTest {
-    private LottoMoneyPrize lottoMoneyPrize = new LottoMoneyPrize();
+    private LottoMoneyPrize lottoMoneyPrize;
 
-    @Test
-    @DisplayName("로또 결과 확인 테스트")
-    void checkLottoResult(){
-        //given
-        List<Integer> userWin = Arrays.asList(1,2,3,4,5,6);
-        List<Integer> randomWin = Arrays.asList(1,30,2,5,6,7);
-        int bonusNum = 37;
+   @Test
+   @DisplayName("로또 결과 확인 테스트")
+   void checkLottoResult(){
+       //given
+       List<Lotto> lottos = Arrays.asList(
+               new Lotto(Arrays.asList(1,2,3,4,5,6)), // 1등
+               new Lotto(Arrays.asList(1,2,3,4,5,7)), // 2
+               new Lotto(Arrays.asList(1,2,3,4,5,45)), // 3
+               new Lotto(Arrays.asList(1,2,3,4,5,40)), //3
+               new Lotto(Arrays.asList(1,2,3,24,27,41)), //5
+               new Lotto(Arrays.asList(13,17,23,25,28,44))  //6
+               );
+
+        List<Integer> randomWin = Arrays.asList(1,2,3,4,5,6);
+        int bonusNum = 7;
 
         //when
-        lottoMoneyPrize.printLottoPrize(userWin,randomWin,bonusNum);
-        Map<LottoMoney, Integer> result = lottoMoneyPrize.getLottoMoneyPrize();
-        int lottoPrizeCount = result.get(LottoMoney.FOURTH);
-        int lottoPrizeCount2 = result.get(LottoMoney.SECOND);
+       LottoMoneyPrize lottoMoneyPrize = new LottoMoneyPrize(lottos,randomWin,bonusNum);
+       Map<LottoMoney, Integer> result = lottoMoneyPrize.getLottoMoneyPrize();
 
         //then
-        assertEquals(lottoPrizeCount,1);
-        assertEquals(lottoPrizeCount2,0);
+       assertEquals(1,result.get(LottoMoney.FIRST));
+       assertEquals(1,result.get(LottoMoney.SECOND));
+       assertEquals(2,result.get(LottoMoney.THIRD));
+       assertEquals(0,result.get(LottoMoney.FOURTH));
+       assertEquals(1,result.get(LottoMoney.FIFTH));
+       assertEquals(1,result.get(LottoMoney.ZERO));
+
     }
 
 }

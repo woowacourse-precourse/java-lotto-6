733b8316f6d7df0d5a30741061b8f6043137b639
Sung Hyobin <hellmir610@gmail.com>
2023-11-08 15:08:18 +0900

    refactor: Modify error message for duplicate number more universally considering lotto numbers

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -10,8 +10,8 @@ public class ErrorMessage {
             + LOTTO_PRICE + ", " + LOTTO_PRICE * 20 + ", " + LOTTO_PRICE * 300;
     public static final String INVALID_NUMBER_SIZE_EXCEPTION
             = "로또 번호의 개수는 " + COUNT + "개여야 합니다. 전송된 개수 : ";
-    public static final String DUPLICATE_NUMBER_EXCEPTION = "중복된 당첨 번호가 존재합니다. ";
-    public static final String NUMBER_INDICATION = "번째 당첨 번호 : ";
+    public static final String DUPLICATE_NUMBER_EXCEPTION = "중복된 번호가 존재합니다. ";
+    public static final String NUMBER_INDICATION = "번째 번호 : ";
     public static final String NUMBER_OUT_OF_RANGE_EXCEPTION
             = "로또 번호는 " + START_INCLUSIVE + "부터 " + END_INCLUSIVE + "사이의 숫자여야 합니다. 유효하지 않은 로또 번호 : ";
     public static final String INVALID_NUMBERS_ORDER_EXCEPTION = "로또 번호들이 오름차순으로 정렬되어 있지 않습니다. ";

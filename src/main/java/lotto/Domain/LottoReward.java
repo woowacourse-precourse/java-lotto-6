98b00d7f984a60bc6927d15e41d8d9326b15e969
S2Hyeon <tmdgus908@naver.com>
2023-11-08 16:21:19 +0900

    :bug: fix : 2등을 제외한 등수들을 위한 생성자 추가

--- src/main/java/lotto/Domain/LottoReward.java
+++ src/main/java/lotto/Domain/LottoReward.java
@@ -2,17 +2,21 @@ package lotto.Domain;
 
 public enum LottoReward {
 
-    FIRST_PLACE(6, false, 2000000000),
+    FIRST_PLACE(6, 2000000000),
     SECOND_PLACE(5, true, 30000000),
-    THIRD_PLACE(5, false, 1500000),
-    FOURTH_PLACE(4, false, 50000),
-    FIFTH_PLACE(3, false, 5000),
-    DEFAULT(0, false, 0);
+    THIRD_PLACE(5, 1500000),
+    FOURTH_PLACE(4, 50000),
+    FIFTH_PLACE(3, 5000),
+    DEFAULT(0, 0);
 
 
     private final int equalCount;
     private final boolean equalBonusNumber;
     private final long reward;
+
+    LottoReward(int equalCount, long reward) {
+        this(equalCount, false, reward);
+    }
 
     LottoReward(int equalCount, boolean equalBonusNumber, long reward) {
         this.equalCount = equalCount;

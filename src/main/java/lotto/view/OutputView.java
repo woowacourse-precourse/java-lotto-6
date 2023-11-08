54ef71e9c655af7b66e06c974d849b369266ea47
namtndus <rnrmf0721@gmail.com>
2023-11-08 17:16:11 +0900

    fix(OutputView): outputLottoResult 함수와 outputRateOfReturn 함수 수정

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -17,15 +17,15 @@ public class OutputView {
     public void outputLottoResult(List<Integer> lottoResult){
         System.out.println("\n당첨 통계");
         System.out.println("---");
-        System.out.println("3개 일치 (5,000원) - 1개");
-        System.out.println("4개 일치 (50,000원) - 0개");
-        System.out.println("5개 일치 (1,500,000원) - 0개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - 0개");
-        System.out.println("6개 일치 (2,000,000,000원) - 0개");
+        System.out.println(String.format("3개 일치 (5,000원) - %d개",lottoResult.get(5)));
+        System.out.println(String.format("4개 일치 (50,000원) - %d개",lottoResult.get(4)));
+        System.out.println(String.format("5개 일치 (1,500,000원) - %d개",lottoResult.get(3)));
+        System.out.println(String.format("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개",lottoResult.get(2)));
+        System.out.println(String.format("6개 일치 (2,000,000,000원) - 0개",lottoResult.get(1)));
     }
 
     public void outputRateOfReturn(double rateOfReturn){
         DecimalFormat df = new DecimalFormat("#,##0.0");
-        System.out.println(df.format(rateOfReturn));
+        System.out.println("총 수익률은 "+df.format(rateOfReturn)+"%입니다.");
     }
 }

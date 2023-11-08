cbe6382f6e3a9933ac2f4e3c99640a15ad1bce32
Jamieie <116743317+Jamieie@users.noreply.github.com>
2023-11-08 10:34:45 +0900

    Feat(Grade): 로또 우승조건 및 상금 enum 클래스 구현

---
+++ src/main/java/lotto/domain/result/Grade.java
@@ -0,0 +1,28 @@
+package lotto.domain.result;
+
+public enum Grade {
+    FIRST("6개 일치", "2,000,000,000원"),
+    SECOND("5개 일치, 보너스 볼 일치", "30,000,000원"),
+    THIRD("5개 일치", "1,500,000원"),
+    FOURTH("4개 일치", "50,000원"),
+    FIFTH("3개 일치", "5,000원");
+
+    private String condition;
+    private String prize;
+
+    private Grade() {
+    }
+
+    private Grade(String condition, String prize) {
+        this.condition = condition;
+        this.prize = prize;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+}

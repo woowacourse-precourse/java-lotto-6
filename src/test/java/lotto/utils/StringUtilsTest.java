a1716119f90382ce2d6c9154418ef1c2a2e7c13d
jjongwon7 <jjongwon7@naver.com>
2023-11-08 23:01:12 +0900

    test: 문자열 정수 변환 기능 예외 케이스 테스트 작성

--- src/test/java/lotto/utils/StringUtilsTest.java
+++ src/test/java/lotto/utils/StringUtilsTest.java
@@ -2,6 +2,7 @@ package lotto.utils;
 
 import static lotto.constant.ErrorMessage.INPUT_CONTAINS_WHITE_SPACE;
 import static lotto.constant.ErrorMessage.INPUT_EMPTY;
+import static lotto.constant.ErrorMessage.INPUT_IS_NOT_INTEGER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -44,5 +45,17 @@ public class StringUtilsTest {
 
         // then
         assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("문자열을 정수로 변환하지 못하는 경우 예외가 발생한다.")
+    void getIntegerValue_예외() {
+        // given
+        String input = "3.3";
+
+        // when & then
+        assertThatThrownBy(() -> StringUtils.getIntegerValue(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INPUT_IS_NOT_INTEGER);
     }
 }

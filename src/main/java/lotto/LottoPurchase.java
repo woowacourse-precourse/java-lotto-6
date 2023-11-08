e280e95520091183eb9e295d9794ea902be82e00
주은 <00-10-21@naver.com>
2023-11-08 17:26:35 +0900

    feat: 잘못된 입력에 대해 다시 루프를 도는 기능 추가

--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -7,12 +7,23 @@ public class LottoPurchase {
     private static final int LOTTO_PRICE_UNIT = 1000;
 
     public int requestPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        String input = Console.readLine();
-        return validateAmount(Integer.parseInt(input));
+        while (true) {
+            try {
+                System.out.println("구입금액을 입력해 주세요.");
+                String input = Console.readLine();
+                return validateAmount(Integer.parseInt(input));
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 숫자를 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public int validateAmount(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 0원 이상이어야 합니다.");
+        }
         if (amount % LOTTO_PRICE_UNIT != 0) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
         }

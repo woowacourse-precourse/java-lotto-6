a26be51e101b6f2a81995613f8b727cf0636715c
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-11-08 20:58:13 +0900

    test(BonusNumberTest): BonusNumber 관련 테스트 추가
    `BonusNumber` 관련 테스트를 추가함.

---
+++ src/test/java/lotto/model/BonusNumberTest.java
@@ -0,0 +1,37 @@
+package lotto.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("[Test - BonusNumber]")
+public class BonusNumberTest {
+    @Test
+    @DisplayName("BonusNumber 생성 테스트 - 정상")
+    void bonusNumberTest() {
+        // Given
+        String userInputBonusNumber = "3";
+
+        // When
+        BonusNumber bonusNumber = BonusNumber.create(userInputBonusNumber);
+
+        // Then
+        assertThat(bonusNumber.getBonusNumber()).isEqualTo(Integer.parseInt(userInputBonusNumber));
+    }
+
+    @Test
+    @DisplayName("BonusNumber 생성 테스트 - 로또 범위 밖의 숫자일 경우")
+    void bonusNumberValidationTest() {
+        // Given
+        String userInputBonusNumber = "70";
+
+        // When & Then
+        assertThatThrownBy(() -> BonusNumber.create(userInputBonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}

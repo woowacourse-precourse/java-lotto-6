abad267560c0cc7064736683d187de00992eae08
WinterHana <haha920@naver.com>
2023-11-08 10:02:52 +0900

    refactor(InputService) : 메서드를 static으로 전환
    - 객체 생성 없이 사용하기 위해 메서드를 static으로 전환

--- src/main/java/lotto/service/InputService.java
+++ src/main/java/lotto/service/InputService.java
@@ -1,23 +1,21 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.controller.LottoController;
-import lotto.controller.ResultController;
 
 public class InputService {
-    public int inputMoney() {
+    public static int inputMoney() {
         System.out.println("구입 금액을 입력해 주세요");
         String money = Console.readLine();
         return Integer.parseInt(money);
     }
 
-    public String inputNumbers() {
+    public static String inputNumbers() {
         System.out.println("당첨 번호를 입력해주세요.");
         String numbers = Console.readLine();
         return numbers;
     }
-    
-    public String inputBonusNumber() {
+
+    public static String inputBonusNumber() {
         System.out.println("보너스 번호를 입력해주세요.");
         String number = Console.readLine();
         return number;

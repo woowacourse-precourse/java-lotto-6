1863eae2f93938f0be186e2d965b0ce228145757
swhyeon98 <smreo75@gmail.com>
2023-11-08 20:01:43 +0900

    test: 로또 결과 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -2,11 +2,16 @@ package lotto.service;
 
 import lotto.domain.Lotto;
 import lotto.domain.RandomLottoGenerator;
+import lotto.domain.Rank;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 class LottoServiceTest {
 
@@ -28,6 +33,22 @@ class LottoServiceTest {
         List<Lotto> lottos = lottoService.buyLotto(purchaseAmount);
 
         // then
-        Assertions.assertThat(lottos.size()).isEqualTo(2);
+        assertThat(lottos.size()).isEqualTo(2);
+        assertThat(lottos).hasSize(2);
+    }
+
+    @Test
+    void 로또_결과() {
+        // given
+        Lotto winningLotto = new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+        List<Lotto> purchasedLottos = Arrays.asList(new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)), new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)));
+
+        // when
+        Map<Rank, Long> results = lottoService.calculateResults(winningLotto, purchasedLottos, bonusNumber);
+
+        // then
+        assertThat(results).containsEntry(Rank.FIRST, 1L);
+        assertThat(results).containsEntry(Rank.SECOND, 1L);
     }
 }

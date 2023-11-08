730f4ae4298c0d1129bb9adc7114bf9f2517b1aa
jinsu4755 <parkjinsu4755@gmail.com>
2023-11-08 16:47:36 +0900

    feat: 구매된 로또 티켓으로 결과 수집 기능 작성

--- src/main/java/lotto/domain/PurchasedLottoTickets.java
+++ src/main/java/lotto/domain/PurchasedLottoTickets.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PurchasedLottoTickets {
 
@@ -17,5 +21,12 @@ public class PurchasedLottoTickets {
 
     public int size() {
         return lottoTickets.size();
+    }
+
+    public PurchasedLottoStatistics collectStatistics(Lotto winningLotto, int bonusNumber) {
+        Map<LottoWinning, Long> statistics = lottoTickets.stream()
+                .map(lotto -> lotto.confirmLottoWinning(winningLotto, bonusNumber))
+                .collect(groupingBy(lottoWinning -> lottoWinning, Collectors.counting()));
+        return new PurchasedLottoStatistics(statistics);
     }
 }

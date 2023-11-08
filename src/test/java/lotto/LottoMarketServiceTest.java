c3cb84ce72359a411c8ec1f679ed6f0983889c87
hwangkyojune <shane5969@naver.com>
2023-11-08 18:08:59 +0900

    generateNonDuplicatedNums테스트 lottoNumSupplie제거, mockStatic으로 테스트

--- src/test/java/lotto/LottoMarketServiceTest.java
+++ src/test/java/lotto/LottoMarketServiceTest.java
@@ -1,16 +1,17 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class LottoMarketServiceTest {
     LottoMarketService lottoMarketService;
@@ -29,22 +30,17 @@ class LottoMarketServiceTest {
 
     @Test
     void generateNonDuplicatedNums() {
-        LottoNumSupplier lottoNumSupplier = mock(LottoNumSupplier.class);
-        when(lottoNumSupplier.get()).thenReturn(1,1,2,3,4,4,5,6,6);
-        List<Integer> lottoNums = lottoMarketService.generateNonDuplicatedNums(lottoNumSupplier);
+        MockedStatic<Randoms> mock = mockStatic(Randoms.class);
+        mock.when(() -> Randoms.pickUniqueNumbersInRange(anyInt(), anyInt(), anyInt()))
+                .thenReturn(List.of(1,2,3,4,5,6));
+        List<Integer> lottoNums = lottoMarketService.generateNonDuplicatedNums();
         Assertions.assertThat(lottoNums.size()).isEqualTo(6);
+        mock.close();
     }
 
     @Test
     void calculateLottoCount_정상_실행() {
         int lottoCount = lottoMarketService.calculateLottoCount(16000);
         Assertions.assertThat(lottoCount).isEqualTo(16);
-    }
-    @Test
-    void calculateLottoCount_예외_상황(){
-        Assertions.assertThatThrownBy(()->{
-            lottoMarketService.calculateLottoCount(15500);
-        }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.INCORRECT_AMOUNT.getErrorMessage());
     }
 }

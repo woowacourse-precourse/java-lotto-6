45f5c0d4d4e096adab44fbc1d934827d9aef637e
동원 <ai06021@gmail.com>
2023-11-08 23:52:16 +0900

    feat : 당첨번호 입력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,12 +1,21 @@
 package lotto;
 
+import java.util.LinkedList;
+import java.util.List;
 import lotto.domain.PlayLotto;
+import lotto.domain.UserLotto;
 import lotto.view.InputMoney;
+import lotto.view.OutputLottoNumbers;
 
 public class Application {
     public static void main(String[] args) {
         int inputMoney = new InputMoney().input();
         int cnt = new PlayLotto(inputMoney).lottoTry();
-
+        System.out.println('\n' + "개를 구매했습니다.");
+        List<List<Integer>> LottoNumbers = new LinkedList<>();
+        while (cnt-- > 0) {
+            List<Integer> lotto = new UserLotto().getNumbers();
+            LottoNumbers.add(OutputLottoNumbers.print_Lotto(lotto));
+        }
     }
 }

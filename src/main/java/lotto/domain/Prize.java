1d0967940db462525222d17ca2a05054238add16
namhoon-kim97 <kskcmh5723@gmail.com>
2023-11-08 15:33:29 +0900

    fix: SIXTH_MATCH, FIFTH_AND_BONUS_MATCH 순서 바꿈. FIFTH_AND_BONUS_MATCH가 7에 저장되어서 totalPrize계산에서 당첨금액 계산 순서가 바뀌었었음.

--- src/main/java/lotto/domain/Prize.java
+++ src/main/java/lotto/domain/Prize.java
@@ -4,8 +4,8 @@ public enum Prize {
     THIRD_MATCH(3, 5000, "3개 일치"),
     FOURTH_MATCH(4, 50000, "4개 일치"),
     FIFTH_MATCH(5, 1500000, "5개 일치"),
-    FIFTH_AND_BONUS_MATCH(7, 30000000, "5개 일치, 보너스 볼 일치"),
-    SIXTH_MATCH(6, 2000000000, "6개 일치");
+    SIXTH_MATCH(6, 2000000000, "6개 일치"),
+    FIFTH_AND_BONUS_MATCH(7, 30000000, "5개 일치, 보너스 볼 일치");
 
     private final int matchingCount;
     private final long prizeAmount;

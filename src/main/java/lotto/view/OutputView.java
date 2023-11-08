6f8b37f10820d6e9f9e834f4e0f4aa92c403bfaf
gomdol <oioifor@naver.com>
2023-11-08 14:19:29 +0900

    feat(OutputView) : 당첨 출력 기능 구현

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,30 @@
+package lotto.view;
+
+import lotto.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+    private final String MSG_STATUS = "\n당첨 통계\n---";
+    private final String MSG_DIVIDER = "---";
+    private final String MSG_TOTAL_PREFIX = "총 수익률은 %.1f%%입니다.";
+    private final  String MSG_TICKET_COUNT = "%d개를 구매했습니다.";
+
+    public void printPurchaseTicket(int ticket){
+        System.out.println(ticket+MSG_TICKET_COUNT);
+    }
+
+    public void printLottonPublish(List<Lotto> publishedLotto){
+        publishedLotto.stream()
+                .map(Lotto::getNumbers)
+                .forEach(System.out::println);
+    }
+    public void printStatusMessage(){
+        System.out.println(MSG_STATUS);
+    }
+
+    public void printTotalPrefix(float result){
+        System.out.println(result + MSG_TOTAL_PREFIX);
+    }
+
+}

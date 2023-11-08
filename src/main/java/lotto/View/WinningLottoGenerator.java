309fc9854d395a71658dc62d59c0bd55ed4abbab
GyungA <GyungA@users.noreply.github.com>
2023-11-08 21:06:10 +0900

    feat: 사용자에게 당첨 로또 번호를 입력받는 기능

---
+++ src/main/java/lotto/View/WinningLottoGenerator.java
@@ -0,0 +1,31 @@
+package lotto.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WinningLottoGenerator {
+    public static List<Integer> WINNING_NUMBERS;
+    private static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요. (쉼표로 구분)";
+    private static final String NUMBER_FORMAT_ERROR_MESSAGE = "입력값 %s는 올바른 숫자 형식이 아닙니다. ";
+    public static void generateWinningNumbers() {
+        System.out.println(WINNING_NUMBER_MESSAGE);
+        String[] numberStrings = Console.readLine().split(",");
+        convertStringToInteger(numberStrings);
+    }
+
+    private static List<Integer> convertStringToInteger(String[] numberStrings) {
+        List<Integer> winningNumbers = new ArrayList<>();
+        for (String numberString : numberStrings) {
+            try {
+                int number = Integer.parseInt(numberString.trim());
+                winningNumbers.add(number);
+            } catch (NumberFormatException e) {
+                System.out.println(String.format(NUMBER_FORMAT_ERROR_MESSAGE, numberString));
+            }
+        }
+        WINNING_NUMBERS = winningNumbers;
+        return winningNumbers;
+    }
+}

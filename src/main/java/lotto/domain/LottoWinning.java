7b7bb0f22a6dbaccbf7e94926a7224a56b28211f
jinsu4755 <parkjinsu4755@gmail.com>
2023-11-08 16:45:48 +0900

    feat: 로또 당첨 규칙에 일치 숫자 개수 및 보너스 번호 일치 여부 추가

--- src/main/java/lotto/domain/LottoWinning.java
+++ src/main/java/lotto/domain/LottoWinning.java
@@ -3,18 +3,57 @@ package lotto.domain;
 import java.util.Arrays;
 
 public enum LottoWinning {
-    FIRST(2_000_000_000L, ((matchCount, isBonusMatched) -> matchCount == 6)),
-    SECOND(30_000_000L, ((matchCount, isBonusMatched) -> matchCount == 5 && isBonusMatched)),
-    THIRD(1_500_000L, ((matchCount, isBonusMatched) -> matchCount ==5)),
-    FOURTH(50_000L, ((matchCount, isBonusMatched) -> matchCount == 4)),
-    FIFTH(5_000L, ((matchCount, isBonusMatched) -> matchCount == 3)),
-    LOSE(0L, ((matchCount, isBonusMatched) -> false));
+    FIRST(
+            2_000_000_000L,
+            6,
+            false,
+            ((matchCount, isBonusMatched) -> matchCount == 6)
+    ),
+    SECOND(
+            30_000_000L,
+            5,
+            true,
+            ((matchCount, isBonusMatched) -> matchCount == 5 && isBonusMatched)
+    ),
+    THIRD(
+            1_500_000L,
+            5,
+            false,
+            ((matchCount, isBonusMatched) -> matchCount ==5)
+    ),
+    FOURTH(
+            50_000L,
+            4,
+            false,
+            ((matchCount, isBonusMatched) -> matchCount == 4)
+    ),
+    FIFTH(
+            5_000L,
+            3,
+            false,
+            ((matchCount, isBonusMatched) -> matchCount == 3)
+    ),
+    LOSE(
+            0L,
+            null,
+            null,
+            ((matchCount, isBonusMatched) -> false)
+    );
 
     public final long prize;
+    public final Integer ruleOfMatchedCount;
+    public final boolean hasBonusMatch;
     private final LottoWinningStrategy lottoWinningStrategy;
 
-    LottoWinning(long prize, LottoWinningStrategy lottoWinningStrategy) {
+    LottoWinning(
+            long prize,
+            Integer ruleOfMatchedCount,
+            Boolean hasBonusMatch,
+            LottoWinningStrategy lottoWinningStrategy
+    ) {
         this.prize = prize;
+        this.ruleOfMatchedCount = ruleOfMatchedCount;
+        this.hasBonusMatch = hasBonusMatch;
         this.lottoWinningStrategy = lottoWinningStrategy;
     }
 

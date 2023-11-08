02ecf709559e14495f9ac715a436278f08a43809
Junseo <91211740+Junseo-tech@users.noreply.github.com>
2023-11-08 11:19:12 +0900

    feat(OutputViewResult) : 출력단 수정 - , 넣어주기 - i를 의미있는 entry로 이름 변경

--- src/main/java/lotto/view/OutputViewResult.java
+++ src/main/java/lotto/view/OutputViewResult.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class OutputViewResult {
     private static final String WIN_STATICS_MESSAGE = "당첨 통계";
     private static final String TOTAL_RATE_IS_MESSAGE = "총 수익률은 ";
-    private static final String TOTAL_RATE_IS_MESSAGE_FINISH = "입니다.";
+함    private static final String TOTAL_RATE_IS_MESSAGE_FINISH = "%입니다.";
     private static final String LOTTO_RESULT_CASE_SECOND_MESSAGE = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
     private static final String LOTTO_RESULT_CASE_BASIC_MESSAGE = "%d개 일치 (%s원) - %d개";
     private static final String SEPARATE = "---";
@@ -20,21 +20,21 @@ public class OutputViewResult {
 
     public static void printResultLotto(Map<LottoMoney,Integer> lottoResult){
         lottoResult.entrySet().stream()
-                .filter(i -> i.getKey() != LottoMoney.ZERO)
-                .forEach(i -> System.out.println(getPrintResultLotto(i.getKey(),i.getValue())));
+                .filter(entry -> entry.getKey() != LottoMoney.ZERO)
+                .forEach(entry -> System.out.println(getPrintResultLotto(entry.getKey(),entry.getValue())));
     }
 
-    private static String getPrintResultLotto(LottoMoney lottoMoney, Integer count) {
+    private static String getPrintResultLotto(LottoMoney lottoMoney, int count) {
         if(lottoMoney == LottoMoney.SECOND){
             return String.format(LOTTO_RESULT_CASE_SECOND_MESSAGE
                     , lottoMoney.getMatchCount()
-                    , String.format("%d", lottoMoney.getMoney())
+                    , String.format("%,d", lottoMoney.getMoney())
                     , count);
 
         }
         return String.format(LOTTO_RESULT_CASE_BASIC_MESSAGE,
                 lottoMoney.getMatchCount(),
-                String.format("%d", lottoMoney.getMoney()),
+                String.format("%,d", lottoMoney.getMoney()),
                 count
                 );
     }

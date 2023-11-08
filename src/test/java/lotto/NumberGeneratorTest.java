c6312b31a3049720041832dca5db37969c822fc6
jihun-24k <gi020477@gmail.com>
2023-11-08 16:33:39 +0900

    test: 입력값이 없을때 예외처리

--- src/test/java/lotto/NumberGeneratorTest.java
+++ src/test/java/lotto/NumberGeneratorTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -11,5 +12,10 @@ class NumberGeneratorTest {
     @ParameterizedTest
     void 숫자_이외의_입력_예외처리(String input) {
         assertThatThrownBy(() -> NumberGenerator.formatNumber(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력값이_없을_때_예외처리() {
+        assertThatThrownBy(() -> NumberGenerator.formatNumber(null)).isInstanceOf(IllegalArgumentException.class);
     }
 }

51534ca9ed157eed3784df9b640d4d125601297b
김명준 <86913355+mjj111@users.noreply.github.com>
2023-11-08 16:59:10 +0900

    refactor(Rank): 사용되지 않는 요소 삭제

--- src/main/java/lotto/view/Rank.java
+++ src/main/java/lotto/view/Rank.java
@@ -1,17 +1,15 @@
 package lotto.view;
 
 public enum Rank {
-    FIFTH_PLACE(3, "3개 일치 (5,000원)"),
-    FOURTH_PLACE(4, "4개 일치 (50,000원)"),
-    THIRD_PLACE(5, "5개 일치 (1,500,000원)"),
-    SECOND_PLACE(6, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    FIRST_PLACE(-1, "6개 일치 (2,000,000,000원)");
+    FIFTH_PLACE("3개 일치 (5,000원)"),
+    FOURTH_PLACE("4개 일치 (50,000원)"),
+    THIRD_PLACE("5개 일치 (1,500,000원)"),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    FIRST_PLACE("6개 일치 (2,000,000,000원)");
 
-    private final int matchedAmount;
     private final String mean;
 
-    Rank(int matchedAmount, String mean) {
-        this.matchedAmount = matchedAmount;
+    Rank(String mean) {
         this.mean = mean;
     }
 

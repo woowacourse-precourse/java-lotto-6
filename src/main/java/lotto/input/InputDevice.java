172e343d3d0a0de1656cfad74f37107064b892c1
seastamp <seastamp10@gmail.com>
2023-11-08 22:27:41 +0900

    feat: 숫자 및 당첨번호 입력기능 구현

--- src/main/java/lotto/input/InputDevice.java
+++ src/main/java/lotto/input/InputDevice.java
@@ -1,4 +1,17 @@
 package lotto.input;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class InputDevice {
+    private InputDevice() {
+    }
+
+    public static int receiveNumber() {
+        return InputHandler.parseInputNumber(Console.readLine());
+    }
+
+    public static List<Integer> receiveNumbers() {
+        return InputHandler.parseInputNumbers(Console.readLine());
+    }
 }
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,22 @@
 package lotto.view;
 
+import static lotto.constants.Message.ENTER_BONUS_NUMBER;
+import static lotto.constants.Message.ENTER_PURCHASE_MONEY;
+import static lotto.constants.Message.ENTER_WINNING_NUMBER;
+
 public class InputView {
+    private InputView() {
+    }
+
+    public static void printEnterAmount() {
+        System.out.println(ENTER_PURCHASE_MONEY.getMessage());
+    }
+
+    public static void printEnterWinningNumber() {
+        System.out.println(ENTER_WINNING_NUMBER.getMessage());
+    }
+
+    public static void printEnterBonusNumber() {
+        System.out.println(ENTER_BONUS_NUMBER.getMessage());
+    }
 }

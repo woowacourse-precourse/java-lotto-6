f77cfe2fb284cc8b2422047a05f2e862fbf03dcd
daeun Kong <daeun113@ajou.ac.kr>
2023-11-08 19:27:56 +0900

    chore(LottoNumbers): 사용하지 않는 import 구문 삭제

--- src/main/java/lotto/model/LottoNumbers.java
+++ src/main/java/lotto/model/LottoNumbers.java
@@ -1,9 +1,6 @@
 package lotto.model;
 
 import lotto.util.RandomNumberGenerator;
-
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class LottoNumbers {

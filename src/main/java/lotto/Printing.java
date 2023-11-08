d149900df3fea630ec92084950575094dfa9b688
soyoungxxx <otherplet@gmail.com>
2023-11-08 15:41:51 +0900

    feat(Printing): 로또 출력 기능 구현

--- src/main/java/lotto/Printing.java
+++ src/main/java/lotto/Printing.java
@@ -1,2 +1,17 @@
-package lotto;public class Printing {
+package lotto;
+
+import java.util.List;
+
+public class Printing {
+    private int numberOfLotto;
+    private final List<Lotto> lottos;
+
+    public Printing(int numberOfLotto, List<Lotto> lottos) {
+        this.numberOfLotto = numberOfLotto;
+        this.lottos = lottos;
+    }
+
+    public void printLotto() {
+        System.out.println(lottos);
+    }
 }

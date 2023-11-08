dc81b6383e5349b64131199070ad936f0f5d43d2
JJONGHYUNI <102425552+JJONGHYUNI@users.noreply.github.com>
2023-11-08 13:23:39 +0900

    feat(util) : 숫자 이외의 값이 있는지 검증하는 메소드 구현 - LottoInputValidator 에서 숫자 이외의 값이 들어온 지 검증 메소드

--- src/main/java/lotto/util/validator/LottoInputValidator.java
+++ src/main/java/lotto/util/validator/LottoInputValidator.java
@@ -1,5 +1,9 @@
 package lotto.util.validator;
 
+import lotto.util.validator.constant.ErrorMessageConstant;
+
+import java.util.Arrays;
+
 public class LottoInputValidator implements InputValidator{
     @Override
     public void validate(String checkValue) {
@@ -8,7 +12,16 @@ public class LottoInputValidator implements InputValidator{
 
     private static void validateCount(String[] checkValues) {
         if(checkValues.length != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessageConstant.LOTTO_LENGTH_NOT_SIX_MESSAGE.getMessage());
+        }
+    }
+    private static void validateConsistingNumber(String[] checkValues) {
+        try {
+            for(int i = 0; i < checkValues.length; i++) {
+                int number = Integer.parseInt(checkValues[i]);
+            }
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessageConstant.LOTTO_NUMBER_IS_NOT_NUMERIC.getMessage());
         }
     }
 }
--- src/main/java/lotto/util/validator/constant/ErrorMessageConstant.java
+++ src/main/java/lotto/util/validator/constant/ErrorMessageConstant.java
@@ -3,6 +3,7 @@ package lotto.util.validator.constant;
 public enum ErrorMessageConstant {
     LOTTO_LENGTH_NOT_SIX_MESSAGE("당첨 번호는 6개 입력하셔야합니다."),
     LOTTO_NUMBER_DUPLICATE_MESSAGE("당첨 번호는 서로 다른 수로 이루어져야합니다."),
+    LOTTO_NUMBER_IS_NOT_NUMERIC("당첨 번호는 숫자로 이루어져야합니다."),
     LOTTO_NUMBER_RANGE_ERROR_MESSAGE("각 당첨 번호는 1~45 의 숫자여야 합니다.");
 
     private static final String ERROR = "[ERROR] ";

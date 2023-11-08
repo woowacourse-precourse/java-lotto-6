0a92393cd268eef857b71f9e73504f011621e76f
c1ce <chalicenuclear@gmail.com>
2023-11-08 13:32:03 +0900

    feat : 유저가 선택할 수 있는 금액과 로또번호를 입력받는 코드 구현

---
+++ src/main/java/lotto/view/UserLottoView.java
@@ -0,0 +1,25 @@
+package lotto.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class UserLottoView {
+    private static final String BUY_MESSAGE="구입금액을 입력해 주세요.";
+    private static final String INPUT_LOTTO_MESSAGE="구입금액을 입력해 주세요.";
+    private static final String INPUT_BONUS_LOTTO_MESSAGE="보너스 번호를 입력해 주세요.";
+    public String inputBuyPrice(){
+        System.out.println(BUY_MESSAGE);
+        String buyPrice =readLine();
+        return buyPrice;
+    }
+    public String inputUserLottoNumber(){
+        System.out.println(INPUT_LOTTO_MESSAGE);
+        String userLottoNumber = readLine();
+        return userLottoNumber;
+    }
+    public String inputUserBonusLotto(){
+        System.out.println(INPUT_BONUS_LOTTO_MESSAGE);
+        String userBonusLotto = readLine();
+        return userBonusLotto;
+    }
+
+}

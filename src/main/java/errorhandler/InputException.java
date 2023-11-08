6df6abfae6ad3c6af83f4f36b633407698e9ee65
yujeonghyeon <dbdb1114@naver.com>
2023-11-08 23:35:20 +0900

    exception 타입 추가

--- src/main/java/errorhandler/InputException.java
+++ src/main/java/errorhandler/InputException.java
@@ -1,9 +1,10 @@
-package util;
+package errorhandler;
 
 public enum InputException {
     RANGE_OF_NUMBER_EXCEPTION("[ERROR] 숫자의 범위는 1부터 45 사ㅇ이입니다."),
     NUMBERS_LENGTH_EXCEPTION("[ERROR] 숫자의 갯수는 6개 입니다."),
-    DUPLE_NUMBER_EXCEPTION("[ERROR] 중복된 숫자를 입력하셨습니다. ");
+    DUPLE_NUMBER_EXCEPTION("[ERROR] 중복된 숫자를 입력하셨습니다. "),
+    WRONG_MONEY("[ERROR] 복권은 천 원 단위로 구매하실 수 있습니다. ");
     String errorMessage;
 
     InputException(String errorMessage) {

d6dd7206f84332d62579cf5d46a49bf40201fc76
ddohyeong <dohyeong7026@gmail.com>
2023-11-08 13:05:33 +0900

    feat(NumberFormatExceptionMessage) : NumberFormatException 처리 메시지 기능 구현

---
+++ src/main/java/exception/NumberFormatExceptionMessage.java
@@ -0,0 +1,21 @@
+package exception;
+
+public enum NumberFormatExceptionMessage {
+	NUMBERS_ONLY("숫자만 입력해 주세요"),
+	PURCHASE_AMOUNT_NUMBERS_ONLY("금액을 숫자로만 다시 입력해 주세요");
+
+	private static final String ERROR_TAG = "[ERROR] ";
+	private final String message;
+
+	NumberFormatExceptionMessage(String message) {
+		this.message = ERROR_TAG + message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void throwException() {
+		throw new NumberFormatException(message);
+	}
+}

4be0616df858f247a13fcc1a407f60ac16ab7891
shining013 <nevertheless113@naver.com>
2023-11-08 20:54:19 +0900

    modify: 변수명 변경

--- src/main/java/lotto/model/Quantity.java
+++ src/main/java/lotto/model/Quantity.java
@@ -1,10 +1,10 @@
 package lotto.model;
 
 public class Quantity {
-    private final int quantity;
+    public final int quantityNum;
 
     public Quantity(int purchaseAmount){
-        this.quantity = calculateQuantity(purchaseAmount);
+        this.quantityNum = calculateQuantity(purchaseAmount);
     }
     public int calculateQuantity(int purchaseAmount) {
         return purchaseAmount / 1000;

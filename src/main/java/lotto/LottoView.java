72bf9a80020a6ec6e1feff2d57fd9d4fc79ed9f9
ks00919 <ks00919@naver.com>
2023-11-08 22:34:18 +0900

    feat: Lotto 리스트 받아서 출력하도록 변경

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -1,8 +1,10 @@
 package lotto;
 
+import java.util.List;
+
 public class LottoView {
 
-    public static void printOf(Lotto... lottos) {
+    public static void printOf(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }

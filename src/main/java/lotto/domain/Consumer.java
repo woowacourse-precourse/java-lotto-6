3dc3300c400bdc27624065046cb666298b9cd112
Mi <mi_aa_@naver.com>
2023-11-09 00:48:07 +0900

    chore: Consumer 잘못된 철자 변경

--- src/main/java/lotto/domain/Consumer.java
+++ src/main/java/lotto/domain/Consumer.java
@@ -2,10 +2,10 @@ package lotto.domain;
 
 import java.util.List;
 
-public class Cunsumer {
+public class Consumer {
     private final List<Lotto> purchasedLotto;
 
-    public Cunsumer(List<Lotto> purchasedLotto) {
+    public Consumer(List<Lotto> purchasedLotto) {
         this.purchasedLotto = purchasedLotto;
     }
 

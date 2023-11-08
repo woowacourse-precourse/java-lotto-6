79f64e2245fd947f29cc715e3e55553723131a16
jay <wlguswhd0809@naver.com>
2023-11-08 21:09:49 +0900

    style: Input Constant 구분

--- src/main/java/lotto/constant/InputViewConstant.java
+++ src/main/java/lotto/constant/InputViewConstant.java
@@ -2,4 +2,6 @@ package lotto.constant;
 
 public class InputViewConstant {
     public static final String START_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
 }

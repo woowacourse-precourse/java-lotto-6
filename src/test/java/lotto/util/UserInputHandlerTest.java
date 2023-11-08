0ef7e388209f69716fed03cba2eed7d0e7900eae
yy8305 <yy8305@kakao.com>
2023-11-08 18:04:34 +0900

    test(UserInputHandler): 로또 번호 입력 기능 테스트 코드 추가

--- src/test/java/lotto/util/UserInputHandlerTest.java
+++ src/test/java/lotto/util/UserInputHandlerTest.java
@@ -2,6 +2,8 @@ package lotto.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import lotto.TestDefault;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -20,7 +22,7 @@ class UserInputHandlerTest extends TestDefault {
         assertThat(amount).isEqualTo(expected);
     }
 
-    @DisplayName("로또 구입 금액을 숫자 말고 다른걸 입력했을때 예외 출력후 다시 입력이 가능하다.")
+    @DisplayName("로또 구입 금액을 숫자 말고 다른걸 입력 했을때 예외 출력후 다시 입력이 가능하다.")
     @ParameterizedTest
     @ValueSource(strings = {"\n", "test", "!@"})
     void testLottoPurchaseAmountInputNumericException(String userInput) {
@@ -29,7 +31,29 @@ class UserInputHandlerTest extends TestDefault {
         setInputs(userInput, nextInput);
 
         assertThat(UserInputHandler.getLottoPurchaseAmount()).isEqualTo(Integer.parseInt(nextInput));
-
     }
 
+    @DisplayName("로또 번호를 정상적으로 입력 했을때 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "7,8,9,10,11,12"})
+    void testLottoNumbersInputSuccess(String userInput) {
+        List<Integer> expected = Arrays.stream(userInput.split(",")).map(e -> Integer.parseInt(e)).toList();
+        setInputs(userInput);
+
+        List<Integer> winningNumbers = UserInputHandler.getWinningNumbers();
+
+        assertThat(winningNumbers).isEqualTo(expected);
+    }
+
+    @DisplayName("로또 번호를 숫자 말고 다른걸 입력 했을때 예외 출력후 다시 입력이 가능하다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"\n", "test", "!@", "1,a,3,b,7,8"})
+    void testLottoNumbersInput(String userInput) {
+        String nextInput = "1,2,3,4,5,6";
+        List<Integer> expected = Arrays.stream(nextInput.split(",")).map(e -> Integer.parseInt(e)).toList();
+
+        setInputs(userInput, nextInput);
+
+        assertThat(UserInputHandler.getWinningNumbers()).isEqualTo(expected);
+    }
 }

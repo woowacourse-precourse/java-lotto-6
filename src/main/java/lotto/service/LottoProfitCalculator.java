25459049ddd15db3e1e16c48344d415f6a582dd2
minsu20 <syt07100@gmail.com>
2023-11-08 23:18:44 +0900

    refactor: prizeAmounts를 매개변수로 입력받지 않게 수정

--- src/main/java/lotto/service/LottoProfitCalculator.java
+++ src/main/java/lotto/service/LottoProfitCalculator.java
@@ -1,16 +1,19 @@
 package lotto.service;
 
+import lotto.constants.LottoPrize;
+
 public class LottoProfitCalculator {
 
-    public double calculateProfitRate(int[] matchCounts, int payMoney, long[] prizeAmounts) {
-        long totalPrize = calculateTotalPrize(matchCounts, prizeAmounts);
-        return (double) totalPrize / (payMoney) * 100.0;
+
+    public double calculateProfitRate(int[] matchCounts, int payMoney) {
+        long totalPrize = calculateTotalPrize(matchCounts);
+        return (double) totalPrize / payMoney * 100.0;
     }
 
-    private long calculateTotalPrize(int[] matchCounts, long[] prizeAmounts) {
+    private long calculateTotalPrize(int[] matchCounts) {
         long totalPrize = 0;
         for (int i = 0; i < matchCounts.length; i++) {
-            totalPrize += matchCounts[i] * prizeAmounts[i];
+            totalPrize += (long) matchCounts[i] * LottoPrize.PRIZES[i];
         }
         return totalPrize;
     }

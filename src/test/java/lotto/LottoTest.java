5637a569919d7168da614cfe4b5f4776879c01ea
MisaSohee <sohee5951@naver.com>
2023-11-08 22:11:31 +0900

    test(Lotto): 다른 로또와 일치하는 번호의 개수 반환 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
@@ -22,5 +23,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("matchNumbers()는 다른 로또와 일치하는 번호의 개수를 반환한다.")
+    @Test
+    void matchNumbersReturnsCorrectCount() {
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 7, 8, 9));
+        assertThat(lotto1.matchNumbers(lotto2)).isEqualTo(3);
+    }
 }

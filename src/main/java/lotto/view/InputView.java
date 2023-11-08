485c3314761a16666c3e967a68d5a29f08d6cc83
Yerinnnn <shinyerin0216@gmail.com>
2023-11-08 14:48:45 +0900

    Refactor: InputView 클래스가 인풋값을 받고 반환하는 기능만 수행하도록 간소화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,38 +1,20 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import lotto.util.Validator;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputView {
 
-    public static int inputPurchasePrice() {
-        int price = Integer.parseInt(Console.readLine());
-
-        try {
-            Validator.purchasePrice(price);
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 구입 금액을 1,000원 단위로 입력하세요.");
-            inputPurchasePrice();
-        }
-
-        return price;
+    public static String inputPurchasePrice() {
+        return Console.readLine();
     }
 
-    public static List<Integer> inputWinNumber() {
-        String[] nums = Console.readLine().replace(" ", "").split(",");
-        List<Integer> winNumber = Arrays.stream(nums).map(number -> Integer.parseInt(number)).collect(Collectors.toList());
-
-        return winNumber;
+    public static String inputWinNumber() {
+        return Console.readLine();
     }
 
-    public static int inputBonusNumber() {
-        int bonusNumber = Integer.parseInt(Console.readLine().replace(" ", ""));
-
-        return bonusNumber;
+    public static String inputBonusNumber() {
+        return Console.readLine();
     }
+
 
 }

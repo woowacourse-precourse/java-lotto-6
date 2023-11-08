d316a4f0a50b3dd3c755f606256c144165da4332
JoonKyo <joonkyolee@gmail.com>
2023-11-08 17:23:05 +0900

    feat(LottoResultIndex): 로또 결과를 담은 배열의 인덱스에 대한 상수 enum 추가

--- src/main/java/lotto/constant/LottoResultIndex.java
+++ src/main/java/lotto/constant/LottoResultIndex.java
@@ -1,2 +1,17 @@
-package lotto.constant;public enum LottoResultIndex {
+package lotto.constant;
+
+public enum LottoResultIndex {
+    SECOND_RANK_INDEX(1),
+    FIFTH_RANK_MATCH_INDEX(3),
+    FIRST_RANK_MATCH_INDEX(6);
+
+    private final int index;
+
+    LottoResultIndex(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }

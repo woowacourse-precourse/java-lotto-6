353b071c100656c2c209d73c94dc70ec32122fb9
rlfrkdms1 <rlfrkdms1@naver.com>
2023-11-08 19:31:34 +0900

    test: 로또 구매금 1000원 단위가 아닐 시 예외발생 테스트

--- src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
+++ src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static lotto.constant.ErrorMessage.INCORRECT_MONEY_UNIT;
 import static lotto.constant.ErrorMessage.NOT_ENOUGH_MONEY;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,5 +17,12 @@ class LottoPurchaseMoneyTest {
                 .hasMessage(NOT_ENOUGH_MONEY);
     }
 
+    @DisplayName("로또 구매금 1000원 단위가 아닐 시 예외가 발생한다.")
+    @Test
+    void createLottoPurchaseMoneyByIncorrectUnit() {
+        assertThatThrownBy(() -> new LottoPurchaseMoney(1100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INCORRECT_MONEY_UNIT);
+    }
 
 }

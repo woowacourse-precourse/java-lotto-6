4d95d89b448cee25b3ba8304139ca5db8bc15c3f
gyuoo <yeomky98@naver.com>
2023-11-08 23:23:19 +0900

    feat: implement user payment announcement

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,24 @@
+package view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class InputView {
+    private static final String PAYMENT_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static String setUserPayment() {
+        System.out.println(PAYMENT_MESSAGE);
+        String userInput = Console.readLine();
+        removeBlank(userInput);
+        removeComma(userInput);
+        return userInput;
+    }
+
+    private static String removeComma(String userInput) {
+        return userInput.replace(",", "");
+    }
+
+    private static String removeBlank(String userInput) {
+        return userInput.replace(" ", "");
+    }
+}

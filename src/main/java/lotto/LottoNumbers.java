d482e01b885c651ac590aaf245c34787deb2d9f3
K-KY <kyuyoungk@naver.com>
2023-11-08 10:11:58 +0900

    Feat(LottoNumbers) : 보너스볼을 포함하는지 확인하는 메서드 추가

--- src/main/java/lotto/LottoNumbers.java
+++ src/main/java/lotto/LottoNumbers.java
@@ -16,13 +16,21 @@ public class LottoNumbers {
         return stringBuilder.toString();
     }
 
-    public Score calculateScore(Lotto lotto) {
+    public Score calculateScore(Lotto lotto, BonusBall bonusBall) {
         List<Integer> score = new ArrayList<>();
         for (int i = 0; i < lottoNumbers.size(); i++) {
             int winCount = lotto.calculateWinNumber(lottoNumbers.get(i));
+            winCount += isBonusBallContains(bonusBall, lottoNumbers.get(i), winCount);
             score.add(winCount);
             System.out.println(winCount);
         }
         return new Score(score);
+    }
+
+    private int isBonusBallContains(BonusBall bonusBall, LottoNumber lottoNumber, int winCount) {
+        if (winCount == 5) {
+            return bonusBall.contains(lottoNumber);
+        }
+        return 0;
     }
 }

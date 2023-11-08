6b8da601292d2ae6a66c008e83899c0c18fd9f59
bsyun0571 <43055740+bsyun0571@users.noreply.github.com>
2023-11-08 15:08:01 +0900

    feat: Lotto 클래스 validate 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,46 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
-    }
+	private final int MIN_NUM = 1;
+	private final int MAX_NUM = 45;
+	private final int LOTTO_SIZE = 6;
+	private final List<Integer> numbers;
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
+	public Lotto(List<Integer> numbers) {
+		validate(numbers);
+		validateDuplicate(numbers);
+		validateRange(numbers);
 
-    // TODO: 추가 기능 구현
+		this.numbers = numbers;
+	}
+
+	private void validate(List<Integer> numbers) {
+		if (numbers.size() != LOTTO_SIZE) {
+			System.out.println("[ERROR] 숫자의 개수가 6개가 아닙니다.");
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateDuplicate(List<Integer> numbers) {
+		Set<Integer> duplicateCheck = new HashSet<>(numbers);
+
+		if (duplicateCheck.size() != LOTTO_SIZE) {
+			System.out.println("[ERROR] 중복된 값이 존재합니다.");
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateRange(List<Integer> numbers) {
+		for (int i = 0; i < numbers.size(); i++) {
+			if (numbers.get(i) < MIN_NUM || numbers.get(i) > MAX_NUM) {
+				System.out.println("[ERROR] 수가 1 ~ 45의 범위 내에 존재하지 않습니다.");
+				throw new IllegalArgumentException();
+			}
+		}
+	}
 }

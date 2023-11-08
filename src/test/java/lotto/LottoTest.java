3fc2445156e492f10a5ea32628602b4220c3dd5c
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2023-11-08 13:17:48 +0900

    test: 로또 번호 검증 테스트 수정
    로또 번호에 범위를 벗어나는 숫자가 있으면 예외가 발생한다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -24,5 +24,10 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // 아래에 추가 테스트 작성 가능
+    @DisplayName("로또 번호에 범위를 벗어나는 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByRangeOver() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 55, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }

7ac88e924c3039f8ac5f51ea94babd6fb52bccff
gundorit <packdev937@gmail.com>
2023-11-08 13:43:14 +0900

    feat (InputView) : 당첨 번호 입력을 검증한다
    - 입력에 쉼표가 연속해서 있다면 예외를 발생한다.
    - 입력에 맨 앞, 맨 뒤에 쉼표가 있다면 예외를 발생한다.
    - 입력에 쉼표 앞뒤 공백이 있다면 예외를 발생한다.
    - 입력에 쉼표를 제외한 특수문자가 있다면 예외를 발생한다.

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,6 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import lotto.util.ValidationUtils;
 
 public class InputView {
 
@@ -13,7 +14,9 @@ public class InputView {
 
     public static List<Integer> readWinningNumbers() {
         OutputView.printWinningNumbersPrompt();
-        return Arrays.stream(Console.readLine().split(","))
+        String input = Console.readLine();
+        validateWinningNumbers(input);
+        return Arrays.stream(input.split(","))
             .map(Integer::parseInt)
             .toList();
     }
@@ -21,5 +24,37 @@ public class InputView {
     public static int readBonusNumber() {
         OutputView.printBonusNumberPrompt();
         return Integer.parseInt(Console.readLine());
+    }
+
+    private static void validateWinningNumbers(String input) {
+        ValidationUtils.validateNotNull(input);
+        validateNotStartOrEndWithComma(input);
+        validateNoConsecutiveCommas(input);
+        validateNoCommasWithSpaces(input);
+        validateNoSpecialCharactersExceptComma(input);
+    }
+
+    private static void validateNotStartOrEndWithComma(String input) {
+        if (input.startsWith(",") || input.endsWith(",")) {
+            throw new IllegalArgumentException("입력의 시작이나 끝에 쉼표가 있습니다");
+        }
+    }
+
+    private static void validateNoConsecutiveCommas(String input) {
+        if (input.contains(",,") || input.contains(", ,")) {
+            throw new IllegalArgumentException("쉼표가 연속해서 있습니다");
+        }
+    }
+
+    private static void validateNoCommasWithSpaces(String input) {
+        if (input.contains(" ,") || input.contains(", ")) {
+            throw new IllegalArgumentException("쉼표 옆에 공백이 있습니다");
+        }
+    }
+
+    private static void validateNoSpecialCharactersExceptComma(String input) {
+        if (input.matches(".*[^0-9,\\s]+.*")) {
+            throw new IllegalArgumentException("쉼표를 제외한 특수문자가 있습니다");
+        }
     }
 }

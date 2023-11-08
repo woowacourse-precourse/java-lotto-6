a1cb0b3202dca3454a332888d8ffae97bfc6acc3
hunko <rhwlgns4386@naver.com>
2023-11-08 20:48:02 +0900

    feat(LottoController): 입력문제시 다시 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,12 +2,11 @@ package lotto;
 
 import lotto.controller.LottoController;
 import lotto.domain.ProfitCalculator;
-import lotto.controller.WinnerLottoFactory;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        LottoController controller = new LottoController(new ProfitCalculator(),new WinnerLottoFactory());
+        LottoController controller = new LottoController(new ProfitCalculator());
         controller.run();
     }
 }
--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import lotto.controller.dto.ResultDto;
 import lotto.controller.numbercandidatestring.NumberCandidateString;
 import lotto.controller.numbercandidatestring.NumberCandidateStrings;
+import lotto.domain.lotto.Lotto;
+import lotto.domain.lotto.LottoNumber;
 import lotto.domain.lotto.Lottos;
 import lotto.domain.money.Money;
 import lotto.domain.ProfitCalculator;
@@ -15,39 +17,68 @@ import lotto.view.OutputView;
 public class LottoController {
 
     private final ProfitCalculator calculator;
-    private final WinnerLottoFactory winnerLottoFactory;
 
-    public LottoController(ProfitCalculator calculator, WinnerLottoFactory winnerLottoFactory) {
+    private Money purchaseAmount;
+
+    public LottoController(ProfitCalculator calculator) {
         this.calculator = calculator;
-        this.winnerLottoFactory = winnerLottoFactory;
     }
 
     public void run() {
+        confirmWinning(buyLotto());
+    }
+
+    private Lottos buyLotto() {
         try {
             NumberCandidateString numberCandidateString = new NumberCandidateString(InputView.collectionOfMoney());
-            Money purchaseAmount = numberCandidateString.toMoney();
-            confirmWinning(buyLotto(purchaseAmount), purchaseAmount);
-        } catch (Exception e) {
+            purchaseAmount = numberCandidateString.toMoney();
+            Lottos lottos = Lottos.from(purchaseAmount.calcBillCount());
+
+            OutputView.renderingPurchaseHistory(lottos.getList());
+            return lottos;
+        }catch (IllegalArgumentException e){
             OutputView.renderingError(e.getMessage());
+            return buyLotto();
         }
+
     }
 
-    private Lottos buyLotto(Money purchaseAmount) {
-        Lottos lottos = Lottos.from(purchaseAmount.calcBillCount());
-
-        OutputView.renderingPurchaseHistory(lottos.getList());
-        return lottos;
-    }
-
-    private void confirmWinning(Lottos lottos, Money purchaseAmount) {
-        WinnerLotto winnerLotto= winnerLottoFactory.generate(
-                NumberCandidateStrings.from(InputView.receiveWinningNumber())
-                ,new NumberCandidateString(InputView.receiveBonusNumber()));
+    private void confirmWinning(Lottos lottos) {
+        WinnerLotto winnerLotto = getWinnerLotto();
 
         List<Rank> confirmResult = lottos.chargeResult(winnerLotto);
         Double profitRate = calculator.calcRate(calculator.calcProfit(confirmResult), purchaseAmount);
 
         OutputView.renderingResult(new ResultDto(confirmResult, profitRate));
+    }
+
+    private WinnerLotto getWinnerLotto() {
+        return getWinnerLotto(getLotto());
+
+    }
+
+    private static WinnerLotto getWinnerLotto(Lotto lotto) {
+        try {
+            return new WinnerLotto(lotto, getBounus());
+        }catch (IllegalArgumentException e){
+            OutputView.renderingError(e.getMessage());
+            return getWinnerLotto(lotto);
+        }
+    }
+
+    private static LottoNumber getBounus() {
+        NumberCandidateString numberCandidateString = new NumberCandidateString(InputView.receiveBonusNumber());
+        return new LottoNumber(numberCandidateString.toNumber());
+    }
+
+    private static Lotto getLotto() {
+        try {
+            NumberCandidateStrings from = NumberCandidateStrings.from(InputView.receiveWinningNumber());
+            return new Lotto(from.toLottoNumberList());
+        }catch (IllegalArgumentException e){
+            OutputView.renderingError(e.getMessage());
+            return getLotto();
+        }
     }
 
 }

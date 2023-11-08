ce5b85f8fe7e8bcbe3fc729567df9213a342fe72
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-08 21:19:46 +0900

    feat: 로또 생성 테스트 구현

--- src/test/java/lotto/utils/LottoMachineTest.java
+++ src/test/java/lotto/utils/LottoMachineTest.java
@@ -1,4 +1,22 @@
 package lotto.utils;
 
+import lotto.domain.Lotto;
+import lotto.exception.LottoGameException;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class LottoMachineTest {
+
+    @DisplayName("로또 생성 테스트")
+    @Test
+    public void testCreateLotto() throws LottoGameException {
+        Lotto lotto = LottoMachine.createLotto();
+
+        assertNotNull(lotto);
+
+        assertEquals(6, lotto.getLottoNumbers().size());
+    }
 }

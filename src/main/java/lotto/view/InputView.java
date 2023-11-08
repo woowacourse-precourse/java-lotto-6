b245153efeec0ac8ae5af7596d0be633aaea5f25
ming-taro <hera002008@naver.com>
2023-11-08 19:57:00 +0900

    feat: 입력 조건 문구 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,9 +1,9 @@
 package lotto.view;
 
 public class InputView {
-    private static final String REQUEST_TO_INPUT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
-    private static final String REQUEST_TO_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
-    private static final String REQUEST_TO_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+    private static final String REQUEST_TO_INPUT_PURCHASE_AMOUNT = "구입 금액을 입력해 주세요.\n(1,000원 ~ 100,000원)";
+    private static final String REQUEST_TO_INPUT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.\n(중복 되지 않는 1~45사이의 수 6개)";
+    private static final String REQUEST_TO_INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.\n(당첨 번호와 중복되지 않는 1~45사이의 수 1개)";
     private static final String ERROR_TAG = "[ERROR]";
 
     public void showAmountInputForm() {

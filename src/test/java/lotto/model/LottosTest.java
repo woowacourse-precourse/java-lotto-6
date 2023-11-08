4f5a3d8f4791e2e934c2ee0ccc12d831bb102424
김현재 <khjmr@naver.com>
2023-11-08 16:53:00 +0900

    test(toStringTest): lottos 의 결과를 뽑아내는 테스트 추가

--- src/test/java/lotto/model/LottosTest.java
+++ src/test/java/lotto/model/LottosTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LottosTest {
+    @DisplayName("Lotto 들의 결과를 출력하는 테스트")
     @Test
-    void getResults() {
+    void toStringTest() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto lotto2 = new Lotto(List.of(2, 3, 4, 5, 6, 7));
+        Lottos lottos = Lottos.create(List.of(lotto, lotto2));
 
+        String expected = "[1, 2, 3, 4, 5, 6]\n" +
+                "[2, 3, 4, 5, 6, 7]\n";
+
+        assertThat(lottos.toString()).isEqualTo(expected);
     }
 
     @DisplayName("로또의 개수를 제대로 세는지 확인하는 테스트")
@@ -24,6 +33,6 @@ class LottosTest {
         List<Lotto> lottoList = List.of(lotto1, lotto2);
         Lottos lottos = Lottos.create(lottoList);
 
-        Assertions.assertThat(lottos.size()).isEqualTo(2);
+        assertThat(lottos.size()).isEqualTo(2);
     }
 }

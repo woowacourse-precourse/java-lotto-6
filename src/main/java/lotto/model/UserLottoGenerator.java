19ea36a35dffe761f0da721ded2ea1e982ead94a
dradnats1012 <herjebi1012@gmail.com>
2023-11-08 20:14:33 +0900

    feat : 로또 번호 생성 클래스 생성

---
+++ src/main/java/lotto/model/UserLottoGenerator.java
@@ -0,0 +1,26 @@
+package lotto.model;
+
+import lotto.NumberGenerator;
+
+import java.util.List;
+
+public class UserLottoGenerator {
+    private final int trial;
+    private final List<Lotto> lottoList;
+    private final NumberGenerator numberGenerator;
+
+    public UserLottoGenerator(NumberGenerator numberGenerator, int trial, List<Lotto> lottoList) {
+        this.numberGenerator = numberGenerator;
+        this.trial = trial;
+        this.lottoList = lottoList;
+    }
+
+    public List<Lotto> makeLottoList(){
+
+        for(int i = 0; i < trial; i++){
+            lottoList.add(new Lotto(numberGenerator.createNumber()));
+        }
+
+        return lottoList;
+    }
+}

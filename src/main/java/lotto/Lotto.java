085a8e2ca9b495eda848e3456ab9d7dc183ed2bf
Jiwon <dtbsmf01@gmail.com>
2023-11-08 17:32:26 +0900

    Lotto 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,12 +1,16 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
         this.numbers = numbers;
     }
 
@@ -16,5 +20,19 @@ public class Lotto {
         }
     }
 
-    // TODO: 추가 기능 구현
+    private static void validateDuplicate(List<Integer> numbers){
+        Set<Integer> nonDupliecateNumber = new HashSet<>(numbers);
+        if(nonDupliecateNumber.size() != 6){
+            throw new IllegalArgumentException("[ERROR]로또 번호는 중복될 수 없습니다.");
+        }
+    }
+
+    private static void validateRange(List<Integer> numbers){
+        for(int matchNumber =0;matchNumber < numbers.size();matchNumber++){
+            if(numbers.get(matchNumber) > 45 || numbers.get(matchNumber) < 1){
+                throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45 사이 입니다.");
+            }
+        }
+    }
+
 }

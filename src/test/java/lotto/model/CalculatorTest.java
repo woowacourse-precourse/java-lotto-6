f0c4739ca32cf9662794c859b44b44b3eff41d74
nayooniverse <kangna99@naver.com>
2023-11-08 15:53:33 +0900

    test: 에러 메시지 테스트 추가

--- src/test/java/lotto/model/CalculatorTest.java
+++ src/test/java/lotto/model/CalculatorTest.java
@@ -5,29 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    @DisplayName("당첨 통계를 계산하는 기능 테스트")
-    @Test
-    public void testCalculateStatistics() {
-        // Arrange
-        WinningNumbers winningNumbers = new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
-        List<Lotto> playerLottos = Arrays.asList(
+    private List<Lotto> playerLottos;
+    private WinningNumbers winningNumbers;
+
+    @BeforeEach
+    void setUp() {
+        playerLottos = Arrays.asList(
                 new Lotto(Arrays.asList(1, 2, 3, 4, 5, 6)),
                 new Lotto(Arrays.asList(1, 2, 3, 4, 5, 7)),
-                new Lotto(Arrays.asList(1, 2, 3, 14, 17, 18))
+                new Lotto(Arrays.asList(1, 2, 3, 14, 17, 18)),
+                new Lotto(Arrays.asList(11, 6, 3, 4, 17, 18)),
+                new Lotto(Arrays.asList(1, 2, 3, 4, 17, 18)),
+                new Lotto(Arrays.asList(3, 4, 5, 6, 17, 18)),
+                new Lotto(Arrays.asList(41, 42, 43, 44, 37, 38))
         );
 
+        winningNumbers = new WinningNumbers(new Lotto(List.of(1, 2, 3, 4, 5, 6)), 7);
+    }
+
+    @DisplayName("당첨 내역을 계산하는 기능 테스트")
+    @Test
+    public void testCalculateStatistics() {
         Calculator calculator = new Calculator(playerLottos, winningNumbers);
 
-        Map<Rank, Integer> statistics = calculator.calculateStatistics();
+        Map<Rank, Integer> winningStatistics = calculator.calculateStatistics();
 
-        assertEquals(1, statistics.get(Rank.FIRST));
-        assertEquals(1, statistics.get(Rank.SECOND));
-        assertEquals(0, statistics.get(Rank.THIRD));
-        assertEquals(0, statistics.get(Rank.FOURTH));
-        assertEquals(1, statistics.get(Rank.FIFTH));
+        assertEquals(1, winningStatistics.get(Rank.FIRST));
+        assertEquals(1, winningStatistics.get(Rank.SECOND));
+        assertEquals(0, winningStatistics.get(Rank.THIRD));
+        assertEquals(2, winningStatistics.get(Rank.FOURTH));
+        assertEquals(2, winningStatistics.get(Rank.FIFTH));
     }
+    
+//    @DisplayName("총 수익률을 계산하는 기능 테스트")
+//    @Test
+//    public void testCalculateWinningRate() {
+//        Calculator calculator = new Calculator(playerLottos, winningNumbers);
+//
+//        Map<Rank, Integer> winningStatistics = calculator.calculateStatistics();
+//
+//        int money = 7000;
+//        double winningRate = calculator.calculateWinningRate(winningStatistics, money);
+//
+//        // Assert
+//        assertEquals(3287285, winningRate, 0.01);
+//    }
 }
---
+++ src/test/java/lotto/model/LottoTest.java
@@ -0,0 +1,36 @@
+package lotto.model;
+
+import static lotto.constants.Message.DUPLICATED_LOTTO_NUMBER;
+import static lotto.constants.Message.INVALID_LOTTO_NUMBER_RANGE;
+import static lotto.constants.Message.INVALID_LOTTO_SIZE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoTest {
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_LOTTO_SIZE);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DUPLICATED_LOTTO_NUMBER);
+    }
+
+    @DisplayName("로또 번호가 중해진 범위를 넘어면 예외가 발생한다.")
+    @Test
+    void createLottoByOutOfRangeNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 55)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(INVALID_LOTTO_NUMBER_RANGE);
+    }
+}

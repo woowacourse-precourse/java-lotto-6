1121affebba630ba6285db7ad06faf29ead4da75
ChoiseU <csw980928@gmail.com>
2023-11-08 14:46:13 +0900

    test : 로또 성공 사례 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,6 +23,12 @@ class LottoTest {
         // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumberSuccess() {
+        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6)));
     }
 
     @DisplayName("로또 번호에 숫자 1미만 이면 예외가 발생한다.")

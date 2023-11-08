7b6b7e924b9122bc95007a99740269391f0a6851
realisshomyang <realisshoman@kakao.com>
2023-11-08 21:12:57 +0900

    feat : 1등 복권 정보와 복권이 얼마나 동일한지 확인하고, Reward를 도출하는 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -35,4 +35,31 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+    public LottoReward getRewardWithMatchedNumbers(WinningLotto winningLotto) {
+        int matchedCount = countMatchedNumbers(winningLotto.getLotto());
+        boolean bonusNumberMatched = isBonusNumberMatched(winningLotto.getBonusNumber());
+        return LottoReward.getReward(matchedCount, bonusNumberMatched);
+    }
+
+    private int countMatchedNumbers(Lotto winningNumbers) {
+        List<Integer> winningNumberList = winningNumbers.getNumbers();
+        int count = 0;
+        for (Integer number : winningNumberList) {
+            if (this.numbers.contains(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean isBonusNumberMatched(Integer bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
 }

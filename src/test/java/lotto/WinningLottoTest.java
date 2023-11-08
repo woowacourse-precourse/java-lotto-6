17a4d36d2b170039099a657d6a08f2705e4cbfa1
MyStraw <kikongdosa@gmail.com>
2023-11-08 09:24:47 +0900

    Test: 추가 보너스 번호의 범위 테스트
    보너스 번호는 1~45 사이에 있어야 하며, 그게 아닐시 예외발생

--- src/test/java/lotto/WinningLottoTest.java
+++ src/test/java/lotto/WinningLottoTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningLottoTest {
 
-    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외를 발생시킨다")
+    @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다")
     @Test
     void createBonusNumberByDuplicated() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
@@ -17,5 +17,15 @@ class WinningLottoTest {
         assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복");
+    }
+
+    @DisplayName("보너스 번호가 1에서 45 사이의 숫자가 아니면 예외가 발생한다")
+    @Test
+    void createBonusNumberOutOfRange() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 50;
+        assertThatThrownBy(() -> new WinningLotto(lotto, bonusNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("범위를 벗어났습니다.");
     }
 }

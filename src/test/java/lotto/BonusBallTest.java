d0af35db0f1489aa446e59bed68998b01f7e740a
K-KY <kyuyoungk@naver.com>
2023-11-08 18:15:29 +0900

    Test(BonusBallTest) : 2등인지 확인 테스트

--- src/test/java/lotto/BonusBallTest.java
+++ src/test/java/lotto/BonusBallTest.java
@@ -1,10 +1,12 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import lotto.domain.BonusBall;
 import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
 import org.junit.jupiter.api.Test;
 
 public class BonusBallTest {
@@ -21,5 +23,12 @@ public class BonusBallTest {
         assertThatThrownBy(() -> new BonusBall(0, new Lotto(List.of(1,2,3,4,5,6))))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 1보다 작은 숫자는 입력 할 수 없습니다.");
+    }
+
+    @Test
+    void 보너스볼_2등인지_확인() {
+        Lotto lotto = new Lotto(List.of(1,2,3,4,5,7));
+        BonusBall bonusBall = new BonusBall(8, lotto);
+        assertThat(bonusBall.contains(new LottoNumber(List.of(1,2,3,4,6,8)))).isEqualTo(2);
     }
 }

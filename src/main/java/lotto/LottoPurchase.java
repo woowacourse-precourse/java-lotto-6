225646cdf4bad22457b033d57ee230fd8a93c263
주은 <00-10-21@naver.com>
2023-11-08 16:16:22 +0900

    feat: 구입 금액 1000원 단위 검증 기능 추가

--- src/main/java/lotto/LottoPurchase.java
+++ src/main/java/lotto/LottoPurchase.java
@@ -3,8 +3,22 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class LottoPurchase {
+
+    private static final int LOTTO_PRICE_UNIT = 1000;
+
     public int requestPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(Console.readLine());
+        String input = Console.readLine();
+        return validateAmount(input);
     }
+
+    private int validateAmount(String input) {
+        int amount = Integer.parseInt(input);
+
+        if (amount % LOTTO_PRICE_UNIT != 0) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
+        }
+        return amount;
+    }
+
 }

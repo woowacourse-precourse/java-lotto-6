d9808e3bd6e06d564f2c3daa8e2115e1c9ae1fe5
kyoo0115 <internetkyoomin@gmail.com>
2023-11-08 15:35:20 +0900

    feat: finalized the final gameplay of the game

--- src/main/java/lotto/controller/GameController.java
+++ src/main/java/lotto/controller/GameController.java
@@ -1,57 +1,63 @@
 package lotto.controller;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import lotto.model.Lotto;
-import lotto.model.LottoMatchService;
-import lotto.model.LottoService;
-import lotto.model.Ranking;
+import lotto.model.entity.Lotto;
+import lotto.model.service.LottoMatchService;
+import lotto.model.service.LottoService;
+import lotto.model.entity.Ranking;
 import lotto.utils.Constants;
 import lotto.view.ResultView;
 
 public class GameController {
+
     private final LottoService lottoService = new LottoService();
     private final LottoMatchService lottoMatchService = new LottoMatchService();
-    private final int ticketCount;
-    private final List<Lotto> lottoList;
-    private final List<Integer> winningNumbers;
-    private final int bonusNumber;
+
+    private int ticketCount;
+    private List<Lotto> lottoList;
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
+    private List<Ranking> rankings;
+    private Map<Ranking, Long> resultMap;
 
     public GameController() {
-        this.ticketCount = lottoService.calculateTicketCount();
-        this.lottoList = lottoService.createLottos(ticketCount);
-        this.winningNumbers = InputController.inputWinningNumbers();
-        this.bonusNumber = InputController.inputBonusNum();
+        run();
     }
 
-    public void run() {
-        try {
-            start();
-            displayWinners();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
+    private void run() {
+        purchaseTickets();
+        showPurchasedTickets();
+        awaitWinningNumberSelection();
+        processGameOutcome();
+        displayGameResults();
     }
 
-    private void start() {
+    private void purchaseTickets() {
+        ticketCount = lottoService.calculateTicketCount();
+        lottoList = lottoService.createLottos(ticketCount);
         ResultView.printTicketCount(ticketCount);
+    }
+
+    private void showPurchasedTickets() {
         ResultView.printLottos(lottoList);
     }
 
-    private void displayWinners() {
-        List<Ranking> rankings = lottoMatchService.calculateWinners(lottoList, winningNumbers, bonusNumber);
-        Map<Ranking, Long> resultMap = lottoMatchService.summarizeResults(rankings);
-        displayResults(resultMap);
+    private void awaitWinningNumberSelection() {
+        winningNumbers = InputController.inputWinningNumbers();
+        bonusNumber = InputController.inputBonusNum(winningNumbers);
     }
 
-    private void displayResults(Map<Ranking, Long> resultMap) {
+    private void processGameOutcome() {
+        rankings = lottoMatchService.determineLottoRankings(lottoList, winningNumbers, bonusNumber);
+        resultMap = lottoMatchService.summarizeRankings(rankings);
+    }
+
+    private void displayGameResults() {
         ResultView.printSuccessResult();
         ResultView.printResults(resultMap);
-        double revenueRate = lottoMatchService.calculateRevenueRate(Constants.LOTTO_TICKET_PRICE, ticketCount, resultMap);
+        double revenueRate = lottoMatchService.calculateRevenueRate(
+                Constants.LOTTO_TICKET_PRICE, ticketCount, rankings);
         ResultView.printRevenueRate(revenueRate);
     }
 }

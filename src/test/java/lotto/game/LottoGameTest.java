c42ae1b19ea9cd58ab59332cc998eb8d15993bb5
ASak1104 <khw56184@gmail.com>
2023-11-08 09:28:47 +0900

    test: 로또 게임 초기화 입력 예외 메시지 테스트 코드 추가

--- src/test/java/lotto/game/LottoGameTest.java
+++ src/test/java/lotto/game/LottoGameTest.java
@@ -1,19 +1,25 @@
 package lotto.game;
 
+import static lotto.game.LottoGameMessage.INVALID_COST_UNIT;
+import static lotto.game.LottoGameMessage.INVALID_LOTTO_NUMBERS;
+import static lotto.game.LottoGameMessage.NON_NUMBER;
+import static lotto.game.LottoGameMessage.NUMBER_OUT_OF_RANGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import lotto.common.ConsoleTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class LottoGameTest extends ConsoleTest {
-    private static final String ERROR_MESSAGE = "[ERROR]";
-
-    @DisplayName("입력 받은 로또 구입 가격이 숫자가 아니면 에러 메시지를 출력한다.")
-    @Test
-    void initCostByNonNumber() {
+    @DisplayName("로또 게임 초기 설정 에러 메시지 테스트")
+    @ParameterizedTest(name = "입력이 {0}일 때 {1} 에러 메시지를 출력한다.")
+    @MethodSource("initSource")
+    void initErrorMessage(String[] input, LottoGameMessage message) {
         // given
-        setStdin("\n", "a", "1000", "1,2,3,4,5,6", "30");
+        setStdin("\n", input);
 
         LottoGame lottoGame = new LottoGame();
 
@@ -23,23 +29,20 @@ class LottoGameTest extends ConsoleTest {
         // then
         String stdout = getStdout();
 
-        assertThat(stdout).contains(LottoGameMessage.NON_NUMBER.getMessage());
+        assertThat(stdout).contains(message.getMessage());
     }
 
-    @DisplayName("입력 받은 로또 구입 가격가 범위를 벗어나면 에러 메시지를 출력한다.")
-    @Test
-    void initCostByOutOfRange() {
-        // given
-        setStdin("\n", "100", "1000", "1,2,3,4,5,6", "30");
-
-        LottoGame lottoGame = new LottoGame();
-
-        // when
-        lottoGame.init();
-
-        // then
-        String stdout = getStdout();
-
-        assertThat(stdout).contains(LottoGameMessage.NUMBER_OUT_OF_RANGE.getMessage());
+    public static Stream<Arguments> initSource() {
+        return Stream.of(
+                Arguments.of(new String[]{"a", "1000", "1,2,3,4,5,6", "30"}, NON_NUMBER),
+                Arguments.of(new String[]{"555", "1000", "1,2,3,4,5,6", "30"}, INVALID_COST_UNIT),
+                Arguments.of(new String[]{"1000", "a,2,3,4,5,6", "1,2,3,4,5,6"}, "30", NON_NUMBER),
+                Arguments.of(new String[]{"1000", "1,1,3,4,5,6", "1,2,3,4,5,6"}, "30", INVALID_LOTTO_NUMBERS),
+                Arguments.of(new String[]{"1000", "1,2,3,4,5", "1,2,3,4,5,6"}, "30", INVALID_LOTTO_NUMBERS),
+                Arguments.of(new String[]{"1000", "1,2,3,4,5,6,7", "1,2,3,4,5,6"}, "30", INVALID_LOTTO_NUMBERS),
+                Arguments.of(new String[]{"1000", "1,2,3,4,5,6", "a", "30"}, NON_NUMBER),
+                Arguments.of(new String[]{"1000", "1,2,3,4,5,6", "0", "30"}, NUMBER_OUT_OF_RANGE),
+                Arguments.of(new String[]{"1000", "1,2,3,4,5,6", "50", "30"}, NUMBER_OUT_OF_RANGE)
+        );
     }
 }

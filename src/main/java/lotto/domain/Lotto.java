8b1b2af23a1d1c93b13c28971aa8d262886e8ee5
seokmyungham <hellotjraud@gmail.com>
2023-11-08 13:44:16 +0900

    refactor(Lotto): 매직 넘버 상수로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.constants.Constants.*;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.function.Predicate;
@@ -35,7 +37,7 @@ public class Lotto {
     }
 
     private void validateNumberCount(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("로또 번호의 개수는 6개여야 합니다.");
         }
     }
@@ -47,7 +49,7 @@ public class Lotto {
     }
 
     protected void validateSingleNumberRange(Integer number) {
-        if (number < 1 || number > 45) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
             throw new IllegalArgumentException("로또 번호는 1부터 45사이의 숫자여야 합니다.");
         }
     }

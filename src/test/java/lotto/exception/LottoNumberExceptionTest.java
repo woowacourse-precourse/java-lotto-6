7644492c8dfb68eab46780fc9a137c65c7bb44df
ming-taro <hera002008@naver.com>
2023-11-08 22:00:04 +0900

    test: 마지막 입력이 숫자로 끝나는지, 6개의 숫자를 입력하는지 테스트

--- src/test/java/lotto/exception/LottoNumberExceptionTest.java
+++ src/test/java/lotto/exception/LottoNumberExceptionTest.java
@@ -28,11 +28,18 @@ class LottoNumberExceptionTest {
     }
 
     @Test
+    @DisplayName("마지막 입력이 숫자로 끝나지 않으면 예외가 발생 한다.")
     void checkEndWithNumber() {
+        assertThatThrownBy(() -> lottoNumberException.checkLottoNumber("1,2,3,4,5,6,"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void checkSixInputs() {
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6,7", "1,2,3", "a,b,3,4,e,f"})
+    @DisplayName("6개의 숫자를 입력 하지 않으면 예외가 발생 한다.")
+    void checkSixInputs(String input) {
+        assertThatThrownBy(() -> lottoNumberException.checkLottoNumber(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test

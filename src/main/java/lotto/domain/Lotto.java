89dd1174ec94a2a37d707817243c859858cae812
TaeDongUm <etd93@naver.com>
2023-11-08 14:16:33 +0900

    [feat] Lotto 클래스 추가, setBonusNumber, getBonusNumber, validateLottoNumber 기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import java.util.List;
+
+import lotto.util.InputLottoException;
+import lotto.view.InputView;
+
+public class Lotto {
+	private List<Integer> lottoNumbers;
+	public InputView inputView;
+	public InputLottoException inputLottoException;
+	
+	public Lotto(List<Integer> lottoNumbers) {
+		validateLottoNumbers(lottoNumbers);
+		this.lottoNumbers = lottoNumbers;
+	}
+	public void setBonusNumber(int inputBonusNumber) {
+		inputLottoException.validateInputBonusNumbers(lottoNumbers, inputBonusNumber);
+		lottoNumbers.add(inputBonusNumber);
+	}
+	public void validateLottoNumbers(List<Integer> lottoNumbers) {
+		
+	}
+	public List<Integer> getLottoNumbers(){
+		return lottoNumbers;
+	}
+	
+	public int getBonusNumber() {
+		return lottoNumbers.get(6);
+	}
+}

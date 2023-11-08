a46f59f4cebb1ebd4b0f9a0b93f4b5ae8c731299
정재웅 <jweidon0821@gmail.com>
2023-11-08 22:52:28 +0900

    refactor(domain): 불필요한 Result 클래스 제거, Winning으로 통합

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -35,7 +35,7 @@ public enum Winning {
         for (Lotto myLotto : myLottos) {
             int match = calculate(myLotto, winningLotto);
             boolean isBonus = myLotto.getNumbers().contains(bonusNumber);
-            Winning winning = Result.win(match, isBonus);
+            Winning winning = win(match, isBonus);
             resultCounter[winning.ordinal()]++;
         }
         OutputView.printWinningResult(resultCounter);
@@ -51,5 +51,14 @@ public enum Winning {
             }
         }
         return count;
+    }
+
+    public static Winning win(int match, boolean bonus) {
+        if (match == 6) return Winning.FIRST;
+        else if (match == 5 && bonus) return Winning.SECOND;
+        else if (match == 5) return Winning.THIRD;
+        else if (match == 4) return Winning.FOURTH;
+        else if (match == 3) return Winning.FIFTH;
+        else return Winning.FAIL;
     }
 }

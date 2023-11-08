694937d632f6e2f0fff2526920117b6b4838cae4
HanDaWoon <bookpia.han@gmail.com>
2023-11-08 10:15:03 +0900

    feat: 예외 발생 시 메세지 포함

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.model;
 
+import lotto.enums.ErrorMsg;
+
 import java.util.List;
 
 public class Lotto {
@@ -12,13 +14,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMsg.INVALID_LOTTO_NUMBER_COUNT.getErrMsg());
         }
         if (numbers.stream().distinct().count() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMsg.INVALID_LOTTO_NUMBER_DUPLICATE.getErrMsg());
         }
         if (numbers.stream().anyMatch(number -> number < 1 || number > 45)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMsg.INVALID_LOTTO_NUMBER_RANGE.getErrMsg());
         }
     }
 

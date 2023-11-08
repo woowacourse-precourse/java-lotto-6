036575d917118d5b71767079e52a4f613168cc65
seokmyungham <hellotjraud@gmail.com>
2023-11-08 14:31:22 +0900

    test(LottoTest): 에러 메시지를 상수로 변경

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -1,11 +1,13 @@
-package lotto;
+package lotto.domain;
 
-import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_COUNT_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessage.LOTTO_NUMBER_RANGE_ERROR_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -14,7 +16,7 @@ class LottoTest {
     void createLottoByOverSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호의 개수는 6개여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
     }
 
     @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
@@ -22,7 +24,7 @@ class LottoTest {
     void createLottoByUnderSize() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호의 개수는 6개여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_COUNT_ERROR_MESSAGE);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
@@ -30,7 +32,7 @@ class LottoTest {
     void createLottoByDuplicatedNumber() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 중복될 수 없습니다.");
+                .hasMessage(LOTTO_NUMBER_DUPLICATE_ERROR_MESSAGE);
     }
 
     @DisplayName("로또 번호가 1부터 45사이의 숫자가 아니라면 예외를 발생한다.")
@@ -38,6 +40,6 @@ class LottoTest {
     void NumbersNotInRange1To45() {
         assertThatThrownBy(() -> new Lotto(List.of(46, 2, 3, 4, 5, 6)))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("로또 번호는 1부터 45사이의 숫자여야 합니다.");
+                .hasMessage(LOTTO_NUMBER_RANGE_ERROR_MESSAGE);
     }
 }

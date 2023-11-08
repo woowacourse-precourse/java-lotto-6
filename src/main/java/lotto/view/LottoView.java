dc18d66e6deddba851d6253ada855456804d08db
dong_hy <kimadonghyon99@gmail.com>
2023-11-08 19:59:54 +0900

    feat : 콘솔 출력부분 추가

--- src/main/java/lotto/view/LottoView.java
+++ src/main/java/lotto/view/LottoView.java
@@ -1,4 +1,6 @@
-package lotto;
+package lotto.view;
+
+import lotto.domain.LottoGame;
 
 public class LottoView {
     public static void msgLottoPrice() {
@@ -17,15 +19,20 @@ public class LottoView {
         System.out.printf("%n%d개를 구매했습니다.%n", count);
     }
 
-    public static void msgLottoNum(String lottoNum) {
-        System.out.println(lottoNum);
-    }
-
-    public static void msgEarningRate(double EarningRate) {
-        System.out.printf("총 수익률은 %f입니다.%n", EarningRate);
+    public static void msgLottoNum(LottoGame lottogame) {
+        System.out.println(lottogame);
+        System.out.println();
     }
 
     public static void msgWinningStatistics() {
         System.out.printf("당첨 통계%n---%n");
+    }
+
+    public static void msgSuccess(String message, int numberOfMatch) {
+        System.out.println(message + numberOfMatch + "개");
+    }
+
+    public static void msgRevenueRate(double EarningRate) {
+        System.out.println("총 수익률은 " + String.format("%.1f", EarningRate) + "%입니다.");
     }
 }

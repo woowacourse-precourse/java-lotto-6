fc9df3318f5eed3daeff68d0b708aeee827c3e17
jjongwon7 <jjongwon7@naver.com>
2023-11-08 22:03:42 +0900

    feat: 로또 번호 오름차순 정렬 기능 추가 생성한 로또 번호를 오름차순으로 정렬하는 메서드 작성

--- src/main/java/lotto/domain/LottoStore.java
+++ src/main/java/lotto/domain/LottoStore.java
@@ -6,6 +6,7 @@ import static lotto.constant.LottoInfo.MIN_LOTTO_NUMBER;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoStore {
@@ -14,7 +15,13 @@ public class LottoStore {
         public static Lotto generateLotto() {
             List<Integer> numbers = new ArrayList<>(
                     Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT));
+            sortLottoNumberAsc(numbers);
+
             return new Lotto(numbers);
+        }
+
+        private static void sortLottoNumberAsc(List<Integer> numbers) {
+            Collections.sort(numbers);
         }
     }
 }

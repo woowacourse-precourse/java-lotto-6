f7e94e75714878095c8833ef3f17b4e355f6d42a
leorb159 <ldgsocial159@naver.com>
2023-11-08 19:57:53 +0900

    feat: LottoWinningResultSummary 생성, 총 상금 계산 메소드 작성

--- src/main/java/lotto/LottoWinningResultSummary.java
+++ src/main/java/lotto/LottoWinningResultSummary.java
@@ -1,2 +1,27 @@
-package lotto;public class LottoSummary {
+package lotto;
+
+import java.util.Map;
+
+public class LottoWinningResultSummary {
+
+    private LottoResult lottoResult;
+    private double grossProfitRate;
+
+    public LottoWinningResultSummary(LottoResult lottoResult) {
+        this.lottoResult = lottoResult;
+
+    }
+
+    private long calcTotalPrize(LottoResult lottoResult) {
+        long totalPrize = 0L;
+        Map<LottoRank, Integer> rankResult = lottoResult.getRankResult();
+        for (Map.Entry<LottoRank, Integer> rank : rankResult.entrySet()) {
+            long prize = rank.getKey().getPrize();
+            int count = rank.getValue();
+            totalPrize += prize * count;
+        }
+        return totalPrize;
+    }
+
+
 }

322a19e89e69d102de21385d84eee55ce1240442
Jung Yunho <a01049048063@gmail.com>
2023-11-08 14:33:08 +0900

    test(InputValidatorTest): 입력값 검증 테스트 작성

---
+++ src/test/java/lotto/io/input/InputValidatorTest.java
@@ -0,0 +1,66 @@
+package lotto.io.input;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+class InputValidatorTest {
+    final InputValidator inputValidator = new InputValidator();
+
+    @Test
+    void validate_Numeric() {
+        //given
+        final String input = "aa";
+
+        //when & then
+        assertThatThrownBy(() -> inputValidator.validateNumeric(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_numeric_without_exception() {
+        //given
+        final String input = "1";
+
+        //when & then
+        assertThatNoException().isThrownBy(() -> inputValidator.validateNumeric(input));
+    }
+
+    @Test
+    void validate_null() {
+        //given
+        final String input = null;
+
+        //when & then
+        assertThatThrownBy(() -> inputValidator.validateNull(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_null_without_exception() {
+        //given
+        final String input = "1";
+
+        //when & then
+        assertThatNoException().isThrownBy(() -> inputValidator.validateNull(input));
+    }
+    @Test
+    void validate_have_not_delimiter() {
+        //given
+        final String input = "1";
+
+        //when & then
+        assertThatThrownBy(() -> inputValidator.validateHasDelimiter(input)).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @Test
+    void validate_have_not_delimiter_without_exception() {
+        //given
+        final String input = "1,2,3,4,5,6";
+
+        //when & then
+        assertThatNoException().isThrownBy(() -> inputValidator.validateHasDelimiter(input));
+    }
+}

824d0b41e7a4c8c565e60c05c7c06d6e41e71654
Kim sun wook <kimsw9603@gmail.com>
2023-11-08 16:18:48 +0900

    2. LottoWinningNumber 생성자 및 validate 구현

--- src/main/java/lotto/domain/LottoWinningNumber.java
+++ src/main/java/lotto/domain/LottoWinningNumber.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class LottoWinningNumber {
+    private final List<Integer> winningNumbers;
+    private final int specialNumber;
+
+    public LottoWinningNumber(List<Integer> winningNumbers, int specialNumber) {
+        validate(winningNumbers);
+        this.winningNumbers = winningNumbers;
+        this.specialNumber = specialNumber;
+    }
+
+    private void validate(List<Integer> winningNumbers) {
+        if (winningNumbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
 }

a407fa498b1fc15ff051155ffb750425e7160779
Park Eunhyeong <silbroeh@gmail.com>
2023-11-08 11:24:08 +0900

    feat : NumberConstants클래스 - 숫자 상수 구현

---
+++ src/main/java/lotto/NumberConstants.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class NumberConstants {
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int HUNDRED = 100;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_PRICE = 1000;
+}

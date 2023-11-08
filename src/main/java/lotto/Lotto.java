90fe1291dc9e160f99c708cc9b36d02770aae87f
hee9841 <lee98417716@gmail.com>
2023-11-08 22:01:20 +0900

    feat: 유효성 검사 기능 추가
    - 시스템 전체 유효성
    - 로또 게임 관련 유효성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,9 @@
 package lotto;
 
+import constant.GameValues;
+import error.ErrorMessage;
 import java.util.List;
+import util.InputValidator;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,8 +14,13 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+        //크기 확인
+        if (!InputValidator.isValidListSize(numbers, GameValues.LOTTO_WINNING_NUM_SIZE.getValue())) {
+            ErrorMessage.INVALID_WINNING_LOTTO_NUM_VALUE.errorHandle();
+        }
+        //중복 확인 lotto만
+        if (InputValidator.hasDuplicateNum(numbers)) {
+            ErrorMessage.DUPLICATE_LOTTO_NUM.errorHandle();
         }
     }
 

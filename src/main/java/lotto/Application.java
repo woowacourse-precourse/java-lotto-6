5d10fa018e9c17cf729a9b2f71cfdd4ada955959
dabin <db021129@naver.com>
2023-11-08 16:18:51 +0900

    feat : correction

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,14 @@
 package lotto;
 
+import lotto.domain.Lottos;
+import lotto.domain.Price;
+import lotto.view.LottoGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoGame lottoGame = new LottoGame();
+
+        Price price = lottoGame.bougthPrice();
+        Lottos lottos = lottoGame.showLotto(price);
     }
 }

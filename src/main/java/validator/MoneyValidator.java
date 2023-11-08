c545d7ad3571e1f363a49cf0061ba1f92ba99826
DonghaOh <donghadarwin@naver.com>
2023-11-08 13:15:26 +0900

    feat: 금액 숫자 아니면 예외
    moneyIsNumber 메소드

--- src/main/java/validator/MoneyValidator.java
+++ src/main/java/validator/MoneyValidator.java
@@ -10,4 +10,10 @@ public class MoneyValidator {
             throw new IllegalArgumentException("[ERROR] 값을 입력해주세요.");
         }
     }
+
+    private static void moneyIsNumber(String money) {
+        if (!money.matches("[0-9]+")) {
+            throw new IllegalArgumentException("[ERROR] 양의 정수를 입력해주세요.");
+        }
+    }
 }

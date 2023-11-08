119dfff763bbf409ef69ce5e980236f9a64efaee
realisshomyang <realisshoman@kakao.com>
2023-11-08 20:56:47 +0900

    feat : 1등 복권과 같은 것을 비교하여 Enum값을 return해주는 기능 구현

--- src/test/java/lotto/LottoRewardTest.java
+++ src/test/java/lotto/LottoRewardTest.java
@@ -3,35 +3,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class LottoRewardTest {
-
     @Test
     public void testGetReward() {
-        assertEquals(LottoReward.FIRST_PRIZE, LottoReward.getReward(6, false));
-        assertEquals(LottoReward.SECOND_PRIZE, LottoReward.getReward(5, true));
-        assertEquals(LottoReward.FOURTH_PRIZE, LottoReward.getReward(4, true));
-        assertEquals(LottoReward.THIRD_PRIZE, LottoReward.getReward(5, false));
-        assertEquals(LottoReward.FOURTH_PRIZE, LottoReward.getReward(4, false));
-        assertEquals(LottoReward.FIFTH_PRIZE, LottoReward.getReward(3, false));
+        assertEquals(LottoReward.FIRST_PLACE, LottoReward.getReward(6, false));
+        assertEquals(LottoReward.SECOND_PLACE, LottoReward.getReward(5, true));
+        assertEquals(LottoReward.FOURTH_PLACE, LottoReward.getReward(4, true));
+        assertEquals(LottoReward.THIRD_PLACE, LottoReward.getReward(5, false));
+        assertEquals(LottoReward.FOURTH_PLACE, LottoReward.getReward(4, false));
+        assertEquals(LottoReward.FIFTH_PLACE, LottoReward.getReward(3, false));
         assertEquals(LottoReward.NOTHING, LottoReward.getReward(0, false));
     }
 
     @Test
     public void testGetMatchNumbersCount() {
-        assertEquals(6, LottoReward.FIRST_PRIZE.getMatchNumbersCount());
-        assertEquals(5, LottoReward.SECOND_PRIZE.getMatchNumbersCount());
-        assertEquals(5, LottoReward.THIRD_PRIZE.getMatchNumbersCount());
-        assertEquals(4, LottoReward.FOURTH_PRIZE.getMatchNumbersCount());
-        assertEquals(3, LottoReward.FIFTH_PRIZE.getMatchNumbersCount());
+        assertEquals(6, LottoReward.FIRST_PLACE.getMatchNumbersCount());
+        assertEquals(5, LottoReward.SECOND_PLACE.getMatchNumbersCount());
+        assertEquals(5, LottoReward.THIRD_PLACE.getMatchNumbersCount());
+        assertEquals(4, LottoReward.FOURTH_PLACE.getMatchNumbersCount());
+        assertEquals(3, LottoReward.FIFTH_PLACE.getMatchNumbersCount());
         assertEquals(0, LottoReward.NOTHING.getMatchNumbersCount());
     }
 
     @Test
     public void testGetWinningReward() {
-        assertEquals(2_000_000_000L, LottoReward.FIRST_PRIZE.getWinningReward());
-        assertEquals(30_000_000L, LottoReward.SECOND_PRIZE.getWinningReward());
-        assertEquals(1_500_000L, LottoReward.THIRD_PRIZE.getWinningReward());
-        assertEquals(50_000L, LottoReward.FOURTH_PRIZE.getWinningReward());
-        assertEquals(5_000L, LottoReward.FIFTH_PRIZE.getWinningReward());
+        assertEquals(2_000_000_000L, LottoReward.FIRST_PLACE.getWinningReward());
+        assertEquals(30_000_000L, LottoReward.SECOND_PLACE.getWinningReward());
+        assertEquals(1_500_000L, LottoReward.THIRD_PLACE.getWinningReward());
+        assertEquals(50_000L, LottoReward.FOURTH_PLACE.getWinningReward());
+        assertEquals(5_000L, LottoReward.FIFTH_PLACE.getWinningReward());
         assertEquals(0L, LottoReward.NOTHING.getWinningReward());
     }
+
 }

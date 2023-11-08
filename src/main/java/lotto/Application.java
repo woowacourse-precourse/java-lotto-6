96357c351fca877ec935d56ed1583da432cb00a9
seunghye218 <seunghye@student.42seoul.kr>
2023-11-08 17:29:11 +0900

    feat: implement Application

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,39 @@
 package lotto;
 
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Game;
+import lotto.domain.Lotto;
+import lotto.domain.LottoMachine;
+import lotto.domain.Rank;
+import lotto.domain.WinningLotto;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        outputView.printEnterPurchaseAmount();
+        int purchaseAmount = inputView.getPurchaseAmount();
+        List<Lotto> lottos = new LottoMachine().issueLottos(purchaseAmount);
+        outputView.printLottos(lottos);
+
+        outputView.printEnterWinningNumber();
+        List<Integer> winningNumbers = inputView.getWinningNumbers();
+
+        outputView.printEnterBonusNumber();
+        int bonusNumber = inputView.getBonusNumber();
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
+
+        Game lottoGame = new Game(lottos, winningLotto);
+        Map<Rank, Integer> result = lottoGame.run();
+        double earningRate = lottoGame.getEarningRate(result, purchaseAmount);
+
+        outputView.printResult(result, earningRate);
     }
+
+
 }

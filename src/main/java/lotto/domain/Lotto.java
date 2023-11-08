55071722f447b4fdee8a48e05e6d1a0cbd8c8d0c
kiwkim <atthenoon@gmail.com>
2023-11-08 19:14:40 +0900

    feat($Lotto): Apply `ErrorMessages` for dealing with exceptions and adjust constructor
    - The field `numbers` is assigned a new ArrayList for using Collections.sort.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,8 @@
 package lotto.domain;
 
+import lotto.constants.ErrorMessages;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 
@@ -9,21 +12,36 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         validateNumberRange(numbers);
-        Collections.sort(numbers);
-        this.numbers = numbers;
+        validNumberUnique(numbers);
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessages.ERR_INPUT_LIST_SIZE.getMessage());
         }
     }
 
     private void validateNumberRange(List<Integer> numbers) {
         for (int number : numbers) {
-            if (1 <= number && number <= 45) {
-                throw new IllegalArgumentException();
+            if (!(1 <= number && number <= 45)) {
+                throw new IllegalArgumentException(ErrorMessages.ERR_OUT_OF_BOUND.getMessage());
             }
         }
+    }
+
+    private void validNumberUnique(List<Integer> numbers) {
+        boolean[] uniqueChecker = new boolean[46];
+        for (int number : numbers){
+            if (uniqueChecker[number]) {
+                throw new IllegalArgumentException(ErrorMessages.ERR_DUPLICATED_NUMBER.getMessage());
+            }
+            uniqueChecker[number] = true;
+        }
+    }
+
+    public List<Integer> getNumbers(){
+        return numbers;
     }
 }

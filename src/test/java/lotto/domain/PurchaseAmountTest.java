46227661e5d276ce6a79773dac2fd4702669f867
jungeun-choi <jungeun5.choi@gmail.com>
2023-11-08 22:13:47 +0900

    test(PurchaseAmount): 입력한 구매 금액만큼 로또를 구매한다.

--- src/test/java/lotto/domain/PurchaseAmountTest.java
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -3,10 +3,15 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import lotto.util.ExceptionMessage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PurchaseAmountTest {
+    private static final int LOTTO_UNIT = 1000;
+
     @DisplayName("입력한 구매 금액이 2147483000 초과이면 예외가 발생한다.")
     @Test
     void inputPurchaseAmountByOverValue() {
@@ -29,5 +34,20 @@ class PurchaseAmountTest {
         assertThatThrownBy(() -> new PurchaseAmount(1111))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ExceptionMessage.INVALID_PURCHASE_UNIT.getMessage());
+    }
+
+    @DisplayName("입력한 구매 금액만큼 로또를 구매한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"97000", "528000"})
+    void purchaseLottoByInputAmount(int amount) {
+        // give
+        PurchaseAmount purchaseAmount = new PurchaseAmount(amount);
+
+        // when
+        int createdLotto = purchaseAmount.calculateLottoCount();
+
+        // then
+        int expected = amount / LOTTO_UNIT;
+        Assertions.assertEquals(expected, createdLotto);
     }
 }

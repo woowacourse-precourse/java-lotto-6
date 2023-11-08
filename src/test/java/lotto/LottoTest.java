e49232ab463b445b8378c05b62709d5b3c0fef40
beomsic <qjatjr29@khu.ac.kr>
2023-11-08 10:59:12 +0900

    :test_tube: Test: 로또 생성 테스트 추가
    - 로또 번호개수가 6개를 초과시 예외 발생
    - 로또 번호에 중복된 숫자가 있다면 예외 발생
    - 로또 번호가 주어진 범위를 벗어났다면 예외 발생
    - 로또를 생성을 하는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,23 +6,78 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+
     @Test
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> Lotto.generateLotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     void createLottoByDuplicatedNumber() {
-        // TODO: 이 테스트가 통과할 수 있게 구현 코드 작성
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> Lotto.generateLotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     // 아래에 추가 테스트 작성 가능
+
+    @Test
+    @DisplayName("로또 번호에 정해진 범위보다 큰 값이 들어오면 예외가 발생한다.")
+    void createLotto_NumberMoreThanThreshold() {
+        // given
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 46);
+
+        // when
+        // then
+        assertThatThrownBy(() -> Lotto.generateLotto(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호에 정해진 범위보다 작은 값이 들어오면 예외가 발생한다.")
+    void createLotto_NumberLessThanThreshold() {
+        // given
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 0);
+
+        // when
+        // then
+        assertThatThrownBy(() -> Lotto.generateLotto(lottoNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("로또 번호 생성 테스트")
+    void createLotto() {
+        // given
+        List<Integer> lottoNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        Lotto lotto = Lotto.generateLotto(lottoNumbers);
+
+        // then
+        assertThat(lotto).isNotNull();
+    }
+
+    @Test
+    @DisplayName("랜덤으로 로또를 생성하는 테스트")
+    void generateGandomLotto() {
+        // given
+
+        // when
+        Lotto lotto = Lotto.generateGandomLotto();
+        // then
+        assertThat(lotto).isNotNull();
+    }
 }

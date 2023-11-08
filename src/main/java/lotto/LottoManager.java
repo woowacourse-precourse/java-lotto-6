acedfc1890e65996b84b250fa96f7f76850b6ce4
choish19 <100904838+choish19@users.noreply.github.com>
2023-11-08 22:56:51 +0900

    Feat: "추가 winningAmount() 함수"
    LottoManager 클래스
    - winningAmount() 함수 추가

--- src/main/java/lotto/LottoManager.java
+++ src/main/java/lotto/LottoManager.java
@@ -11,6 +11,24 @@ public class LottoManager {
     private List<Integer> winningNumbers;
     private int bonusNumber;
 
+    enum winningType {
+        THREE(5000),
+        FOUR(50000),
+        FIVE(1500000),
+        FIVE_ONE(30000000),
+        SIX(2000000000);
+
+        private final int amount;
+
+        private winningType(int amount) {
+            this.amount = amount;
+        }
+
+        public int getAmount() {
+            return this.amount;
+        }
+    }
+
     public void inputLottoPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         purchaseAmount = Integer.getInteger(Console.readLine());
@@ -27,6 +45,7 @@ public class LottoManager {
     }
 
     public void inputLottoWinningNumbers() {
+        winningNumbers = new ArrayList<>();
         System.out.println("당첨 번호를 입력해 주세요.");
         String[] numbers = Console.readLine().split(",");
         for (String number : numbers) {
@@ -34,5 +53,24 @@ public class LottoManager {
         }
         System.out.println("보너스 번호를 입력해 주세요.");
         this.bonusNumber = Integer.parseInt(Console.readLine());
+    }
+
+    public int winningAmount(int winCnt, int bonusCnt) {
+        if(winCnt+bonusCnt == 3) {
+            return winningType.THREE.amount;
+        }
+        if(winCnt+bonusCnt == 4) {
+            return winningType.FOUR.amount;
+        }
+        if(winCnt+bonusCnt == 5) {
+            return winningType.FIVE.amount;
+        }
+        if(winCnt == 5 && bonusCnt == 1) {
+            return winningType.FIVE_ONE.amount;
+        }
+        if(winCnt == 6) {
+            return winningType.SIX.amount;
+        }
+        return 0;
     }
 }

814bc953f2c2e33f2ac77cfe944c5de463239979
Soeun Uhm <uhmturks@yonsei.ac.kr>
2023-11-08 20:46:30 +0900

    refactor : 로또 범위 테스트 parameterizedTest 로 변경

--- src/test/java/lotto/model/lotto/LottoTest.java
+++ src/test/java/lotto/model/lotto/LottoTest.java
@@ -1,10 +1,16 @@
 package lotto.model.lotto;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
 import lotto.exception.lotto.LottoOutOfRangeException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -12,21 +18,36 @@ class LottoTest {
     @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        //given
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        // when & then
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void createLottoByDuplicatedNumber() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+        //given
+        List<Integer> numbers = Arrays.asList(11, 2, 3, 4, 5, 5);
+        // when & then
+        assertThatThrownBy(() -> new Lotto(numbers))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("로또 번호가 1이상 45이하가 아니면 예외가 발생한다.")
-    @Test
-    void createLottoOutOfRange() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 47)))
+    @ParameterizedTest
+    @MethodSource("numbersOutOfRange")
+    void createLottoOutOfRange(List<Integer> numbersOutOfRange) {
+        // when & then
+        assertThatThrownBy(() -> new Lotto(numbersOutOfRange))
                 .isInstanceOf(LottoOutOfRangeException.class);
+    }
+
+    static Stream<Arguments> numbersOutOfRange(){
+        return Stream.of(
+                Arguments.of(Arrays.asList(1,2,3,4,5,46)),
+                Arguments.of(Arrays.asList(0,1,2,3,4,5))
+        );
     }
 }

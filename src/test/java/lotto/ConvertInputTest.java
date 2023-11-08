fcf12142fcff273573741035930e84ac06abc681
JoonKyo <joonkyolee@gmail.com>
2023-11-08 18:48:11 +0900

    test: 로또 번호와 보너스 번호에 대한 예외 처리가 발생하는 지 확인하는 테스트 구현

--- src/test/java/lotto/ConvertInputTest.java
+++ src/test/java/lotto/ConvertInputTest.java
@@ -2,7 +2,6 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import lotto.domain.Player;
 import lotto.util.ConvertInput;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +9,36 @@ import org.junit.jupiter.api.Test;
 public class ConvertInputTest {
     @DisplayName("구입 금액을 두 개 이상 입력하면 예외가 발생한다.")
     @Test
-    void manyValuesTest() {
+    void manyPurchaseAmountTest() {
         assertThatThrownBy(() -> ConvertInput.makePlayerMoneyToInt("1000,2000"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("구입 금액에 수가 아닌 값이 포함되면 예외가 발생한다.")
     @Test
-    void hasNotNumberTest() {
+    void AmountNumberIsNotNumberTest() {
         assertThatThrownBy(() -> ConvertInput.makePlayerMoneyToInt("1e"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호에 수가 아닌 값이 포함되면 예외가 발생한다.")
+    @Test
+    void LottoNumberHasNotNumberTest() {
+        assertThatThrownBy(() -> ConvertInput.makeLottoNumberToList("1,w,4,5,6,7"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호을 두 개 이상 입력하면 예외가 발생한다.")
+    @Test
+    void manyBonusNumberTest() {
+        assertThatThrownBy(() -> ConvertInput.makeBonusNumberToInt("1,2"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호에 수가 아닌 값이 포함되면 예외가 발생한다.")
+    @Test
+    void BonusNumberIsNotNumberTest() {
+        assertThatThrownBy(() -> ConvertInput.makeBonusNumberToInt("1e"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

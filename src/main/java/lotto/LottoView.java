e6385d1d0200112883b6c8ba0f94e3bec5f9d68d
gusgh075 <gusgh075@naver.com>
2023-11-08 11:09:37 +0900

    FIXED : inputWinningLotto()에 ERROR 메세지 추가

--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -7,14 +7,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LottoView {
-    private static final String[] RANK_MESSAGE={
+    private static final String[] RANK_MESSAGE = {
             "3개 일치 (5,000원) - ",
             "4개 일치 (50,000원) - ",
             "5개 일치 (1,500,000원) - ",
             "5개 일치, 보너스 볼 일치 (30,000,000원) - ",
             "6개 일치 (2,000,000,000원) - "};
-    public static int inputMoney(){
-        while(true) {
+
+    public static int inputMoney() {
+        while (true) {
             System.out.println("구입금액을 입력해 주세요.");
             try {
                 int money = Integer.parseInt(Console.readLine());
@@ -24,31 +25,41 @@ public class LottoView {
             }
         }
     }
-    public static Lotto inputWinningLotto(){
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String[] sNumbers = Console.readLine().split(",");
-        List<Integer> winningLottoNumber=new ArrayList<>();
-        for(int i=0;i<sNumbers.length;i++){
-            winningLottoNumber.add(Integer.parseInt(sNumbers[i]));
+
+    public static Lotto inputWinningLotto() {
+        while (true) {
+            System.out.println("당첨 번호를 입력해 주세요.");
+            try {
+                String[] sNumbers = Console.readLine().split(",");
+                List<Integer> winningLottoNumber = new ArrayList<>();
+                for (int i = 0; i < sNumbers.length; i++) {
+                    winningLottoNumber.add(Integer.parseInt(sNumbers[i]));
+                }
+                return new Lotto(winningLottoNumber);
+            }catch (NumberFormatException e){
+                System.out.println("[ERROR] 형식에 맞춰 작성해 주십시오. ex)1,2,3,4,5,6");
+            }
         }
-        return new Lotto(winningLottoNumber);
     }
-    public static int inputBonusNumber(){
+
+    public static int inputBonusNumber() {
         System.out.println("보너스 번호를 입력해 주세요.\n");
         return Integer.parseInt(Console.readLine());
     }
-    public static void showLottos(LottoRepository lottoRepository){
-        System.out.printf("%d개를 구매했습니다.\n",lottoRepository.getLottos().size());
+
+    public static void showLottos(LottoRepository lottoRepository) {
+        System.out.printf("%d개를 구매했습니다.\n", lottoRepository.getLottos().size());
         List<Lotto> lottos = lottoRepository.getLottos();
-        lottos.forEach(lotto->System.out.println(Arrays.toString(lotto.getNumbers().toArray())));
+        lottos.forEach(lotto -> System.out.println(Arrays.toString(lotto.getNumbers().toArray())));
     }
-    public static void showStatus(LottoRepository lottoRepository){
+
+    public static void showStatus(LottoRepository lottoRepository) {
         int[] ranks = lottoRepository.getRanks();
         System.out.println("당첨 통계");
         System.out.println("---");
-        for(int i=4;i>=0;i--){
-            System.out.println(RANK_MESSAGE[4-i]+ranks[i]+"개");
+        for (int i = 4; i >= 0; i--) {
+            System.out.println(RANK_MESSAGE[4 - i] + ranks[i] + "개");
         }
-        System.out.printf("총 수익률은 %.1f%%입니다.\n",lottoRepository.getProfitRate()*100);
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", lottoRepository.getProfitRate() * 100);
     }
 }

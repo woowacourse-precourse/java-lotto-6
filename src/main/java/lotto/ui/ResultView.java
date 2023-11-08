e841eef839862569e3796e11ec3c61da3675dd60
Hyunjun KIM <hihj070914@icloud.com>
2023-11-08 19:20:53 +0900

    feat: UI - printResult in ResultView - 로또 게임에 대한 결과를 출력하는 View를 구현하였습니다.

--- src/main/java/lotto/ui/ResultView.java
+++ src/main/java/lotto/ui/ResultView.java
@@ -21,6 +21,26 @@ public class ResultView {
         }
     }
 
+    private void printSingleResult(GameResult result) {
+        DecimalFormat decFormat = new DecimalFormat("###,###");
+
+        if (result.getIsBonus()) {
+            System.out.printf(RESULT_BONUS_OUTPUT_FORMAT, result.getMatchCount(),
+                    decFormat.format(result.getPrizeMoney()), result.getResultCount());
+            return;
+        }
+        System.out.printf(RESULT_OUTPUT_FORMAT, result.getMatchCount(),
+                decFormat.format(result.getPrizeMoney()), result.getResultCount());
+    }
+
+    public void printResult(GameResult[] values, double revenue) {
+        System.out.printf(RESULT_VIEW_FORMAT);
+        for (GameResult result : values) {
+            printSingleResult(result);
+        }
+        System.out.printf(REVENUE_FORMAT, revenue);
+    }
+
     public void printErrorMessage(String message) {
         System.out.printf(ERROR_MESSAGE_FORMAT, message);
     }

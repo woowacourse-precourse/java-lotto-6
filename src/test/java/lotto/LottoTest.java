58f3d78bc6d0001fc52ddf32286e15551c6c3504
Youth <boysoeng@naver.com>
2023-11-08 21:20:11 +0900

    test(Lotto): 로또 결과 확인 테스트
    - 당첨번호에 따라 결과 반환
    - 당첨되지 않았으면 Result.FAIL 반환

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -3,7 +3,12 @@ package lotto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
+import lotto.model.BonusNumber;
+import lotto.model.GameNumbers;
 import lotto.model.Lotto;
+import lotto.model.Result;
+import lotto.model.WinningNumbers;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,4 +29,99 @@ class LottoTest {
     }
 
     // 아래에 추가 테스트 작성 가능
+    @DisplayName("번호가 6개 모두 일치하면 1등이다.")
+    @Test
+    void 로또_결과_1등_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 4, 5, 6, 1));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(8));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.SIX);
+    }
+
+    @DisplayName("5개의 당첨번호와 보너스번호가 일치하면 2등이다.")
+    @Test
+    void 로또_결과_2등_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 4, 5, 6, 8));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(1));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.BONUS);
+    }
+
+    @DisplayName("번호가 5개 일치하고 보너스번호가 다르면 3등이다.")
+    @Test
+    void 로또_결과_3등_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 4, 5, 6, 11));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(8));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.FIVE);
+    }
+
+    @DisplayName("번호가 4개 일치하면 4등이다.")
+    @Test
+    void 로또_결과_4등_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(1, 2, 3, 4, 44, 43));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(8));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.FOUR);
+    }
+
+    @DisplayName("번호가 3개 일치하면 5등이다.")
+    @Test
+    void 로또_결과_5등_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 4, 15, 16, 11));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(8));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.THREE);
+    }
+
+    @DisplayName("일치하는 번호가 2개 이하면 실패다.")
+    @Test
+    void 로또_결과_실패_확인_기능() {
+        //given
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        WinningNumbers winningNumbers = new WinningNumbers(List.of(2, 3, 14, 15, 16, 11));
+        GameNumbers gameNumbers = new GameNumbers(winningNumbers);
+        gameNumbers.addBonusNumber(new BonusNumber(8));
+
+        //when
+        Result result = lotto.checkResult(gameNumbers);
+
+        //when
+        Assertions.assertThat(result).isEqualTo(Result.FAIL);
+    }
 }

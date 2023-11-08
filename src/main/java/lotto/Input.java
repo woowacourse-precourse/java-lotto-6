49bef43695226acb5db55e4f95d8745ad4a8f844
Sung wook Park <109124287+respectwo2@users.noreply.github.com>
2023-11-08 19:34:18 +0900

    refactor: 출력문을 상수로 변환해서 출력

--- src/main/java/lotto/Input.java
+++ src/main/java/lotto/Input.java
@@ -7,61 +7,52 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
-	public static int calculateLottoCount(String strUserMoney) {
-
-		int userMoney = Integer.parseInt(strUserMoney);
-
-		int lottoCount = (userMoney / 1000);
-
-		return lottoCount;
-
-	}
+	private static final String INPUT_MONEY = "구입금액을 입력해 주세요.";
+	private static final String INPUT_NUNBERS = "당첨 번호를 입력해 주세요.";
+	private static final String INPUT_BONUS = "보너스 번호를 입력해 주세요.";
 
 	public static List<Integer> numbers() {
-	    System.out.println();
-	    System.out.println("당첨 번호를 입력해 주세요.");
 
-	    while (true) {
-	        String inputNumber = Console.readLine();
-	        List<Integer> numbers = getValidNumbers(inputNumber);
-	        if (numbers != null) {
-	            return numbers;
-	        }
-	    }
+		System.out.println();
+		System.out.println(INPUT_NUNBERS);
+
+		while (true) {
+			String inputNumber = Console.readLine();
+			List<Integer> numbers = getValidNumbers(inputNumber);
+			if (numbers != null) {
+				return numbers;
+			}
+		}
 	}
 
 	private static List<Integer> getValidNumbers(String inputNumber) {
-	    String[] usernums = inputNumber.split(",");
-	    try {
-	        List<Integer> numbers = new ArrayList<>();
+		String[] usernums = inputNumber.split(",");
+		try {
+			List<Integer> numbers = new ArrayList<>();
 
-	        for (String usernum : usernums) {
-	            CheckValid.string(usernum);
-	            numbers.add(Integer.parseInt(usernum));
-	        }
+			for (String usernum : usernums) {
+				CheckValid.string(usernum);
+				numbers.add(Integer.parseInt(usernum));
+			}
+			CheckValid.numbers(numbers);
 
-	        CheckValid.numbers(numbers);
-
-	        return numbers;
-
-	    } catch (Exception e) {
-	        System.out.println(e.getMessage());
-	    }
-	    return null;
+			return numbers;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
 	}
 
 	public static int bonus(List<Integer> userNums) {
 
 		System.out.println();
-		System.out.println("보너스 번호를 입력해 주세요.");
+		System.out.println(INPUT_BONUS);
 
 		while (true) {
 			String inputBonus = Console.readLine();
 			try {
 				CheckValid.string(inputBonus);
-
 				int bonus = Integer.parseInt(inputBonus);
-
 				CheckValid.bonus(userNums, bonus);
 
 				return bonus;
@@ -72,15 +63,18 @@ public class Input {
 		}
 	}
 
-	public static String money() {
+	public static int money() {
 
-		System.out.println("구입금액을 입력해 주세요.");
+		System.out.println(INPUT_MONEY);
 
 		while (true) {
 			String inputMoney = Console.readLine();
 			try {
 				CheckValid.money(inputMoney);
-				return inputMoney;
+
+				int userMoney = Integer.parseInt(inputMoney);
+
+				return userMoney;
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
 			}

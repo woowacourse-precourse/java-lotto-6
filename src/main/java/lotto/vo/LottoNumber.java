2e31fab32980a566d7ff5fbbbbba289a00692554
minseo  jo <liging12@naver.com>
2023-11-08 15:27:58 +0900

    refactor: record로 변환 (LottoNumber)

--- src/main/java/lotto/vo/LottoNumber.java
+++ src/main/java/lotto/vo/LottoNumber.java
@@ -3,14 +3,14 @@ package lotto.vo;
 import lotto.constant.LottoConstants;
 import lotto.exception.LottoException;
 
-import java.util.Objects;
-
-public final class LottoNumber implements Comparable<LottoNumber> {
-    private final Integer value;
-
-    public LottoNumber(final Integer value) throws LottoException {
+public record LottoNumber(Integer value) implements Comparable<LottoNumber> {
+    /**
+     * @throws LottoException
+     * 
+     * 생성자에 들어온 Integer 범위가 [1, 45] 아닌 경우 예외를 던진다.
+     */
+    public LottoNumber {
         validate(value);
-        this.value = value;
     }
 
     private void validate(final Integer value) throws LottoException {
@@ -31,26 +31,5 @@ public final class LottoNumber implements Comparable<LottoNumber> {
     @Override
     public String toString() {
         return value.toString();
-    }
-
-    public Integer value() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        LottoNumber other = (LottoNumber) obj;
-        return value.equals(other.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
     }
 }

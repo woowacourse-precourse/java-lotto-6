77a39ac2ab57058a5eef0ce84a7d7536a8cea926
seungjaejeon <wjstmdwo2000@naver.com>
2023-11-08 16:20:09 +0900

    feat: 입력 메시지를 출력하는 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,2 +1,18 @@
-package lotto.view;public class InputView {
+package lotto.view;
+
+import lotto.constant.InputMessage;
+
+public class InputView {
+    public void inputPurchaseAmountView() {
+        System.out.println(InputMessage.INPUT_PURCHASE_AMOUNT_MESSAGE.getMessage());
+    }
+
+    public void inputLottoNumbersView() {
+        System.out.println(InputMessage.INPUT_LOTTO_NUMBERS_MESSAGE.getMessage());
+    }
+
+    public void inputBonusNumberView() {
+        System.out.println(InputMessage.INPUT_BONUS_NUMBER_MESSAGE.getMessage());
+    }
+
 }

86653a6dca80c9d5eba3162db9606731265d1450
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2023-11-08 21:42:14 +0900

    feat : 객체 생성 없이 바로 쓸 수 있게 static으로 변경

--- src/main/java/lotto/util/RandomNumberUtil.java
+++ src/main/java/lotto/util/RandomNumberUtil.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomNumberUtil {
     
-    public List<Integer> getRandomNumbers() {
+    public static List<Integer> getRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }

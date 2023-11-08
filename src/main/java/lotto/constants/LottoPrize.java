8b9a9bffe8cb09049db33ce72b93757604e8794b
Seol_JY <wlsdud5654@gmail.com>
2023-11-08 16:00:25 +0900

    refactor(LottoPrize): 가독성 향상을 위해 상금에 언더바 표현 적용

--- src/main/java/lotto/constants/LottoPrize.java
+++ src/main/java/lotto/constants/LottoPrize.java
@@ -3,11 +3,11 @@ package lotto.constants;
 import java.util.Comparator;
 
 public enum LottoPrize {
-    FIFTH_PLACE(3, false, 5000),
-    FOURTH_PLACE(4, false, 50000),
-    THIRD_PLACE(5, false, 1500000),
-    SECOND_PLACE(5, true, 30000000),
-    FIRST_PLACE(6, false, 2000000000);
+    FIFTH_PLACE(3, false, 5_000),
+    FOURTH_PLACE(4, false, 50_000),
+    THIRD_PLACE(5, false, 1_500_000),
+    SECOND_PLACE(5, true, 30_000_000),
+    FIRST_PLACE(6, false, 2_000_000_000);
 
     private final int matchingNumbers;
     private final boolean hasMatchingBonusNumber;

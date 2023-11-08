9e2fff63f017b6d7bb4561b84310b33b66490908
JongMinCh0i <wonderland314592@gmail.com>
2023-11-08 23:20:25 +0900

    fix(InputView): 문자열 형식 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,16 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    private static final String ASK_PURCHASE_MESSAGE = "구입금액을 입력해 주세요.";
-    private static final String ASK_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
-    private static final String ASK_BONUS_WINNING_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
-    private static final String START_OF_SENTENCE_CHARACTER = "[ERROR] 문장은 첫번째 문자를 숫자 외 허용하지 않습니다.";
-    private static final String END_OF_SENTENCE_CHARACTER = "[ERROR] 문장은 마지막 문자를 숫자 외 허용하지 않습니다.";
-    public static final String PERMIT_ONLY_NUMBER = "[ERROR] 정수 외에는 입력받을 수 없습니다.";
-    public static final String NO_PERMIT_BLANK = "[ERROR] 공백은 입력될 수 없습니다.";
-    public static final String NO_PERMIT_INPUT_LESS_ZERO = "[ERROR] 0 이하의 값은 입력받을 수 없습니다.";
-    public static final String INDEX_OUT_OF_INPUT_LENGTH = "[ERROR] index 가 문자열의 범위를 벗어났습니다.";
-
     public int askPrice() {
         try {
             String input = printHowManyPurchase();
@@ -51,13 +41,13 @@ public class InputView {
 
     public void validateLastCharacter(String input) {
         if (!Character.isDigit(getCharacterAtIndex(input, input.length() - 1))) {
-            throw new IllegalArgumentException(END_OF_SENTENCE_CHARACTER);
+            throw new IllegalArgumentException("[ERROR] 문장은 마지막 문자를 숫자 외 허용하지 않습니다.");
         }
     }
 
     public void validateFirstCharacter(String input) {
         if (!Character.isDigit(getCharacterAtIndex(input, 0))) {
-            throw new IllegalArgumentException(START_OF_SENTENCE_CHARACTER);
+            throw new IllegalArgumentException("[ERROR] 문장은 첫번째 문자를 숫자 외 허용하지 않습니다.");
         }
     }
 
@@ -65,21 +55,21 @@ public class InputView {
         if (index >= 0 && index < input.length()) {
             return input.charAt(index);
         }
-        throw new IndexOutOfBoundsException(INDEX_OUT_OF_INPUT_LENGTH);
+        throw new IndexOutOfBoundsException("[ERROR] index 가 문자열의 범위를 벗어났습니다.");
     }
 
     private String printWinningNumber() {
-        System.out.println(ASK_WINNING_NUMBER_MESSAGE);
+        System.out.println("당첨 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     private String printBonusWinningNumber() {
-        System.out.println(ASK_BONUS_WINNING_NUMBER_MESSAGE);
+        System.out.println("보너스 번호를 입력해 주세요.");
         return Console.readLine();
     }
 
     private String printHowManyPurchase() {
-        System.out.println(ASK_PURCHASE_MESSAGE);
+        System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
 
@@ -87,19 +77,19 @@ public class InputView {
         try {
             return Integer.parseInt(input);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(PERMIT_ONLY_NUMBER);
+            throw new IllegalArgumentException("[ERROR] 정수 외에는 입력받을 수 없습니다.");
         }
     }
 
     private void validateBlankAndEmptyInteger(String input) {
         if (input.isBlank() || input.isEmpty() || input.contains(" ")) {
-            throw new IllegalArgumentException(NO_PERMIT_BLANK);
+            throw new IllegalArgumentException("[ERROR] 공백은 입력될 수 없습니다.");
         }
     }
 
     private int validateNegativeIntegerAndZero(int input) {
         if (input <= 0) {
-            throw new IllegalArgumentException(NO_PERMIT_INPUT_LESS_ZERO);
+            throw new IllegalArgumentException("[ERROR] 0 이하의 값은 입력받을 수 없습니다.");
         }
         return input;
     }

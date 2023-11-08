40c5d86105eb5479830008dd70b8968c31c797c1
chooh <chocomilk66@naver.com>
2023-11-08 22:45:23 +0900

    fix: 재입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,13 +11,23 @@ public class InputView {
 
     public static int inputMoney() {
         System.out.println("구입할 금액을 입력해 주세요.");
-        try {
-            return Integer.parseInt(Console.readLine());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("[ERROR] 금액은 숫자로 입력해주세요.");
-        } finally {
-            System.out.println();
+        while (true) {
+            try {
+                return checkInputMoney(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println();
+            }
         }
+    }
+
+    public static int checkInputMoney(String input) {
+        int inputMoney = 0;
+        try {
+            inputMoney = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(System.lineSeparator() + "[ERROR] 금액은 숫자로 입력해주세요.");
+        }
+        return inputMoney;
     }
 
     public static List<Integer> inputWinningLotto() {

74ecf70bb7bb3edd7e2c0f739a96f4cfb5527a00
wonndrive <nonyok812@gmail.com>
2023-11-08 23:50:32 +0900

    feat: 메시지 상수 입력

---
+++ src/main/java/lotto/constant/MessageConstants.java
@@ -0,0 +1,23 @@
+package lotto.constant;
+
+public class MessageConstants {
+    public static final String ERROR = "[ERROR] ";
+    public static final String NUMBER_ERROR_MESSAGE = "숫자만 입력 가능합니다.";
+    public static final String NUMBER_RANGE_ERROR_MESSAGE = "1부터 45 사이의 숫자만 입력 가능합니다.";
+    public static final String NUMBER_SIZE_ERROR_MESSAGE = "당첨 번호는 총 6개 입력 가능합니다.";
+    public static final String NUMBER_OVERLAP_ERROR_MESSAGE = "당첨 번호는 중복될 수 없습니다.";
+    public static final String NUMBER_MIN_ERROR_MESSAGE = "1000원 이상 입력 가능합니다";
+    public static final String NUMBER_UNIT_ERROR_MESSAGE = "1000원 단위로 입력 가능합니다.";
+
+    public static final String INPUT_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
+    public static final String INPUT_LOTTO_NUMBER = "\n당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+
+    public static final String LOTTO_AMOUNT_MESSAGE = "개를 구매했습니다.";
+    public static final String WINNING_RESULT_MESSAGE = "당첨 통계";
+    public static final String DIVIDE_MESSAGE = "---";
+
+    public static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.%n";
+
+
+}

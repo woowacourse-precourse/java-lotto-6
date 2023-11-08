2a312c5f63218ebd1f6b4a3759a7402fa371b1d8
mayy24th <86235986+mayy24th@users.noreply.github.com>
2023-11-08 19:18:21 +0900

    refactor: 사용하지 않는 import 제거

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,9 +1,6 @@
 package lotto;
 
-import java.util.ArrayList;
-import java.util.List;
 import lotto.controller.Controller;
-import lotto.domain.Rank;
 
 public class Application {
     public static void main(String[] args) {

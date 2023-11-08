5b8cd3c103282c9879f94167170c84499d45e7c2
jh <pv104@naver.com>
2023-11-08 23:05:41 +0900

    fix: 값 변경 Enum class 내의 SECOND 값을 7에서 0으로 변경

--- src/main/java/lotto/model/Rankings.java
+++ src/main/java/lotto/model/Rankings.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 public class Rankings {
     public enum RankingData {
 
-        FIRST("6개 일치 (2,000,000,000원)",6, 2000000000),
-        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원)",7, 30000000),
-        THIRD("5개 일치 (1,500,000원)", 5, 1500000),
-        FOURTH("4개 일치 (50,000원)", 4, 50000),
-        FIFTH("3개 일치 (5,000원)", 3, 5000);
+        FIRST("6개 일치 (2,000,000,000원) - ",6, 2000000000),
+        SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - ",0, 30000000),
+        THIRD("5개 일치 (1,500,000원) - ", 5, 1500000),
+        FOURTH("4개 일치 (50,000원) - ", 4, 50000),
+        FIFTH("3개 일치 (5,000원) - ", 3, 5000);
         final String rankings;
         final int values;
         final int prizes;

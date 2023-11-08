eb932b0d748e90659a2d35cfae372b7e749e161f
pjaehyun <show6756@gmail.com>
2023-11-08 14:01:00 +0900

    feat: 추가 당첨 번호 입력 함수

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -2,7 +2,11 @@ package lotto;
 
 import util.InputUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static validate.InputValidation.*;
+import static validate.InputValidation.validateLottoNumberRange;
 
 
 public class Application {
@@ -10,6 +14,7 @@ public class Application {
         // TODO: 프로그램 구현
         int purchaseAmount = getInputToPurchaseAmount();
 
+        List<Integer> resultNumbers = getInputToResultNumbers();
     }
 
     private static int getInputToPurchaseAmount() {
@@ -33,5 +38,34 @@ public class Application {
         return Integer.parseInt(input);
     }
 
+    private static List<Integer> getInputToResultNumbers() {
+        List<Integer> list = new ArrayList<>();
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                String input = InputUtil.getUserInput("당첨 번호를 입력해 주세요.\n");
+                list = validateAndGetResultNumbers(input);
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return list;
+    }
+
+    private static List<Integer> validateAndGetResultNumbers(String input) {
+        List<Integer> list = new ArrayList<>();
+        String[] numbers = input.split(",");
+        for (String number : numbers) {
+            validateNumericInput(number);
+            validateNonZeroInput(number);
+
+            int lottoNumber = Integer.parseInt(number.trim());
+            validateLottoNumberRange(lottoNumber);
+            list.add(lottoNumber);
+        }
+        return list;
+    }
 
 }

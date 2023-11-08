4194b99ccad4542f0c1e0119f7f3adffc17fa4e0
LooanCheong <jjyejun@naver.com>
2023-11-08 18:35:31 +0900

    <test>번호 6개 초과시 예외 발생 테스트 추가

---
+++ src/test/java/lotto/domain/LottoTest.java
@@ -0,0 +1,22 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.stream.Stream;
+import lotto.Lotto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class LottoTest {
+    @DisplayName("로또 번호의 개수 6개 초과시 예외 발생")
+    @Test
+    void createLottoByOverSize() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}

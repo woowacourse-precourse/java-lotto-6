ff3b12ca89c97a1c7ffa8e629cf1ce0ae204f017
kmw2378 <rlarla677@gmail.com>
2023-11-08 21:46:25 +0900

    feat(lotto): Rewards.getCountFromRank() 메서드 예외 처리코드 추가 long 자료형이 int로 파싱이 불가능한 경우 예외 처리코드를 추가하였습니다.

--- src/main/java/lotto/domain/Rewards.java
+++ src/main/java/lotto/domain/Rewards.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import lotto.exception.StateException;
 import java.util.List;
+
+import static lotto.exception.errorcode.StateErrorCode.FAIL_PARSE_LONG_TO_INT;
 
 public class Rewards {
     private final List<Reward> rewards;
@@ -17,8 +20,13 @@ public class Rewards {
     }
 
     public int getCountFromRank(final int rank) {
-        return (int) rewards.stream()
-                .filter(r -> r.getRank() == rank)
-                .count();
+        try {
+            final long count = rewards.stream()
+                    .filter(r -> r.getRank() == rank)
+                    .count();
+            return Math.toIntExact(count);
+        } catch (ArithmeticException e) {
+            throw new StateException(FAIL_PARSE_LONG_TO_INT);
+        }
     }
 }

3f2b6adf95489f73a9f3ef56e1d7f881f182b0c7
SeoYoung <seoyoung7623@naver.com>
2023-11-10 01:16:29 +0900

    feat: 결과 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,16 +1,32 @@
 package lotto.view;
 
+import lotto.model.Lotto;
 import lotto.model.Number;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class OutputView {
-    public void printBoughtLotto(int count, List<Number> buyedLotto){
+    public void printTicketCount(int count){
         System.out.println(count + "개를 구매했습니다.");
-        for(Number number:buyedLotto){
-            System.out.println(number.getNumberList());
-        }
+    }
+    public void printLottoNumber(List<Integer> lotto){
+        System.out.println(lotto);
+    }
+
+    public static void printSuccessMessage(String message, int numberOfMatch){
+        System.out.println(message + numberOfMatch + "개");
+
+    }
+
+    public static void printRevenueRate(double EarningRate) {
+        System.out.println("총 수익률은 " + String.format("%.1f", EarningRate) + "%입니다.");
+    }
+
+    public static void printSuccessResult(){
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
     }
 
 }

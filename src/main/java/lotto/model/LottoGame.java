685674fa632e5bc56e04db0dde88252334007cd1
LEE-sh1673 <lsh901673@gmail.com>
2023-11-09 18:56:12 +0900

    refactor: LottoGame에 대한 리팩토링 진행
    생성자의 매개 변수들을 통해 바로 필요한 데이터 구조를 계산하도록 생성자 내의 로직을 수정하였습니다.

--- src/main/java/lotto/model/LottoGame.java
+++ src/main/java/lotto/model/LottoGame.java
@@ -2,32 +2,30 @@ package lotto.model;
 
 import java.util.EnumMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 public class LottoGame {
 
-    private final WinningNumber winningNumber;
-
-    private final List<Lotto> lottoTickets;
+    private final EnumMap<LottoRank, Long> rankFrequencies;
 
     public LottoGame(final WinningNumber winningNumber, final List<Lotto> lottoTickets) {
-        this.winningNumber = winningNumber;
-        this.lottoTickets = lottoTickets;
+        this.rankFrequencies = calculateRankFrequencies(winningNumber, lottoTickets);
     }
 
     public WinningResult play() {
-        final Map<LottoRank, Long> frequencies = calculateRankFrequencies();
         final WinningResult winningResult = new WinningResult();
 
-        for (final LottoRank rank : frequencies.keySet()) {
-            winningResult.addItem(WinningSummary.from(rank, frequencies.get(rank)));
+        for (final LottoRank rank : rankFrequencies.keySet()) {
+            winningResult.addItem(WinningSummary.from(rank, rankFrequencies.get(rank)));
         }
         return winningResult;
     }
 
-    private Map<LottoRank, Long> calculateRankFrequencies() {
-        final Map<LottoRank, Long> frequencies = new EnumMap<>(LottoRank.class);
+    private EnumMap<LottoRank, Long> calculateRankFrequencies(
+            final WinningNumber winningNumber,
+            final List<Lotto> lottoTickets
+    ) {
+        final EnumMap<LottoRank, Long> frequencies = new EnumMap<>(LottoRank.class);
         Stream.of(LottoRank.values()).forEach(rank -> frequencies.put(rank, 0L));
 
         for (final Lotto ticket : lottoTickets) {

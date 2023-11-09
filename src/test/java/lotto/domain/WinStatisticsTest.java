439693b4bf0e5a1e054d1504ecb0c2223e536d09
K-KY <kyuyoungk@naver.com>
2023-11-09 12:09:09 +0900

    Test(WinStatisticsTest) : 등수 테스트

--- src/test/java/lotto/domain/WinStatisticsTest.java
+++ src/test/java/lotto/domain/WinStatisticsTest.java
@@ -8,8 +8,20 @@ import org.junit.jupiter.api.Test;
 public class WinStatisticsTest {
 
     @Test
-    void 결과_출력_테스트() {
-        String result = WinStatistics.getStatisticsString(List.of(3,0,0,0,0,0,0,0), 5000);
+    void 결과_출력_테스트_5등() {
+        String result = WinStatistics.getStatisticsString(List.of(3, 0, 0, 0, 0, 0, 0, 0), 5000);
         assertThat(result).contains("62.5%");
+    }
+
+    @Test
+    void 결과_출력_테스트_2등() {
+        String result = WinStatistics.getStatisticsString(List.of(7, 0, 0, 0, 0, 0, 0, 0), 30000000);
+        assertThat(result).contains("30,000,000");
+    }
+
+    @Test
+    void 결과_출력_테스트_1등() {
+        String result = WinStatistics.getStatisticsString(List.of(7, 0, 0, 0, 0, 0, 0, 0), 200000000);
+        assertThat(result).contains("2,000,000,000");
     }
 }

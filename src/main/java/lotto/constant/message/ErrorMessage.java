63f83b2b8aa2952dfdd62c35f71b63ef32db9a83
jdohun <wjdehgns97@naver.com>
2023-11-09 18:53:16 +0900

    Refactor: 사용하지 않는 상수 제거, 에러메시지 접두사를 따로 정의

--- src/main/java/lotto/constant/message/ErrorMessage.java
+++ src/main/java/lotto/constant/message/ErrorMessage.java
@@ -1,13 +1,14 @@
 package lotto.constant.message;
 
 public final class ErrorMessage {
-    public static final String ERROR_NON_EXISTENT_VALUE = "[ERROR] 값이 존재하지 않습니다.\n";
-    public static final String ERROR_NOT_NUMERIC_VALUE = "[ERROR] 숫자로만 이루어진 값을 입력해야 합니다.\n";
-    public static final String ERROR_INDIVISIBLE_BY_1000 = "[ERROR] 1,000원 단위의 금액을 입력해야 합니다.\n";
-    public static final String ERROR_OUT_OF_RANGES = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.\n";
-    public static final String ERROR_COUNT_OF_VALUES = "[ERROR] 6개의 숫자를 입력해야합니다.\n";
-    public static final String ERROR_CONTAIN_NOT_NUMERIC_VALUE = "[ERROR] 쉼표(,) 전후에 숫자가 아닌 값이 포함되어있습니다.\n";
-    public static final String ERROR_CONTAIN_CONSECUTIVE_COMMAS = "[ERROR] 쉼표(,) 전후에 숫자 값을 입력해야 합니다.\n";
-    public static final String ERROR_HAS_DUPLICATE_NUMBERS = "[ERROR] 중복되는 숫자가 존재합니다.\n";
-    public static final String ERROR_CONTAIN_BONUS_NUMBER_IN_WINNING_NUMBER = "[Error] 당첨 번호에 포함되지 않은 숫자를 입력해야 합니다.";
+    public static final String ERROR_MESSAGE_PREFIX = "[ERROR] ";
+
+    public static final String ERROR_NON_EXISTENT_VALUE = ERROR_MESSAGE_PREFIX + "값이 존재하지 않습니다.\n";
+    public static final String ERROR_NOT_NUMERIC_VALUE = ERROR_MESSAGE_PREFIX + "숫자로만 이루어진 값을 입력해야 합니다.\n";
+    public static final String ERROR_INDIVISIBLE_BY_1000 = ERROR_MESSAGE_PREFIX + "1,000원 단위의 금액을 입력해야 합니다.\n";
+    public static final String ERROR_OUT_OF_RANGES = ERROR_MESSAGE_PREFIX + "로또 번호는 1부터 45 사이의 숫자여야 합니다.\n";
+    public static final String ERROR_COUNT_OF_VALUES = ERROR_MESSAGE_PREFIX + "6개의 숫자를 입력해야합니다.\n";
+    public static final String ERROR_CONTAIN_CONSECUTIVE_COMMAS = ERROR_MESSAGE_PREFIX + "쉼표(,) 전후에 숫자 값을 입력해야 합니다.\n";
+    public static final String ERROR_HAS_DUPLICATE_NUMBERS = ERROR_MESSAGE_PREFIX + "중복되는 숫자가 존재합니다.\n";
+    public static final String ERROR_CONTAIN_BONUS_NUMBER_IN_WINNING_NUMBER = ERROR_MESSAGE_PREFIX + "당첨 번호에 포함되지 않은 숫자를 입력해야 합니다.";
 }

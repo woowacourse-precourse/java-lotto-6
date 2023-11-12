8b1e6642bdbd3a85214add658f4a751476019f08
jdohun <wjdehgns97@naver.com>
2023-11-12 16:02:29 +0900

    Refactor: 사용하지 않는 제공 메서드 제거

--- src/test/java/lotto/domain/model/LotteriesTest.java
+++ src/test/java/lotto/domain/model/LotteriesTest.java
@@ -17,23 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LotteriesTest {
 
-    private static List<List<Integer>> provideListsForProduceStatics() {
-        return Arrays.asList(
-                Arrays.asList(1, 2, 3, 4, 5, 6),
-                Arrays.asList(1, 2, 3, 4, 5, 7),
-                Arrays.asList(1, 2, 3, 4, 8, 7),
-                Arrays.asList(1, 2, 3, 9, 8, 7),
-                Arrays.asList(1, 2, 3, 9, 8, 7)
-        );
-    }
-
-    private static Stream<Arguments> provideObjects() {
-        // 여기에서 객체를 생성하고 Arguments로 래핑하여 반환
-        return Stream.of(
-                Arguments.of(new WinningNumber(Arrays.asList(1, 2, 3, 4, 5, 6)), new BonusNumber(7))
-        );
-    }
-
     @DisplayName("당첨 통계가 반환되는지 확인한다.")
     @Test
     void produceStatistics() {

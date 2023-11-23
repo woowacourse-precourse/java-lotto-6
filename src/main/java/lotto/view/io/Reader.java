14cca9a435002d22b1f7adc99bf5e02429e79ae4
zangsu <zangsu_@naver.com>
2023-11-23 14:24:51 +0900

    feat : Reader에서 정수, 정수 리스트 입력 구현

---
+++ src/main/java/lotto/view/io/Reader.java
@@ -0,0 +1,37 @@
+package lotto.view.io;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+
+public class Reader {
+    public int readInteger() {
+        String input = Console.readLine();
+        return parseInt(input);
+    }
+
+    public List<Integer> getIntegers(String delimiter) {
+        String input = Console.readLine();
+        validateNotEndDelimiter(input, delimiter);
+        return Arrays.stream(input.split(delimiter))
+                .map(this::parseInt)
+                .toList();
+    }
+
+    private void validateNotEndDelimiter(String input, String delimiter) {
+        if (input.substring(input.length() - 1).equals(delimiter)) {
+            //todo
+            throw new IllegalArgumentException("구분자로 끝나면 안됨");
+        }
+    }
+
+    private int parseInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            //todo
+            throw new IllegalArgumentException();
+        }
+    }
+
+}

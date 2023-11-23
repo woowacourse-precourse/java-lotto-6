ee8d34db1beb61c27342de5eed075356ccf6f81e
Hugh <lee2317884@gmail.com>
2023-11-23 15:52:22 +0900

    feat: 도메인 공통 상수 관리

--- src/main/java/lotto/domain/constant/Constant.java
+++ src/main/java/lotto/domain/constant/Constant.java
@@ -3,7 +3,12 @@ package lotto.domain.constant;
 public enum Constant {
 
     ZERO(0),
-    ALLOWED_MAXIMUM_AMOUNT(100_000);
+    PRICE_PER_LOTTO(1000),
+    ALLOWED_MAXIMUM_AMOUNT(100_000),
+    ALLOWED_MINIMUM_NUMBER(1),
+    ALLOWED_MAXIMUM_NUMBER(45),
+    ALLOWED_SIZE_PER_LOTTO(6);
+
 
     private final int constant;
 

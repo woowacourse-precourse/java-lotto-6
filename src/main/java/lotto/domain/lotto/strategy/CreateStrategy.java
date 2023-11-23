fdd017f8d43082db5427992eda5d86d440aceff9
Hugh <lee2317884@gmail.com>
2023-11-23 15:50:11 +0900

    feat: 1~45 까지의 중복되지 않는 랜덤한 6개 숫자 생성

---
+++ src/main/java/lotto/domain/lotto/strategy/CreateStrategy.java
@@ -0,0 +1,8 @@
+package lotto.domain.lotto.strategy;
+
+import lotto.domain.lotto.Lotto;
+
+public interface CreateStrategy {
+
+    Lotto createLotto();
+}
---
+++ src/main/java/lotto/domain/lotto/strategy/RandomCreateStrategy.java
@@ -0,0 +1,21 @@
+package lotto.domain.lotto.strategy;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.domain.constant.Constant;
+import lotto.domain.lotto.Lotto;
+
+public class RandomCreateStrategy implements CreateStrategy {
+
+    private List<Integer> create() {
+        return Randoms.pickUniqueNumbersInRange(
+                Constant.ALLOWED_MINIMUM_NUMBER.getConstant(),
+                Constant.ALLOWED_MAXIMUM_NUMBER.getConstant(),
+                Constant.ALLOWED_SIZE_PER_LOTTO.getConstant()
+        );
+    }
+
+    public Lotto createLotto() {
+        return new Lotto(create());
+    }
+}

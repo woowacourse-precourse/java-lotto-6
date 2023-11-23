9baf62c70a466c3c341b6f087da7d4fb85e8f3bb
zangsu <zangsu_@naver.com>
2023-11-23 14:37:13 +0900

    feat : 재입력 예외 핸들러 구현

---
+++ src/main/java/lotto/exception/RetryExceptionHandler.java
@@ -0,0 +1,16 @@
+package lotto.exception;
+
+import java.util.function.Supplier;
+
+public class RetryExceptionHandler {
+    public <T> T get(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                //todo Printer 사용하기
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}

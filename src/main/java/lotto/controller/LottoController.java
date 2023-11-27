3269272cd53eb8f3b0c6acbc32d61ab5a558d7ce
jinhyeon <tiemo0708@naver.com>
2023-11-28 02:06:19 +0900

    feat: 로또 금액을 받을때 숫자 이외의 값이 들어 오면 예외처리후 다시 값을 받는 기능

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,4 +1,16 @@
 package lotto.controller;
 
+import lotto.model.LottoNumberMaker;
+import lotto.view.LottoInput;
+
 public class LottoController {
+    private static LottoNumberMaker lottoNumberMaker = new LottoNumberMaker();
+    private static LottoInput lottoInput = new LottoInput();
+    public static void setPrice() {
+        lottoNumberMaker.checkInt();
+    }
+
+    public String askPrice() {
+        return lottoInput.askPrice();
+    }
 }
--- src/main/java/lotto/model/LottoNumberMaker.java
+++ src/main/java/lotto/model/LottoNumberMaker.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static lotto.model.constants.ErrorMessage.ISNOTINTEGER;
+
 public class LottoNumberMaker {
     private static int buyPrice;
     private static final List<List<Integer>> numbers = new ArrayList<>();
@@ -16,15 +18,36 @@ public class LottoNumberMaker {
     public static int getBuyPrice() {
         return buyPrice;
     }
-    public  void makeLottoNumber(Integer count){
+
+    public void makeLottoNumber(Integer count) {
         buyPrice = count;
-        IntStream.range(0,  count/1000)
+        IntStream.range(0, count / 1000)
                 .forEach(i -> numbers.add(makeRandomNumber()));
     }
+
     private List<Integer> makeRandomNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6).stream()
                 .sorted()
                 .collect(Collectors.toList());
     }
 
+    public void checkInt() {
+        while (true) {
+            try {
+                int number = checkCount();
+                makeLottoNumber(number);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ISNOTINTEGER.getMessage());
+            }
+        }
+    }
+
+    private int checkCount() {
+        try {
+            return Integer.parseInt(lottoController.askPrice());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ISNOTINTEGER.getMessage());
+        }
+    }
 }

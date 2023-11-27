dbfa3722f63bf42266f20ee9bcdcbc0830a3c09f
jinhyeon <tiemo0708@naver.com>
2023-11-28 01:40:44 +0900

    feat: 로또 구매 횟수 만큼 생성하는 메서드 추가

--- src/main/java/lotto/model/LottoNumberMaker.java
+++ src/main/java/lotto/model/LottoNumberMaker.java
@@ -1,4 +1,24 @@
 package lotto.model;
 
+import lotto.controller.LottoController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class LottoNumberMaker {
+    private static int buyPrice;
+    private static final List<List<Integer>> numbers = new ArrayList<>();
+    private static final LottoController lottoController = new LottoController();
+
+    public static int getBuyPrice() {
+        return buyPrice;
+    }
+    public  void makeLottoNumber(Integer count){
+        buyPrice = count;
+        IntStream.range(0,  count/1000)
+                .forEach(i -> numbers.add(makeRandomNumber()));
+    }
+
+
 }

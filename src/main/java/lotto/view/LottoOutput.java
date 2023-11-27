5a8d4835311d5604ac5b159f15ff3715c4f2fc50
jinhyeon <tiemo0708@naver.com>
2023-11-28 03:21:29 +0900

    feat: 로또 보너스 번호를 입력받는 메서드 추가

--- src/main/java/lotto/view/LottoOutput.java
+++ src/main/java/lotto/view/LottoOutput.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 import static lotto.view.ConstantsMessage.*;
@@ -10,6 +12,12 @@ public class LottoOutput {
         printNewLine();
         System.out.println(lottoNumber.size()+BUY_RESULT_MESSAGE.getMessage());
         lottoNumber.forEach(System.out::println);
+    }
+    public Integer bonusNumberInput() {
+        printNewLine();
+        System.out.println(ASK_BONUS_NUMBER.getMessage());
+        String input = Console.readLine();
+        return Integer.parseInt(input);
     }
     private void printNewLine() {
         System.out.println();

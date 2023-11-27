2e2f60d84f353eeddaa3e6840c4a14751a038f9f
jinhyeon <tiemo0708@naver.com>
2023-11-28 01:57:29 +0900

    fix: 클래스명 첫글자 소문자에서 대문자로 변경

--- src/main/java/lotto/view/LottoInput.java
+++ src/main/java/lotto/view/LottoInput.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static lotto.view.ConstantsMessage.ASK_BUY_PRICE;
 
-public class lottoInput {
+public class LottoInput {
     public String askPrice() {
         System.out.println(ASK_BUY_PRICE.getMessage());
         String input = Console.readLine();

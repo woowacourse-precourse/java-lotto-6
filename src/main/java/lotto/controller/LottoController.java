a88d7b6e793475bfc848c0dbd9c739099126a82e
jinhyeon <tiemo0708@naver.com>
2023-11-28 03:22:39 +0900

    feat: 로또 당첨 등수를 기록하는 메서드 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -19,7 +19,7 @@ public class LottoController {
 
     public static void setPrizeNumberInput() {
         Lotto lotto = new Lotto(lottoInput.prizeNumberInput());
-
+        lotto.checkSame(lottoView.bonusNumberInput(),randomIntGenerator.getLottoNumber());
     }
 
     public String askPrice() {

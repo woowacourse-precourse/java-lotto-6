53b4dc6dee09e8e681b3fbaf302d8725ff7bd188
jinhyeon <tiemo0708@naver.com>
2023-11-28 02:19:18 +0900

    feat: 구매한 로또의 개수와 번호를 출력하는 메서드 추가

--- src/main/java/lotto/view/LottoOutput.java
+++ src/main/java/lotto/view/LottoOutput.java
@@ -1,7 +1,16 @@
 package lotto.view;
 
+import java.util.List;
+
+import static lotto.view.ConstantsMessage.*;
+
 public class LottoOutput {
 
+    public void buyLottoNumberPrint(List<List<Integer>> lottoNumber) {
+        printNewLine();
+        System.out.println(lottoNumber.size()+BUY_RESULT_MESSAGE.getMessage());
+        lottoNumber.forEach(System.out::println);
+    }
     private void printNewLine() {
         System.out.println();
     }

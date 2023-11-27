976c8c14d3162e192df42b97177322e784317b3d
jinhyeon <tiemo0708@naver.com>
2023-11-28 01:45:53 +0900

    feat: 로또 구매가격을 받는 메서드 추가

--- src/main/java/lotto/view/lottoInput.java
+++ src/main/java/lotto/view/lottoInput.java
@@ -1,5 +1,14 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static lotto.view.ConstantsMessage.ASK_BUY_PRICE;
+
 public class lottoInput {
-    
+    public String askPrice() {
+        System.out.println(ASK_BUY_PRICE.getMessage());
+        String input = Console.readLine();
+        return input;
+
+    }
 }

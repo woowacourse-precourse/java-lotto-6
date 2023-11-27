b464e29b9f5a18ef2570ebe3fbd583d00ddfbaf5
jinhyeon <tiemo0708@naver.com>
2023-11-28 02:13:51 +0900

    fix: 필드 선언에 final 추가

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -4,11 +4,13 @@ import lotto.model.LottoNumberMaker;
 import lotto.view.LottoInput;
 
 public class LottoController {
-    private static LottoNumberMaker lottoNumberMaker = new LottoNumberMaker();
-    private static LottoInput lottoInput = new LottoInput();
+    private static final LottoNumberMaker lottoNumberMaker = new LottoNumberMaker();
+    private static final LottoInput lottoInput = new LottoInput();
     public static void setPrice() {
         lottoNumberMaker.checkInt();
     }
+
+
 
     public String askPrice() {
         return lottoInput.askPrice();

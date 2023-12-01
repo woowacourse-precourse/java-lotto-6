fb5f1318f50c4ab4cc9714e45c9c5df4591d0394
Haaae <101785628+Haaae@users.noreply.github.com>
2023-12-01 10:27:46 +0900

    add: 수정

--- src/main/java/lotto/view/Notice.java
+++ src/main/java/lotto/view/Notice.java
@@ -2,7 +2,7 @@ package lotto.view;
 
 public enum Notice {
 
-    NOTICE_PURCHASE_PRICE("구입금액을 입력해 주세요. 구입금액은 1,000원 단위로 입력가능합니다.(e.g. 3000"),
+    NOTICE_PURCHASE_PRICE("구입금액을 입력해 주세요. 구입금액은 1,000원 단위로 입력가능합니다.(e.g. 3000)"),
 
     ;
 

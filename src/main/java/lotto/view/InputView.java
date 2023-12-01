97ad36592547758a1954288fe15ad7833071b4d3
Haaae <101785628+Haaae@users.noreply.github.com>
2023-12-01 11:00:41 +0900

    feat: 당첨숫자, 보너스 숫자 받기

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import lotto.exception.ExceptionCode;
 import lotto.utils.converter.Converter;
 
@@ -20,6 +22,29 @@ public class InputView {
         return Converter.toInt(
                 Console.readLine(),
                 ExceptionCode.NO_EXIST_ENTITY
+        );
+    }
+
+    public List<Integer> readLottoNumbers() {
+        Notice.NOTICE_LOTTO_NUMBERS.print();
+
+        String[] splitInput = Console.readLine()
+                .split(Regex.COMMA.getRegex());
+
+        return Arrays.stream(splitInput)
+                .map(number -> Converter.toInt(
+                        number,
+                        ExceptionCode.INVALID_LOTTO_NUMBER)
+                )
+                .toList();
+    }
+
+    public int readBonusNumber() {
+        Notice.NOTICE_BONUS_NUMBERS.print();
+
+        return Converter.toInt(
+                Console.readLine(),
+                ExceptionCode.INVALID_BONUS_NUMBER
         );
     }
 }

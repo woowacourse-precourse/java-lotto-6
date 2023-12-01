0b14d725edc1b560715eda60d499e8187741dec2
Haaae <101785628+Haaae@users.noreply.github.com>
2023-12-01 11:29:28 +0900

    feat: 결과 통계 포멧

--- src/main/java/lotto/view/Format.java
+++ src/main/java/lotto/view/Format.java
@@ -5,7 +5,14 @@ public enum Format {
 
     PRICE("%s원"),
     PURCHASE_LOTTO("%d개를 구매했습니다."),
+    RESULT_OF_FIRST("6개 일치 (2,000,000,000원) - %d개"),
+    RESULT_OF_SECOND("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    RESULT_OF_THIRD("5개 일치 (1,500,000원) - %d개"),
+    RESULT_OF_FORTH("4개 일치 (50,000원) - %d개"),
+    RESULT_OF_FIFTH("3개 일치 (5,000원) - %d개"),
+    EARNING_RATE("총 수익률은 %s%%입니다."),
     ;
+
 
     private final String format;
 

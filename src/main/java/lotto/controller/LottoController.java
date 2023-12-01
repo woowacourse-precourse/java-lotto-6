ce61c8b48a661b07af6583bf7875e95d6af59817
Haaae <101785628+Haaae@users.noreply.github.com>
2023-12-01 11:01:26 +0900

    feat: 당첨숫자, 보너스 숫자 받아 결과 출력

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,12 @@
 package lotto.controller;
 
+import java.util.List;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
 import lotto.domain.LottoBundle;
 import lotto.exception.ExceptionHandler;
 import lotto.service.LottoService;
+import lotto.service.dto.Result;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -25,6 +29,21 @@ public class LottoController {
         LottoBundle lottoBundle = ExceptionHandler.handle(this::createLottoBundleFromUserInput);
         outputView.printLottoBundle(lottoBundle);
 
+        Lotto winningLotto = ExceptionHandler.handle(this::createWinningLottoFromUserInput);
+        BonusNumber bonusNumber = ExceptionHandler.handle(this::createBonusNumberFromUserInput, winningLotto);
+
+        Result result = lottoService.createResult(lottoBundle, winningLotto, bonusNumber);
+        outputView.printResult(result);
+    }
+
+    private BonusNumber createBonusNumberFromUserInput(final Lotto winningLotto) {
+        int number = inputView.readBonusNumber();
+        return lottoService.createBonusNumber(number, winningLotto);
+    }
+
+    private Lotto createWinningLottoFromUserInput() {
+        List<Integer> numbers = inputView.readLottoNumbers();
+        return lottoService.createLotto(numbers);
     }
 
     private LottoBundle createLottoBundleFromUserInput() {

2f3dc266d73a8a5a47bd5965c4bc258f1a0d79af
Haaae <101785628+Haaae@users.noreply.github.com>
2023-12-01 11:28:24 +0900

    feat: 로또 결과 반환

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,13 @@
 package lotto.service;
 
+import java.util.List;
+import java.util.Map;
+import lotto.domain.BonusNumber;
+import lotto.domain.Lotto;
 import lotto.domain.LottoBundle;
 import lotto.domain.LottoGenerator;
+import lotto.domain.Rank;
+import lotto.service.dto.Result;
 
 public class LottoService {
 
@@ -18,5 +24,36 @@ public class LottoService {
 
     public LottoBundle createLottoBundle(int purchasePrice) {
         return lottoGenerator.generateLottoBundle(purchasePrice);
+    }
+
+    public Lotto createLotto(final List<Integer> numbers) {
+        return lottoGenerator.createLotto(numbers);
+    }
+
+    public BonusNumber createBonusNumber(final int number, final Lotto winningLotto) {
+        return lottoGenerator.createBonusNumber(number, winningLotto);
+    }
+
+    public Result createResult(LottoBundle lottoBundle, Lotto winningLotto,
+            BonusNumber bonusNumber) {
+
+        Map<Rank, Integer> totalRankWithCount = lottoBundle.calculateTotalRank(winningLotto,
+                bonusNumber);
+
+        int totalPrize = totalRankWithCount.keySet()
+                .stream()
+                .mapToInt(rank -> rank.multiple(totalRankWithCount.get(rank)))
+                .sum();
+
+        float earningRate = lottoBundle.calculateEarningRate(totalPrize);
+
+        return new Result(
+                totalRankWithCount.get(Rank.FIRST),
+                totalRankWithCount.get(Rank.SECOND),
+                totalRankWithCount.get(Rank.THIRD),
+                totalRankWithCount.get(Rank.FORTH),
+                totalRankWithCount.get(Rank.FIFTH),
+                earningRate
+        );
     }
 }

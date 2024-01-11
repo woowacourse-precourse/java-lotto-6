96f673705c280981a5f288e9bd50b436479e895d
junseoplee <junseoplee@soongsil.ac.kr>
2024-01-11 15:13:47 +0900

    fix: 불필요한 import 삭제

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,5 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
 import lotto.util.LottoManager;
 
 public class Application {

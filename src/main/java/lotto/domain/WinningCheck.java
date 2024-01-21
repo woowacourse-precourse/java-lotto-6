76224f2ccdfe4ca1da4a2b736a07fdccbb506607
junseoplee <junseoplee@soongsil.ac.kr>
2024-01-21 18:17:38 +0900

    fix: 단위 구분을 위해 '_' 삽입

--- src/main/java/lotto/domain/WinningCheck.java
+++ src/main/java/lotto/domain/WinningCheck.java
@@ -2,11 +2,11 @@ package lotto.domain;
 
 public enum WinningCheck {
   LOSE(0, 0),
-  FIFTH_PRIZE(5000, 3),
-  FOURTH_PRIZE(50000, 4),
-  THIRD_PRIZE(1500000, 5, false),
-  SECOND_PRIZE(30000000, 5, true),
-  FIRST_PRIZE(2000000000, 6);
+  FIFTH_PRIZE(5_000, 3),
+  FOURTH_PRIZE(50_000, 4),
+  THIRD_PRIZE(1_500_000, 5, false),
+  SECOND_PRIZE(30_000_000, 5, true),
+  FIRST_PRIZE(2_000_000_000, 6);
 
   private final int prizeAmount;
   private final int matchingCount;

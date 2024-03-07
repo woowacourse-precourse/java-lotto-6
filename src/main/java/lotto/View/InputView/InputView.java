581f5143098d9e00190f524f451b80a84b154991
hongyongjae <developsvai5096@gmail.com>
2024-03-08 01:42:16 +0900

    camp 추가

--- src/main/java/lotto/View/InputView/InputView.java
+++ src/main/java/lotto/View/InputView/InputView.java
@@ -2,14 +2,13 @@ package lotto.View.InputView;
 
 import lotto.View.GeneralInputOutput.Input;
 import lotto.View.GeneralInputOutput.Output;
-import lotto.util.TypeConverter.UserPromptConverter;
+
 
 import static lotto.Common.ErrorMessage.*;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
-
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
     private final Input GeneralInput = new Input();
     private final Output GeneralOutput = new Output();
@@ -23,19 +22,18 @@ public class InputView {
     }
 
     public void validateEmpty( String amount){
+
         if(amount.isEmpty() ){
             throw new IllegalArgumentException(INPUT_EMPTY.getMessage());
         }
+
     }
 
     public String getLottoWinningNumber() {
         System.out.println();
         GeneralOutput.stringPrintln(WINNING_NUMBER_PROMPT);
         return GeneralInput.stringInput();
-
     }
-
-
 
 
     public String getLottoBonusNumber() {
@@ -49,6 +47,4 @@ public class InputView {
      * 값이 비어 있지 않은지
      * ","러 구분 되어 있는지
      */
-
-
 }
